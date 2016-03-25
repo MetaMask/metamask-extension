72904e7a4f2e33a38d78bc83cc245b4e536b1400
Dan Finlay <dan@danfinlay.com>
2016-03-25 15:38:20 -0700

    Correct the bip44 test account

--- test/unit/idStore-test.js
+++ test/unit/idStore-test.js
@@ -26,13 +26,13 @@ describe('IdentityStore', function() {
     })
 
     describe('#recoverFromSeed', function() {
+      let newAccounts = []
 
       before(function() {
         window.localStorage = {} // Hacking localStorage support into JSDom
-        accounts = []
 
         idStore = new IdentityStore({
-          addAccount(acct) { accounts.push(acct) },
+          addAccount(acct) { newAccounts.push(acct) },
         })
       })
 
@@ -42,7 +42,7 @@ describe('IdentityStore', function() {
           assert.ifError(err)
 
           let newKeystore = idStore._idmgmt.keyStore
-          assert.equal(newKeystore, originalKeystore)
+          assert.equal(newAccounts[0], accounts[0])
           done()
         })
       })
@@ -51,7 +51,8 @@ describe('IdentityStore', function() {
 
   describe('#recoverFromSeed BIP44 compliance', function() {
     let seedWords =  'picnic injury awful upper eagle junk alert toss flower renew silly vague'
-    let firstAccount = '0xaceef0221414801dde7f732196b1c9d8ea60b637'
+    let firstAccount = '0x5d8de92c205279c10e5669f797b853ccef4f739a'
+
     let password = 'secret!'
     let accounts = []
     let idStore
@@ -61,7 +62,6 @@ describe('IdentityStore', function() {
 
       idStore = new IdentityStore({
         addAccount(acct) {
-          console.log(`pushing account ${acct}`)
           accounts.push(acct)
         },
       })
