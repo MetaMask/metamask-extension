da31f4daed76a3457df12f94bbf2974621b70a0c
Dan Finlay <dan@danfinlay.com>
2016-03-24 17:51:46 -0700

    Added basic es6 mocha test suite

---
+++ test/index.js
@@ -0,0 +1,11 @@
+var assert = require('assert');
+var idStore = require('../app/scripts/lib/idStore')
+
+describe('IdentityStore', function() {
+  describe('#_createFirstWallet', function () {
+    it('should return the expected keystore', function () {
+
+      assert.equal(1,1)
+    });
+  });
+});
