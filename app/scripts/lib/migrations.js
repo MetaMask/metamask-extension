ab9db44f4a62a6f7e9200fb5d8034cf967da5e34
Dan Finlay <dan@danfinlay.com>
2016-04-22 13:59:42 -0700

    Add migration to actual migration array, fix test

--- app/scripts/lib/migrations.js
+++ app/scripts/lib/migrations.js
@@ -2,7 +2,9 @@ var path = require('path')
 var fs = require('fs')
 
 var migration2 = require('../migrations/002')
+var migration3 = require('../migrations/003')
 
 module.exports = [
   migration2,
+  migration3,
 ]
--- test/unit/migrations-test.js
+++ test/unit/migrations-test.js
@@ -2,21 +2,26 @@ var assert = require('assert')
 var path = require('path')
 
 var wallet1 = require(path.join('..', 'lib', 'migrations', '001.json'))
+
 var migration2 = require(path.join('..', '..', 'app', 'scripts', 'migrations', '002'))
 var migration3 = require(path.join('..', '..', 'app', 'scripts', 'migrations', '003'))
 
 describe('wallet1 is migrated successfully', function() {
 
-  it('should convert etherscan provider', function(done) {
+  it('should convert providers', function(done) {
+
+    wallet1.data.config.provider = { type: 'etherscan', rpcTarget: null }
 
     var firstResult = migration2.migrate(wallet1.data)
     assert.equal(firstResult.config.provider.type, 'rpc', 'provider should be rpc')
-    assert.equal(firstResult.config.provider.rpcTarget, 'https://rpc.metamask.io/', 'provider should be our rpc')
+    assert.equal(firstResult.config.provider.rpcTarget, 'https://rpc.metamask.io/', 'main provider should be our rpc')
 
     var oldTestRpc = 'https://rawtestrpc.metamask.io/'
+    var newTestRpc = 'https://testrpc.metamask.io/'
     firstResult.config.provider.rpcTarget = oldTestRpc
+
     var secondResult = migration3.migrate(firstResult)
-    assert.equal(firstResult.config.provider.rpcTarget, 'https://testrpc.metamask.io/', 'provider should be our rpc')
+    assert.equal(secondResult.config.provider.rpcTarget, newTestRpc)
 
     done()
   })
