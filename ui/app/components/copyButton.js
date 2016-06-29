78e6ed22befcb09b4d5c5e10862b8bfdf1d5fb1a
Dan Finlay <dan@danfinlay.com>
2016-06-29 15:57:59 -0700

    Add tooltip to copy button

--- ui/app/components/copyButton.js
+++ ui/app/components/copyButton.js
@@ -3,6 +3,8 @@ const h = require('react-hyperscript')
 const inherits = require('util').inherits
 const copyToClipboard = require('copy-to-clipboard')
 
+const Tooltip = require('react-tooltip-component')
+
 module.exports = CopyButton
 
 inherits(CopyButton, Component)
@@ -10,22 +12,51 @@ function CopyButton () {
   Component.call(this)
 }
 
+// As parameters, accepts:
+// "value", which is the value to copy (mandatory)
+// "title", which is the text to show on hover (optional, defaults to 'Copy')
 CopyButton.prototype.render = function () {
   const props = this.props
-  const value = props.value
+  const state = this.state || {}
 
-  return h('i.fa.fa-clipboard.cursor-pointer.color-orange', {
-    title: props.title || 'Copy',
+  const value = props.value
+  const copied = state.copied
+
+  const message = copied ? 'Copied' : props.title || ' Copy '
+
+  return h('.copy-button', {
     style: {
       display: 'flex',
-      justifyContent: 'center',
       alignItems: 'center',
-      margin: '5px',
     },
-    onClick: (event) => {
-      event.preventDefault()
-      event.stopPropagation()
-      copyToClipboard(value)
-    },
-  })
+  }, [
+
+    h(Tooltip, {
+      position: 'top',
+      title: message,
+    }, [
+      h('i.fa.fa-clipboard.cursor-pointer.color-orange', {
+        style: {
+          margin: '5px',
+        },
+        onClick: (event) => {
+          event.preventDefault()
+          event.stopPropagation()
+          copyToClipboard(value)
+          this.debounceRestore()
+        },
+      }),
+    ]),
+
+  ])
+}
+
+CopyButton.prototype.debounceRestore = function() {
+
+  this.setState({ copied: true })
+  clearTimeout(this.timeout)
+  this.timeout = setTimeout(() => {
+    this.setState({ copied: false })
+  }, 850)
+
 }
