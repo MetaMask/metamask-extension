d1545245c29dea3beedb0ea1c8daadf5b2c2d93c
Dan Finlay <dan@danfinlay.com>
2016-06-30 22:53:52 -0700

    Clean up beefy usage
    Now properly uses brfs transform so it can reload on state object changes.

--- development/beefy.js
+++ development/beefy.js
@@ -2,28 +2,18 @@ const beefy = require('beefy')
 const http = require('http')
 const fs = require('fs')
 const path = require('path')
-const states = require('./states')
-
-const statesPath = path.join(__dirname, 'states.js')
-const statesJson = JSON.stringify(states)
-fs.writeFileSync(statesPath, statesJson)
 
 const port = 8124
 
 const handler = beefy({
-    entries: ['mocker.js']
+    entries: {'mocker.js': 'bundle.js'}
   , cwd: __dirname
   , live: true
+  , open: true
   , quiet: false
   , bundlerFlags: ['-t', 'brfs']
 })
 
-console.dir(handler)
 
 http.createServer(handler).listen(port)
 console.log(`Now listening on port ${port}`)
-
-function on404(req, resp) {
-  resp.writeHead(404, {})
-  resp.end('sorry folks!')
-}
