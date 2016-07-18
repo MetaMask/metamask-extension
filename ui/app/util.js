061633bf6725422bfb3f35bf3fdb17b514669357
Aust <AustP@users.noreply.github.com>
2016-07-18 09:54:06 -0600

    Make zero balance display 0. Fixes #449

--- ui/app/util.js
+++ ui/app/util.js
@@ -123,7 +123,11 @@ function generateBalanceObject (formattedBalance, decimalsToKeep = 1) {
   var shortBalance = shortenBalance(balance, decimalsToKeep)
 
   if (beforeDecimal === '0' && afterDecimal.substr(0, 5) === '00000') {
-    balance = '<1.0e-5'
+    if (afterDecimal == 0) {
+      balance = '0'
+    } else {
+      balance = '<1.0e-5'
+    }
   } else if (beforeDecimal !== '0') {
     balance = `${beforeDecimal}.${afterDecimal.slice(0, decimalsToKeep)}`
   }
