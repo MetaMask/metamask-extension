f89d7556a50c2d6150956cc1d19255e216e08d7c
Dan Finlay <dan@danfinlay.com>
2016-07-20 16:11:31 -0700

    Move all chrome references into platform-checking module
    Addresses #453

---
+++ app/scripts/lib/extension.js
@@ -0,0 +1,50 @@
+/* Extension.js
+ *
+ * A module for unifying browser differences in the WebExtension API.
+ *
+ * Initially implemented because Chrome hides all of their WebExtension API
+ * behind a global `chrome` variable, but we'd like to start grooming
+ * the code-base for cross-browser extension support.
+ *
+ * You can read more about the WebExtension API here:
+ * https://developer.mozilla.org/en-US/Add-ons/WebExtensions
+ */
+
+const apis = [
+  'alarms',
+  'bookmarks',
+  'browserAction',
+  'commands',
+  'contextMenus',
+  'cookies',
+  'downloads',
+  'events',
+  'extension',
+  'extensionTypes',
+  'history',
+  'i18n',
+  'idle',
+  'notifications',
+  'pageAction',
+  'runtime',
+  'storage',
+  'tabs',
+  'webNavigation',
+  'webRequest',
+  'windows',
+]
+
+module.exports = new Extension()
+
+function Extension() {
+  const _this = this
+  let global = window
+
+  if (chrome) {
+    global = chrome
+  }
+
+  apis.forEach(function(api) {
+    _this[api] = global[api]
+  })
+}
