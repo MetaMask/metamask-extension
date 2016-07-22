5b4e2ffeaccf105d425d83407b82805c124e2cca
Dan Finlay <dan@danfinlay.com>
2016-07-21 18:08:35 -0700

    Add mock dev mode

---
+++ development/mockExtension.js
@@ -0,0 +1,39 @@
+/* MockExtension
+ *
+ * A module for importing the global extension polyfiller
+ * and stubbing out all the extension methods with appropriate mocks.
+ */
+
+const extension = require('../app/scripts/lib/extension')
+const noop = function () {}
+
+const apis = [
+  'alarms',
+  'bookmarks',
+  'browserAction',
+  'commands',
+  'contextMenus',
+  'cookies',
+  'downloads',
+  'events',
+  'extension',
+  'extensionTypes',
+  'history',
+  'i18n',
+  'idle',
+  'notifications',
+  'pageAction',
+  'runtime',
+  'storage',
+  'tabs',
+  'webNavigation',
+  'webRequest',
+  'windows',
+]
+
+apis.forEach(function (api) {
+  extension[api] = {}
+})
+
+extension.runtime.reload = noop
+extension.tabs.create = noop
