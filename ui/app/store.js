00568443359c8638a80f67d00902b030b01a9dc4
Kevin Serrano <kevgagser@gmail.com>
2016-08-11 14:20:26 -0700

    Take out unused dependency.

--- ui/app/store.js
+++ ui/app/store.js
@@ -3,6 +3,7 @@ const applyMiddleware = require('redux').applyMiddleware
 const thunkMiddleware = require('redux-thunk')
 const rootReducer = require('./reducers')
 const developmentMode = require('../../app/scripts/config').developmentMode
+const createLogger = require('redux-logger')
 
 module.exports = configureStore
 
@@ -11,11 +12,8 @@ const middlewares = [thunkMiddleware]
 console.log(developmentMode)
 if (developmentMode) {
   console.log('Development Mode ON.')
-  const createLogger = require('redux-logger')
   const loggerMiddleware = createLogger()
   middlewares.push(loggerMiddleware)
-} else {
-  console.log('Development Mode OFF.')
 }
 
 
