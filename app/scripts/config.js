44545e0f4703e03bd0fd2f51e3262779116c1178
Dan Finlay <dan@danfinlay.com>
2016-08-31 11:10:25 -0700

    Add metamask route to infura urls
    Fixes #597
    
    Allows metamask-specific RPC metrics.

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,5 +1,5 @@
-const MAINET_RPC_URL = 'https://mainnet.infura.io/'
-const TESTNET_RPC_URL = 'https://morden.infura.io/'
+const MAINET_RPC_URL = 'https://mainnet.infura.io/metamask'
+const TESTNET_RPC_URL = 'https://morden.infura.io/metamask'
 const DEFAULT_RPC_URL = TESTNET_RPC_URL
 
 global.METAMASK_DEBUG = false
