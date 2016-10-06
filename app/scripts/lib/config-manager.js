db068134047f1553b926e9b949cb6a6e7100c7be
Kevin Serrano <kevgagser@gmail.com>
2016-10-06 03:23:47 -0700

    Add new functions for storing TOS hashes in config manager.

--- app/scripts/lib/config-manager.js
+++ app/scripts/lib/config-manager.js
@@ -277,6 +277,17 @@ ConfigManager.prototype.getConfirmed = function () {
   return ('isConfirmed' in data) && data.isConfirmed
 }
 
+ConfigManager.prototype.setTOSHash = function (version) {
+  var data = this.getData()
+  data.TOSHash = version
+  this.setData(data)
+}
+
+ConfigManager.prototype.getTOSHash = function () {
+  var data = this.getData()
+  return ('TOSHash' in data) && data.TOSHash
+}
+
 ConfigManager.prototype.setCurrentFiat = function (currency) {
   var data = this.getData()
   data.fiatCurrency = currency
