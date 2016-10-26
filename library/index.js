5ae45fe17f4e897b67852f18fd8d2153f23d09eb
Frankie <frankie.pangilinan@consensys.net>
2016-10-26 13:43:26 -0700

    Fix the size of the pop-up

--- library/index.js
+++ library/index.js
@@ -4,7 +4,7 @@ const setupProvider = require('./lib/setup-provider.js')
 //
 // setup web3
 //
-
+console.log('hello world im here')
 var provider = setupProvider()
 hijackProvider(provider)
 var web3 = new Web3(provider)
@@ -27,7 +27,7 @@ var shouldPop = false
 window.addEventListener('click', function(){
   if (!shouldPop) return
   shouldPop = false
-  window.open('http://127.0.0.1:9001/popup/popup.html', '', 'width=1000')
+  window.open('http://127.0.0.1:9001/popup/popup.html', '', 'width=360 height=500')
   console.log('opening window...')
 })
 
