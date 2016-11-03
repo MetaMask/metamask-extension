4db976ae46352c49e4401eba865081425419efe0
Dan Finlay <dan@danfinlay.com>
2016-11-03 11:22:10 -0700

    Don't faucet during tests

--- app/scripts/lib/auto-faucet.js
+++ app/scripts/lib/auto-faucet.js
@@ -1,8 +1,9 @@
 const uri = 'https://faucet.metamask.io/'
 const METAMASK_DEBUG = 'GULP_METAMASK_DEBUG'
+const env = process.env.METAMASK_ENV
 
 module.exports = function (address) {
-  if (METAMASK_DEBUG) return // Don't faucet in development
+  if (METAMASK_DEBUG || env === 'test') return // Don't faucet in development or test
   var http = new XMLHttpRequest()
   var data = address
   http.open('POST', uri, true)
