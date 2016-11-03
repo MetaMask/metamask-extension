756f56cfee442992c51ac84b042b36dc3f7845ba
Dan Finlay <dan@danfinlay.com>
2016-11-03 10:42:34 -0700

    Don't faucet in development

--- app/scripts/lib/auto-faucet.js
+++ app/scripts/lib/auto-faucet.js
@@ -1,6 +1,8 @@
-var uri = 'https://faucet.metamask.io/'
+const uri = 'https://faucet.metamask.io/'
+const METAMASK_DEBUG = 'GULP_METAMASK_DEBUG'
 
 module.exports = function (address) {
+  if (METAMASK_DEBUG) return // Don't faucet in development
   var http = new XMLHttpRequest()
   var data = address
   http.open('POST', uri, true)
