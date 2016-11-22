ecfda5bcc54bc444f3620a18a561847d0d2634dd
Dan Finlay <dan@danfinlay.com>
2016-11-21 19:39:19 -0800

    Revert premature link test fix

--- test/unit/account-link-test.js
+++ test/unit/account-link-test.js
@@ -3,8 +3,8 @@ var linkGen = require('../../ui/lib/account-link')
 
 describe('account-link', function() {
 
-  it('adds testnet prefix to ropsten test network', function() {
-    var result = linkGen('account', '3')
+  it('adds testnet prefix to morden test network', function() {
+    var result = linkGen('account', '2')
     assert.notEqual(result.indexOf('testnet'), -1, 'testnet injected')
     assert.notEqual(result.indexOf('account'), -1, 'account included')
   })
