9e764b193517c935fa04d4722357cb48abcfb2a2
Dan Finlay <dan@danfinlay.com>
2016-11-28 17:27:20 -0800

    Fix nodeify

--- app/scripts/lib/nodeify.js
+++ app/scripts/lib/nodeify.js
@@ -1,59 +1,17 @@
-/* NODEIFY
- * Modified from original npm package "nodeify"
- * https://github.com/then/nodeify
- *
- * Removed Promise dependency, to only support
- * native Promises and reduce bundle size.
- */
-
-var isPromise = require('is-promise')
-
-var nextTick
-if (typeof setImmediate === 'function') nextTick = setImmediate
-else if (typeof process === 'object' && process && process.nextTick) nextTick = process.nextTick
-else nextTick = function (cb) { setTimeout(cb, 0) }
-
-module.exports = nodeify
-function nodeify(promise, cb) {
-  if (typeof cb !== 'function') return promise
-  return promise
-    .then(function (res) {
-      nextTick(function () {
-        cb(null, res)
-      })
-    }, function (err) {
-      nextTick(function () {
-        cb(err)
-      })
-    })
-}
-function nodeifyThis(cb) {
-  return nodeify(this, cb)
-}
-
-nodeify.extend = extend
-nodeify.Promise = NodeifyPromise
-
-function extend(prom) {
-  if (prom && isPromise(prom)) {
-    prom.nodeify = nodeifyThis
-    var then = prom.then
-    prom.then = function () {
-      return extend(then.apply(this, arguments))
+module.exports = function (promiseFn) {
+  return function () {
+    var args = []
+    for (var i = 0; i < arguments.length - 1; i++) {
+      args.push(arguments[i])
     }
-    return prom
-  } else if (typeof prom === 'function') {
-    prom.prototype.nodeify = nodeifyThis
+    var cb = arguments[arguments.length - 1]
+
+    return promiseFn.apply(this, args)
+    .then(function (result) {
+      cb(null, result)
+    })
+    .catch(function (reason) {
+      cb(reason)
+    })
   }
 }
-
-function NodeifyPromise(fn) {
-  if (!(this instanceof NodeifyPromise)) {
-    return new NodeifyPromise(fn)
-  }
-  Promise.call(this, fn)
-  extend(this)
-}
-
-NodeifyPromise.prototype = Object.create(Promise.prototype)
-NodeifyPromise.prototype.constructor = NodeifyPromise
---
+++ test/unit/nodeify-test.js
@@ -0,0 +1,22 @@
+const assert = require('assert')
+const nodeify = require('../../app/scripts/lib/nodeify')
+
+describe.only('nodeify', function() {
+
+  var obj = {
+    foo: 'bar',
+    promiseFunc: function (a) {
+      var solution = this.foo + a
+      return Promise.resolve(solution)
+    }
+  }
+
+  it('should retain original context', function(done) {
+    var nodified = nodeify(obj.promiseFunc).bind(obj)
+    nodified('baz', function (err, res) {
+      assert.equal(res, 'barbaz')
+      done()
+    })
+  })
+
+})
