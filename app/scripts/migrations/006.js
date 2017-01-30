7a0acb23337772d3204ed037cf320e02f1e5232a
kumavis <aaron@kumavis.me>
2017-01-30 13:01:54 -0800

    migration #6 - move selectedAccount from keyringController to preferencesController

---
+++ app/scripts/migrations/006.js
@@ -0,0 +1,41 @@
+const version = 6
+
+/*
+
+This migration moves KeyringController.selectedAddress to PreferencesController.selectedAddress
+
+*/
+
+const extend = require('xtend')
+
+module.exports = {
+  version,  
+
+  migrate: function (versionedData) {
+    versionedData.meta.version = version
+    try {
+      const state = versionedData.data
+      const newState = migrateState(state)
+      versionedData.data = newState
+    } catch (err) {
+      console.warn(`MetaMask Migration #${version}` + err.stack)
+    }
+    return Promise.resolve(versionedData)
+  },
+}
+
+function migrateState (state) {
+  const config = state.config
+
+  // add new state
+  const newState = extend(state, {
+    PreferencesController: {
+      selectedAccount: config.selectedAccount,
+    },
+  })
+
+  // rm old state
+  delete newState.KeyringController.selectedAccount
+
+  return newState
+}
