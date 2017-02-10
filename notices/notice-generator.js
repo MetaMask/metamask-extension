1d0fcf129c8bd7174aecccca3acf54ee83215d19
Kevin Serrano <kevgagser@gmail.com>
2017-02-09 17:47:51 -0800

    Fix bugs with notice. Add disclaimer notice.

--- notices/notice-generator.js
+++ notices/notice-generator.js
@@ -4,8 +4,8 @@ var prompt = require('prompt')
 var open = require('open')
 var extend = require('extend')
 var notices = require('./notices.json')
+var id = Number(require('./notice-nonce.json'))
 
-var id = 0
 var date = new Date().toDateString()
 
 var notice = {
@@ -13,24 +13,21 @@ var notice = {
   date: date,
 }
 
-fsp.readdir('notices/archive')
-  .then((files) => {
-    files.forEach(file => { id ++ })
-    Promise.resolve()
-  }).then(() => {
-    fsp.writeFile(`notices/archive/notice_${id}.md`,'Message goes here. Please write out your notice and save before proceeding at the command line.')
-      .then(() =>  {
-        open(`notices/archive/notice_${id}.md`)
-        prompt.start()
-        prompt.get(['title'], (err, result) => {
-          notice.title = result.title
-          fsp.readFile(`notices/archive/notice_${id}.md`)
-            .then((body) => {
-              notice.body = body.toString()
-              notice.id = id
-              notices.push(notice)
-              return fsp.writeFile(`notices/notices.json`, JSON.stringify(notices))
-            })
+fsp.writeFile(`notices/archive/notice_${id}.md`,'Message goes here. Please write out your notice and save before proceeding at the command line.')
+  .then(() =>  {
+    open(`notices/archive/notice_${id}.md`)
+    prompt.start()
+    prompt.get(['title'], (err, result) => {
+      notice.title = result.title
+      fsp.readFile(`notices/archive/notice_${id}.md`)
+        .then((body) => {
+          notice.body = body.toString()
+          notice.id = id
+          notices.push(notice)
+          return fsp.writeFile(`notices/notices.json`, JSON.stringify(notices))
+        }).then((completion) => {
+          id += 1
+          return fsp.writeFile(`notices/notice-nonce.json`, id)
         })
-      })
+    })
   })
