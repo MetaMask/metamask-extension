68b99dfb086c282fa3f2e76fbaf93673e92f097d
kumavis <kumavis@users.noreply.github.com>
2017-03-07 22:28:02 -0800

    test - tx-utils
    add tests for `addGasBuffer`

--- test/unit/tx-utils-test.js
+++ test/unit/tx-utils-test.js
@@ -13,14 +13,40 @@ describe('txUtils', function() {
   })
 
   describe('addGasBuffer', function() {
-    it('multiplies by 1.5', function() {
+    it('multiplies by 1.5, when within block gas limit', function() {
+      // naive estimatedGas: 0x123fad (~1.2 mil)
       const input = '0x123fad'
-      const output = txUtils.addGasBuffer(input, '0x3d4c52') //0x3d4c52 is 4mil for dummy gas limit
-
+      // dummy gas limit: 0x3d4c52 (4 mil)
+      const blockGasLimit = '0x3d4c52'
+      const output = txUtils.addGasBuffer(input, blockGasLimit)
       const inputBn = new BN(ethUtil.stripHexPrefix(input), 'hex')
       const outputBn = new BN(ethUtil.stripHexPrefix(output), 'hex')
       const expectedBn = inputBn.muln(1.5)
       assert(outputBn.eq(expectedBn), 'returns 1.5 the input value')
+    })
+    
+    it('uses original estimatedGas, when above block gas limit', function() {
+      // naive estimatedGas: 0x123fad (~1.2 mil)
+      const input = '0x123fad'
+      // dummy gas limit: 0x0f4240 (1 mil)
+      const blockGasLimit = '0x0f4240'
+      const output = txUtils.addGasBuffer(input, blockGasLimit)
+      const inputBn = new BN(ethUtil.stripHexPrefix(input), 'hex')
+      const outputBn = new BN(ethUtil.stripHexPrefix(output), 'hex')
+      const expectedBn = new BN(ethUtil.stripHexPrefix(input), 'hex')
+      assert(outputBn.eq(expectedBn), 'returns the original estimatedGas value')
+    })
+
+    it('buffers up to block gas limit', function() {
+      // naive estimatedGas: 0x123fad (~1.2 mil)
+      const input = '0x1e8480'
+      // dummy gas limit: 0x1e8480 (2 mil)
+      const blockGasLimit = '0x1e8480'
+      const output = txUtils.addGasBuffer(input, blockGasLimit)
+      const inputBn = new BN(ethUtil.stripHexPrefix(input), 'hex')
+      const outputBn = new BN(ethUtil.stripHexPrefix(output), 'hex')
+      const expectedBn = new BN(ethUtil.stripHexPrefix(blockGasLimit), 'hex')
+      assert(outputBn.eq(expectedBn), 'returns the block gas limit value')
     })
   })
 })
