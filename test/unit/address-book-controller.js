dc2d614da68ca851c3b1c0c108e7c2f22185751c
Kevin Serrano <kevgagser@gmail.com>
2017-03-10 09:52:05 -0800

    Add basic tests.

---
+++ test/unit/address-book-controller.js
@@ -0,0 +1,28 @@
+const assert = require('assert')
+const extend = require('xtend')
+const AddressBookController = require('../../app/scripts/controllers/address-book')
+
+describe('address-book-controller', function() {
+  var addressBookController
+
+  beforeEach(function() {
+    addressBookController = new AddressBookController()
+  })
+
+  describe('addres book management', function () {
+    describe('#_getAddressBook', function () {
+      it('should be empty by default.', function () {
+        assert.equal(addressBookController._getAddressBook().length, 0)
+      })
+    })
+    describe('#setAddressBook', function () {
+      it('should properly set a new address.', function () {
+        addressBookController.setAddressBook('0x01234', 'test')
+        var addressBook = addressBookController._getAddressBook()
+        assert.equal(addressBook.length, 1, 'incorrect address book length.')
+        assert.equal(addressBook[0].address, '0x01234', 'incorrect addresss')
+        assert.equal(addressBook[0].name, 'test', 'incorrect nickname')
+      })
+    })
+  })
+})
