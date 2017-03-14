f6e551ef5f3773cf5637c2ae0e11fdeb5f40645b
frankiebee <frankie.diamond@gmail.com>
2017-03-13 21:14:19 -0700

    Swithch the default network to mainnet

--- app/scripts/first-time-state.js
+++ app/scripts/first-time-state.js
@@ -1,11 +1,14 @@
+// test and development environment variables
+const env = process.env.METAMASK_ENV
+const METAMASK_DEBUG = 'GULP_METAMASK_DEBUG'
+
 //
 // The default state of MetaMask
 //
-
 module.exports = {
   config: {
     provider: {
-      type: 'testnet',
+      type: (METAMASK_DEBUG || env === 'test') ? 'testnet' : 'mainnet',
     },
   },
 }
