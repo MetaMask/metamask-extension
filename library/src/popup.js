12065a10aab05690b45c83648ee26f85f403b864
frankiebee <frankie.diamond@gmail.com>
2017-04-03 11:03:58 +0200

    Make Popup close after approving a transaction

--- library/src/popup.js
+++ library/src/popup.js
@@ -1,28 +1,35 @@
 const injectCss = require('inject-css')
+const SWcontroller = require('client-sw-ready-event/lib/sw-client.js')
+const SwStream = require('sw-stream/lib/sw-stream.js')
 const MetaMaskUiCss = require('../../ui/css')
 const setupIframe = require('./lib/setup-iframe.js')
 const MetamaskInpageProvider = require('../../app/scripts/lib/inpage-provider.js')
-const SWcontroller = require('client-sw-ready-event/lib/sw-client.js')
-const SwStream = require('sw-stream/lib/sw-stream.js')
 const startPopup = require('../../app/scripts/popup-core')
-
 
 var css = MetaMaskUiCss()
 injectCss(css)
+const container = document.getElementById('app-content')
 
 var name = 'popup'
 window.METAMASK_UI_TYPE = name
 
-console.log('outside:open')
-
 const background = new SWcontroller({
   fileName: '/popup/sw-build.js',
 })
+
+// Setup listener for when the service worker is read
 background.on('ready', (readSw) => {
-  let swStream = SwStream({
+  let connectionStream = SwStream({
     serviceWorker: background.controller,
+    context: name,
   })
-  startPopup(swStream)
+  startPopup({container, connectionStream}, (err, store) => {
+    if (err) return displayCriticalError(err)
+    store.subscribe(() => {
+      const state = store.getState()
+      if (state.appState.shouldClose) window.close()
+    })
+  })
 })
 
 background.startWorker()
