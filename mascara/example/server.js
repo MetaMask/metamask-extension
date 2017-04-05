0cfad78f5decbce04a7fa56e8f80e4fcecd12a79
kumavis <aaron@kumavis.me>
2017-04-04 22:45:39 -0700

    mascara - rename things + break out mascara asset server

---
+++ mascara/example/server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const createMetamascaraServer = require('../server/')
+const createBundle = require('../server/util').createBundle
+const serveBundle = require('../server/util').serveBundle
+
+//
+// Iframe Server
+//
+
+const mascaraServer = createMetamascaraServer()
+
+// start the server
+const mascaraPort = 9001
+mascaraServer.listen(mascaraPort)
+console.log(`Mascara service listening on port ${mascaraPort}`)
+
+
+//
+// Dapp Server
+//
+
+const dappServer = express()
+
+// serve dapp bundle
+serveBundle(dappServer, '/app.js', createBundle(require.resolve('./app.js')))
+dappServer.use(express.static(__dirname + '/app/'))
+
+// start the server
+const dappPort = '9002'
+dappServer.listen(dappPort)
+console.log(`Dapp listening on port ${dappPort}`)
--- mascara/src/mascara.js
+++ mascara/src/mascara.js
@@ -1,15 +1,22 @@
 const Web3 = require('web3')
 const setupProvider = require('./lib/setup-provider.js')
 
+const MASACARA_DOMAIN = 'http://localhost:9001'
+
 //
 // setup web3
 //
-var provider = setupProvider()
-hijackProvider(provider)
+
+var provider = setupProvider({
+  mascaraUrl: MASACARA_DOMAIN + '/proxy/',
+})
+instrumentForUserInteractionTriggers(provider)
+
 var web3 = new Web3(provider)
 web3.setProvider = function(){
   console.log('MetaMask - overrode web3.setProvider')
 }
+
 //
 //
 // export web3
@@ -25,12 +32,12 @@ var shouldPop = false
 window.addEventListener('click', function(){
   if (!shouldPop) return
   shouldPop = false
-  window.open('http://localhost:9001/popup/popup.html', '', 'width=360 height=500')
+  window.open(MASACARA_DOMAIN, '', 'width=360 height=500')
   console.log('opening window...')
 })
 
 
-function hijackProvider(provider){
+function instrumentForUserInteractionTriggers(provider){
   var _super = provider.sendAsync.bind(provider)
   provider.sendAsync = function(payload, cb){
     if (payload.method === 'eth_sendTransaction') {
