9b9570fd2be5a7bdaf37a663628147976d8c94b4
kumavis <aaron@kumavis.me>
2017-04-04 18:48:33 -0700

    auto-faucet - only skip faucet on explicit test environment

--- app/scripts/lib/auto-faucet.js
+++ app/scripts/lib/auto-faucet.js
@@ -4,7 +4,7 @@ const env = process.env.METAMASK_ENV
 
 module.exports = function (address) {
   // Don't faucet in development or test
-  if (METAMASK_DEBUG || env === 'test') return
+  if (METAMASK_DEBUG === true || env === 'test') return
   global.log.info('auto-fauceting:', address)
   const data = address
   const headers = new Headers()
