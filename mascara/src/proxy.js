719d0f4ad5ba7da2b807641a2afdc969d74d913c
frankiebee <frankie.diamond@gmail.com>
2017-04-06 18:56:13 +0200

    Ping service worker to keep it from going idle

--- mascara/src/proxy.js
+++ mascara/src/proxy.js
@@ -3,10 +3,18 @@ const SWcontroller = require('client-sw-ready-event/lib/sw-client.js')
 const SwStream = require('sw-stream/lib/sw-stream.js')
 const SetupUntrustedComunication = ('./lib/setup-untrusted-connection.js')
 
+let intervalDelay =  Math.floor(Math.random() * (60000 - 1000)) + 1000
 const background = new SWcontroller({
   fileName: '/background.js',
+  letBeIdle: false,
+  intervalDelay,
 })
-
+window.onfocus = () => {
+  background.sendMessage('wakeUp')
+  .catch((err) => {
+    console.error('problem with wake up', err)
+  })
+}
 const pageStream = new ParentStream()
 background.on('ready', (_) => {
   let swStream = SwStream({
