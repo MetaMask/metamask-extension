24737ded3466da61fd96015beb6931e59d8232a7
Dan Finlay <dan@danfinlay.com>
2017-05-17 14:13:05 -0700

    Fix bug where decimals in gas inputs gave strange results

--- ui/app/components/hex-as-decimal-input.js
+++ ui/app/components/hex-as-decimal-input.js
@@ -139,7 +139,7 @@ HexAsDecimalInput.prototype.constructWarning = function () {
 }
 
 function hexify (decimalString) {
-  const hexBN = new BN(decimalString, 10)
+  const hexBN = new BN(decimalString.split('.')[0], 10)
   return '0x' + hexBN.toString('hex')
 }
 
