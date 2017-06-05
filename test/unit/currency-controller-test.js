f7773538ebb583905b5385096abea48c58e7673c
Kevin Serrano <kevin.serrano@consensys.net>
2017-06-05 16:24:32 -0700

    Modify tests for new api mock.'

--- test/unit/currency-controller-test.js
+++ test/unit/currency-controller-test.js
@@ -36,7 +36,7 @@ describe('currency-controller', function () {
     describe('#updateConversionRate', function () {
       it('should retrieve an update for ETH to USD and set it in memory', function (done) {
         this.timeout(15000)
-        nock('https://www.cryptonator.com')
+        nock('https://api.cryptonator.com')
           .get('/api/ticker/eth-USD')
           .reply(200, '{"ticker":{"base":"ETH","target":"USD","price":"11.02456145","volume":"44948.91745289","change":"-0.01472534"},"timestamp":1472072136,"success":true,"error":""}')
 
@@ -57,7 +57,7 @@ describe('currency-controller', function () {
         this.timeout(15000)
         assert.equal(currencyController.getConversionRate(), 0)
 
-        nock('https://www.cryptonator.com')
+        nock('https://api.cryptonator.com')
           .get('/api/ticker/eth-JPY')
           .reply(200, '{"ticker":{"base":"ETH","target":"JPY","price":"11.02456145","volume":"44948.91745289","change":"-0.01472534"},"timestamp":1472072136,"success":true,"error":""}')
 
