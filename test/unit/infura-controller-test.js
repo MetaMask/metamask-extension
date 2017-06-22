1ddcbaad5bdd554d9711a9a7cdb29e71703325f3
frankiebee <frankie.diamond@gmail.com>
2017-06-22 13:46:08 -0700

    add `done` and stub fetch

--- test/unit/infura-controller-test.js
+++ test/unit/infura-controller-test.js
@@ -1,8 +1,9 @@
 // polyfill fetch
-global.fetch = global.fetch || require('isomorphic-fetch')
-
+global.fetch = global.fetch || function () {return Promise.resolve({
+    json: () => { return Promise.resolve({"mainnet": "ok", "ropsten": "degraded", "kovan": "down", "rinkeby": "ok"}) },
+  })
+}
 const assert = require('assert')
-const nock = require('nock')
 const InfuraController = require('../../app/scripts/controllers/infura')
 
 describe('infura-controller', function () {
@@ -14,20 +15,20 @@ describe('infura-controller', function () {
 
   describe('network status queries', function () {
     describe('#checkInfuraNetworkStatus', function () {
-      it('should return an object reflecting the network statuses', function () {
+      it('should return an object reflecting the network statuses', function (done) {
         this.timeout(15000)
-        nock('https://api.infura.io')
-          .get('/v1/status/metamask')
-          .reply(200, '{"mainnet": "ok", "ropsten": "degraded", "kovan": "down", "rinkeby": "ok"}')
-
         infuraController.checkInfuraNetworkStatus()
           .then(() => {
             const networkStatus = infuraController.store.getState().infuraNetworkStatus
+            const networkStatus2 = infuraController.store.getState()
             assert.equal(Object.keys(networkStatus).length, 4)
             assert.equal(networkStatus.mainnet, 'ok')
             assert.equal(networkStatus.ropsten, 'degraded')
             assert.equal(networkStatus.kovan, 'down')
           })
+          .then(() => done())
+          .catch(done)
+
       })
     })
   })
