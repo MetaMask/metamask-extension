3aff9fdd2a708768e8f4d82ad8756c62a5f3e55c
Dan Finlay <dan@danfinlay.com>
2017-06-27 14:49:41 -0700

    Support other network links

--- ui/lib/explorer-link.js
+++ ui/lib/explorer-link.js
@@ -1,21 +1,6 @@
+const prefixForNetwork = require('./etherscan-prefix-for-network')
+
 module.exports = function (hash, network) {
-  const net = parseInt(network)
-  let prefix
-  switch (net) {
-    case 1: // main net
-      prefix = ''
-      break
-    case 3: // ropsten test net
-      prefix = 'ropsten.'
-      break
-    case 4: // rinkeby test net
-      prefix = 'rinkeby.'
-      break
-    case 42: // kovan test net
-      prefix = 'kovan.'
-      break
-    default:
-      prefix = ''
-  }
+  const prefix = prefixForNetwork(network)
   return `http://${prefix}etherscan.io/tx/${hash}`
 }
