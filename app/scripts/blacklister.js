b63f59d408da95547998e9c37bcfe63f07e860fd
Angello Pozo <Angello.pozo@gmail.com>
2017-07-26 14:54:07 -0700

    added Levenstein logic

--- app/scripts/blacklister.js
+++ app/scripts/blacklister.js
@@ -1,8 +1,12 @@
 const blacklistedDomains = require('etheraddresslookup/blacklists/domains.json')
+const levenshtein = require('fast-levenshtein')
 
 function detectBlacklistedDomain() {
   var strCurrentTab = window.location.hostname
-  if (blacklistedDomains && blacklistedDomains.includes(strCurrentTab)) {
+  var score = levenshtein.get(strCurrentTab, 'myetherwallet')
+  var isBlacklisted = blacklistedDomains && blacklistedDomains.includes(strCurrentTab)
+
+  if (score < 7 || isBlacklisted) {
     window.location.href = 'https://metamask.io/phishing.html'
   }
 }
@@ -10,4 +14,3 @@ function detectBlacklistedDomain() {
 window.addEventListener('load', function() {
   detectBlacklistedDomain()
 })
-
