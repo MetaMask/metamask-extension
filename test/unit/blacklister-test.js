9d345e744d61d94879f3c01a263aa9cf73afa36b
kumavis <aaron@kumavis.me>
2017-08-01 16:58:40 -0700

    blacklist - clearer test format

--- test/unit/blacklister-test.js
+++ test/unit/blacklister-test.js
@@ -5,19 +5,19 @@ describe('blacklister', function () {
   describe('#isPhish', function () {
     it('should not flag whitelisted values', function () {
       var result = isPhish({ hostname: 'www.metamask.io' })
-      assert(!result)
+      assert.equal(result, false)
     })
     it('should flag explicit values', function () {
       var result = isPhish({ hostname: 'metamask.com' })
-      assert(result)
+      assert.equal(result, true)
     })
     it('should flag levenshtein values', function () {
-      var result = isPhish({ hostname: 'metmask.com' })
-      assert(result)
+      var result = isPhish({ hostname: 'metmask.io' })
+      assert.equal(result, true)
     })
     it('should not flag not-even-close values', function () {
       var result = isPhish({ hostname: 'example.com' })
-      assert(!result)
+      assert.equal(result, false)
     })
   })
 })
