a88555b5666479884d79cdbdf712714d8d105e38
frankiebee <frankie.diamond@gmail.com>
2017-08-04 18:25:24 -0400

    abbort if no mascara detected

--- app/scripts/lib/mascara-proxy-provider.js
+++ app/scripts/lib/mascara-proxy-provider.js
@@ -1,24 +1,26 @@
 module.exports = function setupMascaraProxyProvider (extensionProvider) {
   let isMascaraActive = false
-  if (global.mascaraProvider) {
-    let networkVersion
-    global.mascaraProvider.publicConfigStore.subscribe((state) => {
-      if (!global.web3.currentProvider.isMascaraActive) return
-      global.web3.eth.defaultAccount = state.selectedAddress
-      // get the initial network
-      const curentNetVersion = state.networkVersion
-      if (!networkVersion) networkVersion = curentNetVersion
-      if (curentNetVersion !== networkVersion && global.web3._used) {
-        const timeSinceUse = Date.now() - global.web3._used
-        // if web3 was recently used then delay the reloading of the page
-        timeSinceUse > 500 ? triggerReset() : setTimeout(triggerReset, 500)
-        // prevent reentry into if statement if state updates again before
-        // reload
-        networkVersion = curentNetVersion
-      }
 
-    })
-  }
+  if (!global.mascaraProvider) return extensionProvider
+
+  let networkVersion
+  global.mascaraProvider.publicConfigStore.subscribe((state) => {
+    if (!global.web3.currentProvider.isMascaraActive) return
+    global.web3.eth.defaultAccount = state.selectedAddress
+    // get the initial network
+    const curentNetVersion = state.networkVersion
+    if (!networkVersion) networkVersion = curentNetVersion
+    if (curentNetVersion !== networkVersion && global.web3._used) {
+      const timeSinceUse = Date.now() - global.web3._used
+      // if web3 was recently used then delay the reloading of the page
+      timeSinceUse > 500 ? triggerReset() : setTimeout(triggerReset, 500)
+      // prevent reentry into if statement if state updates again before
+      // reload
+      networkVersion = curentNetVersion
+    }
+
+  })
+
   const proxyProvider = new Proxy(extensionProvider, {
     get: (extensionProvider, key) => {
       if (key === 'isMascaraActive') return isMascaraActive
