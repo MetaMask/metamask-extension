77b72ee33f1a8384a673b03969426a94d07914ee
sdtsui <szehungdanieltsui@gmail.com>
2017-08-10 19:35:01 -0700

    Add selectors for txsToRender and conversionRate

--- ui/app/selectors.js
+++ ui/app/selectors.js
@@ -1,7 +1,11 @@
+const valuesFor = require('./util').valuesFor
+
 const selectors = {
   getSelectedAddress,
   getSelectedIdentity,
   getSelectedAccount,
+  conversionRateSelector,
+  transactionsSelector,
 }
 
 module.exports = selectors
@@ -24,4 +28,18 @@ function getSelectedAccount(state) {
   const selectedAddress = getSelectedAddress(state)
 
   return accounts[selectedAddress]
+}
+
+function conversionRateSelector(state) {
+  return state.metamask.conversionRate
+}
+
+function transactionsSelector(state) {
+  const { network } = state.metamask
+  const unapprovedMsgs = valuesFor(state.metamask.unapprovedMsgs)
+  const shapeShiftTxList = (network === '1') ? state.metamask.shapeShiftTxList : undefined
+  const transactions = state.metamask.selectedAddressTxList || []
+  const txsToRender = !shapeShiftTxList ? transactions.concat(unapprovedMsgs) : transactions.concat(unapprovedMsgs, shapeShiftTxList)
+
+  return txsToRender.sort((a, b) => b.time - a.time)
 }
