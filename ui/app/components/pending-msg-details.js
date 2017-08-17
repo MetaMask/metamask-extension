98bedfabf932ff75445769898f8f97cf9f6e7f75
Dan Finlay <dan@danfinlay.com>
2017-08-17 15:35:17 -0700

    Fix eth sign formatting

--- ui/app/components/pending-msg-details.js
+++ ui/app/components/pending-msg-details.js
@@ -38,7 +38,7 @@ PendingMsgDetails.prototype.render = function () {
 
       // message data
       h('.tx-data.flex-column.flex-justify-center.flex-grow.select-none', [
-        h('.flex-row.flex-space-between', [
+        h('.flex-column.flex-space-between', [
           h('label.font-small', 'MESSAGE'),
           h('span.font-small', msgParams.data),
         ]),
