306249e89e84db3d3eab9b454b8ef9daad4ac035
Dan Finlay <dan@danfinlay.com>
2017-08-21 11:39:22 -0700

    Add test for no previous txs

--- test/unit/nonce-tracker-test.js
+++ test/unit/nonce-tracker-test.js
@@ -39,10 +39,6 @@ describe('Nonce Tracker', function () {
         await nonceLock.releaseLock()
       })
 
-      it('should return 0 if there are no previous transactions', async function () {
-
-      })
-
       it('should use localNonce if network returns a nonce lower then a confirmed tx in state', async function () {
         this.timeout(15000)
         providerResultStub.result = '0x1'
@@ -50,7 +46,32 @@ describe('Nonce Tracker', function () {
         assert.equal(nonceLock.nextNonce, '4', 'nonce should be 4')
         await nonceLock.releaseLock()
       })
+    })
 
+    describe('with no previous txs', function () {
+      beforeEach(function () {
+        getPendingTransactions = () => []
+        getConfirmedTransactions = () => []
+        providerResultStub.result = '0x0'
+        provider = {
+          sendAsync: (_, cb) => { cb(undefined, providerResultStub) },
+          _blockTracker: {
+            getCurrentBlock: () => '0x11b568',
+          },
+        }
+        nonceTracker = new NonceTracker({
+          provider,
+          getPendingTransactions,
+          getConfirmedTransactions,
+        })
+      })
+
+      it('should return 0', async function () {
+        this.timeout(15000)
+        const nonceLock = await nonceTracker.getNonceLock('0x7d3517b0d011698406d6e0aed8453f0be2697926')
+        assert.equal(nonceLock.nextNonce, '0', 'nonce should be 0')
+        await nonceLock.releaseLock()
+      })
     })
   })
 })
