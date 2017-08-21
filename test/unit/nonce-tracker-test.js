f13c637b23135dcf9ba1b4fbd82da1422e8ea326
Dan Finlay <dan@danfinlay.com>
2017-08-21 11:37:39 -0700

    Confine mock strategy to describe block

--- test/unit/nonce-tracker-test.js
+++ test/unit/nonce-tracker-test.js
@@ -8,45 +8,49 @@ describe('Nonce Tracker', function () {
   let getConfirmedTransactions, confirmedTxs
   let providerResultStub = {}
 
-  beforeEach(function () {
-    const txGen = new MockTxGen()
-    confirmedTxs = txGen.generate({ status: 'confirmed' }, { count: 3 })
-    pendingTxs = txGen.generate({ status: 'pending' }, { count: 1 })
-
-    getPendingTransactions = () => pendingTxs
-    getConfirmedTransactions = () => confirmedTxs
-    providerResultStub.result = '0x3'
-    provider = {
-      sendAsync: (_, cb) => { cb(undefined, providerResultStub) },
-      _blockTracker: {
-        getCurrentBlock: () => '0x11b568',
-      },
-    }
-    nonceTracker = new NonceTracker({
-      provider,
-      getPendingTransactions,
-      getConfirmedTransactions,
-    })
-  })
-
   describe('#getNonceLock', function () {
-    it('should work', async function () {
-      this.timeout(15000)
-      const nonceLock = await nonceTracker.getNonceLock('0x7d3517b0d011698406d6e0aed8453f0be2697926')
-      assert.equal(nonceLock.nextNonce, '4', 'nonce should be 4')
-      await nonceLock.releaseLock()
-    })
 
-    it('should return 0 if there are no previous transactions', async function () {
+    describe('with 3 confirmed and 1 pending', function () {
+      beforeEach(function () {
+        const txGen = new MockTxGen()
+        confirmedTxs = txGen.generate({ status: 'confirmed' }, { count: 3 })
+        pendingTxs = txGen.generate({ status: 'pending' }, { count: 1 })
 
-    })
+        getPendingTransactions = () => pendingTxs
+        getConfirmedTransactions = () => confirmedTxs
+        providerResultStub.result = '0x3'
+        provider = {
+          sendAsync: (_, cb) => { cb(undefined, providerResultStub) },
+          _blockTracker: {
+            getCurrentBlock: () => '0x11b568',
+          },
+        }
+        nonceTracker = new NonceTracker({
+          provider,
+          getPendingTransactions,
+          getConfirmedTransactions,
+        })
+      })
 
-    it('should use localNonce if network returns a nonce lower then a confirmed tx in state', async function () {
-      this.timeout(15000)
-      providerResultStub.result = '0x1'
-      const nonceLock = await nonceTracker.getNonceLock('0x7d3517b0d011698406d6e0aed8453f0be2697926')
-      assert.equal(nonceLock.nextNonce, '4', 'nonce should be 4')
-      await nonceLock.releaseLock()
+      it('should work', async function () {
+        this.timeout(15000)
+        const nonceLock = await nonceTracker.getNonceLock('0x7d3517b0d011698406d6e0aed8453f0be2697926')
+        assert.equal(nonceLock.nextNonce, '4', 'nonce should be 4')
+        await nonceLock.releaseLock()
+      })
+
+      it('should return 0 if there are no previous transactions', async function () {
+
+      })
+
+      it('should use localNonce if network returns a nonce lower then a confirmed tx in state', async function () {
+        this.timeout(15000)
+        providerResultStub.result = '0x1'
+        const nonceLock = await nonceTracker.getNonceLock('0x7d3517b0d011698406d6e0aed8453f0be2697926')
+        assert.equal(nonceLock.nextNonce, '4', 'nonce should be 4')
+        await nonceLock.releaseLock()
+      })
+
     })
   })
 })
