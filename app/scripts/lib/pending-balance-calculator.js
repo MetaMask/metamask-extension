74c6de7d23c979c091028d8bd599f389e2090bc1
Dan Finlay <dan@danfinlay.com>
2017-09-07 12:45:00 -0700

    Add constructor comment

--- app/scripts/lib/pending-balance-calculator.js
+++ app/scripts/lib/pending-balance-calculator.js
@@ -4,6 +4,11 @@ const normalize = require('eth-sig-util').normalize
 
 class PendingBalanceCalculator {
 
+  // Must be initialized with two functions:
+  // getBalance => Returns a promise of a BN of the current balance in Wei
+  // getPendingTransactions => Returns an array of TxMeta Objects,
+  // which have txParams properties, which include value, gasPrice, and gas,
+  // all in a base=16 hex format.
   constructor ({ getBalance, getPendingTransactions }) {
     this.getPendingTransactions = getPendingTransactions
     this.getNetworkBalance = getBalance
