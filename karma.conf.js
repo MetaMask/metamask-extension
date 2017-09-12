e7bbb7f8f3bb39c40ecc0161f89418e505c69a5e
kumavis <aaron@kumavis.me>
2017-09-11 22:09:52 -0700

    ci - attempt to use new chrome

--- karma.conf.js
+++ karma.conf.js
@@ -48,7 +48,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chromium', 'Firefox'],
+    browsers: ['Chrome', 'Firefox'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
