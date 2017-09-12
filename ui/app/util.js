ef6967325e43de26edc0d99ac5853804648024ae
Kevin Serrano <kevin.serrano@consensys.net>
2017-09-12 12:52:13 -0700

    Check if all lower or upper before doing checksum.

--- ui/app/util.js
+++ ui/app/util.js
@@ -70,7 +70,7 @@ function isValidAddress (address) {
 function isInvalidChecksumAddress (address) {
   var prefixed = ethUtil.addHexPrefix(address)
   if (address === '0x0000000000000000000000000000000000000000') return false
-  return ethUtil.isValidAddress(prefixed) && !ethUtil.isValidChecksumAddress(prefixed)
+  return !isAllOneCase(prefixed) && !ethUtil.isValidChecksumAddress(prefixed) && ethUtil.isValidAddress(prefixed)
 }
 
 function isAllOneCase (address) {
