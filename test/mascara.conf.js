cbff5fd450ac0a55a9ef7b48b0ce540b66dfca6b
kumavis <aaron@kumavis.me>
2017-09-13 16:52:38 -0700

    tests - mascara - move temp build files into dist/mascara

--- test/mascara.conf.js
+++ test/mascara.conf.js
@@ -2,13 +2,13 @@ const getBaseConfig = require('./base.conf.js')
 
 module.exports = function(config) {
   const settings = getBaseConfig(config)
-  settings.files.push('mascara/test/ui-bundle.js')
-  settings.files.push('mascara/test/test-bundle.js')
-  // settings.files.push('test/integration/bundle.js')
-  settings.files.push({ pattern: 'mascara/test/background.js', watched: false, included: false, served: true }),
-  // /background.js
 
-  settings.proxies['/background.js'] = '/base/mascara/test/background.js'
+  // ui and tests
+  settings.files.push('dist/mascara/ui.js')
+  settings.files.push('dist/mascara/tests.js')
+  // service worker background
+  settings.files.push({ pattern: 'dist/mascara/background.js', watched: false, included: false, served: true }),
+  settings.proxies['/background.js'] = '/base/dist/mascara/background.js'
 
   config.set(settings)
 }
