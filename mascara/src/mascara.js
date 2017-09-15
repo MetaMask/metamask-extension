50528bfcae50d2dbd716d9e80b66b678dc518a81
frankiebee <frankie.diamond@gmail.com>
2017-09-15 14:39:05 -0700

    export mascara global

--- mascara/src/mascara.js
+++ mascara/src/mascara.js
@@ -1,5 +1,6 @@
 const Web3 = require('web3')
 const setupProvider = require('./lib/setup-provider.js')
+const setupDappAutoReloadForMetaMask = require('./lib/auto-reload-and-export.js')
 const setupDappAutoReloadForWeb3 = require('../../app/scripts/lib/auto-reload.js')
 const MASCARA_ORIGIN = process.env.MASCARA_ORIGIN || 'http://localhost:9001'
 console.log('MASCARA_ORIGIN:', MASCARA_ORIGIN)
@@ -13,6 +14,8 @@ const provider = setupProvider({
 })
 instrumentForUserInteractionTriggers(provider)
 
+// export metamask
+setupDappAutoReloadForMetaMask(provider, provider.publicConfigStore)
 // export web3
 const web3 = new Web3(global.metamask.createDefaultProvider())
 setupDappAutoReloadForWeb3(web3, provider.publicConfigStore)
