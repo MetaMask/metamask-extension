bfd75107f11995e0636def0e1efa6dd14b3ee8a6
frankiebee <frankie.diamond@gmail.com>
2017-09-19 10:45:32 -0700

    add context to platform to not have X-Metamask-Origin in mascara

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -20,6 +20,7 @@ window.log = log
 log.setDefaultLevel(METAMASK_DEBUG ? 'debug' : 'warn')
 
 const platform = new ExtensionPlatform()
+platform.context = 'extension'
 const notificationManager = new NotificationManager()
 global.METAMASK_NOTIFIER = notificationManager
 
--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -196,7 +196,7 @@ module.exports = class MetamaskController extends EventEmitter {
       },
       // rpc data source
       rpcUrl: this.networkController.getCurrentRpcAddress(),
-      originHttpHeaderKey: 'X-Metamask-Origin',
+      originHttpHeaderKey: this.platform.context === 'extension' ? 'X-Metamask-Origin' : undefined,
       // account mgmt
       getAccounts: (cb) => {
         const isUnlocked = this.keyringController.memStore.getState().isUnlocked
