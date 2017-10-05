bbe893a0d8d759ba750ba78ff8aed0f0876a43ff
Dan <danjm.com@gmail.com>
2017-10-05 13:14:45 -0230

    UI for send screen container without form rows.

--- app/scripts/lib/is-popup-or-notification.js
+++ app/scripts/lib/is-popup-or-notification.js
@@ -1,6 +1,9 @@
 module.exports = function isPopupOrNotification () {
   const url = window.location.href
-  if (url.match(/popup.html$/) || url.match(/home.html$/)) {
+  // if (url.match(/popup.html$/) || url.match(/home.html$/)) {
+  // Below regexes needed for feature toggles (e.g. see line ~340 in ui/app/app.js)
+  // Revert below regexes to above commented out regexes before merge to master
+  if (url.match(/popup.html(?:\?.+)*$/) || url.match(/home.html(?:\?.+)*$/)) {
     return 'popup'
   } else {
     return 'notification'
