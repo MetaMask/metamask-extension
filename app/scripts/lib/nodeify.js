be4f7b33f4f0885f2c0f5f4d537f6e9793f3fa30
kumavis <aaron@kumavis.me>
2017-10-06 12:36:08 -0700

    nodeify - allow callback to be optional

--- app/scripts/lib/nodeify.js
+++ app/scripts/lib/nodeify.js
@@ -1,10 +1,18 @@
 const promiseToCallback = require('promise-to-callback')
+const noop = function(){}
 
 module.exports = function nodeify (fn, context) {
   return function(){
     const args = [].slice.call(arguments)
-    const callback = args.pop()
-    if (typeof callback !== 'function') throw new Error('callback is not a function')
+    const lastArg = args[args.length-1]
+    const lastArgIsCallback = typeof lastArg === 'function'
+    let callback
+    if (lastArgIsCallback) {
+      callback = lastArg
+      args.pop()
+    } else {
+      callback = noop
+    }
     promiseToCallback(fn.apply(context, args))(callback)
   }
 }
