c1eb8f16dc4539b72b8f661c723f6c75532e102a
Daijiro Wachi <daijiro.wachi@gmail.com>
2017-10-21 21:04:24 +0200

    Add lint:fix command to gulp and npm-scripts

--- gulpfile.js
+++ gulpfile.js
@@ -161,6 +161,13 @@ gulp.task('lint', function () {
     .pipe(eslint.failAfterError())
 });
 
+gulp.task('lint:fix', function () {
+  return gulp.src(['app/**/*.js', 'ui/**/*.js', 'mascara/src/*.js', 'mascara/server/*.js', '!node_modules/**', '!dist/firefox/**', '!docs/**', '!app/scripts/chromereload.js', '!mascara/test/jquery-3.1.0.min.js'])
+    .pipe(eslint(Object.assign(fs.readFileSync(path.join(__dirname, '.eslintrc')), {fix: true})))
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+});
+
 /*
 gulp.task('default', ['lint'], function () {
     // This will only run if the lint task is successful...
