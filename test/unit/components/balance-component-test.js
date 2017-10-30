426cabed566858320ae8a51596adda5f0bb2caaa
Thomas Huang <thomas.b.huang@gmail.com>
2017-10-30 06:04:24 -0700

    Update balance-component-test with redux-mock-store and shallow render with enzyme

--- test/unit/components/balance-component-test.js
+++ test/unit/components/balance-component-test.js
@@ -1,18 +1,38 @@
-var assert = require('assert')
-var BalanceComponent = require('../../../ui/app/components/balance-component')
+const React = require('react')
+const assert = require('assert')
+const BalanceComponent = require('../../../ui/app/components/balance-component')
+const configureStore = require('redux-mock-store').default
+const thunk = require('redux-thunk').default
+const { shallow } = require('enzyme')
 
 describe('BalanceComponent', function () {
+  const middlewares = [thunk]
+  const mockStore = configureStore(middlewares)
+  const store = mockStore({
+    metamask: {
+      accounts: {
+        address: '0xfdea65c8e26263f6d9a1b5de9555d2931a33b826',
+        balance: '0x100000',
+        code: '0x',
+        nonce: '0x0',
+      },
+      balanceValue: '0x100000',
+      conversionRate: '307',
+      currentCurrency: 'usd',
+      network: '3',
+    },
+  })
   let balanceComponent
 
   beforeEach(function () {
-    balanceComponent = new BalanceComponent()
+    balanceComponent = shallow(<BalanceComponent store={store} />)
   })
 
   it('shows token balance and convert to fiat value based on conversion rate', function () {
     const formattedBalance = '1.23 ETH'
 
-    const tokenBalance = balanceComponent.getTokenBalance(formattedBalance, false)
-    const fiatDisplayNumber = balanceComponent.getFiatDisplayNumber(formattedBalance, 2)
+    const tokenBalance = balanceComponent.dive().instance().getTokenBalance(formattedBalance, false)
+    const fiatDisplayNumber = balanceComponent.dive().instance().getFiatDisplayNumber(formattedBalance, 2)
 
     assert.equal('1.23 ETH', tokenBalance)
     assert.equal(2.46, fiatDisplayNumber)
@@ -21,8 +41,8 @@ describe('BalanceComponent', function () {
   it('shows only the token balance when conversion rate is not available', function () {
     const formattedBalance = '1.23 ETH'
 
-    const tokenBalance = balanceComponent.getTokenBalance(formattedBalance, false)
-    const fiatDisplayNumber = balanceComponent.getFiatDisplayNumber(formattedBalance, 0)
+    const tokenBalance = balanceComponent.dive().instance().getTokenBalance(formattedBalance, false)
+    const fiatDisplayNumber = balanceComponent.dive().instance().getFiatDisplayNumber(formattedBalance, 0)
 
     assert.equal('1.23 ETH', tokenBalance)
     assert.equal('N/A', fiatDisplayNumber)
