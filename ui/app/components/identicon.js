1b89ceb63aa7d96912eb32c8766ef566479dde41
Jason Clark <jason@longview.tech>
2017-11-25 14:33:42 -0700

    swapped out ethereum-blockies lib for MEW blockies library, tightened up identicon.js code

--- ui/app/components/identicon.js
+++ ui/app/components/identicon.js
@@ -5,9 +5,9 @@ const connect = require('react-redux').connect
 const isNode = require('detect-node')
 const findDOMNode = require('react-dom').findDOMNode
 const jazzicon = require('jazzicon')
-const BlockiesIdenticon = require('./blockies/blockies-component')
 const iconFactoryGen = require('../../lib/icon-factory')
 const iconFactory = iconFactoryGen(jazzicon)
+const { toDataUrl } = require('../../lib/blockies')
 
 module.exports = connect(mapStateToProps)(IdenticonComponent)
 
@@ -31,36 +31,19 @@ IdenticonComponent.prototype.render = function () {
 
   return address
     ? (
-      useBlockie
-        ? h('div', {
-            className: `${className} identicon`,
-            style: {
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              height: diameter,
-              width: diameter,
-              borderRadius: diameter / 2,
-              overflow: 'hidden',
-            },
-          }, [
-            h(BlockiesIdenticon, {
-              seed: address
-            })
-          ])
-        : h('div', {
-          className: `${className} identicon`,
-          key: 'identicon-' + address,
-          style: {
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: diameter,
-            width: diameter,
-            borderRadius: diameter / 2,
-            overflow: 'hidden',
-          },
-        })
+      h('div', {
+        className: `${className} identicon`,
+        key: useBlockie ? 'blockie' : 'identicon-' + address,
+        style: {
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: diameter,
+          width: diameter,
+          borderRadius: diameter / 2,
+          overflow: 'hidden',
+        },
+      })
     )
     : (
       h('img.balance-icon', {
@@ -76,38 +59,54 @@ IdenticonComponent.prototype.render = function () {
 
 IdenticonComponent.prototype.componentDidMount = function () {
   var props = this.props
-  const { address } = props
+  const { address, useBlockie } = props
 
   if (!address) return
 
-  // eslint-disable-next-line react/no-find-dom-node
-  var container = findDOMNode(this)
-
-  var diameter = props.diameter || this.defaultDiameter
   if (!isNode) {
-    var img = iconFactory.iconForAddress(address, diameter)
-    container.appendChild(img)
+    // eslint-disable-next-line react/no-find-dom-node
+    var container = findDOMNode(this)
+
+    if (useBlockie) {
+      _generateBlockie(container, address)
+    } else {
+      const diameter = props.diameter || this.defaultDiameter
+      _generateJazzicon(container, address, diameter)
+    }
   }
 }
 
 IdenticonComponent.prototype.componentDidUpdate = function () {
   var props = this.props
-  const { address } = props
+  const { address, useBlockie } = props
 
   if (!address) return
 
-  // eslint-disable-next-line react/no-find-dom-node
-  var container = findDOMNode(this)
-
-  var children = container.children
-  for (var i = 0; i < children.length; i++) {
-    container.removeChild(children[i])
-  }
-
-  var diameter = props.diameter || this.defaultDiameter
   if (!isNode) {
-    var img = iconFactory.iconForAddress(address, diameter)
-    container.appendChild(img)
+    // eslint-disable-next-line react/no-find-dom-node
+    var container = findDOMNode(this)
+
+    var children = container.children
+    for (var i = 0; i < children.length; i++) {
+      container.removeChild(children[i])
+    }
+
+    if (useBlockie) {
+      _generateBlockie(container, address)
+    } else {
+      const diameter = props.diameter || this.defaultDiameter
+      _generateJazzicon(container, address, diameter)
+    }
   }
 }
 
+function _generateBlockie(container, address) {
+  const img = new Image()
+  img.src = toDataUrl(address)
+  container.appendChild(img)
+}
+
+function _generateJazzicon(container, address, diameter) {
+  const img = iconFactory.iconForAddress(address, diameter)
+  container.appendChild(img)
+}
