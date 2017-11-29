ab35a76a240dc0139761678028aa8c187345ab49
Dan Finlay <dan@danfinlay.com>
2017-11-28 16:56:45 -0800

    Migrate old data to include firstVersion info

---
+++ app/scripts/migrations/020.js
@@ -0,0 +1,40 @@
+const version = 20
+
+/*
+
+This migration sets transactions as failed
+whos nonce is too high
+
+*/
+
+const clone = require('clone')
+
+module.exports = {
+  version,
+
+  migrate: function (originalVersionedData) {
+    const versionedData = clone(originalVersionedData)
+    versionedData.meta.version = version
+    try {
+      const state = versionedData.data
+      const newState = transformState(state)
+      versionedData.data = newState
+    } catch (err) {
+      console.warn(`MetaMask Migration #${version}` + err.stack)
+    }
+    return Promise.resolve(versionedData)
+  },
+}
+
+function transformState (state) {
+  const newState = state
+  if ('metamask' in newState &&
+      !('firstTimeInfo' in newState.metamask)) {
+    newState.metamask.firstTimeInfo = {
+      version: '3.12.0',
+      date: Date.now(),
+    }
+  }
+  return newState
+}
+
