bd5ce9461e5119c13ab8be29db5cdfc57228bac8
Dan <danjm.com@gmail.com>
2017-12-06 20:49:45 -0330

    User is automatically added to new UI if they meet conditions at time of login.

--- ui/app/select-app.js
+++ ui/app/select-app.js
@@ -4,18 +4,44 @@ const connect = require('react-redux').connect
 const h = require('react-hyperscript')
 const App = require('./app')
 const OldApp = require('../../old-ui/app/app')
+const { autoAddToBetaUI } = require('./selectors')
+const { setFeatureFlag } = require('./actions')
 
 function mapStateToProps (state) {
-	return { betaUI: state.metamask.featureFlags.betaUI }
+	return {
+		betaUI: state.metamask.featureFlags.betaUI,
+		autoAdd: autoAddToBetaUI(state),
+		isUnlocked: state.metamask.isUnlocked,
+	}
 }
 
-module.exports = connect(mapStateToProps)(SelectedApp)
+function mapDispatchToProps (dispatch) {
+  return {
+    setFeatureFlagToBeta: () => dispatch(setFeatureFlag('betaUI', true)),
+  }
+}
+module.exports = connect(mapStateToProps, mapDispatchToProps)(SelectedApp)
 
 inherits(SelectedApp, Component)
-function SelectedApp () { Component.call(this) }
+function SelectedApp () {
+	this.state = {
+		autoAdd: false,
+	}
+	Component.call(this)
+}
+
+SelectedApp.prototype.componentWillReceiveProps = function (nextProps) {
+	const { isUnlocked, setFeatureFlagToBeta } = this.props
+
+	if (!isUnlocked && nextProps.isUnlocked && nextProps.autoAdd) {
+		this.setState({ autoAdd: nextProps.autoAdd })
+		setFeatureFlagToBeta()
+	}
+}
 
 SelectedApp.prototype.render = function () {
   const { betaUI } = this.props
+  const { autoAdd } = this.state 
   const Selected = betaUI ? App : OldApp
   return h(Selected)
 }
