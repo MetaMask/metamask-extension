5f39844382fb3106b883efb81f55a6e909f28b01
Bruno Barbieri <brunobar79@gmail.com>
2018-01-31 03:36:04 -0500

    clean up

--- app/scripts/lib/tx-state-manager.js
+++ app/scripts/lib/tx-state-manager.js
@@ -221,10 +221,10 @@ module.exports = class TransactionStateManger extends EventEmitter {
     this._setTxStatus(txId, 'failed')
   }
 
-  wipeTransactions(){
-  	this._saveTxList([]);
+  wipeTransactions () {
+    this._saveTxList([]);
   }
-
+  
 //
 //           PRIVATE METHODS
 //
