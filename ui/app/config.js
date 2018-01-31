86d56419f4ff141eb566075c554ad74f16c66c0e
Dan Finlay <dan@danfinlay.com>
2018-01-31 14:30:58 -0800

    Add explanation of account resetting

--- ui/app/config.js
+++ ui/app/config.js
@@ -150,6 +150,22 @@ ConfigScreen.prototype.render = function () {
               marginTop: '20px',
             },
           }, [
+
+            h('p', {
+              style: {
+                fontFamily: 'Montserrat Light',
+                fontSize: '13px',
+              },
+            }, [
+              'Resetting is for developer use only. ',
+              h('a', {
+                href: 'http://metamask.helpscoutdocs.com/article/36-resetting-an-account',
+                target: '_blank',
+                onClick (event) { this.navigateTo(event.target.href) },
+              }, 'Read more.'),
+            ]),
+            h('br'),
+
             h('button', {
               style: {
                 alignSelf: 'center',
@@ -236,4 +252,8 @@ function currentProviderDisplay (metamaskState) {
     h('span', {style: { fontWeight: 'bold', paddingRight: '10px'}}, title),
     h('span', value),
   ])
+}
+
+ConfigScreen.prototype.navigateTo = function (url) {
+  global.platform.openWindow({ url })
 }
