dc3f3e79ca7319b97255456a5225a266d38a4d6f
frankiebee <frankie.diamond@gmail.com>
2018-02-14 14:37:02 -0800

    fix - hex prefix estimatedGas on txMeta

--- app/scripts/lib/tx-gas-utils.js
+++ app/scripts/lib/tx-gas-utils.js
@@ -4,6 +4,7 @@ const {
   BnMultiplyByFraction,
   bnToHex,
 } = require('./util')
+const addHexPrefix = require('ethereumjs-util').addHexPrefix
 const SIMPLE_GAS_COST = '0x5208' // Hex for 21000, cost of a simple send.
 
 /*
@@ -13,7 +14,7 @@ and used to do things like calculate gas of a tx.
 */
 
 module.exports = class TxGasUtil {
-  
+
   constructor (provider) {
     this.query = new EthQuery(provider)
   }
@@ -68,7 +69,7 @@ module.exports = class TxGasUtil {
   }
 
   setTxGas (txMeta, blockGasLimitHex, estimatedGasHex) {
-    txMeta.estimatedGas = estimatedGasHex
+    txMeta.estimatedGas = addHexPrefix(estimatedGasHex)
     const txParams = txMeta.txParams
 
     // if gasLimit was specified and doesnt OOG,
