651e48dbd6a8bbcf2561d62c93897309143e2a9e
kumavis <aaron@kumavis.me>
2018-03-27 12:37:30 -0700

    sentry - simplify all ethjs errors for better batching

--- app/scripts/lib/setupRaven.js
+++ app/scripts/lib/setupRaven.js
@@ -1,5 +1,6 @@
 const Raven = require('raven-js')
 const METAMASK_DEBUG = 'GULP_METAMASK_DEBUG'
+const extractEthjsErrorMessage = require('./extractEthjsErrorMessage')
 const PROD = 'https://3567c198f8a8412082d32655da2961d0@sentry.io/273505'
 const DEV = 'https://f59f3dd640d2429d9d0e2445a87ea8e1@sentry.io/273496'
 
@@ -21,8 +22,12 @@ function setupRaven(opts) {
   const client = Raven.config(ravenTarget, {
     release,
     transport: function(opts) {
-      // modify report urls
       const report = opts.data
+      // simplify ethjs error messages
+      report.exception.values.forEach(item => {
+        item.value = extractEthjsErrorMessage(item.value)
+      })
+      // modify report urls
       rewriteReportUrls(report)
       // make request normally
       client._makeRequest(opts)
