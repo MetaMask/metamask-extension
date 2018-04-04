457a47bf62272deb257e3935a62e0ed265a49d78
frankiebee <frankie.diamond@gmail.com>
2018-04-04 12:25:51 -0700

    transactions - normalize txParams

--- test/unit/tx-controller-test.js
+++ test/unit/tx-controller-test.js
@@ -216,7 +216,7 @@ describe('Transaction Controller', function () {
         from: '0x1678a085c290ebd122dc42cba69373b5953b831d',
         value: '0x01',
       }
-      txController.txGasUtil.validateTxParams(sample).then(() => {
+      txController._validateTxParams(sample).then(() => {
         done()
       }).catch(done)
     })
@@ -226,13 +226,83 @@ describe('Transaction Controller', function () {
         from: '0x1678a085c290ebd122dc42cba69373b5953b831d',
         value: '-0x01',
       }
-      txController.txGasUtil.validateTxParams(sample)
+      txController._validateTxParams(sample)
       .then(() => done('expected to thrown on negativity values but didn\'t'))
       .catch((err) => {
         assert.ok(err, 'error')
         done()
       })
     })
+  })
+
+  describe('#_normalizeTxParams', () => {
+    it('should normalize txParams', () => {
+      let txParams = {
+        chainId: '0x1',
+        from: 'a7df1beDBF813f57096dF77FCd515f0B3900e402',
+        to: null,
+        data: '68656c6c6f20776f726c64',
+      }
+
+      txController._normalizeTxParams(txParams)
+
+      assert(!txParams.chainId, 'their should be no chainId')
+      assert(!txParams.to, 'their should be no to address if null')
+      assert.equal(txParams.from.slice(0, 2), '0x', 'from should be hexPrefixd')
+      assert.equal(txParams.data.slice(0, 2), '0x', 'data should be hexPrefixd')
+
+      txParams.to = 'a7df1beDBF813f57096dF77FCd515f0B3900e402'
+
+      txController._normalizeTxParams(txParams)
+      assert.equal(txParams.to.slice(0, 2), '0x', 'to should be hexPrefixd')
+
+    })
+  })
+
+  describe('#_validateRecipient', () => {
+    it('removes recipient for txParams with 0x when contract data is provided', function () {
+      const zeroRecipientandDataTxParams = {
+        from: '0x1678a085c290ebd122dc42cba69373b5953b831d',
+        to: '0x',
+        data: 'bytecode',
+      }
+      const sanitizedTxParams = txController._validateRecipient(zeroRecipientandDataTxParams)
+      assert.deepEqual(sanitizedTxParams, { from: '0x1678a085c290ebd122dc42cba69373b5953b831d', data: 'bytecode' }, 'no recipient with 0x')
+    })
+
+    it('should error when recipient is 0x', function () {
+      const zeroRecipientTxParams = {
+        from: '0x1678a085c290ebd122dc42cba69373b5953b831d',
+        to: '0x',
+      }
+      assert.throws(() => { txController._validateRecipient(zeroRecipientTxParams) }, Error, 'Invalid recipient address')
+    })
+  })
+
+
+  describe('#_validateFrom', () => {
+    it('should error when from is not a hex string', function () {
+
+      // where from is undefined
+      const txParams = {}
+      assert.throws(() => { txController._validateFrom(txParams) }, Error, `Invalid from address ${txParams.from} not a string`)
+
+      // where from is array
+      txParams.from = []
+      assert.throws(() => { txController._validateFrom(txParams) }, Error, `Invalid from address ${txParams.from} not a string`)
+
+      // where from is a object
+      txParams.from = {}
+      assert.throws(() => { txController._validateFrom(txParams) }, Error, `Invalid from address ${txParams.from} not a string`)
+
+      // where from is a invalid address
+      txParams.from = 'im going to fail'
+      assert.throws(() => { txController._validateFrom(txParams) }, Error, `Invalid from address`)
+
+      // should run
+      txParams.from ='0x1678a085c290ebd122dc42cba69373b5953b831d'
+      txController._validateFrom(txParams)
+      })
   })
 
   describe('#addTx', function () {
