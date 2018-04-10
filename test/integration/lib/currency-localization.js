285265ce8aba4c1c9685c2dff0e16fab3b2daca7
Dan <danjm.com@gmail.com>
2018-04-09 21:31:27 -0230

    Add basic currency localization test.

---
+++ test/integration/lib/currency-localization.js
@@ -0,0 +1,28 @@
+const reactTriggerChange = require('../../lib/react-trigger-change')
+const {
+  timeout,
+  queryAsync,
+  findAsync,
+} = require('../../lib/util')
+
+QUnit.module('currency localization')
+
+QUnit.test('renders localized currency', (assert) => {
+  const done = assert.async()
+  runCurrencyLocalizationTest(assert).then(done).catch((err) => {
+    assert.notOk(err, `Error was thrown: ${err.stack}`)
+    done()
+  })
+})
+
+async function runCurrencyLocalizationTest(assert, done) {
+  console.log('*** start runCurrencyLocalizationTest')
+  const selectState = await queryAsync($, 'select')
+  selectState.val('currency localization')
+  reactTriggerChange(selectState[0])
+  await timeout(1000)
+  const txView = await queryAsync($, '.tx-view')
+  const heroBalance = await findAsync($(txView), '.hero-balance')
+  const fiatAmount = await findAsync($(heroBalance), '.fiat-amount')
+  assert.equal(fiatAmount[0].textContent, '₱102,707.97')
+}
