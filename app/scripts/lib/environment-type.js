8fb1237d6425655b88d0bca6ef000d7b77939617
Dan <danjm.com@gmail.com>
2018-04-12 13:17:46 -0230

    Documentation for environemnt-type.js

--- app/scripts/lib/environment-type.js
+++ app/scripts/lib/environment-type.js
@@ -1,3 +1,12 @@
+/**
+ * Used to determine the window type through which the app is being viewed.
+ *  - 'popup' refers to the extension opened through the browser app icon (in top right corner in chrome and firefox)
+ *  - 'responsive' refers to the main browser window
+ *  - 'notification' refers to the popup that appears in its own window when taking action outside of metamask
+ *
+ * @returns {string} A single word label that represents the type of window through which the app is being viewed
+ *
+ */
 module.exports = function environmentType () {
   const url = window.location.href
   if (url.match(/popup.html$/)) {
