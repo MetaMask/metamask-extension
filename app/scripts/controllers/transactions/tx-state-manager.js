706647785cee23d177d646c3a06f5dc2a2586feb
frankiebee <frankie.diamond@gmail.com>
2018-04-29 16:33:46 -0700

    log emitter errors

--- app/scripts/controllers/transactions/tx-state-manager.js
+++ app/scripts/controllers/transactions/tx-state-manager.js
@@ -399,13 +399,17 @@ class TransactionStateManager extends EventEmitter {
     const txMeta = this.getTx(txId)
     txMeta.status = status
     setTimeout(() => {
-      this.updateTx(txMeta, `txStateManager: setting status to ${status}`)
-      this.emit(`${txMeta.id}:${status}`, txId)
-      this.emit(`tx:status-update`, txId, status)
-      if (['submitted', 'rejected', 'failed'].includes(status)) {
-        this.emit(`${txMeta.id}:finished`, txMeta)
+      try {
+        this.updateTx(txMeta, `txStateManager: setting status to ${status}`)
+        this.emit(`${txMeta.id}:${status}`, txId)
+        this.emit(`tx:status-update`, txId, status)
+        if (['submitted', 'rejected', 'failed'].includes(status)) {
+          this.emit(`${txMeta.id}:finished`, txMeta)
+        }
+        this.emit('update:badge')
+      } catch (error) {
+        log.error(error)
       }
-      this.emit('update:badge')
     })
   }
 
