88a6b4edc79a27e9d94dc4ec25e9de02153abc5d
MikeCheng1208 <a3804430@hotmail.com>
2018-05-07 18:19:17 +0800

    setTimeout bug fix

--- app/scripts/lib/portalnetwork.js
+++ app/scripts/lib/portalnetwork.js
@@ -3,16 +3,18 @@ const resolver = require('./resolver.js');
 module.exports = function (provider) {
     extension.webRequest.onBeforeRequest.addListener(details => {
         let name = details.url.substring(7, details.url.length - 1);
+        let clearTime = null;
         extension.tabs.getSelected(null, tab => {
             extension.tabs.update(tab.id, { url: "loading.html" });
 
-            setTimeout(() => {
+            clearTime = setTimeout(() => {
                 return extension.tabs.update(tab.id, { url: "404.html" });
             }, 60000);
 
             resolver.resolve(name, provider).then(ipfsHash => {
+                clearTimeout(clearTime);
                 let url = "https://gateway.ipfs.io/ipfs/" + ipfsHash;
-                return fetch(url, {method: "HEAD"}).then(response => response.status).then(statusCode => {
+                return fetch(url, { method: "HEAD" }).then(response => response.status).then(statusCode => {
                     if (statusCode !== 200) return "Local"
                     extension.tabs.update(tab.id, { url: url })
                 })
@@ -23,6 +25,7 @@ module.exports = function (provider) {
                 })
             })
             .catch(err => {
+                clearTimeout(clearTime);
                 let nameWithoutTld = name.substring(0, name.lastIndexOf('.'))
                 let url = err === "no_mainnet" ? "no_mainnet" : "error"
                 extension.tabs.update(tab.id, {url: `${url}.html?name=${name}`})
