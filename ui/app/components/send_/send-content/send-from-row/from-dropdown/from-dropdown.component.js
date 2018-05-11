61d35e7abec6ef138ae08f1ec013da773083609a
Dan <danjm.com@gmail.com>
2018-05-11 13:50:43 -0230

    Unit tests for from-dropdown; split out send-dropdown-list from from-dropdown

--- ui/app/components/send_/send-content/send-from-row/from-dropdown/from-dropdown.component.js
+++ ui/app/components/send_/send-content/send-from-row/from-dropdown/from-dropdown.component.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import AccountListItem from '../../../account-list-item/account-list-item.container'
+import SendDropdownList from '../../send-dropdown-list/send-dropdown-list.component'
 
 export default class FromDropdown extends Component {
 
@@ -13,58 +14,28 @@ export default class FromDropdown extends Component {
     selectedAccount: PropTypes.object,
   };
 
-  renderListItemIcon (icon, color) {
-    return <i className={`fa ${icon} fa-lg`} style={ { color } }/>
-  }
-
-  getListItemIcon (currentAccount, selectedAccount) {
-    return currentAccount.address === selectedAccount.address
-      ? this.renderListItemIcon('fa-check', '#02c9b1')
-      : null
-  }
-
-  renderDropdown () {
+  render () {
     const {
       accounts,
       closeDropdown,
-      onSelect,
-      selectedAccount,
-    } = this.props
-
-    return (<div>
-      <div
-        className="send-v2__from-dropdown__close-area"
-        onClick={() => closeDropdown}
-      />
-      <div className="send-v2__from-dropdown__list">
-        {accounts.map((account, index) => <AccountListItem
-          account={account}
-          className="account-list-item__dropdown"
-          handleClick={() => {
-            onSelect(account)
-            closeDropdown()
-          }}
-          icon={this.getListItemIcon(account, selectedAccount.address)}
-          key={`from-dropdown-account-#${index}`}
-        />)}
-      </div>
-    </div>)
-  }
-
-  render () {
-    const {
       dropdownOpen,
       openDropdown,
       selectedAccount,
+      onSelect,
     } = this.props
 
     return <div className="send-v2__from-dropdown">
       <AccountListItem
         account={selectedAccount}
         handleClick={openDropdown}
-        icon={this.renderListItemIcon('fa-caret-down', '#dedede')}
+        icon={<i className={`fa fa-caret-down fa-lg`} style={ { color: '#dedede' } }/>}
       />
-      {dropdownOpen && this.renderDropdown()},
+      {dropdownOpen && <SendDropdownList
+        accounts={accounts}
+        closeDropdown={closeDropdown}
+        onSelect={onSelect}
+        activeAddress={selectedAccount.address}
+      />},
     </div>
   }
 
