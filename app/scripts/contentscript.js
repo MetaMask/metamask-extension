22753d96fd87b7bef8f30c1f778935e7ae39b492
simon <simon.rood@adyen.com>
2018-05-18 16:11:46 +0200

    Added trailing comma for eslint

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -175,7 +175,7 @@ function blacklistedDomainCheck () {
     'dropbox.com',
     'webbyawards.com',
     'cdn.shopify.com/s/javascripts/tricorder/xtld-read-only-frame.html',
-    'adyen.com'
+    'adyen.com',
   ]
   var currentUrl = window.location.href
   var currentRegex
