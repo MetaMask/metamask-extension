a3ca64d3210077cb1da67cfd105e8e22e978ff49
kumavis <aaron@kumavis.me>
2018-05-21 19:59:21 -0700

    test - e2e - ignore 'development build of Redux' warning

--- test/e2e/metamask.spec.js
+++ test/e2e/metamask.spec.js
@@ -315,8 +315,10 @@ describe('Metamask popup page', function () {
       'Warning: Unknown prop `dataset` on ',
       // Third-party Favicon 404s show up as errors
       'favicon.ico - Failed to load resource: the server responded with a status of 404 (Not Found)',
-      // React Minified build - known issue blocked by test build sys
+      // React Development build - known issue blocked by test build sys
       'Warning: It looks like you\'re using a minified copy of the development build of React.',
+      // Redux Development build - known issue blocked by test build sys
+      'This means that you are running a slower development build of Redux.',
     ]
     const browserLogs = await driver.manage().logs().get('browser')
     const errorEntries = browserLogs.filter(entry => !ignoredLogTypes.includes(entry.level.toString()))
