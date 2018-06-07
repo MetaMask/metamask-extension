275c31855da73299f4e0838d9ecbcc4278b8431c
Jenny Pollack <jennypollack3@gmail.com>
2018-06-06 18:10:05 -0700

    first language - add check for brave browser on getAcceptLanguages

--- app/scripts/lib/get-first-preferred-lang-code.js
+++ app/scripts/lib/get-first-preferred-lang-code.js
@@ -1,8 +1,14 @@
 const extension = require('extensionizer')
 const promisify = require('pify')
 const allLocales = require('../../_locales/index.json')
+const log = require('loglevel')
 
+// as far as i can tell, this is truthy in the case of Brave browser
+// where extension.i18n.getAcceptLanguages throws due to not being implemented
+// Unchecked runtime.lastError while running i18n.getAcceptLanguages: Access to extension API denied.
+// https://stackoverflow.com/questions/28431505/unchecked-runtime-lasterror-when-using-chrome-api
 const isSupported = extension.i18n && extension.i18n.getAcceptLanguages
+
 const getPreferredLocales = isSupported ? promisify(
   extension.i18n.getAcceptLanguages,
   { errorFirst: false }
@@ -18,7 +24,11 @@ const existingLocaleCodes = allLocales.map(locale => locale.code.toLowerCase().r
  *
  */
 async function getFirstPreferredLangCode () {
-  const userPreferredLocaleCodes = await getPreferredLocales()
+  let userPreferredLocaleCodes = await getPreferredLocales()
+  if(!userPreferredLocaleCodes){
+    userPreferredLocaleCodes = []    
+  }
+  log.debug(`user preferredLocaleCodes: ${userPreferredLocaleCodes}`)
   const firstPreferredLangCode = userPreferredLocaleCodes
     .map(code => code.toLowerCase())
     .find(code => existingLocaleCodes.includes(code))
