5a3fee7c25b261bad78589ea6ee1887da6f90e90
Dan <danjm.com@gmail.com>
2018-06-11 10:34:27 -0230

    Add test for importing from private key to from-import-beta-ui.spec.js, and remove non-import related tests from that file.

--- test/e2e/beta/run-all.sh
+++ test/e2e/beta/run-all.sh
@@ -7,4 +7,4 @@ set -o pipefail
 export PATH="$PATH:./node_modules/.bin"
 
 shell-parallel -s 'npm run ganache:start' -x 'sleep 5 && mocha test/e2e/beta/metamask-beta-ui.spec'
-shell-parallel -s 'npm run ganache:start' -x 'sleep 5 && mocha test/e2e/beta/from-import-beta-ui.spec'
+shell-parallel -s 'npm run ganache:start -- -d' -x 'sleep 5 && mocha test/e2e/beta/from-import-beta-ui.spec'
