fb02e71e2245d746661e5db42f2cf8000c82c001
tmashuang <thomas.b.huang@gmail.com>
2018-06-26 18:14:21 -0700

    Remove .only on test

--- ui/app/components/send_/send-content/send-gas-row/gas-fee-display/test/gas-fee-display.component.test.js
+++ ui/app/components/send_/send-content/send-gas-row/gas-fee-display/test/gas-fee-display.component.test.js
@@ -10,7 +10,7 @@ const propsMethodSpies = {
   showCustomizeGasModal: sinon.spy(),
 }
 
-describe.only('SendGasRow Component', function() {
+describe('SendGasRow Component', function() {
   let wrapper
 
   beforeEach(() => {
