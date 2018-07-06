0af1702a3fc2f377e0790513639e8cf65fb30525
Bruno Barbieri <bruno.barbieri@consensys.net>
2018-07-06 15:51:35 -0400

    Allow firefox binary to be cached

--- .circleci/scripts/firefox-download.sh
+++ .circleci/scripts/firefox-download.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
+FIREFOX_BINARY="firefox-58.0.tar.bz2"
+echo "Checking if firefox was already downloaded"
+if [ -e $FIREFOX_BINARY ]
+then
+    echo "$FIREFOX_BINARY found. No need to download"
+else
+    echo "Downloading firefox..."
+    wget "https://ftp.mozilla.org/pub/firefox/releases/58.0/linux-x86_64/en-US/$FIREFOX_BINARY" \
+    && tar xjf "$FIREFOX_BINARY"
+    echo "firefox download complete"
+fi
 
-echo "Downloading firefox..."
-wget https://ftp.mozilla.org/pub/firefox/releases/58.0/linux-x86_64/en-US/firefox-58.0.tar.bz2 \
-&& tar xjf firefox-58.0.tar.bz2
-echo "firefox download complete"
