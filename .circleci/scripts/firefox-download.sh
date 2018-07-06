32813b23a252e93f7ec5aa1ef265df7daf3aa834
Bruno Barbieri <bruno.barbieri@consensys.net>
2018-07-06 16:44:06 -0400

    Update firefox-download.sh

--- .circleci/scripts/firefox-download.sh
+++ .circleci/scripts/firefox-download.sh
@@ -4,9 +4,10 @@ if [ -d "firefox" ]
 then
     echo "Firefox found. No need to download"
 else
-    FIREFOX_BINARY="firefox-61.0.1.tar.bz2"
+    FIREFOX_VERSION="61.0.1"
+    FIREFOX_BINARY="firefox-${!FIREFOX_VERSION}.tar.bz2"
     echo "Downloading firefox..."
-    wget "https://ftp.mozilla.org/pub/firefox/releases/58.0/linux-x86_64/en-US/$FIREFOX_BINARY" \
+    wget "https://ftp.mozilla.org/pub/firefox/releases/$FIREFOX_VERSION/linux-x86_64/en-US/$FIREFOX_BINARY" \
     && tar xjf "$FIREFOX_BINARY"
     echo "firefox download complete"
 fi
