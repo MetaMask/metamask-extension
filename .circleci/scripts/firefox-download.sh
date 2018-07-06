759b5b20ec3b6eb2d33f218c28ab3650f5cc86cd
Bruno Barbieri <bruno.barbieri@consensys.net>
2018-07-06 16:48:17 -0400

    Forgot this is linux bash

--- .circleci/scripts/firefox-download.sh
+++ .circleci/scripts/firefox-download.sh
@@ -5,7 +5,7 @@ then
     echo "Firefox found. No need to download"
 else
     FIREFOX_VERSION="61.0.1"
-    FIREFOX_BINARY="firefox-${!FIREFOX_VERSION}.tar.bz2"
+    FIREFOX_BINARY="firefox-$FIREFOX_VERSION.tar.bz2"
     echo "Downloading firefox..."
     wget "https://ftp.mozilla.org/pub/firefox/releases/$FIREFOX_VERSION/linux-x86_64/en-US/$FIREFOX_BINARY" \
     && tar xjf "$FIREFOX_BINARY"
