d19c42fcaeea933793ed459ab5248b42811a0498
Alexander Tseung <alextsg@gmail.com>
2018-07-14 13:47:07 -0700

    Add fallback when no function found, fix network colors, add fiat values for tokens with contract exchange rates

--- ui/app/components/pages/confirm-approve/confirm-approve.component.js
+++ ui/app/components/pages/confirm-approve/confirm-approve.component.js
@@ -1,29 +1,18 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import ConfirmTransactionBase from '../confirm-transaction-base'
+import ConfirmTokenTransactionBase from '../confirm-token-transaction-base'
 
 export default class ConfirmApprove extends Component {
-  static contextTypes = {
-    t: PropTypes.func,
-  }
-
   static propTypes = {
-    tokenAddress: PropTypes.string,
-    toAddress: PropTypes.string,
-    tokenAmount: PropTypes.string,
-    tokenSymbol: PropTypes.string,
+    tokenAmount: PropTypes.number,
   }
 
   render () {
-    const { toAddress, tokenAddress, tokenAmount, tokenSymbol } = this.props
+    const { tokenAmount } = this.props
 
     return (
-      <ConfirmTransactionBase
-        toAddress={toAddress}
-        identiconAddress={tokenAddress}
-        title={`${tokenAmount} ${tokenSymbol}`}
-        warning={`By approving this action, you grant permission for this contract to spend up to ${tokenAmount} of your ${tokenSymbol}.`}
-        hideSubtitle
+      <ConfirmTokenTransactionBase
+        tokenAmount={tokenAmount}
       />
     )
   }
--- ui/app/components/pages/confirm-approve/confirm-approve.container.js
+++ ui/app/components/pages/confirm-approve/confirm-approve.container.js
@@ -1,27 +1,12 @@
 import { connect } from 'react-redux'
 import ConfirmApprove from './confirm-approve.component'
+import { approveTokenAmountAndToAddressSelector } from '../../../selectors/confirm-transaction'
 
 const mapStateToProps = state => {
-  const { confirmTransaction } = state
-  const {
-    tokenData = {},
-    txData: { txParams: { to: tokenAddress } = {} } = {},
-    tokenProps: { tokenSymbol } = {},
-  } = confirmTransaction
-  const { params = [] } = tokenData
-
-  let toAddress = ''
-  let tokenAmount = ''
-
-  if (params && params.length === 2) {
-    [{ value: toAddress }, { value: tokenAmount }] = params
-  }
+  const { tokenAmount } = approveTokenAmountAndToAddressSelector(state)
 
   return {
-    toAddress,
-    tokenAddress,
     tokenAmount,
-    tokenSymbol,
   }
 }
 
