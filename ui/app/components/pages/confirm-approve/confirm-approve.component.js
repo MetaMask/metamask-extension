e16f5ab54d1e69e59fe4689fde3fa34e61c88974
Alexander Tseung <alextsg@gmail.com>
2018-07-14 23:36:42 -0700

    Fix tests

--- ui/app/components/pages/confirm-approve/confirm-approve.component.js
+++ ui/app/components/pages/confirm-approve/confirm-approve.component.js
@@ -5,14 +5,16 @@ import ConfirmTokenTransactionBase from '../confirm-token-transaction-base'
 export default class ConfirmApprove extends Component {
   static propTypes = {
     tokenAmount: PropTypes.number,
+    tokenSymbol: PropTypes.string,
   }
 
   render () {
-    const { tokenAmount } = this.props
+    const { tokenAmount, tokenSymbol } = this.props
 
     return (
       <ConfirmTokenTransactionBase
         tokenAmount={tokenAmount}
+        warning={`By approving this action, you grant permission for this contract to spend up to ${tokenAmount} of your ${tokenSymbol}.`}
       />
     )
   }
--- ui/app/components/pages/confirm-approve/confirm-approve.container.js
+++ ui/app/components/pages/confirm-approve/confirm-approve.container.js
@@ -3,10 +3,12 @@ import ConfirmApprove from './confirm-approve.component'
 import { approveTokenAmountAndToAddressSelector } from '../../../selectors/confirm-transaction'
 
 const mapStateToProps = state => {
+  const { confirmTransaction: { tokenProps: { tokenSymbol } = {} } } = state
   const { tokenAmount } = approveTokenAmountAndToAddressSelector(state)
 
   return {
     tokenAmount,
+    tokenSymbol,
   }
 }
 
