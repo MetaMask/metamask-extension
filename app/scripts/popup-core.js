d4748123a80f9d70345c363d4702e3d4b58cbddf
Esteban MIno <efmino@uc.cl>
2018-07-18 11:34:08 -0400

    shorten condition line

--- app/scripts/popup-core.js
+++ app/scripts/popup-core.js
@@ -8,8 +8,7 @@ const StreamProvider = require('web3-stream-provider')
 const setupMultiplex = require('./lib/stream-utils.js').setupMultiplex
 const { detect } = require('detect-browser')
 import { 
-  ENVIRONMENT_TYPE_NOTIFICATION,
-  ENVIRONMENT_TYPE_FULLSCREEN,
+  ENVIRONMENT_TYPE_POPUP,
  } from '../../app/scripts/lib/enums'
 
 module.exports = initializePopup
@@ -26,12 +25,13 @@ function initializePopup ({ container, connectionStream }, cb) {
     (cb) => connectToAccountManager(connectionStream, cb),
     (accountManager, cb) => launchMetamaskUi({ container, accountManager }, cb),
   ], cb)
-  // firefox/ubuntu workaround, input password issue
-  // open extension in browser
+  // firefox/ubuntu workaround, input password issue, https://github.com/MetaMask/metamask-extension/issues/4791
+  // open extension in fullscreen
   const browser = detect()
-  if (browser && browser.name === 'firefox' && browser.os === 'Linux' && window.METAMASK_UI_TYPE !== ENVIRONMENT_TYPE_NOTIFICATION &&
-    window.METAMASK_UI_TYPE !== ENVIRONMENT_TYPE_FULLSCREEN) {
-      global.platform.openExtensionInBrowser()
+  const isLinuxFirefox = browser && browser.name === 'firefox' && browser.os === 'Linux'
+  const isPopup = window.METAMASK_UI_TYPE === ENVIRONMENT_TYPE_POPUP
+  if (isLinuxFirefox && isPopup) {
+    global.platform.openExtensionInBrowser()
   }
 }
 
