5681b6fb16fc6d9063acf5cea63742dc297db327
Jenny Pollack <jennypollack3@gmail.com>
2018-07-18 12:55:05 -0400

    lint - typed message tests

--- test/unit/app/typed-message-manager-test.js
+++ test/unit/app/typed-message-manager-test.js
@@ -3,22 +3,21 @@ const TypedMessageManager = require('../../../app/scripts/lib/typed-message-mana
 
 describe('Typed Message Manager', function () {
 
-  let validData = '{\"types\":{\"foo\":[]},\"message\":{},\"primaryType\":\"foo\",\"domain\":{}}'
-  let invalidData = '{\"types\":{\"foo\":[]},\"message\":{},\"primaryType\":\"foo\"}'
-  let address = '0x0d0c7188d9c72b019a5da9bca0d127680c22e659'
+  const validData = '{"types":{"foo":[]},"message":{},"primaryType":"foo","domain":{}}'
+  const invalidData = '{"types":{"foo":[]},"message":{},"primaryType":"foo"}'
+  const address = '0x0d0c7188d9c72b019a5da9bca0d127680c22e659'
   let messageManager
 
-  let validMessage = {
+  const validMessage = {
     'from': address,
     'data': validData,
   }
-  let invalidMessage = 
-  {
+  const invalidMessage = {
     'data': validData,
   }
-  let invalidDataMessage = {
+  const invalidDataMessage = {
     'data': invalidData,
-    'from': address
+    'from': address,
   }
 
   beforeEach(function () {
@@ -28,30 +27,27 @@ describe('Typed Message Manager', function () {
 describe('#testing for typed messages', function () {
 
     it('should not throw in validateParams', function () {
-      messageManager.validateParams(validMessage)            
+      messageManager.validateParams(validMessage)
     })
 
-    it('should not throw and the message should be unapproved', function () {      
+    it('should not throw and the message should be unapproved', function () {
       messageManager.addUnapprovedMessage(validMessage)
       var result = messageManager.messages
       assert.equal(result[0].status, 'unapproved')
     })
 
     it('should throw in validateParams, missing from field', function () {
-      try{
+      try {
         messageManager.validateParams(invalidMessage)
-      }
-      catch (e)
-      {
+      } catch (e) {
         assert.equal(e.message, 'Params must include a from field.')
       }
     })
 
     it('should throw due to missing domain data in params', function () {
-      try{
+      try {
       messageManager.addUnapprovedMessage(invalidDataMessage)
-      }
-      catch(e){
+      } catch (e) {
         assert.equal(e.message, 'Data must conform to EIP-712 schema. See https://git.io/fNtcx.')
       }
     })
