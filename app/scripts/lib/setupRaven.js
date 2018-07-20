b5db1fea07c4d2faca40042d381d0a386c677e66
viktor <baranov.viktor.27@gmail.com>
2018-07-20 14:58:40 +0300

    change disclaimer and terms of use

--- app/scripts/lib/setupRaven.js
+++ app/scripts/lib/setupRaven.js
@@ -1,8 +1,8 @@
 const Raven = require('raven-js')
 const METAMASK_DEBUG = process.env.METAMASK_DEBUG
 const extractEthjsErrorMessage = require('./extractEthjsErrorMessage')
-const PROD = 'https://3567c198f8a8412082d32655da2961d0@sentry.io/273505'
-const DEV = 'https://f59f3dd640d2429d9d0e2445a87ea8e1@sentry.io/273496'
+const PROD = 'https://1cbad94ab42e4d40a17cbfee0c30e1cb@sentry.io/1246874'
+const DEV = 'https://9c6228ffe5604981b910275fbfb3fa33@sentry.io/1246875'
 
 module.exports = setupRaven
 
