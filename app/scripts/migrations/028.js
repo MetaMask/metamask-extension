f5107fa12efcafe2d7e6531399f53c524192e6d6
Esteban MIno <efmino@uc.cl>
2018-07-27 14:56:03 -0400

    migration for preferences controller tokens

---
+++ app/scripts/migrations/028.js
@@ -0,0 +1,37 @@
+// next version number
+const version = 28
+
+/*
+
+normalizes txParams on unconfirmed txs
+
+*/
+const clone = require('clone')
+
+module.exports = {
+  version,
+
+  migrate: async function (originalVersionedData) {
+    const versionedData = clone(originalVersionedData)
+    versionedData.meta.version = version
+    const state = versionedData.data
+    const newState = transformState(state)
+    versionedData.data = newState
+    return versionedData
+  },
+}
+
+function transformState (state) {
+  const newState = state
+
+  if (newState.PreferencesController) {
+    if (newState.PreferencesController.tokens) {
+      const tokens = newState.TransactionController.tokens
+      const selectedAddress = newState.PreferencesController.selectedAddress
+      newState.PreferencesController.tokens = []
+      newState.PreferencesController.addressTokens = {'mainnet': {selectedAddress: tokens}}
+    }
+  }
+
+  return newState
+}
