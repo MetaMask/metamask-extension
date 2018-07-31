e5070dd8474903a9530bf10f658203d631a8f203
Dan Miller <danjm.com@gmail.com>
2018-07-30 23:58:05 -0230

    Adds new gas customization modal container (without content)

--- ui/app/components/pages/confirm-transaction-base/confirm-transaction-base.container.js
+++ ui/app/components/pages/confirm-transaction-base/confirm-transaction-base.container.js
@@ -99,7 +99,7 @@ const mapDispatchToProps = dispatch => {
       return dispatch(showModal({ name: 'TRANSACTION_CONFIRMED', onHide }))
     },
     showCustomizeGasModal: ({ txData, onSubmit, validate }) => {
-      return dispatch(showModal({ name: 'CONFIRM_CUSTOMIZE_GAS', txData, onSubmit, validate }))
+      return dispatch(showModal({ name: 'CUSTOMIZE_GAS', txData, onSubmit, validate }))
     },
     updateGasAndCalculate: ({ gasLimit, gasPrice }) => {
       return dispatch(updateGasAndCalculate({ gasLimit, gasPrice }))
