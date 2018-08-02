63af6b20fead4f023aea5a8ab9b0700adf0962e5
Dan Miller <danjm.com@gmail.com>
2018-08-02 13:32:22 -0230

    Adds the content of the advanced tab - w/o chart or dynamic content - to gas customize modal.

--- ui/app/components/gas-customization/gas-modal-page-container/gas-modal-page-container.container.js
+++ ui/app/components/gas-customization/gas-modal-page-container/gas-modal-page-container.container.js
@@ -1,11 +1,28 @@
 import { connect } from 'react-redux'
 import GasModalPageContainer from './gas-modal-page-container.component'
 import { hideModal } from '../../../actions'
+import {
+  setCustomGasPrice,
+  setCustomGasLimit,
+} from '../../../ducks/custom-gas'
+import {
+  getCustomGasPrice,
+  getCustomGasLimit,
+} from '../../../selectors/custom-gas'
+
+const mapStateToProps = state => {
+  return {
+    customGasPrice: getCustomGasPrice(state),
+    customGasLimit: getCustomGasLimit(state),
+  }
+}
 
 const mapDispatchToProps = dispatch => {
   return {
     hideModal: () => dispatch(hideModal()),
+    updateCustomGasPrice: (newPrice) => dispatch(setCustomGasPrice(newPrice)),
+    updateCustomGasLimit: (newLimit) => dispatch(setCustomGasLimit(newLimit)),
   }
 }
 
-export default connect(null, mapDispatchToProps)(GasModalPageContainer)
+export default connect(mapStateToProps, mapDispatchToProps)(GasModalPageContainer)
