2237fc9df43ace309ede68a0a59a8a9d2a41941d
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2018-08-03 16:59:41 -0300

    Add filter on state updates pushed to UI

--- app/scripts/lib/ComposableObservableStore.js
+++ app/scripts/lib/ComposableObservableStore.js
@@ -44,6 +44,36 @@ class ComposableObservableStore extends ObservableStore {
     }
     return flatState
   }
+
+  /**
+   * Merges all child store state into a single object rather than
+   * returning an object keyed by child store class name
+   * Removes heavy objects that are not needed on UI
+   *
+   * @returns {Object} - Object containing merged child store state
+   */
+  getFilteredFlatState () {
+    let flatState = {}
+    for (const key in this.config) {
+      let nextState
+      if (key === 'RecentBlocksController') {
+        nextState = {}
+      } else if (key === 'TxController') {
+        const state = this.config[key].getState()
+        const txList = state.selectedAddressTxList.map(
+          item => ({...item, history: null, nonceDetails: null})
+        )
+        nextState = {
+          ...state,
+          selectedAddressTxList: txList,
+        }
+      } else {
+        nextState = this.config[key].getState()
+      }
+      flatState = { ...flatState, ...nextState }
+    }
+    return flatState
+  }
 }
 
 module.exports = ComposableObservableStore
