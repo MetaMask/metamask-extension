528d5fea8ba8bf9e70a59dd9212de33f8775addb
Dan Miller <danjm.com@gmail.com>
2018-08-09 13:44:03 -0230

    Adds basic tab content to gas customizer, with styled button group (static, for now).

--- ui/app/components/gas-customization/gas-modal-page-container/tests/gas-modal-page-container-container.test.js
+++ ui/app/components/gas-customization/gas-modal-page-container/tests/gas-modal-page-container-container.test.js
@@ -2,25 +2,44 @@ import assert from 'assert'
 import proxyquire from 'proxyquire'
 import sinon from 'sinon'
 
-// let mapStateToProps
+let mapStateToProps
 let mapDispatchToProps
 
 const actionSpies = {
   hideModal: sinon.spy(),
 }
 
+const customGasActionSpies = {
+  setCustomGasPrice: sinon.spy(),
+  setCustomGasLimit: sinon.spy(),
+}
+
 proxyquire('../gas-modal-page-container.container.js', {
   'react-redux': {
     connect: (ms, md) => {
-      // mapStateToProps = ms
+      mapStateToProps = ms
       mapDispatchToProps = md
       return () => ({})
     },
   },
+  '../../../selectors/custom-gas': {
+    getCustomGasPrice: (s) => `mockGasPrice:${s}`,
+    getCustomGasLimit: (s) => `mockGasLimit:${s}`,
+  },
   '../../../actions': actionSpies,
+  '../../../ducks/custom-gas': customGasActionSpies,
 })
 
 describe('gas-modal-page-container container', () => {
+
+  describe('mapStateToProps()', () => {
+
+    it('should map the correct properties to props', () => {
+      assert.equal(mapStateToProps('mockState').customGasPrice, 'mockGasPrice:mockState')
+      assert.equal(mapStateToProps('mockState').customGasLimit, 'mockGasLimit:mockState')
+    })
+
+  })
 
   describe('mapDispatchToProps()', () => {
     let dispatchSpy
@@ -36,6 +55,22 @@ describe('gas-modal-page-container container', () => {
         mapDispatchToPropsObject.hideModal()
         assert(dispatchSpy.calledOnce)
         assert(actionSpies.hideModal.calledOnce)
+      })
+    })
+
+    describe('updateCustomGasPrice()', () => {
+      it('should dispatch a setCustomGasPrice action', () => {
+        mapDispatchToPropsObject.updateCustomGasPrice()
+        assert(dispatchSpy.calledOnce)
+        assert(customGasActionSpies.setCustomGasPrice.calledOnce)
+      })
+    })
+
+    describe('updateCustomGasLimit()', () => {
+      it('should dispatch a setCustomGasLimit action', () => {
+        mapDispatchToPropsObject.updateCustomGasLimit()
+        assert(dispatchSpy.calledOnce)
+        assert(customGasActionSpies.setCustomGasLimit.calledOnce)
       })
     })
 
