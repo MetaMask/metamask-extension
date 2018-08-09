543207b5af63424c1510438af356906696f5eb66
Eduardo Antuña Díez <eduadiez@gmail.com>
2018-08-09 12:14:09 +0200

    Fixed ENS to IPFS functionality in Firefox
    `tabs.getSelected` is deprecated and it doesn't work in firefox replaced by `tabs.query({active: true}`

--- app/scripts/lib/ipfsContent.js
+++ app/scripts/lib/ipfsContent.js
@@ -5,7 +5,7 @@ module.exports = function (provider) {
     function ipfsContent (details) {
       const name = details.url.substring(7, details.url.length - 1)
       let clearTime = null
-      extension.tabs.getSelected(null, tab => {
+      extension.tabs.query({active: true}, tab => {
           extension.tabs.update(tab.id, { url: 'loading.html' })
 
           clearTime = setTimeout(() => {
