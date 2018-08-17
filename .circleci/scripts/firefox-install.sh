e493efb1239d0c730dbcf23faaf7204cf69bc1d9
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2018-08-17 11:13:47 -0230

    ci: Don't cache Firefox install
    Two important notes:
    
    1. The time it takes to download is negligble compared to e2e test runs
    2. Since we cannot use environment variables in CircleCI cache keys we
       can't cache the download correctly and have it update when we switch
       firefox versions—this isn't the end of the world because of point 1

--- .circleci/scripts/firefox-install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Installing firefox..."
-sudo rm -r /opt/firefox
-sudo mv firefox /opt/firefox61
-sudo mv /usr/bin/firefox /usr/bin/firefox-old
-sudo ln -s /opt/firefox61/firefox /usr/bin/firefox
-echo "Firefox installed."
