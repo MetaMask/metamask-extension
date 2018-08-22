fdff7685492bb40c8b6721b2290d9d5a8cc7ceb2
brunobar79 <brunobar79@gmail.com>
2018-08-22 13:39:43 -0400

    handle onDisconnect

--- app/vendor/trezor/content-script.js
+++ app/vendor/trezor/content-script.js
@@ -6,6 +6,10 @@ let port = chrome.runtime.connect({ name: 'trezor-connect' });
 port.onMessage.addListener(message => {
     window.postMessage(message, window.location.origin);
 });
+port.onDisconnect.addListener(d => {
+    port = null;
+});
+
 /*
 Passing messages from popup to background script
 */
