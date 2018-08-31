d0e060d86f65bc10cc9cf736a0476a49ba8280c5
MikeCheng1208 <a3804430@hotmail.com>
2018-08-31 14:26:16 +0800

    timeout bug fix
    deploy

---
+++ app/scripts/pwloading.js
@@ -0,0 +1,26 @@
+const extension = require('extensionizer')
+function UrlSearch(){
+	var strUrl = location.search;
+	var getPara, ParaVal;
+	var aryPara = [];
+	if (strUrl.indexOf("?") != -1) {
+		var getSearch = strUrl.split("?");
+		getPara = getSearch[1].split("&");
+		for (var i = 0; i < getPara.length; i++) {
+				ParaVal = getPara[i].split("=");
+				aryPara.push(ParaVal[0]);
+				aryPara[ParaVal[0]] = ParaVal[1];
+		}
+	}
+	return aryPara;
+}
+console.log('loading')
+let tabid = UrlSearch()["tabid"];
+if(tabid =='undefined') {
+    extension.tabs.update(tabid, { url: '404.html' })
+}else{
+    setTimeout(() => {
+        extension.tabs.update(tabid, { url: '404.html' })
+    }, 60000);
+}
+
