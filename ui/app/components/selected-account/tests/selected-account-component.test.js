ee1b05d4452971ccc4b9a4d6b113eabb804f4690
chadmg <chad@lastblocklabs.com>
2018-09-13 14:07:40 -0400

    compute and display checksummed hash in selected-account component

---
+++ ui/app/components/selected-account/tests/selected-account-component.test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import assert from 'assert'
+import { render } from 'enzyme'
+import SelectedAccount from '../selected-account.component'
+
+describe('SelectedAccount Component', () => {
+  it('should render checksummed address', () => {
+    const wrapper = render(<SelectedAccount
+      selectedAddress="0x1b82543566f41a7db9a9a75fc933c340ffb55c9d"
+      selectedIdentity={{ name: 'testName' }}
+    />, { context: { t: () => {}}})
+    // Checksummed version of address is displayed
+    assert.equal(wrapper.find('.selected-account__address').text(), '0x1B82...5C9D')
+    assert.equal(wrapper.find('.selected-account__name').text(), 'testName')
+  })
+})
