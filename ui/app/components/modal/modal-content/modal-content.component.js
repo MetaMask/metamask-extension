d0d0103bb52cbc032a3b3ea2a2ff5edbf67b0d19
Alexander Tseung <alextsg@gmail.com>
2018-09-17 09:56:59 -0700

    Add Modal component

---
+++ ui/app/components/modal/modal-content/modal-content.component.js
@@ -0,0 +1,24 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+
+export default class ModalContent extends PureComponent {
+  static propTypes = {
+    title: PropTypes.string,
+    description: PropTypes.string,
+  }
+
+  render () {
+    const { title, description } = this.props
+
+    return (
+      <div className="modal-content">
+        <div className="modal-content__title">
+          { title }
+        </div>
+        <div className="modal-content__description">
+          { description }
+        </div>
+      </div>
+    )
+  }
+}
