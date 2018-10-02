ba39fbeb49c0691885a92e3df70b3df912144531
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2018-10-02 11:27:47 -0230

    Don't open metamask.io after install anymore

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -38,7 +38,6 @@ const {
 const firstTimeState = Object.assign({}, rawFirstTimeState, global.METAMASK_TEST_CONFIG)
 
 const STORAGE_KEY = 'metamask-config'
-const METAMASK_DEBUG = process.env.METAMASK_DEBUG
 
 log.setDefaultLevel(process.env.METAMASK_DEBUG ? 'debug' : 'warn')
 
@@ -462,11 +461,3 @@ function showWatchAssetUi () {
     }
   )
 }
-
-// On first install, open a window to MetaMask website to how-it-works.
-extension.runtime.onInstalled.addListener(function (details) {
-  if ((details.reason === 'install') && (!METAMASK_DEBUG)) {
-    extension.tabs.create({url: 'https://metamask.io/#how-it-works'})
-  }
-})
-
