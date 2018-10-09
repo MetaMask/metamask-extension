8458f881cd59af3fe26f37076348e4bb85f07401
brunobar79 <brunobar79@gmail.com>
2018-10-09 01:14:41 -0400

    clean up script

--- test/e2e/beta/run-drizzle.sh
+++ test/e2e/beta/run-drizzle.sh
@@ -6,21 +6,14 @@ set -o pipefail
 
 export PATH="$PATH:./node_modules/.bin"
 
-#!/usr/bin/env bash
-
-set -e
-set -u
-set -o pipefail
-
-export PATH="$PATH:./node_modules/.bin"
-
 npm run ganache:start -- -b 2 >> /dev/null 2>&1 &
 sleep 5
 cd test/e2e/beta/drizzle-test
-npm install -g truffle
+npm install
 echo "Deploying contracts for Drizzle test..."
 truffle migrate
 BROWSER=none npm start >> /dev/null 2>&1 &
+sleep 10000
 cd ../../../../
 mocha test/e2e/beta/drizzle.spec
 
