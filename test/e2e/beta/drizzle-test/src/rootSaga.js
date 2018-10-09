005ccc729d18422aa865a0b402e48ed3caabe198
brunobar79 <brunobar79@gmail.com>
2018-10-09 11:06:38 -0400

    use truffle unbox

--- test/e2e/beta/drizzle-test/src/rootSaga.js
+++
@@ -1,8 +0,0 @@
-import { all, fork } from 'redux-saga/effects'
-import { drizzleSagas } from 'drizzle'
-
-export default function* root() {
-  yield all(
-    drizzleSagas.map(saga => fork(saga))
-  )
-}
--- test/e2e/beta/run-drizzle.sh
+++ test/e2e/beta/run-drizzle.sh
@@ -8,11 +8,13 @@ export PATH="$PATH:./node_modules/.bin"
 
 npm run ganache:start -- -b 2 >> /dev/null 2>&1 &
 sleep 5
-cd test/e2e/beta/drizzle-test
-npm install
+cd test/e2e/beta/
+rm -rf drizzle-test
+mkdir drizzle-test && cd drizzle-test
+npm install -g truffle
+truffle unbox drizzle
 echo "Deploying contracts for Drizzle test..."
-truffle migrate
+truffle compile && truffle migrate
 BROWSER=none npm start >> /dev/null 2>&1 &
 cd ../../../../
 mocha test/e2e/beta/drizzle.spec
-
