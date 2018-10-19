b85ae55cd59b4f8fe52ec75e1b7c6efe52a02e3f
kumavis <aaron@kumavis.me>
2018-10-19 06:42:53 -0400

    fetch debugger - only append source stack if no stack is present

--- app/scripts/lib/setupFetchDebugging.js
+++ app/scripts/lib/setupFetchDebugging.js
@@ -2,7 +2,7 @@ module.exports = setupFetchDebugging
 
 //
 // This is a utility to help resolve cases where `window.fetch` throws a
-// `TypeError: Failed to Fetch` without any stack or context for the request 
+// `TypeError: Failed to Fetch` without any stack or context for the request
 // https://github.com/getsentry/sentry-javascript/pull/1293
 //
 
@@ -17,9 +17,11 @@ function setupFetchDebugging() {
     try {
       return await originalFetch.call(window, ...args)
     } catch (err) {
-      console.warn('FetchDebugger - fetch encountered an Error', err)
-      console.warn('FetchDebugger - overriding stack to point of original call')
-      err.stack = initialStack
+      if (!err.stack) {
+        console.warn('FetchDebugger - fetch encountered an Error without a stack', err)
+        console.warn('FetchDebugger - overriding stack to point of original call')
+        err.stack = initialStack  
+      }
       throw err
     }
   }
