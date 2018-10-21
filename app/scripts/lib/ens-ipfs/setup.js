db8c17252c5f875478aaf84ba46e221a303e5b08
kumavis <aaron@kumavis.me>
2018-10-21 04:58:19 -0400

    ens-ipfs - support path and search

--- app/scripts/lib/ens-ipfs/setup.js
+++ app/scripts/lib/ens-ipfs/setup.js
@@ -1,3 +1,4 @@
+const urlUtil = require('url')
 const extension = require('extensionizer')
 const resolveEnsToIpfsContentId = require('./resolver.js')
 
@@ -8,7 +9,7 @@ module.exports = setupEnsIpfsResolver
 function setupEnsIpfsResolver({ provider }) {
 
   // install listener
-  const urlPatterns = supportedTopLevelDomains.map(tld => `*://*.${tld}/`)
+  const urlPatterns = supportedTopLevelDomains.map(tld => `*://*.${tld}/*`)
   extension.webRequest.onErrorOccurred.addListener(webRequestDidFail, { urls: urlPatterns })
 
   // return api object
@@ -24,20 +25,23 @@ function setupEnsIpfsResolver({ provider }) {
     // ignore requests that are not associated with tabs
     if (tabId === -1) return
     // parse ens name
-    const name = url.substring(7, url.length - 1)
-    const path = name.split('.')
-    const topLevelDomain = path[path.length - 1]
+    const urlData = urlUtil.parse(url)
+    const name = urlData.hostname
+    const path = urlData.path
+    const search = urlData.search
+    const domainParts = name.split('.')
+    const topLevelDomain = domainParts[domainParts.length - 1]
     // if unsupported TLD, abort
     if (!supportedTopLevelDomains.includes(topLevelDomain)) return
     // otherwise attempt resolve
-    attemptResolve({ name, tabId })
+    attemptResolve({ tabId, name, path, search })
   }
 
-  async function attemptResolve({ tabId, name }) {
+  async function attemptResolve({ tabId, name, path, search }) {
     extension.tabs.update(tabId, { url: `loading.html` })
     try {
       const ipfsContentId = await resolveEnsToIpfsContentId({ provider, name })
-      const url = `https://gateway.ipfs.io/ipfs/${ipfsContentId}`
+      const url = `https://gateway.ipfs.io/ipfs/${ipfsContentId}${path}${search ? search : ''}`
       try {
         // check if ipfs gateway has result
         const response = await fetch(url, { method: 'HEAD' })
