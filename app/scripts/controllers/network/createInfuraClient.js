39ad83ab355d88cc8051b29c618babbd6e190013
kumavis <aaron@kumavis.me>
2018-11-04 21:36:09 -0500

    network - infura - hardcode net_version and eth_chainId

--- app/scripts/controllers/network/createInfuraClient.js
+++ app/scripts/controllers/network/createInfuraClient.js
@@ -1,4 +1,5 @@
 const mergeMiddleware = require('json-rpc-engine/src/mergeMiddleware')
+const createScaffoldMiddleware = require('json-rpc-engine/src/createScaffoldMiddleware')
 const createBlockReRefMiddleware = require('eth-json-rpc-middleware/block-ref')
 const createRetryOnEmptyMiddleware = require('eth-json-rpc-middleware/retryOnEmpty')
 const createBlockCacheMiddleware = require('eth-json-rpc-middleware/block-cache')
@@ -16,6 +17,7 @@ function createInfuraClient ({ network }) {
   const blockTracker = new BlockTracker({ provider: infuraProvider })
 
   const networkMiddleware = mergeMiddleware([
+    createNetworkAndChainIdMiddleware({ network }),
     createBlockCacheMiddleware({ blockTracker }),
     createInflightMiddleware(),
     createBlockReRefMiddleware({ blockTracker, provider: infuraProvider }),
@@ -24,4 +26,31 @@ function createInfuraClient ({ network }) {
     infuraMiddleware,
   ])
   return { networkMiddleware, blockTracker }
+}
+
+function createNetworkAndChainIdMiddleware({ network }) {
+  let eth_chainId
+  let net_version
+
+  switch (network) {
+    case 'mainnet':
+      net_version = '1'
+      eth_chainId = '0x01'
+      break
+    case 'ropsten':
+      net_version = '3'
+      eth_chainId = '0x03'
+      break
+    case 'kovan':
+      net_version = '42'
+      eth_chainId = '0x2a'
+      break
+    default:
+      throw new Error(`createInfuraClient - unknown network "${network}"`)
+  }
+
+  return createScaffoldMiddleware({
+    eth_chainId,
+    net_version,
+  })
 }
