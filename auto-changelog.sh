96f10722208774c903e5d91c72c3b4c1f14d778b
Esteban MIno <efmino@uc.cl>
2018-11-08 18:32:56 -0300

    working on macos

--- auto-changelog.sh
+++ auto-changelog.sh
@@ -17,8 +17,7 @@ while read -r line; do
         BODY=$(echo $line | sed -E 's/(.*):{2}(.*)/\2/') 
     fi
     # add entry to CHANGELOG
-    sed -i'' -e "/## Current Develop Branch/a \
-    - [#$PR]($URL/pull/$PR): $BODY
-    " CHANGELOG.md;
+    sed -i'' -e '/## Current Develop Branch/a\
+- [#'"$PR"']('"$URL"'/pull/'"$PR"'): '"$BODY"''$'\n' CHANGELOG.md;
 done <<< "$LOG"
 echo 'CHANGELOG updated'
