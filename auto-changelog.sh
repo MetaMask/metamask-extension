6dce32fc1ee32cbff4cd272629a7fa2dd7dfd47c
Esteban MIno <efmino@uc.cl>
2018-11-07 23:02:12 -0300

    provide automatic changelog since last tag for linux

---
+++ auto-changelog.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+git fetch --tags
+URL="https://github.com/MetaMask/metamask-extension/pull/"
+LOG=$(git log $(git describe --tags $(git rev-list --tags --max-count=1))..HEAD --pretty="%s::%b"  --reverse --grep="Merge pull request #" --grep="(#");
+while read -r line; do
+    SUBJECT="$(echo $line | sed -E  's/(.*):{2}(.*)/\1/')"
+    PR=$(echo $line | grep -Po '#\d+' | sed "s/#//") 
+    if [ -z "$(echo $line | sed -E  's/(.*):{2}(.*)/\2/')" ]; then
+        MESSAGE=$(echo $SUBJECT | sed "s/(#$PR)//g"); else
+        MESSAGE=$(echo $line | sed -E  's/(.*):{2}(.*)/\2/') 
+    fi
+    sed -i "/## Current Develop Branch/a - [#$PR]($URL$PR): $MESSAGE" CHANGELOG.md;
+done <<< "$LOG"
+echo "CHANGELOG updated"
