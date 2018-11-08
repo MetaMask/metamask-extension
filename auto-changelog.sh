82bbfb540bf0af52b35e4de62d0b2a2c2100582c
Esteban MIno <efmino@uc.cl>
2018-11-08 18:47:18 -0300

    fix url and remove -e fr macos

--- auto-changelog.sh
+++ auto-changelog.sh
@@ -2,8 +2,7 @@
 # update tags
 git fetch --tags
 # get origin
-ORIGIN=$(git config --get remote.origin.url)
-URL=$(echo $ORIGIN | sed -E 's/(.*).git{1}(.*)/\1/')
+URL='https://github.com/MetaMask/metamask-extension'
 # get git logs from last tag until HEAD, pretty by 'subject::body' filtered by grep for PRs made with Github squash merge or Github regular merge
 LOG=$(git log $(git describe --tags $(git rev-list --tags --max-count=1))..HEAD --pretty="%s::%b" --reverse --grep="Merge pull request #" --grep="(#");
 while read -r line; do
@@ -17,7 +16,7 @@ while read -r line; do
         BODY=$(echo $line | sed -E 's/(.*):{2}(.*)/\2/') 
     fi
     # add entry to CHANGELOG
-    sed -i'' -e '/## Current Develop Branch/a\
+    sed -i'' '/## Current Develop Branch/a\
 - [#'"$PR"']('"$URL"'/pull/'"$PR"'): '"$BODY"''$'\n' CHANGELOG.md;
 done <<< "$LOG"
 echo 'CHANGELOG updated'
