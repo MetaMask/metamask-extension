3679c5bc134e9106b8a06270db13331ad9d23f99
Esteban MIno <efmino@uc.cl>
2018-11-08 14:51:07 -0300

    add -i suffix -e for macos

--- auto-changelog.sh
+++ auto-changelog.sh
@@ -8,15 +8,15 @@ URL=$(echo $ORIGIN | sed -E 's/(.*).git{1}(.*)/\1/')
 LOG=$(git log $(git describe --tags $(git rev-list --tags --max-count=1))..HEAD --pretty="%s::%b" --reverse --grep="Merge pull request #" --grep="(#");
 while read -r line; do
     # get git log subject
-    SUBJECT="$(echo $line | sed -E 's/(.*):{2}(.*)/\1/')"
+    SUBJECT=$(echo $line | sed -E 's/(.*):{2}(.*)/\1/')
     # get git log PR id, PR made with Github squash merge or Github regular merge
     PR=$(echo $SUBJECT | sed 's/^.*(#\([^&]*\)).*/\1/' | sed 's/^.*#\([^&]*\) from.*/\1/')
     # if PR made with Github squash merge, subject is the body
-    if [ -z "$(echo $line | sed -E 's/(.*):{2}(.*)/\2/')" ]; then
+    if [ -z $(echo $line | sed -E 's/(.*):{2}(.*)/\2/') ]; then
         BODY=$(echo $SUBJECT | sed "s/(#$PR)//g"); else
         BODY=$(echo $line | sed -E 's/(.*):{2}(.*)/\2/') 
     fi
     # add entry to CHANGELOG
-    sed -i "" "/## Current Develop Branch/a - [#$PR]($URL/pull/$PR): $BODY" CHANGELOG.md;
+    sed -i'' -e "/## Current Develop Branch/a - [#$PR]($URL/pull/$PR): $BODY" CHANGELOG.md;
 done <<< "$LOG"
-echo "CHANGELOG updated"
+echo 'CHANGELOG updated'
