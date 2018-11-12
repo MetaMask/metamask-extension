cf6a91e26eb2efbe4bdaf54222dd25e68e7b0342
Victor Baranov <baranov.viktor.27@gmail.com>
2018-11-12 15:30:37 +0300

    display non-zero fizt value

--- old-ui/app/components/fiat-value.js
+++ old-ui/app/components/fiat-value.js
@@ -1,71 +1,91 @@
-const Component = require('react').Component
-const h = require('react-hyperscript')
-const inherits = require('util').inherits
-const formatBalance = require('../util').formatBalance
+import React, { Component } from 'react'
+import { formatBalance } from '../util'
+
+class FiatValue extends Component {
+  render = () => {
+    const props = this.props
+    let { conversionRate } = props
+    const { currentCurrency, network } = props
+    const isSokol = parseInt(network) === 77
+    const isDai = parseInt(network) === 100
+    if (isSokol) {
+      conversionRate = 0
+    } else if (isDai) {
+      conversionRate = 1
+    }
+    const renderedCurrency = currentCurrency || ''
+
+    const value = formatBalance(props.value, 6, undefined, props.network)
+
+    if (value === 'None') return value
+    var fiatDisplayNumber, fiatTooltipNumber
+    var splitBalance = value.split(' ')
+
+    fiatTooltipNumber = Number(splitBalance[0]) * conversionRate
+    fiatDisplayNumber = fiatTooltipNumber.toFixed(this.countSignificantDecimals(fiatTooltipNumber, 2))
+
+    const valueStyle = props.valueStyle ? props.valueStyle : {
+      width: '100%',
+      textAlign: 'right',
+      fontSize: '14px',
+      color: '#ffffff',
+    }
+
+    const dimStyle = props.dimStyle ? props.dimStyle : {
+      color: '#60db97',
+      marginLeft: '5px',
+      fontSize: '14px',
+    }
+
+    return this.fiatDisplay(fiatDisplayNumber, valueStyle, dimStyle, renderedCurrency.toUpperCase())
+  }
+
+  /**
+   * returns the truncated fiat value with the definite length of significant decimals
+   * @param {float} val The float value to be truncated
+   * @param {number} len The length of significant decimals
+   * returns {number} The length of truncated significant decimals
+  **/
+  countSignificantDecimals = (val, len) => {
+      if (Math.floor(val) === val) {
+        return 0
+      }
+      const decimals = val.toString().split('.')[1]
+      const decimalsArr = decimals.split('')
+      let decimalsLen = decimalsArr.slice(0).reduce((res, val, ind, arr) => {
+        if (Number(val) === 0) {
+          res += 1
+        } else {
+          arr.splice(1) // break reduce function
+        }
+        return res
+      }, 0)
+      decimalsLen += len
+      const valWithSignificantDecimals = `${Math.floor(val)}.${decimalsArr.slice(0, decimalsLen).join('')}`
+      decimalsLen = parseFloat(valWithSignificantDecimals).toString().split('.')[1].length
+      return decimalsLen || 0
+  }
+
+  fiatDisplay = (fiatDisplayNumber, valueStyle, dimStyle, fiatSuffix) => {
+
+    if (fiatDisplayNumber !== 'N/A') {
+      return (
+        <div
+          className='flex-row'
+          style={{
+            alignItems: 'flex-end',
+            lineHeight: '14px',
+            textRendering: 'geometricPrecision',
+          }}
+        >
+          <div className='fiat-val' style={valueStyle}>{fiatDisplayNumber}</div>
+          <div className='fiat-dim' style={dimStyle}>{fiatSuffix}</div>
+        </div>
+      )
+    } else {
+      return <div/>
+    }
+  }
+}
 
 module.exports = FiatValue
-
-inherits(FiatValue, Component)
-function FiatValue () {
-  Component.call(this)
-}
-
-FiatValue.prototype.render = function () {
-  const props = this.props
-  let { conversionRate } = props
-  const { currentCurrency, network } = props
-  const isSokol = parseInt(network) === 77
-  const isDai = parseInt(network) === 100
-  if (isSokol) {
-    conversionRate = 0
-  } else if (isDai) {
-    conversionRate = 1
-  }
-  const renderedCurrency = currentCurrency || ''
-
-  const value = formatBalance(props.value, 6, undefined, props.network)
-
-  if (value === 'None') return value
-  var fiatDisplayNumber, fiatTooltipNumber
-  var splitBalance = value.split(' ')
-
-  fiatTooltipNumber = Number(splitBalance[0]) * conversionRate
-  fiatDisplayNumber = fiatTooltipNumber.toFixed(2)
-
-  const valueStyle = props.valueStyle ? props.valueStyle : {
-    width: '100%',
-    textAlign: 'right',
-    fontSize: '14px',
-    color: '#ffffff',
-  }
-
-  const dimStyle = props.dimStyle ? props.dimStyle : {
-    color: '#60db97',
-    marginLeft: '5px',
-    fontSize: '14px',
-  }
-
-  return fiatDisplay(fiatDisplayNumber, valueStyle, dimStyle, renderedCurrency.toUpperCase())
-}
-
-function fiatDisplay (fiatDisplayNumber, valueStyle, dimStyle, fiatSuffix) {
-
-  if (fiatDisplayNumber !== 'N/A') {
-    return h('.flex-row', {
-      style: {
-        alignItems: 'flex-end',
-        lineHeight: '14px',
-        textRendering: 'geometricPrecision',
-      },
-    }, [
-      h('div', {
-        style: valueStyle,
-      }, fiatDisplayNumber),
-      h('div', {
-        style: dimStyle,
-      }, fiatSuffix),
-    ])
-  } else {
-    return h('div')
-  }
-}
---
+++ test/unit/old-ui/app/components/fiat-value.spec.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import assert from 'assert'
+import FiatValue from '../../../../../old-ui/app/components/fiat-value'
+import expect from 'expect'
+import { configure, shallow } from 'enzyme';
+
+let fiatValue
+
+describe('FiatValue component', () => {
+	beforeEach(function () {
+		fiatValue = new FiatValue()
+	})
+
+	describe('countSignificantDecimals(val, len) function', () => {
+		it('returns correct significant decimals', () => {
+			assert.equal(6, fiatValue.countSignificantDecimals(0.00001232756347, 2))
+			assert.equal(4, fiatValue.countSignificantDecimals(0.00010000003454305430504350, 2))
+			assert.equal(0, fiatValue.countSignificantDecimals(1.0000, 2))
+			assert.equal(0, fiatValue.countSignificantDecimals(2, 2))
+			assert.equal(3, fiatValue.countSignificantDecimals("2.03243", 2))
+		})
+	})
+
+	describe('fiatDisplay(fiatDisplayNumber, valueStyle, dimStyle, fiatSuffix) function', () => {
+		const valueStyle = {
+	      width: '100%',
+	      textAlign: 'right',
+	      fontSize: '14px',
+	      color: '#ffffff',
+	    }
+
+	    const dimStyle = {
+	      color: '#60db97',
+	      marginLeft: '5px',
+	      fontSize: '14px',
+	    }
+
+		it('returns correct object', () => {
+			expect(fiatValue.fiatDisplay('N/A')).toEqual(<div/>)
+			const wrapper = shallow(fiatValue.fiatDisplay('10', valueStyle, dimStyle, 'POA'))
+			expect(wrapper.find('.fiat-val').text()).toEqual('10')
+			expect(wrapper.find('.fiat-dim').text()).toEqual('POA')
+		})
+	})
+})
