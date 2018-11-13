1788d81412cc4c224c2e0a703b484af992605dc6
Paul Bouchon <mail@bitpshr.net>
2018-11-13 14:22:31 -0500

    Remove demo URL and use nativeCurrency when fetching token rates

--- app/scripts/controllers/token-rates.js
+++ app/scripts/controllers/token-rates.js
@@ -14,8 +14,9 @@ class TokenRatesController {
    *
    * @param {Object} [config] - Options to configure controller
    */
-  constructor ({ interval = DEFAULT_INTERVAL, preferences } = {}) {
+  constructor ({ interval = DEFAULT_INTERVAL, currency, preferences } = {}) {
     this.store = new ObservableStore()
+    this.currency = currency
     this.preferences = preferences
     this.interval = interval
   }
@@ -26,11 +27,12 @@ class TokenRatesController {
   async updateExchangeRates () {
     if (!this.isActive) { return }
     const contractExchangeRates = {}
-    const pairs = this._tokens.map(token => `pairs[]=${token.address}/ETH`)
+    const nativeCurrency = this.currency ? this.currency.getState().nativeCurrency.toUpperCase() : 'ETH'
+    const pairs = this._tokens.map(token => `pairs[]=${token.address}/${nativeCurrency}`)
     const query = pairs.join('&')
     if (this._tokens.length > 0) {
       try {
-        const response = await fetch(`https://exchanges.demo.balanc3.net/pie?${query}&autoConversion=true`)
+        const response = await fetch(`https://exchanges.balanc3.net/pie?${query}&autoConversion=true`)
         const { prices = [] } = await response.json()
         prices.forEach(({ pair, price }) => {
           contractExchangeRates[pair.split('/')[0]] = typeof price === 'number' ? price : 0
