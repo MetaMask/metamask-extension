47b6849f31908044943080493f56c00b23d24bb6
WhySoS3rious <sydhelly@hotmail.com>
2018-11-19 13:09:24 +0100

    display actions and params from layer 2 scripts

--- ui/app/components/layer2App-view/layer2App-view.component.js
+++ ui/app/components/layer2App-view/layer2App-view.component.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import Media from 'react-media'
 import MenuBar from '../menu-bar'
 import Button from '../button'
+const h = require('react-hyperscript')
 import { DEPOSIT_LAYER2APP_ROUTE } from '../../routes'
 
 export default class Layer2AppView extends PureComponent {
@@ -12,10 +13,47 @@ export default class Layer2AppView extends PureComponent {
     history: PropTypes.object,
   }
 
+  renderLayer2Buttons () {
+    let elements = []
+    const script = this.props.selectedLayer2AppScript
+    if (!script) return (	<div>       </div>)
+    for (var k = 0; k< script.layer2Abi.actions.length; k++){
+      const index = k
+      elements.push(<Button
+		   key={"button"+k}
+		   type="primary"
+		   className="layer2App-view__button"
+		   onClick={() => {
+		     console.log(script.layer2Abi)
+		     script.layer2Abi.actions[index].call()}
+			   }
+		   >
+		   {script.layer2Abi.actions[index].name}
+		   </Button>)
+      for (var i = 0; i< script.layer2Abi.actions[index].params.length; i++){
+	console.log(script.layer2Abi.actions[index].params[i])
+	elements.push(h('input', {
+	  className: 'customize-gas-input',
+	  value: "a",
+	  placeholder: "temp element",
+	  type: 'number',
+	  onChange: e => {
+	    console.log("changed")
+	  },
+	  min: 0,
+	}))
+	
+      }
+    }
+    return elements
+  }
+
   render () {
     const { t } = this.context
     const { history } = this.props
-    const script = this.props.selectedLayer2AppScript    
+
+    //the deposit layer2app button should probably also be delegated to the script logic
+    //for now it is in the depositLayer2 components
     return (
 	<div className="layer2App-view">
 	<Button
@@ -25,20 +63,7 @@ export default class Layer2AppView extends PureComponent {
         >
         {t("depositLayer2App") }
       </Button>
-	<Button
-      type="primary"
-      className="layer2App-view__button"
-      onClick={() => console.log("action1", script.layer2Abi)}
-        >
-        {t("layer2-action1") }
-      </Button>
-	<Button
-      type="primary"
-      className="layer2App-view__button"
-      onClick={() => console.log("action2", script.layer2Abi.actions, script.layer2Abi.getters)}
-        >
-        {t("layer2-action2") }
-      </Button>
+	{this.renderLayer2Buttons.bind(this)()}
 
       </div>
     )
