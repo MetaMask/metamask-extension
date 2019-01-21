a08b31679f8d3f94a29685fec34ff7ae6afcdb83
Esteban MIno <efmino@uc.cl>
2019-01-21 17:51:53 -0300

    check contact metadata

--- ui/app/components/send/send-content/send-to-row/send-to-row.utils.js
+++ ui/app/components/send/send-content/send-to-row/send-to-row.utils.js
@@ -1,8 +1,11 @@
 const {
   REQUIRED_ERROR,
   INVALID_RECIPIENT_ADDRESS_ERROR,
+  KNOWN_RECIPIENT_ADDRESS_ERROR,
 } = require('../../send.constants')
 const { isValidAddress } = require('../../../../util')
+const ethUtil = require('ethereumjs-util')
+const contractMap = require('eth-contract-metadata')
 
 function getToErrorObject (to, toError = null, hasHexData = false) {
   if (!to) {
@@ -11,6 +14,8 @@ function getToErrorObject (to, toError = null, hasHexData = false) {
     }
   } else if (!isValidAddress(to) && !toError) {
     toError = INVALID_RECIPIENT_ADDRESS_ERROR
+  } else if (ethUtil.toChecksumAddress(to) in contractMap) {
+    toError = KNOWN_RECIPIENT_ADDRESS_ERROR
   }
 
   return { to: toError }
