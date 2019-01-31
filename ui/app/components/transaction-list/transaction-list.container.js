51e64a3c517703e5272e1f1a08d6716bccb58979
Chi Kei Chan <chikeichan@gmail.com>
2019-01-31 02:06:21 -0800

    Remove dead code

--- ui/app/components/transaction-list/transaction-list.container.js
+++ ui/app/components/transaction-list/transaction-list.container.js
@@ -11,29 +11,11 @@ import { selectedTokenSelector } from '../../selectors/tokens'
 import { updateNetworkNonce } from '../../actions'
 
 const mapStateToProps = state => {
-  const selectedAddress = getSelectedAddress(state)
   return {
     completedTransactions: nonceSortedCompletedTransactionsSelector(state),
-    // completedTransactions: nonceSortedCompletedTransactionsSelector({
-    //   ...state,
-    //   metamask: {
-    //     ...state.metamask,
-    //     selectedAddressTxList: (state.metamask.selectedAddressTxList).map((txMeta) => {
-    //       return txMeta.txParams.to === selectedAddress
-    //         ? {
-    //           ...txMeta,
-    //           txParams: {
-    //             ...txMeta.txParams,
-    //             nonce: undefined,
-    //           },
-    //         }
-    //         : txMeta
-    //     }),
-    //   },
-    // }),
     pendingTransactions: nonceSortedPendingTransactionsSelector(state),
     selectedToken: selectedTokenSelector(state),
-    selectedAddress,
+    selectedAddress: getSelectedAddress(state),
     assetImages: getAssetImages(state),
   }
 }
