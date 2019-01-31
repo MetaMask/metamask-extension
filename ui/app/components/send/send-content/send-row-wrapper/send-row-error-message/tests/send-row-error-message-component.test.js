c4ec83986dcaf9272343750dfd135bdfa666221e
r001 <rob@nyar.eu>
2019-01-31 15:40:20 +0100

    fixed: tests for customErrors

--- ui/app/components/send/send-content/send-row-wrapper/send-row-error-message/tests/send-row-error-message-component.test.js
+++ ui/app/components/send/send-content/send-row-wrapper/send-row-error-message/tests/send-row-error-message-component.test.js
@@ -10,6 +10,7 @@ describe('SendRowErrorMessage Component', function () {
     wrapper = shallow(<SendRowErrorMessage
       errors={{ error1: 'abc', error2: 'def' }}
       errorType={'error3'}
+      customErrors={{mockErrorType: 'mockError'}}
     />, { context: { t: str => str + '_t' } })
   })
 
@@ -19,8 +20,29 @@ describe('SendRowErrorMessage Component', function () {
       assert.equal(wrapper.html(), null)
     })
 
+    it('should render null if the passed custom errors do not contain an error of errorType', () => {
+      wrapper.setProps({ errors: {} })
+      assert.equal(wrapper.find('.send-v2__error').length, 0)
+      assert.equal(wrapper.html(), null)
+    })
+
     it('should render an error message if the passed errors contain an error of errorType', () => {
       wrapper.setProps({ errors: { error1: 'abc', error2: 'def', error3: 'xyz' } })
+      assert.equal(wrapper.find('.send-v2__error').length, 1)
+      assert.equal(wrapper.find('.send-v2__error').text(), 'xyz_t')
+    })
+
+    it('if both errors and custom errors have the same error type then the error in errors should be displayed', () => {
+      wrapper.setProps({ 
+        errors: { error1: 'abc', error2: 'def', error3: 'xyz' },
+        customErrors: {error1: 'abc', error2: 'def', error3: 'ghi' },
+      })
+      assert.equal(wrapper.find('.send-v2__error').length, 1)
+      assert.equal(wrapper.find('.send-v2__error').text(), 'xyz_t')
+    })
+
+    it('should render an error message if the passed custom errors contain an error of errorType', () => {
+      wrapper.setProps({ errors: {}, customErrors: { error1: 'abc', error2: 'def', error3: 'xyz' } })
       assert.equal(wrapper.find('.send-v2__error').length, 1)
       assert.equal(wrapper.find('.send-v2__error').text(), 'xyz_t')
     })
