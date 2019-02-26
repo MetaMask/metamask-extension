e6ddc12387c5f2897f6cc4eb30697a9fdf25c3b7
Sneh Koul <35871990+Sneh1999@users.noreply.github.com>
2019-02-26 16:09:12 -0500

    Update run-all.sh

--- test/e2e/beta/run-all.sh
+++ test/e2e/beta/run-all.sh
@@ -6,6 +6,7 @@ set -o pipefail
 
 export PATH="$PATH:./node_modules/.bin"
 
+shell-parallel -s 'static-server test/web3 --port 8080' -x 'sleep 5 && mocha test/e2e/beta/web3.spec'
 shell-parallel -s 'npm run ganache:start -- -b 2' -x 'sleep 5 && static-server test/e2e/beta/contract-test --port 8080' -x 'sleep 5 && mocha test/e2e/beta/metamask-beta-ui.spec'
 shell-parallel -s 'npm run ganache:start -- -b 2' -x 'sleep 5 && static-server test/e2e/beta/contract-test --port 8080' -x 'sleep 5 && mocha test/e2e/beta/metamask-beta-responsive-ui.spec'
 shell-parallel -s 'npm run ganache:start -- -d -b 2 --account=0x53CB0AB5226EEBF4D872113D98332C1555DC304443BEE1CF759D15798D3C55A9,25000000000000000000' \
