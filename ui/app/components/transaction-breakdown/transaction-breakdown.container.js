73e3352fd90cd94719b3e8825bc5590906c6c4c7
Dan Miller <danjm.com@gmail.com>
2019-03-06 10:50:44 -0330

    Handle undefined gas limits and prices in transaction-breakdown.component

--- ui/app/components/transaction-breakdown/transaction-breakdown.container.js
+++ ui/app/components/transaction-breakdown/transaction-breakdown.container.js
@@ -1,14 +1,28 @@
 import { connect } from 'react-redux'
 import TransactionBreakdown from './transaction-breakdown.component'
 import {getIsMainnet, getNativeCurrency, preferencesSelector} from '../../selectors'
+import { getHexGasTotal } from '../../helpers/confirm-transaction/util'
+import { sumHexes } from '../../helpers/transactions.util'
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, ownProps) => {
+  const { transaction } = ownProps
+  const { txParams: { gas, gasPrice, value } = {}, txReceipt: { gasUsed } = {} } = transaction
   const { showFiatInTestnets } = preferencesSelector(state)
   const isMainnet = getIsMainnet(state)
+
+  const gasLimit = typeof gasUsed === 'string' ? gasUsed : gas
+
+  const hexGasTotal = gasLimit && gasPrice && getHexGasTotal({ gasLimit, gasPrice }) || '0x0'
+  const totalInHex = sumHexes(hexGasTotal, value)
 
   return {
     nativeCurrency: getNativeCurrency(state),
     showFiat: (isMainnet || !!showFiatInTestnets),
+    totalInHex,
+    gas,
+    gasPrice,
+    value,
+    gasUsed,
   }
 }
 
