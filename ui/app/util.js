22c4fe046990b9d38fa47d0983a40dc3f3c0b079
Thomas Huang <thomas.b.huang@gmail.com>
2019-03-11 13:00:03 -0500

    No longer check network when validating checksum addresses

--- ui/app/util.js
+++ ui/app/util.js
@@ -95,7 +95,8 @@ function miniAddressSummary (address) {
 function isValidAddress (address, network) {
   var prefixed = ethUtil.addHexPrefix(address)
   if (address === '0x0000000000000000000000000000000000000000') return false
-  if (!isEthNetwork(network)) return (ethUtil.isValidAddress(prefixed) && address === address.toLowerCase())
+  // Checksums on non-ETH Blockchains #6001
+  // if (!isEthNetwork(network)) return (ethUtil.isValidAddress(prefixed) && address === address.toLowerCase())
   return (isAllOneCase(prefixed) && ethUtil.isValidAddress(prefixed)) || ethUtil.isValidChecksumAddress(prefixed)
 }
 
@@ -315,7 +316,9 @@ function getTokenAddressFromTokenObject (token) {
  */
 function checksumAddress (address, network) {
   const checksummed = address ? ethUtil.toChecksumAddress(address) : ''
-  return checksummed && network && !isEthNetwork(network) ? checksummed.toLowerCase() : checksummed
+  return checksummed
+  // Checksums on non-ETH Blockchains #6001
+  // return checksummed && network && !isEthNetwork(network) ? checksummed.toLowerCase() : checksummed
 }
 
 function addressSlicer (address = '') {
