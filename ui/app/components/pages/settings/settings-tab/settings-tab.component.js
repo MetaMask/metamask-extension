d20df84bdad5f547e1ae0a3506850bfae5c78f06
Chi Kei Chan <chikeichan@gmail.com>
2019-03-13 17:03:20 -0700

    Move infura-conversion.json to helpers/constants/

--- ui/app/components/pages/settings/settings-tab/settings-tab.component.js
+++ ui/app/components/pages/settings/settings-tab/settings-tab.component.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
-import infuraCurrencies from '../../../../infura-conversion.json'
+import infuraCurrencies from '../../../../helpers/constants/infura-conversion.json'
 import validUrl from 'valid-url'
 import { exportAsFile } from '../../../../util'
 import SimpleDropdown from '../../../dropdowns/simple-dropdown'
