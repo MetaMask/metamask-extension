dc4c40414e6fb76d3c56aa771752dd6ade61c280
pldespaigne <pldespaigne@cascade8.com>
2019-04-04 17:15:57 +0200

    issue#5742

--- app/scripts/lib/ens-ipfs/resolver.js
+++ app/scripts/lib/ens-ipfs/resolver.js
@@ -1,9 +1,9 @@
 const namehash = require('eth-ens-namehash')
-const multihash = require('multihashes')
 const Eth = require('ethjs-query')
 const EthContract = require('ethjs-contract')
 const registryAbi = require('./contracts/registry')
 const resolverAbi = require('./contracts/resolver')
+const contentHash = require('content-hash')
 
 module.exports = resolveEnsToIpfsContentId
 
@@ -26,16 +26,26 @@ async function resolveEnsToIpfsContentId ({ provider, name }) {
     throw new Error(`EnsIpfsResolver - no resolver found for name "${name}"`)
   }
   const Resolver = contract(resolverAbi).at(resolverAddress)
-  // lookup content id
-  const contentLookupResult = await Resolver.content(hash)
-  const contentHash = contentLookupResult[0]
-  if (hexValueIsEmpty(contentHash)) {
-    throw new Error(`EnsIpfsResolver - no content ID found for name "${name}"`)
+
+  const isEIP1577Compliant = await Resolver.supportsInterface('0xbc1c58d1')
+  const isLegacyResolver = await Resolver.supportsInterface('0xd8389dc5')
+  if (isEIP1577Compliant[0]) {
+    const contentLookupResult = await Resolver.contenthash(hash)
+    const rawContentHash = contentLookupResult[0]
+    const decodedContentHash = contentHash.decode(rawContentHash)
+    const type = contentHash.getCodec(rawContentHash)
+    return {type: type, hash: decodedContentHash}
   }
-  const nonPrefixedHex = contentHash.slice(2)
-  const buffer = multihash.fromHexString(nonPrefixedHex)
-  const contentId = multihash.toB58String(multihash.encode(buffer, 'sha2-256'))
-  return contentId
+  if (isLegacyResolver[0]) {
+    // lookup content id
+    const contentLookupResult = await Resolver.content(hash)
+    const content = contentLookupResult[0]
+    if (hexValueIsEmpty(content)) {
+      throw new Error(`EnsIpfsResolver - no content ID found for name "${name}"`)
+    }
+    return {type: 'swarm-ns', hash: content.slice(2)}
+  }
+  throw new Error(`EnsIpfsResolver - the resolver for name "${name}" is not standard, it should either supports contenthash() or content()`)
 }
 
 function hexValueIsEmpty (value) {
--- app/scripts/lib/ens-ipfs/setup.js
+++ app/scripts/lib/ens-ipfs/setup.js
@@ -38,23 +38,29 @@ function setupEnsIpfsResolver ({ provider }) {
   async function attemptResolve ({ tabId, name, path, search }) {
     extension.tabs.update(tabId, { url: `loading.html` })
     try {
-      const ipfsContentId = await resolveEnsToIpfsContentId({ provider, name })
-      const url = `https://gateway.ipfs.io/ipfs/${ipfsContentId}${path}${search || ''}`
-      try {
-        // check if ipfs gateway has result
-        const response = await fetch(url, { method: 'HEAD' })
-        // if failure, redirect to 404 page
-        if (response.status !== 200) {
-          extension.tabs.update(tabId, { url: '404.html' })
-          return
+      let url = '400.html'
+      const {type, hash} = await resolveEnsToIpfsContentId({ provider, name })
+      if (type === 'ipfs-ns') {
+        url = `https://gateway.ipfs.io/ipfs/${hash}${path}${search || ''}`
+        try {
+          // check if ipfs gateway has result
+          const response = await fetch(url, { method: 'HEAD' })
+          // if failure, redirect to 404 page
+          if (response.status !== 200) {
+            extension.tabs.update(tabId, { url: '404.html' })
+            return
+          }
+          // otherwise redirect to the correct page
+          extension.tabs.update(tabId, { url })
+        } catch (err) {
+          console.warn(err)
+          // if HEAD fetch failed, redirect so user can see relevant error page
+          extension.tabs.update(tabId, { url })
         }
-        // otherwise redirect to the correct page
-        extension.tabs.update(tabId, { url })
-      } catch (err) {
-        console.warn(err)
-        // if HEAD fetch failed, redirect so user can see relevant error page
-        extension.tabs.update(tabId, { url })
+      } else if (type === 'swarm-ns') {
+        url = `https://swarm-gateways.net/bzz:/${hash}${path}${search || ''}`
       }
+      extension.tabs.update(tabId, { url })
     } catch (err) {
       console.warn(err)
       extension.tabs.update(tabId, { url: `error.html?name=${name}` })
