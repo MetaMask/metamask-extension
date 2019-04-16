5b96616b2d906194559490d57258355dbaf150ed
Chi Kei Chan <chikeichan@gmail.com>
2019-04-15 21:03:35 -0700

    Change font weight; Update storybook

--- ui/app/components/ui/button/button.stories.js
+++ ui/app/components/ui/button/button.stories.js
@@ -4,11 +4,66 @@ import { action } from '@storybook/addon-actions'
 import Button from '.'
 import { text, boolean, select } from '@storybook/addon-knobs/react'
 
+// ', 'secondary', 'default', 'warning', 'danger', 'danger-primary', 'link'], 'primary')}
 storiesOf('Button', module)
-  .add('button', () =>
+  .add('Button - Primary', () =>
     <Button
       onClick={action('clicked')}
-      type={select('type', ['primary', 'secondary', 'default', 'warning', 'danger', 'danger-primary', 'link'], 'primary')}
+      type="primary"
+      disabled={boolean('disabled', false)}
+    >
+      {text('text', 'Click me')}
+    </Button>
+  )
+  .add('Button - Secondary', () =>
+    <Button
+      onClick={action('clicked')}
+      type="secondary"
+      disabled={boolean('disabled', false)}
+    >
+      {text('text', 'Click me')}
+    </Button>
+  )
+  .add('Button - Default', () =>
+    <Button
+      onClick={action('clicked')}
+      type="default"
+      disabled={boolean('disabled', false)}
+    >
+      {text('text', 'Click me')}
+    </Button>
+  )
+  .add('Button - Warning', () =>
+    <Button
+      onClick={action('clicked')}
+      type="warning"
+      disabled={boolean('disabled', false)}
+    >
+      {text('text', 'Click me')}
+    </Button>
+  )
+  .add('Button - Danger', () =>
+    <Button
+      onClick={action('clicked')}
+      type="danger"
+      disabled={boolean('disabled', false)}
+    >
+      {text('text', 'Click me')}
+    </Button>
+  )
+  .add('Button - Danger Primary', () =>
+    <Button
+      onClick={action('clicked')}
+      type="danger-primary"
+      disabled={boolean('disabled', false)}
+    >
+      {text('text', 'Click me')}
+    </Button>
+  )
+  .add('Button - Link', () =>
+    <Button
+      onClick={action('clicked')}
+      type="link"
       disabled={boolean('disabled', false)}
     >
       {text('text', 'Click me')}
