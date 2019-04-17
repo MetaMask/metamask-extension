ed28ca60e05b3ff5aa13c353a5726e48bac16358
Chi Kei Chan <chikeichan@gmail.com>
2019-04-17 13:50:47 -0700

    Fix PropTypes validation in send-asset-row

--- ui/app/pages/send/send-content/send-asset-row/send-asset-row.component.js
+++ ui/app/pages/send/send-content/send-asset-row/send-asset-row.component.js
@@ -11,7 +11,7 @@ export default class SendAssetRow extends Component {
     tokens: PropTypes.arrayOf(
       PropTypes.shape({
         address: PropTypes.string,
-        decimals: PropTypes.string,
+        decimals: PropTypes.number,
         symbol: PropTypes.string,
       })
     ).isRequired,
