e46aa58b277a5daf20b2dbdfaf8381b5f8e20c16
Dan Miller <danjm.com@gmail.com>
2019-04-29 04:29:14 -0230

    Adds fetchWithCache to guard against unnecessary API calls

---
+++ ui/app/helpers/utils/fetch-with-cache.js
@@ -0,0 +1,22 @@
+import {
+  loadLocalStorageData,
+  saveLocalStorageData,
+} from '../../../lib/local-storage-helpers'
+
+export default function fetchWithCache (url, opts, cacheRefreshTime = 360000) {
+  const currentTime = Date.now()
+  const cachedFetch = loadLocalStorageData('cachedFetch') || {}
+  const { cachedUrl, cachedTime } = cachedFetch[url] || {}
+  if (cachedUrl && currentTime - cachedTime < 360000) {
+    return cachedFetch[url]
+  } else {
+    cachedFetch[url] = { cachedUrl: url, cachedTime: currentTime }
+    saveLocalStorageData(cachedFetch, 'cachedFetch')
+    return fetch(url, {
+      referrerPolicy: 'no-referrer-when-downgrade',
+      body: null,
+      method: 'GET',
+      mode: 'cors',
+    })
+  }
+}
