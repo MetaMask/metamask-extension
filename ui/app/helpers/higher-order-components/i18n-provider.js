6ff9206f04d68edeb57706df66a5bc22520974db
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2019-04-30 23:21:08 -0230

    Add JSDoc for I18nProvider#t context fn

--- ui/app/helpers/higher-order-components/i18n-provider.js
+++ ui/app/helpers/higher-order-components/i18n-provider.js
@@ -15,6 +15,12 @@ class I18nProvider extends Component {
     const { localeMessages } = this.props
     const { current, en } = localeMessages
     return {
+      /**
+       * Returns a localized message for the given key
+       * @param {string} key The message key
+       * @param {string[]} args A list of message substitution replacements
+       * @return {string|undefined|null} The localized message if available
+       */
       t (key, ...args) {
         if (key === undefined || key === null) {
           return key
