4c9278a74ea2d6e05ad2ed08d6b9a8e12821e1b9
Chi Kei Chan <chikeichan@gmail.com>
2019-05-07 16:25:50 -0700

    Remove KNOWN_ADDRESS_ERROR from error objects

--- ui/app/pages/send/send-content/send-to-row/tests/send-to-row-utils.test.js
+++ ui/app/pages/send/send-content/send-to-row/tests/send-to-row-utils.test.js
@@ -57,7 +57,7 @@ describe('send-to-row utils', () => {
 
     it('should return a known address recipient if to is truthy but part of state tokens', () => {
       assert.deepEqual(getToErrorObject('0xabc123', undefined, false, [{'address': '0xabc123'}], {'address': '0xabc123'}), {
-        to: KNOWN_RECIPIENT_ADDRESS_ERROR,
+        to: null,
       })
     })
 
@@ -69,12 +69,12 @@ describe('send-to-row utils', () => {
 
     it('should return a known address recipient if to is truthy but part of contract metadata', () => {
       assert.deepEqual(getToErrorObject('0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359', undefined, false, [{'address': '0xabc123'}], {'address': '0xabc123'}), {
-        to: KNOWN_RECIPIENT_ADDRESS_ERROR,
+        to: null,
       })
     })
     it('should null if to is truthy part of contract metadata but selectedToken falsy', () => {
       assert.deepEqual(getToErrorObject('0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359', undefined, false, [{'address': '0xabc123'}], {'address': '0xabc123'}), {
-        to: KNOWN_RECIPIENT_ADDRESS_ERROR,
+        to: null,
       })
     })
   })
