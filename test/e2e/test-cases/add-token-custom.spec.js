08c539320ea9d927e9df3f8b2a1d87faed16c578
Victor Baranov <baranov.viktor.27@gmail.com>
2019-06-06 15:57:55 +0300

    Fix link to Blockscout in e2e tests

--- test/e2e/test-cases/add-token-custom.spec.js
+++ test/e2e/test-cases/add-token-custom.spec.js
@@ -76,7 +76,7 @@ const addCustomToken = async (f, account1, account2) => {
         await f.delay(2000)
         const title = await f.waitUntilCurrentUrl()
         console.log(title)
-        assert.equal(title.includes('https://blockscout.com/eth/mainnet/tokens/'), true, 'blockscout wasn\'t opened')
+        assert.equal(title.includes('https://blockscout.com/poa/sokol/tokens/'), true, 'blockscout wasn\'t opened')
         await f.switchToFirstPage()
       })
     })
