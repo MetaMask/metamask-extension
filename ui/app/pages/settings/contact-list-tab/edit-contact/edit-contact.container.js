a96ad0f171a631e3e570f534a4e0a5e447e4062a
Jenny Pollack <jennypollack3@gmail.com>
2019-06-10 17:53:29 +0200

    clean up edit

--- ui/app/pages/settings/contact-list-tab/edit-contact/edit-contact.container.js
+++ ui/app/pages/settings/contact-list-tab/edit-contact/edit-contact.container.js
@@ -3,7 +3,7 @@ import { compose } from 'recompose'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import { getAddressBook } from '../../../../selectors/selectors'
-import { addToAddressBook, removeFromAddressBook } from '../../../../store/actions'
+import { addToAddressBook } from '../../../../store/actions'
 
 const mapStateToProps = state => {
   return {
@@ -14,7 +14,6 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
   return {
     addToAddressBook: (recipient, nickname) => dispatch(addToAddressBook(recipient, nickname)),
-    removeFromAddressBook: (addressToRemove) => dispatch(removeFromAddressBook(addressToRemove)),
   }
 }
 
