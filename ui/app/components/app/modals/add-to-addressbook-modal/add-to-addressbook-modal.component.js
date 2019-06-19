de0fe6356c0d9ae8b21ea12235bfdc2daf04d756
Chi Kei Chan <chikeichan@gmail.com>
2019-06-19 16:42:45 -0700

    Create AddToAddressBookModal component

---
+++ ui/app/components/app/modals/add-to-addressbook-modal/add-to-addressbook-modal.component.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export default class AddToAddressBookModal extends Component {
+  render () {
+    return (
+      <div>hi</div>
+    )
+  }
+}
