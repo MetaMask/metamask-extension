3d16c15d6d2fd6d5e863ba6a730da4d0184fdfe4
Chi Kei Chan <chikeichan@gmail.com>
2019-06-20 00:58:41 -0700

    ens wip

--- ui/app/pages/send/send-content/add-recipient/add-recipient.component.js
+++ ui/app/pages/send/send-content/add-recipient/add-recipient.component.js
@@ -48,6 +48,7 @@ export default class AddRecipient extends Component {
     updateSendTo(to, nickname)
     updateGas({ to })
   }
+  
   //
   // resetRecipient = () => this.setState({
   //   recipientName: '',
