2d7b085d1368dcf1af5694baf12b6a51988e9cd7
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2019-07-02 16:54:31 -0230

    Replace shell-parallel with concurrently

--- test/e2e/run-all.sh
+++ test/e2e/run-all.sh
@@ -1,12 +1,33 @@
 #!/usr/bin/env bash
 
+set -x
 set -e
 set -u
 set -o pipefail
 
 export PATH="$PATH:./node_modules/.bin"
+export GANACHE_ARGS='--quiet --blockTime 2'
 
-shell-parallel -s 'npm run ganache:start -- -b 2' -x 'sleep 5 && static-server test/e2e/contract-test --port 8080' -x 'sleep 5 && mocha test/e2e/metamask-ui.spec'
-shell-parallel -s 'npm run ganache:start -- -b 2' -x 'sleep 5 && static-server test/e2e/contract-test --port 8080' -x 'sleep 5 && mocha test/e2e/metamask-responsive-ui.spec'
-shell-parallel -s 'npm run ganache:start -- -d -b 2 --account=0x53CB0AB5226EEBF4D872113D98332C1555DC304443BEE1CF759D15798D3C55A9,25000000000000000000' \
-  -x 'sleep 5 && mocha test/e2e/from-import-ui.spec'
+concurrently --kill-others \
+  --names 'ganache,dapp,e2e' \
+  --prefix '[{time}][{name}]' \
+  --success first \
+  'npm run ganache:start' \
+  'npm run dapp' \
+  'sleep 5 && mocha test/e2e/metamask-ui.spec'
+
+concurrently --kill-others \
+  --names 'ganache,dapp,e2e' \
+  --prefix '[{time}][{name}]' \
+  --success first \
+  'npm run ganache:start' \
+  'npm run dapp' \
+  'sleep 5 && mocha test/e2e/metamask-responsive-ui.spec'
+
+export GANACHE_ARGS="$GANACHE_ARGS --deterministic --account=0x53CB0AB5226EEBF4D872113D98332C1555DC304443BEE1CF759D15798D3C55A9,25000000000000000000"
+concurrently --kill-others \
+  --names 'ganache,e2e' \
+  --prefix '[{time}][{name}]' \
+  --success first \
+  'npm run ganache:start' \
+  'sleep 5 && mocha test/e2e/from-import-ui.spec'
