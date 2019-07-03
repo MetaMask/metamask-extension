9baf04b75afa60a54224bbcd24121a4afab74ef2
Chi Kei Chan <chikeichan@gmail.com>
2019-07-02 17:27:22 -0700

    Make section header say "add recipient” by default

--- ui/app/pages/send/send-header/send-header.selectors.js
+++ ui/app/pages/send/send-header/send-header.selectors.js
@@ -1,6 +1,7 @@
 const {
   getSelectedToken,
   getSendEditingTransactionId,
+  getSendTo,
 } = require('../send.selectors.js')
 
 const selectors = {
@@ -13,6 +14,10 @@ module.exports = selectors
 function getTitleKey (state) {
   const isEditing = Boolean(getSendEditingTransactionId(state))
   const isToken = Boolean(getSelectedToken(state))
+
+  if (!getSendTo(state)) {
+    return 'addRecipient'
+  }
 
   if (isEditing) {
     return 'edit'
