b80b917d86bcaffefbba61bf13e8daa88e2224ba
Luis de Leon <luyzdeleon@gmail.com>
2019-07-03 10:23:13 -0400

    Updated network-dropdown tests

--- ui/app/components/app/dropdowns/tests/network-dropdown.test.js
+++ ui/app/components/app/dropdowns/tests/network-dropdown.test.js
@@ -61,40 +61,56 @@ describe('Network Dropdown', () => {
       )
     })
 
-    it('renders 7 DropDownMenuItems ', () => {
-      assert.equal(wrapper.find(DropdownMenuItem).length, 8)
+    it('renders 13 DropDownMenuItems ', () => {
+      assert.equal(wrapper.find(DropdownMenuItem).length, 13)
     })
 
     it('checks background color for first NetworkDropdownIcon', () => {
-      assert.equal(wrapper.find(NetworkDropdownIcon).at(0).prop('backgroundColor'), '#29B6AF') // Main Ethereum Network Teal
+      assert.equal(wrapper.find(NetworkDropdownIcon).at(0).prop('backgroundColor'), '#29B6AF') // Main Ethereum Network Teal - Infura
     })
 
     it('checks background color for second NetworkDropdownIcon', () => {
-      assert.equal(wrapper.find(NetworkDropdownIcon).at(1).prop('backgroundColor'), '#ff4a8d') // Ropsten Red
+      assert.equal(wrapper.find(NetworkDropdownIcon).at(1).prop('backgroundColor'), '#29B6AF') // Main Ethereum Network Teal - Pocket
     })
 
     it('checks background color for third NetworkDropdownIcon', () => {
-      assert.equal(wrapper.find(NetworkDropdownIcon).at(2).prop('backgroundColor'), '#7057ff') // Kovan Purple
+      assert.equal(wrapper.find(NetworkDropdownIcon).at(2).prop('backgroundColor'), '#ff4a8d') // Ropsten Red - Infura
     })
 
     it('checks background color for fourth NetworkDropdownIcon', () => {
-      assert.equal(wrapper.find(NetworkDropdownIcon).at(3).prop('backgroundColor'), '#f6c343') // Rinkeby Yellow
+      assert.equal(wrapper.find(NetworkDropdownIcon).at(3).prop('backgroundColor'), '#ff4a8d') // Ropsten Red - Pocket
     })
 
     it('checks background color for fifth NetworkDropdownIcon', () => {
-      assert.equal(wrapper.find(NetworkDropdownIcon).at(4).prop('backgroundColor'), '#3099f2') // Goerli Blue
+      assert.equal(wrapper.find(NetworkDropdownIcon).at(4).prop('backgroundColor'), '#7057ff') // Kovan Purple - Infura
     })
 
     it('checks background color for sixth NetworkDropdownIcon', () => {
-      assert.equal(wrapper.find(NetworkDropdownIcon).at(5).prop('innerBorder'), '1px solid #9b9b9b')
-    })
-
-    it('checks dropdown for frequestRPCList from  state ', () => {
-      assert.equal(wrapper.find(DropdownMenuItem).at(6).text(), '✓http://localhost:7545')
+      assert.equal(wrapper.find(NetworkDropdownIcon).at(5).prop('backgroundColor'), '#7057ff') // Kovan Purple - Pocket
     })
 
     it('checks background color for seventh NetworkDropdownIcon', () => {
-      assert.equal(wrapper.find(NetworkDropdownIcon).at(6).prop('innerBorder'), '1px solid #9b9b9b')
+      assert.equal(wrapper.find(NetworkDropdownIcon).at(6).prop('backgroundColor'), '#f6c343') // Rinkeby Yellow - Infura
+    })
+
+    it('checks background color for eigth NetworkDropdownIcon', () => {
+      assert.equal(wrapper.find(NetworkDropdownIcon).at(7).prop('backgroundColor'), '#f6c343') // Rinkeby Yellow - Pocket
+    })
+
+    it('checks background color for ninth NetworkDropdownIcon', () => {
+      assert.equal(wrapper.find(NetworkDropdownIcon).at(8).prop('backgroundColor'), '#3099f2') // Goerli Blue - Infura
+    })
+
+    it('checks background color for tenth NetworkDropdownIcon', () => {
+      assert.equal(wrapper.find(NetworkDropdownIcon).at(9).prop('backgroundColor'), '#3099f2') // Goerli Blue - Pocket
+    })
+
+    it('checks background color for eleventh NetworkDropdownIcon', () => {
+      assert.equal(wrapper.find(NetworkDropdownIcon).at(10).prop('innerBorder'), '1px solid #9b9b9b')
+    })
+
+    it('checks dropdown for frequestRPCList from  state ', () => {
+      assert.equal(wrapper.find(DropdownMenuItem).at(11).text(), '✓http://localhost:7545')
     })
 
   })
