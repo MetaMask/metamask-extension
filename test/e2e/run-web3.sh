caa40912ac686c3b1b7ae4f5e04ee09e38afb373
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2019-07-09 14:09:31 -0230

    Replace deprecated shell-parallel with concurrently (#6807)
    * Replace shell-parallel with concurrently
    
    * Update ganache-cli

--- test/e2e/run-web3.sh
+++ test/e2e/run-web3.sh
@@ -6,4 +6,8 @@ set -o pipefail
 
 export PATH="$PATH:./node_modules/.bin"
 
-shell-parallel -s 'static-server test/web3 --port 8080' -x 'sleep 5 && mocha test/e2e/web3.spec'
+concurrently --kill-others \
+  --names 'dapp,e2e' \
+  --prefix '[{time}][{name}]' \
+  'static-server test/web3 --port 8080' \
+  'sleep 5 && mocha test/e2e/web3.spec'
