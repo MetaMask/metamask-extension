1629bcb75c8f05f16e4833a12c3c52cfb32c8c1a
Mark Stacey <markjstacey@gmail.com>
2019-07-18 09:48:53 -0300

    Set minimum browser version in manifest
    Set the minimum browser version supported in the extension manifest.
    Currently we only ship the extension on Chrome and Firefox, so the
    minimum version has been set for those two browsers.
    
    Relates to #6805

--- gulpfile.js
+++ gulpfile.js
@@ -153,6 +153,7 @@ gulp.task('manifest:chrome', function () {
   return gulp.src('./dist/chrome/manifest.json')
   .pipe(jsoneditor(function (json) {
     delete json.applications
+    json.minimum_chrome_version = '58'
     return json
   }))
   .pipe(gulp.dest('./dist/chrome', { overwrite: true }))
