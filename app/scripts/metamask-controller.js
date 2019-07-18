c60eab8b383975fbc9344a8f9d1e6f230667ac26
Dan Miller <danjm.com@gmail.com>
2019-07-18 09:40:18 -0230

    Update threebox controller for automatic syncing

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -205,6 +205,7 @@ module.exports = class MetamaskController extends EventEmitter {
       keyringController: this.keyringController,
       provider: this.provider,
       restoreFrom3Box: false,
+      initState: initState.ThreeBoxController,
     })
 
     // tx mgmt
@@ -282,6 +283,7 @@ module.exports = class MetamaskController extends EventEmitter {
       OnboardingController: this.onboardingController.store,
       ProviderApprovalController: this.providerApprovalController.store,
       IncomingTransactionsController: this.incomingTransactionsController.store,
+      ThreeBoxController: this.threeBoxController.store,
     })
 
     this.memStore = new ComposableObservableStore(null, {
@@ -306,6 +308,8 @@ module.exports = class MetamaskController extends EventEmitter {
       ProviderApprovalController: this.providerApprovalController.store,
       ProviderApprovalControllerMemStore: this.providerApprovalController.memStore,
       IncomingTransactionsController: this.incomingTransactionsController.store,
+      // ThreeBoxController
+      ThreeBoxController: this.threeBoxController.store,
     })
     this.memStore.subscribe(this.sendUpdate.bind(this))
   }
@@ -420,6 +424,7 @@ module.exports = class MetamaskController extends EventEmitter {
     const networkController = this.networkController
     const providerApprovalController = this.providerApprovalController
     const onboardingController = this.onboardingController
+    const threeBoxController = this.threeBoxController
 
     return {
       // etc
@@ -525,6 +530,9 @@ module.exports = class MetamaskController extends EventEmitter {
 
       // onboarding controller
       setSeedPhraseBackedUp: nodeify(onboardingController.setSeedPhraseBackedUp, onboardingController),
+
+      // 3box syncing
+      setThreeBoxSyncing: nodeify(threeBoxController.setThreeBoxSyncing, threeBoxController),
     }
   }
 
