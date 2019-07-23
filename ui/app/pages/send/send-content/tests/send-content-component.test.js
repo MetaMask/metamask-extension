2545fd9a175838f28209d17d6b2e456344a06d2f
Kristian <kristian.tapia01@gmail.com>
2019-07-23 16:33:36 -0700

    fix lint errors, remove unused import

--- ui/app/pages/send/send-content/tests/send-content-component.test.js
+++ ui/app/pages/send/send-content/tests/send-content-component.test.js
@@ -10,7 +10,6 @@ import SendGasRow from '../send-gas-row/send-gas-row.container'
 import SendToRow from '../send-to-row/send-to-row.container'
 import SendHexDataRow from '../send-hex-data-row/send-hex-data-row.container'
 import SendAssetRow from '../send-asset-row/send-asset-row.container'
-import { SendRowWrapper } from "../send-row-wrapper/send-row-wrapper.component";
 
 describe('SendContent Component', function () {
   let wrapper
