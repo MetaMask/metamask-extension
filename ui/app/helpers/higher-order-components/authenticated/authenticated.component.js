1843cf0f0591fb0f21504cd08a55293fc8794f1b
Mark Stacey <markjstacey@gmail.com>
2019-07-25 21:07:38 -0300

    Ensure that initialization is always opened in browser
    These cases should currently be unreachable because of a check performed
    in the `startPopup` function in `popup-core`. There may be some edge
    case where it's possible though, so they've been updated to correctly
    open a fullscreen tab.

--- ui/app/helpers/higher-order-components/authenticated/authenticated.component.js
+++ ui/app/helpers/higher-order-components/authenticated/authenticated.component.js
@@ -2,6 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Redirect, Route } from 'react-router-dom'
 import { UNLOCK_ROUTE, INITIALIZE_ROUTE } from '../../constants/routes'
+import { ENVIRONMENT_TYPE_FULLSCREEN } from '../../../../../app/scripts/lib/enums'
+import { getEnvironmentType } from '../../../../../app/scripts/lib/util'
 
 export default function Authenticated (props) {
   const { isUnlocked, completedOnboarding } = props
@@ -10,7 +12,10 @@ export default function Authenticated (props) {
     case isUnlocked && completedOnboarding:
       return <Route { ...props } />
     case !completedOnboarding:
-      return <Redirect to={{ pathname: INITIALIZE_ROUTE }} />
+      if (getEnvironmentType(window.location.href) === ENVIRONMENT_TYPE_FULLSCREEN) {
+        return <Redirect to={{ pathname: INITIALIZE_ROUTE }} />
+      }
+      return global.platform.openExtensionInBrowser(INITIALIZE_ROUTE)
     default:
       return <Redirect to={{ pathname: UNLOCK_ROUTE }} />
   }
--- ui/app/helpers/higher-order-components/initialized/initialized.component.js
+++ ui/app/helpers/higher-order-components/initialized/initialized.component.js
@@ -2,11 +2,16 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Redirect, Route } from 'react-router-dom'
 import { INITIALIZE_ROUTE } from '../../constants/routes'
+import { ENVIRONMENT_TYPE_FULLSCREEN } from '../../../../../app/scripts/lib/enums'
+import { getEnvironmentType } from '../../../../../app/scripts/lib/util'
 
 export default function Initialized (props) {
-  return props.completedOnboarding
-    ? <Route { ...props } />
-    : <Redirect to={{ pathname: INITIALIZE_ROUTE }} />
+  if (props.completedOnboarding) {
+    return <Route { ...props } />
+  } else if (getEnvironmentType(window.location.href) === ENVIRONMENT_TYPE_FULLSCREEN) {
+    return <Redirect to={{ pathname: INITIALIZE_ROUTE }} />
+  }
+  return global.platform.openExtensionInBrowser(INITIALIZE_ROUTE)
 }
 
 Initialized.propTypes = {
