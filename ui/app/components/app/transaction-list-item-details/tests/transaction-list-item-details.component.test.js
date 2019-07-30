2761cc5a2d3dc5120112a49f59634b5bed840c6c
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2019-07-29 22:12:29 -0230

    Hide Copy Tx ID and block explorer link for txns w/o hash (#6928)

--- ui/app/components/app/transaction-list-item-details/tests/transaction-list-item-details.component.test.js
+++ ui/app/components/app/transaction-list-item-details/tests/transaction-list-item-details.component.test.js
@@ -78,4 +78,73 @@ describe('TransactionListItemDetails Component', () => {
     assert.ok(wrapper.hasClass('transaction-list-item-details'))
     assert.equal(wrapper.find(Button).length, 3)
   })
+
+  it('should disable the Copy Tx ID and View In Etherscan buttons when tx hash is missing', () => {
+    const transaction = {
+      history: [],
+      id: 1,
+      status: 'confirmed',
+      txParams: {
+        from: '0x1',
+        gas: '0x5208',
+        gasPrice: '0x3b9aca00',
+        nonce: '0xa4',
+        to: '0x2',
+        value: '0x2386f26fc10000',
+      },
+    }
+
+    const transactionGroup = {
+      transactions: [transaction],
+      primaryTransaction: transaction,
+      initialTransaction: transaction,
+    }
+
+    const wrapper = shallow(
+      <TransactionListItemDetails
+        transactionGroup={transactionGroup}
+      />,
+      { context: { t: (str1, str2) => str2 ? str1 + str2 : str1 } }
+    )
+
+    assert.ok(wrapper.hasClass('transaction-list-item-details'))
+    const buttons = wrapper.find(Button)
+    assert.strictEqual(buttons.at(0).prop('disabled'), true)
+    assert.strictEqual(buttons.at(1).prop('disabled'), true)
+  })
+
+  it('should render functional Copy Tx ID and View In Etherscan buttons when tx hash exists', () => {
+    const transaction = {
+      history: [],
+      id: 1,
+      status: 'confirmed',
+      hash: '0xaa',
+      txParams: {
+        from: '0x1',
+        gas: '0x5208',
+        gasPrice: '0x3b9aca00',
+        nonce: '0xa4',
+        to: '0x2',
+        value: '0x2386f26fc10000',
+      },
+    }
+
+    const transactionGroup = {
+      transactions: [transaction],
+      primaryTransaction: transaction,
+      initialTransaction: transaction,
+    }
+
+    const wrapper = shallow(
+      <TransactionListItemDetails
+        transactionGroup={transactionGroup}
+      />,
+      { context: { t: (str1, str2) => str2 ? str1 + str2 : str1 } }
+    )
+
+    assert.ok(wrapper.hasClass('transaction-list-item-details'))
+    const buttons = wrapper.find(Button)
+    assert.strictEqual(buttons.at(0).prop('disabled'), false)
+    assert.strictEqual(buttons.at(1).prop('disabled'), false)
+  })
 })
