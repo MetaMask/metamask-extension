b82567762fb82a469fd58db18341d4a2b401af0d
Dan Miller <danjm.com@gmail.com>
2019-08-16 02:33:56 -0230

    Replace erroneous use of  with

--- ui/app/store/actions.js
+++ ui/app/store/actions.js
@@ -325,7 +325,6 @@ var actions = {
   setShowFiatConversionOnTestnetsPreference,
   setAutoLogoutTimeLimit,
   unsetMigratedPrivacyMode,
-  setThreeBoxSyncing,
 
   // Onboarding
   setCompletedOnboarding,
@@ -2435,24 +2434,6 @@ function setShowFiatConversionOnTestnetsPreference (value) {
 
 function setAutoLogoutTimeLimit (value) {
   return setPreference('autoLogoutTimeLimit', value)
-}
-
-function setThreeBoxSyncing (newThreeBoxSyncingState) {
-  return dispatch => {
-    dispatch(actions.showLoadingIndication())
-    return new Promise((resolve, reject) => {
-      background.setThreeBoxSyncing(newThreeBoxSyncingState, (err) => {
-        dispatch(actions.hideLoadingIndication())
-
-        if (err) {
-          dispatch(actions.displayWarning(err.message))
-          return reject(err)
-        }
-
-        resolve()
-      })
-    })
-  }
 }
 
 function setCompletedOnboarding () {
