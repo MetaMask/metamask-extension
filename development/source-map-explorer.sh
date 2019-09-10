9f3bea656f0e03fd3584722bbc8816313e4c870d
kumavis <aaron@kumavis.me>
2019-09-11 00:22:52 +0800

    ci - source-map-explorer - include all bundles

--- development/source-map-explorer.sh
+++ development/source-map-explorer.sh
@@ -6,3 +6,5 @@ npx source-map-explorer dist/chrome/inpage.js --html build-artifacts/source-map-
 npx source-map-explorer dist/chrome/contentscript.js --html build-artifacts/source-map-explorer/contentscript.html
 npx source-map-explorer dist/chrome/background.js --html build-artifacts/source-map-explorer/background.html
 npx source-map-explorer dist/chrome/ui.js --html build-artifacts/source-map-explorer/ui.html
+npx source-map-explorer dist/chrome/libs.js --html build-artifacts/source-map-explorer/libs.html
+npx source-map-explorer dist/chrome/phishing-detect.js --html build-artifacts/source-map-explorer/phishing-detect.html
