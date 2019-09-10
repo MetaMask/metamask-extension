9e96707e6d59774efd0a470963c99c8ae4387171
Erik Marks <rekmarks@protonmail.com>
2019-09-10 15:34:40 -0700

    fix rinkeby spelling

--- test/unit/app/controllers/transactions/recipient-blacklist-checker-test.js
+++ test/unit/app/controllers/transactions/recipient-blacklist-checker-test.js
@@ -2,7 +2,7 @@ const assert = require('assert')
 const recipientBlackListChecker = require('../../../../../app/scripts/controllers/transactions/lib/recipient-blacklist-checker')
 const {
   ROPSTEN_CODE,
-  RINKEYBY_CODE,
+  RINKEBY_CODE,
   KOVAN_CODE,
   GOERLI_CODE,
 } = require('../../../../../app/scripts/controllers/network/enums')
@@ -28,7 +28,7 @@ describe('Recipient Blacklist Checker', function () {
   describe('#checkAccount', function () {
     it('does not fail on test networks', function () {
       let callCount = 0
-      const networks = [ROPSTEN_CODE, RINKEYBY_CODE, KOVAN_CODE, GOERLI_CODE]
+      const networks = [ROPSTEN_CODE, RINKEBY_CODE, KOVAN_CODE, GOERLI_CODE]
       for (const networkId in networks) {
         publicAccounts.forEach((account) => {
           recipientBlackListChecker.checkAccount(networkId, account)
