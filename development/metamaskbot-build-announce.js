629f5ef221bfcfa15764b580fca017791b8b504b
kumavis <kumavis@users.noreply.github.com>
2019-09-11 23:35:30 +0800

    ci - create source-map-explorer build-artifacts (#7141)
    * ci - create source-map-explorer build-artifacts
    
    * ci - add source-map-explorer builds to metamaskbot comment
    
    * lint fix
    
    * ci - source-map-explorer - include all bundles

--- development/metamaskbot-build-announce.js
+++ development/metamaskbot-build-announce.js
@@ -23,12 +23,25 @@ async function start () {
   const SHORT_SHA1 = CIRCLE_SHA1.slice(0, 7)
   const BUILD_LINK_BASE = `https://${CIRCLE_BUILD_NUM}-42009758-gh.circle-artifacts.com/0`
 
-  const CHROME = `${BUILD_LINK_BASE}/builds/metamask-chrome-${VERSION}.zip`
-  const FIREFOX = `${BUILD_LINK_BASE}/builds/metamask-firefox-${VERSION}.zip`
-  const EDGE = `${BUILD_LINK_BASE}/builds/metamask-edge-${VERSION}.zip`
-  const OPERA = `${BUILD_LINK_BASE}/builds/metamask-opera-${VERSION}.zip`
+  // build the github comment content
 
-  const commentBody = `Builds ready [${SHORT_SHA1}]: <a href="${CHROME}">chrome</a>, <a href="${FIREFOX}">firefox</a>, <a href="${EDGE}">edge</a>, <a href="${OPERA}">opera</a>`
+  // links to extension builds
+  const platforms = ['chrome', 'firefox', 'opera', 'edge']
+  const buildLinks = platforms.map(platform => {
+    const url = `${BUILD_LINK_BASE}/builds/metamask-${platform}-${VERSION}.zip`
+    return `<a href="${url}">${platform}</a>`
+  }).join(', ')
+
+  // links to bundle browser builds
+  const bundles = ['background', 'ui', 'inpage', 'contentscript', 'libs', 'phishing-detect']
+  const bundleLinks = bundles.map(bundle => {
+    const url = `${BUILD_LINK_BASE}/build-artifacts/source-map-explorer/${bundle}.html`
+    return `<a href="${url}">${bundle}</a>`
+  }).join(', ')
+
+  const exposedContent = `Builds ready [${SHORT_SHA1}]`
+  const hiddenContent = `<ul><li>builds: ${buildLinks}</li><li>bundles: ${bundleLinks}</li></ul>`
+  const commentBody = `<details><summary>${exposedContent}</summary>${hiddenContent}</details>`
 
   const JSON_PAYLOAD = JSON.stringify({ body: commentBody })
   const POST_COMMENT_URI = `https://api.github.com/repos/metamask/metamask-extension/issues/${CIRCLE_PR_NUMBER}/comments`
