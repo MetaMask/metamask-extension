cc84ed124db5f3953f62d6acec676c690763e2d2
Julio Delgado <julio.delgado@trustology.io>
2019-09-20 12:06:35 +0100

    feat: added links to FAQ and onboarding in email screen

--- ui/app/pages/create-account/connect-software/trustvault/email/email-screen.js
+++ ui/app/pages/create-account/connect-software/trustvault/email/email-screen.js
@@ -48,9 +48,51 @@ class EmailScreen extends PureComponent {
               this.props.getTrustVaultPinChallenge(this.state.email)
             }
           }
-        })
+        }),
+        h('span', {style: {
+          fontSize: '12px',
+          color: 'red',
+
+        }}, this.context.t('trustVaultGetStarted')),
+        h('span', {
+          style: {
+            cursor: 'pointer',
+            fontSize: '12px',
+            textDecoration: 'underline',
+            color: 'red',
+          },
+          onClick: () => {
+            global.platform.openWindow({
+              url: 'https://trustology.io/get-started/',
+            })
+          },
+        }, this.context.t('here'))
       ]
     )
+  }
+
+  renderLearnMoreLink () {
+    return h(
+      'div.lear-more-link', {}, [
+        h('span', {style: {
+          fontSize: '12px',
+          color: 'red',
+
+        }}, this.context.t('trustVaultLearnMore')),
+        h('span', {
+          style: {
+            cursor: 'pointer',  
+            textDecoration: 'underline',
+            fontSize: '12px',
+            color: 'red',
+          },
+          onClick: () => {
+            global.platform.openWindow({
+              url: 'https://app.intercom.io/a/apps/jbjnxu3c/articles/articles/3350608/show',
+            })
+          },
+        }, this.context.t('FAQ'))
+      ])
   }
 
   renderUnsupportedBrowser () {
@@ -109,7 +151,10 @@ class EmailScreen extends PureComponent {
       this.renderTrustVaultLogo(),
       this.renderHeader(),
       this.renderEmailInputBox(),
-      this.renderNextButton()
+      this.renderNextButton(),
+      this.renderLearnMoreLink()
+      
+      
     ])
   }
 
