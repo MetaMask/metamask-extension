5ef8537ed790245a1c1b5c3ae57d00343407980e
Dan Finlay <dan@danfinlay.com>
2019-09-20 10:18:12 -0700

    Use single root realm to reduce number of iFrames created

--- app/scripts/controllers/plugins.js
+++ app/scripts/controllers/plugins.js
@@ -13,6 +13,8 @@ class PluginsController extends EventEmitter {
     }, opts.initState)
     this.store = new ObservableStore(initState)
 
+    this.rootRealm = SES.makeSESRootRealm({consoleMode: 'allow', errorStackMode: 'allow', mathRandomMode: 'allow'})
+
     this.setupProvider = opts.setupProvider
     this._txController = opts._txController
     this._networkController = opts._networkController
@@ -178,10 +180,9 @@ class PluginsController extends EventEmitter {
   }
 
   _startPlugin (pluginName, approvedPermissions, sourceCode, ethereumProvider) {
-    const s = SES.makeSESRootRealm({consoleMode: 'allow', errorStackMode: 'allow', mathRandomMode: 'allow'})
     const apisToProvide = this._generateApisToProvide(approvedPermissions, pluginName)
     Object.assign(ethereumProvider, apisToProvide)
-    const sessedPlugin = s.evaluate(sourceCode, {
+    const sessedPlugin = this.rootRealm.evaluate(sourceCode, {
       wallet: ethereumProvider,
       console, // Adding console for now for logging purposes.
     })
