bc45ed06a7280f59420fe89232ad03b97308d8d2
kumavis <aaron@kumavis.me>
2019-09-23 11:10:40 +0800

    ci - install deps - limit install scripts to those needed for build

---
+++ .circleci/scripts/deps-install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -x
+
+yarn --frozen-lockfile --har
+
+# run each in subshell so directory change does not persist
+
+# for build
+(cd node_modules/node-sass && yarn run build)
+(cd node_modules/optipng-bin && yarn run postinstall)
+(cd node_modules/gifsicle && yarn run postinstall)
+(cd node_modules/jpegtran-bin && yarn run postinstall)
