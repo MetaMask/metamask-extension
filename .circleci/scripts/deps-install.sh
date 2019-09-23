c235b833fe1ad03e1111f8f27ec003a46a80e525
kumavis <aaron@kumavis.me>
2019-09-23 13:50:57 +0800

    ci - install deps - expand install scripts needed for tests

--- .circleci/scripts/deps-install.sh
+++ .circleci/scripts/deps-install.sh
@@ -4,9 +4,17 @@ set -x
 yarn --frozen-lockfile --ignore-scripts --har
 
 # run each in subshell so directory change does not persist
+# scripts can be any of:
+#   preinstall
+#   install
+#   postinstall
 
 # for build
 (cd node_modules/node-sass && yarn run build)
 (cd node_modules/optipng-bin && yarn run postinstall)
 (cd node_modules/gifsicle && yarn run postinstall)
 (cd node_modules/jpegtran-bin && yarn run postinstall)
+
+# for test
+(cd node_modules/scrypt && yarn run install)
+(cd node_modules/weak && yarn run install)
