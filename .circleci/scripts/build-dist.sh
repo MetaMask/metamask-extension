108e24a38374ba568af7aa2fc9c9486ff66ae637
kumavis <aaron@kumavis.me>
2019-09-24 12:46:31 +0800

    security - download acl tools first + smaller perms

--- .circleci/scripts/build-dist.sh
+++ .circleci/scripts/build-dist.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 set -x
 
+# install acl tools
+sudo apt-get install acl
+
 # create user "buildtask"
 sudo useradd buildtask
+
 # set file perms for the project dir
-setfacl -R -m u:buildtask:rx ~
-setfacl -R -m u:buildtask:rx ./
+setfacl -R -m u:buildtask:r ~/.nvmrc
+setfacl -R -m u:buildtask:rw ui/app/css/output
 setfacl -R -m u:buildtask:rw builds
 setfacl -R -m u:buildtask:rw dist
 
