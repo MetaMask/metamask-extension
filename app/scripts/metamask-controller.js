4b4bee77c708b18531179f1574f1058455c13ff8
Dan Finlay <dan@danfinlay.com>
2019-10-22 11:33:49 -0700

    Move signTypedData signing out to keyrings
    This simplifies the logic of signing and improves security:
    - Private keys are never moved to the base controller.
    - Hardware wallets are abstracted in the same way as local keys.
    
    This also paves the way for allowing even more modular accounts,
    provided by plugins:
    https://github.com/MetaMask/metamask-plugin-beta/pull/63
    
    Fixes #7075.

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -53,7 +53,6 @@ const seedPhraseVerifier = require('./lib/seed-phrase-verifier')
 const log = require('loglevel')
 const TrezorKeyring = require('eth-trezor-keyring')
 const LedgerBridgeKeyring = require('eth-ledger-bridge-keyring')
-const HW_WALLETS_KEYRINGS = [TrezorKeyring.type, LedgerBridgeKeyring.type]
 const EthQuery = require('eth-query')
 const ethUtil = require('ethereumjs-util')
 const sigUtil = require('eth-sig-util')
@@ -1168,28 +1167,14 @@ module.exports = class MetamaskController extends EventEmitter {
     const version = msgParams.version
     try {
       const cleanMsgParams = await this.typedMessageManager.approveMessage(msgParams)
-      const address = sigUtil.normalize(cleanMsgParams.from)
-      const keyring = await this.keyringController.getKeyringForAccount(address)
-      let signature
-      // HW Wallet keyrings don't expose private keys
-      // so we need to handle it separately
-      if (!HW_WALLETS_KEYRINGS.includes(keyring.type)) {
-        const wallet = keyring._getWalletForAccount(address)
-        const privKey = ethUtil.toBuffer(wallet.getPrivateKey())
-        switch (version) {
-          case 'V1':
-            signature = sigUtil.signTypedDataLegacy(privKey, { data: cleanMsgParams.data })
-            break
-          case 'V3':
-            signature = sigUtil.signTypedData(privKey, { data: JSON.parse(cleanMsgParams.data) })
-            break
-          case 'V4':
-            signature = sigUtil.signTypedData_v4(privKey, { data: JSON.parse(cleanMsgParams.data) })
-            break
-        }
-      } else {
-        signature = await keyring.signTypedData(address, cleanMsgParams.data)
+
+      // For some reason every version after V1 has stringified params.
+      if (version !== 'V1') {
+        cleanMsgParams.data = JSON.parse(cleanMsgParams.data)
       }
+
+      const address = sigUtil.normalize(cleanMsgParams.from)
+      const signature = await this.keyringController.signTypedMessage(cleanMsgParams, { version })
       this.typedMessageManager.setMsgStatusSigned(msgId, signature)
       return this.getState()
     } catch (error) {
