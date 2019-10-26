e98ecbd116a4cb5bf6d54a2e74ed0b0a48e1cb1d
rickycodes <ricky.miller@gmail.com>
2019-10-26 11:17:23 -0400

    Start using JSX and make tabs a bit more DRY

--- ui/app/pages/create-account/create-account.component.js
+++ ui/app/pages/create-account/create-account.component.js
@@ -1,4 +1,4 @@
-import { Component } from 'react'
+import React, { Component } from 'react'
 import { Switch, Route, matchPath } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import h from 'react-hyperscript'
@@ -15,43 +15,26 @@ import {
 export default class CreateAccountPage extends Component {
   renderTabs () {
     const { history, location } = this.props
+    const getClassNames = path => classnames('new-account__tabs__tab', {
+      'new-account__tabs__selected': matchPath(location.pathname, {
+        path,
+        exact: true,
+      }),
+    })
 
-    return h('div.new-account__tabs', [
-      h('div.new-account__tabs__tab', {
-        className: classnames('new-account__tabs__tab', {
-          'new-account__tabs__selected': matchPath(location.pathname, {
-            path: NEW_ACCOUNT_ROUTE, exact: true,
-          }),
-        }),
-        onClick: () => history.push(NEW_ACCOUNT_ROUTE),
-      }, [
-        this.context.t('create'),
-      ]),
-
-      h('div.new-account__tabs__tab', {
-        className: classnames('new-account__tabs__tab', {
-          'new-account__tabs__selected': matchPath(location.pathname, {
-            path: IMPORT_ACCOUNT_ROUTE, exact: true,
-          }),
-        }),
-        onClick: () => history.push(IMPORT_ACCOUNT_ROUTE),
-      }, [
-        this.context.t('import'),
-      ]),
-      h(
-        'div.new-account__tabs__tab',
-        {
-          className: classnames('new-account__tabs__tab', {
-            'new-account__tabs__selected': matchPath(location.pathname, {
-              path: CONNECT_HARDWARE_ROUTE,
-              exact: true,
-            }),
-          }),
-          onClick: () => history.push(CONNECT_HARDWARE_ROUTE),
-        },
-        this.context.t('connect')
-      ),
-    ])
+    return (
+      <div className="new-account__tabs">
+        <div className={getClassNames(NEW_ACCOUNT_ROUTE)} onClick={() => history.push(NEW_ACCOUNT_ROUTE)}>{
+          this.context.t('create')
+        }</div>
+        <div className={getClassNames(IMPORT_ACCOUNT_ROUTE)} onClick={() => history.push(IMPORT_ACCOUNT_ROUTE)}>{
+          this.context.t('import')
+        }</div>
+        <div className={getClassNames(CONNECT_HARDWARE_ROUTE)} onClick={() => history.push(CONNECT_HARDWARE_ROUTE)}>{
+          this.context.t('connect')
+        }</div>
+      </div>
+    )
   }
 
   render () {
