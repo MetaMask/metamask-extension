35839a75df6ec56ba1f85bdca64a1314c310a41a
frankiebee <frankie.diamond@gmail.com>
2019-10-29 15:32:07 -1000

    network - add new middleware for eth_getTransactionByHash that the checks pending tx's for a response value

---
+++ app/scripts/controllers/network/middleware/pending.js
@@ -0,0 +1,28 @@
+const { formatTxMetaForRpcResult } = require('../util')
+const createAsyncMiddleware = require('json-rpc-engine/src/createAsyncMiddleware')
+
+function createPendingNonceMiddleware ({ getPendingNonce }) {
+  return createAsyncMiddleware(async (req, res, next) => {
+    const {method, params} = req
+    if (method !== 'eth_getTransactionCount') return next()
+    const [param, blockRef] = params
+    if (blockRef !== 'pending') return next()
+    res.result = await getPendingNonce(param)
+  })
+}
+
+function createPendingTxMiddleware ({ getPendingTransactionByHash }) {
+  return createAsyncMiddleware(async (req, res, next) => {
+    const {method, params} = req
+    if (method !== 'eth_getTransactionByHash') return next()
+    const [hash] = params
+    const txMeta = getPendingTransactionByHash(hash)
+    if (!txMeta) return next()
+    res.result = formatTxMetaForRpcResult(txMeta)
+  })
+}
+
+module.exports = {
+  createPendingTxMiddleware,
+  createPendingNonceMiddleware,
+}
