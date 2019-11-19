ed194b6d727285c1a149b38ab60e6e0ad40776d7
Dan Miller <danjm.com@gmail.com>
2019-11-19 11:52:50 -0330

    Update e2e tests to use new connection flow

--- test/e2e/ethereum-on.spec.js
+++ test/e2e/ethereum-on.spec.js
@@ -113,7 +113,8 @@ describe('MetaMask', function () {
     let extension
     let popup
     let dapp
-    it('switches to a dapp', async () => {
+
+    it('connects to the dapp', async () => {
       await openNewPage(driver, 'http://127.0.0.1:8080/')
       await delay(regularDelayMs)
 
@@ -126,13 +127,20 @@ describe('MetaMask', function () {
       const windowHandles = await driver.getAllWindowHandles()
 
       extension = windowHandles[0]
-      popup = await switchToWindowWithTitle(driver, 'MetaMask Notification', windowHandles)
-      dapp = windowHandles.find(handle => handle !== extension && handle !== popup)
+      dapp = await switchToWindowWithTitle(driver, 'E2E Test Dapp', windowHandles)
+      popup = windowHandles.find(handle => handle !== extension && handle !== dapp)
+
+      await driver.switchTo().window(popup)
 
       await delay(regularDelayMs)
-      const approveButton = await findElement(driver, By.xpath(`//button[contains(text(), 'Connect')]`))
-      await approveButton.click()
 
+      const accountButton = await findElement(driver, By.css('.permissions-connect-choose-account__account'))
+      await accountButton.click()
+
+      const submitButton = await findElement(driver, By.xpath(`//button[contains(text(), 'Submit')]`))
+      await submitButton.click()
+
+      await waitUntilXWindowHandles(driver, 2)
       await driver.switchTo().window(dapp)
       await delay(regularDelayMs)
     })
