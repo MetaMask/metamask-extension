86acfa95eb064854a4d608ec67dfb96f0038ef34
rickycodes <ricky.miller@gmail.com>
2019-11-20 14:16:41 -0500

    Remove console.log(s)

--- ui/app/components/app/sidebars/sidebar.container.js
+++ ui/app/components/app/sidebars/sidebar.container.js
@@ -7,7 +7,6 @@ import {
 } from '../../../selectors/transactions'
 
 const mapStateToProps = state => {
-  console.log(state)
   const {
     isOpen: sidebarIsOpen,
     transitionName: sidebarTransitionName,
@@ -16,11 +15,9 @@ const mapStateToProps = state => {
   } = state.appState.sidebar
   const { transaction: sidebarTransaction } = props || {}
   const submittedPendingTransactions = submittedPendingTransactionsSelector(state)
-  console.log(submittedPendingTransactions)
 
   const sidebarOnOverlayClose = sidebarType === WALLET_VIEW_SIDEBAR
     ? () => {
-      console.log('Closed Sidebare Via Overlay')
       this.context.metricsEvent({
         eventOpts: {
           category: 'Navigation',
