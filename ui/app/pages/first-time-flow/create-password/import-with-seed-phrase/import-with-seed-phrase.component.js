c6259f6eb57c2dbb2b1b9aa7077c26b6cf969fb1
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2019-11-23 01:26:39 -0330

    Fix accessibility of first-time-flow terms checkboxes (#7501)

--- ui/app/pages/first-time-flow/create-password/import-with-seed-phrase/import-with-seed-phrase.component.js
+++ ui/app/pages/first-time-flow/create-password/import-with-seed-phrase/import-with-seed-phrase.component.js
@@ -175,6 +175,12 @@ export default class ImportWithSeedPhrase extends PureComponent {
     return !passwordError && !confirmPasswordError && !seedPhraseError
   }
 
+  onTermsKeyPress = ({key}) => {
+    if (key === ' ' || key === 'Enter') {
+      this.toggleTermsCheck()
+    }
+  }
+
   toggleTermsCheck = () => {
     this.context.metricsEvent({
       eventOpts: {
@@ -183,7 +189,6 @@ export default class ImportWithSeedPhrase extends PureComponent {
         name: 'Check ToS',
       },
     })
-
     this.setState((prevState) => ({
       termsChecked: !prevState.termsChecked,
     }))
@@ -267,10 +272,17 @@ export default class ImportWithSeedPhrase extends PureComponent {
           largeLabel
         />
         <div className="first-time-flow__checkbox-container" onClick={this.toggleTermsCheck}>
-          <div className="first-time-flow__checkbox">
+          <div
+            className="first-time-flow__checkbox"
+            tabIndex="0"
+            role="checkbox"
+            onKeyPress={this.onTermsKeyPress}
+            aria-checked={termsChecked}
+            aria-labelledby="ftf-chk1-label"
+          >
             {termsChecked ? <i className="fa fa-check fa-2x" /> : null}
           </div>
-          <span className="first-time-flow__checkbox-label">
+          <span id="ftf-chk1-label" className="first-time-flow__checkbox-label">
             I have read and agree to the <a
               href="https://metamask.io/terms.html"
               target="_blank"
