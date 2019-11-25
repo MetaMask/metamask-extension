1501742d68888ae6a1dc3de1a98794f061110716
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2019-11-25 10:02:09 -0330

    Convert dev scripts to use JSX (#7555)

--- development/selector.js
+++ development/selector.js
@@ -1,42 +1,49 @@
-const Component = require('react').Component
-const h = require('react-hyperscript')
-const inherits = require('util').inherits
+import PropTypes from 'prop-types'
+import React, {Component} from 'react'
 
-module.exports = NewComponent
+export default class Selector extends Component {
+  state = {}
 
-inherits(NewComponent, Component)
-function NewComponent () {
-  Component.call(this)
+  render () {
+    const {
+      states,
+      selectedKey,
+      actions,
+      store,
+      modifyBackgroundConnection,
+      backGroundConnectionModifiers,
+    } = this.props
+    const selected = this.state.selected || selectedKey
+
+    return (
+      <select
+        style={{ margin: '20px 20px 0px' }}
+        value={selected}
+        onChange={(event) => {
+          const selectedKey = event.target.value
+          const backgroundConnectionModifier = backGroundConnectionModifiers[selectedKey]
+          modifyBackgroundConnection(backgroundConnectionModifier || {})
+          store.dispatch(actions.update(selectedKey))
+          this.setState({ selected: selectedKey })
+        }}
+      >
+        {Object.keys(states).map((stateName, index) => {
+          return (
+            <option key={index} value={stateName}>
+              {stateName}
+            </option>
+          )
+        })}
+      </select>
+    )
+  }
 }
 
-NewComponent.prototype.render = function () {
-  const props = this.props
-  const {
-    states,
-    selectedKey,
-    actions,
-    store,
-    modifyBackgroundConnection,
-    backGroundConnectionModifiers,
-  } = props
-
-  const state = this.state || {}
-  const selected = state.selected || selectedKey
-
-  return h('select', {
-    style: {
-      margin: '20px 20px 0px',
-    },
-    value: selected,
-    onChange: (event) => {
-      const selectedKey = event.target.value
-      const backgroundConnectionModifier = backGroundConnectionModifiers[selectedKey]
-      modifyBackgroundConnection(backgroundConnectionModifier || {})
-      store.dispatch(actions.update(selectedKey))
-      this.setState({ selected: selectedKey })
-    },
-  }, Object.keys(states).map((stateName) => {
-    return h('option', { value: stateName }, stateName)
-  }))
-
+Selector.propTypes = {
+  states: PropTypes.object.isRequired,
+  selectedKey: PropTypes.string.isRequired,
+  actions: PropTypes.object.isRequired,
+  store: PropTypes.object.isRequired,
+  modifyBackgroundConnection: PropTypes.func.isRequired,
+  backGroundConnectionModifiers: PropTypes.object.isRequired,
 }
