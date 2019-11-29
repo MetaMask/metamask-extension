98c8485687bd3e244809cb89329a1659fa13277b
Mark Stacey <markjstacey@gmail.com>
2019-11-28 22:07:54 -0400

    Ensure TAB_ID_NONE is not mistaken for a real tab id

--- app/scripts/lib/createOnboardingMiddleware.js
+++ app/scripts/lib/createOnboardingMiddleware.js
@@ -1,4 +1,5 @@
 import log from 'loglevel'
+import extension from 'extensionizer'
 
 /**
  * Returns a middleware that intercepts `wallet_registerOnboarding` messages
@@ -12,7 +13,7 @@ function createOnboardingMiddleware ({ location, tabId, registerOnboarding }) {
         next()
         return
       }
-      if (tabId) {
+      if (tabId && tabId !== extension.tabs.TAB_ID_NONE) {
         await registerOnboarding(location, tabId)
       } else {
         log.debug(`'wallet_registerOnboarding' message from ${location} ignored due to missing tabId`)
