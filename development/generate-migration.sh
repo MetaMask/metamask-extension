541467d3aa7aa0ddf31f2d8c46a58442c13e26c9
Frankie <frankie.diamond@gmail.com>
2019-12-05 10:39:15 -1000

    make shellCheck happy

--- development/generate-migration.sh
+++ development/generate-migration.sh
@@ -5,11 +5,11 @@ g-migration() {
   if (($1 < 100)); then
     vnum=0$1
   fi
-  touch app/scripts/migrations/$vnum.js
-  cp app/scripts/migrations/template.js app/scripts/migrations/$vnum.js
+  touch app/scripts/migrations/"$vnum".js
+  cp app/scripts/migrations/template.js app/scripts/migrations/"$vnum".js
 
-  touch test/unit/migrations/$vnum.js
-  cp test/unit/migrations/template-test.js test/unit/migrations/$vnum-test.js
+  touch test/unit/migrations/"$vnum".js
+  cp test/unit/migrations/template-test.js test/unit/migrations/"$vnum"-test.js
 }
 
-g-migration $1
+g-migration "$1"
