4fef8f781080ad3270bf9b1f9f8d6020ec19df09
rickycodes <ricky.miller@gmail.com>
2019-12-07 10:22:09 -0500

    destructure props

--- ui/app/components/ui/icon/icon.stories.js
+++ ui/app/components/ui/icon/icon.stories.js
@@ -1,46 +1,50 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import Icon from './icon.component'
-import { color } from '@storybook/addon-knobs/react'
+import { color, number } from '@storybook/addon-knobs/react'
 
 storiesOf('Icon', module)
   .add('Icon - Send', () => (
     <Icon
       type="send"
-      width={28}
-      height={28}
-      borderWidth={1}
-      borderRadius={18}
-      color={color('Blue', '#2F80ED')}
+      width={number('width', 28,)}
+      height={number('height', 28)}
+      color={color('color', '#2F80ED')}
+      // this seems like a bad idea now?
+      // at first these options seemed useful,
+      // but now I am thinking they should just be flat svgs?
+      // (with no extra div for outer border)
+      borderWidth={number('borderWidth', 1)}
+      borderRadius={number('borderRadius', 18)}
     />
   ))
   .add('Icon - Recieve', () => (
     <Icon
       type="recieve"
-      width={28}
-      height={28}
-      borderWidth={1}
-      borderRadius={18}
-      color={color('Blue', '#2F80ED')}
+      width={number('width', 28)}
+      height={number('height', 28)}
+      color={color('color', '#2F80ED')}
+      borderWidth={number('borderWidth', 1)}
+      borderRadius={number('borderRadius', 18)}
     />
   ))
   .add('Icon - Site Interaction', () => (
     <Icon
       type="interaction"
-      width={28}
-      height={28}
-      borderWidth={1}
-      borderRadius={18}
-      color={color('Blue', '#2F80ED')}
+      width={number('width', 28)}
+      height={number('height', 28)}
+      color={color('color', '#2F80ED')}
+      borderWidth={number('borderWidth', 1)}
+      borderRadius={number('borderRadius', 18)}
     />
   ))
   .add('Icon - Approve Send Limit', () => (
     <Icon
       type="approve"
-      width={28}
-      height={28}
-      borderWidth={1}
-      borderRadius={18}
-      color={color('Blue', '#2F80ED')}
+      width={number('width', 28)}
+      height={number('height', 28)}
+      color={color('color', '#2F80ED')}
+      borderWidth={number('borderWidth', 1)}
+      borderRadius={number('borderRadius', 18)}
     />
   ))
