c7eaf479aeb9a596b37c3e21d30c22d2312d0bbb
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2019-12-09 13:09:46 -0330

    Backport "Clean up list of available currencies (#7667)"
    This is backported to make it easier to backport #7986

--- test/unit/balance-formatter-test.js
+++ test/unit/balance-formatter-test.js
@@ -1,13 +1,13 @@
 const assert = require('assert')
 const currencyFormatter = require('currency-formatter')
-const infuraConversion = require('../../ui/app/helpers/constants/infura-conversion.json')
+const availableCurrencies = require('../../ui/app/helpers/constants/available-conversions.json')
 
 describe('currencyFormatting', function () {
   it('be able to format any infura currency', function (done) {
     const number = 10000
 
-    infuraConversion.objects.forEach((conversion) => {
-      const code = conversion.quote.code.toUpperCase()
+    availableCurrencies.forEach((conversion) => {
+      const code = conversion.code.toUpperCase()
       const result = currencyFormatter.format(number, { code })
 
       switch (code) {
