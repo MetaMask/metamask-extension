7327c8d9e383fbe44ded176fb17baf37e9c3ba13
rickycodes <ricky.miller@gmail.com>
2019-12-11 12:56:52 -0500

    Set some flags

--- development/shellcheck.sh
+++ development/shellcheck.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+set -e
+set -u
+set -x
+set -o pipefail
+
 # lint all *.sh files
 shellcheck --version && find . -type f -name '*.sh' ! -path './node_modules/*' -print0 | xargs -0 shellcheck
 # lint all .scripts in package.json
