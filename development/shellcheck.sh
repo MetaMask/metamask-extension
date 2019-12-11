db73d36eedd3943790dcb215cf05a67a4599d7fe
rickycodes <ricky.miller@gmail.com>
2019-12-11 12:58:40 -0500

    Put shellcheck --version on new line

--- development/shellcheck.sh
+++ development/shellcheck.sh
@@ -5,8 +5,9 @@ set -u
 set -x
 set -o pipefail
 
+shellcheck --version
 # lint all *.sh files
-shellcheck --version && find . -type f -name '*.sh' ! -path './node_modules/*' -print0 | xargs -0 shellcheck
+find . -type f -name '*.sh' ! -path './node_modules/*' -print0 | xargs -0 shellcheck
 # lint all .scripts in package.json
 list=$(jq -r '.scripts | keys[] as $key | .[$key]' < package.json)
 printf "#!/bin/bash\n%s\n" "$list" | shellcheck -
