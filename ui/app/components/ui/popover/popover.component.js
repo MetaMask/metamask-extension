3f5adff808e05ee9804a8dc8cc108b535a5706f2
rickycodes <ricky.miller@gmail.com>
2019-12-17 16:21:13 -0500

    Start breaking things up a bit

--- ui/app/components/ui/popover/popover.component.js
+++ ui/app/components/ui/popover/popover.component.js
@@ -6,6 +6,59 @@ const close = e => {
   console.log(e)
 }
 
+const activities = [
+  "Transaction created with a value of 0.000023 ETH at 09:50 on 8/8/2019.",
+  "Transaction submitted with gas fee of 0 WEI at 09:47 on 8/8/2019.",
+  "Transaction confirmed at 09:45 on 8/8/2019.",
+  "Transaction created with a value of 0.000023 ETH at 09:50 on 8/8/2019.",
+  "Transaction submitted with gas fee of 0 WEI at 09:47 on 8/8/2019.",
+  "Transaction confirmed at 09:45 on 8/8/2019.",
+]
+
+const Activity = () => (
+  <div className="activity">
+    <div className="header">
+      <a href="#" onClick={e => e.preventDefault()}className="etherscan">
+        View on Etherscan
+      </a>
+      <h3>Activity</h3>
+    </div>
+    <ul>
+      <div className="line" />
+      {
+        activities.map(item => (
+          <li><span>{item}</span></li>
+        ))
+      }
+    </ul>
+  </div>
+)
+
+const Details = () => (
+  <div className="details">
+    <div className="details-amounts">
+      <div className="col">
+        Amount<br />
+        Transaction Fee
+      </div>
+      <div className="col">
+        $0.00<br />
+        $0.04
+      </div>
+    </div>
+    <hr />
+    <div className="details-totals">
+      <div className="col">
+        <h3>Total amount</h3>
+      </div>
+      <div className="col">
+        <h3>$0.04</h3>
+        <p>0.000185 ETH</p>
+      </div>
+    </div>
+  </div>
+)
+
 const PopOver = () => (
   <div>
     <div className="popover-content">
@@ -49,45 +102,8 @@ const PopOver = () => (
             <h4>0x may spend up to:<br /> 071,992.54744099 DAI</h4>
           </div>
         </div>
-        <div className="details">
-          <div className="details-amounts">
-            <div className="col">
-              Amount<br />
-              Transaction Fee
-            </div>
-            <div className="col">
-              $0.00<br />
-              $0.04
-            </div>
-          </div>
-          <hr />
-          <div className="details-totals">
-            <div className="col">
-              <h3>Total amount</h3>
-            </div>
-            <div className="col">
-              <h3>$0.04</h3>
-              <p>0.000185 ETH</p>
-            </div>
-          </div>
-        </div>
-        <div className="activity">
-          <div className="header">
-            <a href="#" onClick={e => e.preventDefault()}className="etherscan">
-              View on Etherscan
-            </a>
-            <h3>Activity</h3>
-          </div>
-          <ul>
-            <div className="line" />
-            <li><span>Transaction created with a value of 0.000023 ETH at 09:50 on 8/8/2019.</span></li>
-            <li><span>Transaction submitted with gas fee of 0 WEI at 09:47 on 8/8/2019.</span></li>
-            <li><span>Transaction confirmed at 09:45 on 8/8/2019.</span></li>
-            <li><span>Transaction created with a value of 0.000023 ETH at 09:50 on 8/8/2019.</span></li>
-            <li><span>Transaction submitted with gas fee of 0 WEI at 09:47 on 8/8/2019.</span></li>
-            <li><span>Transaction confirmed at 09:45 on 8/8/2019.</span></li>
-          </ul>
-        </div>
+        <Details />
+        <Activity />
       </div>
     </div>
     <a href="#" className="popover-bg" onClick={close} />
