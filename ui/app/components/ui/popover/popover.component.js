e2351b9d5b2543c5372795e76296773e87047808
rickycodes <ricky.miller@gmail.com>
2019-12-16 22:18:46 -0500

    Begin roughtly filling in popover content

--- ui/app/components/ui/popover/popover.component.js
+++ ui/app/components/ui/popover/popover.component.js
@@ -1,20 +1,54 @@
 import React from 'react'
 // import PropTypes from 'prop-types'
 
+const close = e => console.log(e)
+
 const PopOver = () => (
   <div>
     <div className="popover-content">
       <div className="header">
         <h2>Approve spend limit</h2>
-        <a className="close" onClick={e => console.log(e)}>
+        <a className="close" onClick={close}>
           <div>
             <div className="line a"></div>
             <div className="line b"></div>
           </div>
         </a>
       </div>
+      <div className="container">
+        <div className="popover-item">
+          <div className="col">
+            <h3>STATUS</h3>
+            <h4>Confirmed</h4>
+          </div>
+          <div className="col">
+            <h3>DATE</h3>
+            <h4>Sept 20 at 9:08am</h4>
+          </div>
+        </div>
+        <div className="popover-item">
+          <div className="col">
+            <h3>from</h3>
+            <h4>834759....872345</h4>
+          </div>
+          <div className="col">
+            <h3>to</h3>
+            <h4>834759....872345</h4>
+          </div>
+        </div>
+        <div className="popover-item">
+          <div className="col">
+            <h3>nonce</h3>
+            <h4>#5</h4>
+          </div>
+          <div className="col">
+            <h3>description</h3>
+            <h4>0x may spend up to:<br /> 071,992.54744099 DAI</h4>
+          </div>
+        </div>
+      </div>
     </div>
-    <div className="popover-bg" />
+    <a className="popover-bg" onClick={close} />
   </div>
 )
 
