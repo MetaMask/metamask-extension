0027cc2422a3ec5bd96ed1b1703af672e9ec0030
rickycodes <ricky.miller@gmail.com>
2019-12-18 11:56:35 -0500

    Add fees

--- ui/app/components/ui/popover/popover.stories.js
+++ ui/app/components/ui/popover/popover.stories.js
@@ -27,6 +27,35 @@ const activities = [
   'Transaction confirmed at 09:45 on 8/8/2019.',
 ]
 
+const Fees = () => (
+  <div className="fees">
+    <div className="fees__option fast selected">
+      <h3>Fast</h3>
+      <h4>2m 30s</h4>
+      <p>
+        $0.20
+        <span className="eth">0.00354 ETH</span>
+      </p>
+    </div>
+    <div className="fees__option faster">
+      <h3>Faster</h3>
+      <h4>1m 30s</h4>
+      <p>
+        $0.30
+        <span className="eth">0.00456 ETH</span>
+      </p>
+    </div>
+    <div className="fees__option fastest">
+      <h3>Fastest</h3>
+      <h4>30s</h4>
+      <p>
+        $0.50
+        <span className="eth">0.00567 ETH</span>
+      </p>
+    </div>
+  </div>
+)
+
 const Activity = () => (
   <div className="activity">
     <div className="header">
@@ -123,6 +152,7 @@ storiesOf('PopOver', module)
           <TabPanel>
             <div className="container">
               <h3>Select a higher fee to accelerate your transaction.</h3>
+              <Fees />
               <Details />
             </div>
           </TabPanel>
