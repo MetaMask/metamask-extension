7d61c7b2ed3c7e63778db8b20e80aad90a639d20
rickycodes <ricky.miller@gmail.com>
2020-01-09 15:10:05 -0500

    fix lint nits

--- ui/app/components/ui/popover/popover.stories.js
+++ ui/app/components/ui/popover/popover.stories.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { storiesOf } from '@storybook/react'
 import PopOver from './popover.component'
 import TransactionDetails from '../transaction-details'
@@ -73,7 +72,7 @@ const Activity = () => (
             <div className="line" />
             <div className="bullet">
               <svg width="7" height="7" viewBox="0 0 7 7" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="3.5" cy="3.5" r="3.5" fill="#037DD6"/>
+                <circle cx="3.5" cy="3.5" r="3.5" fill="#037DD6" />
               </svg>
             </div>
             <span>{item}</span>
