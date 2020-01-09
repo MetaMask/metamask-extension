56b4c80b650ac6e203d50fd647b0e68abf0bc084
rickycodes <ricky.miller@gmail.com>
2020-01-09 14:04:34 -0500

    Fix up bullets for unordered list

--- ui/app/components/ui/popover/popover.stories.js
+++ ui/app/components/ui/popover/popover.stories.js
@@ -67,7 +67,15 @@ const Activity = () => (
     <ul>
       {
         activities.map((item, index) => (
-          <li key={index}><div className="line" /><span>{item}</span></li>
+          <li key={index}>
+            <div className="line" />
+            <div className="bullet">
+              <svg width="7" height="7" viewBox="0 0 7 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="3.5" cy="3.5" r="3.5" fill="#037DD6"/>
+              </svg>
+            </div>
+            <span>{item}</span>
+          </li>
         ))
       }
     </ul>
