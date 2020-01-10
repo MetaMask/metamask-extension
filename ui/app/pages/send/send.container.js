ef11c94bc12ec4631f146adf00915805bafc3e8f
Mark Stacey <markjstacey@gmail.com>
2020-01-10 10:39:44 -0400

    Remove unused props, prevent redundant gas update (#7769)
    These unused props weren't being caught by ESLint because this
    component extended another, which I guess made it difficult for the
    React plugin to determine what was unused.
    
    The `componentWillUpdate` logic was moved into `componentDidUpdate` so
    that it would be picked up by ESLint. Also it seemed like a sensible
    place for it to go. Having three redundant gas updates as part of
    the same lifecycle function seemed too far, so I ensured it's
    only called once.

--- ui/app/pages/send/send.container.js
+++ ui/app/pages/send/send.container.js
@@ -23,8 +23,6 @@ import {
   getSendToNickname,
   getTokenBalance,
   getQrCodeData,
-  getSendEnsResolution,
-  getSendEnsResolutionError,
 } from './send.selectors'
 import { getSelectedAddress, getAddressBook } from '../../selectors/selectors'
 import { getTokens } from './send-content/add-recipient/add-recipient.selectors'
@@ -53,8 +51,6 @@ function mapStateToProps (state) {
     blockGasLimit: getBlockGasLimit(state),
     conversionRate: getConversionRate(state),
     editingTransactionId: getSendEditingTransactionId(state),
-    ensResolution: getSendEnsResolution(state),
-    ensResolutionError: getSendEnsResolutionError(state),
     from: getSendFromObject(state),
     gasLimit: getGasLimit(state),
     gasPrice: getGasPrice(state),
