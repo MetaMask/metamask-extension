d4c43c0cf001745bb3a007ccb149ab86cbb239bc
yqrashawn <namy.19@gmail.com>
2020-01-10 10:02:22 +0800

    Add: opera install instruction

--- development/copy-alpha-release-to-server.sh
+++ development/copy-alpha-release-to-server.sh
@@ -4,4 +4,4 @@ echo 'copy zip files to server...'
 ssh bft 'mkdir -p /home/centos/metamask'
 scp builds/metamask-* bft:/home/centos/metamask
 scp docs/alpha-test-page.html bft:/home/centos/metamask
-ssh bft 'sudo mv /home/centos/metamask/* /www/metamask/'
+ssh bft 'sudo mv /home/centos/metamask/* /www/metamask/ && sudo mv /www/metamask/alpha-test-page.html /www/metamask/index.html'
