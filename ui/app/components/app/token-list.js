60d14ff875a8b085c8c0246b30469c5f282ee6c5
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2020-01-13 11:39:38 -0330

    Convert TokenList component to an ES6 class (#7785)
    Co-authored-by: Mark Stacey <markjstacey@gmail.com>

--- ui/app/components/app/token-list.js
+++ ui/app/components/app/token-list.js
@@ -2,7 +2,6 @@ import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import TokenCell from './token-cell'
 import fcAbi from 'cfx-fc-abi'
-import { inherits } from 'util'
 import TokenTracker from '@yqrashawn/cfx-token-tracker'
 import { connect } from 'react-redux'
 import { getSelectedAddress } from '../../selectors/selectors'
@@ -18,7 +17,7 @@ function mapStateToProps (state) {
 }
 
 const defaultTokens = []
-import contracts from '@yqrashawn/cfx-contract-metadata'
+import contracts from 'eth-contract-metadata'
 
 for (const address in contracts) {
   const contract = contracts[address]
@@ -28,162 +27,168 @@ for (const address in contracts) {
   }
 }
 
-TokenList.contextTypes = {
-  t: PropTypes.func,
-}
+class TokenList extends Component {
+  static contextTypes = {
+    t: PropTypes.func,
+  }
 
-export default connect(mapStateToProps)(TokenList)
+  static propTypes = {
+    tokens: PropTypes.array.isRequired,
+    userAddress: PropTypes.string.isRequired,
+    network: PropTypes.string.isRequired,
+    assetImages: PropTypes.object.isRequired,
+  }
 
-inherits(TokenList, Component)
-function TokenList () {
-  this.state = {
+  state = {
     tokens: [],
     isLoading: true,
-    network: null,
-  }
-  Component.call(this)
-}
-
-TokenList.prototype.render = function TokenList () {
-  const { userAddress, assetImages } = this.props
-  const state = this.state
-  const { tokens, isLoading, error } = state
-  if (isLoading) {
-    return (
-      <div
-        style={{
-          display: 'flex',
-          height: '250px',
-          alignItems: 'center',
-          justifyContent: 'center',
-          padding: '30px',
-        }}
-      >
-        {this.context.t('loadingTokens')}
-      </div>
-    )
   }
 
-  if (error) {
-    log.error(error)
-    return (
-      <div
-        className="hotFix"
-        style={{
-          padding: '80px',
-        }}
-      >
-        {this.context.t('troubleTokenBalances')}
-        <span
-          className="hotFix"
-          style={{
-            color: 'rgba(247, 134, 28, 1)',
-            cursor: 'pointer',
-          }}
-          onClick={() => {
-            global.platform.openWindow({
-              url: `https://ethplorer.io/address/${userAddress}`,
-            })
-          }}
-        >
-          {this.context.t('here')}
-        </span>
-      </div>
-    )
+  createFreshTokenTracker () {
+    if (this.tracker) {
+      // Clean up old trackers when refreshing:
+      this.tracker.stop()
+      this.tracker.removeListener('update', this.balanceUpdater)
+      this.tracker.removeListener('error', this.showError)
+    }
+
+    if (!global.ethereumProvider) {
+      return
+    }
+    const { userAddress } = this.props
+
+    this.tracker = new TokenTracker({
+      userAddress,
+      provider: global.ethereumProvider,
+      tokens: this.props.tokens.map(token => {
+        if (token.symbol === 'FC') {
+          token.abi = fcAbi
+        }
+        return token
+      }),
+      pollingInterval: 8000,
+    })
+
+    // Set up listener instances for cleaning up
+    this.balanceUpdater = this.updateBalances.bind(this)
+    this.showError = error => {
+      this.setState({ error, isLoading: false })
+    }
+    this.tracker.on('update', this.balanceUpdater)
+    this.tracker.on('error', this.showError)
+
+    this.tracker
+      .updateBalances()
+      .then(() => {
+        this.updateBalances(this.tracker.serialize())
+      })
+      .catch(reason => {
+        log.error(`Problem updating balances`, reason)
+        this.setState({ isLoading: false })
+      })
   }
 
-  return (
-    <div>
-      {tokens.map((tokenData, index) => {
-        tokenData.image = assetImages[tokenData.address]
-        return <TokenCell key={index} {...tokenData} />
-      })}
-    </div>
-  )
-}
+  updateBalances = function (tokens) {
+    if (!this.tracker.running) {
+      return
+    }
+    this.setState({ tokens, isLoading: false })
+  }
 
-TokenList.prototype.componentDidMount = function () {
-  this.createFreshTokenTracker()
-}
+  componentDidMount () {
+    this.createFreshTokenTracker()
+  }
 
-TokenList.prototype.createFreshTokenTracker = function () {
-  if (this.tracker) {
-    // Clean up old trackers when refreshing:
+  componentDidUpdate (prevProps) {
+    const { network: oldNet, userAddress: oldAddress, tokens } = prevProps
+    const {
+      network: newNet,
+      userAddress: newAddress,
+      tokens: newTokens,
+    } = this.props
+
+    const isLoading = newNet === 'loading'
+    const missingInfo = !oldNet || !newNet || !oldAddress || !newAddress
+    const sameUserAndNetwork = oldAddress === newAddress && oldNet === newNet
+    const shouldUpdateTokens = isLoading || missingInfo || sameUserAndNetwork
+
+    const oldTokensLength = tokens ? tokens.length : 0
+    const tokensLengthUnchanged = oldTokensLength === newTokens.length
+
+    if (tokensLengthUnchanged && shouldUpdateTokens) {
+      return
+    }
+
+    this.setState({ isLoading: true })
+    this.createFreshTokenTracker()
+  }
+
+  componentWillUnmount () {
+    if (!this.tracker) {
+      return
+    }
     this.tracker.stop()
     this.tracker.removeListener('update', this.balanceUpdater)
     this.tracker.removeListener('error', this.showError)
   }
 
-  if (!global.ethereumProvider) {
-    return
+  render () {
+    const { userAddress, assetImages } = this.props
+    const state = this.state
+    const { tokens, isLoading, error } = state
+    if (isLoading) {
+      return (
+        <div
+          style={{
+            display: 'flex',
+            height: '250px',
+            alignItems: 'center',
+            justifyContent: 'center',
+            padding: '30px',
+          }}
+        >
+          {this.context.t('loadingTokens')}
+        </div>
+      )
+    }
+
+    if (error) {
+      log.error(error)
+      return (
+        <div
+          className="hotFix"
+          style={{
+            padding: '80px',
+          }}
+        >
+          {this.context.t('troubleTokenBalances')}
+          <span
+            className="hotFix"
+            style={{
+              color: 'rgba(247, 134, 28, 1)',
+              cursor: 'pointer',
+            }}
+            onClick={() => {
+              global.platform.openWindow({
+                url: `https://ethplorer.io/address/${userAddress}`,
+              })
+            }}
+          >
+            {this.context.t('here')}
+          </span>
+        </div>
+      )
+    }
+
+    return (
+      <div>
+        {tokens.map((tokenData, index) => {
+          tokenData.image = assetImages[tokenData.address]
+          return <TokenCell key={index} {...tokenData} />
+        })}
+      </div>
+    )
   }
-  const { userAddress } = this.props
-
-  this.tracker = new TokenTracker({
-    userAddress,
-    provider: global.ethereumProvider,
-    tokens: this.props.tokens.map(token => {
-      if (token.symbol === 'FC') {
-        token.abi = fcAbi
-      }
-      return token
-    }),
-    pollingInterval: 8000,
-  })
-
-  // Set up listener instances for cleaning up
-  this.balanceUpdater = this.updateBalances.bind(this)
-  this.showError = error => {
-    this.setState({ error, isLoading: false })
-  }
-  this.tracker.on('update', this.balanceUpdater)
-  this.tracker.on('error', this.showError)
-
-  this.tracker
-    .updateBalances()
-    .then(() => this.updateBalances(this.tracker.serialize()))
-    .catch(reason => {
-      log.error(`Problem updating balances`, reason)
-      this.setState({ isLoading: false })
-    })
 }
 
-TokenList.prototype.componentDidUpdate = function (prevProps) {
-  const { network: oldNet, userAddress: oldAddress, tokens } = prevProps
-  const {
-    network: newNet,
-    userAddress: newAddress,
-    tokens: newTokens,
-  } = this.props
-
-  const isLoading = newNet === 'loading'
-  const missingInfo = !oldNet || !newNet || !oldAddress || !newAddress
-  const sameUserAndNetwork = oldAddress === newAddress && oldNet === newNet
-  const shouldUpdateTokens = isLoading || missingInfo || sameUserAndNetwork
-
-  const oldTokensLength = tokens ? tokens.length : 0
-  const tokensLengthUnchanged = oldTokensLength === newTokens.length
-
-  if (tokensLengthUnchanged && shouldUpdateTokens) {
-    return
-  }
-
-  this.setState({ isLoading: true })
-  this.createFreshTokenTracker()
-}
-
-TokenList.prototype.updateBalances = function (tokens) {
-  if (!this.tracker.running) {
-    return
-  }
-  this.setState({ tokens, isLoading: false })
-}
-
-TokenList.prototype.componentWillUnmount = function () {
-  if (!this.tracker) {
-    return
-  }
-  this.tracker.stop()
-  this.tracker.removeListener('update', this.balanceUpdater)
-  this.tracker.removeListener('error', this.showError)
-}
+export default connect(mapStateToProps)(TokenList)
