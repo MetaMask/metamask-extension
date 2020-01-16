2d0d2b7a6080aefb7d15a1a0800c20520cf1cef2
yqrashawn <namy.19@gmail.com>
2020-01-16 11:23:16 +0800

    Clean: logs

--- development/prepare-conflux-local-netowrk-lite.js
+++ development/prepare-conflux-local-netowrk-lite.js
@@ -4,7 +4,6 @@ const server = new CGanache()
 server
   .start()
   .then(async () => {
-    console.log('Ganache Testrpc is running on "http://localhost:12539"')
     await server.quit()
   })
   .catch(err => {
