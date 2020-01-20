36749d7a268da22ca56034a3c65073c58bb3edc4
yqrashawn <namy.19@gmail.com>
2020-01-20 11:40:17 +0800

    Docs: move docs to conflux-portal-docs

--- development/copy-alpha-release-to-server.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo 'copy zip files to server...'
-ssh bft 'mkdir -p /home/centos/metamask'
-scp test/e2e/contract-test/* bft:/home/centos/metamask
-ssh bft 'mv /home/centos/metamask/index.html /home/centos/metamask/contract.html'
-scp builds/conflux-portal-* bft:/home/centos/metamask
-scp docs/alpha-test-page.html bft:/home/centos/metamask
-ssh bft 'sudo mv /home/centos/metamask/* /www/metamask/ && sudo mv /www/metamask/alpha-test-page.html /www/metamask/index.html'
