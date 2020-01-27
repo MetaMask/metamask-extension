583b404e02192f87d33b1bb81358ca4e24295354
Mark Stacey <markjstacey@gmail.com>
2020-01-27 12:45:48 -0400

    Switch to full lodash package, and update lodash (#7907)
    * Update lodash
    
    All versions of the full `lodash` package have been updated to 4.17.15.
    The only exception is v4.17.14 which is pinned by `ganache-core`.
    
    * Switch to using `lodash` instead of per-method packages
    
    We have the full lodash package _ten times_ as a production transitive
    dependency, so including per-method packages is not saving space (it
    might instead result in slightly more space being used).

--- ui/app/pages/settings/contact-list-tab/add-contact/add-contact.component.js
+++ ui/app/pages/settings/contact-list-tab/add-contact/add-contact.component.js
@@ -6,7 +6,7 @@ import { CONTACT_LIST_ROUTE } from '../../../../helpers/constants/routes'
 import { isValidAddress, isValidENSAddress } from '../../../../helpers/utils/util'
 import EnsInput from '../../../send/send-content/add-recipient/ens-input'
 import PageContainerFooter from '../../../../components/ui/page-container/page-container-footer'
-import debounce from 'lodash.debounce'
+import { debounce } from 'lodash'
 
 export default class AddContact extends PureComponent {
 
