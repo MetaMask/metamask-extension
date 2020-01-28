4f7d3818a94ea51ca7d855d44380225e2cf7fefe
Mark Stacey <markjstacey@gmail.com>
2020-01-28 17:09:12 -0400

    Improve error handling

--- ui/app/pages/confirm-approve/confirm-approve.util.js
+++ ui/app/pages/confirm-approve/confirm-approve.util.js
@@ -6,7 +6,7 @@ export function getCustomTxParamsData (data, { customPermissionAmount, decimals 
   const tokenData = getTokenData(data)
 
   if (!tokenData) {
-    throw new Error(`Invalid data`)
+    throw new Error('Invalid data')
   } else if (tokenData.name !== 'approve') {
     throw new Error(`Invalid data; should be 'approve' method, but instead is '${tokenData.name}'`)
   }
@@ -16,12 +16,18 @@ export function getCustomTxParamsData (data, { customPermissionAmount, decimals 
   }
   const [signature, tokenValue] = data.split(spender)
 
+  if (!signature || !tokenValue) {
+    throw new Error('Invalid data')
+  } else if (tokenValue.length !== 64) {
+    throw new Error('Invalid token value; should be exactly 64 hex digits long (u256)')
+  }
+
   let customPermissionValue = decimalToHex(calcTokenValue(customPermissionAmount, decimals))
-  if (customPermissionValue.length > tokenValue.length) {
-    throw new Error('Custom value too large')
+  if (customPermissionValue.length > 64) {
+    throw new Error('Custom value is larger than u256')
   }
 
   customPermissionValue = customPermissionValue.padStart(tokenValue.length, '0')
-  const customTxParamsData = signature + spender + customPermissionValue
+  const customTxParamsData = `${signature}${spender}${customPermissionValue}`
   return customTxParamsData
 }
