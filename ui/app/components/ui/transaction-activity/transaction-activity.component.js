4ae446ada272d8e804a430ab834892a69a4aa005
rickycodes <ricky.miller@gmail.com>
2020-02-04 11:24:44 -0500

    Adjust transaction-activity SCSS

--- ui/app/components/ui/transaction-activity/transaction-activity.component.js
+++ ui/app/components/ui/transaction-activity/transaction-activity.component.js
@@ -11,19 +11,19 @@ const activities = [
 ]
 
 const TransactionActivity = () => (
-  <div className="activity">
-    <div className="header">
-      <a href="#" onClick={e => e.preventDefault()} className="etherscan">
+  <div className="transaction-activity">
+    <div className="transaction-activity__header">
+      <a className="transaction-activity__etherscan" href="#" onClick={e => e.preventDefault()}>
         View on Etherscan
       </a>
-      <h3>Activity</h3>
+      <h3 className="transaction-activity__heading">Activity</h3>
     </div>
-    <ul>
+    <ul className="transaction-activity__list">
       {
         activities.map((item, index) => (
-          <li key={index}>
-            <div className="line" />
-            <div className="bullet">
+          <li className="transaction-activity__list-item" key={index}>
+            <div className="transaction-activity__line" />
+            <div className="transaction-activity__bullet">
               <svg width="7" height="7" viewBox="0 0 7 7" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <circle cx="3.5" cy="3.5" r="3.5" fill="#037DD6" />
               </svg>
