351bca8040d8c1ab43635b134e87b8d315aa5b22
rickycodes <ricky.miller@gmail.com>
2020-02-05 11:17:05 -0500

    Update fees SCSS

--- ui/app/components/ui/transaction-fees/transaction-fees.component.js
+++ ui/app/components/ui/transaction-fees/transaction-fees.component.js
@@ -3,27 +3,27 @@ import React from 'react'
 const TransactionFees = () => (
   <div className="fees">
     <div className="fees__option fast fees__option--selected">
-      <h3>Fast</h3>
-      <h4>2m 30s</h4>
-      <p>
+      <h3 className="fees__heading">Fast</h3>
+      <h4 className="fees__subheading">2m 30s</h4>
+      <p className="fees__wrap">
         $0.20
-        <span className="eth">0.00354 ETH</span>
+        <span className="fees__eth">0.00354 ETH</span>
       </p>
     </div>
     <div className="fees__option faster">
-      <h3>Faster</h3>
-      <h4>1m 30s</h4>
-      <p>
+      <h3 className="fees__heading">Faster</h3>
+      <h4 className="fees__subheading">1m 30s</h4>
+      <p className="fees__wrap">
         $0.30
-        <span className="eth">0.00456 ETH</span>
+        <span className="fees__eth">0.00456 ETH</span>
       </p>
     </div>
     <div className="fees__option fastest">
-      <h3>Fastest</h3>
-      <h4>30s</h4>
-      <p>
+      <h3 className="fees__heading">Fastest</h3>
+      <h4 className="fees__subheading">30s</h4>
+      <p className="fees__wrap">
         $0.50
-        <span className="eth">0.00567 ETH</span>
+        <span className="fees__eth">0.00567 ETH</span>
       </p>
     </div>
   </div>
