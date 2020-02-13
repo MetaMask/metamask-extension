ca95e850a894b2bf067231a8d4673cfd2454c5b0
Dan Finlay <dan@danfinlay.com>
2020-02-13 13:29:29 -0800

    Add warning when watchAsset attempts to reuse token symbol

--- ui/app/pages/confirm-add-suggested-token/confirm-add-suggested-token.component.js
+++ ui/app/pages/confirm-add-suggested-token/confirm-add-suggested-token.component.js
@@ -37,6 +37,7 @@ export default class ConfirmAddSuggestedToken extends Component {
     const pendingTokenKey = Object.keys(pendingTokens)[0]
     const pendingToken = pendingTokens[pendingTokenKey]
     const hasTokenDuplicates = this.checkTokenDuplicates(pendingTokens, tokens);
+    const reusesName = this.checkNameReuse(pendingTokens, tokens);
 
     return (
       <div className="page-container">
@@ -50,6 +51,11 @@ export default class ConfirmAddSuggestedToken extends Component {
           { hasTokenDuplicates ?
               <div className="warning">
                 { this.context.t('knownTokenWarning') }
+              </div> : null
+          }
+          { reusesName ?
+              <div className="warning">
+                { this.context.t('reusedTokenNameWarning') }
               </div> : null
           }
         </div>
@@ -134,6 +140,23 @@ export default class ConfirmAddSuggestedToken extends Component {
     })
 
     return dupes.length > 0;
+  }
+
+  /**
+   * Returns true if any pendingTokens both:
+   * - Share a symbol with an existing `tokens` member.
+   * - Does not share an address with that same `tokens` member.
+   * This should be flagged as possibly deceptive or confusing.
+   */
+  checkNameReuse(pendingTokens, tokens) {
+    const duplicates = Object.keys(pendingTokens)
+      .map((addr) => pendingTokens[addr])
+      .filter((token) => {
+        const dupes = tokens.filter(old => old.symbol === token.symbol)
+          .filter(old => old.address !== token.address);
+        return dupes.length > 0;
+      })
+    return duplicates.length > 0;
   }
 
 }
