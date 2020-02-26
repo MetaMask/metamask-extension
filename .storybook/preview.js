b6487f08b7e86518ab4354f6b7c586132ee07d83
Mark Stacey <markjstacey@gmail.com>
2020-02-26 13:40:53 -0400

    Allow changing Storybook preview backgrounds (#8111)
    The `@storybook/addon-backgrounds' addon has been added, which allows
    changing the component preview background. By default no background
    colors are defined, though there is a helpful grid button. A "light"
    and "dark" background option has been added globally. Additional
    story-specific backgrounds or global backgrounds can be added later if
    necessary.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,13 @@
 import React from 'react'
-import { addDecorator } from '@storybook/react'
+import { addDecorator, addParameters } from '@storybook/react'
 import { withKnobs } from '@storybook/addon-knobs/react'
+
+addParameters({
+  backgrounds: [
+    { name: 'light', value: '#FFFFFF'},
+    { name: 'dark', value: '#333333' },
+  ],
+})
 
 const styles = {
   height: '100vh',
