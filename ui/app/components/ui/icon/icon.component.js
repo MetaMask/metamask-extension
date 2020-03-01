4c7f01756feee902756c25977c6ac2a23e114abe
rickycodes <ricky.miller@gmail.com>
2020-03-01 16:16:49 -0500

    Add missing className

--- ui/app/components/ui/icon/icon.component.js
+++ ui/app/components/ui/icon/icon.component.js
@@ -41,7 +41,7 @@ const Icon = ({ className, width, height, color, type }) => {
       </svg>
     ),
     copy: (
-      <svg width={width} height={height} viewBox="0 0 11 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg className={className} width={width} height={height} viewBox="0 0 11 11" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path fillRule="evenodd" clipRule="evenodd" d="M0 0H1H9V1H1V9H0V0ZM2 2H11V11H2V2ZM3 3H10V10H3V3Z" fill={color} />
       </svg>
     ),
