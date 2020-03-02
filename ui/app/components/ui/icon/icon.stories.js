1489957c6ecbcba6a207ee3daace172ea0d50441
rickycodes <ricky.miller@gmail.com>
2020-03-02 11:41:08 -0500

    Use component story format

--- ui/app/components/ui/icon/icon.stories.js
+++ ui/app/components/ui/icon/icon.stories.js
@@ -1,46 +1,52 @@
 import React from 'react'
-import { storiesOf } from '@storybook/react'
 import Icon from './icon.component'
 import { color, number } from '@storybook/addon-knobs/react'
 
-storiesOf('Icon', module)
-  .add('Icon - Copy', () => (
-    <Icon
-      type="copy"
-      width={number('width', 28,)}
-      height={number('height', 28)}
-      color={color('color', '#2F80ED')}
-    />
-  ))
-  .add('Icon - Send', () => (
-    <Icon
-      type="send"
-      width={number('width', 28,)}
-      height={number('height', 28)}
-      color={color('color', '#2F80ED')}
-    />
-  ))
-  .add('Icon - Recieve', () => (
-    <Icon
-      type="recieve"
-      width={number('width', 28)}
-      height={number('height', 28)}
-      color={color('color', '#2F80ED')}
-    />
-  ))
-  .add('Icon - Site Interaction', () => (
-    <Icon
-      type="interaction"
-      width={number('width', 28)}
-      height={number('height', 28)}
-      color={color('color', '#2F80ED')}
-    />
-  ))
-  .add('Icon - Approve Send Limit', () => (
-    <Icon
-      type="approve"
-      width={number('width', 28)}
-      height={number('height', 28)}
-      color={color('color', '#2F80ED')}
-    />
-  ))
+export default {
+  title: 'Icon',
+}
+
+export const copy = () => (
+  <Icon
+    type="copy"
+    width={number('width', 28,)}
+    height={number('height', 28)}
+    color={color('color', '#2F80ED')}
+  />
+)
+
+export const send = () => (
+  <Icon
+    type="send"
+    width={number('width', 28,)}
+    height={number('height', 28)}
+    color={color('color', '#2F80ED')}
+  />
+)
+
+export const receive = () => (
+  <Icon
+    type="receive"
+    width={number('width', 28)}
+    height={number('height', 28)}
+    color={color('color', '#2F80ED')}
+  />
+)
+
+export const siteInteraction = () => (
+  <Icon
+    type="interaction"
+    width={number('width', 28)}
+    height={number('height', 28)}
+    color={color('color', '#2F80ED')}
+  />
+)
+
+export const approveSpendLimit = () => (
+  <Icon
+    type="approve"
+    width={number('width', 28)}
+    height={number('height', 28)}
+    color={color('color', '#2F80ED')}
+  />
+)
