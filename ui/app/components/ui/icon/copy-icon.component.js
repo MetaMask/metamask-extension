e637a7c55585a8905cebed174fe9bf458fa17cb2
rickycodes <ricky.miller@gmail.com>
2020-03-02 19:58:50 -0500

    Move icon props to their own line

--- ui/app/components/ui/icon/copy-icon.component.js
+++ ui/app/components/ui/icon/copy-icon.component.js
@@ -7,8 +7,20 @@ const Copy = ({
   height,
   color,
 }) => (
-  <svg className={className} width={width} height={height} viewBox="0 0 11 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path fillRule="evenodd" clipRule="evenodd" d="M0 0H1H9V1H1V9H0V0ZM2 2H11V11H2V2ZM3 3H10V10H3V3Z" fill={color} />
+  <svg
+    className={className}
+    width={width}
+    height={height}
+    viewBox="0 0 11 11"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M0 0H1H9V1H1V9H0V0ZM2 2H11V11H2V2ZM3 3H10V10H3V3Z"
+      fill={color}
+    />
   </svg>
 )
 
