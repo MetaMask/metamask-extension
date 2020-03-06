58c97d595b4e6d3ed38cd547613b4dcf9a3f3e02
Erik Marks <rekmarks@protonmail.com>
2020-03-05 16:57:34 -0800

    set send state from address in confirm containers

--- ui/app/pages/confirm-send-ether/confirm-send-ether.container.js
+++ ui/app/pages/confirm-send-ether/confirm-send-ether.container.js
@@ -18,6 +18,7 @@ const mapDispatchToProps = (dispatch) => {
     editTransaction: (txData) => {
       const { id, txParams } = txData
       const {
+        from,
         gas: gasLimit,
         gasPrice,
         to,
@@ -25,6 +26,7 @@ const mapDispatchToProps = (dispatch) => {
       } = txParams
 
       dispatch(updateSend({
+        from,
         gasLimit,
         gasPrice,
         gasTotal: null,
