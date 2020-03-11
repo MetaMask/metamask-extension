6801dbf6b57963ff50e7462806d42499e4ce46a5
Dan Miller <danjm.com@gmail.com>
2020-03-11 13:04:03 -0230

    Use story component style in alert-message.stories

--- ui/app/components/ui/alert-message/alert-message.stories.js
+++ ui/app/components/ui/alert-message/alert-message.stories.js
@@ -1,37 +1,43 @@
 import React from 'react'
-import { storiesOf } from '@storybook/react'
 import AlertMessage from './alert-message.component'
 
-storiesOf('StyledMessage', module)
-  .add('Success', () => (
-    <AlertMessage
-      type="success"
-      width="365px"
-    >
-      This is a test message.
-    </AlertMessage>
-  ))
-  .add('Danger', () => (
-    <AlertMessage
-      type="danger"
-      width="365px"
-    >
-      This is a test message.
-    </AlertMessage>
-  ))
-  .add('Warning', () => (
-    <AlertMessage
-      type="warning"
-      width="365px"
-    >
-      This is a test message.
-    </AlertMessage>
-  ))
-  .add('Info', () => (
-    <AlertMessage
-      type="info"
-      width="365px"
-    >
-      This is a test message.
-    </AlertMessage>
-  ))
+export default {
+  title: 'StyledMessage',
+}
+
+export const Success = () => (
+  <AlertMessage
+    type="success"
+    width="365px"
+  >
+    This is a test message.
+  </AlertMessage>
+)
+
+export const Warning = () => (
+  <AlertMessage
+    type="warning"
+    width="365px"
+  >
+    This is a test message.
+  </AlertMessage>
+)
+
+export const Info = () => (
+  <AlertMessage
+    type="info"
+    width="365px"
+  >
+    This is a test message.
+  </AlertMessage>
+)
+
+export const Danger = () => (
+  <AlertMessage
+    type="danger"
+    width="365px"
+  >
+    This is a test message.
+  </AlertMessage>
+)
+
