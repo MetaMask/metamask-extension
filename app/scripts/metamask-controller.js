92dff20d9fa5cc2e6eecd83f6228e87981425a97
Erik Marks <rekmarks@protonmail.com>
2020-03-16 14:01:27 -0700

    cleanup

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -614,11 +614,11 @@ export default class MetamaskController extends EventEmitter {
         this.preferencesController.setAddresses(accounts)
         this.selectFirstIdentity()
       }
-      releaseLock()
       return vault
     } catch (err) {
-      releaseLock()
       throw err
+    } finally {
+      releaseLock()
     }
   }
 
@@ -658,11 +658,11 @@ export default class MetamaskController extends EventEmitter {
       // set new identities
       this.preferencesController.setAddresses(accounts)
       this.selectFirstIdentity()
-      releaseLock()
       return vault
     } catch (err) {
-      releaseLock()
       throw err
+    } finally {
+      releaseLock()
     }
   }
 
