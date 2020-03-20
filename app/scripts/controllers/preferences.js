0af02d51942de1b84294b00f3684a2013a8e96a4
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2020-03-20 12:37:27 -0700

    Update address sync logic (#8224)
    * update address sync logic
    
    * error on sync with no addresses

--- app/scripts/controllers/preferences.js
+++ app/scripts/controllers/preferences.js
@@ -302,7 +302,7 @@ class PreferencesController {
     this.store.updateState({ identities, accountTokens })
   }
 
-  /*
+  /**
    * Synchronizes identity entries with known accounts.
    * Removes any unknown identities, and returns the resulting selected address.
    *
@@ -310,6 +310,11 @@ class PreferencesController {
    * @returns {Promise<string>} - selectedAddress the selected address.
    */
   syncAddresses (addresses) {
+
+    if (!Array.isArray(addresses) || addresses.length === 0) {
+      throw new Error('Expected non-empty array of addresses.')
+    }
+
     const { identities, lostIdentities } = this.store.getState()
 
     const newlyLost = {}
