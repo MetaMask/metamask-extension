cee7988c3681481d016f89097c91f0340afb86a1
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2020-03-31 18:34:44 -0230

    Revert popover close button element

--- ui/app/components/ui/popover/popover.component.js
+++ ui/app/components/ui/popover/popover.component.js
@@ -1,31 +1,45 @@
-import React, { PureComponent } from 'react'
+import React, { PureComponent, useContext } from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
+import { I18nContext } from '../../../contexts/i18n'
 
-const Popover = ({ title, subtitle, children, onBack, onClose }) => (
-  <div className="popover-container">
-    <div className="popover-bg" onClick={onClose} />
-    <div className="popover-wrap">
-      <header className="popover-header">
-        <div className="popover-header__title">
-          <h2 title={title}>
-            {
-              onBack
-                ? <i className="fas fa-chevron-left" onClick={onBack} />
-                : null
-            }
-            {title}
-          </h2>
-          <i className="fas fa-times" onClick={onClose} />
+const Popover = ({ title, subtitle, children, onBack, onClose }) => {
+  const t = useContext(I18nContext)
+  return (
+    <div className="popover-container">
+      <div className="popover-bg" onClick={onClose} />
+      <div className="popover-wrap">
+        <header className="popover-header">
+          <div className="popover-header__title">
+            <h2 title={title}>
+              {
+                onBack
+                  ? (
+                    <button
+                      className="fas fa-chevron-left popover-header__button"
+                      title={t('back')}
+                      onClick={onBack}
+                    />
+                  )
+                  : null
+              }
+              {title}
+            </h2>
+            <button
+              className="fas fa-times popover-header__button"
+              title={t('close')}
+              onClick={onClose}
+            />
+          </div>
+          <p className="popover-header__subtitle">{subtitle}</p>
+        </header>
+        <div className="popover-content">
+          {children}
         </div>
-        <p className="popover-header__subtitle">{subtitle}</p>
-      </header>
-      <div className="popover-content">
-        {children}
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 Popover.propTypes = {
   title: PropTypes.string.isRequired,
