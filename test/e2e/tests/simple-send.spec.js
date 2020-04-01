49ebe558ec16d57d4bd58cdede8a2e0dcdaf9e5f
yqrashawn <namy.19@gmail.com>
2020-04-01 10:14:21 +0800

    Add: update local-network-lite to 2.0.3

--- test/e2e/tests/simple-send.spec.js
+++ test/e2e/tests/simple-send.spec.js
@@ -1,13 +1,13 @@
 const { By, Key } = require('selenium-webdriver')
 const { withFixtures } = require('../helpers')
 
-describe('MetaMask Browser Extension', function () {
-  it('can send a simple transaction from one account to another', async function () {
+describe('MetaMask Browser Extension', function() {
+  it('can send a simple transaction from one account to another', async function() {
     const ganacheOptions = {
       accounts: [
         {
           secretKey:
-            '0x7C9529A67102755B7E6102D6D950AC5D5863C98713805CEC576B945B15B71EAC',
+            '0x680031FFE9A0EE33CC25E4214E8793622F43088EC5E91638720C37B2708469B1',
           balance: 25000000000000000000,
         },
       ],
