MetaMask/metamask-extension

commit d1e078b8dec5653a536619274cd33debc20bab22

Connect flow via popup (#8269)
* Connect screen popup redesign

* Open permission request in notification instead of tab

* Remove no longer user locales

* Update permissions unit test mock to accout for change of opts passed to permissions controller

* Lint fix

* Inline broken line svg in permission-page-container-content.component.js for faster loading

* Add back button to second screen on connect flow

* Add xOfY locale and use for the page count in the connect flow

* Lint fix for svgs permission-page-container-content.component.js

* Fix rebase error

* Lint fix

* Clean up styles on the connect-screen-into-popup branch

* Use closeCurrentWindow to close window on cancel when in full screen connect flow

* Handle errors in rejectPermissionsRequest

* Full screen styles for connect flow

* Lint fixed in permissions-connect and actions.js

* Redirect screen now shows metamask icon instead of users identicon

* Fix subtitle spacing in permissions-connect-header'

* Use window.close instead of closeCurrentWindow() in cancelPermissionsRequest

* Use permissions-connect-header__subtitle in permissions-connect-header.component

ui/app/pages/permissions-connect/permissions-connect.component.js
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
-import PermissionsConnectFooter from './permissions-connect-footer'
 import ChooseAccount from './choose-account'
 import { getEnvironmentType } from '../../../../app/scripts/lib/util'
 import {
@@ -28,7 +27,6 @@ export default class PermissionConnect extends Component {
     nativeCurrency: PropTypes.string,
     permissionsRequest: PropTypes.object,
     addressLastConnectedMap: PropTypes.object,
-    requestAccountTabs: PropTypes.object,
     permissionsRequestId: PropTypes.string,
     domains: PropTypes.object,
     history: PropTypes.object.isRequired,
@@ -44,7 +42,6 @@ export default class PermissionConnect extends Component {
     nativeCurrency: '',
     permissionsRequest: undefined,
     addressLastConnectedMap: {},
-    requestAccountTabs: {},
     permissionsRequestId: '',
     domains: {},
     redirecting: false,
@@ -71,7 +68,7 @@ export default class PermissionConnect extends Component {
   }
 
   removeBeforeUnload = () => {
-    if (getEnvironmentType() === ENVIRONMENT_TYPE_FULLSCREEN) {
+    if (getEnvironmentType() === ENVIRONMENT_TYPE_FULLSCREEN || getEnvironmentType() === ENVIRONMENT_TYPE_NOTIFICATION) {
       window.removeEventListener('beforeunload', this.beforeUnload)
     }
   }
@@ -101,8 +98,7 @@ export default class PermissionConnect extends Component {
   }
 
   redirectFlow (accepted) {
-    const { requestAccountTabs, history } = this.props
-    const { originName } = this.state
+    const { history } = this.props
 
     this.setState({
       redirecting: true,
@@ -113,16 +109,12 @@ export default class PermissionConnect extends Component {
     if (getEnvironmentType() === ENVIRONMENT_TYPE_FULLSCREEN) {
       setTimeout(async () => {
         const currentTab = await global.platform.currentTab()
-        try {
-          if (currentTab.active) {
-            await global.platform.switchToTab(requestAccountTabs[originName])
-          }
-        } finally {
-          global.platform.closeTab(currentTab.id)
-        }
+        global.platform.closeTab(currentTab.id)
       }, 2000)
     } else if (getEnvironmentType() === ENVIRONMENT_TYPE_NOTIFICATION) {
-      history.push(DEFAULT_ROUTE)
+      setTimeout(async () => {
+        global.platform.closeCurrentWindow()
+      }, 2000)
     } else if (getEnvironmentType() === ENVIRONMENT_TYPE_POPUP) {
       history.push(CONNECTED_ROUTE)
     }
@@ -142,9 +134,53 @@ export default class PermissionConnect extends Component {
       return history.push(DEFAULT_ROUTE)
     }
 
-    if (getEnvironmentType() === ENVIRONMENT_TYPE_FULLSCREEN) {
+    if (getEnvironmentType() === ENVIRONMENT_TYPE_FULLSCREEN || getEnvironmentType() === ENVIRONMENT_TYPE_NOTIFICATION) {
       window.addEventListener('beforeunload', this.beforeUnload)
     }
+  }
+
+  cancelPermissionsRequest = async (requestId) => {
+    const { rejectPermissionsRequest } = this.props
+    if (requestId) {
+      await rejectPermissionsRequest(requestId)
+
+      if (getEnvironmentType() === ENVIRONMENT_TYPE_FULLSCREEN || getEnvironmentType() === ENVIRONMENT_TYPE_NOTIFICATION) {
+        window.close()
+      } else if (getEnvironmentType() === ENVIRONMENT_TYPE_POPUP) {
+        history.push(DEFAULT_ROUTE)
+      }
+    }
+  }
+
+  goBack () {
+    const { history, connectPath } = this.props
+    history.push(connectPath)
+  }
+
+  renderTopBar () {
+    const { redirecting } = this.state
+    const { page } = this.props
+    const { t } = this.context
+    return !redirecting
+      ? (
+        <div
+          className="permissions-connect__top-bar"
+        >
+          { page === '2'
+            ? (
+              <div className="permissions-connect__back" onClick={() => this.goBack()}>
+                <i className="fas fa-chevron-left" />
+                { t('back') }
+              </div>
+            )
+            : null
+          }
+          <div className="permissions-connect__page-count">
+            { t('xOfY', [ page, '2' ]) }
+          </div>
+        </div>
+      )
+      : null
   }
 
   render () {
@@ -160,7 +196,6 @@ export default class PermissionConnect extends Component {
       permissionsRequestId,
       connectPath,
       confirmPermissionPath,
-      page,
       targetDomainMetadata,
     } = this.props
     const {
@@ -172,70 +207,50 @@ export default class PermissionConnect extends Component {
 
     return (
       <div className="permissions-connect">
-        { !redirecting
-          ? (
-            <div className="permissions-connect__page-count-wrapper">
-              <div className="permissions-connect-header__page-count">
-                { `${page}/2` }
-              </div>
-            </div>
-          )
-          : null
-        }
+        { this.renderTopBar() }
         <Switch>
           <Route
             path={connectPath}
             exact
             render={() => (
-              <div>
-                <ChooseAccount
-                  accounts={accounts}
-                  originName={originName}
-                  nativeCurrency={nativeCurrency}
-                  selectAccounts={(addresses) => this.selectAccounts(addresses)}
-                  selectNewAccountViaModal={(handleAccountClick) => {
-                    showNewAccountModal({
-                      onCreateNewAccount: (address) => handleAccountClick(address),
-                      newAccountNumber,
-                    })
-                  }}
-                  addressLastConnectedMap={addressLastConnectedMap}
-                  cancelPermissionsRequest={(requestId) => {
-                    if (requestId) {
-                      rejectPermissionsRequest(requestId)
-                      this.redirectFlow(false)
-                    }
-                  }}
-                  permissionsRequestId={permissionsRequestId}
-                  selectedAccountAddresses={selectedAccountAddresses}
-                  targetDomainMetadata={targetDomainMetadata}
-                />
-                { !redirecting ? <PermissionsConnectFooter /> : null }
-              </div>
+              <ChooseAccount
+                accounts={accounts}
+                originName={originName}
+                nativeCurrency={nativeCurrency}
+                selectAccounts={(addresses) => this.selectAccounts(addresses)}
+                selectNewAccountViaModal={(handleAccountClick) => {
+                  showNewAccountModal({
+                    onCreateNewAccount: (address) => handleAccountClick(address),
+                    newAccountNumber,
+                  })
+                }}
+                addressLastConnectedMap={addressLastConnectedMap}
+                cancelPermissionsRequest={(requestId) => this.cancelPermissionsRequest(requestId)}
+                permissionsRequestId={permissionsRequestId}
+                selectedAccountAddresses={selectedAccountAddresses}
+                targetDomainMetadata={targetDomainMetadata}
+              />
             )}
           />
           <Route
             path={confirmPermissionPath}
             exact
             render={() => (
-              <div>
-                <PermissionPageContainer
-                  request={permissionsRequest || {}}
-                  approvePermissionsRequest={(request, accounts) => {
-                    approvePermissionsRequest(request, accounts)
-                    this.redirectFlow(true)
-                  }}
-                  rejectPermissionsRequest={(requestId) => {
-                    rejectPermissionsRequest(requestId)
-                    this.redirectFlow(false)
-                  }}
-                  selectedIdentities={accounts.filter((account) => selectedAccountAddresses.has(account.address))}
-                  redirect={redirecting}
-                  permissionRejected={ permissionAccepted === false }
-                  cachedOrigin={originName}
-                />
-                { !redirecting ? <PermissionsConnectFooter /> : null }
-              </div>
+              <PermissionPageContainer
+                request={permissionsRequest || {}}
+                approvePermissionsRequest={(request, accounts) => {
+                  approvePermissionsRequest(request, accounts)
+                  this.redirectFlow(true)
+                }}
+                rejectPermissionsRequest={(requestId) => {
+                  rejectPermissionsRequest(requestId)
+                  this.redirectFlow(false)
+                }}
+                selectedIdentities={accounts.filter((account) => selectedAccountAddresses.has(account.address))}
+                redirect={redirecting}
+                permissionRejected={ permissionAccepted === false }
+                cachedOrigin={originName}
+              />
             )}
           />
         </Switch>

app/images/broken-line.svg
@@ -1,3 +1,0 @@
-<svg width="131" height="2" viewBox="0 0 131 2" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M0 1H134" stroke="#CDD1E4" stroke-linejoin="round" stroke-dasharray="8 7"/>
-</svg>

app/scripts/background.js
@@ -240,6 +240,7 @@ function setupController (initState, initLangCode) {
     // User confirmation callbacks:
     showUnconfirmedMessage: triggerUi,
     showUnapprovedTx: triggerUi,
+    showPermissionRequest: triggerUi,
     openPopup: openPopup,
     closePopup: notificationManager.closePopup.bind(notificationManager),
     // initial state

app/scripts/controllers/permissions/index.js
@@ -29,7 +29,7 @@ export class PermissionsController {
       getUnlockPromise,
       notifyDomain,
       notifyAllDomains,
-      platform,
+      showPermissionRequest,
     } = {},
     restoredPermissions = {},
     restoredState = {}) {
@@ -44,7 +44,7 @@ export class PermissionsController {
     this.getUnlockPromise = getUnlockPromise
     this._notifyDomain = notifyDomain
     this.notifyAllDomains = notifyAllDomains
-    this._platform = platform
+    this._showPermissionRequest = showPermissionRequest
 
     this._restrictedMethods = getRestrictedMethods(this)
     this.permissionsLog = new PermissionsLogController({
@@ -544,7 +544,7 @@ export class PermissionsController {
           )
         }
 
-        this._platform.openExtensionInBrowser(`connect/${id}`)
+        this._showPermissionRequest()
 
         return new Promise((resolve, reject) => {
           this._addPendingApproval(id, origin, resolve, reject)

app/scripts/metamask-controller.js
@@ -216,7 +216,7 @@ export default class MetamaskController extends EventEmitter {
       getUnlockPromise: this.appStateController.getUnlockPromise.bind(this.appStateController),
       notifyDomain: this.notifyConnections.bind(this),
       notifyAllDomains: this.notifyAllConnections.bind(this),
-      platform: opts.platform,
+      showPermissionRequest: opts.showPermissionRequest,
     }, initState.PermissionsController, initState.PermissionsMetadata)
 
     this.detectTokensController = new DetectTokensController({

test/unit/app/controllers/permissions/mocks.js
@@ -25,10 +25,6 @@ export const noop = () => {}
  * Mock Permissions Controller and Middleware
  */
 
-const platform = {
-  openExtensionInBrowser: noop,
-}
-
 const keyringAccounts = deepFreeze([
   '0x0dcd5d886577d5081b0c52e242ef29e70be3e7bc',
   '0xc42edfcc21ed14dda456aa0756c153f7985d8813',
@@ -67,7 +63,7 @@ const getUnlockPromise = () => Promise.resolve()
  */
 export function getPermControllerOpts () {
   return {
-    platform,
+    showPermissionRequest: noop,
     getKeyringAccounts,
     getUnlockPromise,
     getRestrictedMethods,

ui/app/components/app/index.scss
@@ -99,3 +99,5 @@
 @import '../ui/check-box/index';
 
 @import 'permissions-connect-header/index';
+
+@import 'permissions-connect-footer/index';

ui/app/components/app/permission-page-container/index.scss
@@ -2,9 +2,25 @@
   display: flex;
   border: none;
   box-shadow: none;
-  margin-top: 45px;
-  width: 488px;
-  min-height: 468px;
+  width: 100%;
+  margin-top: 2px;
+  height: 100%;
+  flex-direction: column;
+  justify-content: space-between;
+
+  @media screen and (min-width: 576px) {
+    width: 426px;
+    flex: 1;
+
+    &__footers {
+      display: flex;
+      flex-direction: column-reverse;
+      flex: 1;
+      padding-bottom: 20px;
+      justify-content: space-between;
+    }
+  }
+
 
   &__header {
     display: flex;
@@ -25,12 +41,13 @@
   &__content {
     display: flex;
     overflow-y: auto;
-    flex: 1;
     flex-direction: column;
     color: #7C808E;
+    padding-left: 24px;
+    padding-right: 24px;
 
     &--redirect {
-      margin-top: 50px;
+      margin-top: 140px;
       width: 100%;
       display: flex;
       align-items: center;
@@ -51,17 +68,25 @@
 
     &__permission {
       display: flex;
-      margin-top: 18px;
+      align-items: center;
 
       i {
         font-size: 1.4rem;
         color: $Grey-200;
       }
+
       label {
-        margin-left: 6px;
+        font-size: 14px;
+        margin-left: 16px;
         color: $Black-100;
       }
     }
+  }
+
+  &__content-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
 
   &__permissions-header {
@@ -77,23 +102,28 @@
   &__permissions-container {
     display: flex;
     flex-direction: column;
-    margin-top: 33px;
+    margin-top: 38px;
   }
 
   .page-container__footer {
-    border-top: none;
     align-items: center;
+    margin-top: 8px;
+
+    @media screen and (min-width: 576px) {
+      border-top: none;
+    }
 
     footer {
-      width: 300px;
+      width: 100%;
+      justify-content: space-between;
+
+      button {
+        width: 124px;
+      }
     }
   }
 
   @media screen and (max-width: 575px) {
-    width: 100%;
-    margin-top: 25px;
-    padding: 10px;
-
     &__title {
       position: initial;
     }

commit 1ec720e9ce8bd5128f82b4a72b841f91ac4ac2b0

Use correct `history` object on Permissions Connect page
The browser global `history` was being used here instead of the React
Router `history` object. This was accidentally broken when refactoring
in #8269

ui/app/pages/permissions-connect/permissions-connect.component.js
@@ -140,7 +140,7 @@ export default class PermissionConnect extends Component {
   }
 
   cancelPermissionsRequest = async (requestId) => {
-    const { rejectPermissionsRequest } = this.props
+    const { history, rejectPermissionsRequest } = this.props
     if (requestId) {
       await rejectPermissionsRequest(requestId)
 