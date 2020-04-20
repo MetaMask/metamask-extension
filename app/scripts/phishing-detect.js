d8822b7e11f357f53ad942622dc1cdcf6a469832
kumavis <aaron@kumavis.me>
2020-04-20 14:01:02 +0800

    fix: disable dnode weak on nodejs for node@10 support

--- app/scripts/phishing-detect.js
+++ app/scripts/phishing-detect.js
@@ -40,11 +40,13 @@ function start() {
 
 function setupControllerConnection(connectionStream, cb) {
   const eventEmitter = new EventEmitter()
+  // the "weak: false" option is for nodejs only (eg unit tests)
+  // it is a workaround for node v12 support
   const metaMaskControllerDnode = dnode({
     sendUpdate(state) {
       eventEmitter.emit('update', state)
     },
-  })
+  }, { weak: false })
   connectionStream.pipe(metaMaskControllerDnode).pipe(connectionStream)
   metaMaskControllerDnode.once('remote', (backgroundConnection) =>
     cb(null, backgroundConnection),
