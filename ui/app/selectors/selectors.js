06ba0db8409ab7db32c27fca792f3dc7cb8cbd04
Mark Stacey <markjstacey@gmail.com>
2020-04-28 12:36:25 -0300

    Remove fallback selected address (#8439)
    The `getSelectedAddress` selector has a fallback of selecting the first
    MetaMask account. This is not useful. The only time the
    `selectedAddress` is not set is during onboarding, before any accounts
    exist, so selecting the first account wouldn't be useful anyway.
    Co-authored-by: Erik Marks <25517051+rekmarks@users.noreply.github.com>

--- ui/app/selectors/selectors.js
+++ ui/app/selectors/selectors.js
@@ -86,9 +86,7 @@ export const getMetaMaskAccounts = createSelector(
 )
 
 export function getSelectedAddress (state) {
-  const selectedAddress = state.metamask.selectedAddress || Object.keys(getMetaMaskAccounts(state))[0]
-
-  return selectedAddress
+  return state.metamask.selectedAddress
 }
 
 export function getSelectedIdentity (state) {
