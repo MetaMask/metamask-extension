8a12257f8e46d6a19c6d20482f0eed6dae01ed1a
Mark Stacey <markjstacey@gmail.com>
2020-05-06 11:48:38 -0300

    Add dropdown component (#8524)
    This new dropdown component uses a native `select` element, thus
    avoiding various issues encountered in attempting to reuse our existing
    dropdown components for the new permission system alert modal.
    
    The prefixed forms of `appearance` have been added temporarily so that
    the component can be used in Storybook, as our Storybook config isn't
    setup to do autoprefixing yet. Our real build system does handle
    autoprefixing for this rule correctly already.

---
+++ ui/app/components/ui/dropdown/dropdown.stories.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import { action } from '@storybook/addon-actions'
+import Dropdown from '.'
+import { boolean, select, text } from '@storybook/addon-knobs/react'
+
+export default {
+  title: 'Dropdown',
+}
+
+const unnamedOptions = [...Array(10).keys()].map((index) => {
+  return { value: `option${index}` }
+})
+
+const namedOptions = unnamedOptions.map((option, index) => {
+  return Object.assign({}, option, { name: `Option ${index}` })
+})
+
+const namedOptionsWithVeryLongNames = unnamedOptions.map((option, index) => {
+  return Object.assign({}, option, { name: `Option ${index} with a very${', very'.repeat(index)} long name` })
+})
+
+
+export const simple = () => (
+  <Dropdown
+    disabled={boolean('Disabled', false)}
+    title={text('Title', 'Test dropdown name')}
+    onChange={action('Selection changed')}
+    options={namedOptions}
+    required={boolean('Required', false)}
+    selectedOption={
+      select(
+        'Selected Option',
+        namedOptions.map((option) => option.value),
+        namedOptions[0].value
+      )
+    }
+  />
+)
+
+export const optionsWithoutNames = () => (
+  <Dropdown
+    disabled={boolean('Disabled', false)}
+    title={text('Title', 'Test dropdown name')}
+    onChange={action('Selection changed')}
+    options={unnamedOptions}
+    required={boolean('Required', false)}
+    selectedOption={
+      select(
+        'Selected Option',
+        unnamedOptions.map((option) => option.value),
+        unnamedOptions[0].value
+      )
+    }
+  />
+)
+
+export const optionsWithLongNames = () => (
+  <Dropdown
+    disabled={boolean('Disabled', false)}
+    title={text('Title', 'Test dropdown name')}
+    onChange={action('Selection changed')}
+    options={namedOptionsWithVeryLongNames}
+    required={boolean('Required', false)}
+    selectedOption={
+      select(
+        'Selected Option',
+        namedOptionsWithVeryLongNames.map((option) => option.value),
+        namedOptionsWithVeryLongNames[0].value
+      )
+    }
+  />
+)
+
+export const optionsWithLongNamesAndShortWidth = () => (
+  <Dropdown
+    disabled={boolean('Disabled', false)}
+    title={text('Title', 'Test dropdown name')}
+    onChange={action('Selection changed')}
+    options={namedOptionsWithVeryLongNames}
+    required={boolean('Required', false)}
+    selectedOption={
+      select(
+        'Selected Option',
+        namedOptionsWithVeryLongNames.map((option) => option.value),
+        namedOptionsWithVeryLongNames[0].value
+      )
+    }
+    style={{ width: '200px' }}
+  />
+)
