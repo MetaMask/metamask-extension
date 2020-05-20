f886686db2db176b62f05071dc88fee29676ea41
Mark Stacey <markjstacey@gmail.com>
2020-05-20 14:52:23 -0300

    Extract `Menu` component from `ConnectedAccountsListOptions` (#8632)
    A `Menu` component has been extracted from the
    `ConnectedAccountsListOptions` component. The menu and the menu items
    are now the `Menu` and `MenuItem` components respectively.
    
    A custom body was added to the Storybook preview to ensure that the
    `popover-content` element was present in the DOM before the Menu was
    constructed.

--- ui/app/components/app/connected-accounts-list/connected-accounts-list-options/connected-accounts-list-options.component.js
+++ ui/app/components/app/connected-accounts-list/connected-accounts-list-options/connected-accounts-list-options.component.js
@@ -1,34 +1,23 @@
 import PropTypes from 'prop-types'
-import React, { useRef, useState } from 'react'
-import { createPortal } from 'react-dom'
-import { usePopper } from 'react-popper'
+import React, { useState } from 'react'
+import { Menu } from '../../../ui/menu'
 
 const ConnectedAccountsListOptions = ({ children, onShowOptions, onHideOptions, show }) => {
   const [optionsButtonElement, setOptionsButtonElement] = useState(null)
-  const [popperElement, setPopperElement] = useState(null)
-  const popoverContainerElement = useRef(document.getElementById('popover-content'))
 
-  const { attributes, styles } = usePopper(optionsButtonElement, popperElement, {
-    modifiers: [{ name: 'preventOverflow', options: { altBoundary: true } }],
-  })
   return (
     <>
       <button className="fas fa-ellipsis-v connected-accounts-options__button" onClick={onShowOptions} ref={setOptionsButtonElement} />
       {
         show
-          ? createPortal(
-            <>
-              <div className="connected-accounts-options__background" onClick={onHideOptions} />
-              <div
-                className="connected-accounts-options"
-                ref={setPopperElement}
-                style={styles.popper}
-                {...attributes.popper}
-              >
-                { children }
-              </div>
-            </>,
-            popoverContainerElement.current
+          ? (
+            <Menu
+              anchorElement={optionsButtonElement}
+              onHide={onHideOptions}
+              popperOptions={{ modifiers: [{ name: 'preventOverflow', options: { altBoundary: true } }] }}
+            >
+              { children }
+            </Menu>
           )
           : null
       }
