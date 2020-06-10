a4e5fc934dedec74dbb3991de77361cfbb8c302b
Brad Decker <git@braddecker.dev>
2020-06-10 15:38:34 -0500

    restore status tooltip (#8745)
    initially set out to add the failed tooltip back to the transaction list, but
    in the process rediscovered the transaction-status component which illuminated
    a fair number of statuses that were not properly handled by the refactor of the
    list. These statuses were discussed with UX and engineering team members to come
    up with a definitive list of statuses that should be reflected in the UI
    
    Changes:
    1. normalized the color of status labels to use Red-500 and Orange-500 where applicable
    2. added a new color of icon for pending transactions -- grey
    3. added support for dropped and rejected labels
    4. failed, dropped, rejected and cancelled all have red icons now.
    5. cancelled transactions will reflect a change in the user's balance
    6. tooltip displayed for failed transactions
    7. Icon logic isolated to a new component.

--- ui/app/components/app/transaction-status/tests/transaction-status.component.test.js
+++ ui/app/components/app/transaction-status/tests/transaction-status.component.test.js
@@ -1,33 +1,80 @@
 import React from 'react'
 import assert from 'assert'
 import { mount } from 'enzyme'
+import sinon from 'sinon'
+import * as i18nHook from '../../../../hooks/useI18nContext'
 import TransactionStatus from '../transaction-status.component'
 import Tooltip from '../../../ui/tooltip-v2'
 
 describe('TransactionStatus Component', function () {
-  it('should render APPROVED properly', function () {
+  before(function () {
+    sinon.stub(i18nHook, 'useI18nContext').returns((str) => str.toUpperCase())
+  })
+
+  it('should render CONFIRMED properly', function () {
     const wrapper = mount(
       <TransactionStatus
-        statusKey="approved"
-        title="test-title"
-      />,
-      { context: { t: (str) => str.toUpperCase() } }
+        status="confirmed"
+        date="June 1"
+      />
     )
 
     assert.ok(wrapper)
-    assert.equal(wrapper.text(), 'APPROVED')
+    assert.equal(wrapper.text(), 'June 1 · ')
+  })
+
+  it('should render PENDING properly when status is APPROVED', function () {
+    const wrapper = mount(
+      <TransactionStatus
+        status="approved"
+        isEarliestNonce
+        error={{ message: 'test-title' }}
+      />
+    )
+
+    assert.ok(wrapper)
+    assert.equal(wrapper.text(), 'PENDING · ')
     assert.equal(wrapper.find(Tooltip).props().title, 'test-title')
   })
 
-  it('should render SUBMITTED properly', function () {
+  it('should render PENDING properly', function () {
     const wrapper = mount(
       <TransactionStatus
-        statusKey="submitted"
-      />,
-      { context: { t: (str) => str.toUpperCase() } }
+        date="June 1"
+        status="submitted"
+        isEarliestNonce
+      />
     )
 
     assert.ok(wrapper)
-    assert.equal(wrapper.text(), 'PENDING')
+    assert.equal(wrapper.text(), 'PENDING · ')
+  })
+
+  it('should render QUEUED properly', function () {
+    const wrapper = mount(
+      <TransactionStatus
+        status="queued"
+      />
+    )
+
+    assert.ok(wrapper)
+    assert.ok(wrapper.find('.transaction-status--queued').length, 'queued className not found')
+    assert.equal(wrapper.text(), 'QUEUED · ')
+  })
+
+  it('should render UNAPPROVED properly', function () {
+    const wrapper = mount(
+      <TransactionStatus
+        status="unapproved"
+      />
+    )
+
+    assert.ok(wrapper)
+    assert.ok(wrapper.find('.transaction-status--unapproved').length, 'unapproved className not found')
+    assert.equal(wrapper.text(), 'UNAPPROVED · ')
+  })
+
+  after(function () {
+    sinon.restore()
   })
 })
