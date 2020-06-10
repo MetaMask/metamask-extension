MetaMask/metamask-extension

commit 456684ee7d612e47d69556f4b2a6f66e3a271912

move activation logic into token rates controller (#8744)

app/scripts/controllers/token-rates.js
@@ -17,11 +17,10 @@ export default class TokenRatesController {
    *
    * @param {Object} [config] - Options to configure controller
    */
-  constructor ({ interval = DEFAULT_INTERVAL, currency, preferences } = {}) {
+  constructor ({ currency, preferences } = {}) {
     this.store = new ObservableStore()
     this.currency = currency
     this.preferences = preferences
-    this.interval = interval
   }
 
   /**
@@ -51,19 +50,6 @@ export default class TokenRatesController {
   }
 
   /**
-   * @type {Number}
-   */
-  set interval (interval) {
-    this._handle && clearInterval(this._handle)
-    if (!interval) {
-      return
-    }
-    this._handle = setInterval(() => {
-      this.updateExchangeRates()
-    }, interval)
-  }
-
-  /**
    * @type {Object}
    */
   set preferences (preferences) {
@@ -84,5 +70,20 @@ export default class TokenRatesController {
   set tokens (tokens) {
     this._tokens = tokens
     this.updateExchangeRates()
+  }
+
+  start (interval = DEFAULT_INTERVAL) {
+    this._handle && clearInterval(this._handle)
+    if (!interval) {
+      return
+    }
+    this._handle = setInterval(() => {
+      this.updateExchangeRates()
+    }, interval)
+    this.updateExchangeRates()
+  }
+
+  stop () {
+    this._handle && clearInterval(this._handle)
   }
 }

test/unit/app/controllers/token-rates-controller.js
@@ -13,8 +13,11 @@ describe('TokenRatesController', function () {
 
   it('should poll on correct interval', async function () {
     const stub = sinon.stub(global, 'setInterval')
-    new TokenRatesController({ interval: 1337 }) // eslint-disable-line no-new
+    const rateController = new TokenRatesController() // eslint-disable-line no-new
+    rateController.start(1337)
+
     assert.strictEqual(stub.getCall(0).args[1], 1337)
     stub.restore()
+    rateController.stop()
   })
 })

app/scripts/metamask-controller.js
@@ -170,9 +170,11 @@ export default class MetamaskController extends EventEmitter {
       if (activeControllerConnections > 0) {
         this.accountTracker.start()
         this.incomingTransactionsController.start()
+        this.tokenRatesController.start()
       } else {
         this.accountTracker.stop()
         this.incomingTransactionsController.stop()
+        this.tokenRatesController.stop()
       }
     })
 
@@ -281,10 +283,6 @@ export default class MetamaskController extends EventEmitter {
     this.encryptionPublicKeyManager = new EncryptionPublicKeyManager()
     this.typedMessageManager = new TypedMessageManager({ networkController: this.networkController })
 
-    // ensure isClientOpenAndUnlocked is updated when memState updates
-    this.on('update', (memState) => {
-      this.isClientOpenAndUnlocked = memState.isUnlocked && this._isClientOpen
-    })
 
     this.store.updateStructure({
       AppStateController: this.appStateController.store,
@@ -2032,18 +2030,7 @@ export default class MetamaskController extends EventEmitter {
    */
   set isClientOpen (open) {
     this._isClientOpen = open
-    this.isClientOpenAndUnlocked = this.isUnlocked() && open
     this.detectTokensController.isOpen = open
-  }
-
-  /**
-   * A method for activating the retrieval of price data,
-   * which should only be fetched when the UI is visible.
-   * @private
-   * @param {boolean} active - True if price data should be getting fetched.
-   */
-  set isClientOpenAndUnlocked (active) {
-    this.tokenRatesController.isActive = active
   }
 
   /**

commit f9ba57b373e22db12bc85f891393ca15520505e5

Fix `TokenRatesController`
The `TokenRatesController` was accidentally broken in #8744, when the
logic for starting and stopping polling was moved from the `isActive`
property to start/stop functions.

A reference to the now-obsolete `isActive` property was accidentally
left behind, resulting in no exchange rate updates.

app/scripts/controllers/token-rates.js
@@ -27,9 +27,6 @@ export default class TokenRatesController {
    * Updates exchange rates for all tokens
    */
   async updateExchangeRates () {
-    if (!this.isActive) {
-      return
-    }
     const contractExchangeRates = {}
     const nativeCurrency = this.currency ? this.currency.state.nativeCurrency.toLowerCase() : 'eth'
     const pairs = this._tokens.map((token) => token.address).join(',')

test/unit/app/controllers/token-rates-controller.js
@@ -6,18 +6,20 @@ import ObservableStore from 'obs-store'
 describe('TokenRatesController', function () {
   it('should listen for preferences store updates', function () {
     const preferences = new ObservableStore({ tokens: [] })
-    const controller = new TokenRatesController({ preferences })
     preferences.putState({ tokens: ['foo'] })
+    const controller = new TokenRatesController({ preferences })
     assert.deepEqual(controller._tokens, ['foo'])
   })
 
   it('should poll on correct interval', async function () {
     const stub = sinon.stub(global, 'setInterval')
-    const rateController = new TokenRatesController() // eslint-disable-line no-new
-    rateController.start(1337)
+    const preferences = new ObservableStore({ tokens: [] })
+    preferences.putState({ tokens: ['foo'] })
+    const controller = new TokenRatesController({ preferences })
+    controller.start(1337)
 
     assert.strictEqual(stub.getCall(0).args[1], 1337)
     stub.restore()
-    rateController.stop()
+    controller.stop()
   })
 })