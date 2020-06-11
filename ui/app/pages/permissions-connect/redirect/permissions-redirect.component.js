14a5e77edd8644590a9f1b64168fd3e31b53172f
Erik Marks <rekmarks@protonmail.com>
2020-06-11 13:56:29 -0700

    fix classes

--- ui/app/pages/permissions-connect/redirect/permissions-redirect.component.js
+++ ui/app/pages/permissions-connect/redirect/permissions-redirect.component.js
@@ -8,20 +8,18 @@ export default function PermissionsRedirect ({ domainMetadata }) {
   const t = useContext(I18nContext)
 
   return (
-    <div className="page-container permissions-redirect-container">
-      <div className="permissions-redirect-container__content">
-        <div className="permission-result">
-          { t('connecting') }
-          <div className="permission-result__icons">
-            <IconWithFallBack icon={domainMetadata.icon} name={domainMetadata.name} />
-            <div className="permission-result__center-icon">
-              <span className="permission-result__check" />
-              { renderBrokenLine() }
-            </div>
-            <div className="permission-result__identicon-container">
-              <div className="permission-result__identicon-border">
-                <img src="/images/logo/metamask-fox.svg" />
-              </div>
+    <div className="permissions-redirect">
+      <div className="permissions-redirect__result">
+        { t('connecting') }
+        <div className="permissions-redirect__result__icons">
+          <IconWithFallBack icon={domainMetadata.icon} name={domainMetadata.name} />
+          <div className="permissions-redirect__result__center-icon">
+            <span className="permissions-redirect__result__check" />
+            { renderBrokenLine() }
+          </div>
+          <div className="permissions-redirect__result__identicon-container">
+            <div className="permissions-redirect__result__identicon-border">
+              <img src="/images/logo/metamask-fox.svg" />
             </div>
           </div>
         </div>
