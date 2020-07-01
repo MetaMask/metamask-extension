a1ad7728bb567220a2e320028435392c0ec1481c
Dan Miller <danjm.com@gmail.com>
2020-07-01 05:42:34 -0230

    Update searchable-item-list to support new TokenSearchSelect story

--- ui/app/components/ui/searchable-item-list/item-list/item-list.component.js
+++ ui/app/components/ui/searchable-item-list/item-list/item-list.component.js
@@ -7,6 +7,7 @@ export default function SearchableItemList ({
   onToggleItem = null,
   Placeholder = null,
   listTitle = '',
+  maxListItems = 6,
 }) {
   return results.length === 0
     ? <Placeholder />
@@ -19,9 +20,17 @@ export default function SearchableItemList ({
         )}
         <div className="searchable-item-list__list-container">
           {
-            results.slice(0, 6)
+            results.slice(0, maxListItems)
               .map((result, i) => {
-                const { backgroundImageUrl, selected, disabled, primaryLabel } = result
+                const {
+                  backgroundImageUrl,
+                  selected,
+                  disabled,
+                  primaryLabel,
+                  secondaryLabel,
+                  rightPrimaryLabel,
+                  rightSecondaryLabel,
+                } = result
 
                 return (
                   <div
@@ -36,8 +45,17 @@ export default function SearchableItemList ({
                       className="searchable-item-list__item-icon"
                       style={{ backgroundImage: backgroundImageUrl && `url(${backgroundImageUrl})` }}
                     />
-                    <div className="searchable-item-list__item-labels">
-                      {primaryLabel && <span className="searchable-item-list__primary-label">{ primaryLabel }</span>}
+                    <div className="searchable-item-list__labels">
+                      <div className="searchable-item-list__item-labels">
+                        {primaryLabel && <span className="searchable-item-list__primary-label">{ primaryLabel }</span>}
+                        {secondaryLabel && <span className="searchable-item-list__secondary-label">{ secondaryLabel }</span>}
+                      </div>
+                      {(rightPrimaryLabel || rightSecondaryLabel) && (
+                        <div className="searchable-item-list__right-labels">
+                          {rightPrimaryLabel && <span className="searchable-item-list__right-primary-label">{ rightPrimaryLabel }</span>}
+                          {rightSecondaryLabel && <span className="searchable-item-list__right-secondary-label">{ rightSecondaryLabel }</span>}
+                        </div>
+                      )}
                     </div>
                   </div>
                 )
@@ -53,4 +71,5 @@ SearchableItemList.propTypes = {
   onToggleItem: PropTypes.func,
   Placeholder: PropTypes.element,
   listTitle: PropTypes.string,
+  maxListItems: PropTypes.number,
 }
