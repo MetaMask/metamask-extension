0cc1c220e8828ef5c62b54009fd811a7e8d8ce8b
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2020-07-08 07:15:37 -0700

    Update ui/lib/etherscan-prefix-for-network.js
    Co-authored-by: Mark Stacey <markjstacey@gmail.com>

--- ui/lib/etherscan-prefix-for-network.js
+++ ui/lib/etherscan-prefix-for-network.js
@@ -4,7 +4,7 @@ import * as networkEnums from '../../app/scripts/controllers/network/enums'
  * Gets the etherscan.io URL prefix for a given network ID.
  *
  * @param {string} networkId - The network ID to get the prefix for.
- * @returns {string} - The etherscan.io URL prefix for the given network ID.
+ * @returns {string} The etherscan.io URL prefix for the given network ID.
  */
 export function getEtherscanNetworkPrefix (networkId) {
   switch (networkId) {
