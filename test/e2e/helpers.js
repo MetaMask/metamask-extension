8ff403323600eb7fd31eae94e5e1c00c96414038
Mark Stacey <markjstacey@gmail.com>
2020-07-09 23:43:45 -0300

    Add test-dapp to `withFixtures` environment
    The `withFixtures` helper function now has the option of starting the
    test dapp as well. It will wait to ensure it has started up correctly,
    and it'll shut it down when the test ends.

--- test/e2e/helpers.js
+++ test/e2e/helpers.js
@@ -2,21 +2,34 @@ const path = require('path')
 const Ganache = require('./ganache')
 const FixtureServer = require('./fixture-server')
 const { buildWebDriver } = require('./webdriver')
+const createStaticServer = require('../../development/create-static-server')
 
 const tinyDelayMs = 200
 const regularDelayMs = tinyDelayMs * 2
 const largeDelayMs = regularDelayMs * 2
 
+const dappPort = 8080
+
 async function withFixtures (options, callback) {
-  const { fixtures, ganacheOptions, driverOptions, title } = options
+  const { dapp, fixtures, ganacheOptions, driverOptions, title } = options
   const fixtureServer = new FixtureServer()
   const ganacheServer = new Ganache()
+  let dappServer
 
   let webDriver
   try {
     await ganacheServer.start(ganacheOptions)
     await fixtureServer.start()
     await fixtureServer.loadState(path.join(__dirname, 'fixtures', fixtures))
+    if (dapp) {
+      const dappDirectory = path.resolve(__dirname, '..', '..', 'node_modules', '@metamask', 'test-dapp', 'dist')
+      dappServer = createStaticServer(dappDirectory)
+      dappServer.listen(dappPort)
+      await new Promise((resolve, reject) => {
+        dappServer.on('listening', resolve)
+        dappServer.on('error', reject)
+      })
+    }
     const { driver } = await buildWebDriver(driverOptions)
     webDriver = driver
 
@@ -40,6 +53,16 @@ async function withFixtures (options, callback) {
     await ganacheServer.quit()
     if (webDriver) {
       await webDriver.quit()
+    }
+    if (dappServer) {
+      await new Promise((resolve, reject) => {
+        dappServer.close((error) => {
+          if (error) {
+            return reject(error)
+          }
+          return resolve()
+        })
+      })
     }
   }
 }
