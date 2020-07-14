0052c6f0089396594bc3fa5987c881253ada0519
Mark Stacey <markjstacey@gmail.com>
2020-07-14 16:42:06 -0300

    Add `block-opening-brace-space-before` stylelint rule
    The rule has been uncommented, and violations have been auto-fixed.

--- stylelint.config.js
+++ stylelint.config.js
@@ -17,7 +17,7 @@ module.exports = {
     'block-closing-brace-space-before': 'always-single-line',
     'block-opening-brace-newline-after': 'always-multi-line',
     'block-opening-brace-space-after': 'always-single-line',
-    // 'block-opening-brace-space-before': 'always',
+    'block-opening-brace-space-before': 'always',
     // 'color-hex-case': 'lower',
     // 'color-hex-length': 'short',
     // 'comment-empty-line-before': [
