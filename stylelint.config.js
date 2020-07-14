4014aeaba3bb5fe8202f5cbbb796488dfca04283
Mark Stacey <markjstacey@gmail.com>
2020-07-14 16:29:15 -0300

    Add `at-rule-empty-line-before` stylelint rule
    This rule has been uncommented, and violations have been auto-fixed.

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   rules: {
     // stylelint-config-standard
-    // 'at-rule-empty-line-before': [
-    //   'always',
-    //   {
-    //     except: ['blockless-after-same-name-blockless', 'first-nested'],
-    //     ignore: ['after-comment'],
-    //   },
-    // ],
+    'at-rule-empty-line-before': [
+      'always',
+      {
+        except: ['blockless-after-same-name-blockless', 'first-nested'],
+        ignore: ['after-comment'],
+      },
+    ],
     'at-rule-name-case': 'lower',
     'at-rule-name-space-after': 'always-single-line',
     'at-rule-semicolon-newline-after': 'always',
