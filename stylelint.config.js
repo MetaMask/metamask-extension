2310037e8c165ce847551390f68ed473dd1b7fee
Mark Stacey <markjstacey@gmail.com>
2020-07-14 17:25:01 -0300

    Add `declaration-block-semicolon-newline-after` stylelint rule
    The rule has been uncommented, and violations have been auto-fixed.

--- stylelint.config.js
+++ stylelint.config.js
@@ -37,7 +37,7 @@ module.exports = {
     ],
     'declaration-bang-space-after': 'never',
     'declaration-bang-space-before': 'always',
-    // 'declaration-block-semicolon-newline-after': 'always-multi-line',
+    'declaration-block-semicolon-newline-after': 'always-multi-line',
     // 'declaration-block-semicolon-space-after': 'always-single-line',
     'declaration-block-semicolon-space-before': 'never',
     'declaration-block-single-line-max-declarations': 1,
