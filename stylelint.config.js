ed896e0e782e134dbcd01c89c4cbaeb85ee62f46
Mark Stacey <markjstacey@gmail.com>
2020-07-14 19:08:09 -0300

    Add `rule-empty-line-before` stylelint rule
    The rule has been uncommented, and violations have been auto-fixed.

--- stylelint.config.js
+++ stylelint.config.js
@@ -77,13 +77,13 @@ module.exports = {
     'number-leading-zero': 'always',
     'number-no-trailing-zeros': true,
     'property-case': 'lower',
-    // 'rule-empty-line-before': [
-    //   'always-multi-line',
-    //   {
-    //     except: ['first-nested'],
-    //     ignore: ['after-comment'],
-    //   },
-    // ],
+    'rule-empty-line-before': [
+      'always-multi-line',
+      {
+        except: ['first-nested'],
+        ignore: ['after-comment'],
+      },
+    ],
     'selector-attribute-brackets-space-inside': 'never',
     'selector-attribute-operator-space-after': 'never',
     'selector-attribute-operator-space-before': 'never',
