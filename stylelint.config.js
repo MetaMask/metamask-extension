acd53a4a1c1041028db192a149592ef939d8ab17
Mark Stacey <markjstacey@gmail.com>
2020-07-14 18:27:51 -0300

    Add `declaration-colon-space-after` stylelint rule
    The rule has been uncommented, and violations have been auto-fixed.

--- stylelint.config.js
+++ stylelint.config.js
@@ -43,7 +43,7 @@ module.exports = {
     'declaration-block-single-line-max-declarations': 1,
     'declaration-block-trailing-semicolon': 'always',
     'declaration-colon-newline-after': 'always-multi-line',
-    // 'declaration-colon-space-after': 'always-single-line',
+    'declaration-colon-space-after': 'always-single-line',
     'declaration-colon-space-before': 'never',
     // 'declaration-empty-line-before': [
     //   'always',
