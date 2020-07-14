0444c45d0674b715abc11f1ad294376bb869ee96
Mark Stacey <markjstacey@gmail.com>
2020-07-14 16:36:30 -0300

    Add `block-closing-brace-newline-before` stylelint rule
    The rule has been uncommented, and violations have been auto-fixed.

--- stylelint.config.js
+++ stylelint.config.js
@@ -13,7 +13,7 @@ module.exports = {
     'at-rule-semicolon-newline-after': 'always',
     'block-closing-brace-empty-line-before': 'never',
     'block-closing-brace-newline-after': 'always',
-    // 'block-closing-brace-newline-before': 'always-multi-line',
+    'block-closing-brace-newline-before': 'always-multi-line',
     'block-closing-brace-space-before': 'always-single-line',
     'block-opening-brace-newline-after': 'always-multi-line',
     'block-opening-brace-space-after': 'always-single-line',
