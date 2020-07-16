b282364bc2f25afdc5a3ce6d802b6eafbebd75c9
tmashuang <thomas.b.huang@gmail.com>
2020-07-16 14:51:59 -0700

    Clear AccountTracker accounts and CachedBalances on createNewVaultAndRestore

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -631,6 +631,12 @@ export default class MetamaskController extends EventEmitter {
       // clear permissions
       this.permissionsController.clearPermissions()
 
+      // clear accounts in accountTracker
+      this.accountTracker.clearAccounts()
+
+      // clear cachedBalances
+      this.cachedBalancesController.clearCachedBalances()
+
       // create new vault
       const vault = await keyringController.createNewVaultAndRestore(password, seed)
 
