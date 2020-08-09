3b81fea77f3bcedcb29c6b2560589cea654f96fe
Thomas <thomas.b.huang@gmail.com>
2020-08-08 18:43:16 -0700

    Add category in eventOpts

--- app/scripts/lib/background-metametrics.js
+++ app/scripts/lib/background-metametrics.js
@@ -2,12 +2,14 @@ import { getBackgroundMetaMetricState } from '../../../ui/app/selectors'
 import { sendMetaMetricsEvent } from '../../../ui/app/helpers/utils/metametrics.util'
 
 export default function backgroundMetaMetricsEvent (metaMaskState, eventData) {
+
+  eventData.eventOpts['category'] = 'Background'
+
   const stateEventData = getBackgroundMetaMetricState({ metamask: metaMaskState })
   if (stateEventData.participateInMetaMetrics) {
     sendMetaMetricsEvent({
       ...stateEventData,
       ...eventData,
-      category: 'Background',
       currentPath: '/background',
     })
   }
