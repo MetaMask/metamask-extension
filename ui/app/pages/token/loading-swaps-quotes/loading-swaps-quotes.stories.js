27e8126b9ebbfe0fd29a07fb59a1c5f3c01b09b1
Dan Miller <danjm.com@gmail.com>
2020-08-28 16:39:06 -0230

    Update loading swaps stories to reflect error states and dynamically passed logos

--- ui/app/pages/token/loading-swaps-quotes/loading-swaps-quotes.stories.js
+++ ui/app/pages/token/loading-swaps-quotes/loading-swaps-quotes.stories.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react'
+import { text } from '@storybook/addon-knobs/react'
+import { storiesMetadata } from './loading-swaps-quotes-stories-metadata'
 import LoadingSwapsQuotes from './loading-swaps-quotes'
 
 export default {
@@ -22,25 +24,14 @@ export const FasterThanExpectedCompletion = () => {
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      {!done && (
-        <div style={{ height: '600px', width: '357px', border: '1px solid grey' }}>
-          <LoadingSwapsQuotes
-            loadingComplete={loadingComplete}
-            onDone={() => setDone(true)}
-          />
-        </div>
-      )}
-      {done && (
-        <div
-          className="button btn-primary"
-          style={{ cursor: 'pointer', width: '200px' }}
-          onClick={() => {
-            setLoadingComplete(false)
-            setDone(false)
-          }}
-        >RESTART
-        </div>
-      )}
+      <div style={{ height: '600px', width: '357px', border: '1px solid grey' }}>
+        <LoadingSwapsQuotes
+          loadingComplete={loadingComplete}
+          onDone={() => setDone(true)}
+          aggregatorMetadata={storiesMetadata}
+          loadingError={text('Loading Error 1', '')}
+        />
+      </div>
     </div>
   )
 }
@@ -62,25 +53,45 @@ export const SlowerThanExpectedCompletion = () => {
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      {!done && (
-        <div style={{ height: '600px', width: '357px', border: '1px solid grey' }}>
-          <LoadingSwapsQuotes
-            loadingComplete={loadingComplete}
-            onDone={() => setDone(true)}
-          />
-        </div>
-      )}
-      {done && (
-        <div
-          className="button btn-primary"
-          style={{ cursor: 'pointer', width: '200px' }}
-          onClick={() => {
-            setLoadingComplete(false)
-            setDone(false)
-          }}
-        >RESTART
-        </div>
-      )}
+      <div style={{ height: '600px', width: '357px', border: '1px solid grey' }}>
+        <LoadingSwapsQuotes
+          loadingComplete={loadingComplete}
+          onDone={() => setDone(true)}
+          aggregatorMetadata={storiesMetadata}
+          loadingError={text('Loading Error 2', '')}
+        />
+      </div>
+    </div>
+  )
+}
+
+export const FasterThanExpectedCompletionWithError = () => {
+  const [loading, setLoading] = useState(false)
+  const [loadingComplete, setLoadingComplete] = useState(false)
+  const [loadingError, setLoadingError] = useState('')
+  const [done, setDone] = useState(false)
+
+  useEffect(() => {
+    if (!done && !loading) {
+      setLoading(true)
+      setTimeout(() => {
+        setLoading(false)
+        setLoadingComplete(true)
+        setLoadingError('swapsErrorFetchingQuotes')
+      }, 3000)
+    }
+  }, [done, loading])
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <div style={{ height: '600px', width: '357px', border: '1px solid grey' }}>
+        <LoadingSwapsQuotes
+          loadingComplete={loadingComplete}
+          onDone={() => setDone(true)}
+          aggregatorMetadata={storiesMetadata}
+          loadingError={loadingError}
+        />
+      </div>
     </div>
   )
 }
