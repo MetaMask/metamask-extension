5560b67931f16d49e94f113a2bbdb3019f09ce3d
Dan Miller <danjm.com@gmail.com>
2020-08-28 16:28:39 -0230

    Retrieve logos dynamically instead of using hard coded svgs

--- ui/app/pages/token/loading-swaps-quotes/aggregator-logo.js
+++ ui/app/pages/token/loading-swaps-quotes/aggregator-logo.js
@@ -1,19 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const AggregatorLogoMap = {
-  'totle': <div className="loading-swaps-quotes__totle"><img src="/images/totle-agg-logo.svg" /></div>,
-  'dexag': <div className="loading-swaps-quotes__dexag"><img src="/images/dexag-agg-logo.svg" /></div>,
-  'uniswap': <div className="loading-swaps-quotes__uniswap"><img src="/images/uniswap-agg-logo.svg" /></div>,
-  'paraswap': <div className="loading-swaps-quotes__paraswap"><img src="/images/paraswap-agg-logo.svg" /></div>,
-  '0x': <div className="loading-swaps-quotes__0x"><img src="/images/0x-agg-logo.svg" /></div>,
-  '1inch': <div className="loading-swaps-quotes__1inch"><img src="/images/1inch-agg-logo.svg" /></div>,
-}
-
-export default function AggregatorLogo ({ aggregatorName }) {
-  return (<div className="loading-swaps-quotes__logo">{ AggregatorLogoMap[aggregatorName] }</div>)
+export default function AggregatorLogo ({ icon, color }) {
+  return (
+    <div className="loading-swaps-quotes__logo">
+      <div style={{ background: color }}><img src={icon} /></div>
+    </div>
+  )
 }
 
 AggregatorLogo.propTypes = {
-  aggregatorName: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
 }
