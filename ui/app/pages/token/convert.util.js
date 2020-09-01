eff2d6426b1506d2852f8e0934da89f8f2270c19
Dan Miller <danjm.com@gmail.com>
2020-09-01 04:55:07 -0230

    Add validation for fetchTradesInfo and fetchTokens

--- ui/app/pages/token/convert.util.js
+++ ui/app/pages/token/convert.util.js
@@ -1,7 +1,9 @@
+import { isValidAddress } from 'ethereumjs-util'
 import { calcTokenValue } from '../../helpers/utils/token-util'
 import { constructTxParams } from '../../helpers/utils/util'
 import { decimalToHex } from '../../helpers/utils/conversions.util'
 import { estimateGasFromTxParams } from '../../store/actions'
+import { validateTxParams } from '../../../../app/scripts/controllers/transactions/lib/util'
 
 // A high default that should cover any ERC-20 approve implementation
 const APPROVE_TX_GAS_DEFAULT = '0x1d4c0'
@@ -11,6 +13,75 @@ const METASWAP_GAS_DEFAULT = 800000
 
 const TRADES_BASE_URL = 'https://metaswap-api.airswap-dev.codefi.network/trades?'
 const TOKENS_BASE_URL = 'https://metaswap-api.airswap-dev.codefi.network/tokens'
+
+const validHex = (string) => Boolean(string?.match(/^0x[A-Fa-f0-9]+$/u))
+const truthyString = (string) => Boolean(string?.length)
+const truthyDigitString = (string) => truthyString(string) && Boolean(string.match(/^\d+$/u))
+
+const QUOTE_VALIDATORS = [
+  {
+    property: 'trade',
+    type: 'object',
+    validator: (trade) => trade && validHex(trade.data) && isValidAddress(trade.to) && isValidAddress(trade.from) && truthyString(trade.value),
+  },
+  {
+    property: 'sourceAmount',
+    type: 'string',
+    validator: truthyDigitString,
+  },
+  {
+    property: 'destinationAmount',
+    type: 'string',
+    validator: truthyDigitString,
+  },
+  {
+    property: 'sourceToken',
+    type: 'string',
+    validator: isValidAddress,
+  },
+  {
+    property: 'destinationToken',
+    type: 'string',
+    validator: isValidAddress,
+  },
+  {
+    property: 'aggregator',
+    type: 'string',
+    validator: truthyString,
+  },
+  {
+    property: 'aggType',
+    type: 'string',
+    validator: truthyString,
+  },
+  {
+    property: 'error',
+    type: 'object',
+    validator: (error) => !error,
+  },
+]
+
+const TOKEN_VALIDATORS = [
+  {
+    property: 'address',
+    type: 'string',
+    validator: isValidAddress,
+  },
+  {
+    property: 'symbol',
+    type: 'string',
+    validator: (string) => truthyString(string) && string.length <= 12,
+  },
+  {
+    property: 'decimals',
+    type: 'number',
+    validator: (number) => number >= 0 && number <= 36,
+  },
+]
+
+function validateData (validators, object) {
+  return validators.every(({ property, type, validator }) => typeof object[property] === type && validator(object[property]))
+}
 
 export async function fetchTradesInfo ({ tokens, slippage, sourceToken, sourceDecimals, destinationToken, value, fromAddress }) {
   const urlParams = {
@@ -32,7 +103,7 @@ export async function fetchTradesInfo ({ tokens, slippage, sourceToken, sourceDe
   const destinationTokenInfo = tokens.find(({ address }) => address === destinationToken)
 
   const newQuotes = tradesResponse
-    .filter((response) => response.trade && !response.error)
+    .filter((response) => validateData(QUOTE_VALIDATORS, response))
     .map((response) => ({
       ...response,
       slippage,
@@ -82,5 +153,6 @@ export async function quoteToTxParams (quote, gasPrice) {
 export async function fetchTokens () {
   const response = await window.fetch(TOKENS_BASE_URL, { method: 'GET' })
   const tokens = await response.json()
-  return tokens
+  const filteredTokens = tokens.filter((token) => validateData(TOKEN_VALIDATORS, token))
+  return filteredTokens
 }
