72436feb3de2073f2d31c565554f9203d80ed9ef
Mark Stacey <markjstacey@gmail.com>
2020-09-08 16:03:31 -0300

    Replace custom countdown timer formatting (#16)
    The custom string formatting in the countdown timer has been replaced
    with `luxon`, which is a library we already have that handles
    formatting dates and times. This avoids a lot of unnecessary logic for
    parsing and manipulating strings.

--- ui/app/pages/swaps/countdown-timer/countdown-timer.js
+++ ui/app/pages/swaps/countdown-timer/countdown-timer.js
@@ -1,19 +1,11 @@
 import React, { useState, useEffect, useContext, useRef } from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
+import { Duration } from 'luxon'
 import { I18nContext } from '../../../contexts/i18n'
 import InfoTooltip from '../../../components/ui/info-tooltip'
 
 const TIMER_BASE = 60000
-
-function millisToMinutesAndSeconds (millis) {
-  const minutes = Math.floor(millis / 60000)
-  let seconds = (Math.floor((millis % 60000)) / 1000).toFixed(0)
-  if (seconds === '60') {
-    seconds = 59
-  }
-  return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`
-}
 
 // Return the mm:ss start time of the countdown timer.
 // If time has elapsed between `timeStarted` the time current time,
@@ -21,28 +13,16 @@ function millisToMinutesAndSeconds (millis) {
 // rendering
 function getNewTimer (currentTime, timeStarted, timeBaseStart) {
   const timeAlreadyElapsed = currentTime - timeStarted
-  const timerStart = timeBaseStart - timeAlreadyElapsed
-  const readableTimerStart = millisToMinutesAndSeconds(timerStart)
-  return readableTimerStart
+  return timeBaseStart - timeAlreadyElapsed
 }
 
 function decreaseTimerByOne (timer) {
-  const [minutes, seconds] = timer.split(':')
-  if (timer === '0:00') {
-    return '0:00'
-  } else if (seconds === '00') {
-    return `${Number(minutes) - 1}:59`
-  }
-  return `${minutes}:${String((Number(seconds) - 1)).padStart(2, '0')}`
-
+  return timer - 1000
 }
 
-function timeBelowWarningTime (time, warningTime) {
-  const [timeMinutes, timeSeconds] = time.split(':')
+function timeBelowWarningTime (timer, warningTime) {
   const [warningTimeMinutes, warningTimeSeconds] = warningTime.split(':')
-
-  return (Number(timeMinutes) < Number(warningTimeMinutes)) ||
-    (Number(timeMinutes) === Number(warningTimeMinutes) && (Number(timeSeconds) <= Number(warningTimeSeconds)))
+  return timer <= ((Number(warningTimeMinutes) * 60) + Number(warningTimeSeconds)) * 1000
 }
 
 export default function CountdownTimer ({
@@ -78,7 +58,7 @@ export default function CountdownTimer ({
       initialTimeStartedRef.current = timeStarted || Date.now()
     }
 
-    if (timer === '0:00' && timeStarted !== initialTimeStartedRef.current) {
+    if (timer === 0 && timeStarted !== initialTimeStartedRef.current) {
       initialTimeStartedRef.current = timeStarted
       const newCurrentTime = Date.now()
       setCurrentTime(newCurrentTime)
@@ -91,11 +71,12 @@ export default function CountdownTimer ({
     }
   }, [timeStarted, timer, timerBase])
 
+  const formattedTimer = Duration.fromMillis(timer).toFormat('m:s')
   let time
   if (timeOnly) {
-    time = <div className="countdown-timer__time">{timer}</div>
+    time = <div className="countdown-timer__time">{formattedTimer}</div>
   } else if (labelKey) {
-    time = t(labelKey, [<div key="countdown-time-1" className="countdown-timer__time">{timer}</div>])
+    time = t(labelKey, [<div key="countdown-time-1" className="countdown-timer__time">{formattedTimer}</div>])
   }
 
   return (
