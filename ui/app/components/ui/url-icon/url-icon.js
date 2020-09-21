d4ba87e3333fa8b57b3f24c88c077bb8b4a515fe
Dan Miller <danjm.com@gmail.com>
2020-09-21 13:45:22 -0230

    Handle errors with images passed ti url-icon

--- ui/app/components/ui/url-icon/url-icon.js
+++ ui/app/components/ui/url-icon/url-icon.js
@@ -1,17 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
+import IconWithFallback from '../icon-with-fallback'
 
 export default function UrlIcon ({
   url,
   className,
+  name,
 }) {
   return (
-    <img className={classnames('url-icon', className)} src={url} />
+    <IconWithFallback
+      className={classnames('url-icon', className)}
+      icon={url}
+      name={name}
+      fallbackClassName="url-icon__fallback"
+    />
   )
 }
 
 UrlIcon.propTypes = {
   url: PropTypes.string,
   className: PropTypes.string,
+  name: PropTypes.string,
 }
