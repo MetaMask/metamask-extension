27276880e033c0e6caa7bd98120cf78ef5237dc0
Vicente Dragicevic <vdrg@protonmail.ch>
2020-09-22 16:47:02 -0300

    Add more tests for swaps controller

--- test/unit/app/controllers/swaps-test.js
+++ test/unit/app/controllers/swaps-test.js
@@ -2,9 +2,48 @@ import assert from 'assert'
 import sinon from 'sinon'
 import proxyquire from 'proxyquire'
 
-import { createTestProviderTools, getTestAccounts } from '../../../stub/provider'
+import { ethers } from 'ethers'
+import ObservableStore from 'obs-store'
+import { createTestProviderTools } from '../../../stub/provider'
+// import { fetchTradesInfo } from '../../../../ui/app/pages/swaps/swaps.util'
 
-const MOCK_GET_BUFFERED_GAS_LIMIT = () => ({ gasLimit: 2000000, simulationFails: undefined })
+const MOCK_FETCH_PARAMS = { slippage: 3, sourceToken: '0x6b175474e89094c44da98b954eedeac495271d0f', sourceDecimals: 18, destinationToken: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', value: '1000000000000000000', fromAddress: '0x7F18BB4Dd92CF2404C54CBa1A9BE4A1153bdb078', exchangeList: 'zeroExV1' }
+
+const TEST_AGG_ID = 'zeroExV1'
+const MOCK_QUOTES = {
+  [TEST_AGG_ID]:
+   {
+     trade:
+      {
+        data:
+         '0x00',
+        from: '0x7F18BB4Dd92CF2404C54CBa1A9BE4A1153bdb078',
+        value: '0x17647444f166000',
+        gas: '0xe09c0',
+        gasPrice: undefined,
+        to: '0x9537C111Ea62a8dc39E99718140686f7aD856321',
+      },
+     sourceAmount: '1000000000000000000000000000000000000',
+     destinationAmount: '396493201125465',
+     error: null,
+     sourceToken: '0x6b175474e89094c44da98b954eedeac495271d0f',
+     destinationToken: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+     approvalNeeded: null,
+     maxGas: 920000,
+     averageGas: 312510,
+     estimatedRefund: 343090,
+     fetchTime: 559,
+     aggregator: TEST_AGG_ID,
+     aggType: 'AGG',
+     slippage: 3,
+   },
+}
+
+const MOCK_TOKEN_RATES_STORE = new ObservableStore({ contractExchangeRates: {} })
+
+const MOCK_GET_PROVIDER_CONFIG = () => ({ type: 'FAKE_NETWORK' })
+
+const MOCK_GET_BUFFERED_GAS_LIMIT = async () => ({ gasLimit: 2000000, simulationFails: undefined })
 
 const EMPTY_INIT_STATE = {
   swapsState: {
@@ -31,12 +70,16 @@ const fetchTradesInfoStub = sinon.stub()
 const { default: SwapsController } = proxyquire(
   '../../../../app/scripts/controllers/swaps',
   {
-    '../../../ui/app/pages/swaps/swaps.uti': { fetchTradesInfo: fetchTradesInfoStub },
+    '../../../ui/app/pages/swaps/swaps.util': { fetchTradesInfo: fetchTradesInfoStub },
   },
 )
 
 describe.only('SwapsController', function () {
+  // this.timeout(200000)
   let providerResultStub, provider
+  afterEach(function () {
+    sinon.restore()
+  })
 
   describe('constructor', function () {
     it('should setup correctly', function () {
@@ -48,7 +91,7 @@ describe.only('SwapsController', function () {
       }
       provider = createTestProviderTools({ scaffold: providerResultStub }).provider
 
-      const swapsController = new SwapsController({ getBufferedGasLimit: MOCK_GET_BUFFERED_GAS_LIMIT, provider, getProviderConfig: undefined, tokenRatesStore: undefined })
+      const swapsController = new SwapsController({ getBufferedGasLimit: MOCK_GET_BUFFERED_GAS_LIMIT, provider, getProviderConfig: MOCK_GET_PROVIDER_CONFIG, tokenRatesStore: MOCK_TOKEN_RATES_STORE })
       assert.deepStrictEqual(swapsController.store.getState(), EMPTY_INIT_STATE)
       assert.deepStrictEqual(swapsController.getBufferedGasLimit, MOCK_GET_BUFFERED_GAS_LIMIT)
       assert.strictEqual(swapsController.pollCount, 0)
@@ -58,63 +101,114 @@ describe.only('SwapsController', function () {
     })
   })
 
-  describe('setters', function () {
+  describe('API', function () {
     let swapsController
     beforeEach(function () {
       providerResultStub = {
-      // 1 gwei
+        // 1 gwei
         eth_gasPrice: '0x0de0b6b3a7640000',
         // by default, all accounts are external accounts (not contracts)
         eth_getCode: '0x',
       }
       provider = createTestProviderTools({ scaffold: providerResultStub }).provider
 
-      swapsController = new SwapsController({ getBufferedGasLimit: MOCK_GET_BUFFERED_GAS_LIMIT, provider, getProviderConfig: undefined, tokenRatesStore: undefined })
+      swapsController = new SwapsController({ getBufferedGasLimit: MOCK_GET_BUFFERED_GAS_LIMIT, provider, getProviderConfig: MOCK_GET_PROVIDER_CONFIG, tokenRatesStore: MOCK_TOKEN_RATES_STORE })
 
     })
-    it('should set quotes', function () {
-      const quotes = {}
-      swapsController.setQuotes(quotes)
-      assert.deepStrictEqual(swapsController.store.getState().swapsState.quotes, quotes)
+
+    describe('setters', function () {
+      it('should set quotes', function () {
+        const quotes = {}
+        swapsController.setQuotes(quotes)
+        assert.deepStrictEqual(swapsController.store.getState().swapsState.quotes, quotes)
+      })
+      it('should set selected quote agg id', function () {
+        const selectedAggId = 'test'
+        swapsController.setSelectedQuoteAggId(selectedAggId)
+        assert.deepStrictEqual(swapsController.store.getState().swapsState.selectedAggId, selectedAggId)
+      })
+      it('should set swaps tokens', function () {
+        const tokens = []
+        swapsController.setSwapsTokens(tokens)
+        assert.deepStrictEqual(swapsController.store.getState().swapsState.tokens, tokens)
+      })
+      it('should set swaps error key', function () {
+        const errorKey = 'test'
+        swapsController.setSwapsErrorKey(errorKey)
+        assert.deepStrictEqual(swapsController.store.getState().swapsState.errorKey, errorKey)
+      })
     })
-    it('should set selected quote agg id', function () {
-      const selectedAggId = 'test'
-      swapsController.setSelectedQuoteAggId(selectedAggId)
-      assert.deepStrictEqual(swapsController.store.getState().swapsState.selectedAggId, selectedAggId)
+
+    describe('fetchAndSetQuotes', function () {
+      it('returns null if fetchParams is not provided', async function () {
+        const quotes = await swapsController.fetchAndSetQuotes(undefined)
+        assert.strictEqual(quotes, null)
+      })
+
+      it('calls fetchTradesInfo with the given fetchParams and returns the correct quotes', async function () {
+        fetchTradesInfoStub.resolves(MOCK_QUOTES)
+
+        // Make it so approval is not requires
+        sinon.stub(swapsController, '_getERC20Allowance').resolves(ethers.BigNumber.from(1))
+
+        const [newQuotes, topAggId] = await swapsController.fetchAndSetQuotes(MOCK_FETCH_PARAMS)
+
+        assert.strictEqual(topAggId, TEST_AGG_ID)
+        assert.deepStrictEqual(newQuotes[TEST_AGG_ID], {
+          ...MOCK_QUOTES[TEST_AGG_ID],
+          sourceTokenInfo: undefined,
+          destinationTokenInfo: undefined,
+          // TODO: find a way to calculate these values dynamically
+          gasEstimate: 2000000,
+          gasEstimateWithRefund: '8cd8e',
+        })
+
+        assert.strictEqual(fetchTradesInfoStub.calledOnceWithExactly(MOCK_FETCH_PARAMS), true)
+      })
     })
-    it('should set swaps tokens', function () {
-      const tokens = []
-      swapsController.setSwapsTokens(tokens)
-      assert.deepStrictEqual(swapsController.store.getState().swapsState.tokens, tokens)
+
+    describe('resetSwapsState', function () {
+      it('resets the swaps state correctly', function () {
+        const { swapsState: old } = swapsController.store.getState()
+        swapsController.resetSwapsState()
+        const { swapsState } = swapsController.store.getState()
+        assert.deepStrictEqual(swapsState, { ...EMPTY_INIT_STATE.swapsState, tokens: old.tokens })
+      })
+
+      it('clears polling timeout', function () {
+        swapsController.resetSwapsState()
+        assert.strictEqual(swapsController.pollingTimeout, undefined)
+      })
     })
-    it('should set swaps error key', function () {
-      const errorKey = 'test'
-      swapsController.setSwapsErrorKey(errorKey)
-      assert.deepStrictEqual(swapsController.store.getState().swapsState.errorKey, errorKey)
+
+    describe('stopPollingForQuotes', function () {
+      it('clears polling timeout', function () {
+        swapsController.stopPollingForQuotes()
+        assert.strictEqual(swapsController.pollingTimeout, undefined)
+      })
+
+      it('resets quotes state correctly', function () {
+        swapsController.stopPollingForQuotes()
+        const { swapsState } = swapsController.store.getState()
+        assert.deepStrictEqual(swapsState.quotes, {})
+        assert.strictEqual(swapsState.quotesLastFetched, null)
+      })
+    })
+
+    describe('resetPostFetchState', function () {
+      it('clears polling timeout', function () {
+        swapsController.stopPollingForQuotes()
+        assert.strictEqual(swapsController.pollingTimeout, undefined)
+      })
+
+      it('updates state correctly', function () {
+        swapsController.stopPollingForQuotes()
+        const { swapsState } = swapsController.store.getState()
+        assert.deepStrictEqual(swapsState.quotes, {})
+        assert.strictEqual(swapsState.quotesLastFetched, null)
+
+      })
     })
   })
 
-  describe('fetchAndSetQuotes', function () {
-    let swapsController
-    beforeEach(function () {
-      providerResultStub = {
-      // 1 gwei
-        eth_gasPrice: '0x0de0b6b3a7640000',
-        // by default, all accounts are external accounts (not contracts)
-        eth_getCode: '0x',
-      }
-      provider = createTestProviderTools({ scaffold: providerResultStub }).provider
-
-      swapsController = new SwapsController({ getBufferedGasLimit: MOCK_GET_BUFFERED_GAS_LIMIT, provider, getProviderConfig: undefined, tokenRatesStore: undefined })
-    })
-
-    it('returns null if fetchParams is not provided', async function () {
-      const quotes = await swapsController.fetchAndSetQuotes(undefined)
-      assert.strictEqual(quotes, null)
-    })
-
-    it('calls fetchTradesInfo with the given fetchParams', async function () {
-      const quotes = await swapsController.fetchAndSetQuotes(undefined)
-    })
-  })
 })
