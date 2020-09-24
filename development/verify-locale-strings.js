ede229d01b956aead8bb18763bcb98a96cbc0c2d
Brad Decker <git@braddecker.dev>
2020-09-24 14:28:22 -0500

    scan shared folder for used translations

--- development/verify-locale-strings.js
+++ development/verify-locale-strings.js
@@ -164,7 +164,10 @@ async function verifyLocale (code) {
 
 async function verifyEnglishLocale () {
   const englishLocale = await getLocale('en')
-  const javascriptFiles = await findJavascriptFiles(path.resolve(__dirname, '..', 'ui'))
+  const uiJSFiles = await findJavascriptFiles(path.resolve(__dirname, '..', 'ui'))
+  const sharedJSFiles = await findJavascriptFiles(path.resolve(__dirname, '..', 'shared'))
+
+  const javascriptFiles = sharedJSFiles.concat(uiJSFiles)
 
   // match "t(`...`)" because constructing message keys from template strings
   // prevents this script from finding the messages, and then inappropriately
