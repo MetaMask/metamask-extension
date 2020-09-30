0d1c718ffc2e845b78eed27f4be84d03af1626f4
Brad Decker <git@braddecker.dev>
2020-09-30 13:57:57 -0500

    refactor usage of token-address prop

---
+++ ui/app/hooks/useCurrentAsset.js
@@ -0,0 +1,18 @@
+import { useSelector } from 'react-redux'
+import { useRouteMatch } from 'react-router-dom'
+import { getTokens } from '../ducks/metamask/metamask'
+import { ASSET_ROUTE } from '../helpers/constants/routes'
+import { ETH_SWAPS_TOKEN_OBJECT } from '../helpers/constants/swaps'
+
+export function useCurrentAsset () {
+  // To determine which primary currency to display for swaps transactions we need to be aware
+  // of which asset, if any, we are viewing at present
+  const match = useRouteMatch({ path: `${ASSET_ROUTE}/:asset`, exact: true, strict: true })
+  const tokenAddress = match?.params?.asset
+  const knownTokens = useSelector(getTokens)
+  const token = tokenAddress && knownTokens.find(
+    ({ address }) => address === tokenAddress,
+  )
+
+  return token ?? ETH_SWAPS_TOKEN_OBJECT
+}
