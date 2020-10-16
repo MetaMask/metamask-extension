674fc29fc97fd53285797025c087dd965e33b6ee
David Walsh <davidwalsh83@gmail.com>
2020-10-16 14:44:53 -0500

    Lint fixes

--- ui/app/pages/add-token/token-list/token-list.component.js
+++ ui/app/pages/add-token/token-list/token-list.component.js
@@ -24,45 +24,47 @@ export default class InfoBox extends Component {
       tokens = [],
     } = this.props
 
-    return results.length === 0 ? (
-      <TokenListPlaceholder />
-    ) : (
-      <div className="token-list">
-        <div className="token-list__title">
-          {this.context.t('searchResults')}
-        </div>
-        <div className="token-list__tokens-container">
-          {Array(6)
-            .fill(undefined)
-            .map((_, i) => {
-              const { logo, symbol, name, address } = results[i] || {}
-              const tokenAlreadyAdded = checkExistingAddresses(address, tokens)
-              const onClick = () => !tokenAlreadyAdded && onToggleToken(results[i]);
+    return results.length === 0
+      ? <TokenListPlaceholder />
+      : (
+        <div className="token-list">
+          <div className="token-list__title">
+            { this.context.t('searchResults') }
+          </div>
+          <div className="token-list__tokens-container">
+            {
+              Array(6).fill(undefined)
+                .map((_, i) => {
+                  const { logo, symbol, name, address } = results[i] || {}
+                  const tokenAlreadyAdded = checkExistingAddresses(address, tokens)
 
-              return Boolean(logo || symbol || name) && (
-                <div
-                  className={classnames('token-list__token', {
-                    'token-list__token--selected': selectedTokens[address],
-                    'token-list__token--disabled': tokenAlreadyAdded,
-                  })}
-                  onClick={onClick}
-                  onKeyPress={event => event.key === "Enter" && onClick()}
-                  key={i}
-                  tabIndex="0"
-                >
-                  <div
-                    className="token-list__token-icon"
-                    style={{ backgroundImage: logo && `url(images/contract/${logo})` }}
-                  >
-                  </div>
-                  <div className="token-list__token-data">
-                    <span className="token-list__token-name">{ `${name} (${symbol})` }</span>
-                  </div>
-                </div>
-              )
-            })}
+                  const onClick = () => !tokenAlreadyAdded && onToggleToken(results[i])
+
+                  return Boolean(logo || symbol || name) && (
+                    <div
+                      className={classnames('token-list__token', {
+                        'token-list__token--selected': selectedTokens[address],
+                        'token-list__token--disabled': tokenAlreadyAdded,
+                      })}
+                      onClick={onClick}
+                      onKeyPress={(event) => event.key === 'Enter' && onClick()}
+                      key={i}
+                      tabIndex="0"
+                    >
+                      <div
+                        className="token-list__token-icon"
+                        style={{ backgroundImage: logo && `url(images/contract/${logo})` }}
+                      >
+                      </div>
+                      <div className="token-list__token-data">
+                        <span className="token-list__token-name">{ `${name} (${symbol})` }</span>
+                      </div>
+                    </div>
+                  )
+                })
+            }
+          </div>
         </div>
-      </div>
     )
   }
 }
