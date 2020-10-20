d51a760062dfbe3dfe6ba178ec1904c309a0e07b
Mark Stacey <markjstacey@gmail.com>
2020-10-20 02:10:18 -0230

    Remove non-existent `metaMaskFee` property
    The `metaMaskFee` property on the "quote data" PropType was not used,
    and it never existed in practice. This resulted in PropType errors.
    
    The non-existent property has been removed.

--- ui/app/pages/swaps/select-quote-popover/select-quote-popover-constants.js
+++ ui/app/pages/swaps/select-quote-popover/select-quote-popover-constants.js
@@ -8,7 +8,6 @@ export const QUOTE_DATA_ROWS_PROPTYPES_SHAPE = PropTypes.shape({
   destinationTokenValue: PropTypes.string.isRequired,
   isBestQuote: PropTypes.bool,
   liquiditySource: PropTypes.string.isRequired,
-  metaMaskFee: PropTypes.string.isRequired,
   networkFees: PropTypes.string.isRequired,
   quoteSource: PropTypes.string.isRequired,
   rawNetworkFees: PropTypes.number.isRequired,
