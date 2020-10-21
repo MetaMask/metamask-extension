8ace70d2e0031039bb3bcf2b7e3fbd80a53d0331
Etienne Dusseault <etienne.dusseault@gmail.com>
2020-10-21 15:11:48 +0800

    require babel presets in build sys

--- development/build/index.js
+++ development/build/index.js
@@ -18,6 +18,9 @@ require('@babel/plugin-transform-runtime')
 require('@babel/plugin-proposal-class-properties')
 require('@babel/plugin-proposal-optional-chaining')
 require('@babel/plugin-proposal-nullish-coalescing-operator')
+require('@babel/preset-env')
+require('@babel/preset-react')
+
 
 const browserPlatforms = [
   'firefox',
