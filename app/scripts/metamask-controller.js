4b7a3989d9469a8fda05ace47a87e6606b1403d1
Brad Decker <git@braddecker.dev>
2020-10-22 12:31:08 -0500

    rename remaining trackSegmentEvent reference

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -1894,7 +1894,7 @@ export default class MetamaskController extends EventEmitter {
     const metamaskState = await this.getState()
     const additionalProperties = getBackgroundMetaMetricState(metamaskState)
 
-    this.trackSegmentEvent({
+    this.trackMetaMetricsEvent({
       event: name,
       category: 'Background',
       matomoEvent: true,
