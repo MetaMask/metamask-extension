3ab8e64661f5accadf4d55a77b2916e73be546f1
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2020-10-22 12:14:46 -0230

    Fix dot-notation issues
    Refs #9663
    
    This change enables `dot-notation` and fixes the issues raised by the rule.

--- .eslintrc.js
+++ .eslintrc.js
@@ -94,7 +94,6 @@ module.exports = {
     'node/no-unpublished-import': 'off',
     'node/no-unpublished-require': 'off',
     'node/no-unsupported-features/node-builtins': 'off',
-    'dot-notation': 'off',
     'mocha/max-top-level-suites': 'off',
   },
 
