b82c348bbad836f2644df3f28e679f672dab26d5
Erik Marks <rekmarks@protonmail.com>
2020-10-23 15:53:24 -0700

    fixup! Update test/e2e/metamask-ui.spec.js

--- test/e2e/metamask-ui.spec.js
+++ test/e2e/metamask-ui.spec.js
@@ -763,7 +763,7 @@ describe('MetaMask', function () {
 
   describe('Add a custom token from a dapp', function () {
     it('creates a new token', async function () {
-      let windowHandles = await driver.getAllWindowHandles()
+      const windowHandles = await driver.getAllWindowHandles()
       const extension = windowHandles[0]
       const dapp = windowHandles[1]
       await driver.delay(regularDelayMs * 2)
@@ -772,7 +772,7 @@ describe('MetaMask', function () {
       await driver.delay(regularDelayMs * 2)
 
       await driver.clickElement(By.xpath(`//button[contains(text(), 'Create Token')]`))
-      await waitUntilXWindowHandles(3)
+      await driver.waitUntilXWindowHandles(3)
 
       const popup = windowHandles[2]
       await driver.switchToWindow(popup)
