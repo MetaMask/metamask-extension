d843bed4e3a69485fc1bfd19320fc08f0495c8cf
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2020-10-23 16:24:20 -0700

    Robustify waiting logic in e2e test (#9704)
    * Robustify waiting for popup to open
    
    Co-authored-by: Mark Stacey <markjstacey@gmail.com>

--- test/e2e/metamask-ui.spec.js
+++ test/e2e/metamask-ui.spec.js
@@ -772,9 +772,8 @@ describe('MetaMask', function () {
       await driver.delay(regularDelayMs * 2)
 
       await driver.clickElement(By.xpath(`//button[contains(text(), 'Create Token')]`))
-      await driver.delay(largeDelayMs)
+      windowHandles = await driver.waitUntilXWindowHandles(3)
 
-      windowHandles = await driver.getAllWindowHandles()
       const popup = windowHandles[2]
       await driver.switchToWindow(popup)
       await driver.delay(regularDelayMs)
