232dd45148a870aacb51419ce083e4afa91904b1
Thomas <thomas.b.huang@gmail.com>
2020-10-27 15:36:15 -0700

    Test removeDash

--- ui/app/helpers/utils/common.util.test.js
+++ ui/app/helpers/utils/common.util.test.js
@@ -24,4 +24,43 @@ describe('Common utils', function () {
       })
     })
   })
+
+  describe('removeDash', function () {
+
+    it('should remove dashes in string', function () {
+      const string = '-0.123456'
+
+      const expectedString = '0.123456'
+
+      const result = utils.removeDash(string)
+      assert.equal(result, expectedString)
+    })
+
+    it('should return a string with no dashes', () => {
+      const string = '0.123456'
+
+      const result = utils.removeDash(string)
+
+      assert.equal(result, string)
+    })
+
+    it('should remove the dash if in the middle of the string', function () {
+      const string = '0.32-02'
+
+      const expectedString = '0.3202'
+
+      const result = utils.removeDash(string)
+      assert.equal(result, expectedString)
+    })
+
+    it('should only remove the first dash', function () {
+      const string = '-0.32-02'
+
+      const expectedString = '0.32-02'
+
+      const result = utils.removeDash(string)
+      assert.equal(result, expectedString)
+    })
+
+  })
 })
