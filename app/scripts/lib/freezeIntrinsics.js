10fa2515e1b0ee8bbaa6bb9aebf70b35f69ce6d0
Etienne Dusseault <etienne.dusseault@gmail.com>
2020-11-02 19:55:37 +0800

    make freezeGlobals an exported function

--- app/scripts/lib/freezeIntrinsics.js
+++ app/scripts/lib/freezeIntrinsics.js
@@ -3,6 +3,6 @@ global.globalThis = global // eslint-disable-line node/no-unsupported-features/e
 require('lavamoat-core/lib/ses.umd.js')
 
 export default freezeIntrinsics = () => {
-  //Freezes all intrinsics
+  // Freezes all intrinsics
   lockdown({ errorTaming: 'unsafe', mathTaming: 'unsafe', dateTaming: 'unsafe' }) // eslint-disable-line no-undef
 }
