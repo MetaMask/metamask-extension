2f167e1823d2db6d72078e67cba85e6204ada3e1
Mark Stacey <markjstacey@gmail.com>
2020-11-04 19:04:41 -0330

    Remove unreachable branches from first time flow switch
    The conditions `isUnlocked` and `!isUnlocked` did account for all
    cases, so any branches after that were unreachable.

--- ui/app/pages/first-time-flow/first-time-flow-switch/first-time-flow-switch.container.js
+++ ui/app/pages/first-time-flow/first-time-flow-switch/first-time-flow-switch.container.js
@@ -2,18 +2,12 @@ import { connect } from 'react-redux'
 import FirstTimeFlowSwitch from './first-time-flow-switch.component'
 
 const mapStateToProps = ({ metamask }) => {
-  const {
-    completedOnboarding,
-    isInitialized,
-    isUnlocked,
-    participateInMetaMetrics: optInMetaMetrics,
-  } = metamask
+  const { completedOnboarding, isInitialized, isUnlocked } = metamask
 
   return {
     completedOnboarding,
     isInitialized,
     isUnlocked,
-    optInMetaMetrics,
   }
 }
 
