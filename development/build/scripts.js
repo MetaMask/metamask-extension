164e84553c4d595944f22b8e20de3969d46ed71d
Etienne Dusseault <etienne.dusseault@gmail.com>
2020-11-06 14:13:30 +0800

    revert back child process

--- development/build/scripts.js
+++ development/build/scripts.js
@@ -7,7 +7,7 @@ const log = require('fancy-log')
 const { assign } = require('lodash')
 const watchify = require('watchify')
 const browserify = require('browserify')
-const envify = require('envify/custom')
+const envify = require('loose-envify/custom')
 const sourcemaps = require('gulp-sourcemaps')
 const sesify = require('sesify')
 const terser = require('gulp-terser-js')
@@ -27,7 +27,6 @@ const {
   createTask,
   composeParallel,
   composeSeries,
-  runInChildProcess,
 } = require('./task')
 
 module.exports = createScriptTasks
@@ -136,7 +135,7 @@ function createScriptTasks({ browserPlatforms, livereload }) {
     const allSubtasks = [
       ...standardSubtasks,
       contentscriptSubtask,
-    ].map((subtask) => runInChildProcess(subtask))
+    ]
     // const allSubtasks = [...standardSubtasks, contentscriptSubtask].map(subtask => (subtask))
     // make a parent task that runs each task in a child thread
     return composeParallel(initiateLiveReload, ...allSubtasks)
