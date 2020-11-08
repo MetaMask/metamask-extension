b44ea9893b1ede1d99b88f34e1caab8d49e7596a
Jim Zhang <jim.zhang@kaleido.io>
2020-11-07 23:43:51 -0500

    Added query string to the reconstructed rpcUrl

--- ui/app/helpers/utils/util.js
+++ ui/app/helpers/utils/util.js
@@ -470,14 +470,14 @@ export async function jsonRpcRequest(rpcUrl, rpcMethod, rpcParams = []) {
     'Content-Type': 'application/json',
   }
   // Convert basic auth URL component to Authorization header
-  const { origin, pathname, username, password } = new URL(rpcUrl)
+  const { origin, pathname, username, password, search } = new URL(rpcUrl)
   // URLs containing username and password needs special processing
   if (username && password) {
     const encodedAuth = Buffer.from(`${username}:${password}`).toString(
       'base64',
     )
     headers.Authorization = `Basic ${encodedAuth}`
-    fetchUrl = `${origin}${pathname}`
+    fetchUrl = `${origin}${pathname}${search}`
   }
   const jsonRpcResponse = await window
     .fetch(fetchUrl, {
