6f18ce71d2db2dc2a6c0d2e5d6fcd58261114d36
Dan Miller <danjm.com@gmail.com>
2020-11-09 14:43:08 -0330

    Revert "Update main-quote-summary designs/styles (#9612)"
    This reverts commit 5456d55c88cb8934efdae936a78522a9064e5854.

--- ui/app/pages/swaps/main-quote-summary/main-quote-summary.stories.js
+++ ui/app/pages/swaps/main-quote-summary/main-quote-summary.stories.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { text, number } from '@storybook/addon-knobs/react'
+import { text, number, boolean } from '@storybook/addon-knobs/react'
 import MainQuoteSummary from './main-quote-summary'
 
 export default {
@@ -8,24 +8,28 @@ export default {
 
 export const BestQuote = () => {
   return (
-    <div
-      style={{
-        width: '360px',
-        height: '224px',
-        border: '1px solid black',
-        padding: '24px',
-      }}
-    >
-      <MainQuoteSummary
-        sourceValue={text('sourceValue', '2000000000000000000')}
-        sourceDecimals={number('sourceDecimals', 18)}
-        sourceSymbol={text('sourceSymbol', 'ETH')}
-        destinationValue={text('destinationValue', '200000000000000000')}
-        destinationDecimals={number('destinationDecimals', 18)}
-        destinationSymbol={text('destinationSymbol', 'ABC')}
-        sourceIconUrl=".storybook/images/metamark.svg"
-        destinationIconUrl=".storybook/images/sai.svg"
-      />
-    </div>
+    <MainQuoteSummary
+      sourceValue={text('sourceValue', '2000000000000000000')}
+      sourceDecimals={number('sourceDecimals', 18)}
+      sourceSymbol={text('sourceSymbol', 'ETH')}
+      destinationValue={text('destinationValue', '200000000000000000')}
+      destinationDecimals={number('destinationDecimals', 18)}
+      destinationSymbol={text('destinationSymbol', 'ABC')}
+      isBestQuote={boolean('isBestQuote', true)}
+    />
+  )
+}
+
+export const NotBestQuote = () => {
+  return (
+    <MainQuoteSummary
+      sourceValue={text('sourceValue', '2000000000000000000')}
+      sourceDecimals={number('sourceDecimals', 18)}
+      sourceSymbol={text('sourceSymbol', 'ETH')}
+      destinationValue={text('destinationValue', '200000000000000000')}
+      destinationDecimals={number('destinationDecimals', 18)}
+      destinationSymbol={text('destinationSymbol', 'ABC')}
+      isBestQuote={boolean('isBestQuote', false)}
+    />
   )
 }
