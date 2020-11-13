d139b945f0d5b3361a49f0ad840989b39b4c625d
Dan J Miller <danjm.com@gmail.com>
2020-11-13 13:42:50 -0330

    Update fee card designs to show savings and MM fee (#9629)
    * Update fee card designs to show savings and MM fee
    
    css touch up
    
    More semantic html and remove unnecessary container wrapper
    
    Update message for case when there are no savings, in new swaps fee card designs
    
    Improve display of tilde in savings designs
    
    * Ensure terms of service is shown when insufficient eth warning is shown on view-quote screen
    
    * Logic simplification in fee-card.js
    
    * Better center info tooltip icons in fee-card
    
    * Add comment about use of \!important in fee card css
    
    * Use container class property on info tooltip in fee card
    
    * Remove function call that was made redundant with 980b14089 but not removed during rebase

--- ui/app/pages/swaps/fee-card/fee-card.stories.js
+++ ui/app/pages/swaps/fee-card/fee-card.stories.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { action } from '@storybook/addon-actions'
-import { text } from '@storybook/addon-knobs/react'
+import { text, boolean, number, object } from '@storybook/addon-knobs/react'
 import FeeCard from './fee-card'
 
 const tokenApprovalTextComponent = (
@@ -35,6 +35,13 @@ export const WithAllProps = () => {
         tokenApprovalSourceTokenSymbol="ABC"
         onTokenApprovalClick={action('Clicked third row link')}
         hideTokenApprovalRow={false}
+        metaMaskFee="0.875"
+        savings={object('savings 1', { total: '8.55' })}
+        onQuotesClick={action('Clicked quotes link')}
+        numberOfQuotes={number('numberOfQuotes', 6)}
+        isBestQuote={boolean('isBestQuote', true)}
+        conversionRate={300}
+        currentCurrency="usd"
       />
     </div>
   )
@@ -55,6 +62,11 @@ export const WithoutThirdRow = () => {
         }}
         onFeeCardMaxRowClick={action('Clicked max fee row link')}
         hideTokenApprovalRow
+        onQuotesClick={action('Clicked quotes link')}
+        numberOfQuotes={number('numberOfQuotes', 1)}
+        isBestQuote={boolean('isBestQuote', true)}
+        savings={object('savings 1', { total: '8.55' })}
+        metaMaskFee="0.875"
       />
     </div>
   )
@@ -70,6 +82,9 @@ export const WithOnlyRequiredProps = () => {
         }}
         onFeeCardMaxRowClick={action('Clicked max fee row link')}
         hideTokenApprovalRow
+        metaMaskFee="0.875"
+        onQuotesClick={action('Clicked quotes link')}
+        numberOfQuotes={2}
       />
     </div>
   )
