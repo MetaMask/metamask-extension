5a80f04dcaf1ddbe771d7ac5e83d7f901a969706
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2020-11-16 08:32:53 -0800

    Consolidate notification UI openers (#9885)
    * Consolidate notification UI openers
    
    * Update tests

--- test/unit/ui/app/actions.spec.js
+++ test/unit/ui/app/actions.spec.js
@@ -44,8 +44,7 @@ describe('Actions', function () {
       platform: { getVersion: () => 'foo' },
       provider,
       keyringController: new KeyringController({}),
-      showUnapprovedTx: noop,
-      showUnconfirmedMessage: noop,
+      showUserConfirmation: noop,
       encryptor: {
         encrypt(_, object) {
           this.object = object
