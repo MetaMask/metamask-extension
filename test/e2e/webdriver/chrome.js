466b9cf4f94c40311d4533727f35514501765fc6
Etienne Dusseault <etienne.dusseault@gmail.com>
2020-11-17 12:23:36 +0800

    add another option

--- test/e2e/webdriver/chrome.js
+++ test/e2e/webdriver/chrome.js
@@ -6,7 +6,7 @@ const chrome = require('selenium-webdriver/chrome')
  */
 class ChromeDriver {
   static async build({ extensionPath, responsive, port }) {
-    const args = [`load-extension=${extensionPath}`, "disable-dev-shm-usage"]
+    const args = [`load-extension=${extensionPath}`, "no-sandbox", "disable-dev-shm-usage"]
     if (responsive) {
       args.push('--auto-open-devtools-for-tabs')
     }
