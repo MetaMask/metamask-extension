2c8313e754f0f44227cb449957a08990e423614c
codeoneline <mutouji1@163.com>
2020-11-24 14:11:33 +0800

    fix bug 9937

--- app/scripts/lib/account-tracker.js
+++ app/scripts/lib/account-tracker.js
@@ -109,8 +109,8 @@ export default class AccountTracker {
       }
     })
 
-    this.addAccounts(accountsToAdd)
     this.removeAccount(accountsToRemove)
+    this.addAccounts(accountsToAdd)
   }
 
   /**
