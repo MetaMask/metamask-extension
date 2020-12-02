f2435293d90f201d20928ebbfae7a651fbf2644e
Erik Marks <rekmarks@protonmail.com>
2020-12-02 12:55:08 -0800

    Remove web3 shim implementation

--- app/scripts/inpage.js
+++ app/scripts/inpage.js
@@ -52,36 +52,3 @@ const metamaskStream = new LocalMessageDuplexStream({
 initProvider({
   connectionStream: metamaskStream,
 })
-
-// If there's no existing window.web3, we inject a web3 "shim" to not break
-// dapps that rely on window.web3.currentProvider.
-if (!window.web3) {
-  const SHIM_IDENTIFIER = '__isMetaMaskShim__'
-  const web3Shim = new Proxy(
-    {
-      currentProvider: window.ethereum,
-      __isMetaMaskShim__: true,
-    },
-    {
-      get: (target, property, ...args) => {
-        if (property === 'currentProvider') {
-          console.warn(
-            'You are accessing the MetaMask window.web3 shim. Just use window.ethereum instead. For details, see: https://docs.metamask.io/guide/ethereum-provider.html',
-          )
-        } else if (property !== SHIM_IDENTIFIER) {
-          console.error(
-            `You are accessing the MetaMask window.web3 shim. The property '${property}' does not exist. For details, see: https://docs.metamask.io/guide/ethereum-provider.html`,
-          )
-        }
-        return Reflect.get(target, property, ...args)
-      },
-    },
-  )
-
-  Object.defineProperty(window, 'web3', {
-    value: Object.freeze(web3Shim),
-    enumerable: false,
-    configurable: true,
-    writable: true,
-  })
-}
