7ac7b8312018d0e7fd0f4d4bb47c894e16e7baad
Etienne Dusseault <etienne.dusseault@gmail.com>
2020-12-07 13:43:57 +0800

    use allow scripts in CI

--- .circleci/scripts/deps-install.sh
+++ .circleci/scripts/deps-install.sh
@@ -7,6 +7,7 @@ set -e
 
 yarn --frozen-lockfile --ignore-scripts --har
 
+<<<<<<< HEAD
 # Move HAR file into directory with consistent name so that we can cache it
 mkdir -p build-artifacts/yarn-install-har
 har_files=(./*.har)
@@ -20,18 +21,8 @@ fi
 #   preinstall
 #   install
 #   postinstall
+=======
+# use allow-scripts instead of manually running install scripts so directory change does not persist
+>>>>>>> use allow scripts in CI
 
-# for build
-(cd node_modules/node-sass && yarn run postinstall)
-(cd node_modules/optipng-bin && yarn run postinstall)
-(cd node_modules/gifsicle && yarn run postinstall)
-(cd node_modules/jpegtran-bin && yarn run postinstall)
-
-# for test
-(cd node_modules/scrypt && yarn run install)
-(cd node_modules/weak && yarn run install)
-(cd node_modules/chromedriver && yarn run install)
-(cd node_modules/geckodriver && yarn run postinstall)
-
-# for release
-(cd node_modules/@sentry/cli && yarn run install)
+yarn allow-scripts
