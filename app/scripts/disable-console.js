47856f8169cf62f0799fa75052d910b4e0b6c906
Etienne Dusseault <etienne.dusseault@gmail.com>
2020-12-10 18:01:16 +0800

    lintfix

--- app/scripts/disable-console.js
+++ app/scripts/disable-console.js
@@ -1,4 +1,7 @@
 // disable console.log in contentscript to prevent SES/lockdown logging to external page
+// eslint-disable-next-line import/unambiguous
 if (typeof window.console !== undefined) {
+  // eslint-disable-next-line no-empty-function
   console.log = () => {}
+
 }
