MetaMask/metamask-extension

commit bf65c979d2e5fe844c636b7b336cef6ab456549b

Use async storage instead of localstorage (#9919)

ui/app/helpers/utils/fetch-with-cache.js
@@ -1,7 +1,4 @@
-import {
-  loadLocalStorageData,
-  saveLocalStorageData,
-} from '../../../lib/local-storage-helpers'
+import { getStorageItem, setStorageItem } from '../../../lib/storage-helpers'
 import fetchWithTimeout from '../../../../app/scripts/lib/fetch-with-timeout'
 
 const fetchWithCache = async (
@@ -27,7 +24,7 @@ const fetchWithCache = async (
   }
 
   const currentTime = Date.now()
-  const cachedFetch = loadLocalStorageData('cachedFetch') || {}
+  const cachedFetch = (await getStorageItem('cachedFetch')) || {}
   const { cachedResponse, cachedTime } = cachedFetch[url] || {}
   if (cachedResponse && currentTime - cachedTime < cacheRefreshTime) {
     return cachedResponse
@@ -52,7 +49,7 @@ const fetchWithCache = async (
     cachedTime: currentTime,
   }
   cachedFetch[url] = cacheEntry
-  saveLocalStorageData(cachedFetch, 'cachedFetch')
+  await setStorageItem('cachedFetch', cachedFetch)
   return responseJson
 }
 

ui/app/helpers/utils/fetch-with-cache.test.js
@@ -3,15 +3,15 @@ import nock from 'nock'
 import sinon from 'sinon'
 import proxyquire from 'proxyquire'
 
-const fakeLocalStorageHelpers = {}
+const fakeStorage = {}
 const fetchWithCache = proxyquire('./fetch-with-cache', {
-  '../../../lib/local-storage-helpers': fakeLocalStorageHelpers,
+  '../../../lib/storage-helpers': fakeStorage,
 }).default
 
 describe('Fetch with cache', function () {
   beforeEach(function () {
-    fakeLocalStorageHelpers.loadLocalStorageData = sinon.stub()
-    fakeLocalStorageHelpers.saveLocalStorageData = sinon.stub()
+    fakeStorage.getStorageItem = sinon.stub()
+    fakeStorage.setStorageItem = sinon.stub()
   })
   afterEach(function () {
     sinon.restore()
@@ -36,7 +36,7 @@ describe('Fetch with cache', function () {
       .get('/price')
       .reply(200, '{"average": 2}')
 
-    fakeLocalStorageHelpers.loadLocalStorageData.returns({
+    fakeStorage.getStorageItem.returns({
       'https://fetchwithcache.metamask.io/price': {
         cachedResponse: { average: 1 },
         cachedTime: Date.now(),
@@ -56,7 +56,7 @@ describe('Fetch with cache', function () {
       .get('/price')
       .reply(200, '{"average": 3}')
 
-    fakeLocalStorageHelpers.loadLocalStorageData.returns({
+    fakeStorage.getStorageItem.returns({
       'https://fetchwithcache.metamask.io/cached': {
         cachedResponse: { average: 1 },
         cachedTime: Date.now() - 1000,

ui/app/ducks/gas/gas-duck.test.js
@@ -2,10 +2,10 @@ import assert from 'assert'
 import sinon from 'sinon'
 import proxyquire from 'proxyquire'
 
-const fakeLocalStorage = {}
+const fakeStorage = {}
 
 const GasDuck = proxyquire('./gas.duck.js', {
-  '../../../lib/local-storage-helpers': fakeLocalStorage,
+  '../../../lib/storage-helpers': fakeStorage,
 })
 
 const {
@@ -160,8 +160,8 @@ describe('Gas Duck', function () {
     tempFetch = window.fetch
     tempDateNow = global.Date.now
 
-    fakeLocalStorage.loadLocalStorageData = sinon.stub()
-    fakeLocalStorage.saveLocalStorageData = sinon.spy()
+    fakeStorage.getStorageItem = sinon.stub()
+    fakeStorage.setStorageItem = sinon.spy()
     window.fetch = sinon.stub().callsFake(fakeFetch)
     global.Date.now = () => 2000000
   })
@@ -412,21 +412,19 @@ describe('Gas Duck', function () {
       ])
     })
 
-    it('should fetch recently retrieved estimates from local storage', async function () {
+    it('should fetch recently retrieved estimates from storage', async function () {
       const mockDistpatch = sinon.spy()
-      fakeLocalStorage.loadLocalStorageData
+      fakeStorage.getStorageItem
         .withArgs('BASIC_PRICE_ESTIMATES_LAST_RETRIEVED')
         .returns(2000000 - 1) // one second ago from "now"
-      fakeLocalStorage.loadLocalStorageData
-        .withArgs('BASIC_PRICE_ESTIMATES')
-        .returns({
-          average: 25,
-          blockTime: 'mockBlock_time',
-          blockNum: 'mockBlockNum',
-          fast: 35,
-          fastest: 45,
-          safeLow: 15,
-        })
+      fakeStorage.getStorageItem.withArgs('BASIC_PRICE_ESTIMATES').returns({
+        average: 25,
+        blockTime: 'mockBlock_time',
+        blockNum: 'mockBlockNum',
+        fast: 35,
+        fastest: 45,
+        safeLow: 15,
+      })
 
       await fetchBasicGasEstimates()(mockDistpatch, () => ({
         gas: { ...initState },
@@ -453,9 +451,9 @@ describe('Gas Duck', function () {
       ])
     })
 
-    it('should fallback to network if retrieving estimates from local storage fails', async function () {
+    it('should fallback to network if retrieving estimates from storage fails', async function () {
       const mockDistpatch = sinon.spy()
-      fakeLocalStorage.loadLocalStorageData
+      fakeStorage.getStorageItem
         .withArgs('BASIC_PRICE_ESTIMATES_LAST_RETRIEVED')
         .returns(2000000 - 1) // one second ago from "now"
 
@@ -541,12 +539,12 @@ describe('Gas Duck', function () {
       ])
     })
 
-    it('should fetch recently retrieved estimates from local storage', async function () {
+    it('should fetch recently retrieved estimates from storage', async function () {
       const mockDistpatch = sinon.spy()
-      fakeLocalStorage.loadLocalStorageData
+      fakeStorage.getStorageItem
         .withArgs('BASIC_GAS_AND_TIME_API_ESTIMATES_LAST_RETRIEVED')
         .returns(2000000 - 1) // one second ago from "now"
-      fakeLocalStorage.loadLocalStorageData
+      fakeStorage.getStorageItem
         .withArgs('BASIC_GAS_AND_TIME_API_ESTIMATES')
         .returns({
           average: 5,
@@ -596,9 +594,9 @@ describe('Gas Duck', function () {
       ])
     })
 
-    it('should fallback to network if retrieving estimates from local storage fails', async function () {
+    it('should fallback to network if retrieving estimates from storage fails', async function () {
       const mockDistpatch = sinon.spy()
-      fakeLocalStorage.loadLocalStorageData
+      fakeStorage.getStorageItem
         .withArgs('BASIC_GAS_AND_TIME_API_ESTIMATES_LAST_RETRIEVED')
         .returns(2000000 - 1) // one second ago from "now"
 

ui/app/ducks/gas/gas.duck.js
@@ -1,9 +1,7 @@
 import { uniqBy, cloneDeep, flatten } from 'lodash'
 import BigNumber from 'bignumber.js'
-import {
-  loadLocalStorageData,
-  saveLocalStorageData,
-} from '../../../lib/local-storage-helpers'
+import { getStorageItem, setStorageItem } from '../../../lib/storage-helpers'
+
 import { decGWEIToHexWEI } from '../../helpers/utils/conversions.util'
 import { isEthereumNetwork } from '../../selectors'
 
@@ -209,7 +207,7 @@ export function fetchBasicGasEstimates() {
     const { basicPriceEstimatesLastRetrieved } = getState().gas
     const timeLastRetrieved =
       basicPriceEstimatesLastRetrieved ||
-      loadLocalStorageData('BASIC_PRICE_ESTIMATES_LAST_RETRIEVED') ||
+      (await getStorageItem('BASIC_PRICE_ESTIMATES_LAST_RETRIEVED')) ||
       0
 
     dispatch(basicGasEstimatesLoadingStarted())
@@ -218,7 +216,7 @@ export function fetchBasicGasEstimates() {
     if (Date.now() - timeLastRetrieved > 75000) {
       basicEstimates = await fetchExternalBasicGasEstimates(dispatch)
     } else {
-      const cachedBasicEstimates = loadLocalStorageData('BASIC_PRICE_ESTIMATES')
+      const cachedBasicEstimates = await getStorageItem('BASIC_PRICE_ESTIMATES')
       basicEstimates =
         cachedBasicEstimates || (await fetchExternalBasicGasEstimates(dispatch))
     }
@@ -259,8 +257,10 @@ async function fetchExternalBasicGasEstimates(dispatch) {
   }
 
   const timeRetrieved = Date.now()
-  saveLocalStorageData(basicEstimates, 'BASIC_PRICE_ESTIMATES')
-  saveLocalStorageData(timeRetrieved, 'BASIC_PRICE_ESTIMATES_LAST_RETRIEVED')
+  await Promise.all([
+    setStorageItem('BASIC_PRICE_ESTIMATES', basicEstimates),
+    setStorageItem('BASIC_PRICE_ESTIMATES_LAST_RETRIEVED', timeRetrieved),
+  ])
   dispatch(setBasicPriceEstimatesLastRetrieved(timeRetrieved))
 
   return basicEstimates
@@ -271,7 +271,9 @@ export function fetchBasicGasAndTimeEstimates() {
     const { basicPriceAndTimeEstimatesLastRetrieved } = getState().gas
     const timeLastRetrieved =
       basicPriceAndTimeEstimatesLastRetrieved ||
-      loadLocalStorageData('BASIC_GAS_AND_TIME_API_ESTIMATES_LAST_RETRIEVED') ||
+      (await getStorageItem(
+        'BASIC_GAS_AND_TIME_API_ESTIMATES_LAST_RETRIEVED',
+      )) ||
       0
 
     dispatch(basicGasEstimatesLoadingStarted())
@@ -280,7 +282,7 @@ export function fetchBasicGasAndTimeEstimates() {
     if (Date.now() - timeLastRetrieved > 75000) {
       basicEstimates = await fetchExternalBasicGasAndTimeEstimates(dispatch)
     } else {
-      const cachedBasicEstimates = loadLocalStorageData(
+      const cachedBasicEstimates = await getStorageItem(
         'BASIC_GAS_AND_TIME_API_ESTIMATES',
       )
       basicEstimates =
@@ -332,11 +334,13 @@ async function fetchExternalBasicGasAndTimeEstimates(dispatch) {
   }
 
   const timeRetrieved = Date.now()
-  saveLocalStorageData(basicEstimates, 'BASIC_GAS_AND_TIME_API_ESTIMATES')
-  saveLocalStorageData(
-    timeRetrieved,
-    'BASIC_GAS_AND_TIME_API_ESTIMATES_LAST_RETRIEVED',
-  )
+  await Promise.all([
+    setStorageItem('BASIC_GAS_AND_TIME_API_ESTIMATES', basicEstimates),
+    setStorageItem(
+      'BASIC_GAS_AND_TIME_API_ESTIMATES_LAST_RETRIEVED',
+      timeRetrieved,
+    ),
+  ])
   dispatch(setBasicApiEstimatesLastRetrieved(timeRetrieved))
 
   return basicEstimates
@@ -403,11 +407,11 @@ function inliersByIQR(data, prop) {
 }
 
 export function fetchGasEstimates(blockTime) {
-  return (dispatch, getState) => {
+  return async (dispatch, getState) => {
     const state = getState()
 
     if (!isEthereumNetwork(state)) {
-      return Promise.resolve(null)
+      return
     }
 
     const {
@@ -416,114 +420,112 @@ export function fetchGasEstimates(blockTime) {
     } = state.gas
     const timeLastRetrieved =
       priceAndTimeEstimatesLastRetrieved ||
-      loadLocalStorageData('GAS_API_ESTIMATES_LAST_RETRIEVED') ||
+      (await getStorageItem('GAS_API_ESTIMATES_LAST_RETRIEVED')) ||
       0
 
     dispatch(gasEstimatesLoadingStarted())
 
-    const promiseToFetch =
-      Date.now() - timeLastRetrieved > 75000
-        ? queryEthGasStationPredictionTable()
-            .then((r) => r.json())
-            .then((r) => {
-              const estimatedPricesAndTimes = r.map(
-                ({ expectedTime, expectedWait, gasprice }) => ({
-                  expectedTime,
-                  expectedWait,
-                  gasprice,
-                }),
-              )
-              const estimatedTimeWithUniquePrices = uniqBy(
-                estimatedPricesAndTimes,
-                ({ expectedTime }) => expectedTime,
-              )
+    const shouldGetFreshGasQuote = Date.now() - timeLastRetrieved > 75000
+    let estimates
+    if (shouldGetFreshGasQuote) {
+      const response = await queryEthGasStationPredictionTable()
+      const tableJson = await response.json()
 
-              const withSupplementalTimeEstimates = flatten(
-                estimatedTimeWithUniquePrices.map(
-                  ({ expectedWait, gasprice }, i, arr) => {
-                    const next = arr[i + 1]
-                    if (!next) {
-                      return [{ expectedWait, gasprice }]
-                    }
-                    const supplementalPrice = getRandomArbitrary(
-                      gasprice,
-                      next.gasprice,
-                    )
-                    const supplementalTime = extrapolateY({
-                      higherY: next.expectedWait,
-                      lowerY: expectedWait,
-                      higherX: next.gasprice,
-                      lowerX: gasprice,
-                      xForExtrapolation: supplementalPrice,
-                    })
-                    const supplementalPrice2 = getRandomArbitrary(
-                      supplementalPrice,
-                      next.gasprice,
-                    )
-                    const supplementalTime2 = extrapolateY({
-                      higherY: next.expectedWait,
-                      lowerY: supplementalTime,
-                      higherX: next.gasprice,
-                      lowerX: supplementalPrice,
-                      xForExtrapolation: supplementalPrice2,
-                    })
-                    return [
-                      { expectedWait, gasprice },
-                      {
-                        expectedWait: supplementalTime,
-                        gasprice: supplementalPrice,
-                      },
-                      {
-                        expectedWait: supplementalTime2,
-                        gasprice: supplementalPrice2,
-                      },
-                    ]
-                  },
-                ),
-              )
-              const withOutliersRemoved = inliersByIQR(
-                withSupplementalTimeEstimates.slice(0).reverse(),
-                'expectedWait',
-              ).reverse()
-              const timeMappedToSeconds = withOutliersRemoved.map(
-                ({ expectedWait, gasprice }) => {
-                  const expectedTime = new BigNumber(expectedWait)
-                    .times(Number(blockTime), 10)
-                    .toNumber()
-                  return {
-                    expectedTime,
-                    gasprice: new BigNumber(gasprice, 10).toNumber(),
-                  }
-                },
-              )
+      const estimatedPricesAndTimes = tableJson.map(
+        ({ expectedTime, expectedWait, gasprice }) => ({
+          expectedTime,
+          expectedWait,
+          gasprice,
+        }),
+      )
+      const estimatedTimeWithUniquePrices = uniqBy(
+        estimatedPricesAndTimes,
+        ({ expectedTime }) => expectedTime,
+      )
 
-              const timeRetrieved = Date.now()
-              dispatch(setApiEstimatesLastRetrieved(timeRetrieved))
-              saveLocalStorageData(
-                timeRetrieved,
-                'GAS_API_ESTIMATES_LAST_RETRIEVED',
-              )
-              saveLocalStorageData(timeMappedToSeconds, 'GAS_API_ESTIMATES')
-
-              return timeMappedToSeconds
+      const withSupplementalTimeEstimates = flatten(
+        estimatedTimeWithUniquePrices.map(
+          ({ expectedWait, gasprice }, i, arr) => {
+            const next = arr[i + 1]
+            if (!next) {
+              return [{ expectedWait, gasprice }]
+            }
+            const supplementalPrice = getRandomArbitrary(
+              gasprice,
+              next.gasprice,
+            )
+            const supplementalTime = extrapolateY({
+              higherY: next.expectedWait,
+              lowerY: expectedWait,
+              higherX: next.gasprice,
+              lowerX: gasprice,
+              xForExtrapolation: supplementalPrice,
             })
-        : Promise.resolve(
-            priceAndTimeEstimates.length
-              ? priceAndTimeEstimates
-              : loadLocalStorageData('GAS_API_ESTIMATES'),
-          )
+            const supplementalPrice2 = getRandomArbitrary(
+              supplementalPrice,
+              next.gasprice,
+            )
+            const supplementalTime2 = extrapolateY({
+              higherY: next.expectedWait,
+              lowerY: supplementalTime,
+              higherX: next.gasprice,
+              lowerX: supplementalPrice,
+              xForExtrapolation: supplementalPrice2,
+            })
+            return [
+              { expectedWait, gasprice },
+              {
+                expectedWait: supplementalTime,
+                gasprice: supplementalPrice,
+              },
+              {
+                expectedWait: supplementalTime2,
+                gasprice: supplementalPrice2,
+              },
+            ]
+          },
+        ),
+      )
+      const withOutliersRemoved = inliersByIQR(
+        withSupplementalTimeEstimates.slice(0).reverse(),
+        'expectedWait',
+      ).reverse()
+      const timeMappedToSeconds = withOutliersRemoved.map(
+        ({ expectedWait, gasprice }) => {
+          const expectedTime = new BigNumber(expectedWait)
+            .times(Number(blockTime), 10)
+            .toNumber()
+          return {
+            expectedTime,
+            gasprice: new BigNumber(gasprice, 10).toNumber(),
+          }
+        },
+      )
 
-    return promiseToFetch.then((estimates) => {
-      dispatch(setPricesAndTimeEstimates(estimates))
-      dispatch(gasEstimatesLoadingFinished())
-    })
+      const timeRetrieved = Date.now()
+      dispatch(setApiEstimatesLastRetrieved(timeRetrieved))
+
+      await Promise.all([
+        setStorageItem('GAS_API_ESTIMATES_LAST_RETRIEVED', timeRetrieved),
+        setStorageItem('GAS_API_ESTIMATES', timeMappedToSeconds),
+      ])
+
+      estimates = timeMappedToSeconds
+    } else if (priceAndTimeEstimates.length) {
+      estimates = priceAndTimeEstimates
+    } else {
+      estimates = await getStorageItem('GAS_API_ESTIMATES')
+    }
+
+    dispatch(setPricesAndTimeEstimates(estimates))
+    dispatch(gasEstimatesLoadingFinished())
   }
 }
 
 export function setCustomGasPriceForRetry(newPrice) {
-  return (dispatch) => {
+  return async (dispatch) => {
     if (newPrice === '0x0') {
-      const { fast } = loadLocalStorageData('BASIC_PRICE_ESTIMATES')
+      const { fast } = await getStorageItem('BASIC_PRICE_ESTIMATES')
       dispatch(setCustomGasPrice(decGWEIToHexWEI(fast)))
     } else {
       dispatch(setCustomGasPrice(newPrice))

ui/app/ducks/swaps/swaps.js
@@ -2,10 +2,8 @@ import { createSlice } from '@reduxjs/toolkit'
 import BigNumber from 'bignumber.js'
 import log from 'loglevel'
 
-import {
-  loadLocalStorageData,
-  saveLocalStorageData,
-} from '../../../lib/local-storage-helpers'
+import { getStorageItem, setStorageItem } from '../../../lib/storage-helpers'
+
 import {
   addToken,
   addUnapprovedTransaction,
@@ -754,7 +752,7 @@ export function fetchMetaSwapsGasPriceEstimates() {
     )
     const timeLastRetrieved =
       priceEstimatesLastRetrieved ||
-      loadLocalStorageData('METASWAP_GAS_PRICE_ESTIMATES_LAST_RETRIEVED') ||
+      (await getStorageItem('METASWAP_GAS_PRICE_ESTIMATES_LAST_RETRIEVED')) ||
       0
 
     dispatch(swapGasPriceEstimatesFetchStarted())
@@ -764,7 +762,7 @@ export function fetchMetaSwapsGasPriceEstimates() {
       if (Date.now() - timeLastRetrieved > 30000) {
         priceEstimates = await fetchSwapsGasPrices()
       } else {
-        const cachedPriceEstimates = loadLocalStorageData(
+        const cachedPriceEstimates = await getStorageItem(
           'METASWAP_GAS_PRICE_ESTIMATES',
         )
         priceEstimates = cachedPriceEstimates || (await fetchSwapsGasPrices())
@@ -795,11 +793,13 @@ export function fetchMetaSwapsGasPriceEstimates() {
 
     const timeRetrieved = Date.now()
 
-    saveLocalStorageData(priceEstimates, 'METASWAP_GAS_PRICE_ESTIMATES')
-    saveLocalStorageData(
-      timeRetrieved,
-      'METASWAP_GAS_PRICE_ESTIMATES_LAST_RETRIEVED',
-    )
+    await Promise.all([
+      setStorageItem('METASWAP_GAS_PRICE_ESTIMATES', priceEstimates),
+      setStorageItem(
+        'METASWAP_GAS_PRICE_ESTIMATES_LAST_RETRIEVED',
+        timeRetrieved,
+      ),
+    ])
 
     dispatch(
       swapGasPriceEstimatesFetchCompleted({

ui/app/hooks/useRetryTransaction.js
@@ -38,7 +38,7 @@ export function useRetryTransaction(transactionGroup) {
       await dispatch(fetchGasEstimates(basicEstimates.blockTime))
       const transaction = initialTransaction
       const increasedGasPrice = increaseLastGasPrice(gasPrice)
-      dispatch(
+      await dispatch(
         setCustomGasPriceForRetry(
           increasedGasPrice || transaction.txParams.gasPrice,
         ),

ui/lib/local-storage-helpers.js
@@ -1,20 +1,0 @@
-export function loadLocalStorageData(itemKey) {
-  try {
-    const serializedData = window.localStorage.getItem(itemKey)
-    if (serializedData === null) {
-      return undefined
-    }
-    return JSON.parse(serializedData)
-  } catch (err) {
-    return undefined
-  }
-}
-
-export function saveLocalStorageData(data, itemKey) {
-  try {
-    const serializedData = JSON.stringify(data)
-    window.localStorage.setItem(itemKey, serializedData)
-  } catch (err) {
-    console.warn(err)
-  }
-}

ui/lib/storage-helpers.js
@@ -1,0 +1,23 @@
+import localforage from 'localforage'
+
+export async function getStorageItem(key) {
+  try {
+    const serializedData = await localforage.getItem(key)
+    if (serializedData === null) {
+      return undefined
+    }
+
+    return JSON.parse(serializedData)
+  } catch (err) {
+    return undefined
+  }
+}
+
+export async function setStorageItem(key, value) {
+  try {
+    const serializedData = JSON.stringify(value)
+    await localforage.setItem(key, serializedData)
+  } catch (err) {
+    console.warn(err)
+  }
+}

commit c5fdc9103bf7e265d5541fee0e8d99f6a9f36285

Fix `fetch-with-cache` handling of interwoven requests
A data race was introduced in #9919 when the old synchronous storage
API was replaced with an async storage API. The problem arises when
`fetchWithCache` is called a second time while it's still processing
another call. In this case, the `cachedFetch` object can become
stale while blocked waiting for a fetch response, and result in a cache
being overwritten unintentionally.

See this example (options omitted for simplicity, and assuming an empty
initial cache):

```
await Promise.all([
  fetchWithCache('https://metamask.io/foo'),
  fetchWithCache('https://metamask.io/bar'),
]
```

The order of events could be as follows:

1. Empty cache retrieved for `/foo` route
2. Empty cache retrieved for `/bar` route
3. Call made to `/foo` route
4. Call made to `/bar` route
5. `/foo` response is added to the empty cache object retrieved in
  step 1, then is saved in the cache.
6. `/bar` response is added to the empty cache object retrieved in
  step 2, then is saved in the cache.

In step 6, the cache object saved would not contain the `/foo`
response set in step 5. As a result, `/foo` would never be cached.

This problem was resolved by embedding the URL being cached directly in
the cache key. This prevents simultaneous responses from overwriting
each others caches.

Technically a data race still exists when handing simultaneous
responses to the same route, but the result would be that the last call
to finish would overwrite the previous. This seems acceptable.

ui/app/helpers/utils/fetch-with-cache.js
@@ -16,7 +16,6 @@ const fetchWithCache = async (
     fetchOptions.headers = new window.Headers(fetchOptions.headers)
   }
   if (
-    fetchOptions.headers &&
     fetchOptions.headers.has('Content-Type') &&
     fetchOptions.headers.get('Content-Type') !== 'application/json'
   ) {
@@ -24,8 +23,8 @@ const fetchWithCache = async (
   }
 
   const currentTime = Date.now()
-  const cachedFetch = (await getStorageItem('cachedFetch')) || {}
-  const { cachedResponse, cachedTime } = cachedFetch[url] || {}
+  const cacheKey = `cachedFetch:${url}`
+  const { cachedResponse, cachedTime } = (await getStorageItem(cacheKey)) || {}
   if (cachedResponse && currentTime - cachedTime < cacheRefreshTime) {
     return cachedResponse
   }
@@ -48,8 +47,8 @@ const fetchWithCache = async (
     cachedResponse: responseJson,
     cachedTime: currentTime,
   }
-  cachedFetch[url] = cacheEntry
-  await setStorageItem('cachedFetch', cachedFetch)
+
+  await setStorageItem(cacheKey, cacheEntry)
   return responseJson
 }
 

ui/app/helpers/utils/fetch-with-cache.test.js
@@ -37,10 +37,8 @@ describe('Fetch with cache', function () {
       .reply(200, '{"average": 2}')
 
     fakeStorage.getStorageItem.returns({
-      'https://fetchwithcache.metamask.io/price': {
-        cachedResponse: { average: 1 },
-        cachedTime: Date.now(),
-      },
+      cachedResponse: { average: 1 },
+      cachedTime: Date.now(),
     })
 
     const response = await fetchWithCache(
@@ -57,10 +55,8 @@ describe('Fetch with cache', function () {
       .reply(200, '{"average": 3}')
 
     fakeStorage.getStorageItem.returns({
-      'https://fetchwithcache.metamask.io/cached': {
-        cachedResponse: { average: 1 },
-        cachedTime: Date.now() - 1000,
-      },
+      cachedResponse: { average: 1 },
+      cachedTime: Date.now() - 1000,
     })
 
     const response = await fetchWithCache(
@@ -133,6 +129,45 @@ describe('Fetch with cache', function () {
           headers: { 'Content-Type': 'text/plain' },
         }),
       { message: 'fetchWithCache only supports JSON responses' },
+    )
+  })
+
+  it('should correctly cache responses from interwoven requests', async function () {
+    nock('https://fetchwithcache.metamask.io')
+      .get('/foo')
+      .reply(200, '{"average": 9}')
+    nock('https://fetchwithcache.metamask.io')
+      .get('/bar')
+      .reply(200, '{"average": 9}')
+
+    const testCache = {}
+    fakeStorage.getStorageItem.callsFake((key) => testCache[key])
+    fakeStorage.setStorageItem.callsFake((key, value) => {
+      testCache[key] = value
+    })
+
+    await Promise.all([
+      fetchWithCache(
+        'https://fetchwithcache.metamask.io/foo',
+        {},
+        { cacheRefreshTime: 123 },
+      ),
+      fetchWithCache(
+        'https://fetchwithcache.metamask.io/bar',
+        {},
+        { cacheRefreshTime: 123 },
+      ),
+    ])
+
+    assert.deepStrictEqual(
+      testCache['cachedFetch:https://fetchwithcache.metamask.io/foo']
+        .cachedResponse,
+      { average: 9 },
+    )
+    assert.deepStrictEqual(
+      testCache['cachedFetch:https://fetchwithcache.metamask.io/bar']
+        .cachedResponse,
+      { average: 9 },
     )
   })
 })