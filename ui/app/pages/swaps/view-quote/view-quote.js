1b19f5e7ad411b97e4bfffd22012e20851f91bb8
David Walsh <davidwalsh83@gmail.com>
2020-12-15 11:16:51 -0600

    Fix 9874 - Improve gas maximum estimation  (#10043)

--- ui/app/pages/swaps/view-quote/view-quote.js
+++ ui/app/pages/swaps/view-quote/view-quote.js
@@ -12,6 +12,7 @@ import { useSwapsEthToken } from '../../../hooks/useSwapsEthToken'
 import { MetaMetricsContext } from '../../../contexts/metametrics.new'
 import FeeCard from '../fee-card'
 import {
+  FALLBACK_GAS_MULTIPLIER,
   getQuotes,
   getSelectedQuote,
   getApproveTxParams,
@@ -57,7 +58,6 @@ import {
 } from '../../../helpers/utils/token-util'
 import {
   decimalToHex,
-  hexMax,
   hexToDecimal,
   getValueFromWeiHex,
 } from '../../../helpers/utils/conversions.util'
@@ -123,18 +123,16 @@ export default function ViewQuote() {
     usedQuote?.gasEstimateWithRefund ||
     `0x${decimalToHex(usedQuote?.averageGas || 0)}`
 
-  const gasLimitForMax =
-    usedQuote?.gasEstimate || `0x${decimalToHex(usedQuote?.averageGas || 0)}`
+  const gasLimitForMax = usedQuote?.gasEstimate || `0x0`
 
   const usedGasLimitWithMultiplier = new BigNumber(gasLimitForMax, 16)
-    .times(1.4, 10)
+    .times(usedQuote?.gasMultiplier || FALLBACK_GAS_MULTIPLIER, 10)
     .round(0)
     .toString(16)
 
-  const nonCustomMaxGasLimit = hexMax(
-    `0x${decimalToHex(usedQuote?.maxGas || 0)}`,
-    usedGasLimitWithMultiplier,
-  )
+  const nonCustomMaxGasLimit = usedQuote?.gasEstimate
+    ? usedGasLimitWithMultiplier
+    : `0x${decimalToHex(usedQuote?.maxGas || 0)}`
   const maxGasLimit = customMaxGas || nonCustomMaxGasLimit
 
   const gasTotalInWeiHex = calcGasTotal(maxGasLimit, gasPrice)
