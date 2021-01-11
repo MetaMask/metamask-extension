be702d8345035c96d7e7d05f21a76410542f307c
Dan Miller <danjm.com@gmail.com>
2021-01-11 00:42:16 -0330

    Migration 51: ensure chainId is set in network controller provider state for all infura/default networks

---
+++ app/scripts/migrations/051.js
@@ -0,0 +1,29 @@
+import { cloneDeep } from 'lodash'
+import { NETWORK_TYPE_TO_ID_MAP } from '../controllers/network/enums'
+
+const version = 51
+
+/**
+ * Set the chainId in the Network Controller provider data for all infura networks
+ */
+export default {
+  version,
+  async migrate(originalVersionedData) {
+    const versionedData = cloneDeep(originalVersionedData)
+    versionedData.meta.version = version
+    const state = versionedData.data
+    versionedData.data = transformState(state)
+    return versionedData
+  },
+}
+
+function transformState(state) {
+  const type = state?.NetworkController?.provider?.type
+
+  if (NETWORK_TYPE_TO_ID_MAP[type]) {
+    state.NetworkController.provider.chainId =
+      NETWORK_TYPE_TO_ID_MAP[type]?.chainId
+  }
+
+  return state
+}
