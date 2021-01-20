b150a5aa31c4f22345c96bf107926638b2155dfc
Erik Marks <rekmarks@protonmail.com>
2021-01-20 14:17:48 -0800

    Add justification of MAX_SAFE_CHAIN_ID

--- shared/constants/network.js
+++ shared/constants/network.js
@@ -19,7 +19,7 @@ export const KOVAN_CHAIN_ID = '0x2a'
 
 /**
  * The largest possible chain ID we can handle.
- * TODO: Add link to document explaining why.
+ * Explanation: https://gist.github.com/rekmarks/a47bd5f2525936c4b8eee31a16345553
  */
 export const MAX_SAFE_CHAIN_ID = 4503599627370476
 
