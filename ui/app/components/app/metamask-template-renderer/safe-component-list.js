96933b3faf1327a023a62bc5e3e1d127d98763ef
Brad Decker <git@braddecker.dev>
2021-02-02 12:14:04 -0600

    add MetaMask Template Renderer (#10307)
    * add MetaMask Template Renderer
    
    * add areEqual fn and change acc var name
    
    * use key

---
+++ ui/app/components/app/metamask-template-renderer/safe-component-list.js
@@ -0,0 +1,21 @@
+import Button from '../../ui/button'
+import Chip from '../../ui/chip'
+import DefinitionList from '../../ui/definition-list'
+import TruncatedDefinitionList from '../../ui/truncated-definition-list'
+import Popover from '../../ui/popover'
+import Typography from '../../ui/typography'
+import Box from '../../ui/box'
+
+export const safeComponentList = {
+  b: 'b',
+  p: 'p',
+  div: 'div',
+  span: 'span',
+  Typography,
+  Chip,
+  DefinitionList,
+  TruncatedDefinitionList,
+  Button,
+  Popover,
+  Box,
+}
