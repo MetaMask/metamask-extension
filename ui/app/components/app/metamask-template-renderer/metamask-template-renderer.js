cd94e2725385a94de0c4e26eaf8a74decd62cde2
Brad Decker <git@braddecker.dev>
2021-02-02 10:30:02 -0600

    use key

--- ui/app/components/app/metamask-template-renderer/metamask-template-renderer.js
+++ ui/app/components/app/metamask-template-renderer/metamask-template-renderer.js
@@ -1,41 +1,72 @@
 import React, { memo } from 'react'
 import PropTypes from 'prop-types'
-import hash from 'object-hash'
 import { isEqual } from 'lodash'
 import { safeComponentList } from './safe-component-list'
 
+function getElement(section) {
+  const { element } = section
+  const Element = safeComponentList[element]
+  if (!Element) {
+    throw new Error(
+      `${element} is not in the safe component list for MetaMask template renderer`,
+    )
+  }
+  return Element
+}
+
 const MetaMaskTemplateRenderer = ({ sections }) => {
   if (!sections) {
+    // If sections is null eject early by returning null
     return null
-  }
-  if (typeof sections === 'string') {
+  } else if (typeof sections === 'string') {
+    // React can render strings directly, so return the string
     return sections
+  } else if (
+    sections &&
+    typeof sections === 'object' &&
+    !Array.isArray(sections)
+  ) {
+    // If dealing with a single entry, then render a single object without key
+    const Element = getElement(sections)
+    return (
+      <Element {...sections.props}>
+        {typeof sections.children === 'object' ? (
+          <MetaMaskTemplateRenderer sections={sections.children} />
+        ) : (
+          sections?.children
+        )}
+      </Element>
+    )
   }
+
+  // The last case is dealing with an array of objects
   return (
     <>
-      {sections.reduce((allChildren, child, index) => {
-        // React can render strings directly, so push them into the accumulator
+      {sections.reduce((allChildren, child) => {
         if (typeof child === 'string') {
+          // React can render strings directly, so push them into the accumulator
           allChildren.push(child)
-        } else if (child) {
-          // The other option is one of our Sections, which contains
-          // element, children, and props.
-          const key = hash([sections, child, index])
-          const { element, children, props } = child
-          const Element = safeComponentList[element]
-          if (!Element) {
+        } else {
+          // If the entry in array is not a string, then it must be a Section.
+          // Sections are handled by the main function, but must
+          // be provided a key when a part of an array.
+          if (!child.key) {
             throw new Error(
-              `${element} is not in the safe component list for MetaMask template renderer`,
+              'When using array syntax in MetaMask Template Language, you must specify a key for each child of the array',
             )
           }
-          const childrenOrNull =
-            Element && children ? (
-              <MetaMaskTemplateRenderer key={key} sections={children} />
-            ) : null
-          if (Element) {
+          if (typeof child?.children === 'object') {
+            // If this child has its own children, check if children is an
+            // object, and in that case use recursion to render.
             allChildren.push(
-              <Element key={key} {...props}>
-                {childrenOrNull}
+              <MetaMaskTemplateRenderer sections={child} key={child.key} />,
+            )
+          } else {
+            // Otherwise render the element.
+            const Element = getElement(child)
+            allChildren.push(
+              <Element key={child.key} {...child.props}>
+                {child?.children}
               </Element>,
             )
           }
@@ -49,10 +80,12 @@ const MetaMaskTemplateRenderer = ({ sections }) => {
 const SectionShape = {
   props: PropTypes.object,
   element: PropTypes.oneOf(Object.keys(safeComponentList)).isRequired,
+  key: PropTypes.string,
 }
 
 const ValidChildren = PropTypes.oneOfType([
   PropTypes.string,
+  PropTypes.shape(SectionShape),
   PropTypes.arrayOf(
     PropTypes.oneOfType([PropTypes.shape(SectionShape), PropTypes.string]),
   ),
--- ui/app/components/app/metamask-template-renderer/metamask-template-renderer.stories.js
+++ ui/app/components/app/metamask-template-renderer/metamask-template-renderer.stories.js
@@ -7,79 +7,83 @@ export default {
   title: 'MetaMask Template Renderer',
 }
 
-const SECTIONS = [
-  {
-    element: 'Box',
-    props: {
-      margin: 4,
-      padding: 8,
-      borderColor: COLORS.PRIMARY1,
-      borderWidth: 2,
-    },
-    children: [
-      {
-        element: 'Typography',
-        children: 'A Test String',
-        props: {
-          color: COLORS.UI3,
-          variant: TYPOGRAPHY.H2,
-        },
-      },
-      {
-        element: 'Typography',
-        children: 'Some more text as a paragraph',
-        props: {
-          color: COLORS.UI4,
-          variant: TYPOGRAPHY.Paragraph,
-        },
-      },
-      {
-        element: 'TruncatedDefinitionList',
-        props: {
-          dictionary: {
-            term:
-              'a word or phrase used to describe a thing or to express a concept, especially in a particular kind of language or branch of study.',
-            definition:
-              'a statement of the exact meaning of a word, especially in a dictionary.',
-            dl: 'HTML tag denoting a definition list',
-            dt: 'HTML tag denoting a definition list term',
-            dd: 'HTML tag denoting a definition list definition',
-          },
-          prefaceKeys: ['term', 'definition'],
-        },
-      },
-      {
-        element: 'Box',
-        children: [
-          {
-            element: 'Button',
-            children: 'Cancel',
-            props: {
-              rounded: true,
-              type: 'outlined',
-              style: {
-                width: '45%',
-              },
-            },
-          },
-          {
-            element: 'Button',
-            children: 'OK',
-            props: {
-              rounded: true,
-              type: 'primary',
-              style: {
-                width: '45%',
-              },
-            },
-          },
-        ],
-        props: { justifyContent: 'space-between', padding: [0, 4] },
-      },
-    ],
+const SECTIONS = {
+  element: 'Box',
+  props: {
+    margin: 4,
+    padding: 8,
+    borderColor: COLORS.PRIMARY1,
+    borderWidth: 2,
   },
-]
-
+  children: [
+    {
+      element: 'Typography',
+      key: 'A Test String',
+      children: 'A Test String',
+      props: {
+        color: COLORS.UI3,
+        variant: TYPOGRAPHY.H2,
+      },
+    },
+    {
+      element: 'Typography',
+      key: 'Some more text',
+      children: 'Some more text as a paragraph',
+      props: {
+        color: COLORS.UI4,
+        variant: TYPOGRAPHY.Paragraph,
+      },
+    },
+    {
+      element: 'TruncatedDefinitionList',
+      key: 'TDL',
+      props: {
+        dictionary: {
+          term:
+            'a word or phrase used to describe a thing or to express a concept, especially in a particular kind of language or branch of study.',
+          definition:
+            'a statement of the exact meaning of a word, especially in a dictionary.',
+          dl: 'HTML tag denoting a definition list',
+          dt: 'HTML tag denoting a definition list term',
+          dd: 'HTML tag denoting a definition list definition',
+        },
+        title: 'Full list',
+        prefaceKeys: ['term', 'definition'],
+      },
+    },
+    {
+      element: 'Box',
+      key: 'ActionsBox',
+      children: [
+        {
+          element: 'Button',
+          children: 'Cancel',
+          key: 'cancel-button',
+          props: {
+            rounded: true,
+            type: 'outlined',
+            style: {
+              width: '45%',
+            },
+          },
+        },
+        {
+          element: 'Button',
+          children: 'OK',
+          key: 'ok-button',
+          props: {
+            rounded: true,
+            type: 'primary',
+            style: {
+              width: '45%',
+            },
+          },
+        },
+      ],
+      props: { justifyContent: 'space-between', padding: [0, 4] },
+    },
+  ],
+}
 export const metaMaskTemplateRenderer = () => (
   <MetaMaskTemplateRenderer sections={object('sections', SECTIONS)} />
 )
@@ -87,9 +91,13 @@ export const metaMaskTemplateRenderer = () => (
 export const withInvalidElement = () => (
   <MetaMaskTemplateRenderer
     sections={object('sections', [
-      ...SECTIONS,
+      {
+        ...SECTIONS,
+        key: 'safe-tree',
+      },
       {
         element: 'Unsafe',
+        key: 'unsafe-tree',
         children:
           'I should be displayed, but I wont be due to unsafe component',
       },
