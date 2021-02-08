1279cea50d86977aeb1ab718b9a16e3d63af5e9c
Erik Marks <rekmarks@protonmail.com>
2021-02-08 12:21:32 -0800

    Initial implementation (squashed)
    @mm-snap/*@0.0.5, include filsnap in bundle
    
    * Implement bls-signer support
    * Implement filsnap support
    * Add hooknames and hook selectors to RPC methods
    * Fix filsnap installation on vault creation
    
    Various build and CI config updates
    
    * Update sourcemap validation
    * Update Sentry configuration
    * Change name of production environment to 'filecoin-production'
    * Ignore useless sentry error in e2e tests
    
    Update README.md
    
    post-rebase yarn setup
    
    yarn lint:fix
    
    Snaps update 0.0.7 (#11854)
    
    * update to use 0.0.6
    
    * updatge snaps libs to 0.0.7
    
    * fix remove iframe-execution-environment service
    
    * Remove iframe execution environment
    
    Added iframe execution environment (#11865)
    
    * Added iframe execution environment
    
    * Remove WebWorkerExecutionEnvironmentService import
    
    Ignore filsnap bundle in depcheckrc (#11866)
    
    This stops the `depcheck` check from failing.
    
    Restore Firefox compatibility (#11867)
    
    Now that we have the iframe execution environment, we can remove `unsafe-eval: true` from the extension manifest CSP, reactivate the Firefox lint jobs, and get this show back on the road.
    
    Notify user of snap initial execution errors (#11901)
    
    * Added alert on snap initial execution errors
    
    * Changed window.alert to be a browser notification
    
    * Added guard for pluginController removePlugin
    
    Run yarn setup + fix controllerMessenger reference
    
    Fixed rebase issues
    
    Remove Pre-Installed Filsnap (#12111)
    
    Add more global snap hooks (#12113)
    
    Rebase fixup

--- app/scripts/controllers/permissions/restrictedMethods.js
+++ app/scripts/controllers/permissions/restrictedMethods.js
@@ -1,10 +1,82 @@
+import {
+  restrictedMethods as restrictedPluginMethods,
+  selectHooks,
+} from '@mm-snap/rpc-methods';
+
+/**
+ * @typedef RestrictedMethodHooks
+ *
+ * @property {Function} getIdentities - Gets all account identity abstractions,
+ * containing account metadata.
+ * @property {Function} getKeyringAccounts - Gets all current keyring accounts.
+ * @property {Function} addPlugin - Installs a requested plugin.
+ * @property {Function} getPlugin - Gets metadata for a specific plugin.
+ * @property {Function} getPluginRpcHandler - Gets the RPC message handler for a specific plugin.
+ * @property {Function} showConfirmation - Displays a confirmation for user action.
+ */
+
+/**
+ * @typedef PluginRestrictedMethodHooks
+ *
+ * @property {Function} addPlugin - Installs a requested plugin.
+ * @property {Function} getPlugin - Gets metadata for a specific plugin.
+ * @property {Function} getPluginRpcHandler - Gets the RPC message handler for a specific plugin.
+ * @property {Function} showConfirmation - Displays a confirmation for user action.
+ */
+
+/**
+ * @param {RestrictedMethodHooks} hooks - Restricted method hooks.
+ */
 export default function getRestrictedMethods({
+  addPlugin,
+  clearSnapState,
+  getIdentities,
+  getKeyringAccounts,
+  getMnemonic,
+  getPlugin,
+  getPluginRpcHandler,
+  getSnapState,
+  handleAssetRequest,
+  showConfirmation,
+  updateSnapState,
+}) {
+  return {
+    ...getCommonRestrictedMethods({ getIdentities, getKeyringAccounts }),
+    ...getPluginRestrictedMethods({
+      addPlugin,
+      clearSnapState,
+      getPlugin,
+      getPluginRpcHandler,
+      getMnemonic,
+      getSnapState,
+      handleAssetRequest,
+      showConfirmation,
+      updateSnapState,
+    }),
+  };
+}
+
+/**
+ * @param {PluginRestrictedMethodHooks} hooks - Plugin restricted method hooks.
+ */
+export function getPluginRestrictedMethods(hooks) {
+  return restrictedPluginMethods.reduce((restrictedMethods, handler) => {
+    restrictedMethods[handler.methodNames[0]] = {
+      description: handler.permissionDescription,
+      method: handler.getImplementation(selectHooks(hooks, handler.hookNames)),
+    };
+    return restrictedMethods;
+  }, {});
+}
+
+export function getCommonRestrictedMethods({
   getIdentities,
   getKeyringAccounts,
 }) {
   return {
     eth_accounts: {
-      method: async (_, res, __, end) => {
+      description: 'View Ethereum accounts',
+      method: async (_req, res, _next, end) => {
         try {
           const accounts = await getKeyringAccounts();
           const identities = getIdentities();
