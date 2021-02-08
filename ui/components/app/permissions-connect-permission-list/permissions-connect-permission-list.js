c58afc9f56b501ed0b5c0487abd37de520fa274e
Erik Marks <rekmarks@protonmail.com>
2021-02-08 12:21:32 -0800

    Add Snaps
    * Implement bls-signer support
    * Implement filsnap support
    * Add hooknames and hook selectors to RPC methods
    * Fix filsnap installation on vault creation
    * Various build and CI config updates
    * Update sourcemap validation
    * Update Sentry configuration
    * Change name of production environment to 'filecoin-production'
    * Ignore useless sentry error in e2e tests
    * Update README.md
    * Snaps update 0.0.7 (#11854)
    * Add iframe execution environment (#11865)
    * Remove WebWorkerExecutionEnvironmentService import
    * Ignore filsnap bundle in depcheckrc (#11866)
      * This stops the `depcheck` check from failing.
    * Restore Firefox compatibility (#11867)
    * Notify user of snap initial execution errors (#11901)
    * Add alert on snap initial execution errors
    * Changed window.alert to be a browser notification
    * Add guard for pluginController removePlugin
    * Remove Pre-Installed Filsnap (#12111)
    * Add more global snap hooks (#12113)
    * Changed snap imports to use `@metamask/` org (#12238)
    * Remove all uses of @mm-snaps
    * snaps packages @0.3.1 (#12610) (squashed)
    * Fence all Flask code
    * Remove snap_manageAssets and AssetController
    * Add locale messages for expected production permissions
    * Remove permissionsDescriptions state (replace by locale messages)
    * Remove unnecessary modifications to various files
    * Update Flask LavaMoat policy
    * Fix permission description display
    * Restore LavaMoat beta policy
    * Fix permission locale message handling
    * Fix build errors, regenerate build system policy
    * Fix some broken code fences and translation calls
    * Added snaps error notification (#12860)
    * skunkworks@0.5.0, migrate snap restricted methods
    * Clean up untrusted connection subject type logic
    * Record Snap subject metadata
    * Fix useOriginMetadata React hook
    * Backported bug fix from snaps-0.1 to snaps (#13094)
    * Added Snaps Custom Confirmation (#13036)
    * Fix bug when adding subject metadata (#13102)
    
    Co-authored-by: Erik Marks <rekmarks@protonmail.com>
    Co-authored-by: George Marshall <george.marshall@consensys.net>
    Co-authored-by: Hassan Malik <hassan.malik@consensys.net>
    Co-authored-by: Olaf Tomalka <olaf.tomalka@gmail.com>
    Co-authored-by: Shane Jonas <jonas.shane@gmail.com>

--- ui/components/app/permissions-connect-permission-list/permissions-connect-permission-list.js
+++ ui/components/app/permissions-connect-permission-list/permissions-connect-permission-list.js
@@ -2,9 +2,12 @@ import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { useI18nContext } from '../../../hooks/useI18nContext';
 
+const UNKNOWN_PERMISSION = Symbol('unknown');
+
 export default function PermissionsConnectPermissionList({ permissions }) {
   const t = useI18nContext();
 
+  // TODO:flask Fix Snap permission labels and support namespaced permissions
   const PERMISSION_TYPES = useMemo(() => {
     return {
       eth_accounts: {
@@ -12,18 +15,67 @@ export default function PermissionsConnectPermissionList({ permissions }) {
         label: t('eth_accounts'),
         rightIcon: null,
       },
+      ///: BEGIN:ONLY_INCLUDE_IN(flask)
+      snap_confirm: {
+        leftIcon: 'fas fa-eye',
+        label: 'snap_confirm',
+        rightIcon: null,
+      },
+      // TODO: We need a function to pass substitutions to `t` for this.
+      'snap_getBip44Entropy_*': {
+        leftIcon: 'fas fa-eye',
+        label: 'snap_getBip44Entropy_*',
+        rightIcon: null,
+      },
+      snap_manageState: {
+        leftIcon: 'fas fa-eye',
+        label: 'snap_manageState',
+        rightIcon: null,
+      },
+      'wallet_snap_*': {
+        leftIcon: 'fas fa-eye',
+        label: 'wallet_snap_*',
+        rightIcon: null,
+      },
+      ///: END:ONLY_INCLUDE_IN
+      [UNKNOWN_PERMISSION]: {
+        leftIcon: null,
+        label: 'Unknown Permission',
+        rightIcon: null,
+      },
     };
   }, [t]);
 
+  function getPermissionKey(permissionName) {
+    if (PERMISSION_TYPES[permissionName]) {
+      return permissionName;
+    }
+    ///: BEGIN:ONLY_INCLUDE_IN(flask)
+    else if (permissionName.startsWith('wallet_snap_')) {
+      return 'wallet_snap_*';
+    } else if (permissionName.startsWith('snap_getBip44Entropy_')) {
+      return 'snap_getBip44Entropy_*';
+    }
+    ///: END:ONLY_INCLUDE_IN
+
+    return UNKNOWN_PERMISSION;
+  }
+
   return (
     <div className="permissions-connect-permission-list">
-      {Object.keys(permissions).map((permission) => (
-        <div className="permission" key={PERMISSION_TYPES[permission].label}>
-          <i className={PERMISSION_TYPES[permission].leftIcon} />
-          {PERMISSION_TYPES[permission].label}
-          <i className={PERMISSION_TYPES[permission].rightIcon} />
-        </div>
-      ))}
+      {Object.keys(permissions).map((permission) => {
+        const { label, leftIcon, rightIcon } = PERMISSION_TYPES[
+          getPermissionKey(permission)
+        ];
+
+        return (
+          <div className="permission" key={label}>
+            <i className={leftIcon} />
+            {label}
+            <i className={rightIcon} />
+          </div>
+        );
+      })}
     </div>
   );
 }
