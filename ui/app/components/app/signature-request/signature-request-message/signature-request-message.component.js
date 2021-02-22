5d771296a028e17b81a4a810d9436dbaeb99ad1e
Austin Akers <austin.akers5@gmail.com>
2021-02-21 18:43:08 -0800

    fixed message text overflow removed redundant title

--- ui/app/components/app/signature-request/signature-request-message/signature-request-message.component.js
+++ ui/app/components/app/signature-request/signature-request-message/signature-request-message.component.js
@@ -43,9 +43,6 @@ export default class SignatureRequestMessage extends PureComponent {
 
     return (
       <div className="signature-request-message">
-        <div className="signature-request-message__title">
-          {this.context.t('signatureRequest1')}
-        </div>
         <div className="signature-request-message--root">
           <div className="signature-request-message__type-title">
             {this.context.t('signatureRequest1')}
