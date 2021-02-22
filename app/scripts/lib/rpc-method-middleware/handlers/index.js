0ff3a4365ff1d34da871b98eb4f95544bf2d262b
Brad Decker <git@braddecker.dev>
2021-02-22 14:46:39 -0600

    readd handler for addEthereumChain

--- app/scripts/lib/rpc-method-middleware/handlers/index.js
+++ app/scripts/lib/rpc-method-middleware/handlers/index.js
@@ -1,10 +1,10 @@
-// import addEthereumChain from './add-ethereum-chain';
+import addEthereumChain from './add-ethereum-chain';
 import getProviderState from './get-provider-state';
 import logWeb3ShimUsage from './log-web3-shim-usage';
 import watchAsset from './watch-asset';
 
 const handlers = [
-  // addEthereumChain,
+  addEthereumChain,
   getProviderState,
   logWeb3ShimUsage,
   watchAsset,
