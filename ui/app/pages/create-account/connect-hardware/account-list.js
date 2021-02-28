a686af052de982d3b1300bd5ffc15a0ac0460c9e
jiyun.kim <jiyun.kim@dsrvlabs.com>
2021-02-28 20:13:43 +0900

    read ledger account list
    Signed-off-by: jiyun.kim <jiyun.kim@dsrvlabs.com>

--- ui/app/pages/create-account/connect-hardware/account-list.js
+++ ui/app/pages/create-account/connect-hardware/account-list.js
@@ -9,7 +9,7 @@ class AccountList extends Component {
     return [
       {
         label: `Ledger Live`,
-        value: `m/44'/60'/0'/0/0`,
+        value: `m/44'/52752'/0'/0`,
       },
       {
         label: `Legacy (MEW / MyCrypto)`,
