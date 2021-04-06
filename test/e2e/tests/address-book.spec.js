dc1a0367863cb9e9184d562113106ab36662653a
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2021-04-06 13:39:28 -0400

    Removing unnecessary params from withFixtures function call. (#10831)

--- test/e2e/tests/address-book.spec.js
+++ test/e2e/tests/address-book.spec.js
@@ -91,7 +91,6 @@ describe('Address Book', function () {
   it('Sends to an address book entry', async function () {
     await withFixtures(
       {
-        dapp: true,
         fixtures: 'address-entry',
         ganacheOptions,
         title: this.test.title,
