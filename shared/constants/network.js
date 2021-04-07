284c2cb481132acee4ca796da7defffd0795308c
David Walsh <davidwalsh83@gmail.com>
2021-04-07 10:37:04 -0500

    Move BSC chain ID, rename some BSC-related vars (#10807)

--- shared/constants/network.js
+++ shared/constants/network.js
@@ -18,6 +18,7 @@ export const RINKEBY_CHAIN_ID = '0x4';
 export const GOERLI_CHAIN_ID = '0x5';
 export const KOVAN_CHAIN_ID = '0x2a';
 export const LOCALHOST_CHAIN_ID = '0x539';
+export const BSC_CHAIN_ID = '0x38';
 
 /**
  * The largest possible chain ID we can handle.
