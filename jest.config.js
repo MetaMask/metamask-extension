b7ee97c54cc1de50b21f2c7c46cfd67efff9b3bd
Thomas Huang <tmashuang@users.noreply.github.com>
2021-04-09 13:10:51 -0700

    Add jest coverage (#10868)
    * Add jest coverage
    
    This will add coverage for any tests ran in jest under the `test:coverage:jest` command, which is currently being used in CI. I set the values to the current test coverage in `ui/app/pages/swaps`.
    
    * Lint

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   restoreMocks: true,
   coverageDirectory: 'jest-coverage/',
+  coverageThreshold: {
+    global: {
+      branches: 6.3,
+      functions: 9.43,
+      lines: 8.66,
+      statements: 8.88,
+    },
+  },
   setupFiles: ['./test/setup.js', './test/env.js'],
   testMatch: ['**/ui/app/pages/swaps/**/?(*.)+(test).js'],
 };
