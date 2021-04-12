e84f8aaf50efd26740c29c98988744621d3a06e8
dan437 <80175477+dan437@users.noreply.github.com>
2021-04-12 13:24:32 -0700

    Increase test coverage to the current max

--- jest.config.js
+++ jest.config.js
@@ -3,10 +3,10 @@ module.exports = {
   coverageDirectory: 'jest-coverage/',
   coverageThreshold: {
     global: {
-      branches: 6.3,
-      functions: 9.43,
-      lines: 8.66,
-      statements: 8.88,
+      branches: 6.4,
+      functions: 9.77,
+      lines: 8.87,
+      statements: 0.09,
     },
   },
   setupFiles: ['./test/setup.js', './test/env.js'],
