d5163c5bf33191b3e7cb70d61a5d933e7d4f9e91
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2021-04-22 16:58:24 -0400

    removing console.log

--- ui/app/pages/confirm-transaction-base/confirm-transaction-base.component.js
+++ ui/app/pages/confirm-transaction-base/confirm-transaction-base.component.js
@@ -745,7 +745,6 @@ export default class ConfirmTransactionBase extends Component {
         functionType = t('contractInteraction');
       }
     }
-    console.log(errorKey);
     return (
       <ConfirmPageContainer
         fromName={fromName}
