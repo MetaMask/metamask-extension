f1f938cbacedbfad59d1e8f20ec38c6d823a8fa4
dan437 <80175477+dan437@users.noreply.github.com>
2021-04-22 16:30:37 -0700

    Add more UTs in the awaiting-swap folder

---
+++ ui/app/pages/swaps/awaiting-swap/quotes-timeout-icon.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { renderWithProvider } from '../../../../../test/jest';
+import QuotesTimeoutIcon from './quotes-timeout-icon';
+
+describe('QuotesTimeoutIcon', () => {
+  it('renders the component', () => {
+    const { container } = renderWithProvider(
+      <QuotesTimeoutIcon />,
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
