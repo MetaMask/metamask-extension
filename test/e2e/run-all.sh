96b628ee65c833d2a67c1fb6f09fb74a0fe9133e
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2021-04-23 13:43:07 -0400

    refactoring to use fixture

--- test/e2e/run-all.sh
+++ test/e2e/run-all.sh
@@ -35,9 +35,3 @@ retry concurrently --kill-others \
   'yarn dapp' \
   'mocha test/e2e/metamask-ui.spec'
 
-retry concurrently --kill-others \
-  --names 'sendwithprivatedapp,e2e' \
-  --prefix '[{time}][{name}]' \
-  --success first \
-  'yarn sendwithprivatedapp' \
-  'mocha test/e2e/incremental-security.spec'
