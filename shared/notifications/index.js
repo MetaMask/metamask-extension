a67a8072bcd6006cf9933ef27e5aa46f1d625fe7
Dan Miller <danjm.com@gmail.com>
2021-04-27 17:16:41 -0230

    Clean up locale code for whats-new-popup notifications

--- shared/notifications/index.js
+++ shared/notifications/index.js
@@ -2,25 +2,16 @@
 export const UI_NOTIFICATIONS = {
   1: {
     id: 1,
-    title: 'notificationsTitle1',
-    description: 'notificationsDescription1',
     date: '2020-03-17',
     image: 'images/swaps-logos-small.svg',
-    actionText: 'notificationsActionText1',
   },
   2: {
     id: 2,
-    title: 'notificationsTitle2',
-    description: 'notificationsDescription2',
     date: '2020-03-17',
-    actionText: 'notificationsActionText2',
   },
   3: {
     id: 3,
-    title: 'notificationsTitle3',
-    description: 'notificationsDescription3',
     date: '2020-03-17',
-    actionText: 'notificationsActionText3',
   },
 };
 
@@ -28,21 +19,21 @@ export const getTranslatedUINoficiations = (t) => {
   return {
     1: {
       ...UI_NOTIFICATIONS[1],
-      title: t('notificationsTitle1'),
-      description: t('notificationsDescription1'),
-      actionText: t('notificationsActionText1'),
+      title: t('notifications1Title'),
+      description: t('notifications1Description'),
+      actionText: t('notifications1ActionText'),
     },
     2: {
       ...UI_NOTIFICATIONS[2],
-      title: t('notificationsTitle2'),
-      description: t('notificationsDescription2'),
-      actionText: t('notificationsActionText2'),
+      title: t('notifications2Title'),
+      description: t('notifications2Description'),
+      actionText: t('notifications2ActionText'),
     },
     3: {
       ...UI_NOTIFICATIONS[3],
-      title: t('notificationsTitle3'),
-      description: t('notificationsDescription3'),
-      actionText: t('notificationsActionText3'),
+      title: t('notifications3Title'),
+      description: t('notifications3Description'),
+      actionText: t('notifications3ActionText'),
     },
   };
 };
