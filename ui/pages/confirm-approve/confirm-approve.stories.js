134c5ca3a0780539042555b179a6bf272680083d
Etienne Dusseault <etienne.dusseault@gmail.com>
2021-05-18 21:23:59 +0800

    fix lint

--- ui/pages/confirm-approve/confirm-approve.stories.js
+++ ui/pages/confirm-approve/confirm-approve.stories.js
@@ -1,37 +1,35 @@
 /* eslint-disable react/prop-types */
 
 import React, { useEffect } from 'react';
-import { object, number, select, text} from '@storybook/addon-knobs';
-import ConfirmApprove from './index'
+// import { object, number, select, text } from '@storybook/addon-knobs';
+import { useParams } from 'react-router-dom';
 import { updateMetamaskState } from '../../store/actions';
-import { store } from '../../../.storybook/preview'
-import { useParams } from 'react-router-dom'
+import { store } from '../../../.storybook/preview';
+import ConfirmApprove from '.';
 
 export default {
   title: 'Confirmation Screens',
 };
 
 const PageSet = ({ children }) => {
-  const params = useParams()
-  params.id = 1906703652727041
-  return (
-    children
-  )
-}
+  const params = useParams();
+  params.id = 1906703652727041;
+  return children;
+};
 
 export const ApproveTokens = () => {
-
   // useEffect for creating Redux knobs
   useEffect(() => {
-    store.dispatch(updateMetamaskState({
-      // stateUpdateHere
-    }))
-  }) 
+    store.dispatch(
+      updateMetamaskState({
+        // stateUpdateHere
+      }),
+    );
+  });
 
   return (
     <PageSet>
-      <ConfirmApprove/>
+      <ConfirmApprove />
     </PageSet>
-
-  )
-}
+  );
+};
