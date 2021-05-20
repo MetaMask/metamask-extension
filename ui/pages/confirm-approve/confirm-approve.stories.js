380a02f0afacf1c1acdf69a3843747f44b0f8374
Etienne Dusseault <etienne.dusseault@gmail.com>
2021-05-20 10:37:43 +0800

    lintfix

--- ui/pages/confirm-approve/confirm-approve.stories.js
+++ ui/pages/confirm-approve/confirm-approve.stories.js
@@ -3,11 +3,14 @@
 import React, { useEffect } from 'react';
 import { text } from '@storybook/addon-knobs';
 import { useParams } from 'react-router-dom';
-import { updateMetamaskState } from '../../store/actions';
 import { useSelector } from 'react-redux';
+import { updateMetamaskState } from '../../store/actions';
 import { currentNetworkTxListSelector } from '../../selectors/transactions';
 import { store } from '../../../.storybook/preview';
-import { currentNetworkTxListSample, domainMetadata }  from '../../../.storybook/initial-states/approval-screens/token-approval'
+import {
+  currentNetworkTxListSample,
+  domainMetadata,
+} from '../../../.storybook/initial-states/approval-screens/token-approval';
 import ConfirmApprove from '.';
 
 export default {
@@ -15,25 +18,36 @@ export default {
 };
 
 const PageSet = ({ children }) => {
-  const origin = text("Origin", "https://metamask.github.io")
-  const domainIconUrl = text("Icon URL", "https://metamask.github.io/test-dapp/metamask-fox.svg")
+  const origin = text('Origin', 'https://metamask.github.io');
+  const domainIconUrl = text(
+    'Icon URL',
+    'https://metamask.github.io/test-dapp/metamask-fox.svg',
+  );
   const currentNetworkTxList = useSelector(currentNetworkTxListSelector);
 
   useEffect(() => {
-    //transaction ID, maps to entry in state.metamask.currentNetworkTxList
-    const transaction = currentNetworkTxList.find(({ id }) => id === 7900715443136469);
-    transaction.origin = origin
-    store.dispatch(updateMetamaskState({ currentNetworkTxList: [transaction] }))
-  }, [origin])
+    // transaction ID, maps to entry in state.metamask.currentNetworkTxList
+    const transaction = currentNetworkTxList.find(
+      ({ id }) => id === 7900715443136469,
+    );
+    transaction.origin = origin;
+    store.dispatch(
+      updateMetamaskState({ currentNetworkTxList: [transaction] }),
+    );
+  }, [origin]);
 
   useEffect(() => {
-    store.dispatch(updateMetamaskState({ domainMetadata: {
-      [origin]: {
-        icon: domainIconUrl
-      }
-    } }))
-  }, [domainIconUrl])
-  
+    store.dispatch(
+      updateMetamaskState({
+        domainMetadata: {
+          [origin]: {
+            icon: domainIconUrl,
+          },
+        },
+      }),
+    );
+  }, [domainIconUrl]);
+
   const params = useParams();
   params.id = 7900715443136469;
 
@@ -41,8 +55,10 @@ const PageSet = ({ children }) => {
 };
 
 export const ApproveTokens = () => {
-  store.dispatch(updateMetamaskState({ currentNetworkTxList: [currentNetworkTxListSample] }))
-  store.dispatch(updateMetamaskState({ domainMetadata: domainMetadata }))
+  store.dispatch(
+    updateMetamaskState({ currentNetworkTxList: [currentNetworkTxListSample] }),
+  );
+  store.dispatch(updateMetamaskState({ domainMetadata }));
   return (
     <PageSet>
       <ConfirmApprove />
