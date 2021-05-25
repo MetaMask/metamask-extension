07501dd1b04e0753a20c5f45e3bbbc3084b6b5b0
Etienne Dusseault <etienne.dusseault@gmail.com>
2021-05-26 00:06:48 +0800

    change code

--- ui/pages/confirm-add-token/confirm-add-token.stories.js
+++ ui/pages/confirm-add-token/confirm-add-token.stories.js
@@ -9,6 +9,7 @@ import ConfirmAddToken from '.';
 import { createBrowserHistory } from "history";
 import { currentNetworkTxListSelector } from '../../selectors/transactions';
 import { store } from '../../../.storybook/preview';
+import {suggestedTokens} from '../../../.storybook/initial-states/approval-screens/add-suggested-token'
 import { text } from '@storybook/addon-knobs';
 import { updateMetamaskState } from '../../store/actions';
 import { useParams } from 'react-router-dom';
@@ -22,7 +23,31 @@ const history = createBrowserHistory();
 
 const PageSet = ({ children }) => {
 
+  const symbol = text('symbol', 'DAI');
+  const image = text(
+    'Icon URL',
+    'https://assets.coingecko.com/coins/images/9956/small/dai-multi-collateral-mcd.png?1574218774',
+  );
+
+  const state = store.getState();
+  const suggestedTokensState = state.metamask.suggestedTokens
+
+  useEffect(() => {
+    suggestedTokensState["0x6b175474e89094c44da98b954eedeac495271d0f"].symbol = symbol
+    store.dispatch(
+      updateMetamaskState({ suggestedTokens: suggestedTokensState})
+      
+    );
+  }, [symbol]);
+  useEffect(() => {
+    suggestedTokensState["0x6b175474e89094c44da98b954eedeac495271d0f"].image = image
+    store.dispatch(
+      updateMetamaskState({ suggestedTokens: suggestedTokensState})
+    );
+  }, [image]);
+
   return children;
+
 };
 
 export const AddToken = () => {
@@ -30,9 +55,10 @@ export const AddToken = () => {
   //   updateMetamaskState({ currentNetworkTxList: [currentNetworkTxListSample] }),
   // );
   // store.dispatch(updateMetamaskState({ domainMetadata }));
+  store.dispatch(updateMetamaskState({ suggestedTokens: suggestedTokens}))
   return (
        <ConfirmAddToken
         history={history}
-       />
+       /> 
   );
 };
