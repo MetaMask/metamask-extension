bfd814ecfeb8d8877828544e7c77a6af29671030
Etienne Dusseault <etienne.dusseault@gmail.com>
2021-05-26 10:33:55 +0800

    lintfix

--- ui/pages/confirm-add-suggested-token/confirm-add-suggested-token.component.js
+++ ui/pages/confirm-add-suggested-token/confirm-add-suggested-token.component.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
+import PropTypes from 'prop-types';
 import Button from '../../components/ui/button';
 import { ENVIRONMENT_TYPE_NOTIFICATION } from '../../../shared/constants/app';
 import Identicon from '../../components/ui/identicon';
-import PropTypes from 'prop-types';
 import TokenBalance from '../../components/ui/token-balance';
 import { getEnvironmentType } from '../../../app/scripts/lib/util';
 
