f02f529efa446de2be1a4c888a7bbdaf36d942cd
Etienne Dusseault <etienne.dusseault@gmail.com>
2021-05-27 14:05:14 +0800

    lintfix

--- ui/pages/confirm-add-token/confirm-add-token.stories.js
+++ ui/pages/confirm-add-token/confirm-add-token.stories.js
@@ -1,12 +1,12 @@
 /* eslint-disable react/prop-types */
 import React, { useEffect } from 'react';
 
-import ConfirmAddToken from '.';
-import { createBrowserHistory } from "history";
-import { store } from '../../../.storybook/preview';
-import { tokens } from '../../../.storybook/initial-states/approval-screens/add-token'
+import { createBrowserHistory } from 'history';
 import { text } from '@storybook/addon-knobs';
+import { store } from '../../../.storybook/preview';
+import { tokens } from '../../../.storybook/initial-states/approval-screens/add-token';
 import { updateMetamaskState } from '../../store/actions';
+import ConfirmAddToken from '.';
 
 export default {
   title: 'Confirmation Screens',
@@ -17,27 +17,22 @@ const history = createBrowserHistory();
 const PageSet = ({ children }) => {
   const symbol = text('symbol', 'TRDT');
   const state = store.getState();
-  const pendingTokensState = state.metamask.pendingTokens
+  const pendingTokensState = state.metamask.pendingTokens;
   // only change the first token in the list
   useEffect(() => {
-    const pendingTokens = Object.assign({}, pendingTokensState)
-    pendingTokens["0x33f90dee07c6e8b9682dd20f73e6c358b2ed0f03"].symbol = symbol
-    store.dispatch(
-      updateMetamaskState({ pendingTokens: pendingTokens})
-    );
+    const pendingTokens = { ...pendingTokensState };
+    pendingTokens['0x33f90dee07c6e8b9682dd20f73e6c358b2ed0f03'].symbol = symbol;
+    store.dispatch(updateMetamaskState({ pendingTokens }));
   }, [symbol, pendingTokensState]);
 
   return children;
-
 };
 
 export const AddToken = () => {
-  store.dispatch(updateMetamaskState({ pendingTokens: tokens}))
+  store.dispatch(updateMetamaskState({ pendingTokens: tokens }));
   return (
     <PageSet>
-       <ConfirmAddToken
-        history={history}
-       /> 
-       </PageSet>
+      <ConfirmAddToken history={history} />
+    </PageSet>
   );
 };
