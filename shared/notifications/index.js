aimaniac1989/metamask-review

Adding notification for updated seed phrase wording (#11131)
* Adding notification for updated seed phrase wording

* Updating e2e notification state

* Always displaying image for swaps notification

shared/notifications/index.js
@@ -22,6 +22,10 @@ export const UI_NOTIFICATIONS = {
       width: '100%',
     },
   },
+  5: {
+    id: 5,
+    date: '2021-05-18',
+  },
 };
 
 export const getTranslatedUINoficiations = (t, locale) => {
@@ -51,6 +55,15 @@ export const getTranslatedUINoficiations = (t, locale) => {
       actionText: t('notifications4ActionText'),
       date: new Intl.DateTimeFormat(locale).format(
         new Date(UI_NOTIFICATIONS[4].date),
+      ),
+    },
+    5: {
+      ...UI_NOTIFICATIONS[5],
+      title: t('walletSeed'),
+      description: t('notifications5Description'),
+      actionText: t('notifications3ActionText'),
+      date: new Intl.DateTimeFormat(locale).format(
+        new Date(UI_NOTIFICATIONS[5].date),
       ),
     },
   };

MetaMask/metamask-extension

shared/notifications/index.js
@@ -22,6 +22,10 @@ export const UI_NOTIFICATIONS = {
       width: '100%',
     },
   },
+  5: {
+    id: 5,
+    date: '2021-05-18',
+  },
   6: {
     id: 6,
     date: '2021-05-26',
@@ -55,6 +59,15 @@ export const getTranslatedUINoficiations = (t, locale) => {
       actionText: t('notifications4ActionText'),
       date: new Intl.DateTimeFormat(formattedLocale).format(
         new Date(UI_NOTIFICATIONS[4].date),
+      ),
+    },
+    5: {
+      ...UI_NOTIFICATIONS[5],
+      title: t('walletSeed'),
+      description: t('notifications5Description'),
+      actionText: t('notifications3ActionText'),
+      date: new Intl.DateTimeFormat(formattedLocale).format(
+        new Date(UI_NOTIFICATIONS[5].date),
       ),
     },
     6: {