7db70ce8b4f9a7765232c2edc0454a9b667d71d9
dan437 <80175477+dan437@users.noreply.github.com>
2021-06-25 12:03:42 +0200

    Call Wyre’s API via our backend to generate Wyre’s Checkout URL

--- app/scripts/lib/buy-eth-url.test.js
+++ app/scripts/lib/buy-eth-url.test.js
@@ -1,4 +1,5 @@
 import { strict as assert } from 'assert';
+import nock from 'nock';
 import {
   KOVAN_CHAIN_ID,
   MAINNET_CHAIN_ID,
@@ -11,7 +12,7 @@ describe('buy-eth-url', function () {
   const mainnet = {
     chainId: MAINNET_CHAIN_ID,
     amount: 5,
-    address: '0x0dcd5d886577d5081b0c52e242ef29e70be3e7bc',
+    address: '0x0dcd5d886577d5581b0c524242ef2ee70be3e7bc',
   };
   const ropsten = {
     chainId: ROPSTEN_CHAIN_ID,
@@ -23,27 +24,46 @@ describe('buy-eth-url', function () {
     chainId: KOVAN_CHAIN_ID,
   };
 
-  it('returns wyre url with address for network 1', function () {
-    const wyreUrl = getBuyEthUrl(mainnet);
+  it('returns a Wyre url with an order reservation ID', async function () {
+    nock('https://api.metaswap.codefi.network')
+      .get(
+        '/fiatOnRampUrl?serviceName=wyre&destinationAddress=0x0dcd5d886577d5581b0c524242ef2ee70be3e7bc',
+      )
+      .reply(200, {
+        result: {
+          url:
+            'https://pay.sendwyre.com/purchase?accountId=AC-7AG3W4XH4N2&utm_campaign=AC-7AG3W4XH4N2&destCurrency=ETH&utm_medium=widget&paymentMethod=debit-card&reservation=MLZVUF8FMXZUMARJC23B&dest=ethereum%3A0x0dcd5d886577d5581b0c524242ef2ee70be3e7bc&utm_source=checkout',
+        },
+      });
+    const wyreUrl = await getBuyEthUrl(mainnet);
+    assert.equal(
+      wyreUrl,
+      'https://pay.sendwyre.com/purchase?accountId=AC-7AG3W4XH4N2&utm_campaign=AC-7AG3W4XH4N2&destCurrency=ETH&utm_medium=widget&paymentMethod=debit-card&reservation=MLZVUF8FMXZUMARJC23B&dest=ethereum%3A0x0dcd5d886577d5581b0c524242ef2ee70be3e7bc&utm_source=checkout',
+    );
+    nock.cleanAll();
+  });
+
+  it('returns a fallback Wyre url if /orders/reserve API call fails', async function () {
+    const wyreUrl = await getBuyEthUrl(mainnet);
 
     assert.equal(
       wyreUrl,
-      'https://pay.sendwyre.com/purchase?dest=ethereum:0x0dcd5d886577d5081b0c52e242ef29e70be3e7bc&destCurrency=ETH&accountId=AC-7AG3W4XH4N2&paymentMethod=debit-card',
+      'https://pay.sendwyre.com/purchase?dest=ethereum:0x0dcd5d886577d5581b0c524242ef2ee70be3e7bc&destCurrency=ETH&accountId=AC-7AG3W4XH4N2',
     );
   });
 
-  it('returns metamask ropsten faucet for network 3', function () {
-    const ropstenUrl = getBuyEthUrl(ropsten);
+  it('returns metamask ropsten faucet for network 3', async function () {
+    const ropstenUrl = await getBuyEthUrl(ropsten);
     assert.equal(ropstenUrl, 'https://faucet.metamask.io/');
   });
 
-  it('returns rinkeby dapp for network 4', function () {
-    const rinkebyUrl = getBuyEthUrl(rinkeby);
+  it('returns rinkeby dapp for network 4', async function () {
+    const rinkebyUrl = await getBuyEthUrl(rinkeby);
     assert.equal(rinkebyUrl, 'https://www.rinkeby.io/');
   });
 
-  it('returns kovan github test faucet for network 42', function () {
-    const kovanUrl = getBuyEthUrl(kovan);
+  it('returns kovan github test faucet for network 42', async function () {
+    const kovanUrl = await getBuyEthUrl(kovan);
     assert.equal(kovanUrl, 'https://github.com/kovan-testnet/faucet');
   });
 });
