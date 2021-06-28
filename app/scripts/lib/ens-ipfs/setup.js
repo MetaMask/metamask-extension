df5b5d0805f35fb9b0d70f3de15902ade8e52182
PJ <peterjan.brone@gmail.com>
2021-06-28 13:09:33 +0200

    Fix lint

--- app/scripts/lib/ens-ipfs/setup.js
+++ app/scripts/lib/ens-ipfs/setup.js
@@ -1,9 +1,9 @@
+import base32Encode from 'base32-encode';
+import base64 from 'base64-js';
 import extension from 'extensionizer';
-import getFetchWithTimeout from '../../../../shared/modules/fetch-with-timeout';
 import { SECOND } from '../../../../shared/constants/time';
+import getFetchWithTimeout from '../../../../shared/modules/fetch-with-timeout';
 import resolveEnsToIpfsContentId from './resolver';
-import base64 from "base64-js";
-import base32Encode from "base32-encode";
 
 const fetchWithTimeout = getFetchWithTimeout(SECOND * 30);
 
@@ -86,8 +86,9 @@ export default function setupEnsIpfsResolver({
       } else if (type === 'skynet-ns') {
         const padded = hash.padEnd(hash.length + 4 - (hash.length % 4), "=");
         const decoded = base64.toByteArray(padded);
-        const base32EncodedSkylink = base32Encode(decoded, "RFC4648-HEX", { padding: false }).toLowerCase();
-        url = `https://${base32EncodedSkylink}.siasky.net${pathname}${search || ''}${fragment || ''}`;
+        const base32EncodedSkylink = base32Encode(decoded, 'RFC4648-HEX', { padding: false }).toLowerCase();
+        url = `https://${base32EncodedSkylink}.siasky.net${pathname}${
+          search || ''}${fragment || ''}`;
       }
     } catch (err) {
       console.warn(err);
