2da9acd325c03c78a72de347de133d681316b7ab
Dan J Miller <danjm.com@gmail.com>
2021-07-02 17:00:40 -0230

    Add some metrics events to the 3box controller (#11447)

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -315,6 +315,9 @@ export default class MetamaskController extends EventEmitter {
         this.keyringController.memStore,
       ),
       version,
+      trackMetaMetricsEvent: this.metaMetricsController.trackEvent.bind(
+        this.metaMetricsController,
+      ),
     });
 
     this.txController = new TransactionController({
