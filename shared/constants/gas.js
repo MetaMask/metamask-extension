871d5e9e99485d2dcd8e3d771b2add53498171b6
Brad Decker <git@braddecker.dev>
2021-07-06 17:03:39 -0500

    fix bundle size issue

--- shared/constants/gas.js
+++ shared/constants/gas.js
@@ -1,4 +1,3 @@
-import { GAS_ESTIMATE_TYPES as GasFeeControllerEstimateTypes } from '@metamask/controllers';
 import { addHexPrefix } from 'ethereumjs-util';
 
 const TWENTY_ONE_THOUSAND = 21000;
@@ -11,4 +10,13 @@ export const GAS_LIMITS = {
   BASE_TOKEN_ESTIMATE: addHexPrefix(ONE_HUNDRED_THOUSAND.toString(16)),
 };
 
-export const GAS_ESTIMATE_TYPES = { ...GasFeeControllerEstimateTypes };
+/**
+ * These are already declared in @metamask/controllers but importing them from
+ * that module and re-exporting causes the UI bundle size to expand beyond 4MB
+ */
+export const GAS_ESTIMATE_TYPES = {
+  FEE_MARKET: 'fee-market',
+  LEGACY: 'legacy',
+  ETH_GASPRICE: 'eth_gasPrice',
+  NONE: 'none',
+};
