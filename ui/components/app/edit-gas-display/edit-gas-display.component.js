4caf5a61faf1e08dcff2e2b3a8d15a93eaf5afb7
Brad Decker <git@braddecker.dev>
2021-07-08 16:46:25 -0500

    consume data in new ui

--- ui/components/app/edit-gas-display/edit-gas-display.component.js
+++ ui/components/app/edit-gas-display/edit-gas-display.component.js
@@ -7,15 +7,17 @@ import {
   COLORS,
   TYPOGRAPHY,
   FONT_WEIGHT,
+  TEXT_ALIGN,
 } from '../../../helpers/constants/design-system';
 
 import InfoTooltip from '../../ui/info-tooltip';
 import TransactionTotalBanner from '../transaction-total-banner/transaction-total-banner.component';
 import RadioGroup from '../../ui/radio-group/radio-group.component';
 import AdvancedGasControls from '../advanced-gas-controls/advanced-gas-controls.component';
+import ActionableMessage from '../../ui/actionable-message/actionable-message';
 
 import { I18nContext } from '../../../contexts/i18n';
-import ActionableMessage from '../../ui/actionable-message/actionable-message';
+import { useGasFeeInputs } from '../../../hooks/useGasFeeInputs';
 
 export default function EditGasDisplay({
   alwaysShowForm,
@@ -24,18 +26,44 @@ export default function EditGasDisplay({
   onEducationClick,
   dappSuggestedGasFee,
   dappOrigin,
+  defaultEstimateToUse = 'medium',
 }) {
   const t = useContext(I18nContext);
 
   const [warning] = useState(null);
+
   const [showAdvancedForm, setShowAdvancedForm] = useState(false);
   const [
     dappSuggestedGasFeeAcknowledged,
     setDappSuggestedGasFeeAcknowledged,
   ] = useState(false);
 
-  const requireDappAcknowledgement =
-    dappSuggestedGasFee && !dappSuggestedGasFeeAcknowledged;
+  const requireDappAcknowledgement = Boolean(
+    dappSuggestedGasFee && !dappSuggestedGasFeeAcknowledged,
+  );
+
+  const {
+    maxPriorityFeePerGas,
+    setMaxPriorityFeePerGas,
+    maxPriorityFeePerGasFiat,
+    maxFeePerGas,
+    setMaxFeePerGas,
+    maxFeePerGasFiat,
+    maxFeePerGasPrimary,
+    isGasEstimatesLoading,
+    gasFeeEstimates,
+    gasEstimateType,
+    gasPrice,
+    setGasPrice,
+    gasLimit,
+    setGasLimit,
+    estimateToUse,
+    setEstimateToUse,
+    estimatedMinimumFiat,
+    isMaxFeeError,
+    isMaxPriorityFeeError,
+    isGasTooLow,
+  } = useGasFeeInputs(defaultEstimateToUse);
 
   return (
     <div className="edit-gas-display">
@@ -44,7 +72,7 @@ export default function EditGasDisplay({
           <div className="edit-gas-display__warning">
             <ActionableMessage
               className="actionable-message--warning"
-              message="Swaps are time sensitive. “Medium” is not reccomended."
+              message={warning}
             />
           </div>
         )}
@@ -72,9 +100,25 @@ export default function EditGasDisplay({
             </Typography>
           </div>
         )}
-
-        <TransactionTotalBanner total="" detail="" timing="" />
-
+        <TransactionTotalBanner
+          total={estimatedMinimumFiat}
+          detail={
+            process.env.SHOW_EIP_1559_UI &&
+            t('editGasTotalBannerSubtitle', [
+              <Typography
+                fontWeight={FONT_WEIGHT.BOLD}
+                tag="span"
+                key="secondary"
+              >
+                {maxFeePerGasFiat}
+              </Typography>,
+              <Typography tag="span" key="primary">
+                {maxFeePerGasPrimary}
+              </Typography>,
+            ])
+          }
+          timing=""
+        />
         {requireDappAcknowledgement && (
           <Button
             className="edit-gas-display__dapp-acknowledgement-button"
@@ -83,23 +127,42 @@ export default function EditGasDisplay({
             {t('gasDisplayAcknowledgeDappButtonText')}
           </Button>
         )}
-
+        {isGasTooLow && (
+          <div className="edit-gas-display__error">
+            <Typography
+              color={COLORS.ERROR1}
+              variant={TYPOGRAPHY.H7}
+              align={TEXT_ALIGN.CENTER}
+            >
+              {t('editGasTooLow')}
+            </Typography>
+          </div>
+        )}
         {!requireDappAcknowledgement && (
           <RadioGroup
             name="gas-recommendation"
             options={[
-              { value: 'low', label: t('editGasLow'), recommended: false },
+              {
+                value: 'low',
+                label: t('editGasLow'),
+                recommended: defaultEstimateToUse === 'low',
+              },
               {
                 value: 'medium',
                 label: t('editGasMedium'),
-                recommended: false,
+                recommended: defaultEstimateToUse === 'medium',
               },
-              { value: 'high', label: t('editGasHigh'), recommended: true },
+              {
+                value: 'high',
+                label: t('editGasHigh'),
+                recommended: defaultEstimateToUse === 'high',
+              },
             ]}
-            selectedValue="high"
+            selectedValue={estimateToUse}
+            onChange={setEstimateToUse}
           />
         )}
-        {!requireDappAcknowledgement && !alwaysShowForm && (
+        {!alwaysShowForm && (
           <button
             className="edit-gas-display__advanced-button"
             onClick={() => setShowAdvancedForm(!showAdvancedForm)}
@@ -112,8 +175,29 @@ export default function EditGasDisplay({
             )}
           </button>
         )}
-        {((!requireDappAcknowledgement && alwaysShowForm) ||
-          showAdvancedForm) && <AdvancedGasControls />}
+        {!requireDappAcknowledgement &&
+          (alwaysShowForm || showAdvancedForm) && (
+            <AdvancedGasControls
+              gasFeeEstimates={gasFeeEstimates}
+              gasEstimateType={gasEstimateType}
+              estimateToUse={estimateToUse}
+              isGasEstimatesLoading={isGasEstimatesLoading}
+              gasLimit={gasLimit}
+              setGasLimit={setGasLimit}
+              maxPriorityFee={maxPriorityFeePerGas}
+              setMaxPriorityFee={setMaxPriorityFeePerGas}
+              maxFee={maxFeePerGas}
+              setMaxFee={setMaxFeePerGas}
+              gasPrice={gasPrice}
+              setGasPrice={setGasPrice}
+              maxPriorityFeeFiat={maxPriorityFeePerGasFiat}
+              maxFeeFiat={maxFeePerGasFiat}
+              maxPriorityFeeError={
+                isMaxPriorityFeeError ? t('editGasMaxPriorityFeeLow') : null
+              }
+              maxFeeError={isMaxFeeError ? t('editGasMaxFeeLow') : null}
+            />
+          )}
       </div>
       {!requireDappAcknowledgement && showEducationButton && (
         <div className="edit-gas-display__education">
@@ -133,6 +217,7 @@ EditGasDisplay.propTypes = {
   onEducationClick: PropTypes.func,
   dappSuggestedGasFee: PropTypes.number,
   dappOrigin: PropTypes.string,
+  defaultEstimateToUse: PropTypes.oneOf(['low', 'medium', 'high']),
 };
 
 EditGasDisplay.defaultProps = {
