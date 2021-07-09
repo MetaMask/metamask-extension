2cb807dc20c77390f4d1ec3dbeea6c2c093c792a
Alex Donesky <adonesky@gmail.com>
2021-07-09 09:17:02 -0500

    fix up ens/hex address validation error handling (#11477)
    * fix up ens/hex address validation error handling
    
    * fix lint/tests

--- ui/ducks/send/send.js
+++ ui/ducks/send/send.js
@@ -78,7 +78,7 @@ import {
   isValidDomainName,
 } from '../../helpers/utils/util';
 import { getTokens, getUnapprovedTxs } from '../metamask/metamask';
-import { resetResolution } from '../ens';
+import { resetEnsResolution } from '../ens';
 import {
   isBurnAddress,
   isValidHexAddress,
@@ -1284,7 +1284,7 @@ export function resetRecipientInput() {
   return async (dispatch) => {
     await dispatch(updateRecipientUserInput(''));
     await dispatch(updateRecipient({ address: '', nickname: '' }));
-    await dispatch(resetResolution());
+    await dispatch(resetEnsResolution());
     await dispatch(validateRecipientUserInput());
   };
 }
