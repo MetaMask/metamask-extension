2a6293b4b7b6420ee3024b4f0349dfca13248a71
David Walsh <davidwalsh83@gmail.com>
2021-07-15 14:13:39 -0500

    Add simplified gas timing

--- ui/components/app/gas-timing/gas-timing.component.js
+++ ui/components/app/gas-timing/gas-timing.component.js
@@ -2,12 +2,12 @@ import React, { useContext } from 'react';
 import classNames from 'classnames';
 
 import { useGasFeeEstimates } from '../../../hooks/useGasFeeEstimates';
+import { useGasFeeInputs } from '../../../hooks/useGasFeeInputs';
 import { I18nContext } from '../../../contexts/i18n';
 
 import Typography from '../../ui/typography/typography';
 import { TYPOGRAPHY } from '../../../helpers/constants/design-system';
 import InfoTooltip from '../../ui/info-tooltip';
-import { useGasFeeInputs } from '../../../hooks/useGasFeeInputs';
 
 // Once we reach this second threshold, we switch to minutes as a unit
 const SECOND_CUTOFF = 90;
@@ -20,20 +20,12 @@ export default function GasTiming() {
   const t = useContext(I18nContext);
 
   // Shows "seconds" as unit of time if under SECOND_CUTOFF, otherwise "minutes"
-  const toHumanReadableTime = (milliseconds) => {
+  const toHumanReadableTime = (milliseconds = 1) => {
     const seconds = Math.ceil(milliseconds / 1000);
     if (seconds <= SECOND_CUTOFF) {
       return t('gasTimingSeconds', [seconds]);
     }
     return t('gasTimingMinutes', [Math.ceil(seconds / 60)]);
-  };
-
-  // Prevents a range being shown if the values are the same
-  const getRangeOrSingleValue = (value1, value2) => {
-    if (value1 === value2) {
-      return value1;
-    }
-    return `${value1} - ${value2}`;
   };
 
   // Don't show anything if we don't have enough information
@@ -45,78 +37,25 @@ export default function GasTiming() {
   let attitude = '';
   const tooltipText = '';
 
-  // 1:  Longer than the `low.maxWaitTimeEstimate`
-  if (maxPriorityFeePerGas < low.maxPriorityFeePerGas) {
-    text = t('editGasTooLow');
-    attitude = 'negative';
-  }
-  // 2: Between the `low.minWaitTimeEstimate` and `low.maxWaitTimeEstimate`
-  else if (
-    maxPriorityFeePerGas >= low.maxPriorityFeePerGas &&
-    maxPriorityFeePerGas < medium.maxPriorityFeePerGas
-  ) {
-    if (low.minWaitTimeEstimate === 0) {
-      text = t('gasTimingLessThan', [
-        toHumanReadableTime(low.maxWaitTimeEstimate),
-      ]);
-    } else if (low.maxWaitTimeEstimate === 0) {
-      text = t('gasTimingAtLeast', [
-        toHumanReadableTime(low.maxWaitTimeEstimate),
-      ]);
-    } else {
-      text = getRangeOrSingleValue(
-        toHumanReadableTime(low.minWaitTimeEstimate),
-        toHumanReadableTime(low.maxWaitTimeEstimate),
-      );
-    }
-  }
-  // 3: Between the `medium.minWaitTimeEstimate` and `medium.maxWaitTimeEstimate`
-  else if (
-    maxPriorityFeePerGas >= medium.maxPriorityFeePerGas &&
-    maxPriorityFeePerGas < high.maxPriorityFeePerGas
-  ) {
-    if (medium.minWaitTimeEstimate === 0) {
-      text = t('gasTimingLessThan', [
-        toHumanReadableTime(medium.maxWaitTimeEstimate),
-      ]);
-    } else if (medium.maxWaitTimeEstimate === 0) {
-      text = t('gasTimingAtLeast', [
-        toHumanReadableTime(medium.maxWaitTimeEstimate),
-      ]);
-    } else {
-      text = getRangeOrSingleValue(
-        toHumanReadableTime(medium.minWaitTimeEstimate),
-        toHumanReadableTime(medium.maxWaitTimeEstimate),
-      );
-    }
-  }
-  // 4: Between the `high.minWaitTimeEstimate` and the `high.maxWaitTimeEstimate`
-  else if (maxPriorityFeePerGas === high.maxPriorityFeePerGas) {
-    if (high.minWaitTimeEstimate === 0) {
-      text = t('gasTimingLessThan', [
-        toHumanReadableTime(high.maxWaitTimeEstimate),
-      ]);
-    } else if (high.maxWaitTimeEstimate === 0) {
-      text = t('gasTimingAtLeast', [
-        toHumanReadableTime(high.maxWaitTimeEstimate),
-      ]);
-    } else {
-      text = getRangeOrSingleValue(
-        toHumanReadableTime(high.minWaitTimeEstimate),
-        toHumanReadableTime(high.maxWaitTimeEstimate),
-      );
-    }
-  }
-  // 5: Faster than `high.minWaitTimeEstimate`
-  else if (maxPriorityFeePerGas > high.maxPriorityFeePerGas) {
-    text = t('gasTimingLessThan', [
-      toHumanReadableTime(high.minWaitTimeEstimate),
-    ]);
-  }
+  // Anything medium or faster is positive
+  if (maxPriorityFeePerGas >= medium.maxPriorityFeePerGas) {
+    attitude = 'positive';
 
-  // If we didn't hit any values, do not show anything
-  if (text === '') {
-    return null;
+    // High+ is very likely, medium is likely
+    if (maxPriorityFeePerGas < high.maxPriorityFeePerGas) {
+      text = t('gasTimingPositive', [
+        toHumanReadableTime(medium.maxWaitTimeEstimate),
+      ]);
+    } else {
+      text = t('gasTimingVeryPositive', [
+        toHumanReadableTime(high.maxWaitTimeEstimate),
+      ]);
+    }
+  } else {
+    attitude = 'negative';
+    text = t('gasTimingNegative', [
+      toHumanReadableTime(low.maxWaitTimeEstimate),
+    ]);
   }
 
   return (
