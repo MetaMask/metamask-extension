635d1045b884a79a2c84c5cdacd14e5802f51c47
David Walsh <davidwalsh83@gmail.com>
2021-07-20 10:46:31 -0500

    EIP-1559 - Show gas estimate updating animation in transaction detail

--- ui/components/app/transaction-detail/transaction-detail.component.js
+++ ui/components/app/transaction-detail/transaction-detail.component.js
@@ -3,16 +3,19 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 import { I18nContext } from '../../../contexts/i18n';
+import { useGasFeeEstimates } from '../../../hooks/useGasFeeEstimates';
 
 import TransactionDetailItem from '../transaction-detail-item/transaction-detail-item.component';
 
 export default function TransactionDetail({ rows = [], onEdit }) {
   const t = useContext(I18nContext);
 
+  const { isGasEstimatesLoading } = useGasFeeEstimates();
+
   return (
     <div
       className={classNames('transaction-detail', {
-        'transaction-detail--editable': Boolean(onEdit),
+        'transaction-detail--loading': isGasEstimatesLoading,
       })}
     >
       {onEdit && (
