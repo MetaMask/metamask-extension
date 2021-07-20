7f1e71d7589fe9e31b4108bd14d9d26d30ed075a
Dan Miller <danjm.com@gmail.com>
2021-07-20 10:27:33 -0230

    Fix comment change

--- ui/ducks/send/send.js
+++ ui/ducks/send/send.js
@@ -182,6 +182,7 @@ async function estimateGasLimitForSend({
 }) {
   let isSimpleSendOnNonStandardNetwork = false;
 
+  // blockGasLimit may be a falsy, but defined, value when we receive it from
   // state, so we use logical or to fall back to MIN_GAS_LIMIT_HEX. Some
   // network implementations check the gas parameter supplied to
   // eth_estimateGas for validity. For this reason, we set token sends
