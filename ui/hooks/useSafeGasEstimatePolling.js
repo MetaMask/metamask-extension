ccd71cb691b3d448f45109dce1cf68f38cf19133
Dan Miller <danjm.com@gmail.com>
2021-07-26 07:56:26 -0230

    Documentation improvements for send slice support of EIP1559

--- ui/hooks/useSafeGasEstimatePolling.js
+++ ui/hooks/useSafeGasEstimatePolling.js
@@ -4,6 +4,14 @@ import {
   getGasFeeEstimatesAndStartPolling,
 } from '../store/actions';
 
+/**
+ * Provides a reusable hook that can be used for safely updating the polling
+ * data in the gas fee controller. It makes a request to get estimates and
+ * begin polling, keeping track of the poll token for the lifetime of the hook.
+ * It then disconnects polling upon unmount. If the hook is unmounted while waiting
+ * for `getGasFeeEstimatesAndStartPolling` to resolve, the `active` flag ensures
+ * that a call to disconnect happens after promise resolution.
+ */
 export function useSafeGasEstimatePolling() {
   useEffect(() => {
     let active = true;
