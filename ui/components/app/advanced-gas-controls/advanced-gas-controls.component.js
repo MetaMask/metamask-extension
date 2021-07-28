499bd60a1306877957d8f3da113f566b42c858f9
Dan Miller <danjm.com@gmail.com>
2021-07-28 07:40:34 -0230

    Enforce correct minimum gas limit in swaps

--- ui/components/app/advanced-gas-controls/advanced-gas-controls.component.js
+++ ui/components/app/advanced-gas-controls/advanced-gas-controls.component.js
@@ -33,6 +33,7 @@ export default function AdvancedGasControls({
   maxPriorityFeeFiat,
   maxFeeFiat,
   gasErrors,
+  minimumGasLimit = 21000,
 }) {
   const t = useContext(I18nContext);
 
@@ -65,7 +66,7 @@ export default function AdvancedGasControls({
         titleText={t('gasLimit')}
         error={
           gasErrors?.gasLimit
-            ? getGasFormErrorText(gasErrors.gasLimit, t)
+            ? getGasFormErrorText(gasErrors.gasLimit, t, { minimumGasLimit })
             : null
         }
         onChange={setGasLimit}
@@ -230,4 +231,5 @@ AdvancedGasControls.propTypes = {
   maxPriorityFeeFiat: PropTypes.string,
   maxFeeFiat: PropTypes.string,
   gasErrors: PropTypes.object,
+  minimumGasLimit: PropTypes.number,
 };
--- ui/components/app/edit-gas-display/edit-gas-display.component.js
+++ ui/components/app/edit-gas-display/edit-gas-display.component.js
@@ -56,6 +56,7 @@ export default function EditGasDisplay({
   warning,
   gasErrors,
   onManualChange,
+  minimumGasLimit,
 }) {
   const t = useContext(I18nContext);
 
@@ -203,6 +204,7 @@ export default function EditGasDisplay({
               maxFeeFiat={maxFeePerGasFiat}
               gasErrors={gasErrors}
               onManualChange={onManualChange}
+              minimumGasLimit={minimumGasLimit}
             />
           )}
       </div>
@@ -249,4 +251,5 @@ EditGasDisplay.propTypes = {
   transaction: PropTypes.object,
   gasErrors: PropTypes.object,
   onManualChange: PropTypes.func,
+  minimumGasLimit: PropTypes.number,
 };
