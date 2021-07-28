bafa49b766736a5cc30dc13f49bd854b97b52339
Etienne Dusseault <etienne.dusseault@gmail.com>
2021-07-27 19:43:31 -0500

    separate components

--- ui/pages/permissions-connect/permissions-connect.stories.js
+++ ui/pages/permissions-connect/permissions-connect.stories.js
@@ -1,20 +1,25 @@
 import React from 'react';
-import PermissionConnect from './permissions-connect.component';
+import { action } from '@storybook/addon-actions';
+import { PermissionPageContainerContent } from '../../components/app/permission-page-container';
+import PermissionsConnectFooter from '../../components/app/permissions-connect-footer';
+import { PageContainerFooter } from '../../components/ui/page-container';
+import ChooseAccount from './choose-account';
 
 export default {
   title: 'Permissions Connect',
 };
 
-export const PermissionsConnectComponent = () => {
+export const ChooseAccountComponent = () => {
   return (
-    <PermissionConnect
-      getRequestAccountTabIds={() => {
-        return 1 + 1;
-      }}
-      getCurrentWindowTab={() => {
-        return 1 + 1;
-      }}
-      history={[]}
+    <ChooseAccount
+      selectAccounts={action('Account(s) Selected')}
+      selectedAccountAddresses={
+        new Set([
+          '0xcb47e5e29f925e7482d1712297fb6b268f412344',
+          '0xf68a4b64162906eff0ff6ae34e2bb1cd42fef62d',
+          '0xbe0eb53f46cd790cd13851d5eff43d12404d33e8',
+        ])
+      }
       targetDomainMetadata={{
         host: 'gnosis-safe.io',
         icon: './gnosis.svg',
@@ -43,5 +48,35 @@ export const PermissionsConnectComponent = () => {
         },
       ]}
     />
+  );
+};
+
+export const PermissionPageContainerComponent = () => {
+  return (
+    <div className="page-container permission-approval-container">
+      <PermissionPageContainerContent
+        domainMetadata={{
+          extensionId: '1',
+          host: 'gnosis-safe.io',
+          icon: './gnosis.svg',
+          lastUpdated: 1627423550860,
+          name: 'Gnosis - Manage Digital Assets',
+          origin: 'https://gnosis-safe.io',
+        }}
+        selectedPermissions={{
+          eth_accounts: true,
+        }}
+      />
+      <div className="permission-approval-container__footers">
+        <PermissionsConnectFooter />
+        <PageContainerFooter
+          cancelButtonType="default"
+          onSubmit={action('Account(s) Connected')}
+          submitText="connect"
+          submitButtonType="confirm"
+          buttonSizeLarge={false}
+        />
+      </div>
+    </div>
   );
 };
