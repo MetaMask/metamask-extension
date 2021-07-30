cbfde8a08030110068d97c371cb2c24a49b7ed63
David Walsh <davidwalsh83@gmail.com>
2021-07-30 05:11:27 -0500

    EIP-1559 - Fix duplicated networkSupports1559 variables (#11679)

--- ui/components/app/edit-gas-popover/edit-gas-popover.component.js
+++ ui/components/app/edit-gas-popover/edit-gas-popover.component.js
@@ -5,7 +5,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useGasFeeInputs } from '../../../hooks/useGasFeeInputs';
 import { useShouldAnimateGasEstimations } from '../../../hooks/useShouldAnimateGasEstimations';
 
-import { isEIP1559Network } from '../../../ducks/metamask/metamask';
 import {
   GAS_ESTIMATE_TYPES,
   EDIT_GAS_MODES,
@@ -43,7 +42,6 @@ export default function EditGasPopover({
   const t = useContext(I18nContext);
   const dispatch = useDispatch();
   const showSidebar = useSelector((state) => state.appState.sidebar.isOpen);
-  const networkSupports1559 = useSelector(isEIP1559Network);
 
   const shouldAnimate = useShouldAnimateGasEstimations();
 
@@ -229,7 +227,6 @@ export default function EditGasPopover({
               hasGasErrors={hasGasErrors}
               gasErrors={gasErrors}
               onManualChange={onManualChange}
-              networkSupports1559={networkSupports1559}
               {...editGasDisplayProps}
             />
           </>
