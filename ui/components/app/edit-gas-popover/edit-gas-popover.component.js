c01bec20afb869c94ebeac06a39997475a39f770
David Walsh <davidwalsh83@gmail.com>
2021-07-30 20:22:48 -0500

    Use SIMPLE

--- ui/components/app/edit-gas-popover/edit-gas-popover.component.js
+++ ui/components/app/edit-gas-popover/edit-gas-popover.component.js
@@ -8,7 +8,7 @@ import { useShouldAnimateGasEstimations } from '../../../hooks/useShouldAnimateG
 import {
   GAS_ESTIMATE_TYPES,
   EDIT_GAS_MODES,
-  TWENTY_ONE_THOUSAND,
+  GAS_LIMITS,
 } from '../../../../shared/constants/gas';
 
 import {
@@ -41,7 +41,7 @@ export default function EditGasPopover({
   transaction,
   mode,
   onClose,
-  minimumGasLimit = `0x${decimalToHex(TWENTY_ONE_THOUSAND)}`,
+  minimumGasLimit = GAS_LIMITS.SIMPLE,
 }) {
   const t = useContext(I18nContext);
   const dispatch = useDispatch();
