ddd93f33244b883b3efa370cebb3c1d306aad740
David Walsh <davidwalsh83@gmail.com>
2021-07-31 14:20:56 -0500

    Remove unnecessary props, as well as gas display

--- ui/pages/send/send-content/send-gas-row/send-gas-row.component.js
+++ ui/pages/send/send-content/send-gas-row/send-gas-row.component.js
@@ -3,17 +3,12 @@ import PropTypes from 'prop-types';
 import SendRowWrapper from '../send-row-wrapper';
 import AdvancedGasInputs from '../../../../components/app/gas-customization/advanced-gas-inputs';
 import { GAS_INPUT_MODES } from '../../../../ducks/send';
-import GasFeeDisplay from './gas-fee-display/gas-fee-display.component';
 
 export default class SendGasRow extends Component {
   static propTypes = {
-    gasFeeError: PropTypes.bool,
-    gasLoadingError: PropTypes.bool,
-    gasTotal: PropTypes.string,
     updateGasPrice: PropTypes.func,
     updateGasLimit: PropTypes.func,
     gasInputMode: PropTypes.oneOf(Object.values(GAS_INPUT_MODES)),
-    resetGasButtons: PropTypes.func,
     gasPrice: PropTypes.string,
     gasLimit: PropTypes.string,
     insufficientBalance: PropTypes.bool,
@@ -25,28 +20,23 @@ export default class SendGasRow extends Component {
     trackEvent: PropTypes.func,
   };
 
-  renderContent() {
+  render() {
     const {
-      gasLoadingError,
-      gasTotal,
-      gasInputMode,
-      resetGasButtons,
       updateGasPrice,
       updateGasLimit,
       gasPrice,
       gasLimit,
       insufficientBalance,
       minimumGasLimit,
+      gasInputMode,
     } = this.props;
-    const gasFeeDisplay = (
-      <GasFeeDisplay
-        gasLoadingError={gasLoadingError}
-        gasTotal={gasTotal}
-        onReset={resetGasButtons}
-      />
-    );
-    const advancedGasInputs = (
-      <div>
+
+    if (gasInputMode !== GAS_INPUT_MODES.INLINE) {
+      return null;
+    }
+
+    return (
+      <SendRowWrapper>
         <AdvancedGasInputs
           updateCustomGasPrice={updateGasPrice}
           updateCustomGasLimit={updateGasLimit}
@@ -57,31 +47,7 @@ export default class SendGasRow extends Component {
           customPriceIsSafe
           isSpeedUp={false}
         />
-      </div>
-    );
-    // Tests should behave in same way as mainnet, but are using Localhost
-    switch (gasInputMode) {
-      case GAS_INPUT_MODES.INLINE:
-        return advancedGasInputs;
-      case GAS_INPUT_MODES.CUSTOM:
-      default:
-        return gasFeeDisplay;
-    }
-  }
-
-  render() {
-    const { gasFeeError } = this.props;
-
-    return (
-      <>
-        <SendRowWrapper
-          label={`${this.context.t('transactionFee')}:`}
-          showError={gasFeeError}
-          errorType="gasFee"
-        >
-          {this.renderContent()}
-        </SendRowWrapper>
-      </>
+      </SendRowWrapper>
     );
   }
 }
