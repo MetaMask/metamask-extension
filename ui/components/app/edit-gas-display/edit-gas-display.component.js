5eb884b2bdfe1ccad9368ca42d35645874b64283
David Walsh <davidwalsh83@gmail.com>
2021-07-31 19:00:30 -0500

    EIP-1559 - Fall back to gasLimit and gasPrice for hardware wallets

--- ui/components/app/edit-gas-display/edit-gas-display.component.js
+++ ui/components/app/edit-gas-display/edit-gas-display.component.js
@@ -10,8 +10,10 @@ import {
 
 import Button from '../../ui/button';
 import Typography from '../../ui/typography/typography';
-import { isEIP1559Network } from '../../../ducks/metamask/metamask';
-import { getIsMainnet } from '../../../selectors';
+import {
+  getIsMainnet,
+  networkAndAccountSupports1559,
+} from '../../../selectors';
 
 import {
   COLORS,
@@ -66,8 +68,8 @@ export default function EditGasDisplay({
   estimatesUnavailableWarning,
 }) {
   const t = useContext(I18nContext);
-  const supportsEIP1559 = useSelector(isEIP1559Network);
   const isMainnet = useSelector(getIsMainnet);
+  const use1559 = useSelector(networkAndAccountSupports1559);
 
   const dappSuggestedAndTxParamGasFeesAreTheSame = areDappSuggestedAndTxParamGasFeesTheSame(
     transaction,
@@ -79,11 +81,9 @@ export default function EditGasDisplay({
       dappSuggestedAndTxParamGasFeesAreTheSame,
   );
 
-  const networkSupports1559 = useSelector(isEIP1559Network);
   const showTopError = balanceError || estimatesUnavailableWarning;
-
   const showRadioButtons =
-    networkSupports1559 &&
+    use1559 &&
     gasEstimateType === GAS_ESTIMATE_TYPES.FEE_MARKET &&
     !requireDappAcknowledgement &&
     ![EDIT_GAS_MODES.SPEED_UP, EDIT_GAS_MODES.CANCEL].includes(mode);
@@ -138,12 +138,12 @@ export default function EditGasDisplay({
         )}
         <TransactionTotalBanner
           total={
-            networkSupports1559 || isMainnet
+            use1559 || isMainnet
               ? `~ ${estimatedMinimumFiat}`
               : estimatedMaximumNative
           }
           detail={
-            networkSupports1559 &&
+            use1559 &&
             estimatedMaximumFiat !== undefined &&
             t('editGasTotalBannerSubtitle', [
               <Typography
@@ -173,7 +173,7 @@ export default function EditGasDisplay({
             {t('gasDisplayAcknowledgeDappButtonText')}
           </Button>
         )}
-        {showRadioButtons && (
+        {use1559 && showRadioButtons && (
           <RadioGroup
             name="gas-recommendation"
             options={[
@@ -230,19 +230,16 @@ export default function EditGasDisplay({
             gasErrors={gasErrors}
             onManualChange={onManualChange}
             minimumGasLimit={minimumGasLimit}
-            networkSupportsEIP1559={supportsEIP1559}
           />
         )}
       </div>
-      {networkSupports1559 &&
-        !requireDappAcknowledgement &&
-        showEducationButton && (
-          <div className="edit-gas-display__education">
-            <button onClick={onEducationClick}>
-              {t('editGasEducationButtonText')}
-            </button>
-          </div>
-        )}
+      {use1559 && !requireDappAcknowledgement && showEducationButton && (
+        <div className="edit-gas-display__education">
+          <button onClick={onEducationClick}>
+            {t('editGasEducationButtonText')}
+          </button>
+        </div>
+      )}
     </div>
   );
 }
