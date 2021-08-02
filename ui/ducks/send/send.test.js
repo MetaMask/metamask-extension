98652fee33c8cc079df25d88ed9097ef7084666c
David Walsh <davidwalsh83@gmail.com>
2021-08-02 15:13:59 -0500

    Fix tests

--- ui/ducks/send/send.test.js
+++ ui/ducks/send/send.test.js
@@ -1085,7 +1085,9 @@ describe('Send Slice', () => {
             gasEstimateType: GAS_ESTIMATE_TYPES.NONE,
             gasFeeEstimates: {},
             networkDetails: {
-              EIPS: {},
+              EIPS: {
+                1559: true,
+              },
             },
             accounts: {
               '0xAddress': {
