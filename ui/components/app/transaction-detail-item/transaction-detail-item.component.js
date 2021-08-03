cdbfe0f1329680ca3e12b218e8c319394b8e509e
David Walsh <davidwalsh83@gmail.com>
2021-08-02 21:08:20 -0500

    EIP-1559 - Ensure transaction detail font-size and icon colors are consistent with Figma design (#11735)

--- ui/components/app/transaction-detail-item/transaction-detail-item.component.js
+++ ui/components/app/transaction-detail-item/transaction-detail-item.component.js
@@ -22,14 +22,14 @@ export default function TransactionDetailItem({
         <Typography
           color={detailTitleColor}
           fontWeight={FONT_WEIGHT.BOLD}
-          variant={TYPOGRAPHY.H7}
+          variant={TYPOGRAPHY.H6}
           className="transaction-detail-item__title"
         >
           {detailTitle}
         </Typography>
         {detailText && (
           <Typography
-            variant={TYPOGRAPHY.H7}
+            variant={TYPOGRAPHY.H6}
             className="transaction-detail-item__detail-text"
             color={COLORS.UI4}
           >
@@ -39,7 +39,7 @@ export default function TransactionDetailItem({
         <Typography
           color={COLORS.BLACK}
           fontWeight={FONT_WEIGHT.BOLD}
-          variant={TYPOGRAPHY.H7}
+          variant={TYPOGRAPHY.H6}
           className="transaction-detail-item__total"
         >
           {detailTotal}
