f0daa28fac6e2f763d538567a765b1baf64eb70c
David Walsh <davidwalsh83@gmail.com>
2021-08-04 16:16:18 -0500

    EIP-1559 - Temporarily remove the loading heartbeat from the transaction detail (#11762)

--- ui/components/app/transaction-detail/transaction-detail.component.js
+++ ui/components/app/transaction-detail/transaction-detail.component.js
@@ -2,20 +2,14 @@ import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 
 import { I18nContext } from '../../../contexts/i18n';
-import { useShouldAnimateGasEstimations } from '../../../hooks/useShouldAnimateGasEstimations';
 
 import TransactionDetailItem from '../transaction-detail-item/transaction-detail-item.component';
-import LoadingHeartBeat from '../../ui/loading-heartbeat';
 
 export default function TransactionDetail({ rows = [], onEdit }) {
   const t = useContext(I18nContext);
-  const shouldAnimate = useShouldAnimateGasEstimations();
 
   return (
     <div className="transaction-detail">
-      {process.env.IN_TEST === 'true' ? null : (
-        <LoadingHeartBeat active={shouldAnimate} />
-      )}
       {onEdit && (
         <div className="transaction-detail-edit">
           <button onClick={onEdit}>{t('edit')}</button>
