d75795b73fa465f1c71fd6907e12c21bb236c639
kumavis <kumavis@users.noreply.github.com>
2021-08-10 15:09:42 -0700

    add appropriate test timeouts (#11667)
    Co-authored-by: Etienne Dusseault <etienne.dusseault@gmail.com>

--- test/e2e/helpers.js
+++ test/e2e/helpers.js
@@ -11,6 +11,8 @@ const { buildWebDriver } = require('./webdriver');
 const tinyDelayMs = 200;
 const regularDelayMs = tinyDelayMs * 2;
 const largeDelayMs = regularDelayMs * 2;
+const xLargeDelayMs = largeDelayMs * 2;
+const xxLargeDelayMs = xLargeDelayMs * 2;
 
 const dappPort = 8080;
 
@@ -144,5 +146,7 @@ module.exports = {
   tinyDelayMs,
   regularDelayMs,
   largeDelayMs,
+  xLargeDelayMs,
+  xxLargeDelayMs,
   withFixtures,
 };
