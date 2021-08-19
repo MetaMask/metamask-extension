8258a445aeea71519f78a34e79d5b890e096da50
Elliot Winkler <elliot.winkler@gmail.com>
2021-08-19 11:40:27 -0600

    Update header on Send page to be more accurate
    Change "Add Recipient" to "Send to".

--- ui/pages/send/send-header/send-header.component.js
+++ ui/pages/send/send-header/send-header.component.js
@@ -28,7 +28,7 @@ export default function SendHeader() {
   let title = asset.type === ASSET_TYPES.NATIVE ? t('send') : t('sendTokens');
 
   if (stage === SEND_STAGES.ADD_RECIPIENT || stage === SEND_STAGES.INACTIVE) {
-    title = t('addRecipient');
+    title = t('sendTo');
   } else if (stage === SEND_STAGES.EDIT) {
     title = t('edit');
   }
