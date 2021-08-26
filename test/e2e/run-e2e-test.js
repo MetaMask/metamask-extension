c0bf3d76de96fcc54ace20b8e298232c503d58ce
Elliot Winkler <elliot.winkler@gmail.com>
2021-08-26 16:16:03 -0600

    Call xvfb-run correctly

--- test/e2e/run-e2e-test.js
+++ test/e2e/run-e2e-test.js
@@ -78,12 +78,13 @@ async function main() {
     process.env.CI === 'true'
       ? [
           'xvfb-run',
-          '-a',
-          '--server-args',
-          '-screen 0 1280x800x24 -ac -nolisten tcp -dpi 96 +extension RANDR',
-          '-a',
-          'yarn',
-          ...mochaArgs,
+          [
+            '--server-args',
+            '-screen 0 1280x1024x24 -ac -nolisten tcp -dpi 96 +extension RANDR',
+            '-a',
+            'yarn',
+            ...mochaArgs,
+          ],
         ]
       : ['yarn', mochaArgs];
 
