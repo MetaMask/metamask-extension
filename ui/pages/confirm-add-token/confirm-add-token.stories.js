09e79f273a41723fcb870107cfdc19fd730e2fa3
Etienne Dusseault <etienne.dusseault@gmail.com>
2021-09-07 21:21:41 -0230

    fix state problems with Storybook (#11984)
    * fix state problems with Storybook
    
    * lint
    
    * lint
    
    * Cleanup
    
    Co-authored-by: ryanml <ryanlanese@gmail.com>

--- ui/pages/confirm-add-token/confirm-add-token.stories.js
+++ ui/pages/confirm-add-token/confirm-add-token.stories.js
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 
 import { createBrowserHistory } from 'history';
 import { text } from '@storybook/addon-knobs';
-import { store } from '../../../.storybook/preview';
+import { store, getNewState } from '../../../.storybook/preview';
 import { tokens } from '../../../.storybook/initial-states/approval-screens/add-token';
 import { updateMetamaskState } from '../../store/actions';
 import ConfirmAddToken from '.';
@@ -20,16 +20,30 @@ const PageSet = ({ children }) => {
   const pendingTokensState = state.metamask.pendingTokens;
   // only change the first token in the list
   useEffect(() => {
-    const pendingTokens = { ...pendingTokensState };
-    pendingTokens['0x33f90dee07c6e8b9682dd20f73e6c358b2ed0f03'].symbol = symbol;
-    store.dispatch(updateMetamaskState({ pendingTokens }));
-  }, [symbol, pendingTokensState]);
+    pendingTokensState[
+      '0x33f90dee07c6e8b9682dd20f73e6c358b2ed0f03'
+    ].symbol = symbol;
+    store.dispatch(
+      updateMetamaskState(
+        getNewState(state.metamask, {
+          pendingTokens: pendingTokensState,
+        }),
+      ),
+    );
+  }, [symbol, pendingTokensState, state.metamask]);
 
   return children;
 };
 
 export const AddToken = () => {
-  store.dispatch(updateMetamaskState({ pendingTokens: tokens }));
+  const state = store.getState();
+  store.dispatch(
+    updateMetamaskState(
+      getNewState(state.metamask, {
+        pendingTokens: tokens,
+      }),
+    ),
+  );
   return (
     <PageSet>
       <ConfirmAddToken history={history} />
