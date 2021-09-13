ff6f8c4884e9fd8b87276406eff79b807dcbb947
Etienne Dusseault <etienne.dusseault@gmail.com>
2021-09-12 23:05:14 -0230

    send-hex-data

---
+++ ui/pages/send/send-content/send-hex-data-row/send-hex-data-row.stories.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { boolean } from '@storybook/addon-knobs';
+import SendHexDataRow from './send-hex-data-row.component';
+
+export default {
+  title: 'SendHexDataRow',
+};
+
+export const SendHexDataRowComponent = () => {
+  return (
+    <div style={{ width: 450 }}>
+      <SendHexDataRow
+        inError={boolean('In Error', false)}
+        updateSendHexData={() => null}
+      />
+    </div>
+  );
+};
