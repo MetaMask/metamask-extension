aec31cc8b7fedbb24a1de9068c06248076616b82
Etienne Dusseault <etienne.dusseault@gmail.com>
2021-09-13 18:35:17 -0230

    Add send-footer to Storybook (#12082)

---
+++ ui/pages/send/send-footer/send-footer.stories.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { boolean } from '@storybook/addon-knobs';
+
+import SendFooter from './send-footer.component';
+
+export default {
+  title: 'SendFooter',
+};
+
+export const SendFooterComponent = () => {
+  const disabled = boolean('Disabled', false);
+  return (
+    <SendFooter
+      clearSend={() => action('Cancel Button Pressed')()}
+      sign={() => action('Next Button Pressed')()}
+      // The other props below are only to make the component show no error
+      from={{ address: '' }}
+      history={{ push: () => undefined }}
+      addToAddressBookIfNew={() => undefined}
+      disabled={disabled}
+      mostRecentOverviewPage=""
+      resetSendState={() => undefined}
+      sendErrors={{}}
+    />
+  );
+};
