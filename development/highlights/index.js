2b9fe057db0433b76ba671d71be110b318cd111d
kumavis <aaron@kumavis.me>
2021-09-14 23:46:39 -0700

    ci/announce/highlight - minimal documentation

--- development/highlights/index.js
+++ development/highlights/index.js
@@ -6,6 +6,9 @@ module.exports = { getHighlights };
 
 async function getHighlights({ artifactBase }) {
   let highlights = '';
+  // here we assume the PR base branch ("target") is `develop` in lieu of doing
+  // a query against the github api which requires an access token
+  // see https://discuss.circleci.com/t/how-to-retrieve-a-pull-requests-base-branch-name-github/36911
   const changedFiles = await getChangedFiles({ target: 'develop' });
   console.log(`detected changed files vs develop:`);
   for (const filename of changedFiles) {
