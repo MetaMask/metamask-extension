f2ef98a4fc48a2cf78fb50de8e510a88259f6c29
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2021-10-04 00:41:21 +0100

    Test cases for toggle test networks in advanced tab component.
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- ui/pages/settings/advanced-tab/advanced-tab.component.test.js
+++ ui/pages/settings/advanced-tab/advanced-tab.component.test.js
@@ -4,16 +4,23 @@ import { shallow } from 'enzyme';
 import TextField from '../../../components/ui/text-field';
 import { LEDGER_TRANSPORT_TYPES } from '../../../../shared/constants/hardware-wallets';
 import AdvancedTab from './advanced-tab.component';
+import ToggleButton from '../../../components/ui/toggle-button';
 
 describe('AdvancedTab Component', () => {
-  it('should render correctly when threeBoxFeatureFlag', () => {
-    const root = shallow(
+  let root;
+  const setAutoLockTimeLimitSpy = sinon.spy();
+  const toggleTestnet = sinon.spy();
+
+  beforeAll(() => {    
+    root = shallow(
       <AdvancedTab
         ipfsGateway=""
-        setAutoLockTimeLimit={() => undefined}
+        setAutoLockTimeLimit={setAutoLockTimeLimitSpy}
         setIpfsGateway={() => undefined}
         setShowFiatConversionOnTestnetsPreference={() => undefined}
         setThreeBoxSyncingPermission={() => undefined}
+        setShowTestNetworks={toggleTestnet}
+        showTestNetworks={false}
         threeBoxDisabled
         threeBoxSyncingAllowed={false}
         ledgerTransportType={LEDGER_TRANSPORT_TYPES.U2F}
@@ -27,8 +34,10 @@ describe('AdvancedTab Component', () => {
         },
       },
     );
+  });
 
-    expect(root.find('.settings-page__content-row')).toHaveLength(12);
+  it('should render correctly when threeBoxFeatureFlag', () => {
+    expect(root.find('.settings-page__content-row')).toHaveLength(13);
   });
 
   it('should update autoLockTimeLimit', () => {
@@ -54,7 +63,7 @@ describe('AdvancedTab Component', () => {
       },
     );
 
-    const autoTimeout = root.find('.settings-page__content-row').at(7);
+    const autoTimeout = root.find('.settings-page__content-row').at(8);
     const textField = autoTimeout.find(TextField);
 
     textField.props().onChange({ target: { value: 1440 } });
@@ -63,4 +72,11 @@ describe('AdvancedTab Component', () => {
     autoTimeout.find('.settings-tab__rpc-save-button').simulate('click');
     expect(setAutoLockTimeLimitSpy.args[0][0]).toStrictEqual(1440);
   });
+
+  it('should toggle show test networks', () => {
+    const testNetworks = root.find('.settings-page__content-row').at(6);
+    const toggleButton = testNetworks.find(ToggleButton);
+    toggleButton.first().simulate('toggle');    
+    expect(toggleTestnet.calledOnce).toStrictEqual(true);
+  });  
 });
