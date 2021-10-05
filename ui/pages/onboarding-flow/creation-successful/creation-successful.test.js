24e9bb67f81abf7210063bdfb59604cf113bf42f
Alex <adonesky@gmail.com>
2021-10-05 14:07:50 -0500

    cleaning up test and translation entry typo

--- ui/pages/onboarding-flow/creation-successful/creation-successful.test.js
+++ ui/pages/onboarding-flow/creation-successful/creation-successful.test.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { fireEvent } from '@testing-library/react';
-import configureMockStore from 'redux-mock-store';
 import reactRouterDom from 'react-router-dom';
-import { renderWithProvider } from '../../../../test/lib/render-helpers';
 import {
   ONBOARDING_PIN_EXTENSION_ROUTE,
   ONBOARDING_PRIVACY_SETTINGS_ROUTE,
 } from '../../../helpers/constants/routes';
+import { renderWithProvider } from '../../../../test/jest';
 import CreationSuccessful from './creation-successful';
 
 describe('Creation Successful Onboarding View', () => {
-  const useHistoryOriginal = reactRouterDom.useHistory;
   const pushMock = jest.fn();
   beforeAll(() => {
     jest
@@ -18,28 +16,16 @@ describe('Creation Successful Onboarding View', () => {
       .mockImplementation()
       .mockReturnValue({ push: pushMock });
   });
-  afterAll(() => {
-    reactRouterDom.useHistory = useHistoryOriginal;
-  });
-
-  const mockStore = {
-    metamask: {
-      provider: {
-        type: 'test',
-      },
-    },
-  };
-  const store = configureMockStore()(mockStore);
 
   it('should redirect to pin-extension view when "Done" button is clicked', () => {
-    const { getByText } = renderWithProvider(<CreationSuccessful />, store);
+    const { getByText } = renderWithProvider(<CreationSuccessful />);
     const doneButton = getByText('Done');
     fireEvent.click(doneButton);
     expect(pushMock).toHaveBeenCalledWith(ONBOARDING_PIN_EXTENSION_ROUTE);
   });
 
   it('should redirect to privacy-settings view when "Set advanced privacy settings" button is clicked', () => {
-    const { getByText } = renderWithProvider(<CreationSuccessful />, store);
+    const { getByText } = renderWithProvider(<CreationSuccessful />);
     const privacySettingsButton = getByText('Set advanced privacy settings');
     fireEvent.click(privacySettingsButton);
     expect(pushMock).toHaveBeenCalledWith(ONBOARDING_PRIVACY_SETTINGS_ROUTE);
