3a82c744f3a851336c921fa9de77053902652542
Elliot Winkler <elliot.winkler@gmail.com>
2021-10-12 16:14:52 -0600

    Refactor tests for AdvancedGasInputs
    * Upgrade style from Enzyme to React Testing Library
    * Use real i18n provider instead of a fake one so that we can look for
      English text instead of just i18n strings, improving readability

--- test/jest/index.js
+++ test/jest/index.js
@@ -1,3 +1,4 @@
+export { screen, fireEvent } from '@testing-library/react';
 export { createSwapsMockStore } from './mock-store';
 export { renderWithProvider } from './rendering';
 export { setBackgroundConnection } from './background';
