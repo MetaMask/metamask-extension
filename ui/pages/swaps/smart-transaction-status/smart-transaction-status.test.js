696fb410267a8541cd09f0d211a9458127f171a8
dan437 <80175477+dan437@users.noreply.github.com>
2021-10-13 15:18:07 +0200

    Fix a UT

--- ui/pages/swaps/smart-transaction-status/smart-transaction-status.test.js
+++ ui/pages/swaps/smart-transaction-status/smart-transaction-status.test.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 
 import {
   renderWithProvider,
   createSwapsMockStore,
+  setBackgroundConnection,
 } from '../../../../test/jest';
 import SmartTransactionStatus from '.';
 
+const middleware = [thunk];
+setBackgroundConnection({
+  stopPollingForQuotes: jest.fn(),
+});
+
 describe('SmartTransactionStatus', () => {
   it('renders the component with initial props', () => {
-    const store = configureMockStore()(createSwapsMockStore());
+    const store = configureMockStore(middleware)(createSwapsMockStore());
     const { getByText } = renderWithProvider(<SmartTransactionStatus />, store);
     expect(getByText('Transaction is pending')).toBeInTheDocument();
     expect(
