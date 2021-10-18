16d741348e2ccc07de6e2af210f3f790b7bb1f3c
David Walsh <davidwalsh83@gmail.com>
2021-10-18 17:33:42 -0500

    Fix #12190 - Implement new pin extension view for onboarding (#12290)

--- ui/pages/onboarding-flow/onboarding-flow.js
+++ ui/pages/onboarding-flow/onboarding-flow.js
@@ -13,6 +13,7 @@ import {
   ONBOARDING_PRIVACY_SETTINGS_ROUTE,
   ONBOARDING_COMPLETION_ROUTE,
   ONBOARDING_IMPORT_WITH_SRP_ROUTE,
+  ONBOARDING_PIN_EXTENSION_ROUTE,
 } from '../../helpers/constants/routes';
 import {
   getCompletedOnboarding,
@@ -37,6 +38,7 @@ import PrivacySettings from './privacy-settings/privacy-settings';
 import CreationSuccessful from './creation-successful/creation-successful';
 import OnboardingWelcome from './welcome/welcome';
 import ImportSRP from './import-srp/import-srp';
+import OnboardingPinExtension from './pin-extension/pin-extension';
 
 export default function OnboardingFlow() {
   const [secretRecoveryPhrase, setSecretRecoveryPhrase] = useState('');
@@ -54,7 +56,7 @@ export default function OnboardingFlow() {
     // For ONBOARDING_V2 dev purposes,
     // Remove when ONBOARDING_V2 dev complete
     if (process.env.ONBOARDING_V2) {
-      history.push(ONBOARDING_IMPORT_WITH_SRP_ROUTE);
+      history.push(ONBOARDING_PIN_EXTENSION_ROUTE);
       return;
     }
 
@@ -155,6 +157,10 @@ export default function OnboardingFlow() {
           <Route
             path={ONBOARDING_WELCOME_ROUTE}
             component={OnboardingWelcome}
+          />
+          <Route
+            path={ONBOARDING_PIN_EXTENSION_ROUTE}
+            component={OnboardingPinExtension}
           />
           <Route exact path="*" component={OnboardingFlowSwitch} />
         </Switch>
