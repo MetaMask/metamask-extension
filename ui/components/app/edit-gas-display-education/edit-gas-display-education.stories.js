e1220d15388e29da344428933f06f003ab12c862
Etienne Dusseault <etienne.dusseault@gmail.com>
2021-10-20 13:20:03 -0300

    edit-gas-display (#12388)

--- ui/components/app/edit-gas-display-education/edit-gas-display-education.stories.js
+++ ui/components/app/edit-gas-display-education/edit-gas-display-education.stories.js
@@ -2,7 +2,7 @@ import React from 'react';
 import EditGasDisplayEducation from '.';
 
 export default {
-  title: 'Edit Gas Display',
+  title: 'Edit Gas Display Education',
   id: __filename,
 };
 
