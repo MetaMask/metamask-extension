db20399d817e3f19fe0e83004c4f1deffd70df16
Dan J Miller <danjm.com@gmail.com>
2021-11-01 08:16:05 -0230

    Turn off all old notifications (#12500)
    * Turn off all old notifications
    
    * Remove unnecessary attempt to close whats new popup in e2e test
    
    * Remove unneeded whats new popup closes in e2e tests
    
    * Lint fix

--- test/e2e/metamask-ui.spec.js
+++ test/e2e/metamask-ui.spec.js
@@ -176,24 +176,6 @@ describe('MetaMask', function () {
     });
   });
 
-  describe("Close the what's new popup", function () {
-    it("should show the what's new popover", async function () {
-      const popoverTitle = await driver.findElement(
-        '.popover-header__title h2',
-      );
-
-      assert.equal(await popoverTitle.getText(), "What's new");
-    });
-
-    it("should close the what's new popup", async function () {
-      const popover = await driver.findElement('.popover-container');
-
-      await driver.clickElement('[data-testid="popover-close"]');
-
-      await popover.waitForElementState('hidden');
-    });
-  });
-
   describe('Import Secret Recovery Phrase', function () {
     it('logs out of the vault', async function () {
       await driver.clickElement('.account-menu__icon');
