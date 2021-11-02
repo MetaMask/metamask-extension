39a16dc41ecf1a1255b32ad60792683f638ba773
Etienne Dusseault <etienne.dusseault@gmail.com>
2021-11-01 21:18:41 -0300

    Update ui/components/app/edit-gas-display-education/edit-gas-display-education.stories.js
    Co-authored-by: George Marshall <george.marshall@consensys.net>

--- ui/components/app/edit-gas-display-education/edit-gas-display-education.stories.js
+++ ui/components/app/edit-gas-display-education/edit-gas-display-education.stories.js
@@ -2,7 +2,7 @@ import React from 'react';
 import EditGasDisplayEducation from '.';
 
 export default {
-  title: 'Edit Gas Display Education',
+  title: 'Components/App/Edit Gas Display Education',
   id: __filename,
 };
 
