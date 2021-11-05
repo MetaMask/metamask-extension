660e9fcbf3fbe17effe6be0d6a9a2e5e5666cd7c
jpuri <jyotipuri@gmail.com>
2021-11-06 00:04:39 +0530

    fix unit test

--- ui/pages/confirm-transaction-base/gas-details-item/gas-details-item.test.js
+++ ui/pages/confirm-transaction-base/gas-details-item/gas-details-item.test.js
@@ -27,15 +27,6 @@ describe('GasDetailsItem', () => {
     expect(screen.queryByText('Gas')).toBeInTheDocument();
     expect(screen.queryByText('(estimated)')).toBeInTheDocument();
     expect(screen.queryByText('Max fee:')).toBeInTheDocument();
-  });
-
-  it('should render gas fee details', () => {
-    render({
-      hexMinimumTransactionFee: '0x1ca62a4f7800',
-      hexMaximumTransactionFee: '0x290ee75e3d900',
-    });
-    expect(screen.queryAllByText('0.000031')).toHaveLength(2);
     expect(screen.queryByText('ETH')).toBeInTheDocument();
-    expect(screen.queryByText('0.000722')).toBeInTheDocument();
   });
 });
