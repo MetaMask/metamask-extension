df0b16ecce43b50b18ac425c2acfdd66c4057b35
VSaric <vladimir.saric@consensys.net>
2021-11-09 14:54:41 +0100

    Created a Nickname popover

---
+++ ui/components/ui/nickname-popover/nickname-popover.stories.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { action } from '@storybook/addon-actions';
+import Button from '../button';
+import NicknamePopover from '.';
+
+export default {
+  title: 'NicknamePopover',
+  id: __filename,
+};
+
+export const Default = () => {
+  const [showNicknamePopover, setShowNicknamePopover] = useState(false);
+
+  return (
+    <div>
+      <Button onClick={() => setShowNicknamePopover(true)}>
+        Open Nickname Popover
+      </Button>
+      {showNicknamePopover && (
+        <NicknamePopover
+          address="0x5e6DaAD1BE117e26590F9eEcD509336ABFBe5966"
+          onClose={() => setShowNicknamePopover(false)}
+          onAdd={action('add NicknamePopover')}
+        />
+      )}
+    </div>
+  );
+};
