b9d60a35b5eec082ecfcbed8f50ae0bdc71b474d
dragana8 <dragana.simic@consensys.net>
2021-11-10 14:03:06 +0100

    added update nickname popover

---
+++ ui/components/ui/update-nickname-popover/update-nickname-popover.js
@@ -0,0 +1,110 @@
+import React, { useCallback, useContext } from 'react';
+import PropTypes from 'prop-types';
+
+import Popover from '../popover';
+import Button from '../button';
+import TextField from '../text-field';
+
+import { I18nContext } from '../../../contexts/i18n';
+
+import Identicon from '../identicon/identicon.component';
+import { shortenAddress } from '../../../helpers/utils/util';
+
+export default function UpdateNicknamePopover({
+  nickname,
+  address,
+  onAdd,
+  memo,
+  onClose,
+}) {
+  const t = useContext(I18nContext);
+
+  const closePopover = useCallback(() => {
+    onClose();
+  }, [onClose]);
+
+  const onCancel = () => {
+    onClose();
+  };
+
+  const onSubmit = useCallback(() => {
+    onAdd(address);
+    onClose();
+  }, [address, onClose, onAdd]);
+
+  let title = t('addANickname');
+  if (nickname) {
+    title = t('editAddressNickname');
+  }
+
+  return (
+    <Popover
+      title={title}
+      onClose={closePopover}
+      className="update-nickname"
+      footer={
+        <>
+          <Button
+            className="update-nickname__cancel"
+            type="secondary"
+            onClick={onCancel}
+          >
+            {t('cancel')}
+          </Button>
+          <Button type="primary" onClick={onSubmit} disabled={!nickname}>
+            {t('save')}
+          </Button>
+        </>
+      }
+    >
+      <div
+        className="update-nickname__content"
+        style={{ padding: '0 20px 20px 20px', position: 'relative' }}
+      >
+        <Identicon
+          className="update-nickname__content__indenticon"
+          address={address}
+          diameter={36}
+        />
+        <label className="update-nickname__content__label--capitalized">
+          {t('address')}
+        </label>
+        <div className="update-nickname__content__address">
+          {shortenAddress(address)}
+        </div>
+        <div className="update-nickname__content__nickname-label">
+          {t('nickname')}
+        </div>
+        <TextField
+          className="update-nickname__content__text-field"
+          value={nickname}
+          fullWidth
+        />
+        <div className="update-nickname__content__label--capitalized">
+          {t('memo')}
+        </div>
+        <TextField
+          type="text"
+          id="memo"
+          placeholder={memo}
+          fullWidth
+          margin="dense"
+          multiline
+          rows={3}
+          classes={{
+            inputMultiline: 'update-nickname__content__text-area',
+            inputRoot: 'update-nickname__content__text-area-wrapper',
+          }}
+        />
+      </div>
+    </Popover>
+  );
+}
+
+UpdateNicknamePopover.propTypes = {
+  nickname: PropTypes.string,
+  address: PropTypes.string,
+  memo: PropTypes.string,
+  onAdd: PropTypes.func,
+  onClose: PropTypes.func,
+};
