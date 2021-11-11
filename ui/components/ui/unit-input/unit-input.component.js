3de02a528cedc608fea48231499f24cde792ed8b
Olusegun Akintayo <akintayo.segun@gmail.com>
2021-11-11 22:16:28 +0400

    We shouldn't be changing decimal places as user type, we should do that on blur. (#12631)
    * We shouldn't be changing decimal places as user type, we should do that
    on blur.
    
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>
    
    * Lint fixes.
    
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>
    
    * Refactor code.
    
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>
    
    * Linter fixes.
    
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- ui/components/ui/unit-input/unit-input.component.js
+++ ui/components/ui/unit-input/unit-input.component.js
@@ -17,6 +17,7 @@ export default class UnitInput extends PureComponent {
     actionComponent: PropTypes.node,
     error: PropTypes.bool,
     onChange: PropTypes.func,
+    onBlur: PropTypes.func,
     placeholder: PropTypes.string,
     suffix: PropTypes.string,
     value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
@@ -55,6 +56,8 @@ export default class UnitInput extends PureComponent {
     if (value === '') {
       this.setState({ value: '0' });
     }
+
+    this.props.onBlur && this.props.onBlur(value);
   };
 
   handleChange = (event) => {
