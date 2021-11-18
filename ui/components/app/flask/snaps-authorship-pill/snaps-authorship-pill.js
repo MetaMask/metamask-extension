430d148d99f514c6d51c826aecffae1be2a4c44e
hmalik88 <hassan.malik@consensys.net>
2021-11-18 14:29:41 -0500

    changed label to an h7

--- ui/components/app/flask/snaps-authorship-pill/snaps-authorship-pill.js
+++ ui/components/app/flask/snaps-authorship-pill/snaps-authorship-pill.js
@@ -3,7 +3,12 @@ import PropTypes from 'prop-types';
 import Chip from '../../../ui/chip';
 import Box from '../../../ui/box';
 import NpmIcon from '../../../ui/icon/npm-icon';
-import { COLORS } from '../../../../helpers/constants/design-system';
+import Typography from '../../../ui/typography';
+import {
+  COLORS,
+  TYPOGRAPHY,
+  FONT_WEIGHT,
+} from '../../../../helpers/constants/design-system';
 
 const SnapsAuthorshipPill = ({ packageName, className }) => {
   return (
@@ -16,7 +21,17 @@ const SnapsAuthorshipPill = ({ packageName, className }) => {
       className={className}
       label={packageName}
       backgroundColor={COLORS.UI1}
-    />
+    >
+      <Typography
+        className="chip__label"
+        variant={TYPOGRAPHY.H7}
+        tag="span"
+        color={COLORS.UI4}
+        fontWeight={FONT_WEIGHT['400']}
+      >
+        {packageName}
+      </Typography>
+    </Chip>
   );
 };
 
