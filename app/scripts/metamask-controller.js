3c09480582c43faa7104fa3439e3a185d7f86bfd
Erik Marks <rekmarks@protonmail.com>
2021-11-17 22:14:03 -0800

    Fix build errors, regenerate build system policy

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -2909,7 +2909,7 @@ export default class MetamaskController extends EventEmitter {
     if (!isInternal) {
       // permissions
       engine.push(
-        this.permissionsController.createMiddleware({ origin, extensionId }),
+        permissionsController.createMiddleware({ origin, extensionId }),
       );
     }
 
