0a862838924b8aacee496c117676813c96c76689
hmalik88 <hassan.malik@consensys.net>
2021-11-17 21:11:49 -0500

    added snaps-authorship-pill component and story

---
+++ ui/components/app/flask/snaps-authorship-pill/snaps-authorship-pill.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Chip from '../../../ui/chip';
+import Box from '../../../ui/box';
+import NpmIcon from '../../../ui/icon/npm-icon';
+import { COLORS } from '../../../../helpers/constants/design-system';
+
+const SnapsAuthorshipPill = ({ packageName }) => {
+  return (
+    <Chip
+      leftIcon={
+        <Box paddingLeft={2}>
+          <NpmIcon width={25} height={10} />
+        </Box>
+      }
+      label={packageName}
+      backgroundColor={COLORS.UI1}
+    />
+  );
+};
+
+SnapsAuthorshipPill.propTypes = {
+  /**
+   * NPM package name of the snap
+   */
+  packageName: PropTypes.string,
+};
+
+export default SnapsAuthorshipPill;
---
+++ ui/components/app/flask/snaps-authorship-pill/snaps-authorship-pill.stories.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import SnapsAuthorshipPill from '.';
+
+export default {
+  title: 'App/Flask/SnapsAuthorshipPill',
+  id: __filename,
+  component: SnapsAuthorshipPill,
+  argTypes: {
+    packageName: {
+      control: 'text',
+    },
+  },
+};
+
+export const DefaultStory = (args) => <SnapsAuthorshipPill {...args} />;
+
+DefaultStory.storyName = 'Default';
+
+DefaultStory.args = {
+  packageName: '@airswap/snap',
+};
