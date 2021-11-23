cbe74cb06049a16a6f6769d78e7dd2c0de5d4b09
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2021-11-24 00:37:14 +0400

    Premilimary Sanitize data logic.
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- ui/components/app/signature-request/signature-request.component.js
+++ ui/components/app/signature-request/signature-request.component.js
@@ -5,6 +5,8 @@ import LedgerInstructionField from '../ledger-instruction-field';
 import Header from './signature-request-header';
 import Footer from './signature-request-footer';
 import Message from './signature-request-message';
+import { TypedDataUtils } from 'eth-sig-util';
+import { MESSAGE_TYPE } from '../../../../shared/constants/app';
 
 export default class SignatureRequest extends PureComponent {
   static propTypes = {
@@ -48,6 +50,74 @@ export default class SignatureRequest extends PureComponent {
     const { message, domain = {} } = JSON.parse(data);
     const { metricsEvent } = this.context;
 
+    const mapType = (msgType, definedType, types) => {
+      if (msgType === definedType.type) {
+        // if the javascript type is the same as defined type, return the js type
+        return msgType;
+      } else if (types[definedType.type.replace("[]", "")] !== undefined) {
+        // if the type is defined in types, return it
+        return definedType.type;
+      } else {
+        // try and map it to a solidity type
+        if (
+          (
+            definedType.type.indexOf("int") >= 0 ||
+            definedType.type.indexOf("fixed") >= 0
+          ) && msgType === "number") {
+          return msgType;
+        } else if (
+          (
+            definedType.type.indexOf("bytes") >= 0 ||
+            definedType.type === "address"
+          ) && msgType === "string") {
+          return msgType;
+        }
+      }
+
+      // if we get here we can't find or map the type....what should we do
+      throw Error("Unknwn type found in message: " + definedType.type);
+    }
+
+    const sanitizeMessage = (msg, primaryType, types, sanitizedMessage = {}) => {
+      const msgKeys = Object.keys(msg);
+      const msgTypes = Object.values(msg).map(value => typeof (value));
+
+      msgKeys.forEach((msgKey, index) => {
+        const msgType = msgTypes[index];
+        const definedType = primaryType.find(type => type.name === msgKey);
+        if (msgType === "object") {
+          const newPrimaryType = types[definedType.type];
+          sanitizedMessage[msgKey] = {};
+          sanitizeMessage(msg[msgKey], newPrimaryType, types, sanitizedMessage);
+        } else if (msgType === "array") {
+          const messages = sanitizedMessage[msgKey];
+          messages.forEach(msg => {
+            const type = typeof()
+          })
+          const newPrimaryType = mapType(msgType, definedType. types);
+          sanitizedMessage[msgKey] = [];
+          sanitizeMessage(msg[msgKey], newPrimaryType, types, sanitizedMessage);
+        } else {
+          const newPrimaryType = mapType(msgType, definedType. types);
+
+        }
+      });
+    }
+
+    const sanitizeData = (msg, msgType, msgVersion) => {
+      if (
+        msgType === MESSAGE_TYPE.ETH_SIGN_TYPED_DATA &&
+        (msgVersion === 'V3' || msgVersion === 'V4')
+      ) {
+        const parsed = JSON.parse(data);
+        parsed.message.uselessValue = 1;
+        parsed.message.uselessName = "two";
+        return TypedDataUtils.sanitizeData(parsed);
+      }
+
+      return msg;
+    }
+
     const onSign = (event) => {
       sign(event);
       metricsEvent({
@@ -79,7 +149,7 @@ export default class SignatureRequest extends PureComponent {
     };
 
     return (
-      <div className="signature-request page-container">
+      <div className="signature-request page-container" >
         <Header fromAccount={fromAccount} />
         <div className="signature-request-content">
           <div className="signature-request-content__title">
@@ -105,13 +175,13 @@ export default class SignatureRequest extends PureComponent {
             <LedgerInstructionField showDataInstruction />
           </div>
         ) : null}
-        <Message data={message} />
+        <Message data={sanitizeMessage(message, type, version)} />
         <Footer
           cancelAction={onCancel}
           signAction={onSign}
           disabled={hardwareWalletRequiresConnection}
         />
-      </div>
+      </div >
     );
   }
 }
