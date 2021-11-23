2c0a07be6e0c8dda5c71a470aad41e5f0d4709c8
jpuri <jyotipuri@gmail.com>
2021-11-23 20:45:00 +0530

    Integrating advance gas fee modal on confirm transaction page

--- ui/components/app/advanced-gas-fee-popover/advanced-gas-fee-popover.js
+++ ui/components/app/advanced-gas-fee-popover/advanced-gas-fee-popover.js
@@ -1,20 +1,25 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+
+import { useTransactionModalContext } from '../../../contexts/transaction-modal';
+import { useI18nContext } from '../../../hooks/useI18nContext';
+
 import Popover from '../../ui/popover';
 import Box from '../../ui/box';
 import Button from '../../ui/button';
 import I18nValue from '../../ui/i18n-value';
-import { useI18nContext } from '../../../hooks/useI18nContext';
 
-const AdvancedGasFeePopover = ({ onClose }) => {
+const AdvancedGasFeePopover = () => {
   const t = useI18nContext();
+  const { currentModal, closeModal } = useTransactionModalContext();
+
+  if (currentModal !== 'advancedGasFee') return null;
 
   return (
     <Popover
       className="advanced-gas-fee-popover"
       title={t('advancedGasFeeModalTitle')}
-      onBack={() => onClose()}
-      onClose={() => onClose()}
+      onBack={() => closeModal('advancedGasFee')}
+      onClose={() => closeModal('advancedGasFee')}
       footer={
         <Button type="primary">
           <I18nValue messageKey="save" />
@@ -24,10 +29,6 @@ const AdvancedGasFeePopover = ({ onClose }) => {
       <Box className="advanced-gas-fee-popover" margin={4}></Box>
     </Popover>
   );
-};
-
-AdvancedGasFeePopover.propTypes = {
-  onClose: PropTypes.func,
 };
 
 export default AdvancedGasFeePopover;
--- ui/components/app/edit-gas-fee-popover/edit-gas-fee-popover.js
+++ ui/components/app/edit-gas-fee-popover/edit-gas-fee-popover.js
@@ -1,21 +1,24 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import { PRIORITY_LEVELS } from '../../../../shared/constants/gas';
 import { useI18nContext } from '../../../hooks/useI18nContext';
+import { useTransactionModalContext } from '../../../contexts/transaction-modal';
 import Popover from '../../ui/popover';
 import I18nValue from '../../ui/i18n-value';
 import LoadingHeartBeat from '../../ui/loading-heartbeat';
 
 import EditGasItem from './edit-gas-item';
 
-const EditGasFeePopover = ({ onClose }) => {
+const EditGasFeePopover = () => {
   const t = useI18nContext();
+  const { currentModal, closeModal } = useTransactionModalContext();
+
+  if (currentModal !== 'editGasFee') return null;
 
   return (
     <Popover
       title={t('editGasFeeModalTitle')}
-      onClose={onClose}
+      onClose={() => closeModal('editGasFee')}
       className="edit-gas-fee-popover"
     >
       <>
@@ -33,36 +36,17 @@ const EditGasFeePopover = ({ onClose }) => {
                 <I18nValue messageKey="maxFee" />
               </span>
             </div>
-            <EditGasItem
-              priorityLevel={PRIORITY_LEVELS.LOW}
-              onClose={onClose}
-            />
-            <EditGasItem
-              priorityLevel={PRIORITY_LEVELS.MEDIUM}
-              onClose={onClose}
-            />
-            <EditGasItem
-              priorityLevel={PRIORITY_LEVELS.HIGH}
-              onClose={onClose}
-            />
+            <EditGasItem priorityLevel={PRIORITY_LEVELS.LOW} />
+            <EditGasItem priorityLevel={PRIORITY_LEVELS.MEDIUM} />
+            <EditGasItem priorityLevel={PRIORITY_LEVELS.HIGH} />
             <div className="edit-gas-fee-popover__content__separator" />
-            <EditGasItem
-              priorityLevel={PRIORITY_LEVELS.DAPP_SUGGESTED}
-              onClose={onClose}
-            />
-            <EditGasItem
-              priorityLevel={PRIORITY_LEVELS.CUSTOM}
-              onClose={onClose}
-            />
+            <EditGasItem priorityLevel={PRIORITY_LEVELS.DAPP_SUGGESTED} />
+            <EditGasItem priorityLevel={PRIORITY_LEVELS.CUSTOM} />
           </div>
         </div>
       </>
     </Popover>
   );
-};
-
-EditGasFeePopover.propTypes = {
-  onClose: PropTypes.func,
 };
 
 export default EditGasFeePopover;
