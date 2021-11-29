6037bbd8d907e432f33893735ad482df711c03aa
Alaa Hadad <alaahd@gmail.com>
2021-11-29 16:32:16 +0400

    feat: limiting transaction decoding to tx with data

--- ui/components/app/transaction-list-item-details/transaction-list-item-details.component.js
+++ ui/components/app/transaction-list-item-details/transaction-list-item-details.component.js
@@ -45,7 +45,7 @@ export default class TransactionListItemDetails extends PureComponent {
     tryReverseResolveAddress: PropTypes.func.isRequired,
     senderNickname: PropTypes.string.isRequired,
     recipientNickname: PropTypes.string,
-    transactionStatus: PropTypes.node,
+    transactionStatus: PropTypes.func,
   };
 
   state = {
@@ -262,13 +262,17 @@ export default class TransactionListItemDetails extends PureComponent {
                   isEarliestNonce={isEarliestNonce}
                 />
               </Disclosure>
-              <Disclosure title="Transaction data" size="small">
-                <TransactionDecoding
-                  title="Transaction data"
-                  to={transactionGroup.initialTransaction.txParams?.to}
-                  inputData={transactionGroup.initialTransaction.txParams?.data}
-                />
-              </Disclosure>
+              {transactionGroup.initialTransaction?.txParams?.data ? (
+                <Disclosure title="Transaction data" size="small">
+                  <TransactionDecoding
+                    title="Transaction data"
+                    to={transactionGroup.initialTransaction.txParams?.to}
+                    inputData={
+                      transactionGroup.initialTransaction.txParams?.data
+                    }
+                  />
+                </Disclosure>
+              ) : null}
             </div>
           </div>
         </div>
