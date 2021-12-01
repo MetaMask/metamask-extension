61be76c6332b458045cef1fb16c6daef1cb5a5cd
Alex <adonesky@gmail.com>
2021-12-01 13:14:20 -0600

    put new collectibles related preferences behind feature flag

--- ui/pages/settings/experimental-tab/experimental-tab.component.js
+++ ui/pages/settings/experimental-tab/experimental-tab.component.js
@@ -53,6 +53,10 @@ export default class ExperimentalTab extends PureComponent {
   }
 
   renderCollectibleDetectionToggle() {
+    if (!process.env.COLLECTIBLES_V1) {
+      return null;
+    }
+
     const { t } = this.context;
     const {
       useCollectibleDetection,
@@ -93,6 +97,9 @@ export default class ExperimentalTab extends PureComponent {
   }
 
   renderOpenSeaEnabledToggle() {
+    if (!process.env.COLLECTIBLES_V1) {
+      return null;
+    }
     const { t } = this.context;
     const { openSeaEnabled, setOpenSeaEnabled } = this.props;
 
