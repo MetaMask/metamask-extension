d9cf28b885974bb3032d87a160c5ee35d652d5c5
Dan Miller <danjm.com@gmail.com>
2021-12-02 10:59:25 -0330

    Add FEATURED_RPCS constant

--- shared/constants/network.js
+++ shared/constants/network.js
@@ -58,6 +58,8 @@ export const ETH_TOKEN_IMAGE_URL = './images/eth_logo.svg';
 export const TEST_ETH_TOKEN_IMAGE_URL = './images/black-eth-logo.svg';
 export const BNB_TOKEN_IMAGE_URL = './images/bnb.png';
 export const MATIC_TOKEN_IMAGE_URL = './images/matic-token.png';
+export const ARBITRUM_IMAGE_URL = './images/arbitrum.svg';
+export const OPTIMISM_IMAGE_URL = './images/optimism.svg';
 
 export const INFURA_PROVIDER_TYPES = [ROPSTEN, RINKEBY, KOVAN, MAINNET, GOERLI];
 
@@ -171,3 +173,39 @@ export const UNSUPPORTED_RPC_METHODS = new Set([
   // eth-json-rpc-middleware – but our UI does not support it.
   'eth_signTransaction',
 ]);
+
+export const FEATURED_RPCS = [
+  {
+    chainId: '0x89',
+    nickname: 'Polygon Mumbai',
+    rpcUrl:
+      'https://polygon-mainnet.infura.io/v3/2b6d4a83d89a438eb1b5d036788ab29c',
+    ticker: 'MATIC',
+    rpcPrefs: {
+      blockExplorerUrl: 'https://mumbai.polygonscan.com/',
+      imageUrl: MATIC_TOKEN_IMAGE_URL,
+    },
+  },
+  {
+    chainId: '0x89',
+    nickname: 'Optimism Testnet ',
+    rpcUrl:
+      'https://optimism-kovan.infura.io/v3/2b6d4a83d89a438eb1b5d036788ab29c',
+    ticker: 'KOR',
+    rpcPrefs: {
+      blockExplorerUrl: 'https://kovan-optimistic.etherscan.io/',
+      imageUrl: ARBITRUM_IMAGE_URL,
+    },
+  },
+  {
+    chainId: '0x66eeb',
+    nickname: 'Arbitrum Testnet',
+    rpcUrl:
+      'https://arbitrum-rinkeby.infura.io/v3/2b6d4a83d89a438eb1b5d036788ab29c',
+    ticker: 'ARETH',
+    rpcPrefs: {
+      blockExplorerUrl: 'https://testnet.arbiscan.io/',
+      imageUrl: OPTIMISM_IMAGE_URL,
+    },
+  },
+];
