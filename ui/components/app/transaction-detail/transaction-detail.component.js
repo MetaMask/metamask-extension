6fc9b6b7a26b68cfaa5c3c87f5f39908bf319c38
Jyoti Puri <jyotipuri@gmail.com>
2021-12-06 22:09:35 +0530

    Using EIP-1559 V2 for approve transaction flow (#12906)

--- ui/components/app/transaction-detail/transaction-detail.component.js
+++ ui/components/app/transaction-detail/transaction-detail.component.js
@@ -2,14 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { useGasFeeContext } from '../../../contexts/gasFee';
-import { useTransactionModalContext } from '../../../contexts/transaction-modal';
-import InfoTooltip from '../../ui/info-tooltip/info-tooltip';
-import Typography from '../../ui/typography/typography';
-
-import TransactionDetailItem from '../transaction-detail-item/transaction-detail-item.component';
-import { COLORS, TYPOGRAPHY } from '../../../helpers/constants/design-system';
-import { PRIORITY_LEVEL_ICON_MAP } from '../../../helpers/constants/gas';
 import { useI18nContext } from '../../../hooks/useI18nContext';
+import Box from '../../ui/box';
+
+import EditGasFeeButton from '../edit-gas-fee-button';
+import TransactionDetailItem from '../transaction-detail-item/transaction-detail-item.component';
 
 export default function TransactionDetail({
   rows = [],
@@ -17,89 +14,18 @@ export default function TransactionDetail({
   userAcknowledgedGasMissing,
 }) {
   const t = useI18nContext();
-  const {
-    gasLimit,
-    hasSimulationError,
-    estimateUsed,
-    maxFeePerGas,
-    maxPriorityFeePerGas,
-    supportsEIP1559V2,
-    transaction,
-  } = useGasFeeContext();
-  const { openModal } = useTransactionModalContext();
-
-  if (supportsEIP1559V2 && estimateUsed) {
-    const editEnabled = !hasSimulationError || userAcknowledgedGasMissing;
-    if (!editEnabled) return null;
-
-    return (
-      <div className="transaction-detail">
-        <div className="transaction-detail-edit-V2">
-          <button onClick={() => openModal('editGasFee')}>
-            <span
-              className={`transaction-detail-edit-V2__icon transaction-detail-edit-V2__icon-${estimateUsed}`}
-            >
-              {`${PRIORITY_LEVEL_ICON_MAP[estimateUsed]} `}
-            </span>
-            <span className="transaction-detail-edit-V2__label">
-              {t(estimateUsed)}
-            </span>
-            <i className="fas fa-chevron-right asset-list-item__chevron-right" />
-          </button>
-          {estimateUsed === 'custom' && (
-            <button
-              onClick={() => openModal('advancedGasFee')}
-              className="transaction-detail__edit-button"
-            >
-              {t('edit')}
-            </button>
-          )}
-          {estimateUsed === 'dappSuggested' && (
-            <InfoTooltip
-              contentText={
-                <div className="transaction-detail-edit-V2__tooltip">
-                  <Typography
-                    tag={TYPOGRAPHY.Paragraph}
-                    variant={TYPOGRAPHY.H7}
-                    color={COLORS.GREY}
-                  >
-                    {t('dappSuggestedTooltip', [transaction.origin])}
-                  </Typography>
-                  <Typography
-                    tag={TYPOGRAPHY.Paragraph}
-                    variant={TYPOGRAPHY.H7}
-                  >
-                    <strong>{t('maxBaseFee')}</strong>
-                    {maxFeePerGas}
-                  </Typography>
-                  <Typography
-                    tag={TYPOGRAPHY.Paragraph}
-                    variant={TYPOGRAPHY.H7}
-                  >
-                    <strong>{t('maxPriorityFee')}</strong>
-                    {maxPriorityFeePerGas}
-                  </Typography>
-                  <Typography
-                    tag={TYPOGRAPHY.Paragraph}
-                    variant={TYPOGRAPHY.H7}
-                  >
-                    <strong>{t('gasLimit')}</strong>
-                    {gasLimit}
-                  </Typography>
-                </div>
-              }
-              position="top"
-            />
-          )}
-        </div>
-        <div className="transaction-detail-rows">{rows}</div>
-      </div>
-    );
-  }
+  const { supportsEIP1559V2 } = useGasFeeContext();
 
   return (
     <div className="transaction-detail">
-      {onEdit && (
+      {supportsEIP1559V2 && (
+        <Box display="flex" justifyContent="flex-end" paddingTop={5}>
+          <EditGasFeeButton
+            userAcknowledgedGasMissing={userAcknowledgedGasMissing}
+          />
+        </Box>
+      )}
+      {!supportsEIP1559V2 && onEdit && (
         <div className="transaction-detail-edit">
           <button onClick={onEdit}>{t('edit')}</button>
         </div>
