c87250fb283c1b3b1214f36518ecca60da437a5e
Elliot Winkler <elliot.winkler@gmail.com>
2021-12-09 16:59:46 -0700

    Wire up priority fee range in new gas modal
    Use the `priorityFeeRange` property which is now available in the gas
    fee estimate data.

---
+++ ui/components/app/edit-gas-fee-popover/network-statistics/latest-priority-fee-field/latest-priority-fee-field.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { toBigNumber } from '../../../../../../shared/modules/conversion.utils';
+import { useGasFeeContext } from '../../../../../contexts/gasFee';
+import I18nValue from '../../../../ui/i18n-value';
+import { PriorityFeeTooltip } from '../tooltips';
+
+function uniq(values) {
+  const uniqueValues = [];
+  const seenValues = new Set();
+  values.forEach((value) => {
+    if (!seenValues.has(value)) {
+      uniqueValues.push(value);
+      seenValues.add(value);
+    }
+  });
+  return uniqueValues;
+}
+
+function roundToDecimalPlacesRemovingExtraZeroes(
+  numberish,
+  numberOfDecimalPlaces,
+) {
+  return toBigNumber.dec(
+    toBigNumber.dec(numberish).toFixed(numberOfDecimalPlaces),
+  );
+}
+
+export default function LatestPriorityFeeField() {
+  const { gasFeeEstimates } = useGasFeeContext();
+
+  const renderPriorityFeeRange = () => {
+    const { latestPriorityFeeRange } = gasFeeEstimates;
+    if (latestPriorityFeeRange) {
+      const formattedRange = uniq(
+        latestPriorityFeeRange.map((priorityFee) =>
+          roundToDecimalPlacesRemovingExtraZeroes(priorityFee, 1),
+        ),
+      ).join(' - ');
+      return `${formattedRange} GWEI`;
+    }
+    return null;
+  };
+
+  return (
+    <div className="network-statistics__info__field latest-priority-fee-field">
+      <span className="network-statistics__info__field-data">
+        <PriorityFeeTooltip>{renderPriorityFeeRange()}</PriorityFeeTooltip>
+      </span>
+      <span className="network-statistics__info__field-label">
+        <I18nValue messageKey="priorityFee" />
+      </span>
+    </div>
+  );
+}
