c9cb2ae1a1190346a2ae9698f79b2927bbc68de9
Thomas Huang <tmashuang@users.noreply.github.com>
2021-12-10 11:25:58 -0600

    Jestify app/scripts/controller/network/**/*.test.js (#12985)

--- jest.config.js
+++ jest.config.js
@@ -32,6 +32,7 @@ module.exports = {
     '<rootDir>/app/scripts/lib/**/*.test.js',
     '<rootDir>/app/scripts/migrations/*.test.js',
     '<rootDir>/app/scripts/platforms/*.test.js',
+    '<rootDir>app/scripts/controllers/network/**/*.test.js',
     '<rootDir>/app/scripts/controllers/permissions/*.test.js',
   ],
   testTimeout: 2500,
