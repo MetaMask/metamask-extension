4fdf0abcd833579ab7219791cc02c18ab473c0d8
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2021-12-15 17:40:42 -0500

    adding the theme to the compoenentwillmount

--- ui/pages/routes/routes.component.js
+++ ui/pages/routes/routes.component.js
@@ -118,6 +118,7 @@ export default class Routes extends Component {
       pageChanged,
       setCurrentCurrencyToUSD,
       history,
+      theme,
     } = this.props;
     if (!currentCurrency) {
       setCurrentCurrencyToUSD();
@@ -128,6 +129,9 @@ export default class Routes extends Component {
         pageChanged(locationObj.pathname);
       }
     });
+    if (process.env.DARK_MODE_V1 && theme) {
+      document.documentElement.classList.add(`theme-${theme}`);
+    }
   }
 
   renderRoutes() {
