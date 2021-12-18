0a362737bba932e81b88938219a3efe523f0f8e1
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2021-12-18 13:57:39 -0800

    Add Snaps network access permission (#13119)
    Adds the ability for Snaps to access the Internet via the `endowment:network-access` permission. Also adds permission icons, renames permission locale message keys, and fixes Snap permission locale message retrieval. As part of fixing Snap permission locale messages, adds `@metamask/slip44` as a production dependency in order to display the English names of SLIP-44 protocols in our UI.

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -572,7 +572,7 @@ export default class MetamaskController extends EventEmitter {
     });
 
     this.snapController = new SnapController({
-      endowmentPermissionNames: Object.keys(EndowmentPermissions),
+      endowmentPermissionNames: Object.values(EndowmentPermissions),
       terminateAllSnaps: this.workerController.terminateAllSnaps.bind(
         this.workerController,
       ),
