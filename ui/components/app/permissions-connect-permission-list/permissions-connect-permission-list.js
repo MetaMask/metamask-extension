a0a66bf331b34e4e509a0a38038ec1c7cdbbad51
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2021-12-18 13:57:39 -0800

    Add Snaps network access permission (#13119)
    Adds the ability for Snaps to access the Internet via the `endowment:network-access` permission. Also adds permission icons, renames permission locale message keys, and fixes Snap permission locale message retrieval. As part of fixing Snap permission locale messages, adds `@metamask/slip44` as a production dependency in order to display the English names of SLIP-44 protocols in our UI.

--- ui/components/app/permissions-connect-permission-list/permissions-connect-permission-list.js
+++ ui/components/app/permissions-connect-permission-list/permissions-connect-permission-list.js
@@ -1,72 +1,116 @@
 import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
+import {
+  RestrictedMethods,
+  ///: BEGIN:ONLY_INCLUDE_IN(flask)
+  EndowmentPermissions,
+  PermissionNamespaces,
+  ///: END:ONLY_INCLUDE_IN
+} from '../../../../shared/constants/permissions';
 import { useI18nContext } from '../../../hooks/useI18nContext';
+///: BEGIN:ONLY_INCLUDE_IN(flask)
+import { coinTypeToProtocolName } from '../../../helpers/utils/util';
+///: END:ONLY_INCLUDE_IN
 
 const UNKNOWN_PERMISSION = Symbol('unknown');
+
+/**
+ * @typedef {Object} PermissionLabelObject
+ * @property {string} label - The text label.
+ * @property {string} leftIcon - The left icon.
+ * @property {string} rightIcon - The right icon.
+ */
+
+/**
+ * Gets the permission list label dictionary key for the specified permission
+ * name.
+ *
+ * @param {string} permissionName - The name of the permission whose key to
+ * retrieve.
+ * @param {Record<string, PermissionLabelObject>} permissionDictionary - The
+ * dictionary object mapping permission keys to label objects.
+ */
+function getPermissionKey(permissionName, permissionDictionary) {
+  if (Object.hasOwnProperty.call(permissionDictionary, permissionName)) {
+    return permissionName;
+  }
+  ///: BEGIN:ONLY_INCLUDE_IN(flask)
+  for (const namespace of Object.keys(PermissionNamespaces)) {
+    if (permissionName.startsWith(namespace)) {
+      return PermissionNamespaces[namespace];
+    }
+  }
+  ///: END:ONLY_INCLUDE_IN
+
+  return UNKNOWN_PERMISSION;
+}
 
 export default function PermissionsConnectPermissionList({ permissions }) {
   const t = useI18nContext();
 
-  // TODO:flask Fix Snap permission labels and support namespaced permissions
-  const PERMISSION_TYPES = useMemo(() => {
+  const PERMISSION_LIST_VALUES = useMemo(() => {
     return {
-      eth_accounts: {
+      [RestrictedMethods.eth_accounts]: {
         leftIcon: 'fas fa-eye',
-        label: t('eth_accounts'),
+        label: t('permission_ethereumAccounts'),
         rightIcon: null,
       },
       ///: BEGIN:ONLY_INCLUDE_IN(flask)
-      snap_confirm: {
-        leftIcon: 'fas fa-eye',
-        label: 'snap_confirm',
+      [RestrictedMethods.snap_confirm]: {
+        leftIcon: 'fas fa-user-check',
+        label: t('permission_customConfirmation'),
         rightIcon: null,
       },
-      // TODO: We need a function to pass substitutions to `t` for this.
-      'snap_getBip44Entropy_*': {
-        leftIcon: 'fas fa-eye',
-        label: 'snap_getBip44Entropy_*',
+      [RestrictedMethods['snap_getBip44Entropy_*']]: (permissionName) => {
+        const coinType = permissionName.split('_').slice(-1);
+        return {
+          leftIcon: 'fas fa-door-open',
+          label: t('permission_manageBip44Keys', [
+            coinTypeToProtocolName(coinType) ||
+              `${coinType} (Unrecognized protocol)`,
+          ]),
+          rightIcon: null,
+        };
+      },
+      [RestrictedMethods.snap_manageState]: {
+        leftIcon: 'fas fa-download',
+        label: t('permission_manageState'),
         rightIcon: null,
       },
-      snap_manageState: {
-        leftIcon: 'fas fa-eye',
-        label: 'snap_manageState',
-        rightIcon: null,
+      [RestrictedMethods['wallet_snap_*']]: (permissionName) => {
+        const snapId = permissionName.split('_').slice(-1);
+        return {
+          leftIcon: 'fas fa-bolt',
+          label: t('permission_accessSnap', [snapId]),
+          rightIcon: null,
+        };
       },
-      'wallet_snap_*': {
-        leftIcon: 'fas fa-eye',
-        label: 'wallet_snap_*',
+      [EndowmentPermissions['endowment:network-access']]: {
+        leftIcon: 'fas fa-wifi',
+        label: t('permission_accessNetwork'),
         rightIcon: null,
       },
       ///: END:ONLY_INCLUDE_IN
-      [UNKNOWN_PERMISSION]: {
-        leftIcon: null,
-        label: 'Unknown Permission',
-        rightIcon: null,
+      [UNKNOWN_PERMISSION]: (permissionName) => {
+        return {
+          leftIcon: 'fas fa-times-circle',
+          label: t('permission_unknown', [permissionName ?? 'undefined']),
+          rightIcon: null,
+        };
       },
     };
   }, [t]);
 
-  function getPermissionKey(permissionName) {
-    if (PERMISSION_TYPES[permissionName]) {
-      return permissionName;
-    }
-    ///: BEGIN:ONLY_INCLUDE_IN(flask)
-    else if (permissionName.startsWith('wallet_snap_')) {
-      return 'wallet_snap_*';
-    } else if (permissionName.startsWith('snap_getBip44Entropy_')) {
-      return 'snap_getBip44Entropy_*';
-    }
-    ///: END:ONLY_INCLUDE_IN
-
-    return UNKNOWN_PERMISSION;
-  }
-
   return (
     <div className="permissions-connect-permission-list">
       {Object.keys(permissions).map((permission) => {
-        const { label, leftIcon, rightIcon } = PERMISSION_TYPES[
-          getPermissionKey(permission)
-        ];
+        const listValue =
+          PERMISSION_LIST_VALUES[
+            getPermissionKey(permission, PERMISSION_LIST_VALUES)
+          ];
+
+        const { label, leftIcon, rightIcon } =
+          typeof listValue === 'function' ? listValue(permission) : listValue;
 
         return (
           <div className="permission" key={label}>
