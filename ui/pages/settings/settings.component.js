38da2954289fd6152b7ab5c5b76d754b42ceeb43
hmalik88 <hassan.malik@consensys.net>
2021-12-21 13:20:42 -0500

    removed condition for settings title

--- ui/pages/settings/settings.component.js
+++ ui/pages/settings/settings.component.js
@@ -135,7 +135,7 @@ class SettingsPage extends PureComponent {
       titleText = t('snaps');
     } else if (isPopup && addressName) {
       titleText = t('details');
-    } else if (pathnameI18nKey && pathnameI18nKey !== 'snaps' && isPopup) {
+    } else if (pathnameI18nKey && isPopup) {
       titleText = t(pathnameI18nKey);
     } else {
       titleText = t('settings');
