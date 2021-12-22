bf56af5ddfcf5ba7ab4767dbcd8b5b33f69bbded
jpuri <jyotipuri@gmail.com>
2021-12-22 13:23:51 +0530

    fix build

--- ui/components/app/transaction-list-item/transaction-list-item.component.js
+++ ui/components/app/transaction-list-item/transaction-list-item.component.js
@@ -218,12 +218,12 @@ function TransactionListItemInner({
       >
         <div className="transaction-list-item__pending-actions">
           {speedUpButton}
-          {showCancelButton && (
-            <CancelButton
-              transaction={transactionGroup.primaryTransaction}
-              cancelTransaction={cancelTransaction}
-            />
-          )}
+          {/* {showCancelButton && ( */}
+          <CancelButton
+            transaction={transactionGroup.primaryTransaction}
+            cancelTransaction={cancelTransaction}
+          />
+          {/* )} */}
         </div>
       </ListItem>
       {showDetails && (
