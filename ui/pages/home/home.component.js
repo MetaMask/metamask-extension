b4757af171d3a2cb201968b0889a638d4c883086
Mark Stacey <markjstacey@gmail.com>
2021-12-22 17:29:48 -0330

    Simplify the `removeSnapError` function (#13145)
    The `removeSnapError` function has been changed from an action creator
    to a normal function, as the only actions dispatched were unnecessary.
    It was dispatching actions to update the `metamask` state, but the
    background state will automatically update on its own.
    
    It also uses `async/await` rather than `.then` now, and the callsites
    have been updated to use `async/await`. Platforms that support async
    stack traces rely upon `async/await` for that enhancement to errors, so
    this will result in better stack traces on those platforms.

--- ui/pages/home/home.component.js
+++ ui/pages/home/home.component.js
@@ -267,8 +267,8 @@ export default class Home extends PureComponent {
                         </Typography>
                       </>
                     }
-                    onIgnore={() => {
-                      removeSnapError(errorId);
+                    onIgnore={async () => {
+                      await removeSnapError(errorId);
                     }}
                     ignoreText="Dismiss"
                     key="home-error-message"
