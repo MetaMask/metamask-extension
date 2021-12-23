bde8b3ba8787d1f2a3d81309c4b882ab8986b6a3
Mark Stacey <markjstacey@gmail.com>
2021-12-22 20:52:10 -0330

    Remove stray console.log

--- ui/pages/settings/flask/view-snap/view-snap.js
+++ ui/pages/settings/flask/view-snap/view-snap.js
@@ -31,7 +31,6 @@ function ViewSnap() {
   const pathNameTail = pathname.match(/[^/]+$/u)[0];
   const snaps = useSelector(getSnaps);
   const connectedSubjects = useSelector(getSubjectsWithPermission);
-  console.log(connectedSubjects);
   const snap = snaps
     ? Object.entries(snaps)
         .map(([_, snapState]) => snapState)
