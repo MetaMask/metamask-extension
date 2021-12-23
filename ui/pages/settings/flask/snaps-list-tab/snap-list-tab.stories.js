d5c7e7088b3dee99af0c1b4a997fcf9269467698
Hassan Malik <41640681+hmalik88@users.noreply.github.com>
2021-12-22 17:57:59 -0800

    Snaps settings page (#13114)
    * Add snaps settings page
    
    Fix WIP test-data to use object format
    
    Added snap actions to hook up to background
    
    updated pathname key for snaps view page
    
    added boolean to not render subheader in settings for snaps view page, added settings subheader class to snaps view page
    
    updated locale messages, renamed snap prop to currentSnap, added in title & subtitle and placeholder text for when there are no snaps
    
    added icons folder and included a verified icon for flask
    
    updated snaps test data
    
    updated keys per prettier
    
    made authorship pill self enclosing, added box around pill to introduce padding
    
    added scss file for view-snap and added that to settings page scss file
    
    added isSnapViewPage prop and conditionally display subheader and title
    
    updated story to include backRoute and isSnapViewPage props
    
    fixed spacing
    
    added class to subheader and updated description to use correct color
    
    update test data
    
    updated key for permission div
    
    updated view snap styling
    
    updated view snap to have multiple sections encapsulated by box component
    
    updated locale messages with snapAccess key
    
    added translation call around snap_confirm
    
    added subtext for permissions list section
    
    formatted w/prettier
    
    * linter fixes
    
    * made fixes per comments
    
    * moved components into flask directory and updated imports
    
    * updated remove snap section
    
    * added connected sites list
    
    * lint fixes
    
    * style-lint fix
    
    * changed from snap.name -> snap.id
    
    * fixes per comments, updated test data and used appropriate properties for snaps
    
    * updated dispatch call
    
    * removed unnecessary args
    
    * Fixed some props and action issues with snaps settings page (#13118)
    
    * used idx as key for permission
    
    * added snaps key to messages
    
    * added snaps settings description key
    
    * added code fence
    
    * another code fence
    
    * updated scss, test data, used correct action for onDisconnect
    
    * fixes
    
    * removed memoization in snaps settings card, added action to remove snap permissions from all subjects
    
    * updated padding left and right to be 18px
    
    * added tooltip to settings card
    
    * fixed settings routing issue and added another tooltip to the snaps settings card
    
    * removed padding
    
    * removed condition for settings title
    
    * fixed view-snap page
    
    * style lint fixes
    
    * fixes per comments
    
    * updated actions, moved view snap out of snap-list-tab, updated containers
    
    * style lint fixes
    
    * final fixes
    
    * last fixes
    
    * Remove stray console.log
    
    * Update actions to use convenience function
    
    * Pass `permissionName` to `getSubjectsWithPermission` selector
    
    * Make `removeSnap` a synchronous function
    
    Action creators must be synchronous. The function they return can be
    async though.
    
    * Move navigation after removing snap to `useEffect` hook
    
    The UI would crash after removing a snap because the snap metadata
    object for the current page was no longer present. Instead a
    `useEffect` hook is used to navigate away from the page when the snap
    is removed.
    
    This has the added benefit of ensuring the page doesn't crash when the
    snap is removed in another tab as well.
    
    * Fix lint error accidentally introduced when rebasing
    
    Co-authored-by: shanejonas <jonas.shane@gmail.com>
    Co-authored-by: Mark Stacey <markjstacey@gmail.com>

---
+++ ui/pages/settings/flask/snaps-list-tab/snap-list-tab.stories.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { Provider } from 'react-redux';
+import configureStore from '../../../../store/store';
+import testData from '../../../../../.storybook/test-data';
+import SnapListTab from './snap-list-tab';
+
+// Using Test Data For Redux
+const store = configureStore(testData);
+
+export default {
+  title: 'Pages/Settings/SnapListTab',
+  id: __filename,
+  decorators: [(story) => <Provider store={store}>{story()}</Provider>],
+  argTypes: {
+    onToggle: {
+      action: 'onToggle',
+    },
+    onRemove: {
+      action: 'onRemove',
+    },
+  },
+};
+export const DefaultStory = (args) => {
+  const state = store.getState();
+  const [viewingSnap, setViewingSnap] = useState();
+  const [snap, setSnap] = useState();
+
+  return (
+    <div>
+      <SnapListTab
+        {...args}
+        snaps={state.metamask.snaps}
+        viewingSnap={viewingSnap}
+        currentSnap={snap}
+        onToggle={args.onToggle}
+        onRemove={args.onRemove}
+        onClick={(_, s) => {
+          setSnap(s);
+          setViewingSnap(true);
+        }}
+      />
+    </div>
+  );
+};
+const state = store.getState();
+DefaultStory.args = {
+  snaps: state.metamask.snaps,
+  viewingSnap: false,
+};
+DefaultStory.storyName = 'Default';
