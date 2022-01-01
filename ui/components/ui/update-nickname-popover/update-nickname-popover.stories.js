5caf6053e27247b9e8ed2663b850717cc4df415b
Benjamin Bourgeois <benjishift@gmail.com>
2022-01-01 14:03:29 +0100

    UpdateNicknamePopover stories : convert knobs and actions to controls / args

--- ui/components/ui/update-nickname-popover/update-nickname-popover.stories.js
+++ ui/components/ui/update-nickname-popover/update-nickname-popover.stories.js
@@ -1,32 +1,26 @@
 import React, { useState } from 'react';
-import { action } from '@storybook/addon-actions';
-import { text } from '@storybook/addon-knobs';
 import Button from '../button';
+import README from './README.mdx';
 import UpdateNicknamePopover from '.';
 
 export default {
   title: 'Components/UI/UpdateNickname',
   id: __filename,
+  parameters: {
+    docs: {
+      page: README,
+    },
+  },
+  argTypes: {
+    address: { control: 'text' },
+    onClose: { action: 'Close Update Nickname Popover' },
+    onAdd: { action: 'Submit Nickname' },
+  },
 };
 
-export const AddNickname = () => {
-  const [showPopover, setShowPopover] = useState(false);
-  return (
-    <div style={{ width: '600px' }}>
-      <Button onClick={() => setShowPopover(true)}>
-        Open Add Nickname Popover
-      </Button>
-      {showPopover && (
-        <UpdateNicknamePopover
-          address={text('address', '0x0011244f50ff4')}
-          onClose={() => action(`Close Update Nickname Popover`)()}
-        />
-      )}
-    </div>
-  );
-};
+const address = '0x0011244f50ff4';
 
-export const UpdateNickname = () => {
+export const DefaultStory = (args) => {
   const [showPopover, setShowPopover] = useState(false);
   return (
     <div style={{ width: '600px' }}>
@@ -35,11 +29,33 @@ export const UpdateNickname = () => {
       </Button>
       {showPopover && (
         <UpdateNicknamePopover
-          address={text('address', '0x0011244f50ff4')}
-          nickname={text('nickname', 'user_nickname')}
-          onClose={() => action(`Close Update Nickname Popover`)()}
+          {...args}
+          nickname="user_nickname"
+          memo="This is a memo"
         />
       )}
     </div>
   );
+};
+
+DefaultStory.storyName = 'UpdateNickname';
+
+DefaultStory.args = {
+  address,
+};
+
+export const AddNickname = (args) => {
+  const [showPopover, setShowPopover] = useState(false);
+  return (
+    <div style={{ width: '600px' }}>
+      <Button onClick={() => setShowPopover(true)}>
+        Open Add Nickname Popover
+      </Button>
+      {showPopover && <UpdateNicknamePopover {...args} />}
+    </div>
+  );
+};
+
+AddNickname.args = {
+  address,
 };
