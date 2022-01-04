75e8b4f8b92d4cdfd43dae97f073b0594b24eb65
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2022-01-04 12:57:18 -0800

    Add missing permissions middleware hook (#13205)
    Adds a missing middleware hook for `wallet_requestPermissions` that we failed to add in #12243. Also adds a runtime check that throws an error if any expected hooks are not provided to `createMethodMiddleware`.
    
    Co-authored-by: Mark Stacey <markjstacey@gmail.com>

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -2759,6 +2759,10 @@ export default class MetamaskController extends EventEmitter {
           { origin },
           { eth_accounts: {} },
         ),
+        requestPermissionsForOrigin: this.permissionController.requestPermissions.bind(
+          this.permissionController,
+          { origin },
+        ),
 
         // Custom RPC-related
         addCustomRpc: async ({
