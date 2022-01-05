6d05aebb3420cf85100ed8eb549c9aae106f0e78
Filip Sekulic <filip.sekulic@consensys.net>
2022-01-05 12:20:53 +0100

    More CSS added

--- ui/components/ui/new-network-info/new-network-info.component.js
+++ ui/components/ui/new-network-info/new-network-info.component.js
@@ -4,12 +4,25 @@ import { I18nContext } from '../../../contexts/i18n';
 import Popover from '../popover';
 import Button from '../button';
 import Identicon from '../identicon/identicon.component';
+import I18nValue from '../i18n-value';
+import { getProvider, getUseTokenDetection } from '../../../selectors';
+import { useSelector } from 'react-redux';
 
-const NewNetworkInfo = ({}) => {
+const NewNetworkInfo = ({
+  featuredRPC
+}) => {
   const t = useContext(I18nContext);
 
+  const provider = useSelector(getProvider);
+  const tokenDetection = useSelector(getUseTokenDetection);
+  console.log(tokenDetection);
+
   return (
-    <Popover className="new-network-info__wrapper">
+    <Popover className="new-network-info__wrapper" footer={
+      <Button type="primary" onClick={() => {}}>
+      {t('recoveryPhraseReminderConfirm')}
+    </Button>
+    }>
       <div className="new-network-info__close">
         <img
           src="./images/times.svg"
@@ -20,45 +33,56 @@ const NewNetworkInfo = ({}) => {
       </div>
       <div className="new-network-info__title">{t('switchedTo')}</div>
       <div className="new-network-info__ident-section">
-        <Identicon className="new-network-info__ident-section" diameter={14} />{' '}
-        name
+        {featuredRPC.rpcPrefs.imageUrl ? 
+          (
+          <Identicon image={featuredRPC.rpcPrefs.imageUrl} diameter={14} />
+          ) : (
+            <i className="fa fa-question-circle"/>
+            )} 
+        <label className="new-network-info__ident-section__nickname">{featuredRPC.nickname}</label>
       </div>
       <div className="new-network-info__subtitle">{t('thingsToKeep')}</div>
       <div className="new-network-info__content">
+        {featuredRPC.ticker ? 
+        (
         <div className="new-network-info__content__content-box-1">
           <div className="new-network-info__content__content-box-1__serial-number-1">
-            {t('onboardingPinExtensionStep1')}.
+            &bull;
           </div>
           <div className="new-network-info__content__content-box-1__text-1">
-            {t('nativeToken')}
+            <I18nValue messageKey="nativeToken" options={[<label style={{ fontWeight: '700'}}>{featuredRPC.ticker}</label>]} />
           </div>
         </div>
+        ) : null }
         <div className="new-network-info__content__content-box-1">
           <div className="new-network-info__content__content-box-1__serial-number-1">
-            {t('onboardingPinExtensionStep2')}.
+            &bull;
           </div>
           <div className="new-network-info__content__content-box-1__text-1">
-            {t('attemptSendingAssets')}
+            {t('attemptSendingAssets')} <a href="https://metamask.zendesk.com/hc/en-us/articles/4404424659995" className="new-network-info__content__content-box-1__link-1">{t('learnMoreUpperCase')}</a>
           </div>
         </div>
         <div className="new-network-info__content__content-box-2">
           <div className="new-network-info__content__content-box-1__serial-number-1">
-            {t('onboardingPinExtensionStep3')}.
+            &bull;
           </div>
           <div className="new-network-info__content__content-box-1__text-1">
-            {t('tokenShowUp')} {t('clickToManuallyAdd')}
+            {t('tokenShowUp')} <a href="" className="new-network-info__content__content-box-1__link-1">{t('clickToManuallyAdd')}</a>
           </div>
         </div>
+
       </div>
-      <div className="new-network-info__footer">
+      {/* <div className="new-network-info__footer">
         <Button type="primary" onClick={() => {}}>
           {t('recoveryPhraseReminderConfirm')}
         </Button>
-      </div>
+      </div> */}
     </Popover>
   );
 };
 
-NewNetworkInfo.propTypes = {};
+NewNetworkInfo.propTypes = {
+  featuredRPC: PropTypes.object,
+};
 
 export default NewNetworkInfo;
--- ui/components/ui/new-network-info/new-network-info.stories.js
+++ ui/components/ui/new-network-info/new-network-info.stories.js
@@ -7,7 +7,21 @@ export default {
 };
 
 export const DefaultStory = () => {
-  return <NewNetworkInfo />;
+
+  const featuredRPC =   {
+    chainId: '0x89',
+    nickname: 'Polygon Mumbai',
+    rpcUrl:
+      'https://polygon-mainnet.infura.io/v3/2b6d4a83d89a438eb1b5d036788ab29c',
+    ticker: 'MATIC',
+    rpcPrefs: {
+      blockExplorerUrl: 'https://mumbai.polygonscan.com/',
+      imageUrl: './images/matic-token.png',
+    },
+  }
+
+
+  return <NewNetworkInfo featuredRPC={featuredRPC} />;
 };
 
 DefaultStory.storyName = 'Default';
