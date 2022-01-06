0dae8ebf4c565545b80a1a50ba828da7403422b8
VSaric <vladimir.saric@consensys.net>
2022-01-06 13:51:26 +0100

    SendHeader, ContactListTab and AdvancedTab story: converted knobs and actions to controls / args

--- ui/pages/settings/advanced-tab/advanced-tab.stories.js
+++ ui/pages/settings/advanced-tab/advanced-tab.stories.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import { text, boolean } from '@storybook/addon-knobs';
 import AdvancedTab from './advanced-tab.component';
 
 export default {
   title: 'Pages/Settings/AdvancedTab',
   id: __filename,
+  argTypes: {
+    warning: { control: 'text' },
+    useNonceField: { control: 'boolean' },
+    sendHexData: { control: 'boolean' },
+    advancedInlineGas: { control: 'boolean' },
+    showFiatInTestnets: { control: 'boolean' },
+    threeBoxSyncingAllowed: { control: 'boolean' },
+    threeBoxDisabled: { control: 'boolean' },
+    useLedgerLive: { control: 'boolean' },
+    dismissSeedBackUpReminder: { control: 'boolean' },
+  },
 };
 
-export const DefaultStory = () => {
+export const DefaultStory = (args) => {
   return (
     <div style={{ flex: 1, height: 500 }}>
       <AdvancedTab
@@ -24,25 +34,22 @@ export const DefaultStory = () => {
         history={{ push: () => undefined }}
         showResetAccountConfirmationModal={() => undefined}
         setAdvancedInlineGasFeatureFlag={() => undefined}
-        warning={text('Warning', 'Warning Sample')}
         ipfsGateway="ipfs-gateway"
-        useNonceField={boolean('Customize Transaction Nonce', false)}
-        sendHexData={boolean('Show Hex Data', false)}
-        advancedInlineGas={boolean('Advanced Inline Gas', false)}
-        showFiatInTestnets={boolean('Show Conversion on Testnets', false)}
-        threeBoxSyncingAllowed={boolean(
-          'Sync data with 3Box (experimental)',
-          false,
-        )}
-        threeBoxDisabled={boolean('3Box Disabled', false)}
-        useLedgerLive={boolean('Use Ledger Live', false)}
-        dismissSeedBackUpReminder={boolean(
-          'Dismiss recovery phrase backup reminder',
-          false,
-        )}
+        {...args}
       />
     </div>
   );
 };
 
 DefaultStory.storyName = 'Default';
+DefaultStory.args = {
+  warning: 'Warning Sample',
+  useNonceField: false,
+  sendHexData: false,
+  advancedInlineGas: false,
+  showFiatInTestnets: false,
+  threeBoxSyncingAllowed: false,
+  threeBoxDisabled: false,
+  useLedgerLive: false,
+  dismissSeedBackUpReminder: false,
+};
