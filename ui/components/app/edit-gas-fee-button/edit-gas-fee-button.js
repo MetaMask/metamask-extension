9ee042730f590850f77fe24a09479f2e9beaf43a
jpuri <jyotipuri@gmail.com>
2022-01-06 21:11:27 +0530

    UI change on approve token page

--- ui/components/app/edit-gas-fee-button/edit-gas-fee-button.js
+++ ui/components/app/edit-gas-fee-button/edit-gas-fee-button.js
@@ -64,7 +64,7 @@ export default function EditGasFeeButton({ userAcknowledgedGasMissing }) {
         <InfoTooltip
           contentText={
             <div className="edit-gas-fee-button__tooltip">
-              <Typography variant={TYPOGRAPHY.H7} color={COLORS.GREY}>
+              <Typography variant={TYPOGRAPHY.H7} color={COLORS.NEUTRAL_GREY}>
                 {t('dappSuggestedTooltip', [transaction.origin])}
               </Typography>
               <Typography variant={TYPOGRAPHY.H7}>
