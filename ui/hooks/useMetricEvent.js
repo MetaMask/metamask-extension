b820ef131b06d606c9e049d0dfada0d042bdc7ff
Brad Decker <git@braddecker.dev>
2022-01-12 13:31:54 -0600

    Implement event fragments (#12251)

--- ui/hooks/useMetricEvent.js
+++ ui/hooks/useMetricEvent.js
@@ -1,6 +1,10 @@
 import { useContext, useCallback } from 'react';
+import { useSelector } from 'react-redux';
+import { useRouteMatch } from 'react-router-dom';
 import { MetaMetricsContext } from '../contexts/metametrics';
 import { MetaMetricsContext as NewMetaMetricsContext } from '../contexts/metametrics.new';
+import { PATH_NAME_MAP } from '../helpers/constants/routes';
+import { txDataSelector } from '../selectors';
 import { useEqualityCheck } from './useEqualityCheck';
 
 // Type imports
@@ -37,4 +41,46 @@ export function useNewMetricEvent(payload, options) {
     memoizedPayload,
     memoizedOptions,
   ]);
+}
+
+const PATHS_TO_CHECK = Object.keys(PATH_NAME_MAP);
+
+/**
+ * Returns the current page if it matches our route map, as well as the origin
+ * if there is a confirmation that was triggered by a dapp. These values are
+ * not required but add valuable context to events, and should be included in
+ * the context object on the event payload.
+ *
+ * @returns {{
+ *  page?: MetaMetricsPageObject
+ *  referrer?: MetaMetricsReferrerObject
+ * }}
+ */
+export function useMetaMetricsContext() {
+  const match = useRouteMatch({
+    path: PATHS_TO_CHECK,
+    exact: true,
+    strict: true,
+  });
+  const txData = useSelector(txDataSelector) || {};
+  const confirmTransactionOrigin = txData.origin;
+
+  const referrer = confirmTransactionOrigin
+    ? {
+        url: confirmTransactionOrigin,
+      }
+    : undefined;
+
+  const page = match
+    ? {
+        path: match.path,
+        title: PATH_NAME_MAP[match.path],
+        url: match.path,
+      }
+    : undefined;
+
+  return {
+    page,
+    referrer,
+  };
 }
