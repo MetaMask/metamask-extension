8837c9fc4efbf9ed82ad3981f3321ad80c2c146b
Mirko Garozzo <mirkogarozzo@hotmail.it>
2022-01-18 14:25:38 +0100

    Linted

--- ui/components/app/account-menu/keyring-label.js
+++ ui/components/app/account-menu/keyring-label.js
@@ -1,46 +1,40 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import { useI18nContext } from '../../../hooks/useI18nContext';
 import { KEYRING_TYPES } from '../../../../shared/constants/hardware-wallets';
 
 export default function KeyRingLabel(props) {
-    const t = useI18nContext();
-  
-    const {keyring} = props;
+  const t = useI18nContext();
 
-    const [label, setLabel] = useState(null);
+  const { keyring } = props;
 
-    // Effect #1 Sets keyring label
-    useEffect(() => {
-        // Keyring value might take a while to get a value
-        if(keyring) {   
-            const { type } = keyring;
-            if(type === KEYRING_TYPES.QR) {
-                setLabel('QR');
-            }
-            else if(type === 'Simple Key Pair') {
-                setLabel(t('imported'));
-            }
-            else if(type === KEYRING_TYPES.TREZOR) {
-                setLabel('Trezor');
-            }
-            else if(type === KEYRING_TYPES.LEDGER) {
-                setLabel('Ledger');
-            }
-            else if(type === KEYRING_TYPES.LATTICE) {
-                setLabel('Lattice1')
-            }
-        }
-    }, [props.keyring])
+  const [label, setLabel] = useState(null);
+
+  // Effect #1 Sets keyring label
+  useEffect(() => {
+    // Keyring value might take a while to get a value
+    if (keyring) {
+      const { type } = keyring;
+      if (type === KEYRING_TYPES.QR) {
+        setLabel('QR');
+      } else if (type === 'Simple Key Pair') {
+        setLabel(t('imported'));
+      } else if (type === KEYRING_TYPES.TREZOR) {
+        setLabel('Trezor');
+      } else if (type === KEYRING_TYPES.LEDGER) {
+        setLabel('Ledger');
+      } else if (type === KEYRING_TYPES.LATTICE) {
+        setLabel('Lattice1');
+      }
+    }
+  }, [props.keyring]);
 
   return (
-      <>
-        {label ? <div className="keyring-label allcaps">{label}</div> : null}
-      </>
+    <>{label ? <div className="keyring-label allcaps">{label}</div> : null}</>
   );
 }
 
 KeyRingLabel.propTypes = {
-  keyring: PropTypes.object
+  keyring: PropTypes.object,
 };
