d5f3142009ed3d06139a222fb472fe1262a5319c
Erik Marks <rekmarks@protonmail.com>
2022-01-20 15:18:04 -0800

    Remove useless comment

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -3211,7 +3211,6 @@ export default class MetamaskController extends EventEmitter {
           this.snapController,
           origin,
         ),
-        // TODO:flask Fix in skunkworks (rpc-methods)
         requestPermissions: async (requestedPermissions) => {
           const [
             approvedPermissions,
