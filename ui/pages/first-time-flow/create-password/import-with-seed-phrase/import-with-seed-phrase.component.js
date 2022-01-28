857c50ae56c95731bfac8ed222adc91584d7c02f
Mark Stacey <markjstacey@gmail.com>
2022-01-28 16:34:34 -0330

    Extract "create vault" form to separate component
    The form used for creating a vault on the "Import" page of onboarding
    and on the "Restore vault" page is nearly identical, yet the
    implementation is totally separate. It has now been extracted to a
    separate component, consolidating the two implementations.
    
    There is a "terms of use" checkbox on the import page that isn't on the
    restore vault page, so that part has been made optional. The "submit"
    button text differs between the two uses as well, so that is
    customizable.
    
    There are slight styling differences between the old and new versions
    of this form. The fonts and spacing are all using our new standard
    design system guidelines, and we're using our standard checkbox now as
    well. The spacing and font sizes were chosen somewhat arbitrarily by me
    to resemble the old styles, so please feel free to suggest changes if
    you think they can be improved upon.
    
    There are some slight copy changes to the "Restore vault" page as well;
    the placeholder text and the label for the "Secret Recovery Phrase"
    field now matches the "Import" page copy.

--- ui/pages/first-time-flow/create-password/import-with-seed-phrase/import-with-seed-phrase.component.js
+++ ui/pages/first-time-flow/create-password/import-with-seed-phrase/import-with-seed-phrase.component.js
@@ -1,15 +1,10 @@
-import { ethers } from 'ethers';
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import TextField from '../../../../components/ui/text-field';
-import Button from '../../../../components/ui/button';
 import {
   INITIALIZE_SELECT_ACTION_ROUTE,
   INITIALIZE_END_OF_FLOW_ROUTE,
 } from '../../../../helpers/constants/routes';
-import { clearClipboard } from '../../../../helpers/utils/util';
-
-const { isValidMnemonic } = ethers.utils;
+import CreateNewVault from '../../../../components/app/create-new-vault';
 
 export default class ImportWithSeedPhrase extends PureComponent {
   static contextTypes = {
@@ -23,20 +18,6 @@ export default class ImportWithSeedPhrase extends PureComponent {
     setSeedPhraseBackedUp: PropTypes.func,
     initializeThreeBox: PropTypes.func,
   };
-
-  state = {
-    seedPhrase: '',
-    showSeedPhrase: false,
-    password: '',
-    confirmPassword: '',
-    seedPhraseError: '',
-    passwordError: '',
-    confirmPasswordError: '',
-    termsChecked: false,
-  };
-
-  parseSeedPhrase = (seedPhrase) =>
-    (seedPhrase || '').trim().toLowerCase().match(/\w+/gu)?.join(' ') || '';
 
   UNSAFE_componentWillMount() {
     this._onBeforeUnload = () =>
@@ -58,72 +39,7 @@ export default class ImportWithSeedPhrase extends PureComponent {
     window.removeEventListener('beforeunload', this._onBeforeUnload);
   }
 
-  handleSeedPhraseChange(seedPhrase) {
-    let seedPhraseError = '';
-
-    if (seedPhrase) {
-      const parsedSeedPhrase = this.parseSeedPhrase(seedPhrase);
-      const wordCount = parsedSeedPhrase.split(/\s/u).length;
-      if (wordCount % 3 !== 0 || wordCount > 24 || wordCount < 12) {
-        seedPhraseError = this.context.t('seedPhraseReq');
-      } else if (!isValidMnemonic(parsedSeedPhrase)) {
-        seedPhraseError = this.context.t('invalidSeedPhrase');
-      }
-    }
-
-    this.setState({ seedPhrase, seedPhraseError });
-  }
-
-  handlePasswordChange(password) {
-    const { t } = this.context;
-
-    this.setState((state) => {
-      const { confirmPassword } = state;
-      let confirmPasswordError = '';
-      let passwordError = '';
-
-      if (password && password.length < 8) {
-        passwordError = t('passwordNotLongEnough');
-      }
-
-      if (confirmPassword && password !== confirmPassword) {
-        confirmPasswordError = t('passwordsDontMatch');
-      }
-
-      return {
-        password,
-        passwordError,
-        confirmPasswordError,
-      };
-    });
-  }
-
-  handleConfirmPasswordChange(confirmPassword) {
-    const { t } = this.context;
-
-    this.setState((state) => {
-      const { password } = state;
-      let confirmPasswordError = '';
-
-      if (password !== confirmPassword) {
-        confirmPasswordError = t('passwordsDontMatch');
-      }
-
-      return {
-        confirmPassword,
-        confirmPasswordError,
-      };
-    });
-  }
-
-  handleImport = async (event) => {
-    event.preventDefault();
-
-    if (!this.isValid()) {
-      return;
-    }
-
-    const { password, seedPhrase } = this.state;
+  handleImport = async (password, seedPhrase) => {
     const {
       history,
       onSubmit,
@@ -131,88 +47,26 @@ export default class ImportWithSeedPhrase extends PureComponent {
       initializeThreeBox,
     } = this.props;
 
-    try {
-      await onSubmit(password, this.parseSeedPhrase(seedPhrase));
-      this.context.metricsEvent({
-        eventOpts: {
-          category: 'Onboarding',
-          action: 'Import Seed Phrase',
-          name: 'Import Complete',
-        },
-      });
-
-      setSeedPhraseBackedUp(true).then(async () => {
-        initializeThreeBox();
-        history.replace(INITIALIZE_END_OF_FLOW_ROUTE);
-      });
-    } catch (error) {
-      this.setState({ seedPhraseError: error.message });
-    }
-  };
-
-  isValid() {
-    const {
-      seedPhrase,
-      password,
-      confirmPassword,
-      passwordError,
-      confirmPasswordError,
-      seedPhraseError,
-    } = this.state;
-
-    if (
-      !password ||
-      !confirmPassword ||
-      !seedPhrase ||
-      password !== confirmPassword
-    ) {
-      return false;
-    }
-
-    if (password.length < 8) {
-      return false;
-    }
-
-    return !passwordError && !confirmPasswordError && !seedPhraseError;
-  }
-
-  onTermsKeyPress = ({ key }) => {
-    if (key === ' ' || key === 'Enter') {
-      this.toggleTermsCheck();
-    }
-  };
-
-  toggleTermsCheck = () => {
+    await onSubmit(password, seedPhrase);
     this.context.metricsEvent({
       eventOpts: {
         category: 'Onboarding',
         action: 'Import Seed Phrase',
-        name: 'Check ToS',
+        name: 'Import Complete',
       },
     });
-    this.setState((prevState) => ({
-      termsChecked: !prevState.termsChecked,
-    }));
-  };
 
-  toggleShowSeedPhrase = () => {
-    this.setState(({ showSeedPhrase }) => ({
-      showSeedPhrase: !showSeedPhrase,
-    }));
+    setSeedPhraseBackedUp(true).then(async () => {
+      initializeThreeBox();
+      history.replace(INITIALIZE_END_OF_FLOW_ROUTE);
+    });
   };
 
   render() {
     const { t } = this.context;
-    const {
-      seedPhraseError,
-      showSeedPhrase,
-      passwordError,
-      confirmPasswordError,
-      termsChecked,
-    } = this.state;
 
     return (
-      <form className="first-time-flow__form" onSubmit={this.handleImport}>
+      <div className="first-time-flow__import">
         <div className="first-time-flow__create-back">
           <a
             onClick={(e) => {
@@ -222,10 +76,6 @@ export default class ImportWithSeedPhrase extends PureComponent {
                   category: 'Onboarding',
                   action: 'Import Seed Phrase',
                   name: 'Go Back from Onboarding Import',
-                },
-                customVariables: {
-                  errorLabel: 'Seed Phrase Error',
-                  errorMessage: seedPhraseError,
                 },
               });
               this.props.history.push(INITIALIZE_SELECT_ACTION_ROUTE);
@@ -239,116 +89,12 @@ export default class ImportWithSeedPhrase extends PureComponent {
           {t('importAccountSeedPhrase')}
         </div>
         <div className="first-time-flow__text-block">{t('secretPhrase')}</div>
-        <div className="first-time-flow__textarea-wrapper">
-          <label>{t('secretRecoveryPhrase')}</label>
-          {showSeedPhrase ? (
-            <textarea
-              className="first-time-flow__textarea"
-              onChange={(e) => this.handleSeedPhraseChange(e.target.value)}
-              onPaste={clearClipboard}
-              value={this.state.seedPhrase}
-              placeholder={t('seedPhrasePlaceholder')}
-              autoComplete="off"
-            />
-          ) : (
-            <TextField
-              className="first-time-flow__textarea first-time-flow__seedphrase"
-              type="password"
-              onChange={(e) => this.handleSeedPhraseChange(e.target.value)}
-              value={this.state.seedPhrase}
-              placeholder={t('seedPhrasePlaceholderPaste')}
-              autoComplete="off"
-              onPaste={clearClipboard}
-            />
-          )}
-          {seedPhraseError ? (
-            <span className="error">{seedPhraseError}</span>
-          ) : null}
-          <div
-            className="first-time-flow__checkbox-container"
-            onClick={this.toggleShowSeedPhrase}
-          >
-            <div
-              className="first-time-flow__checkbox"
-              tabIndex="0"
-              role="checkbox"
-              onKeyPress={this.toggleShowSeedPhrase}
-              aria-checked={showSeedPhrase}
-              aria-labelledby="ftf-chk1-label"
-            >
-              {showSeedPhrase ? <i className="fa fa-check fa-2x" /> : null}
-            </div>
-            <span
-              id="ftf-chk1-label"
-              className="first-time-flow__checkbox-label"
-            >
-              {t('showSeedPhrase')}
-            </span>
-          </div>
-        </div>
-        <TextField
-          id="password"
-          label={t('newPassword')}
-          type="password"
-          className="first-time-flow__input"
-          value={this.state.password}
-          onChange={(event) => this.handlePasswordChange(event.target.value)}
-          error={passwordError}
-          autoComplete="new-password"
-          margin="normal"
-          largeLabel
+        <CreateNewVault
+          includeTerms
+          onSubmit={this.handleImport}
+          submitText={t('import')}
         />
-        <TextField
-          id="confirm-password"
-          label={t('confirmPassword')}
-          type="password"
-          className="first-time-flow__input"
-          value={this.state.confirmPassword}
-          onChange={(event) =>
-            this.handleConfirmPasswordChange(event.target.value)
-          }
-          error={confirmPasswordError}
-          autoComplete="new-password"
-          margin="normal"
-          largeLabel
-        />
-        <div
-          className="first-time-flow__checkbox-container"
-          onClick={this.toggleTermsCheck}
-        >
-          <div
-            className="first-time-flow__checkbox first-time-flow__terms"
-            tabIndex="0"
-            role="checkbox"
-            onKeyPress={this.onTermsKeyPress}
-            aria-checked={termsChecked}
-            aria-labelledby="ftf-chk1-label"
-          >
-            {termsChecked ? <i className="fa fa-check fa-2x" /> : null}
-          </div>
-          <span id="ftf-chk1-label" className="first-time-flow__checkbox-label">
-            {t('acceptTermsOfUse', [
-              <a
-                onClick={(e) => e.stopPropagation()}
-                key="first-time-flow__link-text"
-                href="https://metamask.io/terms.html"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="first-time-flow__link-text">{t('terms')}</span>
-              </a>,
-            ])}
-          </span>
-        </div>
-        <Button
-          type="primary"
-          submit
-          className="first-time-flow__button"
-          disabled={!this.isValid() || !termsChecked}
-        >
-          {t('import')}
-        </Button>
-      </form>
+      </div>
     );
   }
 }
--- ui/pages/keychains/restore-vault.js
+++ ui/pages/keychains/restore-vault.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { ethers } from 'ethers';
 import {
   createNewVaultAndRestore,
   unMarkPasswordForgotten,
   initializeThreeBox,
 } from '../../store/actions';
 import { DEFAULT_ROUTE } from '../../helpers/constants/routes';
-import TextField from '../../components/ui/text-field';
-import Button from '../../components/ui/button';
-import { clearClipboard } from '../../helpers/utils/util';
-
-const { isValidMnemonic } = ethers.utils;
+import CreateNewVault from '../../components/app/create-new-vault';
 
 class RestoreVaultPage extends Component {
   static contextTypes = {
@@ -28,70 +23,7 @@ class RestoreVaultPage extends Component {
     initializeThreeBox: PropTypes.func,
   };
 
-  state = {
-    seedPhrase: '',
-    showSeedPhrase: false,
-    password: '',
-    confirmPassword: '',
-    seedPhraseError: null,
-    passwordError: null,
-    confirmPasswordError: null,
-  };
-
-  parseSeedPhrase = (seedPhrase) =>
-    (seedPhrase || '').trim().toLowerCase().match(/\w+/gu)?.join(' ') || '';
-
-  handleSeedPhraseChange(seedPhrase) {
-    const { t } = this.context;
-    let seedPhraseError = null;
-
-    const parseSeedPhrase = this.parseSeedPhrase(seedPhrase);
-    const wordCount = parseSeedPhrase.split(/\s/u).length;
-    if (
-      parseSeedPhrase &&
-      (wordCount % 3 !== 0 || wordCount < 12 || wordCount > 24)
-    ) {
-      seedPhraseError = t('seedPhraseReq');
-    } else if (!isValidMnemonic(parseSeedPhrase)) {
-      seedPhraseError = t('invalidSeedPhrase');
-    }
-
-    this.setState({ seedPhrase, seedPhraseError });
-  }
-
-  handlePasswordChange(password) {
-    const { confirmPassword } = this.state;
-    let confirmPasswordError = null;
-    let passwordError = null;
-
-    if (password && password.length < 8) {
-      passwordError = this.context.t('passwordNotLongEnough');
-    }
-
-    if (confirmPassword && password !== confirmPassword) {
-      confirmPasswordError = this.context.t('passwordsDontMatch');
-    }
-
-    this.setState({ password, passwordError, confirmPasswordError });
-  }
-
-  handleConfirmPasswordChange(confirmPassword) {
-    const { password } = this.state;
-    let confirmPasswordError = null;
-
-    if (password !== confirmPassword) {
-      confirmPasswordError = this.context.t('passwordsDontMatch');
-    }
-
-    this.setState({ confirmPassword, confirmPasswordError });
-  }
-
-  handleImport = (event) => {
-    event.preventDefault();
-    const { password, seedPhrase, disabled } = this.state;
-    if (disabled) {
-      return;
-    }
+  handleImport = (password, seedPhrase) => {
     const {
       // eslint-disable-next-line no-shadow
       createNewVaultAndRestore,
@@ -102,50 +34,22 @@ class RestoreVaultPage extends Component {
     } = this.props;
 
     leaveImportSeedScreenState();
-    createNewVaultAndRestore(password, this.parseSeedPhrase(seedPhrase)).then(
-      () => {
-        this.context.metricsEvent({
-          eventOpts: {
-            category: 'Retention',
-            action: 'userEntersSeedPhrase',
-            name: 'onboardingRestoredVault',
-          },
-        });
-        initializeThreeBox();
-        history.push(DEFAULT_ROUTE);
-      },
-    );
-  };
-
-  hasError() {
-    const { passwordError, confirmPasswordError, seedPhraseError } = this.state;
-    return passwordError || confirmPasswordError || seedPhraseError;
-  }
-
-  toggleShowSeedPhrase = () => {
-    this.setState(({ showSeedPhrase }) => ({
-      showSeedPhrase: !showSeedPhrase,
-    }));
+    createNewVaultAndRestore(password, seedPhrase).then(() => {
+      this.context.metricsEvent({
+        eventOpts: {
+          category: 'Retention',
+          action: 'userEntersSeedPhrase',
+          name: 'onboardingRestoredVault',
+        },
+      });
+      initializeThreeBox();
+      history.push(DEFAULT_ROUTE);
+    });
   };
 
   render() {
-    const {
-      seedPhrase,
-      showSeedPhrase,
-      password,
-      confirmPassword,
-      seedPhraseError,
-      passwordError,
-      confirmPasswordError,
-    } = this.state;
     const { t } = this.context;
     const { isLoading } = this.props;
-    const disabled =
-      !seedPhrase ||
-      !password ||
-      !confirmPassword ||
-      isLoading ||
-      this.hasError();
 
     return (
       <div className="first-view-main-wrapper">
@@ -171,94 +75,11 @@ class RestoreVaultPage extends Component {
             <div className="import-account__selector-typography">
               {this.context.t('secretPhraseWarning')}
             </div>
-            <form
-              className="import-account__input-wrapper"
+            <CreateNewVault
+              disabled={isLoading}
               onSubmit={this.handleImport}
-            >
-              <label className="import-account__input-label">
-                {this.context.t('walletSeedRestore')}
-              </label>
-              {showSeedPhrase ? (
-                <textarea
-                  className="import-account__secret-phrase"
-                  onPaste={clearClipboard}
-                  onChange={(e) => this.handleSeedPhraseChange(e.target.value)}
-                  value={seedPhrase}
-                  autoFocus
-                  placeholder={this.context.t('separateEachWord')}
-                />
-              ) : (
-                <TextField
-                  className="import-account__textarea import-account__seedphrase"
-                  type="password"
-                  onPaste={clearClipboard}
-                  onChange={(e) => this.handleSeedPhraseChange(e.target.value)}
-                  value={seedPhrase}
-                  autoFocus
-                  placeholder={t('seedPhrasePlaceholderPaste')}
-                />
-              )}
-              <span className="error">{seedPhraseError}</span>
-              <div
-                className="import-account__checkbox-container"
-                onClick={this.toggleShowSeedPhrase}
-              >
-                <div
-                  className="import-account__checkbox"
-                  tabIndex="0"
-                  id="seed-checkbox"
-                  role="checkbox"
-                  onKeyPress={this.toggleShowSeedPhrase}
-                  aria-checked={showSeedPhrase}
-                  aria-labelledby="ftf-chk1-label"
-                >
-                  {showSeedPhrase ? <i className="fa fa-check fa-2x" /> : null}
-                </div>
-                <label
-                  htmlFor="seed-checkbox"
-                  id="ftf-chk1-label"
-                  className="import-account__checkbox-label"
-                >
-                  {t('showSeedPhrase')}
-                </label>
-              </div>
-              <TextField
-                id="password"
-                label={t('newPassword')}
-                type="password"
-                className="first-time-flow__input"
-                value={this.state.password}
-                onChange={(event) =>
-                  this.handlePasswordChange(event.target.value)
-                }
-                error={passwordError}
-                autoComplete="new-password"
-                margin="normal"
-                largeLabel
-              />
-              <TextField
-                id="confirm-password"
-                label={t('confirmPassword')}
-                type="password"
-                className="first-time-flow__input"
-                value={this.state.confirmPassword}
-                onChange={(event) =>
-                  this.handleConfirmPasswordChange(event.target.value)
-                }
-                error={confirmPasswordError}
-                autoComplete="confirm-password"
-                margin="normal"
-                largeLabel
-              />
-              <Button
-                type="primary"
-                submit
-                className="first-time-flow__button"
-                disabled={disabled}
-              >
-                {this.context.t('restore')}
-              </Button>
-            </form>
+              submitText={t('restore')}
+            />
           </div>
         </div>
       </div>
