2db2b3bd68eea692967e4366012e4c0c42f287ab
Guillaume Roux <guillaumeroux123@gmail.com>
2022-02-07 19:09:40 +0100

    revert unnecessary change

--- ui/pages/settings/networks-tab/networks-form/networks-form.js
+++ ui/pages/settings/networks-tab/networks-form/networks-form.js
@@ -93,7 +93,7 @@ const NetworksForm = ({
     setRpcUrl(selectedNetwork.rpcUrl);
     setChainId(getDisplayChainId(selectedNetwork.chainId));
     setTicker(selectedNetwork?.ticker);
-    setBlockExplorerUrl(selectedNetwork?.blockExplorerUrl || '');
+    setBlockExplorerUrl(selectedNetwork?.blockExplorerUrl);
     setErrors({});
     setWarnings({});
     setIsSubmitting(false);
