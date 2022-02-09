9239633f57a20328a4feb11ba8fe110e1fe17ece
Filip Sekulic <filip.sekulic@consensys.net>
2022-02-09 17:57:01 +0100

    Created 'Hold to reveal' component

---
+++ ui/components/app/hold-to-reveal/hold-to-reveal.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HoldToReveal from './hold-to-reveal';
+
+export default {
+    title: 'Components/APP/HoldToReveal',
+    id: __filename,
+};
+
+export const DefaultStory = () => {
+    return <HoldToReveal />
+};
+
+DefaultStory.storyName = 'Default';
