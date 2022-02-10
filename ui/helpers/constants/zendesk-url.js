246f6a0abf08fb9ad30d3942a2c5f2c14caaea35
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2022-02-10 16:59:08 -0500

    addressing review comments

--- ui/helpers/constants/zendesk-url.js
+++ ui/helpers/constants/zendesk-url.js
@@ -1,12 +1,12 @@
 const ZENDESK_URLS = {
-  TOKEN_SAFETY_PRACTICES:
-    'https://metamask.zendesk.com/hc/en-us/articles/4403988839451',
+  ADD_CUSTOM_TOKENS:
+    'https://metamask.zendesk.com/hc/en-us/articles/360015489031',
   ADD_MISSING_ACCOUNTS:
     'https://metamask.zendesk.com/hc/en-us/articles/360015489271',
   IMPORT_ACCOUNTS:
     'https://metamask.zendesk.com/hc/en-us/articles/360015489331',
-  ADD_CUSTOM_TOKENS:
-    'https://metamask.zendesk.com/hc/en-us/articles/360015489031',
+  TOKEN_SAFETY_PRACTICES:
+    'https://metamask.zendesk.com/hc/en-us/articles/4403988839451',
 };
 
 export default ZENDESK_URLS;
