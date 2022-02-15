c42defd78e7c7fcbb8082a109f3f5ae536c4aa1b
PeterYinusa <peter.yinusa@consensys.net>
2022-02-15 15:20:12 +0000

    enable mocking by default

--- test/e2e/webdriver/index.js
+++ test/e2e/webdriver/index.js
@@ -4,14 +4,14 @@ const Driver = require('./driver');
 const ChromeDriver = require('./chrome');
 const FirefoxDriver = require('./firefox');
 
-async function buildWebDriver({ responsive, port, mock } = {}) {
+async function buildWebDriver({ responsive, port } = {}) {
   const browser = process.env.SELENIUM_BROWSER;
 
   const {
     driver: seleniumDriver,
     extensionId,
     extensionUrl,
-  } = await buildBrowserWebDriver(browser, { responsive, port, mock });
+  } = await buildBrowserWebDriver(browser, { responsive, port });
   await setupFetchMocking(seleniumDriver);
   const driver = new Driver(seleniumDriver, browser, extensionUrl);
 
