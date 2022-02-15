089e0b9f5c5851f28c400db7582bb9abf0270f22
ryanml <ryanlanese@gmail.com>
2022-02-15 11:39:57 -0700

    Fixing sign type data message formatting, requiring content scroll before sign (#13642)
    * Fixing signature request formatting, requiring scroll before sign
    
    * Ensure sign button not disable when no scroll is required
    
    * Test fix attempt #1
    
    * Clean up e2e tests
    
    Co-authored-by: Dan Miller <danjm.com@gmail.com>

--- test/e2e/tests/signature-request.spec.js
+++ test/e2e/tests/signature-request.spec.js
@@ -1,5 +1,5 @@
 const { strict: assert } = require('assert');
-const { withFixtures } = require('../helpers');
+const { withFixtures, regularDelayMs } = require('../helpers');
 
 describe('Signature Request', function () {
   it('can initiate and confirm a Signature Request', async function () {
@@ -59,6 +59,11 @@ describe('Signature Request', function () {
         );
 
         // Approve signing typed data
+        await driver.executeScript(`
+          const lastNodeInMessage = document.querySelectorAll('.signature-request-message--node')[7];
+          lastNodeInMessage.scrollIntoView();
+        `);
+        await driver.delay(regularDelayMs);
         await driver.clickElement({ text: 'Sign', tag: 'button' }, 10000);
         await driver.waitUntilXWindowHandles(2);
         windowHandles = await driver.getAllWindowHandles();
