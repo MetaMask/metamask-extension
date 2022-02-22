10c7d89266a830f5dbaa84e0d75606df0ee1ed39
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2022-02-23 01:27:17 +0400

    add gasPrice to updateEditableParams
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- app/scripts/controllers/transactions/index.js
+++ app/scripts/controllers/transactions/index.js
@@ -384,8 +384,9 @@ export default class TransactionController extends EventEmitter {
    * @param editableParams.to
    * @param editableParams.value
    * @param editableParams.gas
+   * @param editableParams.gasPrice
    */
-  updateEditableParams(txId, { data, from, to, value, gas }) {
+  updateEditableParams(txId, { data, from, to, value, gas, gasPrice }) {
     if (!this._checkIfTxStatusIsUnapproved(txId)) {
       return;
     }
@@ -397,6 +398,7 @@ export default class TransactionController extends EventEmitter {
         to,
         value,
         gas,
+        gasPrice,
       },
     };
 
