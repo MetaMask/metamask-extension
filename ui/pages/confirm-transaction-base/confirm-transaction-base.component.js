047cbeb695f347dc235a65383da9f0b5081374a2
David Walsh <davidwalsh83@gmail.com>
2022-02-22 18:10:07 -0600

    Assume hardware ready upon connection

--- ui/pages/confirm-transaction-base/confirm-transaction-base.component.js
+++ ui/pages/confirm-transaction-base/confirm-transaction-base.component.js
@@ -648,6 +648,7 @@ export default class ConfirmTransactionBase extends Component {
               this.setState({
                 showingHardwareConnectionAdvancedPopover: false,
                 showingHardwareConnectionContents: false,
+                hardwareIsReady: true,
               });
             }}
             onAdvancedClick={() => {
