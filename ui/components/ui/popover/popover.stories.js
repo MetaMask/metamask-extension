c4433dd4dfa3de2a4e99a4f801a892e0d56fd0bd
hmalik88 <hassan.malik@consensys.net>
2022-03-04 07:32:50 -0500

    updated storybook

--- ui/components/ui/popover/popover.stories.js
+++ ui/components/ui/popover/popover.stories.js
@@ -1,18 +1,12 @@
 import React, { useState } from 'react';
 import Button from '../button';
 import Box from '../box';
-import README from './README.mdx';
 import Popover from './popover.component';
 
 export default {
   title: 'Components/UI/Popover',
   id: __filename,
   component: Popover,
-  parameters: {
-    docs: {
-      page: README,
-    },
-  },
   argTypes: {
     title: { control: 'text' },
     subtitle: { control: 'text' },
