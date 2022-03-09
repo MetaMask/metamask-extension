a523a19a76e89e32b16b6b123642926a527aa7f5
Mark Stacey <markjstacey@gmail.com>
2022-03-09 14:49:22 -0330

    Automate the Flask release
    A Flask release will now be published alongside each main extension
    release. The version of each Flask release will be the same as the
    extension version except it will have the suffix `-flask.0`.

--- .circleci/scripts/release-create-gh-release.sh
+++ .circleci/scripts/release-create-gh-release.sh
@@ -26,25 +26,36 @@ function install_github_cli ()
     popd
 }
 
+function print_flask_version ()
+{
+  local flask_filename
+  flask_filename="$(find ./builds-flask -type f -name 'metamask-flask-chrome-*.zip' -exec basename {} .zip \;)"
+
+  # Use substring parameter expansion to remove the first 22 characters ("metamask-extension-flask-")
+  echo "${flask_filename:22}"
+}
+
 current_commit_msg=$(git show -s --format='%s' HEAD)
 
 if [[ $current_commit_msg =~ Version[-[:space:]](v[[:digit:]]+.[[:digit:]]+.[[:digit:]]+) ]]
 then
     tag="${BASH_REMATCH[1]}"
+    flask_version="$(print_flask_version)"
 
     install_github_cli
 
     printf '%s\n' 'Creating GitHub Release'
     release_body="$(awk -v version="${tag##v}" -f .circleci/scripts/show-changelog.awk CHANGELOG.md)"
-    pushd builds
-        hub release create \
-            --attach metamask-chrome-*.zip \
-            --attach metamask-firefox-*.zip \
-            --message "Version ${tag##v}" \
-            --message "$release_body" \
-            --commitish "$CIRCLE_SHA1" \
-            "$tag"
-    popd
+    hub release create \
+        --attach builds/metamask-chrome-*.zip \
+        --attach builds/metamask-firefox-*.zip \
+        --attach builds-flask/metamask-flask-chrome-*.zip \
+        --attach builds-flask/metamask-flask-firefox-*.zip \
+        --message "Version ${tag##v}" \
+        --message "$release_body" \
+        --commitish "$CIRCLE_SHA1" \
+        "$tag"
+    git tag -a "v${flask_version}" -m "Flask version ${flask_version}"
 else
     printf '%s\n' 'Version not found in commit message; skipping GitHub Release'
     exit 0
