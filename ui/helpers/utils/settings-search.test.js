f7e24ec8f8bd7911c53d49f3b1ceb6b48840d75d
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2022-03-15 17:38:34 -0400

    adding the feature flag and moving the token detetcion toggle to advanced

--- ui/helpers/utils/settings-search.test.js
+++ ui/helpers/utils/settings-search.test.js
@@ -234,10 +234,11 @@ describe('Settings Search Utils', () => {
           section: 'Advanced gas controls',
           tab: 'Advanced',
         },
+        {},
         {
           description:
             'Select this to show the hex data field on the send screen',
-          id: 10,
+          id: 11,
           image: 'advanced-icon.svg',
           route: '/settings/advanced#show-hexdata',
           section: 'Show Hex Data',
@@ -245,7 +246,7 @@ describe('Settings Search Utils', () => {
         },
         {
           description: 'Select this to show fiat conversion on test network',
-          id: 11,
+          id: 12,
           image: 'advanced-icon.svg',
           route: '/settings/advanced#conversion-testnetworks',
           section: 'Show Conversion on test networks',
@@ -253,7 +254,7 @@ describe('Settings Search Utils', () => {
         },
         {
           description: 'Select this to show test networks in network list',
-          id: 12,
+          id: 13,
           image: 'advanced-icon.svg',
           route: '/settings/advanced#show-testnets',
           section: 'Show test networks',
@@ -262,7 +263,7 @@ describe('Settings Search Utils', () => {
         {
           description:
             'Turn this on to change the nonce (transaction number) on confirmation screens. This is an advanced feature, use cautiously.',
-          id: 13,
+          id: 14,
           image: 'advanced-icon.svg',
           route: '/settings/advanced#customize-nonce',
           section: 'Customize transaction nonce',
@@ -271,7 +272,7 @@ describe('Settings Search Utils', () => {
         {
           description:
             'Set the idle time in minutes before MetaMask will become locked.',
-          id: 14,
+          id: 15,
           image: 'advanced-icon.svg',
           route: '/settings/advanced#autolock-timer',
           section: 'Auto-Lock Timer (minutes)',
@@ -280,7 +281,7 @@ describe('Settings Search Utils', () => {
         {
           description:
             'Turn on to have your settings backed up with 3Box. This feature is currently experimental; use at your own risk.',
-          id: 15,
+          id: 16,
           image: 'advanced-icon.svg',
           route: '/settings/advanced#sync-with3box',
           section: 'Sync data with 3Box (experimental)',
@@ -289,7 +290,7 @@ describe('Settings Search Utils', () => {
         {
           description:
             'Enter the URL of the IPFS CID gateway to use for ENS content resolution.',
-          id: 16,
+          id: 17,
           image: 'advanced-icon.svg',
           route: '/settings/advanced#ipfs-gateway',
           section: 'IPFS Gateway',
@@ -297,7 +298,7 @@ describe('Settings Search Utils', () => {
         },
         {
           description: 'Preferred Ledger Connection Type',
-          id: 17,
+          id: 18,
           image: 'advanced-icon.svg',
           route: '/settings/advanced#ledger-connection',
           section: 'Preferred Ledger Connection Type',
@@ -306,7 +307,7 @@ describe('Settings Search Utils', () => {
         {
           description:
             'Turn this on to dismiss the Secret Recovery Phrase backup reminder message. We highly recommend that you back up your Secret Recovery Phrase to avoid loss of funds',
-          id: 18,
+          id: 19,
           image: 'advanced-icon.svg',
           route: '/settings/advanced#dimiss-secretrecovery',
           section: 'Dismiss Secret Recovery Phrase backup reminder',
@@ -314,7 +315,7 @@ describe('Settings Search Utils', () => {
         },
         {
           description: '',
-          id: 19,
+          id: 20,
           image: 'contacts-icon.svg',
           route: '/settings/contact-list',
           section: '',
@@ -322,7 +323,7 @@ describe('Settings Search Utils', () => {
         },
         {
           description: 'Reveal Secret Recovery Phrase',
-          id: 20,
+          id: 21,
           image: 'security-icon.svg',
           route: '/settings/security#reveal-secretrecovery',
           section: 'Reveal Secret Recovery Phrase',
@@ -331,7 +332,7 @@ describe('Settings Search Utils', () => {
         {
           description:
             'Select this to use Etherscan to show incoming transactions in the transactions list',
-          id: 21,
+          id: 22,
           image: 'security-icon.svg',
           route: '/settings/security#incoming-transaction',
           section: 'Show Incoming Transactions',
@@ -340,7 +341,7 @@ describe('Settings Search Utils', () => {
         {
           description:
             'Display a warning for phishing domains targeting Ethereum users',
-          id: 22,
+          id: 23,
           image: 'security-icon.svg',
           route: '/settings/security#phishing-detection',
           section: 'Use Phishing Detection',
@@ -349,7 +350,7 @@ describe('Settings Search Utils', () => {
         {
           description:
             'Participate in MetaMetrics to help us make MetaMask better',
-          id: 23,
+          id: 24,
           image: 'security-icon.svg',
           route: '/settings/security#metrametrics',
           section: 'Participate in MetaMetrics',
@@ -358,7 +359,7 @@ describe('Settings Search Utils', () => {
         {
           description:
             'Browsing a website with an unconnected account selected',
-          id: 24,
+          id: 25,
           image: 'alerts-icon.svg',
           route: '/settings/alerts#unconnected-account',
           section: 'Browsing a website with an unconnected account selected',
@@ -367,7 +368,7 @@ describe('Settings Search Utils', () => {
         {
           description:
             'When a website tries to use the removed window.web3 API',
-          id: 25,
+          id: 26,
           image: 'alerts-icon.svg',
           route: '/settings/alerts#web3-shimusage',
           section: 'When a website tries to use the removed window.web3 API',
@@ -375,7 +376,7 @@ describe('Settings Search Utils', () => {
         },
         {
           description: 'Ethereum Mainnet',
-          id: 26,
+          id: 27,
           image: 'network-icon.svg',
           route: '/settings/networks#networks-mainnet',
           section: 'Ethereum Mainnet',
@@ -383,7 +384,7 @@ describe('Settings Search Utils', () => {
         },
         {
           description: 'Ropsten Test Network',
-          id: 27,
+          id: 28,
           image: 'network-icon.svg',
           route: '/settings/networks#networks-ropsten',
           section: 'Ropsten Test Network',
@@ -391,7 +392,7 @@ describe('Settings Search Utils', () => {
         },
         {
           description: 'Rinkeby Test Network',
-          id: 28,
+          id: 29,
           image: 'network-icon.svg',
           route: '/settings/networks#networks-rinkeby',
           section: 'Rinkeby Test Network',
@@ -399,7 +400,7 @@ describe('Settings Search Utils', () => {
         },
         {
           description: 'Goerli Test Network',
-          id: 29,
+          id: 30,
           image: 'network-icon.svg',
           route: '/settings/networks#networks-goerli',
           section: 'Goerli Test Network',
@@ -407,23 +408,15 @@ describe('Settings Search Utils', () => {
         },
         {
           description: 'Kovan Test Network',
-          id: 30,
+          id: 31,
           image: 'network-icon.svg',
           route: '/settings/networks#networtks-kovan',
           section: 'Kovan Test Network',
           tab: 'Networks',
         },
         {
-          description: 'Localhost 8545',
-          id: 31,
-          image: 'network-icon.svg',
-          route: '/settings/networks#network-localhost',
-          section: 'Localhost 8545',
-          tab: 'Networks',
-        },
-        {
           description: 'MetaMask is designed and built around the world.',
-          id: 35,
+          id: 36,
           image: 'info-icon.svg',
           route: '/settings/about-us#version',
           section: 'MetaMask Version',
@@ -431,7 +424,7 @@ describe('Settings Search Utils', () => {
         },
         {
           description: '',
-          id: 36,
+          id: 37,
           image: 'info-icon.svg',
           route: '/settings/about-us#links',
           section: 'Links',
@@ -439,7 +432,7 @@ describe('Settings Search Utils', () => {
         },
         {
           description: 'Privacy Policy',
-          id: 37,
+          id: 38,
           image: 'info-icon.svg',
           route: '/settings/about-us#privacy-policy',
           section: 'Privacy Policy',
@@ -447,7 +440,7 @@ describe('Settings Search Utils', () => {
         },
         {
           description: 'Terms of Use',
-          id: 38,
+          id: 39,
           image: 'info-icon.svg',
           route: '/settings/about-us#terms',
           section: 'Terms of Use',
@@ -455,7 +448,7 @@ describe('Settings Search Utils', () => {
         },
         {
           description: 'Attributions',
-          id: 39,
+          id: 40,
           image: 'info-icon.svg',
           route: '/settings/about-us#attributions',
           section: 'Attributions',
@@ -463,7 +456,7 @@ describe('Settings Search Utils', () => {
         },
         {
           description: 'Visit our Support Center',
-          id: 40,
+          id: 41,
           image: 'info-icon.svg',
           route: '/settings/about-us#supportcenter',
           section: 'Visit our Support Center',
@@ -471,7 +464,7 @@ describe('Settings Search Utils', () => {
         },
         {
           description: 'Visit our web site',
-          id: 41,
+          id: 42,
           image: 'info-icon.svg',
           route: '/settings/about-us#visitwebsite',
           section: 'Visit our web site',
@@ -479,7 +472,7 @@ describe('Settings Search Utils', () => {
         },
         {
           description: 'Contact us',
-          id: 42,
+          id: 43,
           image: 'info-icon.svg',
           route: '/settings/about-us#contactus',
           section: 'Contact us',
