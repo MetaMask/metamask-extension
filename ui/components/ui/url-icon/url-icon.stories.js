cc118365d5d69a06bf9fba26b88a70dfe7bd6cf6
HuanCheng Bai <bestony@linux.com>
2022-03-16 16:58:26 +0800

    fix: fix style by prettier

--- ui/components/ui/url-icon/url-icon.stories.js
+++ ui/components/ui/url-icon/url-icon.stories.js
@@ -5,22 +5,22 @@ export default {
   title: 'Components/UI/UrlIcon',
   id: __filename,
   argType: {
-    name: { description:"Icon Name", control:"text"},
-    url: { description: "Icon File URL", control: "text" },
-    className: { control: "text" },
-    fallbackClassName: { control: "text" },
+    name: { description: 'Icon Name', control: 'text' },
+    url: { description: 'Icon File URL', control: 'text' },
+    className: { control: 'text' },
+    fallbackClassName: { control: 'text' },
   },
   args: {
-    name: "AST",
-    url: "AST.png",
-    className: "",
-    fallbackClassName:"",
-  }
+    name: 'AST',
+    url: 'AST.png',
+    className: '',
+    fallbackClassName: '',
+  },
 };
 
 export const DefaultStory = (args) => {
   return <UrlIcon name={args.name} url={args.url} />;
-}
+};
 
 export const AST = () => {
   return <UrlIcon name="AST" url="AST.png" />;
