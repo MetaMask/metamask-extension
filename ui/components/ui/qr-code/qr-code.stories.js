b5f9ec7b0288c642869abb465b71db1425bc8855
georgewrmarshall <george.marshall@consensys.net>
2022-03-22 11:11:49 -0700

    Updating error color and adding story

---
+++ ui/components/ui/qr-code/qr-code.stories.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import QrCodeView from '.';
+
+export default {
+  title: 'Components/UI/QrCodeView',
+  id: __filename,
+  argTypes: {
+    warning: {
+      control: 'text',
+    },
+    Qr: {
+      control: 'object',
+    },
+  },
+  args: {
+    warning: 'This is a warning',
+    Qr: {
+      message: <div>message</div>,
+      data: 'data',
+    },
+  },
+};
+
+export const DefaultStory = (args) => <QrCodeView {...args} />;
+
+DefaultStory.storyName = 'Default';
