a9489aa3067688aaa809d790cd0acf13f57c12b9
georgewrmarshall <george.marshall@consensys.net>
2022-03-22 15:29:19 -0700

    Updating story path

--- ui/components/app/modals/transaction-confirmed/transaction-confirmed.stories.js
+++ ui/components/app/modals/transaction-confirmed/transaction-confirmed.stories.js
@@ -2,7 +2,7 @@ import React from 'react';
 import TransactionConfirmed from '.';
 
 export default {
-  title: 'Components/UI/TransactionConfirmed', // title should follow the folder structure location of the component. Don't use spaces.
+  title: 'Components/App/Modals/TransactionConfirmed',
   id: __filename,
 };
 
