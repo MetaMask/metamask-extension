cacffb794d1af1867151d827057386b12ec10b7a
georgewrmarshall <george.marshall@consensys.net>
2022-03-21 21:47:19 -0700

    Dark mode transaction confirmed replacing check icon

---
+++ ui/components/app/modals/transaction-confirmed/transaction-confirmed.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import TransactionConfirmed from '.';
+
+export default {
+  title: 'Components/UI/TransactionConfirmed', // title should follow the folder structure location of the component. Don't use spaces.
+  id: __filename,
+};
+
+export const DefaultStory = () => <TransactionConfirmed />;
+
+DefaultStory.storyName = 'Default';
