8064ec34a16b90ea47ac572e909233a8b56d3157
George Marshall <george.marshall@consensys.net>
2022-03-22 12:44:01 -0700

    Dark mode: colors obj UI, GREY, WHITE (#14118)
    * Removing all COLORS.UI 1,2,3,4 from codebase
    
    * Removing all unused versions of COLORS.GREY, COLORS.WHITE and some left over UI1,UI2,UI3,UI4 in docs

--- ui/helpers/constants/design-system.js
+++ ui/helpers/constants/design-system.js
@@ -56,13 +56,6 @@ export const COLORS = {
   /**
    * !!! DEPRECATED DO NOT USE!!!
    */
-  UI1: 'ui-1',
-  UI2: 'ui-2',
-  UI3: 'ui-3',
-  UI4: 'ui-4',
-  GREY: 'grey',
-  NEUTRAL_GREY: 'neutral-grey',
-  WHITE: 'white',
   PRIMARY1: 'primary-1',
   PRIMARY3: 'primary-3',
   SECONDARY1: 'secondary-1',
