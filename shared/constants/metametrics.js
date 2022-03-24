5b895a85aa6d5a6978fa80e494777bdb5d902941
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2022-03-24 23:09:20 +0400

    80 chars comments length
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- shared/constants/metametrics.js
+++ shared/constants/metametrics.js
@@ -156,10 +156,14 @@
 
 /**
  * @typedef {Object} Traits
- * @property {string} [LEDGER_CONNECTION_TYPE] - when ledger live connnection type is changed we identify the ledger_connection_type trait
- * @property {string} [NETWORKS_ADDED] - when user modifies networks we identify the networks_added trait
- * @property {string} [NUMBER_OF_ACCOUNTS] - when identities change, we identity the new number_of_accounts trait
- * @property {string} [THREE_BOX_ENABLED] - when 3box feature is toggled we identify the 3box_enabled trait
+ * @property {string} [LEDGER_CONNECTION_TYPE] - when ledger live connnection
+ * type is changed we identify the ledger_connection_type trait
+ * @property {string} [NETWORKS_ADDED] - when user modifies networks we
+ * identify the networks_added trait
+ * @property {string} [NUMBER_OF_ACCOUNTS] - when identities change, we
+ * identify the new number_of_accounts trait
+ * @property {string} [THREE_BOX_ENABLED] - when 3box feature is toggled we
+ * identify the 3box_enabled trait
  */
 
 /**
