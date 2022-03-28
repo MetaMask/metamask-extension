a3ab204d1109d170083a0f5117805665cfc83882
Guillaume Roux <guillaumeroux123@gmail.com>
2022-03-28 19:23:27 +0200

    Dark Mode: Fix some colors (#14228)
    * fix some colors
    
    * fix test and address requested changes
    
    * address requested changes

--- ui/pages/unlock-page/unlock-page.component.js
+++ ui/pages/unlock-page/unlock-page.component.js
@@ -129,7 +129,7 @@ export default class UnlockPage extends Component {
   renderSubmitButton() {
     const style = {
       backgroundColor: 'var(--color-primary-default)',
-      color: 'white',
+      color: 'var(--color-primary-inverse)',
       marginTop: '20px',
       height: '60px',
       fontWeight: '400',
