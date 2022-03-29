8ff8da7719439438067c7b70d7747e3a8a0f9cb3
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2022-03-29 22:59:04 +0400

    Add asset_type to 'Tokens Added' event.
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- ui/pages/confirm-add-suggested-token/confirm-add-suggested-token.js
+++ ui/pages/confirm-add-suggested-token/confirm-add-suggested-token.js
@@ -14,6 +14,7 @@ import { isEqualCaseInsensitive } from '../../../shared/modules/string-utils';
 import { getSuggestedAssets } from '../../selectors';
 import { rejectWatchAsset, acceptWatchAsset } from '../../store/actions';
 import { TOKEN_STANDARDS } from '../../helpers/constants/common';
+import { ASSET_TYPES } from '../../../shared/constants/transaction';
 
 function getTokenName(name, symbol) {
   return name === undefined ? symbol : `${name} (${symbol})`;
@@ -122,6 +123,7 @@ const ConfirmAddSuggestedToken = () => {
             unlisted: asset.unlisted,
             source: 'dapp',
             token_standard: TOKEN_STANDARDS.ERC20,
+            asset_type: ASSET_TYPES.TOKEN,
           },
         });
       }),
