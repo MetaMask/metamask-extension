7b67d47d13a6aec6d8815b0a7411d3bc30aca21b
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2022-03-30 13:34:04 -0400

    adding component for token value

--- ui/components/app/detected-token/detected-token-values/detected-token-values.js
+++ ui/components/app/detected-token/detected-token-values/detected-token-values.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import Box from '../../../ui/box';
 import Typography from '../../../ui/typography';
-import Identicon from '../../../ui/identicon';
+import CheckBox from '../../../ui/check-box';
 
 import { getTokenList } from '../../../../selectors';
 import {
@@ -18,6 +18,8 @@ import { useTokenFiatAmount } from '../../../../hooks/useTokenFiatAmount';
 const DetectedTokenValues = ({ tokenAddress }) => {
   const tokenList = useSelector(getTokenList);
   const token = tokenList[tokenAddress];
+
+  const [selectedTokens, setSelectedTokens] = useState(false);
   const { tokensWithBalances } = useTokenTracker([token]);
   const balanceToRender = tokensWithBalances[0]?.string;
   const balance = tokensWithBalances[0]?.balance;
@@ -29,16 +31,19 @@ const DetectedTokenValues = ({ tokenAddress }) => {
 
   return (
     <Box display={DISPLAY.INLINE_FLEX} className="detected-token-values">
-      <Box marginTop={1}>
-        <Identicon address={tokenAddress} diameter={42} />
-      </Box>
-      <Box marginLeft={2}>
+      <Box marginBottom={1}>
         <Typography variant={TYPOGRAPHY.H4}>
           {`${balance || '0'} ${token.symbol}`}
         </Typography>
         <Typography variant={TYPOGRAPHY.H7} color={COLORS.TEXT_ALTERNATIVE}>
           {formattedFiatBalance || '$0'}
         </Typography>
+      </Box>
+      <Box className="detected-token-values__checkbox">
+        <CheckBox
+          checked={selectedTokens}
+          onClick={() => setSelectedTokens((checked) => !checked)}
+        />
       </Box>
     </Box>
   );
