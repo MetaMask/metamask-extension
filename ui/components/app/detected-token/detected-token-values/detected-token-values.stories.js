c2e0d5bc80bb48b960226f298ca3236dbd7ede8f
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2022-03-31 11:09:28 -0400

    updating styles

--- ui/components/app/detected-token/detected-token-values/detected-token-values.stories.js
+++ ui/components/app/detected-token/detected-token-values/detected-token-values.stories.js
@@ -7,6 +7,10 @@ export default {
   id: __filename,
   argTypes: {
     address: { control: 'text' },
+    symbol: { control: 'text' },
+    decimals: { control: 'text' },
+    iconUrl: { control: 'text' },
+    aggregators: { control: 'array' },
   },
   args: {
     address: '0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f',
