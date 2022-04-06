7feb515927964ee284d19f15c4460c01657c24fd
Filip Sekulic <filip.sekulic@consensys.net>
2022-04-06 11:30:11 +0200

    Modified unit test

--- ui/components/app/hold-to-reveal-button/hold-to-reveal-button.test.js
+++ ui/components/app/hold-to-reveal-button/hold-to-reveal-button.test.js
@@ -1,27 +1,28 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 import HoldToRevealButton from './hold-to-reveal-button';
 
 describe('HoldToRevealButton', () => {
-  const onLongPressed = jest.fn();
+  let props = {};
+
+  beforeEach(() => {
+    const mockOnLongPressed = jest.fn();
+
+    props = {
+      onLongPressed: mockOnLongPressed,
+      buttonText: 'Hold to reveal SRP',
+    };
+  });
 
   it('should render a button with label', () => {
-    const { getByText } = render(
-      <HoldToRevealButton
-        buttonText="Hold to reveal SRP"
-        onLongPressed={onLongPressed}
-      />,
-    );
+    const { getByText } = render(<HoldToRevealButton {...props} />);
+
     expect(getByText('Hold to reveal SRP')).toBeInTheDocument();
   });
 
   it('should render a button when mouse is down and up', () => {
-    const { getByText } = render(
-      <HoldToRevealButton
-        buttonText="Hold to reveal SRP"
-        onLongPressed={onLongPressed}
-      />,
-    );
+    const { getByText } = render(<HoldToRevealButton {...props} />);
+
     const button = getByText('Hold to reveal SRP');
 
     fireEvent.mouseDown(button);
@@ -31,5 +32,41 @@ describe('HoldToRevealButton', () => {
     fireEvent.mouseUp(button);
 
     expect(button).toBeDefined();
+  });
+
+  it('should not show the locked padlock when a button is long pressed and then should show it after it was lifted off before the animation concludes', () => {
+    const { getByText } = render(<HoldToRevealButton {...props} />);
+
+    const button = getByText('Hold to reveal SRP');
+
+    fireEvent.mouseDown(button);
+
+    waitFor(() => {
+      expect(button.firstChild).toHaveClass(
+        'hold-to-reveal-button__lock-icon-container',
+      );
+    });
+
+    fireEvent.mouseUp(button);
+
+    waitFor(() => {
+      expect(button.firstChild).not.toHaveClass(
+        'hold-to-reveal-button__lock-icon-container',
+      );
+    });
+  });
+
+  it('should show the unlocked padlock when a button is long pressed for the duration of the animation', () => {
+    const { getByText } = render(<HoldToRevealButton {...props} />);
+
+    const button = getByText('Hold to reveal SRP');
+
+    fireEvent.mouseDown(button);
+
+    waitFor(() => {
+      expect(button.firstChild).toHaveClass(
+        'hold-to-reveal-button__unlock-icon-container',
+      );
+    });
   });
 });
