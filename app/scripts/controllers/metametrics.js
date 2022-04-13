9fe3cec4a2d7aaad59a69a516cce9a001f2ba5fc
digiwand <20778143+digiwand@users.noreply.github.com>
2022-04-13 16:38:44 -0300

    MetaMetrics: alphabetize

--- app/scripts/controllers/metametrics.js
+++ app/scripts/controllers/metametrics.js
@@ -554,11 +554,11 @@ export default class MetaMetricsController {
       [TRAITS.NFT_AUTODETECTION_ENABLED]: metamaskState.useCollectibleDetection,
       [TRAITS.NUMBER_OF_ACCOUNTS]: Object.values(metamaskState.identities)
         .length,
-      [TRAITS.OPENSEA_API_ENABLED]: metamaskState.openSeaEnabled,
-      [TRAITS.THREE_BOX_ENABLED]: metamaskState.threeBoxSyncingAllowed,
       [TRAITS.NUMBER_OF_NFT_COLLECTIONS]: this._getNumberOfNFtCollection(
         metamaskState,
       ),
+      [TRAITS.OPENSEA_API_ENABLED]: metamaskState.openSeaEnabled,
+      [TRAITS.THREE_BOX_ENABLED]: metamaskState.threeBoxSyncingAllowed,
     };
 
     if (!this.previousTraits) {
