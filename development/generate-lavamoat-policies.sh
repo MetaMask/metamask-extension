eeb5110d3f793e760d94e45896b3b43375397292
Dan Miller <danjm.com@gmail.com>
2022-04-19 00:22:31 -0230

    Run main, flask and beta in sequence in generate-lavamoat-policies.sh

--- development/generate-lavamoat-policies.sh
+++ development/generate-lavamoat-policies.sh
@@ -7,7 +7,6 @@ set -o pipefail
 # Generate LavaMoat policies for the extension background script for each build
 # type.
 # ATTN: This may tax your device when running it locally.
-concurrently --kill-others-on-fail -n main,beta,flask \
-  "WRITE_AUTO_POLICY=1 yarn build scripts:prod --policy-only" \
-  "WRITE_AUTO_POLICY=1 yarn build scripts:prod --policy-only --build-type beta" \
-  "WRITE_AUTO_POLICY=1 yarn build scripts:prod --policy-only --build-type flask"
+WRITE_AUTO_POLICY=1 yarn build scripts:prod --policy-only &&
+WRITE_AUTO_POLICY=1 yarn build scripts:prod --policy-only --build-type beta &&
+WRITE_AUTO_POLICY=1 yarn build scripts:prod --policy-only --build-type flask
