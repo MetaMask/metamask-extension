75cebe52da7e487ee09b4d68168c7a1c25a7ed16
digiwand <20778143+digiwand@users.noreply.github.com>
2022-04-21 10:08:00 -0300

    MetaMetrics: memoize _getAllNFTAddresses

--- app/scripts/controllers/metametrics.js
+++ app/scripts/controllers/metametrics.js
@@ -1,4 +1,13 @@
-import { isEqual, merge, omit, omitBy, pickBy, size, sum } from 'lodash';
+import {
+  isEqual,
+  memoize,
+  merge,
+  omit,
+  omitBy,
+  pickBy,
+  size,
+  sum,
+} from 'lodash';
 import { ObservableStore } from '@metamask/obs-store';
 import { bufferToHex, keccak } from 'ethereumjs-util';
 import { generateUUID } from 'pubnub';
@@ -609,12 +618,12 @@ export default class MetaMetricsController {
    * @param allCollectibles
    * @returns {[]} Array of all collectible/NFT addresses
    */
-  _getAllNFTAddresses(allCollectibles = {}) {
+  _getAllNFTAddresses = memoize((allCollectibles = {}) => {
     return Object.values(allCollectibles)
       .flatMap((chainCollectibles) => Object.values(chainCollectibles))
       .flat()
       .map((collectible) => collectible.address);
-  }
+  });
 
   /**
    *
