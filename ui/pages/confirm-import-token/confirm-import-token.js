8f0acc91aa72eb5abb09a58bc4dbf1a77ed955f8
Guillaume Roux <guillaumeroux123@gmail.com>
2022-04-22 18:54:33 +0200

    use getTokenStandardAndDetails to get token standard

--- ui/pages/confirm-import-token/confirm-import-token.js
+++ ui/pages/confirm-import-token/confirm-import-token.js
@@ -13,7 +13,6 @@ import { MetaMetricsContext } from '../../contexts/metametrics';
 import { getMostRecentOverviewPage } from '../../ducks/history/history';
 import { getPendingTokens } from '../../ducks/metamask/metamask';
 import { addTokens, clearPendingTokens } from '../../store/actions';
-import { TOKEN_STANDARDS } from '../../helpers/constants/common';
 import { EVENT } from '../../../shared/constants/metametrics';
 import { ASSET_TYPES } from '../../../shared/constants/transaction';
 
@@ -34,9 +33,11 @@ const ConfirmImportToken = () => {
     await dispatch(addTokens(pendingTokens));
 
     const addedTokenValues = Object.values(pendingTokens);
+    console.log(addedTokenValues);
     const firstTokenAddress = addedTokenValues?.[0].address?.toLowerCase();
 
     addedTokenValues.forEach((pendingToken) => {
+      console.log(pendingToken);
       trackEvent({
         event: 'Token Added',
         category: EVENT.CATEGORIES.WALLET,
@@ -46,7 +47,7 @@ const ConfirmImportToken = () => {
           token_decimal_precision: pendingToken.decimals,
           unlisted: pendingToken.unlisted,
           source: pendingToken.isCustom ? 'custom' : 'list',
-          token_standard: TOKEN_STANDARDS.ERC20,
+          token_standard: pendingToken.standard,
           asset_type: ASSET_TYPES.TOKEN,
         },
       });
