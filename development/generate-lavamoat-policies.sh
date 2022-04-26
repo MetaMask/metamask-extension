d84b57fe80fca5f7f9b5a0e2ff8178959b1aca21
ryanml <ryanlanese@gmail.com>
2022-04-26 11:25:38 -0700

    Adding option to update lavamoat policies in parallel

--- development/generate-lavamoat-policies.sh
+++ development/generate-lavamoat-policies.sh
@@ -7,6 +7,21 @@ set -o pipefail
 # Generate LavaMoat policies for the extension background script for each build
 # type.
 # ATTN: This may tax your device when running it locally.
-WRITE_AUTO_POLICY=1 yarn build scripts:prod --policy-only &&
-WRITE_AUTO_POLICY=1 yarn build scripts:prod --policy-only --build-type beta &&
-WRITE_AUTO_POLICY=1 yarn build scripts:prod --policy-only --build-type flask
+
+function run_sequentially() {
+  WRITE_AUTO_POLICY=1 yarn build scripts:prod --policy-only &&
+  WRITE_AUTO_POLICY=1 yarn build scripts:prod --policy-only --build-type beta &&
+  WRITE_AUTO_POLICY=1 yarn build scripts:prod --policy-only --build-type flask
+}
+
+function run_parallel() {
+  concurrently --kill-others-on-fail -n main,beta,flask \
+    "WRITE_AUTO_POLICY=1 yarn build scripts:prod --policy-only" \
+    "WRITE_AUTO_POLICY=1 yarn build scripts:prod --policy-only --build-type beta" \
+    "WRITE_AUTO_POLICY=1 yarn build scripts:prod --policy-only --build-type flask"
+}
+
+if [[ $# -gt 0 ]]
+  then run_parallel
+  else run_sequentially
+fi
