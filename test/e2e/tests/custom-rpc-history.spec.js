2d852338aecc2f7de8a6be287328509202b0a108
Filip Sekulic <filip.sekulic@consensys.net>
2022-05-10 13:30:02 +0200

    Lint fix and code revert

--- test/e2e/tests/custom-rpc-history.spec.js
+++ test/e2e/tests/custom-rpc-history.spec.js
@@ -35,11 +35,6 @@ describe('Stores custom RPC history', function () {
 
         await driver.clickElement({ text: 'Add Network', tag: 'button' });
 
-        await driver.clickElement({
-          text: 'Add a network manually',
-          tag: 'button',
-        });
-
         await driver.findElement('.networks-tab__sub-header-text');
 
         const customRpcInputs = await driver.findElements('input[type="text"]');
@@ -90,11 +85,6 @@ describe('Stores custom RPC history', function () {
 
         await driver.clickElement({ text: 'Add Network', tag: 'button' });
 
-        await driver.clickElement({
-          text: 'Add a network manually',
-          tag: 'button',
-        });
-
         await driver.findElement('.networks-tab__sub-header-text');
 
         const customRpcInputs = await driver.findElements('input[type="text"]');
@@ -132,11 +122,6 @@ describe('Stores custom RPC history', function () {
         await driver.clickElement('.network-display');
 
         await driver.clickElement({ text: 'Add Network', tag: 'button' });
-
-        await driver.clickElement({
-          text: 'Add a network manually',
-          tag: 'button',
-        });
 
         await driver.findElement('.networks-tab__sub-header-text');
 
@@ -230,11 +215,6 @@ describe('Stores custom RPC history', function () {
         await driver.clickElement('.network-display');
 
         await driver.clickElement({ text: 'Add Network', tag: 'button' });
-
-        await driver.clickElement({
-          text: 'Add a network manually',
-          tag: 'button',
-        });
 
         await driver.findVisibleElement('.settings-page__content');
         // // cancel new custom rpc
