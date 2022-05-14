00175ac4baf40a3b21bfac3096503303d28039d1
digiwand <20778143+digiwand@users.noreply.github.com>
2022-05-13 21:30:06 -0500

    SB:SignatureRequestOriginal: add eth_signTypedData

--- ui/components/app/signature-request-original/signature-request-original.stories.js
+++ ui/components/app/signature-request-original/signature-request-original.stories.js
@@ -7,40 +7,37 @@ import SignatureRequestOriginal from './signature-request-original.component';
 
 const MOCK_PRIMARY_IDENTITY = Object.values(testData.metamask.identities)[0];
 
-const MOCK_TX_MSG_PARAMS = {
-  data: JSON.stringify({
-    domain: {
-      name: 'happydapp.website',
-    },
-    message: {
-      string: 'haay wuurl',
-      number: 42,
-    },
-    primaryType: 'Mail',
-    types: {
-      EIP712Domain: [
-        { name: 'name', type: 'string' },
-        { name: 'version', type: 'string' },
-        { name: 'chainId', type: 'uint256' },
-        { name: 'verifyingContract', type: 'address' },
-      ],
-      Group: [
-        { name: 'name', type: 'string' },
-        { name: 'members', type: 'Person[]' },
-      ],
-      Mail: [
-        { name: 'from', type: 'Person' },
-        { name: 'to', type: 'Person[]' },
-        { name: 'contents', type: 'string' },
-      ],
-      Person: [
-        { name: 'name', type: 'string' },
-        { name: 'wallets', type: 'address[]' },
-      ],
-    },
-  }),
-  origin: 'https://happydapp.website/governance?futarchy=true',
-};
+const MOCK_SIGN_DATA = JSON.stringify({
+  domain: {
+    name: 'happydapp.website',
+  },
+  message: {
+    string: 'haay wuurl',
+    number: 42,
+  },
+  primaryType: 'Mail',
+  types: {
+    EIP712Domain: [
+      { name: 'name', type: 'string' },
+      { name: 'version', type: 'string' },
+      { name: 'chainId', type: 'uint256' },
+      { name: 'verifyingContract', type: 'address' },
+    ],
+    Group: [
+      { name: 'name', type: 'string' },
+      { name: 'members', type: 'Person[]' },
+    ],
+    Mail: [
+      { name: 'from', type: 'Person' },
+      { name: 'to', type: 'Person[]' },
+      { name: 'contents', type: 'string' },
+    ],
+    Person: [
+      { name: 'name', type: 'string' },
+      { name: 'wallets', type: 'address[]' },
+    ],
+  },
+});
 
 export default {
   title: 'Components/App/SignatureRequestOriginal',
@@ -87,7 +84,10 @@ DefaultStory.storyName = 'personal_sign Type';
 
 DefaultStory.args = {
   txData: {
-    msgParams: { ...MOCK_TX_MSG_PARAMS },
+    msgParams: {
+      data: MOCK_SIGN_DATA,
+      origin: 'https://happydapp.website/governance?futarchy=true',
+    },
     type: MESSAGE_TYPE.PERSONAL_SIGN,
   },
 };
@@ -98,7 +98,35 @@ ETHSignStory.storyName = 'eth_sign Type';
 
 ETHSignStory.args = {
   txData: {
-    msgParams: { ...MOCK_TX_MSG_PARAMS },
+    msgParams: {
+      data: MOCK_SIGN_DATA,
+      origin: 'https://happydapp.website/governance?futarchy=true',
+    },
     type: MESSAGE_TYPE.ETH_SIGN,
+  },
+};
+
+export const ETHSignTypedStory = Template.bind({});
+
+ETHSignTypedStory.storyName = 'eth_signTypedData Type';
+
+ETHSignTypedStory.args = {
+  txData: {
+    msgParams: {
+      data: [
+        {
+          type: 'string',
+          name: 'Message',
+          value: 'Hi, Alice!',
+        },
+        {
+          type: 'uint32',
+          name: 'A number',
+          value: '1337',
+        },
+      ],
+      origin: 'https://happydapp.website/governance?futarchy=true',
+    },
+    type: MESSAGE_TYPE.ETH_SIGN_TYPED_DATA,
   },
 };
