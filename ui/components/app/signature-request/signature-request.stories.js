9a131b6085d4b7b47af680a2f0a41f200f1135e9
digiwand <20778143+digiwand@users.noreply.github.com>
2022-05-13 19:24:30 -0500

    SB:SignatureRequest: fix args expected by sanitizeMessage

--- ui/components/app/signature-request/signature-request.stories.js
+++ ui/components/app/signature-request/signature-request.stories.js
@@ -48,6 +48,28 @@ DefaultStory.args = {
           string: 'haay wuurl',
           number: 42,
         },
+        primaryType: 'Mail',
+        types: {
+          EIP712Domain: [
+            { name: 'name', type: 'string' },
+            { name: 'version', type: 'string' },
+            { name: 'chainId', type: 'uint256' },
+            { name: 'verifyingContract', type: 'address' },
+          ],
+          Group: [
+            { name: 'name', type: 'string' },
+            { name: 'members', type: 'Person[]' },
+          ],
+          Mail: [
+            { name: 'from', type: 'Person' },
+            { name: 'to', type: 'Person[]' },
+            { name: 'contents', type: 'string' },
+          ],
+          Person: [
+            { name: 'name', type: 'string' },
+            { name: 'wallets', type: 'address[]' },
+          ],
+        },
       }),
       origin: 'https://happydapp.website/governance?futarchy=true',
     },
