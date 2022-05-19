cd0c1b1c5edf97c426155c0dd8f222113f82df72
Alex Donesky <adonesky@gmail.com>
2022-05-19 09:30:42 -0500

    Apply suggestions from code review
    Co-authored-by: George Marshall <george.marshall@consensys.net>

--- ui/components/ui/site-origin/site-origin.js
+++ ui/components/ui/site-origin/site-origin.js
@@ -23,7 +23,6 @@ export default function SiteOrigin({
               icon={iconSrc}
               name={iconName}
               size={32}
-              className={className}
             />
           }
         />
@@ -35,10 +34,31 @@ export default function SiteOrigin({
 }
 
 SiteOrigin.propTypes = {
+/**
+   * The origin of the site generally the URL
+   */
   siteOrigin: PropTypes.string.isRequired,
+  /**
+   * Icon name is used for the fallback icon and will display the first letter of the string
+   * when iconSrc errors or is not provided.
+   */
   iconName: PropTypes.string,
+  /**
+   * The image src of the icon
+   */
   iconSrc: PropTypes.string,
+  /**
+   * Additional className added to the root html element
+   */
   className: PropTypes.string,
+  /**
+   * title specifies extra information and is added to a title attribute added to the root html element
+   * It is most often shown as a tooltip text when the mouse moves over the element.
+   */
   title: PropTypes.string,
+  /**
+   * Whether the SiteOrigin is displayed as a chip or plain text.
+   * if false iconSrc and iconName props will not be used.
+   */
   chip: PropTypes.bool,
 };
