634cf70a71f63ec98ff7efbc081620480efa8a58
Alex Donesky <adonesky@gmail.com>
2022-05-20 09:35:18 -0500

    Enhance reusable SiteOrigin component (#14720)
    * extend and reuse SiteOrigin component

--- ui/pages/confirmation/confirmation.js
+++ ui/pages/confirmation/confirmation.js
@@ -10,7 +10,6 @@ import { useHistory } from 'react-router-dom';
 import { isEqual } from 'lodash';
 import { produce } from 'immer';
 import Box from '../../components/ui/box';
-import Chip from '../../components/ui/chip';
 import MetaMaskTemplateRenderer from '../../components/app/metamask-template-renderer';
 import SiteIcon from '../../components/ui/site-icon';
 import { DEFAULT_ROUTE } from '../../helpers/constants/routes';
@@ -25,6 +24,7 @@ import { useOriginMetadata } from '../../hooks/useOriginMetadata';
 import { getUnapprovedTemplatedConfirmations } from '../../selectors';
 import NetworkDisplay from '../../components/app/network-display/network-display';
 import Callout from '../../components/ui/callout';
+import SiteOrigin from '../../components/ui/site-origin';
 import ConfirmationFooter from './components/confirmation-footer';
 import { getTemplateValues, getTemplateAlerts } from './templates';
 
@@ -208,9 +208,10 @@ export default function ConfirmationPage() {
             name={originMetadata.hostname}
             size={36}
           />
-          <Chip
-            label={stripHttpsScheme(originMetadata.origin)}
-            borderColor={COLORS.BORDER_DEFAULT}
+          <SiteOrigin
+            chip
+            siteOrigin={stripHttpsScheme(originMetadata.origin)}
+            title={stripHttpsScheme(originMetadata.origin)}
           />
         </Box>
         <MetaMaskTemplateRenderer sections={templatedValues.content} />
