5835b1c85b46a98a34066aa6bd7db3a6af1d9fc8
Sam Gbafa <skgbafa@gmail.com>
2022-05-24 18:22:07 -0700

    remove try/catch

--- ui/components/app/signature-request-siwe/format-message-params.js
+++ ui/components/app/signature-request-siwe/format-message-params.js
@@ -7,100 +7,96 @@
  * @returns {Array} An array of label-value pairs with the type of the value as the label
  */
 export const formatParams = (parsedMessage, t) => {
-  try {
-    const output = [];
+  const output = [];
 
-    const {
-      statement,
-      uri,
-      version,
-      chainId,
-      nonce,
-      issuedAt,
-      expirationTime,
-      notBefore,
-      requestId,
-      resources,
-    } = parsedMessage;
+  const {
+    statement,
+    uri,
+    version,
+    chainId,
+    nonce,
+    issuedAt,
+    expirationTime,
+    notBefore,
+    requestId,
+    resources,
+  } = parsedMessage;
 
-    if (statement) {
-      output.push({
-        label: t('SIWEMessageLabel'),
-        value: statement,
-      });
-    }
-
-    if (uri) {
-      output.push({
-        label: t('SIWEURILabel'),
-        value: uri,
-      });
-    }
-
-    if (version) {
-      output.push({
-        label: t('SIWEVersionLabel'),
-        value: version,
-      });
-    }
-
-    if (chainId) {
-      output.push({
-        label: t('SIWEChainIDLabel'),
-        value: chainId,
-      });
-    }
-
-    if (nonce) {
-      output.push({
-        label: t('SIWENonceLabel'),
-        value: nonce,
-      });
-    }
-
-    if (issuedAt) {
-      output.push({
-        label: t('SIWEIssuedAtLabel'),
-        value: issuedAt,
-      });
-    }
-
-    if (expirationTime) {
-      output.push({
-        label: t('SIWEExpirationTimeLabel'),
-        value: expirationTime,
-      });
-    }
-
-    // not in design
-    if (notBefore) {
-      output.push({
-        label: t('SIWENotBeforeLabel'),
-        value: notBefore,
-      });
-    }
-
-    // not in design
-    if (requestId) {
-      output.push({
-        label: t('SIWERequestIdLabel'),
-        value: requestId,
-      });
-    }
-
-    if (resources && resources.length > 0) {
-      output.push({
-        label: t('SIWEResourcesLabel', [resources.length]),
-        value: resources
-          .reduce((previous, resource) => `${previous}${resource}\n`, '')
-          .trim(),
-      });
-    }
-
-    return output;
-  } catch (error) {
-    return [];
+  if (statement) {
+    output.push({
+      label: t('SIWEMessageLabel'),
+      value: statement,
+    });
   }
+
+  if (uri) {
+    output.push({
+      label: t('SIWEURILabel'),
+      value: uri,
+    });
+  }
+
+  if (version) {
+    output.push({
+      label: t('SIWEVersionLabel'),
+      value: version,
+    });
+  }
+
+  if (chainId) {
+    output.push({
+      label: t('SIWEChainIDLabel'),
+      value: chainId,
+    });
+  }
+
+  if (nonce) {
+    output.push({
+      label: t('SIWENonceLabel'),
+      value: nonce,
+    });
+  }
+
+  if (issuedAt) {
+    output.push({
+      label: t('SIWEIssuedAtLabel'),
+      value: issuedAt,
+    });
+  }
+
+  if (expirationTime) {
+    output.push({
+      label: t('SIWEExpirationTimeLabel'),
+      value: expirationTime,
+    });
+  }
+
+  // not in design
+  if (notBefore) {
+    output.push({
+      label: t('SIWENotBeforeLabel'),
+      value: notBefore,
+    });
+  }
+
+  // not in design
+  if (requestId) {
+    output.push({
+      label: t('SIWERequestIdLabel'),
+      value: requestId,
+    });
+  }
+
+  if (resources && resources.length > 0) {
+    output.push({
+      label: t('SIWEResourcesLabel', [resources.length]),
+      value: resources
+        .reduce((previous, resource) => `${previous}${resource}\n`, '')
+        .trim(),
+    });
+  }
+
+  return output;
 };
 
 export default formatParams;
