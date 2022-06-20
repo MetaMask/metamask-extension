82645ba516ed030ebfecdfb28d53766381ce00da
Frederik Bolding <frederik.bolding@gmail.com>
2022-06-20 10:13:12 +0200

    [FLASK] snaps-skunkworks@0.16.0 (#14952)
    * snaps-skunkworks@0.16.0
    
    * Bump iframe-execution-environment
    
    * Add getAppKey
    
    * Regen LavaMoat policy
    
    * Bump test-snaps

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -642,7 +642,7 @@ export default class MetamaskController extends EventEmitter {
     ///: BEGIN:ONLY_INCLUDE_IN(flask)
     this.snapExecutionService = new IframeExecutionService({
       iframeUrl: new URL(
-        'https://metamask.github.io/iframe-execution-environment/0.4.6',
+        'https://metamask.github.io/iframe-execution-environment/0.5.0',
       ),
       messenger: this.controllerMessenger.getRestricted({
         name: 'ExecutionService',
@@ -682,6 +682,11 @@ export default class MetamaskController extends EventEmitter {
         this.snapExecutionService,
       ),
       closeAllConnections: this.removeAllConnections.bind(this),
+      // Prefix subject with appKeyType to generate separate keys for separate uses
+      getAppKey: async (subject, appKeyType) => {
+        await this.appStateController.getUnlockPromise(true);
+        return this.getAppKeyForSubject(`${appKeyType}:${subject}`);
+      },
       state: initState.SnapController,
       messenger: snapControllerMessenger,
     });
