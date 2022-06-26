3fa6bfa6f885592aaaca412c4b57144704114453
owencraston <owen.craston@consensys.net>
2022-06-25 20:51:29 -0400

    lets get going

--- ui/components/app/confirm-page-container/flask/snap-insight.component.js
+++ ui/components/app/confirm-page-container/flask/snap-insight.component.js
@@ -4,9 +4,11 @@ import { Markdown } from './markdown.component';
 
 export const SnapInsight = ({ transaction }) => {
   const data = useInsightSnap(transaction);
+  console.log(data);
 
   return (
     <div className="confirm-page-container-content__data">
+      {/* useful stuff goes here */}
       {!data && <>Loading...</>}
       {data && <Markdown source={data} />}
     </div>
