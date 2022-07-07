92f8f14325ea7ceb7a3ce4f41d0dbf7f40b20f27
NidhiKJha <menidhikjha@gmail.com>
2022-07-08 00:08:55 +0530

    formatted the files

--- ui/components/ui/breadcrumbs/breadcrumbs.stories.js
+++ ui/components/ui/breadcrumbs/breadcrumbs.stories.js
@@ -1,29 +1,27 @@
-import React from "react";
-import Breadcrumbs from "./breadcrumbs.component";
+import React from 'react';
+import Breadcrumbs from './breadcrumbs.component';
 
 export default {
-    title:'Components/UI/Breadcrumbs ',
-    id: __filename,
-    component: Breadcrumbs ,
-    argsTypes:{
-        currentIndex:{
-            control:'number',
-        },
-        total:{
-            control:'number'
-        },
-        className: { control: 'text' },
-    }
-}
+  title: 'Components/UI/Breadcrumbs ',
+  id: __filename,
+  component: Breadcrumbs,
+  argsTypes: {
+    currentIndex: {
+      control: 'number',
+    },
+    total: {
+      control: 'number',
+    },
+    className: { control: 'text' },
+  },
+};
 
-export const DefaultBreadcrumbs  = (args) => {
-    return(
-        <Breadcrumbs  {...args}/>
-    )
-}
+export const DefaultBreadcrumbs = (args) => {
+  return <Breadcrumbs {...args} />;
+};
 
 DefaultBreadcrumbs.storyName = 'Default';
 DefaultBreadcrumbs.args = {
-    currentIndex: 1,
-    total:3
-}
+  currentIndex: 1,
+  total: 3,
+};
