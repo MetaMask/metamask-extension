4d0d549bc947ddfe56193ead6f0ea7abb6a71b08
jpuri <jyotipuri@gmail.com>
2022-07-08 03:20:29 +0400

    fix

--- ui/store/store.js
+++ ui/store/store.js
@@ -1,4 +1,3 @@
-import { createStore } from 'redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'remote-redux-devtools';
