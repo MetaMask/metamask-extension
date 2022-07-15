a885d28ae56a24d6aa6392690b3ae3c12ed51d37
jpuri <jyotipuri@gmail.com>
2022-07-15 16:40:33 +0400

    fix

--- .circleci/scripts/bundle-stats-commit.sh
+++ .circleci/scripts/bundle-stats-commit.sh
@@ -52,7 +52,7 @@ fi
 
 cp -R test-artifacts/chrome/mv3/bundle_size_stats.json temp/stats
 
-echo "bundle_size_stats-${CIRCLE_SHA1}.json" >> temp/stats/fileList.txt
+echo " bundle_size_stats-${CIRCLE_SHA1}.json" >> temp/stats/fileList.txt
 
 mv temp/stats/bundle_size_stats.json "temp/stats/bundle_size_stats-${CIRCLE_SHA1}.json"
 
