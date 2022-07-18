9f9dbf4bf4a7403291b26d1b543a778746833c34
NidhiKJha <menidhikjha@gmail.com>
2022-07-18 18:19:12 +0530

    added tx details popup to cancelled Swap

--- ui/components/app/transaction-list-item/smart-transaction-list-item.component.js
+++ ui/components/app/transaction-list-item/smart-transaction-list-item.component.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import ListItem from '../../ui/list-item';
 import TransactionStatus from '../transaction-status/transaction-status.component';
 import TransactionIcon from '../transaction-icon';
 import { useI18nContext } from '../../../hooks/useI18nContext';
+import { useTransactionDisplayData } from '../../../hooks/useTransactionDisplayData';
 import { formatDateWithYearContext } from '../../../helpers/utils/util';
 import {
   TRANSACTION_GROUP_CATEGORIES,
@@ -15,14 +16,23 @@ import {
 import CancelButton from '../cancel-button';
 import { cancelSwapsSmartTransaction } from '../../../ducks/swaps/swaps';
 import SiteOrigin from '../../ui/site-origin';
+import TransactionListItemDetails from '../transaction-list-item-details';
 
 export default function SmartTransactionListItem({
   smartTransaction,
+  transactionGroup,
   isEarliestNonce = false,
 }) {
   const dispatch = useDispatch();
   const t = useI18nContext();
   const [cancelSwapLinkClicked, setCancelSwapLinkClicked] = useState(false);
+  const [showDetails, setShowDetails] = useState(false);
+  const {
+    primaryCurrency,
+    recipientAddress,
+    isPending,
+    senderAddress,
+  } = useTransactionDisplayData(transactionGroup);
   const {
     sourceTokenSymbol,
     destinationTokenSymbol,
@@ -34,7 +44,7 @@ export default function SmartTransactionListItem({
     sourceTokenSymbol,
     destinationTokenSymbol,
   ]);
-  const subtitle = 'metamask';
+  const subtitle = 'metamask nidhi';
   const date = formatDateWithYearContext(time);
   let displayedStatusKey;
   if (status === SMART_TRANSACTION_STATUSES.PENDING) {
@@ -45,11 +55,19 @@ export default function SmartTransactionListItem({
   const showCancelSwapLink =
     smartTransaction.cancellable && !cancelSwapLinkClicked;
   const className = 'transaction-list-item transaction-list-item--unconfirmed';
+  const toggleShowDetails = useCallback(() => {
+    console.log(
+      'crystal',
+      smartTransaction,transactionGroup
+    );
+    setShowDetails((prev) => !prev);
+  }, []);
   return (
     <>
       <ListItem
         className={className}
         title={title}
+        onClick={toggleShowDetails}
         icon={
           <TransactionIcon category={category} status={displayedStatusKey} />
         }
@@ -72,6 +90,7 @@ export default function SmartTransactionListItem({
         {displayedStatusKey === TRANSACTION_GROUP_STATUSES.PENDING &&
           showCancelSwapLink && (
             <div className="transaction-list-item__pending-actions">
+              Yoo Nidhi
               <CancelButton
                 transaction={smartTransaction.uuid}
                 cancelTransaction={(e) => {
@@ -83,6 +102,26 @@ export default function SmartTransactionListItem({
             </div>
           )}
       </ListItem>
+      {showDetails && (
+        <TransactionListItemDetails
+          title={title}
+          onClose={toggleShowDetails}
+          senderAddress={senderAddress}
+          recipientAddress={recipientAddress}
+          primaryCurrency={primaryCurrency}
+          isEarliestNonce={isEarliestNonce}
+          transactionGroup={transactionGroup}
+          transactionStatus={() => (
+            <TransactionStatus
+              isPending={isPending}
+              isEarliestNonce={isEarliestNonce}
+              date={date}
+              status={displayedStatusKey}
+              statusOnly
+            />
+          )}
+        />
+      )}
     </>
   );
 }
@@ -90,4 +129,5 @@ export default function SmartTransactionListItem({
 SmartTransactionListItem.propTypes = {
   smartTransaction: PropTypes.object.isRequired,
   isEarliestNonce: PropTypes.bool,
+  transactionGroup: PropTypes.object,
 };
