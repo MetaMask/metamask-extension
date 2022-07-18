d92936475a942fed060bb36f810ee6bfd901f4f4
Alex Donesky <adonesky@gmail.com>
2022-07-18 10:59:38 -0500

    Fix default currency symbol for `wallet_addEthereumChain` + improve warnings for data that doesn't match our validation expectations (#15201)
    * set more appropriate default for ticker symbol when wallet_addEthereumChain is called
    
    * throw error to dapp when site suggests network with same chainId but different ticker symbol from already added network, instead of showing error and disabled notification to user

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -3651,6 +3651,9 @@ export default class MetamaskController extends EventEmitter {
         getCurrentChainId: this.networkController.getCurrentChainId.bind(
           this.networkController,
         ),
+        getCurrentRpcUrl: this.networkController.getCurrentRpcUrl.bind(
+          this.networkController,
+        ),
         setProviderType: this.networkController.setProviderType.bind(
           this.networkController,
         ),
