7f630e9cf12f9fdd1f9eab652169c5d398160cfc
NidhiKJha <menidhikjha@gmail.com>
2022-07-20 16:54:24 +0530

    corrected the argTypes for send-ether-storybook component

--- ui/pages/confirm-send-ether/confirm-send-ether.stories.js
+++ ui/pages/confirm-send-ether/confirm-send-ether.stories.js
@@ -1,42 +1,25 @@
 import React from 'react';
-import { store } from '../../../.storybook/preview';
 import ConfirmSendEther from '.';
 
-const options = [];
-const state = store.getState();
-const { identities } = state.metamask;
-Object.keys(identities).forEach(function (key) {
-  options.push({
-    label: identities[key].name,
-    address: key,
-  });
-});
 export default {
   title: 'Pages/ConfirmSendEther',
   id: __filename,
   component: ConfirmSendEther,
   argTypes: {
-    receiver: {
-      control: {
-        type: 'select',
-      },
-      options: ['Receiver', '0xaD6D458402F60fD3Bd25163575031ACDce07538D'],
+    editTransaction: {
+      action: 'editTransaction',
     },
-    sender: {
-      control: {
-        type: 'select',
-      },
-      options: ['Sender'],
+    history: {
+      control: 'object',
+    },
+    txParams: {
+      control: 'object',
     },
   },
 };
+
 export const DefaultStory = (args) => {
   return <ConfirmSendEther {...args} />;
 };
 
 DefaultStory.storyName = 'Default';
-
-DefaultStory.args = {
-  receiver: '0xaD6D458402F60fD3Bd25163575031ACDce07538D',
-  sender: 'Sender',
-};
