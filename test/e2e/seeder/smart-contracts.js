3f9d144fb5fdedb8a4a0a1490ca2e9cdebb7ecfb
seaona <mariona@gmx.es>
2022-07-21 14:25:51 +0200

    Use node module path for grabbing contract constants

--- test/e2e/seeder/smart-contracts.js
+++ test/e2e/seeder/smart-contracts.js
@@ -1,10 +1,37 @@
-/*
- * Smart contract information used for Ganache seeding (initial local blockchain state)
- */
-const hstFactory = require('./contracts/hst-factory');
-const collectiblesFactory = require('./contracts/collectibles-factory');
-const piggybankFactory = require('./contracts/piggybank-factory');
-const failingContract = require('./contracts/failing-contract');
+const {
+  hstBytecode,
+  hstAbi,
+  piggybankBytecode,
+  piggybankAbi,
+  collectiblesAbi,
+  collectiblesBytecode,
+  failingContractAbi,
+  failingContractBytecode,
+} = require('../../../node_modules/@metamask/test-dapp/dist/constants.json');
+
+const hstFactory = {
+  initialAmount: 100,
+  tokenName: 'TST',
+  decimalUnits: 4,
+  tokenSymbol: 'TST',
+  bytecode: hstBytecode,
+  abi: hstAbi,
+};
+
+const collectiblesFactory = {
+  bytecode: collectiblesBytecode,
+  abi: collectiblesAbi,
+};
+
+const piggybankFactory = {
+  bytecode: piggybankBytecode,
+  abi: piggybankAbi,
+};
+
+const failingContract = {
+  bytecode: failingContractBytecode,
+  abi: failingContractAbi,
+};
 
 const contractConfiguration = {
   hst: hstFactory,
