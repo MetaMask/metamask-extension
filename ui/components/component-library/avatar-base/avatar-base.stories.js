3774ce8b1d9a5f036ff14c3647ff691f3deec135
NidhiKJha <menidhikjha@gmail.com>
2022-07-21 19:34:24 +0530

    added README file to storybook

--- ui/components/component-library/avatar-base/avatar-base.stories.js
+++ ui/components/component-library/avatar-base/avatar-base.stories.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import README from './README.mdx';
 import AvatarBase from './avatar-base';
 
 export default {
   title: 'Components/ComponentLibrary/AvatarBase',
   id: __filename,
   component: AvatarBase,
+  parameters: {
+    docs: {
+      page: README,
+    },
+  },
   argTypes: {
     size: {
       control: 'select',
