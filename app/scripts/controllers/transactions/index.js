5e2d02d1d4de99f0a8ceb4f840cfc9e11106670c
adnansahovic <adnan.grb@hotmail.com>
2022-07-26 12:53:43 +0200

    resolved conflict

--- app/scripts/controllers/transactions/index.js
+++ app/scripts/controllers/transactions/index.js
@@ -2123,7 +2123,10 @@ export default class TransactionController extends EventEmitter {
       asset_type: assetType,
       token_standard: tokenStandard,
       transaction_type: transactionType,
+<<<<<<< HEAD
       transaction_speed_up: type === TRANSACTION_TYPES.RETRY,
+=======
+>>>>>>> develop
     };
 
     const sensitiveProperties = {
