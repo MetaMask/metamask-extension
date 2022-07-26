c543b8a9a8f69af20ae548e170a101c7b15cf578
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2022-07-26 17:29:35 +0400

    Remove dispatch, not needed since we're not dispatching any actions.
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- ui/store/actions.js
+++ ui/store/actions.js
@@ -779,7 +779,7 @@ export function updateTransactionSendFlowHistory(txId, sendFlowHistory) {
 }
 
 export function backupUserData() {
-  return async (dispatch) => {
+  return async () => {
     let backedupData;
     try {
       backedupData = await promisifiedBackground.backupUserData();
@@ -793,7 +793,7 @@ export function backupUserData() {
 }
 
 export function restoreUserData(jsonString) {
-  return async (dispatch) => {
+  return async () => {
     try {
       await promisifiedBackground.restoreUserData(jsonString);
     } catch (error) {
