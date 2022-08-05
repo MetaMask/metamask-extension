1b6233def20e3ec8655af1d95b3b0def0bc8959f
jpuri <jyotipuri@gmail.com>
2022-08-05 23:33:22 +0530

    Adding more test coverage

--- ui/store/action-queue/index.test.js
+++ ui/store/action-queue/index.test.js
@@ -1,6 +1,10 @@
 import sinon from 'sinon';
 
-import { submitRequestToBackground, _setBackgroundConnection } from '.';
+import {
+  callBackgroundMethod,
+  submitRequestToBackground,
+  _setBackgroundConnection,
+} from '.';
 
 // This file tests only MV3 queue scenario
 // MV2 tests are already covered by '../actions.test.js'
@@ -10,7 +14,7 @@ jest.mock('../../../shared/modules/mv3.utils', () => {
     isManifestV3: () => true,
   };
 });
-jest.setTimeout(30000);
+
 describe('ActionQueue', () => {
   describe('submitRequestToBackground', () => {
     afterEach(() => {
@@ -30,7 +34,7 @@ describe('ActionQueue', () => {
       expect(background.backgroundFunction.called).toStrictEqual(true);
     });
 
-    it('does not calls promisified background method if the stream is connected', () => {
+    it('does not calls promisified background method if the stream is not connected', () => {
       const background = {
         connectionStream: {
           readable: false,
@@ -67,11 +71,118 @@ describe('ActionQueue', () => {
     //     connectionStream: {
     //       readable: true,
     //     },
-    //     backgroundFunction: () => Promise.resolve('test'),
+    //     backgroundFunction: async () => {
+    //       await Promise.resolve('test');
+    //     },
     //   };
     //   _setBackgroundConnection(background);
     //   const result = await submitRequestToBackground('backgroundFunction');
     //   expect(result).toStrictEqual('test');
+    // });
+
+    // it('rejects if backgroundFunction throws exception', async () => {
+    //   const background = {
+    //     connectionStream: {
+    //       readable: true,
+    //     },
+    //     backgroundFunction: () => {
+    //       throw Error('test');
+    //     },
+    //   };
+    //   _setBackgroundConnection(background);
+    //   expect(async () => {
+    //     await submitRequestToBackground('backgroundFunction');
+    //   }).toThrow('test');
+    // });
+  });
+
+  describe('callBackgroundMethod', () => {
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it('calls background method if the stream is connected', () => {
+      const background = {
+        connectionStream: {
+          readable: true,
+        },
+        backgroundFunction: sinon.stub(),
+      };
+
+      _setBackgroundConnection(background);
+      callBackgroundMethod('backgroundFunction', [], () => ({}));
+      expect(background.backgroundFunction.called).toStrictEqual(true);
+    });
+
+    it('does not calls background method if the stream is not onnected', () => {
+      const background = {
+        connectionStream: {
+          readable: false,
+        },
+        backgroundFunction: sinon.stub(),
+      };
+
+      _setBackgroundConnection(background);
+      callBackgroundMethod('backgroundFunction', [], () => ({}));
+      expect(background.backgroundFunction.called).toStrictEqual(false);
+    });
+
+    it('calls background method on stream reconnection', () => {
+      const background = {
+        connectionStream: {
+          readable: false,
+        },
+        backgroundFunction: sinon.stub(),
+      };
+      _setBackgroundConnection(background);
+      callBackgroundMethod('backgroundFunction', [], () => ({}));
+      expect(background.backgroundFunction.called).toStrictEqual(false);
+
+      background.connectionStream = {
+        readable: true,
+      };
+      _setBackgroundConnection(background);
+      callBackgroundMethod('backgroundFunction', [], () => ({}));
+      expect(background.backgroundFunction.called).toStrictEqual(true);
+    });
+
+    // it('resolves if backgroundFunction called resolves', async () => {
+    //   const background = {
+    //     connectionStream: {
+    //       readable: true,
+    //     },
+    //     backgroundFunction: async () => {
+    //       await Promise.resolve('test');
+    //     },
+    //   };
+    //   _setBackgroundConnection(background);
+    //   const mockFn = sinon.stub();
+    //   await new Promise((resolve) => {
+    //     callBackgroundMethod('backgroundFunction', [], () => {
+    //       mockFn();
+    //       resolve('test');
+    //     });
+    //   });
+    //   expect(mockFn.called).toStrictEqual(true);
+    // });
+
+    // it('rejects if backgroundFunction called throws exception', async () => {
+    //   const background = {
+    //     connectionStream: {
+    //       readable: true,
+    //     },
+    //     backgroundFunction: async () => {
+    //       await Promise.resolve('test');
+    //     },
+    //   };
+    //   _setBackgroundConnection(background);
+    //   const mockFn = sinon.stub();
+    //   await new Promise((_, reject) => {
+    //     callBackgroundMethod('backgroundFunction', [], () => {
+    //       reject();
+    //     });
+    //   });
+    //   expect(mockFn.called).toStrictEqual(true);
     // });
   });
 });
