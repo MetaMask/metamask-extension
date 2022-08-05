7b42c54728eb65b5d8773ab59928fa017bfe8ba7
Brad Decker <bhdecker84@gmail.com>
2022-08-05 10:04:44 -0500

    Update Babel and dependencies (#15392)

--- development/build/index.js
+++ development/build/index.js
@@ -28,11 +28,6 @@ const { getBrowserVersionMap } = require('./utils');
 // Required for LavaMoat policy generation
 require('loose-envify');
 require('globalthis');
-require('@babel/plugin-proposal-object-rest-spread');
-require('@babel/plugin-transform-runtime');
-require('@babel/plugin-proposal-class-properties');
-require('@babel/plugin-proposal-optional-chaining');
-require('@babel/plugin-proposal-nullish-coalescing-operator');
 require('@babel/preset-env');
 require('@babel/preset-react');
 require('@babel/preset-typescript');
