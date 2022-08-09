d3448b9318b1272f0c5c4bf5127e776a9ff64c00
brad-decker <bhdecker84@gmail.com>
2022-08-09 08:29:36 -0500

    set max processes on concurrently

--- development/generate-lavamoat-policies.sh
+++ development/generate-lavamoat-policies.sh
@@ -4,24 +4,16 @@ set -e
 set -u
 set -o pipefail
 
+extraArgs=()
+if [[ $# -lt 1 ]]; then
+    extraArgs+=(-m 1)
+fi
+
 # Generate LavaMoat policies for the extension background script for each build
 # type.
 # ATTN: This may tax your device when running it locally.
-
-function run_sequentially() {
-  WRITE_AUTO_POLICY=1 yarn build scripts:prod --policy-only &&
-  WRITE_AUTO_POLICY=1 yarn build scripts:prod --policy-only --build-type beta &&
-  WRITE_AUTO_POLICY=1 yarn build scripts:prod --policy-only --build-type flask
-}
-
-function run_parallel() {
-  concurrently --kill-others-on-fail -n main,beta,flask \
+concurrently --kill-others-on-fail -n main,beta,flask \
+    "${extraArgs[@]}" \
     "WRITE_AUTO_POLICY=1 yarn build scripts:prod --policy-only" \
     "WRITE_AUTO_POLICY=1 yarn build scripts:prod --policy-only --build-type beta" \
     "WRITE_AUTO_POLICY=1 yarn build scripts:prod --policy-only --build-type flask"
-}
-
-if [[ $# -gt 0 ]]
-  then run_parallel
-  else run_sequentially
-fi
