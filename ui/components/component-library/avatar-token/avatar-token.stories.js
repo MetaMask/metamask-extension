1a8519c2a45573cdc77c02aa4844b56e4a10eb46
NidhiKJha <menidhikjha@gmail.com>
2022-08-09 19:18:00 +0530

    added changes to avatar token stories

--- ui/components/component-library/avatar-token/avatar-token.stories.js
+++ ui/components/component-library/avatar-token/avatar-token.stories.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import { SIZES } from '../../../helpers/constants/design-system';
+import {
+  ALIGN_ITEMS,
+  COLORS,
+  DISPLAY,
+  SIZES,
+} from '../../../helpers/constants/design-system';
+
+import { BaseAvatar } from '../base-avatar';
 
 import README from './README.mdx';
 import { AvatarToken } from './avatar-token';
@@ -28,25 +35,70 @@ export default {
       control: 'boolean',
     },
   },
+  args: {
+    tokenName: 'ast',
+    tokenImageUrl: './AST.png',
+    size: SIZES.MD,
+    showHalo: false,
+  },
 };
 
-export const DefaultStory = (args) => <AvatarToken {...args} />;
-
+const Template = (args) => {
+  return <AvatarToken {...args} />;
+};
+export const DefaultStory = Template.bind({});
 DefaultStory.storyName = 'Default';
-
-export const AvatarTokenWithHalo = (args) => <AvatarToken {...args} />;
-
-AvatarTokenWithHalo.storyName = 'Halo';
-
-DefaultStory.args = {
-  tokenName: 'ast',
-  tokenImageUrl: './AST.png',
-  size: SIZES.MD,
+export const Size = (args) => (
+  <>
+    <AvatarToken {...args} marginBottom={2} size={SIZES.XS} />
+    <AvatarToken {...args} marginBottom={2} size={SIZES.SM} />
+    <AvatarToken {...args} marginBottom={2} size={SIZES.MD} />
+    <AvatarToken {...args} marginBottom={2} size={SIZES.LG} />
+    <AvatarToken {...args} marginBottom={2} size={SIZES.XL} />
+  </>
+);
+export const tokenName = Template.bind({});
+tokenName.args = {
+  tokenImageUrl: '',
 };
+export const tokenImageUrl = Template.bind({});
 
-AvatarTokenWithHalo.args = {
-  tokenName: 'ast',
-  tokenImageUrl: './AST.png',
-  size: SIZES.MD,
+export const showHalo = Template.bind({});
+showHalo.args = {
   showHalo: true,
+};
+export const BackgroundAndBorderColor = (args) => (
+  <>
+    <AvatarToken
+      {...args}
+      backgroundColor={COLORS.KOVAN}
+      borderColor={COLORS.KOVAN}
+      tokenName="K"
+      color={COLORS.PRIMARY_INVERSE} // This will have to be added to the BaseAvatar component as a prop so we can change the color of the text and to the base avatar
+    />
+    <AvatarToken
+      {...args}
+      backgroundColor={COLORS.RINKEBY}
+      borderColor={COLORS.RINKEBY}
+      tokenName="R"
+      color={COLORS.PRIMARY_INVERSE} // This will have to be added to the BaseAvatar component as a prop so we can change the color of the text and to the base avatar
+    />
+    <AvatarToken
+      {...args}
+      backgroundColor={COLORS.GOERLI}
+      borderColor={COLORS.GOERLI}
+      tokenName="G"
+      color={COLORS.PRIMARY_INVERSE} // This will have to be added to the BaseAvatar component as a prop so we can change the color of the text and to the base avatar
+    />
+    <AvatarToken
+      {...args}
+      backgroundColor={COLORS.ROPSTEN}
+      borderColor={COLORS.ROPSTEN}
+      tokenName="R"
+      color={COLORS.PRIMARY_INVERSE} // This will have to be added to the BaseAvatar component as a prop so we can change the color of the text and to the base avatar
+    />
+  </>
+);
+BackgroundAndBorderColor.args = {
+  tokenImageUrl: '',
 };
