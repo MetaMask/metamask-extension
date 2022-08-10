ce07e0005d9187ef1fc173d1ffa57f0f9df85ee8
brad-decker <bhdecker84@gmail.com>
2022-08-10 14:08:33 -0500

    sort keys rule and initial changes

--- ui/pages/settings/settings.component.test.js
+++ ui/pages/settings/settings.component.test.js
@@ -10,18 +10,18 @@ describe('SettingsPage', () => {
   let wrapper;
 
   const props = {
-    isAddressEntryPage: false,
+    addNewNetwork: false,
+    addressName: '',
     backRoute: '/',
+    conversionDate: Date.now(),
     currentPath: '/settings',
+    initialBreadCrumbKey: undefined,
+    initialBreadCrumbRoute: undefined,
+    isAddressEntryPage: false,
+    isPopup: false,
     location: '/settings',
     mostRecentOverviewPage: '',
-    isPopup: false,
     pathnameI18nKey: undefined,
-    addressName: '',
-    initialBreadCrumbRoute: undefined,
-    initialBreadCrumbKey: undefined,
-    addNewNetwork: false,
-    conversionDate: Date.now(),
   };
 
   beforeEach(() => {
