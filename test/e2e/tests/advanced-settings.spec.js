01bb38833f8c299c9679748e29a94d032aef0402
seaona <mariona@gmx.es>
2022-08-10 14:33:20 +0200

    Test only local network

--- test/e2e/tests/advanced-settings.spec.js
+++ test/e2e/tests/advanced-settings.spec.js
@@ -12,14 +12,7 @@ describe('Advanced Settings', function () {
     ],
   };
 
-  const testNetworks = {
-    ropsten: 'Ropsten Test Network',
-    kovan: 'Kovan Test Network',
-    rinkeby: 'Rinkeby Test Network',
-    goerli: 'Goerli Test Network',
-  };
-
-  it('should show conversion on test networks when activated', async function () {
+  it('should show conversion on test network when activated', async function () {
     await withFixtures(
       {
         dapp: true,
@@ -44,66 +37,19 @@ describe('Advanced Settings', function () {
           '.settings-page__header__title-container__close-button',
         );
 
-        // Ropsten
-        await driver.clickElement('.app-header__network-component-wrapper div');
-        await driver.clickElement({ text: testNetworks.ropsten, tag: 'span' });
-        await driver.waitForSelector({ text: 'RopstenETH', tag: 'span' });
-
-        let secondaryCurrency = await driver.isElementPresent(
+        const secondaryCurrency = await driver.isElementPresent(
           '[data-testid="eth-overview__secondary-currency"]',
         );
         assert.equal(
           await secondaryCurrency,
           true,
-          `Secondary currency is not present in ${testNetworks.ropsten}`,
-        );
-
-        // Kovan
-        await driver.clickElement('.app-header__network-component-wrapper div');
-        await driver.clickElement({ text: testNetworks.kovan, tag: 'span' });
-        await driver.waitForSelector({ text: 'KovanETH', tag: 'span' });
-
-        secondaryCurrency = await driver.isElementPresent(
-          '[data-testid="eth-overview__secondary-currency"]',
-        );
-        assert.equal(
-          await secondaryCurrency,
-          true,
-          `Secondary currency is not present in ${testNetworks.kovan}`,
-        );
-
-        // Rinkeby
-        await driver.clickElement('.app-header__network-component-wrapper div');
-        await driver.clickElement({ text: testNetworks.rinkeby, tag: 'span' });
-        await driver.waitForSelector({ text: 'RinkebyETH', tag: 'span' });
-
-        secondaryCurrency = await driver.isElementPresent(
-          '[data-testid="eth-overview__secondary-currency"]',
-        );
-        assert.equal(
-          await secondaryCurrency,
-          true,
-          `Secondary currency is not present in ${testNetworks.rinkeby}`,
-        );
-
-        // Goerli
-        await driver.clickElement('.app-header__network-component-wrapper div');
-        await driver.clickElement({ text: testNetworks.goerli, tag: 'span' });
-        await driver.waitForSelector({ text: 'GoerliETH', tag: 'span' });
-
-        secondaryCurrency = await driver.isElementPresent(
-          '[data-testid="eth-overview__secondary-currency"]',
-        );
-        assert.equal(
-          await secondaryCurrency,
-          true,
-          `Secondary currency is not present in ${testNetworks.goerli}`,
+          `Secondary currency is not present`,
         );
       },
     );
   });
 
-  it('should not show conversion on test networks when deactivated', async function () {
+  it('should not show conversion on test network when deactivated', async function () {
     await withFixtures(
       {
         dapp: true,
@@ -115,64 +61,14 @@ describe('Advanced Settings', function () {
         await driver.navigate();
         await driver.fill('#password', 'correct horse battery staple');
         await driver.press('#password', driver.Key.ENTER);
-        await driver.clickElement('.app-header__network-component-wrapper div');
-        await driver.clickElement({ text: 'Ethereum Mainnet', tag: 'span' });
-        await driver.waitForSelector({ text: 'Ethereum Mainnet', tag: 'span' });
 
-        // Ropsten
-        await driver.clickElement('.app-header__network-component-wrapper div');
-        await driver.clickElement({ text: testNetworks.ropsten, tag: 'span' });
-        await driver.waitForSelector({ text: 'RopstenETH', tag: 'span' });
-
-        let secondaryCurrency = await driver.isElementPresent(
+        const secondaryCurrency = await driver.isElementPresent(
           '[data-testid="eth-overview__secondary-currency"]',
         );
         assert.equal(
           await secondaryCurrency,
           false,
-          `Secondary currency is present in ${testNetworks.ropsten}`,
-        );
-
-        // Kovan
-        await driver.clickElement('.app-header__network-component-wrapper div');
-        await driver.clickElement({ text: testNetworks.kovan, tag: 'span' });
-        await driver.waitForSelector({ text: 'KovanETH', tag: 'span' });
-
-        secondaryCurrency = await driver.isElementPresent(
-          '[data-testid="eth-overview__secondary-currency"]',
-        );
-        assert.equal(
-          await secondaryCurrency,
-          false,
-          `Secondary currency is present in ${testNetworks.kovan}`,
-        );
-
-        // Rinkeby
-        await driver.clickElement('.app-header__network-component-wrapper div');
-        await driver.clickElement({ text: testNetworks.rinkeby, tag: 'span' });
-        await driver.waitForSelector({ text: 'RinkebyETH', tag: 'span' });
-
-        secondaryCurrency = await driver.isElementPresent(
-          '[data-testid="eth-overview__secondary-currency"]',
-        );
-        assert.equal(
-          await secondaryCurrency,
-          false,
-          `Secondary currency is present in ${testNetworks.rinkeby}`,
-        );
-
-        // Goerli
-        await driver.clickElement('.app-header__network-component-wrapper div');
-        await driver.clickElement({ text: testNetworks.goerli, tag: 'span' });
-        await driver.waitForSelector({ text: 'GoerliETH', tag: 'span' });
-
-        secondaryCurrency = await driver.isElementPresent(
-          '[data-testid="eth-overview__secondary-currency"]',
-        );
-        assert.equal(
-          await secondaryCurrency,
-          false,
-          `Secondary currency is present in ${testNetworks.goerli}`,
+          `Secondary currency is present`,
         );
       },
     );
