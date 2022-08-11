ffbface1c125c1b9df03f1c33f80133879415673
nikoferro <nicolaspatricioferro@gmail.com>
2022-08-11 13:11:28 +0200

    Ability to buy tokens with Coinbase Pay and Transak

---
+++ ui/components/app/modals/deposit-token-modal/deposit-token-modal.container.js
@@ -0,0 +1,59 @@
+import { connect } from 'react-redux';
+import withModalProps from '../../../../helpers/higher-order-components/with-modal-props';
+import {
+  getCurrentChainId,
+  getIsBuyableCoinbasePayChain,
+  getIsMainnet,
+  getIsTestnet,
+  getSelectedAddress,
+  getIsBuyableTransakChain,
+  getIsBuyableCoinbasePayToken,
+  getIsBuyableTransakToken,
+} from '../../../../selectors/selectors';
+import {
+  buyEth,
+  hideModal,
+  hideWarning,
+  showModal,
+} from '../../../../store/actions';
+import DepositTokenModal from './deposit-token-modal.component';
+
+function mapStateToProps(state) {
+  const { symbol } = state.appState.modal.modalState.props.token;
+  return {
+    chainId: getCurrentChainId(state),
+    isTestnet: getIsTestnet(state),
+    isMainnet: getIsMainnet(state),
+    address: getSelectedAddress(state),
+    isBuyableCoinbasePayChain: getIsBuyableCoinbasePayChain(state),
+    isBuyableTransakChain: getIsBuyableTransakChain(state),
+    isTokenBuyableCoinbasePay: getIsBuyableCoinbasePayToken(state, symbol),
+    isTokenBuyableTransak: getIsBuyableTransakToken(state, symbol),
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    toCoinbasePay: (address, chainId, symbol) => {
+      dispatch(buyEth({ service: 'coinbase', address, chainId, symbol }));
+    },
+    toTransak: (address, chainId, symbol) => {
+      dispatch(buyEth({ service: 'transak', address, chainId, symbol }));
+    },
+    hideModal: () => {
+      dispatch(hideModal());
+    },
+    hideWarning: () => {
+      dispatch(hideWarning());
+    },
+    showAccountDetailModal: () => {
+      dispatch(showModal({ name: 'ACCOUNT_DETAILS' }));
+    },
+    toFaucet: (chainId) => dispatch(buyEth({ chainId })),
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(withModalProps(DepositTokenModal));
