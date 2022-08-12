d12d75179fa9ac73b1a22cafa78c0f511686664a
Nicolas Ferro <nicolaspatricioferro@gmail.com>
2022-08-12 19:40:26 +0200

    fix eslint spacing errors

--- ui/store/actions.js
+++ ui/store/actions.js
@@ -1059,7 +1059,7 @@ export function deleteExpiredNotifications() {
 
         return Boolean(
           notification.readDate &&
-          new Date(notification.readDate) < expirationTime,
+            new Date(notification.readDate) < expirationTime,
         );
       })
       .map(({ id }) => id);
@@ -2803,12 +2803,12 @@ export function setPendingTokens(pendingTokens) {
   const tokens =
     address && symbol && decimals >= 0 <= 36
       ? {
-        ...selectedTokens,
-        [address]: {
-          ...customToken,
-          isCustom: true,
-        },
-      }
+          ...selectedTokens,
+          [address]: {
+            ...customToken,
+            isCustom: true,
+          },
+        }
       : selectedTokens;
 
   Object.keys(tokens).forEach((tokenAddress) => {
