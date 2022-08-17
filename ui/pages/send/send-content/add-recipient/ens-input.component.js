9f597b6ec5802b180ec296b6125a6b093788914f
Vincent Shadbolt <vince.shadbolt@gmail.com>
2022-08-17 13:10:20 -0700

    Fixed ENS and UNS crossfiring

--- ui/pages/send/send-content/add-recipient/ens-input.component.js
+++ ui/pages/send/send-content/add-recipient/ens-input.component.js
@@ -69,8 +69,11 @@ export default class EnsInput extends Component {
     // Empty ENS state if input is empty
     // maybe scan ENS
     if (isValidDomainName(input)) {
-      lookupEnsName(input);
-      resolveUNS(input);
+      if (!input.includes(".eth")){
+        resolveUNS(input);
+      } else{
+        lookupEnsName(input);
+      }
     } else {
       resetEnsResolution();
       if (
