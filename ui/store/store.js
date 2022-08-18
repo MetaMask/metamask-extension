c89455332e40bf10285dcd11bc78095685d26801
Matthew Epps <mepps32@gmail.com>
2022-08-18 15:29:04 -0700

    chore: fix redux logger changes

--- ui/store/store.js
+++ ui/store/store.js
@@ -1,11 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'remote-redux-devtools';
-import logger from 'redux-logger'
 import rootReducer from '../ducks';
 
 export default function configureStore(initialState) {
-  let storeEnhancers = applyMiddleware(thunkMiddleware, logger);
+  let storeEnhancers = applyMiddleware(thunkMiddleware);
 
   if (process.env.METAMASK_DEBUG && !process.env.IN_TEST) {
     const composeEnhancers = composeWithDevTools({
