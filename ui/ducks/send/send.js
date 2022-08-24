c7b52bab074d05fd3d3683976e8a3ea716d0c535
jpuri <jyotipuri@gmail.com>
2022-08-24 17:37:59 +0530

    fix

--- ui/ducks/send/send.js
+++ ui/ducks/send/send.js
@@ -2269,7 +2269,7 @@ export function signTransaction() {
       await dispatch(
         updateTransactionSendFlowHistory(
           draftTransaction.id,
-          unapprovedTx.txMeta.sendFlowHistory.length,
+          unapprovedTx.sendFlowHistory.length,
           draftTransaction.history,
         ),
       );
