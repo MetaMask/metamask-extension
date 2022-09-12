d085ce08755e5adec3ecdb0bdc56c18fee72c8a4
Filip Sekulic <filip.sekulic@consensys.net>
2022-09-12 17:22:28 +0200

    Code revert

--- shared/lib/swaps-utils.js
+++ shared/lib/swaps-utils.js
@@ -1,9 +1,9 @@
 import BigNumber from 'bignumber.js';
 import log from 'loglevel';
 import {
+  GOERLI_CHAIN_ID,
   LOCALHOST_CHAIN_ID,
   MAINNET_CHAIN_ID,
-  RINKEBY_CHAIN_ID,
 } from '../constants/network';
 import {
   GAS_API_BASE_URL,
@@ -20,7 +20,7 @@ import { addHexPrefix } from '../../app/scripts/lib/util';
 import fetchWithCache from './fetch-with-cache';
 import { decimalToHex } from './transactions-controller-utils';
 
-const TEST_CHAIN_IDS = [RINKEBY_CHAIN_ID, LOCALHOST_CHAIN_ID];
+const TEST_CHAIN_IDS = [GOERLI_CHAIN_ID, LOCALHOST_CHAIN_ID];
 
 const clientIdHeader = { 'X-Client-Id': SWAPS_CLIENT_ID };
 
