c31668a59dc16da2e5f0f1c1aa481b067c8f578f
hmalik88 <hassan.malik@consensys.net>
2022-09-12 19:26:31 -0400

    add H6 to typography

--- ui/components/app/flask/snap-content-footer/snap-content-footer.js
+++ ui/components/app/flask/snap-content-footer/snap-content-footer.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import Typography from '../../../ui/typography/typography';
 import { useI18nContext } from '../../../../hooks/useI18nContext';
 import { SNAPS_VIEW_ROUTE } from 'ui/helpers/constants/routes';
-import { COLORS } from 'ui/helpers/constants/design-system';
+import { COLORS, TYPOGRAPHY } from 'ui/helpers/constants/design-system';
 
 export default function SnapContentFooter({ snapName, snapId }) {
   const t = useI18nContext();
@@ -12,7 +12,7 @@ export default function SnapContentFooter({ snapName, snapId }) {
   return (
     <div className="snap-content-footer">
       <i className="fas fa-exclamation-circle" />
-      <Typography color={COLORS.TEXT_MUTED}>
+      <Typography color={COLORS.TEXT_MUTED} as={TYPOGRAPHY.H6}>
         {t('snapContent', <a href={route}>{snapName}</a>)}
       </Typography>
     </div>
