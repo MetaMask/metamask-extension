453340d12a08ba01e3134a95b21afddd0163aaec
Olusegun Akintayo <akintayo.segun@gmail.com>
2022-09-13 22:51:46 +0400

    Show 3box deprecation message in whats new (#15763)
    * Show 3box deprecation message in whats new
    
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>
    
    * Add . after message
    
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>
    
    * Change deprecation notification message and remove date
    
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>
    
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- ui/selectors/selectors.js
+++ ui/selectors/selectors.js
@@ -817,6 +817,7 @@ function getAllowedAnnouncementIds(state) {
   const supportsWebHid = window.navigator.hid !== undefined;
   const currentlyUsingLedgerLive =
     getLedgerTransportType(state) === LEDGER_TRANSPORT_TYPES.LIVE;
+  const { threeBoxSyncingAllowed } = state.metamask;
 
   return {
     1: false,
@@ -832,6 +833,7 @@ function getAllowedAnnouncementIds(state) {
     11: true,
     12: false,
     13: true,
+    14: threeBoxSyncingAllowed,
   };
 }
 
