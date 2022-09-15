de266ca4f71ffd38589e6080dc3b923cee2550ab
Filip Sekulic <filip.sekulic@consensys.net>
2022-09-16 00:10:25 +0200

    Fixed errors

--- ui/pages/home/home.component.js
+++ ui/pages/home/home.component.js
@@ -1,7 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { Redirect, Route } from 'react-router-dom';
-///: BEGIN:ONLY_INCLUDE_IN(main)
 import {
   EVENT,
   EVENT_NAMES,
