844cfc444dd0fec6b346a27f9e0f1a9c7c83bbf1
Ben Behrman <bbehrman10@gmail.com>
2022-09-16 12:02:41 -0600

    fixes some unit testing issues

--- ui/ducks/send/send.test.js
+++ ui/ducks/send/send.test.js
@@ -1905,6 +1905,7 @@ describe('Send Slice', () => {
           isProbablyAnAssetContract: false,
           userInput: newUserRecipientInput,
           tokenAddressList: ['0x514910771af9ca656af840dff83e8264ecf986ca'],
+          udTlds: undefined,
         });
       });
     });
