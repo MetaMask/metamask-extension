d7353e29c0ae841b8a15cc61c7a3a1c376b71c4f
Samuel Gbafa <skgbafa@gmail.com>
2022-09-29 18:26:45 +0100

    classname cleanup

--- ui/components/app/signature-request-siwe/signature-request-siwe-icon/index.js
+++ ui/components/app/signature-request-siwe/signature-request-siwe-icon/index.js
@@ -10,17 +10,15 @@ import { Icon, ICON_NAMES } from '../../../component-library/icon';
 
 const SignatureRequestSIWEIcon = () => {
   return (
-    <div className="signature-request-siwe-icon">
-      <Box
-        className="signature-request-siwe-icon__icon"
-        display={DISPLAY.INLINE_FLEX}
-        alignItems={ALIGN_ITEMS.CENTER}
-        backgroundColor={COLORS.ERROR_DEFAULT}
-        justifyContent={JUSTIFY_CONTENT.CENTER}
-      >
-        <Icon name={ICON_NAMES.DANGER_FILLED} color={COLORS.ERROR_INVERSE} />
-      </Box>
-    </div>
+    <Box
+      className="signature-request-siwe-icon"
+      display={DISPLAY.INLINE_FLEX}
+      alignItems={ALIGN_ITEMS.CENTER}
+      backgroundColor={COLORS.ERROR_DEFAULT}
+      justifyContent={JUSTIFY_CONTENT.CENTER}
+    >
+      <Icon name={ICON_NAMES.DANGER_FILLED} color={COLORS.ERROR_INVERSE} />
+    </Box>
   );
 };
 
