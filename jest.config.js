f0e0bb4668b1e750b4b5d9ab596a1b2ee213f631
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2022-10-04 13:25:12 +0200

    Update Jest config

--- jest.config.js
+++ jest.config.js
@@ -58,17 +58,11 @@ module.exports = {
   testTimeout: 2500,
   // We have to specify the environment we are running in, which is jsdom. The
   // default is 'node'. This can be modified *per file* using a comment at the
-  // head of the file. So it may be worth while to switch to 'node' in any
+  // head of the file. So it may be worthwhile to switch to 'node' in any
   // background tests.
   testEnvironment: 'jsdom',
-  // Our configuration somehow is calling into the esm folder / files of
-  // some modules. Jest supports ESM but our code is not set to emit ESM files
-  // so we are telling jest to use babel to transform the node_modules listed.
-  // Note: for some reason I could not hammer down to the node_modules
-  // installed in @metamask/controllers so I had to just blanket specify all
-  // of the @metamask/controllers folder.
-  transformIgnorePatterns: [
-    '/node_modules/(?!(multiformats|uuid|nanoid|@metamask/controllers|@metamask/snap-controllers|@metamask/rpc-methods)/)',
-  ],
+  testEnvironmentOptions: {
+    customExportConditions: ['node', 'node-addons'],
+  },
   workerIdleMemoryLimit: '500MB',
 };
