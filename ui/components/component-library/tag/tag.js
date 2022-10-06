0b0d42f5994dd67dbb0a61d34f32fa0d2bd4f761
NidhiKJha <menidhikjha@gmail.com>
2022-10-06 16:16:20 +0530

    updated styling for tags

--- ui/components/component-library/tag/tag.js
+++ ui/components/component-library/tag/tag.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 import Box from '../../ui/box/box';
 import { Text } from '../text';
 import {
@@ -9,18 +10,19 @@ import {
   SIZES,
 } from '../../../helpers/constants/design-system';
 
-export const Tag = ({ label, ...props }) => {
+export const Tag = ({ label, className, ...props }) => {
   return (
     <Box
-      className="tag"
-      {...props}
+      className={classnames('tag', className)}
       backgroundColor={COLORS.BACKGROUND_DEFAULT}
       borderColor={COLORS.BORDER_DEFAULT}
       borderWidth={1}
       justifyContent={JUSTIFY_CONTENT.CENTER}
       alignItems={ALIGN_ITEMS.CENTER}
       borderRadius={SIZES.XL}
-      padding={[2, 4]}
+      paddingLeft={1}
+      paddingRight={1}
+      {...props}
     >
       <Text variant="body-sm">{label}</Text>
     </Box>
@@ -32,6 +34,10 @@ Tag.propTypes = {
    * The text content of the Tag component
    */
   label: PropTypes.string,
+  /**
+   * Additional classNames to be added to the Tag component
+   */
+  className: PropTypes.string,
 };
 
 export default Tag;
