8cbd4554e437274f701e56138c4a286ceccca5e3
Frederik Bolding <frederik.bolding@gmail.com>
2022-10-06 15:08:15 +0200

    Add snap name

--- ui/components/app/flask/choose-keyring-accounts-list/choose-keyring-accounts-list.js
+++ ui/components/app/flask/choose-keyring-accounts-list/choose-keyring-accounts-list.js
@@ -1,7 +1,15 @@
 import React, { useLayoutEffect, useRef } from 'react';
+import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import CheckBox from '../../../ui/check-box';
 import Identicon from '../../../ui/identicon';
+import { getSnaps } from '../../../../selectors';
+import Typography from '../../../ui/typography/typography';
+import {
+  COLORS,
+  TYPOGRAPHY,
+  FONT_WEIGHT,
+} from '../../../../helpers/constants/design-system';
 
 const ChooseKeyringAccountsList = ({
   accounts,
@@ -27,17 +35,21 @@ const ChooseKeyringAccountsList = ({
     return false;
   };
 
+  const snaps = useSelector(getSnaps);
+
   // TODO: add tooltip to show ChainId, add Icons, add chain count (IF past 3), add extra div to display chain names
   const List = () => {
     return (
       <div className="choose-keyring-accounts-list__wrapper">
         <div className="choose-keyring-accounts-list__list">
           {accounts.map((account, index) => {
+            // eslint-disable-next-line no-unused-vars
             const { address, snapId, suggestedChainNames, namespace } = account;
             const isSelectedAccount = Boolean(
               selectedAccounts[namespace]?.[snapId]?.[address],
             );
             const isConflict = checkIfConflict(account);
+            const snap = snaps[snapId];
             return (
               <div
                 key={`choose-keyring-accounts-list-${index}`}
@@ -55,6 +67,16 @@ const ChooseKeyringAccountsList = ({
                   <div className="choose-keyring-accounts-list__account__info">
                     <div className="choose-keyring-accounts-list__account__label">
                       {address}
+                    </div>
+                    <div>
+                      <Typography
+                        className="choose-keyring-accounts-list__account__snap"
+                        color={COLORS.TEXT_ALTERNATIVE}
+                        variant={TYPOGRAPHY.H6}
+                        fontWeight={FONT_WEIGHT.NORMAL}
+                      >
+                        via {snap.manifest.proposedName}
+                      </Typography>
                     </div>
                   </div>
                 </div>
