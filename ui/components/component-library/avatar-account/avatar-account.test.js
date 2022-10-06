03e7a9beea07a6698b0f0e3b36c67a904278f202
NidhiKJha <menidhikjha@gmail.com>
2022-10-06 23:33:08 +0530

    updated tests for avatar account

--- ui/components/component-library/avatar-account/avatar-account.test.js
+++ ui/components/component-library/avatar-account/avatar-account.test.js
@@ -2,6 +2,7 @@
 import { render } from '@testing-library/react';
 import React from 'react';
 import { AvatarAccount } from './avatar-account';
+import 'jest-canvas-mock';
 
 describe('AvatarAccount', () => {
   const args = {
@@ -10,6 +11,12 @@ describe('AvatarAccount', () => {
   it('should render Jazzicon correctly', () => {
     const { getByTestId } = render(
       <AvatarAccount data-testid="avatar-account" type="Jazzicon" {...args} />,
+    );
+    expect(getByTestId('avatar-account')).toBeDefined();
+  });
+  it('should render Blockie correctly', () => {
+    const { getByTestId } = render(
+      <AvatarAccount data-testid="avatar-account" type="Blockie" {...args} />,
     );
     expect(getByTestId('avatar-account')).toBeDefined();
   });
