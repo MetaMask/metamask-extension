068afe2c2269a0e63a34acb6a1db0f17fca0419f
VSaric <vladimir.saric@consensys.net>
2022-10-07 18:32:29 +0200

    FIxing e2e tests

--- test/e2e/tests/signature-request.spec.js
+++ test/e2e/tests/signature-request.spec.js
@@ -56,7 +56,7 @@ describe('Sign Typed Data V4 Signature Request', function () {
         const origin = content[0];
         const address = content[1];
         const message = await driver.findElement(
-          '.signature-request-message--node-value',
+          '.signature-request-message__node__value',
         );
         assert.equal(await title.getText(), 'Signature request');
         assert.equal(await name.getText(), 'Ether Mail');
@@ -140,7 +140,7 @@ describe('Sign Typed Data V3 Signature Request', function () {
         const origin = content[0];
         const address = content[1];
         const messages = await driver.findElements(
-          '.signature-request-message--node-value',
+          '.signature-request-message__node__value',
         );
         assert.equal(await title.getText(), 'Signature request');
         assert.equal(await name.getText(), 'Ether Mail');
