1cdf9b680ccf5f3da9adb4e9a7728f3edb95560e
NidhiKJha <menidhikjha@gmail.com>
2022-10-11 15:07:57 +0530

    added tag url component

--- ui/components/component-library/tag-url/tag-url.js
+++ ui/components/component-library/tag-url/tag-url.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import { BaseAvatar } from '../base-avatar';
 import Box from '../../ui/box/box';
 import { Text } from '../text';
 import {
   ALIGN_ITEMS,
   COLORS,
+  DISPLAY,
   JUSTIFY_CONTENT,
+  BORDER_COLORS,
   SIZES,
 } from '../../../helpers/constants/design-system';
 
-export const TagUrl = ({ label, className, ...props }) => {
+export const TagUrl = ({ label, className, imageSource, cta, ...props }) => {
   return (
     <Box
       className={classnames('tag-url', className)}
@@ -24,12 +27,32 @@ export const TagUrl = ({ label, className, ...props }) => {
       paddingRight={1}
       {...props}
     >
+      {/* Going to replace this when AvatarFavicon will be merged */}
+      <BaseAvatar
+        size={SIZES.MD}
+        display={DISPLAY.FLEX}
+        alignItems={ALIGN_ITEMS.CENTER}
+        justifyContent={JUSTIFY_CONTENT.CENTER}
+        borderColor={BORDER_COLORS.TRANSPARENT}
+        className={classnames('avatar-favicon', className)}
+        {...props}
+      >
+        <img
+          className="avatar-favicon__image"
+          src={imageSource}
+          alt="avatar favicon"
+        />
+      </BaseAvatar>
       <Text variant="body-sm">{label}</Text>
     </Box>
   );
 };
 
 TagUrl.propTypes = {
+  /**
+   * The imageSource accepts the string of the image to be rendered
+   */
+  imageSource: PropTypes.string,
   /**
    * The text content of the TagUrl component
    */
