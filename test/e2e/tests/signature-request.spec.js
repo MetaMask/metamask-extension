34e6a4157149043f013ea93d8c80a6da108b994a
Filip Sekulic <filip.sekulic@consensys.net>
2022-10-12 17:07:48 +0200

    Fixed e2e test

--- test/e2e/tests/signature-request.spec.js
+++ test/e2e/tests/signature-request.spec.js
@@ -53,8 +53,10 @@ describe('Sign Typed Data V4 Signature Request', function () {
         const content = await driver.findElements(
           '.signature-request-content__info',
         );
+        const verifyContractDetailsButton = await driver.findElement(
+          '.signature-request-content__verify-contract-details',
+        );
         const origin = content[0];
-        const address = content[1];
         const message = await driver.findElement(
           '.signature-request-data__node__value',
         );
@@ -62,10 +64,8 @@ describe('Sign Typed Data V4 Signature Request', function () {
         assert.equal(await name.getText(), 'Ether Mail');
         assert.equal(await origin.getText(), 'http://127.0.0.1:8080');
         assert.equal(
-          await address.getText(),
-          `${publicAddress.slice(0, 8)}...${publicAddress.slice(
-            publicAddress.length - 8,
-          )}`,
+          await verifyContractDetailsButton.getText(),
+          'Verify contract details',
         );
         assert.equal(await message.getText(), 'Hello, Bob!');
         // Approve signing typed data
@@ -137,8 +137,10 @@ describe('Sign Typed Data V3 Signature Request', function () {
         const content = await driver.findElements(
           '.signature-request-content__info',
         );
+        const verifyContractDetailsButton = await driver.findElement(
+          '.signature-request-content__verify-contract-details',
+        );
         const origin = content[0];
-        const address = content[1];
         const messages = await driver.findElements(
           '.signature-request-data__node__value',
         );
@@ -146,10 +148,8 @@ describe('Sign Typed Data V3 Signature Request', function () {
         assert.equal(await name.getText(), 'Ether Mail');
         assert.equal(await origin.getText(), 'http://127.0.0.1:8080');
         assert.equal(
-          await address.getText(),
-          `${publicAddress.slice(0, 8)}...${publicAddress.slice(
-            publicAddress.length - 8,
-          )}`,
+          await verifyContractDetailsButton.getText(),
+          'Verify contract details',
         );
         assert.equal(await messages[4].getText(), 'Hello, Bob!');
 
