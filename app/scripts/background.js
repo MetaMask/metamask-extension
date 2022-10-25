68642ee47af11b726c3f0849837c26ddb3d74885
Jyoti Puri <jyotipuri@gmail.com>
2022-10-25 08:54:02 +0400

    Updating controller dependency (#16033)
    * Updating controller dependency
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fixes
    
    * Lavamoat auto
    
    * Update URLs for phishing detection testcase
    
    * update lavamoat files
    
    * call phishingController.test synchronously again
    
    * bump @metamask/controllers to v32.0.1
    
    * lint
    
    * update policy files
    
    * bump controllers version again
    
    * modify update phishing list strategy
    
    * revert back to use isOutOfDate, but without blocking substream
    
    * possible way to fix e2e tests?
    
    * enable testing
    
    * Remove promise return from setupController in background.js, as it is no longer used
    
    * Ensure updatePhishingLists is called in MM contrller constructer, so that phishing lists are updated right away
    
    Co-authored-by: seaona <mariona@gmx.es>
    Co-authored-by: Alex <adonesky@gmail.com>
    Co-authored-by: Dan Miller <danjm.com@gmail.com>

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -170,7 +170,7 @@ if (isManifestV3) {
 async function initialize(remotePort) {
   const initState = await loadStateFromPersistence();
   const initLangCode = await getFirstPreferredLangCode();
-  await setupController(initState, initLangCode, remotePort);
+  setupController(initState, initLangCode, remotePort);
   if (!isManifestV3) {
     await loadPhishingWarningPage();
   }
@@ -307,7 +307,6 @@ async function loadStateFromPersistence() {
  * @param {object} initState - The initial state to start the controller with, matches the state that is emitted from the controller.
  * @param {string} initLangCode - The region code for the language preferred by the current user.
  * @param {string} remoteSourcePort - remote application port connecting to extension.
- * @returns {Promise} After setup is complete.
  */
 function setupController(initState, initLangCode, remoteSourcePort) {
   //
@@ -662,8 +661,6 @@ function setupController(initState, initLangCode, remoteSourcePort) {
 
     updateBadge();
   }
-
-  return Promise.resolve();
 }
 
 //
