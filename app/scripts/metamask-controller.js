984a1984a623e202a1bd95f446b3a8204ec423e1
David Walsh <davidwalsh83@gmail.com>
2022-10-26 11:12:38 -0500

    Use web extension polyfill instead of chrome storage directly

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -54,6 +54,7 @@ import {
 } from '@metamask/snap-controllers';
 ///: END:ONLY_INCLUDE_IN
 
+import browser from 'webextension-polyfill';
 import {
   ASSET_TYPES,
   TRANSACTION_STATUSES,
@@ -141,6 +142,7 @@ import seedPhraseVerifier from './lib/seed-phrase-verifier';
 import MetaMetricsController from './controllers/metametrics';
 import { segment } from './lib/segment';
 import createMetaRPCHandler from './lib/createMetaRPCHandler';
+
 import {
   CaveatMutatorFactories,
   getCaveatSpecifications,
@@ -2339,7 +2341,7 @@ export default class MetamaskController extends EventEmitter {
     });
 
     try {
-      const { loginToken } = await chrome.storage.session.get(['loginToken']);
+      const { loginToken } = await browser.storage.session.get(['loginToken']);
       if (loginToken) {
         const { vault } = this.keyringController.store.getState();
         await this.keyringController.submitEncryptionKey(loginToken, vault);
@@ -2347,7 +2349,7 @@ export default class MetamaskController extends EventEmitter {
     } catch (e) {
       // If somehow this login token doesn't work properly,
       // remove it and the user will get shown back to the unlock screen
-      await chrome.storage.session.remove(['loginToken']);
+      await browser.storage.session.remove(['loginToken']);
       throw e;
     } finally {
       this.appStateController.store.updateState({
@@ -3940,7 +3942,7 @@ export default class MetamaskController extends EventEmitter {
     );
 
     if (isManifestV3) {
-      await chrome.storage.session.set({ loginToken });
+      await browser.storage.session.set({ loginToken });
     }
 
     if (!addresses.length) {
@@ -4335,7 +4337,7 @@ export default class MetamaskController extends EventEmitter {
     ledgerKeyring?.destroy?.();
 
     if (isManifestV3) {
-      chrome.storage.session.remove(['loginToken']);
+      browser.storage.session.remove(['loginToken']);
     }
 
     return this.keyringController.setLocked();
