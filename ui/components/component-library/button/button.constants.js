16973fe9def336a40887a762e974154bf505979c
garrettbear <gwhisten@gmail.com>
2022-10-28 11:38:47 -0700

    update tests

--- ui/components/component-library/button/button.constants.js
+++ ui/components/component-library/button/button.constants.js
@@ -1,3 +1,12 @@
+import { SIZES } from '../../../helpers/constants/design-system';
+
+export const BUTTON_SIZES = {
+  SM: SIZES.SM,
+  MD: SIZES.MD,
+  LG: SIZES.LG,
+  AUTO: SIZES.AUTO,
+};
+
 export const BUTTON_TYPES = {
   PRIMARY: 'primary',
   SECONDARY: 'secondary',
--- ui/components/component-library/button/button.test.js
+++ ui/components/component-library/button/button.test.js
@@ -1,49 +1,78 @@
 /* eslint-disable jest/require-top-level-describe */
 import { render } from '@testing-library/react';
 import React from 'react';
-import { BUTTON_SIZES } from './button.constants';
-import { ButtonBase } from './button';
+import { BUTTON_SIZES, BUTTON_TYPES } from './button.constants';
+import { Button } from './button';
 
-describe('ButtonBase', () => {
+describe('Button', () => {
   it('should render button element correctly', () => {
     const { getByTestId, getByText, container } = render(
-      <ButtonBase data-testid="button-base">Button base</ButtonBase>,
+      <Button data-testid="button">Button</Button>,
     );
-    expect(getByText('Button base')).toBeDefined();
+    expect(getByText('Button')).toBeDefined();
     expect(container.querySelector('button')).toBeDefined();
-    expect(getByTestId('button-base')).toHaveClass('mm-button');
+    expect(getByTestId('button')).toHaveClass('mm-button');
   });
 
   it('should render anchor element correctly', () => {
     const { getByTestId, container } = render(
-      <ButtonBase as="a" data-testid="button-base" />,
+      <Button as="a" data-testid="button" />,
     );
-    expect(getByTestId('button-base')).toHaveClass('mm-button');
+    expect(getByTestId('button')).toHaveClass('mm-button');
     const anchor = container.getElementsByTagName('a').length;
     expect(anchor).toBe(1);
   });
 
   it('should render anchor element correctly by href only being passed', () => {
     const { getByTestId, container } = render(
-      <ButtonBase href="#" data-testid="button-base" />,
+      <Button href="#" data-testid="button" />,
     );
-    expect(getByTestId('button-base')).toHaveClass('mm-button');
+    expect(getByTestId('button')).toHaveClass('mm-button');
     const anchor = container.getElementsByTagName('a').length;
     expect(anchor).toBe(1);
   });
 
   it('should render button as block', () => {
-    const { getByTestId } = render(<ButtonBase block data-testid="block" />);
+    const { getByTestId } = render(<Button block data-testid="block" />);
     expect(getByTestId('block')).toHaveClass(`mm-button--block`);
+  });
+
+  it('should render with different button types', () => {
+    const { getByTestId } = render(
+      <>
+        <Button
+          type={BUTTON_TYPES.PRIMARY}
+          data-testid={BUTTON_TYPES.PRIMARY}
+        />
+        <Button
+          type={BUTTON_TYPES.SECONDARY}
+          data-testid={BUTTON_TYPES.SECONDARY}
+        />
+        <Button type={BUTTON_TYPES.LINK} data-testid={BUTTON_TYPES.LINK} />
+      </>,
+    );
+    expect(getByTestId(BUTTON_TYPES.PRIMARY)).toHaveClass(
+      `mm-button-${BUTTON_TYPES.PRIMARY}`,
+    );
+    expect(getByTestId(BUTTON_TYPES.SECONDARY)).toHaveClass(
+      `mm-button-${BUTTON_TYPES.SECONDARY}`,
+    );
+    expect(getByTestId(BUTTON_TYPES.LINK)).toHaveClass(
+      `mm-button-${BUTTON_TYPES.LINK}`,
+    );
   });
 
   it('should render with different size classes', () => {
     const { getByTestId } = render(
       <>
-        <ButtonBase size={BUTTON_SIZES.AUTO} data-testid={BUTTON_SIZES.AUTO} />
-        <ButtonBase size={BUTTON_SIZES.SM} data-testid={BUTTON_SIZES.SM} />
-        <ButtonBase size={BUTTON_SIZES.MD} data-testid={BUTTON_SIZES.MD} />
-        <ButtonBase size={BUTTON_SIZES.LG} data-testid={BUTTON_SIZES.LG} />
+        <Button
+          size={BUTTON_SIZES.AUTO}
+          type={BUTTON_TYPES.LINK}
+          data-testid={BUTTON_SIZES.AUTO}
+        />
+        <Button size={BUTTON_SIZES.SM} data-testid={BUTTON_SIZES.SM} />
+        <Button size={BUTTON_SIZES.MD} data-testid={BUTTON_SIZES.MD} />
+        <Button size={BUTTON_SIZES.LG} data-testid={BUTTON_SIZES.LG} />
       </>,
     );
     expect(getByTestId(BUTTON_SIZES.AUTO)).toHaveClass(
@@ -62,7 +91,7 @@ describe('ButtonBase', () => {
 
   it('should render with added classname', () => {
     const { getByTestId } = render(
-      <ButtonBase data-testid="classname" className="mm-button--test" />,
+      <Button data-testid="classname" className="mm-button--test" />,
     );
     expect(getByTestId('classname')).toHaveClass('mm-button--test');
   });
@@ -70,8 +99,8 @@ describe('ButtonBase', () => {
   it('should render with different button states', () => {
     const { getByTestId } = render(
       <>
-        <ButtonBase loading data-testid="loading" />
-        <ButtonBase disabled data-testid="disabled" />
+        <Button loading data-testid="loading" />
+        <Button disabled data-testid="disabled" />
       </>,
     );
     expect(getByTestId('loading')).toHaveClass(`mm-button--loading`);
@@ -79,7 +108,7 @@ describe('ButtonBase', () => {
   });
   it('should render with icon', () => {
     const { getByTestId } = render(
-      <ButtonBase
+      <Button
         data-testid="icon"
         icon="add-square-filled"
         iconProps={{ 'data-testid': 'base-button-icon' }}
@@ -88,4 +117,14 @@ describe('ButtonBase', () => {
 
     expect(getByTestId('base-button-icon')).toBeDefined();
   });
+});
+
+it('should render as danger', () => {
+  const { getByTestId } = render(
+    <>
+      <Button danger data-testid="danger" />
+    </>,
+  );
+
+  expect(getByTestId('danger')).toHaveClass('mm-button-primary--type-danger');
 });
