fd8b70ae1f96b31f5d0fb6cf084c9bbfe22b1d5e
dan437 <80175477+dan437@users.noreply.github.com>
2022-10-31 14:54:06 +0100

    Disable loading of all token icons in E2E tests

--- test/e2e/mock-e2e.js
+++ test/e2e/mock-e2e.js
@@ -189,25 +189,12 @@ async function setupMocking(server, testSpecificMock) {
       };
     });
 
-  await server
-    .forGet(
-      'https://static.metaswap.codefi.network/api/v1/tokenIcons/1337/0x0d8775f648430679a709e98d2b0cb6250d2887ef.png',
-    )
-    .thenCallback(() => {
-      return {
-        statusCode: 200,
-      };
-    });
-
-  await server
-    .forGet(
-      'https://static.metaswap.codefi.network/api/v1/tokenIcons/1337/0x2efa2cb29c2341d8e5ba7d3262c9e9d6f1bf3711.png',
-    )
-    .thenCallback(() => {
-      return {
-        statusCode: 200,
-      };
-    });
+  // It disables loading of all token icons, e.g. this URL: https://static.metaswap.codefi.network/api/v1/tokenIcons/1337/0x0000000000000000000000000000000000000000.png
+  await server.forGet(/\/tokenIcons/u).thenCallback(() => {
+    return {
+      statusCode: 200,
+    };
+  });
 
   await server
     .forGet('https://min-api.cryptocompare.com/data/price')
