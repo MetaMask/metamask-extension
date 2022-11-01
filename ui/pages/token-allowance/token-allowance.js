1bd7d568134eaf6e4df45c552c10fe273449afec
adnansahovic <adnan.grb@hotmail.com>
2022-11-01 14:20:47 +0100

    added i18n interpolation system to token-allowance screen

--- ui/pages/token-allowance/token-allowance.js
+++ ui/pages/token-allowance/token-allowance.js
@@ -259,26 +259,46 @@ export default function TokenAllowance({
           </Typography>
         </Box>
       </Box>
-      <Box marginBottom={5} marginLeft={4} marginRight={4}>
+      <Box marginLeft={4} marginRight={4}>
         <Typography
           variant={TYPOGRAPHY.H3}
           fontWeight={FONT_WEIGHT.BOLD}
           align={TEXT_ALIGN.CENTER}
         >
-          {isFirstPage && t('setSpendingCap')}
+          {isFirstPage &&
+            t('setSpendingCap', [
+              <Box marginTop={4} key={tokenAddress}>
+                <ContractTokenValues
+                  tokenName={tokenSymbol}
+                  address={tokenAddress}
+                  chainId={fullTxData.chainId}
+                  rpcPrefs={rpcPrefs}
+                />
+              </Box>,
+            ])}
           {!isFirstPage &&
             (customTokenAmount === 0
-              ? t('revokeSpendingCap')
-              : t('reviewSpendingCap'))}
+              ? t('revokeSpendingCap', [
+                  <Box marginTop={4} key={tokenAddress}>
+                    <ContractTokenValues
+                      tokenName={tokenSymbol}
+                      address={tokenAddress}
+                      chainId={fullTxData.chainId}
+                      rpcPrefs={rpcPrefs}
+                    />
+                  </Box>,
+                ])
+              : t('reviewSpendingCap', [
+                  <Box marginTop={4} key={tokenAddress}>
+                    <ContractTokenValues
+                      tokenName={tokenSymbol}
+                      address={tokenAddress}
+                      chainId={fullTxData.chainId}
+                      rpcPrefs={rpcPrefs}
+                    />
+                  </Box>,
+                ]))}
         </Typography>
-      </Box>
-      <Box>
-        <ContractTokenValues
-          tokenName={tokenSymbol}
-          address={tokenAddress}
-          chainId={fullTxData.chainId}
-          rpcPrefs={rpcPrefs}
-        />
       </Box>
       <Box
         marginTop={1}
