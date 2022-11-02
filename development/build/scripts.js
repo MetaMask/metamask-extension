6cececbaa6473ac8be3020862ea868519f2a8294
kumavis <aaron@kumavis.me>
2022-11-01 22:15:23 -1000

    fix resolver for exports fields and add esm transpiling for deps

--- development/build/scripts.js
+++ development/build/scripts.js
@@ -794,6 +794,8 @@ function createBuildConfiguration() {
     transform: [],
     plugin: [],
     require: [],
+    // fix resolver for exports field
+    packageFilter: packageExportsCjsFix,
     // non-standard bify options
     manualExternal: [],
     manualIgnore: [],
@@ -836,7 +838,19 @@ function setupBundlerDefaults(
         {
           global: true,
           // ignore all deps, except for @lightclients/kevlar
-          ignore: [/\/node_modules\/(?!@lightclients\/kevlar\/)/],
+          ignore: [
+            composeIgnoreRegex([
+              '@lightclients/kevlar',
+              '@chainsafe/bls',
+              'bigint-crypto-utils',
+              'bigint-mod-arith',
+              '@lodestar/api',
+              '@lodestar/config',
+              '@lodestar/light-client',
+              '@lodestar/params',
+              '@lodestar/types',
+            ]),
+          ],
         },
       ],
       // Inline `fs.readFileSync` files
@@ -1090,4 +1104,82 @@ function beep() {
 function gracefulError(err) {
   console.warn(err);
   beep();
+}
+
+function composeIgnoreRegex(includeDependencies) {
+  // create a regex to match everything other than the specified dependencies
+  return new RegExp(
+    `/node_modules/(?!${includeDependencies.map((d) => `(${d}/)`).join('|')})`,
+    'u',
+  );
+}
+
+function packageExportsCjsFix(pkg, _dir) {
+  if (pkg.exports === undefined) {
+    return pkg;
+  }
+  const newPkg = { ...pkg };
+
+  // polyfill main
+
+  if (newPkg.main === undefined) {
+    const primaryExports = newPkg.exports['.'];
+    if (primaryExports !== undefined) {
+      newPkg.main = getValueForExportsEntry(primaryExports);
+    }
+  }
+
+  // polyfill browser fields
+
+  newPkg.browser = newPkg.browser || {};
+  // migrate primary browser export to obj format
+  if (typeof newPkg.browser === 'string') {
+    if (newPkg.main === undefined) {
+      newPkg.main = '__main_fix__.js';
+    }
+    newPkg.browser = { [newPkg.main]: newPkg.browser };
+  }
+  // populate browser fields with exports
+  for (const key of Object.keys(newPkg.exports)) {
+    if (key === '.') {
+      continue;
+    }
+    if (key in newPkg.browser) {
+      continue;
+    }
+    const exportsEntry = newPkg.exports[key];
+    newPkg.browser[key] = getValueForExportsEntry(exportsEntry);
+  }
+
+  return newPkg;
+}
+
+// "exports": {
+//     ".": {
+//       "node": {
+//         "require": "./dist/cjs/index.node.cjs",
+//         "import": "./dist/esm/index.node.js",
+//         "module": "./dist/esm/index.node.js"
+//       },
+//       "default": "./dist/esm/index.browser.js"
+//     },
+//     "./esm-browser-bundle": "./dist/bundles/esm.min.js",
+//     "./iife-browser-bundle": "./dist/bundles/iife.js",
+//     "./umd-browser-bundle": "./dist/bundles/umd.js",
+//     "./types": "./types/index.d.ts"
+//   },
+
+function getValueForExportsEntry(exportsEntry) {
+  if (typeof exportsEntry === 'string') {
+    return exportsEntry;
+  } else if (typeof exportsEntry === 'object') {
+    const exportValue =
+      exportsEntry.browser || exportsEntry.default || exportsEntry.import;
+    if (typeof exportValue === 'string') {
+      return exportValue;
+    } else if (exportValue === 'object') {
+      return exportValue.require || exportValue.import;
+    }
+  }
+  return undefined;
 }
