3d36c503633c9c2dc2548b769f5e1f354478ca1b
Frederik Bolding <frederik.bolding@gmail.com>
2022-11-08 12:13:59 +0100

    Fix lint

--- test/e2e/run-all.js
+++ test/e2e/run-all.js
@@ -17,7 +17,7 @@ const getTestPathsForTestDir = async (testDir) => {
 function chunk(array, chunkSize) {
   const result = [];
   for (let i = 0; i < array.length; i += chunkSize) {
-      result.push(array.slice(i, i + chunkSize));
+    result.push(array.slice(i, i + chunkSize));
   }
   return result;
 }
