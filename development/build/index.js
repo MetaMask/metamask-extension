26b8f1f3b9484bc4aa7e72a10f5f334e6a0403d4
Mark Stacey <markjstacey@gmail.com>
2022-11-09 11:04:58 -0330

    Remove obsolete builds
    The `brave` and `opera` builds are obsolete; in practice we have been
    using the Chrome build for those browsers, because they are based upon
    Chromium and are compatible with the Chrome extension API.

--- development/build/index.js
+++ development/build/index.js
@@ -71,7 +71,7 @@ async function defineAndRunBuildTasks() {
     version,
   } = await parseArgv();
 
-  const browserPlatforms = ['firefox', 'chrome', 'brave', 'opera'];
+  const browserPlatforms = ['firefox', 'chrome'];
 
   const browserVersionMap = getBrowserVersionMap(browserPlatforms, version);
 
