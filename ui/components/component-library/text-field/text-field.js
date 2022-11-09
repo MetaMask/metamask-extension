f43ff907358bc133971f4cf0ca7faf852e4929e9
georgewrmarshall <george.marshall@consensys.net>
2022-11-08 18:47:39 -0600

    Updating showClearButton prop name and making component dumber

--- ui/components/component-library/text-field/text-field.js
+++ ui/components/component-library/text-field/text-field.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
@@ -18,69 +18,60 @@ import { TextFieldBase } from '../text-field-base';
 
 export const TextField = ({
   className,
-  showClear,
-  clearButtonIconProps,
+  showClearButton,
+  clearButtonOnClick,
   clearButtonProps,
   rightAccessory,
-  value: valueProp,
-  onChange,
-  onClear,
   inputProps,
+  value,
+  onChange,
   ...props
-}) => {
-  const [value, setValue] = useState(valueProp || '');
-  const handleOnChange = (e) => {
-    setValue(e.target.value);
-    onChange?.(e);
-  };
-  const handleClear = (e) => {
-    setValue('');
-    clearButtonProps?.onClick?.(e);
-    onClear?.(e);
-  };
-  return (
-    <TextFieldBase
-      className={classnames('mm-text-field', className)}
-      value={value}
-      onChange={handleOnChange}
-      rightAccessory={
-        value && showClear ? (
-          <>
-            {/* replace with ButtonIcon */}
-            <Box
-              className="mm-text-field__button-clear"
-              as="button"
-              display={DISPLAY.FLEX}
-              alignItems={ALIGN_ITEMS.CENTER}
-              justifyContent={JUSTIFY_CONTENT.CENTER}
-              backgroundColor={COLORS.TRANSPARENT}
-              padding={0}
-              {...clearButtonProps} // don't override onClick
-              onClick={handleClear}
-            >
-              <Icon
-                name={ICON_NAMES.CLOSE_OUTLINE}
-                size={SIZES.SM}
-                aria-label="Clear" // TODO: i18n
-                {...clearButtonIconProps}
-              />
-            </Box>
-            {rightAccessory}
-          </>
-        ) : (
-          rightAccessory
-        )
-      }
-      inputProps={{
-        marginRight: showClear ? 6 : 0,
-        ...inputProps,
-      }}
-      {...props}
-    />
-  );
-};
+}) => (
+  <TextFieldBase
+    className={classnames('mm-text-field', className)}
+    value={value}
+    onChange={onChange}
+    rightAccessory={
+      value && showClearButton ? (
+        <>
+          {/* TODO: replace with ButtonIcon */}
+          <Box
+            className="mm-text-field__button-clear"
+            as="button"
+            display={DISPLAY.FLEX}
+            alignItems={ALIGN_ITEMS.CENTER}
+            justifyContent={JUSTIFY_CONTENT.CENTER}
+            backgroundColor={COLORS.TRANSPARENT}
+            padding={0}
+            onClick={clearButtonOnClick}
+            {...clearButtonProps}
+            aria-label="Clear" // TODO: i18n
+          >
+            <Icon name={ICON_NAMES.CLOSE_OUTLINE} size={SIZES.SM} />
+          </Box>
+          {rightAccessory}
+        </>
+      ) : (
+        rightAccessory
+      )
+    }
+    inputProps={{
+      marginRight: showClearButton ? 6 : 0,
+      ...inputProps,
+    }}
+    {...props}
+  />
+);
 
 TextField.propTypes = {
+  /**
+   * The value af the TextField
+   */
+  value: TextFieldBase.propTypes.value.isRequired,
+  /**
+   * The onChange handler af the TextField
+   */
+  onChange: TextFieldBase.propTypes.onChange.isRequired,
   /**
    * An additional className to apply to the text-field
    */
@@ -88,19 +79,11 @@ TextField.propTypes = {
   /**
    * Show a clear button to clear the input
    */
-  showClear: PropTypes.bool,
-  /**
-   * The event handler for when the clear button is clicked
-   */
-  onClear: PropTypes.func,
+  showClearButton: PropTypes.bool,
   /**
    * The props to pass to the clear button
    */
   clearButtonProps: PropTypes.shape(Box.PropTypes),
-  /**
-   * The props to pass to the icon inside of the close button
-   */
-  clearButtonIconProps: PropTypes.shape(Icon.PropTypes),
   /**
    * TextField accepts all the props from TextFieldBase and Box
    */
--- ui/components/component-library/text-field/text-field.test.js
+++ ui/components/component-library/text-field/text-field.test.js
@@ -1,25 +1,31 @@
 /* eslint-disable jest/require-top-level-describe */
 import React from 'react';
 import { fireEvent, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import { TextField } from './text-field';
+
+// setup function as per testing-library docs
+// https://testing-library.com/docs/user-event/intro
+function setup(jsx) {
+  return {
+    user: userEvent.setup(),
+    ...render(jsx),
+  };
+}
 
 describe('TextField', () => {
   it('should render correctly', () => {
     const { getByRole } = render(<TextField />);
     expect(getByRole('textbox')).toBeDefined();
   });
-  it('should render and be able to input text', () => {
-    const { getByTestId } = render(
-      <TextField inputProps={{ 'data-testid': 'text-field' }} />,
-    );
-    const textField = getByTestId('text-field');
+  it('should render and be able to input text', async () => {
+    const { user, getByRole } = setup(<TextField />);
 
-    expect(textField.value).toBe(''); // initial value is empty string
-    fireEvent.change(textField, { target: { value: 'text value' } });
-    expect(textField.value).toBe('text value');
-    fireEvent.change(textField, { target: { value: '' } }); // reset value
-    expect(textField.value).toBe(''); // value is empty string after reset
+    const textField = getByRole('textbox');
+
+    await user.type(textField, 'text value');
+    expect(textField).toHaveValue('text value');
   });
   it('should render and fire onFocus and onBlur events', () => {
     const onFocus = jest.fn();
@@ -38,46 +44,33 @@ describe('TextField', () => {
     fireEvent.blur(textField);
     expect(onBlur).toHaveBeenCalledTimes(1);
   });
-  it('should render and fire onChange event', () => {
+  it('should render and fire onChange event', async () => {
     const onChange = jest.fn();
-    const { getByTestId } = render(
+    const { user, getByRole } = setup(
       <TextField
         inputProps={{ 'data-testid': 'text-field' }}
         onChange={onChange}
       />,
     );
-    const textField = getByTestId('text-field');
+    const textField = getByRole('textbox');
 
-    fireEvent.change(textField, { target: { value: 'text value' } });
-    expect(onChange).toHaveBeenCalledTimes(1);
+    await user.type(textField, '123');
+    expect(textField).toHaveValue('123');
+    expect(onChange).toHaveBeenCalledTimes(3);
   });
-  it('should render and fire onClick event', () => {
+  it('should render and fire onClick event', async () => {
     const onClick = jest.fn();
-    const { getByTestId } = render(
-      <TextField
-        inputProps={{ 'data-testid': 'text-field' }}
-        onClick={onClick}
-      />,
+    const { user, getByTestId } = setup(
+      <TextField data-testid="text-field" onClick={onClick} />,
     );
-    const textField = getByTestId('text-field');
-
-    fireEvent.click(textField);
+    await user.click(getByTestId('text-field'));
     expect(onClick).toHaveBeenCalledTimes(1);
   });
-  it('should render showClear button when showClear is true and value exists', () => {
-    const { getByRole, getByTestId } = render(
-      <TextField
-        clearButtonProps={{ 'data-testid': 'clear-button' }}
-        clearButtonIconProps={{ 'data-testid': 'clear-button-icon' }}
-        showClear
-      />,
-    );
-    const textField = getByRole('textbox');
-    expect(textField.value).toBe(''); // initial value is empty string
-    fireEvent.change(textField, { target: { value: 'text value' } });
-    expect(textField.value).toBe('text value');
-    expect(getByTestId('clear-button')).toBeDefined();
-    expect(getByTestId('clear-button-icon')).toBeDefined();
+  it('should render showClearButton button when showClearButton is true and value exists', () => {
+    // As TextField is a controlled input we need to pass in a value for the clear button to appear
+    const { getByRole } = render(<TextField value="text" showClearButton />);
+    expect(getByRole('button', { name: /clear/u })).toBeDefined();
+    expect(getByRole('textbox')).toBeDefined();
   });
   it('should render with the rightAccessory', () => {
     const { getByText } = render(
@@ -85,71 +78,41 @@ describe('TextField', () => {
     );
     expect(getByText('right-accessory')).toBeDefined();
   });
-  it('should still render with the rightAccessory when showClear is true', () => {
+  it('should still render with the rightAccessory when showClearButton is true', () => {
     const { getByRole, getByTestId, getByText } = render(
+      // As TextField is a controlled input we need to pass in a value for the clear button to appear
       <TextField
+        value="text"
         clearButtonProps={{ 'data-testid': 'clear-button' }}
-        clearButtonIconProps={{ 'data-testid': 'clear-button-icon' }}
         rightAccessory={<div>right-accessory</div>}
-        showClear
+        showClearButton
       />,
     );
-    const textField = getByRole('textbox');
-    expect(textField.value).toBe(''); // initial value is empty string
-    fireEvent.change(textField, { target: { value: 'text value' } });
-    expect(textField.value).toBe('text value');
     expect(getByTestId('clear-button')).toBeDefined();
-    expect(getByTestId('clear-button-icon')).toBeDefined();
     expect(getByText('right-accessory')).toBeDefined();
+    expect(getByRole('textbox')).toBeDefined();
   });
-  it('should clear text when clear button is clicked', () => {
-    const { getByRole, getByTestId } = render(
+  it('should fire onClick event when passed to clearButtonProps when clear button is clicked', async () => {
+    // As TextField is a controlled input we need to pass in a value for the clear button to appear
+    const fn = jest.fn();
+    const { user, getByRole } = setup(
+      <TextField value="text" clearButtonOnClick={fn} showClearButton />,
+    );
+    await user.click(getByRole('button', { name: /clear/u }));
+    expect(fn).toHaveBeenCalledTimes(1); // clear button onClick is fired
+  });
+  it('should fire clearButtonProps.onClick event when passed to clearButtonProps.onClick prop', async () => {
+    // As TextField is a controlled input we need to pass in a value for the clear button to appear
+    const fn = jest.fn();
+    const { user, getByRole } = setup(
       <TextField
-        clearButtonProps={{ 'data-testid': 'clear-button' }}
-        clearButtonIconProps={{ 'data-testid': 'clear-button-icon' }}
-        rightAccessory={<div>right-accessory</div>}
-        showClear
+        value="text"
+        clearButtonProps={{ onClick: fn }}
+        showClearButton
       />,
     );
-    const textField = getByRole('textbox');
-    fireEvent.change(textField, { target: { value: 'text value' } });
-    expect(textField.value).toBe('text value');
-    fireEvent.click(getByTestId('clear-button'));
-    expect(textField.value).toBe('');
-  });
-  it('should fire onClear event when passed to onClear prop', () => {
-    const onClear = jest.fn();
-    const { getByRole, getByTestId } = render(
-      <TextField
-        onClear={onClear}
-        clearButtonProps={{ 'data-testid': 'clear-button' }}
-        clearButtonIconProps={{ 'data-testid': 'clear-button-icon' }}
-        showClear
-      />,
-    );
-    const textField = getByRole('textbox');
-    fireEvent.change(textField, { target: { value: 'text value' } });
-    expect(textField.value).toBe('text value');
-    fireEvent.click(getByTestId('clear-button'));
-    expect(onClear).toHaveBeenCalledTimes(1);
-  });
-  it('should fire clearButtonProps.onClick event when passed to clearButtonProps.onClick prop', () => {
-    const onClear = jest.fn();
-    const onClick = jest.fn();
-    const { getByRole, getByTestId } = render(
-      <TextField
-        onClear={onClear}
-        clearButtonProps={{ 'data-testid': 'clear-button', onClick }}
-        clearButtonIconProps={{ 'data-testid': 'clear-button-icon' }}
-        showClear
-      />,
-    );
-    const textField = getByRole('textbox');
-    fireEvent.change(textField, { target: { value: 'text value' } });
-    expect(textField.value).toBe('text value');
-    fireEvent.click(getByTestId('clear-button'));
-    expect(onClear).toHaveBeenCalledTimes(1);
-    expect(onClick).toHaveBeenCalledTimes(1);
+    await user.click(getByRole('button', { name: /clear/u }));
+    expect(fn).toHaveBeenCalledTimes(1);
   });
   it('should be able to accept inputProps', () => {
     const { getByRole } = render(
