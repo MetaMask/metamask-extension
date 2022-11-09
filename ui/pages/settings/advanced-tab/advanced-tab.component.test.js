25bb6ef861c96f3746977ffee002d096be52b0da
Thomas Huang <tmashuang@users.noreply.github.com>
2022-11-09 09:20:10 -0800

    Continue converting tests from enzyme to @testing-library/react (#16401)

--- ui/pages/settings/advanced-tab/advanced-tab.component.test.js
+++ ui/pages/settings/advanced-tab/advanced-tab.component.test.js
@@ -1,180 +1,69 @@
 import React from 'react';
-import sinon from 'sinon';
-import { shallow } from 'enzyme';
-import TextField from '../../../components/ui/text-field';
-import { LEDGER_TRANSPORT_TYPES } from '../../../../shared/constants/hardware-wallets';
-import ToggleButton from '../../../components/ui/toggle-button';
-import AdvancedTab from './advanced-tab.component';
+import { fireEvent } from '@testing-library/react';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import mockState from '../../../../test/data/mock-state.json';
+import { renderWithProvider } from '../../../../test/lib/render-helpers';
+import AdvancedTab from '.';
+
+const mockSetAutoLockTimeLimit = jest.fn();
+const mockSetShowTestNetworks = jest.fn();
+const mockSetUseTokenDetection = jest.fn();
+
+jest.mock('../../../store/actions.js', () => {
+  return {
+    setAutoLockTimeLimit: () => mockSetAutoLockTimeLimit,
+    setShowTestNetworks: () => mockSetShowTestNetworks,
+    setUseTokenDetection: () => mockSetUseTokenDetection,
+  };
+});
 
 describe('AdvancedTab Component', () => {
-  let component;
-  let setAutoLockTimeLimitSpy = sinon.spy();
-  const toggleTestnet = sinon.spy();
-  const toggleTokenDetection = sinon.spy();
-
-  beforeAll(() => {
-    component = shallow(
-      <AdvancedTab
-        ipfsGateway=""
-        setAutoLockTimeLimit={setAutoLockTimeLimitSpy}
-        setIpfsGateway={() => undefined}
-        setShowFiatConversionOnTestnetsPreference={() => undefined}
-        setShowTestNetworks={toggleTestnet}
-        showTestNetworks={false}
-        ledgerTransportType={LEDGER_TRANSPORT_TYPES.U2F}
-        setLedgerTransportPreference={() => undefined}
-        setDismissSeedBackUpReminder={() => undefined}
-        dismissSeedBackUpReminder={false}
-        useTokenDetection
-        setUseTokenDetection={toggleTokenDetection}
-        userHasALedgerAccount
-        backupUserData={() => undefined}
-        restoreUserData={() => undefined}
-      />,
-      {
-        context: {
-          t: (s) => `_${s}`,
-        },
-      },
-    );
-  });
+  const mockStore = configureMockStore([thunk])(mockState);
 
   it('should render backup button', () => {
-    expect(component.find('.settings-page__content-row')).toHaveLength(15);
-
-    expect(
-      component
-        .find('.settings-page__content-row')
-        .at(10)
-        .find('.settings-page__content-item'),
-    ).toHaveLength(2);
-
-    expect(
-      component
-        .find('.settings-page__content-row')
-        .at(10)
-        .find('.settings-page__content-item')
-        .at(0)
-        .find('.settings-page__content-description')
-        .props().children,
-    ).toStrictEqual('_backupUserDataDescription');
-
-    expect(
-      component
-        .find('.settings-page__content-row')
-        .at(10)
-        .find('.settings-page__content-item')
-        .at(1)
-        .find('Button')
-        .props().children,
-    ).toStrictEqual('_backup');
+    const { queryByTestId } = renderWithProvider(<AdvancedTab />, mockStore);
+    const backupButton = queryByTestId('backup-button');
+    expect(backupButton).toBeInTheDocument();
   });
 
   it('should render restore button', () => {
-    expect(component.find('.settings-page__content-row')).toHaveLength(15);
-
-    expect(
-      component
-        .find('.settings-page__content-row')
-        .at(11)
-        .find('.settings-page__content-item'),
-    ).toHaveLength(2);
-
-    expect(
-      component
-        .find('.settings-page__content-row')
-        .at(11)
-        .find('.settings-page__content-item')
-        .at(0)
-        .find('.settings-page__content-description')
-        .props().children,
-    ).toStrictEqual('_restoreUserDataDescription');
-
-    expect(
-      component
-        .find('.settings-page__content-row')
-        .at(11)
-        .find('.settings-page__content-item')
-        .at(1)
-        .find('label')
-        .props().children,
-    ).toStrictEqual('_restore');
+    const { queryByTestId } = renderWithProvider(<AdvancedTab />, mockStore);
+    const restoreFile = queryByTestId('restore-file');
+    expect(restoreFile).toBeInTheDocument();
   });
 
   it('should update autoLockTimeLimit', () => {
-    setAutoLockTimeLimitSpy = sinon.spy();
-    component = shallow(
-      <AdvancedTab
-        ipfsGateway=""
-        setAutoLockTimeLimit={setAutoLockTimeLimitSpy}
-        setIpfsGateway={() => undefined}
-        setShowFiatConversionOnTestnetsPreference={() => undefined}
-        ledgerTransportType={LEDGER_TRANSPORT_TYPES.U2F}
-        setLedgerTransportPreference={() => undefined}
-        setDismissSeedBackUpReminder={() => undefined}
-        dismissSeedBackUpReminder={false}
-        setShowTestNetworks={toggleTestnet}
-        useTokenDetection
-        setUseTokenDetection={toggleTokenDetection}
-        userHasALedgerAccount
-        backupUserData={() => undefined}
-        restoreUserData={() => undefined}
-      />,
-      {
-        context: {
-          t: (s) => `_${s}`,
-        },
-      },
-    );
+    const { queryByTestId } = renderWithProvider(<AdvancedTab />, mockStore);
+    const autoLockoutTime = queryByTestId('auto-lockout-time');
+    const autoLockoutButton = queryByTestId('auto-lockout-button');
 
-    const autoTimeout = component.find('.settings-page__content-row').at(9);
-    const textField = autoTimeout.find(TextField);
+    fireEvent.change(autoLockoutTime, { target: { value: 1440 } });
 
-    textField.props().onChange({ target: { value: 1440 } });
-    expect(component.state().autoLockTimeLimit).toStrictEqual(1440);
+    expect(autoLockoutTime).toHaveValue(1440);
 
-    autoTimeout.find('.settings-tab__rpc-save-button').simulate('click');
-    expect(setAutoLockTimeLimitSpy.args[0][0]).toStrictEqual(1440);
+    fireEvent.click(autoLockoutButton);
+
+    expect(mockSetAutoLockTimeLimit).toHaveBeenCalled();
   });
 
   it('should toggle show test networks', () => {
-    const testNetworks = component.find('.settings-page__content-row').at(7);
-    const toggleButton = testNetworks.find(ToggleButton);
-    toggleButton.first().simulate('toggle');
-    expect(toggleTestnet.calledOnce).toStrictEqual(true);
+    const { queryAllByRole } = renderWithProvider(<AdvancedTab />, mockStore);
+
+    const testNetworkToggle = queryAllByRole('checkbox')[4];
+
+    fireEvent.click(testNetworkToggle);
+
+    expect(mockSetShowTestNetworks).toHaveBeenCalled();
   });
 
   it('should toggle token detection', () => {
-    component = shallow(
-      <AdvancedTab
-        ipfsGateway=""
-        setAutoLockTimeLimit={setAutoLockTimeLimitSpy}
-        setIpfsGateway={() => undefined}
-        setShowFiatConversionOnTestnetsPreference={() => undefined}
-        setShowTestNetworks={toggleTestnet}
-        showTestNetworks={false}
-        ledgerTransportType={LEDGER_TRANSPORT_TYPES.U2F}
-        setLedgerTransportPreference={() => undefined}
-        setDismissSeedBackUpReminder={() => undefined}
-        dismissSeedBackUpReminder={false}
-        useTokenDetection
-        setUseTokenDetection={toggleTokenDetection}
-        userHasALedgerAccount
-        backupUserData={() => undefined}
-        restoreUserData={() => undefined}
-      />,
-      {
-        context: {
-          trackEvent: () => undefined,
-          t: (s) => `_${s}`,
-        },
-      },
-    );
-    const useTokenDetection = component
-      .find('.settings-page__content-row')
-      .at(4);
-    const toggleButton = useTokenDetection.find(ToggleButton);
-    toggleButton.first().simulate('toggle');
-    expect(toggleTokenDetection.calledOnce).toStrictEqual(true);
+    const { queryAllByRole } = renderWithProvider(<AdvancedTab />, mockStore);
+
+    const tokenDetectionToggle = queryAllByRole('checkbox')[1];
+
+    fireEvent.click(tokenDetectionToggle);
+
+    expect(mockSetUseTokenDetection).toHaveBeenCalled();
   });
 });
