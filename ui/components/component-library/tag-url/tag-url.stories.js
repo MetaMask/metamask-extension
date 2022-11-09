fbc50819008b937444365ea6ef0ae07f6e2c4441
NidhiKJha <menidhikjha@gmail.com>
2022-11-09 22:27:42 +0530

    updated label as per src

--- ui/components/component-library/tag-url/tag-url.stories.js
+++ ui/components/component-library/tag-url/tag-url.stories.js
@@ -63,11 +63,20 @@ export const Label = (args) => (
 
 export const Src = (args) => (
   <Box display={DISPLAY.FLEX} flexDirection={FLEX_DIRECTION.COLUMN} gap={2}>
-    <TagUrl {...args} src="https://peepeth.com/favicon-32x32.png" />
     <TagUrl
       {...args}
+      label="peepeth.com"
+      src="https://peepeth.com/favicon-32x32.png"
+    />
+    <TagUrl
+      {...args}
+      label="https://1inch.exchange"
       src="https://1inch.exchange/assets/favicon/favicon-32x32.png"
     />
-    <TagUrl {...args} src="https://uniswap.org/favicon.ico" />
+    <TagUrl
+      {...args}
+      label="app.uniswap.org"
+      src="https://uniswap.org/favicon.ico"
+    />
   </Box>
 );
