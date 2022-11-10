2510dac48bc82b01ead39dc498b51dbd8ccfab82
NidhiKJha <menidhikjha@gmail.com>
2022-11-10 20:22:46 +0530

    added readme to story file

--- ui/components/component-library/avatar/avatar.stories.js
+++ ui/components/component-library/avatar/avatar.stories.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { DISPLAY } from '../../../helpers/constants/design-system';
 import Box from '../../ui/box/box';
-// import README from './README.mdx';
 import { AvatarFaviconProps } from '../avatar-favicon/avatar-favicon.stories';
 import { AvatarNetworkProps } from '../avatar-network/avatar-network.stories';
 import { AvatarTokenProps } from '../avatar-token/avatar-token.stories';
 import { AvatarAccountProps } from '../avatar-account/avatar-account.stories';
+import README from './README.mdx';
 
 import { Avatar, AVATAR_TYPES } from '.';
 
@@ -13,6 +13,11 @@ export default {
   title: 'Components/ComponentLibrary/Avatar',
   id: __filename,
   component: Avatar,
+  parameters: {
+    docs: {
+      page: README,
+    },
+  },
 };
 
 export const Types = () => (
