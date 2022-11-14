ff3dafbffff40ee72c9e7c9901499475c03f5f61
weizman <weizmangal@gmail.com>
2022-11-14 13:16:31 +0200

    Update scripts.js

--- development/build/scripts.js
+++ development/build/scripts.js
@@ -616,7 +616,7 @@ function createFactoredBuild({
       }
 
       // wrap sentry so it will still have access to natives after LM scuttling
-      for (const browser of []) {
+      for (const browser of browserPlatforms) {
         const sentryPath = `./dist/${browser}/sentry-install.js`;
         const content = readFileSync(sentryPath, 'utf8');
         const wrappedContent = wrapAgainstScuttling(content, {
