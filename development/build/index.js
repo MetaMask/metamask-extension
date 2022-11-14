29e653749a9d4ec455e2d62a1b70ee8d3b7431fe
weizman <weizmangal@gmail.com>
2022-11-14 12:46:22 +0200

    Update index.js

--- development/build/index.js
+++ development/build/index.js
@@ -74,7 +74,7 @@ async function defineAndRunBuildTasks() {
 
   // build lavamoat runtime file
   await lavapack.buildRuntime({
-    scuttleGlobalThis: !!!true,
+    scuttleGlobalThis: false,
     scuttleGlobalThisExceptions: [
       // globals used by different mm deps outside of lm compartment
       'toString',
