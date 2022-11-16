edaac62ef05e2def728e6447620e8db2bfdb4d09
Frederik Bolding <frederik.bolding@gmail.com>
2022-11-16 13:49:27 +0100

    Bump iframe-execution-environment

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -663,7 +663,7 @@ export default class MetamaskController extends EventEmitter {
     ///: BEGIN:ONLY_INCLUDE_IN(flask)
     this.snapExecutionService = new IframeExecutionService({
       iframeUrl: new URL(
-        'https://metamask.github.io/iframe-execution-environment/0.10.0',
+        'https://metamask.github.io/iframe-execution-environment/0.11.0',
       ),
       messenger: this.controllerMessenger.getRestricted({
         name: 'ExecutionService',
