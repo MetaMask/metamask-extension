167b311b9a99ba631c460966ff4d943aa185c6a9
georgewrmarshall <george.marshall@consensys.net>
2022-11-17 17:48:13 -0600

    Moving icon env vars to jest specific env.js file

--- test/env.js
+++ test/env.js
@@ -1,9 +1,1 @@
-import { generateIconNames } from '../development/generate-icon-names.js';
 process.env.METAMASK_ENV = 'test';
-
-/**
- * Used for testing components that use the Icon component
- * 'ui/components/component-library/icon/icon.js'
- */
-
-process.env.ICON_NAMES = generateIconNames();
