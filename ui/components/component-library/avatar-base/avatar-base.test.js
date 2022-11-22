a3a8f807e952404cfd705b47a39aa18699d0a2dc
NidhiKJha <menidhikjha@gmail.com>
2022-11-22 22:06:20 +0530

    added tests and fixed indentation

--- ui/components/component-library/avatar-base/avatar-base.test.js
+++ ui/components/component-library/avatar-base/avatar-base.test.js
@@ -1,6 +1,8 @@
 /* eslint-disable jest/require-top-level-describe */
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
+
+import { COLORS } from '../../../helpers/constants/design-system';
 
 import { AvatarBase } from './avatar-base';
 
@@ -36,6 +38,87 @@ describe('AvatarBase', () => {
     );
     expect(getByTestId('avatar-base-xl')).toHaveClass(
       'mm-avatar-base--size-xl',
+    );
+  });
+  // className
+  it('should render with custom className', () => {
+    const { getByTestId } = render(
+      <AvatarBase data-testid="avatar-base" className="test-class" />,
+    );
+    expect(getByTestId('avatar-base')).toHaveClass('test-class');
+  });
+  // children
+  it('should render children', () => {
+    render(
+      <AvatarBase data-testid="avatar-base">
+        <img width="100%" src="./images/arbitrum.svg" />
+      </AvatarBase>,
+    );
+    const image = screen.getByRole('img');
+    expect(image).toBeDefined();
+    expect(image).toHaveAttribute('src', './images/arbitrum.svg');
+  });
+  // color
+  it('should render with different colors', () => {
+    const { getByTestId } = render(
+      <>
+        <AvatarBase
+          color={COLORS.SUCCESS_DEFAULT}
+          data-testid={COLORS.SUCCESS_DEFAULT}
+        />
+        <AvatarBase
+          color={COLORS.ERROR_DEFAULT}
+          data-testid={COLORS.ERROR_DEFAULT}
+        />
+      </>,
+    );
+    expect(getByTestId(COLORS.SUCCESS_DEFAULT)).toHaveClass(
+      `box--color-${COLORS.SUCCESS_DEFAULT}`,
+    );
+    expect(getByTestId(COLORS.ERROR_DEFAULT)).toHaveClass(
+      `box--color-${COLORS.ERROR_DEFAULT}`,
+    );
+  });
+  // background color
+  it('should render with different background colors', () => {
+    const { getByTestId } = render(
+      <>
+        <AvatarBase
+          backgroundColor={COLORS.SUCCESS_DEFAULT}
+          data-testid={COLORS.SUCCESS_DEFAULT}
+        />
+        <AvatarBase
+          backgroundColor={COLORS.ERROR_DEFAULT}
+          data-testid={COLORS.ERROR_DEFAULT}
+        />
+      </>,
+    );
+    expect(getByTestId(COLORS.SUCCESS_DEFAULT)).toHaveClass(
+      `box--background-color-${COLORS.SUCCESS_DEFAULT}`,
+    );
+    expect(getByTestId(COLORS.ERROR_DEFAULT)).toHaveClass(
+      `box--background-color-${COLORS.ERROR_DEFAULT}`,
+    );
+  });
+  // border color
+  it('should render with different border colors', () => {
+    const { getByTestId } = render(
+      <>
+        <AvatarBase
+          borderColor={COLORS.SUCCESS_DEFAULT}
+          data-testid={COLORS.SUCCESS_DEFAULT}
+        />
+        <AvatarBase
+          borderColor={COLORS.ERROR_DEFAULT}
+          data-testid={COLORS.ERROR_DEFAULT}
+        />
+      </>,
+    );
+    expect(getByTestId(COLORS.SUCCESS_DEFAULT)).toHaveClass(
+      `box--border-color-${COLORS.SUCCESS_DEFAULT}`,
+    );
+    expect(getByTestId(COLORS.ERROR_DEFAULT)).toHaveClass(
+      `box--border-color-${COLORS.ERROR_DEFAULT}`,
     );
   });
 });
