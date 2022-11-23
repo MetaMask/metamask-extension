494264e87ee66a1842a4209bf15d29857165d69e
weizman <weizmangal@gmail.com>
2022-11-23 13:44:09 +0200

    improve use snow

--- app/scripts/use-snow.js
+++ app/scripts/use-snow.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line import/unambiguous
 (function () {
   const log = console.log.bind(console);
-  window.top.SNOW((w) => {
-    const msg = 'SNOW INTERCEPTED NEW WINDOW CREATION IN METAMASK APP: ';
-    log(msg, w, w?.frameElement);
+  const msg = 'SNOW INTERCEPTED NEW WINDOW CREATION IN METAMASK APP: ';
+  window.top.SNOW((win) => {
+    log(msg, win, win?.frameElement);
   });
 })();
