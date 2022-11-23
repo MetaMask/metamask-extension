792cab754e1f9ba28554d5291dd44b863b856cc1
weizman <weizmangal@gmail.com>
2022-11-23 12:51:30 +0200

    support static files jobs behaviour per browser (#5)

--- app/scripts/use-snow.js
+++ app/scripts/use-snow.js
@@ -1,9 +1,5 @@
 // eslint-disable-next-line import/unambiguous
 (function () {
-  // Snow does not support Firefox yet
-  if (window.navigator.userAgent.includes('Firefox')) {
-    return;
-  }
   const log = console.log.bind(console);
   window.top.SNOW((w) => {
     const msg = 'SNOW INTERCEPTED NEW WINDOW CREATION IN METAMASK APP: ';
