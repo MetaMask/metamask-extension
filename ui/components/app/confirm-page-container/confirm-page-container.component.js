2984e2da398dba936d1e945ecb9fc823aee968de
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2022-11-28 16:02:20 +0000

    satisfy lint

--- ui/components/app/confirm-page-container/confirm-page-container.component.js
+++ ui/components/app/confirm-page-container/confirm-page-container.component.js
@@ -14,7 +14,6 @@ import { NETWORK_TO_NAME_MAP } from '../../../../shared/constants/network';
 import { PageContainerFooter } from '../../ui/page-container';
 import Dialog from '../../ui/dialog';
 import Button from '../../ui/button';
-import Box from '../../ui/box';
 import ActionableMessage from '../../ui/actionable-message/actionable-message';
 import SenderToRecipient from '../../ui/sender-to-recipient';
 
