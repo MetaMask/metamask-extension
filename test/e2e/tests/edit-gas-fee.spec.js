bca73825689d62e8c7f832b2d309da3600307331
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2022-11-28 13:02:40 +0000

    fix broken e2e test

--- test/e2e/tests/edit-gas-fee.spec.js
+++ test/e2e/tests/edit-gas-fee.spec.js
@@ -133,10 +133,10 @@ describe('Editing Confirm Transaction', function () {
         await driver.clickElement('[data-testid="edit-gas-fee-item-custom"]');
 
         // enter max fee
-        await driver.fill('[data-testid="base-fee-input"]', '8.5');
+        await driver.fill('[data-testid="base-fee-input"]', '5');
 
         // enter priority fee
-        await driver.fill('[data-testid="priority-fee-input"]', '8.5');
+        await driver.fill('[data-testid="priority-fee-input"]', '5');
 
         // save default values
         await driver.clickElement('input[type="checkbox"]');
@@ -151,11 +151,11 @@ describe('Editing Confirm Transaction', function () {
         // has correct updated value on the confirm screen the transaction
         await driver.waitForSelector({
           css: '.transaction-detail-item:nth-of-type(1) h6:nth-of-type(2)',
-          text: '0.00085 ETH',
+          text: '0.0005 ETH',
         });
         await driver.waitForSelector({
           css: '.transaction-detail-item:nth-of-type(2) h6:nth-of-type(2)',
-          text: '1.00085 ETH',
+          text: '1.0005 ETH',
         });
 
         // confirms the transaction
