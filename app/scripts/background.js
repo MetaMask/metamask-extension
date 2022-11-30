032153a3751feb0fec424a8b8abcf1556726a0e1
cryptotavares <joao.tavares@consensys.net>
2022-11-30 17:32:05 +0000

    chore: add backgroundController to background.js
    Replace all background.js business logic
    by the new BackgroundController

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,65 +1,28 @@
 /**
  * @file The entry point for the web extension singleton process.
  */
-
-import endOfStream from 'end-of-stream';
-import pump from 'pump';
-import debounce from 'debounce-stream';
 import log from 'loglevel';
 import browser from 'webextension-polyfill';
-import { storeAsStream } from '@metamask/obs-store';
-import PortStream from 'extension-port-stream';
 
-import { ethErrors } from 'eth-rpc-errors';
-import {
-  ENVIRONMENT_TYPE_POPUP,
-  ENVIRONMENT_TYPE_NOTIFICATION,
-  ENVIRONMENT_TYPE_FULLSCREEN,
-  EXTENSION_MESSAGES,
-  PLATFORM_FIREFOX,
-} from '../../shared/constants/app';
-import { SECOND } from '../../shared/constants/time';
-import {
-  REJECT_NOTFICIATION_CLOSE,
-  REJECT_NOTFICIATION_CLOSE_SIG,
-  EVENT,
-  EVENT_NAMES,
-  TRAITS,
-} from '../../shared/constants/metametrics';
+import { EXTENSION_MESSAGES } from '../../shared/constants/app';
+import { EVENT, EVENT_NAMES, TRAITS } from '../../shared/constants/metametrics';
 import { checkForLastErrorAndLog } from '../../shared/modules/browser-runtime.utils';
 import { isManifestV3 } from '../../shared/modules/mv3.utils';
-import { maskObject } from '../../shared/modules/object.utils';
 import migrations from './migrations';
 import Migrator from './lib/migrator';
 import ExtensionPlatform from './platforms/extension';
 import LocalStore from './lib/local-store';
 import ReadOnlyNetworkStore from './lib/network-store';
-import { SENTRY_STATE } from './lib/setupSentry';
 
-import createStreamSink from './lib/createStreamSink';
-import NotificationManager, {
-  NOTIFICATION_MANAGER_EVENTS,
-} from './lib/notification-manager';
-import MetamaskController, {
-  METAMASK_CONTROLLER_EVENTS,
-} from './metamask-controller';
+import NotificationManager from './lib/notification-manager';
 import rawFirstTimeState from './first-time-state';
 import getFirstPreferredLangCode from './lib/get-first-preferred-lang-code';
 import getObjStructure from './lib/getObjStructure';
-import setupEnsIpfsResolver from './lib/ens-ipfs/setup';
-import { deferredPromise, getPlatform } from './lib/util';
+import BackgroundController from './background-controller';
 /* eslint-enable import/first */
 
 const { sentry } = global;
 const firstTimeState = { ...rawFirstTimeState };
-
-const metamaskInternalProcessHash = {
-  [ENVIRONMENT_TYPE_POPUP]: true,
-  [ENVIRONMENT_TYPE_NOTIFICATION]: true,
-  [ENVIRONMENT_TYPE_FULLSCREEN]: true,
-};
-
-const metamaskBlockedPorts = ['trezor-connect'];
 
 log.setDefaultLevel(process.env.METAMASK_DEBUG ? 'debug' : 'info');
 
@@ -68,11 +31,6 @@ const platform = new ExtensionPlatform();
 const notificationManager = new NotificationManager();
 global.METAMASK_NOTIFIER = notificationManager;
 
-let popupIsOpen = false;
-let notificationIsOpen = false;
-let uiIsTriggering = false;
-const openMetamaskTabsIDs = {};
-const requestAccountTabIds = {};
 let controller;
 
 // state persistence
@@ -84,27 +42,9 @@ if (inTest || process.env.METAMASK_DEBUG) {
   global.stateHooks.metamaskGetState = localStore.get.bind(localStore);
 }
 
-const phishingPageUrl = new URL(process.env.PHISHING_WARNING_PAGE_URL);
-
 const ONE_SECOND_IN_MILLISECONDS = 1_000;
 // Timeout for initializing phishing warning page.
 const PHISHING_WARNING_PAGE_TIMEOUT = ONE_SECOND_IN_MILLISECONDS;
-
-const ACK_KEEP_ALIVE_MESSAGE = 'ACK_KEEP_ALIVE_MESSAGE';
-const WORKER_KEEP_ALIVE_MESSAGE = 'WORKER_KEEP_ALIVE_MESSAGE';
-
-/**
- * This deferred Promise is used to track whether initialization has finished.
- *
- * It is very important to ensure that `resolveInitialization` is *always*
- * called once initialization has completed, and that `rejectInitialization` is
- * called if initialization fails in an unrecoverable way.
- */
-const {
-  promise: isInitialized,
-  resolve: resolveInitialization,
-  reject: rejectInitialization,
-} = deferredPromise();
 
 /**
  * Sends a message to the dapp(s) content script to signal it can connect to MetaMask background as
@@ -154,25 +94,6 @@ const sendReadyMessageToTabs = async () => {
       });
   }
 };
-
-// These are set after initialization
-let connectRemote;
-let connectExternal;
-
-browser.runtime.onConnect.addListener(async (...args) => {
-  // Queue up connection attempts here, waiting until after initialization
-  await isInitialized;
-
-  // This is set in `setupController`, which is called as part of initialization
-  connectRemote(...args);
-});
-browser.runtime.onConnectExternal.addListener(async (...args) => {
-  // Queue up connection attempts here, waiting until after initialization
-  await isInitialized;
-
-  // This is set in `setupController`, which is called as part of initialization
-  connectExternal(...args);
-});
 
 initialize().catch(log.error);
 
@@ -237,18 +158,28 @@ initialize().catch(log.error);
  * @returns {Promise} Setup complete.
  */
 async function initialize() {
+  const backgroundController = new BackgroundController({
+    localStore,
+    platform,
+    notificationManager,
+  });
+
   try {
     const initState = await loadStateFromPersistence();
     const initLangCode = await getFirstPreferredLangCode();
-    setupController(initState, initLangCode);
+
+    backgroundController.init(initState, initLangCode);
+
     if (!isManifestV3) {
       await loadPhishingWarningPage();
     }
     await sendReadyMessageToTabs();
+
     log.info('MetaMask initialization complete.');
-    resolveInitialization();
+
+    backgroundController.hasInitialized.resolve();
   } catch (error) {
-    rejectInitialization(error);
+    backgroundController.hasInitialized.reject(error);
   }
 }
 
@@ -373,410 +304,9 @@ async function loadStateFromPersistence() {
   return versionedData.data;
 }
 
-/**
- * Initializes the MetaMask Controller with any initial state and default language.
- * Configures platform-specific error reporting strategy.
- * Streams emitted state updates to platform-specific storage strategy.
- * Creates platform listeners for new Dapps/Contexts, and sets up their data connections to the controller.
- *
- * @param {object} initState - The initial state to start the controller with, matches the state that is emitted from the controller.
- * @param {string} initLangCode - The region code for the language preferred by the current user.
- */
-function setupController(initState, initLangCode) {
-  //
-  // MetaMask Controller
-  //
-
-  controller = new MetamaskController({
-    infuraProjectId: process.env.INFURA_PROJECT_ID,
-    // User confirmation callbacks:
-    showUserConfirmation: triggerUi,
-    openPopup,
-    // initial state
-    initState,
-    // initial locale code
-    initLangCode,
-    // platform specific api
-    platform,
-    notificationManager,
-    browser,
-    getRequestAccountTabIds: () => {
-      return requestAccountTabIds;
-    },
-    getOpenMetamaskTabsIds: () => {
-      return openMetamaskTabsIDs;
-    },
-    localStore,
-  });
-
-  setupEnsIpfsResolver({
-    getCurrentChainId: controller.networkController.getCurrentChainId.bind(
-      controller.networkController,
-    ),
-    getIpfsGateway: controller.preferencesController.getIpfsGateway.bind(
-      controller.preferencesController,
-    ),
-    provider: controller.provider,
-  });
-
-  // setup state persistence
-  pump(
-    storeAsStream(controller.store),
-    debounce(1000),
-    createStreamSink((state) => localStore.set(state)),
-    (error) => {
-      log.error('MetaMask - Persistence pipeline failed', error);
-    },
-  );
-
-  setupSentryGetStateGlobal(controller);
-
-  const isClientOpenStatus = () => {
-    return (
-      popupIsOpen ||
-      Boolean(Object.keys(openMetamaskTabsIDs).length) ||
-      notificationIsOpen
-    );
-  };
-
-  const onCloseEnvironmentInstances = (isClientOpen, environmentType) => {
-    // if all instances of metamask are closed we call a method on the controller to stop gasFeeController polling
-    if (isClientOpen === false) {
-      controller.onClientClosed();
-      // otherwise we want to only remove the polling tokens for the environment type that has closed
-    } else {
-      // in the case of fullscreen environment a user might have multiple tabs open so we don't want to disconnect all of
-      // its corresponding polling tokens unless all tabs are closed.
-      if (
-        environmentType === ENVIRONMENT_TYPE_FULLSCREEN &&
-        Boolean(Object.keys(openMetamaskTabsIDs).length)
-      ) {
-        return;
-      }
-      controller.onEnvironmentTypeClosed(environmentType);
-    }
-  };
-
-  /**
-   * A runtime.Port object, as provided by the browser:
-   *
-   * @see https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/runtime/Port
-   * @typedef Port
-   * @type Object
-   */
-
-  /**
-   * Connects a Port to the MetaMask controller via a multiplexed duplex stream.
-   * This method identifies trusted (MetaMask) interfaces, and connects them differently from untrusted (web pages).
-   *
-   * @param {Port} remotePort - The port provided by a new context.
-   */
-  connectRemote = async (remotePort) => {
-    const processName = remotePort.name;
-
-    if (metamaskBlockedPorts.includes(remotePort.name)) {
-      return;
-    }
-
-    let isMetaMaskInternalProcess = false;
-    const sourcePlatform = getPlatform();
-
-    if (sourcePlatform === PLATFORM_FIREFOX) {
-      isMetaMaskInternalProcess = metamaskInternalProcessHash[processName];
-    } else {
-      isMetaMaskInternalProcess =
-        remotePort.sender.origin === `chrome-extension://${browser.runtime.id}`;
-    }
-
-    const senderUrl = remotePort.sender?.url
-      ? new URL(remotePort.sender.url)
-      : null;
-
-    if (isMetaMaskInternalProcess) {
-      const portStream = new PortStream(remotePort);
-      // communication with popup
-      controller.isClientOpen = true;
-      controller.setupTrustedCommunication(portStream, remotePort.sender);
-
-      if (isManifestV3) {
-        // If we get a WORKER_KEEP_ALIVE message, we respond with an ACK
-        remotePort.onMessage.addListener((message) => {
-          if (message.name === WORKER_KEEP_ALIVE_MESSAGE) {
-            // To test un-comment this line and wait for 1 minute. An error should be shown on MetaMask UI.
-            remotePort.postMessage({ name: ACK_KEEP_ALIVE_MESSAGE });
-          }
-        });
-      }
-
-      if (processName === ENVIRONMENT_TYPE_POPUP) {
-        popupIsOpen = true;
-        endOfStream(portStream, () => {
-          popupIsOpen = false;
-          const isClientOpen = isClientOpenStatus();
-          controller.isClientOpen = isClientOpen;
-          onCloseEnvironmentInstances(isClientOpen, ENVIRONMENT_TYPE_POPUP);
-        });
-      }
-
-      if (processName === ENVIRONMENT_TYPE_NOTIFICATION) {
-        notificationIsOpen = true;
-
-        endOfStream(portStream, () => {
-          notificationIsOpen = false;
-          const isClientOpen = isClientOpenStatus();
-          controller.isClientOpen = isClientOpen;
-          onCloseEnvironmentInstances(
-            isClientOpen,
-            ENVIRONMENT_TYPE_NOTIFICATION,
-          );
-        });
-      }
-
-      if (processName === ENVIRONMENT_TYPE_FULLSCREEN) {
-        const tabId = remotePort.sender.tab.id;
-        openMetamaskTabsIDs[tabId] = true;
-
-        endOfStream(portStream, () => {
-          delete openMetamaskTabsIDs[tabId];
-          const isClientOpen = isClientOpenStatus();
-          controller.isClientOpen = isClientOpen;
-          onCloseEnvironmentInstances(
-            isClientOpen,
-            ENVIRONMENT_TYPE_FULLSCREEN,
-          );
-        });
-      }
-    } else if (
-      senderUrl &&
-      senderUrl.origin === phishingPageUrl.origin &&
-      senderUrl.pathname === phishingPageUrl.pathname
-    ) {
-      const portStream = new PortStream(remotePort);
-      controller.setupPhishingCommunication({
-        connectionStream: portStream,
-      });
-    } else {
-      if (remotePort.sender && remotePort.sender.tab && remotePort.sender.url) {
-        const tabId = remotePort.sender.tab.id;
-        const url = new URL(remotePort.sender.url);
-        const { origin } = url;
-
-        remotePort.onMessage.addListener((msg) => {
-          if (msg.data && msg.data.method === 'eth_requestAccounts') {
-            requestAccountTabIds[origin] = tabId;
-          }
-        });
-      }
-      connectExternal(remotePort);
-    }
-  };
-
-  // communication with page or other extension
-  connectExternal = (remotePort) => {
-    const portStream = new PortStream(remotePort);
-    controller.setupUntrustedCommunication({
-      connectionStream: portStream,
-      sender: remotePort.sender,
-    });
-  };
-
-  //
-  // User Interface setup
-  //
-
-  updateBadge();
-  controller.txController.on(
-    METAMASK_CONTROLLER_EVENTS.UPDATE_BADGE,
-    updateBadge,
-  );
-  controller.messageManager.on(
-    METAMASK_CONTROLLER_EVENTS.UPDATE_BADGE,
-    updateBadge,
-  );
-  controller.personalMessageManager.on(
-    METAMASK_CONTROLLER_EVENTS.UPDATE_BADGE,
-    updateBadge,
-  );
-  controller.decryptMessageManager.on(
-    METAMASK_CONTROLLER_EVENTS.UPDATE_BADGE,
-    updateBadge,
-  );
-  controller.encryptionPublicKeyManager.on(
-    METAMASK_CONTROLLER_EVENTS.UPDATE_BADGE,
-    updateBadge,
-  );
-  controller.typedMessageManager.on(
-    METAMASK_CONTROLLER_EVENTS.UPDATE_BADGE,
-    updateBadge,
-  );
-  controller.appStateController.on(
-    METAMASK_CONTROLLER_EVENTS.UPDATE_BADGE,
-    updateBadge,
-  );
-
-  controller.controllerMessenger.subscribe(
-    METAMASK_CONTROLLER_EVENTS.APPROVAL_STATE_CHANGE,
-    updateBadge,
-  );
-
-  /**
-   * Updates the Web Extension's "badge" number, on the little fox in the toolbar.
-   * The number reflects the current number of pending transactions or message signatures needing user approval.
-   */
-  function updateBadge() {
-    let label = '';
-    const count = getUnapprovedTransactionCount();
-    if (count) {
-      label = String(count);
-    }
-    // browserAction has been replaced by action in MV3
-    if (isManifestV3) {
-      browser.action.setBadgeText({ text: label });
-      browser.action.setBadgeBackgroundColor({ color: '#037DD6' });
-    } else {
-      browser.browserAction.setBadgeText({ text: label });
-      browser.browserAction.setBadgeBackgroundColor({ color: '#037DD6' });
-    }
-  }
-
-  function getUnapprovedTransactionCount() {
-    const unapprovedTxCount = controller.txController.getUnapprovedTxCount();
-    const { unapprovedMsgCount } = controller.messageManager;
-    const { unapprovedPersonalMsgCount } = controller.personalMessageManager;
-    const { unapprovedDecryptMsgCount } = controller.decryptMessageManager;
-    const { unapprovedEncryptionPublicKeyMsgCount } =
-      controller.encryptionPublicKeyManager;
-    const { unapprovedTypedMessagesCount } = controller.typedMessageManager;
-    const pendingApprovalCount =
-      controller.approvalController.getTotalApprovalCount();
-    const waitingForUnlockCount =
-      controller.appStateController.waitingForUnlock.length;
-    return (
-      unapprovedTxCount +
-      unapprovedMsgCount +
-      unapprovedPersonalMsgCount +
-      unapprovedDecryptMsgCount +
-      unapprovedEncryptionPublicKeyMsgCount +
-      unapprovedTypedMessagesCount +
-      pendingApprovalCount +
-      waitingForUnlockCount
-    );
-  }
-
-  notificationManager.on(
-    NOTIFICATION_MANAGER_EVENTS.POPUP_CLOSED,
-    ({ automaticallyClosed }) => {
-      if (!automaticallyClosed) {
-        rejectUnapprovedNotifications();
-      } else if (getUnapprovedTransactionCount() > 0) {
-        triggerUi();
-      }
-    },
-  );
-
-  function rejectUnapprovedNotifications() {
-    Object.keys(
-      controller.txController.txStateManager.getUnapprovedTxList(),
-    ).forEach((txId) =>
-      controller.txController.txStateManager.setTxStatusRejected(txId),
-    );
-    controller.messageManager.messages
-      .filter((msg) => msg.status === 'unapproved')
-      .forEach((tx) =>
-        controller.messageManager.rejectMsg(
-          tx.id,
-          REJECT_NOTFICIATION_CLOSE_SIG,
-        ),
-      );
-    controller.personalMessageManager.messages
-      .filter((msg) => msg.status === 'unapproved')
-      .forEach((tx) =>
-        controller.personalMessageManager.rejectMsg(
-          tx.id,
-          REJECT_NOTFICIATION_CLOSE_SIG,
-        ),
-      );
-    controller.typedMessageManager.messages
-      .filter((msg) => msg.status === 'unapproved')
-      .forEach((tx) =>
-        controller.typedMessageManager.rejectMsg(
-          tx.id,
-          REJECT_NOTFICIATION_CLOSE_SIG,
-        ),
-      );
-    controller.decryptMessageManager.messages
-      .filter((msg) => msg.status === 'unapproved')
-      .forEach((tx) =>
-        controller.decryptMessageManager.rejectMsg(
-          tx.id,
-          REJECT_NOTFICIATION_CLOSE,
-        ),
-      );
-    controller.encryptionPublicKeyManager.messages
-      .filter((msg) => msg.status === 'unapproved')
-      .forEach((tx) =>
-        controller.encryptionPublicKeyManager.rejectMsg(
-          tx.id,
-          REJECT_NOTFICIATION_CLOSE,
-        ),
-      );
-
-    // Finally, reject all approvals managed by the ApprovalController
-    controller.approvalController.clear(
-      ethErrors.provider.userRejectedRequest(),
-    );
-
-    updateBadge();
-  }
-}
-
 //
 // Etc...
 //
-
-/**
- * Opens the browser popup for user confirmation
- */
-async function triggerUi() {
-  const tabs = await platform.getActiveTabs();
-  const currentlyActiveMetamaskTab = Boolean(
-    tabs.find((tab) => openMetamaskTabsIDs[tab.id]),
-  );
-  // Vivaldi is not closing port connection on popup close, so popupIsOpen does not work correctly
-  // To be reviewed in the future if this behaviour is fixed - also the way we determine isVivaldi variable might change at some point
-  const isVivaldi =
-    tabs.length > 0 &&
-    tabs[0].extData &&
-    tabs[0].extData.indexOf('vivaldi_tab') > -1;
-  if (
-    !uiIsTriggering &&
-    (isVivaldi || !popupIsOpen) &&
-    !currentlyActiveMetamaskTab
-  ) {
-    uiIsTriggering = true;
-    try {
-      await notificationManager.showPopup();
-    } finally {
-      uiIsTriggering = false;
-    }
-  }
-}
-
-/**
- * Opens the browser popup for user confirmation of watchAsset
- * then it waits until user interact with the UI
- */
-async function openPopup() {
-  await triggerUi();
-  await new Promise((resolve) => {
-    const interval = setInterval(() => {
-      if (!notificationIsOpen) {
-        clearInterval(interval);
-        resolve();
-      }
-    }, SECOND);
-  });
-}
 
 // It adds the "App Installed" event into a queue of events, which will be tracked only after a user opts into metrics.
 const addAppInstalledEvent = () => {
@@ -807,15 +337,3 @@ browser.runtime.onInstalled.addListener(({ reason }) => {
     platform.openExtensionInBrowser();
   }
 });
-
-function setupSentryGetStateGlobal(store) {
-  global.stateHooks.getSentryState = function () {
-    const fullState = store.getState();
-    const debugState = maskObject({ metamask: fullState }, SENTRY_STATE);
-    return {
-      browser: window.navigator.userAgent,
-      store: debugState,
-      version: platform.getVersion(),
-    };
-  };
-}
