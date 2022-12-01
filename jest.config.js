2ca91183a0ce94e5b730611ee8fb893b70ef9cd2
Thomas Huang <thomas.b.huang@gmail.com>
2022-12-01 00:36:12 -0800

    Coverage bump up

--- jest.config.js
+++ jest.config.js
@@ -10,7 +10,7 @@ module.exports = {
   coverageReporters: ['html', 'text-summary', 'json-summary'],
   coverageThreshold: {
     global: {
-      branches: 44,
+      branches: 49.36,
       functions: 46,
       lines: 52,
       statements: 52,
