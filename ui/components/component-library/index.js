10079300780b92640a3bb9d940b0230e4222ee0e
Garrett Bear <gwhisten@gmail.com>
2022-12-01 09:26:19 -0800

    Feat/16187/text housekeeping (#16589)
    * text housekeeping
    
    * update testing
    
    * Text constant
    
    * TEXT const to story
    
    * format text sizes
    
    * add associated constants to text component
    
    * add all exports to global index.js
    
    * update snapshot
    
    * update text component variants
    
    * Update ui/components/component-library/text/README.mdx
    
    Co-authored-by: George Marshall <george.marshall@consensys.net>
    
    * update rearrangements
    
    * update text in tests
    
    Co-authored-by: George Marshall <george.marshall@consensys.net>

--- ui/components/component-library/index.js
+++ ui/components/component-library/index.js
@@ -17,7 +17,7 @@ export { Label } from './label';
 export { PickerNetwork } from './picker-network';
 export { Tag } from './tag';
 export { TagUrl } from './tag-url';
-export { Text } from './text';
+export { Text, TEXT_VARIANTS } from './text';
 export { TextField, TEXT_FIELD_TYPES, TEXT_FIELD_SIZES } from './text-field';
 export {
   TextFieldBase,
