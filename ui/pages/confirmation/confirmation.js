026607426df10eb01a4aff3bd59fb2eb5f0b073e
Guillaume Roux <guillaumeroux123@gmail.com>
2022-12-01 15:06:04 +0100

    fix rebase and fix crash when validating a confirmation

--- ui/pages/confirmation/confirmation.js
+++ ui/pages/confirmation/confirmation.js
@@ -149,7 +149,7 @@ export default function ConfirmationPage({
   ///: BEGIN:ONLY_INCLUDE_IN(flask)
   const {
     manifest: { proposedName },
-  } = useSelector((state) => getSnap(state, pendingConfirmation.origin));
+  } = useSelector((state) => getSnap(state, pendingConfirmation?.origin));
 
   const SNAP_DIALOG_TYPE = [
     MESSAGE_TYPE.SNAP_DIALOG_ALERT,
