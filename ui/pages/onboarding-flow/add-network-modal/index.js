53f6f498192f2a918753f97b1964e26da9bb7ed5
David Walsh <davidwalsh83@gmail.com>
2022-12-02 11:47:39 -0600

    Add title to modal

--- ui/pages/onboarding-flow/add-network-modal/index.js
+++ ui/pages/onboarding-flow/add-network-modal/index.js
@@ -1,21 +1,41 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { useI18nContext } from '../../../hooks/useI18nContext';
 
 import { hideModal } from '../../../store/actions';
+
+import Typography from '../../../components/ui/typography/typography';
+import {
+  TEXT_ALIGN,
+  TYPOGRAPHY,
+  FONT_WEIGHT,
+} from '../../../helpers/constants/design-system';
 
 import NetworksForm from '../../settings/networks-tab/networks-form/networks-form';
 
 export default function AddNetworkModal() {
   const dispatch = useDispatch();
+  const t = useI18nContext();
+
   const closeCallback = () =>
     dispatch(hideModal({ name: 'ONBOARDING_ADD_NETWORK' }));
 
   return (
-    <NetworksForm
-      addNewNetwork
-      networksToRender={[]}
-      cancelCallback={closeCallback}
-      submitCallback={closeCallback}
-    />
+    <>
+      <Typography
+        variant={TYPOGRAPHY.H4}
+        align={TEXT_ALIGN.CENTER}
+        fontWeight={FONT_WEIGHT.BOLD}
+        paddingTop={4}
+      >
+        {t('onboardingMetametricsModalTitle')}
+      </Typography>
+      <NetworksForm
+        addNewNetwork
+        networksToRender={[]}
+        cancelCallback={closeCallback}
+        submitCallback={closeCallback}
+      />
+    </>
   );
 }
