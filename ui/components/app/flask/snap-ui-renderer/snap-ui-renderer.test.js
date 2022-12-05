ac63a73c386c865be5fe317bd26439f910d7198e
Frederik Bolding <frederik.bolding@gmail.com>
2022-12-05 13:57:07 +0100

    Fix lint

--- ui/components/app/flask/snap-ui-renderer/snap-ui-renderer.test.js
+++ ui/components/app/flask/snap-ui-renderer/snap-ui-renderer.test.js
@@ -2,8 +2,8 @@ import { NodeType } from '@metamask/snaps-ui';
 import { UI_MAPPING } from './snap-ui-renderer';
 
 describe('Snap UI mapping', () => {
-    it('supports all exposed components', () => {
-        const nodes = Object.values(NodeType);
-        expect(Object.keys(UI_MAPPING).sort()).toStrictEqual(nodes.sort());
-    })
-})
+  it('supports all exposed components', () => {
+    const nodes = Object.values(NodeType);
+    expect(Object.keys(UI_MAPPING).sort()).toStrictEqual(nodes.sort());
+  });
+});
