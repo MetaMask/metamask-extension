2441333eda4181a01c095394f5e9bff37223ca09
NidhiKJha <menidhikjha@gmail.com>
2022-12-06 18:57:41 +0530

    updated tag url component

--- ui/components/component-library/tag-url/tag-url.stories.js
+++ ui/components/component-library/tag-url/tag-url.stories.js
@@ -48,10 +48,29 @@ ActionButtonLabel.args = {
   actionButtonLabel: 'Permissions',
 };
 
-export const ShowLockIcon = Template.bind({});
-ShowLockIcon.args = {
-  showLockIcon: true,
-};
+export const ShowLockIcon = (args) => (
+  <Box display={DISPLAY.FLEX} flexDirection={FLEX_DIRECTION.COLUMN} gap={2}>
+    <TagUrl
+      {...args}
+      label="peepeth.com"
+      src="https://peepeth.com/favicon-32x32.png"
+      showLockIcon
+    />
+    <TagUrl
+      {...args}
+      label="1inch.exchange"
+      src="https://1inch.exchange/assets/favicon/favicon-32x32.png"
+      showLockIcon
+    />
+    <TagUrl
+      {...args}
+      label="app.uniswap.org"
+      src="https://uniswap.org/favicon.ico"
+      showLockIcon
+    />
+    <TagUrl label="fallback" src="" />
+  </Box>
+);
 
 export const Src = (args) => (
   <Box display={DISPLAY.FLEX} flexDirection={FLEX_DIRECTION.COLUMN} gap={2}>
