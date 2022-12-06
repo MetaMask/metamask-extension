de3dfa286039218512149e95606ed5bbebd02049
brad-decker <bhdecker84@gmail.com>
2022-12-06 16:56:36 -0600

    add comments and remove har file

--- .circleci/scripts/deps-install.sh
+++ .circleci/scripts/deps-install.sh
@@ -7,10 +7,3 @@ set -e
 
 yarn install --frozen-lockfile
 
-# Move HAR file into directory with consistent name so that we can cache it
-mkdir -p build-artifacts/yarn-install-har
-har_files=(./*.har)
-if [[ -f "${har_files[0]}" ]]
-then
-  mv ./*.har build-artifacts/yarn-install-har/
-fi
