bd31030da7d2748cff55e70383c7212d2561d932
Thomas Huang <thomas.b.huang@gmail.com>
2022-12-08 06:21:02 -0800

    Convert UnitInput component to RTL.

--- ui/components/ui/unit-input/unit-input.component.test.js
+++ ui/components/ui/unit-input/unit-input.component.test.js
@@ -1,98 +1,59 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
-import sinon from 'sinon';
+import { fireEvent } from '@testing-library/react';
+import { renderWithProvider } from '../../../../test/lib/render-helpers';
 import UnitInput from './unit-input.component';
 
 describe('UnitInput Component', () => {
   describe('rendering', () => {
-    it('should render properly without a suffix', () => {
-      const wrapper = shallow(<UnitInput />);
+    it('should match snapshot without a suffix', () => {
+      const { container } = renderWithProvider(<UnitInput />);
 
-      expect(wrapper).toHaveLength(1);
-      expect(wrapper.find('.unit-input__suffix')).toHaveLength(0);
+      expect(container).toMatchSnapshot();
     });
 
-    it('should render properly with a suffix', () => {
-      const wrapper = shallow(<UnitInput suffix="ETH" />);
+    it('should match snapshot with a suffix', () => {
+      const { container } = renderWithProvider(<UnitInput suffix="ETH" />);
 
-      expect(wrapper).toHaveLength(1);
-      expect(wrapper.find('.unit-input__suffix')).toHaveLength(1);
-      expect(wrapper.find('.unit-input__suffix').text()).toStrictEqual('ETH');
+      expect(container).toMatchSnapshot();
     });
 
-    it('should render properly with a child component', () => {
-      const wrapper = shallow(
+    it('should match snapshot with a child component', () => {
+      const { container } = renderWithProvider(
         <UnitInput>
           <div className="testing">TESTCOMPONENT</div>
         </UnitInput>,
       );
 
-      expect(wrapper).toHaveLength(1);
-      expect(wrapper.find('.testing')).toHaveLength(1);
-      expect(wrapper.find('.testing').text()).toStrictEqual('TESTCOMPONENT');
+      expect(container).toMatchSnapshot();
     });
 
-    it('should render with an error class when props.error === true', () => {
-      const wrapper = shallow(<UnitInput error />);
+    it('should match snapshot of error class when props.error === true', () => {
+      const { container } = renderWithProvider(<UnitInput error />);
 
-      expect(wrapper).toHaveLength(1);
-      expect(wrapper.find('.unit-input--error')).toHaveLength(1);
+      expect(container).toMatchSnapshot();
     });
   });
 
   describe('handling actions', () => {
-    const handleChangeSpy = sinon.spy();
-    const handleBlurSpy = sinon.spy();
+    const handleChangeSpy = jest.fn();
+    const handleOnBlurSpy = jest.fn();
 
-    afterEach(() => {
-      handleChangeSpy.resetHistory();
-      handleBlurSpy.resetHistory();
-    });
+    it('should call onChange and onBlur on input changes with the value', async () => {
+      const { queryByTestId } = renderWithProvider(
+        <UnitInput
+          onChange={handleChangeSpy}
+          onBlur={handleOnBlurSpy}
+          dataTestId="unit-input"
+        />,
+      );
 
-    it('should focus the input on component click', () => {
-      const wrapper = mount(<UnitInput />);
+      const input = queryByTestId('unit-input');
 
-      expect(wrapper).toHaveLength(1);
-      const handleFocusSpy = sinon.spy(wrapper.instance(), 'handleFocus');
-      wrapper.instance().forceUpdate();
-      wrapper.update();
-      expect(handleFocusSpy.callCount).toStrictEqual(0);
-      wrapper.find('.unit-input').simulate('click');
-      expect(handleFocusSpy.callCount).toStrictEqual(1);
-    });
+      fireEvent.blur(input);
+      fireEvent.change(input, { target: { value: 2 } });
 
-    it('should call onChange on input changes with the value', () => {
-      const wrapper = mount(<UnitInput onChange={handleChangeSpy} />);
-
-      expect(wrapper).toHaveLength(1);
-      expect(handleChangeSpy.callCount).toStrictEqual(0);
-      const input = wrapper.find('input');
-      input.simulate('change', { target: { value: 123 } });
-      expect(handleChangeSpy.callCount).toStrictEqual(1);
-      expect(handleChangeSpy.calledWith(123)).toStrictEqual(true);
-      expect(wrapper.state('value')).toStrictEqual(123);
-    });
-
-    it('should set the component state value with props.value', () => {
-      const wrapper = mount(<UnitInput value={123} />);
-
-      expect(wrapper).toHaveLength(1);
-      expect(wrapper.state('value')).toStrictEqual(123);
-    });
-
-    it('should update the component state value with props.value', () => {
-      const wrapper = mount(<UnitInput onChange={handleChangeSpy} />);
-
-      expect(wrapper).toHaveLength(1);
-      expect(handleChangeSpy.callCount).toStrictEqual(0);
-      const input = wrapper.find('input');
-      input.simulate('change', { target: { value: 123 } });
-      expect(wrapper.state('value')).toStrictEqual(123);
-      expect(handleChangeSpy.callCount).toStrictEqual(1);
-      expect(handleChangeSpy.calledWith(123)).toStrictEqual(true);
-      wrapper.setProps({ value: 456 });
-      expect(wrapper.state('value')).toStrictEqual(456);
-      expect(handleChangeSpy.callCount).toStrictEqual(1);
+      expect(handleOnBlurSpy).toHaveBeenCalled();
+      expect(handleChangeSpy).toHaveBeenCalledWith('2');
     });
   });
 });
