c9560b75f282592919538a27c3bc2a0a69aeac63
Garrett Bear <gwhisten@gmail.com>
2022-12-08 10:25:19 -0800

    add avatar icon component (#16755)
    * add avatar icon component
    
    * add avatar icon severities
    
    * avatar icon docs
    
    * update tests and remove stylesheet
    
    * aria label
    
    * remove aria label
    
    * update test
    
    * remove aria label
    
    * iconName prop type string
    
    * Update ui/components/component-library/avatar-icon/README.mdx
    
    Co-authored-by: George Marshall <george.marshall@consensys.net>
    
    * Update ui/components/component-library/avatar-icon/README.mdx
    
    Co-authored-by: George Marshall <george.marshall@consensys.net>
    
    * Update ui/components/component-library/avatar-icon/avatar-icon.js
    
    Co-authored-by: George Marshall <george.marshall@consensys.net>
    
    * Update ui/components/component-library/avatar-icon/avatar-icon.stories.js
    
    Co-authored-by: George Marshall <george.marshall@consensys.net>
    
    * marginSizeControlOptions
    
    Co-authored-by: George Marshall <george.marshall@consensys.net>

--- ui/components/component-library/index.js
+++ ui/components/component-library/index.js
@@ -1,5 +1,6 @@
 export { AvatarAccount } from './avatar-account';
 export { AvatarFavicon } from './avatar-favicon';
+export { AvatarIcon, AVATAR_ICON_SIZES } from './avatar-icon';
 export { AvatarNetwork } from './avatar-network';
 export { AvatarToken } from './avatar-token';
 export { AvatarWithBadge } from './avatar-with-badge';
