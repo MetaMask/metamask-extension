658da486072141d4c36d727a0010a2404f85430f
NidhiKJha <menidhikjha@gmail.com>
2022-12-13 17:49:40 +0530

    housekeeping for avatar favicon

---
+++ ui/components/component-library/avatar-favicon/avatar-favicon.constants.js
@@ -0,0 +1,9 @@
+import { SIZES } from '../../../helpers/constants/design-system';
+
+export const AVATAR_FAVICON_SIZES = {
+  XS: SIZES.XS,
+  SM: SIZES.SM,
+  MD: SIZES.MD,
+  LG: SIZES.LG,
+  XL: SIZES.XL,
+};
