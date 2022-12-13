0496502c67ba1b5d08a996fbc6d0a2646fdc135b
Frederik Bolding <frederik.bolding@gmail.com>
2022-12-13 12:18:04 +0100

    Add paragraph wrapper component

--- ui/components/app/flask/snap-ui-markdown/snap-ui-markdown.js
+++ ui/components/app/flask/snap-ui-markdown/snap-ui-markdown.js
@@ -4,17 +4,17 @@ import ReactMarkdown from 'react-markdown';
 import { TYPOGRAPHY } from '../../../../helpers/constants/design-system';
 import Typography from '../../../ui/typography/typography';
 
+const Paragraph = (props) => <Typography {...props} variant={TYPOGRAPHY.H6} />
+
 export const SnapUIMarkdown = ({ children }) => {
   return (
-    <Typography variant={TYPOGRAPHY.H6}>
-      <ReactMarkdown
-        className="snap-ui-markdown__text"
-        allowedElements={['p', 'strong', 'em']}
-        components={{ p: Fragment }}
-      >
-        {children}
-      </ReactMarkdown>
-    </Typography>
+    <ReactMarkdown
+      className="snap-ui-markdown__text"
+      allowedElements={['p', 'strong', 'em']}
+      components={{ p: Paragraph }}
+    >
+      {children}
+    </ReactMarkdown>
   );
 };
 
