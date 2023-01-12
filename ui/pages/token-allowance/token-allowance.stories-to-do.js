96235c8022972972dc53f11a166e0fb14690c0aa
NidhiKJha <menidhikjha@gmail.com>
2023-01-12 23:24:32 +0530

    updated token allowance story

--- ui/pages/token-allowance/token-allowance.stories-to-do.js
+++ ui/pages/token-allowance/token-allowance.stories-to-do.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TokenAllowance from './token-allowance';
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   title: 'Pages/TokenAllowance',
 
