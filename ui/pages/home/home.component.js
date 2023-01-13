fbd5c46a214a42821e1721c2918f360aa29d950a
Filip Sekulic <filip.sekulic@consensys.net>
2023-01-13 10:54:11 +0100

    Minor change

--- ui/pages/home/home.component.js
+++ ui/pages/home/home.component.js
@@ -549,7 +549,7 @@ export default class Home extends PureComponent {
             </Box>
           </Popover>
         )}
-        {!openSeaTransactionSecurityProviderPopoverHasBeenShown && (
+        {!openSeaTransactionSecurityProviderPopoverHasBeenShown ? (
           <Popover
             title={
               <Typography
@@ -630,7 +630,7 @@ export default class Home extends PureComponent {
               </Typography>
             </Box>
           </Popover>
-        )}
+        ) : null}
       </MultipleNotifications>
     );
   }
