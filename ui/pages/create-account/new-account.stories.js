01bcc48f4aa3e9688c39b9e9ff1d7cc09ae2fb57
NidhiKJha <menidhikjha@gmail.com>
2023-01-18 15:14:25 +0530

    updated stories

--- ui/pages/create-account/new-account.stories.js
+++ ui/pages/create-account/new-account.stories.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { action } from '@storybook/addon-actions';
 import NewAccountCreateForm from './new-account.component';
 
-// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   title: 'Pages/CreateAccount/NewAccount',
   argTypes: {
