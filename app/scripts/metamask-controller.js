e74ab293f0cf6d9be93318a8592ffffefdff51d9
Frederik Bolding <frederik.bolding@gmail.com>
2023-01-19 13:09:09 +0100

    Update iframe execution environment

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -737,7 +737,7 @@ export default class MetamaskController extends EventEmitter {
     ///: BEGIN:ONLY_INCLUDE_IN(flask)
     this.snapExecutionService = new IframeExecutionService({
       iframeUrl: new URL(
-        'https://metamask.github.io/iframe-execution-environment/0.11.1',
+        'https://metamask.github.io/iframe-execution-environment/0.12.0',
       ),
       messenger: this.controllerMessenger.getRestricted({
         name: 'ExecutionService',
