2dcf4dd6b35fdac042f14aae06b9edc82b133da1
vthomas13 <10986371+vthomas13@users.noreply.github.com>
2023-01-23 13:27:19 -0500

    Update app/scripts/controllers/app-state.js
    adding documentation details
    
    Co-authored-by: Mark Stacey <markjstacey@gmail.com>

--- app/scripts/controllers/app-state.js
+++ app/scripts/controllers/app-state.js
@@ -162,7 +162,7 @@ export default class AppStateController extends EventEmitter {
   /**
    * Record the timestamp of the last time the user has seen the outdated browser warning
    *
-   * @param {number} lastShown - timestamp when user was last shown the warning.
+   * @param {number} lastShown - Timestamp (in milliseconds) of when the user was last shown the warning.
    */
   setOutdatedBrowserWarningLastShown(lastShown) {
     this.store.updateState({
