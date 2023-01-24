f29683cc0a4779708286e0f1b129a72518b26471
Brad Decker <bhdecker84@gmail.com>
2023-01-24 12:49:26 -0600

    Complete level 36 migration of TypeScript by converting conversion.utils.js and deprecate old conversion util (#17262)

--- shared/modules/conversion.utils.test.js
+++ shared/modules/conversion.utils.test.js
@@ -1,212 +1,11 @@
-import BigNumber from 'bignumber.js';
-import { ETH } from '../../ui/helpers/constants/common';
+import { EtherDenomination } from '../constants/common';
 import {
-  addCurrencies,
-  conversionUtil,
   decWEIToDecETH,
-  divideCurrencies,
   getValueFromWeiHex,
   getWeiHexFromDecimalValue,
 } from './conversion.utils';
 
 describe('conversion utils', () => {
-  describe('addCurrencies()', () => {
-    it('add whole numbers', () => {
-      const result = addCurrencies(3, 9, {
-        aBase: 10,
-        bBase: 10,
-      });
-      expect(result.toNumber()).toStrictEqual(12);
-    });
-
-    it('add decimals', () => {
-      const result = addCurrencies(1.3, 1.9, {
-        aBase: 10,
-        bBase: 10,
-      });
-      expect(result.toNumber()).toStrictEqual(3.2);
-    });
-
-    it('add repeating decimals', () => {
-      const result = addCurrencies(1 / 3, 1 / 9, {
-        aBase: 10,
-        bBase: 10,
-      });
-      expect(result.toNumber()).toStrictEqual(0.4444444444444444);
-    });
-  });
-
-  describe('conversionUtil', () => {
-    it('returns expected types', () => {
-      const conv1 = conversionUtil(1000000000000000000, {
-        fromNumericBase: 'dec',
-        toNumericBase: 'hex',
-      });
-      const conv2 = conversionUtil(1, {
-        fromNumericBase: 'dec',
-        fromDenomination: 'ETH',
-        toDenomination: 'WEI',
-      });
-      expect(typeof conv1 === 'string').toStrictEqual(true);
-      expect(conv2 instanceof BigNumber).toStrictEqual(true);
-    });
-    it('converts from dec to hex', () => {
-      expect(
-        conversionUtil('1000000000000000000', {
-          fromNumericBase: 'dec',
-          toNumericBase: 'hex',
-        }),
-      ).toStrictEqual('de0b6b3a7640000');
-      expect(
-        conversionUtil('1500000000000000000', {
-          fromNumericBase: 'dec',
-          toNumericBase: 'hex',
-        }),
-      ).toStrictEqual('14d1120d7b160000');
-    });
-    it('converts hex formatted numbers to dec', () => {
-      expect(
-        conversionUtil('0xde0b6b3a7640000', {
-          fromNumericBase: 'hex',
-          toNumericBase: 'dec',
-        }),
-      ).toStrictEqual('1000000000000000000');
-      expect(
-        conversionUtil('0x14d1120d7b160000', {
-          fromNumericBase: 'hex',
-          toNumericBase: 'dec',
-        }),
-      ).toStrictEqual('1500000000000000000');
-    });
-    it('converts WEI to ETH', () => {
-      expect(
-        conversionUtil('0xde0b6b3a7640000', {
-          fromNumericBase: 'hex',
-          toNumericBase: 'dec',
-          fromDenomination: 'WEI',
-          toDenomination: 'ETH',
-        }),
-      ).toStrictEqual('1');
-      expect(
-        conversionUtil('0x14d1120d7b160000', {
-          fromNumericBase: 'hex',
-          toNumericBase: 'dec',
-          fromDenomination: 'WEI',
-          toDenomination: 'ETH',
-        }),
-      ).toStrictEqual('1.5');
-    });
-    it('converts ETH to WEI', () => {
-      expect(
-        conversionUtil('1', {
-          fromNumericBase: 'dec',
-          fromDenomination: 'ETH',
-          toDenomination: 'WEI',
-        }).toNumber(),
-      ).toStrictEqual(1000000000000000000);
-      expect(
-        conversionUtil('1.5', {
-          fromNumericBase: 'dec',
-          fromDenomination: 'ETH',
-          toDenomination: 'WEI',
-        }).toNumber(),
-      ).toStrictEqual(1500000000000000000);
-    });
-    it('converts ETH to GWEI', () => {
-      expect(
-        conversionUtil('1', {
-          fromNumericBase: 'dec',
-          fromDenomination: 'ETH',
-          toDenomination: 'GWEI',
-        }).toNumber(),
-      ).toStrictEqual(1000000000);
-      expect(
-        conversionUtil('1.5', {
-          fromNumericBase: 'dec',
-          fromDenomination: 'ETH',
-          toDenomination: 'GWEI',
-        }).toNumber(),
-      ).toStrictEqual(1500000000);
-    });
-    it('converts ETH to USD', () => {
-      expect(
-        conversionUtil('1', {
-          fromNumericBase: 'dec',
-          toNumericBase: 'dec',
-          toCurrency: 'usd',
-          conversionRate: 468.58,
-          numberOfDecimals: 2,
-        }),
-      ).toStrictEqual('468.58');
-      expect(
-        conversionUtil('1.5', {
-          fromNumericBase: 'dec',
-          toNumericBase: 'dec',
-          toCurrency: 'usd',
-          conversionRate: 468.58,
-          numberOfDecimals: 2,
-        }),
-      ).toStrictEqual('702.87');
-    });
-    it('converts USD to ETH', () => {
-      expect(
-        conversionUtil('468.58', {
-          fromNumericBase: 'dec',
-          toNumericBase: 'dec',
-          toCurrency: 'usd',
-          conversionRate: 468.58,
-          numberOfDecimals: 2,
-          invertConversionRate: true,
-        }),
-      ).toStrictEqual('1');
-      expect(
-        conversionUtil('702.87', {
-          fromNumericBase: 'dec',
-          toNumericBase: 'dec',
-          toCurrency: 'usd',
-          conversionRate: 468.58,
-          numberOfDecimals: 2,
-          invertConversionRate: true,
-        }),
-      ).toStrictEqual('1.5');
-    });
-  });
-
-  describe('divideCurrencies()', () => {
-    it('should correctly divide decimal values', () => {
-      const result = divideCurrencies(9, 3, {
-        dividendBase: 10,
-        divisorBase: 10,
-      });
-      expect(result.toNumber()).toStrictEqual(3);
-    });
-
-    it('should correctly divide hexadecimal values', () => {
-      const result = divideCurrencies(1000, 0xa, {
-        dividendBase: 16,
-        divisorBase: 16,
-      });
-      expect(result.toNumber()).toStrictEqual(0x100);
-    });
-
-    it('should correctly divide hexadecimal value from decimal value', () => {
-      const result = divideCurrencies(0x3e8, 0xa, {
-        dividendBase: 16,
-        divisorBase: 16,
-      });
-      expect(result.toNumber()).toStrictEqual(0x100);
-    });
-
-    it('should throw error for wrong base value', () => {
-      expect(() => {
-        divideCurrencies(0x3e8, 0xa, {
-          dividendBase: 10.5,
-          divisorBase: 7,
-        });
-      }).toThrow('Must specify valid dividendBase and divisorBase');
-    });
-  });
-
   describe('decWEIToDecETH', () => {
     it('converts 10000000000000 WEI to ETH', () => {
       const ethDec = decWEIToDecETH('10000000000000');
@@ -223,10 +22,17 @@ describe('conversion utils', () => {
     it('should correctly convert 0 in ETH', () => {
       const weiValue = getWeiHexFromDecimalValue({
         value: '0',
-        fromCurrency: ETH,
-        fromDenomination: ETH,
+        fromDenomination: EtherDenomination.ETH,
       });
       expect(weiValue).toStrictEqual('0');
+    });
+
+    it('should correctly convert 10 in ETH to 8ac7230489e80000 (10000000000000000000) wei', () => {
+      const weiValue = getWeiHexFromDecimalValue({
+        value: '10',
+        fromDenomination: EtherDenomination.ETH,
+      });
+      expect(weiValue).toStrictEqual('8ac7230489e80000');
     });
   });
 
@@ -235,7 +41,6 @@ describe('conversion utils', () => {
       const ethTransactionAmount = getValueFromWeiHex({
         value: '0xde0b6b3a7640000',
         toCurrency: 'ETH',
-        conversionRate: 468.58,
         numberOfDecimals: 6,
       });
 
