123bc4ecdea347b177db73cb72b1c184970035ec
Alex <adonesky@gmail.com>
2023-01-24 10:07:46 -0600

    Fix broken NFT import and auto-detect flows

--- ui/components/app/collectibles-tab/collectibles-tab.test.js
+++ ui/components/app/collectibles-tab/collectibles-tab.test.js
@@ -6,6 +6,7 @@ import { renderWithProvider } from '../../../../test/jest/rendering';
 import { EXPERIMENTAL_ROUTE } from '../../../helpers/constants/routes';
 import { setBackgroundConnection } from '../../../../test/jest';
 import CollectiblesTab from '.';
+import { hexToDecimal } from '../../../../shared/modules/conversion.utils';
 
 const COLLECTIBLES = [
   {
@@ -153,16 +154,17 @@ const render = ({
   useNftDetection,
   onAddNFT = jest.fn(),
 }) => {
+  const chainIdAsDecimal = hexToDecimal(chainId)
   const store = configureStore({
     metamask: {
       allNfts: {
         [ACCOUNT_1]: {
-          [chainId]: collectibles,
+          [chainIdAsDecimal]: collectibles,
         },
       },
       allNftContracts: {
         [ACCOUNT_1]: {
-          [chainId]: collectibleContracts,
+          [chainIdAsDecimal]: collectibleContracts,
         },
       },
       provider: { chainId },
