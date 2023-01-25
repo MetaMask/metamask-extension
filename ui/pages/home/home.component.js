48c3721ac8099aade1f5290227d7fe4c930e4642
David Walsh <davidwalsh83@gmail.com>
2023-01-25 13:33:08 -0600

    Add autohide to the removeal message

--- ui/pages/home/home.component.js
+++ ui/pages/home/home.component.js
@@ -276,6 +276,7 @@ export default class Home extends PureComponent {
     } = this.props;
 
     const onAutoHide = () => setNewCollectibleAddedMessage('');
+    const autoHideDelay = 5 * SECOND;
 
     return (
       <MultipleNotifications>
@@ -320,7 +321,7 @@ export default class Home extends PureComponent {
           <ActionableMessage
             type="success"
             className="home__new-network-notification"
-            autoHideTime={5 * SECOND}
+            autoHideTime={autoHideDelay}
             onAutoHide={onAutoHide}
             message={
               <Box display={DISPLAY.INLINE_FLEX}>
@@ -345,6 +346,8 @@ export default class Home extends PureComponent {
           <ActionableMessage
             type="danger"
             className="home__new-network-notification"
+            autoHideTime={autoHideDelay}
+            onAutoHide={onAutoHide}
             message={
               <Box display={DISPLAY.INLINE_FLEX}>
                 <i className="fa fa-check-circle home__new-nft-notification-icon" />
