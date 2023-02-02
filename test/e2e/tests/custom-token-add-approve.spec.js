8b5f58b360b1bf7db84f52aae4ce38a2a7c7d833
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2023-02-02 12:46:59 -0500

    addressing e2e failures

--- test/e2e/tests/custom-token-add-approve.spec.js
+++ test/e2e/tests/custom-token-add-approve.spec.js
@@ -485,8 +485,9 @@ describe('Create token, approve token and approve token without gas', function (
           tag: 'button',
         });
 
-        await driver.delay(500);
+        await driver.waitForElementNotPresent('.loading-overlay');
         await driver.clickElement({ text: 'Approve', tag: 'button' });
+        await driver.waitForElementNotPresent('.loading-overlay');
 
         // check transaction in Activity tab
         const approveTokenTask = await driver.waitForSelector({
