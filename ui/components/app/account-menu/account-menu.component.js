a2821b113eefa48e008df6f7ef921fbf196f71e4
David Walsh <davidwalsh83@gmail.com>
2023-02-03 13:43:25 -0600

    Fix flask comment fencing

--- ui/components/app/account-menu/account-menu.component.js
+++ ui/components/app/account-menu/account-menu.component.js
@@ -41,7 +41,13 @@ import Button from '../../ui/button';
 import SearchIcon from '../../ui/icon/search-icon';
 import { SUPPORT_LINK } from '../../../../shared/lib/ui-utils';
 import { Color } from '../../../helpers/constants/design-system';
-import { Icon, ICON_NAMES, ICON_SIZES } from '../../component-library';
+import {
+  Icon,
+  ICON_NAMES,
+  ///: BEGIN:ONLY_INCLUDE_IN(flask)
+  ICON_SIZES,
+} from '../../component-library';
+///: END:ONLY_INCLUDE_IN
 import KeyRingLabel from './keyring-label';
 
 export function AccountMenuItem(props) {
