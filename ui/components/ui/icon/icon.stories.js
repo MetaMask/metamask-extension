710b1a3fbfd27c1890b16e2cfa05fd62652dcf75
David Walsh <davidwalsh83@gmail.com>
2023-02-03 16:05:51 -0600

    UX: Update support icon (#17560)

--- ui/components/ui/icon/icon.stories.js
+++ ui/components/ui/icon/icon.stories.js
@@ -30,7 +30,6 @@ import IconCheck from './icon-check';
 import IconCog from './icon-cog';
 import IconConnect from './icon-connect';
 import IconImport from './icon-import';
-import IconSpeechBubbles from './icon-speech-bubbles';
 import IconPlus from './icon-plus';
 import IconEye from './icon-eye';
 import IconEyeSlash from './icon-eye-slash';
@@ -128,7 +127,6 @@ export const DefaultStory = (args) => (
         <IconItem Component={<IconPlus {...args} />} />
         <IconItem Component={<IconImport {...args} />} />
         <IconItem Component={<IconConnect {...args} />} />
-        <IconItem Component={<IconSpeechBubbles {...args} />} />
         <IconItem Component={<IconCog {...args} />} />
         <IconItem Component={<IconTokenSearch {...args} />} />
         <IconItem Component={<SearchIcon {...args} />} />
