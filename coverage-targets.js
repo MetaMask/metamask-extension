e3777319edba682cf82fd100beef0f5bc5e7ede3
PeterYinusa <peter.yinusa@consensys.net>
2023-02-07 14:07:25 +0000

    Reduce coverage targets
    Updated in https://github.com/MetaMask/metamask-extension/pull/17326

--- coverage-targets.js
+++ coverage-targets.js
@@ -6,10 +6,10 @@
 // subset of files to check against these targets.
 module.exports = {
   global: {
-    branches: 22,
-    functions: 33.5,
-    lines: 62.25,
-    statements: 41.75,
+    branches: 20,
+    functions: 30,
+    lines: 57,
+    statements: 40,
   },
   transforms: {
     branches: 100,
