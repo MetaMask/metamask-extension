75f2951f89e1b72645a3e5dc53505524836916fc
Elliot Winkler <elliot.winkler@gmail.com>
2023-02-08 07:33:03 -0700

    [TS dashboard] Reorganize files slightly (#17335)

--- development/ts-migration-dashboard/app/index.tsx
+++ development/ts-migration-dashboard/app/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
 
 const appElement = document.querySelector('#app');
 
