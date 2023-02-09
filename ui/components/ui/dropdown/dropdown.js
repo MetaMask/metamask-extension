713c4f1ecafcf8469bb9408d4b0f3e36f9f97ac0
David Walsh <davidwalsh83@gmail.com>
2023-02-08 18:27:37 -0600

    Remove down chevron

--- ui/components/ui/dropdown/dropdown.js
+++ ui/components/ui/dropdown/dropdown.js
@@ -1,7 +1,7 @@
 import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-import IconCaretDown from '../icon/icon-caret-down';
+import { Icon, ICON_NAMES } from '../../component-library';
 
 const Dropdown = ({
   className,
@@ -41,7 +41,7 @@ const Dropdown = ({
           );
         })}
       </select>
-      <IconCaretDown size={16} className="dropdown__icon-caret-down" />
+      <Icon name={ICON_NAMES.ARROW_DOWN} className="dropdown__icon-caret-down" />
     </div>
   );
 };
--- ui/components/ui/icon/icon.stories.js
+++ ui/components/ui/icon/icon.stories.js
@@ -23,7 +23,6 @@ import Sign from './sign-icon.component';
 import SunCheck from './sun-check-icon.component';
 import Swap from './swap-icon-for-list.component';
 import IconCaretLeft from './icon-caret-left';
-import IconCaretDown from './icon-caret-down';
 import IconCaretUp from './icon-caret-up';
 import IconCheck from './icon-check';
 import IconCog from './icon-cog';
@@ -122,7 +121,6 @@ export const DefaultStory = (args) => (
         }}
       >
         <IconItem Component={<IconCaretLeft {...args} />} />
-        <IconItem Component={<IconCaretDown {...args} />} />
         <IconItem Component={<IconCaretUp {...args} />} />
         <IconItem Component={<IconCheck {...args} />} />
         <IconItem Component={<IconPlus {...args} />} />
