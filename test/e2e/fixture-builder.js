6c58e1e15b2a7336883771428d30997238987ae1
Victor Thomas <10986371+vthomas13@users.noreply.github.com>
2023-02-10 15:05:05 -0500

    adding new notification to fixture builder for e2e

--- test/e2e/fixture-builder.js
+++ test/e2e/fixture-builder.js
@@ -111,6 +111,11 @@ function defaultFixture() {
             id: 17,
             isShown: true,
           },
+          18: {
+            date: null,
+            id: 18,
+            isShown: true,
+          }
         },
       },
       AppStateController: {
