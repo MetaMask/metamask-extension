0e30b145ecac1e100ca30a602aff73bebfd0a09e
Danica Shen <zhaodanica@gmail.com>
2023-02-14 23:26:42 +0000

    Version-v10.25.beta.0

--- .circleci/scripts/trigger-beta-build.sh
+++ .circleci/scripts/trigger-beta-build.sh
@@ -7,17 +7,20 @@ set -o pipefail
 # => Version-v10.24.1
 version="${CIRCLE_BRANCH/Version-v/}"
 current_commit_msg=$(git show -s --format='%s' HEAD)
+build_version="echo $current_commit_msg | cut -d \beta -f 2"
+printf '%s\n' "Creating the prod beta build for $version with beta version $build_version"
+yarn build --build-type beta --build-version "${build_version}" prod
 
-if [[ $current_commit_msg =~ Version[-[:space:]](v[[:digit:]]+.[[:digit:]]+.[[:digit:]]+[-]beta.[[:digit:]]) ]]
-then
-    build_version="echo $current_commit_msg | cut -d \beta -f 2"
-    yarn build --build-type beta --build-version "${build_version}" prod
-    printf '%s\n' "Creating the prod beta build for $version with beta version $current_commit_msg"
-
-else
-  # filter the commit message like Version v10.24.1-beta.1
-    printf '%s\n' 'Commit message does not match commit message for beta pattern; skipping beta automation build'
-    exit 0
-fi
+#if [[ $current_commit_msg =~ Version[-[:space:]](v[[:digit:]]+.[[:digit:]]+.[[:digit:]]+[-]beta.[[:digit:]]) ]]
+#then
+#    build_version="echo $current_commit_msg | cut -d \beta -f 2"
+#    yarn build --build-type beta --build-version "${build_version}" prod
+#    printf '%s\n' "Creating the prod beta build for $version with beta version build_version"
+#
+#else
+#  # filter the commit message like Version v10.24.1-beta.1
+#    printf '%s\n' 'Commit message does not match commit message for beta pattern; skipping beta automation build'
+#    exit 0
+#fi
 
 exit 0
