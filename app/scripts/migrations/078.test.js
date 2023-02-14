1e619e56afeb000ded32d9316c66c9fe4851426e
David Walsh <davidwalsh83@gmail.com>
2023-02-14 11:43:39 -0600

    Update test

--- app/scripts/migrations/078.test.js
+++ app/scripts/migrations/078.test.js
@@ -170,4 +170,85 @@ describe('migration #78', () => {
       },
     });
   });
+
+  it('should make no changes if "showPortfolioToolip" never existed', async () => {
+    const oldStorage = {
+      meta: {
+        version: 77,
+      },
+      data: {
+        metamask: {
+          isInitialized: true,
+          isUnlocked: true,
+          isAccountMenuOpen: false,
+          identities: {
+            '0x00000': {
+              address: '0x00000',
+              lastSelected: 1675966229118,
+              name: 'Account 1',
+            },
+            '0x00001': {
+              address: '0x00001',
+              name: 'Account 2',
+            },
+          },
+          unapprovedTxs: {},
+          frequentRpcList: [],
+          addressBook: {},
+          popupGasPollTokens: [],
+          notificationGasPollTokens: [],
+          fullScreenGasPollTokens: [],
+          recoveryPhraseReminderHasBeenShown: false,
+          recoveryPhraseReminderLastShown: 1675966206345,
+          outdatedBrowserWarningLastShown: 1675966206345,
+          collectiblesDetectionNoticeDismissed: false,
+          showTestnetMessageInDropdown: true,
+          showBetaHeader: false,
+          trezorModel: null,
+          collectiblesDropdownState: {},
+          qrHardware: {},
+        },
+      },
+    };
+
+    const newStorage = await migration78.migrate(oldStorage);
+    expect(newStorage).toStrictEqual({
+      meta: {
+        version: 78,
+      },
+      data: {
+        metamask: {
+          isInitialized: true,
+          isUnlocked: true,
+          isAccountMenuOpen: false,
+          identities: {
+            '0x00000': {
+              address: '0x00000',
+              lastSelected: 1675966229118,
+              name: 'Account 1',
+            },
+            '0x00001': {
+              address: '0x00001',
+              name: 'Account 2',
+            },
+          },
+          unapprovedTxs: {},
+          frequentRpcList: [],
+          addressBook: {},
+          popupGasPollTokens: [],
+          notificationGasPollTokens: [],
+          fullScreenGasPollTokens: [],
+          recoveryPhraseReminderHasBeenShown: false,
+          recoveryPhraseReminderLastShown: 1675966206345,
+          outdatedBrowserWarningLastShown: 1675966206345,
+          collectiblesDetectionNoticeDismissed: false,
+          showTestnetMessageInDropdown: true,
+          showBetaHeader: false,
+          trezorModel: null,
+          collectiblesDropdownState: {},
+          qrHardware: {},
+        },
+      },
+    });
+  });
 });
