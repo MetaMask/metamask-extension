c3503c3e8371c4dca63148c852b741ca4cfec133
NidhiKJha <menidhikjha@gmail.com>
2023-02-15 21:12:54 +0530

    updated 078.test

--- app/scripts/migrations/078.test.js
+++ app/scripts/migrations/078.test.js
@@ -21,8 +21,36 @@ describe('migration #78', () => {
       },
       data: {
         metamask: {
+          isInitialized: true,
+          isUnlocked: true,
+          isAccountMenuOpen: false,
+          identities: {
+            '0x00000': {
+              address: '0x00000',
+              lastSelected: 1675966229118,
+              name: 'Account 1',
+            },
+            '0x00001': {
+              address: '0x00001',
+              name: 'Account 2',
+            },
+          },
+          unapprovedTxs: {},
+          frequentRpcList: [],
+          addressBook: {},
+          popupGasPollTokens: [],
+          notificationGasPollTokens: [],
+          fullScreenGasPollTokens: [],
+          recoveryPhraseReminderHasBeenShown: false,
+          recoveryPhraseReminderLastShown: 1675966206345,
+          outdatedBrowserWarningLastShown: 1675966206345,
           collectiblesDetectionNoticeDismissed: false,
+          showTestnetMessageInDropdown: true,
+          showPortfolioTooltip: false,
+          showBetaHeader: false,
+          trezorModel: null,
           collectiblesDropdownState: {},
+          qrHardware: {},
         },
       },
     };
@@ -34,8 +62,34 @@ describe('migration #78', () => {
       },
       data: {
         metamask: {
-          nftDetectionNoticeDismissed: false,
-          nftsDropdownState: {},
+          isInitialized: true,
+          isUnlocked: true,
+          isAccountMenuOpen: false,
+          identities: {
+            '0x00000': {
+              address: '0x00000',
+              lastSelected: 1675966229118,
+              name: 'Account 1',
+            },
+            '0x00001': {
+              address: '0x00001',
+              name: 'Account 2',
+            },
+          },
+          unapprovedTxs: {},
+          frequentRpcList: [],
+          addressBook: {},
+          popupGasPollTokens: [],
+          notificationGasPollTokens: [],
+          fullScreenGasPollTokens: [],
+          recoveryPhraseReminderHasBeenShown: false,
+          recoveryPhraseReminderLastShown: 1675966206345,
+          outdatedBrowserWarningLastShown: 1675966206345,
+          showTestnetMessageInDropdown: true,
+          showPortfolioTooltip: false,
+          showBetaHeader: false,
+          trezorModel: null,
+          qrHardware: {},
         },
       },
     });
