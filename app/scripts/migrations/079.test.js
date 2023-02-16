f4f01973ecb8ace31f0015591f0fc569fe7aa028
hmalik88 <hassan.malik@consensys.net>
2023-02-16 18:32:47 -0500

    update migration #

--- app/scripts/migrations/079.test.js
+++ app/scripts/migrations/079.test.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import migration79 from './079';
 
 describe('migration #79', () => {
@@ -15,18 +16,71 @@ describe('migration #79', () => {
   });
 
   it('should remove the "collectiblesDetectionNoticeDismissed"', async () => {
+=======
+import {
+  WALLET_SNAP_PERMISSION_KEY,
+  SnapCaveatType,
+} from '@metamask/rpc-methods';
+import migration78 from './079';
+
+describe('migration #78', () => {
+  it('should consolidate snap permissions as caveats under the wallet_snap permission', async () => {
+>>>>>>> 192c62011 (update migration #)
     const oldStorage = {
       meta: {
         version: 78,
       },
       data: {
+<<<<<<< HEAD
         AppStateController: {
           collectiblesDetectionNoticeDismissed: false,
           bar: 'baz',
+=======
+        PermissionController: {
+          subjects: {
+            'example.com': {
+              permissions: {
+                'wallet_snap_npm:foobar': {
+                  caveats: null,
+                  date: 2,
+                  id: 'a7342F4b-beae-4525-a36c-c0635fd03359',
+                  invoker: 'example.com',
+                  parentCapability: 'wallet_snap_npm:foobar',
+                },
+                'wallet_snap_npm:baz': {
+                  caveats: null,
+                  date: 3,
+                  id: 'x342A44-beae-4525-a36c-c0635fd03359',
+                  invoker: 'example.com',
+                  parentCapability: 'wallet_snap_npm:baz',
+                },
+              },
+            },
+            'aave.com': {
+              permissions: {
+                'wallet_snap_npm:filsnap': {
+                  caveats: null,
+                  date: 10,
+                  id: 'a7342F4b-beae-4525-a36c-c0635fd03359',
+                  invoker: 'aave.com',
+                  parentCapability: 'wallet_snap_npm:foobar',
+                },
+                'wallet_snap_npm:btcsnap': {
+                  caveats: null,
+                  date: 3,
+                  id: 'x342A44-beae-4525-a36c-c0635fd03359',
+                  invoker: 'aave.com',
+                  parentCapability: 'wallet_snap_npm:btcsnap',
+                },
+              },
+            },
+          },
+>>>>>>> 192c62011 (update migration #)
         },
       },
     };
 
+<<<<<<< HEAD
     const newStorage = await migration79.migrate(oldStorage);
     expect(newStorage).toStrictEqual({
       meta: {
@@ -35,17 +89,70 @@ describe('migration #79', () => {
       data: {
         AppStateController: {
           bar: 'baz',
+=======
+    const newStorage = await migration78.migrate(oldStorage);
+
+    expect(newStorage).toStrictEqual({
+      meta: { version: 79 },
+      data: {
+        PermissionController: {
+          subjects: {
+            'example.com': {
+              permissions: {
+                [WALLET_SNAP_PERMISSION_KEY]: {
+                  caveats: [
+                    {
+                      type: SnapCaveatType.SnapIds,
+                      value: {
+                        'npm:foobar': {},
+                        'npm:baz': {},
+                      },
+                    },
+                  ],
+                  date: 3,
+                  id: 'x342A44-beae-4525-a36c-c0635fd03359',
+                  invoker: 'example.com',
+                  parentCapability: WALLET_SNAP_PERMISSION_KEY,
+                },
+              },
+            },
+            'aave.com': {
+              permissions: {
+                [WALLET_SNAP_PERMISSION_KEY]: {
+                  caveats: [
+                    {
+                      type: SnapCaveatType.SnapIds,
+                      value: {
+                        'npm:btcsnap': {},
+                        'npm:filsnap': {},
+                      },
+                    },
+                  ],
+                  date: 10,
+                  id: 'a7342F4b-beae-4525-a36c-c0635fd03359',
+                  invoker: 'aave.com',
+                  parentCapability: WALLET_SNAP_PERMISSION_KEY,
+                },
+              },
+            },
+          },
+>>>>>>> 192c62011 (update migration #)
         },
       },
     });
   });
 
+<<<<<<< HEAD
   it('should remove the "collectiblesDropdownState"', async () => {
+=======
+  it('should leave state unchanged if there are no snap permissions', async () => {
+>>>>>>> 192c62011 (update migration #)
     const oldStorage = {
       meta: {
         version: 78,
       },
       data: {
+<<<<<<< HEAD
         metamask: {
           isInitialized: true,
           isUnlocked: true,
@@ -63,10 +170,26 @@ describe('migration #79', () => {
           },
           collectiblesDropdownState: {},
           qrHardware: {},
+=======
+        PermissionController: {
+          subjects: {
+            'example.com': {
+              permissions: {
+                eth_accounts: {
+                  date: 2,
+                  id: 'a7342F4b-beae-4525-a36c-c0635fd03359',
+                  invoker: 'example.com',
+                  parentCapability: 'eth_accounts',
+                },
+              },
+            },
+          },
+>>>>>>> 192c62011 (update migration #)
         },
       },
     };
 
+<<<<<<< HEAD
     const newStorage = await migration79.migrate(oldStorage);
     expect(newStorage).toStrictEqual({
       meta: {
@@ -169,5 +292,10 @@ describe('migration #79', () => {
         },
       },
     });
+=======
+    const newStorage = await migration78.migrate(oldStorage);
+
+    expect(newStorage.data).toStrictEqual(oldStorage.data);
+>>>>>>> 192c62011 (update migration #)
   });
 });
