e5a93ba8806aa1a2ee82d3c6e602d3df0385f22b
Danica Shen <zhaodanica@gmail.com>
2023-02-16 00:34:06 +0000

    feat(17494): test separate commit triggered build

---
+++ .circleci/scripts/trigger-beta-build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -o pipefail
+
+# => Version-v10.24.1
+version="${CIRCLE_BRANCH/Version-v/}"
+current_commit_msg=$(git show -s --format='%s' HEAD)
+printf '%s\n' "Creating the prod beta build for $version under $current_commit_msg"
+
+if [[ $current_commit_msg =~ Version[-[:space:]](v[[:digit:]]+.[[:digit:]]+.[[:digit:]]+[-]beta.[[:digit:]]) ]]
+then
+    # filter the commit message like Version v10.24.1-beta.1
+    printf '%s\n' "Create a build for $version with beta version $current_commit_msg"
+    yarn build --build-type beta prod
+else
+    printf '%s\n' 'Commit message does not match commit message for beta pattern; skipping beta automation build'
+    mkdir builds
+    exit 0
+fi
+
+exit 0
