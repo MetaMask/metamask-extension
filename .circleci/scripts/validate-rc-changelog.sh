48bcc739c8785edcc76265b253947fb6bdd285da
Danica Shen <zhaodanica@gmail.com>
2023-02-16 21:14:24 +0000

    Version v10.27.0-beta.0

--- .circleci/scripts/validate-rc-changelog.sh
+++ .circleci/scripts/validate-rc-changelog.sh
@@ -7,16 +7,15 @@ set -o pipefail
 # => Version-v10.24.1
 version="${CIRCLE_BRANCH/Version-v/}"
 current_commit_msg=$(git show -s --format='%s' HEAD)
-printf '%s\n' "Creating the prod beta build for $version under $current_commit_msg"
 
 if [[ $current_commit_msg =~ Version[[:space:]](v[[:digit:]]+.[[:digit:]]+.[[:digit:]]+[-]beta.[[:digit:]]) ]]
 then
     # filter the commit message like Version v10.24.1-beta.1
-    printf '%s\n' "Create a build for $version with beta version $current_commit_msg"
-    yarn build --build-type beta prod
-else
-    printf '%s\n' 'Commit message does not match commit message for beta pattern; skipping beta automation build'
+    printf '%s\n' "No changelog is validated for beta commit $current_commit_msg"
     exit 0
+else
+    printf '%s\n' "Run validation of changelog for release candidate $version"
+    yarn lint:changelog:rc
 fi
 
 exit 0
