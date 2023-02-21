fff5c31d5d8edb55aaaace5fecb43442444a8d3e
Frederik Bolding <frederik.bolding@gmail.com>
2023-02-21 16:37:42 +0100

    Fix tests and story

--- ui/components/app/signature-request/signature-request-data/signature-request-data.stories.js
+++ ui/components/app/signature-request/signature-request-data/signature-request-data.stories.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { sanitizeMessage } from 'ui/helpers/utils/util';
 import SignatureRequestData from './signature-request-data';
 
 export default {
@@ -16,38 +17,42 @@ export const DefaultStory = (args) => {
 
 DefaultStory.storyName = 'Default';
 
+const rawMessage = {
+  domain: {
+    name: 'happydapp.website',
+  },
+  message: {
+    string: 'haay wuurl',
+    number: 42,
+  },
+  primaryType: 'Mail',
+  types: {
+    EIP712Domain: [
+      { name: 'name', type: 'string' },
+      { name: 'version', type: 'string' },
+      { name: 'chainId', type: 'uint256' },
+      { name: 'verifyingContract', type: 'address' },
+    ],
+    Group: [
+      { name: 'name', type: 'string' },
+      { name: 'members', type: 'Person[]' },
+    ],
+    Mail: [
+      { name: 'from', type: 'Person' },
+      { name: 'to', type: 'Person[]' },
+      { name: 'contents', type: 'string' },
+    ],
+    Person: [
+      { name: 'name', type: 'string' },
+      { name: 'wallets', type: 'address[]' },
+    ],
+  },
+};
+
 DefaultStory.args = {
-  data: JSON.parse(
-    JSON.stringify({
-      domain: {
-        name: 'happydapp.website',
-      },
-      message: {
-        string: 'haay wuurl',
-        number: 42,
-      },
-      primaryType: 'Mail',
-      types: {
-        EIP712Domain: [
-          { name: 'name', type: 'string' },
-          { name: 'version', type: 'string' },
-          { name: 'chainId', type: 'uint256' },
-          { name: 'verifyingContract', type: 'address' },
-        ],
-        Group: [
-          { name: 'name', type: 'string' },
-          { name: 'members', type: 'Person[]' },
-        ],
-        Mail: [
-          { name: 'from', type: 'Person' },
-          { name: 'to', type: 'Person[]' },
-          { name: 'contents', type: 'string' },
-        ],
-        Person: [
-          { name: 'name', type: 'string' },
-          { name: 'wallets', type: 'address[]' },
-        ],
-      },
-    }),
+  data: sanitizeMessage(
+    rawMessage.message,
+    rawMessage.primaryType,
+    rawMessage.types,
   ),
 };
