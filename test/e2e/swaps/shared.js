b97cb9a6ac62811b3928d114080bbb25a8da799f
Davide Brocchetto <davide@paysense.in>
2023-02-21 19:23:20 +0800

    Added test verification for the transaction details

--- test/e2e/swaps/shared.js
+++ test/e2e/swaps/shared.js
@@ -18,32 +18,27 @@ const withFixturesOptions = {
   ganacheOptions,
 };
 
-const loginExtension = async (driver) => {
+const loadExtension = async (driver) => {
   await driver.navigate();
   await driver.fill('#password', 'correct horse battery staple');
   await driver.press('#password', driver.Key.ENTER);
 };
 
-const enterSwapQuote = async (driver, options) => {
+const buildQuote = async (driver, options) => {
   await driver.clickElement(
     '.wallet-overview__buttons .icon-button:nth-child(3)',
   );
   await driver.fill('input[placeholder*="0"]', options.amount);
-  await driver.delay(veryLargeDelayMs); // Need an extra delay after typing an amount.
-  await driver.clickElement(
-    '[class="dropdown-search-list__closed-primary-label dropdown-search-list__select-default"]',
-  );
-  await driver.wait(async () => {
-    const tokens = await driver.findElements('.searchable-item-list__item');
-    return tokens.length > 1;
-  });
-  await driver.clickElement('.searchable-item-list__labels');
+  await driver.delay(veryLargeDelayMs);
+  await driver.waitForSelector('[class="dropdown-input-pair dropdown-input-pair__to"]');
   await driver.clickElement('.dropdown-input-pair__to');
-  await driver.clickElement('input[data-testid="search-list-items"]');
+
   await driver.fill(
     'input[data-testid="search-list-items"]',
     options.swapTo || options.swapToContractAddress,
   );
+  await driver.delay(veryLargeDelayMs);
+
   if (options.swapTo) {
     await driver.wait(async () => {
       const tokenNames = await driver.findElements(
@@ -65,29 +60,48 @@ const enterSwapQuote = async (driver, options) => {
   await driver.clickElement('.searchable-item-list__primary-label');
 };
 
-const reviewQuote = async (driver) => {
+const reviewQuote = async (driver, amount, swapFrom, swapTo) => {
 
-  await driver.delay(veryLargeDelayMs)
   await driver.clickElement({ text: 'Review swap', tag: 'button' });
+  await driver.waitForSelector({ text: 'Swap', tag: 'button' });
 
   await driver.waitForSelector({
     css: '[class*="box--align-items-center"]',
     text: 'Estimated gas fee',
   });
 
-  await driver.waitForSelector(
+  const sourceValue = await driver.waitForSelector('.main-quote-summary__source-row-value')
+  assert.equal(await sourceValue.getText(), amount, 'Error: Quote has wrong amount')
+
+  const sourceSymbol = await driver.waitForSelector('.main-quote-summary__source-row-symbol')
+  assert.equal(await sourceSymbol.getText(), swapFrom, 'Error: Quote has wrong symbol')
+
+  const quoteElement = await driver.waitForSelector(
     '[class="exchange-rate-display main-quote-summary__exchange-rate-display"]',
-  );
+  )
+
+  const quoteText = await quoteElement.getText()
+  const quoteSplit = await quoteText.split('\n')
+
+  assert.equal(quoteSplit[1], swapFrom)
+  assert.equal(quoteSplit[4], swapTo)
+
   await driver.waitForSelector(
     '[class="fee-card__info-tooltip-container"]',
   );
+
+  await driver.waitForSelector({
+    css: '[class="countdown-timer__time"]',
+    text: '0:23',
+  });
 };
 
 const waitForTransactionToComplete = async (driver, tokenName) => {
   const sucessfulTransactionMessage = await driver.waitForSelector({
     css: '[class="awaiting-swap__header"]',
     text: 'Transaction complete',
-  });
+  }, { timeout: 30000 });
+
   assert.equal(
     await sucessfulTransactionMessage.getText(),
     'Transaction complete',
@@ -98,12 +112,54 @@ const waitForTransactionToComplete = async (driver, tokenName) => {
   });
   assert.equal(await sucessfulTransactionToken.getText(), tokenName);
   await driver.clickElement({ text: 'Close', tag: 'button' });
+
+  await driver.waitForSelector('[data-testid="home__asset-tab"]');
+
+};
+
+const checkActivityTransaction = async (driver, index, amount, swapFrom, swapTo) => {
+  let logText
+
+  await driver.clickElement('[data-testid="home__activity-tab"]');
+
+  const itemsText = await driver.findElements('.list-item__title',);
+  assert.equal(await itemsText[index].getText(), `Swap ${swapFrom} to ${swapTo}`);
+
+  const amountValues = await driver.findElements('.transaction-list-item__primary-currency');
+
+  assert.equal(await amountValues[index].getText(), `-${amount} ${swapFrom}`);
+
+  await itemsText[index].click()
+
+  const txStatus = await driver.waitForSelector('.transaction-list-item-details__tx-status >div > div:last-child');
+  assert.equal(await txStatus.getText(), `Confirmed`);
+
+  const txAmount = await driver.waitForSelector('.transaction-breakdown__value--amount')
+  assert.equal(await txAmount.getText(), `-${amount} ${swapFrom}`);
+
+  await driver.clickElement('.disclosure__summary');
+  const symbol = (swapFrom === 'TESTETH') ? 'ETH' : swapFrom
+
+  const first_line = await driver.waitForSelector('.transaction-activity-log__activities-container >div:first-child')
+  logText = await first_line.getText()
+  assert.equal(logText.includes(`Transaction created with a value of ${amount} ${symbol}`), true);
+
+  const second_line = await driver.waitForSelector('.transaction-activity-log__activities-container >div:nth-child(2)')
+  logText = await second_line.getText()
+  assert.equal(logText.includes('Transaction submitted with estimated gas fee'), true);
+
+  const third_line = await driver.waitForSelector('.transaction-activity-log__activities-container >div:nth-child(3)')
+  logText = await third_line.getText()
+  assert.equal(logText.includes('Transaction confirmed'), true);
+
+  await driver.clickElement('[data-testid="popover-close"]');
 };
 
 module.exports = {
   withFixturesOptions,
-  loginExtension,
-  enterSwapQuote,
+  loadExtension,
+  buildQuote,
   reviewQuote,
   waitForTransactionToComplete,
+  checkActivityTransaction,
 };
--- test/e2e/swaps/swap-eth.spec.js
+++ test/e2e/swaps/swap-eth.spec.js
@@ -1,58 +1,52 @@
 const { strict: assert } = require('assert');
 
 const { withFixtures } = require('../helpers');
-const { withFixturesOptions, loginExtension, enterSwapQuote, reviewQuote, waitForTransactionToComplete } = require('./shared');
+const {
+  withFixturesOptions,
+  loadExtension,
+  buildQuote,
+  reviewQuote,
+  waitForTransactionToComplete,
+  checkActivityTransaction
+} = require('./shared');
+
 
 describe('Swap Eth for another Token', function () {
-  it('Completes second Swaps while first swap is processign', async function () {
+  withFixturesOptions.ganacheOptions.blockTime = 20
+
+  it('Completes second Swaps while first swap is processing', async function () {
     await withFixtures(
       {
         ...withFixturesOptions,
+        failOnConsoleError: false,
         title: this.test.title,
       },
       async ({ driver }) => {
-        await loginExtension(driver);
+        await loadExtension(driver);
 
-        await enterSwapQuote(driver, {
+        await buildQuote(driver, {
           amount: .001,
           swapTo: 'USDC',
         });
 
-        await reviewQuote(driver)
+        await reviewQuote(driver, '0.001', 'TESTETH', 'USDC')
+
         await driver.clickElement({ text: 'Swap', tag: 'button' });
         await driver.clickElement({ text: 'View in activity', tag: 'button' });
 
-        await enterSwapQuote(driver, {
+        await buildQuote(driver, {
           amount: .003,
           swapTo: 'DAI',
-        });
+        })
 
-        await reviewQuote(driver)
+        await reviewQuote(driver, '0.003', 'TESTETH', 'DAI')
+
         await driver.clickElement({ text: 'Swap', tag: 'button' });
+
         await waitForTransactionToComplete(driver, 'DAI')
 
-        await driver.clickElement('[data-testid="home__activity-tab"]');
-
-        await driver.wait(async () => {
-          const confirmedTxes = await driver.findElements(
-            '.transaction-list__completed-transactions .transaction-list-item',
-          );
-          return confirmedTxes.length === 2;
-        }, 10000);
-
-        const itemsText = await driver.findElements(
-          '.list-item__title',
-        );
-        assert.equal(itemsText.length, 2);
-        assert.equal(await itemsText[0].getText(), 'Swap TESTETH to DAI');
-        assert.equal(await itemsText[1].getText(), 'Swap TESTETH to USDC');
-
-        const amountValues = await driver.findElements(
-          '.transaction-list-item__primary-currency',
-        );
-        assert.equal(amountValues.length, 2);
-        assert.equal(await amountValues[0].getText(), '-0.003 TESTETH');
-        assert.equal(await amountValues[1].getText(), '-0.001 TESTETH');
+        await checkActivityTransaction(driver, 0, '0.003', 'TESTETH', 'DAI')
+        await checkActivityTransaction(driver, 1, '0.001', 'TESTETH', 'USDC')
       }
     );
   });
@@ -63,36 +57,21 @@ describe('Swap Eth for another Token', function () {
         title: this.test.title,
       },
       async ({ driver }) => {
-        await loginExtension(driver);
-        await enterSwapQuote(driver, {
+        await loadExtension(driver);
+        await buildQuote(driver, {
           amount: 2,
           swapTo: 'DAI',
         });
 
-        await reviewQuote(driver)
-
-        await driver.waitForSelector({
-          css: '[class="countdown-timer__time"]',
-          text: '0:24',
-        });
+        await reviewQuote(driver, '2', 'TESTETH', 'DAI')
 
         await driver.clickElement({ text: 'Swap', tag: 'button' });
 
-
         await waitForTransactionToComplete(driver, 'DAI')
 
-        await driver.clickElement('[data-testid="home__activity-tab"]');
-        const swaptotal = await driver.waitForSelector({
-          css: '[class="transaction-list-item__primary-currency"]',
-          text: '-2 TESTETH',
-        });
-        assert.equal(await swaptotal.getText(), '-2 TESTETH');
-        const swaptotaltext = await driver.waitForSelector({
-          css: '[class="list-item__title"]',
-          text: 'Swap TESTETH to DAI',
-        });
-        assert.equal(await swaptotaltext.getText(), 'Swap TESTETH to DAI');
+        await checkActivityTransaction(driver, 0, '2', 'TESTETH', 'DAI')
       },
     );
   });
+
 });
