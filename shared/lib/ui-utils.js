f26488a2af8c4b6dddb229a4a7007c28463c4416
Antonio Regadas <antonio.regadas@consensys.net>
2023-02-22 13:56:55 +0000

    invert order

--- shared/lib/ui-utils.js
+++ shared/lib/ui-utils.js
@@ -1,11 +1,11 @@
 let _supportLink = 'https://support.metamask.io';
 
-///: BEGIN:ONLY_INCLUDE_IN(flask)
-_supportLink = 'https://metamask-flask.zendesk.com/hc';
-///: END:ONLY_INCLUDE_IN
-
 ///: BEGIN:ONLY_INCLUDE_IN(mmi)
 _supportLink = 'https://mmi-support.zendesk.com/hc/en-us';
+///: END:ONLY_INCLUDE_IN
+
+///: BEGIN:ONLY_INCLUDE_IN(flask)
+_supportLink = 'https://metamask-flask.zendesk.com/hc';
 ///: END:ONLY_INCLUDE_IN
 
 export const SUPPORT_LINK = _supportLink;
