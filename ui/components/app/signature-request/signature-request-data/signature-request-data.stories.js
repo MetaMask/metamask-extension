4688a991cb644f19add2d1236f10bdac119b60d8
Frederik Bolding <frederik.bolding@gmail.com>
2023-02-22 10:47:44 +0100

    Fix stories

--- ui/components/app/signature-request/signature-request-data/signature-request-data.stories.js
+++ ui/components/app/signature-request/signature-request-data/signature-request-data.stories.js
@@ -19,11 +19,31 @@ DefaultStory.storyName = 'Default';
 
 const rawMessage = {
   domain: {
-    name: 'happydapp.website',
+    chainId: 97,
+    name: 'Ether Mail',
+    verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
+    version: '1',
   },
   message: {
-    string: 'haay wuurl',
-    number: 42,
+    contents: 'Hello, Bob!',
+    from: {
+      name: 'Cow',
+      wallets: [
+        '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
+        '0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF',
+        '0x06195827297c7A80a443b6894d3BDB8824b43896',
+      ],
+    },
+    to: [
+      {
+        name: 'Bob',
+        wallets: [
+          '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
+          '0xB0BdaBea57B0BDABeA57b0bdABEA57b0BDabEa57',
+          '0xB0B0b0b0b0b0B000000000000000000000000000',
+        ],
+      },
+    ],
   },
   primaryType: 'Mail',
   types: {
@@ -32,10 +52,6 @@ const rawMessage = {
       { name: 'version', type: 'string' },
       { name: 'chainId', type: 'uint256' },
       { name: 'verifyingContract', type: 'address' },
-    ],
-    Group: [
-      { name: 'name', type: 'string' },
-      { name: 'members', type: 'Person[]' },
     ],
     Mail: [
       { name: 'from', type: 'Person' },
@@ -54,5 +70,5 @@ DefaultStory.args = {
     rawMessage.message,
     rawMessage.primaryType,
     rawMessage.types,
-  ),
+  ).value,
 };
