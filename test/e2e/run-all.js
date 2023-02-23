32d3f7367e9dd94d9f5f48a682104ac23ddf2c60
Thomas Huang <tmashuang@users.noreply.github.com>
2023-02-23 12:05:26 -0800

    ERC1155 Import & Dapp interaction E2E tests

--- test/e2e/run-all.js
+++ test/e2e/run-all.js
@@ -54,6 +54,10 @@ async function main() {
             description: `run nft specific e2e tests`,
             type: 'boolean',
           })
+          .option('erc1155', {
+            description: `run erc1155 specific e2e tests`,
+            type: 'boolean',
+          })
           .option('retries', {
             description:
               'Set how many times the test should be retried upon failure.',
@@ -63,7 +67,7 @@ async function main() {
     .strict()
     .help('help');
 
-  const { browser, debug, retries, snaps, mv3, nft } = argv;
+  const { browser, debug, retries, snaps, mv3, nft, erc1155 } = argv;
 
   let testPaths;
 
@@ -72,6 +76,9 @@ async function main() {
     testPaths = await getTestPathsForTestDir(testDir);
   } else if (nft) {
     const testDir = path.join(__dirname, 'nft');
+    testPaths = await getTestPathsForTestDir(testDir);
+  } else if (erc1155) {
+    const testDir = path.join(__dirname, 'erc1155');
     testPaths = await getTestPathsForTestDir(testDir);
   } else {
     const testDir = path.join(__dirname, 'tests');
