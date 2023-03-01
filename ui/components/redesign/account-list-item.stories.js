67e054d47a228a486397c2995c636928be370932
David Walsh <davidwalsh83@gmail.com>
2023-03-01 10:08:41 -0600

    UX: Multichain: Account Menu List

---
+++ ui/components/redesign/account-list-item.stories.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { AccountListItem } from './account-list-item';
+
+const SampleIdentity = {
+  address: '0x12C7...135f',
+  name: 'Account 1',
+  balance: '0x0000',
+};
+
+const ChaosIdentity = {
+  address: '0x12C7...135f',
+  name: 'pneumonoultramicroscopicsilicovolcanoconiosis',
+  balance: '0x0000',
+};
+
+export default {
+  title: 'Components/Redesign/AccountListItem',
+  component: AccountListItem,
+};
+
+export const DefaultStory = () => (
+  <div style={{ width: '328px', border: '1px solid #eee' }}>
+    <AccountListItem identity={SampleIdentity} />
+  </div>
+);
+
+export const SelectedItem = () => (
+  <div style={{ width: '328px', border: '1px solid #eee' }}>
+    <AccountListItem identity={SampleIdentity} selected />
+  </div>
+);
+
+export const ChaosDataItem = () => (
+  <div style={{ width: '328px', border: '1px solid #eee' }}>
+    <AccountListItem identity={ChaosIdentity} selected />
+  </div>
+);
+
+DefaultStory.storyName = 'Default';
