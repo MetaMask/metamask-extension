29814fc0001f65b1e786e3c8c6169d407a2082f0
Danica Shen <zhaodanica@gmail.com>
2023-03-02 16:36:47 +0000

    feat(17493): bring back some pipelines

--- .circleci/scripts/validate-lavamoat-policy.sh
+++ .circleci/scripts/validate-lavamoat-policy.sh
@@ -4,15 +4,7 @@ set -e
 set -u
 set -o pipefail
 
-current_commit_msg=$(git show -s --format='%s' HEAD)
-
-if [[ $current_commit_msg =~ Version[[:space:]](v[[:digit:]]+.[[:digit:]]+.[[:digit:]]+[-]beta.[[:digit:]]) ]]
-then
-    printf '%s\n' 'Skip LavaMoat policy for beta commit'
-else
-    printf '%s\n' 'Validate lavaMoat policy for release build'
-    yarn lavamoat:auto:ci
-fi
+yarn lavamoat:auto:ci
 
 if git diff --exit-code
 then
