75bb4c612352023453181d9ceb7f71cfdb1e3c5d
NidhiKJha <menidhikjha@gmail.com>
2023-03-04 00:18:37 +0530

    updated tabs with role button

--- ui/components/multichain/token-list/token-list.js
+++ ui/components/multichain/token-list/token-list.js
@@ -36,13 +36,20 @@ export const NewTokenList = ({
       display={DISPLAY.FLEX}
       flexDirection={FLEX_DIRECTION.COLUMN}
       gap={4}
-      margin={4}
     >
       <Box
         className="token-list__container--cell"
         display={DISPLAY.FLEX}
         flexDirection={FLEX_DIRECTION.ROW}
+        padding={4}
         onClick={onClick}
+        role="button"
+        tabIndex={0}
+        onKeyPress={(event) => {
+          if (event.key === 'Enter') {
+            onClick();
+          }
+        }}
       >
         <AvatarWithBadge
           badgePosition={AVATAR_WITH_BADGE_POSTIONS.TOP}
