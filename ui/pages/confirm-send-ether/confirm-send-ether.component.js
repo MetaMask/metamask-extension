af6d4e3231209672a7d1247607d012f35ff804fb
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2023-03-03 10:04:10 -0500

    removing teh files that are added by mistake

--- ui/pages/confirm-send-ether/confirm-send-ether.component.js
+++ ui/pages/confirm-send-ether/confirm-send-ether.component.js
@@ -1,79 +1,42 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import ConfirmTransactionBase from '../confirm-transaction-base';
 import { SEND_ROUTE } from '../../helpers/constants/routes';
-import { editExistingTransaction } from '../../ducks/send';
-import { clearConfirmTransaction } from '../../ducks/confirm-transaction/confirm-transaction.duck';
-import { getTxData } from '../../selectors';
-import { AssetType } from '../../../shared/constants/transaction';
 
-export default function ConfirmSendEther() {
-  const history = useHistory();
-  const dispatch = useDispatch();
-
-  const txData = useSelector(getTxData);
-
-  const handleEditTransaction = async ({ confirmTransactionData }) => {
-    const { id } = confirmTransactionData;
-    await dispatch(editExistingTransaction(AssetType.token, id.toString()));
-    dispatch(clearConfirmTransaction());
+export default class ConfirmSendEther extends Component {
+  static contextTypes = {
+    t: PropTypes.func,
   };
 
-  const handleEdit = (confirmTransactionData) => {
-    handleEditTransaction(confirmTransactionData).then(() => {
+  static propTypes = {
+    editTransaction: PropTypes.func,
+    history: PropTypes.object,
+    txParams: PropTypes.object,
+  };
+
+  handleEdit({ txData }) {
+    const { editTransaction, history } = this.props;
+    editTransaction(txData).then(() => {
       history.push(SEND_ROUTE);
     });
-  };
+  }
 
-  const shouldHideData = () => {
-    const { txParams = {} } = txData;
+  shouldHideData() {
+    const { txParams = {} } = this.props;
     return !txParams.data;
-  };
+  }
 
-  return (
-    <ConfirmTransactionBase
-      actionKey="confirm"
-      hideData={shouldHideData}
-      onEdit={(confirmTransactionData) => handleEdit(confirmTransactionData)}
-    />
-  );
+  render() {
+    const hideData = this.shouldHideData();
+
+    return (
+      <ConfirmTransactionBase
+        actionKey="confirm"
+        hideData={hideData}
+        onEdit={(confirmTransactionData) =>
+          this.handleEdit(confirmTransactionData)
+        }
+      />
+    );
+  }
 }
-
-// export default class ConfirmSendEther extends Component {
-//   static contextTypes = {
-//     t: PropTypes.func,
-//   };
-
-//   static propTypes = {
-//     editTransaction: PropTypes.func,
-//     history: PropTypes.object,
-//     txParams: PropTypes.object,
-//   };
-
-//   handleEdit({ txData }) {
-//     const { editTransaction, history } = this.props;
-//     editTransaction(txData).then(() => {
-//       history.push(SEND_ROUTE);
-//     });
-//   }
-
-//   shouldHideData() {
-//     const { txParams = {} } = this.props;
-//     return !txParams.data;
-//   }
-
-//   render() {
-//     const hideData = this.shouldHideData();
-
-//     return (
-//       <ConfirmTransactionBase
-//         actionKey="confirm"
-//         hideData={hideData}
-//         onEdit={(confirmTransactionData) =>
-//           this.handleEdit(confirmTransactionData)
-//         }
-//       />
-//     );
-//   }
-// }
--- ui/pages/confirm-send-ether/index.js
+++ ui/pages/confirm-send-ether/index.js
@@ -1,1 +1,1 @@
-export { default } from './confirm-send-ether.component';
+export { default } from './confirm-send-ether.container';
