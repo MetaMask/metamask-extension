cffb9f3652b45d2f775eca1ac34b108a4f772a49
Davide Brocchetto <davide@paysense.in>
2023-03-07 10:10:59 +0100

    Removed empty lines and improved coding consistency

--- test/e2e/swaps/shared.js
+++ test/e2e/swaps/shared.js
@@ -62,48 +62,41 @@ const buildQuote = async (driver, options) => {
   await driver.clickElement('.searchable-item-list__primary-label');
 };
 
-const reviewQuote = async (driver, amount, swapFrom, swapTo) => {
+const reviewQuote = async (driver, options) => {
   await driver.clickElement({ text: 'Review swap', tag: 'button' });
   await driver.waitForSelector({ text: 'Swap', tag: 'button' });
-
   await driver.waitForSelector({
     css: '[class*="box--align-items-center"]',
     text: 'Estimated gas fee',
   });
-
   const sourceValue = await driver.waitForSelector(
     '.main-quote-summary__source-row-value',
   );
   assert.equal(
     await sourceValue.getText(),
-    amount,
+    options.amount,
     'Error: Quote has wrong amount',
   );
-
   const sourceSymbol = await driver.waitForSelector(
     '.main-quote-summary__source-row-symbol',
   );
   assert.equal(
     await sourceSymbol.getText(),
-    swapFrom,
+    options.swapFrom,
     'Error: SwapFrom has wrong symbol',
   );
-
   const swapToSymbol = await driver.waitForSelector(
     '.main-quote-summary__destination-row',
   );
   assert.equal(
     await swapToSymbol.getText(),
-    swapTo,
+    options.swapTo,
     'Error: SwapTo has wrong symbol',
   );
-
   await driver.waitForSelector(
     '[class="exchange-rate-display main-quote-summary__exchange-rate-display"]',
   );
-
   await driver.waitForSelector('[class="fee-card__info-tooltip-container"]');
-
   await driver.waitForSelector({
     css: '[class="countdown-timer__time"]',
     text: '0:23',
@@ -118,7 +111,6 @@ const waitForTransactionToComplete = async (driver, tokenName) => {
     },
     { timeout: 30000 },
   );
-
   assert.equal(
     await sucessfulTransactionMessage.getText(),
     'Transaction complete',
@@ -133,40 +125,28 @@ const waitForTransactionToComplete = async (driver, tokenName) => {
     tokenName,
     'Incorrect token name',
   );
-
   await driver.clickElement({ text: 'Close', tag: 'button' });
   await driver.waitForSelector('[data-testid="home__asset-tab"]');
 };
 
-const checkActivityTransaction = async (
-  driver,
-  index,
-  amount,
-  swapFrom,
-  swapTo,
-) => {
+const checkActivityTransaction = async (driver, options) => {
   await driver.clickElement('[data-testid="home__activity-tab"]');
-
   const itemsText = await driver.findElements('.list-item__title');
   assert.equal(
-    await itemsText[index].getText(),
-    `Swap ${swapFrom} to ${swapTo}`,
+    await itemsText[options.index].getText(),
+    `Swap ${options.swapFrom} to ${options.swapTo}`,
     'Title is incorrect',
   );
-
   const amountValues = await driver.findElements(
     '.transaction-list-item__primary-currency',
   );
-
   assert.equal(
-    await amountValues[index].getText(),
-    `-${amount} ${swapFrom}`,
+    await amountValues[options.index].getText(),
+    `-${options.amount} ${options.swapFrom}`,
     'Transaction amount is incorrect',
   );
-
-  await itemsText[index].click();
+  await itemsText[options.index].click();
   await driver.delay(regularDelayMs);
-
   const txStatus = await driver.findElement(
     '.transaction-list-item-details__tx-status >div > div:last-child',
   );
@@ -175,16 +155,14 @@ const checkActivityTransaction = async (
     `Confirmed`,
     `Transaction status is not 'Confirmed'`,
   );
-
   const txAmount = await driver.findElement(
     '.transaction-breakdown__value--amount',
   );
   assert.equal(
     await txAmount.getText(),
-    `-${amount} ${swapFrom}`,
+    `-${options.amount} ${options.swapFrom}`,
     'Transaction breakdown is incorrect',
   );
-
   await driver.clickElement('[data-testid="popover-close"]');
 };
 
--- test/e2e/swaps/swap-eth.spec.js
+++ test/e2e/swaps/swap-eth.spec.js
@@ -20,30 +20,40 @@ describe('Swap Eth for another Token', function () {
       },
       async ({ driver }) => {
         await loadExtension(driver);
-
         await buildQuote(driver, {
           amount: 0.001,
           swapTo: 'USDC',
         });
-
-        await reviewQuote(driver, '0.001', 'TESTETH', 'USDC');
-
+        await reviewQuote(driver, {
+          amount: '0.001',
+          swapFrom: 'TESTETH',
+          swapTo: 'USDC',
+        });
         await driver.clickElement({ text: 'Swap', tag: 'button' });
         await driver.clickElement({ text: 'View in activity', tag: 'button' });
-
         await buildQuote(driver, {
           amount: 0.003,
           swapTo: 'DAI',
         });
-
-        await reviewQuote(driver, '0.003', 'TESTETH', 'DAI');
-
+        await reviewQuote(driver, {
+          amount: '0.003',
+          swapFrom: 'TESTETH',
+          swapTo: 'DAI',
+        });
         await driver.clickElement({ text: 'Swap', tag: 'button' });
-
         await waitForTransactionToComplete(driver, 'DAI');
-
-        await checkActivityTransaction(driver, 0, '0.003', 'TESTETH', 'DAI');
-        await checkActivityTransaction(driver, 1, '0.001', 'TESTETH', 'USDC');
+        await checkActivityTransaction(driver, {
+          index: 0,
+          amount: '0.003',
+          swapFrom: 'TESTETH',
+          swapTo: 'DAI',
+        });
+        await checkActivityTransaction(driver, {
+          index: 1,
+          amount: '0.001',
+          swapFrom: 'TESTETH',
+          swapTo: 'USDC',
+        });
       },
     );
   });
@@ -59,14 +69,19 @@ describe('Swap Eth for another Token', function () {
           amount: 2,
           swapTo: 'DAI',
         });
-
-        await reviewQuote(driver, '2', 'TESTETH', 'DAI');
-
+        await reviewQuote(driver, {
+          amount: '2',
+          swapFrom: 'TESTETH',
+          swapTo: 'DAI',
+        });
         await driver.clickElement({ text: 'Swap', tag: 'button' });
-
         await waitForTransactionToComplete(driver, 'DAI');
-
-        await checkActivityTransaction(driver, 0, '2', 'TESTETH', 'DAI');
+        await checkActivityTransaction(driver, {
+          index: 0,
+          amount: '2',
+          swapFrom: 'TESTETH',
+          swapTo: 'DAI',
+        });
       },
     );
   });
