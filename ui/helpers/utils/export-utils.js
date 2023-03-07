e95a668f00c3160a84048b8d9e520442f561923e
weizman <weizmangal@gmail.com>
2023-03-07 03:43:42 +0200

    use buffer instead of textencoder

--- ui/helpers/utils/export-utils.js
+++ ui/helpers/utils/export-utils.js
@@ -1,18 +1,7 @@
 import { getRandomFileName } from './util';
 
-function encodeB64(text) {
-  return new Promise((resolve) => {
-    const buf = new TextEncoder().encode(text);
-    // eslint-disable-next-line no-undef
-    const reader = new FileReader();
-    reader.onload = () => resolve(reader.result.split(',')[1]);
-    // eslint-disable-next-line no-undef
-    reader.readAsDataURL(new Blob([buf]));
-  });
-}
-
 export async function exportAsFile(filename, data, type = 'text/csv') {
-  const b64 = await encodeB64(data);
+  const b64 = Buffer.from(data, 'utf8').toString('base64');
   // eslint-disable-next-line no-param-reassign
   filename = filename || getRandomFileName();
   const elem = window.document.createElement('a');
