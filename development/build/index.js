8c550e78e9b79e3ce729865f9ab99cf3b24c0ff6
Dan J Miller <danjm.com@gmail.com>
2023-03-08 09:38:24 -0800

    patch lavamoat files to make v10.26.0 compatible with v78 of chrome (#18053)
    Co-authored-by: Peter <53189696+PeterYinusa@users.noreply.github.com>

--- development/build/index.js
+++ development/build/index.js
@@ -94,6 +94,7 @@ async function defineAndRunBuildTasks() {
       'navigator',
       'harden',
       'console',
+      'location',
       // globals chrome driver needs to function (test env)
       /cdc_[a-zA-Z0-9]+_[a-zA-Z]+/iu,
       'performance',
