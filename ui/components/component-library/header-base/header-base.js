b6b90a17cb218eba2e0ae806fd29433bbb3251a6
garrettbear <gwhisten@gmail.com>
2023-03-08 12:45:15 -0800

    header base using flexbox

--- ui/components/component-library/header-base/header-base.js
+++ ui/components/component-library/header-base/header-base.js
@@ -2,13 +2,120 @@ import React, { useRef, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import {
-  AlignItems,
+  // AlignItems,
   DISPLAY,
+  JustifyContent,
   TextVariant,
   TEXT_ALIGN,
+  // FLEX_WRAP,
 } from '../../../helpers/constants/design-system';
 import Box from '../../ui/box';
 import { Text } from '../text';
+
+// export const HeaderBase = ({
+//   startAccessory,
+//   endAccessory,
+//   className,
+//   children,
+//   titleProps,
+//   ...props
+// }) => {
+//   const startAccessoryRef = useRef();
+//   const endAccessoryRef = useRef();
+//   const [accessoryMinWidth, setAccessoryMinWidth] = useState();
+
+//   function getLargerSize(item1, item2) {
+//     const size1 = item1.scrollWidth;
+//     const size2 = item2.scrollWidth;
+//     const largerSize = size1 > size2 ? size1 : size2;
+//     return largerSize;
+//   }
+
+//   useEffect(() => {
+//     function handleResize() {
+//       if (startAccessoryRef.current && endAccessoryRef.current) {
+//         // Both startAccessoryRef and endAccessoryRef exist, so will find the larger of the two
+//         const accMinWidth = getLargerSize(
+//           startAccessoryRef.current,
+//           endAccessoryRef.current,
+//         );
+//         setAccessoryMinWidth(accMinWidth);
+//       } else if (startAccessoryRef.current && !endAccessoryRef.current) {
+//         // Only startAccessoryRef exists
+//         setAccessoryMinWidth(startAccessoryRef.current.scrollWidth);
+//       } else if (!startAccessoryRef.current && endAccessoryRef.current) {
+//         // Only endAccessoryRef exists
+//         setAccessoryMinWidth(endAccessoryRef.current.scrollWidth);
+//       } else {
+//         // Neither startAccessoryRef nor endAccessoryRef exist
+//         setAccessoryMinWidth(0);
+//       }
+//     }
+
+//     handleResize();
+//     window.addEventListener('resize', handleResize);
+
+//     return () => {
+//       window.removeEventListener('resize', handleResize);
+//     };
+//   }, [startAccessoryRef, endAccessoryRef]);
+
+//   return (
+//     <Box
+//       className={classnames('mm-header-base', className)}
+//       // display={DISPLAY.GRID}
+//       alignItems={AlignItems.flexStart}
+//       style={{
+//         display: 'grid',
+//         gridTemplateColumns: `${accessoryMinWidth}px 1fr ${accessoryMinWidth}px`,
+//       }}
+//       {...props}
+//     >
+//       {startAccessory && (
+//         <Box
+//           display={DISPLAY.FLEX}
+//           alignItems={AlignItems.center}
+//           className="mm-header-base__start-accessory"
+//           ref={startAccessoryRef}
+//           style={{
+//             width: 'max-content',
+//             gridColumn: 1,
+//           }}
+//         >
+//           {startAccessory}
+//         </Box>
+//       )}
+//       {children && (
+//         <Text
+//           variant={TextVariant.headingSm}
+//           className="mm-header-base__title"
+//           textAlign={TEXT_ALIGN.CENTER}
+//           {...titleProps}
+//           style={{
+//             gridColumn: 2,
+//           }}
+//         >
+//           {children}
+//         </Text>
+//       )}
+//       {endAccessory && (
+//         <Box
+//           display={DISPLAY.FLEX}
+//           alignItems={AlignItems.center}
+//           marginLeft="auto"
+//           className="mm-header-base__end-accessory"
+//           ref={endAccessoryRef}
+//           style={{
+//             width: 'max-content',
+//             gridColumn: 3,
+//           }}
+//         >
+//           {endAccessory}
+//         </Box>
+//       )}
+//     </Box>
+//   );
+// };
 
 export const HeaderBase = ({
   startAccessory,
@@ -58,47 +165,74 @@ export const HeaderBase = ({
     };
   }, [startAccessoryRef, endAccessoryRef]);
 
-  useEffect(() => {
-    setAccessoryMinWidth(null);
-  }, []);
+  function getTitleStyles() {
+    if (startAccessory && endAccessory) {
+      return {
+        width: '100%',
+        backgroundColor: 'gold',
+      };
+    } else if (startAccessory && !endAccessory) {
+      return {
+        width: '100%',
+        marginRight: `${accessoryMinWidth}px`,
+        backgroundColor: 'green',
+      };
+    } else if (!startAccessory && endAccessory) {
+      return {
+        width: '100%',
+        marginLeft: `${accessoryMinWidth}px`,
+        backgroundColor: 'red',
+      };
+    }
+    return {
+      width: '100%',
+      backgroundColor: 'pink',
+    };
+  }
 
   return (
     <Box
       className={classnames('mm-header-base', className)}
-      display={DISPLAY.GRID}
-      alignItems={AlignItems.flexStart}
-      style={{
-        gridTemplateColumns: `${accessoryMinWidth}px 1fr ${accessoryMinWidth}px`,
-      }}
+      display={DISPLAY.FLEX}
+      justifyContent={JustifyContent.spaceBetween}
       {...props}
     >
       {startAccessory && (
         <Box
-          display={DISPLAY.FLEX}
-          alignItems={AlignItems.center}
           className="mm-header-base__start-accessory"
           ref={startAccessoryRef}
+          style={{
+            minWidth: `${accessoryMinWidth}px`,
+          }}
         >
           {startAccessory}
         </Box>
       )}
       {children && (
-        <Text
-          variant={TextVariant.headingSm}
-          className="mm-header-base__title"
-          textAlign={TEXT_ALIGN.CENTER}
-          {...titleProps}
+        <Box
+          style={getTitleStyles()}
+          display={DISPLAY.FLEX}
+          justifyContent={JustifyContent.center}
         >
-          {children}
-        </Text>
+          <Text
+            variant={TextVariant.headingSm}
+            className="mm-header-base__title"
+            textAlign={TEXT_ALIGN.CENTER}
+            {...titleProps}
+          >
+            {children}
+          </Text>
+        </Box>
       )}
       {endAccessory && (
         <Box
           display={DISPLAY.FLEX}
-          alignItems={AlignItems.center}
-          marginLeft="auto"
+          justifyContent={JustifyContent.flexEnd}
           className="mm-header-base__end-accessory"
           ref={endAccessoryRef}
+          style={{
+            minWidth: `${accessoryMinWidth}px`,
+          }}
         >
           {endAccessory}
         </Box>
@@ -106,213 +240,6 @@ export const HeaderBase = ({
     </Box>
   );
 };
-
-// export const HeaderBase = ({
-//   startAccessory,
-//   endAccessory,
-//   className,
-//   children,
-//   titleProps,
-//   ...props
-// }) => {
-//   const startAccessoryRef = useRef();
-//   const endAccessoryRef = useRef();
-//   const [accessoryMinWidth, setAccessoryMinWidth] = useState();
-
-//   useEffect(() => {
-//     function handleResize() {
-//       console.log('this ran');
-//       if (startAccessoryRef.current && endAccessoryRef.current) {
-//         // Both startAccessoryRef and endAccessoryRef exist, so will find the larger of the two
-//         const accMinWidth = getLargerSize(
-//           startAccessoryRef.current,
-//           endAccessoryRef.current,
-//         );
-//         setAccessoryMinWidth(accMinWidth);
-//       } else if (startAccessoryRef.current && !endAccessoryRef.current) {
-//         // Only startAccessoryRef exists
-//         setAccessoryMinWidth(startAccessoryRef.current.scrollWidth);
-//       } else if (!startAccessoryRef.current && endAccessoryRef.current) {
-//         // Only endAccessoryRef exists
-//         setAccessoryMinWidth(endAccessoryRef.current.scrollWidth);
-//       } else {
-//         // Neither startAccessoryRef nor endAccessoryRef exist
-//         setAccessoryMinWidth(0);
-//       }
-//     }
-
-//     handleResize(); // call once on mount
-
-//     window.addEventListener('resize', handleResize);
-
-//     return () => {
-//       window.removeEventListener('resize', handleResize);
-//     };
-//   }, [startAccessoryRef, endAccessoryRef]);
-
-//   function getLargerSize(item1, item2) {
-//     const size1 = item1.scrollWidth;
-//     const size2 = item2.scrollWidth;
-//     const largerSize = size1 > size2 ? size1 : size2;
-//     return largerSize;
-//   }
-
-//   return (
-//     <Box
-//       className={classnames('mm-header-base', className)}
-//       display={DISPLAY.GRID}
-//       alignItems={AlignItems.flexStart}
-//       style={{
-//         gridTemplateColumns: `${accessoryMinWidth}px 1fr ${accessoryMinWidth}px`,
-//         background: 'blue',
-//       }}
-//       {...props}
-//     >
-//       {startAccessory && (
-//         <Box
-//           display={DISPLAY.FLEX}
-//           alignItems={AlignItems.center}
-//           className="mm-header-base__start-accessory"
-//           ref={startAccessoryRef}
-//           style={{
-//             background: 'red',
-//             width: 'max-content',
-//           }}
-//         >
-//           {startAccessory}
-//         </Box>
-//       )}
-//       {children && (
-//         <Text
-//           variant={TextVariant.headingSm}
-//           className="mm-header-base__title"
-//           {...titleProps}
-//         >
-//           {children}
-//         </Text>
-//       )}
-//       {endAccessory && (
-//         <Box
-//           display={DISPLAY.FLEX}
-//           alignItems={AlignItems.center}
-//           marginLeft="auto"
-//           className="mm-header-base__end-accessory"
-//           ref={endAccessoryRef}
-//           style={{
-//             background: 'red',
-//             width: 'max-content',
-//           }}
-//         >
-//           {endAccessory}
-//         </Box>
-//       )}
-//     </Box>
-//   );
-// };
-
-// import React, { useRef, useEffect, useState } from 'react';
-// import PropTypes from 'prop-types';
-// import classnames from 'classnames';
-// import {
-//   AlignItems,
-//   DISPLAY,
-//   TextVariant,
-// } from '../../../helpers/constants/design-system';
-// import Box from '../../ui/box';
-// import { Text } from '../text';
-
-// export const HeaderBase = ({
-//   startAccessory,
-//   endAccessory,
-//   className,
-//   children,
-//   titleProps,
-//   ...props
-// }) => {
-//   const startAccessoryRef = useRef();
-//   const endAccessoryRef = useRef();
-//   const [accessoryMinWidth, setAccessoryMinWidth] = useState();
-
-//   useEffect(() => {
-//     console.log('this ran');
-//     if (startAccessoryRef.current && endAccessoryRef.current) {
-//       // Both startAccessoryRef and endAccessoryRef exist, so will find the larger of the two
-//       const accMinWidth = getLargerSize(
-//         startAccessoryRef.current,
-//         endAccessoryRef.current,
-//       );
-//       setAccessoryMinWidth(accMinWidth);
-//     } else if (startAccessoryRef.current && !endAccessoryRef.current) {
-//       // Only startAccessoryRef exists
-//       setAccessoryMinWidth(startAccessoryRef.current.scrollWidth);
-//     } else if (!startAccessoryRef.current && endAccessoryRef.current) {
-//       // Only endAccessoryRef exists
-//       setAccessoryMinWidth(endAccessoryRef.current.scrollWidth);
-//     } else {
-//       // Neither startAccessoryRef nor endAccessoryRef exist
-//       setAccessoryMinWidth(0);
-//     }
-//   }, [startAccessoryRef, endAccessoryRef]);
-
-//   function getLargerSize(item1, item2) {
-//     const size1 = item1.scrollWidth;
-//     const size2 = item2.scrollWidth;
-//     const largerSize = size1 > size2 ? size1 : size2;
-//     return largerSize;
-//   }
-
-//   return (
-//     <Box
-//       className={classnames('mm-header-base', className)}
-//       display={DISPLAY.GRID}
-//       alignItems={AlignItems.flexStart}
-//       style={{
-//         gridTemplateColumns: `${accessoryMinWidth}px 1fr ${accessoryMinWidth}px`,
-//         background: 'blue',
-//       }}
-//       {...props}
-//     >
-//       {startAccessory && (
-//         <Box
-//           display={DISPLAY.FLEX}
-//           alignItems={AlignItems.center}
-//           className="mm-header-base__start-accessory"
-//           ref={startAccessoryRef}
-//           style={{
-//             background: 'red',
-//             width: 'max-content',
-//           }}
-//         >
-//           {startAccessory}
-//         </Box>
-//       )}
-//       {children && (
-//         <Text
-//           variant={TextVariant.headingSm}
-//           className="mm-header-base__title"
-//           {...titleProps}
-//         >
-//           {children}
-//         </Text>
-//       )}
-//       {endAccessory && (
-//         <Box
-//           display={DISPLAY.FLEX}
-//           alignItems={AlignItems.center}
-//           marginLeft="auto"
-//           className="mm-header-base__end-accessory"
-//           ref={endAccessoryRef}
-//           style={{
-//             background: 'red',
-//             width: 'max-content',
-//           }}
-//         >
-//           {endAccessory}
-//         </Box>
-//       )}
-//     </Box>
-//   );
-// };
 
 HeaderBase.propTypes = {
   /**
