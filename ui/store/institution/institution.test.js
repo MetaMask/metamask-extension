d5f3d248d0a9e0a7d48b8c872f2298f953185611
Antonio Regadas <antonio.regadas@consensys.net>
2023-03-09 11:45:33 +0000

    MMI lint fix

--- ui/store/institution/institution.test.js
+++ ui/store/institution/institution.test.js
@@ -1,8 +1,8 @@
-import { mmiActionsFactory } from "./institution-background";
+import { mmiActionsFactory } from './institution-background';
 
-describe("MMI Actions", () => {
-  describe("#mmiActionsFactory", () => {
-    it("returns mmiActions object", async () => {
+describe('MMI Actions', () => {
+  describe('#mmiActionsFactory', () => {
+    it('returns mmiActions object', async () => {
       const actionsMock = {
         connectCustodyAddresses: jest.fn(),
         getCustodianAccounts: jest.fn(),
@@ -37,59 +37,85 @@ describe("MMI Actions", () => {
         callBackgroundMethod: jest.fn(() => actionsMock),
       });
 
-      const connectCustodyAddresses = mmiActions.connectCustodyAddresses({}, "0xAddress");
-      mmiActions.getCustodianAccounts("token", "apiUrl", "custody", "getNonImportedAccounts", {});
-      mmiActions.getCustodianAccountsByAddress("jwt", "apiUrl", "address", "custody", {}, 4);
+      const connectCustodyAddresses = mmiActions.connectCustodyAddresses(
+        {},
+        '0xAddress',
+      );
+      mmiActions.getCustodianAccounts(
+        'token',
+        'apiUrl',
+        'custody',
+        'getNonImportedAccounts',
+        {},
+      );
+      mmiActions.getCustodianAccountsByAddress(
+        'jwt',
+        'apiUrl',
+        'address',
+        'custody',
+        {},
+        4,
+      );
       mmiActions.getMmiConfiguration({
         portfolio: {
           enabled: true,
-          url: "https://portfolio.io",
+          url: 'https://portfolio.io',
         },
         custodians: [],
       });
       mmiActions.getCustodianToken({});
       mmiActions.getCustodianConnectRequest({
-        token: "token",
-        custodianType: "custodianType",
-        custodianName: "custodianname",
+        token: 'token',
+        custodianType: 'custodianType',
+        custodianName: 'custodianname',
         apiUrl: undefined,
       });
-      mmiActions.getCustodianTransactionDeepLink("0xAddress", "txId");
-      mmiActions.getCustodianConfirmDeepLink("txId");
-      mmiActions.getCustodianSignMessageDeepLink("0xAddress", "custodyTxId");
+      mmiActions.getCustodianTransactionDeepLink('0xAddress', 'txId');
+      mmiActions.getCustodianConfirmDeepLink('txId');
+      mmiActions.getCustodianSignMessageDeepLink('0xAddress', 'custodyTxId');
       mmiActions.getCustodianJWTList({});
       mmiActions.getAllCustodianAccountsWithToken({
-        custodianType: "custodianType",
-        token: "token",
+        custodianType: 'custodianType',
+        token: 'token',
       });
       mmiActions.setComplianceAuthData({
-        clientId: "id",
-        projectId: "projectId",
+        clientId: 'id',
+        projectId: 'projectId',
       });
       mmiActions.deleteComplianceAuthData();
-      mmiActions.generateComplianceReport("0xAddress");
-      mmiActions.getComplianceHistoricalReportsByAddress("0xAddress", "projectId");
+      mmiActions.generateComplianceReport('0xAddress');
+      mmiActions.getComplianceHistoricalReportsByAddress(
+        '0xAddress',
+        'projectId',
+      );
       mmiActions.syncReportsInProgress({
-        address: "0xAddress",
+        address: '0xAddress',
         historicalReports: [],
       });
       mmiActions.removeConnectInstitutionalFeature({
-        origin: "origin",
-        projectId: "projectId",
+        origin: 'origin',
+        projectId: 'projectId',
       });
       mmiActions.removeAddTokenConnectRequest({
-        origin: "origin",
-        apiUrl: "https://jupiter-custody.codefi.network",
-        token: "token",
+        origin: 'origin',
+        apiUrl: 'https://jupiter-custody.codefi.network',
+        token: 'token',
       });
       mmiActions.setCustodianConnectRequest({
-        token: "token",
-        apiUrl: "https://jupiter-custody.codefi.network",
-        custodianType: "custodianType",
-        custodianName: "custodianname",
+        token: 'token',
+        apiUrl: 'https://jupiter-custody.codefi.network',
+        custodianType: 'custodianType',
+        custodianName: 'custodianname',
       });
-      const setWaitForConfirmDeepLinkDialog = mmiActions.setWaitForConfirmDeepLinkDialog(true);
-      mmiActions.setCustodianNewRefreshToken("address", "oldAuthDetails", "oldApiUrl", "newAuthDetails", "newApiUrl");
+      const setWaitForConfirmDeepLinkDialog =
+        mmiActions.setWaitForConfirmDeepLinkDialog(true);
+      mmiActions.setCustodianNewRefreshToken(
+        'address',
+        'oldAuthDetails',
+        'oldApiUrl',
+        'newAuthDetails',
+        'newApiUrl',
+      );
       connectCustodyAddresses(jest.fn());
       setWaitForConfirmDeepLinkDialog(jest.fn());
       expect(connectCustodyAddresses).toBeDefined();
