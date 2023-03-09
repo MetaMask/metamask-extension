7250430fa2b3cb6d3d74873a9fb632823d04d933
garrettbear <gwhisten@gmail.com>
2023-03-08 17:58:45 -0800

    update tests

--- ui/components/component-library/header-base/header-base.test.js
+++ ui/components/component-library/header-base/header-base.test.js
@@ -1,17 +1,14 @@
 /* eslint-disable jest/require-top-level-describe */
 import { render } from '@testing-library/react';
 import React from 'react';
-
-import { renderWithUserEvent } from '../../../../test/lib/render-helpers';
-
 import { Icon, ICON_NAMES } from '..';
 import { HeaderBase } from './header-base';
 
 describe('HeaderBase', () => {
-  it('should render bannerbase element correctly', () => {
+  it('should render HeaderBase element correctly', () => {
     const { getByTestId, container } = render(
-      <HeaderBase data-testid="header-base" title="Bannerbase test">
-        should render bannerbase element correctly
+      <HeaderBase data-testid="header-base" title="HeaderBase test">
+        should render HeaderBase element correctly
       </HeaderBase>,
     );
     expect(getByTestId('header-base')).toHaveClass('mm-header-base');
@@ -23,58 +20,22 @@ describe('HeaderBase', () => {
       <HeaderBase
         className="mm-header-base--test"
         data-testid="header-base"
-        title="Bannerbase test"
+        title="HeaderBase test"
       >
-        should render bannerbase element correctly
+        should render HeaderBase element correctly
       </HeaderBase>,
     );
     expect(getByTestId('header-base')).toHaveClass('mm-header-base--test');
   });
 
-  it('should render bannerbase title', () => {
-    const { getByText } = render(<HeaderBase title="Bannerbase title test" />);
-    expect(getByText('Bannerbase title test')).toHaveClass(
-      'mm-header-base__title',
-    );
-  });
-
-  it('should render bannerbase description', () => {
+  it('should render HeaderBase children', () => {
     const { getByText } = render(
-      <HeaderBase description="Bannerbase description test" />,
+      <HeaderBase>HeaderBase children test</HeaderBase>,
     );
-    expect(getByText('Bannerbase description test')).toBeDefined();
+    expect(getByText('HeaderBase children test')).toBeDefined();
   });
 
-  it('should render bannerbase children', () => {
-    const { getByText } = render(
-      <HeaderBase>Bannerbase children test</HeaderBase>,
-    );
-    expect(getByText('Bannerbase children test')).toBeDefined();
-  });
-
-  it('should render bannerbase action button', () => {
-    const { getByTestId } = render(
-      <HeaderBase
-        title="Action prop demo"
-        actionButtonLabel="Action"
-        actionButtonProps={{
-          endIconName: ICON_NAMES.ARROW_2_RIGHT,
-          'data-testid': 'action',
-          className: 'mm-header-base__action',
-        }}
-        actionButtonOnClick={() =>
-          console.log('ButtonLink actionButtonOnClick demo')
-        }
-      >
-        Use actionButtonLabel for action text, actionButtonOnClick for the
-        onClick handler, and actionButtonProps to pass any ButtonLink prop types
-        such as iconName
-      </HeaderBase>,
-    );
-    expect(getByTestId('action')).toHaveClass('mm-header-base__action');
-  });
-
-  it('should render bannerbase startAccessory', () => {
+  it('should render HeaderBase startAccessory', () => {
     const { getByTestId } = render(
       <HeaderBase
         startAccessory={
@@ -86,16 +47,15 @@ describe('HeaderBase', () => {
     expect(getByTestId('start-accessory')).toBeDefined();
   });
 
-  it('should render and fire onClose event', async () => {
-    const onClose = jest.fn();
-    const { user, getByTestId } = renderWithUserEvent(
+  it('should render HeaderBase endAccessory', () => {
+    const { getByTestId } = render(
       <HeaderBase
-        title="onClose Test"
-        closeButtonProps={{ 'data-testid': 'close-button' }}
-        onClose={onClose}
+        endAccessory={
+          <Icon data-testid="end-accessory" name={ICON_NAMES.ADD_SQUARE} />
+        }
       />,
     );
-    await user.click(getByTestId('close-button'));
-    expect(onClose).toHaveBeenCalledTimes(1);
+
+    expect(getByTestId('end-accessory')).toBeDefined();
   });
 });
