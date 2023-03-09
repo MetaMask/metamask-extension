535d55ea8b8f73c6a475fc84841a55c24d87a04c
PeterYinusa <peter.yinusa@consensys.net>
2023-03-09 10:37:54 +0000

    bump test coverage

--- coverage-targets.js
+++ coverage-targets.js
@@ -6,10 +6,10 @@
 // subset of files to check against these targets.
 module.exports = {
   global: {
-    lines: 63,
-    branches: 51,
-    statements: 62,
-    functions: 55,
+    lines: 63.5,
+    branches: 52,
+    statements: 63,
+    functions: 56,
   },
   transforms: {
     branches: 100,
