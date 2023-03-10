a892763f928c66669ae52455084c267799f36776
David Walsh <davidwalsh83@gmail.com>
2023-03-10 12:33:50 -0600

    Restructure item menu story

--- ui/components/multichain/account-list-item-menu/account-list-item-menu.stories.js
+++ ui/components/multichain/account-list-item-menu/account-list-item-menu.stories.js
@@ -1,24 +1,37 @@
 import React from 'react';
 import { AccountListItemMenu } from './account-list-item-menu';
 
-const SampleIdentity = {
-  address: '0x12C7...135f',
-  name: 'Account 1',
-  balance: '0x152387ad22c3f0',
-  tokenBalance: '32.09 ETH',
-};
-
 export default {
   title: 'Components/Multichain/AccountListItemMenu',
   component: AccountListItemMenu,
+  argTypes: {
+    anchorElement: {
+      control: 'window.Element',
+    },
+    onClose: {
+      action: 'onClose',
+    },
+    blockExplorerUrlSubTitle: {
+      control: 'text',
+    },
+    isRemovable: {
+      control: 'boolean',
+    },
+    identity: {
+      control: 'object',
+    },
+  },
+  args: {
+    anchorElement: null,
+    identity: {
+      address: '0x12C7...135f',
+      name: 'Account 1',
+      balance: '0x152387ad22c3f0',
+      tokenBalance: '32.09 ETH',
+    },
+    isRemovable: true,
+  },
 };
 
-export const DefaultStory = () => (
-  <AccountListItemMenu
-    anchorElement={null}
-    identity={SampleIdentity}
-    isRemovable
-    blockExplorerUrlSubTitle="etherscan.io"
-    onClose={() => console.log(`Closed menu for ${SampleIdentity.address}`)}
-  />
-);
+export const DefaultStory = (args) => <AccountListItemMenu {...args} />;
+DefaultStory.storyName = 'Default';
