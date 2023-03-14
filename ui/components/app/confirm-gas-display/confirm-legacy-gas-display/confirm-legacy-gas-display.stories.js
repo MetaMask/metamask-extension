852163cbd3ea23a2b22ad3a45ec75337dfc4be7c
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2023-03-14 17:09:01 -0400

    adding storybook entry for confirm-legacy-gas-display

---
+++ ui/components/app/confirm-gas-display/confirm-legacy-gas-display/confirm-legacy-gas-display.stories.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+
+import mockState from '../../../../../test/data/mock-state.json';
+import configureStore from '../../../../store/store';
+
+import README from './README.mdx';
+import ConfirmLegacyGasDisplay from './confirm-legacy-gas-display';
+
+const store = configureStore(mockState);
+
+export default {
+  title: 'Components/App/ConfirmLegacyGasDisplay',
+
+  component: ConfirmLegacyGasDisplay,
+  decorators: [(story) => <Provider store={store}>{story()}</Provider>],
+  parameters: {
+    docs: {
+      page: README,
+    },
+  },
+  argTypes: {
+    draftTransaction: 'object',
+  },
+  args: {
+    draftTransaction: {},
+  },
+};
+
+export const DefaultStory = (args) => {
+  return <ConfirmLegacyGasDisplay {...args} />;
+};
+
+DefaultStory.storyName = 'Default';
