b1f7570bb90a24981a89382ef9a3b139d8312a8e
Antonio Regadas <antonio.regadas@consensys.net>
2023-03-15 11:58:47 +0000

    prettier fix

--- ui/components/institutional/note-to-trader/note-to-trader.stories.js
+++ ui/components/institutional/note-to-trader/note-to-trader.stories.js
@@ -5,16 +5,15 @@ export default {
   title: 'Components/Institutional/NoteToTrader',
   component: NoteToTrader,
   args: {
-    placeholder: 'The approver will see this note when approving the transaction at the custodian.',
+    placeholder:
+      'The approver will see this note when approving the transaction at the custodian.',
     noteText: '',
     labelText: 'Transaction note',
     maxLength: '280',
-    onChange: () => {},
+    onChange: () => {/**/}
   },
 };
 
-export const DefaultStory = (args) => (
-  <NoteToTrader {...args} />
-);
+export const DefaultStory = (args) => <NoteToTrader {...args} />;
 
 DefaultStory.storyName = 'NoteToTrader';
