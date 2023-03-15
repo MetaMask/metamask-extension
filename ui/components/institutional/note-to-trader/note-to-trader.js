b17ad3bbf4ae6387fd711e8ea28c72929ee19e2c
Antonio Regadas <antonio.regadas@consensys.net>
2023-03-15 11:08:45 +0000

    review fixes

--- ui/components/institutional/note-to-trader/note-to-trader.js
+++ ui/components/institutional/note-to-trader/note-to-trader.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {
+  DISPLAY,
+  FLEX_DIRECTION,
+  JustifyContent,
+} from '../../../helpers/constants/design-system';
+import { Label, Text } from '../../component-library';
 import Box from '../../ui/box';
 
 const NoteToTrader = (props) => {
@@ -7,13 +13,21 @@ const NoteToTrader = (props) => {
 
   return (
     <>
-      <Box className="note-header">
-        <label htmlFor="transaction-note">{labelText}</label>
-        <p className="note-header__counter">
+      <Box
+        className="note-header"
+        display={DISPLAY.FLEX}
+        justifyContent={JustifyContent.spaceBetween}
+      >
+        <Label htmlFor="transaction-note">{labelText}</Label>
+        <Text className="note-header__counter">
           {noteText.length}/{maxLength}
-        </p>
+        </Text>
       </Box>
-      <Box className="note-field">
+      <Box
+        display={DISPLAY.FLEX}
+        flexDirection={FLEX_DIRECTION.COLUMN}
+        className="note-field"
+      >
         <textarea
           id="transaction-note"
           data-testid="transaction-note"
