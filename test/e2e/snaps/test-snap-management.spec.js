a1df02cf599f968cb225a1d062c9d6eb8a497c2f
david0xd <david@timechaser.org>
2023-03-16 14:28:48 +0100

    Fix e2e test expectation

--- test/e2e/snaps/test-snap-management.spec.js
+++ test/e2e/snaps/test-snap-management.spec.js
@@ -149,7 +149,7 @@ describe('Test Snap Management', function () {
         );
         assert.equal(
           await removeResult.getText(),
-          'You have no MetaMask Snaps installed.\\nLearn more',
+          'You have no MetaMask Snaps installed.\nLearn more',
         );
       },
     );
