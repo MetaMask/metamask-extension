df1a1401002b0f61726061c09233fe18b2c8c788
David Walsh <davidwalsh83@gmail.com>
2023-03-15 19:23:55 -0500

    UX: Multichain: Account Picker

---
+++ ui/components/multichain/account-picker/index.stories.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { AccountPicker } from '.';
+
+const CHAOS_ACCOUNT = {
+  address: '"0xb19ac54efa18cc3a14a5b821bfec73d284bf0c5e"',
+  name: 'Account That Has A Really Really Really Really Really Long Name',
+  balance: '0x152387ad22c3f0',
+};
+
+export default {
+  title: 'Components/Multichain/AccountPicker',
+  component: AccountPicker,
+  argTypes: {
+    account: {
+      control: 'object',
+    },
+  },
+  args: {
+    account: {
+      address: '"0xb19ac54efa18cc3a14a5b821bfec73d284bf0c5e"',
+      name: 'Account 1',
+      balance: '0x152387ad22c3f0',
+    },
+  },
+};
+
+export const DefaultStory = (args) => <AccountPicker {...args} />;
+DefaultStory.storyName = 'Default';
+
+export const ChaosStory = (args) => (
+  <div style={{ maxWidth: '300px', background: 'pink' }}>
+    <AccountPicker {...args} account={CHAOS_ACCOUNT} />
+  </div>
+);
+DefaultStory.storyName = 'Default';
