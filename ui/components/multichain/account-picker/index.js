91904e372f9bc3a17132bde37801ad5d1a27e80b
David Walsh <davidwalsh83@gmail.com>
2023-03-15 19:23:55 -0500

    ButtonBase ellipsis update
    Update ui/components/multichain/account-picker/index.js
    
    Co-authored-by: Garrett Bear <gwhisten@gmail.com>
    
    Update ui/components/multichain/account-picker/index.js
    
    Co-authored-by: Garrett Bear <gwhisten@gmail.com>
    
    Update ui/components/multichain/account-picker/index.js
    
    Co-authored-by: Garrett Bear <gwhisten@gmail.com>
    
    Update ui/components/multichain/account-picker/index.js
    
    Co-authored-by: Garrett Bear <gwhisten@gmail.com>

---
+++ ui/components/multichain/account-picker/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  ButtonBase,
+  ICON_NAMES,
+  AvatarAccount,
+  Icon,
+  Text,
+} from '../../component-library';
+import { Color, FONT_WEIGHT, Size } from '../../../helpers/constants/design-system';
+
+export const AccountPicker = ({ account, onClick }) => {
+  return (
+    <ButtonBase
+      onClick={onClick}
+      backgroundColor={Color.transparent}
+    >
+      <AvatarAccount address={account.address} size={Size.SM} />
+      <Text
+        marginLeft={1}
+        marginRight={1}
+        fontWeight={FONT_WEIGHT.BOLD}
+        ellipsis
+        style={{
+          width: '100px'
+        }}
+      >
+        {account.name}
+      </Text>
+      <Icon name={ICON_NAMES.ARROW_DOWN} size={Size.SM} />
+    </ButtonBase>
+  );
+};
+
+AccountPicker.propTypes = {
+  account: PropTypes.object.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
