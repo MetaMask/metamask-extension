eb4874ad4bba94e9b1ecadfe8b077085ec7e7250
David Walsh <davidwalsh83@gmail.com>
2023-03-15 19:23:55 -0500

    UX: Multichain: Account Picker

---
+++ ui/components/multichain/account-picker/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  ButtonBase,
+  ICON_NAMES,
+  AvatarAccount,
+  Icon,
+  Text,
+} from '../../component-library';
+import { FONT_WEIGHT, Size } from '../../../helpers/constants/design-system';
+
+export const AccountPicker = ({ account, onClick }) => {
+  return (
+    <ButtonBase
+      onClick={onClick}
+      style={{ background: 'transparent' }}
+    >
+      <AvatarAccount address={account.address} size={Size.SM} />
+      <Text
+        marginLeft={1}
+        marginRight={1}
+        fontWeight={FONT_WEIGHT.BOLD}
+        style={{
+          textOverflow: 'ellipsis',
+          overflow: 'hidden',
+          whiteSpace: 'nowrap',
+          width: '100px'
+        }}
+      >
+        {account.name}
+      </Text>
+      <Icon name={ICON_NAMES.ARROW_DOWN} size={Size.SM} />
+    </ButtonBase>
+  );
+};
+
+AccountPicker.propTypes = {
+  account: PropTypes.object.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
