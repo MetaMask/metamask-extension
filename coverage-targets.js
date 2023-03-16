d7d38536ac067b02e2e110743d77dcd72fbee77c
Monte <monte.lai@consensys.net>
2023-03-16 21:12:05 +0800

    update coverage target

--- coverage-targets.js
+++ coverage-targets.js
@@ -6,10 +6,10 @@
 // subset of files to check against these targets.
 module.exports = {
   global: {
-    lines: 65,
-    branches: 53.5,
-    statements: 64,
-    functions: 57.5,
+    lines: 65.23,
+    branches: 53.77,
+    statements: 64.4,
+    functions: 57.77,
   },
   transforms: {
     branches: 100,
