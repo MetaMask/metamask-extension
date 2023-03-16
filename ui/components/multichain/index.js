e26c0caa48f4a3cf95d9d46d9524a705208bccf2
David Walsh <davidwalsh83@gmail.com>
2023-03-16 18:16:26 -0500

    Add MULTICHAIN flag code

--- ui/components/multichain/index.js
+++ ui/components/multichain/index.js
@@ -4,3 +4,4 @@ export { AccountListMenu } from './account-list-menu';
 export { DetectedTokensBanner } from './detected-token-banner';
 export { MultichainImportTokenLink } from './multichain-import-token-link';
 export { MultichainTokenListItem } from './multichain-token-list-item';
+export { AddressCopyButton } from './address-copy-button';
