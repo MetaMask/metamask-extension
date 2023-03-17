8be1457bea9bce0445cbf2eeab4f601f8c1c285c
David Walsh <davidwalsh83@gmail.com>
2023-03-17 13:45:24 -0500

    Multichain: UX: Network Menu

---
+++ ui/components/multichain/network-list-item/network-list-item.stories.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { NetworkListItem } from '.';
+
+export default {
+  title: 'Components/Multichain/NetworkListItem',
+  component: NetworkListItem,
+  argTypes: {
+    name: {
+      control: 'text',
+    },
+    selected: {
+      control: 'boolean',
+    },
+    onClick: {
+      action: 'onClick',
+    },
+    iconSrc: {
+      action: 'text',
+    },
+  },
+  args: {
+    name: 'Ethereum',
+    iconSrc: '',
+    selected: false,
+    onClick: () => console.log('Network click!'),
+  },
+};
+
+export const DefaultStory = (args) => (
+  <div
+    style={{ width: '328px', border: '1px solid var(--color-border-muted)' }}
+  >
+    <NetworkListItem {...args} />
+  </div>
+);
+
+export const SelectedStory = (args) => (
+  <div
+    style={{ width: '328px', border: '1px solid var(--color-border-muted)' }}
+  >
+    <NetworkListItem {...args} selected />
+  </div>
+);
+
+export const ChaosStory = (args) => (
+  <div
+    style={{ width: '328px', border: '1px solid var(--color-border-muted)' }}
+  >
+    <NetworkListItem
+      {...args}
+      name="This is a super long network name"
+      selected
+    />
+  </div>
+);
