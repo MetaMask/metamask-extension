390b8ce62344162ac655a8de1118b7b6d34ccb59
Aleksandar Mihajlovic <aleksandar.mihajlovic-ext@consensys.net>
2023-03-20 10:21:36 +0100

    Revert "E2e fix"
    This reverts commit c570d54b3dc7fac373f21e3fb176a38e8dab7932.

--- test/e2e/tests/send-hex-address.spec.js
+++ test/e2e/tests/send-hex-address.spec.js
@@ -161,7 +161,7 @@ describe('Send ERC20 to a 40 character hexadecimal address', function () {
         // Confirm transaction
         await driver.waitForSelector({
           css: '.confirm-page-container-summary__title',
-          text: '1 TST',
+          text: '0 TST',
         });
         await driver.clickElement({ text: 'Confirm', tag: 'button' });
         await driver.clickElement('[data-testid="home__activity-tab"]');
@@ -226,7 +226,7 @@ describe('Send ERC20 to a 40 character hexadecimal address', function () {
         // Confirm transaction
         await driver.waitForSelector({
           css: '.confirm-page-container-summary__title',
-          text: '1 TST',
+          text: '0 TST',
         });
         await driver.clickElement({ text: 'Confirm', tag: 'button' });
         await driver.clickElement('[data-testid="home__activity-tab"]');
