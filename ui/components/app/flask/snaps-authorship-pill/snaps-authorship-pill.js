a0a800167279ddf3d89ccc4a56f22ce2b00480e6
Frederik Bolding <frederik.bolding@gmail.com>
2023-03-20 17:14:15 +0100

    Create new snap authorship component

--- ui/components/app/flask/snaps-authorship-pill/snaps-authorship-pill.js
+++ ui/components/app/flask/snaps-authorship-pill/snaps-authorship-pill.js
@@ -2,22 +2,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { getSnapPrefix } from '@metamask/snaps-utils';
-import Chip from '../../../ui/chip';
 import Box from '../../../ui/box';
-import Typography from '../../../ui/typography';
 import {
-  TypographyVariant,
-  TEXT_ALIGN,
   BackgroundColor,
   TextColor,
+  IconColor,
+  FLEX_DIRECTION,
+  TextVariant,
+  BorderColor,
+  AlignItems,
+  DISPLAY,
+  BorderRadius,
 } from '../../../../helpers/constants/design-system';
-import { useI18nContext } from '../../../../hooks/useI18nContext';
 import {
   getSnapName,
   removeSnapIdPrefix,
 } from '../../../../helpers/utils/util';
+import {
+  AvatarFavicon,
+  BadgeWrapper,
+  BadgeWrapperPosition,
+  ICON_NAMES,
+  ICON_SIZES,
+  AvatarIcon,
+  Text,
+  ButtonIcon,
+} from '../../../component-library';
 
-const SnapsAuthorshipPill = ({ snapId, version, className }) => {
+const SnapsAuthorshipPill = ({ snapId, className }) => {
   // We're using optional chaining with snapId, because with the current implementation
   // of snap update in the snap controller, we do not have reference to snapId when an
   // update request is rejected because the reference comes from the request itself and not subject metadata
@@ -28,57 +40,61 @@ const SnapsAuthorshipPill = ({ snapId, version, className }) => {
   const url = isNPM
     ? `https://www.npmjs.com/package/${packageName}`
     : packageName;
-  const icon = isNPM ? 'fab fa-npm fa-lg' : 'fas fa-code';
-  const t = useI18nContext();
 
   const friendlyName = getSnapName(snapId);
 
   return (
-    <a
-      href={url}
-      target="_blank"
-      rel="noopener noreferrer"
-      className={classnames(className, `snaps-authorship-pill`)}
+    <Box
+      className={classnames('mm-tag-url', className)}
+      backgroundColor={BackgroundColor.backgroundDefault}
+      borderColor={BorderColor.borderDefault}
+      borderWidth={1}
+      alignItems={AlignItems.center}
+      paddingLeft={2}
+      paddingTop={2}
+      paddingBottom={2}
+      paddingRight={4}
+      borderRadius={BorderRadius.pill}
+      display={DISPLAY.FLEX}
     >
-      <Chip
-        leftIcon={
-          <Box paddingLeft={2}>
-            <i className={`${icon} snaps-authorship-icon`} />
-          </Box>
-        }
-        rightIcon={
-          version && (
-            <Box
-              className="snaps-authorship-version"
-              backgroundColor={BackgroundColor.primaryDefault}
-              paddingLeft={2}
-              paddingRight={2}
-            >
-              <Typography
-                color={TextColor.primaryInverse}
-                variant={TypographyVariant.H7}
-                align={TEXT_ALIGN.CENTER}
-                as="span"
-                className="version"
-              >
-                {t('shorthandVersion', [version])}
-              </Typography>
-            </Box>
-          )
-        }
-        backgroundColor={BackgroundColor.backgroundDefault}
-      >
-        <Typography
-          className="chip__label"
-          variant={TypographyVariant.H7}
-          as="span"
-          color={TextColor.textAlternative}
-          title={friendlyName}
+      <Box>
+        <BadgeWrapper
+          badge={
+            <AvatarIcon
+              iconName={ICON_NAMES.SNAPS}
+              size={ICON_SIZES.XS}
+              backgroundColor={IconColor.infoDefault}
+              iconProps={{
+                size: ICON_SIZES.XS,
+                color: IconColor.infoInverse,
+              }}
+            />
+          }
+          position={BadgeWrapperPosition.bottomRight}
         >
-          {friendlyName}
-        </Typography>
-      </Chip>
-    </a>
+          <AvatarFavicon />
+        </BadgeWrapper>
+      </Box>
+      <Box
+        marginLeft={4}
+        marginRight={2}
+        display={DISPLAY.FLEX}
+        flexDirection={FLEX_DIRECTION.COLUMN}
+      >
+        <Text>{friendlyName}</Text>
+        <Text variant={TextVariant.bodySm} color={TextColor.textAlternative}>
+          {snapId}
+        </Text>
+      </Box>
+      <ButtonIcon
+        rel="noopener noreferrer"
+        target="_blank"
+        href={url}
+        iconName={ICON_NAMES.EXPORT}
+        color={IconColor.infoDefault}
+        size={ICON_SIZES.MD}
+      />
+    </Box>
   );
 };
 
@@ -87,10 +103,6 @@ SnapsAuthorshipPill.propTypes = {
    * The id of the snap
    */
   snapId: PropTypes.string,
-  /**
-   * The version of the snap
-   */
-  version: PropTypes.string,
   /**
    * The className of the SnapsAuthorshipPill
    */
