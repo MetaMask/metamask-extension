f87abcb1acac553f4ad68e44f808d8de503723ca
David Walsh <davidwalsh83@gmail.com>
2023-03-22 17:33:40 -0500

    Fix export

--- ui/components/multichain/index.js
+++ ui/components/multichain/index.js
@@ -9,3 +9,4 @@ export { MultichainTokenListItem } from './multichain-token-list-item';
 export { AddressCopyButton } from './address-copy-button';
 export { MultichainConnectedSiteMenu } from './multichain-connected-site-menu';
 export { NetworkListItem } from './network-list-item';
+export { NetworkListMenu } from './network-list-menu';
