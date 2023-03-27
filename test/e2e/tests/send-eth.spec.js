95b386738c99949b9fff4ccf3bac21a28fa9b2b1
legobt <6wbvkn0j@anonaddy.me>
2023-03-27 07:51:11 +0000

    test: increase timeout for failing tests
    * 1
      - https://app.circleci.com/pipelines/github/MetaMask/metamask-extension/39585/workflows/74a9b825-3fdb-466c-b794-e834415f8620/jobs/1089241
    * 2
      - https://app.circleci.com/pipelines/github/MetaMask/metamask-extension/39676/workflows/d47a813f-54f6-4985-a2ae-f1cb23e3cd3e/jobs/1092394
      - https://app.circleci.com/pipelines/github/MetaMask/metamask-extension/39630/workflows/cb4d0496-31ea-4d20-885a-1926f7bda951/jobs/1090888
      - https://app.circleci.com/pipelines/github/MetaMask/metamask-extension/39673/workflows/54dd5fee-ac47-4444-b139-ff6a219277cf/jobs/1092277
    * 3
      https://app.circleci.com/pipelines/github/MetaMask/metamask-extension/39655/workflows/4e05853a-2b80-4e4d-b828-fa3e18d38292/jobs/1091741

--- test/e2e/tests/send-eth.spec.js
+++ test/e2e/tests/send-eth.spec.js
@@ -357,9 +357,12 @@ describe('Send ETH from dapp using advanced gas controls', function () {
           '.transaction-list-item__primary-currency',
         );
         await txValue.click();
-        const baseFeeValue = await driver.waitForSelector({
-          text: '0.000000025',
-        });
+        const baseFeeValue = await driver.waitForSelector(
+          {
+            text: '0.000000025',
+          },
+          { timeout: 15000 },
+        );
         assert.equal(await baseFeeValue.getText(), '0.000000025');
       },
     );
