01bae088d9fb0693ac81958aa2315c9350eff50b
garrettbear <gwhisten@gmail.com>
2023-03-28 13:04:42 -0700

    fix types export

--- ui/components/component-library/text/index.ts
+++ ui/components/component-library/text/index.ts
@@ -1,7 +1,3 @@
 export { Text } from './text';
-export type {
-  TextProps,
-  ValidTag,
-  TextDirection,
-  InvisibleCharacter,
-} from './text.types';
+export { ValidTag, TextDirection, InvisibleCharacter } from './text.types';
+export type { TextProps } from './text.types';
