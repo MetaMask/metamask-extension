50f8aada2ca9ee7a3bb0e3608a1c61bd57b39759
Thomas Huang <tmashuang@users.noreply.github.com>
2023-03-29 18:39:41 +0800

    Add test for classname hidden check and confirm recovery phrase indices

--- ui/pages/onboarding-flow/recovery-phrase/recovery-phrase-chips.test.js
+++ ui/pages/onboarding-flow/recovery-phrase/recovery-phrase-chips.test.js
@@ -11,6 +11,40 @@ describe('Recovery Phrase Chips Component', () => {
       secretRecoveryPhrase: TEST_SEED.split(' '),
     };
 
+    const { queryByTestId, container } = renderWithProvider(
+      <RecoveryPhraseChips {...props} />,
+    );
+
+    expect(container).toMatchSnapshot();
+
+    const recoveryPhraseChips = queryByTestId('recovery-phrase-chips');
+    expect(recoveryPhraseChips).not.toHaveClass(
+      'recovery-phrase__chips--hidden',
+    );
+  });
+
+  it('should have --hidden class when phrase revealed is false', () => {
+    const props = {
+      secretRecoveryPhrase: TEST_SEED.split(' '),
+      phraseRevealed: false,
+    };
+
+    const { queryByTestId } = renderWithProvider(
+      <RecoveryPhraseChips {...props} />,
+    );
+
+    const recoveryPhraseChips = queryByTestId('recovery-phrase-chips');
+    expect(recoveryPhraseChips).toHaveClass('recovery-phrase__chips--hidden');
+  });
+
+  it('should match snapshot of confirm recovery phrase with inputs of indicies to check', () => {
+    const props = {
+      secretRecoveryPhrase: TEST_SEED.split(' '),
+      indicesToCheck: [1, 5, 10],
+      confirmPhase: true,
+      inputValue: {},
+    };
+
     const { container } = renderWithProvider(
       <RecoveryPhraseChips {...props} />,
     );
