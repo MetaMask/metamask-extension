e9c9012cae32f8b6e91b1b0712d57530e3c70fa3
David Walsh <davidwalsh83@gmail.com>
2023-03-29 17:40:21 -0500

    Switch to using Button

--- ui/components/multichain/account-picker/account-picker.js
+++ ui/components/multichain/account-picker/account-picker.js
@@ -2,17 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import {
-  ButtonBase,
+  Button,
   ICON_NAMES,
   AvatarAccount,
   AvatarAccountVariant,
   Text,
+  Icon,
 } from '../../component-library';
 import {
+  AlignItems,
   BackgroundColor,
   BorderRadius,
   DISPLAY,
   FONT_WEIGHT,
+  IconColor,
   Size,
 } from '../../../helpers/constants/design-system';
 
@@ -20,14 +23,17 @@ export const AccountPicker = ({ address, name, onClick }) => {
   const useBlockie = useSelector((state) => state.metamask.useBlockie);
 
   return (
-    <ButtonBase
+    <Button
       className="multichain-account-picker"
       onClick={onClick}
       backgroundColor={BackgroundColor.transparent}
-      endIconName={ICON_NAMES.ARROW_DOWN}
       borderRadius={BorderRadius.LG}
       ellipsis
-      textProps={{ display: DISPLAY.FLEX, gap: 2 }}
+      textProps={{
+        display: DISPLAY.FLEX,
+        gap: 2,
+        alignItems: AlignItems.center,
+      }}
     >
       <AvatarAccount
         variant={
@@ -41,7 +47,12 @@ export const AccountPicker = ({ address, name, onClick }) => {
       <Text as="span" fontWeight={FONT_WEIGHT.BOLD} ellipsis>
         {name}
       </Text>
-    </ButtonBase>
+      <Icon
+        name={ICON_NAMES.ARROW_DOWN}
+        color={IconColor.iconDefault}
+        size={Size.SM}
+      />
+    </Button>
   );
 };
 
