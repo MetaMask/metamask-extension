ddde4a0e2d5c58df7635d0f8fb853afe4d1a8f51
Guillaume Roux <guillaumeroux123@gmail.com>
2023-03-30 19:41:23 +0200

    fix fencing

--- ui/pages/confirmation/confirmation.js
+++ ui/pages/confirmation/confirmation.js
@@ -34,11 +34,11 @@ import NetworkDisplay from '../../components/app/network-display/network-display
 import Callout from '../../components/ui/callout';
 import SiteOrigin from '../../components/ui/site-origin';
 import { Icon, ICON_NAMES } from '../../components/component-library';
-import SnapAuthorship from '../../components/app/flask/snap-authorship/snap-authorship';
 ///: BEGIN:ONLY_INCLUDE_IN(flask)
+import SnapAuthorship from '../../components/app/flask/snap-authorship/snap-authorship';
 import { getSnapName } from '../../helpers/utils/util';
-import ConfirmationFooter from './components/confirmation-footer';
 ///: END:ONLY_INCLUDE_IN
+import ConfirmationFooter from './components/confirmation-footer';
 import {
   getTemplateValues,
   getTemplateAlerts,
