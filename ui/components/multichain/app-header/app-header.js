ee5d5a2d9fa7fd8961dc3c09f08da6211470c853
David Walsh <davidwalsh83@gmail.com>
2023-03-30 18:22:23 -0500

    Remove backgrounds, use isUnlocked

--- ui/components/multichain/app-header/app-header.js
+++ ui/components/multichain/app-header/app-header.js
@@ -18,7 +18,7 @@ import {
   ICON_NAMES,
   PickerNetwork,
 } from '../../component-library';
-import { GlobalMenu, AccountPicker } from '../';
+import { GlobalMenu, AccountPicker } from '..';
 
 import Box from '../../ui/box/box';
 import { getSelectedIdentity } from '../../../selectors';
@@ -28,6 +28,8 @@ export const AppHeader = ({}) => {
   const trackEvent = useContext(MetaMetricsContext);
   const [accountOptionsMenuOpen, setAccountOptionsMenuOpen] = useState(false);
   const menuRef = useRef(false);
+
+  const isUnlocked = useSelector((state) => state.metamask.isUnlocked);
 
   // Used for account picker
   const identity = useSelector(getSelectedIdentity);
@@ -46,7 +48,7 @@ export const AppHeader = ({}) => {
     <Box
       display={DISPLAY.FLEX}
       alignItems={AlignItems.center}
-      style={{ background: 'red', maxWidth: '80vw', width: '100%' }}
+      style={{ maxWidth: '80vw', width: '100%' }}
       padding={[5, 4, 5, 4]}
       gap={2}
     >
@@ -55,20 +57,22 @@ export const AppHeader = ({}) => {
         name="TODO"
         src="TODO"
         size={Size.MD}
-        style={{ background: 'pink' }}
       />
       {/* PickerNetwork should only display in full screen mode */}
       <PickerNetwork avatarNetworkProps iconProps label src />
 
-      <Box style={{ flexGrow: 1, textAlign: 'center' }}>
-        <AccountPicker
-          address={identity.address}
-          name={identity.name}
-          onClick={() => dispatch(toggleAccountMenu())}
-        />
-      </Box>
-      <Box style={{ background: 'lightblue' }}>
-        {/*
+      {isUnlocked ? (
+        <Box style={{ flexGrow: 1, textAlign: 'center' }}>
+          <AccountPicker
+            address={identity.address}
+            name={identity.name}
+            onClick={() => dispatch(toggleAccountMenu())}
+          />
+        </Box>
+      ) : null}
+      {isUnlocked ? (
+        <Box>
+          {/*
         Waiting on: https://github.com/MetaMask/metamask-extension/pull/18167
 
         <MultichainConnectedSiteMenu
@@ -77,25 +81,27 @@ export const AppHeader = ({}) => {
           text,
         />
       */}
-        O
-      </Box>
-      <ButtonIcon
-        iconName={ICON_NAMES.MORE_VERTICAL}
-        data-testid="account-options-menu-button"
-        ariaLabel="NEEDS NEW TRANSLATED LABEL"
-        style={{ background: 'lightgreen' }}
-        ref={menuRef}
-        onClick={() => {
-          trackEvent({
-            event: EVENT_NAMES.NAV_ACCOUNT_MENU_OPENED,
-            category: EVENT.CATEGORIES.NAVIGATION,
-            properties: {
-              location: 'Home',
-            },
-          });
-          setAccountOptionsMenuOpen(true);
-        }}
-      />
+          O
+        </Box>
+      ) : null}
+      {isUnlocked ? (
+        <ButtonIcon
+          iconName={ICON_NAMES.MORE_VERTICAL}
+          data-testid="account-options-menu-button"
+          ariaLabel="NEEDS NEW TRANSLATED LABEL"
+          ref={menuRef}
+          onClick={() => {
+            trackEvent({
+              event: EVENT_NAMES.NAV_ACCOUNT_MENU_OPENED,
+              category: EVENT.CATEGORIES.NAVIGATION,
+              properties: {
+                location: 'Home',
+              },
+            });
+            setAccountOptionsMenuOpen(true);
+          }}
+        />
+      ) : null}
       {accountOptionsMenuOpen ? (
         <GlobalMenu
           anchorElement={menuRef.current}
