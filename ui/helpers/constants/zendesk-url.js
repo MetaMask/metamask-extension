08cf3920c47639b846ef4bf6483abd209e56ab8a
jlwllmr <95916148+jlwllmr@users.noreply.github.com>
2023-03-30 13:06:43 +0100

    Updating Zendesk links
    Suggesting these updates to:
    - Remove the en-us locale. Without it, the page defaults to the user's browser locale, and Zendesk displays the appropriate translation for that locale (if there is one). If there isn't a translation, it defaults to en-us. 
    - Remove article titles from the end of some links, as these are unnecessary. 
    - Update the core of the URL to 'support.metamask.io' instead of 'metamask.zendesk.com' to reflect a recent update (does not affect whether or not the links work).

--- ui/helpers/constants/zendesk-url.js
+++ ui/helpers/constants/zendesk-url.js
@@ -1,45 +1,46 @@
 const ZENDESK_URLS = {
   ADD_CUSTOM_TOKENS:
-    'https://metamask.zendesk.com/hc/en-us/articles/360015489031',
+    'https://support.metamask.io/hc/articles/360015489031',
   ADD_MISSING_ACCOUNTS:
-    'https://metamask.zendesk.com/hc/en-us/articles/360015489271',
+    'https://support.metamask.io/hc/articles/360015489271',
   BASIC_SAFETY:
-    'https://metamask.zendesk.com/hc/en-us/articles/360015489591-Basic-Safety-Tips',
+    'https://support.metamask.io/hc/articles/360015489591',
   CUSTOMIZE_NONCE:
-    'https://metamask.zendesk.com/hc/en-us/articles/7417499333531-How-to-customize-a-transaction-nonce',
+    'https://support.metamask.io/hc/articles/7417499333531',
   HARDWARE_CONNECTION:
-    'https://metamask.zendesk.com/hc/en-us/articles/360020394612-How-to-connect-a-Trezor-or-Ledger-Hardware-Wallet',
+    'https://support.metamask.io/hc/articles/4408552261275',
   IMPORT_ACCOUNTS:
-    'https://metamask.zendesk.com/hc/en-us/articles/360015489331',
+    'https://support.metamask.io/hc/articles/360015489331',
   IMPORTED_ACCOUNTS:
-    'https://metamask.zendesk.com/hc/en-us/articles/360015289932',
+    'https://support.metamask.io/hc/articles/360015289932',
   INFURA_BLOCKAGE:
-    'https://metamask.zendesk.com/hc/en-us/articles/360059386712',
-  LEGACY_WEB3: 'https://metamask.zendesk.com/hc/en-us/articles/360053147012',
+    'https://support.metamask.io/hc/articles/360059386712',
+  LEGACY_WEB3: 
+    'https://support.metamask.io/hc/articles/360053147012',
   NFT_TOKENS:
-    'https://metamask.zendesk.com/hc/en-us/articles/360058238591-NFT-tokens-in-MetaMask-wallet',
+    'https://support.metamask.io/hc/articles/360058238591',
   PASSWORD_ARTICLE:
-    'https://metamask.zendesk.com/hc/en-us/articles/4404722782107',
+    'https://support.metamask.io/hc/articles/4405451730331',
   SECRET_RECOVERY_PHRASE:
-    'https://metamask.zendesk.com/hc/en-us/articles/360060826432-What-is-a-Secret-Recovery-Phrase-and-how-to-keep-your-crypto-wallet-secure',
+    'https://support.metamask.io/hc/articles/360060826432',
   SECRET_RECOVERY_PHRASE_USER_GUIDE:
-    'https://metamask.zendesk.com/hc/en-us/articles/4404722782107-User-guide-Secret-Recovery-Phrase-password-and-private-keys',
+    'https://support.metamask.io/hc/articles/4404722782107',
   NON_CUSTODIAL_WALLET:
-    'https://metamask.zendesk.com/hc/en-us/articles/360059952212-MetaMask-is-a-non-custodial-wallet',
+    'https://support.metamask.io/hc/articles/360059952212',
   SPEEDUP_CANCEL:
-    'https://metamask.zendesk.com/hc/en-us/articles/360015489251-How-to-speed-up-or-cancel-a-pending-transaction',
+    'https://support.metamask.io/hc/articles/360015489251',
   TOKEN_SAFETY_PRACTICES:
-    'https://metamask.zendesk.com/hc/en-us/articles/4403988839451',
+    'https://support.metamask.io/hc/articles/4403988839451',
   UNKNOWN_NETWORK:
-    'https://metamask.zendesk.com/hc/en-us/articles/4417500466971',
+    'https://support.metamask.io/hc/articles/4417500466971',
   USER_GUIDE_CUSTOM_NETWORKS:
-    'https://metamask.zendesk.com/hc/en-us/articles/4404424659995',
+    'https://support.metamask.io/hc/articles/4404424659995',
   USER_GUIDE_DAPPS:
-    'https://metamask.zendesk.com/hc/en-us/articles/4405506066331-User-guide-Dapps',
+    'https://support.metamask.io/hc/articles/4405506066331',
   USER_GUIDE_GAS:
-    'https://metamask.zendesk.com/hc/en-us/articles/4404600179227-User-Guide-Gas',
+    'https://support.metamask.io/hc/articles/4404600179227',
   VERIFY_CUSTOM_NETWORK:
-    'https://metamask.zendesk.com/hc/en-us/articles/360057142392',
+    'https://support.metamask.io/hc/articles/360057142392',
 };
 
 export default ZENDESK_URLS;
