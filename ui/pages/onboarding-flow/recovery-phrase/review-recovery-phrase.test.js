4d522e52bcbba58cac1d043928b5d7f5cde39a44
Thomas Huang <tmashuang@users.noreply.github.com>
2023-03-31 10:48:53 +0800

    Lint

--- ui/pages/onboarding-flow/recovery-phrase/review-recovery-phrase.test.js
+++ ui/pages/onboarding-flow/recovery-phrase/review-recovery-phrase.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, screen } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
 import React from 'react';
 import configureMockStore from 'redux-mock-store';
 import { renderWithProvider } from '../../../../test/lib/render-helpers';
