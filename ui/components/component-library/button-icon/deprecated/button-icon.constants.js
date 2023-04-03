758e7f89ea84e35d9c553d70f57386875d095649
garrettbear <gwhisten@gmail.com>
2023-04-03 13:59:37 -0700

    add deprecated folder

---
+++ ui/components/component-library/button-icon/deprecated/button-icon.constants.js
@@ -0,0 +1,6 @@
+import { Size } from '../../../helpers/constants/design-system';
+
+export const BUTTON_ICON_SIZES = {
+  SM: Size.SM,
+  LG: Size.LG,
+};
---
+++ ui/components/component-library/button-icon/deprecated/index.js
@@ -0,0 +1,2 @@
+export { ButtonIcon } from './button-icon';
+export { BUTTON_ICON_SIZES } from './button-icon.constants';
