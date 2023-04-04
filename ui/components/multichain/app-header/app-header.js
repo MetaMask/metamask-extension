42f7849e157219125aba6b9c48db565d1514ae44
NidhiKJha <menidhikjha@gmail.com>
2023-04-04 19:27:45 +0530

    fixed ui for all the screens

--- ui/components/multichain/app-header/app-header.js
+++ ui/components/multichain/app-header/app-header.js
@@ -1,14 +1,20 @@
-/*
-  TODO:
-    * Look at Figma to get the padding of container and spacing between icons
-*/
 import React, { useContext, useState, useRef } from 'react';
+import browser from 'webextension-polyfill';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { MetaMetricsContext } from '../../../contexts/metametrics';
 import { EVENT_NAMES, EVENT } from '../../../../shared/constants/metametrics';
 import {
+  CONNECTED_ACCOUNTS_ROUTE,
+  DEFAULT_ROUTE,
+} from '../../../helpers/constants/routes';
+
+import {
   AlignItems,
+  BackgroundColor,
+  BLOCK_SIZES,
   DISPLAY,
+  JustifyContent,
   Size,
 } from '../../../helpers/constants/design-system';
 import {
@@ -17,17 +23,26 @@ import {
   ICON_NAMES,
   PickerNetwork,
 } from '../../component-library';
-import { getCurrentNetwork, getSelectedIdentity } from '../../../selectors';
+import {
+  getCurrentNetwork,
+  getOriginOfCurrentTab,
+  getSelectedIdentity,
+} from '../../../selectors';
 import { GlobalMenu, AccountPicker } from '..';
 
 import Box from '../../ui/box/box';
 import { toggleAccountMenu, toggleNetworkMenu } from '../../../store/actions';
+import MetafoxLogo from '../../ui/metafox-logo';
+import { getEnvironmentType } from '../../../../app/scripts/lib/util';
+import { ENVIRONMENT_TYPE_POPUP } from '../../../../shared/constants/app';
+import ConnectedStatusIndicator from '../../app/connected-status-indicator';
 
-export const AppHeader = ({}) => {
+export const AppHeader = ({ onClick }) => {
   const trackEvent = useContext(MetaMetricsContext);
   const [accountOptionsMenuOpen, setAccountOptionsMenuOpen] = useState(false);
   const menuRef = useRef(false);
-
+  const origin = useSelector(getOriginOfCurrentTab);
+  const history = useHistory();
   const isUnlocked = useSelector((state) => state.metamask.isUnlocked);
 
   // Used for account picker
@@ -37,83 +52,149 @@ export const AppHeader = ({}) => {
   // Used for network icon / dropdown
   const currentNetwork = useSelector(getCurrentNetwork);
 
+  // used to get the environment and connection status
+  const popupStatus = getEnvironmentType() === ENVIRONMENT_TYPE_POPUP;
+  const showStatus =
+    getEnvironmentType() === ENVIRONMENT_TYPE_POPUP &&
+    origin &&
+    origin !== browser.runtime.id;
+
   return (
-    <Box
-      display={DISPLAY.FLEX}
-      alignItems={AlignItems.center}
-      style={{ maxWidth: '80vw', width: '100%' }}
-      padding={[5, 4, 5, 4]}
-      gap={2}
-    >
-      <Box display={DISPLAY.FLEX}>
-        {/* AvatarNetwork should only display in popup mode */}
-        <AvatarNetwork
-          name={currentNetwork.nickname}
-          src={currentNetwork.rpcPrefs?.imageUrl}
-          size={Size.MD}
-          onClick={() => dispatch(toggleNetworkMenu())}
-        />
-        {/* PickerNetwork should only display in full screen mode */}
-        <PickerNetwork
-          label={currentNetwork.nickname}
-          src={currentNetwork.rpcPrefs?.imageUrl}
-          onClick={() => dispatch(toggleNetworkMenu())}
-        />
-      </Box>
-
-      {isUnlocked ? (
-        <Box style={{ flexGrow: 1, textAlign: 'center' }}>
-          <AccountPicker
-            address={identity.address}
-            name={identity.name}
-            onClick={() => dispatch(toggleAccountMenu())}
-          />
-        </Box>
-      ) : null}
-      {isUnlocked ? (
-        <Box>
-          {/*
-        Waiting on: https://github.com/MetaMask/metamask-extension/pull/18167
-
-        <MultichainConnectedSiteMenu
-          globalMenuColor,
-          status,
-          text,
-        />
-      */}
-          O
-        </Box>
-      ) : null}
-      {isUnlocked ? (
-        <Box ref={menuRef}>
-          <ButtonIcon
-            iconName={ICON_NAMES.MORE_VERTICAL}
-            data-testid="account-options-menu-button"
-            ariaLabel="NEEDS NEW TRANSLATED LABEL"
-            onClick={() => {
-              trackEvent({
-                event: EVENT_NAMES.NAV_ACCOUNT_MENU_OPENED,
-                category: EVENT.CATEGORIES.NAVIGATION,
-                properties: {
-                  location: 'Home',
-                },
-              });
-              setAccountOptionsMenuOpen(true);
+    <>
+      {isUnlocked && !popupStatus ? (
+        <Box
+          style={{ height: '75px', flex: '0 0 auto' }}
+          display={DISPLAY.FLEX}
+          alignItems={AlignItems.center}
+          margin={2}
+        >
+          <MetafoxLogo
+            unsetIconHeight
+            onClick={async () => {
+              if (onClick) {
+                await onClick();
+              }
+              history.push(DEFAULT_ROUTE);
             }}
           />
         </Box>
       ) : null}
-      {accountOptionsMenuOpen ? (
-        <GlobalMenu
-          anchorElement={menuRef.current}
-          closeMenu={() => setAccountOptionsMenuOpen(false)}
-        />
-      ) : null}
-      {isUnlocked ? null : (
-        <Box width={8} style={{ textAlign: 'right', flexGrow: '1' }}>
-          <img src="/images/logo/metamask-fox.svg" alt="" />
-        </Box>
-      )}
-    </Box>
+      <Box
+        display={DISPLAY.FLEX}
+        className={`multichain-app-header ${
+          !isUnlocked || popupStatus ? 'multichain-app-header-shadow' : ''
+        }`}
+        alignItems={AlignItems.center}
+        width={BLOCK_SIZES.FULL}
+        backgroundColor={
+          !isUnlocked || popupStatus
+            ? BackgroundColor.backgroundDefault
+            : BackgroundColor.backgroundAlternative
+        }
+      >
+        <>
+          {isUnlocked ? (
+            <Box
+              className={`multichain-app-header__contents ${
+                isUnlocked && !popupStatus ? 'multichain-app-header-shadow' : ''
+              }`}
+              alignItems={AlignItems.center}
+              width={BLOCK_SIZES.FULL}
+              backgroundColor={BackgroundColor.backgroundDefault}
+              padding={2}
+              gap={2}
+            >
+              {popupStatus ? (
+                <AvatarNetwork
+                  name={currentNetwork?.nickname}
+                  src={currentNetwork?.rpcPrefs?.imageUrl}
+                  size={Size.MD}
+                  onClick={() => dispatch(toggleNetworkMenu())}
+                />
+              ) : (
+                <PickerNetwork
+                  label={currentNetwork.nickname}
+                  src={currentNetwork.rpcPrefs?.imageUrl}
+                  onClick={() => dispatch(toggleNetworkMenu())}
+                />
+              )}
+
+              <AccountPicker
+                address={identity.address}
+                name={identity.name}
+                onClick={() => dispatch(toggleAccountMenu())}
+              />
+              <Box
+                display={DISPLAY.FLEX}
+                alignItems={AlignItems.center}
+                justifyContent={JustifyContent.spaceBetween}
+              >
+                {showStatus ? (
+                  <ConnectedStatusIndicator
+                    onClick={() => history.push(CONNECTED_ACCOUNTS_ROUTE)}
+                  />
+                ) : null}
+                <Box
+                  ref={menuRef}
+                  display={DISPLAY.FLEX}
+                  justifyContent={JustifyContent.flexEnd}
+                  width={BLOCK_SIZES.FULL}
+                >
+                  <ButtonIcon
+                    iconName={ICON_NAMES.MORE_VERTICAL}
+                    data-testid="account-options-menu-button"
+                    ariaLabel="NEEDS NEW TRANSLATED LABEL"
+                    onClick={() => {
+                      trackEvent({
+                        event: EVENT_NAMES.NAV_ACCOUNT_MENU_OPENED,
+                        category: EVENT.CATEGORIES.NAVIGATION,
+                        properties: {
+                          location: 'Home',
+                        },
+                      });
+                      setAccountOptionsMenuOpen(true);
+                    }}
+                  />
+                </Box>
+              </Box>
+              {accountOptionsMenuOpen ? (
+                <GlobalMenu
+                  anchorElement={menuRef.current}
+                  closeMenu={() => setAccountOptionsMenuOpen(false)}
+                />
+              ) : null}
+            </Box>
+          ) : (
+            <Box
+              display={DISPLAY.FLEX}
+              className={`multichain-app-header__lock-contents ${
+                isUnlocked && !popupStatus ? 'multichain-app-header-shadow' : ''
+              }`}
+              alignItems={AlignItems.center}
+              width={BLOCK_SIZES.FULL}
+              justifyContent={JustifyContent.spaceBetween}
+              backgroundColor={BackgroundColor.backgroundDefault}
+              padding={2}
+              gap={2}
+            >
+              <PickerNetwork
+                label={currentNetwork.nickname}
+                src={currentNetwork.rpcPrefs?.imageUrl}
+                onClick={() => dispatch(toggleNetworkMenu())}
+              />
+              <MetafoxLogo
+                unsetIconHeight
+                onClick={async () => {
+                  if (onClick) {
+                    await onClick();
+                  }
+                  history.push(DEFAULT_ROUTE);
+                }}
+              />
+            </Box>
+          )}
+        </>
+      </Box>
+    </>
   );
 };
