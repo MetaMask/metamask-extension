40628da3b4a92233c28fe37a920efac6d9923353
Frederik Bolding <frederik.bolding@gmail.com>
2023-04-05 15:34:33 +0200

    [FLASK] Fix crash when requesting unknown snap permission (#18447)

--- ui/pages/permissions-connect/flask/util.js
+++ ui/pages/permissions-connect/flask/util.js
@@ -1,29 +1,18 @@
-import { isObject } from '@metamask/utils';
-import { PERMISSION_DESCRIPTIONS } from '../../../helpers/utils/permission';
+import { getPermissionDescription } from '../../../helpers/utils/permission';
 
 export function getSnapInstallWarnings(permissions, targetSubjectMetadata, t) {
   const weightOneWarnings = Object.entries(permissions).reduce(
     (filteredWarnings, [permissionName, permissionValue]) => {
-      const permissionDescription = PERMISSION_DESCRIPTIONS[permissionName]({
+      const permissionDescription = getPermissionDescription({
         t,
+        permissionName,
         permissionValue,
         targetSubjectMetadata,
       });
-      if (Array.isArray(permissionDescription)) {
-        permissionDescription.forEach((description) => {
-          if (description.weight === 1) {
-            const { id, message } = description;
-            filteredWarnings.push({ id, message });
-          }
-        });
-      } else if (
-        isObject(permissionDescription) &&
-        permissionDescription.weight === 1
-      ) {
-        const { id, message } = permissionDescription;
-        filteredWarnings.push({ id, message });
-      }
-      return filteredWarnings;
+
+      return filteredWarnings.concat(
+        permissionDescription.filter((description) => description.weight === 1),
+      );
     },
     [],
   );
