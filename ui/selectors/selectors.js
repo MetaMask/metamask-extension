ccc98a254d1fdbd1be0b95fae522591be9f8652d
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2023-04-05 21:45:34 +0100

    remove duplicate message count from selector

--- ui/selectors/selectors.js
+++ ui/selectors/selectors.js
@@ -487,7 +487,6 @@ export function getTotalUnapprovedCount(state) {
     unapprovedMsgCount = 0,
     unapprovedPersonalMsgCount = 0,
     unapprovedDecryptMsgCount = 0,
-    unapprovedEncryptionPublicKeyMsgCount = 0,
     unapprovedTypedMessagesCount = 0,
     pendingApprovalCount = 0,
   } = state.metamask;
@@ -496,7 +495,6 @@ export function getTotalUnapprovedCount(state) {
     unapprovedMsgCount +
     unapprovedPersonalMsgCount +
     unapprovedDecryptMsgCount +
-    unapprovedEncryptionPublicKeyMsgCount +
     unapprovedTypedMessagesCount +
     getUnapprovedTxCount(state) +
     pendingApprovalCount +
