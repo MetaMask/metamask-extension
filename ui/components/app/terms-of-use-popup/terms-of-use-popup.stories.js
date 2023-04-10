1ef74ef220a330eccae0fc02fe54ad55803ecedd
vthomas13 <10986371+vthomas13@users.noreply.github.com>
2023-04-10 10:49:23 -0400

    Update ui/components/app/terms-of-use-popup/terms-of-use-popup.stories.js
    adding args to ToU popup storybook
    
    Co-authored-by: Nidhi Kumari <nidhi.kumari@consensys.net>

--- ui/components/app/terms-of-use-popup/terms-of-use-popup.stories.js
+++ ui/components/app/terms-of-use-popup/terms-of-use-popup.stories.js
@@ -3,8 +3,14 @@ import TermsOfUsePopup from '.';
 
 export default {
   title: 'Components/App/TermsOfUsePopup',
+  component: TermsOfUsePopup,
+  argTypes: {
+    onAccept: {
+      action: 'onAccept',
+    },
+  },
 };
 
-export const DefaultStory = () => <TermsOfUsePopup />;
+export const DefaultStory = (args) => <TermsOfUsePopup {...args} />;
 
 DefaultStory.storyName = 'Default';
