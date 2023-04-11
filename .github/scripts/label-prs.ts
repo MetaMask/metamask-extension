7309c24d3bc7ae2b55df8a90636418d0537a7e53
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2023-04-11 11:25:15 +0100

    wip

--- .github/scripts/label-prs.ts
+++ .github/scripts/label-prs.ts
@@ -1,47 +1,74 @@
-async function runLabelPRsAutomation(context, github) {
-  let issueNumber = getIssueNumberFromPullRequestBody(context);
+import { context } from '@actions/github';
+import { GitHub } from '@actions/github/lib/utils';
 
-  if (issueNumber === -1) {
-    bailIfIsBranchNameInvalid(process.env.HEAD_REF);
-    bailIfIsNotFeatureBranch();
-    issueNumber = getIssueNumberFromBranchName(process.env.HEAD_REF);
+type GithubClient = InstanceType<typeof GitHub>;
+
+main().catch((error: Error): void => {
+  console.error(error);
+  process.exit(1);
+});
+
+async function main(): Promise<void> {
+  const github = new GitHub();
+
+  const logs = JSON.stringify({ github, context });
+  console.log(logs);
+  // github.event.pull_request.head.ref || "";
+  const HEAD_REF = 'test';
+
+  let issueNumber = await getIssueNumberFromPullRequestBody();
+
+  if (issueNumber === "") {
+    bailIfIsBranchNameInvalid(HEAD_REF);
+    bailIfIsNotFeatureBranch(HEAD_REF);
+    issueNumber = getIssueNumberFromBranchName(HEAD_REF);
   }
 
-  await updateLabels(context, github, issueNumber);
+  await updateLabels(github, issueNumber);
 }
 
-function getIssueNumberFromPullRequestBody(context) {
+async function getIssueNumberFromPullRequestBody(): Promise<string> {
   console.log("Checking if the PR's body references an issue...");
 
   let ISSUE_LINK_IN_PR_DESCRIPTION_REGEX =
     /(close|closes|closed|fix|fixes|fixed|resolve|resolves|resolved)\s#\d+/gi;
 
-  const prBody = context.payload.pull_request.body;
+  const prBody = await getPullRequestBody();
 
   let matches = prBody.match(ISSUE_LINK_IN_PR_DESCRIPTION_REGEX);
-  if (matches.length === 0) {
+  if (!matches || matches?.length === 0) {
     console.log(
       'No direct link can be drawn between the PR and an issue from the PR body because no issue number was referenced.',
     );
-    return -1;
+    return "";
   }
 
-  if (matches.length > 1) {
+  if (matches?.length > 1) {
     console.log(
       'No direct link can be drawn between the PR and an issue from the PR body because more than one issue number was referenced.',
     );
-    return -1;
+    return "";
   }
 
-  ISSUE_NUMBER_REGEX = /\d+/;
-  const issueNumber = matches[0].match(ISSUE_NUMBER_REGEX)[0];
+  const ISSUE_NUMBER_REGEX = /\d+/;
+  const issueNumber = matches[0].match(ISSUE_NUMBER_REGEX)?.[0] || '';
 
   console.log(`Found issue number ${issueNumber} in PR body.`);
 
   return issueNumber;
 }
 
-function bailIfIsBranchNameInvalid(branchName) {
+async function getPullRequestBody(): Promise<string> {
+  if (context.eventName !== 'pull_request') {
+    console.log('This action should only run on pull_request events.');
+    process.exit(1);
+  }
+
+  const prBody = context.payload.pull_request?.body || '';
+  return prBody;
+}
+
+function bailIfIsBranchNameInvalid(branchName: string): void {
   const BRANCH_REGEX =
     /^(main|develop|(ci|chore|docs|feat|feature|fix|perf|refactor|revert|style)\/\d*(?:[-](?![-])\w*)*|Version-v\d+\.\d+\.\d+)$/;
   const isValidBranchName = new RegExp(BRANCH_REGEX).test(branchName);
@@ -59,13 +86,13 @@ function bailIfIsBranchNameInvalid(branchName) {
   }
 }
 
-function bailIfIsNotFeatureBranch() {
+function bailIfIsNotFeatureBranch(branchName: string): void {
   if (
-    process.env.HEAD_REF === 'main' ||
-    process.env.HEAD_REF === 'develop' ||
-    process.env.HEAD_REF.startsWith('Version-v')
+    branchName === 'main' ||
+    branchName === 'develop' ||
+    branchName.startsWith('Version-v')
   ) {
-    console.log(`${process.env.HEAD_REF} is not a feature branch.`);
+    console.log(`${branchName} is not a feature branch.`);
     console.log(
       'No issue could be linked to this PR, so no labels were copied',
     );
@@ -73,7 +100,11 @@ function bailIfIsNotFeatureBranch() {
   }
 }
 
-async function updateLabels(context, github, issueNumber) {
+async function updateLabels(github: GithubClient, issueNumber: string): Promise<void> {
+  interface ILabel {
+    name: string;
+  };
+
   const owner = context.repo.owner;
   const repo = context.repo.repo;
 
@@ -82,7 +113,7 @@ async function updateLabels(context, github, issueNumber) {
     { owner, repo, issue_number: issueNumber },
   );
 
-  const issueLabels = issue.data.labels.map((label) => label.name);
+  const issueLabels = issue.data.labels.map((label: ILabel): string => label.name);
 
   const prNumber = context.payload.number;
 
@@ -95,7 +126,7 @@ async function updateLabels(context, github, issueNumber) {
     },
   );
 
-  const startingPRLabels = pr.data.labels.map((label) => label.name);
+  const startingPRLabels = pr.data.labels.map((label: ILabel): string => label.name);
 
   const dedupedFinalPRLabels = [
     ...new Set([...startingPRLabels, ...issueLabels]),
@@ -115,10 +146,10 @@ async function updateLabels(context, github, issueNumber) {
   }
 }
 
-function getIssueNumberFromBranchName(branchName) {
+function getIssueNumberFromBranchName(branchName: string): string {
   console.log('Checking if the branch name references an issue...');
 
-  let issueNumber;
+  let issueNumber: string;
   if (branchName.split('/').length > 1) {
     issueNumber = branchName.split('/')[1].split('-')[0];
   } else {
@@ -130,13 +161,11 @@ function getIssueNumberFromBranchName(branchName) {
   return issueNumber;
 }
 
-function sortedArrayEqual(array1, array2) {
+function sortedArrayEqual(array1: string[], array2: string[]): boolean {
   const lengthsAreEqual = array1.length === array2.length;
   const everyElementMatchesByIndex = array1.every(
-    (value, index) => value === array2[index],
+    (value: string, index: number): boolean => value === array2[index],
   );
 
   return lengthsAreEqual && everyElementMatchesByIndex;
 }
-
-module.exports = { runLabelPRsAutomation };
