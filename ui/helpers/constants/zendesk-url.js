5c3c2796bec8b7773cc0f03f56c626ef5177ec84
legobt <6wbvkn0j@anonaddy.me>
2023-04-11 01:11:45 +0000

    lint:fix

--- ui/helpers/constants/zendesk-url.js
+++ ui/helpers/constants/zendesk-url.js
@@ -1,46 +1,29 @@
 const ZENDESK_URLS = {
-  ADD_CUSTOM_TOKENS:
-    'https://support.metamask.io/hc/articles/360015489031',
-  ADD_MISSING_ACCOUNTS:
-    'https://support.metamask.io/hc/articles/360015489271',
-  BASIC_SAFETY:
-    'https://support.metamask.io/hc/articles/360015489591',
-  CUSTOMIZE_NONCE:
-    'https://support.metamask.io/hc/articles/7417499333531',
-  HARDWARE_CONNECTION:
-    'https://support.metamask.io/hc/articles/4408552261275',
-  IMPORT_ACCOUNTS:
-    'https://support.metamask.io/hc/articles/360015489331',
-  IMPORTED_ACCOUNTS:
-    'https://support.metamask.io/hc/articles/360015289932',
-  INFURA_BLOCKAGE:
-    'https://support.metamask.io/hc/articles/360059386712',
-  LEGACY_WEB3: 
-    'https://support.metamask.io/hc/articles/360053147012',
-  NFT_TOKENS:
-    'https://support.metamask.io/hc/articles/360058238591',
-  PASSWORD_ARTICLE:
-    'https://support.metamask.io/hc/articles/4405451730331',
+  ADD_CUSTOM_TOKENS: 'https://support.metamask.io/hc/articles/360015489031',
+  ADD_MISSING_ACCOUNTS: 'https://support.metamask.io/hc/articles/360015489271',
+  BASIC_SAFETY: 'https://support.metamask.io/hc/articles/360015489591',
+  CUSTOMIZE_NONCE: 'https://support.metamask.io/hc/articles/7417499333531',
+  HARDWARE_CONNECTION: 'https://support.metamask.io/hc/articles/4408552261275',
+  IMPORT_ACCOUNTS: 'https://support.metamask.io/hc/articles/360015489331',
+  IMPORTED_ACCOUNTS: 'https://support.metamask.io/hc/articles/360015289932',
+  INFURA_BLOCKAGE: 'https://support.metamask.io/hc/articles/360059386712',
+  LEGACY_WEB3: 'https://support.metamask.io/hc/articles/360053147012',
+  NFT_TOKENS: 'https://support.metamask.io/hc/articles/360058238591',
+  PASSWORD_ARTICLE: 'https://support.metamask.io/hc/articles/4405451730331',
   SECRET_RECOVERY_PHRASE:
     'https://support.metamask.io/hc/articles/360060826432',
   SECRET_RECOVERY_PHRASE_USER_GUIDE:
     'https://support.metamask.io/hc/articles/4404722782107',
-  NON_CUSTODIAL_WALLET:
-    'https://support.metamask.io/hc/articles/360059952212',
-  SPEEDUP_CANCEL:
-    'https://support.metamask.io/hc/articles/360015489251',
+  NON_CUSTODIAL_WALLET: 'https://support.metamask.io/hc/articles/360059952212',
+  SPEEDUP_CANCEL: 'https://support.metamask.io/hc/articles/360015489251',
   TOKEN_SAFETY_PRACTICES:
     'https://support.metamask.io/hc/articles/4403988839451',
-  UNKNOWN_NETWORK:
-    'https://support.metamask.io/hc/articles/4417500466971',
+  UNKNOWN_NETWORK: 'https://support.metamask.io/hc/articles/4417500466971',
   USER_GUIDE_CUSTOM_NETWORKS:
     'https://support.metamask.io/hc/articles/4404424659995',
-  USER_GUIDE_DAPPS:
-    'https://support.metamask.io/hc/articles/4405506066331',
-  USER_GUIDE_GAS:
-    'https://support.metamask.io/hc/articles/4404600179227',
-  VERIFY_CUSTOM_NETWORK:
-    'https://support.metamask.io/hc/articles/360057142392',
+  USER_GUIDE_DAPPS: 'https://support.metamask.io/hc/articles/4405506066331',
+  USER_GUIDE_GAS: 'https://support.metamask.io/hc/articles/4404600179227',
+  VERIFY_CUSTOM_NETWORK: 'https://support.metamask.io/hc/articles/360057142392',
 };
 
 export default ZENDESK_URLS;
