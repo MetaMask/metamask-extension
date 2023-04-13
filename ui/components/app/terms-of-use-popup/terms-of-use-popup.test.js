5ae12d4bcb014d12ee7dce98af2380420aeab2ab
Victor Thomas <10986371+vthomas13@users.noreply.github.com>
2023-04-13 12:49:32 -0400

    adding unit test

--- ui/components/app/terms-of-use-popup/terms-of-use-popup.test.js
+++ ui/components/app/terms-of-use-popup/terms-of-use-popup.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import { renderWithProvider } from '../../../../test/jest';
 import configureStore from '../../../store/store';
 import mockState from '../../../../test/data/mock-state.json';
@@ -11,7 +11,8 @@ const render = () => {
       ...mockState.metamask,
     },
   });
-  return renderWithProvider(<TermsOfUsePopup />, store);
+  const onAccept = jest.fn();
+  return renderWithProvider(<TermsOfUsePopup onAccept={onAccept} />, store);
 };
 
 describe('TermsOfUsePopup', () => {
@@ -30,5 +31,18 @@ describe('TermsOfUsePopup', () => {
     expect(
       screen.getByText('Our Terms of Use have updated'),
     ).toBeInTheDocument();
+  });
+
+  it('scrolls down when handleScrollDownClick is called', () => {
+    render();
+    const mockScrollIntoView = jest.fn();
+    window.HTMLElement.prototype.scrollIntoView = mockScrollIntoView;
+    const button = document.querySelector(
+      "[data-testid='popover-scroll-button']",
+    );
+    fireEvent.click(button);
+    expect(mockScrollIntoView).toHaveBeenCalledWith({
+      behavior: 'smooth',
+    });
   });
 });
