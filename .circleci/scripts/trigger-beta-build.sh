223fe9799fcaeecc37eb85ec4ed2619179166335
Dan J Miller <danjm.com@gmail.com>
2023-04-20 12:46:13 -0230

    Build beta with mv3 enabled

--- .circleci/scripts/trigger-beta-build.sh
+++ .circleci/scripts/trigger-beta-build.sh
@@ -12,6 +12,7 @@ if [[ $current_commit_msg =~ Version[[:space:]](v[[:digit:]]+.[[:digit:]]+.[[:di
 then
     # filter the commit message like Version v10.24.1-beta.1
     printf '%s\n' "Create a build for $version with beta version $current_commit_msg"
+    export ENABLE_MV3=true
     yarn build --build-type beta dist
     yarn build --build-type beta prod
 else
