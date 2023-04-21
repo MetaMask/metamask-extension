6ba348705d7b603660cf0c07b95b66762d08ae5a
David Walsh <davidwalsh83@gmail.com>
2023-04-21 08:13:33 -0500

    UX: Convert Modal component to use Popover

--- ui/components/app/modals/modal.js
+++ ui/components/app/modals/modal.js
@@ -1,11 +1,10 @@
-import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import { connect } from 'react-redux';
 import * as actions from '../../../store/actions';
-import isMobileView from '../../../helpers/utils/is-mobile-view';
-import { getEnvironmentType } from '../../../../app/scripts/lib/util';
-import { ENVIRONMENT_TYPE_POPUP } from '../../../../shared/constants/app';
+
+import Popover from '../../ui/popover';
 
 // Modal Components
 import AddNetworkModal from '../../../pages/onboarding-flow/add-network-modal';
@@ -19,7 +18,6 @@ import ConfirmRemoveAccount from './confirm-remove-account';
 import ConfirmResetAccount from './confirm-reset-account';
 import TransactionConfirmed from './transaction-confirmed';
 
-import FadeModal from './fade-modal';
 import RejectTransactions from './reject-transactions';
 import ConfirmDeleteNetwork from './confirm-delete-network';
 import EditApprovalPermission from './edit-approval-permission';
@@ -27,265 +25,23 @@ import NewAccountModal from './new-account-modal';
 import CustomizeNonceModal from './customize-nonce';
 import ConvertTokenToNftModal from './convert-token-to-nft-modal/convert-token-to-nft-modal';
 
-const modalContainerBaseStyle = {
-  transform: 'translate3d(-50%, 0, 0px)',
-  border: '1px solid var(--color-border-default)',
-  borderRadius: '8px',
-  backgroundColor: 'var(--color-background-default)',
-  boxShadow: 'var(--shadow-size-sm) var(--color-shadow-default)',
-};
-
-const modalContainerLaptopStyle = {
-  ...modalContainerBaseStyle,
-  width: '344px',
-  top: '15%',
-};
-
-const modalContainerMobileStyle = {
-  ...modalContainerBaseStyle,
-  width: '309px',
-  top: '12.5%',
-};
-
-const accountModalStyle = {
-  mobileModalStyle: {
-    width: '95%',
-    // top: isPopupOrNotification() === 'popup' ? '52vh' : '36.5vh',
-    boxShadow: 'var(--shadow-size-xs) var(--color-shadow-default)',
-    borderRadius: '4px',
-    top: '10%',
-    transform: 'none',
-    left: '0',
-    right: '0',
-    margin: '0 auto',
-  },
-  laptopModalStyle: {
-    width: '335px',
-    // top: 'calc(33% + 45px)',
-    boxShadow: 'var(--shadow-size-xs) var(--color-shadow-default)',
-    borderRadius: '4px',
-    top: '10%',
-    transform: 'none',
-    left: '0',
-    right: '0',
-    margin: '0 auto',
-  },
-  contentStyle: {
-    borderRadius: '4px',
-  },
-};
-
 const MODALS = {
-  ONBOARDING_ADD_NETWORK: {
-    contents: <AddNetworkModal />,
-    ...accountModalStyle,
-  },
-  NEW_ACCOUNT: {
-    contents: <NewAccountModal />,
-    mobileModalStyle: {
-      width: '95%',
-      top: '10%',
-      boxShadow: 'var(--shadow-size-xs) var(--color-shadow-default)',
-      transform: 'none',
-      left: '0',
-      right: '0',
-      margin: '0 auto',
-      borderRadius: '10px',
-    },
-    laptopModalStyle: {
-      width: '375px',
-      top: '10%',
-      boxShadow: 'var(--shadow-size-xs) var(--color-shadow-default)',
-      transform: 'none',
-      left: '0',
-      right: '0',
-      margin: '0 auto',
-      borderRadius: '10px',
-    },
-    contentStyle: {
-      borderRadius: '10px',
-    },
-  },
-
-  ACCOUNT_DETAILS: {
-    contents: <AccountDetailsModal />,
-    ...accountModalStyle,
-  },
-
-  EXPORT_PRIVATE_KEY: {
-    contents: <ExportPrivateKeyModal />,
-    ...accountModalStyle,
-  },
-
-  HOLD_TO_REVEAL_SRP: {
-    contents: <HoldToRevealModal />,
-    mobileModalStyle: {
-      ...modalContainerMobileStyle,
-    },
-    laptopModalStyle: {
-      ...modalContainerLaptopStyle,
-    },
-    contentStyle: {
-      borderRadius: '8px',
-    },
-  },
-
-  HIDE_TOKEN_CONFIRMATION: {
-    contents: <HideTokenConfirmationModal />,
-    mobileModalStyle: {
-      width: '95%',
-      top: getEnvironmentType() === ENVIRONMENT_TYPE_POPUP ? '52vh' : '36.5vh',
-    },
-    laptopModalStyle: {
-      width:
-        getEnvironmentType() === ENVIRONMENT_TYPE_POPUP ? '357px' : '449px',
-      top: 'calc(33% + 45px)',
-      paddingLeft:
-        getEnvironmentType() === ENVIRONMENT_TYPE_POPUP ? '16px' : null,
-      paddingRight:
-        getEnvironmentType() === ENVIRONMENT_TYPE_POPUP ? '16px' : null,
-    },
-  },
-
-  CONFIRM_RESET_ACCOUNT: {
-    contents: <ConfirmResetAccount />,
-    mobileModalStyle: {
-      ...modalContainerMobileStyle,
-    },
-    laptopModalStyle: {
-      ...modalContainerLaptopStyle,
-    },
-    contentStyle: {
-      borderRadius: '8px',
-    },
-  },
-
-  CONFIRM_REMOVE_ACCOUNT: {
-    contents: <ConfirmRemoveAccount />,
-    mobileModalStyle: {
-      ...modalContainerMobileStyle,
-    },
-    laptopModalStyle: {
-      ...modalContainerLaptopStyle,
-    },
-    contentStyle: {
-      borderRadius: '8px',
-    },
-  },
-
-  CONVERT_TOKEN_TO_NFT: {
-    contents: <ConvertTokenToNftModal />,
-    mobileModalStyle: {
-      ...modalContainerMobileStyle,
-    },
-    laptopModalStyle: {
-      ...modalContainerLaptopStyle,
-    },
-    contentStyle: {
-      borderRadius: '8px',
-    },
-  },
-
-  CONFIRM_DELETE_NETWORK: {
-    contents: <ConfirmDeleteNetwork />,
-    mobileModalStyle: {
-      ...modalContainerMobileStyle,
-    },
-    laptopModalStyle: {
-      ...modalContainerLaptopStyle,
-    },
-    contentStyle: {
-      borderRadius: '8px',
-    },
-  },
-
-  EDIT_APPROVAL_PERMISSION: {
-    contents: <EditApprovalPermission />,
-    mobileModalStyle: {
-      width: '95vw',
-      height: '100vh',
-      top: '50px',
-      transform: 'none',
-      left: '0',
-      right: '0',
-      margin: '0 auto',
-    },
-    laptopModalStyle: {
-      width: 'auto',
-      height: '0px',
-      top: '80px',
-      left: '0px',
-      transform: 'none',
-      margin: '0 auto',
-      position: 'relative',
-    },
-    contentStyle: {
-      borderRadius: '8px',
-    },
-  },
-
-  TRANSACTION_CONFIRMED: {
-    disableBackdropClick: true,
-    contents: <TransactionConfirmed />,
-    mobileModalStyle: {
-      ...modalContainerMobileStyle,
-    },
-    laptopModalStyle: {
-      ...modalContainerLaptopStyle,
-    },
-    contentStyle: {
-      borderRadius: '8px',
-    },
-  },
-
-  QR_SCANNER: {
-    contents: <QRScanner />,
-    mobileModalStyle: {
-      ...modalContainerMobileStyle,
-    },
-    laptopModalStyle: {
-      ...modalContainerLaptopStyle,
-    },
-    contentStyle: {
-      borderRadius: '8px',
-    },
-  },
-
-  REJECT_TRANSACTIONS: {
-    contents: <RejectTransactions />,
-    mobileModalStyle: {
-      ...modalContainerMobileStyle,
-    },
-    laptopModalStyle: {
-      ...modalContainerLaptopStyle,
-    },
-    contentStyle: {
-      borderRadius: '8px',
-    },
-  },
-
-  CUSTOMIZE_NONCE: {
-    contents: <CustomizeNonceModal />,
-    mobileModalStyle: {
-      ...modalContainerMobileStyle,
-    },
-    laptopModalStyle: {
-      ...modalContainerLaptopStyle,
-    },
-    contentStyle: {
-      borderRadius: '8px',
-    },
-  },
-
-  DEFAULT: {
-    contents: [],
-    mobileModalStyle: {},
-    laptopModalStyle: {},
-  },
-};
-
-const BACKDROPSTYLE = {
-  backgroundColor: 'var(--color-overlay-default)',
+  ONBOARDING_ADD_NETWORK: { contents: <AddNetworkModal /> },
+  NEW_ACCOUNT: { contents: <NewAccountModal /> },
+  ACCOUNT_DETAILS: { contents: <AccountDetailsModal /> },
+  EXPORT_PRIVATE_KEY: { contents: <ExportPrivateKeyModal /> },
+  HOLD_TO_REVEAL_SRP: { contents: <HoldToRevealModal /> },
+  HIDE_TOKEN_CONFIRMATION: { contents: <HideTokenConfirmationModal /> },
+  CONFIRM_RESET_ACCOUNT: { contents: <ConfirmResetAccount /> },
+  CONFIRM_REMOVE_ACCOUNT: { contents: <ConfirmRemoveAccount /> },
+  CONVERT_TOKEN_TO_NFT: { contents: <ConvertTokenToNftModal /> },
+  CONFIRM_DELETE_NETWORK: { contents: <ConfirmDeleteNetwork /> },
+  EDIT_APPROVAL_PERMISSION: { contents: <EditApprovalPermission /> },
+  TRANSACTION_CONFIRMED: { contents: <TransactionConfirmed /> },
+  QR_SCANNER: { contents: <QRScanner /> },
+  REJECT_TRANSACTIONS: { contents: <RejectTransactions /> },
+  CUSTOMIZE_NONCE: { contents: <CustomizeNonceModal /> },
+  DEFAULT: { contents: [] },
 };
 
 function mapStateToProps(state) {
@@ -311,57 +67,18 @@ function mapDispatchToProps(dispatch) {
 
 class Modal extends Component {
   static propTypes = {
-    active: PropTypes.bool.isRequired,
-    hideModal: PropTypes.func.isRequired,
-    hideWarning: PropTypes.func.isRequired,
     modalState: PropTypes.object.isRequired,
   };
 
-  hide() {
-    this.modalRef.hide();
-  }
-
-  show() {
-    this.modalRef.show();
-  }
-
-  UNSAFE_componentWillReceiveProps(nextProps, _) {
-    if (nextProps.active) {
-      this.show();
-    } else if (this.props.active) {
-      this.hide();
-    }
-  }
-
   render() {
     const modal = MODALS[this.props.modalState.name || 'DEFAULT'];
-    const { contents: children, disableBackdropClick = false } = modal;
-    const modalStyle =
-      modal[isMobileView() ? 'mobileModalStyle' : 'laptopModalStyle'];
-    const contentStyle = modal.contentStyle || {};
+    const { contents: children } = modal;
 
-    return (
-      <FadeModal
-        keyboard={false}
-        onHide={() => {
-          if (modal.onHide) {
-            modal.onHide({
-              hideWarning: this.props.hideWarning,
-            });
-          }
-          this.props.hideModal(modal.customOnHideOpts);
-        }}
-        ref={(ref) => {
-          this.modalRef = ref;
-        }}
-        modalStyle={modalStyle}
-        contentStyle={contentStyle}
-        backdropStyle={BACKDROPSTYLE}
-        closeOnClick={!disableBackdropClick}
-      >
-        {children}
-      </FadeModal>
-    );
+    if (this.props.modalState.name === null) {
+      return null;
+    }
+
+    return <Popover onClose={modal.onHide}>{children}</Popover>;
   }
 }
 
