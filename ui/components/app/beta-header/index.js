c56d17904ac824bb67ebfd4332c9a2e36783391f
Harsh Shukla <125105825+PrgrmrHarshShukla@users.noreply.github.com>
2023-04-22 07:00:00 +0530

    Remove 'as' prop in ui/components/app/beta-header/index.js
    Co-authored-by: George Marshall <georgewrmarshall@gmail.com>

--- ui/components/app/beta-header/index.js
+++ ui/components/app/beta-header/index.js
@@ -34,7 +34,6 @@ const BetaHeader = () => {
     >
       <Text
         variant={TextVariant.bodySm}
-        as="h6"
         marginTop={0}
         marginBottom={0}
         className="beta-header__message"
