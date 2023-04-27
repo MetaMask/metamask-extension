ae2efdb3022cae8f26e3b037394defc73fd5ec1c
Frederik Bolding <frederik.bolding@gmail.com>
2023-04-27 12:57:42 +0200

    Fix tests

--- shared/constants/permissions.test.js
+++ shared/constants/permissions.test.js
@@ -2,20 +2,13 @@ import { endowmentPermissionBuilders } from '@metamask/snaps-controllers';
 import { restrictedMethodPermissionBuilders } from '@metamask/rpc-methods';
 import {
   EndowmentPermissions,
-  ExcludedSnapEndowments,
-  ExcludedSnapPermissions,
   RestrictedMethods,
 } from './permissions';
 
 describe('EndowmentPermissions', () => {
   it('has the expected permission keys', () => {
     expect(Object.keys(EndowmentPermissions).sort()).toStrictEqual(
-      Object.keys(endowmentPermissionBuilders)
-        .filter(
-          (targetKey) =>
-            !Object.keys(ExcludedSnapEndowments).includes(targetKey),
-        )
-        .sort(),
+      Object.keys(endowmentPermissionBuilders).sort(),
     );
   });
 });
@@ -25,10 +18,7 @@ describe('RestrictedMethods', () => {
     expect(Object.keys(RestrictedMethods).sort()).toStrictEqual(
       [
         'eth_accounts',
-        ...Object.keys(restrictedMethodPermissionBuilders).filter(
-          (targetKey) =>
-            !Object.keys(ExcludedSnapPermissions).includes(targetKey),
-        ),
+        ...Object.keys(restrictedMethodPermissionBuilders),
       ].sort(),
     );
   });
