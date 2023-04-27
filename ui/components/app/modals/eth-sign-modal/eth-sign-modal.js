3ecfb98308df1e4e43cdbdd08f3e0abb2cf9738c
NidhiKJha <menidhikjha@gmail.com>
2023-04-27 16:20:11 +0530

    added modal

--- ui/components/app/modals/eth-sign-modal/eth-sign-modal.js
+++ ui/components/app/modals/eth-sign-modal/eth-sign-modal.js
@@ -7,6 +7,7 @@ import {
   Button,
   ButtonIcon,
   BUTTON_TYPES,
+  FormTextField,
   Icon,
   IconName,
   IconSize,
@@ -29,10 +30,24 @@ import { useI18nContext } from '../../../../hooks/useI18nContext';
 import CheckBox from '../../../ui/check-box';
 
 const EthSignModal = ({ hideModal }) => {
-    const [isEthSignChecked, setIsEthSignChecked] = useState(false);
+  const [isEthSignChecked, setIsEthSignChecked] = useState(false);
+  const [showTextField, setShowTextField] = useState(false);
+  const [inputKeyword, setInputKeyword] = useState('');
+  const [showEnable, setShowEnable] = useState(false);
+  const [showError, setShowError] = useState(false);
   const t = useI18nContext();
   const handleCancel = () => {
     hideModal();
+  };
+  const handleKeyPress = (event) => {
+    if (inputKeyword !== '' && event.key === 'Enter') {
+      event.preventDefault();
+      if (inputKeyword === 'I only sign what I understand') {
+        setShowEnable(true);
+      } else {
+        setShowError(true);
+      }
+    }
   };
   return (
     <Box
@@ -40,7 +55,7 @@ const EthSignModal = ({ hideModal }) => {
       display={DISPLAY.FLEX}
       flexDirection={FLEX_DIRECTION.COLUMN}
       justifyContent={JustifyContent.flexStart}
-      padding={6}
+      padding={4}
     >
       <Box
         display={DISPLAY.FLEX}
@@ -71,45 +86,79 @@ const EthSignModal = ({ hideModal }) => {
         Always review the URL and be careful when signing messages that contain
         code. Learn more.
       </Text>
-      <BannerAlert severity={SEVERITIES.DANGER}>
+      <BannerAlert severity={SEVERITIES.DANGER} marginTop={6} marginBottom={6}>
         If you have been asked to turn this setting on, you might be getting
         scammed.
       </BannerAlert>
-      <Box
-        flexDirection={FLEX_DIRECTION.ROW}
-        alignItems={AlignItems.flexStart}
-        marginLeft={3}
-        marginRight={3}
-        gap={2}
-      >
-        <CheckBox
-          id="eth-sign__checkbox"
-          className="eth-sign__checkbox"
-          dataTestId="eth-sign__checkbox"
-          checked={isEthSignChecked}
-          onClick={() => {
-            setIsEthSignChecked(!isEthSignChecked);
+      {showTextField ? (
+        <FormTextField
+          id="enter-eth-sign-text"
+          label="Enter “I only sign what I understand” to continue"
+          error={showError}
+          helpText={showError ? `The text is incorrect. Try again.` : null}
+          onChange={(event) => setInputKeyword(event.target.value)}
+          value={inputKeyword}
+          inputProps={{
+            onKeyPress: handleKeyPress,
           }}
         />
-        <Label htmlFor="eth-sign__checkbox">
-          <Text variant={TextVariant.bodyMd} as="span">
-            I understand that I can lose all of my funds and NFTs if I enable
-            eth_sign requests.
-          </Text>
-        </Label>
-      </Box>
+      ) : (
+        <Box
+          flexDirection={FLEX_DIRECTION.ROW}
+          alignItems={AlignItems.flexStart}
+          gap={2}
+        >
+          <CheckBox
+            id="eth-sign__checkbox"
+            className="eth-sign__checkbox"
+            dataTestId="eth-sign__checkbox"
+            checked={isEthSignChecked}
+            onClick={() => {
+              setIsEthSignChecked(!isEthSignChecked);
+            }}
+          />
+          <Label htmlFor="eth-sign__checkbox">
+            <Text variant={TextVariant.bodyMd} as="span">
+              I understand that I can lose all of my funds and NFTs if I enable
+              eth_sign requests.
+            </Text>
+          </Label>
+        </Box>
+      )}
       <Box
         display={DISPLAY.FLEX}
         flexDirection={FLEX_DIRECTION.ROW}
         justifyContent={JustifyContent.spaceBetween}
         gap={4}
+        marginTop={6}
       >
-        <Button type={BUTTON_TYPES.SECONDARY} width={BLOCK_SIZES.FULL}>
+        <Button
+          type={BUTTON_TYPES.SECONDARY}
+          width={BLOCK_SIZES.FULL}
+          onClick={handleCancel}
+        >
           {t('cancel')}
         </Button>
-        <Button type={BUTTON_TYPES.PRIMARY} width={BLOCK_SIZES.FULL}>
-          {t('continue')}
-        </Button>
+        {showTextField ? (
+          <Button
+            type={BUTTON_TYPES.PRIMARY}
+            danger
+            width={BLOCK_SIZES.FULL}
+            disabled={!showEnable}
+            onClick={() => setShowTextField(true)}
+          >
+            Enable
+          </Button>
+        ) : (
+          <Button
+            type={BUTTON_TYPES.PRIMARY}
+            width={BLOCK_SIZES.FULL}
+            disabled={!isEthSignChecked}
+            onClick={() => setShowTextField(true)}
+          >
+            {t('continue')}
+          </Button>
+        )}
       </Box>
     </Box>
   );
