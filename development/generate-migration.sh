44adf158355255dedbb8ce186603c17df64da93b
Nicholas Ellul <nicholas.ellul1@gmail.com>
2023-04-28 12:50:02 -0400

    Fix script to build migrations

--- development/generate-migration.sh
+++ development/generate-migration.sh
@@ -5,8 +5,8 @@ g-migration() {
   if (($1 < 100)); then
     vnum=0$1
   fi
-  touch app/scripts/migrations/"$vnum".js
-  cp app/scripts/migrations/template.js app/scripts/migrations/"$vnum".js
+  touch app/scripts/migrations/"$vnum".ts
+  cp app/scripts/migrations/template.ts app/scripts/migrations/"$vnum".ts
 
   touch app/scripts/migrations/"$vnum".test.js
   cp app/scripts/migrations/template.test.js app/scripts/migrations/"$vnum".test.js
