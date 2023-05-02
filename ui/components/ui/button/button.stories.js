7a7cf7a1d97cff44128e2657c389a213d9f1c76d
George Marshall <george.marshall@consensys.net>
2023-05-01 19:14:01 -0700

    Update ui/components/ui/button/button.stories.js
    Co-authored-by: legobeat <109787230+legobeat@users.noreply.github.com>

--- ui/components/ui/button/button.stories.js
+++ ui/components/ui/button/button.stories.js
@@ -63,7 +63,7 @@ export const DefaultStory = (args) => (
       marginBottom={4}
       severity={SEVERITIES.WARNING}
       title="Deprecated"
-      description="This version of Button has been deprecated in favor of the component-library version. Contribute to replacing old Button with new Button by submitting a PR"
+      description="This version of Button has been deprecated in favor of the component-library version. Contribute to replacing old Button with new Button by submitting a PR to metamask-extension."
       actionButtonLabel="See details"
       actionButtonProps={{
         href: 'https://github.com/MetaMask/metamask-extension/issues/18896',
