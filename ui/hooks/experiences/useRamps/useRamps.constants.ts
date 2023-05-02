3efe74283300fbc56b85f4893e2241fffa724d59
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2023-05-02 11:41:29 -0400

    refactor(on-ramp): move useRamops mocked networks to test file

--- ui/hooks/experiences/useRamps/useRamps.constants.ts
+++ ui/hooks/experiences/useRamps/useRamps.constants.ts
@@ -1,89 +1,9 @@
-import { CHAIN_IDS } from '../../../../shared/constants/network';
+import { Hex } from '@metamask/utils';
+import { ChainId, CHAIN_IDS } from '../../../../shared/constants/network';
 
 export const portfolioUrl = process.env.PORTFOLIO_URL;
 export const buyPath = '/buy';
 export const entryParam = 'metamaskEntry';
 export const entryParamValue = 'ext_buy_button';
 
-export const MANUALLY_ACTIVE_CHAIN_IDS = [CHAIN_IDS.SEPOLIA];
-
-export const mockedNetworks = [
-  {
-    active: true,
-    chainId: 1,
-    chainName: 'Ethereum Mainnet',
-    nativeTokenSupported: true,
-  },
-  {
-    active: true,
-    chainId: 10,
-    chainName: 'Optimism Mainnet',
-    nativeTokenSupported: true,
-  },
-  {
-    active: true,
-    chainId: 25,
-    chainName: 'Cronos Mainnet',
-    nativeTokenSupported: true,
-  },
-  {
-    active: true,
-    chainId: 56,
-    chainName: 'BNB Chain Mainnet',
-    nativeTokenSupported: true,
-  },
-  {
-    active: true,
-    chainId: 137,
-    chainName: 'Polygon Mainnet',
-    nativeTokenSupported: true,
-  },
-  {
-    active: true,
-    chainId: 250,
-    chainName: 'Fantom Mainnet',
-    nativeTokenSupported: true,
-  },
-  {
-    active: true,
-    chainId: 1285,
-    chainName: 'Moonriver Mainnet',
-    nativeTokenSupported: true,
-  },
-  {
-    active: true,
-    chainId: 42161,
-    chainName: 'Arbitrum Mainnet',
-    nativeTokenSupported: true,
-  },
-  {
-    active: true,
-    chainId: 42220,
-    chainName: 'Celo Mainnet',
-    nativeTokenSupported: true,
-  },
-  {
-    active: true,
-    chainId: 43114,
-    chainName: 'Avalanche C-Chain Mainnet',
-    nativeTokenSupported: true,
-  },
-  {
-    active: true,
-    chainId: 1313161554,
-    chainName: 'Aurora Mainnet',
-    nativeTokenSupported: false,
-  },
-  {
-    active: true,
-    chainId: 1666600000,
-    chainName: 'Harmony Mainnet (Shard 0)',
-    nativeTokenSupported: true,
-  },
-  {
-    active: true,
-    chainId: 11297108109,
-    chainName: 'Palm',
-    nativeTokenSupported: false,
-  },
-];
+export const MANUALLY_ACTIVE_CHAIN_IDS: (Hex | ChainId)[] = [CHAIN_IDS.SEPOLIA];
--- ui/hooks/experiences/useRamps/useRamps.test.tsx
+++ ui/hooks/experiences/useRamps/useRamps.test.tsx
@@ -8,12 +8,92 @@ import {
   buyPath,
   entryParam,
   entryParamValue,
-  mockedNetworks,
   portfolioUrl,
 } from './useRamps.constants';
 
 jest.mock('./OnRampSDK');
 const mockedSDK = SDK as jest.Mocked<typeof SDK>;
+
+const mockedNetworks = [
+  {
+    active: true,
+    chainId: 1,
+    chainName: 'Ethereum Mainnet',
+    nativeTokenSupported: true,
+  },
+  {
+    active: true,
+    chainId: 10,
+    chainName: 'Optimism Mainnet',
+    nativeTokenSupported: true,
+  },
+  {
+    active: true,
+    chainId: 25,
+    chainName: 'Cronos Mainnet',
+    nativeTokenSupported: true,
+  },
+  {
+    active: true,
+    chainId: 56,
+    chainName: 'BNB Chain Mainnet',
+    nativeTokenSupported: true,
+  },
+  {
+    active: true,
+    chainId: 137,
+    chainName: 'Polygon Mainnet',
+    nativeTokenSupported: true,
+  },
+  {
+    active: true,
+    chainId: 250,
+    chainName: 'Fantom Mainnet',
+    nativeTokenSupported: true,
+  },
+  {
+    active: true,
+    chainId: 1285,
+    chainName: 'Moonriver Mainnet',
+    nativeTokenSupported: true,
+  },
+  {
+    active: true,
+    chainId: 42161,
+    chainName: 'Arbitrum Mainnet',
+    nativeTokenSupported: true,
+  },
+  {
+    active: true,
+    chainId: 42220,
+    chainName: 'Celo Mainnet',
+    nativeTokenSupported: true,
+  },
+  {
+    active: true,
+    chainId: 43114,
+    chainName: 'Avalanche C-Chain Mainnet',
+    nativeTokenSupported: true,
+  },
+  {
+    active: true,
+    chainId: 1313161554,
+    chainName: 'Aurora Mainnet',
+    nativeTokenSupported: false,
+  },
+  {
+    active: true,
+    chainId: 1666600000,
+    chainName: 'Harmony Mainnet (Shard 0)',
+    nativeTokenSupported: true,
+  },
+  {
+    active: true,
+    chainId: 11297108109,
+    chainName: 'Palm',
+    nativeTokenSupported: false,
+  },
+];
 
 // test helpers
 const storeWithChainId = (chainId: string) => ({
