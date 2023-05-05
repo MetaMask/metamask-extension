cc531fe7ccafeb17282690423a31641a797c8429
Harsh Shukla <125105825+PrgrmrHarshShukla@users.noreply.github.com>
2023-05-05 07:16:20 +0530

    Update ui/components/app/nft-details/nft-details.js
    Co-authored-by: George Marshall <georgewrmarshall@gmail.com>

--- ui/components/app/nft-details/nft-details.js
+++ ui/components/app/nft-details/nft-details.js
@@ -236,7 +236,6 @@ export default function NftDetails({ nft }) {
                   as="h6"
                   overflowWrap={OverflowWrap.BreakWord}
                   marginBottom={4}
-                  margin={0}
                 >
                   {description}
                 </Text>
