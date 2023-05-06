839c65f660dde70cd59fd13010c45d4eeac5f537
Harsh Shukla <125105825+PrgrmrHarshShukla@users.noreply.github.com>
2023-05-06 07:33:45 +0530

    Update ui/pages/onboarding-flow/metametrics/metametrics.js
    Co-authored-by: Danica Shen <zhaodanica@gmail.com>

--- ui/pages/onboarding-flow/metametrics/metametrics.js
+++ ui/pages/onboarding-flow/metametrics/metametrics.js
@@ -164,7 +164,7 @@ export default function OnboardingMetametrics() {
             />
             {t('onboardingMetametricsNeverSellData', [
               <Text
-                display={DISPLAY.INLINE_BLOCK}
+         as="span"
                 variant={TextVariant.bodyMd}
                 key="never-sell"
                 fontWeight={FontWeight.Bold}
