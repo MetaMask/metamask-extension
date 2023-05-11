1c38b7ff2f5c597eedede483f9e571567c3e96ee
Antonio Regadas <antonio.regadas@consensys.net>
2023-05-11 17:15:30 +0100

    adds component and test

---
+++ ui/components/institutional/wrong-network-notification/wrong-network-notification.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import {
+  DISPLAY,
+  AlignItems,
+  JustifyContent,
+  BackgroundColor,
+  BLOCK_SIZES,
+} from '../../../helpers/constants/design-system';
+import { getSelectedAccountCachedBalance } from '../../../selectors';
+import { getIsCustodianSupportedChain } from '../../../selectors/institutional/selectors';
+import { useI18nContext } from '../../../hooks/useI18nContext';
+import { getProviderConfig } from '../../../ducks/metamask/metamask';
+import { Text } from '../../component-library';
+import Box from '../../ui/box';
+
+const WrongNetworkNotification = () => {
+  const t = useI18nContext();
+  const providerConfig = useSelector(getProviderConfig);
+  const balance = useSelector(getSelectedAccountCachedBalance);
+
+  const isCustodianSupportedChain = useSelector(getIsCustodianSupportedChain);
+
+  const network = providerConfig.nickname || providerConfig.type;
+
+  return !isCustodianSupportedChain && balance ? (
+    <Box
+      className="wrong-network-notification"
+      data-testid="wrong-network-notification"
+      display={DISPLAY.FLEX}
+      justifyContent={JustifyContent.center}
+      alignItems={AlignItems.center}
+      padding={[1, 6]}
+      backgroundColor={BackgroundColor.errorMuted}
+      width={BLOCK_SIZES.FULL}
+    >
+      <img src="images/icons/red-triangle-exclaim.svg" />{' '}
+      <Text>
+        {t('custodyWrongChain', [
+          network ? network.charAt(0).toUpperCase() + network.slice(1) : '',
+        ])}
+      </Text>
+    </Box>
+  ) : null;
+};
+
+export default WrongNetworkNotification;
