edf23ebd07092acf28c2e4adedc04e241215a032
Monte Lai <monte.lai@consensys.net>
2023-05-15 16:17:59 +0800

    add password toggle

--- ui/pages/create-account/import-account/json.js
+++ ui/pages/create-account/import-account/json.js
@@ -5,6 +5,7 @@ import FileInput from 'react-simple-file-input';
 import {
   ButtonLink,
   FormTextField,
+  Label,
   Text,
   TEXT_FIELD_SIZES,
   TEXT_FIELD_TYPES,
@@ -13,10 +14,15 @@ import {
   Size,
   TextVariant,
   TEXT_ALIGN,
+  FLEX_DIRECTION,
+  DISPLAY,
+  JustifyContent,
 } from '../../../helpers/constants/design-system';
 import ZENDESK_URLS from '../../../helpers/constants/zendesk-url';
 import { useI18nContext } from '../../../hooks/useI18nContext';
 import { displayWarning } from '../../../store/actions';
+import Box from '../../../components/ui/box/box';
+import ToggleButton from '../../../components/ui/toggle-button';
 import BottomButtons from './bottom-buttons';
 
 JsonImportSubview.propTypes = {
@@ -28,18 +34,34 @@ export default function JsonImportSubview({ importAccountFunc }) {
   const warning = useSelector((state) => state.appState.warning);
   const [password, setPassword] = useState('');
   const [fileContents, setFileContents] = useState('');
+  const [isPasswordless, setIsPasswordless] = useState(false);
 
-  const isPrimaryDisabled = password === '' || fileContents === '';
+  function isPrimaryDisabled() {
+    if (fileContents === '') {
+      return true;
+    }
+    if (!isPasswordless && password === '') {
+      return true;
+    }
+
+    return false;
+  }
+
+  function handlePasswordlessCheck() {
+    if (password !== '') return;
+    setIsPasswordless(!isPasswordless);
+    setPassword('');
+  }
 
   function handleKeyPress(event) {
-    if (!isPrimaryDisabled && event.key === 'Enter') {
+    if (!isPrimaryDisabled() && event.key === 'Enter') {
       event.preventDefault();
       _importAccountFunc();
     }
   }
 
   function _importAccountFunc() {
-    if (isPrimaryDisabled) {
+    if (isPrimaryDisabled()) {
       displayWarning(t('needImportFile'));
     } else {
       importAccountFunc('JSON File', [fileContents, password]);
@@ -60,7 +82,23 @@ export default function JsonImportSubview({ importAccountFunc }) {
         </ButtonLink>
       </Text>
 
+      <Box
+        display={DISPLAY.FLEX}
+        flexDirection={FLEX_DIRECTION.ROW}
+        justifyContent={JustifyContent.flexEnd}
+      >
+        <Label marginRight={2}>{t('importJsonWithoutPassword')}</Label>
+        <ToggleButton
+          value={isPasswordless}
+          offLabel={t('importJsonWithoutPassword')}
+          onToggle={handlePasswordlessCheck}
+          disabled={password !== ''}
+        ></ToggleButton>
+      </Box>
+
       <FileInput
+        id="file-input"
+        data-testid="file-input"
         readAs="text"
         onLoad={(event) => setFileContents(event.target.result)}
         style={{
@@ -72,25 +110,30 @@ export default function JsonImportSubview({ importAccountFunc }) {
         }}
       />
 
-      <FormTextField
-        id="json-password-box"
-        size={TEXT_FIELD_SIZES.LARGE}
-        autoFocus
-        type={TEXT_FIELD_TYPES.PASSWORD}
-        helpText={warning}
-        error
-        placeholder={t('enterPassword')}
-        value={password}
-        onChange={(event) => setPassword(event.target.value)}
-        inputProps={{
-          onKeyPress: handleKeyPress,
-        }}
-        marginBottom={4}
-      />
+      {!isPasswordless && (
+        <FormTextField
+          id="json-password-box"
+          size={TEXT_FIELD_SIZES.LARGE}
+          autoFocus
+          type={TEXT_FIELD_TYPES.PASSWORD}
+          helpText={warning}
+          error
+          placeholder={t('enterPassword')}
+          value={password}
+          onChange={(event) => {
+            setIsPasswordless(false);
+            setPassword(event.target.value);
+          }}
+          inputProps={{
+            onKeyPress: handleKeyPress,
+          }}
+          marginBottom={4}
+        />
+      )}
 
       <BottomButtons
         importAccountFunc={_importAccountFunc}
-        isPrimaryDisabled={isPrimaryDisabled}
+        isPrimaryDisabled={isPrimaryDisabled()}
       />
     </>
   );
