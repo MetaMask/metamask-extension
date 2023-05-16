a017a1bae088b5c32ad4a2196af064ec398ee9eb
Albert Olivé <albertolivecorbella@gmail.com>
2023-05-16 17:44:08 +0200

    [MMI] Added note to trader code fencing (#18051)
    * Added note to trader code fencing
    
    * Started adding code fences in signature-request
    
    * Finished code fencing
    
    * Improving code
    
    * adds check and runs prettier
    
    * Fixed storybook and code fences bundle
    
    * Added missing dependency
    
    * updates fences
    
    * fewer lines
    
    * undo previously merged PR
    
    * ran lavamoat auto
    
    * adds test
    
    * prettier
    
    ---------
    
    Co-authored-by: António Regadas <apregadas@gmail.com>
    Co-authored-by: Antonio Regadas <antonio.regadas@consensys.net>
    Co-authored-by: Brad Decker <bhdecker84@gmail.com>

--- ui/components/app/confirm-page-container/confirm-page-container.component.js
+++ ui/components/app/confirm-page-container/confirm-page-container.component.js
@@ -99,6 +99,9 @@ const ConfirmPageContainer = (props) => {
     txData,
     assetStandard,
     isApprovalOrRejection,
+    ///: BEGIN:ONLY_INCLUDE_IN(build-mmi)
+    noteComponent,
+    ///: END:ONLY_INCLUDE_IN
   } = props;
 
   const t = useI18nContext();
@@ -238,6 +241,9 @@ const ConfirmPageContainer = (props) => {
             transactionType={currentTransaction.type}
             isBuyableChain={isBuyableChain}
             txData={txData}
+            ///: BEGIN:ONLY_INCLUDE_IN(build-mmi)
+            noteComponent={noteComponent}
+            ///: END:ONLY_INCLUDE_IN
           />
         )}
         {shouldDisplayWarning && errorKey === INSUFFICIENT_FUNDS_ERROR_KEY && (
@@ -398,6 +404,9 @@ ConfirmPageContainer.propTypes = {
   supportsEIP1559: PropTypes.bool,
   nativeCurrency: PropTypes.string,
   isApprovalOrRejection: PropTypes.bool,
+  ///: BEGIN:ONLY_INCLUDE_IN(build-mmi)
+  noteComponent: PropTypes.node,
+  ///: END:ONLY_INCLUDE_IN
 };
 
 export default ConfirmPageContainer;
