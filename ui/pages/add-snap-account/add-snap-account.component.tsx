f800cc60448e444a19845b00748180b73ffb3ca9
Monte Lai <monte.lai@consensys.net>
2023-05-18 07:47:30 +0800

    add snap account page

---
+++ ui/pages/add-snap-account/add-snap-account.component.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Box from '../../components/ui/box/box';
+import { ADD_SNAP_ACCOUNT_ROUTE } from '../../helpers/constants/routes';
+import {
+  BackgroundColor,
+  DISPLAY,
+  JustifyContent,
+} from '../../helpers/constants/design-system';
+import NewSnapAccountPage from './snap-account';
+import SnapAccountDetailPage from './snap-account-detail';
+
+const AddSnapAccountPage = () => {
+  return (
+    <Box
+      display={DISPLAY.FLEX}
+      justifyContent={JustifyContent.center}
+      backgroundColor={BackgroundColor.backgroundDefault}
+      marginLeft={10}
+      marginRight={10}
+    >
+      <Switch>
+        <>
+          <Route
+            exact
+            path={`${ADD_SNAP_ACCOUNT_ROUTE}/:snapId`}
+            component={SnapAccountDetailPage}
+          />
+          <Route
+            exact
+            path={ADD_SNAP_ACCOUNT_ROUTE}
+            component={NewSnapAccountPage}
+          />
+        </>
+      </Switch>
+    </Box>
+  );
+};
+
+export default AddSnapAccountPage;
---
+++ ui/pages/add-snap-account/snap-account/snap-account.tsx
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import Box from '../../../components/ui/box/box';
+import { Text } from '../../../components/component-library';
+import { useI18nContext } from '../../../hooks/useI18nContext';
+import {
+  AlignItems,
+  DISPLAY,
+  FLEX_DIRECTION,
+  FLEX_WRAP,
+  JustifyContent,
+  TextColor,
+  TextVariant,
+} from '../../../helpers/constants/design-system';
+import SnapCard from '../snap-card/snap-card';
+
+export interface SnapDetails {
+  id: string;
+  iconUrl: string;
+  snapTitle: string;
+  snapSlug: string;
+  snapDescription: string;
+  tags: string[];
+  developer: string;
+  website: string;
+  auditUrls: string[];
+  version: string;
+  lastUpdated: string;
+}
+
+export interface SnapCardProps extends SnapDetails {
+  isInstalled: boolean;
+  updateAvailable: boolean;
+}
+
+const mockSnapList: SnapDetails[] = new Array(5).fill({
+  id: 'mockSnapId',
+  iconUrl: '',
+  snapTitle: 'Metamask TSS',
+  snapSlug: 'Secure your account with MetaMask Mobile',
+  snapDescription:
+    'Threshold signature schemes (TSS) allow multiple collaborating participants to sign a message or transaction. The private key is shared between the participants using a technique called multi-party computation (MPC) which ensures that the entire private key is never exposed to any participant; the process for generating key shares is called distributed key generation (DKG). Before a signature can be generated all participants must generate key shares using DKG and store their key shares securely; the number of participants and threshold for signature generation must be decided in advance. Unlike other techniques such as Shamirs Secret Sharing (SSS) the entire private key is never revealed to any single participant and is therefore more secure as it does not have the trusted dealer problem.',
+  tags: ['MPC', 'Shared Custody'],
+  developer: 'Metamask',
+  website: 'https://tss.ac/',
+  auditUrls: ['auditUrl1', 'auditUrl2'],
+  version: '1.0.0',
+  lastUpdated: 'April 20, 2023',
+});
+
+export default function NewSnapAccountPage() {
+  const t = useI18nContext();
+  const [snapList, setSnapList] = useState(mockSnapList);
+  const history = useHistory();
+
+  return (
+    <Box>
+      <Box
+        display={DISPLAY.FLEX}
+        flexDirection={FLEX_DIRECTION.COLUMN}
+        alignItems={AlignItems.center}
+        justifyContent={JustifyContent.center}
+        marginTop={11}
+        marginBottom={11}
+      >
+        <Text variant={TextVariant.headingLg}>
+          {t('snapCreateAccountTitle')}
+        </Text>
+        <Text variant={TextVariant.bodyMd} color={TextColor.textAlternative}>
+          {t('snapCreateAccountSubtitle')}
+        </Text>
+      </Box>
+      <Box display={DISPLAY.FLEX} flexWrap={FLEX_WRAP.WRAP} gap={4}>
+        {snapList.map((snap, index: number) => {
+          const mockInstalled = Math.round(Math.random()) === 0;
+          return (
+            <SnapCard
+              {...snap}
+              key={index}
+              updateAvailable={mockInstalled}
+              isInstalled={mockInstalled}
+              onClickFunc={() => {
+                history.push(`/add-snap-account/${snap.id}`);
+              }}
+            />
+          );
+        })}
+      </Box>
+    </Box>
+  );
+}
