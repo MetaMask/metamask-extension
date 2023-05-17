338f4927d05a8052aa1326b223fb350e46141f3b
Dan J Miller <danjm.com@gmail.com>
2023-05-17 15:44:52 -0230

    Add -m flag to yarn ganache:start, which will run ganache with the --fork flag, using your local infura project id to fork mainnet

--- development/run-ganache.sh
+++ development/run-ganache.sh
@@ -22,8 +22,30 @@ _int () {
 trap _term SIGTERM
 trap _int SIGINT
 
-# shellcheck disable=SC2086
-$ganache_cli --chain.vmErrorsOnRPCResponse false --networkId 1337 --mnemonic "$seed_phrase" ${GANACHE_ARGS:-} &
+mainnet_flag='false'
+while getopts 'm' flag; do
+  case "${flag}" in
+    m) mainnet_flag='true' ;;
+    *) error "Unexpected option ${flag}" ;;
+  esac
+done
+
+if [[ "$mainnet_flag" == 'true' ]]
+then
+  infura_project_id=''
+  while IFS== read -r key value; do
+    if [[ "$key" == 'INFURA_PROJECT_ID' ]]
+    then
+      infura_project_id="$value"
+    fi
+  done < .metamaskrc
+  echo "$infura_project_id"
+  # shellcheck disable=SC2086
+  $ganache_cli --fork https://mainnet.infura.io/v3/${infura_project_id} --chain.chainId 1 &
+else
+  # shellcheck disable=SC2086
+  $ganache_cli --chain.vmErrorsOnRPCResponse false --networkId 1337 --mnemonic "$seed_phrase" ${GANACHE_ARGS:-} &
+fi
 
 child=$!
 wait "$child"
