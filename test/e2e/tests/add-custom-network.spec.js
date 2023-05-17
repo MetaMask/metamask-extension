99a2f831d4c050d78fae1957a119693d381320de
David Walsh <davidwalsh83@gmail.com>
2023-05-17 18:25:47 -0500

    E2E: Fix add-custom-network.spec.js

--- test/e2e/tests/add-custom-network.spec.js
+++ test/e2e/tests/add-custom-network.spec.js
@@ -1,6 +1,11 @@
 const { strict: assert } = require('assert');
 const FixtureBuilder = require('../fixture-builder');
-const { convertToHexValue, withFixtures, openDapp } = require('../helpers');
+const {
+  convertToHexValue,
+  withFixtures,
+  openDapp,
+  regularDelayMs,
+} = require('../helpers');
 
 describe('Custom network', function () {
   const chainID = '42161';
@@ -232,25 +237,28 @@ describe('Custom network', function () {
         title: this.test.title,
       },
       async ({ driver }) => {
+        console.log('gets here -1');
         await driver.navigate();
         await driver.fill('#password', 'correct horse battery staple');
         await driver.press('#password', driver.Key.ENTER);
 
-        await driver.clickElement('.account-menu__icon');
-        await driver.clickElement({ tag: 'div', text: 'Settings' });
+        // Avoid a stale element error
+        await driver.delay(regularDelayMs);
 
-        await driver.clickElement('.network-display');
+        await driver.clickElement('[data-testid="network-display"]');
+
         await driver.clickElement({ tag: 'button', text: 'Add network' });
-
         await driver.clickElement({
           tag: 'button',
           text: 'Add',
         });
+
         // verify network details
         const title = await driver.findElement({
           tag: 'h6',
           text: 'Arbitrum One',
         });
+
         assert.equal(
           await title.getText(),
           'Arbitrum One',
@@ -298,7 +306,7 @@ describe('Custom network', function () {
         });
         // verify network switched
         const networkDisplayed = await driver.findElement({
-          tag: 'span',
+          tag: 'p',
           text: 'Arbitrum One',
         });
         assert.equal(
@@ -322,10 +330,10 @@ describe('Custom network', function () {
         await driver.fill('#password', 'correct horse battery staple');
         await driver.press('#password', driver.Key.ENTER);
 
-        await driver.clickElement('.account-menu__icon');
-        await driver.clickElement({ tag: 'div', text: 'Settings' });
+        // Avoid a stale element error
+        await driver.delay(regularDelayMs);
 
-        await driver.clickElement('.network-display');
+        await driver.clickElement('[data-testid="network-display"]');
         await driver.clickElement({ tag: 'button', text: 'Add network' });
 
         // had to put all Add elements in list since list is changing and networks are not always in same order
@@ -342,11 +350,13 @@ describe('Custom network', function () {
         });
 
         // verify if added network is in list of networks
-        const networkDisplay = await driver.findElement('.network-display');
+        const networkDisplay = await driver.findElement(
+          '[data-testid="network-display"]',
+        );
         await networkDisplay.click();
 
         const arbitrumNetwork = await driver.findElements({
-          text: `Arbitrum One`,
+          text: 'Arbitrum One',
           tag: 'span',
         });
         assert.ok(arbitrumNetwork.length, 1);
@@ -378,12 +388,14 @@ describe('Custom network', function () {
         await driver.fill('#password', 'correct horse battery staple');
         await driver.press('#password', driver.Key.ENTER);
 
-        await driver.clickElement('.account-menu__icon');
+        await driver.clickElement(
+          '[data-testid="account-options-menu-button"]',
+        );
         await driver.clickElement({ text: 'Settings', tag: 'div' });
         await driver.clickElement({ text: 'Networks', tag: 'div' });
 
         const arbitrumNetwork = await driver.clickElement({
-          text: `Arbitrum One`,
+          text: 'Arbitrum One',
           tag: 'div',
         });
 
