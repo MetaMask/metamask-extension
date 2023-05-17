6d7734e7c0ea69a910730789d41772d30734280e
David Walsh <davidwalsh83@gmail.com>
2023-05-17 18:02:45 -0500

    E2E: Fix settings-general.spec.js

--- test/e2e/tests/settings-general.spec.js
+++ test/e2e/tests/settings-general.spec.js
@@ -26,7 +26,9 @@ describe('Settings', function () {
         await driver.press('#password', driver.Key.ENTER);
 
         // goes to the settings screen
-        await driver.clickElement('.account-menu__icon');
+        await driver.clickElement(
+          '[data-testid="account-options-menu-button"]',
+        );
         await driver.clickElement({ text: 'Settings', tag: 'div' });
 
         // finds the jazzicon toggle turned on
