d247a8a26c46da63e56adbf2f8e6a2da23ef0934
Binij Shrestha <shresthabinij@gmail.com>
2023-05-18 19:41:25 +0545

    resolve deprecated

--- ui/components/component-library/button-base/button-base.stories.tsx
+++ ui/components/component-library/button-base/button-base.stories.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { StoryFn, Meta } from '@storybook/react';
 import {
   AlignItems,
   BackgroundColor,
-  DISPLAY,
-  FLEX_DIRECTION,
+  Display,
+  FlexDirection,
 } from '../../../helpers/constants/design-system';
 import Box from '../../ui/box/box';
 import { TextDirection, IconName, ValidTag } from '..';
@@ -96,18 +96,18 @@ export default {
   args: {
     children: 'Button Base',
   },
-} as ComponentMeta<typeof ButtonBase>;
+} as Meta<typeof ButtonBase>;
 
-export const DefaultStory: ComponentStory<typeof ButtonBase> = (args) => (
+export const DefaultStory: StoryFn<typeof ButtonBase> = (args) => (
   <ButtonBase {...args} />
 );
 
 DefaultStory.storyName = 'Default';
 
-export const SizeStory: ComponentStory<typeof ButtonBase> = (args) => (
+export const SizeStory: StoryFn<typeof ButtonBase> = (args) => (
   <>
     <Box
-      display={DISPLAY.FLEX}
+      display={Display.Flex}
       alignItems={AlignItems.baseline}
       gap={1}
       marginBottom={2}
@@ -127,7 +127,7 @@ export const SizeStory: ComponentStory<typeof ButtonBase> = (args) => (
 
 SizeStory.storyName = 'Size';
 
-export const Block: ComponentStory<typeof ButtonBase> = (args) => (
+export const Block: StoryFn<typeof ButtonBase> = (args) => (
   <>
     <ButtonBase {...args} marginBottom={2}>
       Default Button
@@ -138,8 +138,8 @@ export const Block: ComponentStory<typeof ButtonBase> = (args) => (
   </>
 );
 
-export const As: ComponentStory<typeof ButtonBase> = (args) => (
-  <Box display={DISPLAY.FLEX} flexDirection={FLEX_DIRECTION.ROW} gap={2}>
+export const As: StoryFn<typeof ButtonBase> = (args) => (
+  <Box display={Display.Flex} flexDirection={FlexDirection.Row} gap={2}>
     <ButtonBase {...args}>Button Element</ButtonBase>
     <ButtonBase as={ValidTag.A} href="#" {...args}>
       Anchor Element
@@ -147,7 +147,7 @@ export const As: ComponentStory<typeof ButtonBase> = (args) => (
   </Box>
 );
 
-export const Href: ComponentStory<typeof ButtonBase> = (args) => (
+export const Href: StoryFn<typeof ButtonBase> = (args) => (
   <ButtonBase {...args}>Anchor Element</ButtonBase>
 );
 
@@ -155,7 +155,7 @@ Href.args = {
   href: '/metamask',
 };
 
-export const ExternalLink: ComponentStory<typeof ButtonBase> = (args) => (
+export const ExternalLink: StoryFn<typeof ButtonBase> = (args) => (
   <ButtonBase {...args}>Anchor element with external link</ButtonBase>
 );
 
@@ -164,7 +164,7 @@ ExternalLink.args = {
   externalLink: true,
 };
 
-export const Disabled: ComponentStory<typeof ButtonBase> = (args) => (
+export const Disabled: StoryFn<typeof ButtonBase> = (args) => (
   <ButtonBase {...args}>Disabled Button</ButtonBase>
 );
 
@@ -172,7 +172,7 @@ Disabled.args = {
   disabled: true,
 };
 
-export const Loading: ComponentStory<typeof ButtonBase> = (args) => (
+export const Loading: StoryFn<typeof ButtonBase> = (args) => (
   <ButtonBase {...args}>Loading Button</ButtonBase>
 );
 
@@ -180,20 +180,20 @@ Loading.args = {
   loading: true,
 };
 
-export const StartIconName: ComponentStory<typeof ButtonBase> = (args) => (
+export const StartIconName: StoryFn<typeof ButtonBase> = (args) => (
   <ButtonBase {...args} startIconName={IconName.AddSquare}>
     Button
   </ButtonBase>
 );
 
-export const EndIconName: ComponentStory<typeof ButtonBase> = (args) => (
+export const EndIconName: StoryFn<typeof ButtonBase> = (args) => (
   <ButtonBase {...args} endIconName={IconName.Arrow2Right}>
     Button
   </ButtonBase>
 );
 
-export const Rtl: ComponentStory<typeof ButtonBase> = (args) => (
-  <Box display={DISPLAY.FLEX} flexDirection={FLEX_DIRECTION.COLUMN} gap={2}>
+export const Rtl: StoryFn<typeof ButtonBase> = (args) => (
+  <Box display={Display.Flex} flexDirection={FlexDirection.Column} gap={2}>
     <ButtonBase
       {...args}
       startIconName={IconName.AddSquare}
@@ -212,7 +212,7 @@ export const Rtl: ComponentStory<typeof ButtonBase> = (args) => (
   </Box>
 );
 
-export const Ellipsis: ComponentStory<typeof ButtonBase> = (args) => (
+export const Ellipsis: StoryFn<typeof ButtonBase> = (args) => (
   <Box backgroundColor={BackgroundColor.primaryMuted} style={{ width: 150 }}>
     <ButtonBase {...args}>Example without ellipsis</ButtonBase>
     <ButtonBase {...args} ellipsis>
