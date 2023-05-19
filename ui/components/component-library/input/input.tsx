9e3ac52c5fd050baf18c9b6c77cb191aef80f411
Priyanshu <pb.21u10029@btech.nitdgp.ac.in>
2023-05-19 11:37:53 +0530

    Migrated the PropTypes to an interface InputProps

--- ui/components/component-library/input/input.tsx
+++ ui/components/component-library/input/input.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
 import {
@@ -8,13 +8,15 @@ import {
   BorderStyle,
 } from '../../../helpers/constants/design-system';
 
-import Box from '../../ui/box';
+// import Box from '../../ui/box';
 
 import { Text } from '../text';
 
-import { InputType } from './input.types';
+import {InputProps, InputType} from "./input.types"
 
-export const Input = React.forwardRef(
+// import { InputType } from './input.types';
+
+export const Input = forwardRef(
   (
     {
       autoComplete,
@@ -32,13 +34,13 @@ export const Input = React.forwardRef(
       placeholder,
       readOnly,
       required,
-      type = 'text',
+      type = InputType.Text,
       value,
       textVariant = TextVariant.bodyMd,
       disableStateStyles,
       ...props
-    },
-    ref,
+    }: InputProps,
+    ref: Ref<HTMLElement>,
   ) => (
     <Text
       className={classnames(
@@ -76,95 +78,5 @@ export const Input = React.forwardRef(
     />
   ),
 );
-
-Input.propTypes = {
-  /**
-   * Autocomplete allows the browser to predict the value based on earlier typed values
-   */
-  autoComplete: PropTypes.bool,
-  /**
-   * If `true`, the input will be focused during the first mount.
-   */
-  autoFocus: PropTypes.bool,
-  /**
-   * An additional className to apply to the input
-   */
-  className: PropTypes.string,
-  /**
-   * The default input value, useful when not controlling the component.
-   */
-  defaultValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  /**
-   * If `true`, the input will be disabled.
-   */
-  disabled: PropTypes.bool,
-  /**
-   * Disables focus state by setting CSS outline: none;
-   * !!IMPORTANT!!
-   * If this is set to true ensure there is a proper fallback
-   * to enable accessibility for keyboard only and vision impaired users
-   */
-  disableStateStyles: PropTypes.bool,
-  /**
-   * If `true`, aria-invalid will be true
-   */
-  error: PropTypes.bool,
-  /**
-   * The id of the `input` element.
-   */
-  id: PropTypes.string,
-  /**
-   * Max number of characters to allow
-   */
-  maxLength: PropTypes.number,
-  /**
-   * Name attribute of the `input` element.
-   */
-  name: PropTypes.string,
-  /**
-   * Callback fired on blur
-   */
-  onBlur: PropTypes.func,
-  /**
-   * Callback fired when the value is changed.
-   */
-  onChange: PropTypes.func,
-  /**
-   * Callback fired on focus
-   */
-  onFocus: PropTypes.func,
-  /**
-   * The short hint displayed in the input before the user enters a value.
-   */
-  placeholder: PropTypes.string,
-  /**
-   * It prevents the user from changing the value of the field (not from interacting with the field).
-   */
-  readOnly: PropTypes.bool,
-  /**
-   * If `true`, the input will be required. Currently no visual difference is shown.
-   */
-  required: PropTypes.bool,
-  /**
-   * Use this to override the text variant of the Text component.
-   * Should only be used for approved custom input components
-   * Use the TextVariant enum
-   */
-  textVariant: PropTypes.oneOf(Object.values(TextVariant)),
-  /**
-   * Type of the input element. Can be InputType.Text, InputType.Password, InputType.Number
-   * Defaults to InputType.Text ('text')
-   * If you require another type add it to InputType
-   */
-  type: PropTypes.oneOf(Object.values(InputType)),
-  /**
-   * The input value, required for a controlled component.
-   */
-  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  /**
-   * Input accepts all the props from Box
-   */
-  ...Box.propTypes,
-};
 
 Input.displayName = 'Input';
