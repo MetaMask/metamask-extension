dfb2c0e0e843e46f81a9452d05287cc296daaeff
Albert Olivé <albertolivecorbella@gmail.com>
2023-05-23 14:51:39 +0200

    [MMI] Added code fencing in app menu bar (#18069)
    * Added code fencing in app me nu bar
    
    * Fixed lint issues
    
    * Fixed yarn.lock
    
    * Fixed issue
    
    * Fixed tests
    
    * Fixed yarn.lock
    
    * Fixing policies
    
    * Fixing lavamoat
    
    * fixed yarn.lock
    
    * Fixed storybook
    
    * clean up and adds test
    
    * lint
    
    * typo
    
    * adds test
    
    ---------
    
    Co-authored-by: Antonio Regadas <antonio.regadas@consensys.net>

--- ui/components/app/menu-bar/menu-bar.test.js
+++ ui/components/app/menu-bar/menu-bar.test.js
@@ -4,11 +4,14 @@ import { fireEvent, screen, waitFor } from '@testing-library/react';
 import { renderWithProvider } from '../../../../test/lib/render-helpers';
 import { CHAIN_IDS } from '../../../../shared/constants/network';
 import { KeyringType } from '../../../../shared/constants/keyring';
+import mockState from '../../../../test/data/mock-state.json';
 import MenuBar from './menu-bar';
 
 const initState = {
+  ...mockState,
   activeTab: {},
   metamask: {
+    ...mockState.metamask,
     providerConfig: {
       chainId: CHAIN_IDS.GOERLI,
     },
@@ -63,5 +66,77 @@ describe('MenuBar', () => {
     fireEvent.doubleClick(accountOptionsButton);
 
     expect(accountOptionsMenu).not.toBeInTheDocument();
+  });
+
+  it('shows a custodial account detail', async () => {
+    const customState = {
+      ...mockState,
+      activeTab: {},
+      metamask: {
+        ...mockState.metamask,
+        networkConfigurations: {},
+
+        providerConfig: {
+          type: 'test',
+          chainId: '1',
+        },
+        identities: {
+          '0x5Ab19e7091dD208F352F8E727B6DCC6F8aBB6275': {
+            name: 'Custody Account A',
+            address: '0x5Ab19e7091dD208F352F8E727B6DCC6F8aBB6275',
+          },
+        },
+        selectedAddress: '0x5Ab19e7091dD208F352F8E727B6DCC6F8aBB6275',
+        waitForConfirmDeepLinkDialog: '123',
+        keyrings: [
+          {
+            type: 'Custody',
+            accounts: ['0x5Ab19e7091dD208F352F8E727B6DCC6F8aBB6275'],
+          },
+        ],
+        custodyStatusMaps: '123',
+        custodyAccountDetails: {
+          '0x5Ab19e7091dD208F352F8E727B6DCC6F8aBB6275': {
+            custodianName: 'saturn',
+          },
+        },
+        custodianSupportedChains: {
+          '0x5Ab19e7091dD208F352F8E727B6DCC6F8aBB6275': {
+            supportedChains: ['1', '2'],
+            custodianName: 'saturn',
+          },
+        },
+        mmiConfiguration: {
+          portfolio: {
+            enabled: true,
+            url: 'https://dashboard.metamask-institutional.io',
+          },
+          custodians: [
+            {
+              type: 'saturn',
+              name: 'saturn',
+              apiUrl: 'https://saturn-custody.dev.metamask-institutional.io',
+              iconUrl: 'images/saturn.svg',
+              displayName: 'Saturn Custody',
+              production: true,
+              refreshTokenUrl: null,
+              isNoteToTraderSupported: false,
+              version: 1,
+            },
+          ],
+        },
+      },
+    };
+
+    const store = mockStore(customState);
+    renderWithProvider(<MenuBar />, store);
+
+    const accountOptions = screen.queryByTestId('account-options-menu-button');
+    fireEvent.click(accountOptions);
+
+    await waitFor(() => {
+      const custodyLogo = screen.queryByTestId('custody-logo');
+      expect(custodyLogo).toBeInTheDocument();
+    });
   });
 });
