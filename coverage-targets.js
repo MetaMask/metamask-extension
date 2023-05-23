bc68e0c0cbfda691f4465d300a2c74802393faac
Antonio Regadas <antonio.regadas@consensys.net>
2023-05-23 15:03:34 +0100

    yarn dedupe

--- coverage-targets.js
+++ coverage-targets.js
@@ -6,10 +6,10 @@
 // subset of files to check against these targets.
 module.exports = {
   global: {
-    lines: 71.12,
-    branches: 58.94,
-    statements: 70.58,
-    functions: 63.84,
+    lines: 70.93,
+    branches: 58.96,
+    statements: 70.36,
+    functions: 63.62,
   },
   transforms: {
     branches: 100,
