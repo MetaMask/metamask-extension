581b890782b1a357b78eca09794799201600a251
Nicholas Ellul <nicholas.ellul1@gmail.com>
2023-05-26 12:41:16 -0400

    Update phishing detection e2e test

--- test/e2e/tests/phishing-detection/phishing-detection.spec.js
+++ test/e2e/tests/phishing-detection/phishing-detection.spec.js
@@ -1,12 +1,13 @@
 const { strict: assert } = require('assert');
+
+const { convertToHexValue, withFixtures, openDapp } = require('../../helpers');
+const FixtureBuilder = require('../../fixture-builder');
 const {
-  convertToHexValue,
-  withFixtures,
-  openDapp,
   setupPhishingDetectionMocks,
-  mockPhishingDetection,
-} = require('../helpers');
-const FixtureBuilder = require('../fixture-builder');
+  METAMASK_HOTLIST_DIFF_URL,
+  METAMASK_STALELIST_URL,
+  BlockProvider,
+} = require('./helpers');
 
 describe('Phishing Detection', function () {
   const ganacheOptions = {
@@ -19,13 +20,32 @@ describe('Phishing Detection', function () {
     ],
   };
 
+  describe('Phishing Detection Mock', function () {
+    it('should be updated to use v1 of the API', function () {
+      // Update the fixture in phishing-detection/helpers.js if this test fails
+      assert.equal(
+        METAMASK_STALELIST_URL,
+        'https://phishing-detection.metafi.codefi.network/v1/stalelist',
+      );
+      assert.equal(
+        METAMASK_HOTLIST_DIFF_URL,
+        'https://phishing-detection.metafi.codefi.network/v1/diffsSince',
+      );
+    });
+  });
+
   it('should display the MetaMask Phishing Detection page and take the user to the blocked page if they continue', async function () {
     await withFixtures(
       {
         fixtures: new FixtureBuilder().build(),
         ganacheOptions,
         title: this.test.title,
-        testSpecificMock: mockPhishingDetection,
+        testSpecificMock: async (mockServer) => {
+          return setupPhishingDetectionMocks(mockServer, {
+            blockProvider: BlockProvider.MetaMask,
+            blocklist: ['127.0.0.1'],
+          });
+        },
         dapp: true,
         failOnConsoleError: false,
       },
@@ -44,12 +64,20 @@ describe('Phishing Detection', function () {
   });
 
   it('should display the MetaMask Phishing Detection page in an iframe and take the user to the blocked page if they continue', async function () {
+    const DAPP_WITH_IFRAMED_PAGE_ON_BLOCKLIST = 'http://localhost:8080/';
+    const IFRAMED_HOSTNAME = '127.0.0.1';
+
     await withFixtures(
       {
         fixtures: new FixtureBuilder().build(),
         ganacheOptions,
         title: this.test.title,
-        testSpecificMock: mockPhishingDetection,
+        testSpecificMock: async (mockServer) => {
+          return setupPhishingDetectionMocks(mockServer, {
+            blockProvider: BlockProvider.MetaMask,
+            blocklist: [IFRAMED_HOSTNAME],
+          });
+        },
         dapp: true,
         dappPaths: ['mock-page-with-iframe'],
         dappOptions: {
@@ -61,7 +89,7 @@ describe('Phishing Detection', function () {
         await driver.navigate();
         await driver.fill('#password', 'correct horse battery staple');
         await driver.press('#password', driver.Key.ENTER);
-        await driver.openNewPage('http://localhost:8080/');
+        await driver.openNewPage(DAPP_WITH_IFRAMED_PAGE_ON_BLOCKLIST);
 
         const iframe = await driver.findElement('iframe');
 
@@ -85,7 +113,12 @@ describe('Phishing Detection', function () {
         fixtures: new FixtureBuilder().build(),
         ganacheOptions,
         title: this.test.title,
-        testSpecificMock: mockPhishingDetection,
+        testSpecificMock: async (mockServer) => {
+          return setupPhishingDetectionMocks(mockServer, {
+            blockProvider: BlockProvider.MetaMask,
+            blocklist: ['127.0.0.1'],
+          });
+        },
         dapp: true,
         dappPaths: ['mock-page-with-disallowed-iframe'],
         dappOptions: {
@@ -118,7 +151,7 @@ describe('Phishing Detection', function () {
     );
   });
 
-  it('should navigate the user to eth-phishing-detect to dispute a block if the phishing warning page fails to identify the source', async function () {
+  it.skip('should navigate the user to eth-phishing-detect to dispute a block if the phishing warning page fails to identify the source', async function () {
     await withFixtures(
       {
         fixtures: new FixtureBuilder().build(),
@@ -139,7 +172,9 @@ describe('Phishing Detection', function () {
         await driver.clickElement({ text: 'report a detection problem.' });
 
         // wait for page to load before checking URL.
-        await driver.findElement({ text: 'Empty page' });
+        await driver.findElement({
+          text: `Empty page by ${BlockProvider.MetaMask}`,
+        });
         assert.equal(
           await driver.getCurrentUrl(),
           `https://github.com/MetaMask/eth-phishing-detect/issues/new?title=[Legitimate%20Site%20Blocked]%20127.0.0.1&body=http%3A%2F%2F127.0.0.1%3A8080%2F`,
@@ -149,50 +184,18 @@ describe('Phishing Detection', function () {
   });
 
   it('should navigate the user to eth-phishing-detect to dispute a block from MetaMask', async function () {
+    // Must be site on actual eth-phishing-detect blocklist
+    const phishingSite = new URL('https://test.metamask-phishing.io');
+
     await withFixtures(
       {
         fixtures: new FixtureBuilder().build(),
         ganacheOptions,
         title: this.test.title,
-        testSpecificMock: mockPhishingDetection,
-        dapp: true,
-        failOnConsoleError: false,
-      },
-      async ({ driver }) => {
-        await driver.navigate();
-        await driver.fill('#password', 'correct horse battery staple');
-        await driver.press('#password', driver.Key.ENTER);
-        await openDapp(driver);
-
-        await driver.clickElement({ text: 'report a detection problem.' });
-
-        // wait for page to load before checking URL.
-        await driver.findElement({ text: 'Empty page' });
-        assert.equal(
-          await driver.getCurrentUrl(),
-          `https://github.com/MetaMask/eth-phishing-detect/issues/new?title=[Legitimate%20Site%20Blocked]%20127.0.0.1&body=http%3A%2F%2F127.0.0.1%3A8080%2F`,
-        );
-      },
-    );
-  });
-
-  it('should navigate the user to PhishFort to dispute a block from MetaMask', async function () {
-    await withFixtures(
-      {
-        fixtures: new FixtureBuilder().build(),
-        ganacheOptions,
-        title: this.test.title,
-        testSpecificMock: (mockServer) => {
-          setupPhishingDetectionMocks(mockServer, {
-            statusCode: 200,
-            json: {
-              version: 2,
-              tolerance: 2,
-              fuzzylist: [],
-              whitelist: [],
-              blacklist: [],
-              lastUpdated: 0,
-            },
+        testSpecificMock: async (mockServer) => {
+          return setupPhishingDetectionMocks(mockServer, {
+            blockProvider: BlockProvider.MetaMask,
+            blocklist: [phishingSite.hostname],
           });
         },
         dapp: true,
@@ -202,12 +205,51 @@ describe('Phishing Detection', function () {
         await driver.navigate();
         await driver.fill('#password', 'correct horse battery staple');
         await driver.press('#password', driver.Key.ENTER);
-        await openDapp(driver);
+        await driver.openNewPage(phishingSite.href);
 
         await driver.clickElement({ text: 'report a detection problem.' });
 
         // wait for page to load before checking URL.
-        await driver.findElement({ text: 'Empty page' });
+        await driver.findElement({
+          text: `Empty page by ${BlockProvider.MetaMask}`,
+        });
+        assert.equal(
+          await driver.getCurrentUrl(),
+          `https://github.com/MetaMask/eth-phishing-detect/issues/new?title=[Legitimate%20Site%20Blocked]%20${encodeURIComponent(
+            phishingSite.hostname,
+          )}&body=${encodeURIComponent(phishingSite.href)}`,
+        );
+      },
+    );
+  });
+
+  it('should navigate the user to PhishFort to dispute a Phishfort Block', async function () {
+    await withFixtures(
+      {
+        fixtures: new FixtureBuilder().build(),
+        ganacheOptions,
+        title: this.test.title,
+        testSpecificMock: async (mockServer) => {
+          return setupPhishingDetectionMocks(mockServer, {
+            blockProvider: BlockProvider.PhishFort,
+            blocklist: ['127.0.0.1'],
+          });
+        },
+        dapp: true,
+        failOnConsoleError: false,
+      },
+      async ({ driver }) => {
+        await driver.navigate();
+        await driver.fill('#password', 'correct horse battery staple');
+        await driver.press('#password', driver.Key.ENTER);
+        await driver.openNewPage('http://127.0.0.1:8080');
+
+        await driver.clickElement({ text: 'report a detection problem.' });
+
+        // wait for page to load before checking URL.
+        await driver.findElement({
+          text: `Empty page by ${BlockProvider.PhishFort}`,
+        });
         assert.equal(
           await driver.getCurrentUrl(),
           `https://github.com/phishfort/phishfort-lists/issues/new?title=[Legitimate%20Site%20Blocked]%20127.0.0.1&body=http%3A%2F%2F127.0.0.1%3A8080%2F`,
@@ -222,7 +264,12 @@ describe('Phishing Detection', function () {
         fixtures: new FixtureBuilder().build(),
         ganacheOptions,
         title: this.test.title,
-        testSpecificMock: mockPhishingDetection,
+        testSpecificMock: async (mockServer) => {
+          return setupPhishingDetectionMocks(mockServer, {
+            blockProvider: BlockProvider.MetaMask,
+            blocklist: ['127.0.0.1'],
+          });
+        },
         dapp: true,
         dappPaths: ['mock-page-with-disallowed-iframe'],
         dappOptions: {
