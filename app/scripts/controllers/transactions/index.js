6974c9a8509f3ab8457323e90caaeabdc21e9997
Matthew Walsh <matthew.walsh@consensys.net>
2023-05-26 20:32:43 +0100

    Update approval controller patch
    Specify expectsResult in approval requests.

--- app/scripts/controllers/transactions/index.js
+++ app/scripts/controllers/transactions/index.js
@@ -2787,6 +2787,7 @@ export default class TransactionController extends EventEmitter {
         origin,
         type,
         requestData,
+        expectsResult: true,
       },
       shouldShowRequest,
     );
