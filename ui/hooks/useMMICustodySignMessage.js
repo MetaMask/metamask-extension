e8c9b49d4ce663d11a83afa7edd7b645d4a722b4
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2023-05-29 17:45:06 -0400

    adding new hook for the MMISign changes

---
+++ ui/hooks/useMMICustodySignMessage.js
@@ -0,0 +1,66 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { showCustodianDeepLink } from '@metamask-institutional/extension';
+import {
+  mmiActionsFactory,
+  setTypedMessageInProgress,
+} from '../store/institutional/institution-background';
+import {
+  accountsWithSendEtherInfoSelector,
+  getAccountType,
+} from '../selectors';
+import { getAccountByAddress } from '../helpers/utils/util';
+import { getEnvironmentType } from '../../app/scripts/lib/util';
+import { goHome, showModal } from '../store/actions';
+import { ENVIRONMENT_TYPE_NOTIFICATION } from '../../shared/constants/app';
+
+export async function useMMICustodySignMessage(...opts) {
+  const dispatch = useDispatch();
+  const mmiActions = mmiActionsFactory();
+  const envType = getEnvironmentType();
+  const accountType = useSelector(getAccountType);
+  const isNotification = envType === ENVIRONMENT_TYPE_NOTIFICATION;
+  const allAccounts = useSelector(accountsWithSendEtherInfoSelector);
+  const { address: fromAddress } =
+    getAccountByAddress(allAccounts, opts.params.from) || {};
+
+  if (accountType === 'custody') {
+    try {
+      let msgData = opts.params;
+      let id = msgData.custodyId;
+      if (!id) {
+        msgData = await opts.sign(opts.params);
+        id = msgData.custodyId;
+      }
+      dispatch(
+        showCustodianDeepLink({
+          dispatch,
+          mmiActions,
+          txId: undefined,
+          custodyId: id,
+          fromAddress,
+          isSignature: true,
+          closeNotification: isNotification,
+          onDeepLinkFetched: () => undefined,
+          onDeepLinkShown: () => undefined,
+        }),
+      );
+      await dispatch(setTypedMessageInProgress(msgData.metamaskId));
+      await dispatch(mmiActions.setWaitForConfirmDeepLinkDialog(true));
+      await dispatch(goHome());
+      return msgData;
+    } catch (err) {
+      await dispatch(mmiActions.setWaitForConfirmDeepLinkDialog(true));
+      await dispatch(
+        showModal({
+          name: 'TRANSACTION_FAILED',
+          errorMessage: err.message,
+          closeNotification: true,
+          operationFailed: true,
+        }),
+      );
+      return null;
+    }
+  }
+
+  return opts.sign(opts.params);
+}
