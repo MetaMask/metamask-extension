c0463cfd74ba3c984dc1d97fa03763ceec919d9e
Goktug Poyraz <goktug.poyraz@consensys.net>
2023-05-30 09:31:45 +0200

    add reject allow action

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -1263,7 +1263,10 @@ export default class MetamaskController extends EventEmitter {
     this.signatureController = new SignatureController({
       messenger: this.controllerMessenger.getRestricted({
         name: 'SignatureController',
-        allowedActions: [`${this.approvalController.name}:addRequest`],
+        allowedActions: [
+          `${this.approvalController.name}:addRequest`,
+          `${this.approvalController.name}:rejectRequest`,
+        ],
       }),
       keyringController: this.keyringController,
       isEthSignEnabled: () =>
