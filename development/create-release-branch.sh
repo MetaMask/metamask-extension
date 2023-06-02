943358f30657479fbc74d8ce6db045c12d8c76a7
Gauthier Petetin <gauthierpetetin@gmail.com>
2023-06-02 07:46:02 +0200

    fix(action): create branch but not the PR

--- development/create-release-branch.sh
+++ development/create-release-branch.sh
@@ -13,23 +13,12 @@ if [[ -z $NEW_VERSION ]]; then
 fi
 
 RELEASE_BRANCH_NAME="${RELEASE_BRANCH_PREFIX}${NEW_VERSION}"
-RELEASE_BODY="This is the release candidate for version ${NEW_VERSION}."
 
 git config user.name metamaskbot
 git config user.email metamaskbot@users.noreply.github.com
 
 git checkout -b "${RELEASE_BRANCH_NAME}"
 
-if ! (git add . && git commit -m "${NEW_VERSION}");
-then
-    echo "Error: No changes detected."
-    exit 1
-fi
+git add . && git commit --allow-empty -m "${NEW_VERSION}"
 
 git push --set-upstream origin "${RELEASE_BRANCH_NAME}"
-
-gh pr create \
-  --draft \
-  --title "${NEW_VERSION}" \
-  --body "${RELEASE_BODY}" \
-  --head "${RELEASE_BRANCH_NAME}";
