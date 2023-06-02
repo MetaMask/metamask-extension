2311f2a5ada2251c042d007a78129249a6f2679c
Alex <adonesky@gmail.com>
2023-06-02 12:32:35 -0500

    reverting error handling

--- ui/pages/confirm-add-suggested-nft/confirm-add-suggested-nft.js
+++ ui/pages/confirm-add-suggested-nft/confirm-add-suggested-nft.js
@@ -1,9 +1,8 @@
-import React, { useCallback, useContext, useEffect, useMemo } from 'react';
+import React, { useCallback, useContext, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { ethErrors, serializeError } from 'eth-rpc-errors';
 import { getTokenTrackerLink } from '@metamask/etherscan-link';
-import { Severity } from '@sentry/types';
 import classnames from 'classnames';
 import { PageContainerFooter } from '../../components/ui/page-container';
 import { I18nContext } from '../../contexts/i18n';
@@ -21,12 +20,10 @@ import {
 import { AssetType } from '../../../shared/constants/transaction';
 import {
   BUTTON_SIZES,
-  BannerAlert,
   ButtonIcon,
+  ButtonIconSize,
   ButtonLink,
-  Icon,
   IconName,
-  IconSize,
   Text,
 } from '../../components/component-library';
 import {
@@ -44,7 +41,6 @@ import {
   TextVariant,
 } from '../../helpers/constants/design-system';
 import Box from '../../components/ui/box/box';
-import Tooltip from '../../components/ui/tooltip';
 
 const ConfirmAddSuggestedNFT = () => {
   const t = useContext(I18nContext);
@@ -58,43 +54,9 @@ const ConfirmAddSuggestedNFT = () => {
   const ipfsGateway = useSelector(getIpfsGateway);
   const trackEvent = useContext(MetaMetricsContext);
 
-  const errorMap = useMemo(
-    () => ({
-      nftAlreadyWatchedError:
-        'The suggested NFT is already in the user’s wallet',
-      ownerFetchError: 'An error occurred while fetching the owner of the NFT',
-      wrongOwnerError: 'The user does not own the suggested NFT',
-    }),
-    [],
-  );
-
-  const disabled = suggestedNfts.every(({ requestData: { errors } }) =>
-    Object.values(errors).some((val) => val),
-  );
-
-  const errorsPresent = suggestedNfts.some(({ requestData: { errors } }) =>
-    Object.values(errors).some((val) => val),
-  );
-
   const handleAddNftsClick = useCallback(async () => {
     await Promise.all(
-      suggestedNfts.map(async ({ requestData: { asset, errors }, id }) => {
-        const errorKey = Object.entries(errors).find(
-          ([, v]) => v === true,
-        )?.[0];
-        if (errorKey) {
-          await dispatch(
-            rejectPendingApproval(
-              id,
-              serializeError(
-                new Error(
-                  `${errorMap[errorKey]}. Contract Address:${asset.address} TokenId:${asset.tokenId}`,
-                ),
-              ),
-            ),
-          );
-          return;
-        }
+      suggestedNfts.map(async ({ requestData: { asset }, id }) => {
         await dispatch(resolvePendingApproval(id, null));
 
         trackEvent({
@@ -112,34 +74,11 @@ const ConfirmAddSuggestedNFT = () => {
       }),
     );
     history.push(mostRecentOverviewPage);
-  }, [
-    dispatch,
-    history,
-    trackEvent,
-    mostRecentOverviewPage,
-    suggestedNfts,
-    errorMap,
-  ]);
+  }, [dispatch, history, trackEvent, mostRecentOverviewPage, suggestedNfts]);
 
   const handleCancelNftClick = useCallback(async () => {
     await Promise.all(
-      suggestedNfts.map(async ({ id, requestData: { errors, asset } }) => {
-        const errorKey = Object.entries(errors).find(
-          ([, v]) => v === true,
-        )?.[0];
-        if (errorKey) {
-          return await dispatch(
-            rejectPendingApproval(
-              id,
-              serializeError(
-                new Error(
-                  `${errorMap[errorKey]}. ContractAddress:${asset.address}, TokenId:${asset.tokenId}`,
-                ),
-              ),
-            ),
-          );
-        }
-
+      suggestedNfts.map(async ({ id }) => {
         return dispatch(
           rejectPendingApproval(
             id,
@@ -149,7 +88,7 @@ const ConfirmAddSuggestedNFT = () => {
       }),
     );
     history.push(mostRecentOverviewPage);
-  }, [dispatch, history, mostRecentOverviewPage, suggestedNfts, errorMap]);
+  }, [dispatch, history, mostRecentOverviewPage, suggestedNfts]);
 
   const goBackIfNoSuggestedNftsOnFirstRender = () => {
     if (!suggestedNfts.length) {
@@ -170,15 +109,6 @@ const ConfirmAddSuggestedNFT = () => {
   return (
     <div className="page-container">
       <div className="confirm-add-suggested-nft__header">
-        {errorsPresent && (
-          <BannerAlert
-            severity={Severity.Warning}
-            className="confirm-add-suggested-nft__banner-alert"
-          >
-            We are unable to add some or all of the suggested NFTs. Please check
-            the error messages below for details.
-          </BannerAlert>
-        )}
         <Text
           variant={TextVariant.headingLg}
           textAlign={TextAlign.Center}
@@ -210,14 +140,10 @@ const ConfirmAddSuggestedNFT = () => {
                 id,
                 requestData: {
                   asset: { address, tokenId, symbol, image, name },
-                  errors,
                 },
               }) => {
                 const nftImageURL = getAssetImageURL(image, ipfsGateway);
-                const errorKey = Object.entries(errors).find(
-                  ([, val]) => val === true,
-                )?.[0];
-                const blockExplorerLink = getTokenTrackerLink(
+                let blockExplorerLink = getTokenTrackerLink(
                   address,
                   chainId,
                   null,
@@ -226,6 +152,10 @@ const ConfirmAddSuggestedNFT = () => {
                     blockExplorerUrl: rpcPrefs?.blockExplorerUrl ?? null,
                   },
                 );
+                if (blockExplorerLink.includes('etherscan')) {
+                  // this will only work for etherscan
+                  blockExplorerLink = `${blockExplorerLink}?a=${tokenId}`;
+                }
                 if (suggestedNfts.length === 1) {
                   return (
                     <Box
@@ -252,8 +182,7 @@ const ConfirmAddSuggestedNFT = () => {
                           {rpcPrefs.blockExplorerUrl ? (
                             <ButtonLink
                               className="confirm-add-suggested-nft__nft-name"
-                              // this will only work for etherscan
-                              href={`${blockExplorerLink}?a=${tokenId}`}
+                              href={blockExplorerLink}
                               title={address}
                               target="_blank"
                               size={BUTTON_SIZES.INHERIT}
@@ -276,29 +205,6 @@ const ConfirmAddSuggestedNFT = () => {
                             #{tokenId}
                           </Text>
                         </div>
-                        {errorKey && (
-                          <Tooltip
-                            interactive
-                            position="right"
-                            trigger="mouseenter"
-                            title={errorMap[errorKey]}
-                          >
-                            <Icon
-                              name={
-                                errorKey === 'nftAlreadyWatchedError'
-                                  ? IconName.Check
-                                  : IconName.Danger
-                              }
-                              color={
-                                errorKey === 'nftAlreadyWatchedError'
-                                  ? IconColor.successDefault
-                                  : IconColor.warningDefault
-                              }
-                              size={IconSize.Lg}
-                              style={{ margin: '0, 12px, 0, 8px' }}
-                            />
-                          </Tooltip>
-                        )}
                       </div>
                     </Box>
                   );
@@ -311,10 +217,6 @@ const ConfirmAddSuggestedNFT = () => {
                     <div
                       className={classnames(
                         'confirm-add-suggested-nft__nft-details',
-                        {
-                          'confirm-add-suggested-nft__nft-details--error':
-                            errorKey,
-                        },
                       )}
                     >
                       {nftImageURL ? (
@@ -330,8 +232,7 @@ const ConfirmAddSuggestedNFT = () => {
                         {rpcPrefs.blockExplorerUrl ? (
                           <ButtonLink
                             className="confirm-add-suggested-nft__nft-name"
-                            // this will only work for etherscan
-                            href={`${blockExplorerLink}?a=${tokenId}`}
+                            href={blockExplorerLink}
                             title={address}
                             target="_blank"
                             size={BUTTON_SIZES.INHERIT}
@@ -355,49 +256,24 @@ const ConfirmAddSuggestedNFT = () => {
                         </Text>
                       </div>
                     </div>
-                    {errorKey ? (
-                      <Tooltip
-                        interactive
-                        position="right"
-                        trigger="mouseenter"
-                        title={errorMap[errorKey]}
-                      >
-                        <Icon
-                          name={
-                            errorKey === 'nftAlreadyWatchedError'
-                              ? IconName.Check
-                              : IconName.Danger
-                          }
-                          color={
-                            errorKey === 'nftAlreadyWatchedError'
-                              ? IconColor.successDefault
-                              : IconColor.warningDefault
-                          }
-                          size={IconSize.Lg}
-                          style={{ margin: '0, 12px, 0, 8px' }}
-                        />
-                      </Tooltip>
-                    ) : (
-                      <ButtonIcon
-                        className="confirm-add-suggested-nft__nft-remove"
-                        iconName={IconName.Close}
-                        IconSize={IconSize.Xs}
-                        color={IconColor.warningDefault}
-                        disabled={errorKey}
-                        onClick={(e) => {
-                          e.preventDefault();
-                          e.stopPropagation();
-                          dispatch(
-                            rejectPendingApproval(
-                              id,
-                              serializeError(
-                                ethErrors.provider.userRejectedRequest(),
-                              ),
+                    <ButtonIcon
+                      className="confirm-add-suggested-nft__nft-remove"
+                      iconName={IconName.Close}
+                      size={ButtonIconSize.Sm}
+                      color={IconColor.iconMuted}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        e.stopPropagation();
+                        dispatch(
+                          rejectPendingApproval(
+                            id,
+                            serializeError(
+                              ethErrors.provider.userRejectedRequest(),
                             ),
-                          );
-                        }}
-                      />
-                    )}
+                          ),
+                        );
+                      }}
+                    />
                   </div>
                 );
               },
@@ -410,7 +286,6 @@ const ConfirmAddSuggestedNFT = () => {
         submitText={suggestedNfts.length === 1 ? t('addNft') : t('addNfts')}
         onCancel={handleCancelNftClick}
         onSubmit={handleAddNftsClick}
-        disabled={disabled}
       />
     </div>
   );
