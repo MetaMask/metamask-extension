fd3f98c7648d257be5bb2d7dc1d2f79a2c6753a8
Dhruv Parmar <dhruvv173@gmail.com>
2023-06-05 22:47:56 +0530

    Creating story for asset-list component

---
+++ ui/components/app/asset-list/asset-list.stories.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import AssetList from '.';
+
+export default {
+  title: 'Components/App/AssetList',
+
+  argTypes: {
+    onClickAsset: {
+      control: 'onClickAsset',
+    },
+  },
+  args: {
+    onClickAsset: () => console.log('onClickAsset fired')
+  },
+};
+
+export const DefaultStory = (args) => <AssetList {...args} />;
+
+DefaultStory.storyName = 'Default';
