766c958d47c6692abcc36843aa7bb1213055031a
Dhruv Parmar <dhruvv173@gmail.com>
2023-06-07 21:12:03 +0530

    Creating stories

---
+++ ui/components/ui/loading-heartbeat/loading-heartbeat.stories.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import LoadingHeartBeat from './index';
+
+export default {
+  title: 'Components/UI/LoadingHeartBeat',
+  component: LoadingHeartBeat,
+  argTypes: {
+    backgroundColor: {
+      control: 'text',
+    },
+    estimateUsed: {
+      control: 'text',
+    },
+  },
+  args: {
+    backgroundColor: 'blue',
+    estimateUsed: '',
+  },
+};
+
+export const DefaultStory = (args) => <LoadingHeartBeat {...args} />;
+
+DefaultStory.storyName = 'Default';
