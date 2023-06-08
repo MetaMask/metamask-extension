e7c7fda7b32cd091b99b25615d741a9b91be0f07
Rob Turner <rob@flexadata.com>
2023-06-09 08:35:49 +1000

    fix linting errors

--- app/scripts/controllers/permissions/snaps/__mocks__/@metamask/rpc-methods.js
+++ app/scripts/controllers/permissions/snaps/__mocks__/@metamask/rpc-methods.js
@@ -1,1 +1,5 @@
-module.exports = jest.requireActual('@metamask/rpc-methods-flask');
+// eslint-disable-next-line no-undef
+const actual = jest.requireActual('@metamask/rpc-methods-flask');
+export default actual;
+
+// module.exports = jest.requireActual('@metamask/rpc-methods-flask');
--- app/scripts/controllers/permissions/snaps/__mocks__/@metamask/snaps-controllers.js
+++ app/scripts/controllers/permissions/snaps/__mocks__/@metamask/snaps-controllers.js
@@ -1,1 +1,5 @@
-module.exports = jest.requireActual('@metamask/snaps-controllers-flask');
+// eslint-disable-next-line no-undef
+const actual = jest.requireActual('@metamask/snaps-controllers-flask');
+export default actual;
+
+// module.exports = jest.requireActual('@metamask/snaps-controllers-flask');
