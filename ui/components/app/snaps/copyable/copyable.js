875fc81bce8afb31c36105cc6eae89251bf405f0
Dhruv Parmar <dhruvv173@gmail.com>
2023-06-08 20:49:39 +0530

    Replacing deprecated constants

--- ui/components/app/snaps/copyable/copyable.js
+++ ui/components/app/snaps/copyable/copyable.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Box from '../../../ui/box';
-import Typography from '../../../ui/typography';
 import {
   AlignItems,
   BorderRadius,
   JustifyContent,
-  OVERFLOW_WRAP,
-  FLEX_DIRECTION,
-  TypographyVariant,
+  OverflowWrap,
+  FlexDirection,
+  TextVariant,
   BackgroundColor,
   TextColor,
   Color,
 } from '../../../../helpers/constants/design-system';
 import { useCopyToClipboard } from '../../../../hooks/useCopyToClipboard';
-import { Icon, IconName, IconSize } from '../../../component-library';
+import { Icon, IconName, IconSize, Text } from '../../../component-library';
 
 export const Copyable = ({ text }) => {
   const [copied, handleCopy] = useCopyToClipboard();
@@ -30,16 +29,17 @@ export const Copyable = ({ text }) => {
       paddingTop={2}
       paddingBottom={2}
     >
-      <Typography
-        variant={TypographyVariant.H6}
+      <Text
+        variant={TextVariant.bodySm}
+        as="h6"
         color={TextColor.textAlternative}
         marginRight={2}
-        overflowWrap={OVERFLOW_WRAP.ANYWHERE}
+        overflowWrap={OverflowWrap.Anywhere}
       >
         {text}
-      </Typography>
+      </Text>
       <Box
-        flexDirection={FLEX_DIRECTION.COLUMN}
+        flexDirection={FlexDirection.Column}
         alignItems={AlignItems.center}
         justifyContent={JustifyContent.flexStart}
         marginTop={2}
