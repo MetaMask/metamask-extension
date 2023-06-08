1dc34b7c65a1e7d3a22cb6474a446288a812e3e1
Monte Lai <monte.lai@consensys.net>
2023-06-08 22:34:08 +0800

    update to use modal instead of popup

--- ui/components/app/add-snap-account-popup/add-snap-account-popup.tsx
+++ ui/components/app/add-snap-account-popup/add-snap-account-popup.tsx
@@ -12,56 +12,57 @@ import {
   Text,
   Button,
   BUTTON_VARIANT,
-  PopoverHeader,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
 } from '../../component-library';
 import Box from '../../ui/box';
 import { useI18nContext } from '../../../hooks/useI18nContext';
 
-export default function AddSnapAccountPopup({
+export default function AddSnapAccountModa({
   onClose,
+  isOpen,
 }: {
+  isOpen: boolean;
   onClose: () => void;
 }) {
   const t = useI18nContext();
 
   return (
-    <Popover
-      className="add-snap-account-popup"
-      footerProps={{
-        justifyContent: AlignItems.center,
-        flexDirection: FlexDirection.Column,
-      }}
-      footer={
-        <>
+    <Modal className="add-snap-account-popup" isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader onClose={onClose} margin={[4, 4, 4, 4]}>
+          {t('addSnapAccountPopupTitle')}
+        </ModalHeader>
+        <Box
+          display={Display.Flex}
+          padding={[4, 4, 4, 4]}
+          flexDirection={FlexDirection.Column}
+          justifyContent={JustifyContent.center}
+          alignItems={AlignItems.center}
+        >
+          <Box marginBottom={4}>
+            <img src="/images/add-snaps-image.svg" />
+          </Box>
+          <Text
+            variant={TextVariant.bodyLgMedium}
+            textAlign={TextAlign.Center}
+            marginBottom={4}
+          >
+            {t('addSnapAccountPopupDescription')}
+          </Text>
           <Button
             variant={BUTTON_VARIANT.PRIMARY}
             block
             className="get-started_button"
-            // onClick={onAccept}
             data-testid="get-started-button"
           >
             {t('getStarted')}
           </Button>
-        </>
-      }
-    >
-      <PopoverHeader onClose={onClose} margin={[4, 4, 4, 4]}>
-        {t('addSnapAccountPopupTitle')}
-      </PopoverHeader>
-      <Box
-        display={Display.Flex}
-        padding={[4, 4, 4, 4]}
-        flexDirection={FlexDirection.Column}
-        justifyContent={JustifyContent.center}
-        alignItems={AlignItems.center}
-      >
-        <Box marginBottom={4}>
-          <img src="/images/add-snaps-image.svg" />
         </Box>
-        <Text variant={TextVariant.bodyLgMedium} textAlign={TextAlign.Center}>
-          {t('addSnapAccountPopupDescription')}
-        </Text>
-      </Box>
-    </Popover>
+      </ModalContent>
+    </Modal>
   );
 }
