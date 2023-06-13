4fb23709a67e6800b7357affaf9686f0a774598d
Monte Lai <monte.lai@consensys.net>
2023-06-13 22:36:05 +0800

    remove export of DeferredPromise

--- app/scripts/lib/util.ts
+++ app/scripts/lib/util.ts
@@ -180,7 +180,7 @@ export const isValidDate = (d: Date | number) => {
  * @property {() => void} reject - A function that rejects the Promise.
  */
 
-export interface DeferredPromise {
+interface DeferredPromise {
   promise: Promise<any>;
   resolve?: () => void;
   reject?: () => void;
