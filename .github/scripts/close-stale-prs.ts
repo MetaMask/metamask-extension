ffda0a78c577d0ae8c6cd8221093fcc989173435
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2023-06-15 12:13:50 +0100

    close stale prs

--- .github/scripts/close-stale-prs.ts
+++ .github/scripts/close-stale-prs.ts
@@ -2,7 +2,7 @@ import * as core from '@actions/core';
 import { context, getOctokit } from '@actions/github';
 import { GitHub } from '@actions/github/lib/utils';
 
-const MAX_DAYS_OF_INACTIVITY = 30;
+const MAX_DAYS_OF_INACTIVITY = 60;
 
 main().catch((error: Error): void => {
   console.error(error);
@@ -23,8 +23,6 @@ async function main(): Promise<void> {
     const stalenessThreshold = Number(new Date()) - MAX_DAYS_OF_INACTIVITY * 24 * 60 * 60 * 1000;
     const formattedThreshold = convertDateFormat(stalenessThreshold);
 
-    console.log({ formattedThreshold });
-
     const searchQuery = `is:open is:pr repo:${context.repo.owner}/${context.repo.repo} updated:<${formattedThreshold}`;
 
     const response = await octokit.rest.search.issuesAndPullRequests({ q: searchQuery });
@@ -41,12 +39,16 @@ async function main(): Promise<void> {
 }
 
 async function commentAndClosePR(octokit: InstanceType<typeof GitHub>, prNumber: number): Promise<void> {
+  const commentBody = `Thank you for your contribution to MetaMask Extension. In order to maintain a clean and relevant PR queue,
+we close all PRs after ${MAX_DAYS_OF_INACTIVITY} days of inactivity.
+Please reopen this PR once you have new changes to add.`;
+
   // Comment on the PR
   await octokit.rest.issues.createComment({
     owner: context.repo.owner,
     repo: context.repo.repo,
     issue_number: prNumber,
-    body: `Thank you for your contribution to MetaMask Extension. In order to maintain a clean and relevant PR queue, we close all PRs after ${MAX_DAYS_OF_INACTIVITY} days of inactivity. Please reopen this PR once you have new changes to add.`,
+    body: commentBody,
   });
 
   // Close the PR
