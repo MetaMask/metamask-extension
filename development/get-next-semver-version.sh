658bbef19085ffd87a4a6735ef0926ca61089314
Gauthier Petetin <gauthierpetetin@gmail.com>
2023-06-15 07:19:06 +0200

    fix(script): exit script earlier if condition is met

--- development/get-next-semver-version.sh
+++ development/get-next-semver-version.sh
@@ -2,6 +2,13 @@
 
 FORCE_NEXT_SEMVER_VERSION=$1
 
+# If FORCE_NEXT_SEMVER_VERSION is defined and not empty, use its value and skip the next operations
+if [ -n "$FORCE_NEXT_SEMVER_VERSION" ]
+then
+  echo "NEXT_SEMVER_VERSION=${FORCE_NEXT_SEMVER_VERSION}" >> "$GITHUB_ENV"
+  exit 0
+fi
+
 # Get the highest version from release branches
 VERSION_BRANCHES=$(git branch -r | grep -o 'release/[0-9]*\.[0-9]*\.[0-9]*' | grep -o '[0-9]*\.[0-9]*\.[0-9]*' | sort --version-sort | tail -n 1)
 
@@ -16,11 +23,5 @@ HIGHEST_VERSION=$(printf "%s\n%s\n%s" "$VERSION_BRANCHES" "$VERSION_TAGS" "$VERS
 
 # Increment the minor version of the highest version found
 NEXT_VERSION=$(echo "$HIGHEST_VERSION" | awk -F. -v OFS=. '{$2++; print}')
-
-# Check if FORCE_NEXT_SEMVER_VERSION is defined and not empty
-if [ -n "$FORCE_NEXT_SEMVER_VERSION" ]
-then
-  NEXT_VERSION=$FORCE_NEXT_SEMVER_VERSION
-fi
 
 echo "NEXT_SEMVER_VERSION=${NEXT_VERSION}" >> "$GITHUB_ENV"
