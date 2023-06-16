e4923399a9ab939beb886fafbdc70eb82d4a2251
Victorien Gauch <85494462+VGau@users.noreply.github.com>
2023-06-16 18:35:33 +0200

    feat: add new linea mainnet network (#19326)
    * feat: add new linea mainnet network
    
    * fix: removed unused condition + lint code
    
    * fix: update tests + fix network tab issue
    
    * feat: add feature toggle for linea mainnet
    
    * fix: add feature toggle for linea mainnet
    
    * feat: add linea mainnet logo
    
    * update @metamask/eth-json-rpc-infura package to support linea networks
    
    * update linea mainnet block explorer url
    
    * fix: refactor linea mainnet feature toggle
    
    * fix: update linea mainnet chain id and rpc url
    
    * fix: update settings-search unit test
    
    * fix: update linea mainnet feature flag
    
    * fix: remove useless async function keyword for linea mainnet feature flag

--- ui/pages/settings/networks-tab/networks-tab.constants.js
+++ ui/pages/settings/networks-tab/networks-tab.constants.js
@@ -45,7 +45,7 @@ const defaultNetworksData = [
   },
   {
     labelKey: NETWORK_TYPES.LINEA_GOERLI,
-    iconColor: '#234FD5',
+    iconColor: '#61dfff',
     providerType: NETWORK_TYPES.LINEA_GOERLI,
     rpcUrl: getRpcUrl({
       network: NETWORK_TYPES.LINEA_GOERLI,
@@ -54,6 +54,18 @@ const defaultNetworksData = [
     chainId: CHAIN_IDS.LINEA_GOERLI,
     ticker: TEST_NETWORK_TICKER_MAP[NETWORK_TYPES.LINEA_GOERLI],
     blockExplorerUrl: 'https://goerli.lineascan.build',
+  },
+  {
+    labelKey: NETWORK_TYPES.LINEA_MAINNET,
+    iconColor: '#121212',
+    providerType: NETWORK_TYPES.LINEA_MAINNET,
+    rpcUrl: getRpcUrl({
+      network: NETWORK_TYPES.LINEA_MAINNET,
+      excludeProjectId: true,
+    }),
+    chainId: CHAIN_IDS.LINEA_MAINNET,
+    ticker: CURRENCY_SYMBOLS.ETH,
+    blockExplorerUrl: 'https://lineascan.build',
   },
 ];
 
