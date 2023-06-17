546a9b86c06234d83f466b481bc233b3cd8f7fee
Howard Braham <howrad@gmail.com>
2023-06-16 18:30:33 -0700

    Apply suggestion from @georgewrmarshall
    Co-authored-by: George Marshall <george.marshall@consensys.net>

--- ui/components/app/srp-quiz-modal/QuizContent/QuizContent.tsx
+++ ui/components/app/srp-quiz-modal/QuizContent/QuizContent.tsx
@@ -9,8 +9,7 @@ import {
   TextVariant,
 } from '../../../../helpers/constants/design-system';
 import { useI18nContext } from '../../../../hooks/useI18nContext';
-import { Button, Text } from '../../../component-library';
-import Box from '../../../ui/box';
+import { Button, Text, Box } from '../../../component-library';
 import { IQuizInformationProps } from '../types';
 
 export default function QuizContent({
