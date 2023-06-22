4c909fb81ccca881ae3059c52979b7ce46185d03
weizman <weizmangal@gmail.com>
2023-06-22 18:04:15 +0300

    lint

--- app/scripts/use-snow.js
+++ app/scripts/use-snow.js
@@ -1,10 +1,19 @@
+/*
+NOTICE:
+This Snow + LavaMoat scuttling integration is currently being used
+with an experimental API (https://github.com/LavaMoat/LavaMoat/pull/462).
+Changing this code must be done cautiously to avoid breaking the app!
+*/
+
 // eslint-disable-next-line import/unambiguous
 (function () {
   const log = console.log.bind(console);
+  const msg =
+    'Snow detected a new realm creation attempt in MetaMask. Performing scuttling on new realm.';
   Object.defineProperty(window.top, 'SCUTTLER', {
     value: (realm, scuttle) => {
       window.top.SNOW((win) => {
-        log('Snow detected a new realm creation attempt in MetaMask. Performing scuttling on new realm.', win);
+        log(msg, win);
         scuttle(win);
       }, realm);
     },
