5f1be02a5ff3d9a76ec60fb86effdad534d44140
garrettbear <gwhisten@gmail.com>
2023-06-23 12:45:07 -0500

    add stories

--- ui/components/component-library/checkbox/checkbox.stories.tsx
+++ ui/components/component-library/checkbox/checkbox.stories.tsx
@@ -43,3 +43,57 @@ export const Label = (args) => {
     />
   );
 };
+
+export const IsChecked = (args) => {
+  return <Checkbox {...args} label="isChecked Demo" />;
+};
+
+IsChecked.args = {
+  isChecked: true,
+};
+
+export const IsIndeterminate = (args) => {
+  return <Checkbox {...args} label="isIndeterminate Demo" />;
+};
+
+IsIndeterminate.args = {
+  isIndeterminate: true,
+};
+
+export const IsDisabled = (args) => {
+  return <Checkbox {...args} label="isDisabled Demo" />;
+};
+
+IsDisabled.args = {
+  isDisabled: true,
+};
+
+export const IsReadOnly = (args) => {
+  return <Checkbox {...args} label="isReadOnly Demo" />;
+};
+
+IsReadOnly.args = {
+  isReadOnly: true,
+  isChecked: true,
+};
+
+export const OnChange = (args) => {
+  const [isChecked, setIsChecked] = React.useState(false);
+  return (
+    <Checkbox
+      {...args}
+      onChange={() => setIsChecked(!isChecked)}
+      isChecked={isChecked}
+      label="onChange Demo"
+    />
+  );
+};
+
+export const IsRequired = (args) => {
+  return <Checkbox {...args} label="isRequired Demo" />;
+};
+
+IsRequired.args = {
+  isRequired: true,
+  isChecked: true,
+};
