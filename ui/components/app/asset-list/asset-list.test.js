393b13a5c2c091b23819e32f989a839fc4005aaa
David Walsh <davidwalsh83@gmail.com>
2023-06-24 13:15:31 -0500

    Fix tests

--- ui/components/app/asset-list/asset-list.test.js
+++ ui/components/app/asset-list/asset-list.test.js
@@ -5,18 +5,24 @@ import configureStore from '../../../store/store';
 import mockState from '../../../../test/data/mock-state.json';
 import AssetList from './asset-list';
 
-const render = () => {
+const render = (chainId) => {
   const store = configureStore({
     metamask: {
       ...mockState.metamask,
+      providerConfig: { chainId },
     },
   });
-  return renderWithProvider(<AssetList />, store);
+  return renderWithProvider(<AssetList onClickAsset={jest.fn()} />, store);
 };
 
 describe('AssetList', () => {
   it('renders AssetList component and shows Refresh List text', () => {
-    render();
+    render('0x1');
     expect(screen.getByText('Refresh list')).toBeInTheDocument();
+  });
+
+  it('renders AssetList component and hides Refresh List on unsupported network', () => {
+    const { queryByText } = render('0x99');
+    expect(queryByText('Refresh list')).not.toBeInTheDocument();
   });
 });
