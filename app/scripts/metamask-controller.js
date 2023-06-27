d70ae67d1312081dcafb8125fa0085ba8a47f213
Albert Olivé <albertolivecorbella@gmail.com>
2023-06-27 08:30:42 +0200

    [MMI] Added code fences in whats new popup (#19581)
    * added code fences in whats new popup
    
    * Improved code
    
    * Added missing prop
    
    * Update LavaMoat policies
    
    * updated functions by using an options object for the rendering functions in order to bypass possible typsecript issues
    
    ---------
    
    Co-authored-by: MetaMask Bot <metamaskbot@users.noreply.github.com>

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -120,6 +120,9 @@ import {
   ///: END:ONLY_INCLUDE_IN
 } from '../../shared/constants/permissions';
 import { UI_NOTIFICATIONS } from '../../shared/notifications';
+///: BEGIN:ONLY_INCLUDE_IN(build-mmi)
+import { UI_INSTITUTIONAL_NOTIFICATIONS } from '../../shared/notifications/institutional';
+///: END:ONLY_INCLUDE_IN
 import { MILLISECOND, SECOND } from '../../shared/constants/time';
 import {
   ORIGIN_METAMASK,
@@ -617,9 +620,16 @@ export default class MetamaskController extends EventEmitter {
     const announcementMessenger = this.controllerMessenger.getRestricted({
       name: 'AnnouncementController',
     });
+
+    let allAnnouncements = UI_NOTIFICATIONS;
+
+    ///: BEGIN:ONLY_INCLUDE_IN(build-mmi)
+    allAnnouncements = UI_INSTITUTIONAL_NOTIFICATIONS;
+    ///: END:ONLY_INCLUDE_IN
+
     this.announcementController = new AnnouncementController({
       messenger: announcementMessenger,
-      allAnnouncements: UI_NOTIFICATIONS,
+      allAnnouncements,
       state: initState.AnnouncementController,
     });
 
