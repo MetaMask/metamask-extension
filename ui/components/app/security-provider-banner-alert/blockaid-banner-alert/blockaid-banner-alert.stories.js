6b3a618446d0a2c4105dd71708a545ac574646b4
digiwand <20778143+digiwand@users.noreply.github.com>
2023-07-17 15:42:03 +0200

    BlockaidBannerAlert: update stories

--- ui/components/app/security-provider-banner-alert/blockaid-banner-alert/blockaid-banner-alert.stories.js
+++ ui/components/app/security-provider-banner-alert/blockaid-banner-alert/blockaid-banner-alert.stories.js
@@ -5,32 +5,38 @@ import {
 } from '../../../../../shared/constants/security-provider';
 import BlockaidBannerAlert from '.';
 
-const mockPpomResponse = {
-  resultType: BlockaidResultType.Warning,
-  reason: BlockaidReason.setApprovalForAll,
-  description:
-    'A SetApprovalForAll request was made on {contract}. We found the operator {operator} to be malicious',
-  args: {
-    contract: '0xa7206d878c5c3871826dfdb42191c49b1d11f466',
-    operator: '0x92a3b9773b1763efa556f55ccbeb20441962d9b2',
-  },
-  features: [
-    'Operator is an EOA',
-    'Operator is untrusted according to previous activity',
-  ],
-};
+const mockFeatures = [
+  'Operator is an EOA',
+  'Operator is untrusted according to previous activity',
+];
 
 export default {
   title: 'Components/App/SecurityProviderBannerAlert/Blockaid',
   argTypes: {
-    ppomResponse: {
-      control: 'object',
+    features: {
+      control: 'array',
+      description:
+        'ppomResponse.features value which is a list displayed as SecurityProviderBannerAlert details',
+    },
+    reason: {
+      control: 'select',
+      options: Object.values(BlockaidReason),
+      description: 'ppomResponse.reason value',
+    },
+    resultType: {
+      control: 'select',
+      options: Object.values(BlockaidResultType),
+      description: 'ppomResponse.resultType value',
     },
   },
   args: {
-    ppomResponse: mockPpomResponse,
+    features: mockFeatures,
+    reason: BlockaidReason.setApprovalForAll,
+    resultType: BlockaidResultType.Warning,
   },
 };
 
-export const DefaultStory = (args) => <BlockaidBannerAlert {...args} />;
+export const DefaultStory = (args) => (
+  <BlockaidBannerAlert ppomResponse={args} />
+);
 DefaultStory.storyName = 'Default';
