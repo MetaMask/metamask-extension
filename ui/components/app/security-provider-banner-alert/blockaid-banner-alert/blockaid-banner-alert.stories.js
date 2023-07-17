3c1c9482f280a423e086bfa1517e93e20388ba5e
digiwand <20778143+digiwand@users.noreply.github.com>
2023-07-17 15:58:31 +0200

    stories: update BlockaidBannerAlert title

--- ui/components/app/security-provider-banner-alert/blockaid-banner-alert/blockaid-banner-alert.stories.js
+++ ui/components/app/security-provider-banner-alert/blockaid-banner-alert/blockaid-banner-alert.stories.js
@@ -11,7 +11,7 @@ const mockFeatures = [
 ];
 
 export default {
-  title: 'Components/App/SecurityProviderBannerAlert/Blockaid',
+  title: 'Components/App/SecurityProviderBannerAlert/BlockaidBannerAlert',
   argTypes: {
     features: {
       control: 'array',
