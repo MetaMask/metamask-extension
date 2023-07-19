6c7107b7e8b6e5cdd7f8a7d860350ecda0947e96
Nicholas Ellul <nicholas.ellul1@gmail.com>
2023-07-19 09:24:51 -0400

    Have migration delete PhishingController state entirely

--- app/scripts/migrations/089.test.js
+++ app/scripts/migrations/089.test.js
@@ -31,63 +31,16 @@ describe('migration #89', () => {
     expect(newStorage.data).toStrictEqual(oldStorage.data);
   });
 
-  const nonObjects = [undefined, null, 'test', 1, ['test']];
-
-  for (const invalidState of nonObjects) {
-    it(`does not change the state if the phishing controller state is ${invalidState}`, async () => {
-      const oldStorage = {
-        meta: {
-          version: PREVIOUS_VERSION,
-        },
-        data: { PhishingController: invalidState },
-      };
-
-      const newStorage = await migrate(oldStorage);
-
-      expect(newStorage.data).toStrictEqual(oldStorage.data);
-    });
-  }
-
-  it('does not change the state if the listState property does not exist', async () => {
+  it('deletes the PhishingController" state', async () => {
     const oldStorage = {
       meta: {
         version: PREVIOUS_VERSION,
       },
-      data: {
-        PhishingController: { test: 123 },
-      },
+      data: { PhishingController: {} },
     };
 
     const newStorage = await migrate(oldStorage);
 
-    expect(newStorage.data).toStrictEqual(oldStorage.data);
-  });
-
-  it('deletes the "listState" property', async () => {
-    const oldStorage = {
-      meta: {
-        version: PREVIOUS_VERSION,
-      },
-      data: { PhishingController: { listState: {} } },
-    };
-
-    const newStorage = await migrate(oldStorage);
-
-    expect(newStorage.data.PhishingController.listState).toBeUndefined();
-  });
-
-  it('migrates as expected if listState is present', async () => {
-    const oldStorage = {
-      meta: {
-        version: PREVIOUS_VERSION,
-      },
-      data: { PhishingController: { listState: { test: 123 } } },
-    };
-
-    const newStorage = await migrate(oldStorage);
-
-    expect(newStorage.data).toStrictEqual({
-      PhishingController: { phishingLists: [{ test: 123 }] },
-    });
+    expect(newStorage.data.PhishingController).toBeUndefined();
   });
 });
