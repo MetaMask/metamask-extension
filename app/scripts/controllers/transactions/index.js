8009e714c43db67f5d4b97b92cef97d4bf1cc859
Jyoti Puri <jyotipuri@gmail.com>
2023-07-19 16:56:16 +0530

    Update

--- app/scripts/controllers/transactions/index.js
+++ app/scripts/controllers/transactions/index.js
@@ -352,6 +352,7 @@ export default class TransactionController extends EventEmitter {
       `MetaMaskController newUnapprovedTransaction ${JSON.stringify(txParams)}`,
     );
 
+    console.log('-------------- opts', opts);
     const { txMeta: initialTxMeta, isExisting } = await this._createTransaction(
       opts.method,
       txParams,
