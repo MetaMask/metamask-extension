d4de85a0616933483bc993c4af941aef357478b8
Antonio Regadas <antonio.regadas@consensys.net>
2023-07-21 16:59:04 +0100

    typo fix

--- app/scripts/controllers/mmi-controller.js
+++ app/scripts/controllers/mmi-controller.js
@@ -543,8 +543,8 @@ export default class MMIController extends EventEmitter {
       this.custodyController.getAccountDetails(address);
     const extensionId = this.extension.runtime.id;
 
-    const { networkConfigurations: networkConfigurationsById } = this.networkController.state;
-    const networkConfigurations = Object.values(networkConfigurationsById);
+    const { networkConfigurations: networkConfigurationId } = this.networkController.state;
+    const networkConfigurations = Object.values(networkConfigurationId);
 
     const networks = [
       ...networkConfigurations,
