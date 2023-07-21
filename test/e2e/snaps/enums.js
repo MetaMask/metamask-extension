04d14e2e16583138d627c46c4251254f1e2d6424
Bowen Sanders <bowensanders@gmail.com>
2023-07-21 14:32:51 -0700

    Fix snaps E2Es to be compatible with new monorepo-based test-snaps (#20005)
    * initial fixes to E2Es
    
    * changed test website
    
    * final E2E fixes
    
    * fixed getentropy test
    
    * missing scroll fix
    
    * scrollfix for bip44 firefox
    
    * added new function and edited tests

--- test/e2e/snaps/enums.js
+++ test/e2e/snaps/enums.js
@@ -1,5 +1,6 @@
 module.exports = {
-  TEST_SNAPS_WEBSITE_URL: 'https://metamask.github.io/test-snaps/5.5.0/',
+  TEST_SNAPS_WEBSITE_URL:
+    'https://metamask.github.io/snaps/test-snaps/0.37.3-flask.1/',
   TEST_SNAPS_SIMPLE_KEYRING_WEBSITE_URL:
     'https://metamask.github.io/snap-simple-keyring/latest/',
 };
