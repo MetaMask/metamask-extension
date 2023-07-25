030f7680f9aa40ae02eaa8ebac71638a27ec29e7
Monte Lai <monte.lai@consensys.net>
2023-07-25 16:14:40 +0800

    feat: accounts controller

---
+++ app/scripts/controllers/accounts-controller.ts
@@ -0,0 +1,199 @@
+import {
+  BaseControllerV2,
+  RestrictedControllerMessenger,
+} from '@metamask/base-controller';
+import { Patch } from 'immer';
+import { v4 as uuid } from 'uuid';
+import { InternalAccount, SnapKeyring } from '@metamask/eth-snap-keyring';
+import { KeyringController } from '@metamask/eth-keyring-controller';
+import { SnapControllerEvents } from '@metamask/snaps-controllers';
+import PreferencesController from './preferences';
+
+const controllerName = 'AccountsController';
+
+export type AccountsControllerState = {
+  internalAccounts: Record<string, InternalAccount>;
+  selectedAccount: InternalAccount;
+};
+
+export type AccountsControllerGetStateAction = {
+  type: `${typeof controllerName}:getState`;
+  handler: () => AccountsControllerState;
+};
+
+export type AccountsControllerActions = AccountsControllerGetStateAction;
+
+export type AccountsControllerChangeEvent = {
+  type: `${typeof controllerName}:stateChange`;
+  payload: [AccountsControllerState, Patch[]];
+};
+
+export type AccountsControllerEvents =
+  | AccountsControllerChangeEvent
+  | SnapControllerEvents;
+
+export type AccountsControllerMessenger = RestrictedControllerMessenger<
+  typeof controllerName,
+  AccountsControllerActions,
+  AccountsControllerEvents,
+  string,
+  string
+>;
+
+const accountsControllerMetadata = {
+  internalAccounts: {
+    persist: true,
+    anonymous: false,
+  },
+  selectedAccount: {
+    persist: true,
+    anonymous: false,
+  },
+};
+
+export default class AccountsController extends BaseControllerV2<
+  typeof controllerName,
+  AccountsControllerState,
+  AccountsControllerMessenger
+> {
+  #preferenceController: PreferencesController;
+
+  #keyringController: any;
+
+  constructor({
+    messenger,
+    state,
+    preferenceController,
+    keyringController,
+    onPreferencesStateChange,
+  }: {
+    messenger: AccountsControllerMessenger;
+    state: AccountsControllerState;
+    preferenceController: PreferencesController;
+    keyringController: KeyringController;
+    onPreferencesStateChange: (
+      listener: (preferencesState: any) => void,
+    ) => void;
+  }) {
+    super({
+      messenger,
+      name: controllerName,
+      metadata: accountsControllerMetadata,
+      state,
+    });
+
+    this.#preferenceController = preferenceController;
+    this.#keyringController = keyringController;
+
+    this.messagingSystem.subscribe(
+      'SnapController:stateChange',
+      async (snapState) => {
+        console.log('snap state changed', snapState);
+      },
+    );
+
+    onPreferencesStateChange((preferenceState) => {
+      console.log('preference state changed', preferenceState);
+      const { selectedAddress } = preferenceState;
+      console.log('selected address', selectedAddress);
+    });
+  }
+
+  getAccount(address: string): InternalAccount | undefined {
+    return Object.values(this.state.internalAccounts).find(
+      (internalAccount) =>
+        internalAccount.address.toLowerCase() === address.toLowerCase(),
+    );
+  }
+
+  getAccountExpect(address: string): InternalAccount {
+    const account = this.getAccount(address);
+    if (account === undefined) {
+      throw new Error(`Account ${address} not found`);
+    }
+    return account;
+  }
+
+  getAccountId(accountId: string): InternalAccount | undefined {
+    return this.state.internalAccounts[accountId];
+  }
+
+  getAccountByIdExpect(accountId: string): InternalAccount {
+    const account = this.getAccount(accountId);
+    if (account === undefined) {
+      throw new Error(`Account Id ${accountId} not found`);
+    }
+    return account;
+  }
+
+  getAccountsByKeyring(keyring: string): InternalAccount[] {
+    return Object.values(this.state.internalAccounts).filter(
+      (internalAccount) => internalAccount.type === keyring,
+    );
+  }
+
+  getAccountsBySnapId(snapId: string): InternalAccount[] {
+    return Object.values(this.state.internalAccounts).filter(
+      (internalAccount) => internalAccount.metadata.snap?.id === snapId,
+    );
+  }
+
+  async updateAccounts(): Promise<void> {
+    const legacyAccounts = await this.#listLegacyAccounts();
+    const snapAccounts = await this.#listSnapAccounts();
+
+    const internalAccounts: Record<string, InternalAccount> = [
+      ...legacyAccounts,
+      ...snapAccounts,
+    ].reduce((internalAccountMap, internalAccount) => {
+      internalAccountMap[internalAccount.id] = internalAccount;
+      return internalAccountMap;
+    }, {} as Record<string, InternalAccount>);
+
+    this.update((accountsControllerState: AccountsControllerState) => {
+      accountsControllerState.internalAccounts = internalAccounts;
+    });
+  }
+
+  async #listSnapAccounts(): Promise<InternalAccount[]> {
+    const [snapKeyring]: [SnapKeyring] =
+      this.#keyringController.getKeyringsByType(SnapKeyring.type);
+
+    return snapKeyring?.listAccounts(true) ?? [];
+  }
+
+  async #listLegacyAccounts(): Promise<InternalAccount[]> {
+    const addresses = await this.#keyringController.getAccounts();
+    return addresses.map((address: string) => {
+      return {
+        id: uuid(),
+        address,
+        name: '',
+        options: {},
+        supportedMethods: [
+          'personal_sign',
+          'eth_sendTransaction',
+          'eth_sign',
+          'eth_signTransaction',
+          'eth_signTypedData',
+          'eth_signTypedData_v1',
+          'eth_signTypedData_v2',
+          'eth_signTypedData_v3',
+          'eth_signTypedData_v4',
+        ],
+        type: 'eip155:eoa',
+        metadata: {},
+      };
+    });
+  }
+
+  setSelectedAccount(accountId: string): InternalAccount {
+    const account = this.getAccountByIdExpect(accountId);
+
+    this.update((state) => {
+      state.selectedAccount = account;
+    });
+
+    return account;
+  }
+}
--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -62,6 +62,8 @@ import {
 ///: END:ONLY_INCLUDE_IN
 ///: BEGIN:ONLY_INCLUDE_IN(keyring-snaps)
 import { SnapKeyring } from '@metamask/eth-snap-keyring';
+// eslint-disable-next-line import/order
+import AccountsController from './controllers/accounts-controller';
 ///: END:ONLY_INCLUDE_IN
 
 ///: BEGIN:ONLY_INCLUDE_IN(build-mmi)
@@ -1009,6 +1011,19 @@ export default class MetamaskController extends EventEmitter {
       },
     });
 
+    const accountsControllerMessenger = this.controllerMessenger.getRestricted({
+      name: 'AccountsController',
+      allowedEvents: ['SnapController:stateChange'],
+    });
+
+    this.accountsController = new AccountsController({
+      messenger: accountsControllerMessenger,
+      state: initState.accountsController,
+      onPreferencesStateChange: (listener) => {
+        this.preferencesController.store.subscribe(listener);
+      },
+    });
+
     this.notificationController = new NotificationController({
       messenger: this.controllerMessenger.getRestricted({
         name: 'NotificationController',
@@ -1671,6 +1686,9 @@ export default class MetamaskController extends EventEmitter {
     this.memStore = new ComposableObservableStore({
       config: {
         AppStateController: this.appStateController.store,
+        ///: BEGIN:ONLY_INCLUDE_IN(keyring-snaps)
+        AccountsController: this.accountsController,
+        ///: END:ONLY_INCLUDE_IN
         NetworkController: this.networkController,
         CachedBalancesController: this.cachedBalancesController.store,
         KeyringController: this.keyringController.memStore,
@@ -2964,6 +2982,10 @@ export default class MetamaskController extends EventEmitter {
         this.preferencesController.getLedgerTransportPreference();
       this.setLedgerTransportPreference(transportPreference);
 
+      ///: BEGIN:ONLY_INCLUDE_IN(keyring-snaps)
+      await this.accountsController.updateAccounts();
+      ///: END:ONLY_INCLUDE_IN
+
       // set new identities
       this.preferencesController.setAddresses(accounts);
       this.selectFirstIdentity();
@@ -3028,6 +3050,10 @@ export default class MetamaskController extends EventEmitter {
       this.preferencesController.getLedgerTransportPreference();
 
     this.setLedgerTransportPreference(transportPreference);
+
+    ///: BEGIN:ONLY_INCLUDE_IN(keyring-snaps)
+    await this.accountsController.updateAccounts();
+    ///: END:ONLY_INCLUDE_IN
 
     return this.keyringController.fullUpdate();
   }
