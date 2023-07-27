a5dd7ba60c8efb9517aec81a34d57c6fb7089aad
georgewrmarshall <george.marshall@consensys.net>
2023-07-27 16:19:38 -0700

    Updating AdvancedGasControls with component-library components

--- ui/components/app/advanced-gas-controls/advanced-gas-controls.component.js
+++ ui/components/app/advanced-gas-controls/advanced-gas-controls.component.js
@@ -2,8 +2,15 @@ import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 
 import { I18nContext } from '../../../contexts/i18n';
-import FormField from '../../ui/form-field';
 import { getGasFormErrorText } from '../../../helpers/constants/gas';
+import { DECIMAL_REGEX } from '../../../../shared/constants/tokens';
+import {
+  Display,
+  AlignItems,
+  FontWeight,
+} from '../../../helpers/constants/design-system';
+import InfoTooltip from '../../ui/info-tooltip/info-tooltip';
+import { Box, Label, FormTextField, Text } from '../../component-library';
 
 export default function AdvancedGasControls({
   onManualChange,
@@ -15,42 +22,73 @@ export default function AdvancedGasControls({
   minimumGasLimit,
 }) {
   const t = useContext(I18nContext);
-
+  const handleOnChange = (e) => {
+    const newValue = e.target.value;
+    const match = DECIMAL_REGEX.exec(newValue);
+    if (match?.[1]?.length >= 15) {
+      return;
+    }
+    onManualChange?.(parseFloat(newValue || 0, 10));
+  };
   return (
     <div className="advanced-gas-controls">
-      <FormField
-        titleText={t('gasLimit')}
-        error={
+      <Box display={Display.Flex} alignItems={AlignItems.center} gap={1}>
+        <Label htmlFor="gas-limit">{t('gasLimit')}</Label>
+        <InfoTooltip contentText={t('editGasLimitTooltip')} position="bottom" />
+      </Box>
+      <FormTextField
+        id="gas-limit"
+        value={gasLimit}
+        type="number"
+        placeholder="0"
+        marginBottom={4}
+        error={Boolean(gasErrors?.gasLimit)}
+        helpText={
           gasErrors?.gasLimit
             ? getGasFormErrorText(gasErrors.gasLimit, t, { minimumGasLimit })
             : null
         }
-        onChange={(value) => {
-          onManualChange?.();
-          setGasLimit(value);
+        onChange={(e) => {
+          handleOnChange(e);
+          setGasLimit(e);
         }}
-        tooltipText={t('editGasLimitTooltip')}
-        value={gasLimit}
-        allowDecimals={false}
-        numeric
+        inputProps={{
+          onKeyDown: (e) => {
+            // prevent decimals
+            if (e.key === '.') {
+              e.preventDefault();
+            }
+          },
+          min: 0,
+          className: 'advanced-gas-controls__input',
+        }}
       />
       <>
-        <FormField
-          titleText={t('advancedGasPriceTitle')}
-          titleUnit="(GWEI)"
-          onChange={(value) => {
-            onManualChange?.();
-            setGasPrice(value);
-          }}
-          tooltipText={t('editGasPriceTooltip')}
+        <Box display={Display.Flex} alignItems={AlignItems.center} gap={1}>
+          <Label htmlFor="gas-price">
+            {t('advancedGasPriceTitle')}{' '}
+            <Text marginLeft={1} as="span" fontWeight={FontWeight.Normal}>
+              (GWEI)
+            </Text>
+          </Label>
+          <InfoTooltip contentText={t('editGasPriceTooltip')} position="top" />
+        </Box>
+        <FormTextField
+          id="gas-price"
           value={gasPrice}
-          numeric
-          allowDecimals
-          error={
+          type="number"
+          placeholder="0"
+          onChange={(e) => {
+            handleOnChange(e);
+            setGasPrice(e);
+          }}
+          error={Boolean(gasErrors?.gasPrice)}
+          helpText={
             gasErrors?.gasPrice
               ? getGasFormErrorText(gasErrors.gasPrice, t)
               : null
           }
+          inputProps={{ min: 0, className: 'advanced-gas-controls__input' }}
         />
       </>
     </div>
--- ui/components/app/advanced-gas-controls/advanced-gas-controls.stories.js
+++ ui/components/app/advanced-gas-controls/advanced-gas-controls.stories.js
@@ -1,15 +1,38 @@
 import React from 'react';
-
 import AdvancedGasControls from '.';
 
 export default {
   title: 'Components/App/AdvancedGasControls',
+  component: AdvancedGasControls,
+  argTypes: {
+    onManualChange: {
+      action: 'onManualChange',
+    },
+    gasLimit: {
+      control: 'number',
+    },
+    setGasLimit: {
+      action: 'setGasLimit',
+    },
+    gasPrice: {
+      control: 'text',
+    },
+    setGasPrice: {
+      action: 'setGasPrice',
+    },
+    minimumGasLimit: {
+      control: 'text',
+    },
+    gasErrors: {
+      control: 'object',
+    },
+  },
 };
 
-export const DefaultStory = () => {
+export const DefaultStory = (args) => {
   return (
     <div style={{ width: '600px' }}>
-      <AdvancedGasControls />
+      <AdvancedGasControls {...args} />
     </div>
   );
 };
