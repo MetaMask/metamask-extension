c48ca6a0edd293cd2657987dadcfa3d44a9feb88
IF <139582705+infiniteflower@users.noreply.github.com>
2023-07-27 11:32:54 -0400

    Add story for smart tx popover

---
+++ ui/pages/swaps/prepare-swap-page/smart-transactions-popover.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SmartTransactionsPopover from './smart-transactions-popover';
+
+export default {
+  title: 'Pages/Swaps/SmartTransactionsPopover',
+};
+
+export const DefaultStory = () => {
+  return <SmartTransactionsPopover />;
+};
+
+DefaultStory.storyName = 'Default';
