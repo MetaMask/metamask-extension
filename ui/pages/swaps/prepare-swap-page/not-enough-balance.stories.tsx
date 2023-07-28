c7b017501963f905ad53117f06bc3c5114aec3cf
IF <139582705+infiniteflower@users.noreply.github.com>
2023-07-28 14:43:50 -0400

    Add story for not enough balance

---
+++ ui/pages/swaps/prepare-swap-page/not-enough-balance.stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StoryFn, Meta } from '@storybook/react';
+// import { useArgs } from '@storybook/client-api';
+import NotEnoughBalance from './not-enough-balance';
+
+export default {
+  title: 'Pages/Swaps/NotEnoughBalance',
+  component: NotEnoughBalance,
+} as Meta<typeof NotEnoughBalance>;
+
+export const DefaultStory: StoryFn<typeof NotEnoughBalance> = () => {
+  return (
+    <NotEnoughBalance
+      title="Insufficient balance"
+      actionableBalanceErrorMessage="You need 0.0103 more ETH to complete this swap"
+      needsMoreGas={true}
+      openBuyCryptoInPdapp={() => {
+        console.log('openBuyCryptoInPdapp');
+      }}
+      needsMoreGasText="Buy more ETH"
+    />
+  );
+};
+
+DefaultStory.storyName = 'Default';
