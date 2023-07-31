MetaMask/metamask-extension

commit 7e824fdc5ed29e34b03b417c13000e9daf3865a1

Fix non-preview draft PR builds (#20096)
Certain draft PRs that add new dependencies have been failing because
CI will try to use the GitHub npm registry, which we use for preview
builds. This registry does not have non-preview package versions, so
the installation will fail if new non-preview dependencies are needed.

CI has been updated to only use the GitHub npm registry when preview
builds are detected in the manifest.

.circleci/scripts/install-dependencies.sh
@@ -1,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -o pipefail
+
+PR_NUMBER="${CIRCLE_PULL_REQUEST##*/}"
+if [ -n "$PR_NUMBER" ]
+then
+  IS_DRAFT="$(gh pr view --json isDraft --jq '.isDraft' "$PR_NUMBER")"
+else
+  IS_DRAFT='false'
+fi
+
+# Build query to see whether there are any "preview-like" packages in the manifest
+# A "preview-like" package is a `@metamask`-scoped package with a prerelease version that has no period.
+QUERY='.dependencies + .devDependencies'                            # Get list of all dependencies
+QUERY+=' | with_entries( select(.key | startswith("@metamask") ) )' # filter to @metamask-scoped packages
+QUERY+=' | to_entries[].value'                                      # Get version ranges
+QUERY+=' | select(test("^\\d+\\.\\d+\\.\\d+-[^.]+$"))'              # Get pinned versions where the prerelease part has no "."
+
+# Use `-e` flag so that exit code indicates whether any matches were found
+if jq -e "${QUERY}" < ./package.json
+then
+  echo "Preview builds detected"
+  HAS_PREVIEW_BUILDS='true'
+else
+  echo "No preview builds detected"
+  HAS_PREVIEW_BUILDS='false'
+fi
+
+if [[ $IS_DRAFT == 'true' && $HAS_PREVIEW_BUILDS == 'true' ]]
+then
+  # Use GitHub registry on draft PRs, allowing the use of preview builds
+  echo "Installing with preview builds"
+  METAMASK_NPM_REGISTRY=https://npm.pkg.github.com yarn --immutable
+else
+  echo "Installing without preview builds"
+  yarn --immutable
+fi

.circleci/config.yml
@@ -351,26 +351,8 @@ jobs:
             - dependency-cache-v1-
       - gh/install
       - run:
-          name: Set IS_DRAFT environment variable
-          command: |
-            PR_NUMBER="${CIRCLE_PULL_REQUEST##*/}"
-            if [ -n "$PR_NUMBER" ]
-            then
-              echo "IS_DRAFT=$(gh pr view --json isDraft --jq '.isDraft' "$PR_NUMBER")" >> "$BASH_ENV"
-              source "$BASH_ENV"
-            else
-              echo "Not a PR; skipping"
-            fi
-      - run:
           name: Install dependencies
-          command: |
-            if [[ $IS_DRAFT == 'true' ]]
-            then
-              # Use GitHub registry on draft PRs, allowing the use of preview builds
-              METAMASK_NPM_REGISTRY=https://npm.pkg.github.com yarn --immutable
-            else
-              yarn --immutable
-            fi
+          command: .circleci/scripts/install-dependencies.sh
       - save_cache:
           key: dependency-cache-v1-{{ checksum "yarn.lock" }}
           paths:

commit ad53fc3462b4eeccb5694713bf38e1a4a2dd8f54

Fix `prep-deps` CI step
The CI job `prep-deps` was broken in #20096 for forks and non-PR builds
(e.g. the `develop` branch builds). Non-PR builds were broken because
of the `set -u` flag, which complained about a PR-specific environment
variable being unset. Fork builds were broken because the draft check
relied upon secrets (which aren't included in CI runs on forks).

The script is now tolerant of missing environment variables, and skips
the draft check for forks.

.circleci/scripts/install-dependencies.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
 set -e
-set -u
 set -o pipefail
 
-PR_NUMBER="${CIRCLE_PULL_REQUEST##*/}"
-if [ -n "$PR_NUMBER" ]
+IS_NON_FORK_DRAFT='false'
+
+if [[ -n $CIRCLE_PULL_REQUEST ]] && gh auth status
 then
-  IS_DRAFT="$(gh pr view --json isDraft --jq '.isDraft' "$PR_NUMBER")"
-else
-  IS_DRAFT='false'
+  PR_NUMBER="${CIRCLE_PULL_REQUEST##*/}"
+  if [ -n "$PR_NUMBER" ]
+  then
+    IS_NON_FORK_DRAFT="$(gh pr view --json isDraft --jq '.isDraft' "$PR_NUMBER")"
+  fi
 fi
 
 # Build query to see whether there are any "preview-like" packages in the manifest
@@ -29,7 +31,7 @@ else
   HAS_PREVIEW_BUILDS='false'
 fi
 
-if [[ $IS_DRAFT == 'true' && $HAS_PREVIEW_BUILDS == 'true' ]]
+if [[ $IS_NON_FORK_DRAFT == 'true' && $HAS_PREVIEW_BUILDS == 'true' ]]
 then
   # Use GitHub registry on draft PRs, allowing the use of preview builds
   echo "Installing with preview builds"