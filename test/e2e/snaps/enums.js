94bb06e2fbd8bc1ede005372d58c198f95d28516
bowensanders <bowensanders@gmail.com>
2023-08-01 11:42:52 -0700

    enums and new test

--- test/e2e/snaps/enums.js
+++ test/e2e/snaps/enums.js
@@ -1,6 +1,6 @@
 module.exports = {
   TEST_SNAPS_WEBSITE_URL:
-    'https://metamask.github.io/snaps/test-snaps/0.37.3-flask.1/',
+    'https://metamask.github.io/snaps/test-snaps/0.38.0-flask.1/',
   TEST_SNAPS_SIMPLE_KEYRING_WEBSITE_URL:
     'https://metamask.github.io/snap-simple-keyring/latest/',
 };
