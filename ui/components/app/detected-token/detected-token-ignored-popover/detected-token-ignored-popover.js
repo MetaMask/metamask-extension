6db99699ddee0728585e0773a3019fd23e920700
jainex17 <jainexp017@gmail.com>
2023-08-06 14:22:44 +0530

    Replacing deprecated Popover with Modal in detected-token-ignored-popover

--- ui/components/app/detected-token/detected-token-ignored-popover/detected-token-ignored-popover.js
+++ ui/components/app/detected-token/detected-token-ignored-popover/detected-token-ignored-popover.js
@@ -3,10 +3,22 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { useI18nContext } from '../../../../hooks/useI18nContext';
 
-import Popover from '../../../ui/popover';
-import Button from '../../../ui/button';
-import { TextVariant } from '../../../../helpers/constants/design-system';
-import { Text } from '../../../component-library';
+import {
+  Display,
+  JustifyContent,
+  TextVariant,
+} from '../../../../helpers/constants/design-system';
+import {
+  Text,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  Button,
+  BUTTON_VARIANT,
+  Box,
+  BUTTON_SIZES,
+} from '../../../component-library';
 
 const DetectedTokenIgnoredPopover = ({
   partiallyIgnoreDetectedTokens,
@@ -17,50 +29,94 @@ const DetectedTokenIgnoredPopover = ({
 
   const footer = (
     <>
-      <Button
-        className="detected-token-ignored-popover__ignore-button"
-        type="secondary"
-        onClick={onCancelIgnore}
+      <Box
+        display={Display.Flex}
+        justifyContent={JustifyContent.center}
+        marginLeft={4}
+        marginRight={4}
       >
-        {t('cancel')}
-      </Button>
-      <Button
-        className="detected-token-ignored-popover__import-button"
-        type="primary"
-        onClick={handleClearTokensSelection}
-      >
-        {t('confirm')}
-      </Button>
+        <Button
+          className="detected-token-ignored-popover__ignore-button"
+          block
+          variant={BUTTON_VARIANT.SECONDARY}
+          onClick={onCancelIgnore}
+          size={BUTTON_SIZES.LG}
+        >
+          {t('cancel')}
+        </Button>
+        <Button
+          className="detected-token-ignored-popover__import-button"
+          block
+          variant={BUTTON_VARIANT.PRIMARY}
+          onClick={handleClearTokensSelection}
+          size={BUTTON_SIZES.LG}
+        >
+          {t('confirm')}
+        </Button>
+      </Box>
     </>
   );
 
   return (
-    <Popover
-      title={
-        partiallyIgnoreDetectedTokens
-          ? t('importSelectedTokens')
-          : t('areYouSure')
-      }
+    // <Popover
+    //   title={
+    //     partiallyIgnoreDetectedTokens
+    //       ? t('importSelectedTokens')
+    //       : t('areYouSure')
+    //   }
+    //   className={classNames('detected-token-ignored-popover', {
+    //     'detected-token-ignored-popover--import': partiallyIgnoreDetectedTokens,
+    //     'detected-token-ignored-popover--ignore':
+    //       !partiallyIgnoreDetectedTokens,
+    //   })}
+    //   footer={footer}
+    // >
+    //   <Text
+    //     variant={TextVariant.bodySm}
+    //     as="h6"
+    //     marginTop={0}
+    //     marginRight={5}
+    //     marginBottom={7}
+    //     marginLeft={5}
+    //   >
+    //     {partiallyIgnoreDetectedTokens
+    //       ? t('importSelectedTokensDescription')
+    //       : t('ignoreTokenWarning')}
+    //   </Text>
+    // </Popover>
+
+    <Modal
+      isOpen
       className={classNames('detected-token-ignored-popover', {
         'detected-token-ignored-popover--import': partiallyIgnoreDetectedTokens,
         'detected-token-ignored-popover--ignore':
           !partiallyIgnoreDetectedTokens,
       })}
-      footer={footer}
+      onClose={onCancelIgnore}
     >
-      <Text
-        variant={TextVariant.bodySm}
-        as="h6"
-        marginTop={0}
-        marginRight={5}
-        marginBottom={7}
-        marginLeft={5}
-      >
-        {partiallyIgnoreDetectedTokens
-          ? t('importSelectedTokensDescription')
-          : t('ignoreTokenWarning')}
-      </Text>
-    </Popover>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader onClose={onCancelIgnore} marginBottom={4}>
+          {partiallyIgnoreDetectedTokens
+            ? t('importSelectedTokens')
+            : t('areYouSure')}
+        </ModalHeader>
+        <Text
+          variant={TextVariant.bodySm}
+          as="h6"
+          marginTop={0}
+          marginRight={5}
+          marginBottom={7}
+          marginLeft={5}
+        >
+          {partiallyIgnoreDetectedTokens
+            ? t('importSelectedTokensDescription')
+            : t('ignoreTokenWarning')}
+        </Text>
+
+        {footer}
+      </ModalContent>
+    </Modal>
   );
 };
 
