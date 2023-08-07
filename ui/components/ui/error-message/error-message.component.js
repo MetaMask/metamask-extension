f2e0aed65b88972a5024c60357da9dde12ec7b9e
panditdhamdhere <dhamdherepandit@gmail.com>
2023-08-07 11:22:49 +0530

    Replace ErrorMessage with BannerAlert component

--- ui/components/ui/error-message/error-message.component.js
+++ ui/components/ui/error-message/error-message.component.js
@@ -1,47 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Icon, IconName, IconSize } from '../../component-library';
-import { IconColor } from '../../../helpers/constants/design-system';
-
-/**
- * @deprecated - Please use ActionableMessage type danger
- * @see ActionableMessage
- * @param {object} props
- * @param {string} props.errorMessage
- * @param {string} props.errorKey
- * @param {object} context
- */
-const ErrorMessage = (props, context) => {
+import { BannerAlert, BannerType } from '../../component-library'; 
+const BannerAlertMessage = (props, context) => {
   const { errorMessage, errorKey } = props;
   const error = errorKey ? context.t(errorKey) : errorMessage;
 
   return (
-    <div className="error-message">
-      <Icon
-        className="error-message__icon"
-        name={IconName.Warning}
-        size={IconSize.Sm}
-        color={IconColor.errorDefault}
-        marginRight={2}
-      />
-      <div className="error-message__text">{error}</div>
-    </div>
+    <BannerAlert
+      type={BannerType.Error} 
+      icon={<Icon name={IconName.Warning} size={IconSize.Sm} color={IconColor.errorDefault} />} 
+    >
+      {error}
+    </BannerAlert>
   );
 };
 
-ErrorMessage.propTypes = {
-  /**
-   * The text content for the error message
-   */
+BannerAlertMessage.propTypes = {
   errorMessage: PropTypes.string,
-  /**
-   * The translate key for localization. Uses context.t(). Will override the error message
-   */
   errorKey: PropTypes.string,
 };
 
-ErrorMessage.contextTypes = {
+BannerAlertMessage.contextTypes = {
   t: PropTypes.func,
 };
 
-export default ErrorMessage;
+export default BannerAlertMessage;
