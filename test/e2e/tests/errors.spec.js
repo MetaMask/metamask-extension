b1277756e8dc32ffcabb2f5e0e439f69d7746b86
Mark Stacey <markjstacey@gmail.com>
2023-08-08 10:11:43 -0230

    Add error e2e tests for Sentry error state
    E2E tests have been added to test the state object sent along with each
    Sentry error.
    
    At the moment this object is empty in some circumstances, but this will
    change in later PRs.

--- test/e2e/tests/errors.spec.js
+++ test/e2e/tests/errors.spec.js
@@ -150,6 +150,42 @@ describe('Sentry errors', function () {
       );
     });
 
+    it('should capture background application state', async function () {
+      await withFixtures(
+        {
+          fixtures: {
+            ...new FixtureBuilder()
+              .withMetaMetricsController({
+                metaMetricsId: 'fake-metrics-id',
+                participateInMetaMetrics: true,
+              })
+              .build(),
+            // Intentionally corrupt state to trigger migration error during initialization
+            meta: undefined,
+          },
+          ganacheOptions,
+          title: this.test.title,
+          failOnConsoleError: false,
+          testSpecificMock: mockSentryMigratorError,
+        },
+        async ({ driver, mockedEndpoint }) => {
+          await driver.navigate();
+
+          // Wait for Sentry request
+          await driver.wait(async () => {
+            const isPending = await mockedEndpoint.isPending();
+            return isPending === false;
+          }, 3000);
+
+          const [mockedRequest] = await mockedEndpoint.getSeenRequests();
+          const mockTextBody = mockedRequest.body.text.split('\n');
+          const mockJsonBody = JSON.parse(mockTextBody[2]);
+          const appState = mockJsonBody?.extra?.appState;
+          assert.deepStrictEqual(appState, {});
+        },
+      );
+    });
+
     it('should send error events in UI', async function () {
       await withFixtures(
         {
@@ -190,6 +226,46 @@ describe('Sentry errors', function () {
           assert.equal(type, 'TestError');
           assert.equal(value, 'Test Error');
           assert.equal(level, 'error');
+        },
+      );
+    });
+
+    it('should capture UI application state', async function () {
+      await withFixtures(
+        {
+          fixtures: new FixtureBuilder()
+            .withMetaMetricsController({
+              metaMetricsId: 'fake-metrics-id',
+              participateInMetaMetrics: true,
+            })
+            .build(),
+          ganacheOptions,
+          title: this.test.title,
+          failOnConsoleError: false,
+          testSpecificMock: mockSentryTestError,
+        },
+        async ({ driver, mockedEndpoint }) => {
+          await driver.navigate();
+          await driver.fill('#password', 'correct horse battery staple');
+          await driver.press('#password', driver.Key.ENTER);
+          // Erase `getSentryState` hook, simulating a "before initialization" state
+          await driver.executeScript(
+            'window.stateHooks.getSentryState = undefined',
+          );
+
+          // Trigger error
+          await driver.executeScript('window.stateHooks.throwTestError()');
+
+          // Wait for Sentry request
+          await driver.wait(async () => {
+            const isPending = await mockedEndpoint.isPending();
+            return isPending === false;
+          }, 3000);
+          const [mockedRequest] = await mockedEndpoint.getSeenRequests();
+          const mockTextBody = mockedRequest.body.text.split('\n');
+          const mockJsonBody = JSON.parse(mockTextBody[2]);
+          const appState = mockJsonBody?.extra?.appState;
+          assert.deepStrictEqual(appState, {});
         },
       );
     });
@@ -308,6 +384,59 @@ describe('Sentry errors', function () {
       );
     });
 
+    it('should capture background application state', async function () {
+      await withFixtures(
+        {
+          fixtures: new FixtureBuilder()
+            .withMetaMetricsController({
+              metaMetricsId: 'fake-metrics-id',
+              participateInMetaMetrics: true,
+            })
+            .build(),
+          ganacheOptions,
+          title: this.test.title,
+          failOnConsoleError: false,
+          testSpecificMock: mockSentryTestError,
+        },
+        async ({ driver, mockedEndpoint }) => {
+          await driver.navigate();
+          await driver.fill('#password', 'correct horse battery staple');
+          await driver.press('#password', driver.Key.ENTER);
+
+          // Trigger error
+          await driver.executeScript(
+            'window.stateHooks.throwBackgroundTestError()',
+          );
+
+          // Wait for Sentry request
+          await driver.wait(async () => {
+            const isPending = await mockedEndpoint.isPending();
+            return isPending === false;
+          }, 3000);
+          const [mockedRequest] = await mockedEndpoint.getSeenRequests();
+          const mockTextBody = mockedRequest.body.text.split('\n');
+          const mockJsonBody = JSON.parse(mockTextBody[2]);
+          const appState = mockJsonBody?.extra?.appState;
+          assert.deepStrictEqual(Object.keys(appState), [
+            'browser',
+            'store',
+            'version',
+          ]);
+          assert.ok(
+            typeof appState?.browser === 'string' &&
+              appState?.browser.length > 0,
+            'Invalid browser state',
+          );
+          assert.ok(
+            typeof appState?.version === 'string' &&
+              appState?.version.length > 0,
+            'Invalid version state',
+          );
+          assert.deepStrictEqual(Object.keys(appState?.store), ['metamask']);
+        },
+      );
+    });
+
     it('should send error events in UI', async function () {
       await withFixtures(
         {
@@ -346,6 +475,62 @@ describe('Sentry errors', function () {
           assert.equal(value, 'Test Error');
           assert.equal(level, 'error');
           assert.equal(participateInMetaMetrics, true);
+        },
+      );
+    });
+
+    it('should capture UI application state', async function () {
+      await withFixtures(
+        {
+          fixtures: new FixtureBuilder()
+            .withMetaMetricsController({
+              metaMetricsId: 'fake-metrics-id',
+              participateInMetaMetrics: true,
+            })
+            .build(),
+          ganacheOptions,
+          title: this.test.title,
+          failOnConsoleError: false,
+          testSpecificMock: mockSentryTestError,
+        },
+        async ({ driver, mockedEndpoint }) => {
+          await driver.navigate();
+          await driver.fill('#password', 'correct horse battery staple');
+          await driver.press('#password', driver.Key.ENTER);
+
+          // Trigger error
+          await driver.executeScript('window.stateHooks.throwTestError()');
+
+          // Wait for Sentry request
+          await driver.wait(async () => {
+            const isPending = await mockedEndpoint.isPending();
+            return isPending === false;
+          }, 3000);
+          const [mockedRequest] = await mockedEndpoint.getSeenRequests();
+          const mockTextBody = mockedRequest.body.text.split('\n');
+          const mockJsonBody = JSON.parse(mockTextBody[2]);
+          const appState = mockJsonBody?.extra?.appState;
+          assert.deepStrictEqual(Object.keys(appState), [
+            'browser',
+            'store',
+            'version',
+          ]);
+          assert.ok(
+            typeof appState?.browser === 'string' &&
+              appState?.browser.length > 0,
+            'Invalid browser state',
+          );
+          assert.ok(
+            typeof appState?.version === 'string' &&
+              appState?.version.length > 0,
+            'Invalid version state',
+          );
+          assert.deepStrictEqual(Object.keys(appState?.store), [
+            'unconnectedAccount',
+            'metamask',
+            'history',
+            'gas',
+          ]);
         },
       );
     });
