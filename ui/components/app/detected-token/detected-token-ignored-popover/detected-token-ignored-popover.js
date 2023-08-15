3ff37383e626d65674c32ceb433e3d5713be33de
georgewrmarshall <george.marshall@consensys.net>
2023-08-14 17:52:24 -0700

    UI updates and removing unused CSS

--- ui/components/app/detected-token/detected-token-ignored-popover/detected-token-ignored-popover.js
+++ ui/components/app/detected-token/detected-token-ignored-popover/detected-token-ignored-popover.js
@@ -6,7 +6,6 @@ import { useI18nContext } from '../../../../hooks/useI18nContext';
 import {
   Display,
   JustifyContent,
-  TextVariant,
 } from '../../../../helpers/constants/design-system';
 import {
   Text,
@@ -24,70 +23,56 @@ const DetectedTokenIgnoredPopover = ({
   partiallyIgnoreDetectedTokens,
   onCancelIgnore,
   handleClearTokensSelection,
+  isOpen,
 }) => {
   const t = useI18nContext();
-
-  const footer = (
-    <>
-      <Box
-        display={Display.Flex}
-        justifyContent={JustifyContent.center}
-        marginLeft={4}
-        marginRight={4}
-      >
-        <Button
-          className="detected-token-ignored-popover__ignore-button"
-          block
-          variant={BUTTON_VARIANT.SECONDARY}
-          onClick={onCancelIgnore}
-          size={BUTTON_SIZES.LG}
-        >
-          {t('cancel')}
-        </Button>
-        <Button
-          className="detected-token-ignored-popover__import-button"
-          block
-          variant={BUTTON_VARIANT.PRIMARY}
-          onClick={handleClearTokensSelection}
-          size={BUTTON_SIZES.LG}
-        >
-          {t('confirm')}
-        </Button>
-      </Box>
-    </>
-  );
-
   return (
     <Modal
-      isOpen
+      isOpen={isOpen}
       className={classNames('detected-token-ignored-popover', {
         'detected-token-ignored-popover--import': partiallyIgnoreDetectedTokens,
         'detected-token-ignored-popover--ignore':
           !partiallyIgnoreDetectedTokens,
       })}
       onClose={onCancelIgnore}
+      autoFocus={false}
     >
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader onClose={onCancelIgnore} marginBottom={4}>
+        <ModalHeader marginBottom={4}>
           {partiallyIgnoreDetectedTokens
             ? t('importSelectedTokens')
             : t('areYouSure')}
         </ModalHeader>
-        <Text
-          variant={TextVariant.bodySm}
-          as="h6"
-          marginTop={0}
-          marginRight={5}
-          marginBottom={7}
-          marginLeft={5}
-        >
+        <Text marginBottom={4}>
           {partiallyIgnoreDetectedTokens
             ? t('importSelectedTokensDescription')
             : t('ignoreTokenWarning')}
         </Text>
-
-        {footer}
+        <Box
+          display={Display.Flex}
+          justifyContent={JustifyContent.center}
+          gap={4}
+        >
+          <Button
+            className="detected-token-ignored-popover__ignore-button"
+            block
+            variant={BUTTON_VARIANT.SECONDARY}
+            onClick={onCancelIgnore}
+            size={BUTTON_SIZES.LG}
+          >
+            {t('cancel')}
+          </Button>
+          <Button
+            className="detected-token-ignored-popover__import-button"
+            block
+            variant={BUTTON_VARIANT.PRIMARY}
+            onClick={handleClearTokensSelection}
+            size={BUTTON_SIZES.LG}
+          >
+            {t('confirm')}
+          </Button>
+        </Box>
       </ModalContent>
     </Modal>
   );
@@ -97,6 +82,7 @@ DetectedTokenIgnoredPopover.propTypes = {
   partiallyIgnoreDetectedTokens: PropTypes.bool.isRequired,
   onCancelIgnore: PropTypes.func.isRequired,
   handleClearTokensSelection: PropTypes.func.isRequired,
+  isOpen: PropTypes.bool.isRequired,
 };
 
 export default DetectedTokenIgnoredPopover;
