b874a301f5ac855494d4fb5868d96994c41ce584
Dan J Miller <danjm.com@gmail.com>
2023-08-16 16:56:20 -0230

    Capture exception with sentry when invariant conditions are met in migrations (#20427)
    * capture exception for sentry when invariant conditions are met in migration 82
    
    * Code cleanup
    
    * Capture exceptions in invariant conditions for migrations 83,84,85,86,89,91,93,94
    
    * Update app/scripts/migrations/082.test.js
    
    Co-authored-by: Mark Stacey <markjstacey@gmail.com>
    
    * Code cleanup
    
    * Fix SentryObject type declaration
    
    * Stop throwing error if preferences controller is undefined
    
    * Refactor 084 and 086 to remove double negative
    
    * Capture exceptions for invariant states in in migrations 87,88,90 and 92
    
    * lint fix
    
    * log warning in migration 82 when preferences controller is undefined
    
    ---------
    
    Co-authored-by: Mark Stacey <markjstacey@gmail.com>

--- app/scripts/migrations/091.test.ts
+++ app/scripts/migrations/091.test.ts
@@ -1,6 +1,15 @@
 import { cloneDeep } from 'lodash';
 import { migrate, version } from './091';
 
+const sentryCaptureExceptionMock = jest.fn();
+
+global.sentry = {
+  startSession: jest.fn(),
+  endSession: jest.fn(),
+  toggleSession: jest.fn(),
+  captureException: sentryCaptureExceptionMock,
+};
+
 jest.mock('uuid', () => {
   const actual = jest.requireActual('uuid');
 
@@ -11,6 +20,10 @@ jest.mock('uuid', () => {
 });
 
 describe('migration #91', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
   it('should update the version metadata', async () => {
     const oldStorage = {
       meta: {
@@ -40,6 +53,25 @@ describe('migration #91', () => {
     expect(newStorage.data).toStrictEqual(oldData);
   });
 
+  it('should capture an exception if there is no network controller state', async () => {
+    const oldData = {
+      other: 'data',
+    };
+    const oldStorage = {
+      meta: {
+        version: 90,
+      },
+      data: oldData,
+    };
+
+    await migrate(oldStorage);
+
+    expect(sentryCaptureExceptionMock).toHaveBeenCalledTimes(1);
+    expect(sentryCaptureExceptionMock).toHaveBeenCalledWith(
+      new Error(`typeof state.NetworkController is undefined`),
+    );
+  });
+
   it('should return state unaltered if there is no network controller networkConfigurations state', async () => {
     const oldData = {
       other: 'data',
@@ -58,6 +90,32 @@ describe('migration #91', () => {
 
     const newStorage = await migrate(cloneDeep(oldStorage));
     expect(newStorage.data).toStrictEqual(oldData);
+  });
+
+  it('should capture an exception if there is no network controller networkConfigurations state', async () => {
+    const oldData = {
+      other: 'data',
+      NetworkController: {
+        providerConfig: {
+          foo: 'bar',
+        },
+      },
+    };
+    const oldStorage = {
+      meta: {
+        version: 90,
+      },
+      data: oldData,
+    };
+
+    await migrate(oldStorage);
+
+    expect(sentryCaptureExceptionMock).toHaveBeenCalledTimes(1);
+    expect(sentryCaptureExceptionMock).toHaveBeenCalledWith(
+      new Error(
+        `typeof state.NetworkController.networkConfigurations is undefined`,
+      ),
+    );
   });
 
   it('should return state unaltered if the networkConfigurations all have a chainId', async () => {
