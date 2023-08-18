MetaMask/metamask-extension

commit 9cfa9ba6b0a855faa17a48e0dc1ea27bc65b8c8b

Fix pre-initialization UI error state capture (#20529)
In the case where an error is thrown in the UI before initialization
has finished, we aren't capturing the application state correctly for
Sentry errors. We had a test case for this, but the test case was
broken due to a mistake in how the `network-store` was setup (it was
not matching the behavior of the real `local-tstore` module).

The pre-initialization state capture logic was updated to rely solely
upon the `localStore` instance used by Sentry to determine whether the
user had opted-in to metrics or not. This simplifies the logic a great
deal, removing the need for the `getMostRecentPersistedState` state
hook. It also ensures that state is captured corretly pre-
initialization in both the background and UI.

app/scripts/background.js
@@ -71,12 +71,6 @@ import DesktopManager from '@metamask/desktop/dist/desktop-manager';
 ///: END:ONLY_INCLUDE_IN
 /* eslint-enable import/order */
 
-// Setup global hook for improved Sentry state snapshots during initialization
-const inTest = process.env.IN_TEST;
-const localStore = inTest ? new ReadOnlyNetworkStore() : new LocalStore();
-global.stateHooks.getMostRecentPersistedState = () =>
-  localStore.mostRecentRetrievedState;
-
 const { sentry } = global;
 const firstTimeState = { ...rawFirstTimeState };
 
@@ -100,6 +94,9 @@ const openMetamaskTabsIDs = {};
 const requestAccountTabIds = {};
 let controller;
 
+// state persistence
+const inTest = process.env.IN_TEST;
+const localStore = inTest ? new ReadOnlyNetworkStore() : new LocalStore();
 let versionedData;
 
 if (inTest || process.env.METAMASK_DEBUG) {

app/scripts/lib/setup-initial-state-hooks.js
@@ -5,7 +5,9 @@ import ReadOnlyNetworkStore from './network-store';
 import { SENTRY_BACKGROUND_STATE } from './setupSentry';
 
 const platform = new ExtensionPlatform();
-const localStore = process.env.IN_TEST
+
+// This instance of `localStore` is used by Sentry to get the persisted state
+const sentryLocalStore = process.env.IN_TEST
   ? new ReadOnlyNetworkStore()
   : new LocalStore();
 
@@ -15,7 +17,7 @@ const localStore = process.env.IN_TEST
  * @returns The persisted wallet state.
  */
 globalThis.stateHooks.getPersistedState = async function () {
-  return await localStore.get();
+  return await sentryLocalStore.get();
 };
 
 const persistedStateMask = {
@@ -37,25 +39,30 @@ globalThis.stateHooks.getSentryState = function () {
     browser: window.navigator.userAgent,
     version: platform.getVersion(),
   };
+  // If `getSentryAppState` is set, it implies that initialization has completed
   if (globalThis.stateHooks.getSentryAppState) {
     return {
       ...sentryState,
       state: globalThis.stateHooks.getSentryAppState(),
     };
-  } else if (globalThis.stateHooks.getMostRecentPersistedState) {
-    const persistedState = globalThis.stateHooks.getMostRecentPersistedState();
-    if (persistedState) {
-      return {
-        ...sentryState,
-        persistedState: maskObject(
-          // `getMostRecentPersistedState` is used here instead of
-          // `getPersistedState` to avoid making this an asynchronous function.
-          globalThis.stateHooks.getMostRecentPersistedState(),
-          persistedStateMask,
-        ),
-      };
-    }
-    return sentryState;
+  } else if (
+    // This is truthy if Sentry has retrieved state at least once already. This
+    // should always be true because Sentry calls `getPersistedState` during
+    // error processing (before this function is called) if `getSentryAppState`
+    // hasn't been set yet.
+    sentryLocalStore.mostRecentRetrievedState
+  ) {
+    return {
+      ...sentryState,
+      persistedState: maskObject(
+        sentryLocalStore.mostRecentRetrievedState,
+        persistedStateMask,
+      ),
+    };
   }
+  // This branch means that local storage has not yet been read, so we have
+  // no choice but to omit the application state.
+  // This should be unreachable, unless an error was encountered during error
+  // processing.
   return sentryState;
 };

app/scripts/lib/network-store.js
@@ -31,7 +31,6 @@ export default class ReadOnlyNetworkStore {
       const response = await fetchWithTimeout(FIXTURE_SERVER_URL);
       if (response.ok) {
         this._state = await response.json();
-        this.mostRecentRetrievedState = this._state;
       }
     } catch (error) {
       log.debug(`Error loading network state: '${error.message}'`);
@@ -48,6 +47,11 @@ export default class ReadOnlyNetworkStore {
   async get() {
     if (!this._initialized) {
       await this._initializing;
+    }
+    // Delay setting this until after the first read, to match the
+    // behavior of the local store.
+    if (!this.mostRecentRetrievedState) {
+      this.mostRecentRetrievedState = this._state;
     }
     return this._state;
   }

app/scripts/ui.js
@@ -33,14 +33,6 @@ import ExtensionPlatform from './platforms/extension';
 import { setupMultiplex } from './lib/stream-utils';
 import { getEnvironmentType, getPlatform } from './lib/util';
 import metaRPCClientFactory from './lib/metaRPCClientFactory';
-import LocalStore from './lib/local-store';
-import ReadOnlyNetworkStore from './lib/network-store';
-
-// Setup global hook for improved Sentry state snapshots during initialization
-const inTest = process.env.IN_TEST;
-const localStore = inTest ? new ReadOnlyNetworkStore() : new LocalStore();
-global.stateHooks.getMostRecentPersistedState = () =>
-  localStore.mostRecentRetrievedState;
 
 const container = document.getElementById('app-content');
 

commit 94835329776faee0a3e4295c54cdfdbba2ccc81f

Fix Sentry breadcrumb collection during initialization (again)
Sentry breadcrumb collection during initialization was broken in #20529
because we failed to consider that the `getSentryState` check was also
used for an opt-in check in the `beforeBreadcrumb` hook.

I had assumed that `getSentryState` was only used to get state to add
additional context to an error report. But the function has a second
purpose: to get state for the purposes of checking whether the user has
opted into MetaMetrics. In this second case, `mostRecentRetrievedState`
is sometimes unset (which violates an assumption made in #20529)

The `getMostRecentPersistedState` hook removed in #20529 has been
restored, ensuring that the `getSentryState` function returns Sentry
state after loading state for the first time, but before the first
error has occurred.

This mistake didn't cause e2e tests to fail because multiple errors are
currently thrown in the background upon initialization on `develop`
(relating to Snow scuttling). These errors were early enough that they
happened before the console logs that our breadcrumb test was testing
for. When #20529 was ported onto the v10.34.5 RC, these errors were not
present so the test failed correctly.

app/scripts/background.js
@@ -71,6 +71,12 @@ import DesktopManager from '@metamask/desktop/dist/desktop-manager';
 ///: END:ONLY_INCLUDE_IN
 /* eslint-enable import/order */
 
+// Setup global hook for improved Sentry state snapshots during initialization
+const inTest = process.env.IN_TEST;
+const localStore = inTest ? new ReadOnlyNetworkStore() : new LocalStore();
+global.stateHooks.getMostRecentPersistedState = () =>
+  localStore.mostRecentRetrievedState;
+
 const { sentry } = global;
 const firstTimeState = { ...rawFirstTimeState };
 
@@ -93,10 +99,6 @@ let uiIsTriggering = false;
 const openMetamaskTabsIDs = {};
 const requestAccountTabIds = {};
 let controller;
-
-// state persistence
-const inTest = process.env.IN_TEST;
-const localStore = inTest ? new ReadOnlyNetworkStore() : new LocalStore();
 let versionedData;
 
 if (inTest || process.env.METAMASK_DEBUG) {

app/scripts/lib/setup-initial-state-hooks.js
@@ -28,9 +28,12 @@ const persistedStateMask = {
 };
 
 /**
- * Get a state snapshot to include with Sentry error reports. This uses the
- * persisted state pre-initialization, and the in-memory state post-
- * initialization. In both cases the state is anonymized.
+ * Get a state snapshot for Sentry. This is used to add additional context to
+ * error reports, and it's used when processing errors and breadcrumbs to
+ * determine whether the user has opted into Metametrics.
+ *
+ * This uses the persisted state pre-initialization, and the in-memory state
+ * post-initialization. In both cases the state is anonymized.
  *
  * @returns A Sentry state snapshot.
  */
@@ -47,22 +50,27 @@ globalThis.stateHooks.getSentryState = function () {
     };
   } else if (
     // This is truthy if Sentry has retrieved state at least once already. This
-    // should always be true because Sentry calls `getPersistedState` during
-    // error processing (before this function is called) if `getSentryAppState`
-    // hasn't been set yet.
-    sentryLocalStore.mostRecentRetrievedState
+    // should always be true when getting context for an error report, but can
+    // be unset when Sentry is performing the opt-in check.
+    sentryLocalStore.mostRecentRetrievedState ||
+    // This is only set in the background process.
+    globalThis.stateHooks.getMostRecentPersistedState
   ) {
-    return {
-      ...sentryState,
-      persistedState: maskObject(
-        sentryLocalStore.mostRecentRetrievedState,
-        persistedStateMask,
-      ),
-    };
+    const persistedState =
+      sentryLocalStore.mostRecentRetrievedState ||
+      globalThis.stateHooks.getMostRecentPersistedState();
+    // This can be unset when this method is called in the background for an
+    // opt-in check, but the state hasn't been loaded yet.
+    if (persistedState) {
+      return {
+        ...sentryState,
+        persistedState: maskObject(persistedState, persistedStateMask),
+      };
+    }
   }
   // This branch means that local storage has not yet been read, so we have
   // no choice but to omit the application state.
-  // This should be unreachable, unless an error was encountered during error
-  // processing.
+  // This should be unreachable when getting context for an error report, but
+  // can be false when Sentry is performing the opt-in check.
   return sentryState;
 };