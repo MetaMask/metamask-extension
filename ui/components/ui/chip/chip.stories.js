3a542eaf8de0500108a1e52bcf9d91ef0b629a0f
Jase Balderrama <jasebalderrama@icloud.com>
2023-08-25 21:38:36 -0700

    Add propTypes for <Deprecated/> wrapper

--- ui/components/ui/chip/chip.stories.js
+++ ui/components/ui/chip/chip.stories.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
 import {
   TypographyVariant,
@@ -103,6 +104,10 @@ const Deprecated = ({ children }) => (
     {children}
   </>
 );
+
+Deprecated.propTypes = {
+  children: PropTypes.node,
+};
 
 export const DefaultStory = (args) => (
   <Deprecated>
