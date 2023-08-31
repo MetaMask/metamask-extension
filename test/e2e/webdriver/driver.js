c715a0559c343b36b367f73d28c39f8364cb549b
legobt <6wbvkn0j@anonaddy.me>
2023-08-31 02:32:26 +0000

    lint: allow restricted globals in test webdriver collectMetrics

--- test/e2e/webdriver/driver.js
+++ test/e2e/webdriver/driver.js
@@ -546,6 +546,7 @@ function logBrowserError(failOnConsoleError, errorMessage) {
   }
 }
 
+/* eslint-disable no-restricted-globals */
 function collectMetrics() {
   const results = {
     paint: {},
