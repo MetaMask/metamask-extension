7906f181049f8578b768299ad40672e84189883e
Jyoti Puri <jyotipuri@gmail.com>
2023-09-05 15:43:43 +0530

    Blockaid validation should be done on ethereum mainnet only (#20709)

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -4076,7 +4076,11 @@ export default class MetamaskController extends EventEmitter {
 
     ///: BEGIN:ONLY_INCLUDE_IN(blockaid)
     engine.push(
-      createPPOMMiddleware(this.ppomController, this.preferencesController),
+      createPPOMMiddleware(
+        this.ppomController,
+        this.preferencesController,
+        this.networkController,
+      ),
     );
     ///: END:ONLY_INCLUDE_IN
 
