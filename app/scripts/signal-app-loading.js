4cb8ad78a559d2c70639af8553bed4f8806decfd
David Walsh <davidwalsh83@gmail.com>
2023-09-07 10:10:22 -0500

    Use '+' character and green background, fix lint

--- app/scripts/signal-app-loading.js
+++ app/scripts/signal-app-loading.js
@@ -1,5 +1,7 @@
-if(chrome?.browserAction) {
-  chrome.browserAction.setBadgeText({ text: '°' });
-} else if(browser?.browserAction) {
-  browser.browserAction.setBadgeText({ text: '°' });
-}
+// eslint-disable-next-line import/unambiguous
+'use strict';
+
+(({ browserAction }) => {
+  browserAction.setBadgeText({ text: '+' });
+  browserAction.setBadgeBackgroundColor({ color: '#5DD879' });
+})(window.chrome ?? window.browser);
