e89048222895b5150794f2a2284c5c918e7c95e2
Vinicius Stevam <45455812+vinistevam@users.noreply.github.com>
2023-09-13 09:15:26 +0100

    fix: error during persisted transaction approval (#20626)
    This pull request addresses the issue that occurs when a pending transaction is present, the extension is disabled and then re-enabled, and subsequently, the user rejects the transaction. The goal is to prevent the logging of the error message "User rejected the request", while still capturing and logging any other types of errors.
    
    Fixes https://github.com/MetaMask/metamask-extension/issues/20604

--- app/scripts/controllers/transactions/index.js
+++ app/scripts/controllers/transactions/index.js
@@ -346,6 +346,9 @@ export default class TransactionController extends EventEmitter {
       this._requestTransactionApproval(txMeta, {
         shouldShowRequest: false,
       }).catch((error) => {
+        if (error.code === errorCodes.provider.userRejectedRequest) {
+          return;
+        }
         log.error('Error during persisted transaction approval', error);
       });
     });
