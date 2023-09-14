d005c61b8a47258e291d0e792341ff40f48ef0f2
Antonio Regadas <antonio.regadas@consensys.net>
2023-09-14 17:07:38 +0100

    prettier

--- ui/helpers/utils/institutional/find-by-custodian-name.test.ts
+++ ui/helpers/utils/institutional/find-by-custodian-name.test.ts
@@ -18,19 +18,21 @@ describe('findCustodianByDisplayName', () => {
     },
     {
       type: 'JSONRPC',
-      iconUrl: 'https://saturn-custody-ui.dev.metamask-institutional.io/saturn.svg',
+      iconUrl:
+        'https://saturn-custody-ui.dev.metamask-institutional.io/saturn.svg',
       name: 'Saturn Custody',
       website: 'https://saturn-custody-ui.dev.metamask-institutional.io/',
       envName: 'saturn',
       apiUrl: 'https://saturn-custody.dev.metamask-institutional.io/eth',
       displayName: null,
       production: false,
-      refreshTokenUrl: 'https://saturn-custody.dev.metamask-institutional.io/oauth/token',
+      refreshTokenUrl:
+        'https://saturn-custody.dev.metamask-institutional.io/oauth/token',
       websocketApiUrl: 'wss://websocket.dev.metamask-institutional.io/v1/ws',
       isNoteToTraderSupported: true,
       version: 2,
-    }
-  ]
+    },
+  ];
   it('should return the custodian if the display name is found in custodianKey', () => {
     const displayName = 'Qredo';
     const custodian = findCustodianByDisplayName(displayName, custodians);
