03ad2a54f401706eaa06f0a03266063d465d4293
Owen Craston <owengc12@gmail.com>
2023-09-18 18:41:47 -0700

    Snap account removal confirmation flow
    - in the removeAccount callback, request user approval and launch the
      new snap account removal confirmation flow

---
+++ ui/pages/remove-snap-account/remove-snap-account.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Box } from '../../components/component-library';
+import {
+  AlignItems,
+  BackgroundColor,
+  BlockSize,
+  BorderStyle,
+  Display,
+  FlexDirection,
+  JustifyContent,
+} from '../../helpers/constants/design-system';
+import { RemoveSnapAccountContent } from './components';
+
+interface CreateSnapAccountProps {
+  snapId: string;
+  snapName: string;
+  onAccountNameChange: (value: string) => void;
+}
+
+const RemoveSnapAccount = ({ snapId, snapName }: CreateSnapAccountProps) => {
+  return (
+    <Box
+      className="remove-snap-account-page"
+      height={BlockSize.Full}
+      width={BlockSize.Full}
+      backgroundColor={BackgroundColor.backgroundDefault}
+      borderStyle={BorderStyle.none}
+      flexDirection={FlexDirection.Column}
+      alignItems={AlignItems.center}
+      padding={[0, 4]}
+    >
+      <Box
+        display={Display.Flex}
+        flexDirection={FlexDirection.Column}
+        height={BlockSize.Full}
+      >
+        <Box
+          display={Display.Flex}
+          flexDirection={FlexDirection.Column}
+          justifyContent={JustifyContent.center}
+          alignItems={AlignItems.center}
+          height={BlockSize.Full}
+        >
+          <RemoveSnapAccountContent snapName={snapName} snapId={snapId} />
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default RemoveSnapAccount;
