862cadd9e8be0079309418e6fbe555c4308c88c5
Matthew Walsh <matthew.walsh@consensys.net>
2023-09-19 15:18:46 +0100

    Add source ID constants

--- ui/components/app/modals/contract-details-modal/contract-details-modal.js
+++ ui/components/app/modals/contract-details-modal/contract-details-modal.js
@@ -28,6 +28,7 @@ import NftCollectionImage from '../../../ui/nft-collection-image/nft-collection-
 import { ButtonIcon, IconName, Text } from '../../../component-library';
 import Name from '../../name/name';
 import { usePetnamesEnabled } from '../../../../hooks/usePetnamesEnabled';
+import { DEFAULT_NAME_SOURCE_PRIORITY } from '../../../../../shared/constants/names';
 
 export default function ContractDetailsModal({
   onClose,
@@ -220,7 +221,7 @@ export default function ContractDetailsModal({
                 <Name
                   value={toAddress}
                   type={NameType.ETHEREUM_ADDRESS}
-                  sourcePriority={['ens', 'lens', 'token', 'etherscan']}
+                  sourcePriority={DEFAULT_NAME_SOURCE_PRIORITY}
                 />
               </Text>
             ) : (
