5d1857d993a2c73ea635992ac677860f0423f966
Frederik Bolding <frederik.bolding@gmail.com>
2023-09-21 15:23:44 +0200

    Fix updateCaveat not being exposed in the background API

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -2733,6 +2733,7 @@ export default class MetamaskController extends EventEmitter {
       ),
       dismissNotifications: this.dismissNotifications.bind(this),
       markNotificationsAsRead: this.markNotificationsAsRead.bind(this),
+      updateCaveat: this.updateCaveat.bind(this),
       ///: END:ONLY_INCLUDE_IN
       ///: BEGIN:ONLY_INCLUDE_IN(keyring-snaps)
       updateSnapRegistry: this.preferencesController.updateSnapRegistry.bind(
