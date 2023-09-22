aafb30fdf0b57043626123f5f49f7cfe49a5f2b6
Pierre Bertet <hi@bpier.re>
2023-09-22 16:30:49 +0100

    Move from @download/blockies to blo

--- ui/components/ui/identicon/blockieIdenticon/blockieIdenticon.component.js
+++ ui/components/ui/identicon/blockieIdenticon/blockieIdenticon.component.js
@@ -1,34 +1,18 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { renderIcon } from '@download/blockies';
+import { blo } from 'blo';
 
 const BlockieIdenticon = ({ address, diameter, alt = '', borderRadius }) => {
-  const [dataUrl, setDataUrl] = useState(null);
-  const canvasRef = useRef(null);
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    renderIcon({ seed: address.toLowerCase() }, canvas);
-    const updatedDataUrl = canvas.toDataURL();
-
-    if (updatedDataUrl !== dataUrl) {
-      setDataUrl(updatedDataUrl);
-    }
-  }, [dataUrl, address]);
-
   return (
-    <>
-      <canvas ref={canvasRef} style={{ display: 'none' }} />
-      <img
-        src={dataUrl}
-        height={diameter}
-        width={diameter}
-        style={{
-          borderRadius,
-        }}
-        alt={alt}
-      />
-    </>
+    <img
+      src={blo(address)}
+      height={diameter}
+      width={diameter}
+      style={{
+        borderRadius,
+      }}
+      alt={alt}
+    />
   );
 };
 
