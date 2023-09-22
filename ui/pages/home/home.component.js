b3ce4007ad02a3301e6e18f5c8465674b08bee9f
NidhiKJha <menidhikjha@gmail.com>
2023-09-22 17:15:42 +0530

    lint fix

--- ui/pages/home/home.component.js
+++ ui/pages/home/home.component.js
@@ -63,9 +63,6 @@ import {
   BUILD_QUOTE_ROUTE,
   VIEW_QUOTE_ROUTE,
   CONFIRMATION_V_NEXT_ROUTE,
-  ///: BEGIN:ONLY_INCLUDE_IN(build-main,build-beta,build-flask)
-  ONBOARDING_SECURE_YOUR_WALLET_ROUTE,
-  ///: END:ONLY_INCLUDE_IN
   ///: BEGIN:ONLY_INCLUDE_IN(build-mmi)
   CONFIRM_ADD_CUSTODIAN_TOKEN,
   INTERACTIVE_REPLACEMENT_TOKEN_PAGE,
@@ -126,7 +123,6 @@ export default class Home extends PureComponent {
     hasWatchNftPendingApprovals: PropTypes.bool,
     setConnectedStatusPopoverHasBeenShown: PropTypes.func,
     ///: BEGIN:ONLY_INCLUDE_IN(build-main,build-beta,build-flask)
-    shouldShowSeedPhraseReminder: PropTypes.bool.isRequired,
     isPopup: PropTypes.bool,
     connectedStatusPopoverHasBeenShown: PropTypes.bool,
     showRecoveryPhraseReminder: PropTypes.bool.isRequired,
@@ -403,11 +399,6 @@ export default class Home extends PureComponent {
     const { t } = this.context;
 
     const {
-      ///: BEGIN:ONLY_INCLUDE_IN(build-main,build-beta,build-flask)
-      history,
-      shouldShowSeedPhraseReminder,
-      isPopup,
-      ///: END:ONLY_INCLUDE_IN
       shouldShowWeb3ShimUsageNotification,
       setWeb3ShimUsageAlertDismissed,
       originOfCurrentTab,
