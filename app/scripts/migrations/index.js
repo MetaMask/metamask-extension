95e22bc482e112848300925032fc8e649f7372d0
Matthew Walsh <matthew.walsh@consensys.net>
2023-09-27 11:34:02 +0100

    Add migration
    Add unit tests.

--- app/scripts/migrations/index.js
+++ app/scripts/migrations/index.js
@@ -105,6 +105,7 @@ import * as m095 from './095';
 import * as m096 from './096';
 import * as m097 from './097';
 import * as m098 from './098';
+import * as m100 from './100';
 
 const migrations = [
   m002,
@@ -207,5 +208,6 @@ const migrations = [
   m096,
   m097,
   m098,
+  m100,
 ];
 export default migrations;
