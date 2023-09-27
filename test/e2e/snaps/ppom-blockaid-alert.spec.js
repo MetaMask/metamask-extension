d871bc1931bee2b4b4662419b9c58fb80304c701
digiwand <20778143+digiwand@users.noreply.github.com>
2023-09-28 00:04:44 +0200

    test:e2e: use TestDapp btns for ppom warned tx

--- test/e2e/snaps/ppom-blockaid-alert.spec.js
+++ test/e2e/snaps/ppom-blockaid-alert.spec.js
@@ -559,126 +559,70 @@ describe('Confirmation Security Alert - Blockaid', function () {
 
         const expectedTitle = 'This is a deceptive request';
 
-        // Configs sorted by expectedReason
         const testMaliciousConfigs = [
-          // {
-          //   expectedDescription:
-          //   'If you approve this request, a third party known for scams might take all your assets.',
-          // expectedReason: 'approval_farming',
-          //   method: 'eth_sendTransaction',
-          //   params: [
-          //     {
-          //       to: '0x4fabb145d64652a948d72533023f6e7a623c7c53',
-          //       from: selectedAddress,
-          //       data: `0x095ea7b3000000000000000000000000e50a2dbc466d01a34c3e8b7e8e45fce4f7da39e6fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff`,
-          //     },
-          //   ],
-          // },
           {
+            btnSelector: 'maliciousApprovalButton',
             expectedDescription:
               'If you approve this request, a third party known for scams might take all your assets.',
-            expectedReason: 'permit_farming',
-            method: 'eth_signTypedData_v4',
-            params: [
-              selectedAddress,
-              '{"types":{"EIP712Domain":[{"name":"name","type":"string"},{"name":"version","type":"string"},{"name":"chainId","type":"uint256"},{"name":"verifyingContract","type":"address"}],"Permit":[{"name":"owner","type":"address"},{"name":"spender","type":"address"},{"name":"value","type":"uint256"},{"name":"nonce","type":"uint256"},{"name":"deadline","type":"uint256"}]},"primaryType":"Permit","domain":{"name":"USD Coin","verifyingContract":"0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48","chainId":1,"version":"2"},"message":{"owner":"0x12ED7f6ed0491678764c2b222A58452926E44DB6","spender":"0x1661F1B207629e4F385DA89cFF535C8E5Eb23Ee3","value":"1033366316628","nonce":1,"deadline":1678709555}}',
-            ],
+            expectedReason: 'approval_farming',
           },
-          // PPOM Error: block does not exist
-          // {
-          //   expectedDescription:
-          //     'If you approve this request, you might lose your assets.',
-          //   expectedReason: 'raw_ether_transfer',
-          //   method: 'eth_sendTransaction',
-          //   params: [
-          //     {
-          // // added from param
-          //       from: selectedAddress,
-          //       to: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
-          //       value: '0x9184e72a000',
-          //     },
-          //   ],
-          // },
-          {
-            expectedDescription:
-              'If you approve this request, someone can steal your assets listed on OpenSea.',
-            expectedReason: 'seaport_farming',
-            method: 'eth_signTypedData_v4',
-            params: [
-              selectedAddress,
-              '{"types":{"OrderComponents":[{"name":"offerer","type":"address"},{"name":"zone","type":"address"},{"name":"offer","type":"OfferItem[]"},{"name":"consideration","type":"ConsiderationItem[]"},{"name":"orderType","type":"uint8"},{"name":"startTime","type":"uint256"},{"name":"endTime","type":"uint256"},{"name":"zoneHash","type":"bytes32"},{"name":"salt","type":"uint256"},{"name":"conduitKey","type":"bytes32"},{"name":"counter","type":"uint256"}],"OfferItem":[{"name":"itemType","type":"uint8"},{"name":"token","type":"address"},{"name":"identifierOrCriteria","type":"uint256"},{"name":"startAmount","type":"uint256"},{"name":"endAmount","type":"uint256"}],"ConsiderationItem":[{"name":"itemType","type":"uint8"},{"name":"token","type":"address"},{"name":"identifierOrCriteria","type":"uint256"},{"name":"startAmount","type":"uint256"},{"name":"endAmount","type":"uint256"},{"name":"recipient","type":"address"}],"EIP712Domain":[{"name":"name","type":"string"},{"name":"version","type":"string"},{"name":"chainId","type":"uint256"},{"name":"verifyingContract","type":"address"}]},"domain":{"name":"Seaport","version":"1.1","chainId":"1","verifyingContract":"0x00000000006c3852cbef3e08e8df289169ede581"},"primaryType":"OrderComponents","message":{"offerer":"0x5a6f5477bdeb7801ba137a9f0dc39c0599bac994","zone":"0x004c00500000ad104d7dbd00e3ae0a5c00560c00","offer":[{"itemType":"2","token":"0x60e4d786628fea6478f785a6d7e704777c86a7c6","identifierOrCriteria":"26464","startAmount":"1","endAmount":"1"},{"itemType":"2","token":"0x60e4d786628fea6478f785a6d7e704777c86a7c6","identifierOrCriteria":"7779","startAmount":"1","endAmount":"1"},{"itemType":"2","token":"0x60e4d786628fea6478f785a6d7e704777c86a7c6","identifierOrCriteria":"4770","startAmount":"1","endAmount":"1"},{"itemType":"2","token":"0xba30e5f9bb24caa003e9f2f0497ad287fdf95623","identifierOrCriteria":"9594","startAmount":"1","endAmount":"1"},{"itemType":"2","token":"0xba30e5f9bb24caa003e9f2f0497ad287fdf95623","identifierOrCriteria":"2118","startAmount":"1","endAmount":"1"},{"itemType":"2","token":"0xba30e5f9bb24caa003e9f2f0497ad287fdf95623","identifierOrCriteria":"1753","startAmount":"1","endAmount":"1"}],"consideration":[{"itemType":"2","token":"0x60e4d786628fea6478f785a6d7e704777c86a7c6","identifierOrCriteria":"26464","startAmount":"1","endAmount":"1","recipient":"0xdfdc0b1cf8e9950d6a860af6501c4fecf7825cc1"},{"itemType":"2","token":"0x60e4d786628fea6478f785a6d7e704777c86a7c6","identifierOrCriteria":"7779","startAmount":"1","endAmount":"1","recipient":"0xdfdc0b1cf8e9950d6a860af6501c4fecf7825cc1"},{"itemType":"2","token":"0x60e4d786628fea6478f785a6d7e704777c86a7c6","identifierOrCriteria":"4770","startAmount":"1","endAmount":"1","recipient":"0xdfdc0b1cf8e9950d6a860af6501c4fecf7825cc1"},{"itemType":"2","token":"0xba30e5f9bb24caa003e9f2f0497ad287fdf95623","identifierOrCriteria":"9594","startAmount":"1","endAmount":"1","recipient":"0xdfdc0b1cf8e9950d6a860af6501c4fecf7825cc1"},{"itemType":"2","token":"0xba30e5f9bb24caa003e9f2f0497ad287fdf95623","identifierOrCriteria":"2118","startAmount":"1","endAmount":"1","recipient":"0xdfdc0b1cf8e9950d6a860af6501c4fecf7825cc1"},{"itemType":"2","token":"0xba30e5f9bb24caa003e9f2f0497ad287fdf95623","identifierOrCriteria":"1753","startAmount":"1","endAmount":"1","recipient":"0xdfdc0b1cf8e9950d6a860af6501c4fecf7825cc1"}],"orderType":"2","startTime":"1681810415","endTime":"1681983215","zoneHash":"0x0000000000000000000000000000000000000000000000000000000000000000","salt":"1550213294656772168494388599483486699884316127427085531712538817979596","conduitKey":"0x0000007b02230091a7ed01230072f7006a004d60a8d4e71d599b8104250f0000","counter":"0"}}',
-            ],
-          },
-          // {
-          //   expectedDescription:
-          //     'If you approve this request, someone can steal your assets listed on OpenSea.',
-          //   expectedReason: 'set_approval_for_all'
-          //   method: 'eth_sendTransaction',
-          //   params: [
-          //     {
-          //       to: '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d',
-          //       data: '0xa22cb465000000000000000000000000b85492afc686d5ca405e3cd4f50b05d358c75ede0000000000000000000000000000000000000000000000000000000000000001',
-          //     },
-          //   ],
-          // },
-          {
-            expectedDescription:
-              'If you approve this request, you might lose your assets.',
-            expectedReason: 'trade_order_farming',
-            method: 'eth_signTypedData_v4',
-            params: [
-              selectedAddress,
-              `{"types":{"ERC721Order":[{"type":"uint8","name":"direction"},{"type":"address","name":"maker"},{"type":"address","name":"taker"},{"type":"uint256","name":"expiry"},{"type":"uint256","name":"nonce"},{"type":"address","name":"erc20Token"},{"type":"uint256","name":"erc20TokenAmount"},{"type":"Fee[]","name":"fees"},{"type":"address","name":"erc721Token"},{"type":"uint256","name":"erc721TokenId"},{"type":"Property[]","name":"erc721TokenProperties"}],"Fee":[{"type":"address","name":"recipient"},{"type":"uint256","name":"amount"},{"type":"bytes","name":"feeData"}],"Property":[{"type":"address","name":"propertyValidator"},{"type":"bytes","name":"propertyData"}],"EIP712Domain":[{"name":"name","type":"string"},{"name":"version","type":"string"},{"name":"chainId","type":"uint256"},{"name":"verifyingContract","type":"address"}]},"domain":{"name":"ZeroEx","version":"1.0.0","chainId":"1","verifyingContract":"0xdef1c0ded9bec7f1a1670819833240f027b25eff"},"primaryType":"ERC721Order","message":{"direction":"0","maker":"${selectedAddress}","taker":"0x0000000000000000000000000000000000000000","expiry":"2524604400","nonce":"100131415900000000000000000000000000000083840314483690155566137712510085002484","erc20Token":"0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2","erc20TokenAmount":"42000000000000","fees":[],"erc721Token":"0x8a90CAb2b38dba80c64b7734e58Ee1dB38B8992e","erc721TokenId":"2516","erc721TokenProperties":[]}}`,
-            ],
-          },
+
           // PPOM Error: block does not exist
           // Sending ETH - Contract at 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48 could not be found in the project information
           // {
           //   expectedDescription:
           //     'If you approve this request, a third party known for scams will take all your assets.',
           //   expectedReason: 'transfer_farming of ERC20',
-          //   method: 'eth_sendTransaction',
-          //   params: [
-          //     {
-          //       data: '0xa9059cbb0000000000000000000000005fbdb2315678afecb367f032d93f642f64180aa30000000000000000000000000000000000000000000000000000000000000064',
-          //       to: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
-          //     },
-          //   ],
+          //   btnSelector: 'maliciousERC20TransferButton',
           // },
 
           // Error from PPOM: block does not exist
           // Sending ETH - Contract at 0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB could not be found in the project information
-          //
           // transferPunk(address _to, uint _tokenId)
-
           // {
+          //   btnSelector: 'maliciousERC721Transfer',
           //   expectedDescription:
           //     'If you approve this request, a third party known for scams will take all your assets.',
           //   expectedReason: 'transfer_farming of ERC721',
-          //   method: 'eth_sendTransaction',
-          //   params: [
-          //     {
-          //       to: '0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb',
-          //       data: '0x8b72a2ec000000000000000000000000b85492afc686d5ca405e3cd4f50b05d358c75ede0000000000000000000000000000000000000000000000000000000000000001',
-          //       from: selectedAddress
-          //     },
-          //   ],
           // },
+          {
+            btnSelector: 'maliciousPermit',
+            expectedDescription:
+              'If you approve this request, a third party known for scams might take all your assets.',
+            expectedReason: 'permit_farming',
+          },
+          // PPOM Error: block does not exist
+          // {
+          //   btnSelector: 'maliciousRawEthButton',
+          //   expectedDescription:
+          //     'If you approve this request, you might lose your assets.',
+          //   expectedReason: 'raw_ether_transfer',
+          // },
+          {
+            btnSelector: 'maliciousSeaport',
+            expectedDescription:
+              'If you approve this request, someone can steal your assets listed on OpenSea.',
+            expectedReason: 'seaport_farming',
+          },
+          // {
+          //   btnSelector: 'maliciousSetApprovalForAll',
+          //   expectedDescription:
+          //     'If you approve this request, someone can steal your assets listed on OpenSea.',
+          //   expectedReason: 'set_approval_for_all'
+          // },
+          {
+            btnSelector: 'maliciousTradeOrder',
+            expectedDescription:
+              'If you approve this request, you might lose your assets.',
+            expectedReason: 'trade_order_farming',
+          },
         ];
 
         for (const config of testMaliciousConfigs) {
-          const { expectedDescription, expectedReason, method, params } =
-            config;
+          const { expectedDescription, expectedReason, btnSelector } = config;
 
-          // Send JSON-RPC request
-          const request = JSON.stringify({
-            jsonrpc: '2.0',
-            method,
-            params,
-          });
-          await driver.executeScript(
-            `window.transactionHash = window.ethereum.request(${request})`,
-          );
+          // Click TestDapp button to send JSON-RPC request
+          await driver.clickElement(btnSelector);
 
           // Wait for confirmation pop-up
           const windowHandles = await getWindowHandles(driver, 3);
