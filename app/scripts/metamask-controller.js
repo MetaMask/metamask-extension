42514a65251eb73bcd093a48f0cb294e026219f8
david0xd <david@timechaser.org>
2023-09-28 15:59:32 +0200

    Add Snap website to snap settings page and make UI changes
    Fix and update unit test and mock data
    
    Add phishing detection
    
    Refactor, add tests, fix tests
    
    Update file name for PhishingController hook
    
    Add fix for long URLs
    
    Refactor according to requested changes
    
    use PhishingController from metamask-controller
    
    Refactor and fix eslint issue
    
    Fix unit test
    
    Rename phishing test action
    
    Check phishing only if website exists

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -2384,6 +2384,7 @@ export default class MetamaskController extends EventEmitter {
       assetsContractController,
       backup,
       approvalController,
+      phishingController,
     } = this;
 
     return {
@@ -2811,6 +2812,11 @@ export default class MetamaskController extends EventEmitter {
       dismissNotifications: this.dismissNotifications.bind(this),
       markNotificationsAsRead: this.markNotificationsAsRead.bind(this),
       updateCaveat: this.updateCaveat.bind(this),
+      getPhishingResult: async (website) => {
+        await phishingController.maybeUpdateState();
+
+        return phishingController.test(website);
+      },
       ///: END:ONLY_INCLUDE_IN
       ///: BEGIN:ONLY_INCLUDE_IN(keyring-snaps)
       updateSnapRegistry: this.preferencesController.updateSnapRegistry.bind(
