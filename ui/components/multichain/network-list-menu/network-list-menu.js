6db4d74b5cc98b8ff357a7ca219c4580f15254a0
NidhiKJha <menidhikjha@gmail.com>
2023-10-03 14:51:02 +0530

    fixed dnd

--- ui/components/multichain/network-list-menu/network-list-menu.js
+++ ui/components/multichain/network-list-menu/network-list-menu.js
@@ -89,39 +89,11 @@ export const NetworkListMenu = ({ onClose }) => {
   const [searchQuery, setSearchQuery] = useState('');
   const [items, setItems] = useState([...nonTestNetworks]);
 
-  const removeFromList = (list, index) => {
-    const result = Array?.from(list);
-    const [removed] = result.splice(index, 1);
-    return [removed, result];
-  };
-
-  const addToList = (list, index, element) => {
-    const result = Array?.from(list);
-    result.splice(index, 0, element);
-    return result;
-  };
-
   const onDragEnd = (result) => {
-    if (!result.destination) {
-      return;
-    }
-    const listCopy = { ...items };
-
-    const sourceList = listCopy[result.source.droppableId];
-    const [removedElement, newSourceList] = removeFromList(
-      sourceList,
-      result.source.index,
-    );
-    listCopy[result.source.droppableId] = newSourceList;
-
-    const destinationList = listCopy[result.destination.droppableId];
-    listCopy[result.destination.droppableId] = addToList(
-      destinationList,
-      result.destination.index,
-      removedElement,
-    );
-
-    setItems(listCopy);
+    const newItems = [...items];
+    const [removed] = newItems.splice(result.source.index, 1);
+    newItems.splice(result.destination.index, 0, removed);
+    setItems(newItems);
   };
 
   let searchResults = items;
