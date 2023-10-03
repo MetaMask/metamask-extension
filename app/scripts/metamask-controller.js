11565b8bb786024ee28e3429d30f732bd533168f
Matthew Walsh <matthew.walsh@consensys.net>
2023-10-03 10:58:15 +0100

    Add privacy setting to disable external name sources (#21045)

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -1630,6 +1630,9 @@ export default class MetamaskController extends EventEmitter {
     );
 
     ///: BEGIN:ONLY_INCLUDE_IN(petnames)
+    const isExternalNameSourcesEnabled = () =>
+      this.preferencesController.store.getState().useExternalNameSources;
+
     this.nameController = new NameController({
       messenger: this.controllerMessenger.getRestricted({
         name: 'NameController',
@@ -1641,9 +1644,9 @@ export default class MetamaskController extends EventEmitter {
             this.ensController,
           ),
         }),
-        new EtherscanNameProvider({}),
-        new TokenNameProvider({}),
-        new LensNameProvider(),
+        new EtherscanNameProvider({ isEnabled: isExternalNameSourcesEnabled }),
+        new TokenNameProvider({ isEnabled: isExternalNameSourcesEnabled }),
+        new LensNameProvider({ isEnabled: isExternalNameSourcesEnabled }),
         new SnapsNameProvider({
           messenger: this.controllerMessenger.getRestricted({
             name: 'SnapsNameProvider',
@@ -2498,6 +2501,12 @@ export default class MetamaskController extends EventEmitter {
       ///: BEGIN:ONLY_INCLUDE_IN(keyring-snaps)
       setAddSnapAccountEnabled:
         preferencesController.setAddSnapAccountEnabled.bind(
+          preferencesController,
+        ),
+      ///: END:ONLY_INCLUDE_IN
+      ///: BEGIN:ONLY_INCLUDE_IN(petnames)
+      setUseExternalNameSources:
+        preferencesController.setUseExternalNameSources.bind(
           preferencesController,
         ),
       ///: END:ONLY_INCLUDE_IN
