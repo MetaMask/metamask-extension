a6dbf120277a4731f19a57dc1cc28dda90afe021
Vinicius Stevam <vinicius.stevam@consensys.net>
2023-10-04 18:11:16 +0100

    add callback to custodyStatus

--- app/scripts/controllers/transactions/index.js
+++ app/scripts/controllers/transactions/index.js
@@ -125,7 +125,7 @@ const VALID_UNAPPROVED_TRANSACTION_TYPES = [
  * @param {Function} opts.signTransaction - ethTx signer that returns a rawTx
  * @param {number} [opts.txHistoryLimit] - number *optional* for limiting how many transactions are in state
  * @param {Function} opts.hasCompletedOnboarding - Returns whether or not the user has completed the onboarding flow
- * @param {boolean} opts.disablePublish - Skips publishing the transaction.
+ * @param {Function} opts.shouldDisablePublish - Returns whether or not should skips publishing the transaction.
  * @param {object} opts.preferencesStore
  */
 
@@ -155,7 +155,7 @@ export default class TransactionController extends EventEmitter {
     this.snapAndHardwareMessenger = opts.snapAndHardwareMessenger;
     this.messagingSystem = opts.messenger;
     this._hasCompletedOnboarding = opts.hasCompletedOnboarding;
-    this._disablePublish = opts.disablePublish ?? false;
+    this._shouldDisablePublish = opts.shouldDisablePublish;
 
     this.memStore = new ObservableStore({});
 
@@ -248,9 +248,7 @@ export default class TransactionController extends EventEmitter {
     this._updatePendingTxsAfterFirstBlock();
     this._onBootCleanUp();
 
-    this.transactionUpdateController = this._disablePublish
-      ? opts.transactionUpdateController
-      : undefined;
+    this.transactionUpdateController = opts.transactionUpdateController;
   }
 
   /**
@@ -1389,10 +1387,7 @@ export default class TransactionController extends EventEmitter {
       txMeta.custodyStatus ? txMeta : undefined,
     );
 
-    if (this._disablePublish) {
-      txMeta.custodyId = signedEthTx.custodian_transactionId;
-      txMeta.custodyStatus = signedEthTx.transactionStatus;
-
+    if (this._shouldDisablePublish(txMeta, signedEthTx)) {
       this.transactionUpdateController.addTransactionToWatchList(
         txMeta.custodyId,
         fromAddress,
@@ -1736,7 +1731,7 @@ export default class TransactionController extends EventEmitter {
     const txMeta = this.txStateManager.getTransaction(txId);
 
     // MMI does not broadcast transactions, as that is the responsibility of the custodian
-    if (this._disablePublish) {
+    if (this._shouldDisablePublish(txMeta)) {
       this.inProcessOfSigning.delete(txId);
       // Custodial nonces and gas params are set by the custodian, so MMI follows the approve
       // workflow before the transaction parameters are sent to the keyring
@@ -1903,7 +1898,7 @@ export default class TransactionController extends EventEmitter {
       })
       .forEach((txMeta) => {
         // If you create a Tx and its still inside the custodian waiting to be approved we don't want to approve it right away
-        if (!txMeta.custodyStatus) {
+        if (!this._shouldDisablePublish(txMeta)) {
           // Line below will try to publish transaction which is in
           // APPROVED state at the time of controller bootup
           this._approveTransaction(txMeta.id);
