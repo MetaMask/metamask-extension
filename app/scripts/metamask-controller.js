70a9140a943a4d77a410c6b9639ac64dc5047232
David Drazic <david@timechaser.org>
2023-10-04 14:44:39 +0200

    Add Snap website field to Snap settings page and make UI changes (#21095)
    ## **Description**
    This PR adds a website URL to the Snap settings page and makes coupe of
    changes to the UI elements as requested in related task.
    
    PR summary:
    - Add new selector `getSnapRegistryData` for getting Snap registry data.
    - Add Snap website URL to Snap Authorship component.
    - Change origin to be plain text instead of link.
    - Change color of text to default for connected websites.
    - Check website against Phishing list using `PhishingController` before
    displaying it and display it only if it's not blacklisted.
    
    ## **Manual testing steps**
    1. Install one or more Snaps.
    2. Go to Settings page.
    3. Click on one of the Snaps listed.
    4. Check the UI changes requested in related task.
    
    ## **Screenshots/Recordings**
    
    ### **Before**
    ![Screenshot 2023-09-28 at 16 11
    29](https://github.com/MetaMask/metamask-extension/assets/13301024/8d5c8ead-b539-4eb9-bcc8-c161b7c4109d)
    ![Screenshot 2023-09-28 at 16 11
    48](https://github.com/MetaMask/metamask-extension/assets/13301024/fa249118-f536-46ca-a9a9-6e81d13ed078)
    
    ### **After**
    ![Screenshot 2023-09-28 at 15 21
    58](https://github.com/MetaMask/metamask-extension/assets/13301024/4bf8e6e5-b114-4b54-9e4e-3c14ce40db50)
    ![Screenshot 2023-09-28 at 16 07
    43](https://github.com/MetaMask/metamask-extension/assets/13301024/71df9135-d5c1-40a1-a49c-7152765912f8)
    ![Screenshot 2023-10-02 at 17 18
    33](https://github.com/MetaMask/metamask-extension/assets/13301024/1e576df3-8b97-4d46-a978-741669c74daf)
    
    ## **Related issues**
    Fixes: https://github.com/MetaMask/metamask-extension/issues/20988
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've clearly explained:
      - [x] What problem this PR is solving.
      - [x] How this problem was solved.
      - [x] How reviewers can test my changes.
    - [x] I’ve indicated what issue this PR is linked to: Fixes #???
    - [x] I’ve included tests if applicable.
    - [x] I’ve documented any added code.
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/LABELING_GUIDELINES.md)).
    - [x] I’ve properly set the pull request status:
      - [x] In case it's not yet "ready for review", I've set it to "draft".
    - [x] In case it's "ready for review", I've changed it from "draft" to
    "non-draft".
    
    ## **Pre-merge reviewer checklist**
    
    - [x] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [x] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -2477,6 +2477,7 @@ export default class MetamaskController extends EventEmitter {
       assetsContractController,
       backup,
       approvalController,
+      phishingController,
     } = this;
 
     return {
@@ -2906,6 +2907,11 @@ export default class MetamaskController extends EventEmitter {
       dismissNotifications: this.dismissNotifications.bind(this),
       markNotificationsAsRead: this.markNotificationsAsRead.bind(this),
       updateCaveat: this.updateCaveat.bind(this),
+      getPhishingResult: async (website) => {
+        await phishingController.maybeUpdateState();
+
+        return phishingController.test(website);
+      },
       ///: END:ONLY_INCLUDE_IN
       ///: BEGIN:ONLY_INCLUDE_IN(keyring-snaps)
       updateSnapRegistry: this.preferencesController.updateSnapRegistry.bind(
