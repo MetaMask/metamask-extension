4494a0494bcb1ce1f2a25ce33cb17c5d58e261f3
Matthew Walsh <matthew.walsh@consensys.net>
2023-10-04 12:07:57 +0100

    Add swaps support via patch

---
+++ app/scripts/migrations/100.test.ts
@@ -0,0 +1,16 @@
+import { migrate, version } from './100';
+
+const oldVersion = 99;
+
+describe('migration #100', () => {
+  it('updates the version metadata', async () => {
+    const oldStorage = {
+      meta: { version: oldVersion },
+      data: {},
+    };
+
+    const newStorage = await migrate(oldStorage);
+
+    expect(newStorage.meta).toStrictEqual({ version });
+  });
+});
