5173252749515027ed1440cacac95a3a71cbdae0
Salimtb <salim.toubal@consensys.net>
2023-10-06 10:59:22 +0200

    fix: set max decimals to 36

--- shared/modules/Numeric.ts
+++ shared/modules/Numeric.ts
@@ -5,7 +5,7 @@ import { addHexPrefix } from 'ethereumjs-util';
 import { EtherDenomination } from '../constants/common';
 import { stripHexPrefix } from './hexstring-utils';
 
-const MAX_DECIMALS_FOR_TOKENS = 32;
+const MAX_DECIMALS_FOR_TOKENS = 36;
 BigNumber.config({ DECIMAL_PLACES: MAX_DECIMALS_FOR_TOKENS });
 export type NumericValue = string | number | BN | BigNumber;
 export type NumericBase = 10 | 16;
