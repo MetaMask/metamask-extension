9be6e5bbef5f9fbe6e84fd2b0df6d85408e920f5
gantunesr <gustavoantunesrod@gmail.com>
2023-10-06 00:18:48 -0300

    chore: change hour to 10 am EST

--- shared/modules/snap-accounts.ts
+++ shared/modules/snap-accounts.ts
@@ -7,5 +7,5 @@
  * @returns true if the current date is after the specified date, false otherwise.
  */
 export function showSnapAccountExperimentalToggle(): boolean {
-  return new Date().getTime() > Date.UTC(2023, 10, 2, 5);
+  return new Date().getTime() > Date.UTC(2023, 10, 2, 15);
 }
