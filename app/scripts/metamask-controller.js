163126b2a7e6ed26a2fcc3bce18c9d016201a0c0
Frederik Bolding <frederik.bolding@gmail.com>
2023-10-06 12:02:09 +0200

    Improve snap export metrics throttling

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -9,6 +9,8 @@ import {
   debounce,
   ///: BEGIN:ONLY_INCLUDE_IN(snaps)
   throttle,
+  memoize,
+  wrap,
   ///: END:ONLY_INCLUDE_IN
 } from 'lodash';
 import { keyringBuilderFactory } from '@metamask/eth-keyring-controller';
@@ -1982,22 +1984,30 @@ export default class MetamaskController extends EventEmitter {
   ///: BEGIN:ONLY_INCLUDE_IN(snaps)
 
   /**
-   * Tracks snaps export usage. Note: This function is throttled to 1 call per 60 seconds.
+   * Tracks snaps export usage.
+   * Note: This function is throttled to 1 call per 60 seconds per snap id + handler combination.
    *
    * @param {string} snapId - The ID of the snap the handler is being triggered on.
    * @param {string} handler - The handler to trigger on the snap for the request.
    */
-  _trackSnapExportUsage = throttle(
-    (snapId, handler) =>
-      this.metaMetricsController.trackEvent({
-        event: MetaMetricsEventName.SnapExportUsed,
-        category: MetaMetricsEventCategory.Snaps,
-        properties: {
-          snap_id: snapId,
-          export: handler,
-        },
-      }),
-    SECOND * 60,
+  _trackSnapExportUsage = wrap(
+    memoize(
+      () =>
+        throttle(
+          (snapId, handler) =>
+            this.metaMetricsController.trackEvent({
+              event: MetaMetricsEventName.SnapExportUsed,
+              category: MetaMetricsEventCategory.Snaps,
+              properties: {
+                snap_id: snapId,
+                export: handler,
+              },
+            }),
+          SECOND * 60,
+        ),
+      (snapId, handler) => `${snapId}${handler}`,
+    ),
+    (getFunc, ...args) => getFunc(...args)(...args),
   );
 
   /**
