943a65c9d9aa52ba779922c065ee08b537324a7d
David Walsh <davidwalsh83@gmail.com>
2023-10-07 13:23:11 -0500

    UX: Multichain: Provide Page, Header, Content, and Footer components

---
+++ ui/components/multichain/pages/page/components/content/content.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { Box } from '../../../../../component-library';
+import {
+  BlockSize,
+  Display,
+  FlexDirection,
+} from '../../../../../../helpers/constants/design-system';
+
+export const Content = ({ children, className = '', ...props }) => {
+  return (
+    <Box
+      display={Display.Flex}
+      flexDirection={FlexDirection.Column}
+      width={BlockSize.Full}
+      padding={4}
+      height={BlockSize.Full}
+      className={classnames('multichain-page-content', className)}
+      {...props}
+    >
+      {children}
+    </Box>
+  );
+};
+
+Content.propTypes = {
+  /**
+   * Elements that go in the page content section
+   */
+  children: PropTypes.element,
+  /**
+   * Additional CSS class provided to the content
+   */
+  className: PropTypes.string,
+};
