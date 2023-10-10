280c987a84c51bafd2ab55e768582659fbb72fbc
garrettbear <gwhisten@gmail.com>
2023-10-10 14:59:28 -0700

    fix selectbutton and selectoption tests

--- ui/components/component-library/select-button/select-button.test.tsx
+++ ui/components/component-library/select-button/select-button.test.tsx
@@ -1,16 +1,29 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
+import { SelectWrapper } from '../select-wrapper';
 import { SelectButton } from '.';
 
 describe('SelectButton', () => {
   it('should render the SelectButton without crashing', () => {
-    const { container } = render(<SelectButton />);
+    const { container } = render(
+      <SelectWrapper triggerComponent={<SelectButton>Trigger</SelectButton>}>
+        Test
+      </SelectWrapper>,
+    );
     expect(container).toMatchSnapshot();
   });
 
   it('should render the SelectButton with additional className', () => {
     const { getByTestId } = render(
-      <SelectButton data-testid="classname" className="mm-test" />,
+      <SelectWrapper
+        triggerComponent={
+          <SelectButton data-testid="classname" className="mm-test">
+            Trigger
+          </SelectButton>
+        }
+      >
+        Test
+      </SelectWrapper>,
     );
     expect(getByTestId('classname')).toHaveClass('mm-select-button mm-test');
   });
--- ui/components/component-library/select-option/select-option.test.tsx
+++ ui/components/component-library/select-option/select-option.test.tsx
@@ -1,16 +1,23 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
+import { SelectWrapper } from '../select-wrapper';
 import { SelectOption } from '.';
 
 describe('SelectOption', () => {
   it('should render the SelectOption without crashing', () => {
-    const { container } = render(<SelectOption />);
+    const { container } = render(
+      <SelectWrapper isOpen={true} triggerComponent={<button>Trigger</button>}>
+        <SelectOption />
+      </SelectWrapper>,
+    );
     expect(container).toMatchSnapshot();
   });
 
   it('should render the SelectOption with additional className', () => {
     const { getByTestId } = render(
-      <SelectOption data-testid="classname" className="mm-test" />,
+      <SelectWrapper isOpen={true} triggerComponent={<button>Trigger</button>}>
+        <SelectOption data-testid="classname" className="mm-test" />
+      </SelectWrapper>,
     );
     expect(getByTestId('classname')).toHaveClass('mm-select-option mm-test');
   });
