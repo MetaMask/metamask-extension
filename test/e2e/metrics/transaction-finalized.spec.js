f53e52f5128f58c9000b2ca95efdf742d058caaa
Matthew Walsh <matthew.walsh@consensys.net>
2023-10-10 23:01:54 +0100

    Skip finalized metrics test

--- test/e2e/metrics/transaction-finalized.spec.js
+++ test/e2e/metrics/transaction-finalized.spec.js
@@ -133,7 +133,8 @@ const eventHasUserIdWithoutAnonymousId = (payload) =>
 const eventHasZeroAddressAnonymousId = (payload) =>
   payload.anonymousId === '0x0000000000000000';
 
-describe('Transaction Finalized Event', function () {
+// eslint-disable-next-line mocha/no-skipped-tests
+describe.skip('Transaction Finalized Event', function () {
   it('Successfully tracked when sending a transaction @no-mmi', async function () {
     await withFixtures(
       {
