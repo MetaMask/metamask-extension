7f1605ef8143d2f83dabfad97603ac32c5d28031
Daniel Rocha <68558152+danroc@users.noreply.github.com>
2023-10-10 17:46:57 +0200

    test: update permission tests

--- app/scripts/lib/keyring-snaps-permissions.test.ts
+++ app/scripts/lib/keyring-snaps-permissions.test.ts
@@ -6,44 +6,70 @@ import { KeyringRpcMethod } from '@metamask/keyring-api';
 import { keyringSnapPermissionsBuilder } from './keyring-snaps-permissions';
 
 describe('keyringSnapPermissionsBuilder', () => {
-  const mockController = {
-    getSubjectMetadata: jest.fn((origin: string) => {
-      if (origin.startsWith('https://') || origin.startsWith('http://')) {
-        return { subjectType: SubjectType.Website };
-      }
-      return { subjectType: SubjectType.Unknown };
-    }),
-  } as unknown as SubjectMetadataController;
+  const mockController = new SubjectMetadataController({
+    subjectCacheLimit: 100,
+    messenger: {
+      registerActionHandler: jest.fn(),
+      publish: jest.fn(),
+    } as any,
+    state: {},
+  });
+  mockController.addSubjectMetadata({
+    origin: 'https://some-dapp.com',
+    subjectType: SubjectType.Website,
+  });
+
+  it('returns the methods metamask can call', () => {
+    const permissions = keyringSnapPermissionsBuilder(mockController);
+    expect(permissions('metamask')).toStrictEqual([
+      KeyringRpcMethod.ListAccounts,
+      KeyringRpcMethod.GetAccount,
+      KeyringRpcMethod.FilterAccountChains,
+      KeyringRpcMethod.DeleteAccount,
+      KeyringRpcMethod.ListRequests,
+      KeyringRpcMethod.GetRequest,
+      KeyringRpcMethod.SubmitRequest,
+      KeyringRpcMethod.RejectRequest,
+    ]);
+  });
+
+  it('returns the methods a known origin can call', () => {
+    const permissions = keyringSnapPermissionsBuilder(mockController);
+    expect(permissions('https://some-dapp.com')).toStrictEqual([
+      KeyringRpcMethod.ListAccounts,
+      KeyringRpcMethod.GetAccount,
+      KeyringRpcMethod.CreateAccount,
+      KeyringRpcMethod.FilterAccountChains,
+      KeyringRpcMethod.UpdateAccount,
+      KeyringRpcMethod.DeleteAccount,
+      KeyringRpcMethod.ExportAccount,
+      KeyringRpcMethod.ListRequests,
+      KeyringRpcMethod.GetRequest,
+      KeyringRpcMethod.ApproveRequest,
+      KeyringRpcMethod.RejectRequest,
+    ]);
+  });
+
+  it('returns the methods an unknown origin can call', () => {
+    const permissions = keyringSnapPermissionsBuilder(mockController);
+    expect(permissions('https://some-other-dapp.com')).toStrictEqual([]);
+  });
 
   it.each([
-    // MetaMask
-    ['metamask', KeyringRpcMethod.ListAccounts, true],
-    ['metamask', KeyringRpcMethod.GetAccount, true],
-    ['metamask', KeyringRpcMethod.CreateAccount, false],
-    ['metamask', KeyringRpcMethod.FilterAccountChains, true],
-    ['metamask', KeyringRpcMethod.UpdateAccount, false],
-    ['metamask', KeyringRpcMethod.DeleteAccount, true],
-    ['metamask', KeyringRpcMethod.ExportAccount, false],
-    ['metamask', KeyringRpcMethod.ListRequests, true],
-    ['metamask', KeyringRpcMethod.GetRequest, true],
-    ['metamask', KeyringRpcMethod.SubmitRequest, true],
-    ['metamask', KeyringRpcMethod.ApproveRequest, false],
-    ['metamask', KeyringRpcMethod.RejectRequest, true],
-    // Dapp
-    ['https://some-dapp-url', KeyringRpcMethod.ListAccounts, true],
-    ['https://some-dapp-url', KeyringRpcMethod.GetAccount, true],
-    ['https://some-dapp-url', KeyringRpcMethod.CreateAccount, true],
-    ['https://some-dapp-url', KeyringRpcMethod.FilterAccountChains, true],
-    ['https://some-dapp-url', KeyringRpcMethod.UpdateAccount, true],
-    ['https://some-dapp-url', KeyringRpcMethod.DeleteAccount, true],
-    ['https://some-dapp-url', KeyringRpcMethod.ExportAccount, true],
-    ['https://some-dapp-url', KeyringRpcMethod.ListRequests, true],
-    ['https://some-dapp-url', KeyringRpcMethod.GetRequest, true],
-    ['https://some-dapp-url', KeyringRpcMethod.SubmitRequest, false],
-    ['https://some-dapp-url', KeyringRpcMethod.ApproveRequest, true],
-    ['https://some-dapp-url', KeyringRpcMethod.RejectRequest, true],
-  ])('"%s" is allowed to call "%s": %s', (origin, method, expected) => {
+    [''],
+    ['null'],
+    ['sftp://some-dapp.com'],
+    ['0'],
+    [undefined],
+    [null],
+    [true],
+    [false],
+    [1],
+    [0],
+    [-1],
+    ['http://some-dapp.com'],
+  ])('"%s" cannot call any methods', (origin) => {
     const permissions = keyringSnapPermissionsBuilder(mockController);
-    expect(permissions(origin).includes(method)).toBe(expected);
+    expect(permissions(origin as any)).toStrictEqual([]);
   });
 });
