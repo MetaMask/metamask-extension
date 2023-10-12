523c7575f9119e48914b5059d4e2b53f6c5b2a3f
Monte Lai <monte.lai@consensys.net>
2023-10-12 20:24:31 +0800

    fix: make snap-account-redirect story to be more dynamic

--- ui/pages/snap-account-redirect/snap-account-redirect.stories.tsx
+++ ui/pages/snap-account-redirect/snap-account-redirect.stories.tsx
@@ -4,15 +4,35 @@ import SnapAccountRedirect from './snap-account-redirect';
 
 export default {
   title: 'Components/UI/SnapAccountRedirect',
+  argTypes: {
+    snapName: {
+      control: {
+        type: 'text',
+      },
+    },
+    isBlockedUrl: {
+      control: {
+        type: 'boolean',
+      },
+    },
+    url: {
+      control: {
+        type: 'text',
+      },
+    },
+    message: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
+  args: {
+    snapName: 'Snap Simple Keyring',
+    isBlockedUrl: false,
+    url: 'https://metamask.github.io/snap-simple-keyring/0.2.4/',
+    message: 'Redirecting to Snap Simple Keyring',
+  },
 } as Meta<typeof SnapAccountRedirect>;
 
-export const DefaultStory = () => (
-  <SnapAccountRedirect
-    snapName="Snap Simple Keyring"
-    isBlockedUrl={false}
-    url="https://metamask.github.io/snap-simple-keyring/0.2.4/"
-    message="Redirecting to Snap Simple Keyring"
-  />
-);
-
+export const DefaultStory = (args) => <SnapAccountRedirect {...args} />;
 DefaultStory.storyName = 'Default';
