618bc63a75ac9f13be0c9ddc06da13b18f59fe86
Monte Lai <monte.lai@consensys.net>
2023-10-13 19:35:39 +0800

    Add `data-testid` for toggle button (#21337)
    ## **Description**
    
    This PR adds the prop `dataTestId` to the `ToggleButton` for easier
    testing.
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've clearly explained:
      - [x] What problem this PR is solving.
      - [x] How this problem was solved.
      - [x] How reviewers can test my changes.
    - [ ] I’ve indicated what issue this PR is linked to: Fixes #???
    - [x] I’ve included tests if applicable.
    - [x] I’ve documented any added code.
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/LABELING_GUIDELINES.md)).
    - [x] I’ve properly set the pull request status:
      - [ ] In case it's not yet "ready for review", I've set it to "draft".
    - [x] In case it's "ready for review", I've changed it from "draft" to
    "non-draft".
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: Gauthier Petetin <gauthierpetetin@hotmail.com>
    Co-authored-by: Brad Decker <bhdecker84@gmail.com>
    Co-authored-by: Marina Boboc <120041701+benjisclowder@users.noreply.github.com>
    Co-authored-by: Peter <53189696+PeterYinusa@users.noreply.github.com>
    Co-authored-by: Daniel Rocha <68558152+danroc@users.noreply.github.com>
    Co-authored-by: Howard Braham <howrad@gmail.com>
    Co-authored-by: António Regadas <apregadas@gmail.com>
    Co-authored-by: Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>

--- ui/pages/settings/experimental-tab/experimental-tab.component.js
+++ ui/pages/settings/experimental-tab/experimental-tab.component.js
@@ -307,21 +307,20 @@ export default class ExperimentalTab extends PureComponent {
                 >
                   {t('addSnapAccountToggle')}
                 </Text>
-                <Box data-testid="add-snap-account-toggle">
-                  <ToggleButton
-                    value={addSnapAccountEnabled}
-                    onToggle={(value) => {
-                      trackEvent({
-                        event: MetaMetricsEventName.AddSnapAccountEnabled,
-                        category: MetaMetricsEventCategory.Settings,
-                        properties: {
-                          enabled: !value,
-                        },
-                      });
-                      setAddSnapAccountEnabled(!value);
-                    }}
-                  />
-                </Box>
+                <ToggleButton
+                  dataTestId="add-snap-account-toggle"
+                  value={addSnapAccountEnabled}
+                  onToggle={(value) => {
+                    trackEvent({
+                      event: MetaMetricsEventName.AddSnapAccountEnabled,
+                      category: MetaMetricsEventCategory.Settings,
+                      properties: {
+                        enabled: !value,
+                      },
+                    });
+                    setAddSnapAccountEnabled(!value);
+                  }}
+                />
               </div>
               <Text
                 variant={TextVariant.bodySm}
