9b9eb6851028096c339c0acde120aaa0204eabfd
Victor Thomas <10986371+vthomas13@users.noreply.github.com>
2023-10-17 11:47:28 -0400

    wip: fullscreen header layout connections

--- ui/components/multichain/pages/page/components/header/header.js
+++ ui/components/multichain/pages/page/components/header/header.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-import { HeaderBase, Text } from '../../../../../component-library';
+import { Box, HeaderBase, Text } from '../../../../../component-library';
 import {
+  AlignItems,
+  BackgroundColor,
   BlockSize,
   Display,
   TextAlign,
@@ -17,26 +19,28 @@ export const Header = ({
   ...props
 }) => {
   return (
-    <HeaderBase
-      padding={4}
-      width={BlockSize.Full}
-      className={classnames('multichain-page-header', className)}
-      startAccessory={startAccessory}
-      endAccessory={endAccessory}
-      {...props}
-    >
-      <Text
-        as="div"
-        display={Display.Block}
-        variant={TextVariant.bodyMdBold}
-        textAlign={TextAlign.Center}
-        paddingInlineStart={8}
-        paddingInlineEnd={8}
-        ellipsis
+    <Box className="multichain-page-header">
+      <HeaderBase
+        padding={4}
+        width={BlockSize.Full}
+        className={classnames('multichain-page-header__contents', className)}
+        startAccessory={startAccessory}
+        endAccessory={endAccessory}
+        {...props}
       >
-        {children}
-      </Text>
-    </HeaderBase>
+        <Text
+          as="div"
+          display={Display.Block}
+          variant={TextVariant.bodyMdBold}
+          textAlign={TextAlign.Center}
+          paddingInlineStart={8}
+          paddingInlineEnd={8}
+          ellipsis
+        >
+          {children}
+        </Text>
+      </HeaderBase>
+    </Box>
   );
 };
 
