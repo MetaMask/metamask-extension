c45d4ae018813724bd3879c2652d0f8427b48aed
david0xd <david@timechaser.org>
2023-10-17 16:32:39 +0200

    Refactor Snap list component to use Page

--- ui/pages/snaps/snaps-list/snap-list.js
+++ ui/pages/snaps/snaps-list/snap-list.js
@@ -15,6 +15,7 @@ import {
   BlockSize,
   FlexWrap,
   TextVariant,
+  BackgroundColor,
 } from '../../../helpers/constants/design-system';
 import {
   DEFAULT_ROUTE,
@@ -32,8 +33,12 @@ import {
   IconSize,
   Text,
   ButtonIcon,
-  ButtonIconSize,
 } from '../../../components/component-library';
+import {
+  Content,
+  Header,
+  Page,
+} from '../../../components/multichain/pages/page';
 
 const SnapList = () => {
   const t = useI18nContext();
@@ -50,115 +55,121 @@ const SnapList = () => {
   const snapsList = useSelector((state) => getSnapsList(state));
 
   return (
-    <div className="main-container snaps">
-      <div className="snaps__header">
-        <div className="snaps__header__title-container">
-          <ButtonIcon
-            iconName={IconName.ArrowLeft}
-            size={ButtonIconSize.Lg}
-            color={Color.textDefault}
-            onClick={() => history.push(DEFAULT_ROUTE)}
-            ariaLabel="back"
-          />
-          <div className="snaps__header__title-container__title">
-            {t('snaps')}
-          </div>
-        </div>
-      </div>
-      <Box
-        className="snaps__list"
-        display={Display.Flex}
-        flexDirection={FlexDirection.Column}
-        height={BlockSize.Full}
-      >
-        {snapsList.length > 0 && (
-          <div className="snaps__list__body">
-            <div className="snaps__list__wrapper">
-              {snapsList.map((snap) => {
-                return (
-                  <SnapListItem
-                    className="snaps__list-item"
-                    key={snap.key}
-                    packageName={snap.packageName}
-                    name={snap.name}
-                    onClick={() => {
-                      onClick(snap);
-                    }}
-                    snapId={snap.id}
-                  />
-                );
-              })}
-            </div>
-          </div>
-        )}
-        {snapsList.length <= 5 && (
+    <div className="snaps">
+      <Page backgroundColor={BackgroundColor.backgroundDefault}>
+        <Header
+          backgroundColor={BackgroundColor.backgroundDefault}
+          startAccessory={
+            <ButtonIcon
+              ariaLabel="Back"
+              iconName="arrow-left"
+              size="sm"
+              onClick={() => history.push(DEFAULT_ROUTE)}
+            />
+          }
+        >
+          {t('snaps')}
+        </Header>
+        <Content
+          backgroundColor={BackgroundColor.backgroundDefault}
+          className="snaps__content"
+        >
           <Box
+            className="snaps__content__list"
             display={Display.Flex}
+            flexDirection={FlexDirection.Column}
             height={BlockSize.Full}
-            flexDirection={FlexDirection.Row}
-            flexWrap={FlexWrap.Wrap}
-            justifyContent={JustifyContent.center}
-            className="snaps__list__container--snaps-info-content"
           >
-            {snapsList.length < 1 && (
+            {snapsList.length > 0 && (
+              <div className="snaps__content__list__body">
+                <div className="snaps__content__list__wrapper">
+                  {snapsList.map((snap) => {
+                    return (
+                      <SnapListItem
+                        className="snaps__content__list-item"
+                        key={snap.key}
+                        packageName={snap.packageName}
+                        name={snap.name}
+                        onClick={() => {
+                          onClick(snap);
+                        }}
+                        snapId={snap.id}
+                      />
+                    );
+                  })}
+                </div>
+              </div>
+            )}
+            {snapsList.length <= 5 && (
               <Box
-                className="snaps__list__container--no-snaps_inner"
                 display={Display.Flex}
-                flexDirection={FlexDirection.Column}
+                height={BlockSize.Full}
+                flexDirection={FlexDirection.Row}
+                flexWrap={FlexWrap.Wrap}
                 justifyContent={JustifyContent.center}
-                alignItems={AlignItems.center}
+                className="snaps__content__list__container--snaps-info-content"
               >
-                <Icon
-                  name={IconName.Snaps}
-                  color={IconColor.iconMuted}
-                  className="snaps__list__no-snaps_icon"
-                  size={IconSize.Inherit}
-                />
-                <Text
-                  color={Color.textMuted}
-                  align={TextAlign.Center}
-                  marginTop={4}
+                {snapsList.length < 1 && (
+                  <Box
+                    className="snaps__content__list__container--no-snaps_inner"
+                    display={Display.Flex}
+                    flexDirection={FlexDirection.Column}
+                    justifyContent={JustifyContent.center}
+                    alignItems={AlignItems.center}
+                  >
+                    <Icon
+                      name={IconName.Snaps}
+                      color={IconColor.iconMuted}
+                      className="snaps__content__list__no-snaps_icon"
+                      size={IconSize.Inherit}
+                    />
+                    <Text
+                      color={Color.textMuted}
+                      align={TextAlign.Center}
+                      marginTop={4}
+                    >
+                      {t('noSnaps')}
+                    </Text>
+                  </Box>
+                )}
+                <Box
+                  display={Display.Flex}
+                  width={BlockSize.Full}
+                  height={BlockSize.Min}
+                ></Box>
+                <Box
+                  className="snaps__content__list__container--no-snaps_banner-tip"
+                  display={Display.Flex}
+                  flexDirection={FlexDirection.Column}
+                  justifyContent={JustifyContent.flexEnd}
+                  paddingLeft={4}
+                  paddingRight={4}
+                  paddingBottom={4}
                 >
-                  {t('noSnaps')}
-                </Text>
+                  <BannerTip
+                    logoType={BannerTipLogoType.Greeting}
+                    description={t('extendWalletWithSnaps')}
+                    descriptionProps={{ variant: TextVariant.bodyMd }}
+                  >
+                    <ButtonLink
+                      size={Size.auto}
+                      href={
+                        snapsList.length > 0
+                          ? 'https://snaps.metamask.io/'
+                          : 'https://metamask.io/snaps/'
+                      }
+                      target="_blank"
+                      endIconName={IconName.Export}
+                    >
+                      {`${t('discoverSnaps')}`}
+                    </ButtonLink>
+                  </BannerTip>
+                </Box>
               </Box>
             )}
-            <Box
-              display={Display.Flex}
-              width={BlockSize.Full}
-              height={BlockSize.Min}
-            ></Box>
-            <Box
-              className="snaps__list__container--no-snaps_banner-tip"
-              display={Display.Flex}
-              flexDirection={FlexDirection.Column}
-              justifyContent={JustifyContent.flexEnd}
-              paddingLeft={4}
-              paddingRight={4}
-              paddingBottom={4}
-            >
-              <BannerTip
-                logoType={BannerTipLogoType.Greeting}
-                description={t('extendWalletWithSnaps')}
-                descriptionProps={{ variant: TextVariant.bodyMd }}
-              >
-                <ButtonLink
-                  size={Size.auto}
-                  href={
-                    snapsList.length > 0
-                      ? 'https://snaps.metamask.io/'
-                      : 'https://metamask.io/snaps/'
-                  }
-                  target="_blank"
-                  endIconName={IconName.Export}
-                >
-                  {`${t('discoverSnaps')}`}
-                </ButtonLink>
-              </BannerTip>
-            </Box>
           </Box>
-        )}
-      </Box>
+        </Content>
+      </Page>
     </div>
   );
 };
