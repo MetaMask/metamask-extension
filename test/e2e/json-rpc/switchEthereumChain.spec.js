32477fe895148e5f73f309c1775d310ed70a89bb
Thomas Huang <thomas.b.huang@gmail.com>
2023-10-17 11:55:29 -0700

    Fix type of "notification" in switchEthereumChain test comment

--- test/e2e/json-rpc/switchEthereumChain.spec.js
+++ test/e2e/json-rpc/switchEthereumChain.spec.js
@@ -197,7 +197,7 @@ describe('Switch Ethereum Chain for two dapps', function () {
           `window.ethereum.request(${switchEthereumChainRequest})`,
         );
 
-        // Switch to notifiction of switchEthereumChain
+        // Switch to notification of switchEthereumChain
         await switchToNotificationWindow(driver, 4);
         await driver.findClickableElements({
           text: 'Switch network',
