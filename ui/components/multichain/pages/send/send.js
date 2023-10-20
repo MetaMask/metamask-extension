5dc5f78340273e60fc7f2002fc6711c0751abaaf
David Walsh <davidwalsh83@gmail.com>
2023-10-20 13:56:14 -0500

    UX: Multichain: Send Flow: Wire To / Your Accounts

--- ui/components/multichain/pages/send/send.js
+++ ui/components/multichain/pages/send/send.js
@@ -1,6 +1,7 @@
-import React, { useContext } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useCallback, useContext, useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
+import { useLocation } from 'react-router-dom';
 import { Content, Footer, Header, Page } from '../page';
 import { I18nContext } from '../../../../contexts/i18n';
 import {
@@ -29,10 +30,27 @@ import {
   getSelectedIdentity,
 } from '../../../../selectors';
 import { AccountPicker, AccountListItem } from '../..';
-import DomainInput from '../../../../pages/send/send-content/add-recipient/domain-input.component';
+import DomainInput from '../../../../pages/send/send-content/add-recipient/domain-input';
+import {
+  addHistoryEntry,
+  getDraftTransactionExists,
+  getIsUsingMyAccountForRecipientSearch,
+  getRecipient,
+  getRecipientUserInput,
+  resetRecipientInput,
+  resetSendState,
+  startNewDraftTransaction,
+  updateRecipient,
+  updateRecipientUserInput,
+} from '../../../../ducks/send';
+import { AssetType } from '../../../../../shared/constants/transaction';
+import { showQrScanner } from '../../../../store/actions';
+import { MetaMetricsEventCategory } from '../../../../../shared/constants/metametrics';
+import { MetaMetricsContext } from '../../../../contexts/metametrics';
 
 export const SendPage = () => {
   const t = useContext(I18nContext);
+  const trackEvent = useContext(MetaMetricsContext);
 
   // Network
   const currentNetwork = useSelector(getCurrentNetwork);
@@ -41,7 +59,61 @@ export const SendPage = () => {
   const identity = useSelector(getSelectedIdentity);
 
   // Your Accounts
+  const dispatch = useDispatch();
+  const recipient = useSelector(getRecipient);
   const accounts = useSelector(getMetaMaskAccountsOrdered);
+  const userInput = useSelector(getRecipientUserInput);
+  const isUsingMyAccountsForRecipientSearch = useSelector(
+    getIsUsingMyAccountForRecipientSearch,
+  );
+  const draftTransactionExists = useSelector(getDraftTransactionExists);
+  const location = useLocation();
+  const startedNewDraftTransaction = useRef(false);
+
+  const cleanup = useCallback(() => {
+    console.log('cleanup, resetting send state via resetSendState()');
+    dispatch(resetSendState());
+  }, [dispatch]);
+
+  /**
+   * It is possible to route to this page directly, either by typing in the url
+   * or by clicking the browser back button after progressing to the confirm
+   * screen. In the case where a draft transaction does not yet exist, this
+   * hook is responsible for creating it. We will assume that this is a native
+   * asset send.
+   */
+  useEffect(() => {
+    if (
+      draftTransactionExists === false &&
+      startedNewDraftTransaction.current === false
+    ) {
+      console.log('use effect run!');
+      startedNewDraftTransaction.current = true;
+      dispatch(startNewDraftTransaction({ type: AssetType.native }));
+    }
+  }, [draftTransactionExists, dispatch]);
+
+  useEffect(() => {
+    window.addEventListener('beforeunload', cleanup);
+  }, [cleanup]);
+
+  useEffect(() => {
+    return () => {
+      dispatch(resetSendState());
+      window.removeEventListener('beforeunload', cleanup);
+    };
+  }, [dispatch, cleanup]);
+
+  useEffect(() => {
+    if (location.search === '?scan=true') {
+      dispatch(showQrScanner());
+
+      // Clear the queryString param after showing the modal
+      const cleanUrl = window.location.href.split('?')[0];
+      window.history.pushState({}, null, `${cleanUrl}`);
+      window.location.hash = '#send';
+    }
+  }, [location, dispatch]);
 
   const SendPageRow = ({ children }) => (
     <Box
@@ -107,12 +179,38 @@ export const SendPage = () => {
         <SendPageRow>
           <Label paddingBottom={2}>{t('to')}</Label>
           <DomainInput
-            userInput=""
-            onChange={() => undefined}
-            onReset={() => undefined}
-            lookupEnsName={() => undefined}
-            initializeDomainSlice={() => undefined}
-            resetDomainResolution={() => undefined}
+            userInput={userInput}
+            className="send__to-row"
+            onChange={(address) => dispatch(updateRecipientUserInput(address))}
+            onValidAddressTyped={async (address) => {
+              dispatch(
+                addHistoryEntry(`sendFlow - Valid address typed ${address}`),
+              );
+              await dispatch(updateRecipientUserInput(address));
+              dispatch(updateRecipient({ address, nickname: '' }));
+            }}
+            internalSearch={isUsingMyAccountsForRecipientSearch}
+            selectedAddress={recipient.address}
+            selectedName={recipient.nickname}
+            onPaste={(text) => {
+              dispatch(
+                addHistoryEntry(
+                  `sendFlow - User pasted ${text} into address field`,
+                ),
+              );
+            }}
+            onReset={() => dispatch(resetRecipientInput())}
+            scanQrCode={() => {
+              trackEvent({
+                event: 'Used QR scanner',
+                category: MetaMetricsEventCategory.Transactions,
+                properties: {
+                  action: 'Edit Screen',
+                  legacy_event: true,
+                },
+              });
+              dispatch(showQrScanner());
+            }}
           />
         </SendPageRow>
         <SendPageRow>
@@ -121,7 +219,21 @@ export const SendPage = () => {
             <AccountListItem
               identity={account}
               key={account.address}
-              onClick={() => undefined}
+              onClick={() => {
+                console.log('account is: ', account);
+                dispatch(
+                  addHistoryEntry(
+                    `sendFlow - User clicked recipient from my accounts. address: ${account.address}, nickname ${account.name}`,
+                  ),
+                );
+                dispatch(
+                  updateRecipient({
+                    address: account.address,
+                    nickname: account.name,
+                  }),
+                );
+                dispatch(updateRecipientUserInput(account.address));
+              }}
             />
           ))}
         </SendPageRow>
