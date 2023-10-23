db109654cb0812e782b0c341096196da0562c05e
David Walsh <davidwalsh83@gmail.com>
2023-10-23 14:12:34 -0500

    UX: Multichain: Send Flow: Wire To / Your Accounts

--- ui/components/multichain/pages/send/components/index.ts
+++ ui/components/multichain/pages/send/components/index.ts
@@ -2,3 +2,4 @@ export { SendPageRow } from './send-page-row';
 export { SendPageAccountPicker } from './account-picker';
 export { SendPageNetworkPicker } from './network-picker';
 export { SendPageYourAccount } from './your-accounts';
+export { SendPageRecipientInput } from './recipient-input';
