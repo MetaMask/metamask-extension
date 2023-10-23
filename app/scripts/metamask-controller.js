33b8522e134617243e03b14dab51762cc7417f3a
Frederik Bolding <frederik.bolding@gmail.com>
2023-10-23 14:07:32 +0200

    Fix missing action on messenger and E2E snapshots

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -1086,6 +1086,7 @@ export default class MetamaskController extends EventEmitter {
         'SnapsRegistry:get',
         'SnapsRegistry:getMetadata',
         'SnapsRegistry:update',
+        'SnapsRegistry:resolveVersion',
       ],
     });
 
