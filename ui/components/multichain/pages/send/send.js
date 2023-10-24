ddde807dd41adedd339d8c7e30be9d8de23c8a19
David Walsh <davidwalsh83@gmail.com>
2023-10-24 10:29:27 -0500

    UX: Multichain: Send Flow: Move connected components to individual components (#21501)
    ## **Description**
    Having all of the connected components pulling data in the same
    component (`SendPage`) was a major, major performance drain that was
    causing the `SendPage` to re-render over and over again. This PR
    separates out the children into their own components to limit
    re-renders.
    
    ## **Manual testing steps**
    
    1.  Run `MULTICHAIN=1 yarn start`
    2. Go to the Send page
    3. See the new send page layout, as was there before.
    
    ## **Screenshots/Recordings**
    
    <img width="399" alt="SCR-20231023-lvwu"
    src="https://github.com/MetaMask/metamask-extension/assets/46655/6e7d06cd-70e7-42a3-be0b-6eab1aff9341">
    
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've clearly explained:
      - [ ] What problem this PR is solving.
      - [ ] How this problem was solved.
      - [ ] How reviewers can test my changes.
    - [ ] I’ve indicated what issue this PR is linked to: Fixes #???
    - [ ] I’ve included tests if applicable.
    - [ ] I’ve documented any added code.
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/LABELING_GUIDELINES.md)).
    - [ ] I’ve properly set the pull request status:
      - [ ] In case it's not yet "ready for review", I've set it to "draft".
    - [ ] In case it's "ready for review", I've changed it from "draft" to
    "non-draft".
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- ui/components/multichain/pages/send/send.js
+++ ui/components/multichain/pages/send/send.js
@@ -1,10 +1,7 @@
 import React, { useContext } from 'react';
-import { useSelector } from 'react-redux';
-import PropTypes from 'prop-types';
 import { Content, Footer, Header, Page } from '../page';
 import { I18nContext } from '../../../../contexts/i18n';
 import {
-  Box,
   ButtonIcon,
   ButtonIconSize,
   ButtonPrimary,
@@ -13,48 +10,17 @@ import {
   ButtonSecondarySize,
   IconName,
   Label,
-  PickerNetwork,
 } from '../../../component-library';
-import {
-  BlockSize,
-  BorderColor,
-  Display,
-  FlexDirection,
-  JustifyContent,
-  TextAlign,
-} from '../../../../helpers/constants/design-system';
-import {
-  getCurrentNetwork,
-  getMetaMaskAccountsOrdered,
-  getSelectedIdentity,
-} from '../../../../selectors';
-import { AccountPicker, AccountListItem } from '../..';
 import DomainInput from '../../../../pages/send/send-content/add-recipient/domain-input.component';
+import { SendPageNetworkPicker } from './components/network-picker';
+import {
+  SendPageAccountPicker,
+  SendPageRow,
+  SendPageYourAccount,
+} from './components';
 
 export const SendPage = () => {
   const t = useContext(I18nContext);
-
-  // Network
-  const currentNetwork = useSelector(getCurrentNetwork);
-
-  // Account
-  const identity = useSelector(getSelectedIdentity);
-
-  // Your Accounts
-  const accounts = useSelector(getMetaMaskAccountsOrdered);
-
-  const SendPageRow = ({ children }) => (
-    <Box
-      display={Display.Flex}
-      paddingBottom={6}
-      flexDirection={FlexDirection.Column}
-    >
-      {children}
-    </Box>
-  );
-  SendPageRow.propTypes = {
-    children: PropTypes.element,
-  };
 
   return (
     <Page className="multichain-send-page">
@@ -70,40 +36,8 @@ export const SendPage = () => {
         {t('sendAToken')}
       </Header>
       <Content>
-        <SendPageRow>
-          <PickerNetwork
-            label={currentNetwork?.nickname}
-            src={currentNetwork?.rpcPrefs?.imageUrl}
-          />
-        </SendPageRow>
-        <SendPageRow>
-          <Label paddingBottom={2}>{t('from')}</Label>
-          <AccountPicker
-            address={identity.address}
-            name={identity.name}
-            onClick={() => undefined}
-            showAddress
-            borderColor={BorderColor.borderDefault}
-            borderWidth={1}
-            paddingTop={4}
-            paddingBottom={4}
-            block
-            justifyContent={JustifyContent.flexStart}
-            addressProps={{
-              display: Display.Flex,
-              textAlign: TextAlign.Start,
-            }}
-            labelProps={{
-              style: { flexGrow: 1, textAlign: 'start' },
-              paddingInlineStart: 2,
-            }}
-            textProps={{
-              display: Display.Flex,
-              width: BlockSize.Full,
-            }}
-            width={BlockSize.Full}
-          />
-        </SendPageRow>
+        <SendPageNetworkPicker />
+        <SendPageAccountPicker />
         <SendPageRow>
           <Label paddingBottom={2}>{t('to')}</Label>
           <DomainInput
@@ -115,16 +49,7 @@ export const SendPage = () => {
             resetDomainResolution={() => undefined}
           />
         </SendPageRow>
-        <SendPageRow>
-          <Label paddingBottom={2}>{t('yourAccounts')}</Label>
-          {accounts.map((account) => (
-            <AccountListItem
-              identity={account}
-              key={account.address}
-              onClick={() => undefined}
-            />
-          ))}
-        </SendPageRow>
+        <SendPageYourAccount />
       </Content>
       <Footer>
         <ButtonSecondary size={ButtonSecondarySize.Lg} block>
