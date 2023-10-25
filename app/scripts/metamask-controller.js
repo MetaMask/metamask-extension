d4d4cda3675bda1709cc45d66e793eb96ff3dc72
legobt <6wbvkn0j@anonaddy.me>
2023-10-25 00:27:31 +0000

    add getNetworkClientById option to SmartTransactionsController constructor

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -1505,6 +1505,9 @@ export default class MetamaskController extends EventEmitter {
     );
     this.smartTransactionsController = new SmartTransactionsController(
       {
+        getNetworkClientById: this.networkController.getNetworkClientById.bind(
+          this.networkController,
+        ),
         onNetworkStateChange: networkControllerMessenger.subscribe.bind(
           networkControllerMessenger,
           'NetworkController:stateChange',
