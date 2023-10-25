87faf94a718a0193e77a59cea6403c29617e9450
legobt <6wbvkn0j@anonaddy.me>
2023-10-25 09:48:02 +0000

    remove unused notification data

--- shared/notifications/index.js
+++ shared/notifications/index.js
@@ -118,14 +118,6 @@ export const UI_NOTIFICATIONS = {
     id: 20,
     date: null,
   },
-  21: {
-    id: 21,
-    date: null,
-    image: {
-      src: 'images/swaps-redesign.svg',
-      width: '100%',
-    },
-  },
   22: {
     id: 22,
     date: null,
@@ -378,17 +370,6 @@ export const getTranslatedUINotifications = (
       date: UI_NOTIFICATIONS[20].date
         ? new Intl.DateTimeFormat(formattedLocale).format(
             new Date(UI_NOTIFICATIONS[20].date),
-          )
-        : '',
-    },
-    21: {
-      ...UI_NOTIFICATIONS[21],
-      title: t('notifications21Title'),
-      description: t('notifications21Description'),
-      actionText: t('notifications21ActionText'),
-      date: UI_NOTIFICATIONS[21].date
-        ? new Intl.DateTimeFormat(formattedLocale).format(
-            new Date(UI_NOTIFICATIONS[21].date),
           )
         : '',
     },
