845043a45dee13e128d6c4598e09da030aef5632
Brad Decker <bhdecker84@gmail.com>
2023-10-26 14:27:33 -0500

    ci(chore): Run snaps tests in main unit test job (#21543)
    ## **Description**
    This pull request starts the process of moving E2E tests to be ran by
    build type, focusing on snaps and accounts first. Snaps and accounts
    tests were being ran on the flask build only, but the name of the e2e
    test script was flask-snaps which was running all of the flask test
    suite, plus accounts, plus snaps. This was achieved by adding a
    --build-type flag to the command in the circle ci script which was `yarn
    test:e2e:chrome:snaps` the snaps part added a --snaps flag to the e2e
    test command. This PR:
    
    1. removes the `--snaps` flag from the E2E test command, instead looking
    only at the `--build-type` flag to determine if flask versus main tests
    should be run.
    2. Runs most snaps and accounts tests against both main and flask as
    flask is still a developing ground for new features. I might recommend
    we improve this logic in the future by using a combination of extension
    '.flask.spec.js' and '@flask' so that existing features that are
    included in main can be extended in flask with new tests built for them
    instead of the current logic. The current logic just filters out
    specific tests (pre-existing just moved to a new location in the code).
    3. Increases concurrency of e2e test runners. 
    4. the `test-e2e-chrome-snap-flask` and `test-e2e-firefox-snaps-flask`
    has been renamed to drop the `-snaps` portion so its just
    `test-e2e-chrome-flask` etc.
    
    ## **Related issues**
    
    
    ## **Manual testing steps**
    1. Compare circle CI runs to develop.
    2. Verify that snaps tests run in main and flask builds
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    <img width="699" alt="Screenshot 2023-10-26 at 12 32 20 PM"
    src="https://github.com/MetaMask/metamask-extension/assets/4448075/9b75e30b-ab51-4b6f-8c07-822c7603e2f4">
    
    
    
    ### **After**
    <img width="688" alt="Screenshot 2023-10-26 at 12 31 37 PM"
    src="https://github.com/MetaMask/metamask-extension/assets/4448075/69aa3edf-b3aa-4e2d-be39-33ce235075ec">
    
    
    
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've clearly explained what problem this PR is solving and how it
    is solved.
    - [x] I've linked related issues
    - [x] I've included manual testing steps
    - [x] I've included screenshots/recordings if applicable
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/LABELING_GUIDELINES.md)).
    - [x] I’ve properly set the pull request status:
      - [ ] In case it's not yet "ready for review", I've set it to "draft".
    - [x] In case it's "ready for review", I've changed it from "draft" to
    "non-draft".
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- test/e2e/run-all.js
+++ test/e2e/run-all.js
@@ -7,6 +7,13 @@ const { runInShell } = require('../../development/lib/run-command');
 const { exitWithError } = require('../../development/lib/exit-with-error');
 const { loadBuildTypesConfig } = require('../../development/lib/build-type');
 
+// These tests should only be run on Flask for now.
+const FLASK_ONLY_TESTS = [
+  'test-snap-lifecycle.spec.js',
+  'test-snap-get-locale.spec.js',
+  'petnames.spec.js',
+];
+
 const getTestPathsForTestDir = async (testDir) => {
   const testFilenames = await fs.promises.readdir(testDir, {
     withFileTypes: true,
@@ -82,10 +89,6 @@ async function main() {
             description: `Run only mmi related tests`,
             type: 'boolean',
           })
-          .option('snaps', {
-            description: `run snaps e2e tests`,
-            type: 'boolean',
-          })
           .option('rpc', {
             description: `run json-rpc specific e2e tests`,
             type: 'boolean',
@@ -121,7 +124,6 @@ async function main() {
     debug,
     retries,
     mmi,
-    snaps,
     rpc,
     buildType,
     updateSnapshot,
@@ -130,16 +132,24 @@ async function main() {
 
   let testPaths;
 
-  if (snaps) {
+  // These test paths should be run against both flask and main builds.
+  // Eventually we should move all features to this array and test them all
+  // on every build type in which they are running to avoid regressions across
+  // builds.
+  const featureTestsOnMain = [
+    ...(await getTestPathsForTestDir(path.join(__dirname, 'accounts'))),
+    ...(await getTestPathsForTestDir(path.join(__dirname, 'snaps'))),
+  ];
+
+  if (buildType === 'flask') {
     testPaths = [
-      ...(await getTestPathsForTestDir(path.join(__dirname, 'snaps'))),
-      ...(await getTestPathsForTestDir(path.join(__dirname, 'accounts'))),
       ...(await getTestPathsForTestDir(path.join(__dirname, 'flask'))),
+      ...featureTestsOnMain,
     ];
   } else if (rpc) {
     const testDir = path.join(__dirname, 'json-rpc');
     testPaths = await getTestPathsForTestDir(testDir);
-  } else {
+  } else if (buildType === 'mmi') {
     const testDir = path.join(__dirname, 'tests');
     testPaths = [
       ...(await getTestPathsForTestDir(testDir)),
@@ -148,18 +158,19 @@ async function main() {
       ...(await getTestPathsForTestDir(path.join(__dirname, 'metrics'))),
       path.join(__dirname, 'metamask-ui.spec.js'),
     ];
-  }
-
-  // These tests should only be run on Flask for now.
-  if (buildType !== 'flask') {
-    const filteredTests = [
-      'test-snap-lifecycle.spec.js',
-      'test-snap-get-locale.spec.js',
-      'petnames.spec.js',
-    ];
-    testPaths = testPaths.filter((p) =>
-      filteredTests.every((filteredTest) => !p.endsWith(filteredTest)),
+  } else {
+    const testDir = path.join(__dirname, 'tests');
+    const filteredFlaskAndMainTests = featureTestsOnMain.filter((p) =>
+      FLASK_ONLY_TESTS.every((filteredTest) => !p.endsWith(filteredTest)),
     );
+    testPaths = [
+      ...(await getTestPathsForTestDir(testDir)),
+      ...(await getTestPathsForTestDir(path.join(__dirname, 'swaps'))),
+      ...(await getTestPathsForTestDir(path.join(__dirname, 'nft'))),
+      ...(await getTestPathsForTestDir(path.join(__dirname, 'metrics'))),
+      ...filteredFlaskAndMainTests,
+      path.join(__dirname, 'metamask-ui.spec.js'),
+    ];
   }
 
   const runE2eTestPath = path.join(__dirname, 'run-e2e-test.js');
