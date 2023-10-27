6f8027f9a4542f165c3af224e1295e9cf2627dc6
NidhiKJha <menidhikjha@gmail.com>
2023-10-27 14:55:54 +0530

    updated send-page-row to have classNames

--- ui/components/multichain/pages/send/components/send-page-row.tsx
+++ ui/components/multichain/pages/send/components/send-page-row.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
+import classnames from 'classnames';
 import { Box } from '../../../../component-library';
 import {
   Display,
   FlexDirection,
 } from '../../../../../helpers/constants/design-system';
+import { SendProps } from '../send.types';
+import { BoxProps } from '../../../../component-library/box';
 
-export const SendPageRow = ({
+export const SendPageRow: React.FC<SendProps> = ({
+  className = '',
   children,
-}: {
-  children: React.ReactNode | React.ReactNode[];
-}) => (
+  ...props
+}): JSX.Element => (
   <Box
+    className={classnames('multichain-send-page-row', className)}
     display={Display.Flex}
     paddingBottom={6}
     flexDirection={FlexDirection.Column}
+    {...(props as BoxProps<'div'>)}
   >
     {children}
   </Box>
---
+++ ui/components/multichain/pages/send/send.types.tsx
@@ -0,0 +1,6 @@
+import type { StyleUtilityProps } from '../../../component-library/box';
+
+export interface SendProps extends StyleUtilityProps {
+  /** * Additional class name for the AvatarGroup component */
+  className?: string;
+}
