4a1bfb3b675dad51b229a022776846644d6c5a9c
georgewrmarshall <george.marshall@consensys.net>
2023-10-30 15:45:26 -0700

    Adding the initial Textarea component to the component-library

---
+++ ui/components/component-library/textarea/index.ts
@@ -0,0 +1,6 @@
+export { Textarea } from './textarea';
+export type {
+  TextareaProps,
+  TextareaStyleUtilityProps,
+} from './textarea.types';
+export { TextareaResize } from './textarea.types';
---
+++ ui/components/component-library/textarea/textarea.tsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import classnames from 'classnames';
+
+import {
+  BorderRadius,
+  BackgroundColor,
+  BorderColor,
+} from '../../../helpers/constants/design-system';
+
+import { Text } from '..';
+import { TextProps } from '../text';
+
+import { PolymorphicRef } from '../box';
+import {
+  TextareaComponent,
+  TextareaProps,
+  TextareaResize,
+} from './textarea.types';
+
+export const Textarea: TextareaComponent = React.forwardRef(
+  <C extends React.ElementType = 'textarea'>(
+    {
+      autoFocus,
+      className = '',
+      defaultValue,
+      disabled,
+      error,
+      id,
+      resize = TextareaResize.Vertical,
+      rows,
+      cols,
+      maxLength,
+      name,
+      onBlur,
+      onChange,
+      onClick,
+      onFocus,
+      placeholder,
+      readOnly,
+      required,
+      value,
+      ...props
+    }: TextareaProps<C>,
+    ref?: PolymorphicRef<C>,
+  ) => {
+    const handleClick = (event: React.MouseEvent<HTMLTextAreaElement>) => {
+      if (onClick && !disabled) {
+        onClick?.(event);
+      }
+    };
+
+    const handleBlur = (event: React.FocusEvent<HTMLTextAreaElement>) => {
+      onBlur?.(event);
+    };
+
+    const handleFocus = (event: React.FocusEvent<HTMLTextAreaElement>) => {
+      onFocus?.(event);
+    };
+
+    return (
+      <Text
+        className={classnames(
+          'mm-textarea',
+          `mm-textarea--resize-${resize}`,
+          {
+            'mm-textarea--disabled': Boolean(disabled),
+          },
+          className,
+        )}
+        as="textarea"
+        ref={ref}
+        placeholder={placeholder}
+        readOnly={readOnly}
+        required={required}
+        autoFocus={autoFocus}
+        defaultValue={defaultValue}
+        disabled={disabled}
+        {...(error && { 'aria-invalid': error })}
+        id={id}
+        maxLength={maxLength}
+        name={name}
+        value={value}
+        onBlur={handleBlur}
+        onChange={onChange}
+        onClick={handleClick}
+        onFocus={handleFocus}
+        resize={resize}
+        rows={rows}
+        cols={cols}
+        backgroundColor={BackgroundColor.backgroundDefault}
+        borderColor={
+          error ? BorderColor.errorDefault : BorderColor.borderDefault
+        }
+        borderRadius={BorderRadius.SM}
+        borderWidth={1}
+        paddingBottom={1}
+        paddingLeft={4}
+        paddingRight={4}
+        paddingTop={1}
+        {...(props as TextProps<C>)}
+      />
+    );
+  },
+);
