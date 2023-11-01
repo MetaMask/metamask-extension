0030d7d6877e52fb926df41ca782bcb66a2a4618
Kate Johnson <johnson.kat.g@gmail.com>
2023-11-01 15:13:57 -0400

    Added back watch only selector

--- ui/selectors/selectors.js
+++ ui/selectors/selectors.js
@@ -2,42 +2,39 @@
 import { SubjectType } from '@metamask/permission-controller';
 ///: END:ONLY_INCLUDE_IN
 import { ApprovalType } from '@metamask/controller-utils';
-import {
-  ///: BEGIN:ONLY_INCLUDE_IN(snaps)
-  memoize,
-  ///: END:ONLY_INCLUDE_IN
-} from 'lodash';
+import { memoize } from 'lodash';
 import { createSelector } from 'reselect';
 import { NameType } from '@metamask/name-controller';
+import { EthMethod } from '@metamask/keyring-api';
 import { addHexPrefix } from '../../app/scripts/lib/util';
 import {
-  TEST_CHAINS,
-  NATIVE_CURRENCY_TOKEN_IMAGE_MAP,
-  BUYABLE_CHAINS_MAP,
-  MAINNET_DISPLAY_NAME,
-  BSC_DISPLAY_NAME,
-  POLYGON_DISPLAY_NAME,
-  AVALANCHE_DISPLAY_NAME,
   AURORA_DISPLAY_NAME,
+  AVALANCHE_DISPLAY_NAME,
+  BSC_DISPLAY_NAME,
+  BUYABLE_CHAINS_MAP,
+  CHAIN_ID_TO_NETWORK_IMAGE_URL_MAP,
   CHAIN_ID_TO_RPC_URL_MAP,
   CHAIN_IDS,
-  NETWORK_TYPES,
-  NetworkStatus,
-  SEPOLIA_DISPLAY_NAME,
-  GOERLI_DISPLAY_NAME,
-  ETH_TOKEN_IMAGE_URL,
-  LINEA_GOERLI_DISPLAY_NAME,
   CURRENCY_SYMBOLS,
-  TEST_NETWORK_TICKER_MAP,
+  ETH_TOKEN_IMAGE_URL,
+  GOERLI_DISPLAY_NAME,
+  LINEA_GOERLI_DISPLAY_NAME,
   LINEA_GOERLI_TOKEN_IMAGE_URL,
   LINEA_MAINNET_DISPLAY_NAME,
   LINEA_MAINNET_TOKEN_IMAGE_URL,
-  CHAIN_ID_TO_NETWORK_IMAGE_URL_MAP,
+  MAINNET_DISPLAY_NAME,
+  NATIVE_CURRENCY_TOKEN_IMAGE_MAP,
+  NETWORK_TYPES,
+  NetworkStatus,
+  POLYGON_DISPLAY_NAME,
+  SEPOLIA_DISPLAY_NAME,
+  TEST_CHAINS,
+  TEST_NETWORK_TICKER_MAP,
 } from '../../shared/constants/network';
 import {
-  WebHIDConnectedStatuses,
-  LedgerTransportTypes,
   HardwareTransportStates,
+  LedgerTransportTypes,
+  WebHIDConnectedStatuses,
 } from '../../shared/constants/hardware-wallets';
 import { KeyringType } from '../../shared/constants/keyring';
 import { MESSAGE_TYPE } from '../../shared/constants/app';
@@ -45,9 +42,9 @@ import { MESSAGE_TYPE } from '../../shared/constants/app';
 import { TRUNCATED_NAME_CHAR_LIMIT } from '../../shared/constants/labels';
 
 import {
-  SWAPS_CHAINID_DEFAULT_TOKEN_MAP,
-  ALLOWED_PROD_SWAPS_CHAIN_IDS,
   ALLOWED_DEV_SWAPS_CHAIN_IDS,
+  ALLOWED_PROD_SWAPS_CHAIN_IDS,
+  SWAPS_CHAINID_DEFAULT_TOKEN_MAP,
 } from '../../shared/constants/swaps';
 
 import {
@@ -56,13 +53,11 @@ import {
 } from '../../shared/constants/bridge';
 
 import {
-  shortenAddress,
   getAccountByAddress,
-  getURLHostName,
-  ///: BEGIN:ONLY_INCLUDE_IN(snaps)
-  removeSnapIdPrefix,
   getSnapName,
-  ///: END:ONLY_INCLUDE_IN
+  getURLHostName,
+  removeSnapIdPrefix,
+  shortenAddress,
 } from '../helpers/utils/util';
 
 import { TEMPLATED_CONFIRMATION_APPROVAL_TYPES } from '../pages/confirmation/templates';
@@ -70,18 +65,18 @@ import { STATIC_MAINNET_TOKEN_LIST } from '../../shared/constants/tokens';
 import { DAY } from '../../shared/constants/time';
 import { TERMS_OF_USE_LAST_UPDATED } from '../../shared/constants/terms';
 import {
+  findKeyringForAddress,
+  getConversionRate,
+  getLedgerTransportType,
   getNativeCurrency,
   getProviderConfig,
-  getConversionRate,
-  isNotEIP1559Network,
-  isEIP1559Network,
-  getLedgerTransportType,
   isAddressLedger,
-  findKeyringForAddress,
+  isEIP1559Network,
+  isNotEIP1559Network,
 } from '../ducks/metamask/metamask';
 import {
-  getLedgerWebHidConnectedStatus,
   getLedgerTransportStatus,
+  getLedgerWebHidConnectedStatus,
 } from '../ducks/app/app';
 import { isEqualCaseInsensitive } from '../../shared/modules/string-utils';
 import { TransactionStatus } from '../../shared/constants/transaction';
@@ -351,7 +346,8 @@ export function getMetaMaskIdentities(state) {
  * Get account balances state.
  *
  * @param {object} state - Redux state
- * @returns {object} A map of account addresses to account objects (which includes the account balance)
+ * @returns {object} A map of account addresses to account objects (which includes the account
+ *   balance)
  */
 export function getMetaMaskAccountBalances(state) {
   return state.metamask.accounts;
@@ -688,6 +684,7 @@ export function getSubjectMetadata(state) {
 const getEmbeddableSvg = memoize(
   (svgString) => `data:image/svg+xml;utf8,${encodeURIComponent(svgString)}`,
 );
+
 ///: END:ONLY_INCLUDE_IN
 
 export function getTargetSubjectMetadata(state, origin) {
@@ -839,6 +836,7 @@ export function getIsBuyableChain(state) {
   const chainId = getCurrentChainId(state);
   return Object.keys(BUYABLE_CHAINS_MAP).includes(chainId);
 }
+
 export function getNativeCurrencyImage(state) {
   const nativeCurrency = getNativeCurrency(state)?.toUpperCase();
   return NATIVE_CURRENCY_TOKEN_IMAGE_MAP[nativeCurrency];
@@ -968,8 +966,10 @@ export const getNotifySnaps = createDeepEqualSelector(
  * @typedef {object} Notification
  * @property {string} id - A unique identifier for the notification
  * @property {string} origin - A string identifing the snap origin
- * @property {EpochTimeStamp} createdDate - A date in epochTimeStramps, identifying when the notification was first committed
- * @property {EpochTimeStamp} readDate - A date in epochTimeStramps, identifying when the notification was read by the user
+ * @property {EpochTimeStamp} createdDate - A date in epochTimeStramps, identifying when the
+ *   notification was first committed
+ * @property {EpochTimeStamp} readDate - A date in epochTimeStramps, identifying when the
+ *   notification was read by the user
  * @property {string} message - A string containing the notification message
  */
 
@@ -1007,6 +1007,7 @@ export const getUnreadNotificationsCount = createSelector(
   getUnreadNotifications,
   (notifications) => notifications.length,
 );
+
 ///: END:ONLY_INCLUDE_IN
 
 /**
@@ -1050,7 +1051,8 @@ function getAllowedAnnouncementIds(state) {
     23: true,
     ///: END:ONLY_INCLUDE_IN
     24: state.metamask.hadAdvancedGasFeesSetPriorToMigration92_3 === true,
-    // This syntax is unusual, but very helpful here.  It's equivalent to `unnamedObject[NOTIFICATION_DROP_LEDGER_FIREFOX] =`
+    // This syntax is unusual, but very helpful here.  It's equivalent to
+    // `unnamedObject[NOTIFICATION_DROP_LEDGER_FIREFOX] =`
     [NOTIFICATION_DROP_LEDGER_FIREFOX]: currentKeyringIsLedger && isFirefox,
     [NOTIFICATION_OPEN_BETA_SNAPS]: true,
     [NOTIFICATION_BUY_SELL_BUTTON]: true,
@@ -1060,7 +1062,8 @@ function getAllowedAnnouncementIds(state) {
 /**
  * @typedef {object} Announcement
  * @property {number} id - A unique identifier for the announcement
- * @property {string} date - A date in YYYY-MM-DD format, identifying when the notification was first committed
+ * @property {string} date - A date in YYYY-MM-DD format, identifying when the notification was
+ *   first committed
  */
 
 /**
@@ -1128,6 +1131,7 @@ export function getShowBetaHeader(state) {
 export function getShowProductTour(state) {
   return state.metamask.showProductTour;
 }
+
 /**
  * To get the useTokenDetection flag which determines whether a static or dynamic token list is used
  *
@@ -1437,6 +1441,7 @@ export const getTokenDetectionSupportNetworkByChainId = (state) => {
       return '';
   }
 };
+
 /**
  * To check if the chainId supports token detection,
  * currently it returns true for Ethereum Mainnet, BSC, Polygon, Avalanche, Linea and Aurora
@@ -1524,7 +1529,8 @@ export function getIstokenDetectionInactiveOnNonMainnetSupportedNetwork(state) {
 }
 
 /**
- * To get the `transactionSecurityCheckEnabled` value which determines whether we use the transaction security check
+ * To get the `transactionSecurityCheckEnabled` value which determines whether we use the
+ * transaction security check
  *
  * @param {*} state
  * @returns Boolean
@@ -1543,6 +1549,7 @@ export function getIsTransactionSecurityCheckEnabled(state) {
 export function getIsSecurityAlertsEnabled(state) {
   return state.metamask.securityAlertsEnabled;
 }
+
 ///: END:ONLY_INCLUDE_IN
 
 ///: BEGIN:ONLY_INCLUDE_IN(keyring-snaps)
@@ -1555,6 +1562,7 @@ export function getIsSecurityAlertsEnabled(state) {
 export function getIsAddSnapAccountEnabled(state) {
   return state.metamask.addSnapAccountEnabled;
 }
+
 ///: END:ONLY_INCLUDE_IN
 
 export function getIsCustomNetwork(state) {
@@ -1669,6 +1677,7 @@ export function getNameSources(state) {
 export function getIsDesktopEnabled(state) {
   return state.metamask.desktopEnabled;
 }
+
 ///: END:ONLY_INCLUDE_IN
 
 ///: BEGIN:ONLY_INCLUDE_IN(snaps)
@@ -1710,6 +1719,7 @@ export function getSnapsInstallPrivacyWarningShown(state) {
 
   return snapsInstallPrivacyWarningShown;
 }
+
 ///: END:ONLY_INCLUDE_IN
 ///: BEGIN:ONLY_INCLUDE_IN(keyring-snaps)
 export function getsnapsAddSnapAccountModalDismissed(state) {
@@ -1736,6 +1746,24 @@ export function getKeyringSnapAccounts(state) {
 
 export function getKeyringSnapRemovalResult(state) {
   return state.appState.keyringRemovalSnapModal;
+}
+
+/**
+ * Returns true if the account is a watch-only account (i.e. cannot sign transactions).
+ *
+ * @param state - Redux state object.
+ * @returns {boolean} True if the account is a watch-only account.
+ */
+export function isWatchOnlyAccount(state) {
+  const selectedAccount = getSelectedAccount(state);
+  return !selectedAccount?.methods.includes(
+    EthMethod.Sign,
+    EthMethod.PersonalSign,
+    EthMethod.SignTransaction,
+    EthMethod.SignTypedDataV1,
+    EthMethod.SignTypedDataV3,
+    EthMethod.SignTypedDataV4,
+  );
 }
 
 ///: END:ONLY_INCLUDE_IN
