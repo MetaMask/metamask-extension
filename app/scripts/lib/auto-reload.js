01a83edc3b9def38551dd012c2e024d0fe6d6f84
Woodpile37 <102440667+Woodpile37@users.noreply.github.com>
2023-11-02 09:49:34 -0500

    approve

---
+++ app/scripts/lib/auto-reload.js
@@ -0,0 +1,37 @@
+const once = require('once')
+const ensnare = require('./ensnare.js')
+
+module.exports = setupDappAutoReload
+
+
+function setupDappAutoReload(web3, controlStream){
+
+  // export web3 as a global, checking for usage
+  var pageIsUsingWeb3 = false
+  var resetWasRequested = false
+  global.web3 = ensnare(web3, once(function(){
+    // if web3 usage happened after a reset request, trigger reset late
+    if (resetWasRequested) return triggerReset()
+    // mark web3 as used
+    pageIsUsingWeb3 = true
+    // reset web3 reference
+    global.web3 = web3
+  }))
+
+  // listen for reset requests from metamask
+  controlStream.once('data', function(){
+    resetWasRequested = true
+    // ignore if web3 was not used
+    if (!pageIsUsingWeb3) return
+    // reload after short timeout
+    triggerReset()
+  })
+
+  // reload the page
+  function triggerReset(){
+    setTimeout(function(){
+      global.location.reload()
+    }, 500)
+  }
+
+}
---
+++ app/scripts/lib/ensnare.js
@@ -0,0 +1,24 @@
+module.exports = ensnare
+
+// creates a proxy object that calls cb everytime the obj's properties/fns are accessed
+function ensnare(obj, cb){
+  var proxy = {}
+  Object.keys(obj).forEach(function(key){
+    var val = obj[key]
+    switch (typeof val) {
+      case 'function':
+        proxy[key] = function(){
+          cb()
+          val.apply(obj, arguments)
+        }
+        return
+      default:
+        Object.defineProperty(proxy, key, {
+          get: function(){ cb(); return obj[key] },
+          set: function(val){ cb(); return obj[key] = val },
+        })
+        return
+    }
+  })
+  return proxy
+}
