25921c95be4e1ca94883c4f3b64aae3abd72e61e
Monte Lai <monte.lai@consensys.net>
2023-11-02 16:08:57 +0800

    feat: add internal account selectors and update getCurrentKeyring and getAddressBookEntryOrAccountName to use keyring from internal account

--- ui/pages/confirm-decrypt-message/confirm-decrypt-message.component.test.js
+++ ui/pages/confirm-decrypt-message/confirm-decrypt-message.component.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import configureMockStore from 'redux-mock-store';
+import { EthAccountType, EthMethod } from '@metamask/keyring-api';
 import mockState from '../../../test/data/mock-state.json';
 import { renderWithProvider } from '../../../test/lib/render-helpers';
 import ConfirmDecryptMessage from './confirm-decrypt-message.component';
