91b6608320f7576e93460adc3c02b62e05c2fc4e
Monte Lai <monte.lai@consensys.net>
2023-11-03 08:11:23 +0800

    fix: update confirm-page and import-tokens-modal to use InternalAccounts

--- ui/components/app/confirm-page-container/confirm-page-container.component.js
+++ ui/components/app/confirm-page-container/confirm-page-container.component.js
@@ -45,9 +45,9 @@ import useTransactionInsights from '../../../hooks/useTransactionInsights';
 import {
   getAccountName,
   getAddressBookEntry,
+  getInternalAccounts,
   getIsBuyableChain,
   getMetadataContractName,
-  getMetaMaskIdentities,
   getNetworkIdentifier,
   getSwapsDefaultToken,
 } from '../../../selectors';
@@ -124,8 +124,8 @@ const ConfirmPageContainer = (props) => {
   const networkIdentifier = useSelector(getNetworkIdentifier);
   const defaultToken = useSelector(getSwapsDefaultToken);
   const accountBalance = defaultToken.string;
-  const identities = useSelector(getMetaMaskIdentities);
-  const ownedAccountName = getAccountName(identities, toAddress);
+  const accounts = useSelector(getInternalAccounts);
+  const ownedAccountName = getAccountName(accounts, toAddress);
   const toName = ownedAccountName || contact?.name;
   const recipientIsOwnedAccount = Boolean(ownedAccountName);
   const toMetadataName = useSelector((state) =>
