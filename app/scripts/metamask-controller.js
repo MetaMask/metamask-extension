722cd628d1b63c64722dc89acf1ba4b6250aa538
OGPoyraz <omergoktugpoyraz@gmail.com>
2023-11-07 16:10:32 +0100

    Temp

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -168,6 +168,9 @@ import { isManifestV3 } from '../../shared/modules/mv3.utils';
 import { hexToDecimal } from '../../shared/modules/conversion.utils';
 import { convertNetworkId } from '../../shared/modules/network.utils';
 import {
+  ///: BEGIN:ONLY_INCLUDE_IN(build-mmi)
+  handleMMITransactionUpdate,
+  ///: END:ONLY_INCLUDE_IN
   handleTransactionAdded,
   handleTransactionApproved,
   handleTransactionFailed,
@@ -1436,6 +1439,8 @@ export default class MetamaskController extends EventEmitter {
     );
 
     ///: BEGIN:ONLY_INCLUDE_IN(build-mmi)
+    const transactionMetricsRequest = this.getTransactionMetricsRequest();
+
     this.mmiController = new MMIController({
       mmiConfigurationController: this.mmiConfigurationController,
       keyringController: this.keyringController,
@@ -1455,6 +1460,10 @@ export default class MetamaskController extends EventEmitter {
       signatureController: this.signatureController,
       platform: this.platform,
       extension: this.extension,
+      trackTransactionEvents: handleMMITransactionUpdate.bind(
+        null,
+        transactionMetricsRequest,
+      ),
     });
     ///: END:ONLY_INCLUDE_IN
 
