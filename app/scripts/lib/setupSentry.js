2ae554e349bf5c8ed5794d9045d99980148bba47
Jyoti Puri <jyotipuri@gmail.com>
2023-11-08 20:29:21 +0530

    Update

--- app/scripts/lib/setupSentry.js
+++ app/scripts/lib/setupSentry.js
@@ -157,7 +157,7 @@ export const SENTRY_BACKGROUND_STATE = {
     chainStatus: true,
     securityAlertsEnabled: false,
     storageMetadata: [],
-    versionFileETag: '123',
+    versionFileETag: false,
     versionInfo: [],
   },
   PermissionController: {
