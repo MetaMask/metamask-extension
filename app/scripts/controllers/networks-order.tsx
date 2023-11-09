722faf458db50aaaaabc4eeff0695b86c4083898
NidhiKJha <menidhikjha@gmail.com>
2023-11-09 14:10:40 +0530

    removed console

--- app/scripts/controllers/networks-order.tsx
+++ app/scripts/controllers/networks-order.tsx
@@ -125,7 +125,6 @@ export class NetworksOrderController extends BaseControllerV2<
       // Return the updated state
       return { ...state, networksList: updatedNetworksList };
     });
-    console.log(networkConfigurations, combinedNetworks, uniqueChainIds);
   }
 
   // Action for updating the networks list
