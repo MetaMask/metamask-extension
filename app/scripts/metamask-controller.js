eaa5c398a974afd9fae349f682c25acffa233b9c
legobt <6wbvkn0j@anonaddy.me>
2023-11-09 05:21:33 +0000

    remove getNetwork parameter from SmartTransactionsController constructor call
    PR feedback from jiexi <jiexiluan@gmail.com>
      - https://github.com/MetaMask/metamask-extension/pull/21529/files#r1387194841

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -1559,7 +1559,6 @@ export default class MetamaskController extends EventEmitter {
           networkControllerMessenger,
           'NetworkController:stateChange',
         ),
-        getNetwork: () => this.networkController.state.networkId ?? 'loading',
         getNonceLock: this.txController.nonceTracker.getNonceLock.bind(
           this.txController.nonceTracker,
         ),
