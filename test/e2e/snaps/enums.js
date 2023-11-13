7ab70cdff4634f6edf244a5b0c62defbbcd2a554
bowensanders <bowensanders@gmail.com>
2023-11-13 10:35:47 -0800

    fixed enums

--- test/e2e/snaps/enums.js
+++ test/e2e/snaps/enums.js
@@ -1,4 +1,3 @@
 module.exports = {
-  // TEST_SNAPS_WEBSITE_URL: 'https://metamask.github.io/snaps/test-snaps/2.1.0/',
-  TEST_SNAPS_WEBSITE_URL: 'http://localhost:9000/',
+  TEST_SNAPS_WEBSITE_URL: 'https://metamask.github.io/snaps/test-snaps/2.2.1/',
 };
