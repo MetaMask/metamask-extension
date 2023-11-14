bc80a83cdf7d2b33cc59d04a2b99902ccaee5382
Jiexi Luan <jiexiluan@gmail.com>
2023-11-13 17:01:39 -0800

    Fix errors e2e spec

--- app/scripts/lib/setupSentry.js
+++ app/scripts/lib/setupSentry.js
@@ -108,6 +108,7 @@ export const SENTRY_BACKGROUND_STATE = {
     estimatedGasFeeTimeBounds: true,
     gasEstimateType: true,
     gasFeeEstimates: true,
+    gasFeeEstimatesByChainId: true,
   },
   KeyringController: {
     isUnlocked: true,
