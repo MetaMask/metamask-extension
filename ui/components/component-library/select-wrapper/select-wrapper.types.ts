40c532ae3726a7ea117c1a28a37149d0bc5160bf
Garrett Bear <gwhisten@gmail.com>
2023-11-15 08:09:02 -0800

    Update ui/components/component-library/select-wrapper/select-wrapper.types.ts
    Co-authored-by: George Marshall <george.marshall@consensys.net>

--- ui/components/component-library/select-wrapper/select-wrapper.types.ts
+++ ui/components/component-library/select-wrapper/select-wrapper.types.ts
@@ -67,10 +67,6 @@ export interface SelectWrapperStyleUtilityProps extends StyleUtilityProps {
    */
   onOpenChange?: any; // TO DO: confirm type
   /**
-   * Callback function that is called when the SelectWrapper component receives focus.
-   */
-  onFocus?: () => void;
-  /**
    * Callback function that is called when the SelectWrapper component loses focus.
    * The event object is passed as an argument to the function.
    */
