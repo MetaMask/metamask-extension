5d818bb2b3af140dfbe4bb20c95fb0496911e920
Ramon AC <36987446+racitores@users.noreply.github.com>
2023-11-15 17:15:59 +0100

    add: mmi playwright e2e tests (#21432)
    ## **Description**
    This PR adds playwright test capabilities for the mmi extension build.
    It also adds functional and visual tests for that build. This is part of
    the MMI e2e legacy tests.
    
    NOTE: The new test set will be added to the current pipeline as a
    non-blocking step. This means that even if these tests fail, other Pull
    Requests can still be merged. However, a failure will trigger a warning
    indicating potential issues with MMI functionality due to the changes in
    these PRs.
    
    For more detailed info about how the MMI e2e tests works, check the
    README file within the `test/e2e/mmi` folder.
    
    ## **Manual testing steps**
    
    N/A
    
    ## **Screenshots/Recordings**
    
    N/A
    ### **Before**
    
    N/A
    ### **After**
    
    N/A
    ## **Related issues**
    
    N/A
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've clearly explained:
      - [ ] What problem this PR is solving.
      - [ ] How this problem was solved.
      - [ ] How reviewers can test my changes.
    - [ ] I’ve indicated what issue this PR is linked to: Fixes #???
    - [ ] I’ve included tests if applicable.
    - [ ] I’ve documented any added code.
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/LABELING_GUIDELINES.md)).
    - [ ] I’ve properly set the pull request status:
      - [ ] In case it's not yet "ready for review", I've set it to "draft".
    - [ ] In case it's "ready for review", I've changed it from "draft" to
    "non-draft".
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: MetaMask Bot <metamaskbot@users.noreply.github.com>

---
+++ test/e2e/mmi/pageObjects/mmi-mainMenu-page.ts
@@ -0,0 +1,122 @@
+import { type Locator, type Page, expect } from '@playwright/test';
+import { MMIMainPage } from './mmi-main-page';
+
+export class MMIMainMenuPage {
+  readonly page: Page;
+
+  readonly extensionId: string;
+
+  readonly mainMenuBtn: Locator;
+
+  readonly connectCustodianBtn: Locator;
+
+  readonly tokenTxt: Locator;
+
+  readonly connectCustodianConfirmBtn: Locator;
+
+  readonly connectAccountBtn: Locator;
+
+  readonly closeAddAccountBtn: Locator;
+
+  readonly activityTab: Locator;
+
+  readonly NFTsTab: Locator;
+
+  readonly closeSettingsBtn: Locator;
+
+  readonly passwordTxt: Locator;
+
+  readonly accountsMenuBtn: Locator;
+
+  constructor(page: Page, extensionId: string) {
+    this.extensionId = extensionId;
+    this.page = page;
+    this.mainMenuBtn = page.getByTestId('account-options-menu-button');
+    this.connectCustodianBtn = page.getByRole('button', {
+      name: 'Connect Custodial Account',
+    });
+    this.tokenTxt = page.locator('textarea#jwt-box');
+    this.connectCustodianConfirmBtn = page.getByRole('button', {
+      name: 'Connect',
+    });
+    this.connectAccountBtn = page.locator('button:has-text("Connect")');
+    this.closeAddAccountBtn = page.locator('button:has-text("Close")');
+    this.activityTab = page.locator('button:has-text("Activity")');
+    this.NFTsTab = page.locator('button:has-text("NFTs")');
+    this.closeSettingsBtn = page.locator(
+      '.settings-page__header__title-container__close-button',
+    );
+    this.passwordTxt = page.locator('input#password');
+
+    this.accountsMenuBtn = page.getByTestId('account-menu-icon');
+  }
+
+  async goto() {
+    await this.page.goto(`chrome-extension://${this.extensionId}/home.html`);
+  }
+
+  async openMenu() {
+    await this.mainMenuBtn.click();
+  }
+
+  async accountsMenu() {
+    await this.accountsMenuBtn.click();
+  }
+
+  async lockExtension() {
+    await this.openMenu();
+    await this.page.locator('button >> text=Lock').click();
+  }
+
+  async unlockExtension() {
+    await this.passwordTxt.fill(process.env.MMI_E2E_MMI_PASSWORD as string);
+    await this.page.locator('button >> text=Unlock').click();
+  }
+
+  async selectMenuOption(option: string) {
+    await this.openMenu();
+    await this.page.locator(`data-testid=global-menu-${option}`).click();
+  }
+
+  async selectSettings(option: string) {
+    const regex = new RegExp(option, 'iu');
+    await this.page
+      .locator('.tab-bar')
+      .getByRole('button', {
+        name: regex,
+      })
+      .click();
+  }
+
+  async switchTestNetwork() {
+    await this.page
+      .locator(
+        'text=Show test networksSelect this to show test networks in network listOffOn >> label',
+      )
+      .click();
+  }
+
+  async showIncomingTransactionsOff() {
+    await this.page.getByText(/Security & Privacy/iu).click();
+    await this.page
+      .locator(
+        '.settings-page__content-row >> :scope:has-text("Show Incoming Transactions") >> .toggle-button--on',
+      )
+      .click();
+  }
+
+  async isInteractiveReplacementTokenNotificationVisible(account: string) {
+    await expect(
+      this.page.locator('.interactive-replacement-token-notification'),
+    ).toHaveCount(1);
+    const mainPage = new MMIMainPage(this.page);
+    await mainPage.mainPageScreenshot(
+      'token-replacement-notification.png',
+      account,
+    );
+  }
+
+  async closeSettings() {
+    await this.closeSettingsBtn.click();
+  }
+}
