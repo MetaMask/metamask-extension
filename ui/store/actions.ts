b818666395a67e93b77b6dfdad6573934a6c156a
NidhiKJha <menidhikjha@gmail.com>
2023-11-17 11:43:14 +0530

    removed indicators in actions

--- ui/store/actions.ts
+++ ui/store/actions.ts
@@ -3584,10 +3584,8 @@ export function removePermissionsFor(
 export function updateNetworksList(
   orderedNetworkList: [],
 ): ThunkAction<void, MetaMaskReduxState, unknown, AnyAction> {
-  return async (dispatch: MetaMaskReduxDispatch) => {
-    dispatch(showLoadingIndication());
-    await submitRequestToBackground('updateNetworksList', [orderedNetworkList])
-    dispatch(hideLoadingIndication());
+  return async () => {
+    await submitRequestToBackground('updateNetworksList', [orderedNetworkList]);
   };
 }
 
