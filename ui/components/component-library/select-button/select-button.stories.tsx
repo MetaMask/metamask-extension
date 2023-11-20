ee0160ba6009627502cd83a277f58cf7378688fc
garrettbear <gwhisten@gmail.com>
2023-11-20 13:55:15 -0800

    add SelectButton component

---
+++ ui/components/component-library/select-button/select-button.stories.tsx
@@ -0,0 +1,67 @@
+import { StoryFn, Meta } from '@storybook/react';
+import React from 'react';
+import { SelectWrapper } from '../select-wrapper';
+import README from './README.mdx';
+
+import { SelectButtonSize } from './select-button.types';
+import { SelectButton } from '.';
+
+export default {
+  title: 'Components/ComponentLibrary/SelectButton',
+  component: SelectButton,
+  parameters: {
+    docs: {
+      page: README,
+    },
+  },
+  argTypes: {},
+  args: {
+    children: 'SelectButton',
+  },
+} as Meta<typeof SelectButton>;
+
+const SelectButtonStory: StoryFn<typeof SelectButton> = (args) => {
+  return (
+    <SelectWrapper triggerComponent={<SelectButton {...args} />}>
+      Demo
+    </SelectWrapper>
+  );
+};
+
+export const DefaultStory = SelectButtonStory.bind({});
+DefaultStory.storyName = 'Default';
+
+export const Size: StoryFn<typeof SelectButton> = (args) => {
+  return (
+    <>
+      <SelectWrapper
+        triggerComponent={
+          <SelectButton size={SelectButtonSize.Sm} {...args}>
+            {SelectButtonSize.Sm}
+          </SelectButton>
+        }
+      >
+        Demo
+      </SelectWrapper>
+      <SelectWrapper
+        triggerComponent={
+          <SelectButton size={SelectButtonSize.Md} {...args}>
+            {SelectButtonSize.Md}
+          </SelectButton>
+        }
+      >
+        Demo
+      </SelectWrapper>
+      <SelectWrapper
+        triggerComponent={
+          <SelectButton size={SelectButtonSize.Lg} {...args}>
+            {SelectButtonSize.Lg}
+          </SelectButton>
+        }
+      >
+        Demo
+      </SelectWrapper>
+    </>
+  );
+};
+Size.args = {};
