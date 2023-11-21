b6ebec0d23d02fdea89760f8d5c45e27038ce044
BelfordZ <belfordz66@gmail.com>
2023-11-21 09:53:25 -0800

    remove debugger

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -4534,8 +4534,6 @@ export default class MetamaskController extends EventEmitter {
       // handle any middleware cleanup
       engine._middleware.forEach((mid) => {
         if (mid.destroy && typeof mid.destroy === 'function') {
-          console.log('DESTROY MIDDLEWARE!!');
-          debugger;
           mid.destroy();
         }
       });
