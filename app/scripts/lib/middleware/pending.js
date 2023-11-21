a2bbc60f6301d8d3611f977ffbef573a119b66c8
BelfordZ <belfordz66@gmail.com>
2023-11-21 09:32:22 -0800

    wip

--- app/scripts/lib/middleware/pending.js
+++ app/scripts/lib/middleware/pending.js
@@ -2,6 +2,8 @@ import { createAsyncMiddleware } from 'json-rpc-engine';
 import { formatTxMetaForRpcResult } from '../util';
 
 export function createPendingNonceMiddleware({ getPendingNonce }) {
+    // this would not be using the provider for the current domain...
+
   return createAsyncMiddleware(async (req, res, next) => {
     const { method, params } = req;
     if (method !== 'eth_getTransactionCount') {
