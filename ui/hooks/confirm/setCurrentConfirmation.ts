89aba70630a81955d26ea6bb78043abbf361f69b
Jyoti Puri <jyotipuri@gmail.com>
2023-11-29 18:46:44 +0530

    Update ui/hooks/confirm/setCurrentConfirmation.ts
    Co-authored-by: Ariella Vu <20778143+digiwand@users.noreply.github.com>

--- ui/hooks/confirm/setCurrentConfirmation.ts
+++ ui/hooks/confirm/setCurrentConfirmation.ts
@@ -6,7 +6,7 @@ import useCurrentConfirmation from './useCurrentConfirmation';
 
 /*
  * This hook is called from <Confirm /> component to set current transaction.
- * This hook should be required to be invoked only when we are setting or re-setting
+ * This hook should be invoked only when we are setting or resetting the
  * current confirmation displayed to the user.
  */
 const setCurrentConfirmation = () => {
