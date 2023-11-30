0729877c35bca0e0e887d56f9b56fa11ab11ac7d
digiwand <20778143+digiwand@users.noreply.github.com>
2023-11-30 06:35:28 -0800

    feat: create ConfirmInfo WIP

---
+++ ui/components/app/confirm/info/info.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Box } from '../../../component-library';
+import {
+  BackgroundColor,
+  BorderRadius,
+} from '../../../../helpers/constants/design-system';
+
+import type { StyleUtilityProps } from '../../../component-library/box';
+
+interface ConfirmInfoProps extends StyleUtilityProps {
+  children: React.ReactNode | React.ReactNode[];
+}
+
+export const ConfirmInfo: React.FC<ConfirmInfoProps> = ({
+  children,
+  ...props
+}) => {
+  return (
+    <Box
+      className="confirm-info"
+      borderRadius={BorderRadius.LG}
+      backgroundColor={BackgroundColor.backgroundDefault}
+      {...props}
+    >
+      {children}
+    </Box>
+  );
+};
+
+ConfirmInfo.propTypes = {
+  children: PropTypes.node,
+};
