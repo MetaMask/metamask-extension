f32fd9c2087c0179bbeaf1de9ad58266d057b07d
Jyoti Puri <jyotipuri@gmail.com>
2023-11-30 12:58:40 +0530

    Update

--- ui/components/app/confirm/header/header.test.tsx
+++ ui/components/app/confirm/header/header.test.tsx
@@ -4,7 +4,7 @@ import mockState from '../../../../../test/data/mock-state.json';
 import { renderWithProvider } from '../../../../../test/jest';
 import configureStore from '../../../../store/store';
 
-import Header from './Header';
+import Header from './header';
 
 const render = () => {
   const store = configureStore({
