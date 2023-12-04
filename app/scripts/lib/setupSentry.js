29e6904ae729676474b9bbd9ed64bcd907ae606d
NidhiKJha <menidhikjha@gmail.com>
2023-12-04 19:05:57 +0530

    updated spec files for e2e

--- app/scripts/lib/setupSentry.js
+++ app/scripts/lib/setupSentry.js
@@ -83,7 +83,7 @@ export const SENTRY_BACKGROUND_STATE = {
     showBetaHeader: true,
     showProductTour: true,
     showTestnetMessageInDropdown: true,
-    surveyLinkLastClickedOrClosed: true,
+    surveyLinkLastClickedOrClosed: null,
     snapsInstallPrivacyWarningShown: true,
     termsOfUseLastAgreed: true,
     timeoutMinutes: true,
