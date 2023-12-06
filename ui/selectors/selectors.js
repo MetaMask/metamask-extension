f50a3fa90513b0960b46c3e304ef9fe0913ea65b
David Walsh <davidwalsh83@gmail.com>
2023-12-06 10:25:32 -0600

    Remove unneeded Aurora references

--- ui/selectors/selectors.js
+++ ui/selectors/selectors.js
@@ -18,7 +18,6 @@ import {
   BSC_DISPLAY_NAME,
   POLYGON_DISPLAY_NAME,
   AVALANCHE_DISPLAY_NAME,
-  AURORA_DISPLAY_NAME,
   CHAIN_ID_TO_RPC_URL_MAP,
   CHAIN_IDS,
   NETWORK_TYPES,
@@ -1549,8 +1548,6 @@ export const getTokenDetectionSupportNetworkByChainId = (state) => {
       return LINEA_GOERLI_DISPLAY_NAME;
     case CHAIN_IDS.LINEA_MAINNET:
       return LINEA_MAINNET_DISPLAY_NAME;
-    case CHAIN_IDS.AURORA:
-      return AURORA_DISPLAY_NAME;
     default:
       return '';
   }
@@ -1571,7 +1568,6 @@ export function getIsDynamicTokenListAvailable(state) {
     CHAIN_IDS.AVALANCHE,
     CHAIN_IDS.LINEA_GOERLI,
     CHAIN_IDS.LINEA_MAINNET,
-    CHAIN_IDS.AURORA,
   ].includes(chainId);
 }
 
