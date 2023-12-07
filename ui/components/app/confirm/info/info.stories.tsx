253b54e87cef103bfeafe4f1ca15f8d0376aa7c5
digiwand <20778143+digiwand@users.noreply.github.com>
2023-12-06 18:18:57 -0800

    feat: create ConfirmInfo

---
+++ ui/components/app/confirm/info/info.stories.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { ConfirmInfoRow } from '../shared/info/row';
+import { ConfirmInfo, ConfirmInfoRowConfig, ConfirmInfoRowType } from './info';
+
+const mockRowConfigs: ConfirmInfoRowConfig[] = [
+  {
+    label: 'Address',
+    variant: ConfirmInfoRowType.Address,
+    rowProps: {
+      address: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
+    },
+  },
+  {
+    variant: ConfirmInfoRowType.Divider,
+  },
+  // todo: replace row variant for Message. Possible value or value-expandable. neither have been created yet
+  {
+    label: 'Message',
+    variant: ConfirmInfoRowType.ValueDouble,
+    rowProps: {
+      left: '',
+      right:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel suscipit tortor. Curabitur vulputate felis nibh, vel pellentesque erat fermentum eget. Duis id turpis cursus, blandit magna sit amet, tempor sem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas ex nulla, suscipit id eros in, elementum lacinia leo. Etiam dignissim neque vitae nibh pretium, sed egestas est mollis. Nam venenatis tellus sed tempor bibendum.',
+    },
+  },
+];
+
+const ConfirmInfoStory = {
+  title: 'Components/App/Confirm/Info',
+  component: ConfirmInfoRow,
+
+  args: { rowConfigs: [...mockRowConfigs] },
+  argTypes: {
+    rowConfigs: {
+      control: {
+        type: 'object',
+      },
+    },
+  },
+};
+
+export const DefaultStory = (args) => <ConfirmInfo {...args} />;
+
+DefaultStory.storyName = 'Default';
+
+export default ConfirmInfoStory;
--- ui/components/app/confirm/info/info.tsx
+++ ui/components/app/confirm/info/info.tsx
@@ -1,33 +1,86 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Box } from '../../../component-library';
+
 import {
-  BackgroundColor,
-  BorderRadius,
-} from '../../../../helpers/constants/design-system';
+  ConfirmInfoRow,
+  ConfirmInfoRowProps,
+  ConfirmInfoRowAddress,
+  ConfirmInfoRowDivider,
+  ConfirmInfoRowValueDouble,
+  /** todo: rename ConfirmInfoRowVariant to ConfirmInfoRowState */
+  ConfirmInfoRowVariant as ConfirmInfoRowState,
+} from '../shared/info/row';
+import { ConfirmInfoContainer } from './container';
 
-import type { StyleUtilityProps } from '../../../component-library/box';
+/**
+ * todo: rename ConfirmInfoRowVariant to ConfirmInfoRowState
+ * and rename ConfirmInfoRowType to ConfirmInfoRowVariant
+ */
+export enum ConfirmInfoRowType {
+  Address = 'address',
+  Divider = 'divider',
+  ValueDouble = 'value-double',
+}
 
-interface ConfirmInfoProps extends StyleUtilityProps {
-  children: React.ReactNode | React.ReactNode[];
+export type ConfirmInfoRowConfig = {
+  /** The variant of the row e.g. address, divider, value-double */
+  variant: ConfirmInfoRowType;
+
+  /** The display label text. This should be required unless it is a 'divider' variant */
+  label?: ConfirmInfoRowProps['label'];
+
+  /** Optional, and likely needed, props passed to the row */
+  rowProps?: Record<string, any>;
+
+  /** Optional row state */
+  state?: ConfirmInfoRowState;
+};
+
+interface ConfirmInfoProps {
+  rowConfigs: ConfirmInfoRowConfig[];
 }
 
 export const ConfirmInfo: React.FC<ConfirmInfoProps> = ({
-  children,
-  ...props
-}) => {
-  return (
-    <Box
-      className="confirm-info"
-      borderRadius={BorderRadius.LG}
-      backgroundColor={BackgroundColor.backgroundDefault}
-      {...props}
-    >
-      {children}
-    </Box>
-  );
-};
+  rowConfigs = [],
+}) => (
+  <ConfirmInfoContainer>
+    {rowConfigs.map((rowConfig: ConfirmInfoRowConfig, index) => {
+      const { label, rowProps, state, variant } = rowConfig;
+      const key = `confirm-info-row-${label}-${index}`;
 
-ConfirmInfo.propTypes = {
-  children: PropTypes.node,
-};
+      switch (variant) {
+        case ConfirmInfoRowType.Address:
+          return (
+            <React.Fragment key={key}>
+              <ConfirmInfoRow label={label || ''} variant={state}>
+                <ConfirmInfoRowAddress address={rowProps?.address} />
+              </ConfirmInfoRow>
+            </React.Fragment>
+          );
+        case ConfirmInfoRowType.Divider: {
+          const dividerKey = `confirm-info-divider-${rowConfigs
+            .map(({ label: _label }) => _label)
+            .concat('-')}-${index}`;
+
+          return (
+            <React.Fragment key={dividerKey}>
+              <ConfirmInfoRowDivider />;
+            </React.Fragment>
+          );
+        }
+        case ConfirmInfoRowType.ValueDouble:
+          return (
+            <React.Fragment key={key}>
+              <ConfirmInfoRow label={label || ''} variant={state}>
+                <ConfirmInfoRowValueDouble
+                  left={rowProps?.left}
+                  right={rowProps?.right}
+                />
+              </ConfirmInfoRow>
+            </React.Fragment>
+          );
+        default:
+          return null;
+      }
+    })}
+  </ConfirmInfoContainer>
+);
