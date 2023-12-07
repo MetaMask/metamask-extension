813062f1d59d253ac4c0d39c31ec0d6777cb44b8
digiwand <20778143+digiwand@users.noreply.github.com>
2023-12-07 01:03:32 -0800

    fix: rm ; typo

--- ui/components/app/confirm/info/info.tsx
+++ ui/components/app/confirm/info/info.tsx
@@ -63,7 +63,7 @@ export const ConfirmInfo: React.FC<ConfirmInfoProps> = ({
 
           return (
             <React.Fragment key={dividerKey}>
-              <ConfirmInfoRowDivider />;
+              <ConfirmInfoRowDivider />
             </React.Fragment>
           );
         }
