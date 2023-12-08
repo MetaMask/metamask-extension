d7f50592239fb26599358718bcce316e45bca36b
Mako Shan <imakoshan@gmail.com>
2023-12-08 15:39:25 +0800

    Update tutorial link

--- shared/constants/hardware-wallets.ts
+++ shared/constants/hardware-wallets.ts
@@ -48,7 +48,7 @@ export enum HardwareAffiliateTutorialLinks {
   airgap = 'https://support.airgap.it/guides/metamask/',
   coolwallet = 'https://www.coolwallet.io/metamask-step-by-step-guides/',
   dcent = 'https://medium.com/dcentwallet/dcent-wallet-now-supports-qr-based-protocol-to-link-with-metamask-57555f02603f',
-  imtoken = 'https://support.token.im/hc/en-us/articles/24652624775961',
+  imtoken = 'https://support.token.im/hc/en-us/articles/24652624775961/',
 }
 
 /**
