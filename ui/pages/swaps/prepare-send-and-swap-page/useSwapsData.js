e4147e32a0fbe95a741dc74841847fdf19967d66
IF <139582705+infiniteflower@users.noreply.github.com>
2023-12-11 14:57:19 -0500

    chore: remove unneeded code

--- ui/pages/swaps/prepare-send-and-swap-page/useSwapsData.js
+++ ui/pages/swaps/prepare-send-and-swap-page/useSwapsData.js
@@ -8,7 +8,6 @@ import {
 import {
   fetchAndSetSwapsGasPriceInfo,
   fetchSwapsLivenessAndFeatureFlags,
-  // fetchAndSetSwapsGasPriceInfo,
   prepareToLeaveSwaps,
   setAggregatorMetadata,
   setTopAssets,
