f9ef3379a5598da22f7d571b8b739a77835b77a1
Jyoti Puri <jyotipuri@gmail.com>
2023-12-11 15:52:01 +0530

    Update

--- ui/hooks/confirm/setCurrentConfirmation.test.tsx
+++ ui/hooks/confirm/setCurrentConfirmation.test.tsx
@@ -1,27 +1,6 @@
-import * as redux from 'react-redux';
-import { renderHook } from '@testing-library/react-hooks';
-
-import setCurrentConfirmation from './setCurrentConfirmation';
-
-jest.mock('./useCurrentConfirmation', () => () => ({
-  currentConfirmation: { id: '1' },
-}));
-
-jest.mock('react-redux', () => ({
-  ...jest.requireActual('react-redux'),
-  useDispatch: (args: any) => jest.fn(args),
-}));
-
-describe('setCurrentConfirmation', () => {
-  it('should dispatch updateCurrentConfirmation', () => {
-    const useDispatchSpy = jest.spyOn(redux, 'useDispatch');
-    renderHook(() => setCurrentConfirmation());
-    expect(useDispatchSpy).toHaveBeenCalled();
-  });
 import React from 'react';
 import { Provider } from 'react-redux';
 import { renderHook } from '@testing-library/react-hooks';
-
 import * as ConfirmDucks from '../../ducks/confirm/confirm';
 import configureStore from '../../store/store';
 import setCurrentConfirmation from './setCurrentConfirmation';
@@ -33,13 +12,10 @@ const mockState = {
     },
   },
 };
-
 const mockCurrentConfirmation = { id: '1' };
-
 jest.mock('./useCurrentConfirmation', () => () => ({
   currentConfirmation: mockCurrentConfirmation,
 }));
-
 describe('setCurrentConfirmation', () => {
   it('should dispatch updateCurrentConfirmation', () => {
     const updateCurrentConfirmationSpy = jest.spyOn(
@@ -50,7 +26,6 @@ describe('setCurrentConfirmation', () => {
       <Provider store={configureStore(mockState)}>{children}</Provider>
     );
     renderHook(() => setCurrentConfirmation(), { wrapper });
-
     expect(updateCurrentConfirmationSpy).toHaveBeenCalledWith(
       mockCurrentConfirmation,
     );
