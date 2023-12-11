bc36bcb55f25dbe12d3471616eb9fbed67f4a00f
Jyoti Puri <jyotipuri@gmail.com>
2023-12-11 14:27:44 +0530

    Update ui/selectors/index.js
    Co-authored-by: Ariella Vu <20778143+digiwand@users.noreply.github.com>

--- ui/selectors/index.js
+++ ui/selectors/index.js
@@ -1,6 +1,7 @@
 export * from './confirm';
 export * from './confirm-transaction';
 export * from './confirm';
+export * from './confirm-transaction';
 export * from './custom-gas';
 export * from './first-time-flow';
 export * from './metametrics';
