57107e3adf8a1d995c891ef23dbeaec81a01ead8
Jyoti Puri <jyotipuri@gmail.com>
2023-12-11 15:43:53 +0530

    Update ui/hooks/confirm/setCurrentConfirmation.test.ts
    Co-authored-by: Ariella Vu <20778143+digiwand@users.noreply.github.com>

--- ui/hooks/confirm/setCurrentConfirmation.test.ts
+++ ui/hooks/confirm/setCurrentConfirmation.test.ts
@@ -18,4 +18,41 @@ describe('setCurrentConfirmation', () => {
     renderHook(() => setCurrentConfirmation());
     expect(useDispatchSpy).toHaveBeenCalled();
   });
+import React from 'react';
+import { Provider } from 'react-redux';
+import { renderHook } from '@testing-library/react-hooks';
+
+import * as ConfirmDucks from '../../ducks/confirm/confirm';
+import configureStore from '../../store/store';
+import setCurrentConfirmation from './setCurrentConfirmation';
+
+const mockState = {
+  metamask: {
+    confirm: {
+      currentConfirmation: undefined,
+    },
+  },
+};
+
+const mockCurrentConfirmation = { id: '1' };
+
+jest.mock('./useCurrentConfirmation', () => () => ({
+  currentConfirmation: mockCurrentConfirmation,
+}));
+
+describe('setCurrentConfirmation', () => {
+  it('should dispatch updateCurrentConfirmation', () => {
+    const updateCurrentConfirmationSpy = jest.spyOn(
+      ConfirmDucks,
+      'updateCurrentConfirmation',
+    );
+    const wrapper = ({ children }) => (
+      <Provider store={configureStore(mockState)}>{children}</Provider>
+    );
+    renderHook(() => setCurrentConfirmation(), { wrapper });
+
+    expect(updateCurrentConfirmationSpy).toHaveBeenCalledWith(
+      mockCurrentConfirmation,
+    );
+  });
 });
