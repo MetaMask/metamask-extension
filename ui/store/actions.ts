81f7bde183e785d04db598040ba0155d1d131327
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2023-12-14 17:47:11 +0000

    remove redundant `toggleSession()` call

--- ui/store/actions.ts
+++ ui/store/actions.ts
@@ -2938,12 +2938,6 @@ export function setParticipateInMetaMetrics(
             reject(err);
             return;
           }
-          /**
-           * We need to inform sentry that the user's optin preference may have
-           * changed. The logic to determine which way to toggle is in the
-           * toggleSession handler in setupSentry.js.
-           */
-          window.sentry?.toggleSession();
 
           dispatch({
             type: actionConstants.SET_PARTICIPATE_IN_METAMETRICS,
