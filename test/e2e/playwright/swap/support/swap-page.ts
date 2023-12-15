0087475e6fdf6c233613eb2f18856284dcae22b6
Davide Brocchetto <davide.brocchetto@consensys.net>
2023-12-15 17:47:08 +0100

    Lint fix

--- test/e2e/playwright/swap/support/swap-page.ts
+++ test/e2e/playwright/swap/support/swap-page.ts
@@ -1,7 +1,4 @@
-import {
-  type Locator,
-  type Page,
-} from '@playwright/test';
+import { type Locator, type Page } from '@playwright/test';
 
 export class SwapPage {
   static extensionId: string | undefined;
