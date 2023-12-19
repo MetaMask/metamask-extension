d8b91b1c79e991e25f1074695f383d7ddf42eecd
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2023-12-19 10:18:17 +0300

    Comment changes.

--- ui/components/app/security-provider-banner-alert/security-provider-banner-alert.js
+++ ui/components/app/security-provider-banner-alert/security-provider-banner-alert.js
@@ -135,7 +135,7 @@ SecurityProviderBannerAlert.propTypes = {
   /** Function to be called when the support link is clicked */
   onClickSupportLink: PropTypes.func,
 
-  /** URL to open when contact us is linked*/
+  /** URL to open when report a problem link is clicked */
   reportUrl: PropTypes.string,
 };
 
