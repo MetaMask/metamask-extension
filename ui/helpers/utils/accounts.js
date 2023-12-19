0757ded9e56fd8458ea1ebb84dc702149550f987
Kate Johnson <johnson.kat.g@gmail.com>
2023-12-19 16:50:58 -0500

    Added unit tests for getAccountLabel

--- ui/helpers/utils/accounts.js
+++ ui/helpers/utils/accounts.js
@@ -74,10 +74,10 @@ export function getAccountLabel(type, account) {
   switch (type) {
     case KeyringType.hdKeyTree:
       return null;
-    case KeyringType.qr:
-      return HardwareKeyringNames.qr;
     case KeyringType.imported:
       return t('imported');
+    case KeyringType.qr:
+      return HardwareKeyringNames.qr;
     case KeyringType.trezor:
       return HardwareKeyringNames.trezor;
     case KeyringType.ledger:
