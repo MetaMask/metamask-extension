12a488d4b23ac407d6a73fb94473c5659a9af537
Ariella Vu <20778143+digiwand@users.noreply.github.com>
2023-12-19 16:34:51 -0800

    feat: Create ConfirmInfo (#22193)
    ## **Description**
    
    This is the start of ConfirmInfo. 
    
    - Create ConfirmInfo
      - Supports row: address, divider, and value-double
    - Create ConfirmInfoRowDivider
    
    note: 
    The row paddings do not match the figma design. We will need to
    coordinate with Snaps and Design to find the appropriate padding. This
    can be done in a future PR
    
    It's gone through a few iterations and a couple of discussions to get
    here. Please reach out if there are any questions.
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/metamask-extension/issues/21747
    
    ## **Manual testing steps**
    
    1. yarn storybook
    2. http://localhost:6006/?path=/docs/components-app-confirm-info--docs
    
    ## **Screenshots/Recordings**
    
    <img width="676" alt="Screenshot 2023-12-12 at 10 08 26 AM"
    src="https://github.com/MetaMask/metamask-extension/assets/20778143/bdf1e8bf-af76-4897-a2c4-3cb1350a90b3">
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've clearly explained what problem this PR is solving and how it
    is solved.
    - [ ] I've linked related issues
    - [ ] I've included manual testing steps
    - [ ] I've included screenshots/recordings if applicable
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    - [ ] I’ve properly set the pull request status:
      - [ ] In case it's not yet "ready for review", I've set it to "draft".
    - [ ] In case it's "ready for review", I've changed it from "draft" to
    "non-draft".
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

---
+++ ui/components/app/confirm/info/row/divider.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const ConfirmInfoRowDivider: React.FC = () => {
+  return (
+    <div
+      style={{
+        height: '1px',
+        backgroundColor: 'var(--color-border-muted)',
+      }}
+    ></div>
+  );
+};
