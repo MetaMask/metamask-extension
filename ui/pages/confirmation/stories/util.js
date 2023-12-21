50ec47d3fd688b244db530da18b79a1a0ae36c04
gantunesr <gustavoantunesrod@gmail.com>
2023-12-20 21:56:20 -0300

    test: fix mock

--- ui/pages/confirmation/stories/util.js
+++ ui/pages/confirmation/stories/util.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { NetworkStatus } from '@metamask/network-controller';
 import { NetworkType } from '@metamask/controller-utils';
+import { EthAccountType, EthMethod } from '@metamask/keyring-api';
 import configureStore from '../../../store/store';
 import { Box } from '../../../components/component-library';
 
@@ -38,6 +39,37 @@ const STORE_MOCK = {
       },
     },
     tokenList: {},
+    internalAccounts: {
+      selectedAccount: 'cf8dace4-9439-4bd4-b3a8-88c821c8fcb3',
+      accounts: {
+        'cf8dace4-9439-4bd4-b3a8-88c821c8fcb3': {
+          address: '0x64a845a5b02460acf8a3d84503b0d68d028b4bb4',
+          id: 'cf8dace4-9439-4bd4-b3a8-88c821c8fcb3',
+          metadata: {
+            name: 'This is a Really Long Account Name',
+            keyring: {
+              type: 'HD Key Tree',
+            },
+          },
+          options: {},
+          methods: [...Object.values(EthMethod)],
+          type: EthAccountType.Eoa,
+        },
+        '07c2cfec-36c9-46c4-8115-3836d3ac9047': {
+          address: '0xb19ac54efa18cc3a14a5b821bfec73d284bf0c5e',
+          id: '07c2cfec-36c9-46c4-8115-3836d3ac9047',
+          metadata: {
+            name: 'Account 2',
+            keyring: {
+              type: 'HD Key Tree',
+            },
+          },
+          options: {},
+          methods: [...Object.values(EthMethod)],
+          type: EthAccountType.Eoa,
+        },
+      }
+    },
   },
 };
 
