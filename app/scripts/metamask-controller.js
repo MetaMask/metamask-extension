1304c45382c3655116e11633b932b3001f8dab76
adiyanai <adi@blockaid.co>
2023-12-31 16:07:19 +0200

    Revert "Fix tokensControllers instantiation (#22372)"
    This reverts commit c40dbb1ec72b91827debb2fffba4097a838f21c7.

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -547,14 +547,10 @@ export default class MetamaskController extends EventEmitter {
             listener({ selectedAddress: newlySelectedInternalAccount.address });
           },
         ),
-      onNetworkDidChange: (cb) =>
-        networkControllerMessenger.subscribe(
-          'NetworkController:networkDidChange',
-          () => {
-            const networkState = this.networkController.state;
-            return cb(networkState);
-          },
-        ),
+      onNetworkDidChange: networkControllerMessenger.subscribe.bind(
+        networkControllerMessenger,
+        'NetworkController:stateChange',
+      ),
       onTokenListStateChange: (listener) =>
         this.controllerMessenger.subscribe(
           `${this.tokenListController.name}:stateChange`,
@@ -562,6 +558,9 @@ export default class MetamaskController extends EventEmitter {
         ),
       getNetworkClientById: this.networkController.getNetworkClientById.bind(
         this.networkController,
+      ),
+      getERC20TokenName: this.assetsContractController.getERC20TokenName.bind(
+        this.assetsContractController,
       ),
       config: {
         provider: this.provider,
