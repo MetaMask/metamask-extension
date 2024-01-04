df038fab2e930fba2ee8f090e39a5d64ed9deda9
Monte Lai <monte.lai@consensys.net>
2024-01-04 21:49:11 +0800

    fix: previousUserTraits in errors test

--- test/e2e/tests/errors.spec.js
+++ test/e2e/tests/errors.spec.js
@@ -839,6 +839,7 @@ describe('Sentry errors', function () {
       // This can get erased due to a bug in the app state controller's
       // preferences state change handler
       timeoutMinutes: true,
+      previousUserTraits: false, // Initialized as undefined
     };
     await withFixtures(
       {
