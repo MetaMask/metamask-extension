ac837bcb47ff6e611cefd77c2bd727b9d2b8a7d3
Kévin Chassagne <kevin.chassagne@consensys.net>
2024-01-04 16:06:52 +0000

    Update shared/lib/ui-utils.js
    Co-authored-by: Victor Thomas <10986371+vthomas13@users.noreply.github.com>

--- shared/lib/ui-utils.js
+++ shared/lib/ui-utils.js
@@ -9,7 +9,8 @@ export const PRIVACY_POLICY_LINK = 'https://consensys.io/privacy-policy/';
 // TODO make sure these links are correct
 export const ETHERSCAN_PRIVACY_LINK = 'https://etherscan.io/privacyPolicy';
 export const CONSENSYS_PRIVACY_LINK = 'https://consensys.io/privacy-policy/';
-export const AUTO_DETECT_TOKEN_LEARN_MORE_LINK = 'https://consensys.io/privacy-policy/';
+export const AUTO_DETECT_TOKEN_LEARN_MORE_LINK = 
+  'https://consensys.io/privacy-policy/';
 export const CONSENSYS_TERMS_OF_USE = 'https://consensys.io/terms-of-use';
 
 export const OPENSEA_TERMS_OF_USE = 'https://opensea.io/securityproviderterms';
