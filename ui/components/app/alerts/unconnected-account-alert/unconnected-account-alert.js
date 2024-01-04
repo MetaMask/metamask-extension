5d786aad1288abf67496886af7aa445d7e511263
Monte Lai <monte.lai@consensys.net>
2024-01-04 21:05:44 +0800

    fix: lint

--- ui/components/app/alerts/unconnected-account-alert/unconnected-account-alert.js
+++ ui/components/app/alerts/unconnected-account-alert/unconnected-account-alert.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { ALERT_STATE } from '../../../../ducks/alerts';
