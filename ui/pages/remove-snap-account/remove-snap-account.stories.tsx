075db48122e0f020ceec237a781b09309658574b
Frederik Bolding <frederik.bolding@gmail.com>
2024-01-04 12:41:34 +0100

    Improve confirmation screens for account management snaps (#22298)
    ## **Description**
    
    Redesigns the confirmation screens used to manage account management
    snaps according to the Figma. Also bumps ApprovalController to the
    latest version to use new features that allow for more customization of
    success/failure screens. Also fixes some alignment issues and styling
    problems on the snap redirection screen.
    
    ## **Related issues**
    
    Closes https://github.com/MetaMask/MetaMask-planning/issues/1765
    
    ## **Manual testing steps**
    
    1. Go to https://metamask.github.io/snap-simple-keyring/latest/
    2. Connect to the snap
    3. Create an account
    4. Remove the same account
    5. Notice that the flows have changed
    
    Additionally, sign using the redirection flow and notice that the screen
    is properly aligned.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    
    
    https://github.com/MetaMask/metamask-extension/assets/1561200/eee89ee8-40e2-4ff4-87c0-a0ef6db1c697
    
    ---------
    
    Co-authored-by: MetaMask Bot <metamaskbot@users.noreply.github.com>

--- ui/pages/remove-snap-account/remove-snap-account.stories.tsx
+++ ui/pages/remove-snap-account/remove-snap-account.stories.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import configureStore from '../../store/store';
+import testData from '../../../.storybook/test-data';
 import RemoveSnapAccount from './remove-snap-account';
+
+const store = configureStore(testData);
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   title: 'Components/UI/RemoveSnapAccount', // title should follow the folder structure location of the component. Don't use spaces.
+  decorators: [(story) => <Provider store={store}>{story()}</Provider>],
 };
 
 export const DefaultStory = () => (
   <RemoveSnapAccount
     snapId="npm:@metamask/snap-simple-keyring"
     snapName="Test name"
-    publicAddress="0x08e11b189afa860a5f9e52780194a92eee888d43"
+    publicAddress="0x64a845a5b02460acf8a3d84503b0d68d028b4bb4"
   />
 );
 
