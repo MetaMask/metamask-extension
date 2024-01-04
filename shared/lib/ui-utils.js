6032212e5a86ee1e4eae67c9c2dfe655e340c6d6
Kévin Chassagne <kevinchassagne@gmail.com>
2024-01-04 17:45:39 +0100

    Fix lint

--- shared/lib/ui-utils.js
+++ shared/lib/ui-utils.js
@@ -9,7 +9,7 @@ export const PRIVACY_POLICY_LINK = 'https://consensys.io/privacy-policy/';
 // TODO make sure these links are correct
 export const ETHERSCAN_PRIVACY_LINK = 'https://etherscan.io/privacyPolicy';
 export const CONSENSYS_PRIVACY_LINK = 'https://consensys.io/privacy-policy/';
-export const AUTO_DETECT_TOKEN_LEARN_MORE_LINK = 
+export const AUTO_DETECT_TOKEN_LEARN_MORE_LINK =
   'https://consensys.io/privacy-policy/';
 export const CONSENSYS_TERMS_OF_USE = 'https://consensys.io/terms-of-use';
 
