bd6fa6e5d86361e1bcfb1324740693e9bdc038ae
Brian Bergeron <brian.e.bergeron@gmail.com>
2024-01-05 09:56:59 -0800

    temp

--- ui/pages/asset/components/token-asset-v2.tsx
+++ ui/pages/asset/components/token-asset-v2.tsx
@@ -14,19 +14,36 @@ import {
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
 import CrosshairPlugin from 'chartjs-plugin-crosshair';
-import { getCurrentChainId, getCurrentCurrency, getTokenList } from '../../../selectors';
+import {
+  getCurrentChainId,
+  getCurrentCurrency,
+  getTokenList,
+} from '../../../selectors';
 import { isEqualCaseInsensitive } from '../../../../shared/modules/string-utils';
-import { TextVariant } from '../../../helpers/constants/design-system';
+import {
+  Display,
+  JustifyContent,
+  TextColor,
+  TextVariant,
+} from '../../../helpers/constants/design-system';
 import {
   Box,
+  ButtonPrimary,
+  ButtonPrimarySize,
   ButtonSecondary,
   ButtonSecondarySize,
   Text,
 } from '../../../components/component-library';
 import { formatCurrency } from '../../../helpers/utils/confirm-tx.util';
 import { hexToDecimal } from '../../../../shared/modules/conversion.utils';
+import { useI18nContext } from '../../../hooks/useI18nContext';
+
+/** Time range units supported by the price API */
+type TimeRange = `${number}D` | `${number}M` | `${number}Y`;
 
 const TokenAssetV2 = ({ token }: { token: Token }) => {
+  const t = useI18nContext();
+
   // todo what if chain not supported?  how to determine?
   const chainId = hexToDecimal(useSelector(getCurrentChainId));
 
@@ -38,15 +55,15 @@ const TokenAssetV2 = ({ token }: { token: Token }) => {
 
   // todo chain id?
   const tokenData = Object.values(tokenList).find(
-    (t) =>
-      t.symbol === token.symbol &&
-      isEqualCaseInsensitive(t.address, token.address),
+    (listToken) =>
+      listToken.symbol === token.symbol &&
+      isEqualCaseInsensitive(listToken.address, token.address),
   );
   const name = tokenData?.name || token.symbol;
 
   const [spotPrices, setSpotPrices] = useState<any>(); // todo better type
   const [prices, setHistoricalPrices] = useState<any>(); // todo better type
-  const [timeRange, setTimeRange] = useState('1D');
+  const [timeRange, setTimeRange] = useState<TimeRange>('1D');
 
   // todo canonicalize address?  is that necessary?
   // cache these when clicking between????
@@ -74,11 +91,11 @@ const TokenAssetV2 = ({ token }: { token: Token }) => {
     return <></>;
   }
 
-  const firstPrice = prices[0];
-  const lastPrice = prices[prices.length - 1];
-
   // Green if up, red if down
-  const up = lastPrice[1] > firstPrice[1];
+  const firstPrice = prices[0][1];
+  const lastPrice = prices[prices.length - 1][1];
+  const priceDelta = lastPrice - firstPrice;
+  const up = priceDelta >= 0;
   const lineColor = up ? '#28A745' : '#D73847';
   const gradientStart = up
     ? 'rgba(40, 167, 69, 0.30)'
@@ -113,6 +130,7 @@ const TokenAssetV2 = ({ token }: { token: Token }) => {
   };
 
   const options = {
+    layout: { autoPadding: false },
     scales: {
       x: {
         display: false,
@@ -153,6 +171,19 @@ const TokenAssetV2 = ({ token }: { token: Token }) => {
     CrosshairPlugin,
   );
 
+  const getButton = (label: string, range: TimeRange) => {
+    const opts = { onClick: () => setTimeRange(range) };
+    return range === timeRange ? (
+      <ButtonPrimary {...opts} size={ButtonPrimarySize.Sm}>
+        {label}
+      </ButtonPrimary>
+    ) : (
+      <ButtonSecondary {...opts} size={ButtonSecondarySize.Sm}>
+        {label}
+      </ButtonSecondary>
+    );
+  };
+
   return (
     <>
       <Box padding={4}>
@@ -162,45 +193,70 @@ const TokenAssetV2 = ({ token }: { token: Token }) => {
         <Text variant={TextVariant.headingLg}>
           {formatCurrency(spotPrices?.price, currency)}
         </Text>
+        {/* TODO: These icons for chart up/down */}
+        {priceDelta >= 0 ? (
+          <svg
+            width="12"
+            height="12"
+            viewBox="0 0 12 12"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M9.75 3.8125L6.25 7.4875L4.91667 5.3875L2.25 8.1875"
+              stroke="#28A745"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M8.08398 3.8125H9.75065V5.5625"
+              stroke="#28A745"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        ) : (
+          <svg
+            width="10"
+            height="10"
+            viewBox="0 0 10 10"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M8.75 7.1875L5.25 3.5125L3.91667 5.6125L1.25 2.8125"
+              stroke="#D73847"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M7.08398 7.1875H8.75065V5.4375"
+              stroke="#D73847"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        )}
+        <Text
+          display={Display.InlineBlock}
+          margin={1}
+          color={
+            priceDelta >= 0 ? TextColor.successDefault : TextColor.errorDefault
+          }
+        >
+          ${priceDelta.toFixed(2)} (
+          {(100 * ((lastPrice - firstPrice) / firstPrice)).toFixed(2)}%)
+        </Text>
       </Box>
       <Line data={data} options={options} updateMode="none" />
-      <ButtonSecondary
-        onClick={() => setTimeRange('1D')}
-        size={ButtonSecondarySize.Sm}
-      >
-        1D
-      </ButtonSecondary>
-      <ButtonSecondary
-        onClick={() => setTimeRange('7D')}
-        size={ButtonSecondarySize.Sm}
-      >
-        1W
-      </ButtonSecondary>
-      <ButtonSecondary
-        onClick={() => setTimeRange('1M')}
-        size={ButtonSecondarySize.Sm}
-      >
-        1M
-      </ButtonSecondary>
-      <ButtonSecondary
-        onClick={() => setTimeRange('3M')}
-        size={ButtonSecondarySize.Sm}
-      >
-        3M
-      </ButtonSecondary>
-      <ButtonSecondary
-        onClick={() => setTimeRange('1Y')}
-        size={ButtonSecondarySize.Sm}
-      >
-        1Y
-      </ButtonSecondary>
-      <ButtonSecondary
-        onClick={() => setTimeRange('5Y')}
-        size={ButtonSecondarySize.Sm}
-      >
-        5Y
-      </ButtonSecondary>
-      <ButtonSecondary size={ButtonSecondarySize.Sm}>ALL</ButtonSecondary>
+      <Box display={Display.Flex} justifyContent={JustifyContent.spaceEvenly}>
+        {getButton(t('oneDay'), '1D')}
+        {getButton(t('oneWeek'), '7D')}
+        {getButton(t('oneMonth'), '1M')}
+        {getButton(t('threeMonths'), '3M')}
+        {getButton(t('oneYear'), '1Y')}
+        {getButton(t('all'), '1000Y')}
+      </Box>
     </>
   );
 };
