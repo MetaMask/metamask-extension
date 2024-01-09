80526c8da3cf452d8c86442df045707e830cf9e5
David Walsh <davidwalsh83@gmail.com>
2024-01-09 12:21:38 -0600

    Use toFixed

--- test/e2e/helpers.js
+++ test/e2e/helpers.js
@@ -790,7 +790,7 @@ const TEST_SEED_PHRASE_TWO =
 
 // Usually happens when onboarded to make sure the state is retrieved from metamaskState properly, or after txn is made
 const locateAccountBalanceDOM = async (driver, ganacheServer) => {
-  const balance = (await ganacheServer.getFiatBalance()).toLocaleString();
+  const balance = (await ganacheServer.getFiatBalance()).toFixed(2);
   if (process.env.MULTICHAIN) {
     await driver.clickElement(`[data-testid="home__asset-tab"]`);
     await driver.findElement({
