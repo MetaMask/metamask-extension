5db302abfca155c54acde78d8f79674637e57949
weizman <weizmangal@gmail.com>
2024-01-10 14:41:35 +0200

    support lavadome debug for tests

--- ui/components/multichain/account-details/account-details.test.js
+++ ui/components/multichain/account-details/account-details.test.js
@@ -1,3 +1,4 @@
+import {LavaDomeDebug} from '@lavamoat/lavadome-core';
 import { toChecksumHexAddress } from '@metamask/controller-utils';
 import { fireEvent, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
@@ -108,10 +109,7 @@ describe('AccountDetails', () => {
 
     const hook = queryByText(textHook);
     const root = hook.parentElement.querySelector('span');
-    const parts = Array.from(root.shadowRoot.querySelectorAll('span'));
-    const extractedPrivateKey = parts.map(node =>
-      node.shadowRoot?.firstChild?.textContent).join('');
-    expect(extractedPrivateKey).toContain(samplePrivateKey);
+    expect(LavaDomeDebug.getTextByRoot(root)).toContain(samplePrivateKey);
   });
 
   it('should call AccountDetails.onClose()', () => {
