9fb8bc243e4fe1a6f0ec0219cefbfdde490c65dc
David Walsh <davidwalsh83@gmail.com>
2024-01-11 10:03:34 -0600

    Remove header for send flow

--- ui/pages/routes/routes.component.js
+++ ui/pages/routes/routes.component.js
@@ -493,7 +493,7 @@ export default class Routes extends Component {
         exact: false,
       }),
     );
-    if (process.env.MULTICHAIN && isMultichainSend) {
+    if (isMultichainSend) {
       return true;
     }
 
