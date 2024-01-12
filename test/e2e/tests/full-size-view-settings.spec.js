e19cb6bfce4fc10f3985c48b52f16f93c4545dff
dan437 <80175477+dan437@users.noreply.github.com>
2024-01-12 16:03:27 +0100

    Refactor test for full-size view setting

--- test/e2e/tests/full-size-view-settings.spec.js
+++ test/e2e/tests/full-size-view-settings.spec.js
@@ -1,27 +1,23 @@
 const { strict: assert } = require('assert');
-const { Key, By, Actions } = require('selenium-webdriver');
 const {
   withFixtures,
   unlockWallet,
   openDapp,
   defaultGanacheOptions,
-  getWindowHandles,
   WINDOW_TITLES,
 } = require('../helpers');
 const FixtureBuilder = require('../fixture-builder');
 
-const toggleFullSizeSetting = async (driver) => {
+const toggleFullSizeViewSetting = async (driver) => {
   await driver.clickElement('[data-testid="account-options-menu-button"]');
-
   await driver.clickElement({ text: 'Settings', tag: 'div' });
   await driver.clickElement({ text: 'Advanced', tag: 'div' });
-
   await driver.clickElement(
     '[data-testid="advanced-setting-show-extension-in-full-size-view"] .toggle-button > div',
   );
 };
 
-describe('Full-size Settings @no-mmi', function () {
+describe('Full-size View Setting @no-mmi', function () {
   it('opens the extension in popup view when opened from a dapp after enabling it in Advanced Settings', async function () {
     await withFixtures(
       {
@@ -36,11 +32,9 @@ describe('Full-size Settings @no-mmi', function () {
       },
       async ({ driver }) => {
         await unlockWallet(driver);
-        await toggleFullSizeSetting(driver);
+        await toggleFullSizeViewSetting(driver);
         await openDapp(driver);
-
-        await driver.clickElement('#maliciousPermit');
-        // TODO
+        await driver.clickElement('#maliciousPermit'); // Opens the extension in popup view.
         const windowHandles = await driver.waitUntilXWindowHandles(
           3,
           1000,
@@ -62,52 +56,6 @@ describe('Full-size Settings @no-mmi', function () {
         );
         assert.equal(dappWindowTitle, WINDOW_TITLES.TestDApp);
         assert.equal(popUpWindowTitle, WINDOW_TITLES.Dialog);
-      },
-    );
-  });
-
-  it.only('opens the extension in full-size view after enabling it in Advanced Settings', async function () {
-    await withFixtures(
-      {
-        dapp: true,
-        fixtures: new FixtureBuilder()
-          .withNetworkControllerOnMainnet()
-          .withPermissionControllerConnectedToTestDapp()
-          .build(),
-        failOnConsoleError: false,
-        ganacheOptions: defaultGanacheOptions,
-        title: this.test.fullTitle(),
-      },
-      async ({ driver }) => {
-        await unlockWallet(driver);
-
-        await toggleFullSizeSetting(driver);
-
-        const actions = driver.retrieveDriverActions();
-        console.log(driver.Key, driver.Key.OPTION, driver.Key.M);
-        const el = await driver.findElement(By.tagName('html'));
-        console.log({ el });
-        await actions
-          .sendKeys(
-            Key.chord(driver.Key.SHIFT, driver.Key.OPTION, driver.Key.M),
-          )
-          .perform();
-        // await actions
-        //   .keyDown(driver.Key.SHIFT)
-        //   .keyDown(driver.Key.OPTION)
-        //   .keyDown(driver.Key.M)
-        //   .perform();
-        // .keyDown(driver.Key.OPTION)
-        // .keyDown(driver.Key.M)
-        // .perform();
-        // await driver.wait(40000000);
-        // await driver
-        //   .actions()
-        //   .keyDown(Key.SHIFT)
-        //   .keyDown(Key.OPTION)
-        //   .keyDown(Key.M)
-        //   .perform();
-         await getWindowHandles(driver, 2);
       },
     );
   });
