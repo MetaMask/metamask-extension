cbc0280a216fe88e34270e9baac24c99dada3954
Brad Decker <git@braddecker.dev>
2024-01-12 16:58:27 -0600

    docs

--- app/scripts/lib/offscreen-bridge/lattice-offscreen-keyring.ts
+++ app/scripts/lib/offscreen-bridge/lattice-offscreen-keyring.ts
@@ -4,6 +4,23 @@ import {
   KnownOrigins,
 } from '../../../../shared/constants/offscreen-communication';
 
+/**
+ * This keyring extends the default keyring but uses a overwritten _getCreds
+ * method to alter the mechanism by which the device is connected to. The main
+ * reason for this is because in MV3 the window method to open a new tab will
+ * not execute in the service worker as there is no DOM. The original keyring
+ * _getCreds method calls into window.open to open a new window for the lattice
+ * connector. The solution here is to split the keyring execution so that the
+ * portion that requires a normal functioning DOM is executed in the offscreen
+ * document. The lattice-iframe.ts file in the offscreen directory is
+ * responsible for the latter half of the original keyrings execution. The main
+ * difference in the first half that this overwritten method is responsible for
+ * is just to use the chrome.runtime.sendMessage method to send a message to
+ * the offscreen document which picks up on the execution of opening the new
+ * window to the lattice connector. Note that this file differs from the
+ * ledger and trezor offscreen bridges because this keyring requires no bridge
+ * to function appropriately.
+ */
 class LatticeKeyringOffscreen extends LatticeKeyring {
   static type: string;
 
--- offscreen/scripts/lattice-iframe.ts
+++ offscreen/scripts/lattice-iframe.ts
@@ -3,6 +3,19 @@ import {
   OffscreenCommunicationTarget,
 } from '../../shared/constants/offscreen-communication';
 
+/**
+ * The main element of the eth-lattice-keyring library that is impacted by MV3
+ * is the _openConnectorTab method which is responsible for opening a tab to
+ * the lattice connector. This method is called by the _getCreds method of the
+ * keyring. In the default keyring this would be attempted inside the service
+ * worker which has no DOM and therefore would fail. The solution is to split
+ * the functionality so that the openConnectorTab portion operates inside the
+ * offscreen document with its DOM. That is what this file is responsible for.
+ *
+ * When receiving a message from the service worker script targeting the
+ * lattice iframe, this listener will execute and open the new tab for
+ * connecting to the lattice device. The response from the
+ */
 chrome.runtime.onMessage.addListener((msg, _sender, sendResponse) => {
   if (msg.target !== OffscreenCommunicationTarget.latticeOffscreen) {
     return;
@@ -20,7 +33,11 @@ chrome.runtime.onMessage.addListener((msg, _sender, sendResponse) => {
       }
     }, 500);
 
-    // On a Chromium browser we can just listen for a window message
+    // On a Chromium browser we can just listen for a window message from the
+    // lattice tab (using the event.origin property). When we get that it'll be
+    // the response from the lattice connector with the deviceID and password.
+    // We can then forward that response to the lattice-offscreen-keyring's
+    // _getCreds method using sendResponse API from the chrome runtime.
     window.addEventListener(
       'message',
       (event) => {
