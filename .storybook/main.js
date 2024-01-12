280886acf2d08c754e9b1b25f44e175a20961466
David Murdoch <187813+davidmurdoch@users.noreply.github.com>
2024-01-12 17:37:24 -0500

    update sass

--- .storybook/main.js
+++ .storybook/main.js
@@ -69,7 +69,7 @@ module.exports = {
           loader: 'sass-loader',
           options: {
             sourceMap: true,
-            implementation: require('sass'),
+            implementation: require('sass-embedded'),
             sassOptions: {
               includePaths: ['ui/css/'],
             },
