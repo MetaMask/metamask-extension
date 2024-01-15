43064063e2f0ee3e7b3ae4fd3e907e659a8e3490
sahar-fehri <sahar.fehri@consensys.net>
2024-01-15 18:01:01 +0100

    fix: add missing warning when sending to contract

--- ui/components/multichain/pages/send/components/content.tsx
+++ ui/components/multichain/pages/send/components/content.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Box } from '../../../../component-library';
+import {
+  BannerAlert,
+  BannerAlertSeverity,
+  Box,
+  Text,
+} from '../../../../component-library';
 import { getSendHexDataFeatureFlagState } from '../../../../../ducks/metamask/metamask';
 import {
   GAS_INPUT_MODES,
+  acknowledgeRecipientWarning,
   getGasInputMode,
   getGasLimit,
   getGasPrice,
   getIsBalanceInsufficient,
   getMinimumGasLimitForSend,
+  getRecipient,
+  getRecipientWarningAcknowledgement,
   getSendAsset,
 } from '../../../../../ducks/send';
 import AdvancedGasInputs from '../../../../app/advanced-gas-inputs';
@@ -20,9 +28,14 @@ import {
   setCustomGasPrice,
 } from '../../../../../ducks/gas/gas.duck';
 import { AssetPickerAmount } from '../../..';
+import { I18nContext } from '../../../../../contexts/i18n';
+import { CONTRACT_ADDRESS_LINK } from '../../../../../helpers/constants/common';
+import { TextVariant } from '../../../../../helpers/constants/design-system';
+import { KNOWN_RECIPIENT_ADDRESS_WARNING } from '../../../../../pages/send/send.constants';
 import { SendHexData, SendPageRow } from '.';
 
 export const SendPageContent = () => {
+  const t = useContext(I18nContext);
   // Hex data
   const showHexDataFlag = useSelector(getSendHexDataFeatureFlagState);
   const asset = useSelector(getSendAsset);
@@ -42,6 +55,11 @@ export const SendPageContent = () => {
   const gasInputMode = useSelector(getGasInputMode);
   const insufficientBalance = useSelector(getIsBalanceInsufficient);
 
+  const recipient = useSelector(getRecipient);
+  const isWarningAck = useSelector(getRecipientWarningAcknowledgement);
+  const showWarningBanner =
+    recipient.warning === KNOWN_RECIPIENT_ADDRESS_WARNING && !isWarningAck;
+
   const updateGasPrice = (newGasPrice: string) => {
     dispatch(updateGasPrice(newGasPrice));
     dispatch(setCustomGasPrice(newGasPrice));
@@ -52,8 +70,40 @@ export const SendPageContent = () => {
     dispatch(setCustomGasLimit(newLimit));
   };
 
+  const ackRecipientWarning = () => {
+    dispatch(acknowledgeRecipientWarning());
+  };
+
   return (
     <Box>
+      {showWarningBanner ? (
+        <SendPageRow>
+          <BannerAlert
+            severity={BannerAlertSeverity.Danger}
+            actionButtonLabel={t('tooltipApproveButton')}
+            actionButtonOnClick={ackRecipientWarning}
+            actionButtonProps={{
+              marginTop: 2,
+              variant: TextVariant.bodyXs,
+            }}
+          >
+            <Text variant={TextVariant.bodyXs}>
+              {t('sendingToTokenContractWarning', [
+                <a
+                  key="contractWarningSupport"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="send__warning-container__link"
+                  href={CONTRACT_ADDRESS_LINK}
+                >
+                  {t('learnMoreUpperCase')}
+                </a>,
+              ])}
+            </Text>
+          </BannerAlert>
+        </SendPageRow>
+      ) : null}
+
       <SendPageRow>
         <AssetPickerAmount />
       </SendPageRow>
