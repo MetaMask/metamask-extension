16fdee044700f890d05d53cf50dfcdbd7318ff92
digiwand <20778143+digiwand@users.noreply.github.com>
2024-01-16 17:39:42 -0800

    fix:lint:warning SignatureRequested metric

--- ui/components/app/signature-request-siwe/signature-request-siwe.js
+++ ui/components/app/signature-request-siwe/signature-request-siwe.js
@@ -88,6 +88,7 @@ export default function SignatureRequestSIWE({ txData }) {
         },
       });
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const onClickSupportLink = useCallback(() => {
