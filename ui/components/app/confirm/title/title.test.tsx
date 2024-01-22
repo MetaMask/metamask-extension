a7b7d2ba8a222fb2271a5dc53dd3388c7d81fb96
digiwand <20778143+digiwand@users.noreply.github.com>
2024-01-22 07:42:19 -0800

    fix: import default ConfirmTitle

--- ui/components/app/confirm/title/title.test.tsx
+++ ui/components/app/confirm/title/title.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import configureMockStore from 'redux-mock-store';
 import { TransactionType } from '@metamask/transaction-controller';
 import { renderWithProvider } from '../../../../../test/lib/render-helpers';
-import { ConfirmTitle } from './title';
+import ConfirmTitle from './title';
 
 const mockState = {
   confirm: {
