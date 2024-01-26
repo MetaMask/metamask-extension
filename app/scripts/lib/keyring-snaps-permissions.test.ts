30c30b4f2686e180450e424090952ec23f522458
Derek Brans <dbrans@gmail.com>
2024-01-26 13:36:37 -0500

    reverting app/scripts/lib/keyring-snaps-permissions.test.ts

--- app/scripts/lib/keyring-snaps-permissions.test.ts
+++ app/scripts/lib/keyring-snaps-permissions.test.ts
@@ -12,7 +12,6 @@ describe('keyringSnapPermissionsBuilder', () => {
   const mockController = new SubjectMetadataController({
     subjectCacheLimit: 100,
     messenger: {
-      registerInitialEventPayload: jest.fn(),
       registerActionHandler: jest.fn(),
       registerInitialEventPayload: jest.fn(),
       publish: jest.fn(),
