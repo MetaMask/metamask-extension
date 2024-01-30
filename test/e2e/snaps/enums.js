ba3d28cbf7d402212a0d04ee5101fa08a4d66a99
bowensanders <bowensanders@gmail.com>
2024-01-30 15:38:52 -0800

    created test, set enums

--- test/e2e/snaps/enums.js
+++ test/e2e/snaps/enums.js
@@ -1,3 +1,4 @@
 module.exports = {
-  TEST_SNAPS_WEBSITE_URL: 'https://metamask.github.io/snaps/test-snaps/2.2.3/',
+  // TEST_SNAPS_WEBSITE_URL: 'https://metamask.github.io/snaps/test-snaps/2.2.3/',
+  TEST_SNAPS_WEBSITE_URL: 'http://localhost:9000',
 };
