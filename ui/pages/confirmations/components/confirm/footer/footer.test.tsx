2feee5be3433ffffbcd036ef3daf1f9e47fdb4ea
Jyoti Puri <jyotipuri@gmail.com>
2024-01-31 16:43:09 +0530

    Adding mmi section to confirmation page

--- ui/pages/confirmations/components/confirm/footer/footer.test.tsx
+++ ui/pages/confirmations/components/confirm/footer/footer.test.tsx
@@ -4,7 +4,10 @@ import mockState from '../../../../../../test/data/mock-state.json';
 import { fireEvent, renderWithProvider } from '../../../../../../test/jest';
 import * as Actions from '../../../../../store/actions';
 import configureStore from '../../../../../store/store';
-
+import {
+  LedgerTransportTypes,
+  WebHIDConnectedStatuses,
+} from '../../../../../../shared/constants/hardware-wallets';
 import Footer from './footer';
 
 jest.mock('react-redux', () => ({
@@ -12,12 +15,7 @@ jest.mock('react-redux', () => ({
   useDispatch: () => jest.fn(),
 }));
 
-jest.mock('react-redux', () => ({
-  ...jest.requireActual('react-redux'),
-  useDispatch: () => jest.fn(),
-}));
-
-const render = () => {
+const render = (args = {}) => {
   const store = configureStore({
     metamask: {
       ...mockState.metamask,
@@ -29,6 +27,7 @@ const render = () => {
         },
       },
     },
+    ...args,
   });
 
   return renderWithProvider(<Footer />, store);
@@ -60,11 +59,33 @@ describe('ConfirmFooter', () => {
 
   it('invoke action resolvePendingApproval when submit button is clicked', () => {
     const { getAllByRole } = render();
-    const cancelButton = getAllByRole('button')[1];
+    const submitButton = getAllByRole('button')[1];
     const resolveSpy = jest
       .spyOn(Actions, 'resolvePendingApproval')
       .mockImplementation(() => ({} as any));
-    fireEvent.click(cancelButton);
+    fireEvent.click(submitButton);
     expect(resolveSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('disables submit button if required LedgerHidConnection is not yet established', () => {
+    const { getAllByRole } = render({
+      metamask: {
+        ...mockState.metamask,
+        ledgerTransportType: LedgerTransportTypes.webhid,
+      },
+      confirm: {
+        currentConfirmation: {
+          msgParams: {
+            from: '0xc42edfcc21ed14dda456aa0756c153f7985d8813',
+          },
+        },
+      },
+      appState: {
+        ...mockState.appState,
+        ledgerWebHidConnectedStatus: WebHIDConnectedStatuses.notConnected,
+      },
+    });
+    const submitButton = getAllByRole('button')[1];
+    expect(submitButton).toBeDisabled();
   });
 });
