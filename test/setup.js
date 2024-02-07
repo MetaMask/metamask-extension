5358f7e391db233ce1aebc40b2ed2be86e1b417e
Derek Brans <dbrans@gmail.com>
2024-02-07 14:28:04 -0500

    Add _moduleAliases to package.json

--- test/setup.js
+++ test/setup.js
@@ -1,6 +1,6 @@
 require('@babel/register');
+require('module-alias/register');
 require('ts-node').register({ transpileOnly: true });
-
 require('./helpers/setup-helper');
 
 window.SVGPathElement = window.SVGPathElement || { prototype: {} };
