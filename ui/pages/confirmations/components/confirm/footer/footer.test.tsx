90928819763fe47bbed9567ecdc73ef779b2645d
digiwand <20778143+digiwand@users.noreply.github.com>
2024-02-07 17:09:53 -0800

    fix:test: ConfirmFooter currentConfirmation state

--- ui/pages/confirmations/components/confirm/footer/footer.test.tsx
+++ ui/pages/confirmations/components/confirm/footer/footer.test.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import configureMockStore from 'redux-mock-store';
+import { TransactionType } from '@metamask/transaction-controller';
 import { renderWithProvider } from '../../../../../../test/jest';
 import { Footer } from '.';
 
 const render = () => {
-  return renderWithProvider(<Footer />);
+  return renderWithProvider(
+    <Footer />,
+    configureMockStore([])({
+      confirm: {
+        currentConfirmation: {
+          type: TransactionType.personalSign,
+          isScrollToBottomNeeded: false,
+        },
+      },
+    }),
+  );
 };
 
 describe('ConfirmFooter', () => {
