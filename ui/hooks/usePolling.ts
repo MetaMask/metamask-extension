d31ac2d6469934d09c7143fffac0213ff7383534
Shane Jonas <jonas.shane@gmail.com>
2024-02-09 16:03:51 -0500

    Fixed linting

--- ui/hooks/usePolling.ts
+++ ui/hooks/usePolling.ts
@@ -15,18 +15,20 @@ const usePolling = (usePollingOptions: UsePollingOptions) => {
   const pollTokenRef = useRef<null | string>(null);
   useEffect(() => {
     // Start polling when the component mounts
-    usePollingOptions.startPollingByNetworkClientId(
-      usePollingOptions.networkClientId,
-      usePollingOptions.options,
-    ).then((pollToken) => {
-      pollTokenRef.current = pollToken
-      // const cleanup = usePollingOptions.callback?.(pollTokenRef.current);
-    });
+    usePollingOptions
+      .startPollingByNetworkClientId(
+        usePollingOptions.networkClientId,
+        usePollingOptions.options,
+      )
+      .then((pollToken) => {
+        pollTokenRef.current = pollToken;
+        // const cleanup = usePollingOptions.callback?.(pollTokenRef.current);
+      });
 
     // eslint-disable-next-line node/callback-return
     // Return a cleanup function to stop polling when the component unmounts
     return () => {
-      console.log('jiexi usePolling exit', pollTokenRef, pollTokenRef.current)
+      console.log('jiexi usePolling exit', pollTokenRef, pollTokenRef.current);
       if (pollTokenRef.current) {
         usePollingOptions.stopPollingByPollingToken(pollTokenRef.current);
         // cleanup?.(pollTokenRef.current);
