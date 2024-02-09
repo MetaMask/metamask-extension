a2cdd02a82ef1cc2e889b8844325328d6b330a45
Jyoti Puri <jyotipuri@gmail.com>
2024-02-09 18:11:15 +0530

    Update

--- ui/pages/confirmations/components/confirm/header/header.tsx
+++ ui/pages/confirmations/components/confirm/header/header.tsx
@@ -1,45 +1,46 @@
 import React from 'react';
 
+import useConfirmationNetworkInfo from '../../../hooks/useConfirmationNetworkInfo';
+import useConfirmationRecipientInfo from '../../../hooks/useConfirmationRecipientInfo';
+import {
+  AlignItems,
+  Display,
+  TextColor,
+} from '../../../../../helpers/constants/design-system';
+
+import Identicon from '../../../../../components/ui/identicon';
 import {
   AvatarNetwork,
   AvatarNetworkSize,
   Box,
   Text,
 } from '../../../../../components/component-library';
-import { Header as PageHeader } from '../../../../../components/multichain/pages/page';
-import Identicon from '../../../../../components/ui/identicon';
-import {
-  Display,
-  TextAlign,
-  TextColor,
-} from '../../../../../helpers/constants/design-system';
-import useConfirmationNetworkInfo from '../../../hooks/useConfirmationNetworkInfo';
-import useConfirmationRecipientInfo from '../../../hooks/useConfirmationRecipientInfo';
 
 const Header = () => {
   const { networkImageUrl, networkDisplayName } = useConfirmationNetworkInfo();
   const { recipientAddress, recipientName } = useConfirmationRecipientInfo();
 
   return (
-    <PageHeader
-      className="confirm_header"
-      startAccessory={
-        <Box display={Display.Flex}>
-          <Identicon address={recipientAddress} diameter={32} />
-          <AvatarNetwork
-            className="confirm_header__avatar-network"
-            name={networkDisplayName}
-            size={AvatarNetworkSize.Xs}
-            src={networkImageUrl}
-          />
-        </Box>
-      }
+    <Box
+      alignItems={AlignItems.center}
+      display={Display.Flex}
+      padding={4}
+      className="confirm_header__wrapper"
     >
-      <Text textAlign={TextAlign.Left}>{recipientName}</Text>
-      <Text textAlign={TextAlign.Left} color={TextColor.textAlternative}>
-        {networkDisplayName}
-      </Text>
-    </PageHeader>
+      <Box display={Display.Flex}>
+        <Identicon address={recipientAddress} diameter={32} />
+        <AvatarNetwork
+          src={networkImageUrl}
+          name={networkDisplayName}
+          size={AvatarNetworkSize.Xs}
+          className="confirm_header__avatar-network"
+        />
+      </Box>
+      <Box marginInlineStart={4}>
+        <Text>{recipientName}</Text>
+        <Text color={TextColor.textAlternative}>{networkDisplayName}</Text>
+      </Box>
+    </Box>
   );
 };
 
