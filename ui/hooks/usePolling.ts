a08473c81c2d9c7c1633b2dc3ff81f3f28cb0117
Jiexi Luan <jiexiluan@gmail.com>
2024-02-09 12:44:42 -0800

    holy mother of js

--- ui/hooks/usePolling.ts
+++ ui/hooks/usePolling.ts
@@ -1,11 +1,11 @@
 import { useEffect, useRef } from 'react';
 
 type UsePollingOptions = {
-  callback?: (pollingToken: string) => (pollingToken: string) => void;
+  // callback?: (pollingToken: string) => (pollingToken: string) => void;
   startPollingByNetworkClientId: (
     networkClientId: string,
     options: any,
-  ) => string;
+  ) => Promise<string>;
   stopPollingByPollingToken: (pollingToken: string) => void;
   networkClientId: string;
   options?: any;
@@ -15,17 +15,21 @@ const usePolling = (usePollingOptions: UsePollingOptions) => {
   const pollTokenRef = useRef<null | string>(null);
   useEffect(() => {
     // Start polling when the component mounts
-    pollTokenRef.current = usePollingOptions.startPollingByNetworkClientId(
+    usePollingOptions.startPollingByNetworkClientId(
       usePollingOptions.networkClientId,
       usePollingOptions.options,
-    );
+    ).then((pollToken) => {
+      pollTokenRef.current = pollToken
+      // const cleanup = usePollingOptions.callback?.(pollTokenRef.current);
+    });
+
     // eslint-disable-next-line node/callback-return
-    const cleanup = usePollingOptions.callback?.(pollTokenRef.current);
     // Return a cleanup function to stop polling when the component unmounts
     return () => {
+      console.log('jiexi usePolling exit', pollTokenRef, pollTokenRef.current)
       if (pollTokenRef.current) {
         usePollingOptions.stopPollingByPollingToken(pollTokenRef.current);
-        cleanup?.(pollTokenRef.current);
+        // cleanup?.(pollTokenRef.current);
       }
     };
   }, []);
