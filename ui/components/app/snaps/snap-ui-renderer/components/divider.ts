672c8f39a85fdb7b6d0e8ed1b4104359529ab2fc
Guillaume Roux <guillaumeroux123@gmail.com>
2024-02-13 15:15:20 +0100

    fix wrong type

--- ui/components/app/snaps/snap-ui-renderer/components/divider.ts
+++ ui/components/app/snaps/snap-ui-renderer/components/divider.ts
@@ -1,8 +1,8 @@
 import { Divider } from '@metamask/snaps-sdk';
 import { BorderColor } from '../../../../../helpers/constants/design-system';
-import { UIComponent } from './types';
+import { UIComponentFactory } from './types';
 
-export const divider: UIComponent<Divider> = () => ({
+export const divider: UIComponentFactory<Divider> = () => ({
   element: 'Box',
   props: {
     className: 'snap-ui-renderer__divider',
