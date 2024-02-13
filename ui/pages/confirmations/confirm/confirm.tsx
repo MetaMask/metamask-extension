9390999eac374d8d570bd58b68fc454152bf25b4
Jyoti Puri <jyotipuri@gmail.com>
2024-02-13 16:43:33 +0530

    Update

--- ui/pages/confirmations/confirm/confirm.tsx
+++ ui/pages/confirmations/confirm/confirm.tsx
@@ -1,13 +1,7 @@
 import React from 'react';
 
-import { Box } from '../../../components/component-library';
-import {
-  BackgroundColor,
-  BlockSize,
-  Display,
-  FlexDirection,
-  JustifyContent,
-} from '../../../helpers/constants/design-system';
+import { Content, Page } from '../../../components/multichain/pages/page';
+import { BackgroundColor } from '../../../helpers/constants/design-system';
 import { Footer } from '../components/confirm/footer';
 import { Header } from '../components/confirm/header';
 import { Info } from '../components/confirm/info';
@@ -21,24 +15,15 @@ const Confirm = () => {
   syncConfirmPath();
 
   return (
-    <Box
-      backgroundColor={BackgroundColor.backgroundAlternative}
-      display={Display.Flex}
-      flexDirection={FlexDirection.Column}
-      justifyContent={JustifyContent.spaceBetween}
-      height={BlockSize.Full}
-      width={BlockSize.Full}
-    >
-      <Box>
-        <Header />
-        <Box paddingInline={4}>
-          <Title />
-          <Info />
-          <SignatureMessage />
-        </Box>
-      </Box>
+    <Page>
+      <Header />
+      <Content backgroundColor={BackgroundColor.backgroundAlternative}>
+        <Title />
+        <Info />
+        <SignatureMessage />
+      </Content>
       <Footer />
-    </Box>
+    </Page>
   );
 };
 
