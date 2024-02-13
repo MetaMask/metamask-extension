154ce6f415f01b8ba79611ef08b79623abe4db41
David Murdoch <187813+davidmurdoch@users.noreply.github.com>
2024-02-13 17:14:06 -0500

    Update .eslintrc.base.js

--- .eslintrc.base.js
+++ .eslintrc.base.js
@@ -12,7 +12,7 @@ module.exports = {
   },
 
   rules: {
-    // re-enable once the proposed feature at https://github.com/gajus/eslint-plugin-jsdoc/pull/964#issuecomment-1936470252 is available
+    // TODO: re-enable once the proposed feature at https://github.com/gajus/eslint-plugin-jsdoc/pull/964#issuecomment-1936470252 is available
     'jsdoc/check-line-alignment': 'off',
 
     'default-param-last': 'off',
