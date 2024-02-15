79cca74d056915bc42627135e5c272ec61e4da50
seaona <54408225+seaona@users.noreply.github.com>
2024-02-15 23:40:54 +0100

    fix: Update ppom_release to fix fail ppom security validation checks (#22978)
    ## **Description**
    
    The issue described here
    https://github.com/MetaMask/metamask-mobile/issues/8589 is also
    happening in the extension (in prod) in v11.9.x
    
    The root of the problem lies in a geth update and how a
    `eth_createAccessList` request is handled when passed certain variables.
    
    The incompatibility of ppom and the latest changes in geth is handled in
    v1.4.1 of `ppom_release`.
    
    ## **Related issues**
    
    Fixes:
    
    ## **Manual testing steps**
    
    1. Go to https://metamask.github.io/test-dapp/ and connect your wallet
    2. Click "Malicious ERC-20 Transfer"
    3. A confirmation popup should appear with a warning like the one shown
    below
    
    ![Screenshot from 2024-02-15
    18-14-17](https://github.com/MetaMask/metamask-extension/assets/7499938/cca14ca0-eaa2-4e93-9112-d170bef2658e)
    
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    
    
    https://github.com/MetaMask/metamask-extension/assets/54408225/883b58c8-feb3-4e20-9013-2b54d14de68c
    
    
    
    ### **After**
    
    
    
    https://github.com/MetaMask/metamask-extension/assets/54408225/cfc5e10d-ff25-42bb-ace2-052452611e14
    
    
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've clearly explained what problem this PR is solving and how it
    is solved.
    - [ ] I've linked related issues
    - [ ] I've included manual testing steps
    - [ ] I've included screenshots/recordings if applicable
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    - [ ] I’ve properly set the pull request status:
      - [ ] In case it's not yet "ready for review", I've set it to "draft".
    - [ ] In case it's "ready for review", I've changed it from "draft" to
    "non-draft".
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: Dan J Miller <danjm.com@gmail.com>

--- ui/pages/confirmations/components/security-provider-banner-alert/blockaid-banner-alert/blockaid-banner-alert.test.js
+++ ui/pages/confirmations/components/security-provider-banner-alert/blockaid-banner-alert/blockaid-banner-alert.test.js
@@ -243,7 +243,7 @@ describe('Blockaid Banner Alert', () => {
 
     const elm = getByRole('link', { name: 'Report an issue' });
     expect(elm.href).toBe(
-      'https://blockaid-false-positive-portal.metamask.io/?data=%7B%22blockaidVersion%22%3A%221.4.0%22%2C%22classification%22%3A%22set_approval_for_all%22%2C%22resultType%22%3A%22Warning%22%7D&utm_source=metamask-ppom',
+      'https://blockaid-false-positive-portal.metamask.io/?data=%7B%22blockaidVersion%22%3A%221.4.1%22%2C%22classification%22%3A%22set_approval_for_all%22%2C%22resultType%22%3A%22Warning%22%7D&utm_source=metamask-ppom',
     );
   });
 
@@ -264,7 +264,7 @@ describe('Blockaid Banner Alert', () => {
 
         const elm = getByRole('link', { name: 'Report an issue' });
         expect(elm.href).toBe(
-          'https://blockaid-false-positive-portal.metamask.io/?data=%7B%22blockaidVersion%22%3A%221.4.0%22%2C%22classification%22%3A%22error%22%2C%22resultType%22%3A%22Error%22%7D&utm_source=metamask-ppom',
+          'https://blockaid-false-positive-portal.metamask.io/?data=%7B%22blockaidVersion%22%3A%221.4.1%22%2C%22classification%22%3A%22error%22%2C%22resultType%22%3A%22Error%22%7D&utm_source=metamask-ppom',
         );
       });
     });
