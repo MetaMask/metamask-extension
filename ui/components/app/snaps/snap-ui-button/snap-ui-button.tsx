53b4f440afe45c2fb22f68b074776558211fd176
Frederik Bolding <frederik.bolding@gmail.com>
2024-02-16 15:58:58 +0100

    Fix lint

--- ui/components/app/snaps/snap-ui-button/snap-ui-button.tsx
+++ ui/components/app/snaps/snap-ui-button/snap-ui-button.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, MouseEvent } from 'react';
+import React, { FunctionComponent, MouseEvent as ReactMouseEvent } from 'react';
 import { ButtonType, UserInputEventType } from '@metamask/snaps-sdk';
 import { Button, ButtonProps } from '../../../component-library';
 import { useSnapInterfaceContext } from '../../../../contexts/snaps';
@@ -12,7 +12,7 @@ export const SnapUIButton: FunctionComponent<
 > = ({ name, children, type, ...props }) => {
   const { handleEvent } = useSnapInterfaceContext();
 
-  const handleClick = (event: MouseEvent<HTMLElement>) => {
+  const handleClick = (event: ReactMouseEvent<HTMLElement>) => {
     if (type === ButtonType.Button) {
       event.preventDefault();
     }
