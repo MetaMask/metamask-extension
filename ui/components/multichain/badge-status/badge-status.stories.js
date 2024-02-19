10d3b89d11fbc51ae904ac56f1399f4a81c2832d
NidhiKJha <menidhikjha@gmail.com>
2024-02-19 21:10:33 +0530

    updated badge status stories

--- ui/components/multichain/badge-status/badge-status.stories.js
+++ ui/components/multichain/badge-status/badge-status.stories.js
@@ -1,23 +1,36 @@
 import React from 'react';
+import {
+  BackgroundColor,
+  BorderColor,
+  Color,
+} from '../../../helpers/constants/design-system';
 import { BadgeStatus } from './badge-status';
 
 export default {
   title: 'Components/Multichain/BadgeStatus',
   component: BadgeStatus,
   argTypes: {
+    badgeBackgroundColor: {
+      control: 'text',
+    },
+    badgeBorderColor: {
+      control: 'text',
+    },
     text: {
       control: 'text',
     },
     address: {
       control: 'text',
     },
-    isActive: {
+    isConnectedAndNotActive: {
       control: 'boolean',
     },
   },
   args: {
+    badgeBackgroundColor: BackgroundColor.successDefault,
+    badgeBorderColor: BackgroundColor.backgroundDefault,
     address: '0x1',
-    isActive: false,
+    text: 'Tooltip',
   },
 };
 
@@ -26,3 +39,16 @@ const Template = (args) => {
 };
 
 export const DefaultStory = Template.bind({});
+
+export const NotConnectedStory = Template.bind({});
+NotConnectedStory.args = {
+  badgeBackgroundColor: Color.borderMuted,
+  badgeBorderColor: BackgroundColor.backgroundDefault,
+};
+
+export const ConnectedNotActiveStory = Template.bind({});
+ConnectedNotActiveStory.args = {
+  badgeBackgroundColor: BackgroundColor.backgroundDefault,
+  badgeBorderColor: BorderColor.successDefault,
+  isConnectedAndNotActive: true,
+};
