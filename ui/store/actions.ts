50625a893a8c454c5b4333936299b13350ed39cf
Victor Thomas <10986371+vthomas13@users.noreply.github.com>
2024-02-19 11:49:41 -0500

    Adding Permissions Tour & Empty State  (#22905)
    ## **Description**
    This PR is for the conversion of the AllConnections component to the
    PermissionsPage component
    
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    2. What is the improvement/solution?
    -->
    
    ## **Related issues**
    
    Fixes:https://github.com/MetaMask/MetaMask-planning/issues/1989
    
    ## **Manual testing steps**
    
    1. yarn && MULTICHAIN=1 yarn start
    2. open fullscreen, add "#permissions" route to the end of the URL
    3. disconnect all dapps and verify empty state after no dapps are
    connected.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [X] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/CODING_GUIDELINES.md).
    - [X] I've clearly explained what problem this PR is solving and how it
    is solved.
    - [X] I've linked related issues
    - [X] I've included manual testing steps
    - [X] I've included screenshots/recordings if applicable
    - [X] I’ve included tests if applicable
    - [X] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [X] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    - [X] I’ve properly set the pull request status:
      - [X] In case it's not yet "ready for review", I've set it to "draft".
    - [X] In case it's "ready for review", I've changed it from "draft" to
    "non-draft".
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- ui/store/actions.ts
+++ ui/store/actions.ts
@@ -4570,6 +4570,10 @@ export function hideBetaHeader() {
   return submitRequestToBackground('setShowBetaHeader', [false]);
 }
 
+export function hidePermissionsTour() {
+  return submitRequestToBackground('setShowPermissionsTour', [false]);
+}
+
 export function hideProductTour() {
   return submitRequestToBackground('setShowProductTour', [false]);
 }
