59ea9cff423b9f516b100f08030c2dd5dee434bf
NidhiKJha <menidhikjha@gmail.com>
2024-02-21 19:30:27 +0530

    updated lint errors and comments

--- ui/components/multichain/connected-status/connected-status.js
+++ ui/components/multichain/connected-status/connected-status.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
-import { WALLET_SNAP_PERMISSION_KEY } from '@metamask/snaps-rpc-methods';
 import {
   BackgroundColor,
   BorderColor,
@@ -10,12 +9,10 @@ import {
 import {
   getAddressConnectedSubjectMap,
   getOriginOfCurrentTab,
-  getPermissionsForActiveTab,
 } from '../../../selectors';
 import {
   STATUS_CONNECTED,
   STATUS_CONNECTED_TO_ANOTHER_ACCOUNT,
-  STATUS_CONNECTED_TO_SNAP,
   STATUS_NOT_CONNECTED,
 } from '../../../helpers/constants/connected-sites';
 import { useI18nContext } from '../../../hooks/useI18nContext';
@@ -41,8 +38,8 @@ export const ConnectedStatus = ({ address = '', isActive = false }) => {
     status = STATUS_NOT_CONNECTED;
   }
 
-  let badgeBorderColor = BackgroundColor.backgroundDefault;
-  let badgeBackgroundColor = Color.borderMuted;
+  let badgeBorderColor = BackgroundColor.backgroundDefault; //TODO: Replace it once border-color has this value.
+  let badgeBackgroundColor = Color.borderMuted; ////TODO: Replace it once Background color has this value.
   let tooltipText = t('statusNotConnected');
   if (status === STATUS_CONNECTED) {
     badgeBorderColor = BackgroundColor.backgroundDefault;
