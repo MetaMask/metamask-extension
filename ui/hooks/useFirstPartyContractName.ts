5b3ab13254170b25597dfd66675da7bdd9bfc378
Derek Brans <dbrans@gmail.com>
2024-02-22 07:41:36 -0500

    Addressing PR comments

--- ui/hooks/useFirstPartyContractName.ts
+++ ui/hooks/useFirstPartyContractName.ts
@@ -1,7 +1,7 @@
 import { NameType } from '@metamask/name-controller';
 import { useSelector } from 'react-redux';
 import { getCurrentChainId } from '../selectors';
-import { FIRST_PARTY_CONTRACT_NAMES } from './useFirstPartyContractNameData';
+import { FIRST_PARTY_CONTRACT_NAMES } from '../../shared/constants/first-party-contracts';
 
 export function useFirstPartyContractName(
   value: string,
@@ -15,11 +15,11 @@ export function useFirstPartyContractName(
   const chainId = variation ?? currentChainId;
   const normalizedValue = value.toLowerCase();
 
-  for (const name of Object.keys(FIRST_PARTY_CONTRACT_NAMES)) {
-    const contractAddress = FIRST_PARTY_CONTRACT_NAMES[name]?.[chainId];
-    if (normalizedValue === contractAddress?.toLowerCase()) {
-      return name;
-    }
-  }
-  return null;
+  return (
+    Object.keys(FIRST_PARTY_CONTRACT_NAMES).find(
+      (name) =>
+        FIRST_PARTY_CONTRACT_NAMES[name]?.[chainId]?.toLowerCase() ===
+        normalizedValue,
+    ) ?? null
+  );
 }
