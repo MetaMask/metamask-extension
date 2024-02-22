f2dd396360ee98c81b882b5521d389f13e6f887d
Brian Bergeron <brian.e.bergeron@gmail.com>
2024-02-22 15:45:47 -0800

    use jest expect instead of throwing exception

--- shared/constants/network.test.ts
+++ shared/constants/network.test.ts
@@ -1,13 +1,11 @@
+import { existsSync } from 'fs';
+import { join } from 'path';
 import { CHAIN_ID_TO_NETWORK_IMAGE_URL_MAP } from './network';
-import { existsSync } from 'fs'
-import { join } from 'path'
 
 describe('NetworkConstants', () => {
   it('has images files that exist for defined networks', () => {
-    for (const [chainId, image] of Object.entries(CHAIN_ID_TO_NETWORK_IMAGE_URL_MAP)) {
-      if (!existsSync(join('app', image))) {
-        throw `Did not find image '${image}' for chain '${chainId}'`;
-      }
-    }
+    Object.values(CHAIN_ID_TO_NETWORK_IMAGE_URL_MAP).forEach((image) =>
+      expect(existsSync(join('app', image))).toBe(true),
+    );
   });
 });
