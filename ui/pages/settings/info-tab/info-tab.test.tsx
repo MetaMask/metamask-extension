0a73d83687781ed0839427d718ecf1c7aa10764c
HJetpoluru <harika.jetpoluru@consensys.net>
2024-02-23 12:30:00 -0500

    Fixed my lint and some other files

--- ui/pages/settings/info-tab/info-tab.test.tsx
+++ ui/pages/settings/info-tab/info-tab.test.tsx
@@ -13,32 +13,47 @@ describe('InfoTab', () => {
 
     it('should have correct href for "Privacy Policy" link', () => {
       const privacyPolicyLink = getByText('Privacy policy');
-      expect(privacyPolicyLink).toHaveAttribute('href', 'https://metamask.io/privacy.html');
+      expect(privacyPolicyLink).toHaveAttribute(
+        'href',
+        'https://metamask.io/privacy.html',
+      );
     });
 
     it('should have correct href for "Terms of Use" link', () => {
       const termsOfUseLink = getByText('Terms of use');
-      expect(termsOfUseLink).toHaveAttribute('href', 'https://metamask.io/terms.html');
+      expect(termsOfUseLink).toHaveAttribute(
+        'href',
+        'https://metamask.io/terms.html',
+      );
     });
 
     it('should have correct href for "Attributions" link', () => {
-    const attributionsLink = getByText('Attributions');
-    expect(attributionsLink).toHaveAttribute('href', 'https://metamask.io/attributions.html');
+      const attributionsLink = getByText('Attributions');
+      expect(attributionsLink).toHaveAttribute(
+        'href',
+        'https://metamask.io/attributions.html',
+      );
     });
 
     it('should have correct href for "Support" link', () => {
       const supportLink = getByText('Visit our support center');
-      expect(supportLink).toHaveAttribute('href', 'https://support.metamask.io');
+      expect(supportLink).toHaveAttribute(
+        'href',
+        'https://support.metamask.io',
+      );
     });
 
     it('should have correct href for "Visit our website" link', () => {
-        const websiteLink = getByText('Visit our website');
-        expect(websiteLink).toHaveAttribute('href', 'https://metamask.io/');
+      const websiteLink = getByText('Visit our website');
+      expect(websiteLink).toHaveAttribute('href', 'https://metamask.io/');
     });
 
     it('should have correct href for "Contact us" link', () => {
-        const contactUsLink = getByText('Contact us');
-        expect(contactUsLink).toHaveAttribute('href', 'https://metamask.zendesk.com/hc/en-us');
+      const contactUsLink = getByText('Contact us');
+      expect(contactUsLink).toHaveAttribute(
+        'href',
+        'https://metamask.zendesk.com/hc/en-us',
+      );
     });
   });
 });
