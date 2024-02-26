78605142feb654717132c70f253d416ea66c373c
David Walsh <davidwalsh83@gmail.com>
2024-02-26 10:34:19 -0600

    Fix #22938 - UX - Multichain - Send Flow - Update ENS address upon network change (#23024)
    ## **Description**
    
    Changing the network mid-send can effect ENS domains in a negative way.
    Namely, an ENS domain can have a different addresses per chain. This PR
    ensure that ENS lookups are done when the user changes chains.
    
    ## **Related issues**
    
    Fixes:  https://github.com/MetaMask/metamask-extension/issues/22938
    
    ## **Manual testing steps**
    
    ### Scenario 1
    0.  Have an ENS with two different address for Mainnet and Goerli
    1.  Enter the new send flow
    2. Type in the ENS on Mainnet -- see a search result with the correct
    address
    3. Change the network picker to Goerli
    4. See the address change in the search result
    
    ### Scenario 2
    0.  Have an ENS with two different address for Mainnet and Goerli
    1.  Enter the new send flow
    2. Type in the ENS on Mainnet -- see a search result with the correct
    address
    3. Click the search result (you'll go to the Amount screen)
    4. Change the network picker to Goerli
    5. See the address change in the recipient field change
    6. Execute the transaction
    7. Ensure token sent to correct address
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    (In the GitHub issue)
    
    ### **After**
    
    Coming....
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've clearly explained what problem this PR is solving and how it
    is solved.
    - [ ] I've linked related issues
    - [ ] I've included manual testing steps
    - [ ] I've included screenshots/recordings if applicable
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    - [ ] I’ve properly set the pull request status:
      - [ ] In case it's not yet "ready for review", I've set it to "draft".
    - [ ] In case it's "ready for review", I've changed it from "draft" to
    "non-draft".
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- ui/ducks/domains.js
+++ ui/ducks/domains.js
@@ -231,14 +231,15 @@ export async function fetchResolutions({ domain, address, chainId, state }) {
   return filteredResults;
 }
 
-export function lookupDomainName(domainName) {
+export function lookupDomainName(domainName, forceInitialize = false) {
   return async (dispatch, getState) => {
     const trimmedDomainName = domainName.trim();
     let state = getState();
-    if (state[name].stage === 'UNINITIALIZED') {
+    if (state[name].stage === 'UNINITIALIZED' || forceInitialize) {
       await dispatch(initializeDomainSlice());
     }
     state = getState();
+    let address;
     if (
       state[name].stage === 'NO_NETWORK_SUPPORT' &&
       !(
@@ -250,7 +251,6 @@ export function lookupDomainName(domainName) {
       await dispatch(domainNotSupported());
     } else {
       log.info(`Resolvers attempting to resolve name: ${trimmedDomainName}`);
-      let address;
       let fetchedResolutions;
       let hasSnapResolution = false;
       let error;
@@ -290,6 +290,7 @@ export function lookupDomainName(domainName) {
         }),
       );
     }
+    return address;
   };
 }
 
