e29483ef9f426c225aa14c851ea3866e97a579a3
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2024-02-26 11:41:31 +0000

    fix linting errors

--- app/scripts/lib/middleware/onboarding-complete.js
+++ app/scripts/lib/middleware/onboarding-complete.js
@@ -1,12 +1,8 @@
-// blockRequestsBeforeOnboardingMiddleware
 export function blockRequestsBeforeOnboardingMiddleware({ onboardingControllerStore }) {
   return ((_, __, next, end) => {
     const isOnboardingComplete = onboardingControllerStore.getState().completedOnboarding
     if (!isOnboardingComplete) {
       return end();
-      // is this better?
-      // res.error = new Error("Error message")
-      // return next();
     }
 
     return next();
