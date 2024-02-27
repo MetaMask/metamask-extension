fd6c7db1bb683d49cba82b1753a4068580fc12b1
Victor Thomas <10986371+vthomas13@users.noreply.github.com>
2024-02-26 21:08:45 -0500

    Add connectedAccounts message and update ConnectionListItem component

--- ui/components/multichain/pages/permissions-page/connection-list-item.js
+++ ui/components/multichain/pages/permissions-page/connection-list-item.js
@@ -2,10 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { SubjectType } from '@metamask/permission-controller';
 import { Tooltip } from 'react-tippy';
+import { useSelector } from 'react-redux';
 import {
   AlignItems,
   BackgroundColor,
   BlockSize,
+  BorderStyle,
   Display,
   FlexDirection,
   IconColor,
@@ -16,6 +18,9 @@ import {
 } from '../../../../helpers/constants/design-system';
 import { useI18nContext } from '../../../../hooks/useI18nContext';
 import {
+  AvatarAccount,
+  AvatarAccountSize,
+  AvatarAccountVariant,
   AvatarFavicon,
   BadgeWrapper,
   Box,
@@ -28,14 +33,22 @@ import { getURLHost } from '../../../../helpers/utils/util';
 import SnapAvatar from '../../../app/snaps/snap-avatar/snap-avatar';
 import { AvatarGroup } from '../../avatar-group';
 import { AvatarType } from '../../avatar-group/avatar-group.types';
+import { getUseBlockie } from '../../../../selectors';
 
 export const ConnectionListItem = ({ connection, onClick }) => {
   const t = useI18nContext();
   const isSnap = connection.subjectType === SubjectType.Snap;
   const AVATAR_GROUP_LIMIT = 5;
-  const addressIconList = connection.addresses?.map((address) => ({
-    avatarValue: address,
-  }));
+  const TOOLTIP_LIMIT = 7;
+  const addressIconList = connection.addresses
+    ?.slice(0, TOOLTIP_LIMIT)
+    .map((address) => ({
+      avatarValue: address,
+    }));
+  const useBlockie = useSelector(getUseBlockie);
+  const avatarAccountVariant = useBlockie
+    ? AvatarAccountVariant.Blockies
+    : AvatarAccountVariant.Jazzicon;
 
   return (
     <Box
@@ -86,7 +99,12 @@ export const ConnectionListItem = ({ connection, onClick }) => {
           {isSnap ? connection.packageName : getURLHost(connection.origin)}
         </Text>
         {isSnap ? null : (
-          <Box display={Display.Flex} flexDirection={FlexDirection.Row} gap={1}>
+          <Box
+            display={Display.Flex}
+            flexDirection={FlexDirection.Row}
+            alignItems={AlignItems.center}
+            gap={1}
+          >
             <Text
               as="span"
               width={BlockSize.Max}
@@ -96,30 +114,75 @@ export const ConnectionListItem = ({ connection, onClick }) => {
               {t('connectedWith')}
             </Text>
             <Tooltip
-              interactive
               position="bottom"
               html={
-                <>
+                <Box
+                  display={Display.Flex}
+                  flexDirection={FlexDirection.Column}
+                >
                   <Text
-                    as="span"
                     color={TextColor.textDefault}
                     variant={TextVariant.headingSm}
+                    paddingInline={10}
                   >
-                    Connected accounts
+                    {t('connectedAccounts')}
                   </Text>
-                  {connection.addresses.map((address) => {
-                    debugger;
-                    return (
-                      <Text
-                        color={TextColor.textDefault}
-                        variant={TextVariant.bodyMd}
-                        key={address}
+                  <Box
+                    display={Display.Flex}
+                    flexDirection={FlexDirection.Column}
+                  >
+                    {connection.addresses
+                      .slice(0, TOOLTIP_LIMIT)
+                      .map((address) => {
+                        return (
+                          <Box
+                            display={Display.Flex}
+                            flexDirection={FlexDirection.Row}
+                            alignItems={AlignItems.center}
+                            textAlign={TextAlign.Left}
+                            key={address}
+                            padding={1}
+                            paddingInline={2}
+                            gap={2}
+                          >
+                            <AvatarAccount
+                              size={AvatarAccountSize.Xs}
+                              address={address}
+                              variant={avatarAccountVariant}
+                              borderStyle={BorderStyle.none}
+                            />
+                            <Text
+                              color={TextColor.textDefault}
+                              variant={TextVariant.bodyMdMedium}
+                              key={address}
+                              data-testid="connection-list-item-connected-account-name"
+                              ellipsis
+                            >
+                              {connection.addressToNameMap[address]}
+                            </Text>
+                          </Box>
+                        );
+                      })}
+                    {connection.addresses.length > TOOLTIP_LIMIT && (
+                      <Box
+                        display={Display.Flex}
+                        alignItems={AlignItems.center}
+                        textAlign={TextAlign.Left}
+                        paddingInline={2}
                       >
-                        {connection.addressToNameMap[address]}
-                      </Text>
-                    );
-                  })}
-                </>
+                        <Text
+                          color={TextColor.textAlternative}
+                          variant={TextVariant.bodyMdMedium}
+                          data-testid="connection-list-item-plus-more-tooltip"
+                        >
+                          {t('plusMore', [
+                            connection.addresses.length - TOOLTIP_LIMIT,
+                          ])}
+                        </Text>
+                      </Box>
+                    )}
+                  </Box>
+                </Box>
               }
               arrow
               offset={0}
