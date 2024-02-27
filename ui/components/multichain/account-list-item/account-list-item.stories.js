0a787220414b095e2980e6fe1e9460c161d875dd
NidhiKJha <menidhikjha@gmail.com>
2024-02-27 15:18:07 +0530

    updated account list item menu to have a start accessory

--- ui/components/multichain/account-list-item/account-list-item.stories.js
+++ ui/components/multichain/account-list-item/account-list-item.stories.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import testData from '../../../../.storybook/test-data';
 import configureStore from '../../../store/store';
 import { AccountListItem, AccountListItemMenuTypes } from '.';
+import { Checkbox } from '../../component-library/index';
 
 const store = configureStore(testData);
 
@@ -176,6 +177,16 @@ ConnectedSiteChaosItem.args = {
   identity: CHAOS_ACCOUNT,
   connectedAvatar: 'https://uniswap.org/favicon.ico',
   connectedAvatarName: 'Uniswap',
+};
+
+export const ChaosStartAccessoryDataItem = (args) => (
+  <div {...CONTAINER_STYLES}>
+    <AccountListItem {...args} />
+  </div>
+);
+ChaosStartAccessoryDataItem.args = {
+  identity: CHAOS_IDENTITY,
+  startAccessory: <Checkbox />,
 };
 
 export const SnapItem = (args) => (
