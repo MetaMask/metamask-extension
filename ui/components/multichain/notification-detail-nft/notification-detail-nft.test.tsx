9f880a1af37ac95dc380379d35a71dc3d87e5758
Matteo Scurati <matteo.scurati@gmail.com>
2024-02-28 10:26:15 +0100

    test: :white_check_mark: fix a test

--- ui/components/multichain/notification-detail-nft/notification-detail-nft.test.tsx
+++ ui/components/multichain/notification-detail-nft/notification-detail-nft.test.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import { renderWithProvider } from '../../../../test/lib/render-helpers';
+import configureStore from '../../../store/store';
+import mockState from '../../../../test/data/mock-state.json';
 import { NotificationDetailNft } from './notification-detail-nft';
 
 describe('NotificationDetailNft', () => {
   const defaultProps = {
-    badgeIcon: 'https://example.com/badge.jpg',
-    nftIcon: 'https://example.com/nft.jpg',
+    tokenSrc:
+      'https://i.seadn.io/s/raw/files/a96f90ec8ebf55a2300c66a0c46d6a16.png?w=500&auto=format',
+    networkSrc:
+      'https://token.metaswap.codefi.network/assets/nativeCurrencyLogos/ethereum.svg',
+    tokenId: 'NFT ID',
+    tokenName: 'NFT Name',
+    networkName: 'Ethereum',
   };
 
   it('renders the NFT image', () => {
-    render(<NotificationDetailNft {...defaultProps} />);
+    const store = configureStore(mockState);
+    renderWithProvider(<NotificationDetailNft {...defaultProps} />, store);
     const images = screen.getAllByRole('img');
     const nftImage = images.find(
-      (img) => img.getAttribute('src') === defaultProps.nftIcon,
+      (img) => img.getAttribute('src') === defaultProps.tokenSrc,
     );
     expect(nftImage).toBeInTheDocument();
   });
