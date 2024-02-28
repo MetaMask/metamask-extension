4fe5034c394ca6760b4c0f7f1298f45748bb5789
Jonathan Bursztyn <jonathan@bursztyn.io>
2024-02-28 15:40:21 +0000

    Fix test warnings

--- ui/components/multichain/connected-accounts-menu/connected-accounts-menu.test.tsx
+++ ui/components/multichain/connected-accounts-menu/connected-accounts-menu.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent } from '@testing-library/react';
+import { fireEvent, act } from '@testing-library/react';
 import { renderWithProvider } from '../../../../test/jest';
 import configureStore from '../../../store/store';
 import mockState from '../../../../test/data/mock-state.json';
@@ -18,7 +18,7 @@ const render = (props = {}) => {
   });
   const allProps = { ...DEFAULT_PROPS, ...props };
   document.body.innerHTML = '<div id="anchor"></div>';
-  const anchorElement = document.getElementById('anchor') as HTMLElement;
+  const anchorElement = document.getElementById('anchor');
   return renderWithProvider(
     <ConnectedAccountsMenu anchorElement={anchorElement} {...allProps} />,
     store,
@@ -26,26 +26,34 @@ const render = (props = {}) => {
 };
 
 describe('ConnectedAccountsMenu', () => {
-  it('renders permission details menu item', () => {
-    const { getByText } = render();
-    expect(getByText('Permission details')).toBeInTheDocument();
+  it('renders permission details menu item', async () => {
+    await act(async () => {
+      const { getByText } = render();
+      expect(getByText('Permission details')).toBeInTheDocument();
+    });
   });
 
-  it('renders switch to this account menu item', () => {
-    const { getByText } = render();
-    expect(getByText('Switch to this account')).toBeInTheDocument();
+  it('renders switch to this account menu item', async () => {
+    await act(async () => {
+      const { getByText } = render();
+      expect(getByText('Switch to this account')).toBeInTheDocument();
+    });
   });
 
-  it('renders disconnect menu item', () => {
-    const { getByText } = render();
-    expect(getByText('Disconnect')).toBeInTheDocument();
+  it('renders disconnect menu item', async () => {
+    await act(async () => {
+      const { getByText } = render();
+      expect(getByText('Disconnect')).toBeInTheDocument();
+    });
   });
 
-  it('closes the menu on tab key down when focus is on the last menu item', () => {
+  it('closes the menu on tab key down when focus is on the last menu item', async () => {
     const onClose = jest.fn();
-    const { getByText } = render({ onClose });
-    const disconnectMenuItem = getByText('Disconnect');
-    fireEvent.keyDown(disconnectMenuItem, { key: 'Tab' });
+    await act(async () => {
+      const { getByText } = render({ onClose });
+      const disconnectMenuItem = getByText('Disconnect');
+      fireEvent.keyDown(disconnectMenuItem, { key: 'Tab' });
+    });
     expect(onClose).toHaveBeenCalled();
   });
 });
