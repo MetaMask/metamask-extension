92cbdb4db6049dbd02c9bfa55eeba58dc1976175
Bilal Zahory <bilal.zahory@consensys.net>
2024-02-28 00:13:25 -0500

    test: add dispatches to story

--- ui/components/multichain/asset-picker-amount/asset-picker-amount.stories.tsx
+++ ui/components/multichain/asset-picker-amount/asset-picker-amount.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import configureStore from '../../../store/store';
-import { DraftTransaction } from '../../../ducks/send';
+import { updateSendAmount } from '../../../ducks/send';
 import { AssetType } from '../../../../shared/constants/transaction';
 import mockState from '../../../../test/data/mock-state.json';
 import { AssetPickerAmount } from './asset-picker-amount';
@@ -15,10 +15,102 @@ const storybook = {
 };
 export default storybook;
 
+const store = configureStore({
+  ...mockState,
+  metamask: {
+    ...mockState.metamask,
+    currentCurrency: 'usd',
+    currencyRates: {
+      ETH: {
+        conversionRate: 231.06,
+      },
+    },
+    contractExchangeRates: {
+      ...mockState.metamask.contractExchangeRates,
+      '0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e': 0.01,
+    },
+    tokens: [
+      {
+        address: '0x0000000000000000000000000000000000000000',
+        symbol: 'TEST',
+        decimals: '6',
+      },
+      {
+        address: '0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e',
+        symbol: 'TEST',
+        decimals: '6',
+      },
+      {
+        address: '0xd8f6a2ffb0fc5952d16c9768b71cfd35b6399aa5',
+        decimals: '8',
+        symbol: 'TEST2',
+      },
+      {
+        address: '0x617b3f8050a0bd94b6b1da02b4384ee5b4df13f4',
+        symbol: 'META',
+        decimals: '18',
+      },
+    ],
+    providerConfig: {
+      chainId: '0x1',
+      ticker: 'ETH',
+    },
+    preferences: {
+      showFiatInTestnets: true,
+    },
+    useCurrencyRateCheck: true,
+  },
+  send: {
+    ...mockState.send,
+    selectedAccount: { address: '0x0', balance: '0x1' },
+    currentTransactionUUID: '1-tx',
+    draftTransactions: {
+      '1-tx': {
+        amount: {
+          error: null,
+          value: '0xde0b6b3a7640000',
+        },
+        asset: {
+          balance: '0x1158e460913d00000',
+          details: null,
+          error: null,
+          type: 'NATIVE',
+        },
+        fromAccount: null,
+        gas: {
+          error: null,
+          gasLimit: '0x5208',
+          gasPrice: '0x0',
+          gasTotal: '0x1ca62a544678',
+          maxFeePerGas: '0x59682f0f',
+          maxPriorityFeePerGas: '0x59682f00',
+          wasManuallyEdited: false,
+        },
+        history: [],
+        id: null,
+        recipient: {
+          address: '0xec1adf982415d2ef5ec55899b9bfb8bc0f29251b',
+          error: null,
+          nickname: 'Account 2',
+          warning: null,
+          type: '',
+          recipientWarningAcknowledged: false,
+        },
+        status: 'VALID',
+        transactionType: '0x2',
+        userInputHexData: null,
+      },
+    },
+  },
+});
+
 export const DefaultStory = () => (
   <div style={{ width: '400px' }}>
     <AssetPickerAmount
-      onAmountChange={noop}
+      onAmountChange={(newAmount: string) => {
+        console.log(newAmount);
+        store.dispatch(updateSendAmount(newAmount));
+      }}
       onAssetChange={noop}
       amount={{ value: '100' }}
       asset={{ type: AssetType.native, balance: '100' }}
@@ -27,13 +119,15 @@ export const DefaultStory = () => (
 );
 DefaultStory.storyName = 'Default';
 DefaultStory.decorators = [
-  (story) => <Provider store={store()}>{story()}</Provider>,
+  (story) => <Provider store={store}>{story()}</Provider>,
 ];
 
 export const TokenStory = () => (
   <div style={{ width: '400px' }}>
     <AssetPickerAmount
-      onAmountChange={noop}
+      onAmountChange={(newAmount: string) =>
+        store.dispatch(updateSendAmount(newAmount))
+      }
       onAssetChange={noop}
       amount={{ value: '0xff' }}
       asset={{
@@ -49,13 +143,15 @@ export const TokenStory = () => (
 );
 TokenStory.storyName = 'ERC20 Token';
 TokenStory.decorators = [
-  (story) => <Provider store={store()}>{story()}</Provider>,
+  (story) => <Provider store={store}>{story()}</Provider>,
 ];
 
 export const NFTStory = () => (
   <div style={{ width: '400px' }}>
     <AssetPickerAmount
-      onAmountChange={noop}
+      onAmountChange={(newAmount: string) =>
+        store.dispatch(updateSendAmount(newAmount))
+      }
       onAssetChange={noop}
       amount={{ value: '0xff' }}
       asset={{
@@ -72,14 +168,14 @@ export const NFTStory = () => (
   </div>
 );
 NFTStory.storyName = 'ERC721 Token';
-NFTStory.decorators = [
-  (story) => <Provider store={store()}>{story()}</Provider>,
-];
+NFTStory.decorators = [(story) => <Provider store={store}>{story()}</Provider>];
 
 export const TokenStoryWithLargeNameAndValue = () => (
   <div style={{ width: '400px' }}>
     <AssetPickerAmount
-      onAmountChange={noop}
+      onAmountChange={(newAmount: string) =>
+        store.dispatch(updateSendAmount(newAmount))
+      }
       onAssetChange={noop}
       amount={{ value: '0x1ED09BEAD87C0378D8E6400000000' }}
       asset={{
@@ -97,13 +193,15 @@ export const TokenStoryWithLargeNameAndValue = () => (
 TokenStoryWithLargeNameAndValue.storyName =
   'ERC20 Token with large name and value';
 TokenStoryWithLargeNameAndValue.decorators = [
-  (story) => <Provider store={store()}>{story()}</Provider>,
+  (story) => <Provider store={store}>{story()}</Provider>,
 ];
 
 export const ErrorStory = () => (
   <div style={{ width: '400px' }}>
     <AssetPickerAmount
-      onAmountChange={noop}
+      onAmountChange={(newAmount: string) =>
+        store.dispatch(updateSendAmount(newAmount))
+      }
       onAssetChange={noop}
       amount={{ error: INSUFFICIENT_FUNDS_ERROR_KEY, value: '100' }}
       asset={{ type: AssetType.native, balance: '0' }}
@@ -111,57 +209,3 @@ export const ErrorStory = () => (
   </div>
 );
 ErrorStory.storyName = 'Error';
-
-function store() {
-  return configureStore({
-    ...mockState,
-    metamask: {
-      ...mockState.metamask,
-      currentCurrency: 'usd',
-      currencyRates: {
-        ETH: {
-          conversionRate: 231.06,
-        },
-      },
-      contractExchangeRates: {
-        ...mockState.metamask.contractExchangeRates,
-        '0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e': 0.01,
-      },
-      tokens: [
-        {
-          address: '0x0000000000000000000000000000000000000000',
-          symbol: 'TEST',
-          decimals: '6',
-        },
-        {
-          address: '0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e',
-          symbol: 'TEST',
-          decimals: '6',
-        },
-        {
-          address: '0xd8f6a2ffb0fc5952d16c9768b71cfd35b6399aa5',
-          decimals: '8',
-          symbol: 'TEST2',
-        },
-        {
-          address: '0x617b3f8050a0bd94b6b1da02b4384ee5b4df13f4',
-          symbol: 'META',
-          decimals: '18',
-        },
-      ],
-      providerConfig: {
-        chainId: '0x1',
-        ticker: 'ETH',
-      },
-      preferences: {
-        showFiatInTestnets: true,
-      },
-      useCurrencyRateCheck: true,
-    },
-    send: {
-      ...mockState.send,
-      selectedAccount: { address: '0x0', balance: '0x1' },
-      currentTransactionUUID: 'uuid',
-    },
-  });
-}
