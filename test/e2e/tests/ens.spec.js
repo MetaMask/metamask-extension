94db70954e2209d56f371902b439a9024a2682f6
David Walsh <davidwalsh83@gmail.com>
2024-02-29 16:42:05 -0600

    UX: Multichain: Send Flow: Add e2e for ENS change during domain

--- test/e2e/tests/ens.spec.js
+++ test/e2e/tests/ens.spec.js
@@ -8,9 +8,14 @@ const FixtureBuilder = require('../fixture-builder');
 
 describe('ENS', function () {
   const sampleAddress = '1111111111111111111111111111111111111111';
+  const sampleAddress2 = '2222222222222222222222222222222222222222';
+
   const sampleEnsDomain = 'test.eth';
   const infuraUrl =
     'https://mainnet.infura.io/v3/00000000000000000000000000000000';
+
+  const infuraUrl2 =
+    'https://mainnet.infura.io/v3/00000000000000000000000000000002';
 
   async function mockInfura(mockServer) {
     await mockServer
@@ -68,13 +73,23 @@ describe('ENS', function () {
           },
         };
       });
+
+    await mockServer
+      .forPost(infuraUrl2)
+      .withJsonBodyIncluding({ method: 'eth_call' })
+      .thenCallback(() => {
+        return {
+          statusCode: 200,
+          json: {
+            jsonrpc: '2.0',
+            id: '1111111111111111',
+            result: `0x000000000000000000000000${sampleAddress2}`,
+          },
+        };
+      });
   }
 
   it('domain resolves to a correct address', async function () {
-    // TODO: Update Test when Multichain Send Flow is added
-    if (process.env.MULTICHAIN) {
-      return;
-    }
     await withFixtures(
       {
         fixtures: new FixtureBuilder().withNetworkControllerOnMainnet().build(),
@@ -109,6 +124,56 @@ describe('ENS', function () {
         await driver.findElement({
           css: '.ens-input__selected-input__subtitle',
           text: `0x${sampleAddress}`,
+        });
+      },
+    );
+  });
+
+  it.only('domain resolves to a correct address after changing network', async function () {
+    await withFixtures(
+      {
+        fixtures: new FixtureBuilder().withNetworkControllerOnMainnet().build(),
+        ganacheOptions: defaultGanacheOptions,
+        title: this.test.fullTitle(),
+        testSpecificMock: mockInfura,
+      },
+      async ({ driver }) => {
+        await unlockWallet(driver);
+
+        await driver.assertElementNotPresent('.loading-overlay');
+
+        await openActionMenuAndStartSendFlow(driver);
+
+        await driver.pasteIntoField(
+          'input[placeholder="Enter public address (0x) or ENS name"]',
+          sampleEnsDomain,
+        );
+
+        await driver.waitForSelector({
+          text: sampleEnsDomain,
+          css: '[data-testid="address-list-item-label"]',
+        });
+
+        await driver.clickElement('.address-list-item');
+
+        await driver.findElement({
+          css: '.ens-input__selected-input__title',
+          text: 'test.eth',
+        });
+
+        await driver.findElement({
+          css: '.ens-input__selected-input__subtitle',
+          text: `0x${sampleAddress}`,
+        });
+
+        // Change network
+        await driver.clickElement('[data-testid="send-page-network-picker"]');
+        await driver.clickElement('.toggle-button');
+        await driver.clickElement('[data-testid="Localhost 8545"]');
+
+        await driver.findElement({
+          css: '.ens-input__selected-input__subtitle',
+          text: `0x${sampleAddress2}`,
         });
       },
     );
