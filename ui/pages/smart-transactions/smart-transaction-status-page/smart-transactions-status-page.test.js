ad70bdad584466005383341f01e3f196b54d48ca
dan437 <80175477+dan437@users.noreply.github.com>
2024-03-01 14:12:40 +0100

    Fix tests

--- ui/pages/smart-transactions/smart-transaction-status-page/smart-transactions-status-page.test.js
+++ ui/pages/smart-transactions/smart-transaction-status-page/smart-transactions-status-page.test.js
@@ -14,7 +14,7 @@ describe('SmartTransactionStatusPage', () => {
   const requestState = {
     smartTransaction: {
       status: 'pending',
-      creationTime: 1519211809934,
+      creationTime: Date.now(),
     },
   };
 
@@ -25,6 +25,22 @@ describe('SmartTransactionStatusPage', () => {
       store,
     );
     expect(getByText('Submitting your transaction')).toBeInTheDocument();
+  });
+
+  it('renders the "Sorry for the wait" pending status', () => {
+    const store = configureMockStore(middleware)(createSwapsMockStore());
+    const newRequestState = {
+      ...requestState,
+      smartTransaction: {
+        ...requestState.smartTransaction,
+        creationTime: 1519211809934,
+      },
+    };
+    const { getByText } = renderWithProvider(
+      <SmartTransactionStatusPage requestState={newRequestState} />,
+      store,
+    );
+    expect(getByText('Sorry for the wait')).toBeInTheDocument();
   });
 
   it('renders the "success" STX status', () => {
@@ -40,7 +56,7 @@ describe('SmartTransactionStatusPage', () => {
       <SmartTransactionStatusPage requestState={requestState} />,
       store,
     );
-    expect(getByText('Transaction successful')).toBeInTheDocument();
+    expect(getByText('Your transaction is complete')).toBeInTheDocument();
   });
 
   it('renders the "reverted" STX status', () => {
@@ -56,23 +72,33 @@ describe('SmartTransactionStatusPage', () => {
       <SmartTransactionStatusPage requestState={requestState} />,
       store,
     );
-    expect(getByText('Smart Transaction had an error')).toBeInTheDocument();
+    expect(getByText('Your transaction failed')).toBeInTheDocument();
+    expect(
+      getByText(
+        'Sudden market changes can cause failures. If the problem continues, reach out to MetaMask customer support.',
+      ),
+    ).toBeInTheDocument();
   });
 
-  it('renders the "cancelled_user_cancelled" STX status', () => {
+  it('renders the "cancelled" STX status', () => {
     const mockStore = createSwapsMockStore();
     const latestSmartTransaction =
       mockStore.metamask.smartTransactionsState.smartTransactions[
         CHAIN_IDS.MAINNET
       ][1];
     requestState.smartTransaction = latestSmartTransaction;
-    latestSmartTransaction.status = 'cancelled_user_cancelled';
+    latestSmartTransaction.status = 'cancelled_would_revert';
     const store = configureMockStore(middleware)(mockStore);
     const { getByText } = renderWithProvider(
       <SmartTransactionStatusPage requestState={requestState} />,
       store,
     );
-    expect(getByText('Smart Transaction was cancelled')).toBeInTheDocument();
+    expect(getByText('Your transaction was canceled')).toBeInTheDocument();
+    expect(
+      getByText(
+        `Your transaction couldn't be completed, so it was canceled to save you from paying unnecessary gas fees.`,
+      ),
+    ).toBeInTheDocument();
   });
 
   it('renders the "deadline_missed" STX status', () => {
@@ -88,7 +114,7 @@ describe('SmartTransactionStatusPage', () => {
       <SmartTransactionStatusPage requestState={requestState} />,
       store,
     );
-    expect(getByText('Smart Transaction had an error')).toBeInTheDocument();
+    expect(getByText('Your transaction failed')).toBeInTheDocument();
   });
 
   it('renders the "unknown" STX status', () => {
@@ -104,6 +130,6 @@ describe('SmartTransactionStatusPage', () => {
       <SmartTransactionStatusPage requestState={requestState} />,
       store,
     );
-    expect(getByText('Smart Transaction had an error')).toBeInTheDocument();
+    expect(getByText('Your transaction failed')).toBeInTheDocument();
   });
 });
