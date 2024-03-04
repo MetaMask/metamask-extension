5f46342d6caef57d028b54429716049f7095efc7
Prithpal Sooriya <prithpal.sooriya@gmail.com>
2024-03-04 15:03:14 +0000

    refactor: fix lavamoat build issues

--- app/scripts/snaps/preinstalled-snaps.js
+++ app/scripts/snaps/preinstalled-snaps.js
@@ -4,34 +4,32 @@ const fs = require('fs');
 const PREINSTALLED_SNAPS = [];
 
 ///: BEGIN:ONLY_INCLUDE_IF(snaps)
-function tryAddSnap(snapName) {
-  try {
-    PREINSTALLED_SNAPS.push(
-      getPreinstalledSnap(
-        snapName,
-        fs.readFileSync(
-          require.resolve(`${snapName}/snap.manifest.json`),
-          'utf-8',
-        ),
-        [
-          {
-            path: 'images/icon.svg',
-            value: fs.readFileSync(
-              require.resolve(`${snapName}/images/icon.svg`),
-            ),
-          },
-          {
-            path: 'dist/bundle.js',
-            value: fs.readFileSync(
-              require.resolve(`${snapName}/dist/bundle.js`),
-            ),
-          },
-        ],
+try {
+  PREINSTALLED_SNAPS.push(
+    getPreinstalledSnap(
+      '@metamask/message-signing-snap',
+      fs.readFileSync(
+        require.resolve('@metamask/message-signing-snap/snap.manifest.json'),
+        'utf-8',
       ),
-    );
-  } catch {
-    // Failed adding snap
-  }
+      [
+        {
+          path: 'images/icon.svg',
+          value: fs.readFileSync(
+            require.resolve('@metamask/message-signing-snap/images/icon.svg'),
+          ),
+        },
+        {
+          path: 'dist/bundle.js',
+          value: fs.readFileSync(
+            require.resolve('@metamask/message-signing-snap/dist/bundle.js'),
+          ),
+        },
+      ],
+    ),
+  );
+} catch {
+  // Failed adding snap
 }
 
 function getPreinstalledSnap(npmPackage, manifest, files) {
@@ -42,8 +40,6 @@ function getPreinstalledSnap(npmPackage, manifest, files) {
     removable: false,
   };
 }
-
-tryAddSnap('@metamask/message-signing-snap');
 ///: END:ONLY_INCLUDE_IF
 
 module.exports.PREINSTALLED_SNAPS = Object.freeze(PREINSTALLED_SNAPS);
