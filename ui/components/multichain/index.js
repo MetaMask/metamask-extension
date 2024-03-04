80a59bfa3464b41a925995804f7cd65574035ebb
Jonathan Bursztyn <jonathan@bursztyn.io>
2024-03-04 22:03:46 +0900

    Readd AccountListMenu export

--- ui/components/multichain/index.js
+++ ui/components/multichain/index.js
@@ -1,5 +1,6 @@
 export { AccountListItem, AccountListItemMenuTypes } from './account-list-item';
 export { AccountListItemMenu } from './account-list-item-menu';
+export { AccountListMenu } from './account-list-menu';
 export { AccountPicker } from './account-picker';
 export { ActivityListItem } from './activity-list-item';
 export { AppHeader } from './app-header';
