0641b0e4f1df8f313e11acd62fd9b6b783c4c0ee
Frederik Bolding <frederik.bolding@gmail.com>
2024-03-05 13:48:19 +0100

    Fix form margins

--- ui/components/app/snaps/snap-ui-form/snap-ui-form.tsx
+++ ui/components/app/snaps/snap-ui-form/snap-ui-form.tsx
@@ -1,6 +1,12 @@
 import React, { FormEvent, FunctionComponent } from 'react';
 import { UserInputEventType } from '@metamask/snaps-sdk';
 import { useSnapInterfaceContext } from '../../../../contexts/snaps';
+import { Box } from '../../../component-library';
+import {
+  Display,
+  FlexDirection,
+  TextColor,
+} from '../../../../helpers/constants/design-system';
 
 export type SnapUIFormProps = {
   name: string;
@@ -18,8 +24,15 @@ export const SnapUIForm: FunctionComponent<SnapUIFormProps> = ({
   };
 
   return (
-    <form className="snap-ui-renderer__form" onSubmit={handleSubmit} id={name}>
+    <Box
+      as="form"
+      className="snap-ui-renderer__form"
+      onSubmit={handleSubmit}
+      id={name}
+      display={Display.Flex}
+      flexDirection={FlexDirection.Column}
+    >
       {children}
-    </form>
+    </Box>
   );
 };
