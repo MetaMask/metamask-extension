02a0c73d28e7b61dbaeb91589d5bb9a9b98aa129
Alex <adonesky@gmail.com>
2024-03-06 09:47:16 -0600

    use renderHookWithProvider

--- ui/hooks/usePolling.test.js
+++ ui/hooks/usePolling.test.js
@@ -1,7 +1,5 @@
-import React from 'react';
-import { renderHook, cleanup } from '@testing-library/react-hooks';
-import { Provider } from 'react-redux';
-import configureStore from '../store/store';
+import { cleanup } from '@testing-library/react-hooks';
+import { renderHookWithProvider } from '../../test/lib/render-helpers';
 import usePolling from './usePolling';
 
 describe('usePolling', () => {
@@ -17,31 +15,22 @@ describe('usePolling', () => {
       metamask: {},
     };
 
-    const wrapper = ({ children }) => (
-      <>
-        <Provider store={configureStore(mockState)}>{children}</Provider>
-      </>
-    );
-
-    renderHook(
-      () => {
-        usePolling({
-          callback: (pollingToken) => {
-            expect(mockStart).toHaveBeenCalledWith(networkClientId, options);
-            expect(pollingToken).toBeDefined();
-            done();
-            return (_pollingToken) => {
-              // noop
-            };
-          },
-          startPollingByNetworkClientId: mockStart,
-          stopPollingByPollingToken: mockStop,
-          networkClientId,
-          options,
-        });
-      },
-      { wrapper },
-    );
+    renderHookWithProvider(() => {
+      usePolling({
+        callback: (pollingToken) => {
+          expect(mockStart).toHaveBeenCalledWith(networkClientId, options);
+          expect(pollingToken).toBeDefined();
+          done();
+          return (_pollingToken) => {
+            // noop
+          };
+        },
+        startPollingByNetworkClientId: mockStart,
+        stopPollingByPollingToken: mockStop,
+        networkClientId,
+        options,
+      });
+    }, mockState);
   });
   // eslint-disable-next-line jest/no-done-callback
   it('calls the cleanup function with the correct pollingToken when unmounted', (done) => {
@@ -55,14 +44,8 @@ describe('usePolling', () => {
       metamask: {},
     };
 
-    const wrapper = ({ children }) => (
-      <>
-        <Provider store={configureStore(mockState)}>{children}</Provider>
-      </>
-    );
-
-    renderHook(
-      () => {
+    renderHookWithProvider(
+      () =>
         usePolling({
           callback: () => {
             return (_pollingToken) => {
@@ -75,9 +58,8 @@ describe('usePolling', () => {
           stopPollingByPollingToken: mockStop,
           networkClientId,
           options,
-        });
-      },
-      { wrapper },
+        }),
+      mockState,
     );
     cleanup();
   });
