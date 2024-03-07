02dcee52ecaab5a6292b350dcabdc2a244b3aeb9
Brian Bergeron <brian.e.bergeron@gmail.com>
2024-03-06 23:33:38 -0800

    feat: support bulk balance checks on blast (#23358)
    Support bulk balance checks on blast. This will reduce the number of RPC
    calls.
    
    I deployed this contract to blast to support the feature:
    https://github.com/wbobeirne/eth-balance-checker
    
    This was the tx:
    https://blastscan.io/tx/0x89a96f29e38ae2c42528ad0b0f07df9465bba6433eda12911c7520bcccc8fbaa
    
    Also verified the source code on blastscan.
    
    [![Open in GitHub
    Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/23358?quickstart=1)
    
    Fixes:
    
    Switch to blast network, should show native balances correctly across
    all accounts.
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    <!-- [screenshots/recordings] -->
    
    <!-- [screenshots/recordings] -->
    
    - [ ] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've clearly explained what problem this PR is solving and how it
    is solved.
    - [ ] I've linked related issues
    - [ ] I've included manual testing steps
    - [ ] I've included screenshots/recordings if applicable
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    - [ ] I’ve properly set the pull request status:
      - [ ] In case it's not yet "ready for review", I've set it to "draft".
    - [ ] In case it's "ready for review", I've changed it from "draft" to
    "non-draft".
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- shared/constants/network.ts
+++ shared/constants/network.ts
@@ -145,6 +145,7 @@ export const CHAIN_IDS = {
   GNOSIS: '0x64',
   ZKSYNC_ERA: '0x144',
   TEST_ETH: '0x539',
+  BLAST: '0x13e31',
 } as const;
 
 const CHAINLIST_CHAIN_IDS_MAP = {
@@ -852,6 +853,7 @@ export const BUYABLE_CHAINS_MAP: {
     | typeof CHAIN_IDS.SEPOLIA
     | typeof CHAIN_IDS.GNOSIS
     | typeof CHAIN_IDS.AURORA
+    | typeof CHAIN_IDS.BLAST
   >]: BuyableChainSettings;
 } = {
   [CHAIN_IDS.MAINNET]: {
