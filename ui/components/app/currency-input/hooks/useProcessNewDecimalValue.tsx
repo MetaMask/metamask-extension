a923d080aad7df3e2df8c3fa66a66d5bba16f136
Bilal Zahory <bilal.zahory@consensys.net>
2024-03-07 00:23:49 -0500

    chore: impl `useProcessNewDecimalValue`

---
+++ ui/components/app/currency-input/hooks/useProcessNewDecimalValue.tsx
@@ -0,0 +1,56 @@
+import { useCallback } from 'react';
+import { Numeric } from '../../../../../shared/modules/Numeric';
+
+const MAX_DECIMALS_TOKEN_SECONDARY = 6;
+
+/**
+ * A hook that creates a function which processes a new decimal value and returns the new fiat and token decimal values
+ *
+ * @param assetDecimals The number of decimals that asset supports
+ * @param isFiatPrimary If the fiat currency is the input currency
+ * @param tokenToFiatConversionRate The conversion rate from the asset to the user's fiat currency
+ * @returns A function that processes a new decimal value and returns the new fiat and token decimal values
+ */
+export default function useProcessNewDecimalValue(
+  assetDecimals: number,
+  isFiatPrimary: boolean,
+  tokenToFiatConversionRate: Numeric,
+) {
+  return useCallback(
+    (newDecimalValue) => {
+      let newFiatDecimalValue, newTokenDecimalValue;
+
+      const truncateToDecimals = (
+        numeric: Numeric,
+        maxDecimals = assetDecimals,
+      ) => {
+        const digitsAfterDecimal = numeric.toString().split('.')[1] || '';
+
+        const digitsCutoff = Math.min(
+          digitsAfterDecimal.length,
+          Math.min(maxDecimals, assetDecimals),
+        );
+
+        return numeric.toFixed(digitsCutoff);
+      };
+
+      const numericDecimalValue = new Numeric(newDecimalValue, 10);
+
+      if (isFiatPrimary) {
+        newFiatDecimalValue = numericDecimalValue.toFixed(2);
+        newTokenDecimalValue = truncateToDecimals(
+          numericDecimalValue.divide(tokenToFiatConversionRate),
+          MAX_DECIMALS_TOKEN_SECONDARY,
+        );
+      } else {
+        newFiatDecimalValue = numericDecimalValue
+          .times(tokenToFiatConversionRate)
+          .toFixed(2);
+        newTokenDecimalValue = truncateToDecimals(numericDecimalValue);
+      }
+
+      return { newFiatDecimalValue, newTokenDecimalValue };
+    },
+    [tokenToFiatConversionRate, isFiatPrimary, assetDecimals],
+  );
+}
