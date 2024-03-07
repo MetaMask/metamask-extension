577d59d9d14d01fb17bae8f55ffec548c3f75e82
JSoufer <jonathan.ferreira@consensys.net>
2024-03-07 10:25:09 +0000

    chore: use TextColor.Default always

--- ui/components/app/snaps/snap-delineator/snap-delineator.js
+++ ui/components/app/snaps/snap-delineator/snap-delineator.js
@@ -83,7 +83,7 @@ export const SnapDelineator = ({
           />
           <Text
             variant={TextVariant.bodySm}
-            color={isError ? TextColor.errorDefault : TextColor.textAlternative}
+            color={TextColor.Default}
             className="snap-delineator__header__text"
             marginLeft={1}
             marginTop={0}
