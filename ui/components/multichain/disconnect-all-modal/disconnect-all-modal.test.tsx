bcffd2c42499dd14cda038fef1e45db5959740f8
NidhiKJha <menidhikjha@gmail.com>
2024-03-08 20:44:16 +0530

    lint fix

--- ui/components/multichain/disconnect-all-modal/disconnect-all-modal.test.tsx
+++ ui/components/multichain/disconnect-all-modal/disconnect-all-modal.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { fireEvent, screen } from '../../../../test/jest';
+import { fireEvent } from '../../../../test/jest';
 
 import { DisconnectType } from './disconnect-all-modal';
 import { DisconnectAllModal } from '.';
