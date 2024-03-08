1e8fbff44bbedfc1863cad3e41cdf57d1faebd7e
Brian Bergeron <brian.e.bergeron@gmail.com>
2024-03-07 22:11:16 -0800

    fix e2e test click send btn

--- test/e2e/tests/send-to-contract.spec.js
+++ test/e2e/tests/send-to-contract.spec.js
@@ -33,7 +33,12 @@ describe('Send ERC20 token to contract address', function () {
         await driver.clickElement(
           '[data-testid="multichain-token-list-button"]',
         );
-        await driver.clickElement('[data-testid="eth-overview-send"]');
+
+        const sendButton = await driver.findElement(
+          '[data-testid="asset-send-button"]',
+        );
+        await driver.scrollToElement(sendButton);
+        sendButton.click();
 
         // Type contract address
         await driver.fill(
