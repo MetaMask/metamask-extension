ba1f3cae5a02e83c15ac51bfdcb7debd61766539
dan437 <80175477+dan437@users.noreply.github.com>
2024-03-12 15:27:55 +0100

    Check if an RPC URL is allowed for STX
    Infura RPC URL check

--- shared/modules/selectors.ts
+++ shared/modules/selectors.ts
@@ -1,15 +1,11 @@
 import { ALLOWED_SMART_TRANSACTIONS_CHAIN_IDS } from '../constants/smartTransactions';
-import { KeyringType } from '../constants/keyring';
+import { ENVIRONMENT } from '../../development/build/constants';
+import {
+  getCurrentChainId,
+  getCurrentNetwork,
+  accountSupportsSmartTx,
+} from '../../ui/selectors/selectors'; // TODO: Migrate shared selectors to this file.
 import { getNetworkNameByChainId } from './feature-flags';
-
-export const getProviderConfig = (state: Record<string, any>) => {
-  return state.metamask.providerConfig;
-};
-
-export const getCurrentChainId = (state: Record<string, any>): string => {
-  const { chainId } = getProviderConfig(state);
-  return chainId;
-};
 
 export const getSmartTransactionsOptInStatus = (
   state: Record<string, any>,
@@ -17,100 +13,29 @@ export const getSmartTransactionsOptInStatus = (
   return state.metamask.preferences?.stxOptIn;
 };
 
-export function getSelectedInternalAccount(state: Record<string, any>) {
-  const accountId = state.metamask.internalAccounts?.selectedAccount;
-  if (!accountId) {
-    return null;
-  }
-  return state.metamask.internalAccounts?.accounts[accountId];
-}
-
-export function getCurrentKeyring(state: Record<string, any>) {
-  const internalAccount = getSelectedInternalAccount(state);
-
-  if (!internalAccount) {
-    return null;
-  }
-
-  return internalAccount.metadata.keyring;
-}
-
-/**
- * Checks if the current wallet is a hardware wallet.
- *
- * @param state
- * @returns
- */
-export function isHardwareWallet(state: Record<string, any>): boolean {
-  const keyring = getCurrentKeyring(state);
-  return Boolean(keyring?.type?.includes('Hardware'));
-}
-
-export function getAccountTypeForKeyring(
-  keyring: { type: string } | null,
-): string {
-  if (!keyring) {
-    return '';
-  }
-
-  const { type } = keyring;
-
-  ///: BEGIN:ONLY_INCLUDE_IF(build-mmi)
-  if (type.startsWith('Custody')) {
-    return 'custody';
-  }
-  ///: END:ONLY_INCLUDE_IF
-
-  switch (type) {
-    case KeyringType.trezor:
-    case KeyringType.ledger:
-    case KeyringType.lattice:
-    case KeyringType.qr:
-      return 'hardware';
-    case KeyringType.imported:
-      return 'imported';
-    ///: BEGIN:ONLY_INCLUDE_IF(keyring-snaps)
-    case KeyringType.snap:
-      return 'snap';
-    ///: END:ONLY_INCLUDE_IF
-    default:
-      return 'default';
-  }
-}
-
-export function getAccountType(state: Record<string, any>): string {
-  const currentKeyring = getCurrentKeyring(state);
-  return getAccountTypeForKeyring(currentKeyring);
-}
-
-/**
- * Checks if the account supports smart transactions.
- *
- * @param state - The state object.
- * @returns
- */
-export function accountSupportsSmartTx(state: Record<string, any>): boolean {
-  const accountType = getAccountType(state);
-
-  return Boolean(accountType !== 'hardware' && accountType !== 'snap');
-}
-
-/**
- * Get a HW wallet type, e.g. "Ledger Hardware"
- *
- * @param state
- * @returns
- */
-export function getHardwareWalletType(
-  state: Record<string, any>,
-): string | undefined {
-  const keyring = getCurrentKeyring(state);
-  return isHardwareWallet(state) ? keyring.type : undefined;
-}
-
 export const getIsAllowedStxChainId = (state: Record<string, any>): boolean => {
   const chainId = getCurrentChainId(state);
   return ALLOWED_SMART_TRANSACTIONS_CHAIN_IDS.includes(chainId);
+};
+
+const getIsAllowedRpcUrlForStx = (state: Record<string, any>) => {
+  const isNotDevelopment =
+    process.env.METAMASK_ENVIRONMENT !== ENVIRONMENT.DEVELOPMENT &&
+    process.env.METAMASK_ENVIRONMENT !== ENVIRONMENT.TESTING;
+  if (!isNotDevelopment) {
+    return true; // Allow any STX RPC URL in development and testing environments.
+  }
+  const currentNetwork = getCurrentNetwork(state);
+  if (!currentNetwork?.rpcUrl) {
+    return false;
+  }
+  const rpcUrl = new URL(currentNetwork.rpcUrl);
+  // Only allow STX in prod if an Infura RPC URL is being used.
+  return rpcUrl?.hostname?.endsWith('.infura.io');
+};
+
+export const getIsStxOptInAvailable = (state: Record<string, any>) => {
+  return getIsAllowedStxChainId(state) && getIsAllowedRpcUrlForStx(state);
 };
 
 export const getSmartTransactionsEnabled = (
@@ -124,7 +49,7 @@ export const getSmartTransactionsEnabled = (
   const smartTransactionsLiveness =
     state.metamask.smartTransactionsState?.liveness;
   return Boolean(
-    getIsAllowedStxChainId(state) &&
+    getIsStxOptInAvailable(state) &&
       supportedAccount &&
       smartTransactionsFeatureFlagEnabled &&
       smartTransactionsLiveness,
@@ -158,6 +83,7 @@ const sharedSelectors = {
   getIsSmartTransaction,
   getIsAllowedStxChainId,
   getFeatureFlagsByChainId,
+  getIsStxOptInAvailable,
 };
 
 export default sharedSelectors;
