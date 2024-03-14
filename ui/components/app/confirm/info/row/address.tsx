1a5f98afd85ef8e9b40b2e7a509501b87cb29fc4
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2024-03-14 18:52:54 +0000

    fix implementation for snaps

--- ui/components/app/confirm/info/row/address.tsx
+++ ui/components/app/confirm/info/row/address.tsx
@@ -8,7 +8,7 @@ import {
   FlexDirection,
   TextColor,
 } from '../../../../../helpers/constants/design-system';
-import { getPetnamesEnabled, getSnaps } from '../../../../../selectors';
+import { getPetnamesEnabled } from '../../../../../selectors';
 import {
   AvatarAccount,
   AvatarAccountSize,
@@ -21,22 +21,16 @@ import { useFallbackDisplayName } from './hook';
 
 export type ConfirmInfoRowAddressProps = {
   address: string;
+  isSnapUsingThis?: boolean;
 };
 
 export const ConfirmInfoRowAddress = ({
   address,
+  isSnapUsingThis,
 }: ConfirmInfoRowAddressProps) => {
   const isPetNamesEnabled = useSelector(getPetnamesEnabled);
   const { displayName, hexAddress } = useFallbackDisplayName(address);
   const [isNicknamePopoverShown, setIsNicknamePopoverShown] = useState(false);
-
-  // PetNames on this component are disabled for snaps until the `<Name />`
-  // component can support variations. See this comment for context:
-  // https://github.com/MetaMask/metamask-extension/pull/23487#discussion_r1525055546
-  let isSnapInstalled = false;
-  ///: BEGIN:ONLY_INCLUDE_IF(snaps)
-  isSnapInstalled = Object.values(useSelector(getSnaps)).length > 0;
-  ///: END:ONLY_INCLUDE_IF
 
   return (
     <Box
@@ -44,37 +38,42 @@ export const ConfirmInfoRowAddress = ({
       flexDirection={FlexDirection.Row}
       alignItems={AlignItems.center}
     >
-      {isPetNamesEnabled && !isSnapInstalled ? (
-        <Name value={hexAddress} type={NameType.ETHEREUM_ADDRESS} />
-      ) : (
-        <>
-          <Box
-            display={Display.Flex}
-            flexDirection={FlexDirection.Row}
-            alignItems={AlignItems.center}
-            onClick={() => setIsNicknamePopoverShown(true)}
-          >
-            <AvatarAccount
-              address={address}
-              size={AvatarAccountSize.Xs}
-              borderColor={BorderColor.transparent}
-            />
-            <Text
-              marginLeft={2}
-              color={TextColor.inherit}
-              data-testid="confirm-info-row-display-name"
+      {
+        // PetNames on this component are disabled for snaps until the `<Name />`
+        // component can support variations. See this comment for context: //
+        // https://github.com/MetaMask/metamask-extension/pull/23487#discussion_r1525055546
+        isPetNamesEnabled && !isSnapUsingThis ? (
+          <Name value={hexAddress} type={NameType.ETHEREUM_ADDRESS} />
+        ) : (
+          <>
+            <Box
+              display={Display.Flex}
+              flexDirection={FlexDirection.Row}
+              alignItems={AlignItems.center}
+              onClick={() => setIsNicknamePopoverShown(true)}
             >
-              {displayName}
-            </Text>
-          </Box>
-          {isNicknamePopoverShown ? (
-            <NicknamePopovers
-              onClose={() => setIsNicknamePopoverShown(false)}
-              address={hexAddress}
-            />
-          ) : null}
-        </>
-      )}
+              <AvatarAccount
+                address={address}
+                size={AvatarAccountSize.Xs}
+                borderColor={BorderColor.transparent}
+              />
+              <Text
+                marginLeft={2}
+                color={TextColor.inherit}
+                data-testid="confirm-info-row-display-name"
+              >
+                {displayName}
+              </Text>
+            </Box>
+            {isNicknamePopoverShown ? (
+              <NicknamePopovers
+                onClose={() => setIsNicknamePopoverShown(false)}
+                address={hexAddress}
+              />
+            ) : null}
+          </>
+        )
+      }
     </Box>
   );
 };
