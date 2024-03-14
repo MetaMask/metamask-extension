24c20815c6486e1b6427694ccd1f19d5c7c43d7e
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2024-03-14 14:08:35 +0000

    Fix ConfirmInfoRowTypedSignData tests

--- ui/pages/confirmations/components/confirm/row/typed-sign-data/typedSignData.test.tsx
+++ ui/pages/confirmations/components/confirm/row/typed-sign-data/typedSignData.test.tsx
@@ -1,21 +1,29 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-
 import { unapprovedTypedSignMsg } from '../../../../../../../test/data/confirmations/typed_sign';
+import mockState from '../../../../../../../test/data/mock-state.json';
+import { renderWithProvider } from '../../../../../../../test/lib/render-helpers';
+import configureStore from '../../../../../../store/store';
 import { ConfirmInfoRowTypedSignData } from './typedSignData';
 
 describe('ConfirmInfoRowTypedSignData', () => {
-  it('should match snapshot', () => {
-    const { container } = render(
-      <ConfirmInfoRowTypedSignData
-        data={unapprovedTypedSignMsg.msgParams.data}
-      />,
+  const render = (data: string = unapprovedTypedSignMsg.msgParams.data) => {
+    const store = configureStore({
+      metamask: { ...mockState.metamask },
+    });
+
+    return renderWithProvider(
+      <ConfirmInfoRowTypedSignData data={data} />,
+      store,
     );
+  };
+
+  it('should match snapshot', () => {
+    const { container } = render(unapprovedTypedSignMsg.msgParams.data);
     expect(container).toMatchSnapshot();
   });
 
   it('should return null if data is not defined', () => {
-    const { container } = render(<ConfirmInfoRowTypedSignData data={''} />);
+    const { container } = render('');
     expect(container).toBeEmptyDOMElement();
   });
 });
