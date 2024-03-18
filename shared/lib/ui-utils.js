c61f0c4ac55ea8e9b1412031aac90871f2c9963a
Jyoti Puri <jyotipuri@gmail.com>
2024-03-18 17:29:29 +0530

    Security setting UI changes

--- shared/lib/ui-utils.js
+++ shared/lib/ui-utils.js
@@ -12,5 +12,3 @@ export const CONSENSYS_PRIVACY_LINK = 'https://consensys.io/privacy-policy/';
 export const AUTO_DETECT_TOKEN_LEARN_MORE_LINK =
   'https://consensys.io/privacy-policy/';
 export const CONSENSYS_TERMS_OF_USE = 'https://consensys.io/terms-of-use';
-
-export const OPENSEA_TERMS_OF_USE = 'https://opensea.io/securityproviderterms';
