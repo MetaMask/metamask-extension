2950a3d537982ea99681c64c23b37d39f83001ba
David Walsh <davidwalsh83@gmail.com>
2024-03-19 11:42:09 -0500

    Fix toChecksumHexAddress import

--- ui/components/multichain/account-picker/account-picker.js
+++ ui/components/multichain/account-picker/account-picker.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { useSelector } from 'react-redux';
-import { toChecksumHexAddress } from '@metamask/controller-utils';
+import { toChecksumHexAddress } from '../../../../shared/modules/hexstring-utils';
 import {
   AvatarAccount,
   AvatarAccountVariant,
