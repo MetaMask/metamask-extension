041f0768f2637af92651410d32c91ddf04abadcd
OGPoyraz <omergoktugpoyraz@gmail.com>
2024-03-22 10:57:00 +0100

    fix fence

--- ui/helpers/constants/settings.js
+++ ui/helpers/constants/settings.js
@@ -287,7 +287,7 @@ export const SETTINGS_CONSTANTS = [
     route: `${SECURITY_ROUTE}#security-alerts-opensea`,
     icon: 'fa fa-lock',
   },
-  ///: BEGIN:ONLY_INCLUDE_IF(transaction-simulations)
+  ///: BEGIN:ONLY_INCLUDE_IF(transaction-simulation)
   // securityAndPrivacy settingsRefs[18]
   {
     tabMessage: (t) => t('securityAndPrivacy'),
