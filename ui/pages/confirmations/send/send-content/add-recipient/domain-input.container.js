fe2c097e204312a7e3267cc3d654f80093372840
David Walsh <davidwalsh83@gmail.com>
2024-03-25 16:37:55 -0500

    Revert "Fix #22938 - UX - Multichain - Send Flow - Update ENS address upon network change (#23024)"
    This reverts commit f17a26b9810150f6bccb665e6eb4774a8eda4a3e.

--- ui/pages/confirmations/send/send-content/add-recipient/domain-input.container.js
+++ ui/pages/confirmations/send/send-content/add-recipient/domain-input.container.js
@@ -5,13 +5,7 @@ import {
   initializeDomainSlice,
   resetDomainResolution,
 } from '../../../../../ducks/domains';
-import { getCurrentChainId } from '../../../../../selectors';
 import DomainInput from './domain-input.component';
-
-// Trigger onChange when chainId changes using MapStateToProps
-function mapStateToProps(state) {
-  return { chainId: getCurrentChainId(state) };
-}
 
 function mapDispatchToProps(dispatch) {
   return {
@@ -27,4 +21,4 @@ function mapDispatchToProps(dispatch) {
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(DomainInput);
+export default connect(null, mapDispatchToProps)(DomainInput);
