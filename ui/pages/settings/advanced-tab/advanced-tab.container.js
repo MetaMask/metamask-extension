44c5cdcffaddcf67f5e54b7474be3f5ddb59c590
dan437 <80175477+dan437@users.noreply.github.com>
2024-03-26 15:30:17 +0100

    Remove the conditional disabled status from the STX opt-in toggle

--- ui/pages/settings/advanced-tab/advanced-tab.container.js
+++ ui/pages/settings/advanced-tab/advanced-tab.container.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { compose } from 'redux';
 import { DEFAULT_AUTO_LOCK_TIME_LIMIT } from '../../../../shared/constants/preferences';
-import { getPreferences, getIsStxOptInAvailable } from '../../../selectors';
+import { getPreferences } from '../../../selectors';
 import {
   backupUserData,
   displayWarning,
@@ -38,7 +38,6 @@ export const mapStateToProps = (state) => {
     stxOptIn,
     autoLockTimeLimit = DEFAULT_AUTO_LOCK_TIME_LIMIT,
   } = getPreferences(state);
-  const isStxOptInAvailable = getIsStxOptInAvailable(state);
 
   return {
     warning,
@@ -51,7 +50,6 @@ export const mapStateToProps = (state) => {
     useNonceField,
     dismissSeedBackUpReminder,
     disabledRpcMethodPreferences,
-    isStxOptInAvailable,
   };
 };
 
