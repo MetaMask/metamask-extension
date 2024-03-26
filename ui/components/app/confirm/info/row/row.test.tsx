19485246f3066bf02ba7d47f1970a5d1a0e90d4a
Vinicius Stevam <vinicius.stevam@consensys.net>
2024-03-26 10:56:31 +0000

    fix lint

--- ui/components/app/confirm/info/row/row.test.tsx
+++ ui/components/app/confirm/info/row/row.test.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import configureMockStore from 'redux-mock-store';
-import { ConfirmInfoRow } from './row';
 import { Text } from '../../../../component-library';
 import { renderWithProvider } from '../../../../../../test/lib/render-helpers';
 import { Severity } from '../../../../../helpers/constants/design-system';
+import { ConfirmInfoRow } from './row';
 
 describe('ConfirmInfoRow', () => {
   const OWNER_ID_MOCK = '123';
@@ -41,7 +40,10 @@ describe('ConfirmInfoRow', () => {
 
   it('should match snapshot', () => {
     const { container } = renderWithProvider(
-      <ConfirmInfoRow label={KEY_ALERT_KEY_MOCK} children={<Text>value</Text>} />,
+      <ConfirmInfoRow
+        label={KEY_ALERT_KEY_MOCK}
+        children={<Text>value</Text>}
+      />,
       mockStore,
     );
     expect(container).toMatchSnapshot();
@@ -49,10 +51,13 @@ describe('ConfirmInfoRow', () => {
 
   it('renders row with alert', () => {
     const { getAllByTestId } = renderWithProvider(
-      <ConfirmInfoRow label={KEY_ALERT_KEY_MOCK} children={<Text>value</Text>} alertKey={KEY_ALERT_KEY_MOCK} />,
+      <ConfirmInfoRow
+        label={KEY_ALERT_KEY_MOCK}
+        children={<Text>value</Text>}
+        alertKey={KEY_ALERT_KEY_MOCK}
+      />,
       mockStore,
     );
     expect(getAllByTestId('inline-alert')).toBeDefined();
   });
-
 });
