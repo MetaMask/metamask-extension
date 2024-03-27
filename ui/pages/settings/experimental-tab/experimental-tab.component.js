1a31ce2ada48fb121b0fc5c22c2ea723692c8a8f
Dan J Miller <danjm.com@gmail.com>
2024-03-27 05:53:29 -0230

    Move experimental toggle behind a feature flag

--- ui/pages/settings/experimental-tab/experimental-tab.component.js
+++ ui/pages/settings/experimental-tab/experimental-tab.component.js
@@ -280,7 +280,7 @@ export default class ExperimentalTab extends PureComponent {
     return (
       <div className="settings-page__body">
         {this.renderTogglePetnames()}
-        {this.renderNotificationsToggle()}
+        {process.env.NOTIFICATIONS ? this.renderNotificationsToggle() : null}
         {
           ///: BEGIN:ONLY_INCLUDE_IF(keyring-snaps)
           this.renderKeyringSnapsToggle()
