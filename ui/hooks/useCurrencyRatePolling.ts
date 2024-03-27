04de49f1a9c8f41d1eee2b2ed4fcab5bf3bd9be6
Jiexi Luan <jiexiluan@gmail.com>
2024-03-27 15:37:04 -0700

    guard useCurrencyRatePolling with completedOnboarding

--- ui/hooks/useCurrencyRatePolling.ts
+++ ui/hooks/useCurrencyRatePolling.ts
@@ -4,16 +4,18 @@ import {
   currencyRateStartPollingByNetworkClientId,
   currencyRateStopPollingByPollingToken,
 } from '../store/actions';
+import { getCompletedOnboarding } from '../ducks/metamask/metamask';
 import usePolling from './usePolling';
 
 const useCurrencyRatePolling = (networkClientId: string) => {
   const useCurrencyRateCheck = useSelector(getUseCurrencyRateCheck);
+  const completedOnboarding = useSelector(getCompletedOnboarding);
 
   usePolling({
     startPollingByNetworkClientId: currencyRateStartPollingByNetworkClientId,
     stopPollingByPollingToken: currencyRateStopPollingByPollingToken,
     networkClientId,
-    enabled: useCurrencyRateCheck,
+    enabled: useCurrencyRateCheck && completedOnboarding,
   });
 };
 
