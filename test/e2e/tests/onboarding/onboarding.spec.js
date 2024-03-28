f5476c2b3f6c03ecedddf61fe6144cc2c4e5780b
Thomas Huang <tmashuang@users.noreply.github.com>
2024-03-28 07:54:00 -0700

    Add ability to have any number to ganache servers for e2e

--- test/e2e/tests/onboarding/onboarding.spec.js
+++ test/e2e/tests/onboarding/onboarding.spec.js
@@ -258,7 +258,7 @@ describe('MetaMask onboarding @no-mmi', function () {
         fixtures: new FixtureBuilder({ onboarding: true }).build(),
         ganacheOptions: {
           ...defaultGanacheOptions,
-          concurrent: { port, chainId, ganacheOptions2 },
+          concurrent: [{ port, chainId, ganacheOptions2 }],
         },
         title: this.test.fullTitle(),
       },
