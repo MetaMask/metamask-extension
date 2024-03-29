ff44bfe02b33b7b49257d336dd4256497e5f213c
Victor Thomas <10986371+vthomas13@users.noreply.github.com>
2024-03-29 11:04:44 -0400

    Update disableExternalServices flag in state snapshots

--- test/e2e/fixture-builder.js
+++ test/e2e/fixture-builder.js
@@ -156,6 +156,7 @@ function defaultFixture(inputChainId = CHAIN_IDS.LOCALHOST) {
       PreferencesController: {
         advancedGasFee: null,
         currentLocale: 'en',
+        disableExternalServices: false,
         dismissSeedBackUpReminder: true,
         featureFlags: {},
         forgottenPassword: false,
@@ -180,7 +181,6 @@ function defaultFixture(inputChainId = CHAIN_IDS.LOCALHOST) {
           useNativeCurrencyAsPrimaryCurrency: true,
           petnamesEnabled: true,
         },
-        disableExternalServices: false,
         selectedAddress: '0x5cfe73b6021e818b776b421b1c4db2474086a7e1',
         theme: 'light',
         useBlockie: false,
