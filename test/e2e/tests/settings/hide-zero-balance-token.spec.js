c9593f8469641157b9afea96f0c74ddcb84eb60c
Chloe Gao <chloe.gao@consensys.net>
2024-04-02 13:48:40 +0100

    offsite demo

---
+++ test/e2e/tests/settings/hide-zero-balance-token.spec.js
@@ -0,0 +1,76 @@
+const { strict: assert } = require('assert');
+const { toHex } = require('@metamask/controller-utils');
+const {
+  defaultGanacheOptions,
+  withFixtures,
+  unlockWallet,
+} = require('../../helpers');
+const FixtureBuilder = require('../../fixture-builder');
+const { SMART_CONTRACTS } = require('../../seeder/smart-contracts');
+
+describe('Hide token with zero-balance', function () {
+  // /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // This script tests the "hide zero-balance tokens" feature in wallet settings, ensuring that tokens with zero        //
+  // balances are correctly hidden from the tokens list tab when corresponding toggle is enabled.                       //
+  // /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  it('user can activate hide token with zero-balance feature via settings', async function () {
+    const smartContract = SMART_CONTRACTS.HST;
+    await withFixtures(
+      {
+        fixtures: new FixtureBuilder()
+        .withTokensController({
+          allTokens: {
+            [toHex(1337)]: {
+              '0x5cfe73b6021e818b776b421b1c4db2474086a7e1': [
+                {
+                  address: '0x581c3C1A2A4EBDE2A0Df29B5cf4c116E42945947',
+                  decimals: 4,
+                  image: null,
+                  isERC721: false,
+                  symbol: 'TST',
+                },
+                  {
+                  address: '0x581c3C1A2A4EBDE2A0Df29B5cf4c116E42945948',
+                  decimals: 4,
+                  image: null,
+                  isERC721: false,
+                  symbol: 'TST2',
+                },
+              ],
+            },
+          },
+          tokens: [
+             {
+              address: '0x581c3C1A2A4EBDE2A0Df29B5cf4c116E42945948',
+              decimals: 4,
+              image: null,
+              isERC721: false,
+              symbol: 'TST2',
+            },
+              {
+              address: '0x581c3C1A2A4EBDE2A0Df29B5cf4c116E42945947',
+              decimals: 4,
+              image: null,
+              isERC721: false,
+              symbol: 'TST',
+            },
+          ],
+        })
+        .build(),
+        ganacheOptions: defaultGanacheOptions,
+        smartContract,
+        title: this.test.fullTitle(),
+      },
+      async ({ driver }) => {
+        await unlockWallet(driver);
+
+        // Verify that both zero-balance tokens and non-zero-balance tokens are displayed by default
+        // Navigate to settings and enable the "hide zero-balance tokens" feature by toggling it on
+        // Check that tokens with zero balances are hidden, tokens with non-zero balances remain visible
+
+
+
+      },
+    );
+  });
+});
