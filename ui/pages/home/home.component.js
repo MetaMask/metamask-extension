92c54b138f836e86b5de6a63038aa535080b9315
dan437 <80175477+dan437@users.noreply.github.com>
2024-04-04 17:05:39 +0200

    Refactor code for showing modals on homepage

--- ui/pages/home/home.component.js
+++ ui/pages/home/home.component.js
@@ -838,22 +838,21 @@ export default class Home extends PureComponent {
     }
     const tabPadding = process.env.MULTICHAIN ? 4 : 0; // TODO: Remove tabPadding and add paddingTop={4} to parent container Box of Tabs
 
-    const showSmartTransactionsOptInModal =
-      completedOnboarding &&
-      (!onboardedInThisUISession || firstTimeFlowType === 'import') &&
-      !process.env.IN_TEST &&
-      !newNetworkAddedConfigurationId &&
-      isSmartTransactionsOptInModalAvailable;
-
     ///: BEGIN:ONLY_INCLUDE_IF(build-main,build-beta,build-flask)
-    const showWhatsNew =
+    const canSeeModals =
       completedOnboarding &&
       (!onboardedInThisUISession ||
         firstTimeFlowType === FirstTimeFlowType.import) &&
+      !process.env.IN_TEST &&
+      !newNetworkAddedConfigurationId;
+
+    const showSmartTransactionsOptInModal =
+      canSeeModals && isSmartTransactionsOptInModalAvailable;
+
+    const showWhatsNew =
+      canSeeModals &&
       announcementsToShow &&
       showWhatsNewPopup &&
-      !process.env.IN_TEST &&
-      !newNetworkAddedConfigurationId &&
       !showSmartTransactionsOptInModal;
 
     const showTermsOfUse =
