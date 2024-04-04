763008b9a516cc3d1a36eefc733291a0b4ab4c77
Prithpal Sooriya <prithpal.sooriya@consensys.net>
2024-04-04 14:37:54 +0100

    refactor: use preinstalled snaps bundle

--- app/scripts/snaps/preinstalled-snaps.ts
+++ app/scripts/snaps/preinstalled-snaps.ts
@@ -1,14 +1,8 @@
 import type { PreinstalledSnap } from '@metamask/snaps-controllers';
-
-// TODO - use the new version of the snap (once released), it should have this file.
-// Interim - build the snap locally (using `yarn build:clean`) and copy/paste this file & update the snap package.json exports to match.
 import MessageSigningSnap from '@metamask/message-signing-snap/dist/preinstalled-snap.json';
 
-const PREINSTALLED_SNAPS = Object.freeze<PreinstalledSnap[]>([
+const PREINSTALLED_SNAPS: readonly PreinstalledSnap[] = Object.freeze([
   MessageSigningSnap as PreinstalledSnap,
 ]);
 
 export default PREINSTALLED_SNAPS;
-
-// TODO - this is for testing purposes
-console.log({ PREINSTALLED_SNAPS });
