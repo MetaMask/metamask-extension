78abe4eb1522ce75c0495aba8886cf34fd0a99ad
Bilal Zahory <bilal.zahory@consensys.net>
2024-04-05 16:41:53 -0400

    feat: add quote card

--- ui/components/multichain/pages/send/components/quote-card/index.tsx
+++ ui/components/multichain/pages/send/components/quote-card/index.tsx
@@ -2,10 +2,11 @@ import React, { useEffect, useMemo, useState } from 'react';
 import { Box, Text } from '../../../../../component-library';
 import {
   AlignItems,
+  BackgroundColor,
+  BlockSize,
+  BorderRadius,
   Display,
   FlexDirection,
-  FontWeight,
-  TextAlign,
   TextColor,
   TextVariant,
 } from '../../../../../../helpers/constants/design-system';
@@ -17,8 +18,14 @@ import {
 } from '../../../../../../ducks/send';
 import { useI18nContext } from '../../../../../../hooks/useI18nContext';
 import { SECOND } from '../../../../../../../shared/constants/time';
+import ExchangeRateDisplay from '../../../../../../pages/swaps/exchange-rate-display';
+import { Quote } from '../../../../../../ducks/send/swap-and-send-utils';
+import { getNativeCurrency } from '../../../../../../ducks/metamask/metamask';
+import { AssetType } from '../../../../../../../shared/constants/transaction';
+import useEthFeeData from './useEthFeeData';
 
 const REFRESH_INTERVAL = 30;
+const NATIVE_CURRENCY_DECIMALS = 18;
 
 /**
  * All the info about the current quote; handles polling and displaying the best quote
@@ -28,11 +35,28 @@ export function QuoteCard() {
   const t = useI18nContext();
   const dispatch = useDispatch();
 
-  const { isSwapQuoteLoading } = useSelector(getCurrentDraftTransaction);
+  const { isSwapQuoteLoading, sendAsset, receiveAsset } = useSelector(
+    getCurrentDraftTransaction,
+  );
+  const nativeCurrencySymbol = useSelector(getNativeCurrency);
 
-  const bestQuote = useSelector(getBestQuote);
+  const bestQuote: Quote | undefined = useSelector(getBestQuote);
 
   const [timeLeft, setTimeLeft] = useState<number | undefined>(undefined);
+
+  const sourceTokenSymbol =
+    sendAsset.type === AssetType.native
+      ? nativeCurrencySymbol
+      : sendAsset.details?.symbol;
+
+  const destinationTokenSymbol =
+    receiveAsset.type === AssetType.native
+      ? nativeCurrencySymbol
+      : receiveAsset.details?.symbol;
+
+  const { formattedEthGasFee, formattedFiatGasFee } = useEthFeeData(
+    bestQuote?.gasParams.maxGas,
+  );
 
   useEffect(() => {
     if (bestQuote) {
@@ -75,10 +99,56 @@ export function QuoteCard() {
         <Text
           color={TextColor.textAlternative}
           variant={TextVariant.bodySm}
-          className="countdown-timer"
+          className="quote-card__fetch-status"
         >
           {infoText}
         </Text>
+      )}
+      {bestQuote && (
+        <Box
+          backgroundColor={BackgroundColor.backgroundAlternative}
+          borderRadius={BorderRadius.LG}
+          width={BlockSize.Full}
+          gap={2}
+          padding={3}
+          className="quote-card__quote-info"
+        >
+          <Box display={Display.Flex} alignItems={AlignItems.stretch}>
+            <Text color={TextColor.textAlternative} marginRight={'auto'}>
+              {t('quoteRate')}
+            </Text>
+            <Text marginLeft={'auto'}>
+              <ExchangeRateDisplay
+                primaryTokenValue={bestQuote.sourceAmount}
+                primaryTokenDecimals={
+                  sendAsset.details?.decimals || NATIVE_CURRENCY_DECIMALS
+                }
+                primaryTokenSymbol={sourceTokenSymbol}
+                secondaryTokenValue={bestQuote.destinationAmount}
+                secondaryTokenDecimals={
+                  receiveAsset.details?.decimals || NATIVE_CURRENCY_DECIMALS
+                }
+                secondaryTokenSymbol={destinationTokenSymbol}
+                boldSymbols={false}
+                showIconForSwappingTokens={false}
+                className={'quote-card__quote-info'}
+              />
+            </Text>
+          </Box>
+          <Box display={Display.Flex} alignItems={AlignItems.stretch}>
+            <Text color={TextColor.textAlternative} marginRight={'auto'}>
+              {t('transactionDetailGasHeading')}
+            </Text>
+            <Box display={Display.Flex} marginLeft={'auto'}>
+              <Text>{formattedEthGasFee}</Text>
+              {formattedFiatGasFee && (
+                <Text color={TextColor.textAlternative} marginLeft={1}>
+                  ≈ {formattedFiatGasFee}
+                </Text>
+              )}
+            </Box>
+          </Box>
+        </Box>
       )}
     </Box>
   );
