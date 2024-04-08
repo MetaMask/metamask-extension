18f6b0a6f83f630a8c939eeac076a557be4450b5
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2024-04-08 17:05:37 -0400

    adding the types needed for request_accounts file

--- app/scripts/lib/rpc-method-middleware/handlers/handlers-helper.ts
+++ app/scripts/lib/rpc-method-middleware/handlers/handlers-helper.ts
@@ -10,10 +10,22 @@ import {
   ProviderConfig,
 } from '@metamask/network-controller';
 import type {
+  CaveatSpecificationConstraint,
+  ExtractCaveats,
+  ExtractPermission,
+  OriginString,
+  PermissionSpecificationConstraint,
   PermissionSubjectMetadata,
+  RequestedPermissions,
+  SubjectPermissions,
   SubjectType,
+  ValidPermission,
 } from '@metamask/permission-controller';
 import type { Hex, Json } from '@metamask/utils';
+import {
+  MetaMetricsEventPayload,
+  MetaMetricsPageOptions,
+} from '../../../../../shared/constants/metametrics';
 
 export type HandlerWrapper = {
   methodNames: [string];
@@ -51,32 +63,73 @@ export type EndApprovalFlow = ({ id }: EndFlowOptions) => void;
 export type FindNetworkConfigurationBy = (
   rpcInfo: Record<string, string>,
 ) => ProviderConfig | null;
-export type HasPermissions = (origin: string) => boolean;
+export type HasPermissions = (origin: OriginString) => boolean;
 export type GetAccounts = () => Promise<string[]>;
 export type GetCurrentChainId = () => Hex;
 export type GetCurrentRpcUrl = () => string | undefined;
+export type GetPermissionsForOrigin<
+  ControllerCaveatSpecification extends CaveatSpecificationConstraint = CaveatSpecificationConstraint,
+> = (
+  origin: OriginString,
+) =>
+  | SubjectPermissions<
+      ValidPermission<string, ExtractCaveats<ControllerCaveatSpecification>>
+    >
+  | undefined;
 export type GetProviderConfig = () => ProviderConfig;
 export type GetProviderState = (
-  origin: string,
+  origin: OriginString,
 ) => Promise<ProviderStateHandlerResult>;
-export type GetWeb3ShimUsageState = (origin: string) => undefined | 1 | 2;
+export type GetUnlockPromise = (
+  shouldShowUnlockRequest: boolean,
+) => Promise<void>;
+export type GetWeb3ShimUsageState = (origin: OriginString) => undefined | 1 | 2;
 export type HandleWatchAssetRequest = (
   options: Record<string, string>,
 ) => Promise<void>;
-export type StartApprovalFlow = (
-  options?: StartFlowOptions,
-) => ApprovalFlowStartResult;
-export type SetNetworkClientIdForDomain = (
-  domain: string,
-  networkClientId: NetworkClientId,
+export type RequestAccountsPermission<
+  ControllerPermissionSpecification extends PermissionSpecificationConstraint = PermissionSpecificationConstraint,
+  ControllerCaveatSpecification extends CaveatSpecificationConstraint = CaveatSpecificationConstraint,
+> = (
+  subject?: PermissionSubjectMetadata,
+  requestedPermissions?: RequestedPermissions,
+  options?: {
+    id?: string;
+    preserveExistingPermissions?: boolean;
+  },
+) => Promise<
+  [
+    SubjectPermissions<
+      ExtractPermission<
+        ControllerPermissionSpecification,
+        ControllerCaveatSpecification
+      >
+    >,
+    {
+      data?: Record<string, unknown>;
+      id: string;
+      origin: OriginString;
+    },
+  ]
+>;
+export type RequestUserApproval = (
+  options?: AddApprovalOptions,
+) => Promise<unknown>;
+export type SendMetrics = (
+  payload: MetaMetricsEventPayload,
+  options?: MetaMetricsPageOptions,
 ) => void;
 export type SetActiveNetwork = (
   networkConfigurationIdOrType: string,
 ) => Promise<void>;
-export type SetWeb3ShimUsageRecorded = (origin: string) => void;
-export type RequestUserApproval = (
-  options?: AddApprovalOptions,
-) => Promise<unknown>;
+export type SetNetworkClientIdForDomain = (
+  domain: string,
+  networkClientId: NetworkClientId,
+) => void;
+export type SetWeb3ShimUsageRecorded = (origin: OriginString) => void;
+export type StartApprovalFlow = (
+  options?: StartFlowOptions,
+) => ApprovalFlowStartResult;
 export type UpsertNetworkConfiguration = (
   networkConfiguration: NetworkConfiguration,
   options?: UpsertNetworkConfigurationOptions,
