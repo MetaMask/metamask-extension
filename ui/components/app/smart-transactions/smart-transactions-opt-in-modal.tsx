348c2b5ad962d4110ea979b0ebae5fe3cc5956c9
dan437 <80175477+dan437@users.noreply.github.com>
2024-04-08 10:34:56 +0200

    Update UI / content for the STX opt-in modal, refactoring

--- ui/components/app/smart-transactions/smart-transactions-opt-in-modal.tsx
+++ ui/components/app/smart-transactions/smart-transactions-opt-in-modal.tsx
@@ -6,11 +6,12 @@ import {
   TextColor,
   Display,
   FlexDirection,
-  FontWeight,
   BlockSize,
   AlignItems,
+  TextAlign,
   JustifyContent,
   TextVariant,
+  IconColor,
 } from '../../../helpers/constants/design-system';
 import {
   Modal,
@@ -21,8 +22,14 @@ import {
   ButtonVariant,
   ModalHeader,
   ModalContent,
+  ButtonLink,
+  ButtonLinkSize,
+  AvatarIcon,
+  IconName,
+  IconSize,
 } from '../../component-library';
 import { setSmartTransactionsOptInStatus } from '../../../store/actions';
+import { SMART_TRANSACTIONS_LEARN_MORE_URL } from '../../../../shared/constants/smartTransactions';
 
 export type SmartTransactionsOptInModalProps = {
   isOpen: boolean;
@@ -34,20 +41,131 @@ export default function SmartTransactionsOptInModal({
   const t = useI18nContext();
   const dispatch = useDispatch();
 
-  const onEnable = () => {
+  const onEnableSmartTransactions = () => {
     dispatch(setSmartTransactionsOptInStatus(true));
   };
   const onNotRightNow = () => {
     dispatch(setSmartTransactionsOptInStatus(false));
   };
 
+  const LearnMoreLink = () => {
+    return (
+      <ButtonLink
+        size={ButtonLinkSize.Inherit}
+        textProps={{
+          variant: TextVariant.bodyMd,
+          alignItems: AlignItems.flexStart,
+        }}
+        as="a"
+        href={SMART_TRANSACTIONS_LEARN_MORE_URL}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {t('learnMoreUpperCaseWithDot')}
+      </ButtonLink>
+    );
+  };
+
+  const EnableSmartTransactionsButton = () => {
+    return (
+      <Button
+        marginTop={8}
+        variant={ButtonVariant.Primary}
+        onClick={onEnableSmartTransactions}
+        width={BlockSize.Full}
+      >
+        {t('enableSmartTransactions')}
+      </Button>
+    );
+  };
+
+  const NotRightNowLink = () => {
+    return (
+      <Button
+        marginTop={2}
+        type="link"
+        variant={ButtonVariant.Link}
+        onClick={onNotRightNow}
+        width={BlockSize.Full}
+      >
+        {t('notRightNow')}
+      </Button>
+    );
+  };
+
+  const Description = () => {
+    return (
+      <Box display={Display.Flex} flexDirection={FlexDirection.Column}>
+        <Text variant={TextVariant.bodyMd} marginTop={4}>
+          {t('smartTransactionsDescription')}
+        </Text>
+        <Text variant={TextVariant.bodyMd} marginTop={4}>
+          {t('smartTransactionsDescription2')}
+        </Text>
+        <Text variant={TextVariant.bodyMd} marginTop={4}>
+          {t('smartTransactionsDescription3', [<LearnMoreLink />])}
+        </Text>
+      </Box>
+    );
+  };
+
+  const Benefit = ({ text, iconName }: { text: string; iconName: string }) => {
+    return (
+      <Box
+        display={Display.Flex}
+        flexDirection={FlexDirection.Column}
+        className="mm-smart-transactions-opt-in-modal__benefit"
+        textAlign={TextAlign.Center}
+        alignItems={AlignItems.center}
+        justifyContent={JustifyContent.flexStart}
+      >
+        <AvatarIcon
+          iconName={iconName}
+          size={IconSize.Md}
+          color={IconColor.primaryDefault}
+        />
+        <Text
+          variant={TextVariant.bodyXs}
+          color={TextColor.textAlternative}
+          marginTop={1}
+        >
+          {text}
+        </Text>
+      </Box>
+    );
+  };
+
+  const Benefits = () => {
+    return (
+      <Box
+        display={Display.Flex}
+        flexDirection={FlexDirection.Row}
+        justifyContent={JustifyContent.center}
+        marginTop={4}
+      >
+        <Benefit
+          text={t('smartTransactionsBenefit1')}
+          iconName={IconName.Confirmation}
+        />
+        <Benefit
+          text={t('smartTransactionsBenefit2')}
+          iconName={IconName.SecurityTick}
+        />
+        <Benefit
+          text={t('smartTransactionsBenefit3')}
+          iconName={IconName.Clock}
+        />
+      </Box>
+    );
+  };
+
   return (
     <Modal
       isOpen={isOpen}
-      onClose={onEnable}
+      onClose={onEnableSmartTransactions}
       isClosedOnOutsideClick={false}
       isClosedOnEscapeKey={false}
-      className="mm-modal__custom-scrollbar"
+      className="mm-modal__custom-scrollbar mm-smart-transactions-opt-in-modal"
       autoFocus={false}
     >
       <ModalOverlay />
@@ -64,43 +182,10 @@ export default function SmartTransactionsOptInModal({
           paddingLeft={4}
           paddingRight={4}
         >
-          <Box display={Display.Flex} flexDirection={FlexDirection.Column}>
-            <img
-              src="./images/logo/metamask-smart-transactions.png"
-              alt={t('swapSwapSwitch')}
-            />
-          </Box>
-          <Text
-            marginTop={2}
-            color={TextColor.textDefault}
-            variant={TextVariant.bodyMd}
-            fontWeight={FontWeight.Bold}
-          >
-            {t('whatAreSmartTransactions')}
-          </Text>
-          <Text variant={TextVariant.bodyMd} marginTop={1}>
-            {t('smartTransactionsDescription')}
-          </Text>
-          <Text variant={TextVariant.bodyMd} marginTop={5}>
-            {t('smartTransactionsDescription2')}
-          </Text>
-          <Button
-            marginTop={6}
-            type="link"
-            variant={ButtonVariant.Link}
-            onClick={onNotRightNow}
-            width={BlockSize.Full}
-          >
-            {t('notRightNow')}
-          </Button>
-          <Button
-            marginTop={4}
-            variant={ButtonVariant.Primary}
-            onClick={onEnable}
-            width={BlockSize.Full}
-          >
-            {t('enable')}
-          </Button>
+          <Benefits />
+          <Description />
+          <EnableSmartTransactionsButton />
+          <NotRightNowLink />
         </Box>
       </ModalContent>
     </Modal>
