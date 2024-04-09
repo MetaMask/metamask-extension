323bdbd5dd172968065e84ba3bc5475e623e287c
Bilal Zahory <bilal.zahory@consensys.net>
2024-04-09 10:40:24 -0400

    refactor: extract helper function

--- ui/ducks/send/send.js
+++ ui/ducks/send/send.js
@@ -129,6 +129,7 @@ import {
   estimateGasLimitForSend,
   generateTransactionParams,
   getRoundedGasPrice,
+  calculateBestQuote,
 } from './helpers';
 
 const RECENT_REQUEST_ERROR =
@@ -2723,14 +2724,7 @@ export const getBestQuote = createSelector(
       return undefined;
     }
 
-    // TODO: account for gas
-    const bestQuote = quotesAsArray.reduce(
-      (best, current) =>
-        current?.destinationAmount > (best?.destinationAmount || 0)
-          ? current
-          : best,
-      undefined,
-    );
+    const bestQuote = calculateBestQuote(quotesAsArray);
 
     return bestQuote;
   },
