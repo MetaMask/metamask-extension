fa73f1bd182949c782440856cf982b4fe6cb8b45
sahar-fehri <sahar.fehri@consensys.net>
2024-04-09 17:00:18 +0200

    fix: refactor test

--- test/e2e/tests/tokens/nft/remove-nft.spec.js
+++ test/e2e/tests/tokens/nft/remove-nft.spec.js
@@ -28,49 +28,13 @@ async function mockedNftRemoved(mockServer) {
 describe('Remove NFT', function () {
   const smartContract = SMART_CONTRACTS.NFTS;
 
-  it('user should be able to remove ERC721 NFT on details page', async function () {
-    await withFixtures(
-      {
-        dapp: true,
-        fixtures: new FixtureBuilder().withNftControllerERC721().build(),
-        ganacheOptions: defaultGanacheOptions,
-        smartContract,
-        title: this.test.fullTitle(),
-      },
-      async ({ driver }) => {
-        await unlockWallet(driver);
-
-        // Open the details and click remove nft button
-        await driver.clickElement('[data-testid="home__nfts-tab"]');
-        await driver.clickElement('.nft-item__container');
-        await driver.clickElement('[data-testid="nft-options__button"]');
-        await driver.clickElement('[data-testid="nft-item-remove"]');
-
-        // Check the remove NFT toaster is displayed
-        const removeNftNotification = await driver.findElement({
-          text: 'NFT was successfully removed!',
-          tag: 'h6',
-        });
-        assert.equal(await removeNftNotification.isDisplayed(), true);
-
-        // Check the imported NFT disappeared in the NFT tab
-        const noNftInfo = await driver.waitForSelector({
-          css: 'h4',
-          text: 'No NFTs yet',
-        });
-        assert.equal(await noNftInfo.isDisplayed(), true);
-      },
-    );
-  });
-
-  it('user should be able to remove ERC721 NFT on details page and get an event', async function () {
+  it('user should be able to remove ERC721 NFT on details page and removeNft event should be emitted', async function () {
     async function mockSegment(mockServer) {
       return [await mockedNftRemoved(mockServer)];
     }
     await withFixtures(
       {
         dapp: true,
-        //  fixtures: new FixtureBuilder().withNftControllerERC721().build(),
         fixtures: new FixtureBuilder()
           .withNftControllerERC721()
           .withMetaMetricsController({
@@ -102,6 +66,13 @@ describe('Remove NFT', function () {
           tag: 'h6',
         });
         assert.equal(await removeNftNotification.isDisplayed(), true);
+
+        // Check the imported NFT disappeared in the NFT tab
+        const noNftInfo = await driver.waitForSelector({
+          css: 'h4',
+          text: 'No NFTs yet',
+        });
+        assert.equal(await noNftInfo.isDisplayed(), true);
 
         // Check if event was emitted
         const events = await getEventPayloads(driver, mockedEndpoints);
