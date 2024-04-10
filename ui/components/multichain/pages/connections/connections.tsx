e94d04c66bef1a16ed8ee2b9ee9ec2536285b6e3
NidhiKJha <menidhikjha@gmail.com>
2024-04-10 17:49:58 +0530

    updated avatar favicon condition for single account disconnect

--- ui/components/multichain/pages/connections/connections.tsx
+++ ui/components/multichain/pages/connections/connections.tsx
@@ -334,9 +334,9 @@ export const Connections = () => {
                 onClose={() => setShowAccountDisconnectedToast('')}
                 startAdornment={
                   <AvatarFavicon
-                    name={connectedSubjectsMetadata.name}
+                    name={connectedSiteMetadata.name}
                     size={AvatarFaviconSize.Sm}
-                    src={connectedSubjectsMetadata.iconUrl}
+                    src={connectedSiteMetadata.iconUrl}
                   />
                 }
               />
