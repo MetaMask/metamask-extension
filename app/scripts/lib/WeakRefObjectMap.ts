6ab32641fc8708824d0891389600da868ae3a152
Alex <adonesky@gmail.com>
2024-04-11 10:02:44 -0500

    WeakRefObjectMap implements Map<string, object>

--- app/scripts/lib/WeakRefObjectMap.ts
+++ app/scripts/lib/WeakRefObjectMap.ts
@@ -2,21 +2,21 @@ type WeakRefCompatibleObject<T> = {
   [P in keyof T]: T[P] extends object ? WeakRef<T[P]> : never;
 };
 
-export class WeakRefObjectMap {
+export class WeakRefObjectMap implements Map<string, object> {
   map: Map<string, WeakRefCompatibleObject<Record<string, object>>>;
 
   constructor() {
     this.map = new Map();
   }
 
-  set(key: string, value: Record<string, object>) {
-    const weakReffedValueObj = {} as WeakRefCompatibleObject<
-      Record<string, object>
-    >;
+  set(key: string, value: Record<string, object>): this {
+    const weakReffedValueObj: WeakRefCompatibleObject<Record<string, object>> =
+      {};
     Object.keys(value).forEach((valueKey) => {
       weakReffedValueObj[valueKey] = new WeakRef(value[valueKey]);
     });
     this.map.set(key, weakReffedValueObj);
+    return this;
   }
 
   get(key: string) {
@@ -25,17 +25,17 @@ export class WeakRefObjectMap {
       return undefined;
     }
 
-    const deReffedValue = {} as { [dereffedValueObjKey: string]: WeakKey };
+    const dereffedValue: { [dereffedValueObjKey: string]: object } = {};
     for (const weakReffedValueKey of Object.keys(weakReffedValue)) {
       const deref = weakReffedValue[weakReffedValueKey].deref();
       if (deref === undefined) {
         this.map.delete(weakReffedValueKey);
         return undefined;
       }
-      deReffedValue[weakReffedValueKey] = deref;
+      dereffedValue[weakReffedValueKey] = deref;
     }
 
-    return deReffedValue;
+    return dereffedValue;
   }
 
   has(key: string) {
@@ -52,6 +52,44 @@ export class WeakRefObjectMap {
 
   clear() {
     this.map.clear();
+  }
+
+  get size(): number {
+    return this.map.size;
+  }
+
+  entries(): IterableIterator<[string, object]> {
+    const entries: Array<[string, object]> = [];
+    this.map.forEach((value, key) => {
+      const derefValue = this.get(key);
+      if (derefValue !== undefined) {
+        entries.push([key, derefValue]);
+      }
+    });
+    return entries.values();
+  }
+
+  keys(): IterableIterator<string> {
+    return this.map.keys();
+  }
+
+  values(): IterableIterator<object> {
+    const values: Array<object> = [];
+    this.map.forEach((_, key) => {
+      const derefValue = this.get(key);
+      if (derefValue !== undefined) {
+        values.push(derefValue);
+      }
+    });
+    return values.values();
+  }
+
+  [Symbol.iterator](): IterableIterator<[string, object]> {
+    return this.entries();
+  }
+
+  get [Symbol.toStringTag](): string {
+    return "WeakRefObjectMap";
   }
 
   forEach(
