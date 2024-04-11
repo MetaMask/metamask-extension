b038d113828de7b8efd91f4efebc59c837eacb9f
Jiexi Luan <jiexiluan@gmail.com>
2024-04-11 14:35:44 -0700

    make networkClientId default to getSelectedNetworkClientId in useCurrencyRatePolling

--- ui/contexts/currencyRate.js
+++ ui/contexts/currencyRate.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
-import { getSelectedNetworkClientId } from '../selectors';
 import useCurrencyRatePolling from '../hooks/useCurrencyRatePolling';
 
 export const CurrencyRateProvider = ({ children }) => {
-  const selectedNetworkClientId = useSelector(getSelectedNetworkClientId);
-
-  useCurrencyRatePolling(selectedNetworkClientId);
+  useCurrencyRatePolling();
 
   return <>{children}</>;
 };
--- ui/hooks/useCurrencyRatePolling.ts
+++ ui/hooks/useCurrencyRatePolling.ts
@@ -1,5 +1,8 @@
 import { useSelector } from 'react-redux';
-import { getUseCurrencyRateCheck } from '../selectors';
+import {
+  getSelectedNetworkClientId,
+  getUseCurrencyRateCheck,
+} from '../selectors';
 import {
   currencyRateStartPollingByNetworkClientId,
   currencyRateStopPollingByPollingToken,
@@ -7,14 +10,15 @@ import {
 import { getCompletedOnboarding } from '../ducks/metamask/metamask';
 import usePolling from './usePolling';
 
-const useCurrencyRatePolling = (networkClientId: string) => {
+const useCurrencyRatePolling = (networkClientId?: string) => {
   const useCurrencyRateCheck = useSelector(getUseCurrencyRateCheck);
   const completedOnboarding = useSelector(getCompletedOnboarding);
+  const selectedNetworkClientId = useSelector(getSelectedNetworkClientId);
 
   usePolling({
     startPollingByNetworkClientId: currencyRateStartPollingByNetworkClientId,
     stopPollingByPollingToken: currencyRateStopPollingByPollingToken,
-    networkClientId,
+    networkClientId: networkClientId ?? selectedNetworkClientId,
     enabled: useCurrencyRateCheck && completedOnboarding,
   });
 };
