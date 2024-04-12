b3aff16b4a957979550b793202f71c42bca5e390
David Walsh <davidwalsh83@gmail.com>
2024-04-12 11:57:38 -0500

    Remove confirmation of sendScreenToConfirmScreen to avoid PPOM issues

--- test/e2e/helpers.js
+++ test/e2e/helpers.js
@@ -794,11 +794,6 @@ const sendScreenToConfirmScreen = async (
     );
 
   await driver.clickElement({ text: 'Continue', tag: 'button' });
-
-  await driver.clickElement({
-    text: 'Continue',
-    tag: 'button',
-  });
 };
 
 const sendTransaction = async (
