0f3582ccd0aa364a14661d85591bf63f0bd4efb2
Jiexi Luan <jiexiluan@gmail.com>
2024-04-15 16:19:58 -0700

    initial dummy provider-authorize

--- app/scripts/lib/rpc-method-middleware/handlers/caip-25.ts
+++ app/scripts/lib/rpc-method-middleware/handlers/caip-25.ts
@@ -1,10 +1,15 @@
-import { CaipChainId, isCaipChainId, isCaipNamespace, parseCaipChainId } from "@metamask/utils"
+import {
+  CaipChainId,
+  isCaipChainId,
+  isCaipNamespace,
+  parseCaipChainId,
+} from '@metamask/utils';
 
 // {scopeString} (conditional) = EITHER a namespace identifier string registered in the CASA namespaces registry to authorize multiple chains with identical properties OR a single, valid [CAIP-2][] identifier, i.e., a specific chain_id within a namespace.
 // scopes (conditional) = An array of 0 or more [CAIP-2][] chainIds. For each entry in scopes, all the other properties of the scopeObject apply, but in some cases, such as when members of accounts are specific to 1 or more chains in scopes, they may be ignored or filtered where inapplicable; namespace-specific rules for organizing or interpreting properties in multi-scope MAY be specified in a namespace-specific profile of this specification.
-  // This property MUST NOT be present if the object is already scoped to a single chainId in the string value above.
-  // This property MUST NOT be present if the scope is an entire namespace in which chainIds are not defined.
-  // This property MAY be present if the scope is an entire namespace in which chainIds are defined.
+//  This property MUST NOT be present if the object is already scoped to a single chainId in the string value above.
+//  This property MUST NOT be present if the scope is an entire namespace in which chainIds are not defined.
+//  This property MAY be present if the scope is an entire namespace in which chainIds are defined.
 // methods = An array of 0 or more JSON-RPC methods that an application can call on the agent and/or an agent can call on an application.
 // notifications = An array of 0 or more JSON-RPC notifications that an application send to or expect from the agent.
 // accounts (optional) = An array of 0 or more CAIP-10 identifiers, each valid within the scope of authorization.
@@ -19,63 +24,78 @@ import { CaipChainId, isCaipChainId, isCaipNamespace, parseCaipChainId } from "@
 //       "notifications": ["accountsChanged", "chainChanged"]
 //     },
 
-export interface ScopeObject {
-  scopes?: CaipChainId[] // CaipChainId[]
-  methods: string[]
-  notifications: string[]
-  accounts?: string[] //CaipAccountId
-  rpcDocuments?: string[]
-  rpcEndpoints?: string[]
-}
+export type ScopeObject = {
+  scopes?: CaipChainId[]; // CaipChainId[]
+  methods: string[];
+  notifications: string[];
+  accounts?: string[]; // CaipAccountId
+  rpcDocuments?: string[];
+  rpcEndpoints?: string[];
+};
 
 // Make this an assert
-export const isValidScopeObject = (scopeString: string, scopeObject: ScopeObject): boolean => {
-  const isNamespaceScoped = isCaipNamespace(scopeString)
-  const isChainScoped = isCaipChainId(scopeString)
+export const isValidScope = (
+  scopeString: string,
+  scopeObject: ScopeObject,
+): boolean => {
+  const isNamespaceScoped = isCaipNamespace(scopeString);
+  const isChainScoped = isCaipChainId(scopeString);
 
-  if(!isNamespaceScoped && !isChainScoped) {
-    return false
+  if (!isNamespaceScoped && !isChainScoped) {
+    return false;
   }
 
-  const {scopes, methods, notifications, accounts, rpcDocuments, rpcEndpoints, ...restScopeObject} = scopeObject
+  const {
+    scopes,
+    methods,
+    notifications,
+    accounts,
+    rpcDocuments,
+    rpcEndpoints,
+    ...restScopeObject
+  } = scopeObject;
 
   // These assume that the namespace has a notion of chainIds
-  if(isChainScoped && scopes) {
-    return false
+  if (isChainScoped && scopes) {
+    return false;
   }
-  if(isNamespaceScoped && scopes) {
-    const namespace = scopeString
+  if (isNamespaceScoped && scopes) {
+    const namespace = scopeString;
     const areScopesValid = scopes.every((scope) => {
       try {
-        return parseCaipChainId(scope).namespace === namespace
+        return parseCaipChainId(scope).namespace === namespace;
       } catch (e) {
         // parsing caipChainId failed
-        console.log(e)
-        return false
+        console.log(e);
+        return false;
       }
-    })
+    });
 
     if (!areScopesValid) {
-      return false
+      return false;
     }
   }
 
-  const areMethodsValid = methods.every((method) => typeof method === 'string' && method !== '')
+  const areMethodsValid = methods.every(
+    (method) => typeof method === 'string' && method !== '',
+  );
   if (!areMethodsValid) {
-    return false
+    return false;
   }
 
-  const areNotificationsValid = notifications.every((notification) => typeof notification === 'string' && notification !== '')
+  const areNotificationsValid = notifications.every(
+    (notification) => typeof notification === 'string' && notification !== '',
+  );
   if (!areNotificationsValid) {
-    return false
+    return false;
   }
 
   // not validating rpcDocuments or rpcEndpoints currently
 
   // unexpected properties found on scopeObject
   if (Object.keys(restScopeObject)) {
-    return false
+    return false;
   }
 
-  return true
-}
+  return true;
+};
--- app/scripts/lib/rpc-method-middleware/handlers/provider-authorize.js
+++ app/scripts/lib/rpc-method-middleware/handlers/provider-authorize.js
@@ -1,4 +1,5 @@
 import { MESSAGE_TYPE } from '../../../../../shared/constants/app';
+import { isValidScope } from './caip-25';
 
 // {
 //   "requiredScopes": {
@@ -39,37 +40,63 @@ const providerAuthorize = {
 };
 export default providerAuthorize;
 
+async function providerAuthorizeHandler(_req, res, _next, end, _hooks) {
+  const { requiredScopes, optionalScopes, sessionProperties } = _req.params;
 
-async function providerAuthorizeHandler(_req, res, _next, end, { getAccounts }) {
-  const {requiredScopes, optionalScopes, sessionProperties} = _req.params;
-  res.result = {
-    "sessionId": "0xdeadbeef",
-    "sessionScopes": {
-      "eip155": {
-        "chains": ["eip155:1", "eip155:137"],
-        "methods": ["eth_sendTransaction", "eth_signTransaction", "get_balance", "eth_sign", "personal_sign"],
-        "notifications": ["accountsChanged", "chainChanged"],
-        "accounts": ["eip155:1:0xab16a96d359ec26a11e2c2b3d8f8b8942d5bfcdb", "eip155:137:0xab16a96d359ec26a11e2c2b3d8f8b8942d5bfcdb"]
-      },
-      "eip155:10": {
-        "methods": ["get_balance"],
-        "notifications": ["accountsChanged", "chainChanged"],
-        "accounts:": [],
-      },
-      "eip155:42161": {
-        "methods": ["personal_sign"],
-        "notifications": ["accountsChanged", "chainChanged"],
-        "accounts":["eip155:42161:0x0910e12C68d02B561a34569E1367c9AAb42bd810"]
-      },
-      "wallet": {
-        "methods": ["wallet_getPermissions", "wallet_creds_store", "wallet_creds_verify", "wallet_creds_issue", "wallet_creds_present"],
-        "notifications": []
-      },
-      "cosmos": {}
-    },
-    "sessionProperties": {
-      "expiry": "2022-11-31T17:07:31+00:00"
+  const sessionId = '0xdeadbeef';
+
+  const validRequiredScopes = {};
+  for (const [scopeString, scopeObject] of Object.entries(requiredScopes)) {
+    if (isValidScope(scopeString, scopeObject)) {
+      validRequiredScopes[scopeString] = {
+        accounts: [],
+        ...scopeObject,
+      };
     }
   }
+  if (requiredScopes && Object.keys(validRequiredScopes).length === 0) {
+    throw new Error(
+      '`requiredScopes` object MUST contain 1 more `scopeObjects`, if present',
+    );
+  }
+
+  const validOptionalScopes = {};
+  for (const [scopeString, scopeObject] of Object.entries(optionalScopes)) {
+    if (isValidScope(scopeString, scopeObject)) {
+      validOptionalScopes[scopeString] = {
+        accounts: [],
+        ...scopeObject,
+      };
+    }
+  }
+  if (optionalScopes && Object.keys(validOptionalScopes).length === 0) {
+    throw new Error(
+      '`optionalScopes` object MUST contain 1 more `scopeObjects`, if present',
+    );
+  }
+
+  const randomSessionProperties = {}; // session properties do not have to be honored by the wallet
+  for (const [key, value] of Object.entries(sessionProperties)) {
+    if (Math.random() > 0.5) {
+      randomSessionProperties[key] = value;
+    }
+  }
+  if (sessionProperties && Object.key(sessionProperties).length === 0) {
+    throw new Error(
+      '`sessionProperties` object MUST contain 1 or more properties if present',
+    );
+  }
+
+  res.result = {
+    sessionId,
+    sessionScopes: {
+      // what happens if these keys collide?
+      ...validRequiredScopes,
+      ...validOptionalScopes,
+    },
+    sessionProperties: {
+      expiry: '2022-11-31T17:07:31+00:00',
+    },
+  };
   return end();
 }
