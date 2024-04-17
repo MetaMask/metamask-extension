14c37656e35eaece679b1ea2c1be37095cbffcd6
Monte Lai <monte.lai@consensys.net>
2024-04-17 15:20:42 +0800

    fix: remove unnecessary return

--- ui/components/app/snaps/keyring-snap-removal-warning/keyring-snap-removal-warning.tsx
+++ ui/components/app/snaps/keyring-snap-removal-warning/keyring-snap-removal-warning.tsx
@@ -154,7 +154,6 @@ export default function KeyringRemovalSnapWarning({
                   }}
                   onPaste={(e: React.ClipboardEvent<HTMLInputElement>) => {
                     e.preventDefault();
-                    return false;
                   }}
                   error={error}
                   inputProps={{
