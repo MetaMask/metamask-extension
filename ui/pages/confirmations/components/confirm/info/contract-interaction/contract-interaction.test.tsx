af2cb510efe883fb56b452aa6a078fb5de69fa2a
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2024-04-19 15:33:14 +0100

    Create info component for contract interactions

---
+++ ui/pages/confirmations/components/confirm/info/contract-interaction/contract-interaction.test.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import configureMockStore from 'redux-mock-store';
+import { unapprovedContractInteractionConfirmation } from '../../../../../../../test/data/confirmations/contract-interaction';
+import mockState from '../../../../../../../test/data/mock-state.json';
+import { renderWithProvider } from '../../../../../../../test/lib/render-helpers';
+import ContractInteractionInfo from './contract-interaction';
+
+describe('<ContractInteractionInfo />', () => {
+  it('renders component for contract interaction request', () => {
+    const state = {
+      ...mockState,
+      confirm: {
+        currentConfirmation: unapprovedContractInteractionConfirmation,
+      },
+    };
+    const mockStore = configureMockStore([])(state);
+    const { container } = renderWithProvider(
+      <ContractInteractionInfo />,
+      mockStore,
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  it('does not render if required data is not present in the transaction', () => {
+    const state = {
+      ...mockState,
+      confirm: {
+        currentConfirmation: {
+          id: '0050d5b0-c023-11ee-a0cb-3390a510a0ab',
+          status: 'unapproved',
+          time: new Date().getTime(),
+          type: 'json_request',
+        },
+      },
+    };
+    const mockStore = configureMockStore([])(state);
+    const { container } = renderWithProvider(
+      <ContractInteractionInfo />,
+      mockStore,
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
