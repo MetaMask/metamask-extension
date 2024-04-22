707f3870587a55d9a996e280c37b9974eab04a2a
georgeweiler <georgejweiler@gmail.com>
2024-04-22 08:24:05 -0600

    chore: use set instead of append for url params

--- ui/helpers/ramps/rampApi/rampAPI.ts
+++ ui/helpers/ramps/rampApi/rampAPI.ts
@@ -10,7 +10,7 @@ const rampApiBaseUrl =
 const RampAPI = {
   async getNetworks(): Promise<AggregatorNetwork[]> {
     const url = new URL('/regions/networks', rampApiBaseUrl);
-    url.searchParams.append('context', 'extension');
+    url.searchParams.set('context', 'extension');
     const response = await fetchWithTimeout(url.toString());
 
     const { networks } = await response.json();
