172dffc940032857bcd2bea2fbf44327e38dcc21
sahar-fehri <sahar.fehri@consensys.net>
2024-04-23 14:58:16 +0200

    fix: update e2e test

--- test/e2e/tests/tokens/nft/auto-detect-nft.spec.js
+++ test/e2e/tests/tokens/nft/auto-detect-nft.spec.js
@@ -1,14 +1,58 @@
 const { strict: assert } = require('assert');
 const {
-  defaultGanacheOptions,
-  unlockWallet,
   withFixtures,
+  completeImportSRPOnboardingFlow,
+  WALLET_PASSWORD,
+  TEST_SEED_PHRASE_TWO,
 } = require('../../../helpers');
 const FixtureBuilder = require('../../../fixture-builder');
+const { GANACHE_ACCOUNT } = require('../../../constants');
 const { setupAutoDetectMocking } = require('./mocks');
 
 describe('NFT detection', function () {
-  it('displays NFT media', async function () {
+  it('Should detect nfts after onboarding', async function () {
+    await withFixtures(
+      {
+        fixtures: new FixtureBuilder({ onboarding: true })
+          .withNetworkControllerOnMainnet()
+          .withPreferencesControllerNftDetectionEnabled()
+          .build(),
+        //   driverOptions,
+        //  ganacheOptions: defaultGanacheOptions,
+        title: this.test.fullTitle(),
+        testSpecificMock: async (mockServer) => {
+          return setupAutoDetectMocking(mockServer, GANACHE_ACCOUNT);
+        },
+      },
+      async ({ driver }) => {
+        await driver.navigate();
+
+        await completeImportSRPOnboardingFlow(
+          driver,
+          TEST_SEED_PHRASE_TWO,
+          WALLET_PASSWORD,
+        );
+
+        const homePage = await driver.findElement('.home__main-view');
+        const homePageDisplayed = await homePage.isDisplayed();
+
+        assert.equal(homePageDisplayed, true);
+
+        await driver.clickElement('[data-testid="home__nfts-tab"]');
+        const collection = await driver.findElement(
+          '[data-testid="collection-expander-button"]',
+        );
+        const nftImage = await driver.findElement('[data-testid="nft-item"]');
+
+        assert.equal(
+          await collection.getText(),
+          'ENS: Ethereum Name Service (1)',
+        );
+        assert.equal(await nftImage.isDisplayed(), true);
+      },
+    );
+  });
+  /*   it('displays NFT media', async function () {
     const driverOptions = { mock: true };
     await withFixtures(
       {
@@ -36,5 +80,5 @@ describe('NFT detection', function () {
         assert.equal(await nftImage.isDisplayed(), true);
       },
     );
-  });
+  }); */
 });
