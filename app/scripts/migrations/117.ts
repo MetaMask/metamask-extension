1fa8ffac6708daa94fc5584e2e0edce973e7782e
Monte Lai <monte.lai@consensys.net>
2024-04-23 16:29:38 +0800

    fix: add migration

---
+++ app/scripts/migrations/117.ts
@@ -0,0 +1,56 @@
+import { cloneDeep, isObject } from 'lodash';
+import { hasProperty } from '@metamask/utils';
+import { AccountsControllerState } from '@metamask/accounts-controller';
+import { InternalAccount } from '@metamask/keyring-api';
+
+type VersionedData = {
+  meta: { version: number };
+  data: Record<string, unknown>;
+};
+
+export const version = 117;
+
+/**
+ * As we have removed Product tour from Home Page so this migration is to remove showProductTour from AppState
+ *
+ * @param originalVersionedData
+ */
+export async function migrate(
+  originalVersionedData: VersionedData,
+): Promise<VersionedData> {
+  const versionedData = cloneDeep(originalVersionedData);
+  versionedData.meta.version = version;
+  transformState(versionedData.data);
+  return versionedData;
+}
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function transformState(state: Record<string, any>) {
+  const AccountsController = state?.AccountsController || {};
+
+  if (
+    hasProperty(state, 'AccountsController') &&
+    isObject(state.AccountsController) &&
+    hasProperty(state.AccountsController, 'internalAccounts') &&
+    hasProperty(
+      state.AccountsController.internalAccounts as Object,
+      'accounts',
+    ) &&
+    Object.values(
+      (state.AccountsController as AccountsControllerState).internalAccounts
+        .accounts,
+    ).length > 0
+  ) {
+    Object.values(AccountsController.internalAccounts.accounts).forEach(
+      (internalAccount: InternalAccount) => {
+        if (!internalAccount.metadata?.importTime) {
+          internalAccount.metadata.importTime = Date.now();
+        }
+      },
+    );
+  }
+
+  return {
+    ...state,
+    AccountsController,
+  };
+}
