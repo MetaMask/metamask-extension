f49cd66e604b305e18805cb5dd5e131990e59e07
sahar-fehri <sahar.fehri@consensys.net>
2024-04-23 20:05:02 +0200

    fix: fix e2e test

--- test/e2e/tests/tokens/nft/auto-detect-nft.spec.js
+++ test/e2e/tests/tokens/nft/auto-detect-nft.spec.js
@@ -1,72 +1,21 @@
 const { strict: assert } = require('assert');
 const {
   withFixtures,
-  completeImportSRPOnboardingFlow,
-  WALLET_PASSWORD,
-  TEST_SEED_PHRASE_TWO,
+  defaultGanacheOptions,
+  unlockWallet,
 } = require('../../../helpers');
 const FixtureBuilder = require('../../../fixture-builder');
-const { GANACHE_ACCOUNT } = require('../../../constants');
 const { setupAutoDetectMocking } = require('./mocks');
 
 describe('NFT detection', function () {
-  it('Should detect nfts after onboarding', async function () {
-    await withFixtures(
-      {
-        fixtures: new FixtureBuilder({ onboarding: true })
-          .withNetworkControllerOnMainnet()
-          .withPreferencesControllerNftDetectionEnabled()
-          .build(),
-        //   driverOptions,
-        //  ganacheOptions: defaultGanacheOptions,
-        title: this.test.fullTitle(),
-        testSpecificMock: async (mockServer) => {
-          return setupAutoDetectMocking(mockServer, GANACHE_ACCOUNT);
-        },
-      },
-      async ({ driver }) => {
-        await driver.navigate();
-
-        await completeImportSRPOnboardingFlow(
-          driver,
-          TEST_SEED_PHRASE_TWO,
-          WALLET_PASSWORD,
-        );
-
-        const homePageDisplayed = await (
-          await driver.findElement('.home__main-view')
-        ).isDisplayed();
-
-        assert.equal(homePageDisplayed, true);
-
-        await driver.clickElement('[data-testid="home__nfts-tab"]');
-        await driver.delay(1000);
-
-        assert.equal(
-          await (
-            await driver.findElement(
-              '[data-testid="collection-expander-button"]',
-            )
-          ).getText(),
-          'ENS: Ethereum Name Service (1)',
-        );
-        assert.equal(
-          await (
-            await driver.findElement('[data-testid="nft-item"]')
-          ).isDisplayed(),
-          true,
-        );
-      },
-    );
-  });
-  /*   it('displays NFT media', async function () {
+  /**
+   * TODO Revisit this test once we enable nft auto detection by default. Use .withPreferencesControllerNftDetectionEnabled().
+   */
+  it('displays NFT media', async function () {
     const driverOptions = { mock: true };
     await withFixtures(
       {
-        fixtures: new FixtureBuilder()
-          .withNetworkControllerOnMainnet()
-          .withPreferencesControllerNftDetectionEnabled()
-          .build(),
+        fixtures: new FixtureBuilder().withNetworkControllerOnMainnet().build(),
         driverOptions,
         ganacheOptions: defaultGanacheOptions,
         title: this.test.fullTitle(),
@@ -74,6 +23,21 @@ describe('NFT detection', function () {
       },
       async ({ driver }) => {
         await unlockWallet(driver);
+
+        // go to settings
+        await driver.clickElement(
+          '[data-testid="account-options-menu-button"]',
+        );
+        await driver.clickElement({ text: 'Settings', tag: 'div' });
+        await driver.clickElement({ text: 'Security & privacy', tag: 'div' });
+        await driver.clickElement(
+          '[data-testid="useNftDetection"] .toggle-button > div',
+        );
+        await driver.clickElement(
+          '.settings-page__header__title-container__close-button',
+        );
+        await driver.clickElement('[data-testid="home__asset-tab"]');
+
         await driver.clickElement('[data-testid="home__nfts-tab"]');
         await driver.delay(1000);
         const collection = await driver.findElement(
@@ -87,5 +51,5 @@ describe('NFT detection', function () {
         assert.equal(await nftImage.isDisplayed(), true);
       },
     );
-  }); */
+  });
 });
