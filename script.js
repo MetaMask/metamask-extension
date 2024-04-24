2444f919680801b3c078ba8b8754dc7d7f426314
georgewrmarshall <george.marshall@consensys.net>
2024-04-23 18:43:09 -0700

    chore: script update

--- script.js
+++ script.js
@@ -2,20 +2,39 @@ const fs = require('fs');
 const path = require('path');
 const glob = require('glob');
 
-const getStoryName = (id) => {
-  const parts = id.split('--');
-  return parts[1];
+const updateImports = (content) => {
+  // Replace the Storybook docs imports
+  content = content.replace(
+    /import { Story, Canvas, ArgTypes } from '@storybook\/addon-docs';/g,
+    "import { Controls, Canvas } from '@storybook/addon-docs';",
+  );
+
+  // Find component imports and convert to Stories import
+  content = content.replace(
+    /import { ([\w, ]+) } from '(\.\/[\w-]+)';/g,
+    (match, components, path) => {
+      // Create a modified import statement for the stories
+      const storiesPath = `${path}.stories`;
+      return `import * as ${components.trim()}Stories from '${storiesPath}';`;
+    },
+  );
+
+  return content;
 };
 
-const updateMDXContent = (content) => {
+const updateStoryReferences = (content) => {
   const storyRegex = /<Canvas>\s*<Story id="([\w-]+)" \/\>\s*<\/Canvas>/g;
   return content.replace(storyRegex, (_, id) => {
-    const storyName = getStoryName(id);
-    const storyImportName =
-      storyName.charAt(0).toUpperCase() + storyName.slice(1) + 'Story';
-    return `<Canvas of={ModalStories.${storyImportName}} />`;
+    const parts = id.split('--');
+    const storyComponent =
+      parts[0].split('-').map(capitalize).join('') + 'Stories';
+    const storyName =
+      parts[1].charAt(0).toUpperCase() + parts[1].slice(1) + 'Story';
+    return `<Canvas of={${storyComponent}.${storyName}} />`;
   });
 };
+
+const capitalize = (s) => s.charAt(0).toUpperCase() + s.slice(1);
 
 const processMDXFiles = (pattern) => {
   glob(pattern, (err, files) => {
@@ -31,7 +50,9 @@ const processMDXFiles = (pattern) => {
           return;
         }
 
-        const updatedContent = updateMDXContent(data);
+        let updatedContent = updateImports(data);
+        updatedContent = updateStoryReferences(updatedContent);
+
         fs.writeFile(file, updatedContent, 'utf8', (err) => {
           if (err) {
             console.error(`Error writing file ${file}:`, err);
@@ -45,10 +66,5 @@ const processMDXFiles = (pattern) => {
 };
 
 // Update the directory path as needed
-const mdxFilesPattern = path.join(
-  __dirname,
-  'ui/components/component-library',
-  '**',
-  '*.mdx',
-);
+const mdxFilesPattern = path.join(__dirname, 'ui', '**', '*.mdx');
 processMDXFiles(mdxFilesPattern);
