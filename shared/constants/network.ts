a7406103bf05624e05c528ec251323faa699cdf1
Prithpal Sooriya <prithpal.sooriya@consensys.net>
2024-04-24 15:32:02 +0100

    refactor: revert file changes that are not needed

--- shared/constants/network.ts
+++ shared/constants/network.ts
@@ -306,7 +306,7 @@ export const CURRENCY_SYMBOLS = {
   ONE: 'ONE',
 } as const;
 
-export const CHAINLIST_CURRENCY_SYMBOLS_MAP = {
+const CHAINLIST_CURRENCY_SYMBOLS_MAP = {
   ...CURRENCY_SYMBOLS,
   BASE: 'ETH',
   LINEA_MAINNET: 'ETH',
