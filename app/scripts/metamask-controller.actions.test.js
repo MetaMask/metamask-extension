67f3a7d04d56f90ef497fcba261e10ca7eface59
Chloe Gao <chloe.gao@consensys.net>
2024-04-25 13:56:05 +0200

    ganache path

--- app/scripts/metamask-controller.actions.test.js
+++ app/scripts/metamask-controller.actions.test.js
@@ -14,7 +14,7 @@ import { PermissionsRequestNotFoundError } from '@metamask/permission-controller
 import nock from 'nock';
 import mockEncryptor from '../../test/lib/mock-encryptor';
 
-const { Ganache } = require('../../test/e2e/ganache');
+const { Ganache } = require('../../test/e2e/seeder/ganache');
 
 const ganacheServer = new Ganache();
 
