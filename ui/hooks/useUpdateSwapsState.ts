f9347ffa2c93df12ca0a83899fdb4df7eb8bb709
Bilal Zahory <bilal.zahory@consensys.net>
2024-04-29 13:25:11 -0400

    refactor: extract hook

---
+++ ui/hooks/useUpdateSwapsState.ts
@@ -0,0 +1,52 @@
+import { useEffect } from 'react';
+import {
+  fetchAggregatorMetadata,
+  fetchTokens,
+  fetchTopAssets,
+} from '../pages/swaps/swaps.util';
+import {
+  fetchAndSetSwapsGasPriceInfo,
+  prepareToLeaveSwaps,
+  setAggregatorMetadata,
+  setTopAssets,
+} from '../ducks/swaps/swaps';
+import { setSwapsTokens } from '../store/actions';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  checkNetworkAndAccountSupports1559,
+  getCurrentChainId,
+  getIsSwapsChain,
+} from '../selectors';
+
+export default function useUpdateSwapsState() {
+  const dispatch = useDispatch();
+
+  const chainId = useSelector(getCurrentChainId);
+  const isSwapsChain = useSelector(getIsSwapsChain);
+  const networkAndAccountSupports1559 = useSelector(
+    checkNetworkAndAccountSupports1559,
+  );
+
+  useEffect(() => {
+    if (!isSwapsChain) {
+      return undefined;
+    }
+    fetchTokens(chainId)
+      .then((tokens) => {
+        dispatch(setSwapsTokens(tokens));
+      })
+      .catch((error) => console.error(error));
+    fetchTopAssets(chainId).then((topAssets) => {
+      dispatch(setTopAssets(topAssets));
+    });
+    fetchAggregatorMetadata(chainId).then((newAggregatorMetadata) => {
+      dispatch(setAggregatorMetadata(newAggregatorMetadata));
+    });
+    if (!networkAndAccountSupports1559) {
+      dispatch(fetchAndSetSwapsGasPriceInfo());
+    }
+    return () => {
+      dispatch(prepareToLeaveSwaps());
+    };
+  }, [dispatch, chainId, networkAndAccountSupports1559, isSwapsChain]);
+}
