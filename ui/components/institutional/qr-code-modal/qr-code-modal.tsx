2e65fbe5078476fb5d225cd35f7bcb2a8163a90b
Albert Olive <albertolivecorbella@gmail.com>
2024-05-02 10:31:24 +0200

    fix lint

--- ui/components/institutional/qr-code-modal/qr-code-modal.tsx
+++ ui/components/institutional/qr-code-modal/qr-code-modal.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-// @ts-expect-error
+// @ts-expect-error: No available type definitions for module 'qrcode.react'
 import QRCode from 'qrcode.react';
 import { v4 as uuid } from 'uuid';
 import { captureException } from '@sentry/browser';
