30cb388e63f7532fa328b9ef9430d45c140d4b8d
NidhiKJha <menidhikjha@gmail.com>
2024-05-02 16:56:32 +0530

    updated provider api spec

--- test/e2e/tests/dapp-interactions/provider-api.spec.js
+++ test/e2e/tests/dapp-interactions/provider-api.spec.js
@@ -9,47 +9,6 @@ const {
 const FixtureBuilder = require('../../fixture-builder');
 
 describe('MetaMask', function () {
-  it('provider should inform dapp when switching networks', async function () {
-    await withFixtures(
-      {
-        dapp: true,
-        fixtures: new FixtureBuilder()
-          .withPermissionControllerConnectedToTestDapp()
-          .build(),
-        ganacheOptions: defaultGanacheOptions,
-        title: this.test.fullTitle(),
-      },
-      async ({ driver, ganacheServer }) => {
-        const addresses = await ganacheServer.getAccounts();
-        const publicAddress = addresses[0];
-        await unlockWallet(driver);
-
-        await openDapp(driver);
-        const chainIdDiv = await driver.waitForSelector({
-          css: '#chainId',
-          text: '0x539',
-        });
-        assert.equal(await chainIdDiv.getText(), '0x539');
-
-        const windowHandles = await driver.getAllWindowHandles();
-        await driver.switchToWindow(windowHandles[0]);
-
-        await driver.clickElement('[data-testid="network-display"]');
-        await driver.clickElement({ text: 'Ethereum Mainnet', tag: 'p' });
-
-        await driver.switchToWindowWithTitle('E2E Test Dapp', windowHandles);
-        const switchedChainIdDiv = await driver.waitForSelector({
-          css: '#chainId',
-          text: '0x1',
-        });
-        const accountsDiv = await driver.findElement('#accounts');
-
-        assert.equal(await switchedChainIdDiv.getText(), '0x1');
-        assert.equal(await accountsDiv.getText(), publicAddress);
-      },
-    );
-  });
-
   it('should reject unsupported methods', async function () {
     await withFixtures(
       {
