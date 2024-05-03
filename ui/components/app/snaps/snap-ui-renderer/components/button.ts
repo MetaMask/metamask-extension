c326fca8f72f473b0d6a61002516363d9fb43de8
Frederik Bolding <frederik.bolding@gmail.com>
2024-05-03 13:30:08 +0200

    Set button variant to primary for now

--- ui/components/app/snaps/snap-ui-renderer/components/button.ts
+++ ui/components/app/snaps/snap-ui-renderer/components/button.ts
@@ -5,7 +5,8 @@ export const button: UIComponentFactory<ButtonElement> = ({ element }) => ({
   element: 'SnapUIButton',
   props: {
     type: element.props.type,
-    variant: element.props.variant,
+    // Temporarily set to a static value
+    variant: 'primary', // TODO: Support the new variants for the buttons
     name: element.props.name,
     disabled: element.props.disabled,
   },
