MetaMask/metamask-extension

commit cc509de3a81edd16cdf153f2be363d50a9909311

fix: currency displayed for user balance in re-designed confirmation header info modal (#24328)

ui/pages/confirmations/hooks/useConfirmationRecipientInfo.ts
@@ -11,6 +11,7 @@ function useConfirmationRecipientInfo() {
   const currentConfirmation = useSelector(
     currentConfirmationSelector,
   ) as Confirmation;
+  const allAccounts = useSelector(accountsWithSendEtherInfoSelector);
 
   const allAccounts = useSelector(accountsWithSendEtherInfoSelector);
 

ui/components/app/confirm/info/row/currency.test.tsx
@@ -9,14 +9,9 @@ import { ConfirmInfoRowCurrency } from './currency';
 // TODO: Replace `any` with type
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const render = (props: Record<string, any> = {}) => {
+  const { metamask } = mockState;
   const store = configureStore({
-    metamask: {
-      ...mockState.metamask,
-      preferences: {
-        ...mockState.metamask.preferences,
-        useNativeCurrencyAsPrimaryCurrency: false,
-      },
-    },
+    metamask,
   });
 
   return renderWithProvider(

ui/components/app/confirm/info/row/currency.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { PRIMARY } from '../../../../../helpers/constants/common';
 import {
   AlignItems,
   Display,
@@ -37,7 +38,7 @@ export const ConfirmInfoRowCurrency = ({
     {currency ? (
       <CurrencyDisplay currency={currency} value={`${value}`} />
     ) : (
-      <UserPreferencedCurrencyDisplay value={`${value}`} />
+      <UserPreferencedCurrencyDisplay type={PRIMARY} value={`${value}`} />
     )}
   </Box>
 );

commit dbdde78b35bae92bdd49c0d060b2f25fb28c5c3d

fix(syntax): remove double-declared variable
Fixes regression introduced in #24328
(cc509de3a81edd16cdf153f2be363d50a9909311)

ui/pages/confirmations/hooks/useConfirmationRecipientInfo.ts
@@ -11,7 +11,6 @@ function useConfirmationRecipientInfo() {
   const currentConfirmation = useSelector(
     currentConfirmationSelector,
   ) as Confirmation;
-  const allAccounts = useSelector(accountsWithSendEtherInfoSelector);
 
   const allAccounts = useSelector(accountsWithSendEtherInfoSelector);
 