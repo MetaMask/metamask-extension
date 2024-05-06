844725e67c5bf61b8f750e35e2231f40d2bec14e
Bilal Zahory <bilal.zahory@consensys.net>
2024-05-06 01:54:02 -0400

    fix: only reset gas limit if no swap

--- ui/ducks/send/send.js
+++ ui/ducks/send/send.js
@@ -2131,9 +2131,6 @@ export function updateSendQuote(
 
     if (isComputingSendGasLimit) {
       await dispatch(computeEstimatedGasLimit());
-      await dispatch({
-        type: CLEAR_SWAP_AND_SEND_STATE,
-      });
     }
   };
 }
