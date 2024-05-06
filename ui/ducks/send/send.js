0283fa624411533f4f8d774e64fd81918eb7db3b
Bilal Zahory <bilal.zahory@consensys.net>
2024-05-06 10:56:33 -0400

    refactor: reorder switch cases

--- ui/ducks/send/send.js
+++ ui/ducks/send/send.js
@@ -1563,44 +1563,6 @@ const slice = createSlice({
 
       if (draftTransaction) {
         switch (true) {
-          case Boolean(
-            bestQuote &&
-              bestQuote.recipient !== draftTransaction.recipient.address,
-          ):
-            slice.caseReducers.addHistoryEntry(state, {
-              payload: `Recipient is not match ${draftTransaction.recipient.address} ${bestQuote.recipient}`,
-            });
-            draftTransaction.status = SEND_STATUSES.INVALID;
-            break;
-          case Boolean(
-            bestQuote && bestQuote.trade.from !== state.selectedAccount.address,
-          ):
-            slice.caseReducers.addHistoryEntry(state, {
-              payload: `Sender is not match ${state.selectedAccount.address} ${bestQuote.trade.from}`,
-            });
-            draftTransaction.status = SEND_STATUSES.INVALID;
-            break;
-          case Boolean(
-            bestQuote &&
-              (draftTransaction.sendAsset?.details?.address ||
-                zeroAddress()) !== bestQuote.sourceToken,
-          ):
-            slice.caseReducers.addHistoryEntry(state, {
-              payload: `Source token is not match ${draftTransaction.sendAsset?.details?.address} ${bestQuote.sourceToken}`,
-            });
-            draftTransaction.status = SEND_STATUSES.INVALID;
-            break;
-          case Boolean(
-            bestQuote &&
-              bestQuote.destinationToken !==
-                (draftTransaction.receiveAsset?.details?.address ||
-                  zeroAddress()),
-          ):
-            slice.caseReducers.addHistoryEntry(state, {
-              payload: `Destination token is not match ${draftTransaction.receiveAsset?.details?.address} ${bestQuote.destinationToken}`,
-            });
-            draftTransaction.status = SEND_STATUSES.INVALID;
-            break;
           case Boolean(draftTransaction.amount.error):
             slice.caseReducers.addHistoryEntry(state, {
               payload: `Amount is in error ${draftTransaction.amount.error}`,
@@ -1677,6 +1639,50 @@ const slice = createSlice({
             draftTransaction.recipient.recipientWarningAcknowledged === false:
             slice.caseReducers.addHistoryEntry(state, {
               payload: `Form is invalid because recipient warning not acknolwedged`,
+            });
+            draftTransaction.status = SEND_STATUSES.INVALID;
+            break;
+          case Boolean(
+            bestQuote &&
+              bestQuote.recipient !== draftTransaction.recipient.address,
+          ):
+            slice.caseReducers.addHistoryEntry(state, {
+              payload: `Recipient is not match ${draftTransaction.recipient.address} ${bestQuote.recipient}`,
+            });
+            draftTransaction.status = SEND_STATUSES.INVALID;
+            break;
+          case Boolean(
+            bestQuote && bestQuote.trade.from !== state.selectedAccount.address,
+          ):
+            slice.caseReducers.addHistoryEntry(state, {
+              payload: `Sender is not match ${state.selectedAccount.address} ${bestQuote.trade.from}`,
+            });
+            draftTransaction.status = SEND_STATUSES.INVALID;
+            break;
+          case Boolean(
+            bestQuote &&
+              (draftTransaction.sendAsset?.details?.address ||
+                zeroAddress()) !== bestQuote.sourceToken,
+          ):
+            slice.caseReducers.addHistoryEntry(state, {
+              payload: `Source token is not match ${draftTransaction.sendAsset?.details?.address} ${bestQuote.sourceToken}`,
+            });
+            draftTransaction.status = SEND_STATUSES.INVALID;
+            break;
+          case Boolean(
+            bestQuote &&
+              bestQuote.destinationToken !==
+                (draftTransaction.receiveAsset?.details?.address ||
+                  zeroAddress()),
+          ):
+            slice.caseReducers.addHistoryEntry(state, {
+              payload: `Destination token is not match ${draftTransaction.receiveAsset?.details?.address} ${bestQuote.destinationToken}`,
+            });
+            draftTransaction.status = SEND_STATUSES.INVALID;
+            break;
+          case draftTransaction.isSwapQuoteLoading:
+            slice.caseReducers.addHistoryEntry(state, {
+              payload: `A swap quote is loading`,
             });
             draftTransaction.status = SEND_STATUSES.INVALID;
             break;
