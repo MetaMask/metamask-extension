84cafd1861b1f86f6c2a2313293ab288de4bebba
Jonathan Bursztyn <jonathan@bursztyn.io>
2024-05-07 13:55:41 +0100

    Fix tests

--- ui/pages/confirmations/confirmation/confirmation.js
+++ ui/pages/confirmations/confirmation/confirmation.js
@@ -461,20 +461,22 @@ export default function ConfirmationPage({
   const handleSubmit = async () => {
     setLoading(true);
 
-    trackEvent({
-      category: MetaMetricsEventCategory.Network,
-      event: MetaMetricsEventName.NavNetworkSwitched,
-      properties: {
-        location: 'Switch Modal',
-        from_network:
-          pendingConfirmation.requestData.fromNetworkConfiguration.chainId,
-        to_network:
-          pendingConfirmation.requestData.toNetworkConfiguration.chainId,
-        referrer: {
-          url: window.location.origin,
+    if (trackEvent) {
+      trackEvent({
+        category: MetaMetricsEventCategory.Network,
+        event: MetaMetricsEventName.NavNetworkSwitched,
+        properties: {
+          location: 'Switch Modal',
+          from_network:
+            pendingConfirmation.requestData.fromNetworkConfiguration.chainId,
+          to_network:
+            pendingConfirmation.requestData.toNetworkConfiguration.chainId,
+          referrer: {
+            url: window.location.origin,
+          },
         },
-      },
-    });
+      });
+    }
 
     if (templateState[pendingConfirmation.id]?.useWarningModal) {
       setShowWarningModal(true);
