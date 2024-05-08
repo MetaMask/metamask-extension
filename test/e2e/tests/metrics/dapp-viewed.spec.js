fffd36a9392f29d24c89bcf3bf4b30e9e1bdb8c0
Victor Thomas <10986371+vthomas13@users.noreply.github.com>
2024-05-08 14:12:02 -0400

    fix: Removing usage of driver.navigate before unlockWallet (#24436)
    The use of driver.navigate() in combination with the use of
    driver.navigate() in unlockWallet helper function was causing the page
    to reload in e2e tests. This is causing issues with filling in the
    password when unlocking the wallet.
    <img width="881" alt="image"
    src="https://github.com/MetaMask/metamask-extension/assets/10986371/db981ad9-4705-46f1-ae80-30456706e952">
    I suspect the test was trying to access DOM elements created before the
    re-rendering.
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    2. What is the improvement/solution?
    -->
    
    [![Open in GitHub
    Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/24436?quickstart=1)
    
    Fixes:
    
    1. Go to this page...
    2.
    3.
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    <!-- [screenshots/recordings] -->
    
    <!-- [screenshots/recordings] -->
    
    - [ ] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- test/e2e/tests/metrics/dapp-viewed.spec.js
+++ test/e2e/tests/metrics/dapp-viewed.spec.js
@@ -83,7 +83,6 @@ describe('Dapp viewed Event @no-mmi', function () {
         testSpecificMock: mockSegment,
       },
       async ({ driver, mockedEndpoint: mockedEndpoints }) => {
-        await driver.navigate();
         await unlockWallet(driver);
 
         await connectToDapp(driver);
@@ -119,7 +118,6 @@ describe('Dapp viewed Event @no-mmi', function () {
         testSpecificMock: mockSegment,
       },
       async ({ driver, mockedEndpoint: mockedEndpoints }) => {
-        await driver.navigate();
         await unlockWallet(driver);
         await waitForAccountRendered(driver);
         await connectToDapp(driver);
@@ -158,7 +156,6 @@ describe('Dapp viewed Event @no-mmi', function () {
         testSpecificMock: mockSegment,
       },
       async ({ driver, mockedEndpoint: mockedEndpoints }) => {
-        await driver.navigate();
         await unlockWallet(driver);
         await waitForAccountRendered(driver);
         await connectToDapp(driver);
@@ -202,7 +199,6 @@ describe('Dapp viewed Event @no-mmi', function () {
         testSpecificMock: mockSegment,
       },
       async ({ driver, mockedEndpoint: mockedEndpoints }) => {
-        await driver.navigate();
         await unlockWallet(driver);
         await waitForAccountRendered(driver);
         await connectToDapp(driver);
@@ -294,7 +290,6 @@ describe('Dapp viewed Event @no-mmi', function () {
         testSpecificMock: mockSegment,
       },
       async ({ driver, mockedEndpoint: mockedEndpoints }) => {
-        await driver.navigate();
         await unlockWallet(driver);
         await waitForAccountRendered(driver);
         await connectToDapp(driver);
