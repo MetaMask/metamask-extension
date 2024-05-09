66c766a8dbacc98ed0cd6854478549c5ce33fad7
Dan Miller <danjm.com@gmail.com>
2024-05-09 06:06:28 -0230

    privacy snapshot and more logging

--- test/e2e/webdriver/chrome.js
+++ test/e2e/webdriver/chrome.js
@@ -35,7 +35,7 @@ class ChromeDriver {
       args.push('--auto-open-devtools-for-tabs');
     }
 
-    if (false && process.env.ENABLE_MV3) {
+    if (process.env.ENABLE_MV3) {
       args.push('--log-level=0');
       args.push('--enable-logging');
       args.push(`--user-data-dir=${process.cwd()}/test-artifacts/chrome`);
