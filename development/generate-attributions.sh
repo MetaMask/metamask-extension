2ecc8f0b8832dd5afc31c8280ed070a5d78a404f
Salah-Eddine Saakoun <salah-eddine.saakoun@consensys.net>
2024-05-09 18:16:46 +0200

    fix: attribution script to use node instead of python

--- development/generate-attributions.sh
+++ development/generate-attributions.sh
@@ -20,9 +20,41 @@ main() {
   local tmp=".yarnrc.tmp.yml"
   cp ".yarnrc.yml" "$tmp"
 
-  # Convert YAML to JSON using Python and then use jq to remove the plugins section
-  yarnrc=$(python -c 'import sys, yaml, json; print(json.dumps(yaml.safe_load(sys.stdin.read())))' <"$tmp" | jq 'del(.plugins)')
-  echo "$yarnrc" >".yarnrc.yml"
+  # Remove a specific plugin from the plugins array using a node script
+  # This is a workaround for the issue where the `allow-scripts` plugin which is defined in devDependencies
+  # is not required for the attribution generation script
+  node -e "const fs = require('fs');
+           const targetPath = '.yarn/plugins/@yarnpkg/plugin-allow-scripts.cjs';
+           const lines = fs.readFileSync('$tmp', 'utf8').split('\n');
+           let inPluginsSection = false;
+           let inTargetPluginBlock = false;
+           const result = [];
+           lines.forEach(line => {
+             if (line.trim() === 'plugins:') {
+               inPluginsSection = true;  // Start of plugins section
+               result.push(line);
+               return;
+             }
+             if (inPluginsSection) {
+               if (line.trim().startsWith('- path:') && line.includes(targetPath)) {
+                 inTargetPluginBlock = true;  // Found the target plugin block, start skipping
+                 return;
+               }
+               if (line.trim().startsWith('- path:')) {
+                 inTargetPluginBlock = false;  // Found a new plugin block, ensure not skipping this
+               }
+               if (line.trim() === '' || !line.startsWith(' ')) {
+                 inPluginsSection = false;  // Likely end of plugins section
+                 inTargetPluginBlock = false;
+               }
+               if (!inTargetPluginBlock) {
+                 result.push(line);  // Add line if not in target plugin block
+               }
+             } else {
+               result.push(line);  // Outside plugins section, always add line
+             }
+           });
+           fs.writeFileSync('.yarnrc.yml', result.join('\n'));"
 
   # Run yarn command
   yarn workspaces focus --production
