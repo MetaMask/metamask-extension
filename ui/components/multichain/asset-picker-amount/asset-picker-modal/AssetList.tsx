192738e201b334354ffd50b2328333ddc33dd4d7
Bilal Zahory <bilal.zahory@consensys.net>
2024-05-09 11:50:17 -0400

    refactor: avoid usage of `useTokensToSeach`

--- ui/components/multichain/asset-picker-amount/asset-picker-modal/AssetList.tsx
+++ ui/components/multichain/asset-picker-amount/asset-picker-modal/AssetList.tsx
@@ -1,10 +1,7 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import classnames from 'classnames';
-import {
-  getNativeCurrencyImage,
-  getSelectedAccountCachedBalance,
-} from '../../../../selectors';
+import { getSelectedAccountCachedBalance } from '../../../../selectors';
 import { getNativeCurrency } from '../../../../ducks/metamask/metamask';
 import { useUserPreferencedCurrency } from '../../../../hooks/useUserPreferencedCurrency';
 import { PRIMARY, SECONDARY } from '../../../../helpers/constants/common';
@@ -22,25 +19,19 @@ import { TokenListItem } from '../..';
 import { Asset, Token } from './types';
 import AssetComponent from './Asset';
 
-const MAX_UNOWNED_TOKENS_RENDERED = 30;
-
 type AssetListProps = {
   handleAssetChange: (token: Token) => void;
   asset: Asset;
   tokenList: Token[];
-  // searchQuery and all attached logic (e.g., filteredTokenList) could be pulled up if appropriate for a future refactor
-  searchQuery: string;
 };
 
 export default function AssetList({
   handleAssetChange,
   asset,
   tokenList,
-  searchQuery,
 }: AssetListProps) {
   const selectedToken = asset.details?.address;
 
-  const nativeCurrencyImage = useSelector(getNativeCurrencyImage);
   const nativeCurrency = useSelector(getNativeCurrency);
   const balanceValue = useSelector(getSelectedAccountCachedBalance);
 
@@ -66,49 +57,9 @@ export default function AssetList({
       hideLabel: true,
     });
 
-  const filteredTokenList = useMemo(() => {
-    const filteredTokens: Token[] = [];
-
-    let token: Token;
-    for (token of tokenList) {
-      if (
-        token.symbol?.toLowerCase().includes(searchQuery.toLowerCase()) &&
-        token.symbol !== nativeCurrency
-      ) {
-        filteredTokens.push(token);
-      }
-
-      if (filteredTokens.length > MAX_UNOWNED_TOKENS_RENDERED) {
-        break;
-      }
-    }
-
-    // prepend native currency to token list if it matches search query
-    if (nativeCurrency?.toLowerCase().includes(searchQuery.toLowerCase())) {
-      filteredTokens.unshift({
-        address: null,
-        symbol: nativeCurrency,
-        decimals: 18,
-        image: nativeCurrencyImage,
-        balance: balanceValue,
-        string: primaryCurrencyProperties.value,
-        type: AssetType.native,
-      });
-    }
-
-    return filteredTokens;
-  }, [
-    tokenList,
-    searchQuery,
-    nativeCurrency,
-    nativeCurrencyImage,
-    balanceValue,
-    primaryCurrencyProperties.value,
-  ]);
-
   return (
     <Box className="tokens-main-view-modal">
-      {filteredTokenList.map((token) => {
+      {tokenList.map((token) => {
         const isSelected =
           token.address?.toLowerCase() === selectedToken?.toLowerCase();
         return (
