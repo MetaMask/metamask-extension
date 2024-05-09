3c8d14e8e9aa5772424315e97f002759286f9fef
Salah-Eddine Saakoun <salah-eddine.saakoun@consensys.net>
2024-05-09 15:40:32 +0200

    fix: attribution script

--- development/generate-attributions.sh
+++ development/generate-attributions.sh
@@ -16,14 +16,28 @@ main() {
   # relative to the project root irrespective of where this script was run.
   cd "${PROJECT_DIRECTORY}"
 
+  # Save .yarnrc.yml to temp file
+  local tmp=".yarnrc.tmp.yml"
+  cp ".yarnrc.yml" "$tmp"
+
+  # Convert YAML to JSON using Python and then use jq to remove the plugins section
+  yarnrc=$(python -c 'import sys, yaml, json; print(json.dumps(yaml.safe_load(sys.stdin.read())))' <"$tmp" | jq 'del(.plugins)')
+  echo "$yarnrc" >".yarnrc.yml"
+
+  # Run yarn command
   yarn workspaces focus --production
 
+  # Restore original .yarnrc.yml
+  mv "$tmp" ".yarnrc.yml"
+
+  # Change directory to the sub-project
   cd "${PROJECT_DIRECTORY}/development/generate-attributions"
 
   # Install sub-project that just contains attribution generation package
   # so that it can be used without installing `devDependencies` in root.
   yarn && yarn allow-scripts
 
+  # Generate attribution file
   yarn generate-attribution -o "${PROJECT_DIRECTORY}" -b "${PROJECT_DIRECTORY}"
 }
 
