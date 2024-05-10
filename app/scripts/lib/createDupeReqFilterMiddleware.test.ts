2483ec7eec4f967edbe442213902d4d34358b503
Erik Marks <rekmarks@protonmail.com>
2024-05-10 10:47:49 -0700

    feat: Expire duplicate RPC requests after five minutes

--- app/scripts/lib/createDupeReqFilterMiddleware.test.ts
+++ app/scripts/lib/createDupeReqFilterMiddleware.test.ts
@@ -1,5 +1,7 @@
 import { jsonrpc2 } from '@metamask/utils';
-import createDupeReqFilterMiddleware from './createDupeReqFilterMiddleware';
+import createDupeReqFilterMiddleware, {
+  THREE_MINUTES,
+} from './createDupeReqFilterMiddleware';
 
 describe('createDupeReqFilterMiddleware', () => {
   const getMockRequest = (id: number | string) => ({
@@ -9,7 +11,11 @@ describe('createDupeReqFilterMiddleware', () => {
   });
   const getMockResponse = () => ({ jsonrpc: jsonrpc2, id: 'foo' });
 
-  it('call function next if request is seen first time', () => {
+  beforeEach(() => {
+    jest.useFakeTimers({ now: 10 });
+  });
+
+  it('forwards requests with ids seen for the first time', () => {
     const filterFn = createDupeReqFilterMiddleware();
     const nextMock = jest.fn();
     const endMock = jest.fn();
@@ -20,17 +26,86 @@ describe('createDupeReqFilterMiddleware', () => {
     expect(endMock).not.toHaveBeenCalled();
   });
 
-  it('call function end if request is seen second time', () => {
+  it('ends the request if the id has been seen before', () => {
     const filterFn = createDupeReqFilterMiddleware();
     const nextMock = jest.fn();
     const endMock = jest.fn();
 
     filterFn(getMockRequest(1), getMockResponse(), nextMock, endMock);
+
     expect(nextMock).toHaveBeenCalledTimes(1);
     expect(endMock).not.toHaveBeenCalled();
 
-    filterFn(getMockRequest(1), getMockResponse(), nextMock, endMock);
+    const response = getMockResponse();
+    filterFn(getMockRequest(1), response, nextMock, endMock);
+    expect('result' in response).toBe(false);
     expect(nextMock).toHaveBeenCalledTimes(1);
     expect(endMock).toHaveBeenCalledTimes(1);
+  });
+
+  it('forwards JSON-RPC notifications (requests without ids)', () => {
+    const filterFn = createDupeReqFilterMiddleware();
+    const nextMock = jest.fn();
+    const endMock = jest.fn();
+
+    const notification = getMockRequest(1);
+    // @ts-expect-error Intentional destructive testing
+    delete notification.id;
+    filterFn(notification, getMockResponse(), nextMock, endMock);
+    filterFn(notification, getMockResponse(), nextMock, endMock);
+
+    expect(nextMock).toHaveBeenCalledTimes(2);
+    expect(endMock).not.toHaveBeenCalled();
+  });
+
+  it('expires single id after five minutes', () => {
+    const filterFn = createDupeReqFilterMiddleware();
+    const nextMock = jest.fn();
+    const endMock = jest.fn();
+
+    filterFn(getMockRequest(0), getMockResponse(), nextMock, endMock);
+
+    jest.advanceTimersByTime(THREE_MINUTES);
+
+    filterFn(getMockRequest(0), getMockResponse(), nextMock, endMock);
+
+    expect(nextMock).toHaveBeenCalledTimes(2);
+    expect(endMock).not.toHaveBeenCalled();
+  });
+
+  it('expires multiple ids after five minutes', () => {
+    const filterFn = createDupeReqFilterMiddleware();
+    const nextMock = jest.fn();
+    const endMock = jest.fn();
+
+    filterFn(getMockRequest(0), getMockResponse(), nextMock, endMock);
+    filterFn(getMockRequest(1), getMockResponse(), nextMock, endMock);
+
+    jest.advanceTimersByTime(1);
+
+    filterFn(getMockRequest(2), getMockResponse(), nextMock, endMock);
+
+    jest.advanceTimersByTime(THREE_MINUTES);
+
+    filterFn(getMockRequest(0), getMockResponse(), nextMock, endMock);
+    filterFn(getMockRequest(1), getMockResponse(), nextMock, endMock);
+    // This should be ignored since id 2 has yet to expire.
+    filterFn(getMockRequest(2), getMockResponse(), nextMock, endMock);
+
+    expect(nextMock).toHaveBeenCalledTimes(5);
+    expect(endMock).toHaveBeenCalledTimes(1);
+  });
+
+  it('handles running expiry job without seeing any ids', () => {
+    const filterFn = createDupeReqFilterMiddleware();
+    const nextMock = jest.fn();
+    const endMock = jest.fn();
+
+    jest.runAllTimers();
+
+    filterFn(getMockRequest(0), getMockResponse(), nextMock, endMock);
+
+    expect(nextMock).toHaveBeenCalledTimes(1);
+    expect(endMock).not.toHaveBeenCalled();
   });
 });
--- app/scripts/lib/createDupeReqFilterMiddleware.ts
+++ app/scripts/lib/createDupeReqFilterMiddleware.ts
@@ -1,22 +1,86 @@
-import type { JsonRpcId, JsonRpcMiddleware } from 'json-rpc-engine';
+import { JsonRpcMiddleware } from 'json-rpc-engine';
 import log from 'loglevel';
 
+const SECOND = 1000;
+const MINUTE = SECOND * 60;
+export const THREE_MINUTES = MINUTE * 3;
+
 /**
- * Returns a middleware that filters out requests already seen
+ * Creates a set abstraction whose values expire after five minutes.
  *
- * @returns
+ * @returns The expiry set.
+ */
+const makeExpirySet = () => {
+  const seenValues: (number | string)[] = [];
+  const timestamps: number[] = [];
+
+  setTimeout(() => {
+    if (timestamps.length === 0) {
+      return;
+    }
+
+    const cutoffTime = Date.now() - THREE_MINUTES;
+
+    if (timestamps.length === 1 && timestamps[0] <= cutoffTime) {
+      seenValues.shift();
+      timestamps.shift();
+      return;
+    }
+
+    for (let i = 0; i < timestamps.length; i++) {
+      if (timestamps[i] > cutoffTime) {
+        seenValues.splice(0, i);
+        timestamps.splice(0, i);
+        break;
+      }
+    }
+  }, THREE_MINUTES);
+
+  // Implement our `has` operation as a search in descending order, under the assumption
+  // that repeats are more likely to occur in short succession.
+  const has = (value: string | number) => {
+    return (
+      seenValues.findLast((seenValue) => seenValue === value) !== undefined
+    );
+  };
+
+  return {
+    /**
+     * Attempts to add a value to the set.
+     *
+     * @param value - The value to add.
+     * @returns `true` if the value was added, and `false` if it already existed.
+     */
+    add(value: string | number) {
+      if (!has(value)) {
+        seenValues.push(value);
+        timestamps.push(Date.now());
+        return true;
+      }
+      return false;
+    },
+  };
+};
+
+/**
+ * Returns a middleware that filters out requests whose ids we've already seen.
+ * Ignores JSON-RPC notifications, i.e. requests with an `undefined` id.
+ *
+ * @returns The middleware function.
  */
 export default function createDupeReqFilterMiddleware(): JsonRpcMiddleware<
   unknown,
   void
 > {
-  const processedRequestId: JsonRpcId[] = [];
+  const seenRequestIds = makeExpirySet();
   return function filterDuplicateRequestMiddleware(req, _res, next, end) {
-    if (processedRequestId.indexOf(req.id) >= 0) {
-      log.info(`RPC request with id ${req.id} already seen.`);
+    if (req.id === undefined) {
+      // JSON-RPC notifications have no ids; our only recourse is to let them through.
+      return next();
+    } else if (!seenRequestIds.add(req.id)) {
+      log.info(`RPC request with id "${req.id}" already seen.`);
       return end();
     }
-    processedRequestId.push(req.id);
     return next();
   };
 }
