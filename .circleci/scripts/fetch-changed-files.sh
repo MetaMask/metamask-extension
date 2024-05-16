d93b6df2854e241c87a96acb933ed78bd65bb3d4
seaona <mariona@gmx.es>
2024-05-16 12:11:13 +0200

    echo git diff

--- .circleci/scripts/fetch-changed-files.sh
+++ .circleci/scripts/fetch-changed-files.sh
@@ -5,16 +5,16 @@ set -u
 set -o pipefail
 
 #mkdir tmp
-echo "Last 10 commits current branch:"
-LOG=$(git log --oneline -n 10 "$CIRCLE_BRANCH")
+echo "Last 25 commits current branch:"
+LOG=$(git log --oneline -n 25 "$CIRCLE_BRANCH")
 echo "$LOG"
 
 echo "Last 10 commits develop:"
-LOG=$(git log --oneline -n 10 origin/develop)
+LOG=$(git log --oneline -n 15 origin/develop)
 echo "$LOG"
 
-#DIFF_RESULT=$(git diff --name-only develop..."$CIRCLE_BRANCH")
-#echo "$DIFF_RESULT"
+DIFF_RESULT=$(git diff --name-only origin/develop..."$CIRCLE_BRANCH")
+echo "$DIFF_RESULT"
 
 # Store the output of git diff in a temporary file
 #git diff --name-only origin/develop..."$CIRCLE_SHA1" >> tmp/changed_files.txt
