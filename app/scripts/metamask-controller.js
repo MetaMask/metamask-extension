1f83353351f97d429600b4b28bed36447910b13b
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2024-05-15 22:01:21 -0400

    fixing circle ci failure

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -3721,6 +3721,15 @@ export default class MetamaskController extends EventEmitter {
         this.nameController,
       ),
       setName: this.nameController.setName.bind(this.nameController),
+      // metrics data deleteion
+      createMetaMetricsDataDeletionTask:
+        this.metaMetricsDataDeletionController.createMetaMetricsDataDeletionTask.bind(
+          this.metaMetricsDataDeletionController,
+        ),
+      checkDataDeletionTaskStatus:
+        this.metaMetricsDataDeletionController.checkDataDeletionTaskStatus.bind(
+          this.metaMetricsDataDeletionController,
+        ),
     };
   }
 
@@ -5111,15 +5120,6 @@ export default class MetamaskController extends EventEmitter {
         sendMetrics: this.metaMetricsController.trackEvent.bind(
           this.metaMetricsController,
         ),
-        // metrics data deleteion
-        createMetaMetricsDataDeletionTask:
-          this.metaMetricsDataDeletionController.createMetaMetricsDataDeletionTask.bind(
-            this.metaMetricsDataDeletionController,
-          ),
-        checkDataDeletionTaskStatus:
-          this.metaMetricsDataDeletionController.checkDataDeletionTaskStatus.bind(
-            this.metaMetricsDataDeletionController,
-          ),
         // Permission-related
         getAccounts: this.getPermittedAccounts.bind(this, origin),
         getPermissionsForOrigin: this.permissionController.getPermissions.bind(
