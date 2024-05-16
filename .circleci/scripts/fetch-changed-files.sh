8e840a5d945cb5f11a673c27e61bef5b471fbf7b
seaona <mariona@gmx.es>
2024-05-16 11:39:54 +0200

    fetching dev branch

--- .circleci/scripts/fetch-changed-files.sh
+++ .circleci/scripts/fetch-changed-files.sh
@@ -6,11 +6,7 @@ set -o pipefail
 
 #mkdir tmp
 
-# Fetch the changes from the origin
-FETCH_RESULT=$(git fetch)
-echo "$FETCH_RESULT"
-
-DIFF_RESULT=$(git diff --name-only develop..."$CIRCLE_SHA1")
+DIFF_RESULT=$(git diff --name-only origin/develop..."$CIRCLE_SHA1")
 echo "$DIFF_RESULT"
 
 # Store the output of git diff in a temporary file
