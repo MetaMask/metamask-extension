695404606b3973b421cc36fae4ebab90494ccd1d
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2024-05-16 13:13:38 -0400

    adding new state variable and addressing review comments

--- app/scripts/controllers/metametrics-data-deletion/services/services.ts
+++ app/scripts/controllers/metametrics-data-deletion/services/services.ts
@@ -1,48 +1,45 @@
 import getFetchWithTimeout from '../../../../../shared/modules/fetch-with-timeout';
 import { DeleteRegulationAPIResponse } from '../metametrics-data-deletion';
 
+const segmentSourceId = process.env.SEGMENT_DELETE_API_SOURCE_ID;
+const segmentRegulationEndpoint = process.env.SEGMENT_REGULATIONS_ENDPOINT;
+
 const fetchWithTimeout = getFetchWithTimeout();
 
 export async function createDataDeletionRegulationTask(
   metaMetricsId: string,
 ): Promise<DeleteRegulationAPIResponse> {
-  const segmentSourceId = process.env.SEGMENT_DELETE_API_SOURCE_ID;
-  const segmentRegulationEndpoint = process.env.SEGMENT_REGULATIONS_ENDPOINT;
-
-  try {
-    const response: unknown = await fetchWithTimeout(
-      `${segmentRegulationEndpoint}/regulations/sources/${segmentSourceId}`,
-      {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/vnd.segment.v1+json' },
-        body: JSON.stringify({
-          regulationType: 'DELETE_ONLY',
-          subjectType: 'USER_ID',
-          subjectIds: [metaMetricsId],
-        }),
-      },
-    );
-    return response as DeleteRegulationAPIResponse;
-  } catch (error: unknown) {
-    throw new Error('Analytics Deletion Task Error');
+  if (!segmentRegulationEndpoint) {
+    throw new Error('Segment API source ID or endpoint not found');
   }
+
+  const response: unknown = await fetchWithTimeout(
+    `${segmentRegulationEndpoint}/regulations/sources/${segmentSourceId}`,
+    {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/vnd.segment.v1+json' },
+      body: JSON.stringify({
+        regulationType: 'DELETE_ONLY',
+        subjectType: 'USER_ID',
+        subjectIds: [metaMetricsId],
+      }),
+    },
+  );
+  return response as DeleteRegulationAPIResponse;
 }
 
 export async function fetchDeletionRegulationStatus(
   deleteRegulationId: string,
 ): Promise<DeleteRegulationAPIResponse> {
-  const segmentRegulationEndpoint = process.env.SEGMENT_REGULATIONS_ENDPOINT;
-
-  try {
-    const response: unknown = await fetchWithTimeout(
-      `${segmentRegulationEndpoint}/regulations/${deleteRegulationId}`,
-      {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/vnd.segment.v1+json' },
-      },
-    );
-    return response as DeleteRegulationAPIResponse;
-  } catch (error: unknown) {
-    throw new Error('Analytics Deletion Task Error');
+  if (!segmentSourceId || !segmentRegulationEndpoint) {
+    throw new Error('Segment API source ID or endpoint not found');
   }
+  const response: unknown = await fetchWithTimeout(
+    `${segmentRegulationEndpoint}/regulations/${deleteRegulationId}`,
+    {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/vnd.segment.v1+json' },
+    },
+  );
+  return response as DeleteRegulationAPIResponse;
 }
--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -2928,6 +2928,7 @@ export default class MetamaskController extends EventEmitter {
       userStorageController,
       metamaskNotificationsController,
       pushPlatformNotificationsController,
+      metaMetricsDataDeletionController,
     } = this;
 
     return {
@@ -3723,12 +3724,12 @@ export default class MetamaskController extends EventEmitter {
       setName: this.nameController.setName.bind(this.nameController),
       // metrics data deleteion
       createMetaMetricsDataDeletionTask:
-        this.metaMetricsDataDeletionController.createMetaMetricsDataDeletionTask.bind(
-          this.metaMetricsDataDeletionController,
+        metaMetricsDataDeletionController.createMetaMetricsDataDeletionTask.bind(
+          metaMetricsDataDeletionController,
         ),
       checkDataDeletionTaskStatus:
-        this.metaMetricsDataDeletionController.checkDataDeletionTaskStatus.bind(
-          this.metaMetricsDataDeletionController,
+        metaMetricsDataDeletionController.checkDataDeletionTaskStatus.bind(
+          metaMetricsDataDeletionController,
         ),
     };
   }
