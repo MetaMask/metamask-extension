f7985cb91137a0d156a86f1117acda28061eb182
seaona <mariona@gmx.es>
2024-05-16 08:53:33 +0200

    fix path for sh file

--- .circleci/scripts/fetch-changed-files.sh
+++ .circleci/scripts/fetch-changed-files.sh
@@ -4,8 +4,10 @@ set -e
 set -u
 set -o pipefail
 
+mkdir tmp
+
 # Fetch the changes from the origin
 git fetch origin
 
 # Store the output of git diff in a temporary file
-git diff --name-only origin/develop...$CIRCLE_SHA1 > /tmp/changed_files.txt
+git diff --name-only origin/develop...$CIRCLE_SHA1 >> tmp/changed_files.txt
