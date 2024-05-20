9b095f6bb8a12e7623b977822e51676a524eaff1
Xiaoming Wang <dawnseeker8@gmail.com>
2024-05-20 12:18:08 +0100

    feat: Update index.test.tsx to match the english text in screen

--- ui/pages/create-account/connect-hardware/index.test.tsx
+++ ui/pages/create-account/connect-hardware/index.test.tsx
@@ -190,7 +190,7 @@ describe('ConnectHardwareForm', () => {
         expect(getByText('CoolWallet')).toBeInTheDocument();
         expect(getByText("D'Cent")).toBeInTheDocument();
         expect(getByText('imToken')).toBeInTheDocument();
-        expect(getByText('onekey')).toBeInTheDocument();
+        expect(getByText('OneKey')).toBeInTheDocument();
       });
     });
   });
