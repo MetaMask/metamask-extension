5784c0308069d20c0102b1197fb4af554c6fb4be
Kate Johnson <johnson.kat.g@gmail.com>
2024-05-20 11:08:03 -0400

    feat: add account-watcher bundle to preinstalled snaps

--- app/scripts/snaps/preinstalled-snaps.ts
+++ app/scripts/snaps/preinstalled-snaps.ts
@@ -1,6 +1,6 @@
 import type { PreinstalledSnap } from '@metamask/snaps-controllers';
 import MessageSigningSnap from '@metamask/message-signing-snap/dist/preinstalled-snap.json';
-import AccountWatcherSnap from './account-watcher-test-preinstalled-snap.json';
+import AccountWatcherSnap from '@metamask/account-watcher/dist/preinstalled-snap.json';
 
 const PREINSTALLED_SNAPS: readonly PreinstalledSnap[] = Object.freeze([
   MessageSigningSnap as PreinstalledSnap,
