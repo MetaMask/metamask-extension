47958183dd7b17dbf6f579c2e5489790eed14f97
legobt <6wbvkn0j@anonaddy.me>
2024-05-20 08:17:42 +0000

    devDeps: add readable-stream-2, readable-stream-3 as alias for readable-stream by version

--- app/scripts/lib/stream-utils.test.ts
+++ app/scripts/lib/stream-utils.test.ts
@@ -1,10 +1,15 @@
 import { isStreamWritable } from './stream-utils';
-import ReadableStream2 from 'readable-stream';
+// Redundantly include used version twice for regression-detection purposes
+import ReadableStream from 'readable-stream';
+import ReadableStream2 from 'readable-stream-2';
+import ReadableStream3 from 'readable-stream-3';
 
 describe('Stream Utils', () => {
   describe('isStreamWritable', () => {
     ([
+      ['readable-stream', ReadableStream] as [string, typeof ReadableStream],
       ['readable-stream v2', ReadableStream2] as [string, typeof ReadableStream2],
+      ['readable-stream v3', ReadableStream3] as [string, typeof ReadableStream3],
     ]).forEach(([name, streamsImpl]) => {
       describe(`Using Streams implementation: ${name}`, () => {
         it('should something', () => {
