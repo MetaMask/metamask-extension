cae93dcd10315598776f677a6c05351891cb5c68
Frederik Bolding <frederik.bolding@gmail.com>
2024-05-21 14:22:32 +0200

    feat: add Snap UI dropdown

---
+++ ui/components/app/snaps/snap-ui-renderer/components/dropdown.ts
@@ -0,0 +1,29 @@
+import {
+  DropdownElement,
+  DropdownOptionElement,
+} from '@metamask/snaps-sdk/jsx';
+import { getJsxChildren } from '@metamask/snaps-utils';
+
+import { UIComponentFactory } from './types';
+
+export const dropdown: UIComponentFactory<DropdownElement> = ({
+  element,
+  form,
+}) => {
+  const children = getJsxChildren(element) as DropdownOptionElement[];
+
+  const options = children.map((child) => ({
+    value: child.props.value,
+    name: child.props.children,
+  }));
+
+  return {
+    element: 'SnapUIDropdown',
+    props: {
+      id: element.props.name,
+      name: element.props.name,
+      form,
+      options,
+    },
+  };
+};
--- ui/components/app/snaps/snap-ui-renderer/components/field.ts
+++ ui/components/app/snaps/snap-ui-renderer/components/field.ts
@@ -2,43 +2,67 @@ import {
   FieldElement,
   InputElement,
   ButtonElement,
+  JSXElement,
+  DropdownElement,
 } from '@metamask/snaps-sdk/jsx';
 import { getJsxChildren } from '@metamask/snaps-utils';
-import { button as buttonMapper } from './button';
-
+import { button as buttonFn } from './button';
+import { dropdown as dropdownFn } from './dropdown';
 import { UIComponentFactory, UIComponentParams } from './types';
 
 export const field: UIComponentFactory<FieldElement> = ({ element, form }) => {
   // For fields we don't render the Input itself, we just adapt SnapUIInput.
   const children = getJsxChildren(element);
-  const input = children[0] as InputElement;
-  const button = children[1] as ButtonElement | undefined;
-  const mappedButton =
-    button &&
-    buttonMapper({ element: button } as UIComponentParams<ButtonElement>);
+  const child = children[0] as JSXElement;
 
-  return {
-    element: 'SnapUIInput',
-    props: {
-      id: input.props.name,
-      placeholder: input.props.placeholder,
-      label: element.props.label,
-      textFieldProps: {
-        type: input.props.type,
-      },
-      name: input.props.name,
-      form,
-      error: element.props.error !== undefined,
-      helpText: element.props.error,
-    },
-    propComponents: {
-      endAccessory: mappedButton && {
-        ...mappedButton,
+  switch (child.type) {
+    case 'Input': {
+      const input = child as InputElement;
+      const button = children[1] as ButtonElement;
+      const buttonMapped = buttonFn({ element: button });
+      return {
+        element: 'SnapUIInput',
         props: {
-          ...mappedButton.props,
-          padding: 0,
+          id: input.props.name,
+          placeholder: input.props.placeholder,
+          label: element.props.label,
+          textFieldProps: {
+            type: input.props.type,
+          },
+          name: input.props.name,
+          form,
+          error: element.props.error !== undefined,
+          helpText: element.props.error,
+          propComponents: {
+            endAccessory: buttonMapped && {
+              ...buttonMapped,
+              props: {
+                ...buttonMapped.props,
+                padding: 0,
+              },
+            },
+          },
         },
-      },
-    },
-  };
+      };
+    }
+
+    case 'Dropdown': {
+      const dropdown = child as DropdownElement;
+      const dropdownMapped = dropdownFn({ element: dropdown });
+      return {
+        element: 'SnapUIDropdown',
+        props: {
+          ...dropdownMapped.props,
+          id: dropdown.props.name,
+          label: element.props.label,
+          name: dropdown.props.name,
+          form,
+          error: element.props.error,
+        },
+      };
+    }
+
+    default:
+      throw new Error(`Invalid Field child: ${child.type}`);
+  }
 };
