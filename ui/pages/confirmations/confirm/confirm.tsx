00f463515fcf21870e52e0cd758e98edc5b8dedf
Jyoti Puri <jyotipuri@gmail.com>
2024-05-21 14:39:18 +0530

    Refactoring alertActionHandler context

--- ui/pages/confirmations/confirm/confirm.tsx
+++ ui/pages/confirmations/confirm/confirm.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+
+import { AlertActionHandlerProvider } from '../../../contexts/alertActionHandler';
 import ScrollToBottom from '../components/confirm/scroll-to-bottom';
 import { Footer } from '../components/confirm/footer';
 import { Header } from '../components/confirm/header';
@@ -15,7 +17,6 @@ import syncConfirmPath from '../hooks/syncConfirmPath';
 import { BlockaidAlert } from '../components/confirm/blockaid-alert';
 ///: END:ONLY_INCLUDE_IF
 import { LedgerInfo } from '../components/confirm/ledger-info';
-import { AlertActionHandlerProvider } from '../../../contexts/alertActionHandler';
 import setConfirmationAlerts from '../hooks/setConfirmationAlerts';
 
 const Confirm = () => {
