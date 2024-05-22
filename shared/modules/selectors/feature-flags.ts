7ba056d87671cc3924a21b4d980dc77e5d02ad2e
Derek Brans <dbrans@gmail.com>
2024-05-22 15:59:27 -0400

    refractor feature–flags.ts

--- shared/modules/selectors/feature-flags.ts
+++ shared/modules/selectors/feature-flags.ts
@@ -1,25 +1,44 @@
 import { getCurrentChainId } from '../../../ui/selectors/selectors'; // TODO: Migrate shared selectors to this file.
 import { getNetworkNameByChainId } from '../feature-flags';
 
+export type SmartTransactionsFeatureFlags = {
+  extensionActive?: boolean;
+  mobileActive?: boolean;
+  expectedDeadline?: number;
+  maxDeadline?: number;
+  returnTxHashAsap?: boolean;
+  optInModalMinVersion?: string;
+};
+
+export type ChainSpecificFeatureFlags = {
+  extensionActive: boolean;
+  mobileActive: boolean;
+  smartTransactions: SmartTransactionsFeatureFlags;
+};
+
+/**
+ * All feature flags. Currently only smartTransactions.
+ */
+export type FeatureFlags = {
+  smartTransactions?: SmartTransactionsFeatureFlags;
+};
+
 type FeatureFlagsMetaMaskState = {
   metamask: {
     swapsState: {
       swapsFeatureFlags: {
-        [key: string]: {
-          extensionActive: boolean;
-          mobileActive: boolean;
-          smartTransactions: {
-            expectedDeadline?: number;
-            maxDeadline?: number;
-            returnTxHashAsap?: boolean;
-          };
-        };
+        [key: string]:
+          | SmartTransactionsFeatureFlags
+          | ChainSpecificFeatureFlags;
+        smartTransactions: SmartTransactionsFeatureFlags;
       };
     };
   };
 };
 
-export function getFeatureFlagsByChainId(state: FeatureFlagsMetaMaskState) {
+export function getFeatureFlagsByChainId(
+  state: FeatureFlagsMetaMaskState,
+): FeatureFlags | null {
   const chainId = getCurrentChainId(state);
   const networkName = getNetworkNameByChainId(chainId);
   const featureFlags = state.metamask.swapsState?.swapsFeatureFlags;
@@ -29,7 +48,8 @@ export function getFeatureFlagsByChainId(state: FeatureFlagsMetaMaskState) {
   return {
     smartTransactions: {
       ...featureFlags.smartTransactions,
-      ...featureFlags[networkName].smartTransactions,
+      ...(featureFlags[networkName] as ChainSpecificFeatureFlags)
+        .smartTransactions,
     },
   };
 }
