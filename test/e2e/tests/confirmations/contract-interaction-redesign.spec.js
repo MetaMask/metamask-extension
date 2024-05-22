029e7c609bf3c41683a134cb8df2577d2c2f763e
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2024-05-22 15:23:14 +0100

    wip

--- test/e2e/tests/confirmations/contract-interaction-redesign.spec.js
+++ test/e2e/tests/confirmations/contract-interaction-redesign.spec.js
@@ -8,13 +8,68 @@ const {
   withFixtures,
 } = require('../../helpers');
 const FixtureBuilder = require('../../fixture-builder');
+const { CHAIN_IDS } = require('../../../../shared/constants/network');
+const { hexToNumber } = require('@metamask/utils');
 
 describe('Confirmation Redesign Contract Interaction Component', function () {
   if (!process.env.ENABLE_CONFIRMATION_REDESIGN) {
     return;
   }
 
-  it(`Opens a contract interaction type 2 transaction`, async function () {
+  it.skip(`Opens a contract interaction type 0 transaction`, async function () {
+    await withFixtures(
+      {
+        dapp: true,
+        fixtures: new FixtureBuilder()
+          .withPermissionControllerConnectedToTestDapp()
+          .withPreferencesController({
+            preferences: { redesignedConfirmationsEnabled: true },
+          })
+          .build(),
+        ganacheOptions: defaultGanacheOptions,
+        title: this.test?.fullTitle(),
+      },
+      async ({ driver }) => {
+        await unlockWallet(driver);
+        await openDapp(driver);
+
+        await driver.switchToWindowWithTitle(WINDOW_TITLES.TestDApp);
+        await driver.clickElement(`#deployButton`);
+
+        await driver.delay(2 ** 5);
+
+        await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
+        await driver.waitForSelector({
+          css: '.confirm-page-container-summary__action__name',
+          text: 'Contract deployment',
+        });
+
+        await driver.clickElement({ text: 'Confirm', tag: 'button' });
+        await driver.waitUntilXWindowHandles(2);
+        await driver.switchToWindowWithTitle(
+          WINDOW_TITLES.ExtensionInFullScreenView,
+        );
+        await driver.clickElement({ text: 'Activity', tag: 'button' });
+        await driver.waitForSelector(
+          '.transaction-list__completed-transactions .activity-list-item:nth-of-type(1)',
+        );
+
+        await driver.switchToWindowWithTitle(WINDOW_TITLES.TestDApp);
+        await driver.clickElement(`#depositButton`);
+
+        await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
+
+        await driver.waitForSelector({
+          css: 'h2',
+          text: 'Transaction request',
+        });
+
+        // await driver.delay(1024 ** 2);
+      },
+    );
+  });
+
+  it.skip(`Opens a contract interaction type 2 transaction`, async function () {
     await withFixtures(
       {
         dapp: true,
@@ -68,111 +123,28 @@ describe('Confirmation Redesign Contract Interaction Component', function () {
     );
   });
 
-  it(`Opens a contract interaction type 1 transaction`, async function () {
+  it.only(`Opens a contract interaction type 2 transaction that includes layer 1 fees breakdown on a layer 2`, async function () {
     await withFixtures(
       {
         dapp: true,
-        fixtures: new FixtureBuilder()
+        fixtures: new FixtureBuilder({ inputChainId: CHAIN_IDS.OPTIMISM })
           .withPermissionControllerConnectedToTestDapp()
           .withPreferencesController({
             preferences: { redesignedConfirmationsEnabled: true },
           })
+          .withTransactionControllerOPLayer2Transaction()
           .build(),
-        ganacheOptions: defaultGanacheOptions,
+        ganacheOptions: {
+          ...defaultGanacheOptionsForType2Transactions,
+          network_id: hexToNumber(CHAIN_IDS.OPTIMISM),
+          chainId: hexToNumber(CHAIN_IDS.OPTIMISM),
+        },
         title: this.test?.fullTitle(),
       },
       async ({ driver }) => {
         await unlockWallet(driver);
-        await openDapp(driver);
 
-        await driver.switchToWindowWithTitle(WINDOW_TITLES.TestDApp);
-        await driver.clickElement(`#deployButton`);
-
-        await driver.delay(2 ** 5);
-
-        await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
-        await driver.waitForSelector({
-          css: '.confirm-page-container-summary__action__name',
-          text: 'Contract deployment',
-        });
-
-        await driver.clickElement({ text: 'Confirm', tag: 'button' });
-        await driver.waitUntilXWindowHandles(2);
-        await driver.switchToWindowWithTitle(
-          WINDOW_TITLES.ExtensionInFullScreenView,
-        );
-        await driver.clickElement({ text: 'Activity', tag: 'button' });
-        await driver.waitForSelector(
-          '.transaction-list__completed-transactions .activity-list-item:nth-of-type(1)',
-        );
-
-        await driver.switchToWindowWithTitle(WINDOW_TITLES.TestDApp);
-        await driver.clickElement(`#depositButton`);
-
-        await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
-
-        await driver.waitForSelector({
-          css: 'h2',
-          text: 'Transaction request',
-        });
-
-        // await driver.delay(1024 ** 2);
-      },
-    );
-  });
-
-  it(`Opens a contract interaction type 2 transaction in a layer 2`, async function () {
-    await withFixtures(
-      {
-        dapp: true,
-        fixtures: new FixtureBuilder()
-          .withPermissionControllerConnectedToTestDapp()
-          .withPreferencesController({
-            preferences: { redesignedConfirmationsEnabled: true },
-          })
-          .withTransactionController({
-            // populate tx info including the layer 2 fees
-          })
-          .build(),
-        ganacheOptions: defaultGanacheOptions,
-        title: this.test?.fullTitle(),
-      },
-      async ({ driver }) => {
-        await unlockWallet(driver);
-        await openDapp(driver);
-
-        await driver.switchToWindowWithTitle(WINDOW_TITLES.TestDApp);
-        await driver.clickElement(`#deployButton`);
-
-        await driver.delay(2 ** 5);
-
-        await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
-        await driver.waitForSelector({
-          css: '.confirm-page-container-summary__action__name',
-          text: 'Contract deployment',
-        });
-
-        await driver.clickElement({ text: 'Confirm', tag: 'button' });
-        await driver.waitUntilXWindowHandles(2);
-        await driver.switchToWindowWithTitle(
-          WINDOW_TITLES.ExtensionInFullScreenView,
-        );
-        await driver.clickElement({ text: 'Activity', tag: 'button' });
-        await driver.waitForSelector(
-          '.transaction-list__completed-transactions .activity-list-item:nth-of-type(1)',
-        );
-
-        await driver.switchToWindowWithTitle(WINDOW_TITLES.TestDApp);
-        await driver.clickElement(`#depositButton`);
-
-        await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
-
-        await driver.waitForSelector({
-          css: 'h2',
-          text: 'Transaction request',
-        });
-
-        // await driver.delay(1024 ** 2);
+        await driver.delay(1024 ** 2);
       },
     );
   });
