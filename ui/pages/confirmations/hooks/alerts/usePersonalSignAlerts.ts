4fd9d95a662fc04746d987fe8400edc8233b9067
Vinicius Stevam <vinicius.stevam@consensys.net>
2024-05-22 16:59:06 +0100

    Revert "temp"
    This reverts commit 5a55670b95ee4a0ac39edf73b65a6da21bae892f.

--- ui/pages/confirmations/hooks/alerts/usePersonalSignAlerts.ts
+++ ui/pages/confirmations/hooks/alerts/usePersonalSignAlerts.ts
@@ -2,14 +2,10 @@ import { useMemo } from 'react';
 import { ApprovalType } from '@metamask/controller-utils';
 import useCurrentConfirmation from '../useCurrentConfirmation';
 import { Alert } from '../../../../ducks/confirm-alerts/confirm-alerts';
-import { Severity } from '../../../../helpers/constants/design-system';
 import { SecurityAlertResponse } from '../../types/confirm';
 import { useI18nContext } from '../../../../hooks/useI18nContext';
-import { providerAlertNormalizer } from '../../../../components/app/alerts-system/utils';
-import {
-  BlockaidResultType,
-  SecurityProvider,
-} from '../../../../../shared/constants/security-provider';
+import { providerAlertNormalizer } from '../../../../components/app/confirmations/alerts/utils';
+import { BlockaidResultType } from '../../../../../shared/constants/security-provider';
 import useSignatureSecurityAlertResponse from '../useSignatureSecurityAlertResponse';
 
 const usePersonalSignAlerts = (): Alert[] => {
@@ -34,45 +30,7 @@ const usePersonalSignAlerts = (): Alert[] => {
       return [];
     }
 
-    return [
-      providerAlertNormalizer(securityAlertResponse, t),
-      {
-        key: 'Message',
-        field: 'Message',
-        severity: Severity.Danger,
-        message: `Test 1 - Message is a pishing attempt. Please do not sign this message.`,
-        reason: 'Test Reason',
-        alertDetails: ['Detail 1 of the alert', 'Detail 2 of the alert'],
-        provider: SecurityProvider.Blockaid,
-      },
-      {
-        key: 'fieldTest1',
-        severity: Severity.Danger,
-        message: 'Test 2',
-        reason: 'Test Reason',
-        alertDetails: ['Detail 1 of the alert', 'Detail 2 of the alert'],
-      },
-      {
-        key: 'fieldTest2',
-        field: 'testField',
-        severity: Severity.Info,
-        message: 'General Test 2',
-        reason: 'General Test Reason 2',
-        alertDetails: ['Detail 1 of the alert', 'Detail 2 of the alert'],
-      },
-      {
-        key: 'fieldTest3',
-        field: 'from',
-        severity: Severity.Warning,
-        message: 'Test 3',
-        actions: [
-          {
-            key: 'dispatchAction',
-            label: 'Update gas option',
-          },
-        ],
-      },
-    ];
+    return [providerAlertNormalizer(securityAlertResponse, t)];
   }, [currentConfirmation, signatureSecurityAlertResponse]);
 
   return alerts;
