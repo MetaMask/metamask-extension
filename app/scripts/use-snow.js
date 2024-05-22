ca09a970abbe68317c46b70da1ccfef73c8ba1fb
Dan J Miller <danjm.com@gmail.com>
2024-05-22 08:08:23 -0230

    Limit unscuttled fetch to only fetching images from the extension itself

--- app/scripts/use-snow.js
+++ app/scripts/use-snow.js
@@ -12,16 +12,45 @@ Changing this code must be done cautiously to avoid breaking the app!
   const isWorker = !self.document;
   const msg =
     'Snow detected a new realm creation attempt in MetaMask. Performing scuttling on new realm.';
+
+  // eslint-disable-next-line no-undef
+  const chromeExtensionId = chrome && chrome.runtime && chrome.runtime.id;
+  const tamedFetch = (path) => {
+    const regex = new RegExp(
+      `^chrome-extension://${chromeExtensionId}/.*?/images/.*`,
+      'u',
+    );
+    if (regex.test(path)) {
+      return fetch(path);
+    }
+    throw new Error(
+      'Unscuttled fetch can only be used on images within the extension',
+    );
+  };
+
   // eslint-disable-next-line no-undef
   Object.defineProperty(self, 'SCUTTLER', {
     value: (realm, scuttle) => {
+      const scuttleWithRestrictedException = (_ref, restrictedExceptions) => {
+        const ref = _ref;
+        for (const key in restrictedExceptions) {
+          if (Object.hasOwn(restrictedExceptions, key)) {
+            ref[key] = restrictedExceptions[key];
+          }
+        }
+        return scuttle(ref);
+      };
       if (isWorker) {
-        scuttle(realm);
+        chromeExtensionId
+          ? scuttleWithRestrictedException(realm, { fetch: tamedFetch })
+          : scuttle(realm);
       } else {
         // eslint-disable-next-line no-undef
         self.SNOW((win) => {
           log(msg, win);
-          scuttle(win);
+          chromeExtensionId
+            ? scuttleWithRestrictedException(win, { fetch: tamedFetch })
+            : scuttle(win);
         }, realm);
       }
     },
