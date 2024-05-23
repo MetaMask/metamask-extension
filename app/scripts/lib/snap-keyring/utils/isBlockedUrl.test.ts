5bb2a45c0303cb29050eee309f8893f7f27794c4
Michele Esposito <michele@esposito.codes>
2024-05-23 15:19:45 +0200

    fix: `isBlockedUrl.test.ts` and remove ts-expect-error directive

--- app/scripts/lib/snap-keyring/utils/isBlockedUrl.test.ts
+++ app/scripts/lib/snap-keyring/utils/isBlockedUrl.test.ts
@@ -6,11 +6,10 @@ describe('isBlockedUrl', () => {
   const messenger = new ControllerMessenger();
   const phishingControllerMessenger = messenger.getRestricted({
     name: 'PhishingController',
+    allowedActions: [],
+    allowedEvents: [],
   });
   const phishingController = new PhishingController({
-    // @ts-expect-error The PhishingController uses a newer verison of the package
-    // `@metamask/base-controller`, which has a different messenger type. This error will be
-    // resolved shortly when the `@metamask/base-controller` package is updated.
     messenger: phishingControllerMessenger,
     state: {
       phishingLists: [
