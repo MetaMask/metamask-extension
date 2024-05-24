040071a04fbfffde0f1ca44815d89dec707db2ea
Priya Narayanaswamy <priya.narayanaswamy@consensys.net>
2024-05-24 15:52:04 +0530

    test: lint fix

--- test/e2e/tests/confirmations/helpers.ts
+++ test/e2e/tests/confirmations/helpers.ts
@@ -21,7 +21,7 @@ export async function scrollAndConfirmAndAssertConfirm (driver: Driver) {
   confirmButton.getAttribute('disabled').then((disabled) => {
     assert.equal(disabled, null);
   });
-  await driver.clickElement('[data-testid="confirm-footer-button"]')
+  await driver.clickElement('[data-testid="confirm-footer-button"]');
 }
 
 export function withRedesignConfirmationFixtures (title: string = '', testFunction: Function) {
