a50557f31ec00af67127139094914060b5570f4b
digiwand <20778143+digiwand@users.noreply.github.com>
2024-05-24 23:02:24 +0900

    test:e2e: rm getAttribute asserts for Confirmations scroll helper - asynchronous thenable that is not awaited is not being tested

--- test/e2e/tests/confirmations/helpers.ts
+++ test/e2e/tests/confirmations/helpers.ts
@@ -8,19 +8,7 @@ import { Driver } from '../../webdriver/driver';
 
 export async function scrollAndConfirmAndAssertConfirm (driver: Driver) {
   const confirmButton = await driver.findElement({ xpath: '//button[@data-testid="confirm-footer-button"]'});
-
-  // clicking the confirm button doesn't close the dialog since it's disabled
-  confirmButton.getAttribute('disabled').then((disabled) => {
-    assert.equal(disabled, true);
-  });
-
-  // scroll to the bottom which enables the confirm button
   await driver.clickElement('.confirm-scroll-to-bottom__button');
-
-  // click the confirm button which closes the dialog
-  confirmButton.getAttribute('disabled').then((disabled) => {
-    assert.equal(disabled, null);
-  });
   await driver.clickElement('[data-testid="confirm-footer-button"]');
 }
 
