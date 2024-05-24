1e5d2d6cd3d2f11c0dfb7d4f8043a322092ec806
Bilal Zahory <bilal.zahory@consensys.net>
2024-05-24 05:52:36 -0400

    test: add test for disable

--- ui/components/multichain/asset-picker-amount/nft-input/nft-input.test.tsx
+++ ui/components/multichain/asset-picker-amount/nft-input/nft-input.test.tsx
@@ -35,6 +35,21 @@ describe('NFTInput', () => {
     expect(asFragment()).toMatchSnapshot();
   });
 
+  it('disables input when change handler is not passed', () => {
+    const { getByTestId } = render(
+      <Provider
+        store={createStore({
+          useNativeCurrencyAsPrimaryCurrency: true,
+          sendInputCurrencySwitched: true,
+        })}
+      >
+        <NFTInput integerValue={1} />
+      </Provider>,
+    );
+
+    expect(getByTestId('nft-input')).toBeDisabled();
+  });
+
   it('registers changes', () => {
     const mockAssetChange = jest.fn();
 
