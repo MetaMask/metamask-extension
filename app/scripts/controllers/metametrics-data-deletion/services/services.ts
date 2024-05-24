3e759f752afae1b953c065ebd2681cecd01b9226
Mark Stacey <markjstacey@gmail.com>
2024-05-24 13:43:18 -0230

    Add dev build fallbacks for environment variables

--- app/scripts/controllers/metametrics-data-deletion/services/services.ts
+++ app/scripts/controllers/metametrics-data-deletion/services/services.ts
@@ -13,9 +13,9 @@ import getFetchWithTimeout from '../../../../../shared/modules/fetch-with-timeou
 import { CurrentRegulationStatus, RegulationId } from '../types';
 
 const DEFAULT_ANALYTICS_DATA_DELETION_SOURCE_ID =
-  process.env.ANALYTICS_DATA_DELETION_SOURCE_ID;
+  process.env.ANALYTICS_DATA_DELETION_SOURCE_ID ?? 'test';
 const DEFAULT_ANALYTICS_DATA_DELETION_ENDPOINT =
-  process.env.ANALYTICS_DATA_DELETION_ENDPOINT;
+  process.env.ANALYTICS_DATA_DELETION_ENDPOINT ?? 'https://metametrics.metamask.test/';
 
 const fetchWithTimeout = getFetchWithTimeout();
 
