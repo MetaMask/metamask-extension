b506c11bb3d61afe4b4b28e87d6e23ab3adb7d89
seaona <mariona@gmx.es>
2024-05-27 15:03:25 +0200

    small tweak

--- .circleci/scripts/get-changed-files.sh
+++ .circleci/scripts/get-changed-files.sh
@@ -4,7 +4,6 @@ set -e
 set -u
 set -o pipefail
 
-#mkdir tmp
 echo "Last 10 commits current branch:"
 LOG=$(git log --oneline -n 10 "$CIRCLE_BRANCH")
 echo "$LOG"
@@ -16,7 +15,7 @@ echo "$LOG"
 DIFF_RESULT=$(git diff --name-only develop..."$CIRCLE_BRANCH")
 echo "$DIFF_RESULT"
 
-# Store the output of git diff in a temporary file
-git diff --name-only origin/develop..."$CIRCLE_SHA1" >> changed-files/changed-files.txt
+# Store the output of git diff
+git diff --name-only develop..."$CIRCLE_SHA1" >> changed-files/changed-files.txt
 
 exit 0
