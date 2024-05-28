d98bd2e907eb2f7336187cf5b18be1ef9e3613ce
Frederik Bolding <frederik.bolding@gmail.com>
2024-05-28 16:04:33 +0200

    feat: allow action buttons in fields (#24537)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    2. What is the improvement/solution?
    -->
    
    [![Open in GitHub
    Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/24537?quickstart=1)
    
    ## **Related issues**
    
    Fixes:
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    
    ![image](https://github.com/MetaMask/metamask-extension/assets/1561200/adcbf754-6699-413b-b18b-4039ad002e76)

--- ui/components/app/snaps/snap-ui-renderer/components/field.ts
+++ ui/components/app/snaps/snap-ui-renderer/components/field.ts
@@ -1,11 +1,21 @@
-import { FieldElement, InputElement } from '@metamask/snaps-sdk/jsx';
+import {
+  FieldElement,
+  InputElement,
+  ButtonElement,
+} from '@metamask/snaps-sdk/jsx';
 import { getJsxChildren } from '@metamask/snaps-utils';
+import { button as buttonMapper } from './button';
 
-import { UIComponentFactory } from './types';
+import { UIComponentFactory, UIComponentParams } from './types';
 
 export const field: UIComponentFactory<FieldElement> = ({ element, form }) => {
   // For fields we don't render the Input itself, we just adapt SnapUIInput.
-  const input = getJsxChildren(element)[0] as InputElement;
+  const children = getJsxChildren(element);
+  const input = children[0] as InputElement;
+  const button = children[1] as ButtonElement | undefined;
+  const mappedButton =
+    button &&
+    buttonMapper({ element: button } as UIComponentParams<ButtonElement>);
 
   return {
     element: 'SnapUIInput',
@@ -20,6 +30,15 @@ export const field: UIComponentFactory<FieldElement> = ({ element, form }) => {
       form,
       error: element.props.error !== undefined,
       helpText: element.props.error,
+    },
+    propComponents: {
+      endAccessory: mappedButton && {
+        ...mappedButton,
+        props: {
+          ...mappedButton.props,
+          padding: 0,
+        },
+      },
     },
   };
 };
