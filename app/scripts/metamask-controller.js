35ad6c81d991203fc8ca3a237662fc1b32f533dd
Mark Stacey <markjstacey@gmail.com>
2024-05-29 10:22:32 -0230

    refactor: Create dedicated services directory
    The `DataDeletionService` has been moved to a `services` directory,
    independent of controllers. This was done to reinforce that services
    are independent of controllers, and can be used outside of them.

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -330,7 +330,7 @@ import UserStorageController from './controllers/user-storage/user-storage-contr
 import { PushPlatformNotificationsController } from './controllers/push-platform-notifications/push-platform-notifications';
 import { MetamaskNotificationsController } from './controllers/metamask-notifications/metamask-notifications';
 import MetaMetricsDataDeletionController from './controllers/metametrics-data-deletion/metametrics-data-deletion';
-import { DataDeletionService } from './controllers/metametrics-data-deletion/services/services';
+import { DataDeletionService } from './services/data-deletion-service';
 
 export const METAMASK_CONTROLLER_EVENTS = {
   // Fired after state changes that impact the extension badge (unapproved msg count)
