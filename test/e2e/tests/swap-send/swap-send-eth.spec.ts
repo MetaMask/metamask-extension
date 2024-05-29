96c8e98681226beada311a5798963129c7e7b28a
Micaela Estabillo <micaela.estabillo@consensys.net>
2024-05-29 16:53:09 -0400

    chore: re-enable swap-send-eth test

--- test/e2e/tests/swap-send/swap-send-eth.spec.ts
+++ test/e2e/tests/swap-send/swap-send-eth.spec.ts
@@ -15,9 +15,7 @@ const RECIPIENT_ADDRESS = '0xc427D562164062a23a5cFf596A4a3208e72Acd28';
 
 describe('Swap-Send ETH', function () {
   describe('to non-contract address with data that matches swap data signature', function (this: Suite) {
-    // TODO: reenable this in follow-up
-    // eslint-disable-next-line mocha/no-skipped-tests
-    it.skip('submits a transaction successfully', async function () {
+    it('submits a transaction successfully', async function () {
       await withFixtures(
         getSwapSendFixtures(this.test?.fullTitle()),
         async ({
