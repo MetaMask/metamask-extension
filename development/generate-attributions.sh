ab1d5cea8e88791677b3cf4fd434f4e7f6073d70
Salah-Eddine Saakoun <salah-eddine.saakoun@consensys.net>
2024-05-30 11:39:07 +0200

    fix: attributions

--- development/generate-attributions.sh
+++ development/generate-attributions.sh
@@ -17,8 +17,10 @@ main() {
 
   yarn && yarn allow-scripts
 
-  # Add the `node_modules/.bin` directory to the PATH so that we can run the `allow-scripts` script.
-  # Because line 28 will try to run allow-scripts after installing the dependencies.
+  # Add the `node_modules/.bin` directory to the PATH
+  # This is necessary for `yarn workspaces focus --production` to work, as it
+  # requires the allow-script executable in bin, which is used by the
+  # `plugin-allow-scripts.cjs` defined in the .yarnrc file
   export PATH="${SCRIPT_DIRECTORY}/generate-attributions/node_modules/.bin:${PATH}"
 
   # Switching to the project directory explicitly, so that we can use paths
@@ -32,6 +34,12 @@ main() {
 
   # Generate attributions file
   yarn generate-attribution -o "${PROJECT_DIRECTORY}" -b "${PROJECT_DIRECTORY}"
+
+  # Check if the script is running in a CI environment (GitHub Actions sets the CI variable to true)
+  if [ -z "${CI:-}" ]; then
+    # If not running in CI, prompt to restore development dependencies
+    echo "To restore development dependencies, please run 'yarn install'"
+  fi
 }
 
 main "$@"
