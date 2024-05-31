aec1ebe0ac5ae220b3d6c6e7448ab738712d7228
legobt <6wbvkn0j@anonaddy.me>
2024-05-31 06:28:54 +0000

    chore: fix createUnsupportedMethodMiddleware return type

--- app/scripts/lib/rpc-method-middleware/createUnsupportedMethodMiddleware.ts
+++ app/scripts/lib/rpc-method-middleware/createUnsupportedMethodMiddleware.ts
@@ -1,4 +1,5 @@
 import type { JsonRpcMiddleware } from '@metamask/json-rpc-engine';
+import type { JsonRpcParams } from '@metamask/utils';
 import { rpcErrors } from '@metamask/rpc-errors';
 import { UNSUPPORTED_RPC_METHODS } from '../../../../shared/constants/network';
 
@@ -7,8 +8,8 @@ import { UNSUPPORTED_RPC_METHODS } from '../../../../shared/constants/network';
  * appropriate error.
  */
 export function createUnsupportedMethodMiddleware(): JsonRpcMiddleware<
-  unknown,
-  void
+  JsonRpcParams,
+  null
 > {
   return async function unsupportedMethodMiddleware(req, _res, next, end) {
     if ((UNSUPPORTED_RPC_METHODS as Set<string>).has(req.method)) {
