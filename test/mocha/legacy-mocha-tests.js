5140ac2bd54749b9300d0cadc67e87caca3a54a9
Mark Stacey <markjstacey@gmail.com>
2024-06-03 20:15:00 -0230

    test: Fix test config
    Our Mocha and Jest configuration has been messy because we're in the
    middle of a migration from Mocha to Jest. Each file had to be
    explicitly included in the Jest configuration or ignored in the Mocha
    configuration. This was inconvenient and error-prone, and resulted in
    some tests not being run at all.
    
    Both test configurations have been updated to use a shared list of
    Mocha test files. There are only a few of these files left, and this
    list should only get shorter as we migrate more tests to Jest. No
    further configuration changes will be needed to add Jest tests.

---
+++ test/mocha/legacy-mocha-tests.js
@@ -0,0 +1,10 @@
+// This is a list of remaining unit tests written for Mocha.
+// We are in the middle of a migration from Mocha to Jest. Please do not add any additional test
+// files to this list. We should only be removing files.
+const legacyMochaTests = [
+  'app/scripts/metamask-controller.actions.test.js',
+]
+
+module.exports = {
+  legacyMochaTests,
+}
