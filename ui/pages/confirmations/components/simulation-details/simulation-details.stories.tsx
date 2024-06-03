9bba25d13c57d62d8ec8f3c2d25a69745edd29ce
OGPoyraz <omergoktugpoyraz@gmail.com>
2024-06-03 10:13:24 +0200

    Fix simulation stories

--- ui/pages/confirmations/components/simulation-details/simulation-details.stories.tsx
+++ ui/pages/confirmations/components/simulation-details/simulation-details.stories.tsx
@@ -24,6 +24,9 @@ const ERC721_TOKEN_MOCK = '0x06012c8cf97bead5deae237070f9587f8e7a266d'; // Crypt
 const ERC1155_TOKEN_MOCK = '0x60e4d786628fea6478f785a6d7e704777c86a7c6'; // MAYC
 
 const storeMock = configureStore({
+  localeMessages: {
+    currentLocale: 'en',
+  },
   metamask: {
     ...mockState.metamask,
     preferences: {
@@ -112,33 +115,38 @@ export const MultipleTokens: Story = {
         difference: '0x12345678912345678',
         isDecrease: true,
       },
-      tokenBalanceChanges: [{
-        ...DUMMY_BALANCE_CHANGE,
-        address: ERC20_TOKEN_1_MOCK,
-        difference: '0x123456',
-        isDecrease: false,
-        standard: SimulationTokenStandard.erc20,
-      }, {
-        ...DUMMY_BALANCE_CHANGE,
-        address: ERC20_TOKEN_2_MOCK,
-        difference: '0x123456901',
-        isDecrease: false,
-        standard: SimulationTokenStandard.erc20,
-      }, {
-        ...DUMMY_BALANCE_CHANGE,
-        address: ERC721_TOKEN_MOCK,
-        difference: '0x1',
-        isDecrease: false,
-        id: '0x721',
-        standard: SimulationTokenStandard.erc721,
-      }, {
-        ...DUMMY_BALANCE_CHANGE,
-        address: ERC1155_TOKEN_MOCK,
-        difference: '0x13',
-        isDecrease: false,
-        id: '0x1155',
-        standard: SimulationTokenStandard.erc1155,
-      }],
+      tokenBalanceChanges: [
+        {
+          ...DUMMY_BALANCE_CHANGE,
+          address: ERC20_TOKEN_1_MOCK,
+          difference: '0x123456',
+          isDecrease: false,
+          standard: SimulationTokenStandard.erc20,
+        },
+        {
+          ...DUMMY_BALANCE_CHANGE,
+          address: ERC20_TOKEN_2_MOCK,
+          difference: '0x123456901',
+          isDecrease: false,
+          standard: SimulationTokenStandard.erc20,
+        },
+        {
+          ...DUMMY_BALANCE_CHANGE,
+          address: ERC721_TOKEN_MOCK,
+          difference: '0x1',
+          isDecrease: false,
+          id: '0x721',
+          standard: SimulationTokenStandard.erc721,
+        },
+        {
+          ...DUMMY_BALANCE_CHANGE,
+          address: ERC1155_TOKEN_MOCK,
+          difference: '0x13',
+          isDecrease: false,
+          id: '0x1155',
+          standard: SimulationTokenStandard.erc1155,
+        },
+      ],
     },
   },
 };
@@ -173,11 +181,11 @@ export const LongValuesAndNames: Story = {
           standard: SimulationTokenStandard.erc20,
         },
         {
-        ...DUMMY_BALANCE_CHANGE,
-        address: ERC20_TOKEN_2_MOCK,
-        difference: '0x123456901',
-        isDecrease: false,
-        standard: SimulationTokenStandard.erc20,
+          ...DUMMY_BALANCE_CHANGE,
+          address: ERC20_TOKEN_2_MOCK,
+          difference: '0x123456901',
+          isDecrease: false,
+          standard: SimulationTokenStandard.erc20,
         },
       ],
     },
