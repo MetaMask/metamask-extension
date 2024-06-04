66a7d54e5fc90df98b9c3e4ae1036160d7420c40
Salah-Eddine Saakoun <salah-eddine.saakoun@consensys.net>
2024-06-04 22:16:18 +0200

    fix: attribution bot command

--- development/attributions-check.sh
+++ development/attributions-check.sh
@@ -7,6 +7,6 @@ ATTRIBUTIONS_FILE="./attribution.txt"
 if ! git diff --exit-code "$ATTRIBUTIONS_FILE"; then
   echo "The set of attributions kept in \`attribution.txt\` are out of date."
   echo "Run \`yarn attributions:generate\` to regenerate them and commit and push the changes."
-  echo "If you're looking at a pull request, you can also post the comment \`@metamaskbot regenerate-attributions\` and the file will be automatically regenerated for you."
+  echo "If you're looking at a pull request, you can also post the comment \`@metamaskbot update-attributions\` and the file will be automatically regenerated for you."
   exit 1
 fi
