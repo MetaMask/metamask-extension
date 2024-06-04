cc22eeb3fa32d8a065374f55a3d787ba8d80ed3b
Charly Chevalier <charly.chevalier@consensys.net>
2024-06-04 16:08:12 +0200

    test(multichain): fix address tests + proper typing

--- app/scripts/lib/multichain/address.test.ts
+++ app/scripts/lib/multichain/address.test.ts
@@ -5,9 +5,23 @@ import {
   normalizeSafeAddress,
 } from './address';
 
+type TestAddress = {
+  address: string;
+  normalizedAddress: string;
+  checksumAddress: string;
+};
+
 const ETH_ADDRESSES = [
+  // Lower-case address
   {
     address: '0x6431726eee67570bf6f0cf892ae0a3988f03903f',
+    normalizedAddress: '0x6431726eee67570bf6f0cf892ae0a3988f03903f',
+    checksumAddress: '0x6431726EEE67570BF6f0Cf892aE0a3988F03903F',
+  },
+  // Checksum address
+  {
+    address: '0x6431726EEE67570BF6f0Cf892aE0a3988F03903F',
+    normalizedAddress: '0x6431726eee67570bf6f0cf892ae0a3988f03903f',
     checksumAddress: '0x6431726EEE67570BF6f0Cf892aE0a3988F03903F',
   },
 ];
@@ -25,61 +39,63 @@ function toLowerCaseEthAddress(address: string) {
   return `0x${stripHexPrefix(address).toLowerCase()}`;
 }
 
-function toUpperCaseEthAddress(address: string) {
-  return `0x${stripHexPrefix(address).toUpperCase()}`;
-}
-
 describe('address', () => {
   describe('isEthAddress', () => {
+    // @ts-expect-error This is missing from the Mocha type definitions
     it.each(ETH_ADDRESSES)(
       'returns true if address is an ethereum address: $address',
-      ({ address }) => {
+      ({ address }: TestAddress) => {
         expect(isEthAddress(address)).toBe(true);
         expect(isEthAddress(toLowerCaseEthAddress(address))).toBe(true);
-        expect(isEthAddress(toUpperCaseEthAddress(address))).toBe(true);
       },
     );
 
+    // @ts-expect-error This is missing from the Mocha type definitions
     it.each(NON_EVM_ADDRESSES)(
       'returns false if address is not an ethereum address: $address',
-      ({ address }) => {
+      ({ address }: TestAddress) => {
         expect(isEthAddress(address)).toBe(false);
       },
     );
   });
 
   describe('normalizeAddress', () => {
-    it.each(ETH_ADDRESSES)('normalizes address: $address', ({ address }) => {
-      expect(normalizeAddress(address)).toBe(address);
-      expect(normalizeAddress(toLowerCaseEthAddress(address))).toBe(address);
-      expect(normalizeAddress(toUpperCaseEthAddress(address))).toBe(address);
-    });
+    // @ts-expect-error This is missing from the Mocha type definitions
+    it.each(ETH_ADDRESSES)(
+      'normalizes address: $address',
+      ({ address, normalizedAddress }: TestAddress) => {
+        expect(normalizeAddress(address)).toBe(normalizedAddress);
+        expect(normalizeAddress(toLowerCaseEthAddress(address))).toBe(
+          normalizedAddress,
+        );
+      },
+    );
 
+    // @ts-expect-error This is missing from the Mocha type definitions
     it.each(NON_EVM_ADDRESSES)(
       'returns the original address if its a non-EVM address',
-      ({ address }) => {
+      ({ address }: TestAddress) => {
         expect(normalizeAddress(address)).toBe(address);
       },
     );
   });
 
   describe('normalizeSafeAddress', () => {
+    // @ts-expect-error This is missing from the Mocha type definitions
     it.each(ETH_ADDRESSES)(
       'normalizes address to its "safe" form: $address to: $checksumAddress',
-      ({ address, checksumAddress }) => {
+      ({ address, checksumAddress }: TestAddress) => {
         expect(normalizeSafeAddress(address)).toBe(checksumAddress);
         expect(normalizeSafeAddress(toLowerCaseEthAddress(address))).toBe(
-          checksumAddress,
-        );
-        expect(normalizeSafeAddress(toUpperCaseEthAddress(address))).toBe(
           checksumAddress,
         );
       },
     );
 
+    // @ts-expect-error This is missing from the Mocha type definitions
     it.each(NON_EVM_ADDRESSES)(
       'returns the original address if its a non-EVM address',
-      ({ address }) => {
+      ({ address }: TestAddress) => {
         expect(normalizeSafeAddress(address)).toBe(address);
       },
     );
