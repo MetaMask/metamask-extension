8a35726205be12db9ca435a373729854eb343691
Charly Chevalier <charly.chevalier@consensys.net>
2024-06-04 11:09:00 +0200

    docs(multichain): add missing docs for address helpers

--- app/scripts/lib/multichain/address.ts
+++ app/scripts/lib/multichain/address.ts
@@ -3,10 +3,22 @@ import { EthAddressStruct } from '@metamask/keyring-api';
 import { normalize as normalizeEthAddress } from '@metamask/eth-sig-util';
 import { toChecksumHexAddress } from '../../../../shared/modules/hexstring-utils';
 
+/**
+ * Checks if an address is an ethereum one.
+ *
+ * @param address - An address.
+ * @returns True if the address is an ethereum one, false otherwise.
+ */
 export function isEthAddress(address: string): boolean {
   return is(address, EthAddressStruct);
 }
 
+/**
+ * Normalize an address. The address might be returned as-is, if there's no normalizer available.
+ *
+ * @param address - An address to normalize.
+ * @returns The normalized address.
+ */
 export function normalizeAddress(address: string): string {
   // NOTE: We assume that the overhead over checking the address format
   // at runtime is small
@@ -15,6 +27,13 @@ export function normalizeAddress(address: string): string {
     : address;
 }
 
+/**
+ * Normalize an address to a "safer" representation. The address might be returned as-is, if
+ * there's no normalizer available.
+ *
+ * @param address - An address to normalize.
+ * @returns The "safer" normalized address.
+ */
 export function normalizeSafeAddress(address: string): string {
   // NOTE: We assume that the overhead over checking the address format
   // at runtime is small
