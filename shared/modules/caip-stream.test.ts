a9b741f177b0ff2f6d30596bb7b4d6ecd310f7e7
Jiexi Luan <jiexiluan@gmail.com>
2024-06-05 15:53:25 -0700

    add SplitStream specs

--- shared/modules/caip-stream.test.ts
+++ shared/modules/caip-stream.test.ts
@@ -10,7 +10,7 @@ import { WriteStream } from 'fs'
 
 describe('CAIP Stream', () => {
   describe('SplitStream', () => {
-    it('redirects writes to its substream', async () => {
+    it('redirects writes from the main stream to the substream', async () => {
       const splitStream = new SplitStream()
 
       const outerStreamChunks: unknown[] = []
@@ -33,6 +33,31 @@ describe('CAIP Stream', () => {
       await isWritten
       expect(outerStreamChunks).toStrictEqual([])
       expect(innerStreamChunks).toStrictEqual([{foo: 'bar'}])
+    })
+
+    it('redirects writes from the substream to the main stream', async () => {
+      const splitStream = new SplitStream()
+
+      const outerStreamChunks: unknown[] = []
+      splitStream.on('data', (chunk: unknown) => {
+        outerStreamChunks.push(chunk)
+      })
+
+      const innerStreamChunks: unknown[] = []
+      splitStream.substream.on('data', (chunk: unknown) => {
+        innerStreamChunks.push(chunk)
+      })
+
+      const {
+        promise: isWritten,
+        resolve: writeCallback,
+      } = deferredPromise();
+
+      splitStream.substream.write({foo: 'bar'}, writeCallback)
+
+      await isWritten
+      expect(outerStreamChunks).toStrictEqual([{foo: 'bar'}])
+      expect(innerStreamChunks).toStrictEqual([])
     })
   })
 })
