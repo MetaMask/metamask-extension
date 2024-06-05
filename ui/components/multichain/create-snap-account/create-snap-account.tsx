1b7e5eef10628ccdd25ecb6e93fbf3c624f11aa2
Kate Johnson <johnson.kat.g@gmail.com>
2024-06-05 13:03:17 -0400

    feat: added create-snap-account component

---
+++ ui/components/multichain/create-snap-account/create-snap-account.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import {
+  addNewAccount,
+  getNextAvailableAccountName as getNextAvailableAccountNameFromController,
+  setAccountLabel,
+} from '../../../store/actions';
+import { CreateAccount } from '..';
+
+type CreateSnapAccountProps = {
+  /**
+   * Callback called once the account has been created
+   */
+  onActionComplete: (completed: boolean) => Promise<void>;
+
+  /**
+   * Suggested account name from the snap
+   */
+  snapSuggestedAccountName?: string;
+};
+
+export const CreateSnapAccount: React.FC<CreateSnapAccountProps> = ({
+  onActionComplete,
+  snapSuggestedAccountName,
+}) => {
+  const dispatch = useDispatch();
+
+  const onCreateAccount = async (name: string) => {
+    const newAccountAddress = dispatch(addNewAccount()) as unknown as string;
+    if (newAccountAddress) {
+      if (name) {
+        dispatch(setAccountLabel(newAccountAddress, name));
+      }
+      await onActionComplete(true);
+    } else {
+      console.error(
+        'Failed to create new account or invalid account address type.',
+      );
+      await onActionComplete(false);
+    }
+  };
+
+  const getNextAccountName = async (): Promise<string> => {
+    const defaultAccountName =
+      await getNextAvailableAccountNameFromController();
+    return snapSuggestedAccountName || defaultAccountName;
+  };
+
+  return (
+    <CreateAccount
+      onActionComplete={onActionComplete}
+      onCreateAccount={onCreateAccount}
+      getNextAvailableAccountName={getNextAccountName}
+    />
+  );
+};
