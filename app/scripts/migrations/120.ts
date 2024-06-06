089e0c1e83fec7e9df6465591dc3d25b9dbe9697
Salim Toubal <salim.toubal@outlook.com>
2024-06-06 10:12:43 +0200

    fix: fix conflict rebase

--- app/scripts/migrations/120.ts
+++ app/scripts/migrations/120.ts
@@ -1,24 +1,16 @@
-import { cloneDeep, isObject } from 'lodash';
-import { NetworkType } from '@metamask/controller-utils';
+import { cloneDeep } from 'lodash';
 import { hasProperty } from '@metamask/utils';
-import { NetworkStatus } from '@metamask/network-controller';
-import {
-  CHAIN_IDS,
-  CHAIN_ID_TO_RPC_URL_MAP,
-  NETWORK_TYPES,
-  TEST_NETWORK_TICKER_MAP,
-  LINEA_SEPOLIA_DISPLAY_NAME,
-} from '../../../shared/constants/network';
 
 type VersionedData = {
   meta: { version: number };
   data: Record<string, unknown>;
 };
 
-export const version = 119;
+export const version = 120;
 
 /**
- * Migrates the user network to Linea Sepolia if the user is on Linea Goerli network.
+ * This migration removes the deprecated fields `contractExchangeRates`
+ * and `contractExchangeRatesByChainId` from the TokenRatesController.
  *
  * @param originalVersionedData - Versioned MetaMask extension state, exactly what we persist to dist.
  * @param originalVersionedData.meta - State metadata.
@@ -34,48 +26,12 @@ export async function migrate(
   transformState(versionedData.data);
   return versionedData;
 }
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function transformState(state: Record<string, any>) {
-  const NetworkController = state?.NetworkController || {};
-  const provider = NetworkController?.providerConfig || {};
-
-  if (provider?.chainId !== CHAIN_IDS.LINEA_GOERLI) {
-    return state;
+  if (hasProperty(state, 'TokenRatesController')) {
+    delete state.TokenRatesController.contractExchangeRates;
+    delete state.TokenRatesController.contractExchangeRatesByChainId;
   }
-  const networkControllerState = state.NetworkController;
-
-  if (
-    hasProperty(state, 'NetworkController') &&
-    isObject(state.NetworkController) &&
-    hasProperty(state.NetworkController, 'providerConfig') &&
-    isObject(state.NetworkController.providerConfig) &&
-    hasProperty(state.NetworkController.providerConfig, 'chainId') &&
-    state.NetworkController.providerConfig.chainId === CHAIN_IDS.LINEA_GOERLI
-  ) {
-    networkControllerState.providerConfig = {
-      type: NetworkType['linea-sepolia'],
-      rpcPrefs: {},
-      chainId: CHAIN_IDS.LINEA_SEPOLIA,
-      nickname: LINEA_SEPOLIA_DISPLAY_NAME,
-      rpcUrl: CHAIN_ID_TO_RPC_URL_MAP[CHAIN_IDS.LINEA_SEPOLIA],
-      providerType: NETWORK_TYPES.LINEA_SEPOLIA,
-      ticker: TEST_NETWORK_TICKER_MAP[NETWORK_TYPES.LINEA_SEPOLIA],
-      id: NETWORK_TYPES.LINEA_SEPOLIA,
-    };
-    networkControllerState.selectedNetworkClientId =
-      NETWORK_TYPES.LINEA_SEPOLIA;
-    networkControllerState.networksMetadata = {
-      ...networkControllerState.networksMetadata,
-      'linea-sepolia': {
-        EIPS: {
-          '1559': true,
-        },
-        status: NetworkStatus.Available,
-      },
-    };
-  }
-  return {
-    ...state,
-    NetworkController: networkControllerState,
-  };
+  return state;
 }
