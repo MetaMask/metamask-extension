2a1af94d209a45708f25fba15a8496ded2063a73
Charly Chevalier <charly.chevalier@consensys.net>
2024-06-06 11:40:43 +0200

    feat(ui): add new accounts selector + tests for isSelectedInternalAccountEth

--- ui/components/multichain/account-overview/account-overview.tsx
+++ ui/components/multichain/account-overview/account-overview.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { useI18nContext } from '../../../hooks/useI18nContext';
 import { BannerAlert, BannerAlertSeverity } from '../../component-library';
-import { isSelectedInternalAccountEth } from '../../../selectors';
+import { isSelectedInternalAccountEth } from '../../../selectors/accounts';
 import { AccountOverviewEth } from './account-overview-eth';
 import { AccountOverviewUnknown } from './account-overview-unknown';
 import { AccountOverviewCommonProps } from './common';
