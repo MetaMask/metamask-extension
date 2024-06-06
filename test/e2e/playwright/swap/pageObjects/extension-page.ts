867801c79e5823db46c2fecdf795c13699e820c4
Davide Brocchetto <davide.brocchetto@consensys.net>
2024-06-06 12:42:17 +0700

    adding Debugging info

--- test/e2e/playwright/swap/pageObjects/extension-page.ts
+++ test/e2e/playwright/swap/pageObjects/extension-page.ts
@@ -1,4 +1,5 @@
 import path from 'path';
+import * as fs from 'fs';
 import { type Page, chromium } from '@playwright/test';
 
 const wait = (n: number) => new Promise((resolve) => setTimeout(resolve, n));
@@ -20,7 +21,16 @@ export class ChromeExtensionPage {
     const context = await chromium.launchPersistentContext('', launchOptions);
     // let the extension load on the second tab of the browser
     await wait(2000);
+    console.log(context);
 
+    const filePath = 'dist/chrome/home.html';
+
+    // Check if the file exists
+    if (fs.existsSync(filePath)) {
+      console.log('File exists.');
+    } else {
+      console.log('File does not exist.');
+    }
     const pages = context.pages();
     return pages[1]; // return the page object of the second tab
   }
