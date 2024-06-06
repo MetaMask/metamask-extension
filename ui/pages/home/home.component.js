0383c5a070212501a243c31185ca56dbbb2349f3
sahar-fehri <sahar.fehri@consensys.net>
2024-06-06 21:40:12 +0200

    fix: fix code fencing import

--- ui/pages/home/home.component.js
+++ ui/pages/home/home.component.js
@@ -15,6 +15,7 @@ import WhatsNewPopup from '../../components/app/whats-new-popup';
 import { FirstTimeFlowType } from '../../../shared/constants/onboarding';
 import SmartTransactionsOptInModal from '../../components/app/smart-transactions/smart-transactions-opt-in-modal';
 import AutoDetectTokenModal from '../../components/app/auto-detect-token/auto-detect-token-modal';
+import AutoDetectNftModal from '../../components/app/auto-detect-nft/auto-detect-nft-modal';
 ///: END:ONLY_INCLUDE_IF
 import HomeNotification from '../../components/app/home-notification';
 import MultipleNotifications from '../../components/app/multiple-notifications';
@@ -72,9 +73,6 @@ import BetaHomeFooter from './beta/beta-home-footer.component';
 ///: BEGIN:ONLY_INCLUDE_IF(build-flask)
 import FlaskHomeFooter from './flask/flask-home-footer.component';
 ///: END:ONLY_INCLUDE_IF
-
-// eslint-disable-next-line import/order
-import AutoDetectNftModal from '../../components/app/auto-detect-nft/auto-detect-nft-modal';
 
 function shouldCloseNotificationPopup({
   isNotification,
@@ -185,6 +183,7 @@ export default class Home extends PureComponent {
     setTokenAutodetectModal: PropTypes.func,
     // eslint-disable-next-line react/no-unused-prop-types
     setShowTokenAutodetectModalOnUpgrade: PropTypes.func,
+    // eslint-disable-next-line react/no-unused-prop-types
     setNftAutodetectModal: PropTypes.func,
     // eslint-disable-next-line react/no-unused-prop-types
     setShowNftAutodetectModalOnUpgrade: PropTypes.func,
