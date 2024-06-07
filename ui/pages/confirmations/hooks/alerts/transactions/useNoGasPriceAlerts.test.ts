7c1ec03f0d99e0b5926b1929541bd46f12cc3942
Matthew Walsh <matthew.walsh@consensys.net>
2024-06-07 17:42:22 +0100

    Add unit tests for no gas price alert

---
+++ ui/pages/confirmations/hooks/alerts/transactions/useNoGasPriceAlerts.test.ts
@@ -0,0 +1,151 @@
+import { useGasEstimateFailedAlerts } from './useGasEstimateFailedAlerts';
+import { Severity } from '../../../../../helpers/constants/design-system';
+import { RowAlertKey } from '../../../../../components/app/confirm/info/row/constants';
+import { renderHookWithProvider } from '../../../../../../test/lib/render-helpers';
+import mockState from '../../../../../../test/data/mock-state.json';
+import {
+  GasFeeEstimateType,
+  TransactionMeta,
+  TransactionParams,
+  UserFeeLevel,
+} from '@metamask/transaction-controller';
+import { useNoGasPriceAlerts } from './useNoGasPriceAlerts';
+import {
+  GAS_ESTIMATE_TYPES,
+  GasFeeEstimates,
+} from '@metamask/gas-fee-controller';
+import { GasEstimateTypes } from '../../../../../../shared/constants/gas';
+
+const TRANSACTION_ID_MOCK = '123-456';
+const TRANSACTION_ID_MOCK_2 = '456-789';
+
+function buildState({
+  currentConfirmation,
+  gasEstimateType,
+  transaction,
+}: {
+  currentConfirmation?: Partial<TransactionMeta>;
+  gasEstimateType?: GasEstimateTypes;
+  transaction?: Partial<TransactionMeta>;
+} = {}) {
+  return {
+    ...mockState,
+    confirm: {
+      currentConfirmation,
+    },
+    metamask: {
+      ...mockState.metamask,
+      gasEstimateType,
+      transactions: transaction ? [transaction] : [],
+    },
+  };
+}
+
+function runHook({
+  currentConfirmation,
+  gasEstimateType,
+  transaction,
+}: {
+  currentConfirmation?: Partial<TransactionMeta>;
+  gasEstimateType?: GasEstimateTypes;
+  transaction?: Partial<TransactionMeta>;
+} = {}) {
+  const state = buildState({
+    currentConfirmation,
+    gasEstimateType,
+    transaction,
+  });
+  const response = renderHookWithProvider(useNoGasPriceAlerts, state);
+
+  return response.result.current;
+}
+
+describe('useNoGasPriceAlerts', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('returns no alerts if no confirmation', () => {
+    expect(runHook()).toEqual([]);
+  });
+
+  it('returns no alerts if no transaction matching confirmation', () => {
+    expect(
+      runHook({
+        currentConfirmation: { id: TRANSACTION_ID_MOCK },
+        transaction: {
+          id: TRANSACTION_ID_MOCK,
+          userFeeLevel: UserFeeLevel.MEDIUM,
+        },
+      }),
+    ).toEqual([]);
+  });
+
+  it('returns no alerts if transaction has custom gas fee and no fee estimate', () => {
+    expect(
+      runHook({
+        currentConfirmation: { id: TRANSACTION_ID_MOCK },
+        gasEstimateType: GasEstimateTypes.none,
+        transaction: {
+          id: TRANSACTION_ID_MOCK,
+          userFeeLevel: UserFeeLevel.CUSTOM,
+        },
+      }),
+    ).toEqual([]);
+  });
+
+  it('returns no alerts if transaction has dApp suggested gas fee and no fee estimate', () => {
+    expect(
+      runHook({
+        currentConfirmation: { id: TRANSACTION_ID_MOCK },
+        gasEstimateType: GasEstimateTypes.none,
+        transaction: {
+          id: TRANSACTION_ID_MOCK,
+          dappSuggestedGasFees: {
+            maxFeePerGas: '0x2',
+            maxPriorityFeePerGas: '0x1',
+          },
+          txParams: {
+            maxFeePerGas: '0x2',
+            maxPriorityFeePerGas: '0x1',
+          } as TransactionParams,
+        },
+      }),
+    ).toEqual([]);
+  });
+
+  it('returns no alerts if gas fee is not custom and has fee estimate', () => {
+    expect(
+      runHook({
+        currentConfirmation: { id: TRANSACTION_ID_MOCK },
+        gasEstimateType: GasEstimateTypes.feeMarket,
+        transaction: {
+          id: TRANSACTION_ID_MOCK,
+          userFeeLevel: UserFeeLevel.MEDIUM,
+        },
+      }),
+    ).toEqual([]);
+  });
+
+  it('returns alert if gas fee is not custom and no fee estimate', () => {
+    const alerts = runHook({
+      currentConfirmation: { id: TRANSACTION_ID_MOCK },
+      gasEstimateType: GasEstimateTypes.none,
+      transaction: {
+        id: TRANSACTION_ID_MOCK,
+        userFeeLevel: UserFeeLevel.MEDIUM,
+      },
+    });
+
+    expect(alerts).toEqual([
+      {
+        field: RowAlertKey.EstimatedFee,
+        isBlocking: true,
+        key: 'noGasPrice',
+        message: 'Gas price estimation failed due to network error.',
+        reason: 'No Gas Price',
+        severity: Severity.Danger,
+      },
+    ]);
+  });
+});
--- ui/pages/confirmations/hooks/alerts/transactions/useNoGasPriceAlerts.ts
+++ ui/pages/confirmations/hooks/alerts/transactions/useNoGasPriceAlerts.ts
@@ -1,16 +1,17 @@
 import { useSelector } from 'react-redux';
 import { Alert } from '../../../../../ducks/confirm-alerts/confirm-alerts';
-import { getNoGasPriceFetched, selectTransactionMetadata } from '../../../../../selectors';
+import { currentConfirmationSelector, getNoGasPriceFetched, selectTransactionMetadata } from '../../../../../selectors';
 import useCurrentConfirmation from '../../useCurrentConfirmation';
 import { TransactionMeta, UserFeeLevel } from '@metamask/transaction-controller';
 import { txParamsAreDappSuggested } from '../../../../../../shared/modules/transaction.utils';
 import { useMemo } from 'react';
 import { Severity } from '../../../../../helpers/constants/design-system';
 import { useI18nContext } from '../../../../../hooks/useI18nContext';
+import { RowAlertKey } from '../../../../../components/app/confirm/info/row/constants';
 
 export function useNoGasPriceAlerts(): Alert[] {
   const t = useI18nContext();
-  const {currentConfirmation} = useCurrentConfirmation();
+  const currentConfirmation = useSelector(currentConfirmationSelector);
   const transaction = (currentConfirmation ?? {}) as TransactionMeta;
   const { id: transactionId } = transaction;
   const transactionMeta = useSelector((state) => selectTransactionMetadata(state, transactionId));
@@ -25,7 +26,7 @@ export function useNoGasPriceAlerts(): Alert[] {
 
     return [
       {
-        field: 'estimatedFee',
+        field: RowAlertKey.EstimatedFee,
         isBlocking: true,
         key: 'noGasPrice',
         message: t('gasPriceFetchFailed'),
