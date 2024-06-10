6e9d89288bb05956cfd40fbc394e8b5416e78ae7
OGPoyraz <omergoktugpoyraz@gmail.com>
2024-06-10 12:21:58 +0200

    add function description

--- ui/hooks/useHideFiatForTestnet.ts
+++ ui/hooks/useHideFiatForTestnet.ts
@@ -2,6 +2,11 @@ import { useSelector } from 'react-redux';
 import { getShowFiatInTestnets, getCurrentChainId } from '../selectors';
 import { TEST_NETWORK_IDS } from '../../shared/constants/network';
 
+/**
+ * Returns true if the fiat value should be hidden for testnet networks.
+ *
+ * @returns boolean
+ */
 export const useHideFiatForTestnet = (): boolean => {
   const showFiatInTestnets = useSelector(getShowFiatInTestnets);
   const chainId = useSelector(getCurrentChainId);
