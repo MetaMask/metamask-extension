46bfee780634a67fb6f6b0ffef865c610099f446
Jiexi Luan <jiexiluan@gmail.com>
2024-06-11 15:03:16 -0700

    Fix createCaipStream specs

--- shared/modules/caip-stream.test.ts
+++ shared/modules/caip-stream.test.ts
@@ -1,153 +1,52 @@
 import { Duplex, PassThrough } from 'readable-stream';
 import { deferredPromise } from '../../app/scripts/lib/util';
-import {
-  createCaipStream,
-  SplitStream,
-  CaipToMultiplexStream,
-  MultiplexToCaipStream,
-} from './caip-stream';
+import { createCaipStream } from './caip-stream';
 
 const writeToStream = async (stream: Duplex, message: unknown) => {
   const { promise: isWritten, resolve: writeCallback } = deferredPromise();
 
-  stream.write(message, writeCallback);
+  stream.write(message, () => writeCallback());
   await isWritten;
 };
 
+const onData = (stream: Duplex): unknown[] => {
+  const chunks: unknown[] = [];
+  stream.on('data', (chunk: unknown) => {
+    chunks.push(chunk);
+  });
+
+  return chunks;
+};
+
+class MockStream extends Duplex {
+  chunks: unknown[] = [];
+
+  constructor() {
+    super({ objectMode: true });
+  }
+
+  _read() {
+    return undefined;
+  }
+
+  _write(
+    value: unknown,
+    _encoding: BufferEncoding,
+    callback: (error?: Error | null) => void,
+  ) {
+    this.chunks.push(value);
+    callback();
+  }
+}
+
 describe('CAIP Stream', () => {
-  describe('SplitStream', () => {
-    it('redirects writes from the main stream to the substream', async () => {
-      const splitStream = new SplitStream();
-
-      const outerStreamChunks: unknown[] = [];
-      splitStream.on('data', (chunk: unknown) => {
-        outerStreamChunks.push(chunk);
-      });
-
-      const innerStreamChunks: unknown[] = [];
-      splitStream.substream.on('data', (chunk: unknown) => {
-        innerStreamChunks.push(chunk);
-      });
-
-      await writeToStream(splitStream, { foo: 'bar' });
-
-      expect(outerStreamChunks).toStrictEqual([]);
-      expect(innerStreamChunks).toStrictEqual([{ foo: 'bar' }]);
-    });
-
-    it('redirects writes from the substream to the main stream', async () => {
-      const splitStream = new SplitStream();
-
-      const outerStreamChunks: unknown[] = [];
-      splitStream.on('data', (chunk: unknown) => {
-        outerStreamChunks.push(chunk);
-      });
-
-      const innerStreamChunks: unknown[] = [];
-      splitStream.substream.on('data', (chunk: unknown) => {
-        innerStreamChunks.push(chunk);
-      });
-
-      await writeToStream(splitStream.substream, { foo: 'bar' });
-
-      expect(outerStreamChunks).toStrictEqual([{ foo: 'bar' }]);
-      expect(innerStreamChunks).toStrictEqual([]);
-    });
-  });
-
-  describe('CaipToMultiplexStream', () => {
-    it('drops non caip-x messages', async () => {
-      const caipToMultiplexStream = new CaipToMultiplexStream();
-
-      const streamChunks: unknown[] = [];
-      caipToMultiplexStream.on('data', (chunk: unknown) => {
-        streamChunks.push(chunk);
-      });
-
-      await writeToStream(caipToMultiplexStream, { foo: 'bar' });
-      await writeToStream(caipToMultiplexStream, {
-        type: 'caip-wrong',
-        data: { foo: 'bar' },
-      });
-
-      expect(streamChunks).toStrictEqual([]);
-    });
-
-    it('rewraps caip-x messages into multiplexed `metamask-provider` messages', async () => {
-      const caipToMultiplexStream = new CaipToMultiplexStream();
-
-      const streamChunks: unknown[] = [];
-      caipToMultiplexStream.on('data', (chunk: unknown) => {
-        streamChunks.push(chunk);
-      });
-
-      await writeToStream(caipToMultiplexStream, {
-        type: 'caip-x',
-        data: { foo: 'bar' },
-      });
-
-      expect(streamChunks).toStrictEqual([
-        {
-          name: 'metamask-provider',
-          data: { foo: 'bar' },
-        },
-      ]);
-    });
-  });
-
-  describe('MultiplexToCaipStream', () => {
-    it('drops non multiplexed `metamask-provider` messages', async () => {
-      const multiplexToCaipStream = new MultiplexToCaipStream();
-
-      const streamChunks: unknown[] = [];
-      multiplexToCaipStream.on('data', (chunk: unknown) => {
-        streamChunks.push(chunk);
-      });
-
-      await writeToStream(multiplexToCaipStream, { foo: 'bar' });
-      await writeToStream(multiplexToCaipStream, {
-        name: 'wrong-multiplex',
-        data: { foo: 'bar' },
-      });
-
-      expect(streamChunks).toStrictEqual([]);
-    });
-
-    it('rewraps multiplexed `metamask-provider` messages into caip-x messages', async () => {
-      const multiplexToCaipStream = new MultiplexToCaipStream();
-
-      const streamChunks: unknown[] = [];
-      multiplexToCaipStream.on('data', (chunk: unknown) => {
-        streamChunks.push(chunk);
-      });
-
-      await writeToStream(multiplexToCaipStream, {
-        name: 'metamask-provider',
-        data: { foo: 'bar' },
-      });
-
-      expect(streamChunks).toStrictEqual([
-        {
-          type: 'caip-x',
-          data: { foo: 'bar' },
-        },
-      ]);
-    });
-  });
-
   describe('createCaipStream', () => {
-    it('pipes a caip-x message from source stream to the substream as a multiplexed `metamask-provider` message', async () => {
+    it('pipes and unwraps a caip-x message from source stream to the substream', async () => {
       const sourceStream = new PassThrough({ objectMode: true });
-      const sourceStreamChunks: unknown[] = [];
-      sourceStream.on('data', (chunk: unknown) => {
-        sourceStreamChunks.push(chunk);
-      });
+      const sourceStreamChunks = onData(sourceStream);
 
       const providerStream = createCaipStream(sourceStream);
-      const providerStreamChunks: unknown[] = [];
-      providerStream.on('data', (chunk: unknown) => {
-        providerStreamChunks.push(chunk);
-      });
+      const providerStreamChunks = onData(providerStream);
 
       await writeToStream(sourceStream, {
         type: 'caip-x',
@@ -157,34 +56,23 @@ describe('CAIP Stream', () => {
       expect(sourceStreamChunks).toStrictEqual([
         { type: 'caip-x', data: { foo: 'bar' } },
       ]);
-      expect(providerStreamChunks).toStrictEqual([
-        { name: 'metamask-provider', data: { foo: 'bar' } },
-      ]);
+      expect(providerStreamChunks).toStrictEqual([{ foo: 'bar' }]);
     });
 
-    it('pipes a multiplexed `metamask-provider` message from the substream to the source stream as a caip-x message', async () => {
-      // using a SplitStream here instead of PassThrough to prevent a loop
-      // when sourceStream gets written to at the end of the CAIP pipeline
-      const sourceStream = new SplitStream();
-      const sourceStreamChunks: unknown[] = [];
-      sourceStream.substream.on('data', (chunk: unknown) => {
-        sourceStreamChunks.push(chunk);
-      });
+    it('pipes and wraps a message from the substream to the source stream', async () => {
+      // using a fake stream here instead of PassThrough to prevent a loop
+      // when sourceStream gets written back to at the end of the CAIP pipeline
+      const sourceStream = new MockStream();
 
       const providerStream = createCaipStream(sourceStream);
-      const providerStreamChunks: unknown[] = [];
-      providerStream.on('data', (chunk: unknown) => {
-        providerStreamChunks.push(chunk);
-      });
 
       await writeToStream(providerStream, {
-        name: 'metamask-provider',
-        data: { foo: 'bar' },
+        foo: 'bar',
       });
 
       // Note that it's not possible to verify the output side of the internal SplitStream
       // instantiated inside createCaipStream as only the substream is actually exported
-      expect(sourceStreamChunks).toStrictEqual([
+      expect(sourceStream.chunks).toStrictEqual([
         { type: 'caip-x', data: { foo: 'bar' } },
       ]);
     });
