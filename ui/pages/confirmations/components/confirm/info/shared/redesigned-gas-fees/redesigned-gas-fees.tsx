e00ed641be82dd86b55ecd57186e602d6e676968
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2024-06-11 15:01:19 +0100

    wip

--- ui/pages/confirmations/components/confirm/info/shared/redesigned-gas-fees/redesigned-gas-fees.tsx
+++ ui/pages/confirmations/components/confirm/info/shared/redesigned-gas-fees/redesigned-gas-fees.tsx
@@ -48,131 +48,67 @@ import { EditGasIconButton } from '../edit-gas-icon/edit-gas-icon-button';
 
 // TODO(pnf): review localization once design and copy are finalized
 
-export const RedesignedGasFees = ({
-  transactionMeta,
-}: {
-  transactionMeta: TransactionMeta;
-}) => {
-  if (!transactionMeta?.txParams) {
-    return null;
+function getGasEstimate(
+  transactionMeta: TransactionMeta,
+  supportsEIP1559: boolean,
+  maxFeePerGas: string,
+  maxPriorityFeePerGas: string,
+): string {
+  const { gas: gasLimit } = (transactionMeta as TransactionMeta).txParams;
+
+  let gasEstimate;
+  if (supportsEIP1559) {
+    const baseFeePerGas = subtractHexes(maxFeePerGas, maxPriorityFeePerGas);
+
+    gasEstimate = getMinimumGasTotalInHexWei({
+      ...transactionMeta.txParams,
+      gasLimit,
+      baseFeePerGas,
+    });
+  } else {
+    gasEstimate = getMinimumGasTotalInHexWei({
+      ...transactionMeta.txParams,
+      gasLimit,
+    });
   }
 
-  const [showCustomizeGasPopover, setShowCustomizeGasPopover] = useState(false);
-  const closeCustomizeGasPopover = () => setShowCustomizeGasPopover(false);
-  const { supportsEIP1559 } = useSupportsEIP1559(transactionMeta);
+  return gasEstimate;
+}
 
-  const { maxFeePerGas, maxPriorityFeePerGas } =
-    useEIP1559TxFees(transactionMeta);
-
-  const gasEstimate = getGasEstimate(
-    transactionMeta,
-    supportsEIP1559,
-    maxFeePerGas,
-    maxPriorityFeePerGas,
-  );
-
-  const nativeCurrencyFees = getEthConversionFromWeiHex({
-    value: gasEstimate,
-    fromCurrency: EtherDenomination.GWEI,
-    numberOfDecimals: 4,
-  });
-
-  const currentCurrency = useSelector(getCurrentCurrency);
-  const conversionRate = useSelector(getConversionRate);
-
-  const fiatFormatter = useFiatFormatter();
-
-  const currentCurrencyFees = fiatFormatter(
-    Number(
-      getValueFromWeiHex({
-        value: gasEstimate,
-        conversionRate,
-        fromCurrency: EtherDenomination.GWEI,
-        toCurrency: currentCurrency,
-        numberOfDecimals: 2,
-      }),
-    ),
-  );
-
+const Type2TxGasModal = () => {
   return (
     <>
-      <GasFeeInfo
-        currentCurrencyFees={currentCurrencyFees}
-        nativeCurrencyFees={nativeCurrencyFees}
-        supportsEIP1559={supportsEIP1559}
-        setShowCustomizeGasPopover={setShowCustomizeGasPopover}
-        maxFeePerGas={maxFeePerGas}
-        maxPriorityFeePerGas={maxPriorityFeePerGas}
-      />
-
-      <Layer2GasFeesDetails
-        transactionMeta={transactionMeta}
-        gasEstimate={gasEstimate}
-      />
-
-      {!supportsEIP1559 && showCustomizeGasPopover && (
-        <Type0TxGasModal
-          closeCustomizeGasPopover={closeCustomizeGasPopover}
-          transactionMeta={transactionMeta}
-        />
-      )}
-      {supportsEIP1559 && <Type2TxGasModal />}
+      <EditGasFeePopover />
+      <AdvancedGasFeePopover />
     </>
   );
 };
 
-const GasFeeInfo = ({
-  currentCurrencyFees,
-  nativeCurrencyFees,
-  supportsEIP1559,
-  setShowCustomizeGasPopover,
-  maxFeePerGas,
-  maxPriorityFeePerGas,
+const Type0TxGasModal = ({
+  closeCustomizeGasPopover,
+  transactionMeta,
 }: {
-  currentCurrencyFees: string;
-  nativeCurrencyFees: string | undefined;
-  supportsEIP1559: boolean;
-  setShowCustomizeGasPopover: Dispatch<SetStateAction<boolean>>;
-  maxFeePerGas: string;
-  maxPriorityFeePerGas: string;
+  closeCustomizeGasPopover: () => void;
+  transactionMeta: TransactionMeta;
 }) => {
   return (
-    <>
-      <EditGasFeesRow
-        currentCurrencyFees={currentCurrencyFees}
-        nativeCurrencyFees={nativeCurrencyFees}
-        supportsEIP1559={supportsEIP1559}
-        setShowCustomizeGasPopover={setShowCustomizeGasPopover}
-      />
-      {supportsEIP1559 && (
-        <GasTimings
-          maxFeePerGas={maxFeePerGas}
-          maxPriorityFeePerGas={maxPriorityFeePerGas}
-        />
-      )}
-      <Divider />
-      <TotalGasFees nativeCurrencyFees={nativeCurrencyFees} />
-    </>
+    <EditGasPopover
+      onClose={closeCustomizeGasPopover}
+      mode={EditGasModes.modifyInPlace}
+      transaction={transactionMeta}
+    />
   );
 };
 
-const EditGasFeesRow = ({
-  currentCurrencyFees,
-  nativeCurrencyFees,
-  supportsEIP1559,
-  setShowCustomizeGasPopover,
+const TotalFeesRow = ({
+  currentCurrencyTotalFees,
+  nativeCurrencyTotalFees,
 }: {
-  currentCurrencyFees: string;
-  nativeCurrencyFees: string | undefined;
-  supportsEIP1559: boolean;
-  setShowCustomizeGasPopover: Dispatch<SetStateAction<boolean>>;
+  currentCurrencyTotalFees: string | null;
+  nativeCurrencyTotalFees: string | null | undefined;
 }) => {
   return (
-    <ConfirmInfoRow
-      label="Estimated fee"
-      variant={ConfirmInfoRowVariant.Default}
-      tooltip="estimated fee tooltip"
-    >
+    <ConfirmInfoRow label="Total" variant={ConfirmInfoRowVariant.Default}>
       <Box
         display={Display.Flex}
         flexDirection={FlexDirection.Row}
@@ -180,69 +116,97 @@ const EditGasFeesRow = ({
         alignItems={AlignItems.center}
         textAlign={TextAlign.Center}
         style={{ flexGrow: '1' }}
+        marginLeft={8}
       >
-        <Box
-          display={Display.Flex}
-          flexDirection={FlexDirection.Row}
-          justifyContent={JustifyContent.spaceEvenly}
-          alignItems={AlignItems.center}
-          textAlign={TextAlign.Center}
-          style={{ flexGrow: '1' }}
-        >
-          <Text color={TextColor.textAlternative}>{currentCurrencyFees}</Text>
-          <Text color={TextColor.textAlternative}>{nativeCurrencyFees}</Text>
-        </Box>
-
-        <EditGasIconButton
-          supportsEIP1559={supportsEIP1559}
-          setShowCustomizeGasPopover={setShowCustomizeGasPopover}
-        />
+        <Text color={TextColor.textAlternative}>
+          {currentCurrencyTotalFees}
+        </Text>
+        <Text color={TextColor.textAlternative}>{nativeCurrencyTotalFees}</Text>
       </Box>
     </ConfirmInfoRow>
   );
 };
 
-const GasTimings = ({
-  maxFeePerGas,
-  maxPriorityFeePerGas,
+const Layer1FeesRow = ({
+  currentCurrencyL1Fees,
+  nativeCurrencyL1Fees,
 }: {
-  maxFeePerGas: string;
-  maxPriorityFeePerGas: string;
+  currentCurrencyL1Fees: string | null;
+  nativeCurrencyL1Fees: string | null | undefined;
 }) => {
   return (
-    <ConfirmInfoRow label="Gas speed" variant={ConfirmInfoRowVariant.Default}>
-      <Box display={Display.Flex} alignItems={AlignItems.center}>
-        {/* TODO: Fix bug in the gas timing component after selection is made */}
-        <GasTiming
-          maxFeePerGas={maxFeePerGas}
-          maxPriorityFeePerGas={maxPriorityFeePerGas}
-        />
+    <ConfirmInfoRow label="L1 Fees" variant={ConfirmInfoRowVariant.Default}>
+      <Box
+        display={Display.Flex}
+        flexDirection={FlexDirection.Row}
+        justifyContent={JustifyContent.spaceBetween}
+        alignItems={AlignItems.center}
+        textAlign={TextAlign.Center}
+        style={{ flexGrow: '1' }}
+        marginLeft={8}
+      >
+        <Text color={TextColor.textAlternative}>{currentCurrencyL1Fees}</Text>
+        <Text color={TextColor.textAlternative}>{nativeCurrencyL1Fees}</Text>
       </Box>
     </ConfirmInfoRow>
   );
 };
 
-const Divider = () => (
-  <Box
-    borderColor={BorderColor.borderMuted}
-    borderWidth={1}
-    width={BlockSize.Full}
-  />
-);
-
-const TotalGasFees = ({
+const Layer2FeesRow = ({
+  currentCurrencyFees,
   nativeCurrencyFees,
 }: {
+  currentCurrencyFees: string;
   nativeCurrencyFees: string | undefined;
 }) => {
   return (
-    <ConfirmInfoRow
-      label="Total"
-      variant={ConfirmInfoRowVariant.Default}
-      tooltip="total tooltip"
-    >
-      <Text>{nativeCurrencyFees}</Text>
+    <ConfirmInfoRow label="L2 Fees" variant={ConfirmInfoRowVariant.Default}>
+      <Box
+        display={Display.Flex}
+        flexDirection={FlexDirection.Row}
+        justifyContent={JustifyContent.spaceBetween}
+        alignItems={AlignItems.center}
+        textAlign={TextAlign.Center}
+        style={{ flexGrow: '1' }}
+        marginLeft={8}
+      >
+        <Text color={TextColor.textAlternative}>{currentCurrencyFees}</Text>
+        <Text color={TextColor.textAlternative}>{nativeCurrencyFees}</Text>
+      </Box>
     </ConfirmInfoRow>
+  );
+};
+
+const Layer2GasFeesExpandBtn = ({
+  expandFeeDetails,
+  setExpandFeeDetails,
+}: {
+  expandFeeDetails: boolean;
+  setExpandFeeDetails: (currentExpandFeeDetails: boolean) => void;
+}) => {
+  const t = useI18nContext();
+
+  return (
+    <Box
+      padding={4}
+      display={Display.Flex}
+      alignItems={AlignItems.center}
+      justifyContent={JustifyContent.center}
+    >
+      <Button
+        style={{ textDecoration: 'none' }}
+        size={ButtonSize.Sm}
+        variant={ButtonVariant.Link}
+        endIconName={expandFeeDetails ? IconName.ArrowUp : IconName.ArrowDown}
+        color={IconColor.primaryDefault}
+        data-testid="expand-fee-details-button"
+        onClick={() => setExpandFeeDetails(!expandFeeDetails)}
+      >
+        <Text variant={TextVariant.bodySm} color={IconColor.primaryDefault}>
+          {t('feeDetails')}
+        </Text>
+      </Button>
+    </Box>
   );
 };
 
@@ -357,48 +321,67 @@ const Layer2GasFeesDetails = ({
   );
 };
 
-const Layer2GasFeesExpandBtn = ({
-  expandFeeDetails,
-  setExpandFeeDetails,
-}: {
-  expandFeeDetails: boolean;
-  setExpandFeeDetails: (currentExpandFeeDetails: boolean) => void;
-}) => {
-  const t = useI18nContext();
-
-  return (
-    <Box
-      padding={4}
-      display={Display.Flex}
-      alignItems={AlignItems.center}
-      justifyContent={JustifyContent.center}
-    >
-      <Button
-        style={{ textDecoration: 'none' }}
-        size={ButtonSize.Sm}
-        variant={ButtonVariant.Link}
-        endIconName={expandFeeDetails ? IconName.ArrowUp : IconName.ArrowDown}
-        color={IconColor.primaryDefault}
-        data-testid="expand-fee-details-button"
-        onClick={() => setExpandFeeDetails(!expandFeeDetails)}
-      >
-        <Text variant={TextVariant.bodySm} color={IconColor.primaryDefault}>
-          {t('feeDetails')}
-        </Text>
-      </Button>
-    </Box>
-  );
-};
-
-const Layer2FeesRow = ({
-  currentCurrencyFees,
+const TotalGasFees = ({
   nativeCurrencyFees,
 }: {
-  currentCurrencyFees: string;
   nativeCurrencyFees: string | undefined;
 }) => {
   return (
-    <ConfirmInfoRow label="L2 Fees" variant={ConfirmInfoRowVariant.Default}>
+    <ConfirmInfoRow
+      label="Total"
+      variant={ConfirmInfoRowVariant.Default}
+      tooltip="total tooltip"
+    >
+      <Text>{nativeCurrencyFees}</Text>
+    </ConfirmInfoRow>
+  );
+};
+
+const Divider = () => (
+  <Box
+    borderColor={BorderColor.borderMuted}
+    borderWidth={1}
+    width={BlockSize.Full}
+  />
+);
+
+const GasTimings = ({
+  maxFeePerGas,
+  maxPriorityFeePerGas,
+}: {
+  maxFeePerGas: string;
+  maxPriorityFeePerGas: string;
+}) => {
+  return (
+    <ConfirmInfoRow label="Gas speed" variant={ConfirmInfoRowVariant.Default}>
+      <Box display={Display.Flex} alignItems={AlignItems.center}>
+        {/* TODO: Fix bug in the gas timing component after selection is made */}
+        <GasTiming
+          maxFeePerGas={maxFeePerGas}
+          maxPriorityFeePerGas={maxPriorityFeePerGas}
+        />
+      </Box>
+    </ConfirmInfoRow>
+  );
+};
+
+const EditGasFeesRow = ({
+  currentCurrencyFees,
+  nativeCurrencyFees,
+  supportsEIP1559,
+  setShowCustomizeGasPopover,
+}: {
+  currentCurrencyFees: string;
+  nativeCurrencyFees: string | undefined;
+  supportsEIP1559: boolean;
+  setShowCustomizeGasPopover: Dispatch<SetStateAction<boolean>>;
+}) => {
+  return (
+    <ConfirmInfoRow
+      label="Estimated fee"
+      variant={ConfirmInfoRowVariant.Default}
+      tooltip="estimated fee tooltip"
+    >
       <Box
         display={Display.Flex}
         flexDirection={FlexDirection.Row}
@@ -406,115 +389,132 @@ const Layer2FeesRow = ({
         alignItems={AlignItems.center}
         textAlign={TextAlign.Center}
         style={{ flexGrow: '1' }}
-        marginLeft={8}
       >
-        <Text color={TextColor.textAlternative}>{currentCurrencyFees}</Text>
-        <Text color={TextColor.textAlternative}>{nativeCurrencyFees}</Text>
+        <Box
+          display={Display.Flex}
+          flexDirection={FlexDirection.Row}
+          justifyContent={JustifyContent.spaceEvenly}
+          alignItems={AlignItems.center}
+          textAlign={TextAlign.Center}
+          style={{ flexGrow: '1' }}
+        >
+          <Text color={TextColor.textAlternative}>{currentCurrencyFees}</Text>
+          <Text color={TextColor.textAlternative}>{nativeCurrencyFees}</Text>
+        </Box>
+
+        <EditGasIconButton
+          supportsEIP1559={supportsEIP1559}
+          setShowCustomizeGasPopover={setShowCustomizeGasPopover}
+        />
       </Box>
     </ConfirmInfoRow>
   );
 };
 
-const Layer1FeesRow = ({
-  currentCurrencyL1Fees,
-  nativeCurrencyL1Fees,
+const GasFeeInfo = ({
+  currentCurrencyFees,
+  nativeCurrencyFees,
+  supportsEIP1559,
+  setShowCustomizeGasPopover,
+  maxFeePerGas,
+  maxPriorityFeePerGas,
 }: {
-  currentCurrencyL1Fees: string | null;
-  nativeCurrencyL1Fees: string | null | undefined;
+  currentCurrencyFees: string;
+  nativeCurrencyFees: string | undefined;
+  supportsEIP1559: boolean;
+  setShowCustomizeGasPopover: Dispatch<SetStateAction<boolean>>;
+  maxFeePerGas: string;
+  maxPriorityFeePerGas: string;
 }) => {
-  return (
-    <ConfirmInfoRow label="L1 Fees" variant={ConfirmInfoRowVariant.Default}>
-      <Box
-        display={Display.Flex}
-        flexDirection={FlexDirection.Row}
-        justifyContent={JustifyContent.spaceBetween}
-        alignItems={AlignItems.center}
-        textAlign={TextAlign.Center}
-        style={{ flexGrow: '1' }}
-        marginLeft={8}
-      >
-        <Text color={TextColor.textAlternative}>{currentCurrencyL1Fees}</Text>
-        <Text color={TextColor.textAlternative}>{nativeCurrencyL1Fees}</Text>
-      </Box>
-    </ConfirmInfoRow>
-  );
-};
-
-const TotalFeesRow = ({
-  currentCurrencyTotalFees,
-  nativeCurrencyTotalFees,
-}: {
-  currentCurrencyTotalFees: string | null;
-  nativeCurrencyTotalFees: string | null | undefined;
-}) => {
-  return (
-    <ConfirmInfoRow label="Total" variant={ConfirmInfoRowVariant.Default}>
-      <Box
-        display={Display.Flex}
-        flexDirection={FlexDirection.Row}
-        justifyContent={JustifyContent.spaceBetween}
-        alignItems={AlignItems.center}
-        textAlign={TextAlign.Center}
-        style={{ flexGrow: '1' }}
-        marginLeft={8}
-      >
-        <Text color={TextColor.textAlternative}>
-          {currentCurrencyTotalFees}
-        </Text>
-        <Text color={TextColor.textAlternative}>{nativeCurrencyTotalFees}</Text>
-      </Box>
-    </ConfirmInfoRow>
-  );
-};
-
-const Type0TxGasModal = ({
-  closeCustomizeGasPopover,
-  transactionMeta,
-}: {
-  closeCustomizeGasPopover: () => void;
-  transactionMeta: TransactionMeta;
-}) => {
-  return (
-    <EditGasPopover
-      onClose={closeCustomizeGasPopover}
-      mode={EditGasModes.modifyInPlace}
-      transaction={transactionMeta}
-    />
-  );
-};
-
-const Type2TxGasModal = () => {
   return (
     <>
-      <EditGasFeePopover />
-      <AdvancedGasFeePopover />
+      <EditGasFeesRow
+        currentCurrencyFees={currentCurrencyFees}
+        nativeCurrencyFees={nativeCurrencyFees}
+        supportsEIP1559={supportsEIP1559}
+        setShowCustomizeGasPopover={setShowCustomizeGasPopover}
+      />
+      {supportsEIP1559 && (
+        <GasTimings
+          maxFeePerGas={maxFeePerGas}
+          maxPriorityFeePerGas={maxPriorityFeePerGas}
+        />
+      )}
+      <Divider />
+      <TotalGasFees nativeCurrencyFees={nativeCurrencyFees} />
     </>
   );
 };
 
-function getGasEstimate(
-  transactionMeta: TransactionMeta,
-  supportsEIP1559: boolean,
-  maxFeePerGas: string,
-  maxPriorityFeePerGas: string,
-): string {
-  const { gas: gasLimit } = (transactionMeta as TransactionMeta).txParams;
-
-  let gasEstimate;
-  if (supportsEIP1559) {
-    const baseFeePerGas = subtractHexes(maxFeePerGas, maxPriorityFeePerGas);
-
-    gasEstimate = getMinimumGasTotalInHexWei({
-      ...transactionMeta.txParams,
-      gasLimit,
-      baseFeePerGas,
-    });
-  } else {
-    gasEstimate = getMinimumGasTotalInHexWei({
-      ...transactionMeta.txParams,
-      gasLimit,
-    });
+export const RedesignedGasFees = ({
+  transactionMeta,
+}: {
+  transactionMeta: TransactionMeta;
+}) => {
+  if (!transactionMeta?.txParams) {
+    return null;
   }
 
-  return gasEstimate;
-}
+  const [showCustomizeGasPopover, setShowCustomizeGasPopover] = useState(false);
+  const closeCustomizeGasPopover = () => setShowCustomizeGasPopover(false);
+  const { supportsEIP1559 } = useSupportsEIP1559(transactionMeta);
+
+  const { maxFeePerGas, maxPriorityFeePerGas } =
+    useEIP1559TxFees(transactionMeta);
+
+  const gasEstimate = getGasEstimate(
+    transactionMeta,
+    supportsEIP1559,
+    maxFeePerGas,
+    maxPriorityFeePerGas,
+  );
+
+  const nativeCurrencyFees = getEthConversionFromWeiHex({
+    value: gasEstimate,
+    fromCurrency: EtherDenomination.GWEI,
+    numberOfDecimals: 4,
+  });
+
+  const currentCurrency = useSelector(getCurrentCurrency);
+  const conversionRate = useSelector(getConversionRate);
+
+  const fiatFormatter = useFiatFormatter();
+
+  const currentCurrencyFees = fiatFormatter(
+    Number(
+      getValueFromWeiHex({
+        value: gasEstimate,
+        conversionRate,
+        fromCurrency: EtherDenomination.GWEI,
+        toCurrency: currentCurrency,
+        numberOfDecimals: 2,
+      }),
+    ),
+  );
+
+  return (
+    <>
+      <GasFeeInfo
+        currentCurrencyFees={currentCurrencyFees}
+        nativeCurrencyFees={nativeCurrencyFees}
+        supportsEIP1559={supportsEIP1559}
+        setShowCustomizeGasPopover={setShowCustomizeGasPopover}
+        maxFeePerGas={maxFeePerGas}
+        maxPriorityFeePerGas={maxPriorityFeePerGas}
+      />
+
+      <Layer2GasFeesDetails
+        transactionMeta={transactionMeta}
+        gasEstimate={gasEstimate}
+      />
+
+      {!supportsEIP1559 && showCustomizeGasPopover && (
+        <Type0TxGasModal
+          closeCustomizeGasPopover={closeCustomizeGasPopover}
+          transactionMeta={transactionMeta}
+        />
+      )}
+      {supportsEIP1559 && <Type2TxGasModal />}
+    </>
+  );
+};
