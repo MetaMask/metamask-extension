42fbf95c46e381e0aefdd9096477c9ad2d9895d6
Jyoti Puri <jyotipuri@gmail.com>
2024-06-12 21:31:44 +0530

    update

--- ui/pages/confirmations/utils/date.ts
+++ ui/pages/confirmations/utils/date.ts
@@ -1,28 +1,12 @@
-const months = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
-];
+import { DateTime } from 'luxon';
 
 export const formatDate = (dateString: string) => {
   if (!dateString) {
     return dateString;
   }
-  const date = new Date(dateString);
-  const day = String(date.getUTCDate()).padStart(2, '0');
-  const month = months[date.getUTCMonth()];
-  const year = date.getUTCFullYear();
-  const hours = String(date.getUTCHours()).padStart(2, '0');
-  const minutes = String(date.getUTCMinutes()).padStart(2, '0');
 
-  return `${day} ${month} ${year}, ${hours}:${minutes}`;
+  return DateTime.fromISO(dateString)
+    .setLocale('en')
+    .setZone('utc')
+    .toFormat('dd LLLL yyyy, HH:mm');
 };
