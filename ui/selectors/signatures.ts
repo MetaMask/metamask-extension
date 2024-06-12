395e6553e11d642a9b07cdc0bd20eadaea67cf00
Matthew Walsh <matthew.walsh@consensys.net>
2024-06-12 21:04:55 +0100

    Extend unit tests for current confirmation hook

--- ui/selectors/signatures.ts
+++ ui/selectors/signatures.ts
@@ -1,20 +1,13 @@
 import { createSelector } from 'reselect';
+import { DefaultRootState } from 'react-redux';
 import {
   unapprovedMsgsSelector,
   unapprovedPersonalMsgsSelector,
   unapprovedTypedMessagesSelector,
 } from './transactions';
 import { createDeepEqualSelector } from './util';
-import { DefaultRootState } from 'react-redux';
-import { AbstractMessage, AbstractMessageParams } from '@metamask/message-manager';
 
-export const selectUnapprovedMessages = createSelector<
-  any,
-  any,
-  any,
-  any,
-  Record<string, AbstractMessage & {msgParams: AbstractMessageParams}>
->(
+export const selectUnapprovedMessages = createSelector(
   unapprovedMsgsSelector,
   unapprovedPersonalMsgsSelector,
   unapprovedTypedMessagesSelector,
