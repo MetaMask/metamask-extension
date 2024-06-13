c782bcd407481bded585125d6f1374f29c78d372
Charly Chevalier <charly.chevalier@consensys.net>
2024-06-13 11:00:06 +0200

    test(multichain): deep clone evm/non-evm state for each tests

--- ui/selectors/multichain.test.ts
+++ ui/selectors/multichain.test.ts
@@ -35,38 +35,42 @@ type TestState = AccountsState & {
   };
 };
 
-const MOCK_EVM_STATE: TestState = {
-  metamask: {
-    preferences: {
-      showFiatInTestnets: false,
-    },
-    providerConfig: {
-      ticker: 'ETH',
-      chainId: '0x1',
-    },
-    currentCurrency: 'ETH',
-    currencyRates: {
-      ETH: {
-        conversionRate: 'usd',
+function getEvmState(): TestState {
+  return {
+    metamask: {
+      preferences: {
+        showFiatInTestnets: false,
+      },
+      providerConfig: {
+        ticker: 'ETH',
+        chainId: '0x1',
+      },
+      currentCurrency: 'ETH',
+      currencyRates: {
+        ETH: {
+          conversionRate: 'usd',
+        },
+      },
+      completedOnboarding: true,
+      internalAccounts: {
+        selectedAccount: MOCK_ACCOUNT_EOA.id,
+        accounts: MOCK_ACCOUNTS,
       },
     },
-    completedOnboarding: true,
-    internalAccounts: {
-      selectedAccount: MOCK_ACCOUNT_EOA.id,
-      accounts: MOCK_ACCOUNTS,
-    },
-  },
-};
+  };
+}
 
-const MOCK_NON_EVM_STATE: AccountsState = {
-  metamask: {
-    ...MOCK_EVM_STATE.metamask,
-    internalAccounts: {
-      selectedAccount: MOCK_ACCOUNT_BIP122_P2WPKH.id,
-      accounts: MOCK_ACCOUNTS,
+function getNonEvmState(): TestState {
+  return {
+    metamask: {
+      ...getEvmState().metamask,
+      internalAccounts: {
+        selectedAccount: MOCK_ACCOUNT_BIP122_P2WPKH.id,
+        accounts: MOCK_ACCOUNTS,
+      },
     },
-  },
-};
+  };
+}
 
 function getBip122ProviderConfig(): MultichainProviderConfig {
   // For now, we only have Bitcoin non-EVM network, so we are expecting to have
@@ -77,7 +81,7 @@ function getBip122ProviderConfig(): MultichainProviderConfig {
 describe('Multichain Selectors', () => {
   describe('getMultichainNetworkProviders', () => {
     it('has some providers', () => {
-      const state = MOCK_EVM_STATE;
+      const state = getEvmState();
 
       const networkProviders = getMultichainNetworkProviders(state);
       expect(Array.isArray(networkProviders)).toBe(true);
@@ -87,21 +91,21 @@ describe('Multichain Selectors', () => {
 
   describe('getMultichainNetwork', () => {
     it('returns an EVM network provider if account is EVM', () => {
-      const state = MOCK_EVM_STATE;
+      const state = getEvmState();
 
       const network = getMultichainNetwork(state);
       expect(network.isEvmNetwork).toBe(true);
     });
 
     it('returns an non-EVM network provider if account is non-EVM', () => {
-      const state = MOCK_NON_EVM_STATE;
+      const state = getNonEvmState();
 
       const network = getMultichainNetwork(state);
       expect(network.isEvmNetwork).toBe(false);
     });
 
     it('returns an EVM network provider if user is not onboarded', () => {
-      const state = MOCK_EVM_STATE;
+      const state = getEvmState();
       state.metamask.completedOnboarding = false;
       state.metamask.internalAccounts.selectedAccount = '';
 
@@ -112,13 +116,13 @@ describe('Multichain Selectors', () => {
 
   describe('getMultichainIsEvm', () => {
     it('returns true if selected account is EVM compatible', () => {
-      const state = MOCK_EVM_STATE;
+      const state = getEvmState();
 
       expect(getMultichainIsEvm(state)).toBe(true);
     });
 
     it('returns false if selected account is not EVM compatible', () => {
-      const state = MOCK_NON_EVM_STATE;
+      const state = getNonEvmState();
 
       expect(getMultichainIsEvm(state)).toBe(false);
     });
@@ -126,13 +130,13 @@ describe('Multichain Selectors', () => {
 
   describe('getMultichain{ProviderConfig,CurrentNetwork}', () => {
     it('returns a ProviderConfig if account is EVM', () => {
-      const state = MOCK_EVM_STATE;
+      const state = getEvmState();
 
       expect(getMultichainProviderConfig(state)).toBe(getProviderConfig(state));
     });
 
     it('returns a MultichainProviderConfig if account is non-EVM (bip122:*)', () => {
-      const state = MOCK_NON_EVM_STATE;
+      const state = getNonEvmState();
 
       const bip122ProviderConfig = getBip122ProviderConfig();
       expect(getMultichainProviderConfig(state)).toBe(bip122ProviderConfig);
@@ -141,13 +145,13 @@ describe('Multichain Selectors', () => {
 
   describe('getMultichainNativeCurrency', () => {
     it('returns same native currency if account is EVM', () => {
-      const state = MOCK_EVM_STATE;
+      const state = getEvmState();
 
       expect(getMultichainNativeCurrency(state)).toBe(getNativeCurrency(state));
     });
 
     it('returns MultichainProviderConfig.ticker if account is non-EVM (bip122:*)', () => {
-      const state = MOCK_NON_EVM_STATE;
+      const state = getNonEvmState();
 
       const bip122ProviderConfig = getBip122ProviderConfig();
       expect(getMultichainNativeCurrency(state)).toBe(
@@ -158,7 +162,7 @@ describe('Multichain Selectors', () => {
 
   describe('getMultichainCurrentCurrency', () => {
     it('returns same currency currency if account is EVM', () => {
-      const state = MOCK_EVM_STATE;
+      const state = getEvmState();
 
       expect(getMultichainCurrentCurrency(state)).toBe(
         getCurrentCurrency(state),
@@ -169,7 +173,7 @@ describe('Multichain Selectors', () => {
     it.each(['usd', 'ETH'])(
       "returns current currency '%s' if account is EVM",
       (currency: string) => {
-        const state = MOCK_EVM_STATE;
+        const state = getEvmState();
 
         state.metamask.currentCurrency = currency;
         expect(getCurrentCurrency(state)).toBe(currency);
@@ -178,7 +182,7 @@ describe('Multichain Selectors', () => {
     );
 
     it('fallbacks to ticker as currency if account is non-EVM (bip122:*)', () => {
-      const state = MOCK_NON_EVM_STATE; // .currentCurrency = 'ETH'
+      const state = getNonEvmState(); // .currentCurrency = 'ETH'
 
       const bip122ProviderConfig = getBip122ProviderConfig();
       expect(getCurrentCurrency(state).toLowerCase()).not.toBe('usd');
@@ -190,13 +194,13 @@ describe('Multichain Selectors', () => {
 
   describe('getMultichainShouldShowFiat', () => {
     it('returns same value as getShouldShowFiat if account is EVM', () => {
-      const state = MOCK_EVM_STATE;
+      const state = getEvmState();
 
       expect(getMultichainShouldShowFiat(state)).toBe(getShouldShowFiat(state));
     });
 
     it('returns true if account is non-EVM', () => {
-      const state = MOCK_NON_EVM_STATE;
+      const state = getNonEvmState();
 
       expect(getMultichainShouldShowFiat(state)).toBe(true);
     });
@@ -204,7 +208,7 @@ describe('Multichain Selectors', () => {
 
   describe('getMultichainDefaultToken', () => {
     it('returns ETH if account is EVM', () => {
-      const state = MOCK_EVM_STATE;
+      const state = getEvmState();
 
       expect(getMultichainDefaultToken(state)).toEqual({
         symbol: 'ETH',
@@ -212,7 +216,7 @@ describe('Multichain Selectors', () => {
     });
 
     it('returns true if account is non-EVM (bip122:*)', () => {
-      const state = MOCK_NON_EVM_STATE;
+      const state = getNonEvmState();
 
       const bip122ProviderConfig = getBip122ProviderConfig();
       expect(getMultichainDefaultToken(state)).toEqual({
