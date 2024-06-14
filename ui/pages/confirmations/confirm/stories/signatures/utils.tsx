b19706f47800ea666f3ba4943a8e33435978e678
digiwand <20778143+digiwand@users.noreply.github.com>
2024-06-13 20:58:21 -0400

    fix: 404 ConfirmPage storybook error preventing from further bundling

--- ui/pages/confirmations/confirm/stories/signatures/utils.tsx
+++ ui/pages/confirmations/confirm/stories/signatures/utils.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { MemoryRouter, Route } from 'react-router-dom';
 import { cloneDeep } from 'lodash';
 import mockState from '../../../../../../test/data/mock-state.json';
 import configureStore from '../../../../../store/store';
@@ -35,7 +36,12 @@ export function SignatureStoryTemplate(
 
   return (
     <Provider store={store}>
-      <ConfirmPage />
+      {/* Adding the MemoryRouter and Route is a workaround to bypass a 404 error in storybook that
+        is caused when the 'ui/pages/confirmations/hooks/syncConfirmPath.ts' hook calls
+        history.replace. To avoid history.replace, we can provide a param id. */}
+      <MemoryRouter initialEntries={['/confirmation/:0']}>
+        <Route path="/confirmation/:id" render={() => <ConfirmPage />} />
+      </MemoryRouter>
     </Provider>
   );
 }
