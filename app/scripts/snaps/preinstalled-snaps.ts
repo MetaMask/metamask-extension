3d21f13131ef33a3c441aba56a3a209c1bcae4c7
Charly Chevalier <charly.chevalier@consensys.net>
2024-06-18 22:41:08 +0200

    feat: add bitcoin preinstalled snap

--- app/scripts/snaps/preinstalled-snaps.ts
+++ app/scripts/snaps/preinstalled-snaps.ts
@@ -1,8 +1,10 @@
 import type { PreinstalledSnap } from '@metamask/snaps-controllers';
 import MessageSigningSnap from '@metamask/message-signing-snap/dist/preinstalled-snap.json';
+import BitcoinSnap from './bitcoin-preinstalled-snap.json';
 
 const PREINSTALLED_SNAPS: readonly PreinstalledSnap[] = Object.freeze([
   MessageSigningSnap as PreinstalledSnap,
+  BitcoinSnap as PreinstalledSnap,
 ]);
 
 export default PREINSTALLED_SNAPS;
--- shared/constants/bitcoin-manager-snap.ts
+++ shared/constants/bitcoin-manager-snap.ts
@@ -1,7 +1,8 @@
 import { SnapId } from '@metamask/snaps-sdk';
 
 export const BITCOIN_MANAGER_SNAP_ID: SnapId =
-  'local:http://localhost:8080' as SnapId;
+  'npm:@metamask/bitcoin' as SnapId;
+  //'local:http://localhost:8080' as SnapId;
 
 // FIXME: This one should probably somewhere else with other CAIP-2 chain ID identifiers!
 export const BITCOIN_MANAGER_SCOPE_MAINNET =
