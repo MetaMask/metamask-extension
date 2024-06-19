643bb6a3bb9b51fab99b4f333f7fb656803a8341
Monte Lai <monte.lai@consensys.net>
2024-06-19 09:50:23 +0800

    fix: lint

--- ui/hooks/useMultichainSelector.ts
+++ ui/hooks/useMultichainSelector.ts
@@ -1,10 +1,11 @@
 import { useSelector, DefaultRootState } from 'react-redux';
-import { InternalAccount, isEvmAccountType } from '@metamask/keyring-api';
-import { getNativeCurrency } from '../ducks/metamask/metamask';
-import { useMultichainNetwork } from './useMultichainNetwork';
-import { getCurrentCurrency, getSelectedInternalAccount } from '../selectors';
+import { InternalAccount } from '@metamask/keyring-api';
+import { getSelectedInternalAccount } from '../selectors';
 
-export function useMultichainSelector<TState = DefaultRootState, TSelected = unknown>(
+export function useMultichainSelector<
+  TState = DefaultRootState,
+  TSelected = unknown,
+>(
   selector: (state: TState, account?: InternalAccount) => TSelected,
   account?: InternalAccount,
 ) {
