1e1d07f470b59ff4c02faf338177c62c5edec0d9
Kate Johnson <johnson.kat.g@gmail.com>
2024-06-20 10:43:01 -0400

    feat: added JSDOC to getValues in create-named-snap-account.js

--- ui/pages/confirmations/confirmation/templates/create-named-snap-account.js
+++ ui/pages/confirmations/confirmation/templates/create-named-snap-account.js
@@ -4,6 +4,18 @@ import {
   MetaMetricsEventName,
 } from '../../../../../shared/constants/metametrics';
 
+/**
+ * Returns the templated values to be consumed in the confirmation page.
+ *
+ * @param {object} pendingApproval - The pending confirmatioobject.
+ * @param {Function} t - Translation function.
+ * @param {object} actions - Object containing safe actions that the template can invoke.
+ * @param {object} _history - The application's history object (not used in this function).
+ * @param {object} _data - The data object passed into the template from the confirmation page (not
+ *   used in this function).
+ * @param {object} contexts - Context objects passed into the template from the confirmation page.
+ * @returns {object} An object containing templated values for the confirmation page.
+ */
 function getValues(pendingApproval, t, actions, _history, _data, contexts) {
   const { origin: snapId, snapName, requestData } = pendingApproval;
   const { address, snapSuggestedAccountName } = requestData;
