4f51e69bd462d64d44412f54607f248828b2c921
Jyoti Puri <jyotipuri@gmail.com>
2024-06-20 18:57:31 +0530

    Update

--- ui/pages/confirmations/components/confirm/info/typed-sign/permit-simulation/permit-simulation.tsx
+++ ui/pages/confirmations/components/confirm/info/typed-sign/permit-simulation/permit-simulation.tsx
@@ -56,7 +56,7 @@ const PermitSimulation: React.FC = () => {
         <ConfirmInfoRowText text={t('permitSimulationDetailInfo')} />
       </ConfirmInfoRow>
       <ConfirmInfoRow label={t('approve')}>
-        <Box style={{ alignSelf: 'flex-end', marginLeft: 'auto' }}>
+        <Box style={{ marginLeft: 'auto' }}>
           <Box display={Display.Flex}>
             <Box display={Display.Inline} marginInlineEnd={1}>
               <Text
