eb79585792aae57a5cd0bb5e049fc0b84bce55de
bowensanders <bowensanders@gmail.com>
2024-06-24 13:01:21 -0700

    initial commit

---
+++ test/e2e/snaps/test-snap-jsx.spec.js
@@ -0,0 +1,83 @@
+const {
+  defaultGanacheOptions,
+  withFixtures,
+  unlockWallet,
+  switchToNotificationWindow,
+  WINDOW_TITLES,
+} = require('../helpers');
+const FixtureBuilder = require('../fixture-builder');
+const { TEST_SNAPS_WEBSITE_URL } = require('./enums');
+
+describe('Test Snap JSX', function () {
+  it('can use JSX for snap dialog', async function () {
+    await withFixtures(
+      {
+        fixtures: new FixtureBuilder().build(),
+        ganacheOptions: defaultGanacheOptions,
+        title: this.test.fullTitle(),
+      },
+      async ({ driver }) => {
+        await unlockWallet(driver);
+
+        // navigate to test snaps page and connect to wasm snap
+        await driver.openNewPage(TEST_SNAPS_WEBSITE_URL);
+
+        // wait for page to load
+        await driver.waitForSelector({
+          text: 'Installed Snaps',
+          tag: 'h2',
+        });
+
+        const snapButton = await driver.findElement('#connectjsx');
+        await driver.scrollToElement(snapButton);
+        await driver.delay(1000);
+        await driver.clickElement('#connectjsx');
+
+        // switch to metamask extension and click connect
+        await switchToNotificationWindow(driver);
+        await driver.clickElement({
+          text: 'Connect',
+          tag: 'button',
+        });
+
+        await driver.waitForSelector({ text: 'Confirm' });
+
+        await driver.clickElement({
+          text: 'Confirm',
+          tag: 'button',
+        });
+
+        await driver.waitForSelector({ text: 'OK' });
+
+        await driver.clickElement({
+          text: 'OK',
+          tag: 'button',
+        });
+
+        // click send inputs on test snap page
+        await driver.switchToWindowWithTitle(WINDOW_TITLES.TestSnaps);
+
+        // wait for npm installation success
+        await driver.waitForSelector({
+          css: '#connectjsx',
+          text: 'Reconnect to JSX Snap',
+        });
+
+        // click on show jsx dialog
+        await driver.clickElement('#displayJsx');
+
+        // switch to dialog window
+        await switchToNotificationWindow(driver);
+
+        // click increment
+        await driver.clickElement('#increment');
+
+        // wait for count to be 2
+        await driver.waitForSelector({
+          text: 'Count:',
+          tag: 'b',
+        });
+      },
+    );
+  });
+});
