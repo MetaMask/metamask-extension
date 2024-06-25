c164a35f02fe731a58876f446c80801ff51dbd4e
Micaela Estabillo <micaela.estabillo@consensys.net>
2024-06-25 14:10:55 -0700

    chore: create placeholder bridge page

---
+++ ui/pages/bridge/index.tsx
@@ -0,0 +1,138 @@
+import React, { useContext } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Switch, useLocation, useHistory } from 'react-router-dom';
+import classnames from 'classnames';
+import { I18nContext } from '../../contexts/i18n';
+
+import {
+  clearSwapsState,
+  getSwapsFeatureIsLive,
+  setTransactionSettingsOpened,
+} from '../../ducks/swaps/swaps';
+import {
+  AWAITING_SIGNATURES_ROUTE,
+  AWAITING_SWAP_ROUTE,
+  SMART_TRANSACTION_STATUS_ROUTE,
+  DEFAULT_ROUTE,
+  SWAPS_MAINTENANCE_ROUTE,
+  PREPARE_SWAP_ROUTE,
+  CROSS_CHAIN_SWAP_ROUTE,
+} from '../../helpers/constants/routes';
+
+import { resetBackgroundSwapsState } from '../../store/actions';
+
+import FeatureToggledRoute from '../../helpers/higher-order-components/feature-toggled-route';
+import { Icon, IconName, IconSize } from '../../components/component-library';
+import Box from '../../components/ui/box';
+import {
+  DISPLAY,
+  JustifyContent,
+  IconColor,
+  FRACTIONS,
+} from '../../helpers/constants/design-system';
+import { PrepareBridgePage } from './prepare/prepare-bridge-page';
+
+const CrossChainSwap = () => {
+  const t = useContext(I18nContext);
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const { pathname } = useLocation();
+  const isAwaitingSwapRoute = pathname === AWAITING_SWAP_ROUTE;
+  const isAwaitingSignaturesRoute = pathname === AWAITING_SIGNATURES_ROUTE;
+  const isSmartTransactionStatusRoute =
+    pathname === SMART_TRANSACTION_STATUS_ROUTE;
+  const isPrepareSwapRoute = pathname === PREPARE_SWAP_ROUTE;
+  const swapsEnabled = useSelector(getSwapsFeatureIsLive);
+
+  const redirectToDefaultRoute = async () => {
+    history.push({
+      pathname: DEFAULT_ROUTE,
+      // @ts-expect-error - property 'state' does not exist on type PartialPath.
+      state: { stayOnHomePage: true },
+    });
+    dispatch(clearSwapsState());
+    await dispatch(resetBackgroundSwapsState());
+  };
+
+  return (
+    <div className="bridge">
+      <div className="bridge__container">
+        <div className="bridge__header">
+          <Box
+            display={DISPLAY.FLEX}
+            justifyContent={JustifyContent.center}
+            marginLeft={4}
+            width={FRACTIONS.ONE_TWELFTH}
+            tabIndex={0}
+            onKeyUp={(e) => {
+              if (e.key === 'Enter') {
+                redirectToDefaultRoute();
+              }
+            }}
+          >
+            {!isAwaitingSwapRoute &&
+              !isAwaitingSignaturesRoute &&
+              !isSmartTransactionStatusRoute && (
+                <Icon
+                  name={IconName.Arrow2Left}
+                  size={IconSize.Lg}
+                  color={IconColor.iconAlternative}
+                  onClick={redirectToDefaultRoute}
+                  style={{ cursor: 'pointer' }}
+                  title={t('cancel')}
+                />
+              )}
+          </Box>
+
+          <div className="bridge__title">{t('bridge')}</div>
+
+          <Box
+            display={DISPLAY.FLEX}
+            justifyContent={JustifyContent.center}
+            marginRight={4}
+            width={FRACTIONS.ONE_TWELFTH}
+            tabIndex={0}
+            onKeyUp={(e) => {
+              if (e.key === 'Enter') {
+                dispatch(setTransactionSettingsOpened(true));
+              }
+            }}
+          >
+            {isPrepareSwapRoute && (
+              <Icon
+                name={IconName.Setting}
+                size={IconSize.Lg}
+                color={IconColor.iconAlternative}
+                onClick={() => {
+                  dispatch(setTransactionSettingsOpened(true));
+                }}
+                style={{ cursor: 'pointer' }}
+                title={t('transactionSettings')}
+              />
+            )}
+          </Box>
+        </div>
+        <div
+          className={classnames(
+            'bridge__content',
+            'bridge__content--redesign-enabled',
+          )}
+        >
+          <Switch>
+            <FeatureToggledRoute
+              redirectRoute={SWAPS_MAINTENANCE_ROUTE}
+              flag={swapsEnabled}
+              path={CROSS_CHAIN_SWAP_ROUTE + PREPARE_SWAP_ROUTE}
+              render={() => {
+                return <PrepareBridgePage />;
+              }}
+            />
+          </Switch>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CrossChainSwap;
---
+++ ui/pages/bridge/prepare/prepare-bridge-page.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useSelector, shallowEqual } from 'react-redux';
+import { isEqual, shuffle } from 'lodash';
+import PrepareSwapPage from '../../swaps/prepare-swap-page/prepare-swap-page';
+import { getSelectedAccount, getTokenList } from '../../../selectors';
+
+export const PrepareBridgePage = () => {
+  const selectedAccount = useSelector(getSelectedAccount, shallowEqual);
+  const { balance: ethBalance, address: selectedAccountAddress } =
+    selectedAccount;
+
+  const tokenList = useSelector(getTokenList, isEqual);
+  const shuffledTokensList = shuffle(Object.values(tokenList));
+
+  // TODO copy in effects from PrepareSwapPage as needed
+  // TODO implement new UI
+  return (
+    <div>
+      <PrepareSwapPage
+        ethBalance={ethBalance}
+        selectedAccountAddress={selectedAccountAddress}
+        shuffledTokensList={shuffledTokensList}
+      />
+    </div>
+  );
+};
