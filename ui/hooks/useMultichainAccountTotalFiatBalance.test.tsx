4e2596f295da747611fe0ed849d896e4d6800853
Monte Lai <monte.lai@consensys.net>
2024-06-26 23:36:50 +0800

    fix: multichain account list item (#25437)
    ## **Description**
    
    This PR updates the account list item to support displaying multichain
    balances.
    
    ## **Related issues**
    
    Fixes https://github.com/MetaMask/accounts-planning/issues/425
    
    ## **Manual testing steps**
    
    This does not affect evm accounts, their balances and behaviour should
    be the same as before.
    
    1. Add a BTC account
    2. Click on the account picker
    3. See that there are balances for BTC now.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    
    
    ### **After**
    
    ![Screenshot 2024-06-26 at 23 14
    18](https://github.com/MetaMask/metamask-extension/assets/96463427/60d7d1e7-9ef1-434b-8f6c-1acfd8a099a3)
    
    
    ## **Pre-merge author checklist**
    
    - [x] I've followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
    Extension Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- ui/hooks/useMultichainAccountTotalFiatBalance.test.tsx
+++ ui/hooks/useMultichainAccountTotalFiatBalance.test.tsx
@@ -1,63 +1,90 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import { renderHook } from '@testing-library/react-hooks';
-
+import { Provider } from 'react-redux';
+import { BtcAccountType, InternalAccount } from '@metamask/keyring-api';
 import mockState from '../../test/data/mock-state.json';
 import configureStore from '../store/store';
-
-import { CHAIN_IDS } from '../../shared/constants/network';
 import { createMockInternalAccount } from '../../test/jest/mocks';
-import { useAccountTotalFiatBalance } from './useAccountTotalFiatBalance';
+import { CHAIN_IDS } from '../../shared/constants/network';
+import { useMultichainAccountTotalFiatBalance } from './useMultichainAccountTotalFiatBalance';
 
-const mockAccount = createMockInternalAccount({
-  address: '0x0836f5ed6b62baf60706fe3adc0ff0fd1df833da',
-});
+const mockTokenBalances = [
+  {
+    address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+    balance: '48573',
+    balanceError: null,
+    decimals: 6,
+    image: undefined,
+    isERC721: undefined,
+    string: '0.04857',
+    symbol: 'USDC',
+  },
+  {
+    address: '0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e',
+    symbol: 'YFI',
+    balance: '1409247882142934',
+    decimals: 18,
+    string: '0.001409247882142934',
+    balanceError: null,
+  },
+];
 
 jest.mock('./useTokenTracker', () => {
   return {
     useTokenTracker: () => ({
       loading: false,
-      tokensWithBalances: [
-        {
-          address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
-          balance: '48573',
-          balanceError: null,
-          decimals: 6,
-          image: undefined,
-          isERC721: undefined,
-          string: '0.04857',
-          symbol: 'USDC',
-        },
-        {
-          address: '0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e',
-          symbol: 'YFI',
-          balance: '1409247882142934',
-          decimals: 18,
-          string: '0.001409247882142934',
-          balanceError: null,
-        },
-      ],
+      tokensWithBalances: mockTokenBalances,
       error: null,
     }),
   };
 });
 
-const renderUseAccountTotalFiatBalance = (address) => {
+const mockAccount = createMockInternalAccount({
+  name: 'Account 1',
+  address: '0x0836f5ed6b62baf60706fe3adc0ff0fd1df833da',
+  snapOptions: null,
+});
+const mockNonEvmAccount = {
+  ...mockAccount,
+  id: 'b7893c59-e376-4cc0-93ad-05ddaab574a6',
+  addres: 'bc1qn3stuu6g37rpxk3jfxr4h4zmj68g0lwxx5eker',
+  type: BtcAccountType.P2wpkh,
+};
+
+const renderUseMultichainAccountTotalFiatBalance = (
+  account: InternalAccount,
+) => {
   const state = {
     ...mockState,
     metamask: {
       ...mockState.metamask,
+      completedOnboarding: true,
+      internalAccounts: {
+        accounts: {
+          [mockAccount.id]: mockAccount,
+          [mockNonEvmAccount.id]: mockNonEvmAccount,
+        },
+        selectedAccount: mockAccount.id,
+      },
+      balances: {
+        [mockNonEvmAccount.id]: {
+          'bip122:000000000019d6689c085ae165831e93/slip44:0': {
+            amount: '1.00000000',
+            unit: 'BTC',
+          },
+        },
+      },
+      rates: {
+        btc: {
+          conversionDate: 0,
+          conversionRate: '100000',
+        },
+      },
       currentCurrency: 'usd',
       currencyRates: {
         ETH: {
           conversionRate: 1612.92,
         },
-      },
-      internalAccounts: {
-        accounts: {
-          [mockAccount.id]: mockAccount,
-        },
-        selectedAccount: mockAccount.id,
       },
       marketData: {
         [CHAIN_IDS.MAINNET]: {
@@ -100,45 +127,25 @@ const renderUseAccountTotalFiatBalance = (address) => {
     },
   };
 
-  const wrapper = ({ children }) => (
+  const wrapper = ({ children }: { children: React.ReactNode }) => (
     <Provider store={configureStore(state)}>{children}</Provider>
   );
 
-  return renderHook(() => useAccountTotalFiatBalance(address), { wrapper });
+  return renderHook(() => useMultichainAccountTotalFiatBalance(account), {
+    wrapper,
+  });
 };
 
-describe('useAccountTotalFiatBalance', () => {
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
+describe('useMultichainAccountTotalFiatBalance', () => {
+  it('return uses useAccountTotalFiatBalance if the an EVM account is passed', () => {
+    const { result } = renderUseMultichainAccountTotalFiatBalance(mockAccount);
 
-  it('should render the correct result for account 1', () => {
-    const { result } = renderUseAccountTotalFiatBalance(mockAccount);
     expect(result.current).toStrictEqual({
       formattedFiat: '$9.41',
-      totalWeiBalance: '14ba1e6a08a9ed',
-      totalFiatBalance: '9.41',
-      tokensWithBalances: [
-        {
-          address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
-          symbol: 'USDC',
-          balance: '48573',
-          image: undefined,
-          isERC721: undefined,
-          decimals: 6,
-          string: '0.04857',
-          balanceError: null,
-        },
-        {
-          address: '0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e',
-          symbol: 'YFI',
-          balance: '1409247882142934',
-          decimals: 18,
-          string: '0.001409247882142934',
-          balanceError: null,
-        },
-      ],
       loading: false,
+      totalWeiBalance: '14ba1e6a08a9ed',
+      tokensWithBalances: mockTokenBalances,
+      totalFiatBalance: '9.41',
       orderedTokenList: [
         {
           fiatBalance: '1.85',
@@ -173,6 +180,26 @@ describe('useAccountTotalFiatBalance', () => {
           symbol: 'YFI',
         },
       ],
+    });
+  });
+
+  it('returns the total fiat balance for a non-EVM account', () => {
+    const { result } =
+      renderUseMultichainAccountTotalFiatBalance(mockNonEvmAccount);
+
+    expect(result.current).toStrictEqual({
+      formattedFiat: '$100,000.00',
+      loading: false,
+      orderedTokenList: [
+        {
+          fiatBalance: '100000',
+          iconUrl: './images/bitcoin-logo.svg',
+          symbol: 'BTC',
+        },
+      ],
+      tokensWithBalances: [],
+      totalFiatBalance: '100000',
+      totalWeiBalance: '',
     });
   });
 });
