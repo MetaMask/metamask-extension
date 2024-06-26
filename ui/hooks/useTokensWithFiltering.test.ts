51e0c74cf9123980845d1f7f5e197be70a968788
Micaela Estabillo <micaela.estabillo@consensys.net>
2024-06-26 15:39:00 -0700

    chore: add useTokensWithFiltering hook

---
+++ ui/hooks/useTokensWithFiltering.test.ts
@@ -0,0 +1,153 @@
+import { renderHookWithProvider } from '../../test/lib/render-helpers';
+import { createBridgeMockStore } from '../../test/jest/mock-store';
+import { STATIC_MAINNET_TOKEN_LIST } from '../../shared/constants/tokens';
+import {
+  SWAPS_CHAINID_DEFAULT_TOKEN_MAP,
+  SwapsTokenObject,
+  TokenBucketPriority,
+} from '../../shared/constants/swaps';
+import { useTokensWithFiltering } from './useTokensWithFiltering';
+
+const mockUseTokenTracker = jest
+  .fn()
+  .mockReturnValue({ tokensWithBalances: [] });
+jest.mock('./useTokenTracker', () => ({
+  useTokenTracker: () => mockUseTokenTracker(),
+}));
+
+const TEST_CHAIN_ID = '0x1';
+const NATIVE_TOKEN = SWAPS_CHAINID_DEFAULT_TOKEN_MAP[TEST_CHAIN_ID];
+
+const MOCK_TOP_ASSETS = [
+  { address: '0x6b3595068778dd592e39a122f4f5a5cf09c90fe2' }, // UNI
+  { address: NATIVE_TOKEN.address },
+  { address: '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984' }, // USDC
+  { address: '0xdac17f958d2ee523a2206206994597c13d831ec7' }, // USDT
+];
+
+const MOCK_TOKEN_LIST_BY_ADDRESS: Record<string, SwapsTokenObject> = {
+  [NATIVE_TOKEN.address]: NATIVE_TOKEN,
+  ...STATIC_MAINNET_TOKEN_LIST,
+};
+
+describe('useTokensWithFiltering should return token list generator', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('when chainId === activeChainId and sorted by topAssets', () => {
+    const mockStore = createBridgeMockStore();
+    const { result } = renderHookWithProvider(
+      () =>
+        useTokensWithFiltering(
+          MOCK_TOKEN_LIST_BY_ADDRESS,
+          MOCK_TOP_ASSETS,
+          TokenBucketPriority.top,
+          TEST_CHAIN_ID,
+        ),
+      mockStore,
+    );
+
+    expect(result.current).toHaveLength(1);
+    expect(typeof result.current).toStrictEqual('function');
+    const tokenGenerator = result.current(() => true);
+    expect(tokenGenerator.next().value).toStrictEqual({
+      address: '0x0000000000000000000000000000000000000000',
+      balance: undefined,
+      decimals: 18,
+      iconUrl: './images/eth_logo.svg',
+      identiconAddress: null,
+      image: './images/eth_logo.svg',
+      name: 'Ether',
+      primaryLabel: 'ETH',
+      rawFiat: '',
+      rightPrimaryLabel: undefined,
+      rightSecondaryLabel: '',
+      secondaryLabel: 'Ether',
+      symbol: 'ETH',
+      type: 'NATIVE',
+    });
+    expect(tokenGenerator.next().value).toStrictEqual({
+      address: '0x6b3595068778dd592e39a122f4f5a5cf09c90fe2',
+      aggregators: [],
+      balance: undefined,
+      decimals: 18,
+      erc20: true,
+      erc721: false,
+      iconUrl:
+        'https://static.cx.metamask.io/api/v1/tokenIcons/1/0x6b3595068778dd592e39a122f4f5a5cf09c90fe2.png',
+      identiconAddress: null,
+      image: 'images/contract/sushi.svg',
+      name: 'SushiSwap',
+      primaryLabel: 'SUSHI',
+      rawFiat: '',
+      rightPrimaryLabel: undefined,
+      rightSecondaryLabel: '',
+      secondaryLabel: 'SushiSwap',
+      symbol: 'SUSHI',
+      type: 'TOKEN',
+    });
+  });
+
+  it('when chainId === activeChainId and sorted by balance', () => {
+    const mockStore = createBridgeMockStore();
+    mockUseTokenTracker.mockReturnValue({
+      tokensWithBalances: [
+        {
+          address: '0xdac17f958d2ee523a2206206994597c13d831ec7',
+          balance: '0xa',
+        },
+      ],
+    });
+    const { result } = renderHookWithProvider(
+      () =>
+        useTokensWithFiltering(
+          MOCK_TOKEN_LIST_BY_ADDRESS,
+          MOCK_TOP_ASSETS,
+          TokenBucketPriority.owned,
+          TEST_CHAIN_ID,
+        ),
+      mockStore,
+    );
+
+    expect(result.current).toHaveLength(1);
+    expect(typeof result.current).toStrictEqual('function');
+    const tokenGenerator = result.current(() => true);
+    expect(tokenGenerator.next().value).toStrictEqual({
+      address: '0x0000000000000000000000000000000000000000',
+      balance: '0x0',
+      decimals: 18,
+      iconUrl: './images/eth_logo.svg',
+      identiconAddress: null,
+      image: './images/eth_logo.svg',
+      name: 'Ether',
+      primaryLabel: 'ETH',
+      rawFiat: '0',
+      rightPrimaryLabel: '0 ETH',
+      rightSecondaryLabel: '0 ETH',
+      secondaryLabel: 'Ether',
+      string: '0',
+      symbol: 'ETH',
+      type: 'NATIVE',
+    });
+    expect(tokenGenerator.next().value).toStrictEqual({
+      address: '0xdac17f958d2ee523a2206206994597c13d831ec7',
+      aggregators: [],
+      balance: '0xa',
+      decimals: 6,
+      erc20: true,
+      iconUrl:
+        'https://static.cx.metamask.io/api/v1/tokenIcons/1/0xdac17f958d2ee523a2206206994597c13d831ec7.png',
+      identiconAddress: null,
+      image: 'images/contract/usdt.svg',
+      name: 'Tether USD',
+      primaryLabel: 'USDT',
+      rawFiat: '',
+      rightPrimaryLabel: undefined,
+      rightSecondaryLabel: '',
+      secondaryLabel: 'Tether USD',
+      symbol: 'USDT',
+      type: 'TOKEN',
+    });
+  });
+});
---
+++ ui/hooks/useTokensWithFiltering.ts
@@ -0,0 +1,175 @@
+import { useCallback, useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { isEqual } from 'lodash';
+import { ChainId, hexToBN } from '@metamask/controller-utils';
+import { Hex } from '@metamask/utils';
+import {
+  getAllTokens,
+  getCurrentCurrency,
+  getSelectedInternalAccountWithBalance,
+  getShouldHideZeroBalanceTokens,
+  getTokenExchangeRates,
+} from '../selectors';
+import { getConversionRate } from '../ducks/metamask/metamask';
+import {
+  SWAPS_CHAINID_DEFAULT_TOKEN_MAP,
+  SwapsTokenObject,
+  TokenBucketPriority,
+} from '../../shared/constants/swaps';
+import { getValueFromWeiHex } from '../../shared/modules/conversion.utils';
+import { EtherDenomination } from '../../shared/constants/common';
+import {
+  AssetWithDisplayData,
+  ERC20Asset,
+  NativeAsset,
+  TokenWithBalance,
+} from '../components/multichain/asset-picker-amount/asset-picker-modal/types';
+import { AssetType } from '../../shared/constants/transaction';
+import { isSwapsDefaultTokenSymbol } from '../../shared/modules/swaps.utils';
+import { useTokenTracker } from './useTokenTracker';
+import { getRenderableTokenData } from './useTokensToSearch';
+
+/*
+ * Returns a token list generator that filters and sorts tokens based on
+ * query match, balance/popularity, all other tokens
+ */
+export const useTokensWithFiltering = (
+  tokenList: Record<string, SwapsTokenObject>,
+  topTokens: { address: string }[],
+  sortOrder: TokenBucketPriority = TokenBucketPriority.owned,
+  chainId?: ChainId | Hex,
+) => {
+  // Only includes non-native tokens
+  const allDetectedTokens = useSelector(getAllTokens);
+  const { address: selectedAddress, balance: balanceOnActiveChain } =
+    useSelector(getSelectedInternalAccountWithBalance);
+
+  const allDetectedTokensForChainAndAddress = chainId
+    ? allDetectedTokens?.[chainId]?.[selectedAddress] ?? []
+    : [];
+
+  const shouldHideZeroBalanceTokens = useSelector(
+    getShouldHideZeroBalanceTokens,
+  );
+  const {
+    tokensWithBalances: erc20TokensWithBalances,
+  }: { tokensWithBalances: TokenWithBalance[] } = useTokenTracker({
+    tokens: allDetectedTokensForChainAndAddress,
+    address: selectedAddress,
+    hideZeroBalanceTokens: Boolean(shouldHideZeroBalanceTokens),
+  });
+
+  const tokenConversionRates = useSelector(getTokenExchangeRates, isEqual);
+  const conversionRate = useSelector(getConversionRate);
+  const currentCurrency = useSelector(getCurrentCurrency);
+
+  const sortedErc20TokensWithBalances = useMemo(
+    () =>
+      erc20TokensWithBalances.sort(
+        (a, b) => Number(b.string) - Number(a.string),
+      ),
+    [erc20TokensWithBalances],
+  );
+
+  const filteredTokenListGenerator = useCallback(
+    (shouldAddToken: (symbol: string, address?: string) => boolean) => {
+      const buildTokenData = (
+        token: SwapsTokenObject,
+      ):
+        | AssetWithDisplayData<NativeAsset>
+        | AssetWithDisplayData<ERC20Asset>
+        | undefined => {
+        if (chainId && shouldAddToken(token.symbol, token.address)) {
+          return getRenderableTokenData(
+            {
+              ...token,
+              type: isSwapsDefaultTokenSymbol(token.symbol, chainId)
+                ? AssetType.native
+                : AssetType.token,
+              image: token.iconUrl,
+            },
+            tokenConversionRates,
+            conversionRate,
+            currentCurrency,
+            chainId,
+            tokenList,
+          );
+        }
+        return undefined;
+      };
+
+      return (function* (): Generator<
+        AssetWithDisplayData<NativeAsset> | AssetWithDisplayData<ERC20Asset>
+      > {
+        const balance = hexToBN(balanceOnActiveChain);
+        const nativeToken = buildTokenData({
+          ...SWAPS_CHAINID_DEFAULT_TOKEN_MAP[
+            chainId as keyof typeof SWAPS_CHAINID_DEFAULT_TOKEN_MAP
+          ],
+          ...(sortOrder === TokenBucketPriority.owned
+            ? {
+                balance: balanceOnActiveChain,
+                string: getValueFromWeiHex({
+                  value: balance,
+                  numberOfDecimals: 4,
+                  toDenomination: EtherDenomination.ETH,
+                }),
+              }
+            : {}),
+        });
+        if (nativeToken) {
+          yield nativeToken;
+        }
+
+        if (sortOrder === TokenBucketPriority.owned) {
+          for (const token of sortedErc20TokensWithBalances) {
+            const cachedTokenData =
+              token.address &&
+              tokenList &&
+              (tokenList[token.address] ??
+                tokenList[token.address.toLowerCase()]);
+            if (cachedTokenData) {
+              const combinedTokenData = buildTokenData({
+                ...token,
+                ...(cachedTokenData ?? {}),
+              });
+              if (combinedTokenData) {
+                yield combinedTokenData;
+              }
+            }
+          }
+        }
+
+        for (const t of topTokens) {
+          const token =
+            tokenList?.[t.address] ?? tokenList?.[t.address.toLowerCase()];
+          if (token) {
+            const tokenWithTokenListData = buildTokenData(token);
+            if (tokenWithTokenListData) {
+              yield tokenWithTokenListData;
+            }
+          }
+        }
+
+        for (const token of Object.values(tokenList)) {
+          const tokenWithTokenListData = buildTokenData(token);
+          if (tokenWithTokenListData) {
+            yield tokenWithTokenListData;
+          }
+        }
+      })();
+    },
+    [
+      balanceOnActiveChain,
+      sortedErc20TokensWithBalances,
+      topTokens,
+      tokenConversionRates,
+      conversionRate,
+      currentCurrency,
+      chainId,
+      tokenList,
+    ],
+  );
+
+  return filteredTokenListGenerator;
+};
