5a154c0a3cd4f04395f6928fdea32b8a0c490925
Charly Chevalier <charly.chevalier@consensys.net>
2024-06-27 11:54:13 +0200

    test(btc): add BtcOverview tests

--- test/setup.js
+++ test/setup.js
@@ -7,3 +7,8 @@ window.SVGPathElement = window.SVGPathElement || { prototype: {} };
 global.indexedDB = {};
 // scrollIntoView is not available in JSDOM
 window.HTMLElement.prototype.scrollIntoView = () => undefined
+
+// Required for coin overviews components
+global.platform = {
+  openTab: () => undefined,
+};
