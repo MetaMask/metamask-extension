bf8f4aa4199f9894c0cdaedc15fbf560aec56e89
Plasma Corral <32695229+plasmacorral@users.noreply.github.com>
2024-06-27 08:47:06 -0400

    add e2e to swap with snap account

---
+++ test/e2e/accounts/snap-account-eth-swap.spec.ts
@@ -0,0 +1,41 @@
+import { withFixtures, WINDOW_TITLES } from '../helpers';
+import { Driver } from '../webdriver/driver';
+import { accountSnapFixtures, installSnapSimpleKeyring } from './common';
+
+const {
+  buildQuote,
+  reviewQuote,
+  waitForTransactionToComplete,
+  checkActivityTransaction,
+} = require('../tests/swaps/shared');
+
+describe('Snap account swap', function () {
+  it('exchanges native token (ETH) for DAI with snap account', async function () {
+    await withFixtures(
+      accountSnapFixtures(this.test?.fullTitle()),
+      async ({ driver }: { driver: Driver }) => {
+        await installSnapSimpleKeyring(driver, false);
+        await driver.switchToWindowWithTitle(
+          WINDOW_TITLES.ExtensionInFullScreenView,
+        );
+        await buildQuote(driver, {
+          amount: 0.001,
+          swapTo: 'DAI',
+        });
+        await reviewQuote(driver, {
+          amount: 0.001,
+          swapFrom: 'TESTETH',
+          swapTo: 'DAI',
+        });
+        await driver.clickElement({ text: 'Swap', tag: 'button' });
+        await waitForTransactionToComplete(driver, { tokenName: 'DAI' });
+        await checkActivityTransaction(driver, {
+          index: 0,
+          amount: '0.001',
+          swapFrom: 'TESTETH',
+          swapTo: 'DAI',
+        });
+      },
+    );
+  });
+});
