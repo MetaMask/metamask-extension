138ce049b8e92f320033eb9f88efd116bedd0098
Charly Chevalier <charly.chevalier@consensys.net>
2024-06-27 23:55:46 +0200

    fix(hooks): fix useMultichainAccountTotalFiatBalance test

--- ui/hooks/useMultichainAccountTotalFiatBalance.test.tsx
+++ ui/hooks/useMultichainAccountTotalFiatBalance.test.tsx
@@ -47,7 +47,7 @@ const mockAccount = createMockInternalAccount({
 const mockNonEvmAccount = {
   ...mockAccount,
   id: 'b7893c59-e376-4cc0-93ad-05ddaab574a6',
-  addres: 'bc1qn3stuu6g37rpxk3jfxr4h4zmj68g0lwxx5eker',
+  address: 'bc1qn3stuu6g37rpxk3jfxr4h4zmj68g0lwxx5eker',
   type: BtcAccountType.P2wpkh,
 };
 
