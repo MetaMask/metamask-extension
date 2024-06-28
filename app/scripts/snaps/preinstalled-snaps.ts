a9efa5210abd11f7891a35a9035e210d5fa69f18
Charly Chevalier <charly.chevalier@consensys.net>
2024-06-28 11:04:13 +0200

    feat(snap): use preinstalled bitcoin-manager-snap 0.1.4-rc4

--- app/scripts/snaps/preinstalled-snaps.ts
+++ app/scripts/snaps/preinstalled-snaps.ts
@@ -1,8 +1,10 @@
 import type { PreinstalledSnap } from '@metamask/snaps-controllers';
 import MessageSigningSnap from '@metamask/message-signing-snap/dist/preinstalled-snap.json';
+import BitcoinManagerSnap from '@consensys/bitcoin-manager-snap/dist/preinstalled-snap.json';
 
 const PREINSTALLED_SNAPS: readonly PreinstalledSnap[] = Object.freeze([
   MessageSigningSnap as PreinstalledSnap,
+  BitcoinManagerSnap as PreinstalledSnap,
 ]);
 
 export default PREINSTALLED_SNAPS;
