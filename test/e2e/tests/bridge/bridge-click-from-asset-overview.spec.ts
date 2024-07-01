e364f53650e18635461e6193cd53f99dfa7848fe
Micaela Estabillo <micaela.estabillo@consensys.net>
2024-07-01 14:56:00 -0700

    chore: wip e2e test fix, switch window not working

--- test/e2e/tests/bridge/bridge-click-from-asset-overview.spec.ts
+++ test/e2e/tests/bridge/bridge-click-from-asset-overview.spec.ts
@@ -29,7 +29,41 @@ describe('Click bridge button from asset page @no-mmi', function (this: Suite) {
           'https://portfolio.metamask.io/bridge?metametricsId=null',
         );
 
-        await bridgePage.reloadHome();
+        // await bridgePage.reloadHome();
+
+        // // TST
+        // await bridgePage.loadAssetPage(contractRegistry, 'TST');
+        // await bridgePage.load('token-overview');
+        // await bridgePage.verifyPortfolioTab(
+        //   'https://portfolio.metamask.io/bridge?metametricsId=null',
+        // );
+      },
+    );
+  });
+  it('loads portfolio tab when flag is turned off - TST', async function () {
+    await withFixtures(
+      // withErc20 param is false, as we test it manually below
+      getBridgeFixtures(this.test?.fullTitle(), mockServer(), false),
+      async ({
+        driver,
+        ganacheServer,
+        contractRegistry,
+      }: {
+        driver: Driver;
+        ganacheServer: Ganache;
+        contractRegistry: GanacheContractAddressRegistry;
+      }) => {
+        const bridgePage = new BridgePage(driver);
+        await logInWithBalanceValidation(driver, ganacheServer);
+
+        // // ETH
+        // await bridgePage.loadAssetPage(contractRegistry);
+        // await bridgePage.load('coin-overview');
+        // await bridgePage.verifyPortfolioTab(
+        //   'https://portfolio.metamask.io/bridge?metametricsId=null',
+        // );
+
+        // // await bridgePage.reloadHome();
 
         // TST
         await bridgePage.loadAssetPage(contractRegistry, 'TST');
@@ -46,6 +80,7 @@ describe('Click bridge button from asset page @no-mmi', function (this: Suite) {
       getBridgeFixtures(
         this.test?.fullTitle(),
         mockServer({ 'extension-support': true }),
+        false,
       ),
       async ({
         driver,
@@ -58,13 +93,44 @@ describe('Click bridge button from asset page @no-mmi', function (this: Suite) {
       }) => {
         const bridgePage = new BridgePage(driver);
         await logInWithBalanceValidation(driver, ganacheServer);
-
         // ETH
         await bridgePage.loadAssetPage(contractRegistry);
         await bridgePage.load('coin-overview');
         await bridgePage.verifySwapPage();
 
-        await bridgePage.reloadHome(false);
+        // await bridgePage.reloadHome(false);
+
+        // // TST
+        // await bridgePage.loadAssetPage(contractRegistry, 'TST');
+        // await bridgePage.load('token-overview');
+        // await bridgePage.verifySwapPage();
+      },
+    );
+  });
+  it('loads placeholder swap route when flag is turned on - TST', async function () {
+    await withFixtures(
+      getBridgeFixtures(
+        this.test?.fullTitle(),
+        mockServer({ 'extension-support': true }),
+        false,
+      ),
+      async ({
+        driver,
+        ganacheServer,
+        contractRegistry,
+      }: {
+        driver: Driver;
+        ganacheServer: Ganache;
+        contractRegistry: GanacheContractAddressRegistry;
+      }) => {
+        const bridgePage = new BridgePage(driver);
+        await logInWithBalanceValidation(driver, ganacheServer);
+        // ETH
+        // await bridgePage.loadAssetPage(contractRegistry);
+        // await bridgePage.load('coin-overview');
+        // await bridgePage.verifySwapPage();
+
+        // await bridgePage.reloadHome(false);
 
         // TST
         await bridgePage.loadAssetPage(contractRegistry, 'TST');
