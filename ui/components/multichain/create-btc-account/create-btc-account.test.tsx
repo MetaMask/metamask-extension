ac85ac7518f7efd8ffbcb9d931eb80402b9d357c
Charly Chevalier <charly.chevalier@consensys.net>
2024-07-02 14:38:17 +0200

    test(btc): fix CreateBtcAccount tests

--- ui/components/multichain/create-btc-account/create-btc-account.test.tsx
+++ ui/components/multichain/create-btc-account/create-btc-account.test.tsx
@@ -1,8 +1,14 @@
 /* eslint-disable jest/require-top-level-describe */
 import React from 'react';
-import { fireEvent, renderWithProvider } from '../../../../test/jest';
+import { JsonRpcRequest } from '@metamask/utils';
+import {
+  BtcAccountType,
+  BtcMethod,
+} from '@metamask/keyring-api';
+import { fireEvent, renderWithProvider, waitFor } from '../../../../test/jest';
 import configureStore from '../../../store/store';
 import mockState from '../../../../test/data/mock-state.json';
+import { MultichainNetworks } from '../../../../shared/constants/multichain/networks';
 import { CreateBtcAccount } from '.';
 
 const render = (props = { onActionComplete: jest.fn() }) => {
@@ -12,22 +18,55 @@ const render = (props = { onActionComplete: jest.fn() }) => {
 
 const ACCOUNT_NAME = 'Bitcoin Account';
 
+const mockBtcAccount = {
+  type: BtcAccountType.P2wpkh,
+  id: '8a323a0b-9ff5-4ab6-95e0-d51ec7e09763',
+  address: 'bc1qwl8399fz829uqvqly9tcatgrgtwp3udnhxfq4k',
+  options: {
+    scope: MultichainNetworks.BITCOIN,
+    index: 0,
+  },
+  methods: [BtcMethod.SendMany],
+};
+const mockBitcoinManagerSnapSend = jest.fn().mockReturnValue(mockBtcAccount);
+const mockSetAccountLabel = jest.fn().mockReturnValue({ type: 'TYPE' });
+
+jest.mock('../../../store/actions', () => ({
+  setAccountLabel: (address: string, label: string) =>
+    mockSetAccountLabel(address, label),
+}));
+
+jest.mock(
+  '../../../../app/scripts/lib/snap-keyring/bitcoin-manager-snap',
+  () => ({
+    BitcoinManagerSnapSender: jest.fn().mockImplementation(() => {
+      return {
+        send: (_request: JsonRpcRequest) => {
+          return mockBitcoinManagerSnapSend();
+        },
+      };
+    }),
+  }),
+);
+
 describe('CreateBtcAccount', () => {
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  it('displays account name input and suggests name', () => {
+  it('displays account name input and suggests name', async () => {
     const { getByPlaceholderText } = render();
 
-    expect(getByPlaceholderText(ACCOUNT_NAME)).toBeInTheDocument();
+    await waitFor(() =>
+      expect(getByPlaceholderText(ACCOUNT_NAME)).toBeInTheDocument(),
+    );
   });
 
   it('fires onActionComplete when clicked', async () => {
     const onActionComplete = jest.fn();
     const { getByText, getByPlaceholderText } = render({ onActionComplete });
 
-    const input = getByPlaceholderText(ACCOUNT_NAME);
+    const input = await waitFor(() => getByPlaceholderText(ACCOUNT_NAME));
     const newAccountName = 'New Account Name';
 
     fireEvent.change(input, {
@@ -35,13 +74,19 @@ describe('CreateBtcAccount', () => {
     });
     fireEvent.click(getByText('Create'));
 
-    // TODO: Add logic to rename account
+    await waitFor(() =>
+      expect(mockSetAccountLabel).toHaveBeenCalledWith(
+        mockBtcAccount.address,
+        newAccountName,
+      ),
+    );
+    await waitFor(() => expect(onActionComplete).toHaveBeenCalled());
   });
 
   it(`doesn't allow duplicate account names`, async () => {
     const { getByText, getByPlaceholderText } = render();
 
-    const input = getByPlaceholderText(ACCOUNT_NAME);
+    const input = await waitFor(() => getByPlaceholderText(ACCOUNT_NAME));
     const usedAccountName =
       mockState.metamask.internalAccounts.accounts[
         '07c2cfec-36c9-46c4-8115-3836d3ac9047'
