28c94743635413e48c68ade1450392cdee2f4757
Charly Chevalier <charly.chevalier@consensys.net>
2024-07-02 21:53:55 +0200

    Revert "chore(btc): use bitcoin-manager-snap as a dependency"
    This reverts commit 34a1e1572e9a53a2cd12182a1160d280eceb961c.

--- app/scripts/snaps/preinstalled-snaps.ts
+++ app/scripts/snaps/preinstalled-snaps.ts
@@ -1,7 +1,7 @@
 import type { PreinstalledSnap } from '@metamask/snaps-controllers';
 import MessageSigningSnap from '@metamask/message-signing-snap/dist/preinstalled-snap.json';
 ///: BEGIN:ONLY_INCLUDE_IF(build-flask)
-import BitcoinManagerSnap from '@consensys/bitcoin-manager-snap/dist/preinstalled-snap.json';
+import BitcoinManagerSnap from './bitcoin-manager-snap-0.1.4-rc4-preinstalled-snap.json';
 ///: END:ONLY_INCLUDE_IF
 
 const PREINSTALLED_SNAPS: readonly PreinstalledSnap[] = Object.freeze([
