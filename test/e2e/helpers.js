6e9575dcbb4d0093ba20ec7df7b93d3ca23d74e9
Monte Lai <monte.lai@consensys.net>
2024-07-02 20:17:29 +0800

    fix: account missing in connection page (#25500)
    ## **Description**
    
    This PR fixes a regression where the connected account is not showing up
    on the page.
    
    ## **Related issues**
    
    ## **Manual testing steps**
    
    1. Go to a test dapp
    2. Connect the account
    3. Open the popup
    4. Click on the connection icon and see the connected account. 
    
    ## **Screenshots/Recordings**
    
    ### **Before**
    
    <img width="360" alt="Screenshot 2024-06-25 at 15 30 55"
    src="https://github.com/MetaMask/metamask-extension/assets/96463427/35fe21b3-7434-4bde-93a8-d3bbfe0126b6">
    
    
    ### **After**
    <img width="362" alt="Screenshot 2024-06-25 at 15 31 01"
    src="https://github.com/MetaMask/metamask-extension/assets/96463427/474a9681-75b7-4e00-a8a9-95fb60daad74">
    
    
    ## **Pre-merge author checklist**
    
    - [x] I've followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
    Extension Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: Nidhi Kumari <nidhi.kumari@consensys.net>
    Co-authored-by: Charly Chevalier <charly.chevalier@consensys.net>

--- test/e2e/helpers.js
+++ test/e2e/helpers.js
@@ -672,6 +672,14 @@ const openDapp = async (driver, contract = null, dappURL = DAPP_URL) => {
     : await driver.openNewPage(dappURL);
 };
 
+const openDappConnectionsPage = async (driver) => {
+  await driver.openNewPage(
+    `${driver.extensionUrl}/home.html#connections/${encodeURIComponent(
+      DAPP_URL,
+    )}`,
+  );
+};
+
 const createDappTransaction = async (driver, transaction) => {
   await openDapp(
     driver,
@@ -1150,6 +1158,7 @@ module.exports = {
   importWrongSRPOnboardingFlow,
   testSRPDropdownIterations,
   openDapp,
+  openDappConnectionsPage,
   createDappTransaction,
   switchToOrOpenDapp,
   connectToDapp,
