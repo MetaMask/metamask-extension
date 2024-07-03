f471e36957935057de962614e5a4118d06bdd7eb
Charly Chevalier <charly.chevalier@consensys.net>
2024-07-03 11:07:07 +0200

    refactor(btc): remove shared/constants/bitcoin-manager-snap.ts

--- shared/constants/bitcoin-manager-snap.ts
+++
@@ -1,8 +0,0 @@
-import { SnapId } from '@metamask/snaps-sdk';
-
-export const BITCOIN_MANAGER_SNAP_ID: SnapId =
-  'local:http://localhost:8080' as SnapId;
-
-// FIXME: This one should probably somewhere else with other CAIP-2 chain ID identifiers!
-export const BITCOIN_MANAGER_SCOPE_MAINNET =
-  'bip122:000000000019d6689c085ae165831e93';
