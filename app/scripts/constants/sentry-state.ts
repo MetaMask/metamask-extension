648ddb35e6831b98c8412f9c292c7be1b3657186
Micaela Estabillo <micaela.estabillo@consensys.net>
2024-07-03 17:52:15 -0700

    chore: add bridge SELECT_DEST_NETWORK action

--- app/scripts/constants/sentry-state.ts
+++ app/scripts/constants/sentry-state.ts
@@ -102,6 +102,7 @@ export const SENTRY_BACKGROUND_STATE = {
         destNetworkAllowlist: [],
         srcNetworkAllowlist: [],
       },
+      destTokens: {},
     },
   },
   CronjobController: {
