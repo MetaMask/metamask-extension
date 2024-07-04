304f410a0204522ddf0ceb9d362c698340f02d1c
cryptotavares <joao.tavares@consensys.net>
2024-07-04 13:59:37 +0100

    chore: move common jest setup after test setup after helper

--- test/integration/config/setupAfter.js
+++ test/integration/config/setupAfter.js
@@ -1,24 +1,2 @@
 // This file is for Jest-specific setup only and runs before our Jest tests.
-
-import nock from 'nock';
-import '@testing-library/jest-dom';
-
-jest.mock('webextension-polyfill', () => {
-  return {
-    runtime: {
-      getManifest: () => ({ manifest_version: 2 }),
-      onMessage: {
-        removeListener: jest.fn(),
-        addListener: jest.fn(),
-      },
-    },
-  };
-});
-
-/* eslint-disable-next-line jest/require-top-level-describe */
-beforeEach(() => {
-  nock.cleanAll();
-});
-
-// Setup window.prompt
-global.prompt = () => undefined;
+import '../../helpers/setup-after-helper';
