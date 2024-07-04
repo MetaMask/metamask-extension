360a6b5e38413512536bbdc61b2a49b5eedb59d8
cryptotavares <joao.tavares@consensys.net>
2024-07-04 13:53:57 +0100

    chore: move common jest setup test setup helper

--- test/integration/config/setup.js
+++ test/integration/config/setup.js
@@ -3,8 +3,4 @@ require('ts-node').register({ transpileOnly: true });
 
 require('../../helpers/setup-helper');
 
-window.SVGPathElement = window.SVGPathElement || { prototype: {} };
-global.indexedDB = {};
 global.metamask = {};
-// scrollIntoView is not available in JSDOM
-window.HTMLElement.prototype.scrollIntoView = () => undefined
