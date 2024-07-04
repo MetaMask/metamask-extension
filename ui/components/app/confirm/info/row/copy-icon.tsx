3180a0d2ba5a35b7219bfc09ca9ec5c3de53f22e
Jyoti Puri <jyotipuri@gmail.com>
2024-07-04 18:13:39 +0530

    Adding copy option to info row component

---
+++ ui/components/app/confirm/info/row/copy-icon.tsx
@@ -0,0 +1,27 @@
+import React, { useCallback, useState } from 'react';
+
+import { IconColor } from '../../../../../helpers/constants/design-system';
+import { Icon, IconName, IconSize } from '../../../../component-library';
+
+export const CopyIcon: React.FC<{ copyText: string }> = ({ copyText }) => {
+  const [success, setSuccess] = useState(false);
+
+  const copyToClipboard = useCallback(async () => {
+    try {
+      await navigator.clipboard.writeText(copyText);
+      setSuccess(true);
+    } catch (error: any) {
+      console.error(error);
+    }
+  }, [copyText]);
+
+  return (
+    <Icon
+      color={IconColor.iconAlternative}
+      name={success ? IconName.CopySuccess : IconName.Copy}
+      size={IconSize.Sm}
+      style={{ cursor: 'pointer', position: 'absolute', right: 0, top: 2 }}
+      onClick={copyToClipboard}
+    />
+  );
+};
