b9e4891d3eb1e783e0af7e7694e4ffc27d32b4c0
Gauthier Petetin <gauthierpetetin@gmail.com>
2024-07-05 11:38:46 +0200

    fix(action): comment was not up to date

--- .github/scripts/check-template-and-add-labels.ts
+++ .github/scripts/check-template-and-add-labels.ts
@@ -246,7 +246,7 @@ function extractReleaseVersionFromBugReportIssueBody(
   return version;
 }
 
-// This function adds the correct "regression-prod-x.y.z" label to the issue, and removes other ones
+// This function adds the correct regression label to the issue, and removes other ones
 async function addRegressionLabelToIssue(
   octokit: InstanceType<typeof GitHub>,
   issue: Labelable,
