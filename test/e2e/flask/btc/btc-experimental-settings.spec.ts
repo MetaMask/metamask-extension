66f21f4e1041b6d3e218298d46f00bb0fe2cabb0
Monte Lai <monte.lai@consensys.net>
2024-07-09 20:51:06 +0800

    feat: add connection and experimental settings

---
+++ test/e2e/flask/btc/btc-experimental-settings.spec.ts
@@ -0,0 +1,50 @@
+import { Suite } from 'mocha';
+
+import FixtureBuilder from '../../fixture-builder';
+import {
+  defaultGanacheOptions,
+  unlockWallet,
+  withFixtures,
+} from '../../helpers';
+import { Driver } from '../../webdriver/driver';
+
+describe('BTC Experimental Settings', function (this: Suite) {
+  it('will show `Add new BTC account` option when setting is enabled', async function () {
+    await withFixtures(
+      {
+        fixtures: new FixtureBuilder().build(),
+        ganacheOptions: defaultGanacheOptions,
+        title: this.test?.fullTitle(),
+      },
+      async ({ driver }: { driver: Driver }) => {
+        await unlockWallet(driver);
+        await driver.clickElement(
+          '[data-testid="account-options-menu-button"]',
+        );
+        await driver.clickElement({ text: 'Settings', tag: 'div' });
+        await driver.clickElement({ text: 'Experimental', tag: 'div' });
+
+        // The option requires tapping the experimental title 5 times before appearing.
+        for (let i = 0; i < 5; i++) {
+          await driver.clickElement(
+            '[data-testid="experimental-container-title"]',
+          );
+        }
+        await driver.waitForSelector({
+          text: 'Enable "Add Bitcoin Account (Beta)"',
+          tag: 'span',
+        });
+        await driver.clickElement('button[aria-label="Close"]');
+
+        await driver.clickElement('[data-testid="account-menu-icon"]');
+        await driver.clickElement(
+          '[data-testid="multichain-account-menu-popover-action-button"]',
+        );
+        await driver.waitForSelector({
+          text: 'Add a new Bitcoin account',
+          tag: 'button',
+        });
+      },
+    );
+  });
+});
