e63b0dd378da08517e98e4e340eb90130084aab5
Charly Chevalier <charly.chevalier@consensys.net>
2024-07-10 10:30:50 +0200

    fix(btc): fix code fences

--- app/scripts/lib/snap-keyring/bitcoin-wallet-snap.ts
+++ app/scripts/lib/snap-keyring/bitcoin-wallet-snap.ts
@@ -2,10 +2,10 @@ import { SnapId } from '@metamask/snaps-sdk';
 import { Sender } from '@metamask/keyring-api';
 import { HandlerType } from '@metamask/snaps-utils';
 import { Json, JsonRpcRequest } from '@metamask/utils';
-import BitcoinWalletSnap from '@metamask/bitcoin-wallet-snap/dist/preinstalled-snap.json';
-import { handleSnapRequest } from '../../../../ui/store/actions';
 ///: BEGIN:ONLY_INCLUDE_IF(build-flask)
+import BitcoinWalletSnap from '@metamask/bitcoin-wallet-snap/dist/preinstalled-snap.json';
 ///: END:ONLY_INCLUDE_IF
+import { handleSnapRequest } from '../../../../ui/store/actions';
 
 export const BITCOIN_WALLET_SNAP_ID: SnapId =
   BitcoinWalletSnap.snapId as SnapId;
