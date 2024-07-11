2401be16e242d928cd6c214d84ead8dc44a91d3f
seaona <mariona@gmx.es>
2024-07-11 14:12:42 +0200

    update mocks

--- test/e2e/tests/settings/ipfs-ens-resolution.spec.js
+++ test/e2e/tests/settings/ipfs-ens-resolution.spec.js
@@ -1,7 +1,6 @@
 const { strict: assert } = require('node:assert');
 const { PAGES } = require('../../webdriver/driver');
 const {
-  defaultGanacheOptions,
   tinyDelayMs,
   unlockWallet,
   withFixtures,
@@ -14,154 +13,30 @@ const BALANCE_CHECKER = '0xb1f8e55c7f64d203c1400b9d8555d050f94adf39';
 const ENS_PUBLIC_RESOLVER = '0x4976fb03c32e5b8cfe2b6ccb31c09ba78ebaba41';
 const ENS_REGISTRY_WITH_FALLBACK = '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e';
 
-async function mockInfura(mockServer) {
-  await mockServerJsonRpc(mockServer, [
-    [
-      'eth_blockNumber',
-      {
-        methodResultVariant: 'custom',
-      },
-    ],
-    ['eth_estimateGas'],
-    ['eth_feeHistory'],
-    ['eth_gasPrice'],
-    ['eth_getBalance'],
-    ['eth_getBlockByNumber'],
-    ['eth_getTransactionCount'],
-    ['net_version'],
-  ]);
-
-  // Resolver Call
-  await mockServer
-    .forPost()
-    .withJsonBodyIncluding({
-      method: 'eth_call',
-      params: [
-        {
-          to: ENS_REGISTRY_WITH_FALLBACK,
-        },
-      ],
-    })
-    .thenCallback(async (req) => {
-      return {
-        statusCode: 200,
-        json: {
-          jsonrpc: '2.0',
-          id: (await req.body.getJson()).id,
-          result:
-            '0x0000000000000000000000004976fb03c32e5b8cfe2b6ccb31c09ba78ebaba41',
-        },
-      };
-    });
-
-  // Supports Interface Call
-  await mockServer
-    .forPost()
-    .withJsonBodyIncluding({
-      method: 'eth_call',
-      params: [
-        {
-          data: '0x01ffc9a7bc1c58d100000000000000000000000000000000000000000000000000000000',
-          to: ENS_PUBLIC_RESOLVER,
-        },
-      ],
-    })
-    .thenCallback(async (req) => {
-      return {
-        statusCode: 200,
-        json: {
-          jsonrpc: '2.0',
-          id: (await req.body.getJson()).id,
-          result:
-            '0x0000000000000000000000000000000000000000000000000000000000000001',
-        },
-      };
-    });
-
-  // Supports Interface Call
-  await mockServer
-    .forPost()
-    .withJsonBodyIncluding({
-      method: 'eth_call',
-      params: [
-        {
-          data: '0x01ffc9a7d8389dc500000000000000000000000000000000000000000000000000000000',
-          to: ENS_PUBLIC_RESOLVER,
-        },
-      ],
-    })
-    .thenCallback(async (req) => {
-      return {
-        statusCode: 200,
-        json: {
-          jsonrpc: '2.0',
-          id: (await req.body.getJson()).id,
-          result:
-            '0x0000000000000000000000000000000000000000000000000000000000000000',
-        },
-      };
-    });
-
-  // Content Hash Call
-  await mockServer
-    .forPost()
-    .withJsonBodyIncluding({
-      method: 'eth_call',
-      params: [
-        {
-          data: '0xbc1c58d1e650784434622eeb4ffbbb3220ebb371e26ad1a77f388680d42d8b1624baa6df',
-          to: ENS_PUBLIC_RESOLVER,
-        },
-      ],
-    })
-    .thenCallback(async (req) => {
-      return {
-        statusCode: 200,
-        json: {
-          jsonrpc: '2.0',
-          id: (await req.body.getJson()).id,
-          result:
-            '0x00000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000000',
-        },
-      };
-    });
-
-  // Token Balance Call
-  await mockServer
-    .forPost()
-    .withJsonBodyIncluding({
-      method: 'eth_call',
-      params: [
-        {
-          to: BALANCE_CHECKER,
-        },
-      ],
-    })
-    .thenCallback(async (req) => {
-      return {
-        statusCode: 200,
-        json: {
-          jsonrpc: '2.0',
-          id: (await req.body.getJson()).id,
-          result:
-            '0x0000000000000000000000000000000000000000000000000000000000000000',
-        },
-      };
-    });
-}
-
 describe('Settings', function () {
   const ENS_NAME = 'metamask.eth';
   const ENS_NAME_URL = `https://${ENS_NAME}/`;
   const ENS_DESTINATION_URL = `https://app.ens.domains/name/${ENS_NAME}`;
 
   it(`Redirects to ENS domains when user inputs ENS into address bar`, async function () {
+    async function mockMetaMaskDotEth(mockServer) {
+      return await mockServer.forGet(ENS_NAME_URL).thenResetConnection();
+    }
+    async function mockEnsDotDomains(mockServer) {
+      return await mockServer
+        .forGet(ENS_DESTINATION_URL)
+        .thenReply(200, 'mocked ENS domain');
+    }
+    async function mockEns(mockServer) {
+      return [
+        await mockMetaMaskDotEth(mockServer),
+        await mockEnsDotDomains(mockServer),
+      ];
+    }
     await withFixtures(
       {
-        fixtures: new FixtureBuilder().withNetworkControllerOnMainnet().build(),
-        ganacheOptions: defaultGanacheOptions,
         title: this.test.fullTitle(),
-        testSpecificMock: mockInfura,
+        testSpecificMock: mockEns,
         driverOptions: {
           proxyPort: '8001',
         },
@@ -187,6 +62,7 @@ describe('Settings', function () {
           }
         }
 
+        // Ensure that the redirect to ENS Domains has happened
         const currentUrl = await driver.getCurrentUrl();
         assert.equal(currentUrl, ENS_DESTINATION_URL);
       },
