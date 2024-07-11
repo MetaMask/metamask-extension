a92b5a65af75e814fcfc16e8ed868695f9821ef8
Victor Thomas <10986371+vthomas13@users.noreply.github.com>
2024-07-11 16:42:22 -0400

    Added validate-locales-only job

---
+++ .circleci/scripts/validate-locales-only.ts
@@ -0,0 +1,40 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+const BASE_PATH = path.resolve(__dirname, '..', '..');
+const CHANGED_FILES_PATH = path.join(
+  BASE_PATH,
+  'changed-files',
+  'changed-files.txt',
+);
+
+/**
+ * Reads the list of changed files from the git diff file.
+ *
+ * @returns {Promise<string[]>} An array of changed file paths.
+ */
+async function readChangedFiles(): Promise<string[]>{
+  try {
+    const data = await fs.readFile(CHANGED_FILES_PATH, 'utf8');
+    const changedFiles = data.split('\n');
+    return changedFiles;
+  } catch (error) {
+    console.error('Error reading from file:', error);
+    return [];
+  }
+}
+
+async function validateChangedFiles() {
+  const changedFiles = await readChangedFiles();
+  const invalidFiles = changedFiles.filter((file) => !file.startsWith('app/_locales/'));
+  if (invalidFiles.length > 0) {
+    console.error('Invalid files found:', invalidFiles);
+    process.exit(1);
+  }
+  else {
+    console.log('All changed files are in the /_locales/ directory.');
+    process.exit(0);
+  }
+}
+
+validateChangedFiles();
