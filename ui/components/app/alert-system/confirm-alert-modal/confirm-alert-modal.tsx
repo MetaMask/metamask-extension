284f7fab0e31f8fe9a88f25095e0c691d10392fe
David Walsh <davidwalsh83@gmail.com>
2024-07-11 14:05:10 -0500

    fix: Code cleanup on quoted attributes

--- ui/components/app/alert-system/confirm-alert-modal/confirm-alert-modal.tsx
+++ ui/components/app/alert-system/confirm-alert-modal/confirm-alert-modal.tsx
@@ -95,7 +95,7 @@ function ConfirmDetails({
           onClick={onAlertLinkClick}
           target="_blank"
           rel="noopener noreferrer"
-          data-testid={'confirm-alert-modal-review-all-alerts'}
+          data-testid="confirm-alert-modal-review-all-alerts"
         >
           <Icon
             name={IconName.SecuritySearch}
