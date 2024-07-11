ce982e3b098fcd6d719ee95dd476ee492381acbb
Jyoti Puri <jyotipuri@gmail.com>
2024-07-11 14:41:58 +0530

    cherry-pick: feat improvement for how we display big and small numbers (#25720)

--- ui/pages/confirmations/components/confirm/info/typed-sign/permit-simulation/permit-simulation.tsx
+++ ui/pages/confirmations/components/confirm/info/typed-sign/permit-simulation/permit-simulation.tsx
@@ -1,6 +1,7 @@
 import React, { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import { NameType } from '@metamask/name-controller';
+import { BigNumber } from 'bignumber.js';
 
 import { parseTypedDataMessage } from '../../../../../../../../shared/modules/transaction.utils';
 import { Numeric } from '../../../../../../../../shared/modules/Numeric';
@@ -12,6 +13,7 @@ import {
 import { useI18nContext } from '../../../../../../../hooks/useI18nContext';
 import { currentConfirmationSelector } from '../../../../../../../selectors';
 import { Box, Text } from '../../../../../../../components/component-library';
+import Tooltip from '../../../../../../../components/ui/tooltip';
 import {
   BackgroundColor,
   BorderRadius,
@@ -21,7 +23,10 @@ import {
 import { SignatureRequestType } from '../../../../../types/confirm';
 import useTokenExchangeRate from '../../../../../../../components/app/currency-input/hooks/useTokenExchangeRate';
 import { IndividualFiatDisplay } from '../../../../simulation-details/fiat-display';
-import { formatNumber } from '../../../utils';
+import {
+  formatAmount,
+  formatAmountMaxPrecision,
+} from '../../../../simulation-details/formatAmount';
 import { ConfirmInfoSection } from '../../../../../../../components/app/confirm/info/row/section';
 
 const PermitSimulation: React.FC<{
@@ -46,6 +51,14 @@ const PermitSimulation: React.FC<{
     return undefined;
   }, [exchangeRate, value]);
 
+  const { tokenValue, tokenValueMaxPrecision } = useMemo(() => {
+    const valueBN = new BigNumber(value / Math.pow(10, tokenDecimals));
+    return {
+      tokenValue: formatAmount('en-US', valueBN),
+      tokenValueMaxPrecision: formatAmountMaxPrecision('en-US', valueBN),
+    };
+  }, [tokenDecimals, value]);
+
   return (
     <ConfirmInfoSection>
       <ConfirmInfoRow
@@ -58,17 +71,21 @@ const PermitSimulation: React.FC<{
         <Box style={{ marginLeft: 'auto' }}>
           <Box display={Display.Flex}>
             <Box display={Display.Inline} marginInlineEnd={1}>
-              <Text
-                backgroundColor={BackgroundColor.backgroundAlternative}
-                borderRadius={BorderRadius.XL}
-                paddingInline={2}
-                textAlign={TextAlign.Center}
+              <Tooltip
+                position="bottom"
+                title={tokenValueMaxPrecision}
+                wrapperStyle={{ minWidth: 0 }}
+                interactive
               >
-                {formatNumber(
-                  value / Math.pow(10, tokenDecimals),
-                  tokenDecimals,
-                )}
-              </Text>
+                <Text
+                  backgroundColor={BackgroundColor.backgroundAlternative}
+                  borderRadius={BorderRadius.XL}
+                  paddingInline={2}
+                  textAlign={TextAlign.Center}
+                >
+                  {tokenValue}
+                </Text>
+              </Tooltip>
             </Box>
             <Name value={verifyingContract} type={NameType.ETHEREUM_ADDRESS} />
           </Box>
