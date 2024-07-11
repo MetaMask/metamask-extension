f523651a0aa7874a111d83759367af51723a921c
Dan J Miller <danjm.com@gmail.com>
2024-07-11 18:00:39 -0230

    temp

--- .circleci/scripts/trigger-beta-build.sh
+++ .circleci/scripts/trigger-beta-build.sh
@@ -7,17 +7,10 @@ set -o pipefail
 # => Version-v10.24.1
 current_commit_msg=$(git show -s --format='%s' HEAD)
 
-if [[ $current_commit_msg =~ Version[[:space:]](v[[:digit:]]+.[[:digit:]]+.[[:digit:]]+[-]beta.[[:digit:]]) ]]
-then
-    # filter the commit message like Version v10.24.1-beta.1
-    printf '%s\n' "Create a build for 12 with beta version $current_commit_msg"
-    yarn build --build-type beta --platform='chrome' dist
-    yarn build --build-type beta --platform='chrome' prod
-else
-    printf '%s\n' 'Commit message does not match commit message for beta pattern; skipping beta automation build'
-    mkdir dist
-    mkdir builds
-    exit 0
-fi
+# filter the commit message like Version v10.24.1-beta.1
+printf '%s\n' "Create a build for 12 with beta version $current_commit_msg"
+yarn build --build-type beta --platform='chrome' dist
+yarn build --build-type beta --platform='chrome' prod
+
 
 exit 0
