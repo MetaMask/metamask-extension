b256d1d01fa8cc7d18c6a1dd106e2538b4707e2a
Victor Thomas <10986371+vthomas13@users.noreply.github.com>
2024-07-12 14:17:38 -0400

    adding edge case and documentation

--- .circleci/scripts/validate-locales-only.ts
+++ .circleci/scripts/validate-locales-only.ts
@@ -24,16 +24,23 @@ async function readChangedFiles(): Promise<string[]>{
   }
 }
 
+/**
+ * Verifies that all changed files are in the /_locales/ directory.
+ * Fails the build if any changed files are outside of the /_locales/ directory.
+ * Fails if no changed files are detected.
+ */
 async function validateChangedFiles() {
   const changedFiles = await readChangedFiles();
   const invalidFiles = changedFiles.filter((file) => !file.startsWith('app/_locales/'));
   if (invalidFiles.length > 0) {
-    console.log('Changed Files:', changedFiles);
-    console.error('Invalid files found:', invalidFiles);
+    console.error('Failure: Changed files must be in the /_locales/ directory.\n Changed Files:', changedFiles, '\n Invalid Files:', invalidFiles);
     process.exit(1);
   }
-  else {
-    console.log('All changed files are in the /_locales/ directory.');
+  else if (changedFiles.length === 0) {
+    console.error('Failure: No changed files detected.');
+    process.exit(1);
+  } else {
+    console.log('Passed validation');
     process.exit(0);
   }
 }
