e6e26e641e05e7ad0018f0bebe38adb6dc54195b
legobt <6wbvkn0j@anonaddy.me>
2024-07-16 10:03:18 +0000

    wip: allow running lavamoat under webpack build

--- app/scripts/load/_initialize.ts
+++ app/scripts/load/_initialize.ts
@@ -4,25 +4,20 @@
 // The root compartment will populate this with hooks
 global.stateHooks = {} as typeof stateHooks;
 
-if (process.env.ENABLE_LAVAMOAT === 'true') {
-  // TODO: lavamoat support
-  throw new Error('LAVAMOAT not supported in webpack build yet');
-} else {
-  if (process.env.ENABLE_SENTRY === 'true') {
-    require('../sentry-install');
-  }
-  if (process.env.ENABLE_SNOW === 'true') {
-    require('@lavamoat/snow/snow.prod');
-    require('../use-snow');
-  }
-  if (process.env.ENABLE_LOCKDOWN === 'true') {
-    require('../lockdown-install');
-    require('../lockdown-run');
-    require('../lockdown-more');
-  }
-
-  require('../init-globals');
-  require('../runtime-cjs');
+if (process.env.ENABLE_SENTRY === 'true') {
+  require('../sentry-install');
 }
+if (process.env.ENABLE_SNOW === 'true') {
+  require('@lavamoat/snow/snow.prod');
+  require('../use-snow');
+}
+if (process.env.ENABLE_LOCKDOWN === 'true') {
+  require('../lockdown-install');
+  require('../lockdown-run');
+  require('../lockdown-more');
+}
+
+require('../init-globals');
+require('../runtime-cjs');
 
 export {};
