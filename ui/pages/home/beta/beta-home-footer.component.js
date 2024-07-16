eaee5d66dd3538ccd094cc326f19faee0e7e7534
Dan J Miller <danjm.com@gmail.com>
2024-07-16 17:03:08 -0230

    Revert "Fix #16852 - Update support link for BETA (#16853)"
    This reverts commit 7a1f75d17f374df2f133aefc02ab48d505673090.

--- ui/pages/home/beta/beta-home-footer.component.js
+++ ui/pages/home/beta/beta-home-footer.component.js
@@ -13,15 +13,12 @@ const BetaHomeFooter = () => {
   const t = useI18nContext();
   const trackEvent = useContext(MetaMetricsContext);
 
-  const SUPPORT_LINK =
-    'https://docs.google.com/forms/d/e/1FAIpQLSfpkpeVMvfFw_1CWzrs5M3TEqE9VPMSspLPJgk1u4ZyUkQRbg/viewform?usp=sharing';
-
   return (
     <>
       <a
         target="_blank"
         rel="noopener noreferrer"
-        href={SUPPORT_LINK}
+        href={SUPPORT_REQUEST_LINK}
         onClick={() => {
           trackEvent(
             {
@@ -42,7 +39,11 @@ const BetaHomeFooter = () => {
         {t('needHelpSubmitTicket')}
       </a>{' '}
       |{' '}
-      <a href={SUPPORT_LINK} target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://community.metamask.io/c/metamask-beta"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         {t('needHelpFeedback')}
       </a>
     </>
