3408c1901c8b74c8e74c6aeb3e68b7da3a0636ef
Jiexi Luan <jiexiluan@gmail.com>
2024-07-16 09:14:12 -0700

    Add light validation

--- app/scripts/lib/multichain-api/provider-authorize.js
+++ app/scripts/lib/multichain-api/provider-authorize.js
@@ -1,11 +1,12 @@
 import { EthereumRpcError } from 'eth-rpc-errors';
 import { RestrictedMethods } from '../../../../shared/constants/permissions';
 import { validateAddEthereumChainParams } from '../rpc-method-middleware/handlers/ethereum-chain-utils';
-import { processScopes, mergeScopes } from './scope';
+import { processScopes, mergeScopes, parseScopeString, KnownCaipNamespace } from './scope';
 import {
   Caip25CaveatType,
   Caip25EndowmentPermissionName,
 } from './caip25permissions';
+import { toHex } from '@metamask/controller-utils';
 
 const getAccountsFromPermission = (permission) => {
   return permission.eth_accounts.caveats.find(
@@ -67,6 +68,12 @@ export async function providerAuthorizeHandler(req, res, _next, end, hooks) {
   }
 
   try {
+    const { flattenedRequiredScopes, flattenedOptionalScopes } = processScopes(
+      requiredScopes,
+      optionalScopes,
+      { findNetworkClientIdByChainId },
+    );
+
     // use old account popup for now to get the accounts
     const [subjectPermission] = await hooks.requestPermissions(
       { origin },
@@ -75,11 +82,6 @@ export async function providerAuthorizeHandler(req, res, _next, end, hooks) {
       },
     );
     const permittedAccounts = getAccountsFromPermission(subjectPermission);
-    const { flattenedRequiredScopes, flattenedOptionalScopes } = processScopes(
-      requiredScopes,
-      optionalScopes,
-      { findNetworkClientIdByChainId },
-    );
 
     Object.keys(flattenedRequiredScopes).forEach((scope) => {
       if (scope !== 'wallet') {
@@ -106,12 +108,28 @@ export async function providerAuthorizeHandler(req, res, _next, end, hooks) {
 
     await Promise.all(
       Object.keys(scopedProperties || {}).map(async (scopeString) => {
+        let namespace, reference;
+        try {
+          ({namespace, reference} = parseScopeString(scopeString))
+
+          if (!namespace || !reference) {
+            return;
+          }
+
+          if (namespace !== KnownCaipNamespace.Eip155) {
+            return;
+          }
+        } catch (err) {
+          return;
+        }
+
         const scope = sessionScopes[scopeString];
         if (!scope) {
           // scopedProperty defined for scope without authorization
           console.log('skipping because missing matching scope', {scopeString})
           return;
         }
+
 
         const eip3085Params = scopedProperties[scopeString].eip3085;
         if (!eip3085Params) {
@@ -135,6 +153,10 @@ export async function providerAuthorizeHandler(req, res, _next, end, hooks) {
           firstValidRPCUrl,
           ticker,
         } = validParams;
+
+        if (chainId !== toHex(reference)) {
+          return;
+        }
 
         await hooks.upsertNetworkConfiguration(
           {
