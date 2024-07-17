4bb99536f3166fa88904972527f4215ead230dcd
Antonio Regadas <antonio.regadas@consensys.net>
2024-07-17 12:07:46 +0100

    fix: adds code fence

--- ui/components/multichain/pages/send/components/recipient-content.tsx
+++ ui/components/multichain/pages/send/components/recipient-content.tsx
@@ -1,4 +1,10 @@
-import React, { useCallback, useMemo, useRef } from 'react';
+import React, {
+  useCallback,
+  ///: BEGIN:ONLY_INCLUDE_IF(build-main,build-beta,build-flask)
+  useMemo,
+  ///: END:ONLY_INCLUDE_IF
+  useRef,
+} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   BannerAlert,
@@ -11,9 +17,11 @@ import {
   acknowledgeRecipientWarning,
   getBestQuote,
   getCurrentDraftTransaction,
+  ///: BEGIN:ONLY_INCLUDE_IF(build-main,build-beta,build-flask)
   getIsSwapAndSendDisabledForNetwork,
-  getSendAsset,
   getSwapsBlockedTokens,
+  ///: END:ONLY_INCLUDE_IF
+  getSendAsset,
 } from '../../../../../ducks/send';
 import { AssetType } from '../../../../../../shared/constants/transaction';
 import { CONTRACT_ADDRESS_LINK } from '../../../../../helpers/constants/common';
@@ -21,10 +29,13 @@ import { Display } from '../../../../../helpers/constants/design-system';
 import { useI18nContext } from '../../../../../hooks/useI18nContext';
 import { AssetPickerAmount } from '../../..';
 import { decimalToHex } from '../../../../../../shared/modules/conversion.utils';
+///: BEGIN:ONLY_INCLUDE_IF(build-main,build-beta,build-flask)
 import {
   getIsSwapsChain,
   getUseExternalServices,
 } from '../../../../../selectors';
+///: END:ONLY_INCLUDE_IF
+
 import type { Quote } from '../../../../../ducks/send/swap-and-send-utils';
 import { isEqualCaseInsensitive } from '../../../../../../shared/modules/string-utils';
 import { SendHexData, SendPageRow, QuoteCard } from '.';
@@ -45,6 +56,13 @@ export const SendPageRecipientContent = ({
     isSwapQuoteLoading,
   } = useSelector(getCurrentDraftTransaction);
 
+  let isSwapAllowed;
+
+  ///: BEGIN:ONLY_INCLUDE_IF(build-mmi)
+  isSwapAllowed = false;
+  ///: END:ONLY_INCLUDE_IF
+
+  ///: BEGIN:ONLY_INCLUDE_IF(build-main,build-beta,build-flask)
   const isBasicFunctionality = useSelector(getUseExternalServices);
   const isSwapsChain = useSelector(getIsSwapsChain);
   const isSwapAndSendDisabledForNetwork = useSelector(
@@ -55,13 +73,6 @@ export const SendPageRecipientContent = ({
     return new Set(swapsBlockedTokens);
   }, [swapsBlockedTokens]);
 
-  let isSwapAllowed;
-
-  ///: BEGIN:ONLY_INCLUDE_IF(build-mmi)
-  isSwapAllowed = false;
-  ///: END:ONLY_INCLUDE_IF
-
-  ///: BEGIN:ONLY_INCLUDE_IF(build-main,build-beta,build-flask)
   isSwapAllowed =
     isSwapsChain &&
     !isSwapAndSendDisabledForNetwork &&
