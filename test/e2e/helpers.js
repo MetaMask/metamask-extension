b1b6df6f16a3949244f8bde80d0d9e5e69f275dc
Micaela Estabillo <micaela.estabillo@consensys.net>
2024-07-17 17:50:33 -0600

    feat: deploy smartContracts to secondary ganache networks

--- test/e2e/helpers.js
+++ test/e2e/helpers.js
@@ -86,6 +86,7 @@ async function withFixtures(options, testSuite) {
   const https = await mockttp.generateCACertificate();
   const mockServer = mockttp.getLocal({ https, cors: true });
   const secondaryGanacheServer = [];
+  const secondaryContractRegistry = [];
   let numberOfDapps = dapp ? 1 : 0;
   const dappServer = [];
   const phishingPageServer = new PhishingWarningPageServer();
@@ -127,6 +128,21 @@ async function withFixtures(options, testSuite) {
           vmErrorsOnRPCResponse: false,
           ...ganacheOptions2,
         });
+        if (smartContract && !disableGanache) {
+          const secondaryGanacheSeeder = new GanacheSeeder(
+            server.getProvider(),
+          );
+          const contracts =
+            smartContract instanceof Array ? smartContract : [smartContract];
+          await Promise.all(
+            contracts.map((contract) =>
+              secondaryGanacheSeeder.deploySmartContract(contract),
+            ),
+          );
+          secondaryContractRegistry.push(
+            secondaryGanacheSeeder.getContractRegistry(),
+          );
+        }
       });
     }
 
@@ -217,6 +233,7 @@ async function withFixtures(options, testSuite) {
       mockedEndpoint,
       bundlerServer,
       mockServer,
+      secondaryContractRegistry,
     });
 
     const errorsAndExceptions = driver.summarizeErrorsAndExceptions();
