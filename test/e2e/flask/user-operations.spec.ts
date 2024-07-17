648064572b48dbf413664b0a12a326d5d19eb1dc
HJetpoluru <harika.jetpoluru@consensys.net>
2024-07-17 16:39:59 -0400

    conditional wait added

--- test/e2e/flask/user-operations.spec.ts
+++ test/e2e/flask/user-operations.spec.ts
@@ -105,11 +105,13 @@ async function createSwap(driver: Driver) {
 
   await driver.waitForSelector({ text: 'Swap', tag: 'button' });
   await driver.clickElement({ text: 'Swap', tag: 'button' });
-  await driver.waitForSelector({ text: 'Close', tag: 'button' });
-  await driver.assertElementNotPresent({
-    text: 'View in activity',
-    tag: 'button',
-  });
+  await driver.assertElementNotPresent(
+    {
+      text: 'View in activity',
+      tag: 'button',
+    },
+    { findElementGuard: { text: 'Close', tag: 'button' } },
+  ); // wait for close button to appear
   await driver.clickElement({ text: 'Close', tag: 'button' });
 }
 
