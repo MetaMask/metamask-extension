f2308a5315d121ed298d0c0764d62e1319b6706f
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2024-07-22 11:23:26 +0100

    wip

--- ui/pages/confirmations/hooks/useQueuedConfirmationEvents.ts
+++ ui/pages/confirmations/hooks/useQueuedConfirmationEvents.ts
@@ -16,20 +16,25 @@ export const useQueuedConfirmationsEvent = (queueType: QueueType) => {
   const queuedRequestCount = useSelector(getQueuedRequestCount);
   const trackEvent = useContext(MetaMetricsContext);
 
-  const properties = {
-    confirmation_type: pendingApprovals[0].type,
-    referrer: pendingApprovals[0].origin,
-    queue_size: queuedRequestCount,
-    queue_type: queueType,
-  };
-
   useEffect(() => {
-    if (queuedRequestCount > 0) {
+    if (pendingApprovals.length > 0 && queuedRequestCount > 0) {
+      const properties = {
+        confirmation_type: pendingApprovals[0].type,
+        referrer: pendingApprovals[0].origin,
+        queue_size: queuedRequestCount,
+        queue_type: queueType,
+      };
+
       trackEvent({
         event: MetaMetricsEventName.ConfirmationQueued,
         category: MetaMetricsEventCategory.Confirmations,
         properties,
       });
     }
-  }, [JSON.stringify(properties), queuedRequestCount]);
+  }, [
+    JSON.stringify(pendingApprovals),
+    queuedRequestCount,
+    queueType,
+    trackEvent,
+  ]);
 };
