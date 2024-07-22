b32f047c75a3d565369f934649afbf0ab8a7b49d
Michele Esposito <michele@esposito.codes>
2024-07-22 16:02:51 +0200

    fix: revert `metamask-controller.test.js` changes

--- app/scripts/metamask-controller.test.js
+++ app/scripts/metamask-controller.test.js
@@ -40,10 +40,6 @@ import { flushPromises } from '../../test/lib/timer-helpers';
 import { ETH_EOA_METHODS } from '../../shared/constants/eth-methods';
 import { createMockInternalAccount } from '../../test/jest/mocks';
 import {
-  KNOWN_PUBLIC_KEY,
-  KNOWN_PUBLIC_KEY_ADDRESSES,
-} from '../../test/stub/keyring-bridge';
-import {
   BalancesController as MultichainBalancesController,
   BALANCES_UPDATE_TIME as MULTICHAIN_BALANCES_UPDATE_TIME,
 } from './lib/accounts/BalancesController';
@@ -133,6 +129,37 @@ jest.mock(
       return controller;
     },
 );
+
+const KNOWN_PUBLIC_KEY =
+  '02065bc80d3d12b3688e4ad5ab1e9eda6adf24aec2518bfc21b87c99d4c5077ab0';
+
+const KNOWN_PUBLIC_KEY_ADDRESSES = [
+  {
+    address: '0x0e122670701207DB7c6d7ba9aE07868a4572dB3f',
+    balance: null,
+    index: 0,
+  },
+  {
+    address: '0x2ae19DAd8b2569F7Bb4606D951Cc9495631e818E',
+    balance: null,
+    index: 1,
+  },
+  {
+    address: '0x0051140bAaDC3E9AC92A4a90D18Bb6760c87e7ac',
+    balance: null,
+    index: 2,
+  },
+  {
+    address: '0x9DBCF67CC721dBd8Df28D7A0CbA0fa9b0aFc6472',
+    balance: null,
+    index: 3,
+  },
+  {
+    address: '0x828B2c51c5C1bB0c57fCD2C108857212c95903DE',
+    balance: null,
+    index: 4,
+  },
+];
 
 const buildMockKeyringBridge = (publicKeyPayload) =>
   jest.fn(() => ({
