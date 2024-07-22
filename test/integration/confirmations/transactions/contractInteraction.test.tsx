637134c291e0c1b2e3bebad560a44dbe17a8863a
Priya Narayanaswamy <priya.narayanaswamy@consensys.net>
2024-07-22 13:56:29 +0200

    test: refactor create new file for transaction data

--- test/integration/confirmations/transactions/contractInteraction.test.tsx
+++ test/integration/confirmations/transactions/contractInteraction.test.tsx
@@ -1,17 +1,17 @@
 import { fireEvent, waitFor } from '@testing-library/react';
 import { ApprovalType } from '@metamask/controller-utils';
+import nock from 'nock';
+import { TransactionType } from '@metamask/transaction-controller';
 import mockMetaMaskState from '../../data/integration-init-state.json';
 import { integrationTestRender } from '../../../lib/render-helpers';
-import { shortenAddress } from '../../../../ui/helpers/utils/util';
 import * as backgroundConnection from '../../../../ui/store/background-connection';
 import {
   MetaMetricsEventCategory,
   MetaMetricsEventName,
   MetaMetricsEventLocation,
 } from '../../../../shared/constants/metametrics';
-import nock from 'nock';
 import { createMockImplementation, mock4byte } from '../../helpers';
-import { TransactionType } from '@metamask/transaction-controller';
+import { getUnapprovedTransaction } from './transactionDataHelpers';
 
 jest.mock('../../../../ui/store/background-connection', () => ({
   ...jest.requireActual('../../../../ui/store/background-connection'),
@@ -23,10 +23,12 @@ const mockedBackgroundConnection = jest.mocked(backgroundConnection);
 const backgroundConnectionMocked = {
   onNotification: jest.fn(),
 };
+export const pendingTransactionId = '48a75190-45ca-11ef-9001-f3886ec2397c';
+export const pendingTransactionTime = new Date().getTime();
 
-const getMetaMaskStateWithUnapprovedContractInteraction = (accountAddress: string) => {
-  const pendingTransactionId = '48a75190-45ca-11ef-9001-f3886ec2397c';
-  const pendingTransactionTime = new Date().getTime();
+const getMetaMaskStateWithUnapprovedContractInteraction = (
+  accountAddress: string,
+) => {
   return {
     ...mockMetaMaskState,
     preferences: {
@@ -47,80 +49,22 @@ const getMetaMaskStateWithUnapprovedContractInteraction = (accountAddress: strin
       },
     },
     pendingApprovalCount: 1,
-    "knownMethodData": {
-      "0x3b4b1381": {
-        "name": "Mint N F Ts",
-        "params": [
+    knownMethodData: {
+      '0x3b4b1381': {
+        name: 'Mint NFTs',
+        params: [
           {
-            "type": "uint256"
-          }
-        ]
-      }
+            type: 'uint256',
+          },
+        ],
+      },
     },
     transactions: [
-    {
-      "actionId": 4256525906,
-      "chainId": "0x5",
-      "dappSuggestedGasFees": {
-          "gas": "0x16a92"
-      },
-      "id": pendingTransactionId,
-      "origin": 'origin',
-      "securityAlertResponse": {},
-      "status": "unapproved",
-      "time": pendingTransactionTime,
-      "txParams": {
-          "from": accountAddress,
-          "data": "0x3b4b13810000000000000000000000000000000000000000000000000000000000000001",
-          "gas": "0x16a92",
-          "to": "0x076146c765189d51be3160a2140cf80bfc73ad68",
-          "value": "0x0",
-          "maxFeePerGas": "0x5b06b0c0d",
-          "maxPriorityFeePerGas": "0x59682f00"
-      },
-      "userEditedGasLimit": false,
-      "verifiedOnBlockchain": false,
-      "type": TransactionType.contractInteraction,
-      "networkClientId": "sepolia",
-      "defaultGasEstimates": {
-        "gas": "0x16a92",
-        "maxFeePerGas": "0x5b06b0c0d",
-        "maxPriorityFeePerGas": "0x59682f00",
-        "estimateType": "medium"
-      },
-      "userFeeLevel": "medium",
-      "sendFlowHistory": [],
-      "history": [],
-      "simulationData": {
-        "tokenBalanceChanges": [
-          {
-            "address": "0x076146c765189d51be3160a2140cf80bfc73ad68",
-            "standard": "erc721",
-            "id": "0x01",
-            "previousBalance": "0x0",
-            "newBalance": "0x1",
-            "difference": "0x1",
-            "isDecrease": false
-          }
-        ]
-      },
-      "gasFeeEstimates": {
-        "type": "fee-market",
-        "low": {
-          "maxFeePerGas": "0x451632798",
-          "maxPriorityFeePerGas": "0x3b9aca00"
-        },
-        "medium": {
-          "maxFeePerGas": "0x5dd36ad5a",
-          "maxPriorityFeePerGas": "0x59682f00"
-        },
-        "high": {
-          "maxFeePerGas": "0x7690a331c",
-          "maxPriorityFeePerGas": "0x77359400"
-        }
-    },
-    "gasFeeEstimatesLoaded": true
-    }
+      getUnapprovedTransaction(
+        accountAddress,
+        pendingTransactionId,
+        pendingTransactionTime,
+      ),
     ],
   };
 };
@@ -129,12 +73,17 @@ describe('Contract Interaction Confirmation', () => {
   beforeEach(() => {
     jest.resetAllMocks();
     mock4byte();
-      mockedBackgroundConnection.submitRequestToBackground.mockImplementation(
-        createMockImplementation('getGasFeeTimeEstimate', { lowerTimeBound: new Date().getTime(), upperTimeBound: new Date().getTime()}),
-      );
+    mockedBackgroundConnection.submitRequestToBackground.mockImplementation(
+      createMockImplementation('getGasFeeTimeEstimate', {
+        lowerTimeBound: new Date().getTime(),
+        upperTimeBound: new Date().getTime(),
+      }),
+    );
   });
 
-  afterEach(() => { nock.cleanAll(); });
+  afterEach(() => {
+    nock.cleanAll();
+  });
 
   it('displays the header account modal with correct data', async () => {
     const account =
@@ -144,9 +93,8 @@ describe('Contract Interaction Confirmation', () => {
       ];
 
     const accountName = account.metadata.name;
-    const mockedMetaMaskState = getMetaMaskStateWithUnapprovedContractInteraction(
-      account.address,
-    );
+    const mockedMetaMaskState =
+      getMetaMaskStateWithUnapprovedContractInteraction(account.address);
 
     const { getByTestId, queryByTestId } = await integrationTestRender({
       preloadedState: mockedMetaMaskState,
