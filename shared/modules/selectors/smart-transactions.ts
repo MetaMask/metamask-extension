MetaMask/metamask-extension

commit 4e1ccf23f8bbe9a18231798d1b8b74ab936027fb

Cherry pick: feat: Replace "Manage in settings" with "No thanks" in the STX Opt In modal, only show the modal for non-zero balances (#25893)
Conflicts:
app/_locales/de/messages.json
app/_locales/el/messages.json
app/_locales/en/messages.json
app/_locales/es/messages.json
app/_locales/fr/messages.json
app/_locales/hi/messages.json
app/_locales/id/messages.json
app/_locales/ja/messages.json
app/_locales/ko/messages.json
app/_locales/pt/messages.json
app/_locales/ru/messages.json
app/_locales/tl/messages.json
app/_locales/tr/messages.json
app/_locales/vi/messages.json
app/_locales/zh_CN/messages.json
test/e2e/playwright/swap/pageObjects/swap-page.ts #
ui/pages/onboarding-flow/metametrics/metametrics.js

## **Description**
Cherry pick of this PR:
https://github.com/MetaMask/metamask-extension/pull/25848

[![Open in GitHub
Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/25893?quickstart=1)

## **Related issues**

Fixes:

## **Manual testing steps**

1. Go to this page...
2.
3.

## **Screenshots/Recordings**

<!-- If applicable, add screenshots and/or recordings to visualize the
before and after of your change. -->

### **Before**

<!-- [screenshots/recordings] -->

### **After**

<!-- [screenshots/recordings] -->

## **Pre-merge author checklist**

- [ ] I've followed [MetaMask Contributor
Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
Extension Coding
Standards](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/CODING_GUIDELINES.md).
- [ ] I've completed the PR template to the best of my ability
- [ ] I’ve included tests if applicable
- [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
if applicable
- [ ] I’ve applied the right labels on the PR (see [labeling
guidelines](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/LABELING_GUIDELINES.md)).

shared/modules/selectors/smart-transactions.ts
@@ -7,8 +7,11 @@ import {
   getCurrentChainId,
   getCurrentNetwork,
   accountSupportsSmartTx,
+  getSelectedAccount,
 } from '../../../ui/selectors/selectors'; // TODO: Migrate shared selectors to this file.
 import { isProduction } from '../environment';
+
+import { MultichainState } from '../../../ui/selectors/multichain';
 
 type SmartTransactionsMetaMaskState = {
   metamask: {
@@ -89,13 +92,27 @@ const getIsAllowedRpcUrlForSmartTransactions = (
   return rpcUrl?.hostname?.endsWith('.infura.io');
 };
 
+/**
+ * Checks if the selected account has a non-zero balance.
+ *
+ * @param state - The state object containing account information.
+ * @returns true if the selected account has a non-zero balance, otherwise false.
+ */
+const hasNonZeroBalance = (state: SmartTransactionsMetaMaskState) => {
+  const selectedAccount = getSelectedAccount(
+    state as unknown as MultichainState,
+  );
+  return BigInt(selectedAccount?.balance || '0x0') > 0n;
+};
+
 export const getIsSmartTransactionsOptInModalAvailable = (
   state: SmartTransactionsMetaMaskState,
 ) => {
   return (
     getCurrentChainSupportsSmartTransactions(state) &&
     getIsAllowedRpcUrlForSmartTransactions(state) &&
-    getSmartTransactionsOptInStatus(state) === null
+    getSmartTransactionsOptInStatus(state) === null &&
+    hasNonZeroBalance(state)
   );
 };
 

shared/modules/selectors/index.test.ts
@@ -5,6 +5,7 @@ import {
   getCurrentChainSupportsSmartTransactions,
   getSmartTransactionsEnabled,
   getIsSmartTransaction,
+  getIsSmartTransactionsOptInModalAvailable,
 } from '.';
 
 describe('Selectors', () => {
@@ -24,7 +25,15 @@ describe('Selectors', () => {
                   type: 'Hardware',
                 },
               },
+              address: '0x123',
+              type: 'eip155:eoa',
             },
+          },
+        },
+        accounts: {
+          '0x123': {
+            address: '0x123',
+            balance: '0x15f6f0b9d4f8d000',
           },
         },
         providerConfig: {
@@ -248,6 +257,113 @@ describe('Selectors', () => {
       };
       const result = getIsSmartTransaction(newState);
       expect(result).toBe(false);
+    });
+  });
+
+  describe('getIsSmartTransactionsOptInModalAvailable', () => {
+    it('returns true for Ethereum Mainnet + supported RPC URL + null opt-in status and non-zero balance', () => {
+      const state = createMockState();
+      const newState = {
+        ...state,
+        metamask: {
+          ...state.metamask,
+          preferences: {
+            ...state.metamask.preferences,
+            smartTransactionsOptInStatus: null,
+          },
+        },
+      };
+      expect(getIsSmartTransactionsOptInModalAvailable(newState)).toBe(true);
+    });
+
+    it('returns false for Polygon Mainnet + supported RPC URL + null opt-in status and non-zero balance', () => {
+      const state = createMockState();
+      const newState = {
+        ...state,
+        metamask: {
+          ...state.metamask,
+          preferences: {
+            ...state.metamask.preferences,
+            smartTransactionsOptInStatus: null,
+          },
+          providerConfig: {
+            ...state.metamask.providerConfig,
+            chainId: CHAIN_IDS.POLYGON,
+          },
+        },
+      };
+      expect(getIsSmartTransactionsOptInModalAvailable(newState)).toBe(false);
+    });
+
+    it('returns false for Ethereum Mainnet + unsupported RPC URL + null opt-in status and non-zero balance', () => {
+      const state = createMockState();
+      const newState = {
+        ...state,
+        metamask: {
+          ...state.metamask,
+          preferences: {
+            ...state.metamask.preferences,
+            smartTransactionsOptInStatus: null,
+          },
+          networkConfigurations: {
+            'network-configuration-id-1': {
+              chainId: CHAIN_IDS.MAINNET,
+              ticker: CURRENCY_SYMBOLS.ETH,
+              rpcUrl: 'https://mainnet.quiknode.pro/',
+            },
+          },
+        },
+      };
+      expect(getIsSmartTransactionsOptInModalAvailable(newState)).toBe(false);
+    });
+
+    it('returns false for Ethereum Mainnet + supported RPC URL + true opt-in status and non-zero balance', () => {
+      const state = createMockState();
+      expect(getIsSmartTransactionsOptInModalAvailable(state)).toBe(false);
+    });
+
+    it('returns false for Ethereum Mainnet + supported RPC URL + null opt-in status and zero balance (0x0)', () => {
+      const state = createMockState();
+      const newState = {
+        ...state,
+        metamask: {
+          ...state.metamask,
+          preferences: {
+            ...state.metamask.preferences,
+            smartTransactionsOptInStatus: null,
+          },
+          accounts: {
+            ...state.metamask.accounts,
+            '0x123': {
+              address: '0x123',
+              balance: '0x0',
+            },
+          },
+        },
+      };
+      expect(getIsSmartTransactionsOptInModalAvailable(newState)).toBe(false);
+    });
+
+    it('returns false for Ethereum Mainnet + supported RPC URL + null opt-in status and zero balance (0x00)', () => {
+      const state = createMockState();
+      const newState = {
+        ...state,
+        metamask: {
+          ...state.metamask,
+          preferences: {
+            ...state.metamask.preferences,
+            smartTransactionsOptInStatus: null,
+          },
+          accounts: {
+            ...state.metamask.accounts,
+            '0x123': {
+              address: '0x123',
+              balance: '0x00',
+            },
+          },
+        },
+      };
+      expect(getIsSmartTransactionsOptInModalAvailable(newState)).toBe(false);
     });
   });
 });

ui/components/app/smart-transactions/smart-transactions-opt-in-modal.test.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { fireEvent } from '@testing-library/react';
 import thunk from 'redux-thunk';
 import configureMockStore from 'redux-mock-store';
-import { useHistory } from 'react-router-dom';
 
 import {
   renderWithProvider,
   createSwapsMockStore,
 } from '../../../../test/jest';
 import { setSmartTransactionsOptInStatus } from '../../../store/actions';
-import { ADVANCED_ROUTE } from '../../../helpers/constants/routes';
 import SmartTransactionsOptInModal from './smart-transactions-opt-in-modal';
 
 const middleware = [thunk];
@@ -27,18 +25,14 @@ describe('SmartTransactionsOptInModal', () => {
       store,
     );
     expect(getByText('Enable')).toBeInTheDocument();
-    expect(getByText('Manage in settings')).toBeInTheDocument();
+    expect(getByText('No thanks')).toBeInTheDocument();
     expect(container).toMatchSnapshot();
   });
 
-  it('calls setSmartTransactionsOptInStatus with false when the "Manage in settings" link is clicked and redirects to Advanced Settings', () => {
+  it('calls setSmartTransactionsOptInStatus with false when the "No thanks" link is clicked', () => {
     (setSmartTransactionsOptInStatus as jest.Mock).mockImplementationOnce(() =>
       jest.fn(),
     );
-    const historyPushMock = jest.fn();
-    (useHistory as jest.Mock).mockImplementationOnce(() => ({
-      push: historyPushMock,
-    }));
     const store = configureMockStore(middleware)(createSwapsMockStore());
     const { getByText } = renderWithProvider(
       <SmartTransactionsOptInModal
@@ -47,12 +41,9 @@ describe('SmartTransactionsOptInModal', () => {
       />,
       store,
     );
-    const manageInSettingsLink = getByText('Manage in settings');
-    fireEvent.click(manageInSettingsLink);
+    const noThanksLink = getByText('No thanks');
+    fireEvent.click(noThanksLink);
     expect(setSmartTransactionsOptInStatus).toHaveBeenCalledWith(false);
-    expect(historyPushMock).toHaveBeenCalledWith(
-      `${ADVANCED_ROUTE}#smart-transactions`,
-    );
   });
 
   it('calls setSmartTransactionsOptInStatus with true when the "Enable" button is clicked', () => {

ui/components/app/smart-transactions/smart-transactions-opt-in-modal.tsx
@@ -1,5 +1,4 @@
 import React, { useCallback, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
 import { useI18nContext } from '../../../hooks/useI18nContext';
@@ -31,7 +30,6 @@ import {
 } from '../../component-library';
 import { setSmartTransactionsOptInStatus } from '../../../store/actions';
 import { SMART_TRANSACTIONS_LEARN_MORE_URL } from '../../../../shared/constants/smartTransactions';
-import { ADVANCED_ROUTE } from '../../../helpers/constants/routes';
 
 export type SmartTransactionsOptInModalProps = {
   isOpen: boolean;
@@ -75,10 +73,10 @@ const EnableSmartTransactionsButton = ({
   );
 };
 
-const ManageInSettingsLink = ({
-  handleManageInSettingsLinkClick,
+const NoThanksLink = ({
+  handleNoThanksLinkClick,
 }: {
-  handleManageInSettingsLinkClick: () => void;
+  handleNoThanksLinkClick: () => void;
 }) => {
   const t = useI18nContext();
   return (
@@ -87,11 +85,11 @@ const ManageInSettingsLink = ({
       type="link"
       variant={ButtonVariant.Link}
       color={TextColor.textAlternative}
-      onClick={handleManageInSettingsLinkClick}
+      onClick={handleNoThanksLinkClick}
       width={BlockSize.Full}
       className="mm-smart-transactions-opt-in-modal__no-thanks-link"
     >
-      {t('manageInSettings')}
+      {t('noThanks')}
     </Button>
   );
 };
@@ -166,16 +164,14 @@ export default function SmartTransactionsOptInModal({
 }: SmartTransactionsOptInModalProps) {
   const t = useI18nContext();
   const dispatch = useDispatch();
-  const history = useHistory();
 
   const handleEnableButtonClick = useCallback(() => {
     dispatch(setSmartTransactionsOptInStatus(true));
   }, [dispatch]);
 
-  const handleManageInSettingsLinkClick = useCallback(() => {
+  const handleNoThanksLinkClick = useCallback(() => {
     // Set the Smart Transactions opt-in status to false, so the opt-in modal is not shown again.
     dispatch(setSmartTransactionsOptInStatus(false));
-    history.push(`${ADVANCED_ROUTE}#smart-transactions`);
   }, [dispatch]);
 
   useEffect(() => {
@@ -215,9 +211,7 @@ export default function SmartTransactionsOptInModal({
           <EnableSmartTransactionsButton
             handleEnableButtonClick={handleEnableButtonClick}
           />
-          <ManageInSettingsLink
-            handleManageInSettingsLinkClick={handleManageInSettingsLinkClick}
-          />
+          <NoThanksLink handleNoThanksLinkClick={handleNoThanksLinkClick} />
         </Box>
       </ModalContent>
     </Modal>

ui/pages/onboarding-flow/metametrics/metametrics.js
@@ -258,7 +258,7 @@ export default function OnboardingMetametrics() {
             large
             onClick={onCancel}
           >
-            {t('onboardingMetametricsDisagree')}
+            {t('noThanks')}
           </Button>
         </div>
       </div>
@@ -392,7 +392,7 @@ export default function OnboardingMetametrics() {
             large
             onClick={onCancel}
           >
-            {t('onboardingMetametricsDisagree')}
+            {t('noThanks')}
           </Button>
         </div>
       </div>

ui/pages/onboarding-flow/metametrics/metametrics.test.js
@@ -6,7 +6,7 @@ import { renderWithProvider } from '../../../../test/lib/render-helpers';
 import { ONBOARDING_CREATE_PASSWORD_ROUTE } from '../../../helpers/constants/routes';
 import {
   onboardingMetametricsAgree,
-  onboardingMetametricsDisagree,
+  noThanks,
 } from '../../../../app/_locales/en/messages.json';
 import {
   setParticipateInMetaMetrics,
@@ -102,7 +102,7 @@ describe('Onboarding Metametrics Component', () => {
       mockStore,
     );
 
-    const confirmCancel = queryByText(onboardingMetametricsDisagree.message);
+    const confirmCancel = queryByText(noThanks.message);
 
     fireEvent.click(confirmCancel);
 
@@ -120,7 +120,7 @@ describe('Onboarding Metametrics Component', () => {
       mockStore,
     );
 
-    const confirmCancel = queryByText(onboardingMetametricsDisagree.message);
+    const confirmCancel = queryByText(noThanks.message);
 
     fireEvent.click(confirmCancel);
 

commit bd750974b133477e906526211dfa257d0653e647

fix: Fix v12.0.0 lint error
Fix a lint error introduced in #25893. A type was imported that does
not exist on this branch. The type was not needed on this branch
either.

shared/modules/selectors/smart-transactions.ts
@@ -11,8 +11,6 @@ import {
 } from '../../../ui/selectors/selectors'; // TODO: Migrate shared selectors to this file.
 import { isProduction } from '../environment';
 
-import { MultichainState } from '../../../ui/selectors/multichain';
-
 type SmartTransactionsMetaMaskState = {
   metamask: {
     preferences: {
@@ -99,9 +97,7 @@ const getIsAllowedRpcUrlForSmartTransactions = (
  * @returns true if the selected account has a non-zero balance, otherwise false.
  */
 const hasNonZeroBalance = (state: SmartTransactionsMetaMaskState) => {
-  const selectedAccount = getSelectedAccount(
-    state as unknown as MultichainState,
-  );
+  const selectedAccount = getSelectedAccount(state);
   return BigInt(selectedAccount?.balance || '0x0') > 0n;
 };
 