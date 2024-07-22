a0b82c71abce052936ebeafec635d1321ac7c08c
OGPoyraz <omergoktugpoyraz@gmail.com>
2024-07-22 14:40:41 +0200

    fix lint

--- ui/hooks/useFiatFormatter.ts
+++ ui/hooks/useFiatFormatter.ts
@@ -5,16 +5,23 @@ import { shortenString } from '../helpers/utils/util';
 
 /**
  * Returns a function that formats a fiat amount as a localized string.
+ * The hook takes an optional boolean parameter to shorten the fiat value.
  *
  * Example usage:
  *
  * ```
  * const formatFiat = useFiatFormatter();
  * const formattedAmount = formatFiat(1000);
+ *
+ * const shorteningFiatFormatter = useFiatFormatter(true);
+ * const shortenedAmount = shorteningFiatFormatter(100000000000000000);
  * ```
  *
  * @returns A function that takes a fiat amount as a number and returns a formatted string.
  */
+
+const TRUNCATED_CHAR_LIMIT_FOR_SHORTENED_FIAT = 15;
+const TRUNCATED_START_CHARS_FOR_SHORTENED_FIAT = 12;
 
 type FiatFormatter = (fiatAmount: number) => string;
 
@@ -48,8 +55,8 @@ export const useFiatFormatter = (shortenFiatValue?: boolean): FiatFormatter => {
 
       // Shorten the number part while preserving commas
       const shortenedNumberString = shortenString(numberString, {
-        truncatedCharLimit: 15,
-        truncatedStartChars: 12,
+        truncatedCharLimit: TRUNCATED_CHAR_LIMIT_FOR_SHORTENED_FIAT,
+        truncatedStartChars: TRUNCATED_START_CHARS_FOR_SHORTENED_FIAT,
         truncatedEndChars: 0,
         skipCharacterInEnd: true,
       });
@@ -67,8 +74,8 @@ export const useFiatFormatter = (shortenFiatValue?: boolean): FiatFormatter => {
       // Fallback for unknown or unsupported currencies
       const formattedNumber = new Intl.NumberFormat(locale).format(fiatAmount);
       const shortenedNumberString = shortenString(formattedNumber, {
-        truncatedCharLimit: 15,
-        truncatedStartChars: 12,
+        truncatedCharLimit: TRUNCATED_CHAR_LIMIT_FOR_SHORTENED_FIAT,
+        truncatedStartChars: TRUNCATED_START_CHARS_FOR_SHORTENED_FIAT,
         truncatedEndChars: 0,
         skipCharacterInEnd: true,
       });
