4f88ce0cda276960154f46aca3bb7477d48b1217
Jiexi Luan <jiexiluan@gmail.com>
2024-07-23 09:43:02 -0700

    fix wallet_revokePermissions

--- app/scripts/lib/multichain-api/wallet-revokePermissions.js
+++ app/scripts/lib/multichain-api/wallet-revokePermissions.js
@@ -47,7 +47,9 @@ async function revokePermissionsImplementation(
 
   // For now, this API revokes the entire permission key
   // even if caveats are specified.
-  const permissionKeys = Object.keys(param);
+  const permissionKeys = Object.keys(param).filter(
+    (name) => name !== Caip25EndowmentPermissionName,
+  );
 
   if (!isNonEmptyArray(permissionKeys)) {
     return end(invalidParams({ data: { request: req } }));
@@ -60,52 +62,50 @@ async function revokePermissionsImplementation(
     permissionKeys.includes(RestrictedMethods.eth_accounts)
   ) {
     const permissions = getPermissionsForOrigin(origin);
-    const caip25endowment = permissions[Caip25EndowmentPermissionName];
-    if (caip25endowment) {
-      const caip25caveat = caip25endowment.caveats.find(
-        ({ type }) => type === Caip25CaveatType,
-      );
-      if (!caip25caveat) {
-        return 'what...';
-      }
-
-      // should we remove accounts from required scopes? if so doesn't that mean we should
-      // just revoke the caip25endowment entirely?
-
-      const requiredScopesWithoutEip155Accounts = {};
-      Object.entries(caip25caveat.value.requiredScopes).forEach(
-        ([scopeString, scopeObject]) => {
-          const { namespace } = parseScopeString(scopeString);
-          requiredScopesWithoutEip155Accounts[scopeString] = {
-            ...scopeObject,
-            accounts:
-              namespace === KnownCaipNamespace.Eip155
-                ? []
-                : scopeObject.accounts,
-          };
-        },
-      );
-
-      const optionalScopesWithoutEip155Accounts = {};
-      Object.entries(caip25caveat.value.optionalScopes).forEach(
-        ([scopeString, scopeObject]) => {
-          const { namespace } = parseScopeString(scopeString);
-          optionalScopesWithoutEip155Accounts[scopeString] = {
-            ...scopeObject,
-            accounts:
-              namespace === KnownCaipNamespace.Eip155
-                ? []
-                : scopeObject.accounts,
-          };
-        },
-      );
-
-      updateCaveat(origin, Caip25EndowmentPermissionName, Caip25CaveatType, {
-        ...caip25caveat.value,
-        requiredScopes: requiredScopesWithoutEip155Accounts,
-        optionalScopes: optionalScopesWithoutEip155Accounts,
-      });
+    const caip25endowment = permissions?.[Caip25EndowmentPermissionName];
+    const caip25caveat = caip25endowment?.caveats.find(
+      ({ type }) => type === Caip25CaveatType,
+    );
+    if (!caip25caveat) {
+      return 'what...';
     }
+
+    // should we remove accounts from required scopes? if so doesn't that mean we should
+    // just revoke the caip25endowment entirely?
+
+    const requiredScopesWithoutEip155Accounts = {};
+    Object.entries(caip25caveat.value.requiredScopes).forEach(
+      ([scopeString, scopeObject]) => {
+        const { namespace } = parseScopeString(scopeString);
+        requiredScopesWithoutEip155Accounts[scopeString] = {
+          ...scopeObject,
+          accounts:
+            namespace === KnownCaipNamespace.Eip155
+              ? []
+              : scopeObject.accounts,
+        };
+      },
+    );
+
+    const optionalScopesWithoutEip155Accounts = {};
+    Object.entries(caip25caveat.value.optionalScopes).forEach(
+      ([scopeString, scopeObject]) => {
+        const { namespace } = parseScopeString(scopeString);
+        optionalScopesWithoutEip155Accounts[scopeString] = {
+          ...scopeObject,
+          accounts:
+            namespace === KnownCaipNamespace.Eip155
+              ? []
+              : scopeObject.accounts,
+        };
+      },
+    );
+
+    updateCaveat(origin, Caip25EndowmentPermissionName, Caip25CaveatType, {
+      ...caip25caveat.value,
+      requiredScopes: requiredScopesWithoutEip155Accounts,
+      optionalScopes: optionalScopesWithoutEip155Accounts,
+    });
   }
 
   res.result = null;
