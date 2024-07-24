c4282fc15dc4e12ab79b4a508c067b10adf7b74a
Alex <adonesky@gmail.com>
2024-07-24 10:24:46 -0500

    fix issue with signature-request-original

--- ui/pages/confirmations/components/signature-request-original/signature-request-original.component.js
+++ ui/pages/confirmations/components/signature-request-original/signature-request-original.component.js
@@ -129,298 +129,292 @@ export default class SignatureRequestOriginal extends Component {
       rows = [{ name: this.context.t('message'), value: hexToText(data) }];
     } else if (type === MESSAGE_TYPE.ETH_SIGN_TYPED_DATA) {
       rows = data;
-
-      const targetSubjectMetadata = txData.msgParams.origin
-        ? subjectMetadata?.[txData.msgParams.origin]
-        : null;
-
-      return (
-        <div className="request-signature__body">
-          <BlockaidBannerAlert
-            txData={txData}
-            marginTop={4}
-            marginLeft={4}
-            marginRight={4}
-          />
-          {isSuspiciousResponse(txData?.securityProviderResponse) && (
-            <SecurityProviderBannerMessage
-              securityProviderResponse={txData.securityProviderResponse}
-            />
-          )}
-          {
-            ///: BEGIN:ONLY_INCLUDE_IF(build-mmi)
-            this.props.selectedAccount.address ===
-            this.props.fromAccount.address ? null : (
-              <Box
-                className="request-signature__mismatch-info"
-                display={Display.Flex}
-                width={BlockSize.Full}
-                padding={4}
-                marginBottom={4}
-                backgroundColor={BackgroundColor.primaryMuted}
-              >
-                <Icon
-                  name={IconName.Info}
-                  color={IconColor.infoDefault}
-                  marginRight={2}
-                />
-                <Text
-                  variant={TextVariant.bodyXs}
-                  color={TextColor.textDefault}
-                >
-                  {this.context.t('mismatchAccount', [
-                    shortenAddress(this.props.selectedAccount.address),
-                    shortenAddress(this.props.fromAccount.address),
-                  ])}
-                </Text>
-              </Box>
-            )
-            ///: END:ONLY_INCLUDE_IF
-          }
-          <div className="request-signature__origin">
-            {
-              // Use legacy authorship header for snaps
-              targetSubjectMetadata?.subjectType === SubjectType.Snap ? (
-                <SnapLegacyAuthorshipHeader
-                  snapId={targetSubjectMetadata.origin}
-                  marginLeft={4}
-                  marginRight={4}
-                />
-              ) : (
-                <SiteOrigin
-                  title={txData.msgParams.origin}
-                  siteOrigin={txData.msgParams.origin}
-                  iconSrc={targetSubjectMetadata?.iconUrl}
-                  iconName={
-                    getURLHostName(targetSubjectMetadata?.origin) ||
-                    targetSubjectMetadata?.origin
-                  }
-                  chip
-                />
-              )
-            }
-          </div>
-          <Typography
-            className="request-signature__content__title"
-            variant={TypographyVariant.H3}
-            fontWeight={FontWeight.Bold}
-          >
-            {this.context.t('sigRequest')}
-          </Typography>
-          <Typography
-            className="request-signature__content__subtitle"
-            variant={TypographyVariant.H7}
-            color={TextColor.textAlternative}
-            align={TextAlign.Center}
-            margin={12}
-            marginTop={3}
-          >
-            {this.context.t('signatureRequestGuidance')}
-          </Typography>
-          <div className={classnames('request-signature__notice')}>
-            {notice}
-          </div>
-          <div className="request-signature__rows">
-            {rows.map(({ name, value }, index) => {
-              if (typeof value === 'boolean') {
-                // eslint-disable-next-line no-param-reassign
-                value = value.toString();
-              }
-              return (
-                <div
-                  className="request-signature__row"
-                  key={`request-signature-row-${index}`}
-                >
-                  <div className="request-signature__row-title">
-                    {sanitizeString(`${name}:`)}
-                  </div>
-                  <div className="request-signature__row-value">
-                    {sanitizeString(value)}
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      );
     }
 
-    onSubmit = async () => {
-      const {
-        resolvePendingApproval,
-        completedTx,
-        clearConfirmTransaction,
-        history,
-        mostRecentOverviewPage,
-        txData,
-      } = this.props;
-      ///: BEGIN:ONLY_INCLUDE_IF(build-mmi)
-      if (this.props.mmiOnSignCallback) {
-        await this.props.mmiOnSignCallback(txData);
-        return;
-      }
-      ///: END:ONLY_INCLUDE_IF
+    const targetSubjectMetadata = txData.msgParams.origin
+      ? subjectMetadata?.[txData.msgParams.origin]
+      : null;
 
-      await resolvePendingApproval(txData.id);
-      completedTx(txData.id);
-      clearConfirmTransaction();
-      history.push(mostRecentOverviewPage);
-    };
-
-    onCancel = async () => {
-      const {
-        clearConfirmTransaction,
-        history,
-        mostRecentOverviewPage,
-        rejectPendingApproval,
-        txData: { id },
-      } = this.props;
-
-      await rejectPendingApproval(
-        id,
-        serializeError(ethErrors.provider.userRejectedRequest()),
-      );
-      clearConfirmTransaction();
-      history.push(mostRecentOverviewPage);
-    };
-
-    renderFooter = () => {
-      const {
-        clearConfirmTransaction,
-        history,
-        mostRecentOverviewPage,
-        txData,
-        hardwareWalletRequiresConnection,
-        rejectPendingApproval,
-        warnings,
-      } = this.props;
-      const { t } = this.context;
-
-      const submitButtonType =
-        txData.securityAlertResponse?.result_type ===
-        BlockaidResultType.Malicious
-          ? 'danger-primary'
-          : 'primary';
-      return (
-        <PageContainerFooter
-          cancelText={t('reject')}
-          submitText={t('sign')}
-          onCancel={async () => {
-            await rejectPendingApproval(
-              txData.id,
-              serializeError(ethErrors.provider.userRejectedRequest()),
-            );
-            clearConfirmTransaction();
-            history.push(mostRecentOverviewPage);
-          }}
-          onSubmit={async () => {
-            if (warnings?.length >= 1) {
-              return this.setState({ showSignatureInsights: true });
-            }
-
-            return await this.onSubmit();
-          }}
-          disabled={
-            ///: BEGIN:ONLY_INCLUDE_IF(build-mmi)
-            Boolean(txData?.custodyId) ||
-            ///: END:ONLY_INCLUDE_IF
-            hardwareWalletRequiresConnection
-          }
-          submitButtonType={submitButtonType}
+    return (
+      <div className="request-signature__body">
+        <BlockaidBannerAlert
+          txData={txData}
+          marginTop={4}
+          marginLeft={4}
+          marginRight={4}
         />
-      );
-    };
+        {isSuspiciousResponse(txData?.securityProviderResponse) && (
+          <SecurityProviderBannerMessage
+            securityProviderResponse={txData.securityProviderResponse}
+          />
+        )}
+        {
+          ///: BEGIN:ONLY_INCLUDE_IF(build-mmi)
+          this.props.selectedAccount.address ===
+          this.props.fromAccount.address ? null : (
+            <Box
+              className="request-signature__mismatch-info"
+              display={Display.Flex}
+              width={BlockSize.Full}
+              padding={4}
+              marginBottom={4}
+              backgroundColor={BackgroundColor.primaryMuted}
+            >
+              <Icon
+                name={IconName.Info}
+                color={IconColor.infoDefault}
+                marginRight={2}
+              />
+              <Text variant={TextVariant.bodyXs} color={TextColor.textDefault}>
+                {this.context.t('mismatchAccount', [
+                  shortenAddress(this.props.selectedAccount.address),
+                  shortenAddress(this.props.fromAccount.address),
+                ])}
+              </Text>
+            </Box>
+          )
+          ///: END:ONLY_INCLUDE_IF
+        }
+        <div className="request-signature__origin">
+          {
+            // Use legacy authorship header for snaps
+            targetSubjectMetadata?.subjectType === SubjectType.Snap ? (
+              <SnapLegacyAuthorshipHeader
+                snapId={targetSubjectMetadata.origin}
+                marginLeft={4}
+                marginRight={4}
+              />
+            ) : (
+              <SiteOrigin
+                title={txData.msgParams.origin}
+                siteOrigin={txData.msgParams.origin}
+                iconSrc={targetSubjectMetadata?.iconUrl}
+                iconName={
+                  getURLHostName(targetSubjectMetadata?.origin) ||
+                  targetSubjectMetadata?.origin
+                }
+                chip
+              />
+            )
+          }
+        </div>
+        <Typography
+          className="request-signature__content__title"
+          variant={TypographyVariant.H3}
+          fontWeight={FontWeight.Bold}
+        >
+          {this.context.t('sigRequest')}
+        </Typography>
+        <Typography
+          className="request-signature__content__subtitle"
+          variant={TypographyVariant.H7}
+          color={TextColor.textAlternative}
+          align={TextAlign.Center}
+          margin={12}
+          marginTop={3}
+        >
+          {this.context.t('signatureRequestGuidance')}
+        </Typography>
+        <div className={classnames('request-signature__notice')}>{notice}</div>
+        <div className="request-signature__rows">
+          {rows.map(({ name, value }, index) => {
+            if (typeof value === 'boolean') {
+              // eslint-disable-next-line no-param-reassign
+              value = value.toString();
+            }
+            return (
+              <div
+                className="request-signature__row"
+                key={`request-signature-row-${index}`}
+              >
+                <div className="request-signature__row-title">
+                  {sanitizeString(`${name}:`)}
+                </div>
+                <div className="request-signature__row-value">
+                  {sanitizeString(value)}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  };
 
-    handleCancelAll = () => {
-      const {
-        clearConfirmTransaction,
-        history,
-        mostRecentOverviewPage,
-        showRejectTransactionsConfirmationModal,
-        messagesCount,
-        cancelAllApprovals,
-      } = this.props;
-      const unapprovedTxCount = messagesCount;
+  onSubmit = async () => {
+    const {
+      resolvePendingApproval,
+      completedTx,
+      clearConfirmTransaction,
+      history,
+      mostRecentOverviewPage,
+      txData,
+    } = this.props;
+    ///: BEGIN:ONLY_INCLUDE_IF(build-mmi)
+    if (this.props.mmiOnSignCallback) {
+      await this.props.mmiOnSignCallback(txData);
+      return;
+    }
+    ///: END:ONLY_INCLUDE_IF
 
-      showRejectTransactionsConfirmationModal({
-        unapprovedTxCount,
-        onSubmit: async () => {
-          await cancelAllApprovals();
+    await resolvePendingApproval(txData.id);
+    completedTx(txData.id);
+    clearConfirmTransaction();
+    history.push(mostRecentOverviewPage);
+  };
+
+  onCancel = async () => {
+    const {
+      clearConfirmTransaction,
+      history,
+      mostRecentOverviewPage,
+      rejectPendingApproval,
+      txData: { id },
+    } = this.props;
+
+    await rejectPendingApproval(
+      id,
+      serializeError(ethErrors.provider.userRejectedRequest()),
+    );
+    clearConfirmTransaction();
+    history.push(mostRecentOverviewPage);
+  };
+
+  renderFooter = () => {
+    const {
+      clearConfirmTransaction,
+      history,
+      mostRecentOverviewPage,
+      txData,
+      hardwareWalletRequiresConnection,
+      rejectPendingApproval,
+      warnings,
+    } = this.props;
+    const { t } = this.context;
+
+    const submitButtonType =
+      txData.securityAlertResponse?.result_type === BlockaidResultType.Malicious
+        ? 'danger-primary'
+        : 'primary';
+    return (
+      <PageContainerFooter
+        cancelText={t('reject')}
+        submitText={t('sign')}
+        onCancel={async () => {
+          await rejectPendingApproval(
+            txData.id,
+            serializeError(ethErrors.provider.userRejectedRequest()),
+          );
           clearConfirmTransaction();
           history.push(mostRecentOverviewPage);
-        },
-      });
-    };
+        }}
+        onSubmit={async () => {
+          if (warnings?.length >= 1) {
+            return this.setState({ showSignatureInsights: true });
+          }
 
-    render = () => {
-      const {
-        messagesCount,
-        fromAccount: { address, name },
-        txData,
-        warnings,
-      } = this.props;
-      const { showSignatureRequestWarning } = this.state;
-      const { t } = this.context;
+          return await this.onSubmit();
+        }}
+        disabled={
+          ///: BEGIN:ONLY_INCLUDE_IF(build-mmi)
+          Boolean(txData?.custodyId) ||
+          ///: END:ONLY_INCLUDE_IF
+          hardwareWalletRequiresConnection
+        }
+        submitButtonType={submitButtonType}
+      />
+    );
+  };
 
-      const rejectNText = t('rejectRequestsN', [messagesCount]);
+  handleCancelAll = () => {
+    const {
+      clearConfirmTransaction,
+      history,
+      mostRecentOverviewPage,
+      showRejectTransactionsConfirmationModal,
+      messagesCount,
+      cancelAllApprovals,
+    } = this.props;
+    const unapprovedTxCount = messagesCount;
 
-      return (
-        <div className="request-signature__container">
-          <div className="request-signature__navigation">
-            <ConfirmPageContainerNavigation />
-          </div>
-          <div className="request-signature__account">
-            <SignatureRequestHeader txData={txData} />
-          </div>
-          {this.renderBody()}
-          {this.props.isLedgerWallet ? (
-            <div className="confirm-approve-content__ledger-instruction-wrapper">
-              <LedgerInstructionField showDataInstruction />
-            </div>
-          ) : null}
-          {showSignatureRequestWarning && (
-            <SignatureRequestOriginalWarning
-              senderAddress={address}
-              name={name}
-              onSubmit={async () => {
-                if (warnings?.length >= 1) {
-                  return this.setState({
-                    showSignatureInsights: true,
-                    showSignatureRequestWarning: false,
-                  });
-                }
+    showRejectTransactionsConfirmationModal({
+      unapprovedTxCount,
+      onSubmit: async () => {
+        await cancelAllApprovals();
+        clearConfirmTransaction();
+        history.push(mostRecentOverviewPage);
+      },
+    });
+  };
 
-                return await this.onSubmit();
-              }}
-              onCancel={async (event) => await this.onCancel(event)}
-            />
-          )}
-          {this.state.showSignatureInsights && (
-            <InsightWarnings
-              warnings={warnings}
-              action="signing"
-              origin={txData.msgParams.origin}
-              onCancel={() => {
-                this.setState({ showSignatureInsights: false });
-              }}
-              onSubmit={async () => {
-                await this.onSubmit();
-                this.setState({ showSignatureInsights: false });
-              }}
-            />
-          )}
-          {this.renderFooter()}
-          {messagesCount > 1 ? (
-            <ButtonLink
-              size={Size.inherit}
-              className="request-signature__container__reject"
-              onClick={() => this.handleCancelAll()}
-            >
-              {rejectNText}
-            </ButtonLink>
-          ) : null}
+  render = () => {
+    const {
+      messagesCount,
+      fromAccount: { address, name },
+      txData,
+      warnings,
+    } = this.props;
+    const { showSignatureRequestWarning } = this.state;
+    const { t } = this.context;
+
+    const rejectNText = t('rejectRequestsN', [messagesCount]);
+
+    return (
+      <div className="request-signature__container">
+        <div className="request-signature__navigation">
+          <ConfirmPageContainerNavigation />
         </div>
-      );
-    };
+        <div className="request-signature__account">
+          <SignatureRequestHeader txData={txData} />
+        </div>
+        {this.renderBody()}
+        {this.props.isLedgerWallet ? (
+          <div className="confirm-approve-content__ledger-instruction-wrapper">
+            <LedgerInstructionField showDataInstruction />
+          </div>
+        ) : null}
+        {showSignatureRequestWarning && (
+          <SignatureRequestOriginalWarning
+            senderAddress={address}
+            name={name}
+            onSubmit={async () => {
+              if (warnings?.length >= 1) {
+                return this.setState({
+                  showSignatureInsights: true,
+                  showSignatureRequestWarning: false,
+                });
+              }
+
+              return await this.onSubmit();
+            }}
+            onCancel={async (event) => await this.onCancel(event)}
+          />
+        )}
+        {this.state.showSignatureInsights && (
+          <InsightWarnings
+            warnings={warnings}
+            action="signing"
+            origin={txData.msgParams.origin}
+            onCancel={() => {
+              this.setState({ showSignatureInsights: false });
+            }}
+            onSubmit={async () => {
+              await this.onSubmit();
+              this.setState({ showSignatureInsights: false });
+            }}
+          />
+        )}
+        {this.renderFooter()}
+        {messagesCount > 1 ? (
+          <ButtonLink
+            size={Size.inherit}
+            className="request-signature__container__reject"
+            onClick={() => this.handleCancelAll()}
+          >
+            {rejectNText}
+          </ButtonLink>
+        ) : null}
+      </div>
+    );
   };
 }
