d6da04edb1950d5eea81b75880900966f6060f33
digiwand <20778143+digiwand@users.noreply.github.com>
2024-07-25 19:00:56 +0300

    refactor: freeze PERMIT_PRIMARY_TYPE object

--- shared/constants/signatures.ts
+++ shared/constants/signatures.ts
@@ -1,7 +1,7 @@
 /**
  * EIP-712 Permit PrimaryTypes
  */
-export const PERMIT_PRIMARY_TYPE = {
+export const PERMIT_PRIMARY_TYPE = Object.freeze({
   PERMIT: 'Permit',
   PERMIT_BATCH: 'PermitBatch',
   PERMIT_BATCH_TRANSFER_FROM: 'PermitBatchTransferFrom',
@@ -9,7 +9,7 @@ export const PERMIT_PRIMARY_TYPE = {
   PERMIT_TRANSFER_FROM: 'PermitTransferFrom',
   ORDER: 'Order',
   ORDER_COMPONENTS: 'OrderComponents',
-};
+});
 
 export const PERMIT_PRIMARY_TYPES: string[] =
   Object.values(PERMIT_PRIMARY_TYPE);
