c7a77bf3c0cd8d000ea447cdb675847330bfe145
HJetpoluru <harika.jetpoluru@consensys.net>
2024-07-25 12:33:21 -0400

    Replaced with openMenuSafe

--- test/e2e/tests/settings/4byte-directory.spec.js
+++ test/e2e/tests/settings/4byte-directory.spec.js
@@ -4,6 +4,7 @@ const {
   withFixtures,
   openDapp,
   unlockWallet,
+  openMenuSafe,
   largeDelayMs,
   veryLargeDelayMs,
   WINDOW_TITLES,
@@ -69,14 +70,7 @@ describe('4byte setting', function () {
         await unlockWallet(driver);
 
         // goes to the settings screen
-        await driver.clickElement(
-          '[data-testid="account-options-menu-button"]',
-        );
-        if (process.env.MMI) {
-          await driver.waitForSelector(
-            '[data-testid="global-menu-mmi-portfolio"]',
-          );
-        }
+        await openMenuSafe(driver);
         await driver.clickElement({ text: 'Settings', tag: 'div' });
         await driver.clickElement({ text: 'Security & privacy', tag: 'div' });
 
