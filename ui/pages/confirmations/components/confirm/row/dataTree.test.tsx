a48250ab905fdbff0410ee3057e291eb6505e33f
digiwand <20778143+digiwand@users.noreply.github.com>
2024-07-25 11:52:35 +0300

    refactor: create dataTree mock-data file

--- ui/pages/confirmations/components/confirm/row/dataTree.test.tsx
+++ ui/pages/confirmations/components/confirm/row/dataTree.test.tsx
@@ -4,67 +4,16 @@ import { permitSignatureMsg } from '../../../../../../test/data/confirmations/ty
 import mockState from '../../../../../../test/data/mock-state.json';
 import { renderWithProvider } from '../../../../../../test/lib/render-helpers';
 import configureStore from '../../../../../store/store';
+import { MOCK_PERMIT } from './dataTree.test.mock-data';
 
 import { DataTree } from './dataTree';
-
-const mockData = {
-  contents: { value: 'Hello, Bob!', type: 'string' },
-  from: {
-    value: {
-      name: { value: 'Cow', type: 'string' },
-      wallets: {
-        value: [
-          {
-            value: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
-            type: 'address',
-          },
-          {
-            value: '0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF',
-            type: 'address',
-          },
-        ],
-        type: 'address[]',
-      },
-    },
-    type: 'Person',
-  },
-  to: {
-    value: [
-      {
-        value: {
-          name: { value: 'Bob', type: 'string' },
-          wallets: {
-            value: [
-              {
-                value: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
-                type: 'address',
-              },
-              {
-                value: '0xB0BdaBea57B0BDABeA57b0bdABEA57b0BDabEa57',
-                type: 'address',
-              },
-              {
-                value: '0xB0B0b0b0b0b0B000000000000000000000000000',
-                type: 'address',
-              },
-            ],
-            type: 'address[]',
-          },
-        },
-        type: 'Person',
-      },
-    ],
-    type: 'Person[]',
-  },
-  attachment: { value: '0x', type: 'bytes' },
-};
 
 const store = configureStore(mockState);
 
 describe('DataTree', () => {
   it('should match snapshot', () => {
     const { container } = renderWithProvider(
-      <DataTree data={mockData} />,
+      <DataTree data={MOCK_PERMIT} />,
       store,
     );
     expect(container).toMatchSnapshot();
