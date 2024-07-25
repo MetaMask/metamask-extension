211e0aada788c82b61ad83dccb6aa770d52e1e82
digiwand <20778143+digiwand@users.noreply.github.com>
2024-07-25 18:05:59 +0300

    fix: get tokenDecimals for relevant token

--- ui/pages/confirmations/components/confirm/row/dataTree.tsx
+++ ui/pages/confirmations/components/confirm/row/dataTree.tsx
@@ -1,8 +1,9 @@
-import React, { memo } from 'react';
+import React, { memo, useEffect, useState } from 'react';
 
 import { PERMIT_PRIMARY_TYPE } from '../../../../../../shared/constants/transaction';
 import { isValidHexAddress } from '../../../../../../shared/modules/hexstring-utils';
 import { sanitizeString } from '../../../../../helpers/utils/util';
+import { getTokenStandardAndDetails } from '../../../../../store/actions';
 
 import { Box } from '../../../../../components/component-library';
 import { BlockSize } from '../../../../../helpers/constants/design-system';
@@ -21,6 +22,27 @@ export type TreeData = {
   type: string;
 };
 
+const getTokenDecimalsFromDataTree = async (
+  dataTreeData: Record<string, TreeData> | TreeData[],
+): Promise<void | number> => {
+  if (Array.isArray(dataTreeData)) {
+    return undefined;
+  }
+
+  const tokenContract = (dataTreeData as Record<string, TreeData>).token
+    ?.value as string;
+  if (!tokenContract) {
+    return undefined;
+  }
+
+  const decimals = parseInt(
+    (await getTokenStandardAndDetails(tokenContract)).decimals ?? '0',
+    10,
+  );
+
+  return decimals;
+};
+
 export const DataTree = ({
   data,
   isPermit = false,
@@ -31,31 +53,45 @@ export const DataTree = ({
   isPermit?: boolean;
   primaryType?: PERMIT_PRIMARY_TYPE;
   tokenDecimals?: number;
-}) => (
-  <Box width={BlockSize.Full}>
-    {Object.entries(data).map(([label, { value, type }], i) => (
-      <ConfirmInfoRow
-        label={`${sanitizeString(
-          label.charAt(0).toUpperCase() + label.slice(1),
-        )}:`}
-        style={{ paddingRight: 0 }}
-        key={`tree-data-${label}-index-${i}`}
-      >
-        {
-          // eslint-disable-next-line @typescript-eslint/no-use-before-define
-          <DataField
-            label={label}
-            isPermit={isPermit}
-            primaryType={primaryType}
-            value={value}
-            type={type}
-            tokenDecimals={tokenDecimals}
-          />
-        }
-      </ConfirmInfoRow>
-    ))}
-  </Box>
-);
+}) => {
+  const [tokenContractDecimals, setTokenContractDecimals] = useState<
+    number | undefined
+  >(undefined);
+
+  useEffect(() => {
+    getTokenDecimalsFromDataTree(data).then((decimals) => {
+      if (typeof decimals === 'number') {
+        setTokenContractDecimals(decimals);
+      }
+    });
+  }, [data]);
+
+  return (
+    <Box width={BlockSize.Full}>
+      {Object.entries(data).map(([label, { value, type }], i) => (
+        <ConfirmInfoRow
+          label={`${sanitizeString(
+            label.charAt(0).toUpperCase() + label.slice(1),
+          )}:`}
+          style={{ paddingRight: 0 }}
+          key={`tree-data-${label}-index-${i}`}
+        >
+          {
+            // eslint-disable-next-line @typescript-eslint/no-use-before-define
+            <DataField
+              label={label}
+              isPermit={isPermit}
+              primaryType={primaryType}
+              value={value}
+              type={type}
+              tokenDecimals={tokenContractDecimals ?? tokenDecimals}
+            />
+          }
+        </ConfirmInfoRow>
+      ))}
+    </Box>
+  );
+};
 
 const DataField = memo(
   ({
