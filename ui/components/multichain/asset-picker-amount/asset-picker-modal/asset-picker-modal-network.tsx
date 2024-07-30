54bb76173b13bed07d4e3ac16a2c286fccb1c8ca
Micaela Estabillo <micaela.estabillo@consensys.net>
2024-07-29 20:20:54 -0700

    feat: add network picker to asset-picker

---
+++ ui/components/multichain/asset-picker-amount/asset-picker-modal/asset-picker-modal-network.tsx
@@ -0,0 +1,99 @@
+import React from 'react';
+
+import { ProviderConfig } from '@metamask/network-controller';
+import { useSelector } from 'react-redux';
+import {
+  Display,
+  FlexDirection,
+  BlockSize,
+} from '../../../../helpers/constants/design-system';
+import {
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  Modal,
+  Box,
+} from '../../../component-library';
+import { RPCDefinition } from '../../../../../shared/constants/network';
+///: BEGIN:ONLY_INCLUDE_IF(build-main,build-beta,build-flask)
+import { useI18nContext } from '../../../../hooks/useI18nContext';
+///: END:ONLY_INCLUDE_IF
+import { NetworkListItem } from '../../network-list-item';
+import { getAllNetworks } from '../../../../selectors';
+import { getProviderConfig } from '../../../../ducks/metamask/metamask';
+
+/**
+ * AssetPickerModalNetwork component displays a modal for selecting a network in the asset picker.
+ *
+ * @param props
+ * @param props.isOpen - Determines whether the modal is open or not.
+ * @param props.network - The currently selected network, not necessarily the active wallet network.
+ * @param props.networks - The list of selectable networks.
+ * @param props.onNetworkChange - The callback function to handle network change.
+ * @param props.onClose - The callback function to handle modal close.
+ * @param props.onBack - The callback function to handle going back in the modal.
+ * @returns A modal with a list of selectable networks.
+ */
+export const AssetPickerModalNetwork = ({
+  isOpen,
+  onClose,
+  onBack,
+  network,
+  networks,
+  onNetworkChange,
+}: {
+  isOpen: boolean;
+  network?: ProviderConfig;
+  networks?: (ProviderConfig | RPCDefinition)[];
+  onNetworkChange: (network: ProviderConfig | RPCDefinition) => void;
+  onClose: () => void;
+  onBack: () => void;
+}) => {
+  ///: BEGIN:ONLY_INCLUDE_IF(build-main,build-beta,build-flask)
+  const t = useI18nContext();
+  ///: END:ONLY_INCLUDE_IF
+
+  const currentNetwork = useSelector(getProviderConfig);
+  const allNetworks: (ProviderConfig | RPCDefinition)[] =
+    useSelector(getAllNetworks);
+
+  const selectedNetwork: ProviderConfig = network ?? currentNetwork;
+  const networksList = networks ?? allNetworks ?? [];
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent modalDialogProps={{ padding: 0 }}>
+        <ModalHeader onBack={onBack} onClose={onClose}>
+          {t('bridgeSelectNetwork')}
+        </ModalHeader>
+        <Box className="multichain-asset-picker__network-list">
+          <Box
+            style={{
+              gridColumnStart: 1,
+              gridColumnEnd: 3,
+            }}
+            display={Display.Flex}
+            flexDirection={FlexDirection.Column}
+            height={BlockSize.Full}
+          >
+            {networksList.map((networkConfig) => {
+              const { nickname } = networkConfig;
+              return (
+                <NetworkListItem
+                  key={networkConfig.chainId}
+                  name={nickname ?? networkConfig.chainId}
+                  selected={selectedNetwork?.chainId === networkConfig.chainId}
+                  onClick={() => {
+                    onNetworkChange(networkConfig);
+                    onBack();
+                  }}
+                />
+              );
+            })}
+          </Box>
+        </Box>
+      </ModalContent>
+    </Modal>
+  );
+};
