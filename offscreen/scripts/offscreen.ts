a7ba45626252faac715c49a7170514dbf2425698
Michele Esposito <michele@esposito.codes>
2024-07-30 15:18:35 +0200

    fix: wait for ledger offscreen iframe load

--- offscreen/scripts/offscreen.ts
+++ offscreen/scripts/offscreen.ts
@@ -9,41 +9,53 @@ import initLedger from './ledger';
 import initTrezor from './trezor';
 import initLattice from './lattice';
 
-initLedger();
-initTrezor();
-initLattice();
-
 /**
  * Initialize a post message stream with the parent window that is initialized
  * in the metamask-controller (background/serivce worker) process. This will be
  * utilized by snaps for communication with snaps running in the offscreen
  * document.
  */
-const parentStream = new BrowserRuntimePostMessageStream({
-  name: 'child',
-  target: 'parent',
-});
-
-ProxySnapExecutor.initialize(parentStream, './snaps/index.html');
-
-if (process.env.IN_TEST) {
-  chrome.runtime.onMessage.addListener((message) => {
-    if (
-      message &&
-      typeof message === 'object' &&
-      message.event === OffscreenCommunicationEvents.metamaskBackgroundReady &&
-      message.target === OffscreenCommunicationTarget.extension
-    ) {
-      window.document?.documentElement?.classList?.add('controller-loaded');
-    }
+function initializePostMessageStream() {
+  const parentStream = new BrowserRuntimePostMessageStream({
+    name: 'child',
+    target: 'parent',
   });
+
+  ProxySnapExecutor.initialize(parentStream, './snaps/index.html');
 }
 
-chrome.runtime.sendMessage({
-  target: OffscreenCommunicationTarget.extensionMain,
-  isBooted: true,
+/**
+ * Initialize the ledger, trezor, and lattice keyring connections, and the
+ * post message stream for the Snaps environment.
+ */
+async function init(): Promise<void> {
+  await initLedger();
+  initTrezor();
+  initLattice();
+  initializePostMessageStream();
+}
 
-  // This message is being sent from the Offscreen Document to the Service Worker.
-  // The Service Worker has no way to query `navigator.webdriver`, so we send it here.
-  webdriverPresent: navigator.webdriver === true,
+init().then(() => {
+  if (process.env.IN_TEST) {
+    chrome.runtime.onMessage.addListener((message) => {
+      if (
+        message &&
+        typeof message === 'object' &&
+        message.event ===
+          OffscreenCommunicationEvents.metamaskBackgroundReady &&
+        message.target === OffscreenCommunicationTarget.extension
+      ) {
+        window.document?.documentElement?.classList?.add('controller-loaded');
+      }
+    });
+  }
+
+  chrome.runtime.sendMessage({
+    target: OffscreenCommunicationTarget.extensionMain,
+    isBooted: true,
+
+    // This message is being sent from the Offscreen Document to the Service Worker.
+    // The Service Worker has no way to query `navigator.webdriver`, so we send it here.
+    webdriverPresent: navigator.webdriver === true,
+  });
 });
