4001062d996f24ee8dffe323d4e1d829970f3af6
Nicholas Gambino <nicholas.gambino@consensys.net>
2024-07-31 12:10:49 -0700

    Add useSafeChains hook, repIdentifierUtility, and remove sha256 hash due to privacy concerns

---
+++ ui/pages/settings/networks-tab/networks-form/use-safe-chains.ts
@@ -0,0 +1,59 @@
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+
+import { useSafeChainsListValidationSelector } from '../../../../selectors';
+import fetchWithCache from '../../../../../shared/lib/fetch-with-cache';
+import { DAY } from '../../../../../shared/constants/time';
+
+export type SafeChain = {
+  chainId: string;
+  name: string;
+  nativeCurrency: { symbol: string };
+  rpc: string[];
+};
+
+export const useSafeChains = () => {
+  const useSafeChainsListValidation = useSelector(
+    useSafeChainsListValidationSelector,
+  );
+
+  const [safeChains, setSafeChains] = useState<{
+    safeChains?: SafeChain[];
+    error?: any;
+  }>({ safeChains: [] });
+
+  if (useSafeChainsListValidation) {
+    useEffect(() => {
+      fetchWithCache({
+        url: 'https://chainid.network/chains.json',
+        functionName: 'getSafeChainsList',
+        cacheOptions: { cacheRefreshTime: 0, timeout: DAY },
+      })
+        .then((response) => {
+          setSafeChains({ safeChains: response });
+        })
+        .catch((error) => {
+          setSafeChains({ error });
+        });
+    }, []);
+  }
+
+  return safeChains;
+};
+
+export const rpcIdentifierUtility = (
+  rpcUrl: string,
+  safeChains: SafeChain[],
+) => {
+  const knownRpcUrls = new Set();
+
+  safeChains.forEach((chain) => {
+    chain.rpc.forEach((rpc) => {
+      const sanitizedRpc = new URL(rpc).host;
+      knownRpcUrls.add(sanitizedRpc);
+    });
+  });
+
+  const sanitizedRpc = new URL(rpcUrl).host;
+  return knownRpcUrls.has(sanitizedRpc) ? sanitizedRpc : 'Unknown rpcUrl';
+};
