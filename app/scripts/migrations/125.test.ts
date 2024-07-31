90e58a93fd723ceae61b5b39918ceb82625f1334
Alex <adonesky@gmail.com>
2024-07-31 14:15:33 -0500

    add migration to remove any dangling instances of perDomainNetwork on SelectedNetworkController state

---
+++ app/scripts/migrations/125.test.ts
@@ -0,0 +1,97 @@
+import { migrate, version } from './125';
+
+const oldVersion = 124;
+
+describe('migration #125', () => {
+  it('updates the version metadata', async () => {
+    const oldStorage = {
+      meta: { version: oldVersion },
+      data: {},
+    };
+
+    const newStorage = await migrate(oldStorage);
+
+    expect(newStorage.meta).toStrictEqual({ version });
+  });
+
+  it('does nothing if SelectedNetworkController state is not set', async () => {
+    const oldState = {
+      OtherController: {},
+    };
+
+    const transformedState = await migrate({
+      meta: { version: oldVersion },
+      data: oldState,
+    });
+
+    expect(transformedState.data).toEqual(oldState);
+  });
+
+  it('sets SelectedNetworkController state to an object containing an empty object "domains" if SelectedNetworkController state is not itself an object', async () => {
+    const oldState = {
+      SelectedNetworkController: 'foo',
+    };
+
+    const expectedState = {
+      SelectedNetworkController: { domains: {} },
+    };
+
+    const transformedState = await migrate({
+      meta: { version: oldVersion },
+      data: oldState,
+    });
+
+    expect(transformedState.data).toEqual(expectedState);
+  });
+
+  it('removes "perDomainNetwork" property and resets "domains" object in SelectedNetworkController state if "perDomainNetwork" property is present', async () => {
+    const oldState = {
+      SelectedNetworkController: {
+        domains: {
+          'https://metamask.io': {
+            network: 'mainnet',
+          },
+        },
+        perDomainNetwork: true,
+      },
+    };
+
+    const expectedState = {
+      SelectedNetworkController: {
+        domains: {},
+      },
+    };
+
+    const transformedState = await migrate({
+      meta: { version: oldVersion },
+      data: oldState,
+    });
+
+    expect(transformedState.data).toEqual(expectedState);
+  });
+
+  it('leavs "domains" state unchanged in SelectedNetworkController if "perDomainNetwork" property is not present in SelectedNetworkController state', async () => {
+    const oldState = {
+      SelectedNetworkController: {
+        domains: {
+          'https://metamask.io': {
+            network: 'mainnet',
+          },
+          'https://test.io': {
+            network: 'linea',
+          },
+          'https://uniswap.io': {
+            network: 'optimism',
+          },
+        },
+      },
+    };
+
+    const transformedState = await migrate({
+      meta: { version: oldVersion },
+      data: oldState,
+    });
+
+    expect(transformedState.data).toEqual(oldState);
+  });
+});
---
+++ app/scripts/migrations/125.ts
@@ -0,0 +1,48 @@
+import { cloneDeep } from 'lodash';
+import { hasProperty, isObject } from '@metamask/utils';
+
+type VersionedData = {
+  meta: { version: number };
+  data: Record<string, unknown>;
+};
+
+export const version = 125;
+
+/**
+ * This migration removes any dangling instances of SelectedNetworkController.perDomainNetwork
+ *
+ * @param originalVersionedData - Versioned MetaMask extension state, exactly what we persist to dist.
+ * @param originalVersionedData.meta - State metadata.
+ * @param originalVersionedData.meta.version - The current state version.
+ * @param originalVersionedData.data - The persisted MetaMask state, keyed by controller.
+ * @returns Updated versioned MetaMask extension state.
+ */
+export async function migrate(
+  originalVersionedData: VersionedData,
+): Promise<VersionedData> {
+  const versionedData = cloneDeep(originalVersionedData);
+  versionedData.meta.version = version;
+  transformState(versionedData.data);
+  return versionedData;
+}
+
+function transformState(state: Record<string, any>) {
+  if (!hasProperty(state, 'SelectedNetworkController')) {
+    return state;
+  }
+
+  if (!isObject(state.SelectedNetworkController)) {
+    global.sentry?.captureException?.(
+      new Error(
+        `state.SelectedNetworkController is type: ${typeof state.SelectedNetworkController}`,
+      ),
+    );
+    state.SelectedNetworkController = { domains: {} };
+  } else if (hasProperty(state.SelectedNetworkController, 'perDomainNetwork')) {
+    state.SelectedNetworkController = {
+      domains: {},
+    };
+  }
+
+  return state;
+}
