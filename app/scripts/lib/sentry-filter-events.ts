e6d9d9a6bea2fd7b7a69d98976aa7d977465fd7a
Matthew Walsh <matthew.walsh@consensys.net>
2024-07-31 11:57:12 +0100

    fix: sentry sessions (#26192)
    Remove all dynamic `autoSessionTracking` enablement.
    Add a custom Sentry transport to prevent any requests reaching Sentry if metrics are disabled.
    Only consider metrics enabled when preference is set and onboarding is complete.
    Enable Sentry if `METAMASK_ENVIRONMENT` is not `production` and `SENTRY_DSN_DEV` is specified.
    Remove the arguments to the `setupSentry` function.
    Flatten all the functions within `setupSentry.js`.
    Log all Sentry messages via the `debug` package based on the `DEBUG` environment variable.
    Add additional log messages for easier debug.
    Add mock `SENTRY_DSN_DEV` to Flask and MMI test builds.
    Remove duplication in package scripts.

--- app/scripts/lib/sentry-filter-events.ts
+++ app/scripts/lib/sentry-filter-events.ts
@@ -1,5 +1,4 @@
 import { Event as SentryEvent, Integration } from '@sentry/types';
-import { logger } from '@sentry/utils';
 
 const NAME = 'FilterEvents';
 
@@ -8,17 +7,27 @@ const NAME = 'FilterEvents';
  *
  * @param options - Options bag.
  * @param options.getMetaMetricsEnabled - Function that returns whether MetaMetrics is enabled.
+ * @param options.log - Function to log messages.
  */
 export function filterEvents({
   getMetaMetricsEnabled,
+  log,
 }: {
   getMetaMetricsEnabled: () => Promise<boolean>;
+  log: (message: string) => void;
 }): Integration {
   return {
     name: NAME,
     processEvent: async (event: SentryEvent) => {
-      if (!(await getMetaMetricsEnabled())) {
-        logger.warn(`Event dropped due to MetaMetrics setting.`);
+      // This integration is required in addition to the custom transport as it provides an
+      // asynchronous context which we may need in order to read the persisted state from the
+      // store, so it can later be added to the event via the `beforeSend` overload.
+      // It also provides a more native solution for discarding events, but any
+      // session requests will always be handled by the custom transport.
+      const metricsEnabled = await getMetaMetricsEnabled();
+
+      if (!metricsEnabled) {
+        log('Event dropped as metrics disabled');
         return null;
       }
 
