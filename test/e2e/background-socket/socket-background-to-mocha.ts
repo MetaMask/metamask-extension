3842060b6b5af1ea35a43f68a248cadf665efa76
Howard Braham <howrad@gmail.com>
2024-07-31 22:09:47 -0400

    added to socket-background-to-mocha.ts

--- test/e2e/background-socket/socket-background-to-mocha.ts
+++ test/e2e/background-socket/socket-background-to-mocha.ts
@@ -1,5 +1,6 @@
 import log from 'loglevel';
 import { MessageType, WindowProperties } from './types';
+import { isManifestV3 } from '../../../shared/modules/mv3.utils';
 
 /**
  * This singleton class runs on the Extension background script (service worker in MV3).
@@ -86,10 +87,7 @@ class SocketBackgroundToMocha {
 
   // This function exists to support both MV2 and MV3
   private async queryTabs(queryInfo: object): Promise<chrome.tabs.Tab[]> {
-    if (
-      process.env.ENABLE_MV3 === 'true' ||
-      process.env.ENABLE_MV3 === undefined
-    ) {
+    if (isManifestV3) {
       // With MV3, chrome.tabs.query has an await form
       return await chrome.tabs.query(queryInfo);
     }
