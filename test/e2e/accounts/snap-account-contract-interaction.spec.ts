3bfb266e39b94407592650e4a8787ecc888baca7
Plasma Corral <32695229+plasmacorral@users.noreply.github.com>
2024-08-02 18:03:31 -0400

    reorganize

--- test/e2e/accounts/snap-account-contract-interaction.spec.ts
+++ test/e2e/accounts/snap-account-contract-interaction.spec.ts
@@ -1,25 +1,23 @@
-/* eslint-disable @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires */
 import GanacheContractAddressRegistry from '../seeder/ganache-contract-address-registry';
 import { scrollAndConfirmAndAssertConfirm } from '../tests/confirmations/helpers';
 import {
   createDepositTransaction,
   TestSuiteArguments,
 } from '../tests/confirmations/transactions/shared';
-import { installSnapSimpleKeyring, importKeyAndSwitch } from './common';
-
-const {
+import {
   multipleGanacheOptionsForType2Transactions,
   withFixtures,
   openDapp,
   WINDOW_TITLES,
-} = require('../helpers');
-const FixtureBuilder = require('../fixture-builder');
-const { SMART_CONTRACTS } = require('../seeder/smart-contracts');
+} from '../helpers';
+import FixtureBuilder from '../fixture-builder';
+import { SMART_CONTRACTS } from '../seeder/smart-contracts';
+import { installSnapSimpleKeyring, importKeyAndSwitch } from './common';
 
 describe('Snap Account - Contract interaction', function () {
   const smartContract = SMART_CONTRACTS.PIGGYBANK;
 
-  it(`deposit to piggybank contract`, async function () {
+  it('deposits to piggybank contract', async function () {
     await withFixtures(
       {
         dapp: true,
@@ -40,11 +38,13 @@ describe('Snap Account - Contract interaction', function () {
         // Install Snap Simple Keyring and import key
         await installSnapSimpleKeyring(driver, false);
         await importKeyAndSwitch(driver);
+
         // Open DApp with contract
         const contractAddress = await (
           contractRegistry as GanacheContractAddressRegistry
         ).getContractAddress(smartContract);
         await openDapp(driver, contractAddress);
+
         // Create and confirm deposit transaction
         await driver.switchToWindowWithTitle(
           WINDOW_TITLES.ExtensionInFullScreenView,
