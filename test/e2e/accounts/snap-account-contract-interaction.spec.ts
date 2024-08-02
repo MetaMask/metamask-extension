548292919118b1ae6d4011fd69f6e85fe58a1b3c
Plasma Corral <32695229+plasmacorral@users.noreply.github.com>
2024-08-01 22:38:10 -0400

    Update test

--- test/e2e/accounts/snap-account-contract-interaction.spec.ts
+++ test/e2e/accounts/snap-account-contract-interaction.spec.ts
@@ -1,18 +1,16 @@
 /* eslint-disable @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires */
+import GanacheContractAddressRegistry from '../seeder/ganache-contract-address-registry';
 import {
-  confirmDepositTransaction,
   createDepositTransaction,
   TestSuiteArguments,
 } from '../tests/confirmations/transactions/shared';
-import GanacheContractAddressRegistry from '../seeder/ganache-contract-address-registry';
+import { installSnapSimpleKeyring, importKeyAndSwitch } from './common';
 
 const {
   multipleGanacheOptionsForType2Transactions,
   withFixtures,
-  logInWithBalanceValidation,
   openDapp,
   WINDOW_TITLES,
-  locateAccountBalanceDOM,
 } = require('../helpers');
 const {
   scrollAndConfirmAndAssertConfirm,
@@ -23,7 +21,7 @@ const { SMART_CONTRACTS } = require('../seeder/smart-contracts');
 describe('Snap Account - Contract interaction', function () {
   const smartContract = SMART_CONTRACTS.PIGGYBANK;
 
-  it(`deposit and then withdraw from piggybank contract`, async function () {
+  it(`deposit to piggybank contract`, async function () {
     await withFixtures(
       {
         dapp: true,
@@ -35,66 +33,32 @@ describe('Snap Account - Contract interaction', function () {
               isRedesignedConfirmationsDeveloperEnabled: true,
             },
           })
-          .withKeyringControllerSnapAccountVault()
-          .withPreferencesControllerSnapAccountIdentities()
-          .withAccountsControllerAdditionalSnapAccount()
           .build(),
         ganacheOptions: multipleGanacheOptionsForType2Transactions,
         smartContract,
         title: this.test?.fullTitle(),
       },
-      async ({
-        driver,
-        contractRegistry,
-        ganacheServer,
-      }: TestSuiteArguments) => {
+      async ({ driver, contractRegistry }: TestSuiteArguments) => {
+        // Install Snap Simple Keyring and import key
+        await installSnapSimpleKeyring(driver, false);
+        await importKeyAndSwitch(driver);
+        // Open DApp with contract
         const contractAddress = await (
           contractRegistry as GanacheContractAddressRegistry
         ).getContractAddress(smartContract);
-
-        await logInWithBalanceValidation(driver);
-        await driver.clickElement('[data-testid="account-menu-icon"]');
-        await driver.clickElement({
-          tag: 'Button',
-          text: 'Snap Account 1',
-        });
-        await driver.assertElementNotPresent({
-          tag: 'header',
-          text: 'Select an account',
-        });
         await openDapp(driver, contractAddress);
-
+        await driver.switchToWindowWithTitle(
+          WINDOW_TITLES.ExtensionInFullScreenView,
+        );
+        // Create and confirm deposit transaction
         await createDepositTransaction(driver);
-        await driver.delay(7000);
-        await confirmDepositTransaction(driver);
-
-        // calls and confirms a contract method where ETH is received
-        await driver.switchToWindowWithTitle(WINDOW_TITLES.TestDApp);
-        await driver.clickElement('#withdrawButton');
-        await driver.waitUntilXWindowHandles(3);
-
+        await driver.waitUntilXWindowHandles(4);
         await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
-        await scrollAndConfirmAndAssertConfirm(driver);
-        await driver.waitUntilXWindowHandles(2);
-        await driver.switchToWindowWithTitle(
-          WINDOW_TITLES.ExtensionInFullScreenView,
-        );
-        await driver.clickElement(
-          '[data-testid="account-overview__activity-tab"]',
-        );
-        await driver.waitForSelector(
-          '.transaction-list__completed-transactions .activity-list-item:nth-of-type(2)',
-        );
         await driver.waitForSelector({
-          css: '[data-testid="transaction-list-item-primary-currency"]',
-          text: '-0 ETH',
+          css: 'h2',
+          text: 'Transaction request',
         });
-
-        // renders the correct ETH balance
-        await driver.switchToWindowWithTitle(
-          WINDOW_TITLES.ExtensionInFullScreenView,
-        );
-        await locateAccountBalanceDOM(driver, ganacheServer);
+        await scrollAndConfirmAndAssertConfirm(driver);
       },
     );
   });
