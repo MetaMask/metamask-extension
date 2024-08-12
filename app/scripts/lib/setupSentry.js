7667aa4936bf5e36b7a04d73bee06aaa6f16d18a
Jyoti Puri <jyotipuri@gmail.com>
2024-08-12 18:42:29 +0530

    update

--- app/scripts/lib/setupSentry.js
+++ app/scripts/lib/setupSentry.js
@@ -123,7 +123,6 @@ export const SENTRY_BACKGROUND_STATE = {
     timeoutMinutes: true,
     trezorModel: true,
     usedNetworks: true,
-    lastInteractedConfirmationInfo: undefined,
   },
   MultichainBalancesController: {
     balances: false,
