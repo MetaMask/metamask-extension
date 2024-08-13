6576598875f9fc1457afbef82a5baa7bbc1d0722
Owen Craston <owengc12@gmail.com>
2024-08-13 11:05:49 -0700

    new env variable

--- wdyr.js
+++ wdyr.js
@@ -1,7 +1,6 @@
-/// <reference types="@welldone-software/why-did-you-render" />
 import React from 'react';
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.WHY_DID_YOU_RENDER_ENABLED) {
   const whyDidYouRender = require('@welldone-software/why-did-you-render');
   whyDidYouRender(React, {
     trackAllPureComponents: true,
