ec26ac7c2edf3d54b682cbca9aef6446f844c0d1
Owen Craston <owengc12@gmail.com>
2024-08-14 12:46:08 -0700

    address comments

--- wdyr.ts
+++ wdyr.ts
@@ -1,6 +1,9 @@
+// eslint-disable-next-line spaced-comment
+/// <reference types="@welldone-software/why-did-you-render" />
 import React from 'react';
 
 if (process.env.WHY_DID_YOU_RENDER_ENABLED) {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
   const whyDidYouRender = require('@welldone-software/why-did-you-render');
   whyDidYouRender(React, {
     trackAllPureComponents: true,
