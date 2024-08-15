d5592f29dc2e76474bae7020c548c8260a7299b5
Jyoti Puri <jyotipuri@gmail.com>
2024-08-15 20:55:37 +0530

    fix: display toast message if user quickly sends transaction on different networks (#26114)

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -3385,6 +3385,14 @@ export default class MetamaskController extends EventEmitter {
         appStateController.setSwitchedNetworkNeverShowMessage.bind(
           appStateController,
         ),
+      getLastInteractedConfirmationInfo:
+        appStateController.getLastInteractedConfirmationInfo.bind(
+          appStateController,
+        ),
+      setLastInteractedConfirmationInfo:
+        appStateController.setLastInteractedConfirmationInfo.bind(
+          appStateController,
+        ),
 
       // EnsController
       tryReverseResolveAddress:
