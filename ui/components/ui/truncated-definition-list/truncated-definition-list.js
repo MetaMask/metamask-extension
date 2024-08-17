212af81a5318b45e6bf7260ab7e8d8270ecfe312
NidhiKJha <menidhikjha@gmail.com>
2024-08-17 17:19:14 +0530

    updated translation

--- ui/components/ui/truncated-definition-list/truncated-definition-list.js
+++ ui/components/ui/truncated-definition-list/truncated-definition-list.js
@@ -49,7 +49,7 @@ export default function TruncatedDefinitionList({
             style={{ width: '50%' }}
             onClick={() => setIsPopoverOpen(false)}
           >
-            Close
+            {t('close')}
           </Button>
         }
       >
