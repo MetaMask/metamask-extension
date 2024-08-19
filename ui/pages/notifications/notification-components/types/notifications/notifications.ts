e979f0ac6f5ce883a3e36ea8544168fec1641427
Matteo Scurati <matteo.scurati@gmail.com>
2024-08-19 14:12:05 +0200

    feat: :sparkles: add support for external links in feature announcements

--- ui/pages/notifications/notification-components/types/notifications/notifications.ts
+++ ui/pages/notifications/notification-components/types/notifications/notifications.ts
@@ -40,6 +40,7 @@ type FooterOnChainNotification<N = Notification> = {
 type FooterFeatureAnnouncement<N = Notification> = {
   type: 'footer_feature_announcement';
   ExtensionLink: NotificationFC<N>;
+  ExternalLink: NotificationFC<N>;
 };
 
 /**
