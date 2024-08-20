93c00776f7e57ffcdef63b9f5aeb51f48477855e
Danica Shen <zhaodanica@gmail.com>
2024-08-20 19:31:35 +0100

    fix(2946): reimplement the logic

--- ui/helpers/utils/settings-search.test.js
+++ ui/helpers/utils/settings-search.test.js
@@ -160,7 +160,7 @@ describe('Settings Search Utils', () => {
     });
 
     it('returns "Advanced" section count', () => {
-      expect(getNumberOfSettingRoutesInTab(t, t('advanced'))).toStrictEqual(12);
+      expect(getNumberOfSettingRoutesInTab(t, t('advanced'))).toStrictEqual(11);
     });
 
     it('returns "Contact" section count', () => {
