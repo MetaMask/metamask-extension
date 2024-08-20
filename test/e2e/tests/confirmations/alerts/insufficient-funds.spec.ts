483300042728550ae4226183deb2cf7751e35549
Priya Narayanaswamy <priya.narayanaswamy@consensys.net>
2024-08-20 09:31:31 +0200

    fix:lint

--- test/e2e/tests/confirmations/alerts/insufficient-funds.spec.ts
+++ test/e2e/tests/confirmations/alerts/insufficient-funds.spec.ts
@@ -1,9 +1,18 @@
-const { strict: assert } = require('assert');
-import FixtureBuilder from "../../../fixture-builder";
-import { PRIVATE_KEY, convertETHToHexGwei, withFixtures, WINDOW_TITLES } from "../../../helpers";
-import { SMART_CONTRACTS } from "../../../seeder/smart-contracts";
-import { scrollAndConfirmAndAssertConfirm } from "../helpers";
-import { TestSuiteArguments, openDAppWithContract } from "../transactions/shared";
+import FixtureBuilder from '../../../fixture-builder';
+import {
+  PRIVATE_KEY,
+  convertETHToHexGwei,
+  withFixtures,
+  WINDOW_TITLES,
+} from '../../../helpers';
+import { SMART_CONTRACTS } from '../../../seeder/smart-contracts';
+import { scrollAndConfirmAndAssertConfirm } from '../helpers';
+import {
+  TestSuiteArguments,
+  openDAppWithContract,
+} from '../transactions/shared';
+
+import { strict as assert } from 'assert';
 
 describe.only('Alert for insufficient funds', function () {
   it.only('Shows an alert when the user tries to send a transaction with insufficient funds', async function () {
@@ -33,20 +42,14 @@ describe.only('Alert for insufficient funds', function () {
         title: this.test?.fullTitle(),
       },
       async ({ driver, contractRegistry }: TestSuiteArguments) => {
-        await openDAppWithContract(
-          driver,
-          contractRegistry,
-          nftSmartContract,
-        );
+        await openDAppWithContract(driver, contractRegistry, nftSmartContract);
         await driver.switchToWindowWithTitle(WINDOW_TITLES.TestDApp);
         await driver.clickElement(`#mintButton`);
 
         await driver.waitUntilXWindowHandles(3);
         await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
 
-        const alert = await driver.findElement(
-          '[data-testid="inline-alert"]',
-        );
+        const alert = await driver.findElement('[data-testid="inline-alert"]');
         assert.equal(await alert.getText(), 'Alert');
         await driver.clickElementSafe('.confirm-scroll-to-bottom__button');
         await driver.clickElement('[data-testid="inline-alert"]');
