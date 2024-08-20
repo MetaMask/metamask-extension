74b62a196c1569c3e572927bce2d2413fbe1beb1
Kate Johnson <johnson.kat.g@gmail.com>
2024-08-19 20:02:23 -0400

    feat: finished adding e2e test cases for account-watcher

--- test/e2e/accounts/create-watch-account.spec.ts
+++ test/e2e/accounts/create-watch-account.spec.ts
@@ -1,20 +1,25 @@
 import { Suite } from 'mocha';
 
 import FixtureBuilder from '../fixture-builder';
-import { multipleGanacheOptions, unlockWallet, withFixtures } from '../helpers';
+import { defaultGanacheOptions, unlockWallet, withFixtures } from '../helpers';
 import { Driver } from '../webdriver/driver';
 
-const eoaAddress = '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045';
-const ensName = 'vitalik.eth';
+const ACCOUNT_1 = '0x5cfe73b6021e818b776b421b1c4db2474086a7e1';
+const EOA_ADDRESS = '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045';
+const SHORTENED_EOA_ADDRESS = '0xd8dA6...96045';
+const DEFAULT_WATCHED_ACCOUNT_NAME = 'Watched Account 1';
 
 /**
- * Starts the flow to create a watch account.
+ * Start the flow to create a watch account by clicking the account menu and selecting the option to add a watch account.
  *
  * @param driver - The WebDriver instance used to control the browser.
- * @returns A promise that resolves when the setup steps are complete.
+ * @param unlockWalletFirst - Whether to unlock the wallet before starting the flow.
  */
-async function startCreateWatchAccountFlow(driver: Driver): Promise<void> {
-  await unlockWallet(driver);
+async function startCreateWatchAccountFlow(
+  driver: Driver,
+  unlockWalletFirst: boolean = true,
+): Promise<void> {
+  if (unlockWalletFirst) await unlockWallet(driver);
 
   await driver.clickElement('[data-testid="account-menu-icon"]');
   await driver.clickElement(
@@ -25,8 +30,42 @@ async function startCreateWatchAccountFlow(driver: Driver): Promise<void> {
   );
 }
 
+/**
+ * Watches an EOA address.
+ *
+ * @param driver - The WebDriver instance used to control the browser.
+ * @param unlockWallet - Whether to unlock the wallet before watching the address.
+ * @param address - The EOA address to watch.
+ */
+async function watchEoaAddress(
+  driver: Driver,
+  unlockWallet: boolean = true,
+  address: string = EOA_ADDRESS,
+): Promise<void> {
+  await startCreateWatchAccountFlow(driver, unlockWallet);
+  await driver.fill(
+    '[placeholder="Enter a public address or ENS name"]',
+    address,
+  );
+  await driver.clickElement({ text: 'Watch account', tag: 'button' });
+  await driver.clickElement('[data-testid="submit-add-account-with-name"]');
+}
+
+/**
+ * Removes the selected account.
+ *
+ * @param driver - The WebDriver instance used to control the browser.
+ */
+async function removeSelectedAccount(driver: Driver): Promise<void> {
+  await driver.clickElement('[data-testid="account-menu-icon"]');
+  await driver.clickElement(
+    '.multichain-account-list-item--selected [data-testid="account-list-item-menu-button"]',
+  );
+  await driver.clickElement('[data-testid="account-list-menu-remove"]');
+  await driver.clickElement({ text: 'Remove', tag: 'button' });
+}
+
 describe('Account-watcher snap', function (this: Suite) {
-  // ** Fails because cannot resolve address to expected ENS name **
   it('user can add watch account with valid EOA address', async function () {
     await withFixtures(
       {
@@ -36,34 +75,27 @@ describe('Account-watcher snap', function (this: Suite) {
           })
           .withNetworkControllerOnMainnet()
           .build(),
-        ganacheOptions: multipleGanacheOptions,
+        ganacheOptions: defaultGanacheOptions,
         title: this.test?.fullTitle(),
       },
       async ({ driver }: { driver: Driver }) => {
-        // start the add watch account flow and switch to dialog window
-        await startCreateWatchAccountFlow(driver);
+        // watch EOA address
+        await watchEoaAddress(driver);
 
-        // fill in the address and click the watch account button
-        await driver.fill(
-          '[placeholder="Enter a public address or ENS name"]',
-          eoaAddress,
-        );
-        await driver.clickElement({ text: 'Watch account', tag: 'button' });
-        // click the add account button on the naming screen
-        await driver.clickElement(
-          '[data-testid="submit-add-account-with-name"]',
-        );
-        // check wallet now has the watched account with address resolved
+        // new account should be displayed in the account list
         await driver.findElement({
           css: '[data-testid="account-menu-icon"]',
-          text: ensName, // shows default unresolved address name "Watched Account 1"
+          text: DEFAULT_WATCHED_ACCOUNT_NAME,
+        });
+        await driver.findElement({
+          css: '.mm-text--ellipsis',
+          text: SHORTENED_EOA_ADDRESS,
         });
       },
     );
   });
 
-  // ** Fails because cannot resolve ENS name to the expected address **
-  it('user can add watch account with valid ENS name', async function () {
+  it("'Send' 'Swap' and 'Bridge' buttons are disabled for watch accounts", async function () {
     await withFixtures(
       {
         fixtures: new FixtureBuilder()
@@ -72,27 +104,242 @@ describe('Account-watcher snap', function (this: Suite) {
           })
           .withNetworkControllerOnMainnet()
           .build(),
-        ganacheOptions: multipleGanacheOptions,
+        ganacheOptions: defaultGanacheOptions,
         title: this.test?.fullTitle(),
       },
       async ({ driver }: { driver: Driver }) => {
-        // start the add watch account flow and switch to dialog window
-        await startCreateWatchAccountFlow(driver);
+        // watch EOA address
+        await watchEoaAddress(driver);
 
-        // fill in the ENS and click the watch account button
-        await driver.fill(
-          '[placeholder="Enter a public address or ENS name"]',
-          ensName,
+        // 'Send' button should be disabled
+        await driver.findElement('[data-testid="eth-overview-send"][disabled]');
+        await driver.findElement(
+          '[data-testid="eth-overview-send"].icon-button--disabled',
         );
-        await driver.clickElement({ text: 'Watch account', tag: 'button' });
-        // click the add account button on the naming screen
+
+        // 'Swap' button should be disabled
+        await driver.findElement(
+          '[data-testid="token-overview-button-swap"][disabled]',
+        );
+        await driver.findElement(
+          '[data-testid="token-overview-button-swap"].icon-button--disabled',
+        );
+
+        // 'Bridge' button should be disabled
+        await driver.findElement(
+          '[data-testid="eth-overview-bridge"][disabled]',
+        );
+        await driver.findElement(
+          '[data-testid="eth-overview-bridge"].icon-button--disabled',
+        );
+
+        // Check tooltips for disabled buttons
+        await driver.findElement(
+          '.icon-button--disabled [data-tooltipped][data-original-title="Not supported with this account."]',
+        );
+      },
+    );
+  });
+
+  const invalidInputTests = [
+    {
+      input: 'd8dA6BF26964aF9D7eEd9e03E53415D37aA96045',
+      message: 'Invalid input',
+      description: 'missing 0x prefix',
+    },
+    {
+      input: '0x123ABC',
+      message: 'Invalid address',
+      description: 'invalid address',
+    },
+    {
+      input: 'invalid.eth',
+      message: 'Invalid ENS name',
+      description: 'invalid ENS name',
+    },
+    {
+      input: ACCOUNT_1,
+      message:
+        "Unknown snap error: Account address '0x5CfE73b6021E818B776b421B1c4Db2474086a7e1' already exists",
+      description: 'existing address',
+    },
+  ];
+
+  invalidInputTests.forEach(({ input, message, description }) => {
+    it(`handles invalid input: ${description}`, async function () {
+      await withFixtures(
+        {
+          fixtures: new FixtureBuilder()
+            .withPreferencesControllerAndFeatureFlag({
+              watchEthereumAccountEnabled: true,
+            })
+            .withNetworkControllerOnMainnet()
+            .build(),
+          ganacheOptions: defaultGanacheOptions,
+          title: this.test?.fullTitle(),
+        },
+        async ({ driver }: { driver: Driver }) => {
+          await startCreateWatchAccountFlow(driver);
+
+          await driver.fill(
+            '[placeholder="Enter a public address or ENS name"]',
+            input,
+          );
+          await driver.clickElement({ text: 'Watch account', tag: 'button' });
+
+          // error message should be displayed by the snap
+          await driver.findElement({
+            css: '.snap-ui-renderer__text',
+            text: message,
+          });
+        },
+      );
+    });
+  });
+
+  it('user cannot input private key of watched address', async function () {
+    const PRIVATE_KEY_TWO =
+      '0xf444f52ea41e3a39586d7069cb8e8233e9f6b9dea9cbb700cce69ae860661cc8';
+    const ACCOUNT_2 = '0x09781764c08de8ca82e156bbf156a3ca217c7950';
+
+    await withFixtures(
+      {
+        fixtures: new FixtureBuilder()
+          .withPreferencesControllerAndFeatureFlag({
+            watchEthereumAccountEnabled: true,
+          })
+          .withNetworkControllerOnMainnet()
+          .build(),
+        ganacheOptions: defaultGanacheOptions,
+        title: this.test?.fullTitle(),
+      },
+      async ({ driver }: { driver: Driver }) => {
+        // watch EOA address
+        await watchEoaAddress(driver, true, ACCOUNT_2);
+
+        // import private key of account 2
+        await driver.clickElement('[data-testid="account-menu-icon"]');
         await driver.clickElement(
-          '[data-testid="submit-add-account-with-name"]',
+          '[data-testid="multichain-account-menu-popover-action-button"]',
         );
-        // switch back to the main window
+        await driver.clickElement({ text: 'Import account', tag: 'button' });
+        await driver.findClickableElement('#private-key-box');
+        await driver.fill('#private-key-box', PRIVATE_KEY_TWO);
+        await driver.clickElement(
+          '[data-testid="import-account-confirm-button"]',
+        );
+
+        // error message should be displayed
+        await driver.findElement({
+          css: '.mm-box--color-error-default',
+          text: 'KeyringController - The account you are trying to import is a duplicate',
+        });
+      },
+    );
+  });
+
+  it('Show private key button is not displayed for watch accounts', async function () {
+    await withFixtures(
+      {
+        fixtures: new FixtureBuilder()
+          .withPreferencesControllerAndFeatureFlag({
+            watchEthereumAccountEnabled: true,
+          })
+          .withNetworkControllerOnMainnet()
+          .build(),
+        ganacheOptions: defaultGanacheOptions,
+        title: this.test?.fullTitle(),
+      },
+      async ({ driver }: { driver: Driver }) => {
+        // watch EOA address
+        await watchEoaAddress(driver);
+
+        // click to view account details
+        await driver.clickElement(
+          '[data-testid="account-options-menu-button"]',
+        );
+        await driver.clickElement('[data-testid="account-list-menu-details"]');
+        // show private key button should not be displayed
+        await driver.assertElementNotPresent({
+          css: 'button',
+          text: 'Show private key',
+        });
+      },
+    );
+  });
+
+  it('removes a watch account', async function () {
+    await withFixtures(
+      {
+        fixtures: new FixtureBuilder()
+          .withPreferencesControllerAndFeatureFlag({
+            watchEthereumAccountEnabled: true,
+          })
+          .withNetworkControllerOnMainnet()
+          .build(),
+        ganacheOptions: defaultGanacheOptions,
+        title: this.test?.fullTitle(),
+      },
+      async ({ driver }: { driver: Driver }) => {
+        // watch EOA address
+        await watchEoaAddress(driver);
+
+        // remove the selected watch account
+        await removeSelectedAccount(driver);
+
+        // account should be removed from the account list
+        await driver.assertElementNotPresent({
+          css: '[data-testid="account-menu-icon"]',
+          text: DEFAULT_WATCHED_ACCOUNT_NAME,
+        });
+        await driver.assertElementNotPresent({
+          css: '.mm-text--ellipsis',
+          text: SHORTENED_EOA_ADDRESS,
+        });
+      },
+    );
+  });
+
+  it('removes and re-adds a watch account', async function () {
+    await withFixtures(
+      {
+        fixtures: new FixtureBuilder()
+          .withPreferencesControllerAndFeatureFlag({
+            watchEthereumAccountEnabled: true,
+          })
+          .withNetworkControllerOnMainnet()
+          .build(),
+        ganacheOptions: defaultGanacheOptions,
+        title: this.test?.fullTitle(),
+      },
+      async ({ driver }: { driver: Driver }) => {
+        // watch EOA address
+        await watchEoaAddress(driver);
+
+        // remove the selected watch account
+        await removeSelectedAccount(driver);
+
+        // account should be removed from the account list
+        await driver.assertElementNotPresent({
+          css: '[data-testid="account-menu-icon"]',
+          text: DEFAULT_WATCHED_ACCOUNT_NAME,
+        });
+        await driver.assertElementNotPresent({
+          css: '.mm-text--ellipsis',
+          text: SHORTENED_EOA_ADDRESS,
+        });
+
+        // re-add the watch account
+        await watchEoaAddress(driver, false);
+
+        // new account should be displayed in the account list
         await driver.findElement({
           css: '[data-testid="account-menu-icon"]',
-          text: ensName,
+          text: DEFAULT_WATCHED_ACCOUNT_NAME,
+        });
+        await driver.findElement({
+          css: '.mm-text--ellipsis',
+          text: SHORTENED_EOA_ADDRESS,
         });
       },
     );
