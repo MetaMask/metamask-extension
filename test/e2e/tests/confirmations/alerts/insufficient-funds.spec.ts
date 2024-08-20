f9fa4fecf1d2c3342d58dfd6567dc53bf565f1fe
Priya Narayanaswamy <priya.narayanaswamy@consensys.net>
2024-08-20 14:06:21 +0200

    fix: address review comments

--- test/e2e/tests/confirmations/alerts/insufficient-funds.spec.ts
+++ test/e2e/tests/confirmations/alerts/insufficient-funds.spec.ts
@@ -12,15 +12,16 @@ import {
   TestSuiteArguments,
   openDAppWithContract,
 } from '../transactions/shared';
+import { Driver } from '../../../webdriver/driver';
 
-describe.only('Alert for insufficient funds', function () {
-  it.only('Shows an alert when the user tries to send a transaction with insufficient funds', async function () {
+describe('Alert for insufficient funds', function () {
+  it('Shows an alert when the user tries to send a transaction with insufficient funds', async function () {
     const nftSmartContract = SMART_CONTRACTS.NFTS;
     const ganacheOptions = {
       accounts: [
         {
           secretKey: PRIVATE_KEY,
-          balance: convertETHToHexGwei(0.0053),
+          balance: convertETHToHexGwei(0.0053), //Low balance only to create the contract and then trigger the alert for insufficient funds
         },
       ],
     };
@@ -42,34 +43,48 @@ describe.only('Alert for insufficient funds', function () {
       },
       async ({ driver, contractRegistry }: TestSuiteArguments) => {
         await openDAppWithContract(driver, contractRegistry, nftSmartContract);
-        await driver.switchToWindowWithTitle(WINDOW_TITLES.TestDApp);
-        await driver.clickElement(`#mintButton`);
+        
+        await mintNft(driver);
 
-        await driver.waitUntilXWindowHandles(3);
-        await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
-
-        const alert = await driver.findElement('[data-testid="inline-alert"]');
-        assert.equal(await alert.getText(), 'Alert');
-        await driver.clickElementSafe('.confirm-scroll-to-bottom__button');
-        await driver.clickElement('[data-testid="inline-alert"]');
-
-        const alertDescription = await driver.findElement(
-          '[data-testid="alert-modal__selected-alert"]',
-        );
-        const alertDescriptionText = await alertDescription.getText();
-        assert.equal(
-          alertDescriptionText,
-          'You do not have enough ETH in your account to pay for transaction fees.',
-        );
-        await driver.clickElement('[data-testid="alert-modal-close-button"]');
+        await verifyAlertForInsufficientBalance(driver);
 
         await scrollAndConfirmAndAssertConfirm(driver);
 
-        const confirmButton = await driver.findElement(
-          '[data-testid="confirm-alert-modal-submit-button"]',
-        );
-        assert.equal(await confirmButton.isEnabled(), false);
+        await verifyConfirmationIsDisabled(driver);
       },
     );
   });
 });
+
+async function verifyConfirmationIsDisabled(driver: Driver) {
+  const confirmButton = await driver.findElement(
+    '[data-testid="confirm-alert-modal-submit-button"]'
+  );
+  assert.equal(await confirmButton.isEnabled(), false);
+}
+
+async function verifyAlertForInsufficientBalance(driver: Driver) {
+  const alert = await driver.findElement('[data-testid="inline-alert"]');
+  assert.equal(await alert.getText(), 'Alert');
+  await driver.clickElementSafe('.confirm-scroll-to-bottom__button');
+  await driver.clickElement('[data-testid="inline-alert"]');
+
+  const alertDescription = await driver.findElement(
+    '[data-testid="alert-modal__selected-alert"]'
+  );
+  const alertDescriptionText = await alertDescription.getText();
+  assert.equal(
+    alertDescriptionText,
+    'You do not have enough ETH in your account to pay for transaction fees.'
+  );
+  await driver.clickElement('[data-testid="alert-modal-close-button"]');
+}
+
+async function mintNft(driver: Driver) {
+  await driver.switchToWindowWithTitle(WINDOW_TITLES.TestDApp);
+  await driver.clickElement(`#mintButton`);
+
+  await driver.waitUntilXWindowHandles(3);
+  await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
+}
+
