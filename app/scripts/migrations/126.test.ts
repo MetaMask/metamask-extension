0e1cb3ea86e8eee7e7e2b0bd85a0b54866305808
Monte Lai <monte.lai@consensys.net>
2024-08-21 20:55:44 +0800

    fix: bump accounts controller and add migration to fix undefined selectedAccount

---
+++ app/scripts/migrations/126.test.ts
@@ -0,0 +1,69 @@
+import { migrate, version } from './126';
+import { createMockInternalAccount } from '../../../test/jest/mocks';
+import { AccountsControllerState } from '@metamask/accounts-controller';
+
+const oldVersion = 125;
+
+const mockInternalAccount = createMockInternalAccount();
+const mockAccountsControllerState: AccountsControllerState = {
+  internalAccounts: {
+    accounts: {
+      [mockInternalAccount.id]: mockInternalAccount,
+    },
+    selectedAccount: mockInternalAccount.id,
+  },
+};
+
+describe('migration #126', () => {
+  afterEach(() => jest.resetAllMocks());
+
+  it('updates the version metadata', async () => {
+    const oldStorage = {
+      meta: { version: oldVersion },
+      data: {
+        AccountsController: mockAccountsControllerState,
+      },
+    };
+
+    const newStorage = await migrate(oldStorage);
+    expect(newStorage.meta).toStrictEqual({ version });
+  });
+
+  it('updates selected account if it is not found in the list of accounts', async () => {
+    const oldStorage = {
+      meta: { version: oldVersion },
+      data: {
+        AccountsController: {
+          ...mockAccountsControllerState,
+          internalAccounts: {
+            ...mockAccountsControllerState.internalAccounts,
+            selectedAccount: 'unknown id',
+          },
+        },
+      },
+    };
+
+    const newStorage = await migrate(oldStorage);
+    const selectedAccount = (
+      newStorage.data.AccountsController as AccountsControllerState
+    ).internalAccounts.selectedAccount;
+    expect(selectedAccount).toStrictEqual(mockInternalAccount.id);
+    expect(newStorage.data.AccountsController).toStrictEqual(
+      mockAccountsControllerState,
+    );
+  });
+
+  it('does nothing if the selectedAccount is found in the list of accounts', async () => {
+    const oldStorage = {
+      meta: { version: oldVersion },
+      data: {
+        AccountsController: mockAccountsControllerState,
+      },
+    };
+
+    const newStorage = await migrate(oldStorage);
+    expect(newStorage.data.AccountsController).toStrictEqual(
+      mockAccountsControllerState,
+    );
+  });
+});
