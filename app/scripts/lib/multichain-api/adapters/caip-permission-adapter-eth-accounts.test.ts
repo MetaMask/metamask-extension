132f2656a80273b4a9682e5aad34458b0afd7eae
Jiexi Luan <jiexiluan@gmail.com>
2024-08-21 14:06:19 -0700

    Add caip-permision-adapter-eth-accounts.test.ts

---
+++ app/scripts/lib/multichain-api/adapters/caip-permission-adapter-eth-accounts.test.ts
@@ -0,0 +1,164 @@
+import { Caip25CaveatValue } from '../caip25permissions';
+import {
+  getEthAccounts,
+  setEthAccounts,
+} from './caip-permission-adapter-eth-accounts';
+
+describe('CAIP-25 eth_accounts adapters', () => {
+  describe('getEthAccounts', () => {
+    it('returns the unique set of EIP-155 accounts from the CAIP-25 caveat value', () => {
+      const ethAccounts = getEthAccounts({
+        requiredScopes: {
+          'eip155:1': {
+            methods: [],
+            notifications: [],
+            accounts: ['eip155:1:0x1', 'eip155:1:0x2'],
+          },
+          'eip155:5': {
+            methods: [],
+            notifications: [],
+            accounts: ['eip155:5:0x2', 'eip155:1:0x3'],
+          },
+          'bip122:000000000019d6689c085ae165831e93': {
+            methods: [],
+            notifications: [],
+            accounts: [
+              'bip122:000000000019d6689c085ae165831e93:128Lkh3S7CkDTBZ8W7BbpsN3YYizJMp8p6',
+            ],
+          },
+        },
+        optionalScopes: {
+          'eip155:1': {
+            methods: [],
+            notifications: [],
+            accounts: ['eip155:1:0x1', 'eip155:1:0x4'],
+          },
+          'eip155:a': {
+            methods: [],
+            notifications: [],
+          },
+          'eip155:100': {
+            methods: [],
+            notifications: [],
+            accounts: ['eip155:100:0x100'],
+          },
+        },
+        isMultichainOrigin: false,
+      });
+
+      expect(ethAccounts).toStrictEqual(['0x1', '0x2', '0x4', '0x3', '0x100']);
+    });
+  });
+
+  describe('setEthAccounts', () => {
+    it('returns a CAIP-25 caveat value with all EIP-155 scopeObject.accounts set to CAIP-10 account addresses formed from the accounts param', () => {
+      const input: Caip25CaveatValue = {
+        requiredScopes: {
+          'eip155:1': {
+            methods: [],
+            notifications: [],
+            accounts: ['eip155:1:0x1', 'eip155:1:0x2'],
+          },
+          'eip155:5': {
+            methods: [],
+            notifications: [],
+            accounts: ['eip155:5:0x2', 'eip155:1:0x3'],
+          },
+          'bip122:000000000019d6689c085ae165831e93': {
+            methods: [],
+            notifications: [],
+            accounts: [
+              'bip122:000000000019d6689c085ae165831e93:128Lkh3S7CkDTBZ8W7BbpsN3YYizJMp8p6',
+            ],
+          },
+        },
+        optionalScopes: {
+          'eip155:1': {
+            methods: [],
+            notifications: [],
+            accounts: ['eip155:1:0x1', 'eip155:1:0x4'],
+          },
+          'eip155:a': {
+            methods: [],
+            notifications: [],
+          },
+          'eip155:100': {
+            methods: [],
+            notifications: [],
+            accounts: ['eip155:100:0x100'],
+          },
+        },
+        isMultichainOrigin: false,
+      };
+
+      const result = setEthAccounts(input, ['0x1', '0x2', '0x3']);
+      expect(result).toStrictEqual({
+        requiredScopes: {
+          'eip155:1': {
+            methods: [],
+            notifications: [],
+            accounts: ['eip155:1:0x1', 'eip155:1:0x2', 'eip155:1:0x3'],
+          },
+          'eip155:5': {
+            methods: [],
+            notifications: [],
+            accounts: ['eip155:5:0x1', 'eip155:5:0x2', 'eip155:5:0x3'],
+          },
+          'bip122:000000000019d6689c085ae165831e93': {
+            methods: [],
+            notifications: [],
+            accounts: [
+              'bip122:000000000019d6689c085ae165831e93:128Lkh3S7CkDTBZ8W7BbpsN3YYizJMp8p6',
+            ],
+          },
+        },
+        optionalScopes: {
+          'eip155:1': {
+            methods: [],
+            notifications: [],
+            accounts: ['eip155:1:0x1', 'eip155:1:0x2', 'eip155:1:0x3'],
+          },
+          'eip155:a': {
+            methods: [],
+            notifications: [],
+            accounts: ['eip155:a:0x1', 'eip155:a:0x2', 'eip155:a:0x3'],
+          },
+          'eip155:100': {
+            methods: [],
+            notifications: [],
+            accounts: ['eip155:100:0x1', 'eip155:100:0x2', 'eip155:100:0x3'],
+          },
+        },
+        isMultichainOrigin: false,
+      });
+    });
+
+    it('does not modify the input CAIP-25 caveat value object', () => {
+      const input: Caip25CaveatValue = {
+        requiredScopes: {
+          'eip155:1': {
+            methods: [],
+            notifications: [],
+            accounts: [],
+          },
+        },
+        optionalScopes: {},
+        isMultichainOrigin: false,
+      };
+
+      const result = setEthAccounts(input, ['0x1', '0x2', '0x3']);
+      expect(input).toStrictEqual({
+        requiredScopes: {
+          'eip155:1': {
+            methods: [],
+            notifications: [],
+            accounts: [],
+          },
+        },
+        optionalScopes: {},
+        isMultichainOrigin: false,
+      });
+      expect(input).not.toStrictEqual(result);
+    });
+  });
+});
