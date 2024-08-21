d60e7272b8c2c9574edaa35f2e0b67ae1bd89290
Jiexi Luan <jiexiluan@gmail.com>
2024-08-21 13:36:25 -0700

    fix caip-permission-adapter-middleware.test.js

--- app/scripts/lib/multichain-api/adapters/caip-permission-adapter-middleware.test.js
+++ app/scripts/lib/multichain-api/adapters/caip-permission-adapter-middleware.test.js
@@ -74,83 +74,59 @@ const createMockedHandler = () => {
 };
 
 describe('CaipPermissionAdapterMiddleware', () => {
-  describe('BARAD_DUR feature flag is not set', () => {
-    beforeAll(() => {
-      delete process.env.BARAD_DUR;
-    });
-
-    it('allows the request when BARAD_DUR feature flag is not set', async () => {
-      const { handler, next } = createMockedHandler();
-      await handler(baseRequest);
-      expect(next).toHaveBeenCalled();
-    });
-
-    it('does not read the permission state', async () => {
-      const { handler, getCaveat } = createMockedHandler();
-      await handler(baseRequest);
-      expect(getCaveat).not.toHaveBeenCalled();
-    });
+  it('gets the authorized scopes from the CAIP-25 endowment permission', async () => {
+    const { handler, getCaveat } = createMockedHandler();
+    await handler(baseRequest);
+    expect(getCaveat).toHaveBeenCalledWith(
+      'http://test.com',
+      Caip25EndowmentPermissionName,
+      Caip25CaveatType,
+    );
   });
 
-  describe('BARAD_DUR feature flag is set', () => {
-    beforeAll(() => {
-      process.env.BARAD_DUR = 1;
+  it('allows the request when there is no CAIP-25 endowment permission', async () => {
+    const { handler, getCaveat, next } = createMockedHandler();
+    getCaveat.mockReturnValue(null);
+    await handler(baseRequest);
+    expect(next).toHaveBeenCalled();
+  });
+
+  it('allows the request when the CAIP-25 endowment permission was not granted from the multichain API', async () => {
+    const { handler, getCaveat, next } = createMockedHandler();
+    getCaveat.mockReturnValue({
+      value: {
+        isMultichainOrigin: false,
+      },
+    });
+    await handler(baseRequest);
+    expect(next).toHaveBeenCalled();
+  });
+
+  it('gets the chainId for the request networkClientId', async () => {
+    const { handler, getNetworkConfigurationByNetworkClientId } =
+      createMockedHandler();
+    await handler(baseRequest);
+    expect(getNetworkConfigurationByNetworkClientId).toHaveBeenCalledWith(
+      'mainnet',
+    );
+  });
+
+  describe('when the CAIP-25 endowment permission was granted over the multichain API', () => {
+    it('throws an error if the requested method is not authorized for the scope specified in the request', async () => {
+      const { handler, end } = createMockedHandler();
+
+      await handler({
+        ...baseRequest,
+        method: 'unauthorized_method',
+      });
+      expect(end).toHaveBeenCalledWith(providerErrors.unauthorized());
     });
 
-    it('gets the authorized scopes from the CAIP-25 endowment permission', async () => {
-      const { handler, getCaveat } = createMockedHandler();
-      await handler(baseRequest);
-      expect(getCaveat).toHaveBeenCalledWith(
-        'http://test.com',
-        Caip25EndowmentPermissionName,
-        Caip25CaveatType,
-      );
-    });
+    it('allows the request if the requested scope method is authorized in the current scope', async () => {
+      const { handler, next } = createMockedHandler();
 
-    it('allows the request when there is no CAIP-25 endowment permission', async () => {
-      const { handler, getCaveat, next } = createMockedHandler();
-      getCaveat.mockReturnValue(null);
       await handler(baseRequest);
       expect(next).toHaveBeenCalled();
-    });
-
-    it('allows the request when the CAIP-25 endowment permission was not granted from the multichain API', async () => {
-      const { handler, getCaveat, next } = createMockedHandler();
-      getCaveat.mockReturnValue({
-        value: {
-          isMultichainOrigin: false,
-        },
-      });
-      await handler(baseRequest);
-      expect(next).toHaveBeenCalled();
-    });
-
-    it('gets the chainId for the request networkClientId', async () => {
-      const { handler, getNetworkConfigurationByNetworkClientId } =
-        createMockedHandler();
-      await handler(baseRequest);
-      expect(getNetworkConfigurationByNetworkClientId).toHaveBeenCalledWith(
-        'mainnet',
-      );
-    });
-
-    describe('when the CAIP-25 endowment permission was granted over the multichain API', () => {
-      it('throws an error if the requested method is not authorized for the scope specified in the request', async () => {
-        const { handler, end } = createMockedHandler();
-
-        await handler({
-          ...baseRequest,
-          method: 'unauthorized_method',
-        });
-        expect(end).toHaveBeenCalledWith(providerErrors.unauthorized());
-      });
-
-      it('allows the request if the requested scope method is authorized in the current scope', async () => {
-        const { handler, next } = createMockedHandler();
-
-        await handler(baseRequest);
-        expect(next).toHaveBeenCalled();
-      });
     });
   });
 });
