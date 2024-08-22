824bc381cbb585d3da1fa21cd4382bbeda1f8997
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2024-08-22 12:34:00 +0100

    wip

--- ui/pages/confirmations/components/confirm/info/typed-sign/permit-simulation/permit-simulation.tsx
+++ ui/pages/confirmations/components/confirm/info/typed-sign/permit-simulation/permit-simulation.tsx
@@ -1,6 +1,5 @@
 import { Hex } from '@metamask/utils';
 import React from 'react';
-import { Box } from '../../../../../../../components/component-library';
 import { PrimaryType } from '../../../../../../../../shared/constants/signatures';
 import { parseTypedDataMessage } from '../../../../../../../../shared/modules/transaction.utils';
 import { ConfirmInfoRow } from '../../../../../../../components/app/confirm/info/row';
