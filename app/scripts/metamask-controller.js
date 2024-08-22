d2d48d22957918d1bcd15cd0245cfafe0fa09d40
Mark Stacey <markjstacey@gmail.com>
2024-08-22 11:29:59 -0230

    refactor: Replace usages of the deprecated `setProviderType` (#22619)
    ## **Description**
    
    All usages of the deprecated `NetworkController` method
    `setProviderType` have been replaced. The method `setActiveNetwork` is
    now used instead; it was updated in a recent release to accept types as
    well as IDs.
    
    ## **Related issues**
    
    N/A
    
    ## **Manual testing steps**
    
    No functional changes
    
    ## **Screenshots/Recordings**
    
    N/A
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've clearly explained what problem this PR is solving and how it
    is solved.
    - [x] I've linked related issues
    - [x] I've included manual testing steps
    - [x] I've included screenshots/recordings if applicable
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    - [x] I’ve properly set the pull request status:
      - [x] In case it's not yet "ready for review", I've set it to "draft".
    - [x] In case it's "ready for review", I've changed it from "draft" to
    "non-draft".
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -3254,9 +3254,6 @@ export default class MetamaskController extends EventEmitter {
       verifyPassword: this.verifyPassword.bind(this),
 
       // network management
-      setProviderType: (type) => {
-        return this.networkController.setProviderType(type);
-      },
       setActiveNetwork: (networkConfigurationId) => {
         return this.networkController.setActiveNetwork(networkConfigurationId);
       },
