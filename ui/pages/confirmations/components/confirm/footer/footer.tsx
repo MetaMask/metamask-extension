f005ed73d64f353a39cc9b06500ed69ebb6037c1
Antonio Regadas <antonio.regadas@consensys.net>
2024-08-26 09:42:42 +0100

    chore: update logic

--- ui/pages/confirmations/components/confirm/footer/footer.tsx
+++ ui/pages/confirmations/components/confirm/footer/footer.tsx
@@ -1,6 +1,12 @@
 import { TransactionMeta } from '@metamask/transaction-controller';
 import { ethErrors, serializeError } from 'eth-rpc-errors';
 import React, { useCallback, useState } from 'react';
+
+///: BEGIN:ONLY_INCLUDE_IF(build-mmi)
+import { useHistory } from 'react-router-dom';
+import { showCustodianDeepLink } from '@metamask-institutional/extension';
+///: END:ONLY_INCLUDE_IF
+
 import { useDispatch, useSelector } from 'react-redux';
 import { ConfirmAlertModal } from '../../../../../components/app/alert-system/confirm-alert-modal';
 import {
@@ -17,6 +23,7 @@ import {
 } from '../../../../../selectors';
 ///: BEGIN:ONLY_INCLUDE_IF(build-mmi)
 import { useMMIConfirmations } from '../../../../../hooks/useMMIConfirmations';
+import { clearConfirmTransaction } from '../../../../../ducks/confirm-transaction/confirm-transaction.duck';
 ///: END:ONLY_INCLUDE_IF
 import useAlerts from '../../../../../hooks/useAlerts';
 import {
@@ -27,6 +34,18 @@ import {
 import { confirmSelector } from '../../../selectors';
 import { REDESIGN_TRANSACTION_TYPES } from '../../../utils';
 import { getConfirmationSender } from '../utils';
+
+///: BEGIN:ONLY_INCLUDE_IF(build-mmi)
+import { getMostRecentOverviewPage } from '../../../../../ducks/history/history';
+
+import { getAccountType } from '../../../../../selectors/selectors';
+import { mmiActionsFactory } from '../../../../../store/institutional/institution-background';
+import { showCustodyConfirmLink } from '../../../../../store/institutional/institution-actions';
+import {
+  AccountType,
+  CustodyStatus,
+} from '../../../../../../shared/constants/custody';
+///: END:ONLY_INCLUDE_IF
 
 const ConfirmButton = ({
   alertOwnerId = '',
@@ -102,40 +121,51 @@ const Footer = () => {
   const { from } = getConfirmationSender(currentConfirmation);
 
   ///: BEGIN:ONLY_INCLUDE_IF(build-mmi)
+  const mostRecentOverviewPage = useSelector(getMostRecentOverviewPage);
   const { mmiOnSignCallback, mmiSubmitDisabled } = useMMIConfirmations();
-  ///: END:ONLY_INCLUDE_IF
+  const history = useHistory();
 
+  const accountType = useSelector((state) =>
+    getAccountType(state),
+  );
+  const mmiActions = mmiActionsFactory();
 
-  ///: BEGIN:ONLY_INCLUDE_IF(build-mmi)
   const mmiApprovalFlow = () => {
-    if (accountType === AccountType.CUSTODY) {
-      fullTxData.custodyStatus = CustodyStatus.CREATED;
-      fullTxData.metadata = fullTxData.metadata || {};
+    if (currentConfirmation && accountType === AccountType.CUSTODY) {
+      // @ts-expect-error
+      currentConfirmation.custodyStatus = CustodyStatus.CREATED;
+      // @ts-expect-error
+      currentConfirmation.metadata = currentConfirmation.metadata || {};
 
       dispatch(mmiActions.setWaitForConfirmDeepLinkDialog(true));
 
-      const txId = fullTxData.id;
-      const fromAddress = fullTxData.txParams.from;
+      const txId = currentConfirmation.id;
+      // @ts-expect-error
+      const fromAddress = currentConfirmation.txParams.from;
       const closeNotification = false;
-      dispatch(updateAndApproveTx(customNonceMerge(fullTxData))).then(() => {
+      // @ts-expect-error
+      dispatch(updateAndApproveTx(currentConfirmation, true, '')).then(() => {
         showCustodianDeepLink({
           dispatch,
           mmiActions,
           txId,
           fromAddress,
           closeNotification,
+          isSignature: false,
+          custodyId: '',
           onDeepLinkFetched: () => undefined,
           onDeepLinkShown: () => {
             dispatch(clearConfirmTransaction());
-            history.push(mostRecentOverviewPage);
+            // history.push(mostRecentOverviewPage);
           },
           showCustodyConfirmLink,
         });
-        history.push(mostRecentOverviewPage);
+        // history.push(mostRecentOverviewPage);
       });
     } else {
       // Non Custody accounts follow normal flow
-      dispatch(updateAndApproveTx(customNonceMerge(fullTxData))).then(() => {
+      // @ts-expect-error
+      dispatch(updateAndApproveTx(currentConfirmation, true, '')).then(() => {
         dispatch(clearConfirmTransaction());
         history.push(mostRecentOverviewPage);
       });
