aa585ab494e4304530168decadef858869000b9d
salimtb <salim.toubal@outlook.com>
2024-08-26 22:07:08 +0200

    fix: execute migrations

--- app/scripts/migrations/index.js
+++ app/scripts/migrations/index.js
@@ -143,8 +143,8 @@ const migrations = [
   require('./124'),
   require('./125'),
   require('./126'),
-  require('./127'),
   require('./128'),
+  require('./129'),
 ];
 
 export default migrations;
