e742627209b347def1f3c064051fe0cecb432a45
augmentedmode <jacob.lebowitz@consensys.net>
2024-08-26 14:51:52 -0400

    chore: add @ts-expect-error for phishing controller test

--- app/scripts/lib/snap-keyring/utils/isBlockedUrl.test.ts
+++ app/scripts/lib/snap-keyring/utils/isBlockedUrl.test.ts
@@ -10,6 +10,7 @@ describe('isBlockedUrl', () => {
     allowedEvents: [],
   });
   const phishingController = new PhishingController({
+    // @ts-expect-error TODO: Resolve/patch mismatch between messenger types
     messenger: phishingControllerMessenger,
     state: {
       phishingLists: [
