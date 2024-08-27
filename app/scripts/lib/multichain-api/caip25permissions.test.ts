5ed5509f3e8815d358cbf94a71f7e8dc0ecddb32
Jiexi Luan <jiexiluan@gmail.com>
2024-08-27 14:52:10 -0700

    add caip25 permission validator spec

--- app/scripts/lib/multichain-api/caip25permissions.test.ts
+++ app/scripts/lib/multichain-api/caip25permissions.test.ts
@@ -1,9 +1,11 @@
 import {
+  CaveatConstraint,
   CaveatMutatorOperation,
   PermissionType,
   SubjectType,
 } from '@metamask/permission-controller';
-
+import { NonEmptyArray } from '@metamask/controller-utils';
+import * as Scope from './scope';
 import {
   Caip25CaveatType,
   Caip25CaveatValue,
@@ -13,9 +15,26 @@ import {
   removeScope,
 } from './caip25permissions';
 
+jest.mock('./scope', () => ({
+  validateAndFlattenScopes: jest.fn(),
+  assertScopesSupported: jest.fn(),
+}));
+const MockScope = jest.mocked(Scope);
+
 const { removeAccount } = Caip25CaveatMutatorFactories[Caip25CaveatType];
 
 describe('endowment:caip25', () => {
+  beforeEach(() => {
+    MockScope.validateAndFlattenScopes.mockReturnValue({
+      flattenedRequiredScopes: {},
+      flattenedOptionalScopes: {},
+    });
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
   it('builds the expected permission specification', () => {
     const specification = caip25EndowmentBuilder.specificationBuilder({});
     expect(specification).toStrictEqual({
@@ -215,5 +234,397 @@ describe('endowment:caip25', () => {
     });
   });
 
-  // it.todo('permission validator');
+  describe('permission validator', () => {
+    const findNetworkClientIdByChainId = jest.fn();
+    const { validator } = caip25EndowmentBuilder.specificationBuilder({
+      findNetworkClientIdByChainId,
+    });
+
+    it('throws an error if there is not exactly one caveat', () => {
+      expect(() => {
+        validator({
+          caveats: [
+            {
+              type: 'caveatType',
+              value: {},
+            },
+            {
+              type: 'caveatType',
+              value: {},
+            },
+          ],
+          date: 1234,
+          id: '1',
+          invoker: 'test.com',
+          parentCapability: Caip25EndowmentPermissionName,
+        });
+      }).toThrow(new Error('missing required caveat'));
+
+      expect(() => {
+        validator({
+          caveats: [] as unknown as NonEmptyArray<CaveatConstraint>,
+          date: 1234,
+          id: '1',
+          invoker: 'test.com',
+          parentCapability: Caip25EndowmentPermissionName,
+        });
+      }).toThrow(new Error('missing required caveat'));
+    });
+
+    it('throws an error if there is no CAIP-25 caveat', () => {
+      expect(() => {
+        validator({
+          caveats: [
+            {
+              type: 'NotCaip25Caveat',
+              value: {},
+            },
+          ],
+          date: 1234,
+          id: '1',
+          invoker: 'test.com',
+          parentCapability: Caip25EndowmentPermissionName,
+        });
+      }).toThrow(new Error('missing required caveat'));
+    });
+
+    it('throws an error if the CAIP-25 caveat is malformed', () => {
+      expect(() => {
+        validator({
+          caveats: [
+            {
+              type: Caip25CaveatType,
+              value: {
+                missingRequiredScopes: {},
+                optionalScopes: {},
+                isMultichainOrigin: true,
+              },
+            },
+          ],
+          date: 1234,
+          id: '1',
+          invoker: 'test.com',
+          parentCapability: Caip25EndowmentPermissionName,
+        });
+      }).toThrow(new Error('missing expected caveat values'));
+
+      expect(() => {
+        validator({
+          caveats: [
+            {
+              type: Caip25CaveatType,
+              value: {
+                requiredScopes: {},
+                missingOptionalScopes: {},
+                isMultichainOrigin: true,
+              },
+            },
+          ],
+          date: 1234,
+          id: '1',
+          invoker: 'test.com',
+          parentCapability: Caip25EndowmentPermissionName,
+        });
+      }).toThrow(new Error('missing expected caveat values'));
+
+      expect(() => {
+        validator({
+          caveats: [
+            {
+              type: Caip25CaveatType,
+              value: {
+                requiredScopes: {},
+                optionalScopes: {},
+                isMultichainOrigin: 'NotABoolean',
+              },
+            },
+          ],
+          date: 1234,
+          id: '1',
+          invoker: 'test.com',
+          parentCapability: Caip25EndowmentPermissionName,
+        });
+      }).toThrow(new Error('missing expected caveat values'));
+    });
+
+    it('validates and flattens the ScopesObjects', () => {
+      try {
+        validator({
+          caveats: [
+            {
+              type: Caip25CaveatType,
+              value: {
+                requiredScopes: {
+                  'eip155:1': {
+                    methods: ['eth_chainId'],
+                    notifications: [],
+                    accounts: ['eip155:1:0xdead'],
+                  },
+                },
+                optionalScopes: {
+                  'eip155:5': {
+                    methods: [],
+                    notifications: [],
+                    accounts: ['eip155:5:0xbeef'],
+                  },
+                },
+                isMultichainOrigin: true,
+              },
+            },
+          ],
+          date: 1234,
+          id: '1',
+          invoker: 'test.com',
+          parentCapability: Caip25EndowmentPermissionName,
+        });
+      } catch (err) {
+        // noop
+      }
+      expect(MockScope.validateAndFlattenScopes).toHaveBeenCalledWith(
+        {
+          'eip155:1': {
+            methods: ['eth_chainId'],
+            notifications: [],
+            accounts: ['eip155:1:0xdead'],
+          },
+        },
+        {
+          'eip155:5': {
+            methods: [],
+            notifications: [],
+            accounts: ['eip155:5:0xbeef'],
+          },
+        },
+      );
+    });
+
+    it('asserts the validated and flattened required scopes are supported', () => {
+      MockScope.validateAndFlattenScopes.mockReturnValue({
+        flattenedRequiredScopes: 'flattenedRequiredScopes',
+        flattenedOptionalScopes: 'flattenedOptionalScopes',
+      });
+      try {
+        validator({
+          caveats: [
+            {
+              type: Caip25CaveatType,
+              value: {
+                requiredScopes: {
+                  'eip155:1': {
+                    methods: ['eth_chainId'],
+                    notifications: [],
+                    accounts: ['eip155:1:0xdead'],
+                  },
+                },
+                optionalScopes: {
+                  'eip155:5': {
+                    methods: [],
+                    notifications: [],
+                    accounts: ['eip155:5:0xbeef'],
+                  },
+                },
+                isMultichainOrigin: true,
+              },
+            },
+          ],
+          date: 1234,
+          id: '1',
+          invoker: 'test.com',
+          parentCapability: Caip25EndowmentPermissionName,
+        });
+      } catch (err) {
+        // noop
+      }
+      expect(MockScope.assertScopesSupported).toHaveBeenCalledWith(
+        'flattenedRequiredScopes',
+        expect.objectContaining({
+          isChainIdSupported: expect.any(Function),
+        }),
+      );
+      const isChainIdSupportedBody =
+        MockScope.assertScopesSupported.mock.calls[0][1].isChainIdSupported.toString();
+      expect(isChainIdSupportedBody).toContain('findNetworkClientIdByChainId');
+    });
+
+    it('asserts the validated and flattened optional scopes are supported', () => {
+      MockScope.validateAndFlattenScopes.mockReturnValue({
+        flattenedRequiredScopes: 'flattenedRequiredScopes',
+        flattenedOptionalScopes: 'flattenedOptionalScopes',
+      });
+      try {
+        validator({
+          caveats: [
+            {
+              type: Caip25CaveatType,
+              value: {
+                requiredScopes: {
+                  'eip155:1': {
+                    methods: ['eth_chainId'],
+                    notifications: [],
+                    accounts: ['eip155:1:0xdead'],
+                  },
+                },
+                optionalScopes: {
+                  'eip155:5': {
+                    methods: [],
+                    notifications: [],
+                    accounts: ['eip155:5:0xbeef'],
+                  },
+                },
+                isMultichainOrigin: true,
+              },
+            },
+          ],
+          date: 1234,
+          id: '1',
+          invoker: 'test.com',
+          parentCapability: Caip25EndowmentPermissionName,
+        });
+      } catch (err) {
+        // noop
+      }
+      expect(MockScope.assertScopesSupported).toHaveBeenCalledWith(
+        'flattenedOptionalScopes',
+        expect.objectContaining({
+          isChainIdSupported: expect.any(Function),
+        }),
+      );
+      const isChainIdSupportedBody =
+        MockScope.assertScopesSupported.mock.calls[1][1].isChainIdSupported.toString();
+      expect(isChainIdSupportedBody).toContain('findNetworkClientIdByChainId');
+    });
+
+    it('throws if the input requiredScopes ScopesObject is not already validated and flattened', () => {
+      MockScope.validateAndFlattenScopes.mockReturnValue({
+        flattenedRequiredScopes: {},
+        flattenedOptionalScopes: {
+          'eip155:5': {
+            methods: [],
+            notifications: [],
+            accounts: ['eip155:5:0xbeef'],
+          },
+        },
+      });
+      expect(() => {
+        validator({
+          caveats: [
+            {
+              type: Caip25CaveatType,
+              value: {
+                requiredScopes: {
+                  'eip155:1': {
+                    methods: ['eth_chainId'],
+                    notifications: [],
+                    accounts: ['eip155:1:0xdead'],
+                  },
+                },
+                optionalScopes: {
+                  'eip155:5': {
+                    methods: [],
+                    notifications: [],
+                    accounts: ['eip155:5:0xbeef'],
+                  },
+                },
+                isMultichainOrigin: true,
+              },
+            },
+          ],
+          date: 1234,
+          id: '1',
+          invoker: 'test.com',
+          parentCapability: Caip25EndowmentPermissionName,
+        });
+      }).toThrow(/Expected values to be strictly deep-equal/u);
+    });
+
+    it('throws if the input optionalScopes ScopesObject is not already validated and flattened', () => {
+      MockScope.validateAndFlattenScopes.mockReturnValue({
+        flattenedRequiredScopes: {
+          'eip155:1': {
+            methods: ['eth_chainId'],
+            notifications: [],
+            accounts: ['eip155:1:0xdead'],
+          },
+        },
+        flattenedOptionalScopes: {},
+      });
+      expect(() => {
+        validator({
+          caveats: [
+            {
+              type: Caip25CaveatType,
+              value: {
+                requiredScopes: {
+                  'eip155:1': {
+                    methods: ['eth_chainId'],
+                    notifications: [],
+                    accounts: ['eip155:1:0xdead'],
+                  },
+                },
+                optionalScopes: {
+                  'eip155:5': {
+                    methods: [],
+                    notifications: [],
+                    accounts: ['eip155:5:0xbeef'],
+                  },
+                },
+                isMultichainOrigin: true,
+              },
+            },
+          ],
+          date: 1234,
+          id: '1',
+          invoker: 'test.com',
+          parentCapability: Caip25EndowmentPermissionName,
+        });
+      }).toThrow(/Expected values to be strictly deep-equal/u);
+    });
+
+    it('does not throw if the input requiredScopes and optionalScopes ScopesObject are already validated and flattened', () => {
+      MockScope.validateAndFlattenScopes.mockReturnValue({
+        flattenedRequiredScopes: {
+          'eip155:1': {
+            methods: ['eth_chainId'],
+            notifications: [],
+            accounts: ['eip155:1:0xdead'],
+          },
+        },
+        flattenedOptionalScopes: {
+          'eip155:5': {
+            methods: [],
+            notifications: [],
+            accounts: ['eip155:5:0xbeef'],
+          },
+        },
+      });
+      validator({
+        caveats: [
+          {
+            type: Caip25CaveatType,
+            value: {
+              requiredScopes: {
+                'eip155:1': {
+                  methods: ['eth_chainId'],
+                  notifications: [],
+                  accounts: ['eip155:1:0xdead'],
+                },
+              },
+              optionalScopes: {
+                'eip155:5': {
+                  methods: [],
+                  notifications: [],
+                  accounts: ['eip155:5:0xbeef'],
+                },
+              },
+              isMultichainOrigin: true,
+            },
+          },
+        ],
+        date: 1234,
+        id: '1',
+        invoker: 'test.com',
+        parentCapability: Caip25EndowmentPermissionName,
+      });
+    });
+  });
 });
