7f7e379ca2c7227764088faa8c0ab662878ded3e
Nicholas Gambino <nicholas.gambino@consensys.net>
2024-08-27 13:08:27 -0700

    Add migration for 128, rename MATIC to POL

---
+++ app/scripts/migrations/128.ts
@@ -0,0 +1,48 @@
+import { hasProperty, isObject } from '@metamask/utils';
+import { cloneDeep } from 'lodash';
+
+export const version = 128;
+
+type VersionedData = {
+  meta: { version: number };
+  data: Record<string, unknown>;
+};
+
+/**
+ * Explain the purpose of the migration here.
+ *
+ * @param originalVersionedData - Versioned MetaMask extension state, exactly what we persist to dist.
+ * @param originalVersionedData.meta - State metadata.
+ * @param originalVersionedData.meta.version - The current state version.
+ * @param originalVersionedData.data - The persisted MetaMask state, keyed by controller.
+ * @returns Updated versioned MetaMask extension state.
+ */
+export async function migrate(
+  originalVersionedData: VersionedData,
+): Promise<VersionedData> {
+  const versionedData = cloneDeep(originalVersionedData);
+  versionedData.meta.version = version;
+  transformState(versionedData.data);
+  return versionedData;
+}
+
+function transformState(state: Record<string, unknown>) {
+  if (
+    hasProperty(state, 'NetworkController') &&
+    isObject(state.NetworkController)
+  ) {
+    const existingNetworkConfigsCopy = state.NetworkController
+      .networkConfigurations as Record<string, any>;
+
+    Object.values(existingNetworkConfigsCopy).forEach((networkConfig) => {
+      if (networkConfig.ticker === 'MATIC') {
+        existingNetworkConfigsCopy[networkConfig.id].ticker = 'POL';
+      }
+    });
+
+    state.NetworkController.networkConfigurations = existingNetworkConfigsCopy;
+  }
+  const newState = state;
+  // transform state here
+  return newState;
+}
