945b77ca29902996449d013747b54194a1752f9e
Mark Stacey <markjstacey@gmail.com>
2024-08-28 16:00:37 -0230

    Fix Sentry state merge conflict error

--- app/scripts/constants/sentry-state.ts
+++ app/scripts/constants/sentry-state.ts
@@ -319,6 +319,7 @@ export const SENTRY_BACKGROUND_STATE = {
       swapsQuotePrefetchingRefreshTime: true,
       swapsQuoteRefreshTime: true,
       swapsStxBatchStatusRefreshTime: true,
+      swapsStxStatusDeadline: true,
       swapsStxGetTransactionsRefreshTime: true,
       swapsStxMaxFeeMultiplier: true,
       swapsUserFeeLevel: true,
