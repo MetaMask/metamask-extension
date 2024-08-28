a0040a88aace9e8f74edc61b77e65470ebfe071b
Micaela Estabillo <micaela.estabillo@consensys.net>
2024-08-28 15:45:40 -0700

    feat: implement bridge to/from inputs

--- ui/pages/bridge/prepare/prepare-bridge-page.tsx
+++ ui/pages/bridge/prepare/prepare-bridge-page.tsx
@@ -1,24 +1,130 @@
 import React from 'react';
-import { useSelector, shallowEqual } from 'react-redux';
-import { isEqual, shuffle } from 'lodash';
-import PrepareSwapPage from '../../swaps/prepare-swap-page/prepare-swap-page';
-import { getSelectedAccount, getTokenList } from '../../../selectors';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  setFromChain,
+  setFromToken,
+  setFromTokenInputValue,
+  setToChain,
+  setToToken,
+} from '../../../ducks/bridge/actions';
+import {
+  getFromAmount,
+  getFromChain,
+  getFromChains,
+  getFromToken,
+  getFromTokens,
+  getFromTopAssets,
+  getToAmount,
+  getToChain,
+  getToChains,
+  getToToken,
+  getToTokens,
+  getToTopAssets,
+} from '../../../ducks/bridge/selectors';
+import { Box } from '../../../components/component-library';
+import { useI18nContext } from '../../../hooks/useI18nContext';
+import { TokenBucketPriority } from '../../../../shared/constants/swaps';
+import { useTokensWithFiltering } from '../../../hooks/useTokensWithFiltering';
+import { setActiveNetwork } from '../../../store/actions';
+import { BridgeInputGroup } from './bridge-input-group';
 
-export const PrepareBridgePage = () => {
-  const selectedAccount = useSelector(getSelectedAccount, shallowEqual);
-  const { balance: ethBalance, address: selectedAccountAddress } =
-    selectedAccount;
+const PrepareBridgePage = () => {
+  const dispatch = useDispatch();
 
-  const tokenList = useSelector(getTokenList, isEqual);
-  const shuffledTokensList = shuffle(Object.values(tokenList));
+  const t = useI18nContext();
+
+  const fromToken = useSelector(getFromToken);
+  const fromTokens = useSelector(getFromTokens);
+  const fromTopAssets = useSelector(getFromTopAssets);
+
+  const toToken = useSelector(getToToken);
+  const toTokens = useSelector(getToTokens);
+  const toTopAssets = useSelector(getToTopAssets);
+
+  const fromChains = useSelector(getFromChains);
+  const toChains = useSelector(getToChains);
+  const fromChain = useSelector(getFromChain);
+  const toChain = useSelector(getToChain);
+
+  const fromAmount = useSelector(getFromAmount);
+  const toAmount = useSelector(getToAmount);
+
+  const fromTokenListGenerator = useTokensWithFiltering(
+    fromTokens,
+    fromTopAssets,
+    TokenBucketPriority.owned,
+    fromChain?.chainId,
+  );
+  const toTokenListGenerator = useTokensWithFiltering(
+    toTokens,
+    toTopAssets,
+    TokenBucketPriority.top,
+    toChain?.chainId,
+  );
 
   return (
-    <div>
-      <PrepareSwapPage
-        ethBalance={ethBalance}
-        selectedAccountAddress={selectedAccountAddress}
-        shuffledTokensList={shuffledTokensList}
-      />
+    <div className="prepare-bridge-page">
+      <Box className="prepare-bridge-page__content">
+        <BridgeInputGroup
+          className="prepare-bridge-page__from"
+          header={t('bridgeFrom')}
+          token={fromToken}
+          onAmountChange={(e) => {
+            dispatch(setFromTokenInputValue(e));
+          }}
+          onAssetChange={(token) => dispatch(setFromToken(token))}
+          networkProps={{
+            network: fromChain,
+            networks: fromChains,
+            onNetworkChange: (networkConfig) => {
+              dispatch(
+                setActiveNetwork(
+                  networkConfig.rpcEndpoints[
+                    networkConfig.defaultRpcEndpointIndex
+                  ].networkClientId,
+                ),
+              );
+              dispatch(setFromChain(networkConfig.chainId));
+              // TODO emit metric
+            },
+          }}
+          customTokenListGenerator={
+            fromTokens && fromTopAssets ? fromTokenListGenerator : undefined
+          }
+          amountFieldProps={{
+            testId: 'from-amount',
+            autoFocus: true,
+            value: fromAmount || undefined,
+          }}
+        />
+
+        <BridgeInputGroup
+          className="prepare-bridge-page__to"
+          header={t('bridgeTo')}
+          token={toToken}
+          onAssetChange={(token) => dispatch(setToToken(token))}
+          networkProps={{
+            network: toChain,
+            networks: toChains,
+            onNetworkChange: (networkConfig) => {
+              dispatch(setToChain(networkConfig.chainId));
+            },
+          }}
+          customTokenListGenerator={
+            toChain && toTokens && toTopAssets
+              ? toTokenListGenerator
+              : fromTokenListGenerator
+          }
+          amountFieldProps={{
+            testId: 'to-amount',
+            readOnly: true,
+            disabled: true,
+            value: toAmount,
+          }}
+        />
+      </Box>
     </div>
   );
 };
+
+export default PrepareBridgePage;
