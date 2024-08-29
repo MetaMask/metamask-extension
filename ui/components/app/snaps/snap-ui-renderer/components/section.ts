c2c38a4c2b0f3b454a678d6e19224c8c04e38128
Hassan Malik <hbmalik88@gmail.com>
2024-08-29 13:34:29 -0400

    apply pr suggestions

--- ui/components/app/snaps/snap-ui-renderer/components/section.ts
+++ ui/components/app/snaps/snap-ui-renderer/components/section.ts
@@ -1,54 +1,25 @@
-import { SectionElement, JSXElement, SectionProps } from '@metamask/snaps-sdk/jsx';
-import { getJsxChildren } from '@metamask/snaps-utils';
-import { NonEmptyArray } from '@metamask/utils';
+import { SectionElement, BoxElement } from '@metamask/snaps-sdk/jsx';
 import {
-  Display,
-  FlexDirection,
   BackgroundColor,
   BorderRadius,
-  JustifyContent,
 } from '../../../../../helpers/constants/design-system';
-import { mapToTemplate } from '../utils';
-import { UIComponent, UIComponentFactory } from './types';
-
-function generateJustifyContent(alignment?: SectionProps['alignment']) {
-  switch (alignment) {
-    default:
-    case 'start':
-      return JustifyContent.flexStart;
-
-    case 'center':
-      return JustifyContent.center;
-
-    case 'end':
-      return JustifyContent.flexEnd;
-
-    case 'space-between':
-      return JustifyContent.spaceBetween;
-
-    case 'space-around':
-      return JustifyContent.spaceAround;
-  }
-}
+import { UIComponentFactory, UIComponentParams } from './types';
+import { box } from './box';
 
 export const section: UIComponentFactory<SectionElement> = ({
   element,
   ...params
-}) => ({
-  element: 'Box',
-  children: getJsxChildren(element).map((children) =>
-    mapToTemplate({ ...params, element: children as JSXElement }),
-  ) as NonEmptyArray<UIComponent>,
-  props: {
-    display: Display.Flex,
-    flexDirection:
-      element.props.direction === 'horizontal'
-        ? FlexDirection.Row
-        : FlexDirection.Column,
-    className: 'snap-ui-renderer__section',
-    padding: 4,
-    backgroundColor: BackgroundColor.backgroundDefault,
-    borderRadius: BorderRadius.LG,
-    justifyContent: generateJustifyContent(element.props.alignment)
-  },
-});
+}) => {
+  const { children, props } = box({ element, ...params } as unknown as UIComponentParams<BoxElement>);
+  return {
+    element: 'Section',
+    children,
+    props: {
+      ...props,
+      className: 'snap-ui-renderer__section',
+      padding: 4,
+      backgroundColor: BackgroundColor.backgroundDefault,
+      borderRadius: BorderRadius.LG,
+    },
+  }
+};
