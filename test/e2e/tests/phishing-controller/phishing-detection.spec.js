b63c0b6102fde9072acbe57376209afb3c18b9ec
HJetpoluru <harika.jetpoluru@consensys.net>
2024-08-29 17:06:00 -0400

    Added wait for window to load

--- test/e2e/tests/phishing-controller/phishing-detection.spec.js
+++ test/e2e/tests/phishing-controller/phishing-detection.spec.js
@@ -105,6 +105,9 @@ describe('Phishing Detection', function () {
         await driver.clickElement({
           text: 'continue to the site.',
         });
+
+        // Windows: MetaMask, Test Dapp to load
+        await driver.waitUntilXWindowHandles(2);
         const header = await driver.findElement('h1');
         assert.equal(await header.getText(), 'E2E Test Dapp');
       };
