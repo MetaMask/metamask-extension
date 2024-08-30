0490020bf7307d303e958e8cfbb3471b76e24bc9
Ariella Vu <20778143+digiwand@users.noreply.github.com>
2024-08-30 06:19:46 -0700

    fix: PermitTransferFrom or PermitBatchTransferFrom crashes MM when value for Name address has not been loaded  (#26791)

--- ui/components/app/name/name-details/name-details.tsx
+++ ui/components/app/name/name-details/name-details.tsx
@@ -71,6 +71,10 @@ type ProposedNameOption = Required<FormComboFieldOption> & {
 };
 
 function formatValue(value: string, type: NameType): string {
+  if (!value.length) {
+    return value;
+  }
+
   switch (type) {
     case NameType.ETHEREUM_ADDRESS:
       return toChecksumAddress(value);
