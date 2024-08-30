b59c93336e8b2a44c123206e71504cf45968c80c
Jongsun Suh <jongsun.suh@icloud.com>
2024-08-30 11:44:06 -0400

    Partial cherry-pick 95f775363ae6f7d2f6bf55efc516831bf811de96 (#25980)
    - Convert test/jest/mocks.js to TypeScript

--- app/scripts/lib/accounts/BalancesController.test.ts
+++ app/scripts/lib/accounts/BalancesController.test.ts
@@ -19,7 +19,6 @@ import { Poller } from './Poller';
 const mockBtcAccount = createMockInternalAccount({
   address: '',
   name: 'Btc Account',
-  // @ts-expect-error - account type may be btc or eth, mock file is not typed
   type: BtcAccountType.P2wpkh,
   snapOptions: {
     id: 'mock-btc-snap',
