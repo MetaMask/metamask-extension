fbace3db75172154c78d17533e927f67850a9c16
Chloe Gao <chloe.gao@consensys.net>
2024-09-02 15:07:06 +0200

    chloe modification

--- ui/pages/create-account/connect-hardware/account-list.js
+++ ui/pages/create-account/connect-hardware/account-list.js
@@ -133,7 +133,7 @@ class AccountList extends Component {
                   <span className="hw-account-list__item__index">
                     {account.index + 1}
                   </span>
-                  <span>{`${account.address.slice(
+                  <span data-testid="hw-account-list__item__address">{`${account.address.slice(
                     0,
                     4,
                   )}...${account.address.slice(-4)}`}</span>
