93185cadea876ba79ada4eb65cf633292a12ca0a
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2024-09-03 14:29:05 +0100

    fix linting errors

--- ui/pages/confirmations/components/confirm/info/approve/edit-spending-cap-modal/edit-spending-cap-modal.test.tsx
+++ ui/pages/confirmations/components/confirm/info/approve/edit-spending-cap-modal/edit-spending-cap-modal.test.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { genUnapprovedApproveConfirmation } from '../../../../../../../../test/data/confirmations/contract-interaction';
-import mockState from '../../../../../../../../test/data/mock-state.json';
-import { renderWithConfirmContextProvider } from '../../../../../../../../test/lib/confirmations/render-helpers';
-import { useAssetDetails } from '../../../../../hooks/useAssetDetails';
-import { EditSpendingCapModal } from './edit-spending-cap-modal';
-import { useApproveTokenSimulation } from '../hooks/use-approve-token-simulation';
 import { getMockApproveConfirmState } from '../../../../../../../../test/data/confirmations/helper';
+import { renderWithConfirmContextProvider } from '../../../../../../../../test/lib/confirmations/render-helpers';
+import { EditSpendingCapModal } from './edit-spending-cap-modal';
 
 jest.mock('../hooks/use-approve-token-simulation', () => ({
   useApproveTokenSimulation: jest.fn(() => ({
