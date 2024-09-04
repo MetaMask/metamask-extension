218ad5e1e3c74bc0b69366f09f35e9786a061396
Jiexi Luan <jiexiluan@gmail.com>
2024-09-04 13:08:23 -0700

    Add comments

--- app/scripts/lib/multichain-api/scope/scope.ts
+++ app/scripts/lib/multichain-api/scope/scope.ts
@@ -49,13 +49,21 @@ export const KnownNotifications: Record<NonWalletKnownCaipNamespace, string[]> =
     eip155: ['accountsChanged', 'chainChanged', 'eth_subscription'],
   };
 
-
+// These External prefixed types represent the CAIP-217
+// Scope and ScopeObject as defined in the spec.
 export type ExternalScope = CaipChainId | CaipReference;
 export type ExternalScopeObject = ScopeObject & {
   scopes?: CaipChainId[];
 };
 export type ExternalScopesObject = Record<ExternalScope, ExternalScopeObject>;
 
+// These non-prefixed types represent CAIP-217 Scope and
+// ScopeObject as defined by the spec but without
+// namespace-only Scopes (except for "wallet") and without
+// the `scopes` array of CAIP Chain IDs on ScopeObject.
+// These deviations from the spec are necessary as MetaMask
+// does not support wildcarded Scopes, i.e. Scopes that only
+// specify a namespace but no specific reference.
 export type Scope = CaipChainId | KnownCaipNamespace.Wallet;
 export type ScopeObject = {
   methods: string[];
