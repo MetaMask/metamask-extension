f570beb7cb3d354ccdd8f1720644a9e6db3b3c49
seaona <mariona@gmx.es>
2024-09-10 10:22:02 +0200

    add delay

--- test/e2e/tests/transaction/change-assets.spec.js
+++ test/e2e/tests/transaction/change-assets.spec.js
@@ -343,6 +343,7 @@ describe('Change assets', function () {
         // Make sure gas is updated by resetting amount and hex data
         // Note: this is needed until the race condition is fixed on the wallet level (issue #25243)
         await driver.fill('[data-testid="currency-input"]', '2');
+        await driver.delay(3000);
         await hexDataLocator.fill('0x');
         await hexDataLocator.fill('');
 
