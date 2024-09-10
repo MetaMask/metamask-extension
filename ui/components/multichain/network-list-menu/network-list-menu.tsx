db211e2c110939bcf92cb90de4be6751192ac0c3
Brian Bergeron <brian.e.bergeron@gmail.com>
2024-09-10 15:26:21 -0700

    Fix static code analysis typescript:S6443 - React state setter function should not be called with its matching state variable

--- ui/components/multichain/network-list-menu/network-list-menu.tsx
+++ ui/components/multichain/network-list-menu/network-list-menu.tsx
@@ -183,10 +183,11 @@ export const NetworkListMenu = ({ onClose }: { onClose: () => void }) => {
   // Re-orders networks when the user drag + drops them
   const onDragEnd = (result: DropResult) => {
     if (result.destination) {
-      const [removed] = orderedNetworks.splice(result.source.index, 1);
-      orderedNetworks.splice(result.destination.index, 0, removed);
-      dispatch(updateNetworksList(orderedNetworks.map((n) => n.chainId)));
-      setOrderedNetworks(orderedNetworks);
+      const newOrderedNetworks = [...orderedNetworks];
+      const [removed] = newOrderedNetworks.splice(result.source.index, 1);
+      newOrderedNetworks.splice(result.destination.index, 0, removed);
+      dispatch(updateNetworksList(newOrderedNetworks.map((n) => n.chainId)));
+      setOrderedNetworks(newOrderedNetworks);
     }
   };
 
