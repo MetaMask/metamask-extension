4f7f80876f976785997a587b20b667ae045ee344
Ethan Wessel <ethan.wessel@consensys.net>
2024-09-11 13:57:31 -0700

    chore: optimized regex

--- ui/components/app/modals/qr-scanner/scan-util.ts
+++ ui/components/app/modals/qr-scanner/scan-util.ts
@@ -1,5 +1,5 @@
 export function parseScanContent(content: string): string | null {
-  const matches = content.match(/.*:(0x[0-9a-fA-F]{40})(?:@.*)?/u);
+  const matches = content.match(/^[a-zA-Z]+:(0x[0-9a-fA-F]{40})(?:@.*)?/u);
   if (!matches) {
     return null;
   }
