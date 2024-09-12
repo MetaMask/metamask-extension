09a9fee1e6471cbf0634125325d8f44d24adffd9
digiwand <20778143+digiwand@users.noreply.github.com>
2024-09-12 23:57:53 +0400

    fix: lint utils/token comment

--- ui/pages/confirmations/utils/token.ts
+++ ui/pages/confirmations/utils/token.ts
@@ -5,9 +5,10 @@ import { ERC20_DEFAULT_DECIMALS } from '../constants/token';
 
 /**
  * Fetches the decimals for the given token address.
+ *
  * @param {Hex | string} address - The ethereum token contract address. It is expected to be in hex format.
- *    We currently accept strings since we have a patch that accepts a custom string
- *    {@see .yarn/patches/@metamask-eth-json-rpc-middleware-npm-14.0.1-b6c2ccbe8c.patch}
+ * We currently accept strings since we have a patch that accepts a custom string
+ * {@see .yarn/patches/@metamask-eth-json-rpc-middleware-npm-14.0.1-b6c2ccbe8c.patch}
  */
 export const fetchErc20Decimals = memoize(
   async (address: Hex | string): Promise<number> => {
