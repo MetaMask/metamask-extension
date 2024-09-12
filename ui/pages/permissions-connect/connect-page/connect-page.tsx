2a3ab14c75d4d4f6585686af34cf5950a60cc2a3
NidhiKJha <menidhikjha@gmail.com>
2024-09-12 22:47:57 +0100

    updated stories

--- ui/pages/permissions-connect/connect-page/connect-page.tsx
+++ ui/pages/permissions-connect/connect-page/connect-page.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { useI18nContext } from '../../../hooks/useI18nContext';
 import {
