eff052573a4409ba3065ce329e301e859fed443d
digiwand <20778143+digiwand@users.noreply.github.com>
2024-09-12 11:20:59 +0400

    refactor: memoize fetchErc20Decimals

--- ui/pages/confirmations/utils/token.ts
+++ ui/pages/confirmations/utils/token.ts
@@ -1,9 +1,10 @@
+import { memoize } from 'lodash';
 import { Hex } from '@metamask/utils';
 import { getTokenStandardAndDetails } from '../../../store/actions';
 import { ERC20_DEFAULT_DECIMALS } from '../constants/token';
 
 // Fetches the decimals for the given token address.
-export async function fetchErc20Decimals(address: Hex): Promise<number> {
+export const fetchErc20Decimals = memoize(async (address: Hex): Promise<number> => {
   try {
     const { decimals: decStr } = await getTokenStandardAndDetails(address);
     if (!decStr) {
@@ -19,4 +20,4 @@ export async function fetchErc20Decimals(address: Hex): Promise<number> {
   } catch {
     return ERC20_DEFAULT_DECIMALS;
   }
-}
+})
