fba56f2638af6b69584fe7e5ef05639640765922
Guillaume Roux <guillaumeroux123@gmail.com>
2024-09-13 12:32:46 +0200

    fix Selector submiting form

--- ui/components/app/snaps/snap-ui-selector/snap-ui-selector.tsx
+++ ui/components/app/snaps/snap-ui-selector/snap-ui-selector.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, MouseEvent } from 'react';
 import {
   Box,
   ButtonBase,
@@ -103,7 +103,10 @@ export const SnapUISelector: React.FunctionComponent<SnapUISelectorProps> = ({
     }
   }, [initialValue]);
 
-  const handleModalOpen = () => setIsModalOpen(true);
+  const handleModalOpen = (event: MouseEvent<HTMLElement>) => {
+    event.preventDefault();
+    setIsModalOpen(true);
+  };
 
   const handleModalClose = () => setIsModalOpen(false);
 
