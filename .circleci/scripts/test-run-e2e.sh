de6461f85ad0abada31f38613e4c77d7a47b44b0
Howard Braham <howrad@gmail.com>
2024-09-16 14:43:10 -0400

    15 minutes, plus 3 minutes for every changed file, up to a maximum of 30 minutes

--- .circleci/scripts/test-run-e2e.sh
+++ .circleci/scripts/test-run-e2e.sh
@@ -19,8 +19,11 @@ then
     exit 1
 fi
 
+TIMEOUT_MINUTES=$(yarn tsx .circleci/scripts/test-run-e2e-timeout-minutes.ts)
+echo "TIMEOUT_MINUTES: $TIMEOUT_MINUTES"
+
 # Run the actual test command from the parameters
-timeout 25m "$@" --retries 1
+timeout ${TIMEOUT_MINUTES}m "$@" --retries 1
 
 # Error code 124 means the command timed out
 if [ $? -eq 124 ]
