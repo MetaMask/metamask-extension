2a69cbaa463aa0891b9da58de2fea773ac0ccadf
Antonio Regadas <antonio.regadas@consensys.net>
2024-09-16 15:00:16 +0100

    chore: prettier and lint

--- ui/components/institutional/note-to-trader/note-to-trader.tsx
+++ ui/components/institutional/note-to-trader/note-to-trader.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { TransactionMeta } from '@metamask/transaction-controller';
 import {
   BackgroundColor,
   BorderRadius,
