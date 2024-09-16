062a48d616ca185611980e921682ee683da742be
cryptotavares <joao.tavares@consensys.net>
2024-09-16 16:35:17 +0100

    test: improve erc20 approve integration test

--- test/integration/confirmations/transactions/erc20-approve.test.tsx
+++ test/integration/confirmations/transactions/erc20-approve.test.tsx
@@ -1,13 +1,14 @@
 import { ApprovalType } from '@metamask/controller-utils';
 import { act, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import nock from 'nock';
 import * as backgroundConnection from '../../../../ui/store/background-connection';
 import { integrationTestRender } from '../../../lib/render-helpers';
 import mockMetaMaskState from '../../data/integration-init-state.json';
 import { createMockImplementation, mock4byte } from '../../helpers';
-import { getUnapprovedApproveTransaction } from './transactionDataHelpers';
 import { TokenStandard } from '../../../../shared/constants/transaction';
 import { createTestProviderTools } from '../../../stub/provider';
+import { getUnapprovedApproveTransaction } from './transactionDataHelpers';
 
 jest.mock('../../../../ui/store/background-connection', () => ({
   ...jest.requireActual('../../../../ui/store/background-connection'),
@@ -23,14 +24,21 @@ const backgroundConnectionMocked = {
 export const pendingTransactionId = '48a75190-45ca-11ef-9001-f3886ec2397c';
 export const pendingTransactionTime = new Date().getTime();
 
-const getMetaMaskStateWithUnapprovedApproveTransaction = (
-  accountAddress: string,
-) => {
+const getMetaMaskStateWithUnapprovedApproveTransaction = (opts?: {
+  showAdvanceDetails: boolean;
+}) => {
+  const account =
+    mockMetaMaskState.internalAccounts.accounts[
+      mockMetaMaskState.internalAccounts
+        .selectedAccount as keyof typeof mockMetaMaskState.internalAccounts.accounts
+    ];
+
   return {
     ...mockMetaMaskState,
     preferences: {
       ...mockMetaMaskState.preferences,
       redesignedConfirmationsEnabled: true,
+      showConfirmationAdvancedDetails: opts?.showAdvanceDetails ?? false,
     },
     pendingApprovals: {
       [pendingTransactionId]: {
@@ -47,9 +55,12 @@ const getMetaMaskStateWithUnapprovedApproveTransaction = (
     },
     pendingApprovalCount: 1,
     knownMethodData: {
-      '0x3b4b1381': {
-        name: 'Mint NFTs',
+      '0x095ea7b3': {
+        name: 'Approve',
         params: [
+          {
+            type: 'address',
+          },
           {
             type: 'uint256',
           },
@@ -58,7 +69,7 @@ const getMetaMaskStateWithUnapprovedApproveTransaction = (
     },
     transactions: [
       getUnapprovedApproveTransaction(
-        accountAddress,
+        account.address,
         pendingTransactionId,
         pendingTransactionTime,
       ),
@@ -75,7 +86,7 @@ const advancedDetailsMockedRequests = {
   decodeTransactionData: {
     data: [
       {
-        name: 'approve',
+        name: 'Approve',
         params: [
           {
             type: 'address',
@@ -90,9 +101,6 @@ const advancedDetailsMockedRequests = {
     ],
     source: 'FourByte',
   },
-  getTokenStandardAndDetails: {
-    standard: TokenStandard.ERC20,
-  },
 };
 
 const setupSubmitRequestToBackgroundMocks = (
@@ -103,6 +111,10 @@ const setupSubmitRequestToBackgroundMocks = (
       ...advancedDetailsMockedRequests,
       ...(mockRequests ?? {}),
     }),
+  );
+
+  mockedBackgroundConnection.callBackgroundMethod.mockImplementation(
+    createMockImplementation({ addKnownMethodData: {} }),
   );
 };
 
@@ -119,7 +131,11 @@ describe('ERC20 Approve Confirmation', () => {
 
   beforeEach(() => {
     jest.resetAllMocks();
-    setupSubmitRequestToBackgroundMocks();
+    setupSubmitRequestToBackgroundMocks({
+      getTokenStandardAndDetails: {
+        standard: TokenStandard.ERC20,
+      },
+    });
     const APPROVE_ERC20_HEX_SIG = '0x095ea7b3';
     const APPROVE_ERC20_TEXT_SIG = 'approve(address,uint256)';
     mock4byte(APPROVE_ERC20_HEX_SIG, APPROVE_ERC20_TEXT_SIG);
@@ -134,33 +150,223 @@ describe('ERC20 Approve Confirmation', () => {
     delete (global as any).ethereumProvider;
   });
 
-  it('displays approve details with correct data', async () => {
-    mockedBackgroundConnection.callBackgroundMethod.mockImplementation(
-      createMockImplementation({ addKnownMethodData: {} }),
-    );
-
-    const account =
-      mockMetaMaskState.internalAccounts.accounts[
-        mockMetaMaskState.internalAccounts
-          .selectedAccount as keyof typeof mockMetaMaskState.internalAccounts.accounts
-      ];
-
+  it('displays spending cap request title', async () => {
     const mockedMetaMaskState =
-      getMetaMaskStateWithUnapprovedApproveTransaction(account.address);
+      getMetaMaskStateWithUnapprovedApproveTransaction();
 
-    let testContainer;
     await act(async () => {
-      const { container } = await integrationTestRender({
+      await integrationTestRender({
         preloadedState: mockedMetaMaskState,
         backgroundConnection: backgroundConnectionMocked,
       });
-
-      testContainer = container;
     });
 
-    expect(await screen.findByText('Spending cap request')).toBeInTheDocument();
-    expect(await screen.findByText('Request from')).toBeInTheDocument();
+    expect(screen.getByText('Spending cap request')).toBeInTheDocument();
+    expect(
+      screen.getByText('This site wants permission to withdraw your tokens'),
+    ).toBeInTheDocument();
+  });
 
-    expect(nock.isDone()).toBe(true);
+  it('displays approve simulation section', async () => {
+    const mockedMetaMaskState =
+      getMetaMaskStateWithUnapprovedApproveTransaction();
+
+    await act(async () => {
+      await integrationTestRender({
+        preloadedState: mockedMetaMaskState,
+        backgroundConnection: backgroundConnectionMocked,
+      });
+    });
+
+    const simulationSection = screen.getByTestId(
+      'confirmation__simulation_section',
+    );
+    expect(simulationSection).toBeInTheDocument();
+
+    // TODO - fix this copy
+    expect(simulationSection).toHaveTextContent(
+      "You're giving someone else permission to withdraw NFTs from your account",
+    );
+    expect(simulationSection).toHaveTextContent('Spending cap');
+    const spendingCapValue = screen.getByTestId('simulation-token-value');
+    expect(simulationSection).toContainElement(spendingCapValue);
+    expect(spendingCapValue).toHaveTextContent('1');
+    expect(simulationSection).toHaveTextContent('0x07614...3ad68');
+  });
+
+  it('displays approve details with correct data', async () => {
+    const testUser = userEvent.setup();
+
+    const mockedMetaMaskState =
+      getMetaMaskStateWithUnapprovedApproveTransaction();
+
+    await act(async () => {
+      await integrationTestRender({
+        preloadedState: mockedMetaMaskState,
+        backgroundConnection: backgroundConnectionMocked,
+      });
+    });
+
+    const approveDetails = screen.getByTestId('confirmation__approve-details');
+    expect(approveDetails).toBeInTheDocument();
+    const approveDetailsSpender = screen.getByTestId(
+      'confirmation__approve-spender',
+    );
+
+    expect(approveDetails).toContainElement(approveDetailsSpender);
+    expect(approveDetailsSpender).toHaveTextContent('Spender');
+    expect(approveDetailsSpender).toHaveTextContent('0x2e0D7...5d09B');
+    const spenderTooltip = screen.getByTestId(
+      'confirmation__approve-spender-tooltip',
+    );
+    expect(approveDetailsSpender).toContainElement(spenderTooltip);
+    await testUser.hover(spenderTooltip);
+    // TODO - fix this copy
+    const spenderTooltipContent = await screen.findByText(
+      'This is the address that will be able to withdraw your NFTs.',
+    );
+    expect(spenderTooltipContent).toBeInTheDocument();
+
+    const approveDetailsRequestFrom = screen.getByTestId(
+      'transaction-details-origin-row',
+    );
+    expect(approveDetails).toContainElement(approveDetailsRequestFrom);
+    expect(approveDetailsRequestFrom).toHaveTextContent('Request from');
+    expect(approveDetailsRequestFrom).toHaveTextContent(
+      'http://localhost:8086/',
+    );
+
+    const approveDetailsRequestFromTooltip = screen.getByTestId(
+      'transaction-details-origin-row-tooltip',
+    );
+    expect(approveDetailsRequestFrom).toContainElement(
+      approveDetailsRequestFromTooltip,
+    );
+    await testUser.hover(approveDetailsRequestFromTooltip);
+    const requestFromTooltipContent = await screen.findByText(
+      'This is the site asking for your confirmation.',
+    );
+    expect(requestFromTooltipContent).toBeInTheDocument();
+  });
+
+  it('displays spending cap section with correct data', async () => {
+    const testUser = userEvent.setup();
+
+    const mockedMetaMaskState =
+      getMetaMaskStateWithUnapprovedApproveTransaction();
+
+    await act(async () => {
+      await integrationTestRender({
+        preloadedState: mockedMetaMaskState,
+        backgroundConnection: backgroundConnectionMocked,
+      });
+    });
+
+    const spendingCapSection = screen.getByTestId(
+      'confirmation__approve-spending-cap-section',
+    );
+    expect(spendingCapSection).toBeInTheDocument();
+
+    expect(spendingCapSection).toHaveTextContent('Account balance');
+    expect(spendingCapSection).toHaveTextContent('0');
+    const spendingCapGroup = screen.getByTestId(
+      'confirmation__approve-spending-cap-group',
+    );
+    expect(spendingCapSection).toContainElement(spendingCapGroup);
+    expect(spendingCapGroup).toHaveTextContent('Spending cap');
+    expect(spendingCapGroup).toHaveTextContent('1');
+
+    const spendingCapGroupTooltip = screen.getByTestId(
+      'confirmation__approve-spending-cap-group-tooltip',
+    );
+    expect(spendingCapGroup).toContainElement(spendingCapGroupTooltip);
+    await testUser.hover(spendingCapGroupTooltip);
+    const requestFromTooltipContent = await screen.findByText(
+      'This is the amount of tokens the spender will be able to access on your behalf.',
+    );
+    expect(requestFromTooltipContent).toBeInTheDocument();
+  });
+
+  it('displays the advanced transaction details section', async () => {
+    const testUser = userEvent.setup();
+
+    const mockedMetaMaskState =
+      getMetaMaskStateWithUnapprovedApproveTransaction({
+        showAdvanceDetails: true,
+      });
+
+    await act(async () => {
+      await integrationTestRender({
+        preloadedState: mockedMetaMaskState,
+        backgroundConnection: backgroundConnectionMocked,
+      });
+    });
+
+    const approveDetails = screen.getByTestId('confirmation__approve-details');
+    expect(approveDetails).toBeInTheDocument();
+
+    const approveDetailsRecipient = screen.getByTestId(
+      'transaction-details-recipient-row',
+    );
+    expect(approveDetails).toContainElement(approveDetailsRecipient);
+    expect(approveDetailsRecipient).toHaveTextContent('Interacting with');
+    expect(approveDetailsRecipient).toHaveTextContent('0x07614...3ad68');
+
+    const approveDetailsRecipientTooltip = screen.getByTestId(
+      'transaction-details-recipient-row-tooltip',
+    );
+    expect(approveDetailsRecipient).toContainElement(
+      approveDetailsRecipientTooltip,
+    );
+    await testUser.hover(approveDetailsRecipientTooltip);
+    const recipientTooltipContent = await screen.findByText(
+      "This is the contract you're interacting with. Protect yourself from scammers by verifying the details.",
+    );
+    expect(recipientTooltipContent).toBeInTheDocument();
+
+    const approveMethodData = await screen.findByTestId(
+      'transaction-details-method-data-row',
+    );
+    expect(approveDetails).toContainElement(approveMethodData);
+    expect(approveMethodData).toHaveTextContent('Method');
+    expect(approveMethodData).toHaveTextContent('Approve');
+    const approveMethodDataTooltip = screen.getByTestId(
+      'transaction-details-method-data-row-tooltip',
+    );
+    expect(approveMethodData).toContainElement(approveMethodDataTooltip);
+    await testUser.hover(approveMethodDataTooltip);
+    const approveMethodDataTooltipContent = await screen.findByText(
+      'Function executed based on decoded input data.',
+    );
+    expect(approveMethodDataTooltipContent).toBeInTheDocument();
+
+    const approveDetailsNonce = screen.getByTestId(
+      'advanced-details-nonce-section',
+    );
+    expect(approveDetailsNonce).toBeInTheDocument();
+
+    const dataSection = screen.getByTestId('advanced-details-data-section');
+    expect(dataSection).toBeInTheDocument();
+
+    const dataSectionFunction = screen.getByTestId(
+      'advanced-details-data-function',
+    );
+    expect(dataSection).toContainElement(dataSectionFunction);
+    expect(dataSectionFunction).toHaveTextContent('Function');
+    expect(dataSectionFunction).toHaveTextContent('Approve');
+
+    const approveDataParams1 = screen.getByTestId(
+      'advanced-details-data-param-0',
+    );
+    expect(dataSection).toContainElement(approveDataParams1);
+    expect(approveDataParams1).toHaveTextContent('Param #1');
+    expect(approveDataParams1).toHaveTextContent('0x2e0D7...5d09B');
+
+    const approveDataParams2 = screen.getByTestId(
+      'advanced-details-data-param-1',
+    );
+    expect(dataSection).toContainElement(approveDataParams2);
+    expect(approveDataParams2).toHaveTextContent('Param #2');
+    expect(approveDataParams2).toHaveTextContent('1');
   });
 });
