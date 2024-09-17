06c143adef05b6a67df00ae5ab31a22e3f1e0b12
seaona <mariona@gmx.es>
2024-09-17 08:13:22 +0200

    commit 1

--- test/e2e/tests/signature/signature-request.spec.js
+++ test/e2e/tests/signature/signature-request.spec.js
@@ -81,6 +81,7 @@ describe('Sign Typed Data Signature Request', function () {
           await tempToggleSettingRedesignedConfirmations(driver);
 
           await openDapp(driver);
+          await driver.delay(200);
 
           // creates a sign typed data signature request
           await driver.clickElement(data.buttonId);
