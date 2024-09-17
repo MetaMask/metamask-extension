ebc5a8a4a34c3b4258b7da0abe230cfd202c12cf
Matthew Walsh <matthew.walsh@consensys.net>
2024-09-17 11:02:44 +0100

    Fix storybook

---
+++ ui/__mocks__/ethereumjs-util.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line import/no-extraneous-dependencies, node/no-extraneous-require
+const { toBuffer, toUtf8 } = require('ethereumjs-util');
+
+module.exports = {
+  toBuffer,
+  toUtf8,
+};
