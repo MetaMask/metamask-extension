3fb71715cebf96ec1d9a5f0c28b2d54249ed560a
Harika <153644847+hjetpoluru@users.noreply.github.com>
2024-09-17 15:22:34 +0000

    Added addition args in chrome driver

--- test/e2e/webdriver/chrome.js
+++ test/e2e/webdriver/chrome.js
@@ -34,6 +34,7 @@ class ChromeDriver {
       '--disable-features=OptimizationGuideModelDownloading,OptimizationHintsFetching,OptimizationTargetPrediction,OptimizationHints,NetworkTimeServiceQuerying', // Stop chrome from calling home so much (auto-downloads of AI models; time sync)
       '--disable-component-update', // Stop chrome from calling home so much (auto-update)
       '--disable-dev-shm-usage',
+      '--verbose'
     ];
 
     if (process.env.MULTIPROVIDER) {
