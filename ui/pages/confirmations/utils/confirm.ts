b906eea43dccb115f3ff32487c19b9a2ea715120
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2024-09-23 16:36:07 +0100

    fix: Hide contract interaction redesign

--- ui/pages/confirmations/utils/confirm.ts
+++ ui/pages/confirmations/utils/confirm.ts
@@ -16,7 +16,7 @@ export const REDESIGN_APPROVAL_TYPES = [
   ApprovalType.PersonalSign,
 ];
 
-export const REDESIGN_TRANSACTION_TYPES = [TransactionType.contractInteraction];
+export const REDESIGN_TRANSACTION_TYPES = [];
 
 const SIGNATURE_APPROVAL_TYPES = [
   ApprovalType.PersonalSign,
