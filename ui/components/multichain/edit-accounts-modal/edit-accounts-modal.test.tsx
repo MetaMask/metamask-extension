171d52e8f9b48c0df250176e98f10d2d733c383c
Jiexi Luan <jiexiluan@gmail.com>
2024-09-23 16:33:06 -0700

    Add edit modal scenarios

--- ui/components/multichain/edit-accounts-modal/edit-accounts-modal.test.tsx
+++ ui/components/multichain/edit-accounts-modal/edit-accounts-modal.test.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import { fireEvent } from '@testing-library/react';
 import { renderWithProvider } from '../../../../test/jest/rendering';
 import mockState from '../../../../test/data/mock-state.json';
 import configureStore from '../../../store/store';
+import { MergedInternalAccount } from '../../../selectors/selectors.types';
 import { EditAccountsModal } from '.';
 
 const render = (
   props: {
+    onSubmit: (addresses: string[]) => void;
     onClose: () => void;
-    activeTabOrigin: string;
   } = {
-    onClose: () => jest.fn(),
-    activeTabOrigin: 'https://test.dapp',
+    onSubmit: jest.fn(),
+    onClose: jest.fn(),
   },
   state = {},
 ) => {
@@ -33,13 +35,16 @@ const render = (
       origin: 'https://test.dapp',
     },
   });
+
+  const accounts = Object.values(
+    mockState.metamask.internalAccounts.accounts,
+  ) as unknown as MergedInternalAccount[];
+
   return renderWithProvider(
     <EditAccountsModal
-      onSubmit={function (): void {
-        throw new Error('Function not implemented.');
-      }}
-      accounts={[]}
-      defaultSelectedAccountAddresses={[]}
+      accounts={accounts}
+      defaultSelectedAccountAddresses={[accounts[0].address]}
+      activeTabOrigin={'https://test.dapp'}
       {...props}
     />,
     store,
@@ -55,4 +60,33 @@ describe('EditAccountsModal', () => {
     const { getByLabelText } = render();
     expect(getByLabelText('Select all')).toBeInTheDocument();
   });
+
+  it('calls onSubmit with the selected account addresses when the connect button is clicked', async () => {
+    const onSubmit = jest.fn();
+    const { getByTestId } = render({
+      onSubmit,
+      onClose: jest.fn(),
+    });
+    fireEvent.click(getByTestId('connect-more-accounts-button'));
+    expect(onSubmit).toHaveBeenCalledWith([
+      '0x0dcd5d886577d5081b0c52e242ef29e70be3e7bc',
+    ]);
+  });
+
+  it('calls onClose when the connect button is clicked', async () => {
+    const onClose = jest.fn();
+    const { getByTestId } = render({
+      onSubmit: jest.fn(),
+      onClose,
+    });
+    fireEvent.click(getByTestId('connect-more-accounts-button'));
+    expect(onClose).toHaveBeenCalledWith();
+  });
+
+  it('shows the disconnect text button when nothing is selected', () => {
+    const { getByLabelText, getByTestId } = render();
+    fireEvent.click(getByLabelText('Select all'));
+    fireEvent.click(getByLabelText('Select all'));
+    expect(getByTestId('disconnect-accounts-button')).toHaveTextContent('Disconnect')
+  })
 });
--- ui/components/multichain/edit-networks-modal/edit-networks-modal.test.js
+++ ui/components/multichain/edit-networks-modal/edit-networks-modal.test.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { fireEvent } from '@testing-library/react';
 import { renderWithProvider } from '../../../../test/jest/rendering';
 import mockState from '../../../../test/data/mock-state.json';
 import configureStore from '../../../store/store';
+import { TEST_NETWORKS } from '../../../../shared/constants/network';
 import { EditNetworksModal } from '.';
 
 const render = (
   props = {
-    onClose: () => jest.fn(),
-    activeTabOrigin: 'https://test.dapp',
+    onSubmit: jest.fn(),
+    onClose: jest.fn(),
   },
   state = {},
 ) => {
@@ -30,14 +32,20 @@ const render = (
       origin: 'https://test.dapp',
     },
   });
+
+  const networks = Object.values(
+    mockState.metamask.networkConfigurationsByChainId,
+  ).map(({ chainId, name }) => ({ chainId, name }));
+
   return renderWithProvider(
     <EditNetworksModal
       onSubmit={() => {
         throw new Error('Function not implemented.');
       }}
-      testNetworks={[]}
-      nonTestNetworks={[]}
-      defaultSelectedChainIds={[]}
+      testNetworks={[networks[0]]}
+      nonTestNetworks={[networks[1]]}
+      defaultSelectedChainIds={[networks[0].chainId]}
+      activeTabOrigin="https://test.dapp"
       {...props}
     />,
     store,
@@ -53,4 +61,31 @@ describe('EditNetworksModal', () => {
     const { getByLabelText } = render();
     expect(getByLabelText('Select all')).toBeInTheDocument();
   });
+
+  it('calls onSubmit with the selected chain IDs when the connect button is clicked', async () => {
+    const onSubmit = jest.fn();
+    const { getByTestId } = render({
+      onSubmit,
+      onClose: jest.fn(),
+    });
+    fireEvent.click(getByTestId('connect-more-chains-button'));
+    expect(onSubmit).toHaveBeenCalledWith(['0x1']);
+  });
+
+  it('calls onClose when the connect button is clicked', async () => {
+    const onClose = jest.fn();
+    const { getByTestId } = render({
+      onSubmit: jest.fn(),
+      onClose,
+    });
+    fireEvent.click(getByTestId('connect-more-chains-button'));
+    expect(onClose).toHaveBeenCalledWith();
+  });
+
+  it('shows the disconnect text button when nothing is selected', () => {
+    const { getByLabelText, getByTestId } = render();
+    fireEvent.click(getByLabelText('Select all'));
+    fireEvent.click(getByLabelText('Select all'));
+    expect(getByTestId('disconnect-chains-button')).toHaveTextContent('Disconnect')
+  })
 });
