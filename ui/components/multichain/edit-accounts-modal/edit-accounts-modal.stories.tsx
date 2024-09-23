b9b9a8f8fcb861c9ce23c19b64bfec0fb6efa5ab
NidhiKJha <menidhikjha@gmail.com>
2024-09-23 14:02:05 +0100

    fixed story for accounts

--- ui/components/multichain/edit-accounts-modal/edit-accounts-modal.stories.tsx
+++ ui/components/multichain/edit-accounts-modal/edit-accounts-modal.stories.tsx
@@ -12,6 +12,7 @@ export default {
   argTypes: {
     onClose: { action: 'onClose' },
     activeTabOrigin: { control: 'text' },
+    accounts: { control: 'array' },
   },
   args: {
     onClose: () => undefined,
@@ -20,6 +21,34 @@ export default {
     approvedAccounts: [],
     activeTabOrigin: 'https://test.dapp',
     currentTabHasNoAccounts: false,
+    defaultSelectedAccountAddresses: [
+      '0x860092756917d3e069926ba130099375eeeb9440',
+    ],
+    accounts: [
+      {
+        id: '689821df-0e8f-4093-bbbb-b95cf0fa79cb',
+        address: '0x860092756917d3e069926ba130099375eeeb9440',
+        options: {},
+        methods: [
+          'personal_sign',
+          'eth_sign',
+          'eth_signTransaction',
+          'eth_signTypedData_v1',
+          'eth_signTypedData_v3',
+          'eth_signTypedData_v4',
+        ],
+        type: 'eip155:eoa',
+        metadata: {
+          name: 'Account 1',
+          importTime: 1726046726882,
+          keyring: {
+            type: 'HD Key Tree',
+          },
+          lastSelected: 1726046726882,
+        },
+        balance: '0x00',
+      },
+    ],
   },
 };
 
