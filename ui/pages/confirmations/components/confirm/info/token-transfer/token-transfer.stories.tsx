c7328695588abeea386bd1caf4683c413e9d09a2
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2024-09-23 16:21:52 +0100

    feat: Custom header for wallet initiated confirmations

---
+++ ui/pages/confirmations/components/confirm/info/token-transfer/token-transfer.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { getMockTokenTransferConfirmState } from '../../../../../../../test/data/confirmations/helper';
+import configureStore from '../../../../../../store/store';
+import { ConfirmContextProvider } from '../../../../context/confirm';
+import TokenTransferInfo from './token-transfer';
+
+const store = configureStore(getMockTokenTransferConfirmState());
+
+const Story = {
+  title: 'Components/App/Confirm/info/TokenTransferInfo',
+  component: TokenTransferInfo,
+  decorators: [
+    (story: () => any) => (
+      <Provider store={store}>
+        <ConfirmContextProvider>{story()}</ConfirmContextProvider>
+      </Provider>
+    ),
+  ],
+};
+
+export default Story;
+
+export const DefaultStory = () => <TokenTransferInfo />;
+
+DefaultStory.storyName = 'Default';
