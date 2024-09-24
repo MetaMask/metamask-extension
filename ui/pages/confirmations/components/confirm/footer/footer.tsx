82fc376ec8d3c903f91603749aec987266c45821
Vinicius Stevam <vinicius.stevam@consensys.io>
2024-09-24 14:44:22 +0100

    fix merge conflict

--- ui/pages/confirmations/components/confirm/footer/footer.tsx
+++ ui/pages/confirmations/components/confirm/footer/footer.tsx
@@ -137,11 +137,6 @@ const ConfirmButton = ({
             hasDangerBlockingAlerts,
             disabled,
           )}
-          disabled={getButtonDisabledState(
-            hasUnconfirmedDangerAlerts,
-            hasDangerBlockingAlerts,
-            disabled,
-          )}
           onClick={handleOpenConfirmModal}
           size={ButtonSize.Lg}
           startIconName={
