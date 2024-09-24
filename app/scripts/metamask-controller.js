b77ff2f5e04040aa8892e636500ddceae7852ed7
Hassan Malik <hbmalik88@gmail.com>
2024-09-24 16:43:57 -0400

    Add action for interface controller

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -1458,6 +1458,7 @@ export default class MetamaskController extends EventEmitter {
           `${this.phishingController.name}:testOrigin`,
           `${this.approvalController.name}:hasRequest`,
           `${this.approvalController.name}:acceptRequest`,
+          `${this.snapController.name}:get`,
         ],
       });
 
