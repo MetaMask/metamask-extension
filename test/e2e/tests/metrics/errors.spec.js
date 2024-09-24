9c891f124afea8a74eda96bb8e234dc9954268e5
Nicholas Gambino <nicholas.gambino@consensys.net>
2024-09-23 19:50:35 -0700

    Update snapshots

--- test/e2e/tests/metrics/errors.spec.js
+++ test/e2e/tests/metrics/errors.spec.js
@@ -410,7 +410,7 @@ describe('Sentry errors', function () {
 
     // todo: reenable this test https://github.com/MetaMask/metamask-extension/issues/21807
     // eslint-disable-next-line mocha/no-skipped-tests
-    it.skip('should capture migration log breadcrumbs when there is an invariant state error in a migration', async function () {
+    it('should capture migration log breadcrumbs when there is an invariant state error in a migration', async function () {
       await withFixtures(
         {
           fixtures: {
