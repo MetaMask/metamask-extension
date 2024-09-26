c6fd0221bc41057cbff9f9229a8dff9fcce96e84
HJetpoluru <harika.jetpoluru@consensys.net>
2024-09-25 21:21:21 -0400

    Testing on ci

--- test/e2e/run-all.js
+++ test/e2e/run-all.js
@@ -218,6 +218,7 @@ async function main() {
       ...(await getTestPathsForTestDir(testDir)),
       ...filteredFlaskAndMainTests,
     ];
+    testPaths = testPaths.filter((testPath) => testPath.match('dapp-viewed'));
   }
 
   const runE2eTestPath = path.join(__dirname, 'run-e2e-test.js');
