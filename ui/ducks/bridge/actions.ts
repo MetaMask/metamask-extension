7410c0e55ef3c8ed1031275b2379d536529b851c
IF <139582705+infiniteflower@users.noreply.github.com>
2024-09-27 14:38:32 -0400

    fix: allowance not being a big number on the ui side

--- ui/ducks/bridge/actions.ts
+++ ui/ducks/bridge/actions.ts
@@ -242,10 +242,12 @@ export const signBridgeTransaction = (
     }
 
     const handleEthUsdtAllowanceReset = async (hexChainId: Hex) => {
-      const allowance = await getBridgeERC20Allowance(
-        ETH_USDT_ADDRESS,
-        quoteMeta.approval.from,
-        hexChainId,
+      const allowance = BigNumber.from(
+        await getBridgeERC20Allowance(
+          ETH_USDT_ADDRESS,
+          quoteMeta.approval.from,
+          hexChainId,
+        ),
       );
 
       // quote.srcTokenAmount is actually after the fees
@@ -268,7 +270,7 @@ export const signBridgeTransaction = (
         });
 
         await addTransactionAndWaitForPublish(txParams, {
-          requireApproval: false,
+          // requireApproval: false,
           // @ts-expect-error Need TransactionController v37+, TODO add this type
           type: 'bridgeApproval', // TransactionType.bridgeApproval,
 
