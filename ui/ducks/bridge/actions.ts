94ef4e0a1fec044486874dc191e1dcfeafdeac13
IF <139582705+infiniteflower@users.noreply.github.com>
2024-09-27 16:42:24 -0400

    chore: block tx submission if not bridge chain

--- ui/ducks/bridge/actions.ts
+++ ui/ducks/bridge/actions.ts
@@ -32,6 +32,7 @@ import {
 import {
   SwapsEthToken,
   checkNetworkAndAccountSupports1559,
+  getIsBridgeChain,
   getIsBridgeEnabled,
   getNetworkConfigurations,
   getSelectedNetworkClientId,
@@ -152,8 +153,9 @@ export const signBridgeTransaction = (
   ) => {
     const state = getState();
     const isBridgeEnabled = getIsBridgeEnabled(state);
+    const isBridgeChain = getIsBridgeChain(state);
 
-    if (!isBridgeEnabled) {
+    if (!(isBridgeEnabled && isBridgeChain)) {
       // TODO do we want to do something here?
       return;
     }
