1c02d9f7c4be06c33bbbff382391f4bd53c5af60
dan437 <80175477+dan437@users.noreply.github.com>
2024-10-01 17:23:32 +0200

    Fix a lint issue

--- ui/store/actions.ts
+++ ui/store/actions.ts
@@ -4764,7 +4764,7 @@ export function signAndSendSmartTransaction({
         [
           {
             signedTransactions,
-            // The "signedCanceledTransactions" parameter is still expected by the STX controller but is no longer used. 
+            // The "signedCanceledTransactions" parameter is still expected by the STX controller but is no longer used.
             // So we are passing an empty array. The parameter may be deprecated in a future update.
             signedCanceledTransactions: [],
             txParams: unsignedTransaction,
