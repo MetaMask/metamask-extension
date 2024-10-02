835a39709d21d0a748327f6956c693bbea0356f1
IF <139582705+infiniteflower@users.noreply.github.com>
2024-10-02 16:34:44 -0400

    chore: add skeleton for bridge action tests

---
+++ ui/ducks/bridge/actions.test.ts
@@ -0,0 +1,10 @@
+describe('bridge/actions', () => {
+  describe('signBridgeTransaction', () => {
+    it.todo('executes bridge transaction');
+    it.todo('executes approval transaction if it exists');
+    it.todo('adds source token if it does not exist');
+    it.todo('adds dest token if it does not exist');
+    it.todo('adds network if it does not exist');
+    it.todo('routes to activity tab');
+  });
+});
