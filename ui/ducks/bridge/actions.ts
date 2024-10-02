d40cd6a39b0b5bbab169d8f4b2528d3a5d093b4a
IF <139582705+infiniteflower@users.noreply.github.com>
2024-10-02 16:06:49 -0400

    fix: wrong types

--- ui/ducks/bridge/actions.ts
+++ ui/ducks/bridge/actions.ts
@@ -212,8 +212,8 @@ export const signBridgeTransaction = (
       maxPriorityFeePerGas,
     }: {
       hexChainId: Hex;
-      maxFeePerGas: string;
-      maxPriorityFeePerGas: string;
+      maxFeePerGas: string | undefined;
+      maxPriorityFeePerGas: string | undefined;
     }) => {
       const allowance = new BigNumber(
         await getBridgeERC20Allowance(
