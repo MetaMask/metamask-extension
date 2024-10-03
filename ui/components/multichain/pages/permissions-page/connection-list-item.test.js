f3cec2594065e0eb4ead8907d07ad23af88538ea
NidhiKJha <menidhikjha@gmail.com>
2024-10-03 16:50:17 +0100

    removed old unit test for permission connect and updated connection list test

--- ui/components/multichain/pages/permissions-page/connection-list-item.test.js
+++ ui/components/multichain/pages/permissions-page/connection-list-item.test.js
@@ -37,6 +37,10 @@ describe('ConnectionListItem', () => {
       iconUrl: 'https://metamask.github.io/test-dapp/metamask-fox.svg',
       networkIconUrl: 'https://metamask.github.io/test-dapp/metamask-fox.svg',
       networkName: 'Test Dapp Network',
+      addresses: [
+        '0xaaaF07C80ce267F3132cE7e6048B66E6E669365B',
+        '0xbbbD671F1Fcc94bCF0ebC6Ec4790Da35E8d5e1E1',
+      ],
     };
 
     const { getByText, getByTestId } = renderWithProvider(
