d9fe6b2c7c4afeb5065d4a1b0455b8d58261f76e
HJetpoluru <harika.jetpoluru@consensys.net>
2024-10-03 08:07:38 -0400

    Added integration test

--- test/integration/network/switch-network.test.tsx
+++ test/integration/network/switch-network.test.tsx
@@ -1,36 +1,151 @@
-import { fireEvent, waitFor } from '@testing-library/react';
+import mockMetaMaskState from '../data/integration-init-state.json';
 import { integrationTestRender } from '../../lib/render-helpers';
 import * as backgroundConnection from '../../../ui/store/background-connection';
-import mockMetaMaskState from '../data/integration-init-state.json';
+import {
+  createMockImplementation,
+  mockGetChains,
+  mockNetworkBaseLlama,
+} from '../helpers';
+import { act, fireEvent, screen, waitFor } from '@testing-library/react';
 
 jest.mock('../../../ui/store/background-connection', () => ({
   ...jest.requireActual('../../../ui/store/background-connection'),
   submitRequestToBackground: jest.fn(),
+  callBackgroundMethod: jest.fn(),
 }));
 
 const mockedBackgroundConnection = jest.mocked(backgroundConnection);
+const backgroundConnectionMocked = {
+  onNotification: jest.fn(),
+};
 
-describe.skip('Switch Network', () => {
+const setupSubmitRequestToBackgroundMocks = (
+  mockRequests?: Record<string, unknown>,
+) => {
+  mockedBackgroundConnection.submitRequestToBackground.mockImplementation(
+    createMockImplementation({
+      ...mockRequests,
+    }),
+  );
+};
+
+describe('Switch network - ', () => {
   beforeEach(() => {
     jest.resetAllMocks();
+    mockGetChains();
+    setupSubmitRequestToBackgroundMocks();
   });
 
-  it('should switch to a new network when a different network is selected', async () => {
-    const { getByTestId, queryByTestId } = await integrationTestRender({
-        preloadedState: mockMetaMaskState,
-      backgroundConnection: backgroundConnection,
+  it('validate the list of networks', async () => {
+    process.env.IN_TEST = '';
+    const mockedMetamaskState = {
+      ...mockMetaMaskState,
+      preferences: {
+        ...mockMetaMaskState.preferences,
+        showMultiRpcModal: true,
+      },
+    };
+
+    const mockedRequests = {
+      getState: mockedMetamaskState,
+      getTokenStandardAndDetails: { decimal: '10' },
+    };
+
+    setupSubmitRequestToBackgroundMocks(mockedRequests);
+
+    await act(async () => {
+      await integrationTestRender({
+        preloadedState: mockedMetamaskState,
+        backgroundConnection: backgroundConnectionMocked,
+      });
     });
 
-    // click to open the network list
-    fireEvent.click(getByTestId('network-dropdown'));
-
-    // click test id 'network-option-3' for the new network
-    fireEvent.click(getByTestId('network-option-3'));
-
-    // Wait for the network to change
-    await waitFor(() => expect(mockedBackgroundConnection.submitRequestToBackground).toHaveBeenCalledWith({
-      method: 'metamask_switchEthereumChain',
-      params: [{ chainId: '3' }], // new network
-    }));
+    const multiRpcModal = screen.getByTestId('multi-rpc-edit-modal');
+    expect(multiRpcModal).toBeInTheDocument();
+    expect(multiRpcModal).toHaveTextContent('Network RPCs Updated');
+    expect(multiRpcModal).toHaveTextContent(
+      'We now support multiple RPCs for a single network. Your most recent RPC has been selected as the default one to resolve conflicting information.',
+    );
+    expect(screen.getByTestId('mm-modal-confirm')).toBeInTheDocument();
   });
+
+  it('edits the name of an RPC network', async () => {
+    const mockedRequests = {
+      getState: mockMetaMaskState,
+      getTokenStandardAndDetails: { decimal: '10' },
+    };
+
+    setupSubmitRequestToBackgroundMocks(mockedRequests);
+
+    await act(async () => {
+      await integrationTestRender({
+        preloadedState: mockMetaMaskState,
+        backgroundConnection: backgroundConnectionMocked,
+      });
+    });
+
+    fireEvent.click(screen.getByTestId('network-display'));
+    expect(
+      screen.queryByTestId('multichain-network-list-menu'),
+    ).toBeInTheDocument();
+
+    fireEvent.click(
+      screen.getByTestId('network-list-item-options-button-0x2105'),
+    );
+    fireEvent.click(screen.getByTestId('network-list-item-options-edit'));
+
+    expect(screen.queryByTestId('network-form-name-input')).toBeInTheDocument();
+
+    const networkNameInput = screen.getByTestId('network-form-network-name');
+    expect(networkNameInput).toHaveValue('Base Mainnet');
+    fireEvent.input(networkNameInput, { target: { value: 'New Base Name' } });
+    fireEvent.click(screen.getByTestId('network-form-save'));
+
+    let updateNetworkCall;
+
+    await waitFor(() => {
+      updateNetworkCall =
+        mockedBackgroundConnection.submitRequestToBackground.mock.calls?.find(
+          (call) => call[0] === 'updateNetwork',
+        );
+      expect(updateNetworkCall?.[0]).toBe('updateNetwork');
+    });
+
+    expect(updateNetworkCall?.[1]).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          chainId: '0x2105',
+          name: 'New Base Name',
+          nativeCurrency: 'ETH',
+          rpcEndpoints: expect.arrayContaining([
+            expect.objectContaining({
+              networkClientId: '225ae6de-deda-4078-9f6c-4a4fb758ae00',
+              url: 'https://base.meowrpc.com',
+              type: 'custom',
+              name: 'Base Meow',
+            }),
+            expect.objectContaining({
+              networkClientId: '78d85c4e-eeb0-4b81-ae5b-52d0fca7e5e3',
+              url: 'https://base-pokt.nodies.app',
+              type: 'custom',
+              name: 'Base',
+            }),
+            expect.objectContaining({
+              networkClientId: 'b3742c56-a2be-48ef-8811-6e9d265b420b',
+              url: 'https://base.llamarpc.com',
+              type: 'custom',
+              name: 'Base LlamaNodes',
+            }),
+          ]),
+          defaultRpcEndpointIndex: 2,
+          blockExplorerUrls: expect.arrayContaining(['https://basescan.org']),
+          defaultBlockExplorerUrlIndex: 0,
+        }),
+        expect.objectContaining({
+          replacementSelectedRpcEndpointIndex: 2,
+        }),
+      ]),
+    );
+  });
+
 });
