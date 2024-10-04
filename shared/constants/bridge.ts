16e9dfeb8b88d0eaa235cfb30f388f4e681cace9
IF <139582705+infiniteflower@users.noreply.github.com>
2024-10-04 14:11:05 -0400

    chore: remove unneeded comments

--- shared/constants/bridge.ts
+++ shared/constants/bridge.ts
@@ -21,9 +21,9 @@ export const BRIDGE_API_BASE_URL = process.env.BRIDGE_USE_DEV_APIS
 
 export const BRIDGE_CLIENT_ID = 'extension';
 
-export const ETH_USDT_ADDRESS = '0xdac17f958d2ee523a2206206994597c13d831ec7'; // ETH:USDT's address
+export const ETH_USDT_ADDRESS = '0xdac17f958d2ee523a2206206994597c13d831ec7';
 export const METABRIDGE_ETHEREUM_ADDRESS =
-  '0x0439e60F02a8900a951603950d8D4527f400C3f1'; // the address the user approves to spend bridgeable tokens
+  '0x0439e60F02a8900a951603950d8D4527f400C3f1';
 
 export enum FeeType {
   METABRIDGE = 'metabridge',
