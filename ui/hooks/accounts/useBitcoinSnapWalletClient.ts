ad76645e53cae22ff307eda4ea83f37b81d4bd2a
Charly Chevalier <charly.chevalier@consensys.net>
2024-10-04 16:52:50 +0200

    fix(btc): force balance fetch after account creation

--- ui/hooks/accounts/useBitcoinSnapWalletClient.ts
+++ ui/hooks/accounts/useBitcoinSnapWalletClient.ts
@@ -2,7 +2,10 @@ import { KeyringClient, Sender } from '@metamask/keyring-api';
 import { HandlerType } from '@metamask/snaps-utils';
 import { CaipChainId, Json, JsonRpcRequest } from '@metamask/utils';
 import { useMemo } from 'react';
-import { handleSnapRequest } from '../../store/actions';
+import {
+  handleSnapRequest,
+  multichainUpdateBalance,
+} from '../../store/actions';
 import { BITCOIN_WALLET_SNAP_ID } from '../../../shared/lib/accounts/bitcoin-wallet-snap';
 
 export class BitcoinWalletSnapSender implements Sender {
@@ -28,9 +31,15 @@ export class BitcoinWalletSnapClient {
 
   async createAccount(scope: CaipChainId) {
     // This will trigger the Snap account creation flow (+ account renaming)
-    return await this.#client.createAccount({
+    const account = await this.#client.createAccount({
       scope,
     });
+
+    // NOTE: The account's balance is going to be tracked automatically on when the new account
+    // will be added to the Snap bridge keyring (see `BalancesController:#handleOnAccountAdded`).
+    // However, the balance won't be fetched right away. To workaround this, we trigger the
+    // fetch explicitly here (since we are already in a `async` call) and wait for it to be updated!
+    await multichainUpdateBalance(account.id);
   }
 }
 
