eca69876b34630e9ba76cde7eee69876fcbe1756
Nicholas Gambino <nicholas.gambino@consensys.net>
2024-10-07 12:10:30 -0700

    chore: Add unit tests for tokenSortConfig migration file

---
+++ app/scripts/migrations/130.test.ts
@@ -0,0 +1,89 @@
+import { migrate, version } from './128';
+const oldVersion = 127;
+describe(`migration #${version}`, () => {
+  it('updates the version metadata', async () => {
+    const oldStorage = {
+      meta: { version: oldVersion },
+      data: {},
+    };
+    const newStorage = await migrate(oldStorage);
+    expect(newStorage.meta).toStrictEqual({ version });
+  });
+  describe(`migration #${version}`, () => {
+    it('updates the preferences with a default tokenSortConfig', async () => {
+      const oldStorage = {
+        meta: { version: oldVersion },
+        data: {
+          PreferencesController: {
+            preferences: {},
+          },
+        },
+      };
+      const expectedData = {
+        PreferencesController: {
+          preferences: {
+            tokenSortConfig: {
+              key: 'tokenFiatAmount',
+              order: 'dsc',
+              sortCallback: 'stringNumeric',
+            },
+          },
+        },
+      };
+      const newStorage = await migrate(oldStorage);
+
+      expect(newStorage.data).toStrictEqual(expectedData);
+    });
+
+    it('does nothing if the preferences already has a tokenSortConfig', async () => {
+      const oldStorage = {
+        meta: { version: oldVersion },
+        data: {
+          PreferencesController: {
+            preferences: {
+              tokenSortConfig: {
+                key: 'fooKey',
+                order: 'foo',
+                sortCallback: 'fooCallback',
+              },
+            },
+          },
+        },
+      };
+
+      const newStorage = await migrate(oldStorage);
+
+      expect(newStorage.data).toStrictEqual(oldStorage.data);
+    });
+
+    it('does nothing to other preferences if they exist without a tokenSortConfig', async () => {
+      const oldStorage = {
+        meta: { version: oldVersion },
+        data: {
+          PreferencesController: {
+            preferences: {
+              existingPreference: true,
+            },
+          },
+        },
+      };
+
+      const expectedData = {
+        PreferencesController: {
+          preferences: {
+            existingPreference: true,
+            tokenSortConfig: {
+              key: 'tokenFiatAmount',
+              order: 'dsc',
+              sortCallback: 'stringNumeric',
+            },
+          },
+        },
+      };
+
+      const newStorage = await migrate(oldStorage);
+
+      expect(newStorage.data).toStrictEqual(expectedData);
+    });
+  });
+});
