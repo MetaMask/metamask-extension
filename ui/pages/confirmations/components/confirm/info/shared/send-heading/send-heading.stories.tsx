001727c435c48ede651c216f98b82dfb60960c39
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2024-10-07 15:49:09 +0100

    fix storybook import

--- ui/pages/confirmations/components/confirm/info/shared/send-heading/send-heading.stories.tsx
+++ ui/pages/confirmations/components/confirm/info/shared/send-heading/send-heading.stories.tsx
@@ -1,29 +1,18 @@
 import { Meta } from '@storybook/react';
 import React from 'react';
 import { Provider } from 'react-redux';
-import mockState from '../../../../../../../../test/data/mock-state.json';
+import { getMockTokenTransferConfirmState } from '../../../../../../../../test/data/confirmations/helper';
 import configureStore from '../../../../../../../store/store';
 import SendHeading from './send-heading';
 
-function getStore() {
-  return configureStore(mockState);
-}
+const store = configureStore(getMockTokenTransferConfirmState({}));
 
 const Story = {
   title: 'Components/App/Confirm/info/SendHeading',
   component: SendHeading,
   decorators: [
     (story: () => Meta<typeof SendHeading>) => (
-      <Provider store={getStore()}>
-        <div
-          style={{
-            backgroundColor: 'var(--color-background-alternative)',
-            padding: 30,
-          }}
-        >
-          {story()}
-        </div>
-      </Provider>
+      <Provider store={store}>{story()}</Provider>
     ),
   ],
 };
