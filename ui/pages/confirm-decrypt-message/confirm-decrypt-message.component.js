1f4549be8653b9f05ebff05cb5625e4743382791
OGPoyraz <omergoktugpoyraz@gmail.com>
2024-10-08 16:01:05 +0200

    Fix suggestions

--- ui/pages/confirm-decrypt-message/confirm-decrypt-message.component.js
+++ ui/pages/confirm-decrypt-message/confirm-decrypt-message.component.js
@@ -1,15 +1,17 @@
 /* eslint-disable react/prop-types */
-import React, { useState, useContext } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useContext, forwardRef } from 'react';
 import copyToClipboard from 'copy-to-clipboard';
 import classnames from 'classnames';
 import log from 'loglevel';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { cloneDeep } from 'lodash';
 
 import AccountListItem from '../../components/app/account-list-item';
-import Identicon from '../../components/ui/identicon';
 import Tooltip from '../../components/ui/tooltip';
 import { PageContainerFooter } from '../../components/ui/page-container';
-
+import { getMostRecentOverviewPage } from '../../ducks/history/history';
+import { getNativeCurrency } from '../../ducks/metamask/metamask';
 import { MetaMetricsEventCategory } from '../../../shared/constants/metametrics';
 import { SECOND } from '../../../shared/constants/time';
 import { Numeric } from '../../../shared/modules/Numeric';
@@ -20,8 +22,6 @@ import {
   IconName,
   Icon,
 } from '../../components/component-library';
-import { formatCurrency } from '../../helpers/utils/confirm-tx.util';
-import { getValueFromWeiHex } from '../../../shared/modules/conversion.utils';
 import { COPY_OPTIONS } from '../../../shared/constants/copy';
 import { useI18nContext } from '../../hooks/useI18nContext';
 import { useScrollRequired } from '../../hooks/useScrollRequired';
@@ -32,6 +32,16 @@ import {
   Display,
   IconColor,
 } from '../../helpers/constants/design-system';
+import { clearConfirmTransaction } from '../../ducks/confirm-transaction/confirm-transaction.duck';
+import {
+  decryptMsg,
+  cancelDecryptMsg,
+  decryptMsgInline,
+} from '../../store/actions';
+import {
+  getTargetAccountWithSendEtherInfo,
+  unconfirmedTransactionsListSelector,
+} from '../../selectors';
 
 const Header = () => {
   const t = useI18nContext();
@@ -48,32 +58,18 @@ const Header = () => {
   );
 };
 
-const Account = ({
-  conversionRate,
-  currentCurrency,
-  fromAccount,
-  nativeCurrency,
-  requesterAddress,
-}) => {
+const Account = ({ fromAccount, nativeCurrency }) => {
   const t = useI18nContext();
 
-  const nativeCurrencyBalance = conversionRate
-    ? formatCurrency(
-        getValueFromWeiHex({
-          value: fromAccount.balance,
-          fromCurrency: nativeCurrency,
-          toCurrency: currentCurrency,
-          conversionRate,
-          numberOfDecimals: 6,
-          toDenomination: EtherDenomination.ETH,
-        }),
-        currentCurrency,
-      )
-    : new Numeric(fromAccount.balance, 16, EtherDenomination.WEI)
-        .toDenomination(EtherDenomination.ETH)
-        .round(6)
-        .toBase(10)
-        .toString();
+  const nativeCurrencyBalance = new Numeric(
+    fromAccount.balance,
+    16,
+    EtherDenomination.WEI,
+  )
+    .toDenomination(EtherDenomination.ETH)
+    .round(6)
+    .toBase(10)
+    .toString();
 
   return (
     <div className="request-decrypt-message__account-info">
@@ -85,17 +81,12 @@ const Account = ({
           <AccountListItem account={fromAccount} />
         </div>
       </div>
-      <div className="request-decrypt-message__request-icon">
-        <Identicon diameter={40} address={requesterAddress} />
-      </div>
       <div className="request-decrypt-message__balance">
         <div className="request-decrypt-message__balance-text">
           {`${t('balance')}:`}
         </div>
         <div className="request-decrypt-message__balance-value">
-          {`${nativeCurrencyBalance} ${
-            conversionRate ? currentCurrency?.toUpperCase() : nativeCurrency
-          }`}
+          {`${nativeCurrencyBalance} ${nativeCurrency}`}
         </div>
       </div>
     </div>
@@ -121,21 +112,181 @@ const VisualSection = ({ name, notice, targetSubjectMetadata }) => (
   </div>
 );
 
+const ScrollToBottomButton = ({
+  isScrollable,
+  isScrolledToBottom,
+  hasDecrypted,
+  hasError,
+  scrollToBottom,
+}) => {
+  const t = useI18nContext();
+
+  const isScrollToBottomVisible =
+    hasDecrypted && !hasError && isScrollable && !isScrolledToBottom;
+
+  if (!isScrollToBottomVisible) {
+    return null;
+  }
+
+  return (
+    <ButtonIcon
+      ariaLabel={t('scrollDown')}
+      backgroundColor={BackgroundColor.primaryDefault}
+      borderRadius={BorderRadius.full}
+      className="scroll-to-bottom__button"
+      color={IconColor.primaryInverse}
+      data-testid="scroll-to-bottom"
+      display={Display.Flex}
+      iconName={IconName.Arrow2Down}
+      onClick={scrollToBottom}
+      size={ButtonIconSize.Md}
+    />
+  );
+};
+
+const MessageBody = forwardRef(
+  (
+    {
+      isScrollable,
+      isScrolledToBottom,
+      onScroll,
+      rawMessage,
+      scrollToBottom,
+      setRawMessage,
+      txData,
+    },
+    ref,
+  ) => {
+    const dispatch = useDispatch();
+    const trackEvent = useContext(MetaMetricsContext);
+    const t = useI18nContext();
+
+    const [copyToClipboardPressed, setCopyToClipboardPressed] = useState(false);
+    const [hasCopied, setHasCopied] = useState(false);
+    const [hasDecrypted, setHasDecrypted] = useState(false);
+    const [hasError, setHasError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const copyMessage = () => {
+      copyToClipboard(rawMessage, COPY_OPTIONS);
+      trackEvent({
+        category: MetaMetricsEventCategory.Messages,
+        event: 'Copy',
+        properties: {
+          action: 'Decrypt Message Copy',
+          legacy_event: true,
+        },
+      });
+      setHasCopied(true);
+      setTimeout(() => setHasCopied(false), SECOND * 3);
+    };
+
+    const onDecryptMessage = async (event) => {
+      event.stopPropagation(event);
+
+      const params = txData.msgParams;
+      params.metamaskId = txData.id;
+
+      const result = await dispatch(decryptMsgInline(params));
+      if (result.error) {
+        setHasError(true);
+        setErrorMessage(t('decryptInlineError', [result.error]));
+      } else {
+        setHasDecrypted(true);
+        setRawMessage(result.rawSig);
+      }
+    };
+
+    return (
+      <div className="request-decrypt-message__message-container">
+        <div className="request-decrypt-message__message">
+          <div
+            className="request-decrypt-message__message-text"
+            ref={ref}
+            onScroll={onScroll}
+          >
+            {!hasDecrypted && !hasError ? txData.msgParams.data : rawMessage}
+            {hasError ? errorMessage : ''}
+          </div>
+          <div
+            className={classnames('request-decrypt-message__message-cover', {
+              'request-decrypt-message__message-lock--pressed':
+                hasDecrypted || hasError,
+            })}
+          />
+          <div
+            className={classnames('request-decrypt-message__message-lock', {
+              'request-decrypt-message__message-lock--pressed':
+                hasDecrypted || hasError,
+            })}
+            data-testid="message-lock"
+            onClick={onDecryptMessage}
+          >
+            <div className="request-decrypt-message__message-lock__container">
+              <i className="fa fa-lock fa-lg request-decrypt-message__message-lock__container__icon" />
+              <div className="request-decrypt-message__message-lock__container__text">
+                {t('decryptMetamask')}
+              </div>
+            </div>
+          </div>
+        </div>
+        <ScrollToBottomButton
+          isScrollable={isScrollable}
+          hasError={hasError}
+          hasDecrypted={hasDecrypted}
+          isScrolledToBottom={isScrolledToBottom}
+          scrollToBottom={scrollToBottom}
+        />
+        {hasDecrypted ? (
+          <div
+            className={classnames({
+              'request-decrypt-message__message-copy': true,
+              'request-decrypt-message__message-copy--pressed':
+                copyToClipboardPressed,
+            })}
+            onClick={copyMessage}
+            onMouseDown={() => setCopyToClipboardPressed(true)}
+            onMouseUp={() => setCopyToClipboardPressed(false)}
+          >
+            <Tooltip
+              position="bottom"
+              title={hasCopied ? t('copiedExclamation') : t('copyToClipboard')}
+              wrapperClassName="request-decrypt-message__message-copy-tooltip"
+              style={{ display: 'flex', alignItems: 'center' }}
+            >
+              <div className="request-decrypt-message__message-copy-text">
+                {t('decryptCopy')}
+              </div>
+              <Icon
+                name={hasCopied ? IconName.CopySuccess : IconName.Copy}
+                color={IconColor.primaryDefault}
+              />
+            </Tooltip>
+          </div>
+        ) : (
+          <div />
+        )}
+      </div>
+    );
+  },
+);
+MessageBody.displayName = 'MessageBody';
+
 const Footer = ({
-  cancelDecryptMessage,
-  clearConfirmTransaction,
-  decryptMessage,
   hasScrolledToBottom,
-  history,
   isScrollable,
   mostRecentOverviewPage,
   txData,
 }) => {
+  const dispatch = useDispatch();
+  const history = useHistory();
   const t = useI18nContext();
   const trackEvent = useContext(MetaMetricsContext);
 
   const onCancelClick = async (event) => {
-    await cancelDecryptMessage(txData, event);
+    event.stopPropagation(event);
+
+    await dispatch(cancelDecryptMsg(txData));
     trackEvent({
       category: MetaMetricsEventCategory.Messages,
       event: 'Cancel',
@@ -144,12 +295,16 @@ const Footer = ({
         legacy_event: true,
       },
     });
-    clearConfirmTransaction();
+    dispatch(clearConfirmTransaction());
     history.push(mostRecentOverviewPage);
   };
 
   const onSubmitClick = async (event) => {
-    await decryptMessage(txData, event);
+    event.stopPropagation(event);
+    const params = txData.msgParams;
+    params.metamaskId = txData.id;
+
+    await dispatch(decryptMsg(params));
     trackEvent({
       category: MetaMetricsEventCategory.Messages,
       event: 'Confirm',
@@ -158,7 +313,7 @@ const Footer = ({
         legacy_event: true,
       },
     });
-    clearConfirmTransaction();
+    dispatch(clearConfirmTransaction());
     history.push(mostRecentOverviewPage);
   };
 
@@ -173,30 +328,24 @@ const Footer = ({
   );
 };
 
-const ConfirmDecryptMessage = ({
-  cancelDecryptMessage,
-  clearConfirmTransaction,
-  conversionRate,
-  currentCurrency,
-  decryptMessage,
-  decryptMessageInline,
-  fromAccount,
-  history,
-  mostRecentOverviewPage,
-  nativeCurrency,
-  requesterAddress,
-  subjectMetadata,
-  txData,
-}) => {
-  const trackEvent = useContext(MetaMetricsContext);
+const ConfirmDecryptMessage = () => {
   const t = useI18nContext();
-
-  const [copyToClipboardPressed, setCopyToClipboardPressed] = useState(false);
-  const [hasCopied, setHasCopied] = useState(false);
-  const [hasDecrypted, setHasDecrypted] = useState(false);
-  const [hasError, setHasError] = useState(false);
   const [rawMessage, setRawMessage] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const mostRecentOverviewPage = useSelector(getMostRecentOverviewPage);
+  const nativeCurrency = useSelector(getNativeCurrency);
+
+  const unconfirmedTransactions = useSelector(
+    unconfirmedTransactionsListSelector,
+  );
+  const txData = cloneDeep(unconfirmedTransactions[0]);
+
+  const fromAccount = useSelector((state) =>
+    getTargetAccountWithSendEtherInfo(state, txData?.msgParams?.from),
+  );
+
+  const subjectMetadata = useSelector(
+    (state) => state.metamask.subjectMetadata || {},
+  );
 
   const {
     hasScrolledToBottom,
@@ -208,20 +357,6 @@ const ConfirmDecryptMessage = ({
   } = useScrollRequired([rawMessage], {
     offsetPxFromBottom: 0,
   });
-
-  const copyMessage = () => {
-    copyToClipboard(rawMessage, COPY_OPTIONS);
-    trackEvent({
-      category: MetaMetricsEventCategory.Messages,
-      event: 'Copy',
-      properties: {
-        action: 'Decrypt Message Copy',
-        legacy_event: true,
-      },
-    });
-    setHasCopied(true);
-    setTimeout(() => setHasCopied(false), SECOND * 3);
-  };
 
   if (!txData) {
     log.warn('ConfirmDecryptMessage Page: Missing txData prop.');
@@ -236,137 +371,31 @@ const ConfirmDecryptMessage = ({
     <div className="request-decrypt-message__container">
       <Header />
       <div className="request-decrypt-message__body">
-        <Account
-          conversionRate={conversionRate}
-          currentCurrency={currentCurrency}
-          fromAccount={fromAccount}
-          nativeCurrency={nativeCurrency}
-          requesterAddress={requesterAddress}
-        />
+        <Account fromAccount={fromAccount} nativeCurrency={nativeCurrency} />
         <VisualSection
           name={name}
           notice={notice}
           targetSubjectMetadata={targetSubjectMetadata}
         />
-        <div className="request-decrypt-message__message-container">
-          <div className="request-decrypt-message__message">
-            <div
-              className="request-decrypt-message__message-text"
-              ref={ref}
-              onScroll={onScroll}
-            >
-              {!hasDecrypted && !hasError ? txData.msgParams.data : rawMessage}
-              {hasError ? errorMessage : ''}
-            </div>
-            <div
-              className={classnames('request-decrypt-message__message-cover', {
-                'request-decrypt-message__message-lock--pressed':
-                  hasDecrypted || hasError,
-              })}
-            />
-            <div
-              className={classnames('request-decrypt-message__message-lock', {
-                'request-decrypt-message__message-lock--pressed':
-                  hasDecrypted || hasError,
-              })}
-              onClick={(event) => {
-                decryptMessageInline(txData, event).then((result) => {
-                  if (result.error) {
-                    setHasError(true);
-                    setErrorMessage(t('decryptInlineError', [result.error]));
-                  } else {
-                    setHasDecrypted(true);
-                    setRawMessage(result.rawSig);
-                  }
-                });
-              }}
-            >
-              <div className="request-decrypt-message__message-lock__container">
-                <i className="fa fa-lock fa-lg request-decrypt-message__message-lock__container__icon" />
-                <div className="request-decrypt-message__message-lock__container__text">
-                  {t('decryptMetamask')}
-                </div>
-              </div>
-            </div>
-          </div>
-          {hasDecrypted && !hasError && isScrollable && !isScrolledToBottom && (
-            <ButtonIcon
-              className="scroll-to-bottom__button"
-              onClick={scrollToBottom}
-              iconName={IconName.Arrow2Down}
-              ariaLabel={t('scrollDown')}
-              backgroundColor={BackgroundColor.primaryDefault}
-              borderRadius={BorderRadius.full}
-              color={IconColor.primaryInverse}
-              display={Display.Flex}
-              size={ButtonIconSize.Md}
-            />
-          )}
-          {hasDecrypted ? (
-            <div
-              className={classnames({
-                'request-decrypt-message__message-copy': true,
-                'request-decrypt-message__message-copy--pressed':
-                  copyToClipboardPressed,
-              })}
-              onClick={copyMessage}
-              onMouseDown={() => setCopyToClipboardPressed(true)}
-              onMouseUp={() => setCopyToClipboardPressed(false)}
-            >
-              <Tooltip
-                position="bottom"
-                title={
-                  hasCopied ? t('copiedExclamation') : t('copyToClipboard')
-                }
-                wrapperClassName="request-decrypt-message__message-copy-tooltip"
-                style={{ display: 'flex', alignItems: 'center' }}
-              >
-                <div className="request-decrypt-message__message-copy-text">
-                  {t('decryptCopy')}
-                </div>
-                <Icon
-                  name={hasCopied ? IconName.CopySuccess : IconName.Copy}
-                  color={IconColor.primaryDefault}
-                />
-              </Tooltip>
-            </div>
-          ) : (
-            <div />
-          )}
-        </div>
+        <MessageBody
+          isScrollable={isScrollable}
+          isScrolledToBottom={isScrolledToBottom}
+          onScroll={onScroll}
+          rawMessage={rawMessage}
+          ref={ref}
+          scrollToBottom={scrollToBottom}
+          setRawMessage={setRawMessage}
+          txData={txData}
+        />
       </div>
       <Footer
-        cancelDecryptMessage={cancelDecryptMessage}
-        clearConfirmTransaction={clearConfirmTransaction}
-        decryptMessage={decryptMessage}
         hasScrolledToBottom={hasScrolledToBottom}
-        history={history}
         isScrollable={isScrollable}
         mostRecentOverviewPage={mostRecentOverviewPage}
         txData={txData}
       />
     </div>
   );
-};
-
-ConfirmDecryptMessage.propTypes = {
-  cancelDecryptMessage: PropTypes.func.isRequired,
-  clearConfirmTransaction: PropTypes.func.isRequired,
-  conversionRate: PropTypes.number,
-  currentCurrency: PropTypes.string.isRequired,
-  decryptMessage: PropTypes.func.isRequired,
-  decryptMessageInline: PropTypes.func.isRequired,
-  fromAccount: PropTypes.shape({
-    address: PropTypes.string.isRequired,
-    balance: PropTypes.string,
-    name: PropTypes.string,
-  }).isRequired,
-  history: PropTypes.object.isRequired,
-  mostRecentOverviewPage: PropTypes.string.isRequired,
-  nativeCurrency: PropTypes.string.isRequired,
-  requesterAddress: PropTypes.string,
-  subjectMetadata: PropTypes.object,
-  txData: PropTypes.object,
 };
 
 export default ConfirmDecryptMessage;
--- ui/pages/confirm-decrypt-message/confirm-decrypt-message.component.test.js
+++ ui/pages/confirm-decrypt-message/confirm-decrypt-message.component.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import configureMockStore from 'redux-mock-store';
-import { EthAccountType } from '@metamask/keyring-api';
+import { merge } from 'lodash';
 import mockState from '../../../test/data/mock-state.json';
 import { renderWithProvider } from '../../../test/lib/render-helpers';
-import { ETH_EOA_METHODS } from '../../../shared/constants/eth-methods';
+import { flushPromises } from '../../../test/lib/timer-helpers';
+import { decryptMsgInline } from '../../store/actions';
 import ConfirmDecryptMessage from './confirm-decrypt-message.component';
 
 const messageData = {
@@ -13,129 +14,71 @@ const messageData = {
     verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
     version: '1',
   },
-  message: {
-    contents: 'Hello, Bob!',
-    from: {
-      name: 'Cow',
-      wallets: [
-        '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
-        '0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF',
-      ],
-    },
-    to: [
-      {
-        name: 'Bob',
-        wallets: [
-          '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
-          '0xB0BdaBea57B0BDABeA57b0bdABEA57b0BDabEa57',
-          '0xB0B0b0b0b0b0B000000000000000000000000000',
-        ],
-      },
-    ],
-  },
-  primaryType: 'Mail',
-  types: {
-    EIP712Domain: [
-      { name: 'name', type: 'string' },
-      { name: 'version', type: 'string' },
-      { name: 'chainId', type: 'uint256' },
-      { name: 'verifyingContract', type: 'address' },
-    ],
-    Mail: [
-      { name: 'from', type: 'Person' },
-      { name: 'to', type: 'Person[]' },
-      { name: 'contents', type: 'string' },
-    ],
-    Person: [
-      { name: 'name', type: 'string' },
-      { name: 'wallets', type: 'address[]' },
-    ],
+};
+const messageIdMock = '12345';
+const messageMock = {
+  id: messageIdMock,
+  time: 123,
+  status: 'unapproved',
+  type: 'testType',
+  rawSig: undefined,
+  data: JSON.stringify(messageData),
+  chainId: '0x5',
+  msgParams: {
+    data: JSON.stringify(messageData),
+    from: '0x0dcd5d886577d5081b0c52e242ef29e70be3e7bc',
+    version: 'V4',
+    origin: 'test',
   },
 };
-const baseProps = {
-  clearConfirmTransaction: () => undefined,
-  cancelDecryptMessage: () => undefined,
-  decryptMessage: () => undefined,
-  decryptMessageInline: () => undefined,
-  mostRecentOverviewPage: '/',
-  history: { push: '/' },
-  requesterAddress: '0x123456789abcdef',
-  txData: {
-    msgParams: {
-      data: JSON.stringify(messageData),
-      version: 'V4',
-      origin: 'test',
-    },
+const mockRawSignatureMessage = 'raw message';
+
+jest.mock('../../store/background-connection', () => ({
+  ...jest.requireActual('../../store/background-connection'),
+  submitRequestToBackground: jest.fn(),
+}));
+
+jest.mock('../../store/actions', () => ({
+  decryptMsgInline: jest.fn(),
+}));
+
+const state = merge({}, mockState, {
+  history: {
+    mostRecentOverviewPage: '/',
   },
-  subjectMetadata: {
-    'peepeth.com': {
-      iconUrl: 'https://peepeth.com/favicon-32x32.png',
-      name: 'Peepeth',
-    },
-    'https://remix.ethereum.org': {
-      iconUrl: 'https://remix.ethereum.org/icon.png',
-      name: 'Remix - Ethereum IDE',
-    },
+  metamask: {
+    unapprovedDecryptMsgs: [messageMock],
   },
-  nativeCurrency: 'ABC',
-  currentCurrency: 'def',
-  fromAccount: {
-    address: '0x123456789abcdef',
-    balance: '0x346ba7725f412cbfdb',
-    id: 'cf8dace4-9439-4bd4-b3a8-88c821c8fcb3',
-    metadata: {
-      name: 'Antonio',
-      keyring: {
-        type: 'HD Key Tree',
-      },
-    },
-    options: {},
-    methods: ETH_EOA_METHODS,
-    type: EthAccountType.Eoa,
-  },
-};
+});
 
 describe('ConfirmDecryptMessage Component', () => {
-  const store = configureMockStore()(mockState);
+  const mockedDecryptMsgInline = jest.mocked(decryptMsgInline);
+  let store;
 
-  it('should match snapshot when preference is ETH currency', () => {
-    const { container } = renderWithProvider(
-      <ConfirmDecryptMessage {...baseProps} conversionRate={null} />,
-      store,
-    );
+  beforeEach(() => {
+    store = configureMockStore()(state);
 
-    expect(container).toMatchSnapshot();
-    expect(
-      container.querySelector('.request-decrypt-message__balance-value')
-        .textContent,
-    ).toMatchInlineSnapshot(`"966.987986 ABC"`);
+    mockedDecryptMsgInline.mockReturnValue({
+      rawSig: mockRawSignatureMessage,
+      type: 'DECRYPT_MESSAGE',
+    });
   });
 
-  it('should match snapshot when preference is Fiat currency', () => {
-    const { container } = renderWithProvider(
-      <ConfirmDecryptMessage {...baseProps} conversionRate={1572.88} />,
-      store,
-    );
-
+  it('should match snapshot', () => {
+    const { container } = renderWithProvider(<ConfirmDecryptMessage />, store);
     expect(container).toMatchSnapshot();
-    expect(
-      container.querySelector('.request-decrypt-message__balance-value')
-        .textContent,
-    ).toMatchInlineSnapshot(`"1520956.064158 DEF"`);
   });
 
-  it('should match snapshot when there is no txData', () => {
-    const newProps = {
-      ...baseProps,
-      txData: null,
-    };
-    const { container } = renderWithProvider(
-      <ConfirmDecryptMessage {...newProps} conversionRate={1572.88} />,
+  it('should show the correct message data', async () => {
+    const { container, getByTestId, getByText } = renderWithProvider(
+      <ConfirmDecryptMessage />,
       store,
     );
+    const unlockButton = getByTestId('message-lock');
+    unlockButton.click();
+    await flushPromises();
 
-    expect(
-      container.querySelector('.request-decrypt-message__container'),
-    ).toBeNull();
+    expect(container).toMatchSnapshot();
+    expect(getByText(mockRawSignatureMessage)).toBeInTheDocument();
   });
 });
