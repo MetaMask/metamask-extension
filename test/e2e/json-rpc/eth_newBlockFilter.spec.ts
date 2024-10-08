599617d840f400dfbbb1a4c22f740a990bfab002
Chloe Gao <chloe.gao@consensys.net>
2024-10-08 11:31:25 +0200

    address comment

--- test/e2e/json-rpc/eth_newBlockFilter.spec.ts
+++ test/e2e/json-rpc/eth_newBlockFilter.spec.ts
@@ -1,5 +1,5 @@
 import { strict as assert } from 'assert';
-import { withFixtures, defaultGanacheOptions } from '../helpers';
+import { defaultGanacheOptions, withFixtures } from '../helpers';
 import { loginWithBalanceValidation } from '../page-objects/flows/login.flow';
 import FixtureBuilder from '../fixture-builder';
 import { Driver } from '../webdriver/driver';
