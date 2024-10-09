c86dc417a9257b72fd98c4e6ad4c8f22beb06bdf
IF <139582705+infiniteflower@users.noreply.github.com>
2024-10-09 12:02:13 -0400

    fix: ts errors with redux state and bridge state

--- ui/ducks/bridge/actions.ts
+++ ui/ducks/bridge/actions.ts
@@ -50,6 +50,7 @@ import { ETH_USDT_ADDRESS, FeeType } from '../../../shared/constants/bridge';
 import BridgeController from '../../../app/scripts/controllers/bridge/bridge-controller';
 import { bridgeSlice } from './bridge';
 import {
+  BridgeAppState,
   getApprovalGasMultipliers,
   getBridgeGasMultipliers,
   getQuotes,
@@ -206,7 +207,7 @@ export const submitBridgeTransaction = (
 ) => {
   return async (
     dispatch: MetaMaskReduxDispatch,
-    getState: () => MetaMaskReduxState,
+    getState: () => MetaMaskReduxState & BridgeAppState,
   ) => {
     const state = getState();
     const isBridgeEnabled = getIsBridgeEnabled(state);
