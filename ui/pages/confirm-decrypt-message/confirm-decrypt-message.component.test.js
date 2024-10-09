f8626d2fd23258472747adbe06081f2cf2bf5507
OGPoyraz <omergoktugpoyraz@gmail.com>
2024-10-09 11:11:43 +0200

    Fix suggestions

--- ui/pages/confirm-decrypt-message/confirm-decrypt-message.component.test.js
+++ ui/pages/confirm-decrypt-message/confirm-decrypt-message.component.test.js
@@ -5,6 +5,7 @@ import mockState from '../../../test/data/mock-state.json';
 import { renderWithProvider } from '../../../test/lib/render-helpers';
 import { flushPromises } from '../../../test/lib/timer-helpers';
 import { decryptMsgInline } from '../../store/actions';
+import { useScrollRequired } from '../../hooks/useScrollRequired';
 import ConfirmDecryptMessage from './confirm-decrypt-message.component';
 
 const messageData = {
@@ -32,10 +33,26 @@ const messageMock = {
   },
 };
 const mockRawSignatureMessage = 'raw message';
+const mockRawSignatureLongMessage = 'raw message '.repeat(100);
+const mockUseScrollRequiredResult = {
+  hasScrolledToBottom: false,
+  isScrollable: false,
+  isScrolledToBottom: false,
+  onScroll: jest.fn(),
+  scrollToBottom: jest.fn(),
+  setHasScrolledToBottom: jest.fn(),
+  ref: {
+    current: {},
+  },
+};
 
 jest.mock('../../store/background-connection', () => ({
   ...jest.requireActual('../../store/background-connection'),
   submitRequestToBackground: jest.fn(),
+}));
+
+jest.mock('../../hooks/useScrollRequired', () => ({
+  useScrollRequired: jest.fn(),
 }));
 
 jest.mock('../../store/actions', () => ({
@@ -53,6 +70,8 @@ const state = merge({}, mockState, {
 
 describe('ConfirmDecryptMessage Component', () => {
   const mockedDecryptMsgInline = jest.mocked(decryptMsgInline);
+  const mockedUseScrollRequiredResult = jest.mocked(useScrollRequired);
+
   let store;
 
   beforeEach(() => {
@@ -62,23 +81,91 @@ describe('ConfirmDecryptMessage Component', () => {
       rawSig: mockRawSignatureMessage,
       type: 'DECRYPT_MESSAGE',
     });
+
+    mockedUseScrollRequiredResult.mockReturnValue(mockUseScrollRequiredResult);
   });
 
-  it('should match snapshot', () => {
+  const renderAndUnlockMessage = async () => {
+    const result = renderWithProvider(<ConfirmDecryptMessage />, store);
+
+    const unlockButton = result.getByTestId('message-lock');
+    unlockButton.click();
+    await flushPromises();
+    return result;
+  };
+
+  it('matches snapshot', () => {
     const { container } = renderWithProvider(<ConfirmDecryptMessage />, store);
     expect(container).toMatchSnapshot();
   });
 
-  it('should show the correct message data', async () => {
-    const { container, getByTestId, getByText } = renderWithProvider(
-      <ConfirmDecryptMessage />,
-      store,
-    );
-    const unlockButton = getByTestId('message-lock');
-    unlockButton.click();
-    await flushPromises();
+  it('shows the correct message data', async () => {
+    const { container, getByText } = await renderAndUnlockMessage();
 
     expect(container).toMatchSnapshot();
     expect(getByText(mockRawSignatureMessage)).toBeInTheDocument();
+  });
+
+  describe('on long message', () => {
+    beforeEach(() => {
+      mockedDecryptMsgInline.mockReturnValue({
+        rawSig: mockRawSignatureLongMessage,
+        type: 'DECRYPT_MESSAGE',
+      });
+    });
+
+    it('shows scroll to bottom button', async () => {
+      mockedUseScrollRequiredResult.mockReturnValue(
+        merge({}, mockUseScrollRequiredResult, { isScrollable: true }),
+      );
+
+      const { getByTestId } = await renderAndUnlockMessage();
+
+      const scrollToBottomButton = getByTestId('scroll-to-bottom');
+      expect(scrollToBottomButton).toBeInTheDocument();
+    });
+
+    it('confirm action is disabled if scroll not finished', async () => {
+      mockedUseScrollRequiredResult.mockReturnValue(
+        merge({}, mockUseScrollRequiredResult, { isScrollable: true }),
+      );
+
+      const { getByText } = await renderAndUnlockMessage();
+
+      const confirmButton = getByText('Decrypt');
+      expect(confirmButton).toBeDisabled();
+    });
+
+    it('scroll to bottom button click calls scroll to bottom action', async () => {
+      const spyScrollToBottomAction = jest.fn();
+      mockedUseScrollRequiredResult.mockReturnValue(
+        merge({}, mockUseScrollRequiredResult, {
+          isScrollable: true,
+          scrollToBottom: spyScrollToBottomAction,
+        }),
+      );
+
+      const { getByTestId } = await renderAndUnlockMessage();
+
+      const scrollToBottomButton = getByTestId('scroll-to-bottom');
+      scrollToBottomButton.click();
+
+      expect(spyScrollToBottomAction).toHaveBeenCalled();
+    });
+
+    it('enables confirm action if scrolled to bottom', async () => {
+      mockedUseScrollRequiredResult.mockReturnValue(
+        merge({}, mockUseScrollRequiredResult, {
+          isScrollable: true,
+          hasScrolledToBottom: true,
+          isScrolledToBottom: true,
+        }),
+      );
+
+      const { getByText } = await renderAndUnlockMessage();
+
+      const confirmButton = getByText('Decrypt');
+      expect(confirmButton).not.toBeDisabled();
+    });
   });
 });
