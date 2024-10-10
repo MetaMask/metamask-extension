50ec84a630509831f309560f1cca463ab53d6118
Jiexi Luan <jiexiluan@gmail.com>
2024-10-10 15:49:55 -0700

    initial pass

--- app/scripts/lib/multichain-api/scope/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './assert';
-export * from './authorization';
-export * from './filter';
-export * from './scope';
-export * from './supported';
-export * from './transform';
-export * from './validation';
