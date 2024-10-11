abc245d3f71b420e3239d7e8bb976bf3378931c0
Chloe Gao <chloe.gao@consensys.net>
2024-10-11 14:59:57 +0200

    fix

--- test/e2e/tests/account/snap-account-signatures.spec.ts
+++ test/e2e/tests/account/snap-account-signatures.spec.ts
@@ -3,13 +3,17 @@ import { withFixtures } from '../../helpers';
 import { Driver } from '../../webdriver/driver';
 import { accountSnapFixtures } from '../../accounts/common';
 import { SnapAccountPage } from '../../page-objects/snap-account-page';
+import SnapSimpleKeyringPage from '../../page-objects/pages/snap-simple-keyring-page';
+import { installSnapSimpleKeyring } from '../../page-objects/flows/snap-simple-keyring.flow';
+import { loginWithBalanceValidation } from '../../page-objects/flows/login.flow';
 
 describe('Snap Account Signatures', function (this: Suite) {
   this.timeout(120000); // This test is very long, so we need an unusually high timeout
 
   // Run sync, async approve, and async reject flows
   // (in Jest we could do this with test.each, but that does not exist here)
-  ['sync', 'approve', 'reject'].forEach((flowType) => {
+  //['sync', 'approve', 'reject'].forEach((flowType) => {
+  ['sync'].forEach((flowType) => {
     // generate title of the test from flowType
     const title = `can sign with ${flowType} flow`;
 
@@ -18,21 +22,23 @@ describe('Snap Account Signatures', function (this: Suite) {
         accountSnapFixtures(title),
         async ({ driver }: { driver: Driver }) => {
           const snapAccountPage = new SnapAccountPage(driver);
-          const isAsyncFlow = flowType !== 'sync';
+          const isSyncFlow = flowType === 'sync';
 
-          await snapAccountPage.installSnapSimpleKeyring(isAsyncFlow);
+          await loginWithBalanceValidation(driver);
+          await installSnapSimpleKeyring(driver, isSyncFlow);
+          const snapSimpleKeyringPage = new SnapSimpleKeyringPage(driver);
+          await snapSimpleKeyringPage.createNewAccount();
 
           const newPublicKey = await snapAccountPage.makeNewAccountAndSwitch();
-
           await snapAccountPage.tempToggleSettingRedesignedConfirmations();
 
           // Run all 5 signature types
           const locatorIDs = [
             '#personalSign',
-            '#signTypedData',
+/*             '#signTypedData',
             '#signTypedDataV3',
             '#signTypedDataV4',
-            '#signPermit',
+            '#signPermit', */
           ];
 
           for (const locatorID of locatorIDs) {
