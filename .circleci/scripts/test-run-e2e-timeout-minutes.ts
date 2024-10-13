097eddb6871870fc849c38c0c6d6a8a26017d0c0
Howard Braham <howrad@gmail.com>
2024-10-12 17:55:48 -0700

    ci: add more time for the Merge Queue e2e tests

--- .circleci/scripts/test-run-e2e-timeout-minutes.ts
+++ .circleci/scripts/test-run-e2e-timeout-minutes.ts
@@ -2,7 +2,12 @@ import { filterE2eChangedFiles } from '../../test/e2e/changedFilesUtil';
 
 const changedOrNewTests = filterE2eChangedFiles();
 
-//15 minutes, plus 3 minutes for every changed file, up to a maximum of 30 minutes
-const extraTime = Math.min(15 + changedOrNewTests.length * 3, 30);
+// 15 minutes, plus 3 minutes for every changed file, up to a maximum of 30 minutes
+let extraTime = Math.min(15 + changedOrNewTests.length * 3, 30);
+
+// If this is the Merge Queue, add 5 minutes
+if (process.env.CIRCLE_BRANCH?.startsWith('gh-readonly-queue')) {
+  extraTime += 5;
+}
 
 console.log(extraTime);
