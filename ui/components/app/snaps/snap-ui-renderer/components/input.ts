cde27753fe7e4548c928b64981fc941a2fed6d2a
Guillaume Roux <guillaumeroux123@gmail.com>
2024-10-15 11:55:53 +0200

    fix input

--- ui/components/app/snaps/snap-ui-renderer/components/input.ts
+++ ui/components/app/snaps/snap-ui-renderer/components/input.ts
@@ -1,21 +1,50 @@
-import { InputElement } from '@metamask/snaps-sdk/jsx';
+import { InputElement, NumberInputProps } from '@metamask/snaps-sdk/jsx';
 
+import { hasProperty } from '@metamask/utils';
 import { UIComponentFactory } from './types';
 
-export const input: UIComponentFactory<InputElement> = ({ element, form }) => ({
-  element: 'SnapUIInput',
-  props: {
-    id: element.props.name,
-    placeholder: element.props.placeholder,
-    textFieldProps: {
-      type: element.props.type,
-      inputProps: {
-        step: element.props.step,
-        min: element.props.min,
-        max: element.props.max,
-      },
+export const input: UIComponentFactory<InputElement> = ({ element, form }) => {
+  const constructInputProps = (props: InputElement['props']) => {
+    if (!hasProperty(props, 'type')) {
+      return {
+        textFieldProps: {
+          type: 'text',
+        },
+      };
+    }
+
+    switch (props.type) {
+      case 'number': {
+        const { step, min, max, type } = props as NumberInputProps;
+
+        return {
+          textFieldProps: {
+            type,
+            inputProps: {
+              step: step?.toString(),
+              min: min?.toString(),
+              max: max?.toString(),
+            },
+          },
+        };
+      }
+      default:
+        return {
+          textFieldProps: {
+            type: props.type,
+          },
+        };
+    }
+  };
+
+  return {
+    element: 'SnapUIInput',
+    props: {
+      id: element.props.name,
+      placeholder: element.props.placeholder,
+      ...constructInputProps(element.props),
     },
     name: element.props.name,
     form,
-  },
-});
+  };
+};
