98bc70c13991b85b7a9dc4e78b0836cbac3247a0
seaona <mariona@gmx.es>
2024-10-15 10:30:36 +0200

    deploy contract

--- test/e2e/tests/tokens/add-hide-token.spec.js
+++ test/e2e/tests/tokens/add-hide-token.spec.js
@@ -8,6 +8,7 @@ const {
   clickNestedButton,
 } = require('../../helpers');
 const FixtureBuilder = require('../../fixture-builder');
+const { SMART_CONTRACTS } = require('../../seeder/smart-contracts');
 const { CHAIN_IDS } = require('../../../../shared/constants/network');
 
 describe('Add hide token', function () {
@@ -126,7 +127,7 @@ describe('Add existing token using search', function () {
           tag: 'p',
         });
         await driver.clickElement({ text: 'Next', tag: 'button' });
-        await driver.clickElement(
+        await driver.clickElementAndWaitToDisappear(
           '[data-testid="import-tokens-modal-import-button"]',
         );
         await driver.clickElement(
@@ -147,6 +148,8 @@ describe('Add existing token using search', function () {
 });
 
 describe('Add token using wallet_watchAsset', function () {
+  const smartContract = SMART_CONTRACTS.HST;
+
   it('opens a notification that adds a token when wallet_watchAsset is executed, then approves', async function () {
     await withFixtures(
       {
@@ -155,9 +158,13 @@ describe('Add token using wallet_watchAsset', function () {
           .withPermissionControllerConnectedToTestDapp()
           .build(),
         ganacheOptions: defaultGanacheOptions,
+        smartContract,
         title: this.test.fullTitle(),
       },
-      async ({ driver }) => {
+      async ({ driver, contractRegistry }) => {
+        const contractAddress = await contractRegistry.getContractAddress(
+          smartContract,
+        );
         await unlockWallet(driver);
 
         await driver.openNewPage('http://127.0.0.1:8080/');
@@ -168,7 +175,7 @@ describe('Add token using wallet_watchAsset', function () {
             params: {
               type: 'ERC20',
               options: {
-                address: '0x86002be4cdd922de1ccb831582bf99284b99ac12',
+                address: '${contractAddress}',
                 symbol: 'TST',
                 decimals: 4
               },
@@ -203,9 +210,13 @@ describe('Add token using wallet_watchAsset', function () {
           .withPermissionControllerConnectedToTestDapp()
           .build(),
         ganacheOptions: defaultGanacheOptions,
+        smartContract,
         title: this.test.fullTitle(),
       },
-      async ({ driver }) => {
+      async ({ driver, contractRegistry }) => {
+        const contractAddress = await contractRegistry.getContractAddress(
+          smartContract,
+        );
         await unlockWallet(driver);
 
         await driver.openNewPage('http://127.0.0.1:8080/');
@@ -216,7 +227,7 @@ describe('Add token using wallet_watchAsset', function () {
             params: {
               type: 'ERC20',
               options: {
-                address: '0x86002be4cdd922de1ccb831582bf99284b99ac12',
+                address: '${contractAddress}',
                 symbol: 'TST',
                 decimals: 4
               },
