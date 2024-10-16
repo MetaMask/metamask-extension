014a6ca41376746ab7e638bf7e3937b09f99361d
IF <139582705+infiniteflower@users.noreply.github.com>
2024-10-16 15:01:48 -0400

    chore: remove gas multipliers as bridge api's gasLimit already has multipliers in it

--- ui/ducks/bridge/actions.ts
+++ ui/ducks/bridge/actions.ts
@@ -22,7 +22,6 @@ import { submitRequestToBackground } from '../../store/background-connection';
 import {
   ChainId,
   FeeType,
-  GasMultiplierByChainId,
   QuoteRequest,
   QuoteResponse,
   TxData,
@@ -42,11 +41,7 @@ import { getEthUsdtResetData, isEthUsdt } from '../../pages/bridge/bridge.util';
 import { ETH_USDT_ADDRESS } from '../../../shared/constants/bridge';
 import BridgeController from '../../../app/scripts/controllers/bridge/bridge-controller';
 import { bridgeSlice } from './bridge';
-import {
-  BridgeAppState,
-  getApprovalGasMultipliers,
-  getBridgeGasMultipliers,
-} from './selectors';
+import { BridgeAppState } from './selectors';
 
 const {
   setToChainId,
@@ -165,9 +160,10 @@ export const submitBridgeTransaction = (
       };
     };
 
-    const calcMaxGasLimit = (gasLimit: number, gasMultiplier = 1) => {
+    // We don't need to use gas multipliers here because the gasLimit from Bridge API already included it
+    const getMaxGasLimit = (gasLimit: number) => {
       return new Numeric(
-        new BigNumber(gasLimit).times(gasMultiplier).round(0).toString(),
+        new BigNumber(gasLimit).toString(),
         10,
       ).toPrefixedHexString();
     };
@@ -175,7 +171,6 @@ export const submitBridgeTransaction = (
     const handleTx = async ({
       txType,
       txParams,
-      gasMultipliers,
       maxFeePerGas,
       maxPriorityFeePerGas,
       meta,
@@ -189,7 +184,6 @@ export const submitBridgeTransaction = (
         data: string;
         gasLimit: number | null;
       };
-      gasMultipliers: GasMultiplierByChainId;
       maxFeePerGas: string | undefined;
       maxPriorityFeePerGas: string | undefined;
       meta: Record<string, unknown>;
@@ -202,10 +196,7 @@ export const submitBridgeTransaction = (
         throw new Error('Invalid chain ID');
       }
 
-      const maxGasLimit = calcMaxGasLimit(
-        txParams.gasLimit ?? 0,
-        gasMultipliers[hexChainId],
-      );
+      const maxGasLimit = getMaxGasLimit(txParams.gasLimit ?? 0);
 
       const finalTxParams = {
         ...txParams,
@@ -261,12 +252,10 @@ export const submitBridgeTransaction = (
           ...approval,
           data: resetData,
         };
-        const gasMultipliers = getApprovalGasMultipliers(state);
 
         await handleTx({
           txType: 'bridgeApproval',
           txParams,
-          gasMultipliers,
           maxFeePerGas,
           maxPriorityFeePerGas,
           meta: {
@@ -301,11 +290,9 @@ export const submitBridgeTransaction = (
         });
       }
 
-      const gasMultipliers = getApprovalGasMultipliers(state);
       const txMeta = await handleTx({
         txType: 'bridgeApproval',
         txParams: approval,
-        gasMultipliers,
         maxFeePerGas,
         maxPriorityFeePerGas,
         meta: {
@@ -326,11 +313,9 @@ export const submitBridgeTransaction = (
       maxFeePerGas: string | undefined;
       maxPriorityFeePerGas: string | undefined;
     }) => {
-      const gasMultipliers = getBridgeGasMultipliers(state);
       const txMeta = await handleTx({
         txType: 'bridge',
         txParams: quoteResponse.trade,
-        gasMultipliers,
         maxFeePerGas,
         maxPriorityFeePerGas,
         meta: {
