902bd5afeb91296993a5c927cec507eaf3a8750d
Jiexi Luan <jiexiluan@gmail.com>
2024-10-16 10:31:10 -0700

    Remove selectors intended for multichain API

--- app/scripts/controllers/permissions/selectors.js
+++ app/scripts/controllers/permissions/selectors.js
@@ -45,33 +45,6 @@ export const getPermittedAccountsByOrigin = createSelector(
 );
 
 /**
- * Get the authorized CAIP-25 scopes for each subject, keyed by origin.
- * The values of the returned map are immutable values from the
- * PermissionController state.
- *
- * @returns {Map<string, Caip25Authorization>} The current origin:authorization map.
- */
-export const getAuthorizedScopesByOrigin = createSelector(
-  getSubjects,
-  (subjects) => {
-    return Object.values(subjects).reduce(
-      (originToAuthorizationsMap, subject) => {
-        const caveats =
-          subject.permissions?.[Caip25EndowmentPermissionName]?.caveats || [];
-
-        const caveat = caveats.find(({ type }) => type === Caip25CaveatType);
-
-        if (caveat) {
-          originToAuthorizationsMap.set(subject.origin, caveat.value);
-        }
-        return originToAuthorizationsMap;
-      },
-      new Map(),
-    );
-  },
-);
-
-/**
  * Get the permitted chains for each subject, keyed by origin.
  * The values of the returned map are immutable values from the
  * PermissionController state.
@@ -138,58 +111,6 @@ export const diffMap = (currentMap, previousMap) => {
     changedMap.set(origin, currentMap.get(origin));
   }
   return changedMap;
-};
-
-/**
- * Given the current and previous exposed CAIP-25 authorization for each PermissionController
- * subject, returns a new map containing all authorizations that have changed.
- * The values of each map must be immutable values directly from the
- * PermissionController state, or an empty object instantiated in this
- * function.
- *
- * @param {Map<string, Caip25Authorization>} newAuthorizationsMap - The new origin:authorization map.
- * @param {Map<string, Caip25Authorization>} [previousAuthorizationsMap] - The previous origin:authorization map.
- * @returns {Map<string, Caip25Authorization>} The origin:authorization map of changed authorizations.
- */
-export const getChangedAuthorizations = (
-  newAuthorizationsMap,
-  previousAuthorizationsMap,
-) => {
-  if (previousAuthorizationsMap === undefined) {
-    return newAuthorizationsMap;
-  }
-
-  const changedAuthorizations = new Map();
-  if (newAuthorizationsMap === previousAuthorizationsMap) {
-    return changedAuthorizations;
-  }
-
-  const newOrigins = new Set([...newAuthorizationsMap.keys()]);
-
-  for (const origin of previousAuthorizationsMap.keys()) {
-    const newAuthorizations = newAuthorizationsMap.get(origin) ?? {
-      requiredScopes: {},
-      optionalScopes: {},
-    };
-
-    // The values of these maps are references to immutable values, which is why
-    // a strict equality check is enough for diffing. The values are either from
-    // PermissionController state, or an empty object initialized in the previous
-    // call to this function. `newAuthorizationsMap` will never contain any empty
-    // objects.
-    if (previousAuthorizationsMap.get(origin) !== newAuthorizations) {
-      changedAuthorizations.set(origin, newAuthorizations);
-    }
-
-    newOrigins.delete(origin);
-  }
-
-  // By now, newOrigins is either empty or contains some number of previously
-  // unencountered origins, and all of their authorizations have "changed".
-  for (const origin of newOrigins.keys()) {
-    changedAuthorizations.set(origin, newAuthorizationsMap.get(origin));
-  }
-  return changedAuthorizations;
 };
 
 /**
