e5a2b3fa0057d371ffb5913351b2550e0fdc24d8
David Walsh <davidwalsh83@gmail.com>
2024-10-16 07:58:33 -0500

    Remove delays from onboarding tests

--- test/e2e/tests/privacy/basic-functionality.spec.js
+++ test/e2e/tests/privacy/basic-functionality.spec.js
@@ -4,9 +4,6 @@ const {
   withFixtures,
   importSRPOnboardingFlow,
   WALLET_PASSWORD,
-  tinyDelayMs,
-  regularDelayMs,
-  largeDelayMs,
   defaultGanacheOptions,
 } = require('../../helpers');
 const { METAMASK_STALELIST_URL } = require('../phishing-controller/helpers');
@@ -65,8 +62,6 @@ describe('MetaMask onboarding @no-mmi', function () {
         });
         await driver.clickElement('[data-testid="category-item-General"]');
 
-        await driver.delay(regularDelayMs);
-
         await driver.clickElement(
           '[data-testid="basic-functionality-toggle"] .toggle-button',
         );
@@ -74,18 +69,14 @@ describe('MetaMask onboarding @no-mmi', function () {
         await driver.clickElement('[id="basic-configuration-checkbox"]');
         await driver.clickElement({ text: 'Turn off', tag: 'button' });
         await driver.clickElement('[data-testid="category-back-button"]');
-        await driver.delay(regularDelayMs);
         await driver.clickElement('[data-testid="category-item-Assets"]');
-        await driver.delay(regularDelayMs);
         await driver.clickElement(
           '[data-testid="currency-rate-check-toggle"] .toggle-button',
         );
         await driver.clickElement('[data-testid="category-back-button"]');
-        await driver.delay(regularDelayMs);
         await driver.clickElement(
           '[data-testid="privacy-settings-back-button"]',
         );
-        await driver.delay(regularDelayMs);
 
         await driver.clickElement({ text: 'Done', tag: 'button' });
         await driver.clickElement('[data-testid="pin-extension-next"]');
@@ -94,7 +85,6 @@ describe('MetaMask onboarding @no-mmi', function () {
         await driver.clickElement('[data-testid="network-display"]');
 
         await driver.clickElement({ text: 'Ethereum Mainnet', tag: 'p' });
-        await driver.delay(tinyDelayMs);
 
         // Wait until network is fully switched and refresh tokens before asserting to mitigate flakiness
         await driver.assertElementNotPresent('.loading-overlay');
@@ -134,13 +124,10 @@ describe('MetaMask onboarding @no-mmi', function () {
           tag: 'button',
         });
         await driver.clickElement('[data-testid="category-item-General"]');
-        await driver.delay(largeDelayMs);
         await driver.clickElement('[data-testid="category-back-button"]');
-        await driver.delay(largeDelayMs);
         await driver.clickElement(
           '[data-testid="privacy-settings-back-button"]',
         );
-        await driver.delay(largeDelayMs);
         await driver.clickElement({ text: 'Done', tag: 'button' });
         await driver.clickElement('[data-testid="pin-extension-next"]');
         await driver.clickElement({ text: 'Done', tag: 'button' });
