fbff8fba45147677e2397c474aa481d50727b394
Kanthesha Devaramane <kanthesha.devaramane@consensys.net>
2024-10-16 11:08:23 +0100

    remove preferences controller from mmi controller constructor

--- app/scripts/controllers/mmi-controller.ts
+++ app/scripts/controllers/mmi-controller.ts
@@ -44,7 +44,6 @@ import {
 import { getCurrentChainId } from '../../../ui/selectors';
 import MetaMetricsController from './metametrics';
 import { getPermissionBackgroundApiMethods } from './permissions';
-import { PreferencesController } from './preferences-controller';
 import AccountTrackerController from './account-tracker-controller';
 import { AppStateController } from './app-state';
 
@@ -69,8 +68,6 @@ export default class MMIController extends EventEmitter {
   // TODO: Replace `any` with type
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public keyringController: any;
-
-  public preferencesController: PreferencesController;
 
   public appStateController: AppStateController;
 
@@ -142,7 +139,6 @@ export default class MMIController extends EventEmitter {
     this.messenger = opts.messenger;
     this.mmiConfigurationController = opts.mmiConfigurationController;
     this.keyringController = opts.keyringController;
-    this.preferencesController = opts.preferencesController;
     this.appStateController = opts.appStateController;
     this.transactionUpdateController = opts.transactionUpdateController;
     this.custodyController = opts.custodyController;
