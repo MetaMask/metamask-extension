01ea106de1f6bcd2b122d3b9b8c3fc862591f35a
legobeat <109787230+legobeat@users.noreply.github.com>
2024-10-17 04:16:11 +0000

    fix: fall back to bundled chainlist (#23392)
    ## **Description**
    
    The list of known chains is fetched at runtime from
    `https://chainid.network/chains.json` and cached. There are some issues
    with the way this works:
    - MetaMask will not have a list of chains until online (if ever)
    - When the 24h cache timeout expires, the chainlist becomes unavailable
    
    This PR addresses this by:
    - Refactoring out `https://chainid.network/chains.json` into constant
    `CHAIN_SPEC_URL`
    - Add new optional option `allowStale` to `fetchWithCache`. If set to
    `true`, it will falling back to return any entry instead of throwing an
    error when a request fail.
    - Set `allowStale` to `true` for all requests to `CHAIN_SPEC_URL`
    - Seed the fetch cache for `CHAIN_SPEC_URL` with
    [`eth-chainlist`](https://www.npmjs.com/package/eth-chainlist), which is
    the same data exposed via a published npm package.
    - Open for suggestions on if this should be bundled differently - maybe
    we want our own equivalent mirror?
    
    While an improvement, this could still be further improved.
    
    - The bundled result could be used immediately in all cases without
    waiting for response
    - The cached data could be updated asynchronously in the background,
    without being prompted by user action
    
    I currently consider these out-of-scope for this PR.
    
    Or put more generally: Decoupling the fetching of the data from its use
    would be even better.
    [![Open in GitHub
    Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/PR?quickstart=1)
    
    ## **Related issues**
    
    Fixes:
    
    ## **Manual testing steps**
    
    1. Go to this page...
    2.
    3.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've clearly explained what problem this PR is solving and how it
    is solved.
    - [ ] I've linked related issues
    - [x] I've included manual testing steps
    - [x] I've included screenshots/recordings if applicable
    - [ ] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    - [x] I’ve properly set the pull request status:
      - [x] In case it's not yet "ready for review", I've set it to "draft".
    - [x] In case it's "ready for review", I've changed it from "draft" to
    "non-draft".
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -42,6 +42,7 @@ import {
   LedgerIframeBridge,
 } from '@metamask/eth-ledger-bridge-keyring';
 import LatticeKeyring from 'eth-lattice-keyring';
+import { rawChainData } from 'eth-chainlist';
 import { MetaMaskKeyring as QRHardwareKeyring } from '@keystonehq/metamask-airgapped-keyring';
 import EthQuery from '@metamask/eth-query';
 import EthJSQuery from '@metamask/ethjs-query';
@@ -169,6 +170,7 @@ import {
 } from '../../shared/constants/swaps';
 import {
   CHAIN_IDS,
+  CHAIN_SPEC_URL,
   NETWORK_TYPES,
   NetworkStatus,
   MAINNET_DISPLAY_NAME,
@@ -200,6 +202,10 @@ import {
 } from '../../shared/constants/metametrics';
 import { LOG_EVENT } from '../../shared/constants/logs';
 
+import {
+  getStorageItem,
+  setStorageItem,
+} from '../../shared/lib/storage-helpers';
 import {
   getTokenIdParam,
   fetchTokenBalance,
@@ -412,6 +418,8 @@ export default class MetamaskController extends EventEmitter {
 
     this.getRequestAccountTabIds = opts.getRequestAccountTabIds;
     this.getOpenMetamaskTabsIds = opts.getOpenMetamaskTabsIds;
+
+    this.initializeChainlist();
 
     this.controllerMessenger = new ControllerMessenger();
 
@@ -6301,6 +6309,23 @@ export default class MetamaskController extends EventEmitter {
     return this.smartTransactionsController.getTransactions({
       addressFrom: address,
       status: 'pending',
+    });
+  }
+
+  /**
+   * The chain list is fetched live at runtime, falling back to a cache.
+   * This preseeds the cache at startup with a static list provided at build.
+   */
+  async initializeChainlist() {
+    const cacheKey = `cachedFetch:${CHAIN_SPEC_URL}`;
+    const { cachedResponse } = (await getStorageItem(cacheKey)) || {};
+    if (cachedResponse) {
+      return;
+    }
+    await setStorageItem(cacheKey, {
+      cachedResponse: rawChainData(),
+      // Cached value is immediately invalidated
+      cachedTime: 0,
     });
   }
 
