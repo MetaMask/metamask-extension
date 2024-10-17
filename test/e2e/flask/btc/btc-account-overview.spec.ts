df8620145d69536b5c1120f406a4f8f7fb5b9140
Charly Chevalier <charly.chevalier@consensys.net>
2024-10-17 21:07:53 +0200

    test(btc): fix e2e tests

--- test/e2e/flask/btc/btc-account-overview.spec.ts
+++ test/e2e/flask/btc/btc-account-overview.spec.ts
@@ -16,6 +16,7 @@ describe('BTC Account - Overview', function (this: Suite) {
         await driver.waitForSelector({
           text: 'Send',
           tag: 'button',
+          css: '[data-testid="coin-overview-send"]',
         });
 
         await driver.waitForSelector({
