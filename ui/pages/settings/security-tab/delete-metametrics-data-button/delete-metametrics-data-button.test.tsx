d614f5dc7b3b1f0491f4870cc65ce9fc3be009ea
Monte Lai <monte.lai@consensys.net>
2024-10-18 00:24:34 +0800

    fix: snapshot

--- ui/pages/settings/security-tab/delete-metametrics-data-button/delete-metametrics-data-button.test.tsx
+++ ui/pages/settings/security-tab/delete-metametrics-data-button/delete-metametrics-data-button.test.tsx
@@ -160,7 +160,7 @@ describe('DeleteMetaMetricsDataButton', () => {
       container.querySelector('.settings-page__content-description')
         ?.textContent,
     ).toMatchInlineSnapshot(
-      `" You initiated this action on 7/06/2024. This process can take up to 30 days. View the Privacy policy "`,
+      `" You initiated this action on 8/06/2024. This process can take up to 30 days. View the Privacy policy "`,
     );
   });
 
