e70582e020b69ed3d4ff49227a724a3772c9a172
Chloe Gao <chloe.gao@consensys.net>
2024-10-17 15:21:25 +0200

    fix

--- test/e2e/page-objects/pages/test-dapp.ts
+++ test/e2e/page-objects/pages/test-dapp.ts
@@ -197,7 +197,7 @@ class TestDapp {
     await this.driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
     await this.driver.waitForSelector(this.connectMetaMaskMessage);
 
-    // TODO:Extra steps needed to preserve the current network.
+    // TODO: Extra steps needed to preserve the current network.
     // Following steps can be removed once the issue is fixed (#27891)
     const editNetworkButton = await this.driver.findClickableElements(
       this.editConnectButton,
