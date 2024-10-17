4eb5a8db02ad26fd2bd1174a87000ace9f59e54f
salimtb <salim.toubal@outlook.com>
2024-10-17 21:08:08 +0200

    fix: fix storybook

--- ui/components/multichain/nft-item/nft-item.stories.js
+++ ui/components/multichain/nft-item/nft-item.stories.js
@@ -27,12 +27,15 @@ export default {
     },
   },
   args: {
-    alt: 'Join Archer and his 6,969 frens as they take a trip further down the rabbit hole in search of a world with vibrant art, great vibes, and psychedelic tales.',
-    name: 'Monkey Trip #2422',
-    src: 'https://i.seadn.io/gcs/files/878e670c38e0f02e58bf730c51c30d0c.jpg',
-    networkName: 'Ethereum Mainnet',
-    networkSrc: './images/eth_logo.svg',
-    tokenId: '2422',
+    nft: {
+      alt: 'Join Archer and his 6,969 frens as they take a trip further down the rabbit hole in search of a world with vibrant art, great vibes, and psychedelic tales.',
+      name: 'Monkey Trip #2422',
+      image:
+        'https://i.seadn.io/gcs/files/878e670c38e0f02e58bf730c51c30d0c.jpg',
+      networkName: 'Ethereum Mainnet',
+      networkSrc: './images/eth_logo.svg',
+      tokenId: '2422',
+    },
   },
 };
 
