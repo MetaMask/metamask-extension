896208ef8745977e006a10d681a833c7b13c5b10
Chloe Gao <chloe.gao@consensys.net>
2024-10-17 12:22:25 +0200

    remove unused code

--- test/e2e/flask/btc/common-btc.ts
+++ test/e2e/flask/btc/common-btc.ts
@@ -4,7 +4,7 @@ import { withFixtures, unlockWallet } from '../../helpers';
 import { DEFAULT_BTC_ACCOUNT } from '../../constants';
 import { MultichainNetworks } from '../../../../shared/constants/multichain/networks';
 import { Driver } from '../../webdriver/driver';
-import { createBtcAccount } from '../../accounts/common';
+import messages from '../../../../app/_locales/en/messages.json';
 
 const GENERATE_MOCK_BTC_BALANCE_CALL = (
   address: string = DEFAULT_BTC_ACCOUNT,
@@ -15,6 +15,27 @@ const GENERATE_MOCK_BTC_BALANCE_CALL = (
     },
   };
 };
+
+export async function createBtcAccount(driver: Driver) {
+  await driver.clickElement('[data-testid="account-menu-icon"]');
+  await driver.clickElement(
+    '[data-testid="multichain-account-menu-popover-action-button"]',
+  );
+  await driver.clickElement({
+    text: messages.addNewBitcoinAccount.message,
+    tag: 'button',
+  });
+  await driver.clickElementAndWaitToDisappear(
+    {
+      text: 'Add account',
+      tag: 'button',
+    },
+    // Longer timeout than usual, this reduces the flakiness
+    // around Bitcoin account creation (mainly required for
+    // Firefox)
+    5000,
+  );
+}
 
 export async function mockBtcBalanceQuote(
   mockServer: Mockttp,
