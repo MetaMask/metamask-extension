9bc756d1c18217ee9fe824f2eb703d1e3e290057
HJetpoluru <harika.jetpoluru@consensys.net>
2024-10-18 14:05:04 -0400

    Removed test that was not applicable and updated integration test

--- test/e2e/tests/network/switch-network.spec.ts
+++ test/e2e/tests/network/switch-network.spec.ts
@@ -1,11 +1,6 @@
 import { Suite } from 'mocha';
 import { Driver } from '../../webdriver/driver';
-import {
-  withFixtures,
-  defaultGanacheOptions,
-  unlockWallet,
-  tinyDelayMs,
-} from '../../helpers';
+import { withFixtures, defaultGanacheOptions } from '../../helpers';
 import FixtureBuilder from '../../fixture-builder';
 import { Ganache } from '../../seeder/ganache';
 import { loginWithBalanceValidation } from '../../page-objects/flows/login.flow';
@@ -33,55 +28,19 @@ describe('Switch network - ', function (this: Suite) {
         const headerNavbar = new HeaderNavbar(driver);
         const selectNetwork = new SelectNetwork(driver);
 
-        //Validate the switch network functionality to default Ethereum Mainnet
+        // Validate the switch network functionality to default Ethereum Mainnet
         await headerNavbar.clickSwitchNetworkDropDown();
-        await selectNetwork.clickNetworkName("Ethereum Mainnet");
-        await homePage.check_expectedBalanceIsDisplayed("25");
+        await selectNetwork.clickNetworkName('Ethereum Mainnet');
+        await homePage.check_expectedBalanceIsDisplayed('25');
         await headerNavbar.check_networkNameSwitchDropDown('Ethereum Mainnet');
-        //Validate the switch network functionality to test network Sepolia
+        // Validate the switch network functionality to test network Sepolia
         await headerNavbar.clickSwitchNetworkDropDown();
         await selectNetwork.clickToggleButton();
-        await selectNetwork.clickNetworkName("Sepolia");
-        //Validate the transaction made in Ethereum network is not displayed in Sepolia network
-        await homePage.check_expectedBalanceIsDisplayed("25 Sepolia");
-        await headerNavbar.check_networkNameSwitchDropDown("Sepolia");
-        await driver.delay(tinyDelayMs);
+        await selectNetwork.clickNetworkName('Sepolia');
+        // Validate the transaction made in Ethereum network is not displayed in Sepolia network
+        await homePage.check_expectedBalanceIsDisplayed('25 Sepolia');
+        await headerNavbar.check_networkNameSwitchDropDown('Sepolia');
       },
     );
   });
-
-  it.skip('Validate the networks and UI', async function () {
-    await withFixtures(
-      {
-        fixtures: new FixtureBuilder().build(),
-        ganacheOptions: defaultGanacheOptions,
-        title: this.test?.fullTitle(),
-      },
-      async ({ driver }: { driver: Driver }) => {
-        await loginWithBalanceValidation(driver);
-        const homePage = new HomePage(driver);
-        const headerNavbar = new HeaderNavbar(driver);
-        const selectNetwork = new SelectNetwork(driver);
-        await headerNavbar.clickSwitchNetworkDropDown();
-
-        await driver.isElementPresent({
-          tag: 'h6',
-          text: '“Tenderly” was successfully added!',
-        });
-
-        // Validate the networks
-
-        // Validate the search functionality in switch network dialog
-        // Enter this Localhost 8545
-        //data-testid="network-redesign-modal-search-input"
-        // Validate the delete functionality in switch network dialog
-
-        // Validate the add network functionality
-
-        // Validate the cancel functionality in switch network dialog
-        //aria-label="Close"
-      },
-    );
-  });
-
 });
--- test/integration/network/switch-network.test.tsx
+++ test/integration/network/switch-network.test.tsx
@@ -1,11 +1,9 @@
+import { act, screen } from '@testing-library/react';
+import nock from 'nock';
 import mockMetaMaskState from '../data/integration-init-state.json';
 import { integrationTestRender } from '../../lib/render-helpers';
 import * as backgroundConnection from '../../../ui/store/background-connection';
-import {
-  createMockImplementation,
-  mockGetChains,
-} from '../helpers';
-import { act, fireEvent, screen, waitFor } from '@testing-library/react';
+import { createMockImplementation } from '../helpers';
 
 jest.mock('../../../ui/store/background-connection', () => ({
   ...jest.requireActual('../../../ui/store/background-connection'),
@@ -18,66 +16,22 @@ const backgroundConnectionMocked = {
   onNotification: jest.fn(),
 };
 
-const setupSubmitRequestToBackgroundMocks = (
-  mockRequests?: Record<string, unknown>,
-) => {
-  mockedBackgroundConnection.submitRequestToBackground.mockImplementation(
-    createMockImplementation({
-      ...mockRequests,
-    }),
-  );
-};
-
-describe('Switch network - ', () => {
+describe('Switch network', () => {
   beforeEach(() => {
     jest.resetAllMocks();
-    mockGetChains();
-    setupSubmitRequestToBackgroundMocks();
+    mockedBackgroundConnection.submitRequestToBackground.mockImplementation(
+      createMockImplementation({
+        getState: mockMetaMaskState,
+        getTokenStandardAndDetails: { decimals: '2' },
+      }),
+    );
   });
 
-  it.only('validate the list of networks', async () => {
-    process.env.IN_TEST = '';
-    const mockedMetamaskState = {
-      ...mockMetaMaskState,
-      preferences: {
-        ...mockMetaMaskState.preferences,
-        showMultiRpcModal: false,
-      },
-    };
-
-    const mockedRequests = {
-      getState: mockedMetamaskState,
-      getTokenStandardAndDetails: { decimal: '10' },
-    };
-
-    setupSubmitRequestToBackgroundMocks(mockedRequests);
-
-    await act(async () => {
-      await integrationTestRender({
-        preloadedState: mockedMetamaskState,
-        backgroundConnection: backgroundConnectionMocked,
-      });
-    });
-
-    expect(
-      screen.getByTestId('network-display'),
-    ).toBeInTheDocument();
-
-    // fireEvent.click(screen.getByTestId('network-display'));
-    // expect(
-    //   screen.queryByRole('dialog'),
-    // ).toBeInTheDocument();
-
+  afterEach(() => {
+    nock.cleanAll();
   });
 
-  it('edits the name of an RPC network', async () => {
-    const mockedRequests = {
-      getState: mockMetaMaskState,
-      getTokenStandardAndDetails: { decimal: '10' },
-    };
-
-    setupSubmitRequestToBackgroundMocks(mockedRequests);
-
+  it('displays the network', async () => {
     await act(async () => {
       await integrationTestRender({
         preloadedState: mockMetaMaskState,
@@ -85,68 +39,10 @@ describe('Switch network - ', () => {
       });
     });
 
-    fireEvent.click(screen.getByTestId('network-display'));
-    expect(
-      screen.queryByTestId('multichain-network-list-menu'),
-    ).toBeInTheDocument();
+    expect(screen.getByTestId('network-display')).toBeInTheDocument();
 
-    fireEvent.click(
-      screen.getByTestId('network-list-item-options-button-0x2105'),
-    );
-    fireEvent.click(screen.getByTestId('network-list-item-options-edit'));
-
-    expect(screen.queryByTestId('network-form-name-input')).toBeInTheDocument();
-
-    const networkNameInput = screen.getByTestId('network-form-network-name');
-    expect(networkNameInput).toHaveValue('Base Mainnet');
-    fireEvent.input(networkNameInput, { target: { value: 'New Base Name' } });
-    fireEvent.click(screen.getByTestId('network-form-save'));
-
-    let updateNetworkCall;
-
-    await waitFor(() => {
-      updateNetworkCall =
-        mockedBackgroundConnection.submitRequestToBackground.mock.calls?.find(
-          (call) => call[0] === 'updateNetwork',
-        );
-      expect(updateNetworkCall?.[0]).toBe('updateNetwork');
-    });
-
-    expect(updateNetworkCall?.[1]).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({
-          chainId: '0x2105',
-          name: 'New Base Name',
-          nativeCurrency: 'ETH',
-          rpcEndpoints: expect.arrayContaining([
-            expect.objectContaining({
-              networkClientId: '225ae6de-deda-4078-9f6c-4a4fb758ae00',
-              url: 'https://base.meowrpc.com',
-              type: 'custom',
-              name: 'Base Meow',
-            }),
-            expect.objectContaining({
-              networkClientId: '78d85c4e-eeb0-4b81-ae5b-52d0fca7e5e3',
-              url: 'https://base-pokt.nodies.app',
-              type: 'custom',
-              name: 'Base',
-            }),
-            expect.objectContaining({
-              networkClientId: 'b3742c56-a2be-48ef-8811-6e9d265b420b',
-              url: 'https://base.llamarpc.com',
-              type: 'custom',
-              name: 'Base LlamaNodes',
-            }),
-          ]),
-          defaultRpcEndpointIndex: 2,
-          blockExplorerUrls: expect.arrayContaining(['https://basescan.org']),
-          defaultBlockExplorerUrlIndex: 0,
-        }),
-        expect.objectContaining({
-          replacementSelectedRpcEndpointIndex: 2,
-        }),
-      ]),
+    console.log(
+      mockedBackgroundConnection.submitRequestToBackground.mock.calls,
     );
   });
-
 });
