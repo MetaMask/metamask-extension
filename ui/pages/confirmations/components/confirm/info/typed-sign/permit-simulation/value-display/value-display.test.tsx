9b370bd06d3937ed43b1e770a249fe6178f0a8bf
digiwand <20778143+digiwand@users.noreply.github.com>
2024-10-18 22:06:01 +0800

    feat: add “Incomplete Asset Displayed” metric & fix: should only set default decimals if ERC20 (#27494)
    ## **Description**
    
    ### Overall
    - Adds "Incomplete Asset Displayed" metric when token detail decimals
    are not found for Permit Simulations of ERC20 tokens
    - Fixes issue where decimals may default to 18 when the token is not
    identified as an ERC20 token
    
    ### Details
    - Refactors fetchErc20Decimals
    - Defines types in token.ts
    - Create new useGetTokenStandardAndDetails
    - Includes new `decimalsNumber` value in return to be used instead of
    `decimals` (string type)
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/metamask-extension/issues/27333
    
    ## **Manual testing steps**
    NA
    
    ## **Screenshots/Recordings**
    NA
    
    ## **Pre-merge author checklist**
    
    - [X] I've followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
    Extension Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/CODING_GUIDELINES.md).
    - [X] I've completed the PR template to the best of my ability
    - [X] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [X] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: Jyoti Puri <jyotipuri@gmail.com>

--- ui/pages/confirmations/components/confirm/info/typed-sign/permit-simulation/value-display/value-display.test.tsx
+++ ui/pages/confirmations/components/confirm/info/typed-sign/permit-simulation/value-display/value-display.test.tsx
@@ -4,13 +4,23 @@ import configureMockStore from 'redux-mock-store';
 
 import mockState from '../../../../../../../../../test/data/mock-state.json';
 import { renderWithProvider } from '../../../../../../../../../test/lib/render-helpers';
+import useTrackERC20WithoutDecimalInformation from '../../../../../../hooks/useTrackERC20WithoutDecimalInformation';
 import PermitSimulationValueDisplay from './value-display';
 
 jest.mock('../../../../../../../../store/actions', () => {
   return {
-    getTokenStandardAndDetails: jest.fn().mockResolvedValue({ decimals: 4 }),
+    getTokenStandardAndDetails: jest
+      .fn()
+      .mockResolvedValue({ decimals: 4, standard: 'ERC20' }),
   };
 });
+
+jest.mock(
+  '../../../../../../hooks/useTrackERC20WithoutDecimalInformation',
+  () => {
+    return jest.fn();
+  },
+);
 
 describe('PermitSimulationValueDisplay', () => {
   it('renders component correctly', async () => {
@@ -30,20 +40,19 @@ describe('PermitSimulationValueDisplay', () => {
     });
   });
 
-  it('renders component correctly for NFT token', async () => {
+  it('should invoke method to track missing decimal information for ERC20 tokens', async () => {
     const mockStore = configureMockStore([])(mockState);
 
     await act(async () => {
-      const { container, findByText } = renderWithProvider(
+      renderWithProvider(
         <PermitSimulationValueDisplay
           tokenContract="0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
-          tokenId="4321"
+          value="4321"
         />,
         mockStore,
       );
 
-      expect(await findByText('#4321')).toBeInTheDocument();
-      expect(container).toMatchSnapshot();
+      expect(useTrackERC20WithoutDecimalInformation).toHaveBeenCalled();
     });
   });
 });
