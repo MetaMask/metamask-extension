136603fabb3d465df6a9a100c75998f1e6a0a203
Guillaume Roux <hello@guillaumerx.fr>
2024-10-18 16:41:52 +0200

    remove unused import

--- ui/components/app/snaps/snap-ui-link/snap-ui-link.js
+++ ui/components/app/snaps/snap-ui-link/snap-ui-link.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import {
   AlignItems,
-  BlockSize,
   Display,
   FlexDirection,
 } from '../../../../helpers/constants/design-system';
