ffce58d1a362b2228ff6a25300501f3155003395
Danica Shen <zhaodanica@gmail.com>
2024-10-19 23:53:44 +0100

    feat(1852): add eventId to sentry feedback report

--- test/e2e/helpers.js
+++ test/e2e/helpers.js
@@ -1262,6 +1262,9 @@ async function openMenuSafe(driver) {
   }
 }
 
+// https://sentry.io/api/0000000/envelope/?sentry_key=fake&sentry_version=7&sentry_client=sentry.javascript.browser%2F8.33.1
+const sentryRegEx = /^https:\/\/sentry\.io\/api\/\d+\/envelope/gu;
+
 module.exports = {
   DAPP_HOST_ADDRESS,
   DAPP_URL,
@@ -1334,4 +1337,5 @@ module.exports = {
   getSelectedAccountAddress,
   tempToggleSettingRedesignedConfirmations,
   openMenuSafe,
+  sentryRegEx,
 };
--- test/e2e/page-objects/pages/error-page.ts
+++ test/e2e/page-objects/pages/error-page.ts
@@ -2,6 +2,12 @@ import { Driver } from '../../webdriver/driver';
 import HeaderNavbar from './header-navbar';
 import SettingsPage from './settings-page';
 import DevelopOptionsPage from './developer-options-page';
+import { MockedEndpoint } from 'mockttp';
+import { strict as assert } from 'assert';
+
+const WAIT_FOR_SENTRY_MS = 5000;
+const FEEDBACK_MESSAGE =
+  'Message: Unable to find value of key "developerOptions" for locale "en"';
 
 class ErrorPage {
   private readonly driver: Driver;
@@ -17,10 +23,20 @@ class ErrorPage {
   private readonly sendReportToSentryButton =
     '[data-testid="error-page-describe-what-happened-button"]';
 
-  private readonly sentryReportForm = '#sentry-feedback';
+  private readonly sentryReportForm =
+    '[data-testid="error-page-sentry-feedback-modal"]';
 
   private readonly contactSupportButton =
     '[data-testid="error-page-contact-support-button"]';
+
+  private readonly sentryFeedbackTextarea =
+    '[data-testid="error-page-sentry-feedback-textarea"]';
+
+  private readonly sentryFeedbackSubmitButton =
+    '[data-testid="error-page-sentry-feedback-submit-button"]';
+
+  private readonly sentryFeedbackSuccessModal =
+    '[data-testid="error-page-sentry-feedback-success-modal"]';
 
   constructor(driver: Driver) {
     this.driver = driver;
@@ -59,6 +75,10 @@ class ErrorPage {
     console.log(`Open sentry user feedback form in error page`);
     await this.driver.clickElement(this.sendReportToSentryButton);
     await this.driver.waitForSelector(this.sentryReportForm);
+    await this.driver.fill(this.sentryFeedbackTextarea, FEEDBACK_MESSAGE);
+    await this.driver.clickElementAndWaitToDisappear(
+      this.sentryFeedbackSubmitButton,
+    );
   }
 
   async contactAndValidateMetaMaskSupport(): Promise<void> {
@@ -67,6 +87,31 @@ class ErrorPage {
     await this.driver.clickElement(this.contactSupportButton);
     // metamask, help page
     await this.driver.waitUntilXWindowHandles(2);
+  }
+
+  async waitForSentryRequestSent(
+    mockedEndpoint: MockedEndpoint,
+  ): Promise<void> {
+    console.log(`Wait for sentry request to be sent`);
+    await this.driver.wait(async () => {
+      const isPending = await mockedEndpoint.isPending();
+      return !isPending;
+    }, WAIT_FOR_SENTRY_MS);
+    const [mockedRequest] = await mockedEndpoint.getSeenRequests();
+    const mockTextBody = (await mockedRequest.body.getText()).split('\n');
+    const mockJsonBody = JSON.parse(mockTextBody[2]);
+    const feedbackBody = mockJsonBody.contexts.feedback['message'];
+    const feedbackEventId =
+      mockJsonBody.contexts.feedback['associated_event_id'];
+    assert.equal(feedbackBody, FEEDBACK_MESSAGE);
+    assert.ok(
+      feedbackEventId,
+      'feedbackEventId should not be null or undefined',
+    );
+  }
+
+  async waitForSentrySuccessModal(): Promise<void> {
+    await this.driver.waitForSelector(this.sentryFeedbackSuccessModal);
   }
 }
 
