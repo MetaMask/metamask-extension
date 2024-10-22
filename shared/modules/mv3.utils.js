44e3c7f419ef4d60e57deb131ba43ff6994c5719
Prithpal Sooriya <prithpal.sooriya@consensys.net>
2024-10-22 16:09:46 +0100

    fix: resolve storybook unable to get manifest

--- shared/modules/mv3.utils.js
+++ shared/modules/mv3.utils.js
@@ -1,7 +1,10 @@
+import browser from 'webextension-polyfill';
 /* eslint-disable import/unambiguous -- Not an external module and not of concern */
 
 const runtimeManifest =
-  global.chrome?.runtime.getManifest() || global.browser?.runtime.getManifest();
+  global.chrome?.runtime?.getManifest() ||
+  global.browser?.runtime?.getManifest() ||
+  browser.runtime.getManifest();
 
 /**
  * A boolean indicating whether the manifest of the current extension is set to manifest version 3.
@@ -10,7 +13,7 @@ const runtimeManifest =
  * If this function is running in Node doing a build job, it will read process.env.ENABLE_MV3.
  * If this function is running in Node doing an E2E test, it will `fs.readFileSync` the manifest.json file.
  */
-const isManifestV3 = runtimeManifest
+export const isManifestV3 = runtimeManifest
   ? runtimeManifest.manifest_version === 3
   : // Our build system sets this as a boolean, but in a Node.js context (e.g. unit tests) it can be a string
     process.env.ENABLE_MV3 === true ||
@@ -22,7 +25,7 @@ const isManifestV3 = runtimeManifest
  * This is only available in when the manifest is version 3, and only in chromium
  * versions 109 and higher. As of June 7, 2024, it is not available in firefox.
  */
-const isOffscreenAvailable = Boolean(global.chrome?.offscreen);
+export const isOffscreenAvailable = Boolean(global.chrome?.offscreen);
 
 /**
  * A boolean indicating whether the current extension's manifest is version 3
@@ -30,10 +33,5 @@ const isOffscreenAvailable = Boolean(global.chrome?.offscreen);
  * happen to users on MetaMask versions 11.16.7 and higher, who are using a
  * chromium browser with a version below 109.
  */
-const isMv3ButOffscreenDocIsMissing = isManifestV3 && !isOffscreenAvailable;
-
-module.exports = {
-  isManifestV3,
-  isOffscreenAvailable,
-  isMv3ButOffscreenDocIsMissing,
-};
+export const isMv3ButOffscreenDocIsMissing =
+  isManifestV3 && !isOffscreenAvailable;
