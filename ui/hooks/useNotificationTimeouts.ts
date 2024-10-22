473a4ff06d708e396d1f3b0a6145075d19371dd1
Hassan Malik <hbmalik88@gmail.com>
2024-10-22 14:59:51 -0400

    lint fix

--- ui/hooks/useNotificationTimeouts.ts
+++ ui/hooks/useNotificationTimeouts.ts
@@ -1,8 +1,8 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { INotification } from '@metamask/notification-services-controller/notification-services';
 import { deleteNotificationsById } from '../store/actions';
 import { NOTIFICATIONS_EXPIRATION_DELAY } from '../helpers/constants/notifications';
-import { INotification } from '@metamask/notification-services-controller/notification-services';
 
 export const useSnapNotificationTimeouts = (notifications: INotification[]) => {
   const timerMap = new Map<string, NodeJS.Timeout>();
@@ -13,19 +13,19 @@ export const useSnapNotificationTimeouts = (notifications: INotification[]) => {
       await dispatch(deleteNotificationsById([id]));
     }, NOTIFICATIONS_EXPIRATION_DELAY);
     timerMap.set(id, timerId);
-  }
+  };
 
   const clearTimeouts = () => {
     [...timerMap.keys()].forEach((id) => {
-      if (!notifications.find(notification => notification.id === id)) {
+      if (!notifications.find((notification) => notification.id === id)) {
         clearTimeout(timerMap.get(id));
       }
-    })
-  }
+    });
+  };
 
   useEffect(() => {
-    return () => clearTimeouts()
-  }, [])
+    return () => clearTimeouts();
+  }, []);
 
   return { setNotificationTimeout };
-}
+};
