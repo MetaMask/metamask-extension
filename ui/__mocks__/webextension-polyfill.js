e045737eabcf7260599d3b477f63d09927d884e7
Prithpal Sooriya <prithpal.sooriya@consensys.net>
2024-10-22 17:41:21 +0100

    fix: fix the mocks to polyfill optional data

--- ui/__mocks__/webextension-polyfill.js
+++ ui/__mocks__/webextension-polyfill.js
@@ -3,9 +3,9 @@ const getManifest = () => ({ manifest_version: 3 });
 // Polyfill chrome.runtime for environments that do not support it
 // E.g. Storybook
 global.chrome = {
-  ...global.chrome,
+  ...global?.chrome,
   runtime: {
-    ...global.chrome.runtime,
+    ...global?.chrome?.runtime,
     getManifest,
   },
 };
