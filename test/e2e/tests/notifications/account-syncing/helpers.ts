3959c70c0535c13e645ba9b18e592060aa903172
Mathieu Artu <mathieu.artu@consensys.net>
2024-10-22 20:37:53 +0200

    fix: lint

--- test/e2e/tests/notifications/account-syncing/helpers.ts
+++ test/e2e/tests/notifications/account-syncing/helpers.ts
@@ -1,3 +1,3 @@
-import { isManifestV3 } from "../../../../../shared/modules/mv3.utils";
+import { isManifestV3 } from '../../../../../shared/modules/mv3.utils';
 
 export const IS_ACCOUNT_SYNCING_ENABLED = isManifestV3;
