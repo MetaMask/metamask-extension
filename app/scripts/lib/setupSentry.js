cb6ccccc8ed9763d57ca43ad4d89b70cad0bcd79
Victor Thomas <10986371+vthomas13@users.noreply.github.com>
2024-10-23 12:54:49 -0400

    adding sentry tag

--- app/scripts/lib/setupSentry.js
+++ app/scripts/lib/setupSentry.js
@@ -58,7 +58,6 @@ export default function setupSentry() {
     .then((extensionInfo) => {
       if (extensionInfo.installType) {
         installType = extensionInfo.installType;
-        Sentry.setTag('installType', installType);
       }
     })
     .catch((error) => {
@@ -425,12 +424,17 @@ export function rewriteReport(report) {
     if (!report.extra) {
       report.extra = {};
     }
-
-    report.extra.appState = appState;
-    if (browser.runtime && browser.runtime.id) {
-      report.extra.extensionId = browser.runtime.id;
+    if (!report.tags) {
+      report.tags = {};
     }
-    report.extra.installType = installType;
+
+    Object.assign(report.extra, {
+      appState,
+      installType,
+      extensionId: browser.runtime?.id,
+    });
+
+    report.tags.installType = installType;
   } catch (err) {
     log('Error rewriting report', err);
   }
