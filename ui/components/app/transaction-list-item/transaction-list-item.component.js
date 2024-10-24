f81b0a2f2d1d3e07e562f4054f94f0fde5547626
IF <139582705+infiniteflower@users.noreply.github.com>
2024-10-24 17:29:08 -0400

    fix: properly show the bridge status or the confirmed status

--- ui/components/app/transaction-list-item/transaction-list-item.component.js
+++ ui/components/app/transaction-list-item/transaction-list-item.component.js
@@ -351,11 +351,25 @@ function TransactionListItemInner({
           ///: END:ONLY_INCLUDE_IF
         }
         subtitle={
-          isBridgeTx && !isBridgeComplete ? (
+          isBridgeTx && isBridgeComplete === false ? (
             <div>
               <div>status: {bridgeTxHistoryItem?.status?.status}</div>
-              <div>tx 1: {bridgeTxHistoryItem?.status?.srcChain.txHash}</div>
-              <div>tx 2: {bridgeTxHistoryItem?.status?.destChain.txHash}</div>
+              <div>
+                tx 1:{' '}
+                {`${transactionGroup.initialTransaction.hash.substring(
+                  0,
+                  6,
+                )}...`}
+                , {transactionGroup.initialTransaction.status}
+              </div>
+              <div>
+                tx 2:{' '}
+                {`${bridgeTxHistoryItem?.status?.destChain.txHash?.substring(
+                  0,
+                  6,
+                )}...`}
+                ...
+              </div>
             </div>
           ) : (
             <TransactionStatusLabel
