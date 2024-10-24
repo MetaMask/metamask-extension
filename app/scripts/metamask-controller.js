fd6868f0d28bd88b19ed3df6ea099daaa3f1cbf5
Salah-Eddine Saakoun <salah-eddine.saakoun@consensys.net>
2024-10-24 17:09:04 +0200

    feat: convert MetaMetricsController to typescript

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -755,31 +755,25 @@ export default class MetamaskController extends EventEmitter {
     });
 
     this.metaMetricsController = new MetaMetricsController({
+      initState: initState.MetaMetricsController,
       segment,
-      onPreferencesStateChange: preferencesMessenger.subscribe.bind(
-        preferencesMessenger,
-        'PreferencesController:stateChange',
-      ),
       preferencesControllerState: {
         currentLocale: this.preferencesController.state.currentLocale,
         selectedAddress: this.preferencesController.state.selectedAddress,
       },
+      onPreferencesStateChange: preferencesMessenger.subscribe.bind(
+        preferencesMessenger,
+        'PreferencesController:stateChange',
+      ),
       onNetworkDidChange: networkControllerMessenger.subscribe.bind(
         networkControllerMessenger,
         'NetworkController:networkDidChange',
       ),
-      getNetworkIdentifier: () => {
-        const { type, rpcUrl } = getProviderConfig({
-          metamask: this.networkController.state,
-        });
-        return type === NETWORK_TYPES.RPC ? rpcUrl : type;
-      },
       getCurrentChainId: () =>
         getCurrentChainId({ metamask: this.networkController.state }),
       version: process.env.METAMASK_VERSION,
       environment: process.env.METAMASK_ENVIRONMENT,
       extension: this.extension,
-      initState: initState.MetaMetricsController,
       captureException,
     });
 
@@ -6317,6 +6311,7 @@ export default class MetamaskController extends EventEmitter {
    * @private
    */
   privateSendUpdate() {
+    console.log('salah', this.getState());
     this.emit('update', this.getState());
   }
 
