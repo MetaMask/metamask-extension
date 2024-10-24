693443487c8d196647dbdb47fedc98445dba602b
Nicholas Gambino <nicholas.gambino@consensys.net>
2024-10-24 09:27:33 -0700

    fix: Clean up filterAssets logic

--- ui/components/app/assets/util/filter.ts
+++ ui/components/app/assets/util/filter.ts
@@ -2,8 +2,8 @@ import { get } from 'lodash';
 
 export type FilterCriteria = {
   key: string;
-  opts?: Record<string, FilterType>; // Use opts for range, inclusion, etc.
-  filterCallback?: FilterCallbackKeys; // Specify the type of filter: 'range', 'inclusive', etc.
+  opts: Record<string, FilterType>; // Use opts for range, inclusion, etc.
+  filterCallback: FilterCallbackKeys; // Specify the type of filter: 'range', 'inclusive', etc.
 };
 
 export type FilterType = string | number | boolean | Date;
@@ -24,44 +24,34 @@ function getNestedValue<T>(obj: T, keyPath: string): FilterType {
   return get(obj, keyPath);
 }
 
-export function filterAssets<T>(array: T[], criteria: FilterCriteria[]): T[] {
+export function filterAssets<T>(assets: T[], criteria: FilterCriteria[]): T[] {
   if (criteria.length === 0) {
-    return array;
+    return assets;
   }
 
-  return array.filter((item) => {
-    return criteria.every((criterion) => {
-      const { key, opts, filterCallback } = criterion;
-      const nestedValue = getNestedValue(item, key);
+  return assets.filter((asset) =>
+    criteria.every(({ key, opts, filterCallback }) => {
+      const nestedValue = getNestedValue(asset, key);
 
-      if (filterCallback && opts) {
-        switch (filterCallback) {
-          case 'inclusive':
-            if (typeof nestedValue === 'string') {
-              return filterCallbacks.inclusive(
-                nestedValue,
-                opts as Record<string, boolean>,
-              );
-            }
-            return false;
-          case 'range':
-            // Type guard to ensure nestedValue is a number
-            if (typeof nestedValue === 'number') {
-              return filterCallbacks.range(
-                nestedValue,
-                opts as {
-                  min: number;
-                  max: number;
-                },
-              );
-            }
-            return false; // If not a number, range filtering cannot be applied
-          default:
-            return true;
-        }
+      // If there's no callback or options, exit early and don't filter based on this criterion.
+      if (!filterCallback || !opts) {
+        return true;
       }
 
-      return true; // Default case, return true to include item if no conditions are met
-    });
-  });
+      switch (filterCallback) {
+        case 'inclusive':
+          return filterCallbacks.inclusive(
+            nestedValue as string,
+            opts as Record<string, boolean>,
+          );
+        case 'range':
+          return filterCallbacks.range(
+            nestedValue as number,
+            opts as { min: number; max: number },
+          );
+        default:
+          return true;
+      }
+    }),
+  );
 }
