9d42c23b6575a416af849c5fd1f2336cefb2ee18
Vinicius Stevam <vinicius.stevam@consensys.io>
2024-10-25 05:46:50 +0100

    test e2e

--- test/e2e/tests/request-queuing/dapp1-send-dapp2-signTypedData.spec.js
+++ test/e2e/tests/request-queuing/dapp1-send-dapp2-signTypedData.spec.js
@@ -145,8 +145,9 @@ describe('Request Queuing Dapp 1, Switch Tx -> Dapp 2 Send Tx', function () {
 
         await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
 
+        await driver.delay(regularDelayMs);
         // Check correct network on the signTypedData confirmation.
-        await driver.waitForSelector({
+        await driver.findElement({
           css: '[data-testid="signature-request-network-display"]',
           text: 'Localhost 8546',
         });
