8e6dc5a5c5230e0035e8123a6a3224b1a999cfcf
Micaela Estabillo <micaela.estabillo@consensys.net>
2024-10-25 13:58:46 -0700

    chore: format token amounts

--- ui/pages/bridge/utils/quote.ts
+++ ui/pages/bridge/utils/quote.ts
@@ -3,6 +3,7 @@ import { BigNumber } from 'bignumber.js';
 import { getAddress } from 'ethers/lib/utils';
 import { calcTokenAmount } from '../../../../shared/lib/transactions-controller-utils';
 import { QuoteResponse, QuoteRequest, Quote } from '../types';
+import { formatCurrency } from '../../../helpers/utils/confirm-tx.util';
 
 export const isValidQuoteRequest = (
   partialRequest: Partial<QuoteRequest>,
@@ -180,3 +181,12 @@ export const calcCost = (
 
 export const formatEtaInMinutes = (estimatedProcessingTimeInSeconds: number) =>
   (estimatedProcessingTimeInSeconds / 60).toFixed();
+
+export const formatTokenAmount = (
+  amount: BigNumber,
+  symbol: string,
+  precision: number = 2,
+) => `${amount.toFixed(precision)} ${symbol}`;
+
+export const formatFiatAmount = (amount: BigNumber | null, currency: string) =>
+  amount ? formatCurrency(amount.toString(), currency) : undefined;
