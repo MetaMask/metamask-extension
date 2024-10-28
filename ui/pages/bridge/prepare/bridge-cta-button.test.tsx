d6d6eb7986b5e132a02ddc6f54debfddf8979152
Micaela Estabillo <micaela.estabillo@consensys.net>
2024-10-28 14:08:09 -0700

    fix: test data import

--- ui/pages/bridge/prepare/bridge-cta-button.test.tsx
+++ ui/pages/bridge/prepare/bridge-cta-button.test.tsx
@@ -3,7 +3,7 @@ import { renderWithProvider } from '../../../../test/jest';
 import configureStore from '../../../store/store';
 import { createBridgeMockStore } from '../../../../test/jest/mock-store';
 import { CHAIN_IDS } from '../../../../shared/constants/network';
-import { mockBridgeQuotesNativeErc20 } from '../../../../test/data/bridge/mock-quotes-native-erc20';
+import mockBridgeQuotesNativeErc20 from '../../../../test/data/bridge/mock-quotes-native-erc20.json';
 // TODO: Remove restricted import
 // eslint-disable-next-line import/no-restricted-paths
 import { RequestStatus } from '../../../../app/scripts/controllers/bridge/constants';
