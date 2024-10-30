0633b8e03141d0284f84b3560faeca264b04d373
Nicholas Gambino <nicholas.gambino@consensys.net>
2024-10-30 14:39:16 -0700

    feat: Introduce useTokenListPolling hook

---
+++ ui/hooks/useTokenListPolling.ts
@@ -0,0 +1,25 @@
+import { useSelector } from 'react-redux';
+import { getNetworkConfigurationsByChainId } from '../selectors';
+import {
+  tokenListStartPolling,
+  tokenListStopPollingByPollingToken,
+} from '../store/actions';
+import useMultiPolling from './useMultiPolling';
+
+const useTokenListPolling = () => {
+  const networkConfigurations = useSelector(getNetworkConfigurationsByChainId);
+
+  const chainIds = Object.keys(networkConfigurations);
+
+  useMultiPolling({
+    startPolling: tokenListStartPolling,
+    stopPollingByPollingToken: tokenListStopPollingByPollingToken,
+    input: [chainIds],
+  });
+
+  return {
+    // TODO: Eventually return currency rates here. UI elements will
+    // consume them from this hook instead of a selector directly.
+  };
+};
+export default useTokenListPolling;
