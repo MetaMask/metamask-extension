73a22d3b48d26d3c3978570c1cc2fd10733765e0
Norbert Elter <72046715+itsyoboieltr@users.noreply.github.com>
2024-10-30 23:37:46 +0400

    fix: placement

--- development/webpack/utils/plugins/SelfInjectPlugin/index.ts
+++ development/webpack/utils/plugins/SelfInjectPlugin/index.ts
@@ -1,11 +1,23 @@
 import { dirname, relative } from 'node:path';
 import { ModuleFilenameHelpers, Compilation, sources } from 'webpack';
 import { validate } from 'schema-utils';
-import { getRuntimeURLExpression } from './helpers';
 import { schema } from './schema';
 import type { SelfInjectPluginOptions, Source, Compiler } from './types';
 
 export { type SelfInjectPluginOptions } from './types';
+
+/**
+ * Generates a runtime URL expression for a given path.
+ *
+ * This function constructs a URL string using the `runtime.getURL` method
+ * from either the `globalThis.browser` or `chrome` object, depending on
+ * which one is available in the global scope.
+ *
+ * @param path - The path of the runtime URL.
+ * @returns The constructed runtime URL string.
+ */
+const getRuntimeURLExpression = (path: string) =>
+  `(globalThis.browser||chrome).runtime.getURL(${JSON.stringify(path)})`;
 
 /**
  * Default options for the SelfInjectPlugin.
