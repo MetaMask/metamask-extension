552273b4db2583e08bb792c652020cf2b46367b5
Mark Stacey <markjstacey@gmail.com>
2024-10-31 11:28:55 -0230

    fix: Fix left-aligned fullscreen UI
    The Home screen was recently updated to make the overview left-aligned.
    However the fullscreen UI was not considered, and it ended up looking
    ugly/broken.
    
    The fullscreen UI has been updated to be centered, as it was before.
    The Home screen remains left-aligned in the popup.
    
    Fixes #27593

--- ui/components/app/wallet-overview/wallet-overview.js
+++ ui/components/app/wallet-overview/wallet-overview.js
@@ -2,9 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
+// TODO: Move this function to shared
+// eslint-disable-next-line import/no-restricted-paths
+import { getEnvironmentType } from '../../../../app/scripts/lib/util';
+import { ENVIRONMENT_TYPE_FULLSCREEN } from '../../../../shared/constants/app';
+
 const WalletOverview = ({ balance, buttons, className }) => {
+  const overviewClasses = ['wallet-overview'];
+  if (getEnvironmentType() === ENVIRONMENT_TYPE_FULLSCREEN) {
+    overviewClasses.push('wallet-overview-fullscreen');
+  }
+
   return (
-    <div className={classnames('wallet-overview', className)}>
+    <div className={classnames(...overviewClasses, className)}>
       <div className="wallet-overview__balance">{balance}</div>
       <div className="wallet-overview__buttons">{buttons}</div>
     </div>
