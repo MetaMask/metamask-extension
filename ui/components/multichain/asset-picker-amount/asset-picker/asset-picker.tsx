1e53a1be9023c108eebca44aa17fb1fe9b528a10
Micaela Estabillo <micaela.estabillo@consensys.net>
2024-11-01 12:24:25 -0700

    chore: enable overriding asset-picker button layout

--- ui/components/multichain/asset-picker-amount/asset-picker/asset-picker.tsx
+++ ui/components/multichain/asset-picker-amount/asset-picker/asset-picker.tsx
@@ -49,6 +49,10 @@ import {
 const ELLIPSIFY_LENGTH = 13; // 6 (start) + 4 (end) + 3 (...)
 
 export type AssetPickerProps = {
+  children?: (
+    onClick: () => void,
+    networkImageSrc?: string,
+  ) => React.ReactElement; // Overrides default button
   asset?:
     | ERC20Asset
     | NativeAsset
@@ -76,6 +80,7 @@ export type AssetPickerProps = {
 
 // A component that lets the user pick from a list of assets.
 export function AssetPicker({
+  children,
   header,
   asset,
   onAssetChange,
@@ -122,7 +127,22 @@ export function AssetPicker({
     return undefined;
   };
 
+  const networkImageSrc =
+    selectedNetwork?.chainId &&
+    CHAIN_ID_TO_NETWORK_IMAGE_URL_MAP[
+      selectedNetwork.chainId as keyof typeof CHAIN_ID_TO_NETWORK_IMAGE_URL_MAP
+    ];
+
   const [isSelectingNetwork, setIsSelectingNetwork] = useState(false);
+
+  const handleButtonClick = () => {
+    if (networkProps && !networkProps.network) {
+      setIsSelectingNetwork(true);
+    } else {
+      setShowAssetPickerModal(true);
+    }
+    onClick?.();
+  };
 
   return (
     <>
@@ -171,35 +191,28 @@ export function AssetPicker({
         customTokenListGenerator={customTokenListGenerator}
       />
 
-      <ButtonBase
-        data-testid="asset-picker-button"
-        className="asset-picker"
-        disabled={isDisabled}
-        display={Display.Flex}
-        alignItems={AlignItems.center}
-        gap={2}
-        padding={2}
-        paddingLeft={2}
-        paddingRight={2}
-        justifyContent={isNFT ? JustifyContent.spaceBetween : undefined}
-        backgroundColor={BackgroundColor.transparent}
-        onClick={() => {
-          if (networkProps && !networkProps.network) {
-            setIsSelectingNetwork(true);
-          } else {
-            setShowAssetPickerModal(true);
-          }
-          onClick?.();
-        }}
-        endIconName={IconName.ArrowDown}
-        endIconProps={{
-          color: IconColor.iconDefault,
-          marginInlineStart: 0,
-          display: isDisabled ? Display.None : Display.InlineBlock,
-        }}
-        title={handleAssetPickerTitle()}
-      >
-        {asset ? (
+      {children?.(handleButtonClick, networkImageSrc) || (
+        <ButtonBase
+          data-testid="asset-picker-button"
+          className="asset-picker"
+          disabled={isDisabled}
+          display={Display.Flex}
+          alignItems={AlignItems.center}
+          gap={2}
+          padding={2}
+          paddingLeft={2}
+          paddingRight={2}
+          justifyContent={isNFT ? JustifyContent.spaceBetween : undefined}
+          backgroundColor={BackgroundColor.transparent}
+          onClick={handleButtonClick}
+          endIconName={IconName.ArrowDown}
+          endIconProps={{
+            color: IconColor.iconDefault,
+            marginInlineStart: 0,
+            display: isDisabled ? Display.None : Display.InlineBlock,
+          }}
+          title={handleAssetPickerTitle()}
+        >
           <Box display={Display.Flex} alignItems={AlignItems.center} gap={3}>
             <Box display={Display.Flex}>
               <BadgeWrapper
@@ -207,12 +220,7 @@ export function AssetPicker({
                   <AvatarNetwork
                     size={AvatarNetworkSize.Xs}
                     name={selectedNetwork?.name ?? ''}
-                    src={
-                      selectedNetwork?.chainId &&
-                      CHAIN_ID_TO_NETWORK_IMAGE_URL_MAP[
-                        selectedNetwork.chainId as keyof typeof CHAIN_ID_TO_NETWORK_IMAGE_URL_MAP
-                      ]
-                    }
+                    src={networkImageSrc}
                     backgroundColor={
                       Object.entries({
                         [GOERLI_DISPLAY_NAME]: BackgroundColor.goerli,
@@ -269,12 +277,8 @@ export function AssetPicker({
               )}
             </Tooltip>
           </Box>
-        ) : (
-          <Text className="asset-picker__fallback" variant={TextVariant.bodyMd}>
-            {t('swapSelectToken')}
-          </Text>
-        )}
-      </ButtonBase>
+        </ButtonBase>
+      )}
     </>
   );
 }
