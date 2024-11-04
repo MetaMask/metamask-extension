bb86ca49efda59a6955957eaaef7e881d7c12c6e
Priya Narayanaswamy <priya.narayanaswamy@consensys.net>
2024-11-04 18:18:01 +0700

    test: fix failing tests

--- test/e2e/tests/confirmations/signatures/sign-typed-data-v3.spec.ts
+++ test/e2e/tests/confirmations/signatures/sign-typed-data-v3.spec.ts
@@ -48,20 +48,16 @@ describe('Confirmation Signature - Sign Typed Data V3 @no-mmi', function (this: 
         await copyAddressAndPasteWalletAddress(driver);
         await assertPastedAddress(driver);
         await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
+        await driver.delay(1000);
 
         await assertInfoValues(driver);
         await scrollAndConfirmAndAssertConfirm(driver);
-        await driver.delay(1000);
 
         await assertAccountDetailsMetrics(
           driver,
           mockedEndpoints as MockedEndpoint[],
           'eth_signTypedData_v3',
         );
-        await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
-
-        await assertInfoValues(driver);
-        await scrollAndConfirmAndAssertConfirm(driver);
         await assertSignatureConfirmedMetrics({
           driver,
           mockedEndpoints: mockedEndpoints as MockedEndpoint[],
