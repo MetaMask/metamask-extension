efa96d1aeb774348e00a7930e982ef86830362f9
Prithpal Sooriya <prithpal.sooriya@consensys.net>
2024-11-04 15:17:16 +0000

    fix: fix potential onboarding crash
    fix potential bug where onboarding crashes due to failing to sign in or enable/disable profile sync

--- ui/hooks/metamask-notifications/useCreateSession.ts
+++ ui/hooks/metamask-notifications/useCreateSession.ts
@@ -31,6 +31,14 @@ export function useCreateSession(): {
   );
 
   const createSession = useCallback(async () => {
+    const safeDispatchDisableProfileSync = async () => {
+      try {
+        await dispatch(disableProfileSyncing());
+      } catch {
+        // Do Nothing
+      }
+    };
+
     // If the user is already signed in, no need to create a new session
     if (isSignedIn) {
       return;
@@ -47,7 +55,7 @@ export function useCreateSession(): {
         await dispatch(performSignIn());
       } catch (e) {
         // If an error occurs during the sign-in process, disable profile syncing
-        await dispatch(disableProfileSyncing());
+        await safeDispatchDisableProfileSync();
         const errorMessage =
           e instanceof Error ? e.message : JSON.stringify(e ?? '');
         log.error(errorMessage);
