316da04ac09575c33f2490c3f10a5c62f5094630
Brian Bergeron <brian.e.bergeron@gmail.com>
2024-11-03 17:51:54 -0800

    tsx

--- ui/contexts/assetPolling.tsx
+++ ui/contexts/assetPolling.tsx
@@ -1,18 +1,13 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { ReactNode } from 'react';
 import useCurrencyRatePolling from '../hooks/useCurrencyRatePolling';
 import useTokenRatesPolling from '../hooks/useTokenRatesPolling';
 
 // This provider is a step towards making controller polling fully UI based.
 // Eventually, individual UI components will call the use*Polling hooks to
 // poll and return particular data. This polls globally in the meantime.
-export const AssetPollingProvider = ({ children }) => {
+export const AssetPollingProvider = ({ children }: { children: ReactNode }) => {
   useCurrencyRatePolling();
   useTokenRatesPolling();
 
   return <>{children}</>;
-};
-
-AssetPollingProvider.propTypes = {
-  children: PropTypes.node,
 };
