4d5fef55ae4c21cc566c25721db1a41183ce8ef9
IF <139582705+infiniteflower@users.noreply.github.com>
2024-11-05 10:46:25 -0500

    chore: color the nodes and edges properly of the progress bar

--- ui/pages/bridge/transaction-details/step-progress-item.tsx
+++ ui/pages/bridge/transaction-details/step-progress-item.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { StatusTypes } from '../../../../app/scripts/controllers/bridge-status/types';
-import { Icon, IconName } from '../../../components/component-library';
+import {
+  Icon,
+  IconName,
+  IconSize,
+} from '../../../components/component-library';
 import { Color, IconColor } from '../../../helpers/constants/design-system';
 import HollowCircle from './hollow-circle';
 
-const ICON_COLOR = IconColor.primaryDefault;
+const ICON_SIZE = IconSize.Xs;
+const ICON_STYLE = { marginTop: '0.25rem', marginBottom: '0.25rem' };
 
-const VerticalLine = () => (
+const VerticalLine = ({ color }: { color: IconColor }) => (
   <div
     style={{
       height: '46px',
       width: '1px',
-      backgroundColor: `var(--color-${Color.iconMuted})`,
+      backgroundColor: `var(--color-${color})`,
     }}
   />
 );
@@ -19,26 +24,47 @@ const VerticalLine = () => (
 type StepsProgressItemProps = {
   stepStatus: StatusTypes | null;
   isLastItem: boolean;
+  isEdgeComplete: boolean;
 };
 
 export default function StepProgressItem({
   stepStatus,
   isLastItem,
+  isEdgeComplete,
 }: StepsProgressItemProps) {
   return (
     <>
-      {stepStatus === null && <HollowCircle color={ICON_COLOR} />}
+      {stepStatus === null && (
+        <HollowCircle
+          color={IconColor.iconMuted}
+          size={ICON_SIZE}
+          style={ICON_STYLE}
+        />
+      )}
       {stepStatus === StatusTypes.PENDING && (
         <Icon
           className="bridge-transaction-details__icon-loading" // Needed for animation
           name={IconName.Loading}
-          color={ICON_COLOR}
+          color={IconColor.primaryDefault}
+          size={ICON_SIZE}
+          style={ICON_STYLE}
         />
       )}
       {stepStatus === StatusTypes.COMPLETE && (
-        <Icon name={IconName.FullCircle} color={ICON_COLOR} />
+        <Icon
+          name={IconName.FullCircle}
+          color={IconColor.primaryDefault}
+          size={ICON_SIZE}
+          style={ICON_STYLE}
+        />
       )}
-      {!isLastItem && <VerticalLine />}
+      {!isLastItem && (
+        <VerticalLine
+          color={
+            isEdgeComplete ? IconColor.primaryDefault : IconColor.iconMuted
+          }
+        />
+      )}
     </>
   );
 }
