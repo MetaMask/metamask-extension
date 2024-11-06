3632f9b286e9c62ec2667490565f8bbe5211c1f1
Antonio Regadas <antonio.regadas@consensys.net>
2024-11-06 17:05:46 +0000

    chore: update imports and merge develop

--- app/scripts/snaps/preinstalled-snaps.ts
+++ app/scripts/snaps/preinstalled-snaps.ts
@@ -4,8 +4,10 @@ import EnsResolverSnap from '@metamask/ens-resolver-snap/dist/preinstalled-snap.
 ///: BEGIN:ONLY_INCLUDE_IF(build-flask)
 import AccountWatcherSnap from '@metamask/account-watcher/dist/preinstalled-snap.json';
 import BitcoinWalletSnap from '@metamask/bitcoin-wallet-snap/dist/preinstalled-snap.json';
-import SolanaWalletSnap from '@metamask/solana-wallet-snap/dist/preinstalled-snap.json';
 import PreinstalledExampleSnap from '@metamask/preinstalled-example-snap/dist/preinstalled-snap.json';
+///: END:ONLY_INCLUDE_IF
+///: BEGIN:ONLY_INCLUDE_IF(solana)
+import SolanaWalletSnap from '@metamask/solana-wallet-snap/dist/preinstalled-snap.json';
 ///: END:ONLY_INCLUDE_IF
 
 // The casts here are less than ideal but we expect the SnapController to validate the inputs.
@@ -15,8 +17,10 @@ const PREINSTALLED_SNAPS = Object.freeze<PreinstalledSnap[]>([
   ///: BEGIN:ONLY_INCLUDE_IF(build-flask)
   AccountWatcherSnap as PreinstalledSnap,
   BitcoinWalletSnap as unknown as PreinstalledSnap,
-  SolanaWalletSnap as unknown as PreinstalledSnap,
   PreinstalledExampleSnap as unknown as PreinstalledSnap,
+  ///: END:ONLY_INCLUDE_IF
+  ///: BEGIN:ONLY_INCLUDE_IF(solana)
+  SolanaWalletSnap as unknown as PreinstalledSnap,
   ///: END:ONLY_INCLUDE_IF
 ]);
 
