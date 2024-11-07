5987b17b17c15aa2dc54c42c8ac8452947a1675a
Antonio Regadas <antonio.regadas@consensys.net>
2024-11-07 11:48:36 +0000

    chore: clean up comment

--- ui/hooks/accounts/useMultichainClient.ts
+++ ui/hooks/accounts/useMultichainClient.ts
@@ -55,10 +55,6 @@ export class MultichainClient {
       scope,
     });
 
-    // NOTE: The account's balance is going to be tracked automatically on when the new account
-    // will be added to the Snap bridge keyring (see `BalancesController:#handleOnAccountAdded`).
-    // However, the balance won't be fetched right away. To workaround this, we trigger the
-    // fetch explicitly here (since we are already in a `async` call) and wait for it to be updated!
     await multichainUpdateBalance(account.id);
   }
 }
