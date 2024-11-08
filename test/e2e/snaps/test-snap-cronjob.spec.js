15b15bde647756c01ea586f8629f0259217ba2c3
HJetpoluru <harika.jetpoluru@consensys.net>
2024-11-08 09:24:29 -0500

    reverted to the origin code with additional wait

--- test/e2e/snaps/test-snap-cronjob.spec.js
+++ test/e2e/snaps/test-snap-cronjob.spec.js
@@ -10,9 +10,6 @@ const { TEST_SNAPS_WEBSITE_URL } = require('./enums');
 
 describe('Test Snap Cronjob', function () {
   it('can trigger a cronjob to open a dialog every minute', async function () {
-    const MAX_RETRIES = 3;
-    const WAIT_TIME = 65000; // 65 seconds
-
     await withFixtures(
       {
         fixtures: new FixtureBuilder().build(),
@@ -80,37 +77,20 @@ describe('Test Snap Cronjob', function () {
 
         await driver.delay(largeDelayMs);
 
-        for (let attempt = 1; attempt <= MAX_RETRIES; attempt++) {
-          try {
-            // Switch to dialog popup, wait for a maximum of 65 seconds
-            await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
+        // switch to dialog popup, wait for a maximum of 65 seconds
+        await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
 
-            // Look for the dialog popup to verify cronjob fired
-            await driver.waitForSelector(
-              {
-                css: '.snap-ui-renderer__content',
-                text: 'This dialog was triggered by a cronjob',
-              },
-              WAIT_TIME,
-            );
+        // look for the dialog popup to verify cronjob fired
+        await driver.waitForSelector({
+          css: '.snap-ui-renderer__content',
+          text: 'This dialog was triggered by a cronjob',
+        });
 
-            // Try to click on the Ok button and pass test if window closes
-            await driver.clickElementAndWaitForWindowToClose({
-              text: 'OK',
-              tag: 'button',
-            });
-
-            // If the above steps succeed, log success and break out of the loop
-            console.log('Dialog appeared and was handled successfully.');
-            break;
-          } catch (error) {
-            console.error(`Attempt ${attempt} failed: ${error.message}`);
-            if (attempt === MAX_RETRIES) {
-              // If the maximum number of retries is reached, throw an error
-              throw new Error('Failed to handle the dialog after 3 attempts.');
-            }
-          }
-        }
+        // try to click on the Ok button and pass test if window closes
+        await driver.clickElementAndWaitForWindowToClose({
+          text: 'OK',
+          tag: 'button',
+        });
       },
     );
   });
