6f16dad9df392512dbc13efce941067dbe350247
Ulisses Ferreira <ulisses@hey.com>
2024-11-11 12:12:12 +0000

    chore: solana changes to work locally

--- shared/lib/accounts/solana-wallet-snap.ts
+++ shared/lib/accounts/solana-wallet-snap.ts
@@ -3,7 +3,9 @@ import { SnapId } from '@metamask/snaps-sdk';
 // the Snap is being pre-installed only for Flask build (for the moment).
 import SolanaWalletSnap from '@metamask/solana-wallet-snap/dist/preinstalled-snap.json';
 
-export const SOLANA_WALLET_SNAP_ID: SnapId = SolanaWalletSnap.snapId as SnapId;
+// export const SOLANA_WALLET_SNAP_ID: SnapId = SolanaWalletSnap.snapId as SnapId;
+export const SOLANA_WALLET_SNAP_ID: SnapId =
+  'local:http://localhost:8080' as SnapId;
 
 export const SOLANA_WALLET_NAME: string =
   SolanaWalletSnap.manifest.proposedName;
