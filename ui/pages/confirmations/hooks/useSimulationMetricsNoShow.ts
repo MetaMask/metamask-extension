09928e8db057c6d6576269c4ddb2e5092c734f7e
digiwand <20778143+digiwand@users.noreply.github.com>
2024-11-13 03:29:07 +0700

    yarn lint:fix

--- ui/pages/confirmations/hooks/useSimulationMetricsNoShow.ts
+++ ui/pages/confirmations/hooks/useSimulationMetricsNoShow.ts
@@ -1,16 +1,26 @@
 import { useSelector } from 'react-redux';
-import { UseSimulationMetricsProps, useSimulationMetrics } from '../components/simulation-details/useSimulationMetrics';
+import {
+  UseSimulationMetricsProps,
+  useSimulationMetrics,
+} from '../components/simulation-details/useSimulationMetrics';
 import { selectUseTransactionSimulations } from '../selectors/preferences';
 
 /**
  * This hook is to capture simulation metrics in the case where the simulation UI is not visible
  * e.g. when a send transaction is initiated through the wallet itself.
  * (only exception is if the user had disabled it themselves).
+ *
+ * @param options0
+ * @param options0.enableMetrics
+ * @param options0.transactionId
  */
 export function useSimulationMetricsNoShow({
   enableMetrics,
   transactionId,
-}: { enableMetrics: boolean, transactionId: string }) {
+}: {
+  enableMetrics: boolean;
+  transactionId: string;
+}) {
   const useTransactionSimulations = useSelector(
     selectUseTransactionSimulations,
   );
