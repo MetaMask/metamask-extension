eadc49371bb5e7f6d1c133e46c1166f82bad102c
Jongsun Suh <jongsun.suh@icloud.com>
2024-11-12 13:09:11 -0500

    stash

--- app/scripts/metamask-controller-stores.ts
+++ app/scripts/metamask-controller-stores.ts
@@ -1,19 +1,31 @@
-import type {
-  CurrencyRateState,
-  NftControllerState,
-  TokenListState,
-  TokenRatesControllerState,
-  TokensControllerState,
-  RatesControllerState,
-  AccountTrackerControllerState,
+import {
+  type CurrencyRateState,
+  type NftControllerState,
+  type TokenListState,
+  type TokenRatesControllerState,
+  type TokensControllerState,
+  type RatesControllerState,
+  type AccountTrackerControllerState,
+  getDefaultNftControllerState,
+  getDefaultTokenRatesControllerState,
 } from '@metamask/assets-controllers';
-import type { KeyringControllerState } from '@metamask/keyring-controller';
-import type { AddressBookControllerState } from '@metamask/address-book-controller';
+import {
+  getDefaultKeyringState,
+  type KeyringControllerState,
+} from '@metamask/keyring-controller';
+import {
+  getDefaultAddressBookControllerState,
+  type AddressBookControllerState,
+} from '@metamask/address-book-controller';
 import type { ApprovalControllerState } from '@metamask/approval-controller';
 import type { EnsControllerState } from '@metamask/ens-controller';
 import type { PhishingControllerState } from '@metamask/phishing-controller';
+import { getDefaultPreferencesState } from '@metamask/preferences-controller';
 import type { AnnouncementControllerState } from '@metamask/announcement-controller';
-import type { NetworkState } from '@metamask/network-controller';
+import {
+  getDefaultNetworkControllerState,
+  type NetworkState,
+} from '@metamask/network-controller';
 import type { GasFeeState } from '@metamask/gas-fee-controller';
 import type {
   PermissionConstraint,
@@ -21,6 +33,7 @@ import type {
   SubjectMetadataControllerState,
 } from '@metamask/permission-controller';
 import type { SmartTransactionsControllerState } from '@metamask/smart-transactions-controller';
+import { getDefaultSmartTransactionsControllerState } from '@metamask/smart-transactions-controller/dist/SmartTransactionsController';
 import type { SelectedNetworkControllerState } from '@metamask/selected-network-controller';
 import type { LoggingControllerState } from '@metamask/logging-controller';
 import type { PermissionLogControllerState } from '@metamask/permission-log-controller';
@@ -64,14 +77,24 @@ import type { AccountOrderControllerState } from './controllers/account-order';
 import type { PreferencesControllerState } from './controllers/preferences-controller';
 import type { AppStateController } from './controllers/app-state-controller';
 import type { AlertController } from './controllers/alert-controller';
-import type { OnboardingControllerState } from './controllers/onboarding';
+import {
+  getDefaultOnboardingControllerState,
+  type OnboardingControllerState,
+} from './controllers/onboarding';
 import MetaMetricsController from './controllers/metametrics';
 import type { EncryptionPublicKeyControllerState } from './controllers/encryption-public-key';
 import AppMetadataController from './controllers/app-metadata';
-import type { DecryptMessageControllerState } from './controllers/decrypt-message';
+import {
+  getDefaultState as getDefaultDecryptMessageControllerState,
+  type DecryptMessageControllerState,
+} from './controllers/decrypt-message';
 import type { SwapsControllerState } from './controllers/swaps/swaps.types';
 import type { BridgeControllerState } from './controllers/bridge/types';
 import { MetaMetricsDataDeletionState } from './controllers/metametrics-data-deletion/metametrics-data-deletion';
+import { SENTRY_BACKGROUND_STATE } from './constants/sentry-state';
+import { getDefaultSwapsControllerState } from './controllers/swaps/swaps.constants';
+import { getDefaultAlertControllerState } from './controllers/alert-controller';
+import { getDefaultAccountTrackerControllerState } from './controllers/account-tracker-controller';
 
 export type ResetOnRestartStoresComposedState = {
   AccountTracker: AccountTrackerControllerState;
@@ -144,4 +167,261 @@ export type MemStoreControllersComposedState = Omit<
 > & {
   TxController: TransactionControllerState;
   QueuedRequestController: QueuedRequestControllerState;
+};
+
+type defaultComposedStateProperties = typeof SENTRY_BACKGROUND_STATE;
+
+export const defaultComposedState: MemStoreControllersComposedState = {
+  AccountsController: {
+    internalAccounts: {
+      accounts: {},
+      selectedAccount: '',
+    },
+  },
+  AccountTracker: getDefaultAccountTrackerControllerState(),
+  AddressBookController: getDefaultAddressBookControllerState(),
+  AlertController: getDefaultAlertControllerState(),
+  AnnouncementController: {
+    announcements: false,
+  },
+  AuthenticationController: {
+    isSignedIn: false,
+  },
+  NetworkOrderController: {
+    orderedNetworkList: [],
+  },
+  AccountOrderController: {
+    pinnedAccountList: [],
+    hiddenAccountList: [],
+  },
+  AppMetadataController: {
+    currentAppVersion: true,
+    currentMigrationVersion: true,
+    previousAppVersion: true,
+    previousMigrationVersion: true,
+  },
+  ApprovalController: {
+    approvalFlows: false,
+    pendingApprovals: false,
+    pendingApprovalCount: false,
+  },
+  AppStateController: {
+    browserEnvironment: true,
+    connectedStatusPopoverHasBeenShown: true,
+    currentPopupId: false,
+    onboardingDate: false,
+    currentExtensionPopupId: false,
+    defaultHomeActiveTabName: true,
+    fullScreenGasPollTokens: true,
+    hadAdvancedGasFeesSetPriorToMigration92_3: true,
+    nftsDetectionNoticeDismissed: true,
+    nftsDropdownState: true,
+    notificationGasPollTokens: true,
+    outdatedBrowserWarningLastShown: true,
+    popupGasPollTokens: true,
+    qrHardware: true,
+    recoveryPhraseReminderHasBeenShown: true,
+    recoveryPhraseReminderLastShown: true,
+    showBetaHeader: true,
+    showPermissionsTour: true,
+    showNetworkBanner: true,
+    showAccountBanner: true,
+    switchedNetworkDetails: false,
+    switchedNetworkNeverShowMessage: false,
+    showTestnetMessageInDropdown: true,
+    surveyLinkLastClickedOrClosed: true,
+    snapsInstallPrivacyWarningShown: true,
+    termsOfUseLastAgreed: true,
+    timeoutMinutes: true,
+    trezorModel: true,
+    usedNetworks: true,
+  },
+  MultichainBalancesController: {
+    balances: false,
+  },
+  BridgeController: {
+    bridgeState: {
+      bridgeFeatureFlags: {
+        extensionConfig: false,
+        extensionSupport: false,
+        destNetworkAllowlist: [],
+        srcNetworkAllowlist: [],
+      },
+      destTokens: {},
+      destTopAssets: [],
+      srcTokens: {},
+      srcTopAssets: [],
+      quoteRequest: {
+        walletAddress: false,
+        srcTokenAddress: true,
+        slippage: true,
+        srcChainId: true,
+        destChainId: true,
+        destTokenAddress: true,
+        srcTokenAmount: true,
+      },
+      quotes: [],
+      quotesLastFetched: true,
+      quotesLoadingStatus: true,
+    },
+  },
+  CronjobController: {
+    jobs: false,
+  },
+  CurrencyController: {
+    currentCurrency: true,
+    currencyRates: true,
+  },
+  DecryptMessageController: getDefaultDecryptMessageControllerState(),
+  EncryptionPublicKeyController: {
+    unapprovedEncryptionPublicKeyMsgs: false,
+    unapprovedEncryptionPublicKeyMsgCount: true,
+  },
+  EnsController: {
+    ensResolutionsByAddress: false,
+    ensEntries: false,
+  },
+  GasFeeController: {
+    estimatedGasFeeTimeBounds: true,
+    gasEstimateType: true,
+    gasFeeEstimates: true,
+    gasFeeEstimatesByChainId: true,
+    nonRPCGasFeeApisDisabled: false,
+  },
+  KeyringController: getDefaultKeyringState(),
+  LoggingController: {
+    logs: false,
+  },
+  NotificationServicesController: {
+    subscriptionAccountsSeen: false,
+    isMetamaskNotificationsFeatureSeen: false,
+    isNotificationServicesEnabled: false,
+    isFeatureAnnouncementsEnabled: false,
+    metamaskNotificationsList: false,
+    metamaskNotificationsReadList: false,
+    isCheckingAccountsPresence: false,
+    isFetchingMetamaskNotifications: false,
+    isUpdatingMetamaskNotifications: false,
+    isUpdatingMetamaskNotificationsAccount: false,
+  },
+  MetaMetricsController: {
+    eventsBeforeMetricsOptIn: false,
+    fragments: false,
+    metaMetricsId: true,
+    participateInMetaMetrics: true,
+    previousUserTraits: false,
+    segmentApiCalls: false,
+    traits: false,
+    dataCollectionForMarketing: false,
+    marketingCampaignCookieId: true,
+    latestNonAnonymousEventTimestamp: true,
+  },
+  MetaMetricsDataDeletionController: {
+    metaMetricsDataDeletionId: true,
+    metaMetricsDataDeletionTimestamp: true,
+  },
+  NameController: {
+    names: false,
+    nameSources: false,
+    useExternalNameSources: false,
+  },
+  NetworkController: getDefaultNetworkControllerState(),
+  NftController: getDefaultNftControllerState(),
+  NotificationController: {
+    notifications: false,
+  },
+  OnboardingController: getDefaultOnboardingControllerState(),
+  PPOMController: {
+    securityAlertsEnabled: false,
+    storageMetadata: [],
+    versionInfo: [],
+  },
+  PermissionController: {
+    subjects: false,
+  },
+  PermissionLogController: {
+    permissionActivityLog: false,
+    permissionHistory: false,
+  },
+  PhishingController: {},
+  PreferencesController: getDefaultPreferencesState(),
+  NotificationServicesPushController: {
+    fcmToken: false,
+  },
+  MultichainRatesController: {
+    fiatCurrency: true,
+    rates: true,
+    cryptocurrencies: true,
+  },
+  QueuedRequestController: {
+    queuedRequestCount: true,
+  },
+  SelectedNetworkController: { domains: false },
+  SignatureController: {
+    unapprovedPersonalMsgCount: true,
+    unapprovedPersonalMsgs: false,
+    unapprovedTypedMessages: false,
+    unapprovedTypedMessagesCount: true,
+  },
+  SmartTransactionsController: getDefaultSmartTransactionsControllerState(),
+  SnapController: {
+    snaps: false,
+  },
+  SnapInterfaceController: {
+    interfaces: false,
+  },
+  SnapInsightsController: {
+    insights: false,
+  },
+  SnapsRegistry: {
+    database: false,
+    lastUpdated: false,
+    databaseUnavailable: false,
+  },
+  SubjectMetadataController: {
+    subjectMetadata: false,
+  },
+  SwapsController: getDefaultSwapsControllerState(),
+  TokenDetectionController: {},
+  TokenListController: {
+    preventPollingOnNetworkRestart: true,
+    tokenList: false,
+    tokensChainsCache: {},
+  },
+  TokenRatesController: getDefaultTokenRatesControllerState(),
+  TokensController: {
+    allDetectedTokens: {},
+    allIgnoredTokens: {},
+    allTokens: {},
+    detectedTokens: false,
+    ignoredTokens: false,
+    tokens: false,
+  },
+  TransactionController: {
+    transactions: false,
+    lastFetchedBlockNumbers: false,
+    methodData: false,
+  },
+  TxController: {
+    transactions: false,
+  },
+  UserOperationController: {
+    userOperations: false,
+  },
+  UserStorageController: {
+    isProfileSyncingEnabled: true,
+    isProfileSyncingUpdateLoading: false,
+  },
+  ///: BEGIN:ONLY_INCLUDE_IF(build-mmi)
+  MMIController: {
+    opts: true,
+  },
+  CustodyController: {
+    store: true,
+  },
+  MmiConfigurationController: {
+    store: true,
+    configurationClient: true,
+  },
+  ///: END:ONLY_INCLUDE_IF
 };
