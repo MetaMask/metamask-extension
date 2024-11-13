6fa708bd258ed575c327e5367d73450558ee2261
Nicholas Gambino <nicholas.gambino@consensys.net>
2024-11-13 15:48:32 -0800

    fix: Dont force currency suffix in user-preferenced-currency-display.component.js

--- ui/components/app/user-preferenced-currency-display/user-preferenced-currency-display.component.js
+++ ui/components/app/user-preferenced-currency-display/user-preferenced-currency-display.component.js
@@ -27,7 +27,7 @@ export default function UserPreferencedCurrencyDisplay({
   showFiat,
   showNative,
   shouldCheckShowNativeToken,
-  showCurrencySuffix = true,
+  showCurrencySuffix,
   privacyMode = false,
   ...restProps
 }) {
