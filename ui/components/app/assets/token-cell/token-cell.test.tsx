fc8e334af6b6d37cf83b6c0ff279e819f7417265
Nicholas Gambino <nicholas.gambino@consensys.net>
2024-11-13 12:54:15 -0800

    fix: Update token cell test, by tweaking primary formatWithThreshold function signature

--- ui/components/app/assets/token-cell/token-cell.test.tsx
+++ ui/components/app/assets/token-cell/token-cell.test.tsx
@@ -5,7 +5,11 @@ import { fireEvent } from '@testing-library/react';
 import { useSelector } from 'react-redux';
 import { renderWithProvider } from '../../../../../test/lib/render-helpers';
 import { useTokenFiatAmount } from '../../../../hooks/useTokenFiatAmount';
-import { getTokenList, getPreferences } from '../../../../selectors';
+import {
+  getTokenList,
+  getPreferences,
+  getCurrentCurrency,
+} from '../../../../selectors';
 import {
   getMultichainCurrentChainId,
   getMultichainIsEvm,
@@ -39,7 +43,7 @@ describe('Token Cell', () => {
   const mockState = {
     metamask: {
       marketData: {
-        '0x1': {
+        '0x89': {
           '0xAnotherToken': { price: 0.015 },
         },
       },
@@ -85,7 +89,7 @@ describe('Token Cell', () => {
     string: '5.000',
     currentCurrency: 'usd',
     image: '',
-    chainId: '0x1',
+    chainId: '0x89',
     tokenFiatAmount: 5,
     onClick: jest.fn(),
   };
@@ -96,7 +100,7 @@ describe('Token Cell', () => {
     string: '5000000',
     currentCurrency: 'usd',
     image: '',
-    chainId: '0x1',
+    chainId: '0x89',
     tokenFiatAmount: 5000000,
     onClick: jest.fn(),
   };
@@ -116,6 +120,9 @@ describe('Token Cell', () => {
     }
     if (selector === getIntlLocale) {
       return 'en-US';
+    }
+    if (selector === getCurrentCurrency) {
+      return 'usd';
     }
     return undefined;
   });
