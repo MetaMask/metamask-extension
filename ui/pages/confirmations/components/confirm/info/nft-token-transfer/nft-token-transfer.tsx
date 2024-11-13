be991f6ebb69b1bf6f5044adfc3b4a9654eaba8c
Vinicius Stevam <vinicius.stevam@consensys.io>
2024-11-13 14:18:13 +0000

    add metricsOnly option in simulationsDetails

--- ui/pages/confirmations/components/confirm/info/nft-token-transfer/nft-token-transfer.tsx
+++ ui/pages/confirmations/components/confirm/info/nft-token-transfer/nft-token-transfer.tsx
@@ -2,7 +2,6 @@ import { TransactionMeta } from '@metamask/transaction-controller';
 import React from 'react';
 import { ConfirmInfoSection } from '../../../../../../components/app/confirm/info/row/section';
 import { useConfirmContext } from '../../../../context/confirm';
-import { useSimulationMetricsNoShow } from '../../../../hooks/useSimulationMetricsNoShow';
 import { SimulationDetails } from '../../../simulation-details';
 import { AdvancedDetails } from '../shared/advanced-details/advanced-details';
 import { GasFeesSection } from '../shared/gas-fees-section/gas-fees-section';
@@ -15,24 +14,21 @@ const NFTTokenTransferInfo = () => {
     useConfirmContext<TransactionMeta>();
 
   const isWalletInitiated = transactionMeta.origin === 'metamask';
-  useSimulationMetricsNoShow({
-    enableMetrics: isWalletInitiated,
-    transactionId: transactionMeta.id,
-  });
 
   return (
     <>
       <NFTSendHeading />
       <TransactionFlowSection />
-      {!isWalletInitiated && (
+      {
         <ConfirmInfoSection noPadding>
           <SimulationDetails
             transaction={transactionMeta}
             isTransactionsRedesign
             enableMetrics
+            metricsOnly={isWalletInitiated}
           />
         </ConfirmInfoSection>
-      )}
+      }
       <TokenDetailsSection />
       <GasFeesSection />
       <AdvancedDetails />
