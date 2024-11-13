154597a38c98d9f3899ffdb4a6ef1c49770bcbaf
sahar-fehri <sahar.fehri@consensys.net>
2024-11-13 21:43:39 +0100

    fix: fix add feature flag

--- ui/hooks/useAccountTrackerPolling.ts
+++ ui/hooks/useAccountTrackerPolling.ts
@@ -1,5 +1,8 @@
 import { useSelector } from 'react-redux';
-import { getNetworkConfigurationsByChainId } from '../selectors';
+import {
+  getCurrentChainId,
+  getNetworkConfigurationsByChainId,
+} from '../selectors';
 import {
   accountTrackerStartPolling,
   accountTrackerStopPollingByPollingToken,
@@ -13,6 +16,11 @@ import useMultiPolling from './useMultiPolling';
 const useAccountTrackerPolling = () => {
   // Selectors to determine polling input
   const networkConfigurations = useSelector(getNetworkConfigurationsByChainId);
+  const currentChainId = useSelector(getCurrentChainId);
+  const currentNetwork = networkConfigurations[currentChainId];
+  const currentRpcEndpoint =
+    currentNetwork.rpcEndpoints[currentNetwork.defaultRpcEndpointIndex];
+
   const completedOnboarding = useSelector(getCompletedOnboarding);
   const isUnlocked = useSelector(getIsUnlocked);
   const availableNetworkClientIds = Object.values(networkConfigurations).map(
@@ -21,10 +29,23 @@ const useAccountTrackerPolling = () => {
         networkConfiguration.defaultRpcEndpointIndex
       ].networkClientId,
   );
+  const canStartPolling = completedOnboarding && isUnlocked;
+  const portfolioViewNetworks = canStartPolling
+    ? availableNetworkClientIds
+    : [];
+  const nonPortfolioViewNetworks = canStartPolling
+    ? [currentRpcEndpoint.networkClientId]
+    : [];
+
+  const networkArrayToPollFor = process.env.FILTER_TOKENS_TOGGLE
+    ? portfolioViewNetworks
+    : nonPortfolioViewNetworks;
+
   useMultiPolling({
     startPolling: accountTrackerStartPolling,
     stopPollingByPollingToken: accountTrackerStopPollingByPollingToken,
-    input: completedOnboarding && isUnlocked ? availableNetworkClientIds : [],
+    input: networkArrayToPollFor,
   });
 };
+
 export default useAccountTrackerPolling;
