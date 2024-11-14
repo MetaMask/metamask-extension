0a5eb58d86b4805215712f365161668d028c4148
Nick Gambino <35090461+gambinish@users.noreply.github.com>
2024-11-13 23:37:10 -0800

    chore: Begin introducing patterns for Multichain AssetList (#28429)
    ## **Description**
    
    This PR introduces some non-breaking patterns from
    https://github.com/MetaMask/metamask-extension/pull/28386
    
    The idea is to introduce this as a smaller PR to reduce the diff in
    https://github.com/MetaMask/metamask-extension/pull/28386 to make it
    easier to review
    
    [![Open in GitHub
    Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/28429?quickstart=1)
    
    ## **Related issues**
    
    Fixes: N/A
    
    ## **Manual testing steps**
    
    App should be fully functional with no regressions
    
    ## **Screenshots/Recordings**
    
    ## **Pre-merge author checklist**
    
    - [x] I've followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
    Extension Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: chloeYue <105063779+chloeYue@users.noreply.github.com>
    Co-authored-by: seaona <54408225+seaona@users.noreply.github.com>
    Co-authored-by: Monte Lai <monte.lai@consensys.net>
    Co-authored-by: Charly Chevalier <charly.chevalier@consensys.net>
    Co-authored-by: Pedro Figueiredo <pedro.figueiredo@consensys.net>

--- ui/components/multichain/account-overview/account-overview-tabs.tsx
+++ ui/components/multichain/account-overview/account-overview-tabs.tsx
@@ -149,7 +149,9 @@ export const AccountOverviewTabs = ({
             <Box marginTop={2}>
               <AssetList
                 showTokensLinks={showTokensLinks ?? true}
-                onClickAsset={(asset: string) =>
+                // TODO: chainID to be incorporated in unified asset list
+                // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                onClickAsset={(chainId: string, asset: string) =>
                   history.push(`${ASSET_ROUTE}/${asset}`)
                 }
               />
