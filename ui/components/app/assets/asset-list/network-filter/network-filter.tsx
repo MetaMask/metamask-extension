ecc79cbb4e6a6c56a496d8091f411fe331058875
Nicholas Gambino <nicholas.gambino@consensys.net>
2024-11-15 15:54:46 -0800

    feat: Render aggregated fiat balance and current chain fiat balance in network filter dropdown

--- ui/components/app/assets/asset-list/network-filter/network-filter.tsx
+++ ui/components/app/assets/asset-list/network-filter/network-filter.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setTokenNetworkFilter } from '../../../../../store/actions';
 import {
@@ -7,6 +7,13 @@ import {
   getIsTestnet,
   getPreferences,
   getNetworkConfigurationsByChainId,
+  getSelectedAccount,
+  getShouldHideZeroBalanceTokens,
+  getCurrentCurrency,
+  getSelectedAccountTokensAcrossChains,
+  getSelectedAccountNativeTokenCachedBalanceByChainId,
+  getMarketData,
+  getCurrencyRates,
 } from '../../../../../selectors';
 import { useI18nContext } from '../../../../../hooks/useI18nContext';
 import { SelectableListItem } from '../sort-control/sort-control';
@@ -23,11 +30,19 @@ import {
   AvatarNetwork,
   AvatarNetworkSize,
 } from '../../../../component-library';
-import UserPreferencedCurrencyDisplay from '../../../user-preferenced-currency-display';
 import {
   CHAIN_ID_TO_NETWORK_IMAGE_URL_MAP,
   TEST_CHAINS,
 } from '../../../../../../shared/constants/network';
+import { useAccountTotalCrossChainFiatBalance } from '../../../../../hooks/useAccountTotalCrossChainFiatBalance';
+import { useGetFormattedTokensPerChain } from '../../../../../hooks/useGetFormattedTokensPerChain';
+import { formatWithThreshold } from '../../token-cell/token-cell';
+import { getIntlLocale } from '../../../../../ducks/locale/locale';
+import { useTokenBalances } from '../../../../../hooks/useTokenBalances';
+import { calculateTokenBalance } from '../../util/calculateTokenBalance';
+import { Hex } from '@metamask/utils';
+import { ChainAddressMarketData, Token } from '../../token-list/token-list';
+import { calculateTokenFiatAmount } from '../../util/calculateTokenFiatAmount';
 
 type SortControlProps = {
   handleClose: () => void;
@@ -36,16 +51,122 @@ type SortControlProps = {
 const NetworkFilter = ({ handleClose }: SortControlProps) => {
   const t = useI18nContext();
   const dispatch = useDispatch();
+  const locale = useSelector(getIntlLocale);
   const chainId = useSelector(getCurrentChainId);
+  const selectedAccount = useSelector(getSelectedAccount);
   const currentNetwork = useSelector(getCurrentNetwork);
+  const currentCurrency = useSelector(getCurrentCurrency);
   const allNetworks = useSelector(getNetworkConfigurationsByChainId);
   const isTestnet = useSelector(getIsTestnet);
   const { tokenNetworkFilter, showNativeTokenAsMainBalance } =
     useSelector(getPreferences);
+  const shouldHideZeroBalanceTokens = useSelector(
+    getShouldHideZeroBalanceTokens,
+  );
+
+  const selectedAccountTokensChains: Record<Hex, Token[]> = useSelector(
+    getSelectedAccountTokensAcrossChains,
+  ) as Record<Hex, Token[]>;
+
+  const nativeBalances: Record<Hex, Hex> = useSelector(
+    getSelectedAccountNativeTokenCachedBalanceByChainId,
+  ) as Record<Hex, Hex>;
+
+  const marketData: ChainAddressMarketData = useSelector(
+    getMarketData,
+  ) as ChainAddressMarketData;
+
+  const currencyRates = useSelector(getCurrencyRates);
+
+  const { formattedTokensWithBalancesPerChain } = useGetFormattedTokensPerChain(
+    selectedAccount,
+    shouldHideZeroBalanceTokens,
+  );
+  const { totalFiatBalance } = useAccountTotalCrossChainFiatBalance(
+    selectedAccount,
+    formattedTokensWithBalancesPerChain,
+  );
+
+  const { tokenBalances } = useTokenBalances();
 
   const [chainsToShow, setChainsToShow] = useState<string[]>([]);
 
-  const selectedAccountBalance = '100';
+  const selectedAccountFiatBalance = useMemo(() => {
+    const currentChainBalances =
+      tokenBalances[selectedAccount.address]?.[chainId];
+
+    const currentChainTokenInfo = selectedAccountTokensChains[chainId];
+
+    if (!currentChainBalances || !currentChainTokenInfo) {
+      return 0; // Return 0 if no balances or token info
+    }
+
+    // Calculate the sum of all tokenFiatAmount
+    const totalFiatAmount = Object.entries(currentChainBalances).reduce(
+      (total, [address, balance]) => {
+        const tokenInfo = currentChainTokenInfo.find(
+          (token) => token.address === address,
+        );
+
+        if (!tokenInfo) {
+          return total; // Skip if tokenInfo is not found
+        }
+
+        const calculatedTokenBalance =
+          calculateTokenBalance({
+            isNative: tokenInfo.isNative,
+            chainId: tokenInfo.chainId,
+            address: tokenInfo.address,
+            decimals: tokenInfo.decimals,
+            nativeBalances,
+            selectedAccountTokenBalancesAcrossChains:
+              tokenBalances[selectedAccount.address],
+          }) || '0';
+
+        const tokenFiatAmount = calculateTokenFiatAmount({
+          token: tokenInfo,
+          chainId: tokenInfo.chainId,
+          balance: calculatedTokenBalance,
+          marketData,
+          currencyRates,
+        });
+
+        // @ts-ignore
+        return total + parseFloat(tokenFiatAmount || '0'); // Accumulate the fiat amount
+      },
+      0, // Initial total is 0
+    );
+
+    return totalFiatAmount; // Return the total fiat amount
+  }, [
+    tokenBalances,
+    selectedAccount.address,
+    chainId,
+    selectedAccountTokensChains,
+    nativeBalances,
+    marketData,
+    currencyRates,
+  ]);
+
+  const formattedAggregatedMultichainBalance = formatWithThreshold(
+    totalFiatBalance,
+    0.01,
+    locale,
+    {
+      style: 'currency',
+      currency: currentCurrency.toUpperCase(),
+    },
+  );
+
+  const formattedAggregatedCurrentChainBalance = formatWithThreshold(
+    selectedAccountFiatBalance,
+    0.01,
+    locale,
+    {
+      style: 'currency',
+      currency: currentCurrency.toUpperCase(),
+    },
+  );
 
   const handleFilter = (chainFilters: Record<string, boolean>) => {
     dispatch(setTokenNetworkFilter(chainFilters));
@@ -91,8 +212,7 @@ const NetworkFilter = ({ handleClose }: SortControlProps) => {
               variant={TextVariant.bodySmMedium}
               color={TextColor.textAlternative}
             >
-              {/* TODO: Should query cross chain account balance */}
-              $1,000.00
+              {formattedAggregatedMultichainBalance}
             </Text>
           </Box>
           <Box display={Display.Flex} alignItems={AlignItems.center}>
@@ -139,21 +259,12 @@ const NetworkFilter = ({ handleClose }: SortControlProps) => {
             >
               {t('currentNetwork')}
             </Text>
-            <UserPreferencedCurrencyDisplay
-              value={selectedAccountBalance}
-              type="PRIMARY"
-              textProps={{
-                color: TextColor.textAlternative,
-                variant: TextVariant.bodySmMedium,
-              }}
-              ethNumberOfDecimals={4}
-              hideTitle
-              showCurrencySuffix={false}
-              shouldCheckShowNativeToken
-              isAggregatedFiatOverviewBalance={
-                !showNativeTokenAsMainBalance && !isTestnet
-              }
-            />
+            <Text
+              variant={TextVariant.bodySmMedium}
+              color={TextColor.textAlternative}
+            >
+              {formattedAggregatedCurrentChainBalance}
+            </Text>
           </Box>
           <AvatarNetwork
             name="Current"
