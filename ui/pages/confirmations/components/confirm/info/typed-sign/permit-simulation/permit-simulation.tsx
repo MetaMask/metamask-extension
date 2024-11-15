2367e4e1a6a5b29f83bdb8e7e81b0974ce889812
Jyoti Puri <jyotipuri@gmail.com>
2024-11-15 17:20:28 +0530

    feat: UI changes to show decoding data for permits (#28342)
    ## **Description**
    
    Changes in permit simulation component to show decoding api results if
    present. Changes will currently not be visible as signature controller
    update is not yet done.
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/MetaMask-planning/issues/3554
    
    ## **Manual testing steps**
    
    1. Locally enable signature decoding using env variables
    2. Submit a permit on test dapp
    3. Check simulation section
    
    ## **Screenshots/Recordings**
    <img width="360" alt="Screenshot 2024-11-07 at 3 07 29 PM"
    src="https://github.com/user-attachments/assets/57097f47-f0db-461a-a1f6-ca62f397ea93">
    
    ## **Pre-merge author checklist**
    
    - [X] I've followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
    Extension Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/CODING_GUIDELINES.md).
    - [X] I've completed the PR template to the best of my ability
    - [X] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [X] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: MetaMask Bot <metamaskbot@users.noreply.github.com>

--- ui/pages/confirmations/components/confirm/info/typed-sign/permit-simulation/permit-simulation.tsx
+++ ui/pages/confirmations/components/confirm/info/typed-sign/permit-simulation/permit-simulation.tsx
@@ -1,118 +1,22 @@
-import { Hex } from '@metamask/utils';
 import React from 'react';
-import { PrimaryType } from '../../../../../../../../shared/constants/signatures';
-import { parseTypedDataMessage } from '../../../../../../../../shared/modules/transaction.utils';
-import { ConfirmInfoRow } from '../../../../../../../components/app/confirm/info/row';
-import { Box } from '../../../../../../../components/component-library';
-import {
-  Display,
-  FlexDirection,
-} from '../../../../../../../helpers/constants/design-system';
-import { useI18nContext } from '../../../../../../../hooks/useI18nContext';
-import { useConfirmContext } from '../../../../../context/confirm';
+
 import { SignatureRequestType } from '../../../../../types/confirm';
-import StaticSimulation from '../../shared/static-simulation/static-simulation';
-import PermitSimulationValueDisplay from './value-display/value-display';
-
-function extractTokenDetailsByPrimaryType(
-  message: Record<string, unknown>,
-  primaryType: PrimaryType,
-): object[] | unknown {
-  let tokenDetails;
-
-  switch (primaryType) {
-    case PrimaryType.PermitBatch:
-    case PrimaryType.PermitSingle:
-      tokenDetails = message?.details;
-      break;
-    case PrimaryType.PermitBatchTransferFrom:
-    case PrimaryType.PermitTransferFrom:
-      tokenDetails = message?.permitted;
-      break;
-    default:
-      break;
-  }
-
-  const isNonArrayObject = tokenDetails && !Array.isArray(tokenDetails);
-
-  return isNonArrayObject ? [tokenDetails] : tokenDetails;
-}
+import { useConfirmContext } from '../../../../../context/confirm';
+import { DefaultSimulation } from './default-simulation';
+import { DecodedSimulation } from './decoded-simulation';
 
 const PermitSimulation: React.FC<object> = () => {
-  const t = useI18nContext();
   const { currentConfirmation } = useConfirmContext<SignatureRequestType>();
-  const msgData = currentConfirmation.msgParams?.data;
-  const chainId = currentConfirmation.chainId as Hex;
-  const {
-    domain: { verifyingContract },
-    message,
-    message: { tokenId },
-    primaryType,
-  } = parseTypedDataMessage(msgData as string);
-  const isNFT = tokenId !== undefined;
+  const { decodingLoading, decodingData } = currentConfirmation;
 
-  const tokenDetails = extractTokenDetailsByPrimaryType(message, primaryType);
+  if (
+    decodingData?.error ||
+    (decodingData === undefined && decodingLoading !== true)
+  ) {
+    return <DefaultSimulation />;
+  }
 
-  const TokenDetail = ({
-    token,
-    amount,
-    i,
-  }: {
-    token: Hex | string;
-    amount: number | string;
-    i: number;
-  }) => (
-    <PermitSimulationValueDisplay
-      key={`${token}-${i}`}
-      primaryType={primaryType}
-      tokenContract={token}
-      value={amount}
-      chainId={chainId}
-    />
-  );
-
-  const SpendingCapRow = (
-    <ConfirmInfoRow
-      label={t(isNFT ? 'simulationApproveHeading' : 'spendingCap')}
-    >
-      <Box style={{ marginLeft: 'auto', maxWidth: '100%' }}>
-        {Array.isArray(tokenDetails) ? (
-          <Box
-            display={Display.Flex}
-            flexDirection={FlexDirection.Column}
-            gap={2}
-          >
-            {tokenDetails.map(
-              (
-                { token, amount }: { token: string; amount: string },
-                i: number,
-              ) => (
-                <TokenDetail token={token} amount={amount} i={i} />
-              ),
-            )}
-          </Box>
-        ) : (
-          <PermitSimulationValueDisplay
-            tokenContract={verifyingContract}
-            value={message.value}
-            tokenId={message.tokenId}
-            chainId={chainId}
-          />
-        )}
-      </Box>
-    </ConfirmInfoRow>
-  );
-
-  return (
-    <StaticSimulation
-      title={t('simulationDetailsTitle')}
-      titleTooltip={t('simulationDetailsTitleTooltip')}
-      description={t(
-        isNFT ? 'simulationDetailsApproveDesc' : 'permitSimulationDetailInfo',
-      )}
-      simulationElements={SpendingCapRow}
-    />
-  );
+  return <DecodedSimulation />;
 };
 
 export default PermitSimulation;
