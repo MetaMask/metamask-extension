89ff44e0435c98d383f8812db20e04a3ccb9751e
Nicholas Gambino <nicholas.gambino@consensys.net>
2024-11-16 14:07:28 -0800

    fix: Revert temporary hack to userPerferencedCurrencyDisplay

--- ui/components/app/user-preferenced-currency-display/user-preferenced-currency-display.component.js
+++ ui/components/app/user-preferenced-currency-display/user-preferenced-currency-display.component.js
@@ -83,7 +83,7 @@ export default function UserPreferencedCurrencyDisplay({
       data-testid={dataTestId}
       numberOfDecimals={numberOfDecimals}
       prefixComponent={prefixComponent}
-      //hideLabel={!showCurrencySuffix}
+      hideLabel={!showCurrencySuffix}
       suffix={showCurrencySuffix && !showEthLogo && currency}
       privacyMode={privacyMode}
     />
