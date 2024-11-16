0e2c013b86523b5d3d720e69fb4d2ab65af03754
Nicholas Gambino <nicholas.gambino@consensys.net>
2024-11-16 13:16:17 -0800

    fix: Address inconsistencies after merge

--- ui/store/actions.ts
+++ ui/store/actions.ts
@@ -4565,26 +4565,6 @@ export async function currencyRateStopPollingByPollingToken(
   await removePollingTokenFromAppState(pollingToken);
 }
 
-export async function tokenBalancesStartPolling(
-  chainId: string,
-): Promise<string> {
-  const pollingToken = await submitRequestToBackground(
-    'tokenBalancesStartPolling',
-    [{ chainId }],
-  );
-  await addPollingTokenToAppState(pollingToken);
-  return pollingToken;
-}
-
-export async function tokenBalancesStopPollingByPollingToken(
-  pollingToken: string,
-) {
-  await submitRequestToBackground('tokenBalancesStopPollingByPollingToken', [
-    pollingToken,
-  ]);
-  await removePollingTokenFromAppState(pollingToken);
-}
-
 /**
  * Informs the TokenDetectionController that the UI requires token detection polling
  *
