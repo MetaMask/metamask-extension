1efaef73d0828f9481e2c17643be51e1fae0a42e
IF <139582705+infiniteflower@users.noreply.github.com>
2024-11-19 10:50:23 -0500

    chore: remove usecallback for now

--- ui/pages/bridge/hooks/useSubmitBridgeTransaction.ts
+++ ui/pages/bridge/hooks/useSubmitBridgeTransaction.ts
@@ -16,36 +16,33 @@ export default function useSubmitBridgeTransaction() {
   const { handleApprovalTx } = useHandleApprovalTx();
   const { handleBridgeTx } = useHandleBridgeTx();
 
-  const submitBridgeTransaction = useCallback(
-    async (quoteResponse: QuoteResponse) => {
-      // Execute transaction(s)
-      let approvalTxId: string | undefined;
-      if (quoteResponse?.approval) {
-        approvalTxId = await handleApprovalTx({
-          approval: quoteResponse.approval,
-          quoteResponse,
-        });
-      }
-
-      await handleBridgeTx({
+  const submitBridgeTransaction = async (quoteResponse: QuoteResponse) => {
+    // Execute transaction(s)
+    let approvalTxId: string | undefined;
+    if (quoteResponse?.approval) {
+      approvalTxId = await handleApprovalTx({
+        approval: quoteResponse.approval,
         quoteResponse,
-        approvalTxId,
       });
+    }
 
-      // Add tokens if not the native gas token
-      if (quoteResponse.quote.srcAsset.address !== zeroAddress()) {
-        addSourceToken(quoteResponse);
-      }
-      if (quoteResponse.quote.destAsset.address !== zeroAddress()) {
-        await addDestToken(quoteResponse);
-      }
+    await handleBridgeTx({
+      quoteResponse,
+      approvalTxId,
+    });
 
-      // Route user to activity tab on Home page
-      await dispatch(setDefaultHomeActiveTabName('activity'));
-      history.push(DEFAULT_ROUTE);
-    },
-    [dispatch],
-  );
+    // Add tokens if not the native gas token
+    if (quoteResponse.quote.srcAsset.address !== zeroAddress()) {
+      addSourceToken(quoteResponse);
+    }
+    if (quoteResponse.quote.destAsset.address !== zeroAddress()) {
+      await addDestToken(quoteResponse);
+    }
+
+    // Route user to activity tab on Home page
+    await dispatch(setDefaultHomeActiveTabName('activity'));
+    history.push(DEFAULT_ROUTE);
+  };
 
   return {
     submitBridgeTransaction,
