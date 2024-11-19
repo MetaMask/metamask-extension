b947091166df5f497b39d10cd1764f839bc9da9e
Nicholas Gambino <nicholas.gambino@consensys.net>
2024-11-18 20:14:47 -0800

    fix: Capizalize suffix in currency display

--- ui/components/ui/currency-display/currency-display.component.js
+++ ui/components/ui/currency-display/currency-display.component.js
@@ -94,7 +94,7 @@ export default function CurrencyDisplay({
           isHidden={privacyMode}
           {...suffixProps}
         >
-          {parts.suffix}
+          {parts.suffix.toUpperCase()}
         </SensitiveText>
       ) : null}
     </Box>
