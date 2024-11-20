180b8d6e210a8bbd21e98040f0bcfddfed3fc488
Guillaume Roux <hello@guillaumerx.fr>
2024-11-20 12:23:43 +0100

    address comments

--- ui/components/app/snaps/snap-ui-renderer/components/card.ts
+++ ui/components/app/snaps/snap-ui-renderer/components/card.ts
@@ -1,3 +1,4 @@
+import { title } from 'node:process';
 import { CardElement } from '@metamask/snaps-sdk/jsx';
 import { mapToTemplate } from '../utils';
 import { UIComponentFactory } from './types';
@@ -5,19 +6,30 @@ import { UIComponentFactory } from './types';
 export const card: UIComponentFactory<CardElement> = ({
   element,
   ...params
-}) => ({
-  element: 'SnapUICard',
-  props: {
-    image: element.props.image,
+}) => {
+  if (element.props.title !== 'string') {
+    return {
+      element: 'SnapUICard',
+      props: {
+        image: element.props.image,
+        description: element.props.description,
+        value: element.props.value,
+        extra: element.props.extra,
+      },
+      propComponents: {
+        title: mapToTemplate({ element: element.props.title, ...params }),
+      },
+    };
+  }
 
-    description: element.props.description,
-    value: element.props.value,
-    extra: element.props.extra,
-  },
-  propComponents: {
-    title:
-      typeof element.props.title === 'string'
-        ? element.props.title
-        : mapToTemplate({ element: element.props.title, ...params }),
-  },
-});
+  return {
+    element: 'SnapUICard',
+    props: {
+      image: element.props.image,
+      title: element.props.title,
+      description: element.props.description,
+      value: element.props.value,
+      extra: element.props.extra,
+    },
+  };
+};
