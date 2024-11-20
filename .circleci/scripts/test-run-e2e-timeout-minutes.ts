7c90dc256735b4476c9049cd5409ca6a4236de02
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2024-11-20 12:20:06 +0000

    wip

--- .circleci/scripts/test-run-e2e-timeout-minutes.ts
+++ .circleci/scripts/test-run-e2e-timeout-minutes.ts
@@ -3,6 +3,6 @@ import { filterE2eChangedFiles } from '../../test/e2e/changedFilesUtil';
 const changedOrNewTests = filterE2eChangedFiles();
 
 // 20 minutes, plus 3 minutes for every changed file, up to a maximum of 30 minutes
-const extraTime = Math.min(20 + changedOrNewTests.length * 3, 30);
+const extraTime = Math.min(20 + changedOrNewTests.length * 3, 120);
 
 console.log(extraTime);
