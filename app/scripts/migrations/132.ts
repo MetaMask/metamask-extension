0497b5866b0a9269369d138c7416bdf1bdbf8931
httpjunkie <eric@httpjunkie.com>
2024-11-21 11:03:54 -0500

    refactor: update migration to use transformState function

--- app/scripts/migrations/132.ts
+++ app/scripts/migrations/132.ts
@@ -3,77 +3,80 @@ import { cloneDeep } from 'lodash';
 import type { SmartTransaction } from '@metamask/smart-transactions-controller/dist/types';
 
 export type VersionedData = {
- meta: {
-   version: number;
- };
-
- data: {
-   PreferencesController?: {
-     smartTransactionsOptInStatus?: boolean | null;
-   };
-
-   SmartTransactionsController?: {
-     smartTransactionsState: {
-       smartTransactions: Record<string, SmartTransaction[]>;
-     };
-   };
- };
+  meta: {
+    version: number;
+  };
+  data: {
+    PreferencesController?: {
+      smartTransactionsOptInStatus?: boolean | null;
+    };
+    SmartTransactionsController?: {
+      smartTransactionsState: {
+        smartTransactions: Record<string, SmartTransaction[]>;
+      };
+    };
+  };
 };
 
 const version = 132;
 
-const migration = {
- version,
+function transformState(state: VersionedData['data']) {
+  if (
+    !hasProperty(state, 'PreferencesController') ||
+    !isObject(state.PreferencesController)
+  ) {
+    global.sentry?.captureException?.(
+      new Error(
+        `Invalid PreferencesController state: ${typeof state.PreferencesController}`,
+      ),
+    );
+    return state;
+  }
 
- async migrate(originalVersionedData: VersionedData): Promise<VersionedData> {
-   const versionedData = cloneDeep(originalVersionedData);
-   versionedData.meta.version = version;
-   const state = versionedData.data;
+  const { PreferencesController } = state;
+  const currentOptInStatus =
+    PreferencesController?.smartTransactionsOptInStatus;
 
-   if (
-     !hasProperty(state, 'PreferencesController') ||
-     !isObject(state.PreferencesController)
-   ) {
-     global.sentry?.captureException?.(
-       new Error(
-         `Invalid PreferencesController state: ${typeof state.PreferencesController}`,
-       ),
-     );
+  if (currentOptInStatus === undefined || currentOptInStatus === null) {
+    PreferencesController.smartTransactionsOptInStatus = true;
+  } else if (
+    currentOptInStatus === false &&
+    !hasExistingSmartTransactions(state)
+  ) {
+    PreferencesController.smartTransactionsOptInStatus = true;
+  }
 
-     return versionedData;
-   }
-
-   const { PreferencesController } = state;
-   const currentOptInStatus =
-     PreferencesController?.smartTransactionsOptInStatus;
-
-   if (currentOptInStatus === undefined || currentOptInStatus === null) {
-     PreferencesController.smartTransactionsOptInStatus = true;
-   } else if (currentOptInStatus === false && !hasExistingSmartTransactions(state)) {
-     PreferencesController.smartTransactionsOptInStatus = true;
-   }
-
-   return versionedData;
- },
-};
+  return state;
+}
 
 function hasExistingSmartTransactions(state: VersionedData['data']): boolean {
- if (
-   !hasProperty(state, 'SmartTransactionsController') ||
-   !isObject(
-     state.SmartTransactionsController?.smartTransactionsState
-       ?.smartTransactions,
-   )
- ) {
-   return false;
- }
+  if (
+    !hasProperty(state, 'SmartTransactionsController') ||
+    !isObject(
+      state.SmartTransactionsController?.smartTransactionsState
+        ?.smartTransactions,
+    )
+  ) {
+    return false;
+  }
 
- const { smartTransactions } =
-   state.SmartTransactionsController.smartTransactionsState;
+  const { smartTransactions } =
+    state.SmartTransactionsController.smartTransactionsState;
 
- return Object.values(smartTransactions).some(
-   (stxTransactions: SmartTransaction[]) => stxTransactions.length > 0,
- );
+  return Object.values(smartTransactions).some(
+    (chainSmartTransactions: SmartTransaction[]) =>
+      chainSmartTransactions.length > 0,
+  );
 }
+
+const migration = {
+  version,
+  async migrate(originalVersionedData: VersionedData): Promise<VersionedData> {
+    const versionedData = cloneDeep(originalVersionedData);
+    versionedData.meta.version = version;
+    versionedData.data = transformState(versionedData.data);
+    return versionedData;
+  },
+};
 
 export default migration;
