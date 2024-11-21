bd54e42591f93f50776ded41c020d2e3257e9071
httpjunkie <eric@httpjunkie.com>
2024-11-21 12:18:00 -0500

    Revert: Checked out from `develop` the `token-transfer.tsx` file. Previous implementation in `TokenTransferInfo` component should not be necessary. Reverted.

--- ui/pages/confirmations/components/confirm/info/token-transfer/token-transfer.tsx
+++ ui/pages/confirmations/components/confirm/info/token-transfer/token-transfer.tsx
@@ -1,17 +1,11 @@
 import { TransactionMeta } from '@metamask/transaction-controller';
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 import { ConfirmInfoSection } from '../../../../../../components/app/confirm/info/row/section';
 import { useConfirmContext } from '../../../../context/confirm';
 import { SimulationDetails } from '../../../simulation-details';
 import { AdvancedDetails } from '../shared/advanced-details/advanced-details';
 import { GasFeesSection } from '../shared/gas-fees-section/gas-fees-section';
 import SendHeading from '../shared/send-heading/send-heading';
-import {
-  stxAlertIsOpen,
-  dismissSTXMigrationAlert,
-} from '../../../../../../ducks/alerts/stx-migration';
-import { STXBannerAlert } from '../../../stx-banner-alert';
 import { TokenDetailsSection } from './token-details-section';
 import { TransactionFlowSection } from './transaction-flow-section';
 
@@ -20,14 +14,9 @@ const TokenTransferInfo = () => {
     useConfirmContext<TransactionMeta>();
 
   const isWalletInitiated = transactionMeta.origin === 'metamask';
-  const dispatch = useDispatch();
-  const shouldShowSTXAlert = useSelector(stxAlertIsOpen);
 
   return (
     <>
-      {shouldShowSTXAlert && (
-        <STXBannerAlert onClose={() => dispatch(dismissSTXMigrationAlert())} />
-      )}
       <SendHeading />
       <TransactionFlowSection />
       {
