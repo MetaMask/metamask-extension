2d2398f4180ca55f87b2b68077b7b6dfcf635cb0
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2024-11-22 11:27:20 +0000

    Revert test run timeout

--- .circleci/scripts/test-run-e2e-timeout-minutes.ts
+++ .circleci/scripts/test-run-e2e-timeout-minutes.ts
@@ -2,7 +2,7 @@ import { filterE2eChangedFiles } from '../../test/e2e/changedFilesUtil';
 
 const changedOrNewTests = filterE2eChangedFiles();
 
-// 20 minutes, plus 3 minutes for every changed file, up to a maximum of 60 minutes
-const extraTime = Math.min(20 + changedOrNewTests.length * 3, 60);
+// 20 minutes, plus 3 minutes for every changed file, up to a maximum of 30 minutes
+const extraTime = Math.min(20 + changedOrNewTests.length * 3, 30);
 
 console.log(extraTime);
