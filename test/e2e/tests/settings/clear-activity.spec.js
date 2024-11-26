1e40b35dbd49fe28579838ea45cb8ed1a19bd4f4
Matthew Walsh <matthew.walsh@consensys.net>
2024-11-26 22:17:34 +0000

    Fix E2E test

--- test/e2e/tests/settings/clear-activity.spec.js
+++ test/e2e/tests/settings/clear-activity.spec.js
@@ -60,7 +60,7 @@ describe('Clear account activity', function () {
           text: 'Receive',
         });
         assert.equal(sendTransaction, false);
-        assert.equal(receiveTransaction, false);
+        assert.equal(receiveTransaction, true);
       },
     );
   });
