1b9b48eda9c6d3f82bc068bb2c345a84aac1ad21
Hassan Malik <hbmalik88@gmail.com>
2024-11-27 17:03:03 -0500

    fix notification-details

--- ui/pages/notification-details/notification-details.tsx
+++ ui/pages/notification-details/notification-details.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useHistory, useLocation } from 'react-router-dom';
 import { Box } from '../../components/component-library';
 import {
@@ -15,14 +15,9 @@ import { useMarkNotificationAsRead } from '../../hooks/metamask-notifications/us
 import { getMetamaskNotificationById } from '../../selectors/metamask-notifications/metamask-notifications';
 import {
   NotificationComponents,
-  TRIGGER_TYPES,
   hasNotificationComponents,
 } from '../notifications/notification-components';
-import { getNotificationById } from '../../selectors';
 import { type Notification } from '../notifications/notification-components/types/notifications/notifications';
-import { markNotificationsAsRead as markSnapNotificationsAsRead } from '../../store/actions';
-import { processSnapNotifications } from '../notifications/snap/utils/utils';
-import { RawSnapNotification } from '../notifications/snap/types/types';
 import { getExtractIdentifier } from './utils/utils';
 import { NotificationDetailsHeader } from './notification-details-header/notification-details-header';
 import { NotificationDetailsBody } from './notification-details-body/notification-details-body';
@@ -43,19 +38,7 @@ function useModalNavigation() {
 function useNotificationByPath() {
   const { pathname } = useLocation();
   const id = getExtractIdentifier(pathname);
-  let notification = useSelector(
-    getMetamaskNotificationById(id),
-  ) as unknown as Notification;
-  if (!notification) {
-    // we only reach here if it is a snap notification
-    const snapNotification = useSelector((state) =>
-      // @ts-expect-error improperly typed
-      getNotificationById(state, id),
-    ) as unknown as RawSnapNotification;
-    if (snapNotification) {
-      notification = processSnapNotifications([snapNotification])[0];
-    }
-  }
+  const notification = useSelector(getMetamaskNotificationById(id));
 
   return {
     notification,
@@ -64,20 +47,15 @@ function useNotificationByPath() {
 
 function useEffectOnNotificationView(notificationData?: Notification) {
   const { markNotificationAsRead } = useMarkNotificationAsRead();
-  const dispatch = useDispatch();
   useEffect(() => {
     if (notificationData) {
-      if (notificationData.type === TRIGGER_TYPES.SNAP) {
-        dispatch(markSnapNotificationsAsRead([notificationData.id]));
-      } else {
-        markNotificationAsRead([
-          {
-            id: notificationData.id,
-            type: notificationData.type,
-            isRead: notificationData.isRead,
-          },
-        ]);
-      }
+      markNotificationAsRead([
+        {
+          id: notificationData.id,
+          type: notificationData.type,
+          isRead: notificationData.isRead,
+        },
+      ]);
     }
   }, [markNotificationAsRead, notificationData]);
 }
