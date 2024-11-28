3d6eb8f0532e64ec3e20f424fa19d3e57b7900ae
Alejandro Garcia Anglada <aganglada@gmail.com>
2024-11-28 11:56:41 +0100

    chore: fix test

--- ui/components/app/wallet-overview/non-evm-overview.test.tsx
+++ ui/components/app/wallet-overview/non-evm-overview.test.tsx
@@ -17,8 +17,8 @@ import {
   MetaMetricsEventName,
 } from '../../../../shared/constants/metametrics';
 import useMultiPolling from '../../../hooks/useMultiPolling';
-import NonEvmOverview from './non-evm-overview';
 import { BITCOIN_WALLET_SNAP_ID } from '../../../../shared/lib/accounts/bitcoin-wallet-snap';
+import NonEvmOverview from './non-evm-overview';
 
 // We need to mock `dispatch` since we use it for `setDefaultHomeActiveTabName`.
 const mockDispatch = jest.fn().mockReturnValue(() => jest.fn());
