391eef3aa5526ab2a5007dbb08e77532c49c655c
IF <139582705+infiniteflower@users.noreply.github.com>
2024-11-28 12:08:16 -0500

    chore: improve typing for active quote to have better pricing data

--- ui/pages/bridge/hooks/useSubmitBridgeTransaction.ts
+++ ui/pages/bridge/hooks/useSubmitBridgeTransaction.ts
@@ -2,7 +2,7 @@ import { useDispatch } from 'react-redux';
 import { zeroAddress } from 'ethereumjs-util';
 import { useHistory } from 'react-router-dom';
 import { TransactionMeta } from '@metamask/transaction-controller';
-import { QuoteResponse } from '../types';
+import { QuoteMetadata, QuoteResponse } from '../types';
 import { DEFAULT_ROUTE } from '../../../helpers/constants/routes';
 import { setDefaultHomeActiveTabName } from '../../../store/actions';
 import { startPollingForBridgeTxStatus } from '../../../ducks/bridge-status/actions';
@@ -17,7 +17,9 @@ export default function useSubmitBridgeTransaction() {
   const { handleApprovalTx } = useHandleApprovalTx();
   const { handleBridgeTx } = useHandleBridgeTx();
 
-  const submitBridgeTransaction = async (quoteResponse: QuoteResponse) => {
+  const submitBridgeTransaction = async (
+    quoteResponse: QuoteResponse & QuoteMetadata,
+  ) => {
     // Execute transaction(s)
     let approvalTxMeta: TransactionMeta | undefined;
     if (quoteResponse?.approval) {
