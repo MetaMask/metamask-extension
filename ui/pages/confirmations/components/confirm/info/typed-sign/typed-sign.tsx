9b067c82e96d78e93e716b146810471ebe3dbcad
Jyoti Puri <jyotipuri@gmail.com>
2024-11-28 21:37:21 +0530

    feat: adding e2e for signature decoding api and enable it in extension (#28423)
    ## **Description**
    
    adding e2e for signature decoding api and enable it in extension
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/MetaMask-planning/issues/3625,
    https://github.com/MetaMask/MetaMask-planning/issues/3692
    
    ## **Manual testing steps**
    NA
    
    ## **Screenshots/Recordings**
    NA
    
    ## **Pre-merge author checklist**
    
    - [X] I've followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
    Extension Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/CODING_GUIDELINES.md).
    - [X] I've completed the PR template to the best of my ability
    - [X] I’ve included tests if applicable
    - [X] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [X] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- ui/pages/confirmations/components/confirm/info/typed-sign/typed-sign.tsx
+++ ui/pages/confirmations/components/confirm/info/typed-sign/typed-sign.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { isValidAddress } from 'ethereumjs-util';
 
 import { ConfirmInfoAlertRow } from '../../../../../../components/app/confirm/info/row/alert-row/alert-row';
-import { MESSAGE_TYPE } from '../../../../../../../shared/constants/app';
 import { parseTypedDataMessage } from '../../../../../../../shared/modules/transaction.utils';
 import { RowAlertKey } from '../../../../../../components/app/confirm/info/row/constants';
 import {
@@ -21,7 +19,7 @@ import {
   isPermitSignatureRequest,
 } from '../../../../utils';
 import { useConfirmContext } from '../../../../context/confirm';
-import { selectUseTransactionSimulations } from '../../../../selectors/preferences';
+import { useTypesSignSimulationEnabledInfo } from '../../../../hooks/useTypesSignSimulationEnabledInfo';
 import { ConfirmInfoRowTypedSignData } from '../../row/typed-sign-data/typedSignData';
 import { isSnapId } from '../../../../../../helpers/utils/snaps';
 import { SigningInWithRow } from '../shared/sign-in-with-row/sign-in-with-row';
@@ -30,9 +28,7 @@ import { TypedSignV4Simulation } from './typed-sign-v4-simulation';
 const TypedSignInfo: React.FC = () => {
   const t = useI18nContext();
   const { currentConfirmation } = useConfirmContext<SignatureRequestType>();
-  const useTransactionSimulations = useSelector(
-    selectUseTransactionSimulations,
-  );
+  const isSimulationSupported = useTypesSignSimulationEnabledInfo();
 
   if (!currentConfirmation?.msgParams) {
     return null;
@@ -44,9 +40,6 @@ const TypedSignInfo: React.FC = () => {
   } = parseTypedDataMessage(currentConfirmation.msgParams.data as string);
 
   const isPermit = isPermitSignatureRequest(currentConfirmation);
-  const isTypedSignV4 =
-    currentConfirmation.msgParams.signatureMethod ===
-    MESSAGE_TYPE.ETH_SIGN_TYPED_DATA_V4;
   const isOrder = isOrderSignatureRequest(currentConfirmation);
   const tokenContract = isPermit || isOrder ? verifyingContract : undefined;
   const { decimalsNumber } = useGetTokenStandardAndDetails(tokenContract);
@@ -60,7 +53,7 @@ const TypedSignInfo: React.FC = () => {
 
   return (
     <>
-      {isTypedSignV4 && useTransactionSimulations && <TypedSignV4Simulation />}
+      {isSimulationSupported && <TypedSignV4Simulation />}
       <ConfirmInfoSection data-testid="confirmation_request-section">
         {isPermit && (
           <>
@@ -91,7 +84,7 @@ const TypedSignInfo: React.FC = () => {
       <ConfirmInfoSection data-testid="confirmation_message-section">
         <ConfirmInfoRow
           label={t('message')}
-          collapsed={isPermit && useTransactionSimulations}
+          collapsed={isSimulationSupported}
           copyEnabled
           copyText={JSON.stringify(parseTypedDataMessage(msgData ?? {}))}
         >
