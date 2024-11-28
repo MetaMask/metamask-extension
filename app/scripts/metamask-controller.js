730b3055263ed5dcb9a28e0dbdb4281db213600d
Monte Lai <monte.lai@consensys.net>
2024-11-28 12:27:05 +0800

    feat: add subscription to CurrencyRatesController

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -3244,6 +3244,13 @@ export default class MetamaskController extends EventEmitter {
     );
     this.multichainBalancesController.start();
     this.multichainBalancesController.updateBalances();
+
+    this.controllerMessenger.subscribe(
+      'CurrencyRateController:stateChange',
+      ({ currentCurrency }) => {
+        this.multichainRatesController.setFiatCurrency(currentCurrency);
+      },
+    );
   }
 
   /**
