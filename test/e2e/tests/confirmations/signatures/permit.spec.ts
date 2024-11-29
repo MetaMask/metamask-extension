85978ed34cea1a12573d156ada1e48143c07ed42
Jyoti Puri <jyotipuri@gmail.com>
2024-11-29 19:13:31 +0530

    update

--- test/e2e/tests/confirmations/signatures/permit.spec.ts
+++ test/e2e/tests/confirmations/signatures/permit.spec.ts
@@ -69,8 +69,6 @@ describe('Confirmation Signature - Permit @no-mmi', function (this: Suite) {
           signatureType: 'eth_signTypedData_v4',
           primaryType: 'Permit',
           uiCustomizations: ['redesigned_confirmation', 'permit'],
-          decodingChangeTypes: ['RECEIVE', 'LISTING'],
-          decodingResponse: 'CHANGE',
         });
 
         await assertVerifiedResults(driver, publicAddress);
@@ -107,8 +105,6 @@ describe('Confirmation Signature - Permit @no-mmi', function (this: Suite) {
           primaryType: 'Permit',
           uiCustomizations: ['redesigned_confirmation', 'permit'],
           location: 'confirmation',
-          decodingChangeTypes: ['RECEIVE', 'LISTING'],
-          decodingResponse: 'CHANGE',
         });
       },
       mockSignatureRejected,
