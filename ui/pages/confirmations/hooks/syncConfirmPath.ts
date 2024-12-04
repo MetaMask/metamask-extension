71bea9497481c5b18240b7dd516d8f8ef3831110
Matthew Walsh <matthew.walsh@consensys.net>
2024-12-04 20:22:09 +0000

    Fix E2E and integration tests

--- ui/pages/confirmations/hooks/syncConfirmPath.ts
+++ ui/pages/confirmations/hooks/syncConfirmPath.ts
@@ -1,6 +1,9 @@
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
+import { ApprovalType, ORIGIN_METAMASK } from '@metamask/controller-utils';
+import { ApprovalRequest } from '@metamask/approval-controller';
+import { Json } from '@metamask/utils';
 import { Confirmation } from '../types/confirm';
 import { useConfirmationNavigation } from './useConfirmationNavigation';
 
@@ -11,6 +14,19 @@ const syncConfirmPath = (currentConfirmation?: Confirmation) => {
 
   useEffect(() => {
     if (!confirmationId) {
+      return;
+    }
+
+    const approvalRequest = currentConfirmation as ApprovalRequest<
+      Record<string, Json>
+    >;
+
+    // Do not redirect if the current confirmation is a wallet triggered add network approval.
+    // Handled by NetworkConfirmationPopover component in routes.
+    if (
+      approvalRequest.type === ApprovalType.AddEthereumChain &&
+      approvalRequest.origin === ORIGIN_METAMASK
+    ) {
       return;
     }
 
