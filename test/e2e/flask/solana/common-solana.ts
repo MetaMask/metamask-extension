7fd295c447bac4df0de9e03d55cb400cde05a55d
Javier <javier.vera@consensys.net>
2024-12-05 10:49:28 +0100

    feat: tests done

--- test/e2e/flask/solana/common-solana.ts
+++ test/e2e/flask/solana/common-solana.ts
@@ -1,5 +1,4 @@
 import { Mockttp } from 'mockttp';
-import FixtureBuilder from '../../fixture-builder';
 import { withFixtures, unlockWallet } from '../../helpers';
 import {
   DEFAULT_BTC_ACCOUNT,
@@ -13,37 +12,16 @@ import {
 } from '../../constants';
 import { MultichainNetworks } from '../../../../shared/constants/multichain/networks';
 import { Driver } from '../../webdriver/driver';
-import messages from '../../../../app/_locales/en/messages.json';
 import HeaderNavbar from '../../page-objects/pages/header-navbar';
 import AccountListPage, { AccountType } from '../../page-objects/pages/account-list-page';
+import FixtureBuilder = require('../../fixture-builder');
 
-const QUICKNODE_URL_REGEX = /^https:\/\/.*\.solana.*\.quiknode\.pro(\/|$)/u;
+const SOLANA_URL_REGEX = /^https:\/\/.*\.solana.*/;
 
 export enum SendFlowPlaceHolders {
   AMOUNT = 'Enter amount to send',
   RECIPIENT = 'Enter receiving address',
   LOADING = 'Preparing transaction',
-}
-
-export async function createSolanaAccount(driver: Driver) {
-  await driver.clickElement('[data-testid="account-menu-icon"]');
-  await driver.clickElement(
-    '[data-testid="multichain-account-menu-popover-action-button"]',
-  );
-  await driver.clickElement({
-    text: messages.addNewSolanaAccount.message,
-    tag: 'button',
-  });
-  await driver.clickElementAndWaitToDisappear(
-    {
-      text: 'Add account',
-      tag: 'button',
-    },
-    // Longer timeout than usual, this reduces the flakiness
-    // around Bitcoin account creation (mainly required for
-    // Firefox)
-    5000,
-  );
 }
 
 export function solanaToSats(btc: number): number {
@@ -56,7 +34,7 @@ export async function mockSolanaBalanceQuote(
   address: string = DEFAULT_SOLANA_ACCOUNT,
 ) {
   return await mockServer
-    .forPost(QUICKNODE_URL_REGEX)
+    .forPost(SOLANA_URL_REGEX)
     .withJsonBodyIncluding({
       method: 'getBalance',
     })
@@ -65,145 +43,18 @@ export async function mockSolanaBalanceQuote(
         statusCode: 200,
         json: {
           result: {
-            address,
-            balance: solanaToSats(DEFAULT_BTC_BALANCE).toString(), // Converts from BTC to sats
-            totalReceived: '0',
-            totalSent: '0',
-            unconfirmedBalance: '0',
-            unconfirmedTxs: 0,
-            txs: 0,
-          },
-        },
-      };
-    });
-}
-
-export async function mockBtcFeeCallQuote(mockServer: Mockttp) {
-  return await mockServer
-    .forPost(QUICKNODE_URL_REGEX)
-    .withJsonBodyIncluding({
-      method: 'estimatesmartfee',
-    })
-    .thenCallback(() => {
-      return {
-        statusCode: 200,
-        json: {
-          result: {
-            blocks: 1,
-            feerate: DEFAULT_BTC_FEES_RATE, // sats
-          },
-        },
-      };
-    });
-}
-
-export async function mockMempoolInfo(mockServer: Mockttp) {
-  return await mockServer
-    .forPost(QUICKNODE_URL_REGEX)
-    .withJsonBodyIncluding({
-      method: 'getmempoolinfo',
-    })
-    .thenCallback(() => {
-      return {
-        statusCode: 200,
-        json: {
-          result: {
-            loaded: true,
-            size: 165194,
-            bytes: 93042828,
-            usage: 550175264,
-            total_fee: 1.60127931,
-            maxmempool: 2048000000,
-            mempoolminfee: DEFAULT_BTC_FEES_RATE,
-            minrelaytxfee: DEFAULT_BTC_FEES_RATE,
-            incrementalrelayfee: 0.00001,
-            unbroadcastcount: 0,
-            fullrbf: true,
-          },
-        },
-      };
-    });
-}
-
-export async function mockGetUTXO(mockServer: Mockttp) {
-  return await mockServer
-    .forPost(QUICKNODE_URL_REGEX)
-    .withJsonBodyIncluding({
-      method: 'bb_getutxos',
-    })
-    .thenCallback(() => {
-      return {
-        statusCode: 200,
-        json: {
-          result: [
-            {
-              txid: DEFAULT_BTC_TRANSACTION_ID,
-              vout: 0,
-              value: solanaToSats(DEFAULT_BTC_BALANCE).toString(),
-              height: 101100110,
-              confirmations: 6,
+            context: {
+              apiVersion: '2.0.15',
+              slot: 305352614
             },
-          ],
-        },
-      };
-    });
-}
-
-export async function mockSendTransaction(mockServer: Mockttp) {
-  return await mockServer
-    .forPost(QUICKNODE_URL_REGEX)
-    .withJsonBodyIncluding({
-      method: 'sendrawtransaction',
-    })
-    .thenCallback(() => {
-      return {
-        statusCode: 200,
-        json: {
-          result: DEFAULT_BTC_TRANSACTION_ID,
-        },
-      };
-    });
-}
-
-export async function mockRatesCall(mockServer: Mockttp) {
-  return await mockServer
-    .forGet('https://min-api.cryptocompare.com/data/pricemulti')
-    .withQuery({ fsyms: 'sol', tsyms: 'usd,USD' })
-    .thenCallback(() => {
-      return {
-        statusCode: 200,
-        json: { SOL: { USD: DEFAULT_SOL_CONVERSION_RATE } },
-      };
-    });
-}
-
-export async function mockRampsDynamicFeatureFlag(
-  mockServer: Mockttp,
-  subDomain: string,
-) {
-  return await mockServer
-    .forGet(
-      `https://on-ramp-content.${subDomain}.cx.metamask.io/regions/networks`,
-    )
-    .withQuery({
-      context: 'extension',
-    })
-    .thenCallback(() => ({
-      statusCode: 200,
-      json: {
-        networks: [
-          {
-            active: true,
-            chainId: MultichainNetworks.SOLANA,
-            chainName: 'Solana',
-            shortName: 'Solana',
-            nativeTokenSupported: true,
-            isEvm: false,
+            value: 0,
           },
-        ],
-      },
-    }));
+        },
+      };
+    });
 }
+
+
 
 export async function withSolanaAccountSnap(
   {
@@ -222,16 +73,7 @@ export async function withSolanaAccountSnap(
       title,
       dapp: true,
       testSpecificMock: async (mockServer: Mockttp) => [
-        await mockRatesCall(mockServer),
         await mockSolanaBalanceQuote(mockServer),
-        // See: PROD_RAMP_API_BASE_URL
-        //await mockRampsDynamicFeatureFlag(mockServer, 'api'),
-        // See: UAT_RAMP_API_BASE_URL
-        //await mockRampsDynamicFeatureFlag(mockServer, 'uat-api'),
-       // await mockMempoolInfo(mockServer),
-        //await mockBtcFeeCallQuote(mockServer),
-        //await mockGetUTXO(mockServer),
-        //await mockSendTransaction(mockServer),
       ],
     },
     async ({ driver, mockServer }: { driver: Driver; mockServer: Mockttp }) => {
@@ -244,26 +86,4 @@ export async function withSolanaAccountSnap(
       await test(driver, mockServer);
     },
   );
-}
-
-export async function getQuickNodeSeenRequests(mockServer: Mockttp) {
-  const seenRequests = await Promise.all(
-    (
-      await mockServer.getMockedEndpoints()
-    ).map((mockedEndpoint) => mockedEndpoint.getSeenRequests()),
-  );
-  return seenRequests
-    .flat()
-    .filter((request) => request.url.match(QUICKNODE_URL_REGEX));
-}
-
-export async function getTransactionRequest(mockServer: Mockttp) {
-  // Check that the transaction has been sent.
-  const transactionRequest = (await getQuickNodeSeenRequests(mockServer)).find(
-    async (request) => {
-      const body = (await request.body.getJson()) as { method: string };
-      return body.method === 'sendrawtransaction';
-    },
-  );
-  return transactionRequest;
 }
--- test/e2e/flask/solana/create-solana-account.spec.ts
+++ test/e2e/flask/solana/create-solana-account.spec.ts
@@ -10,7 +10,11 @@ describe('Create/Remove Solana Account', function (this: Suite) {
       async (driver) => {
         const headerNavbar = new HeaderNavbar(driver);
         await headerNavbar.check_pageIsLoaded();
-        await headerNavbar.check_accountLabel('Solana Account 0');
+        await headerNavbar.check_accountLabel('Solana Account 1');
+        await headerNavbar.openAccountMenu();
+        const accountListPage = new AccountListPage(driver);
+        await accountListPage.check_accountDisplayedInAccountList('Account 1');
+        await accountListPage.check_numberOfAvailableAccounts(2)
       },
     );
   });
@@ -21,21 +25,39 @@ describe('Create/Remove Solana Account', function (this: Suite) {
         // check that we have one Solana account
         const headerNavbar = new HeaderNavbar(driver);
         await headerNavbar.check_pageIsLoaded();
-        await headerNavbar.check_accountLabel('Solana Account 0');
-
+        await headerNavbar.check_accountLabel('Solana Account 1');
         // check user can cancel the removal of the Solana account
         await headerNavbar.openAccountMenu();
         const accountListPage = new AccountListPage(driver);
-        await accountListPage.check_pageIsLoaded();
-        await accountListPage.removeAccount('Solana Account 0', true);
-        // check the number of accounts. it should be 1.
+        await accountListPage.check_accountDisplayedInAccountList('Account 1');
+        await accountListPage.check_numberOfAvailableAccounts(2)
+        await accountListPage.removeAccount('Solana Account 1', true);
+        await headerNavbar.check_accountLabel('Account 1');
         await headerNavbar.openAccountMenu();
-        try {
-          await accountListPage.check_numberOfAvailableAccounts(1);
-        } catch (error) {
-          console.error('Error checking number of available accounts:', error);
-          throw error; // Re-throw the error to fail the test
-        }
+        await accountListPage.check_accountDisplayedInAccountList('Account 1');
+        await accountListPage.check_numberOfAvailableAccounts(1)
+      },
+    );
+  });
+  it('Create 2 Solana accounts', async function () {
+    await withSolanaAccountSnap(
+      { title: this.test?.fullTitle() },
+      async (driver) => {
+        // check that we have one Solana account
+        const headerNavbar = new HeaderNavbar(driver);
+        await headerNavbar.check_pageIsLoaded();
+        await headerNavbar.check_accountLabel('Solana Account 1');
+
+        await headerNavbar.openAccountMenu();
+        const accountListPage = new AccountListPage(driver);
+        await accountListPage.openAddAccountModal();
+        await accountListPage.addNewSolanaAccount();
+        await headerNavbar.check_accountLabel('Solana Account 2');
+        await headerNavbar.openAccountMenu();
+        await accountListPage.check_numberOfAvailableAccounts(3)
+        await accountListPage.check_accountDisplayedInAccountList('Account 1');
+        await accountListPage.check_accountDisplayedInAccountList('Solana Account 1');
+        await accountListPage.check_accountDisplayedInAccountList('Solana Account 2');
       },
     );
   });
