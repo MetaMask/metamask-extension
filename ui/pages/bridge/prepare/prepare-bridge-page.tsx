70c2e7a1c4fd52191ba3da8af0f89defec21b2c5
Micaela Estabillo <micaela.estabillo@consensys.net>
2024-12-05 13:42:56 -0800

    chore: enable multichain selection in bridge page

--- ui/pages/bridge/prepare/prepare-bridge-page.tsx
+++ ui/pages/bridge/prepare/prepare-bridge-page.tsx
@@ -240,6 +240,7 @@ const PrepareBridgePage = () => {
             autoFocus: true,
             value: fromAmount || undefined,
           }}
+          isMultiselectEnabled={true}
         />
 
         <Box className="prepare-bridge-page__switch-tokens">
