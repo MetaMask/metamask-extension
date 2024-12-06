1b569fb61ae0a0290d1dfa0e18f1e8e09713864d
Micaela Estabillo <micaela.estabillo@consensys.net>
2024-12-06 09:46:22 -0800

    fix: rm AssetList margin-right if no token is selected

--- ui/components/multichain/asset-picker-amount/asset-picker-modal/AssetList.tsx
+++ ui/components/multichain/asset-picker-amount/asset-picker-modal/AssetList.tsx
@@ -121,7 +121,7 @@ export default function AssetList({
               flexWrap={FlexWrap.NoWrap}
               alignItems={AlignItems.center}
             >
-              <Box marginInlineStart={2}>
+              <Box marginInlineStart={selectedToken ? 2 : 0}>
                 {token.type === AssetType.native &&
                 token.chainId === chainId &&
                 isSelectedNetworkActive ? (
