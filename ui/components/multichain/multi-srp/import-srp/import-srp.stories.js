ae06ccf0c7441ae667fd29601e6b3d81af4a9137
Patryk Łucka <5708018+PatrykLucka@users.noreply.github.com>
2024-12-09 19:07:15 +0100

    chore(multi-srp): add import codefence, tests and story

---
+++ ui/components/multichain/multi-srp/import-srp/import-srp.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { ImportSRP } from '.';
+
+export default {
+  title: 'Components/Multichain/ImportSRP',
+  component: ImportSRP,
+};
+
+export const DefaultStory = (args) => <ImportSRP {...args} />;
+
+DefaultStory.storyName = 'Default';
---
+++ ui/components/multichain/multi-srp/import-srp/index.js
@@ -0,0 +1,1 @@
+export { ImportSRP } from './import-srp';
