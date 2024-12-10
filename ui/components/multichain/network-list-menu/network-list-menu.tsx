297edee90f0f36e1d52ac1e1a4f5b481e592af6b
Brian Bergeron <brian.e.bergeron@gmail.com>
2024-12-10 13:09:25 -0800

    .

--- ui/components/multichain/network-list-menu/network-list-menu.tsx
+++ ui/components/multichain/network-list-menu/network-list-menu.tsx
@@ -55,7 +55,6 @@ import {
   getPermittedChainsForSelectedTab,
   getPermittedAccountsForSelectedTab,
   getPreferences,
-  getTokenNetworkFilter,
 } from '../../../selectors';
 import ToggleButton from '../../ui/toggle-button';
 import {
@@ -117,7 +116,7 @@ export const NetworkListMenu = ({ onClose }: { onClose: () => void }) => {
   const dispatch = useDispatch();
   const trackEvent = useContext(MetaMetricsContext);
 
-  const tokenNetworkFilter = useSelector(getTokenNetworkFilter);
+  const { tokenNetworkFilter } = useSelector(getPreferences);
   const showTestNetworks = useSelector(getShowTestNetworks);
   const currentChainId = useSelector(getCurrentChainId);
   const selectedTabOrigin = useSelector(getOriginOfCurrentTab);
@@ -295,7 +294,7 @@ export const NetworkListMenu = ({ onClose }: { onClose: () => void }) => {
 
           // as a user, I don't want my network selection to force update my filter when I have "All Networks" toggled on
           // however, if I am already filtered on "Current Network", we'll want to filter by the selected network when the network changes
-          if (Object.keys(tokenNetworkFilter).length <= 1) {
+          if (Object.keys(tokenNetworkFilter ?? {}).length <= 1) {
             dispatch(setTokenNetworkFilter({ [network.chainId]: true }));
           } else if (process.env.PORTFOLIO_VIEW) {
             dispatch(setTokenNetworkFilter(allOpts));
