61e886c1f284f18b6e9226aea540f512afbcbb6d
Jiexi Luan <jiexiluan@gmail.com>
2024-12-11 11:28:18 -0800

    mvoe endowment:caip25 into ExcludedSnapEndowments

--- shared/constants/snaps/permissions.ts
+++ shared/constants/snaps/permissions.ts
@@ -16,10 +16,11 @@ export const EndowmentPermissions = Object.freeze({
 
 // Methods / permissions in external packages that we are temporarily excluding.
 export const ExcludedSnapPermissions = Object.freeze({
+});
+
+export const ExcludedSnapEndowments = Object.freeze({
   'endowment:caip25':
     'eth_accounts is disabled. For more information please see https://github.com/MetaMask/snaps/issues/990.',
 });
-
-export const ExcludedSnapEndowments = Object.freeze({});
 
 export const DynamicSnapPermissions = Object.freeze(['endowment:caip25']);
