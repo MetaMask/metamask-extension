5395c515d4e56f29c147be6bbfeb72d518db3943
Micaela Estabillo <micaela.estabillo@consensys.net>
2024-12-11 11:32:03 -0800

    use multichain asset in bridge

--- ui/pages/bridge/types.ts
+++ ui/pages/bridge/types.ts
@@ -29,7 +29,7 @@ export enum SortOrder {
 }
 
 export type BridgeToken =
-  | ((AssetWithDisplayData<NativeAsset> | AssetWithDisplayData<ERC20Asset>) & {
+  | (AssetWithDisplayData<NativeAsset | ERC20Asset> & {
       aggregators?: string[];
       address: string;
     })
