47ebd5ee77ee45e3e58aac052385eb8c88e176c1
Brian Bergeron <brian.e.bergeron@gmail.com>
2024-12-10 21:11:17 -0800

    feat: only poll popular networks (#29071)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    To reduce the impact of showing assets across all networks, which can be
    unbounded, this PR scopes the portfolio view to the 9 popular networks
    built into metamask.
    
    [![Open in GitHub
    Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/29071?quickstart=1)
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/metamask-extension/issues/29055
    
    ## **Manual testing steps**
    
    1. On popular networks:
    - The token filter should allow switching between popular networks and
    current network
      - The filter should work as described
      - RPC requests in the background should only hit the popular networks
    
    2. On other networks
    - The token filter should become disabled and scoped to the current
    network
      - RPC requests in the background should only hit the current network
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [ ] I've followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
    Extension Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: Nicholas Gambino <nicholas.gambino@consensys.net>

--- ui/components/app/assets/asset-list/network-filter/network-filter.tsx
+++ ui/components/app/assets/asset-list/network-filter/network-filter.tsx
@@ -119,7 +119,7 @@ const NetworkFilter = ({ handleClose }: SortControlProps) => {
               variant={TextVariant.bodyMdMedium}
               color={TextColor.textDefault}
             >
-              {t('allNetworks')}
+              {t('popularNetworks')}
             </Text>
             <Text
               variant={TextVariant.bodySmMedium}
