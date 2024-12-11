21764622618a93d55274c954f5431b4adbf5814d
ffmcgee <jc.992@hotmail.com>
2024-12-11 20:05:54 +0100

    feat: send extensionId in metamask_getProviderState

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -3412,7 +3412,7 @@ export default class MetamaskController extends EventEmitter {
    * Gets relevant state for the provider of an external origin.
    *
    * @param {string} origin - The origin to get the provider state for.
-   * @returns {Promise<{ isUnlocked: boolean, networkVersion: string, chainId: string, accounts: string[] }>} An object with relevant state properties.
+   * @returns {Promise<{ isUnlocked: boolean, networkVersion: string, chainId: string, accounts: string[], extensionId: string | undefined }>} An object with relevant state properties.
    */
   async getProviderState(origin) {
     const providerNetworkState = await this.getProviderNetworkState(
@@ -3422,6 +3422,9 @@ export default class MetamaskController extends EventEmitter {
     return {
       isUnlocked: this.isUnlocked(),
       accounts: this.getPermittedAccounts(origin),
+      ///: BEGIN:ONLY_INCLUDE_IF(build-flask)
+      extensionId: chrome?.runtime?.id,
+      ///: END:ONLY_INCLUDE_IF
       ...providerNetworkState,
     };
   }
