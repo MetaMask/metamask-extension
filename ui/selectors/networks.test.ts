8fe8b2255380c7f8790877f523c98d176a11c4f0
Jongsun Suh <jongsun.suh@icloud.com>
2024-12-11 12:54:07 -0500

    Reduce diffs

--- ui/selectors/networks.test.ts
+++ ui/selectors/networks.test.ts
@@ -43,30 +43,83 @@ describe('Network Selectors', () => {
       expect(
         networks.getNetworkConfigurationsByChainId({
           metamask: {
-            NetworkController: {
-              networkConfigurationsByChainId,
-            },
+            networkConfigurationsByChainId,
           },
         }),
       ).toStrictEqual(networkConfigurationsByChainId);
     });
   });
 
+  describe('#getNetworkConfigurations', () => {
+    it('returns undefined if state.metamask.networkConfigurations is undefined', () => {
+      expect(
+        networks.getNetworkConfigurations({
+          metamask: {
+            // @ts-expect-error the types forbid `undefined`. this is a strange test.
+            networkConfigurations: undefined,
+          },
+        }),
+      ).toBeUndefined();
+    });
+
+    it('returns networkConfigurations', () => {
+      const networkConfigurations = {
+        '0x1351': {
+          name: 'TEST',
+          chainId: '0x1351' as const,
+          nativeCurrency: 'TEST',
+          defaultRpcEndpointUrl: 'https://mock-rpc-url-1',
+          defaultRpcEndpointIndex: 0,
+          rpcEndpoints: [
+            {
+              type: RpcEndpointType.Custom as const,
+              networkClientId: 'testNetworkConfigurationId1',
+              url: 'https://mock-rpc-url-1',
+            },
+          ],
+          blockExplorerUrls: [],
+        },
+        '0x1337': {
+          name: 'RPC',
+          chainId: '0x1337' as const,
+          nativeCurrency: 'RPC',
+          defaultRpcEndpointUrl: 'https://mock-rpc-url-2',
+          defaultRpcEndpointIndex: 0,
+          rpcEndpoints: [
+            {
+              type: RpcEndpointType.Custom as const,
+              networkClientId: 'testNetworkConfigurationId2',
+              url: 'https://mock-rpc-url-2',
+            },
+          ],
+          blockExplorerUrls: [],
+        },
+      };
+      expect(
+        networks.getNetworkConfigurations({
+          metamask: {
+            networkConfigurations,
+          },
+        }),
+      ).toStrictEqual(networkConfigurations);
+    });
+  });
+
   describe('#getInfuraBlocked', () => {
     it('returns getInfuraBlocked', () => {
-      let isInfuraBlocked = networks.getInfuraBlocked(mockState);
+      let isInfuraBlocked = networks.getInfuraBlocked(
+        mockState as networks.NetworkState,
+      );
       expect(isInfuraBlocked).toBe(false);
 
       const modifiedMockState = {
         ...mockState,
         metamask: {
-          NetworkController: {
-            ...mockState.metamask,
-            ...mockNetworkState({
-              chainId: CHAIN_IDS.GOERLI,
-              metadata: { status: NetworkStatus.Blocked, EIPS: {} },
-            }),
-          },
+          ...mockState.metamask,
+          ...mockNetworkState({
+            chainId: CHAIN_IDS.GOERLI,
+            metadata: { status: NetworkStatus.Blocked, EIPS: {} },
+          }),
         },
       };
       isInfuraBlocked = networks.getInfuraBlocked(modifiedMockState);
