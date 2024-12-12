8c9d3902db96f40fcec2f7aa6e0fd42178675536
Jongsun Suh <jongsun.suh@icloud.com>
2024-12-12 12:19:14 -0500

    Move from `app/scripts/controllers/bridge/constants.ts` to `shared/constants/bridge.ts`: `RequestStatus`, `METABRIDGE_CHAIN_TO_ADDRESS_MAP`

--- shared/constants/bridge.ts
+++ shared/constants/bridge.ts
@@ -1,3 +1,4 @@
+import { Hex } from '@metamask/utils';
 import { CHAIN_IDS, NETWORK_TO_NAME_MAP } from './network';
 
 // TODO read from feature flags
@@ -13,6 +14,12 @@ export const ALLOWED_BRIDGE_CHAIN_IDS = [
   CHAIN_IDS.BASE,
 ];
 
+export enum RequestStatus {
+  LOADING,
+  FETCHED,
+  ERROR,
+}
+
 export type AllowedBridgeChainIds = (typeof ALLOWED_BRIDGE_CHAIN_IDS)[number];
 
 export const BRIDGE_DEV_API_BASE_URL = 'https://bridge.dev-api.cx.metamask.io';
@@ -26,6 +33,11 @@ export const BRIDGE_CLIENT_ID = 'extension';
 export const ETH_USDT_ADDRESS = '0xdac17f958d2ee523a2206206994597c13d831ec7';
 export const METABRIDGE_ETHEREUM_ADDRESS =
   '0x0439e60F02a8900a951603950d8D4527f400C3f1';
+
+export const METABRIDGE_CHAIN_TO_ADDRESS_MAP: Record<Hex, string> = {
+  [CHAIN_IDS.MAINNET]: METABRIDGE_ETHEREUM_ADDRESS,
+};
+
 export const BRIDGE_QUOTE_MAX_ETA_SECONDS = 60 * 60; // 1 hour
 export const BRIDGE_QUOTE_MAX_RETURN_DIFFERENCE_PERCENTAGE = 0.8; // if a quote returns in x times less return than the best quote, ignore it
 
