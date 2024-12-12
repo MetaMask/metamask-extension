50be87afb6e43fe8779fbf776ea5bbb2c5b96fad
httpjunkie <eric@httpjunkie.com>
2024-12-12 08:07:22 -0500

    This seems to be the correct way to skip these tests rather than `xdescribe`

--- test/e2e/tests/metrics/swaps.spec.js
+++ test/e2e/tests/metrics/swaps.spec.js
@@ -90,7 +90,9 @@ async function mockSegmentAndMetaswapRequests(mockServer) {
   ];
 }
 
-xdescribe('Swap Eth for another Token @no-mmi', function () {
+// TODO: (MM-PENDING) These tests are planned for deprecation as part of swaps testing revamp
+// eslint-disable-next-line mocha/no-skipped-tests
+describe.skip('Swap Eth for another Token @no-mmi', function () {
   it('Completes a Swap between ETH and DAI after changing initial rate', async function () {
     const { initialBalanceInHex } = genRandInitBal();
 
