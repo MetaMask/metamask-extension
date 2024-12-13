370f06e2385007ffddf7109a8f4a10d82c70a9fd
Charly Chevalier <charly.chevalier@consensys.net>
2024-12-13 18:11:34 +0100

    chore: add local bitcoin snap (hack, needs to be removed)

--- app/scripts/snaps/preinstalled-snaps.ts
+++ app/scripts/snaps/preinstalled-snaps.ts
@@ -3,11 +3,12 @@ import MessageSigningSnap from '@metamask/message-signing-snap/dist/preinstalled
 import EnsResolverSnap from '@metamask/ens-resolver-snap/dist/preinstalled-snap.json';
 ///: BEGIN:ONLY_INCLUDE_IF(build-flask)
 import AccountWatcherSnap from '@metamask/account-watcher/dist/preinstalled-snap.json';
-import BitcoinWalletSnap from '@metamask/bitcoin-wallet-snap/dist/preinstalled-snap.json';
+// import BitcoinWalletSnap from '@metamask/bitcoin-wallet-snap/dist/preinstalled-snap.json';
 import PreinstalledExampleSnap from '@metamask/preinstalled-example-snap/dist/preinstalled-snap.json';
 ///: END:ONLY_INCLUDE_IF
 ///: BEGIN:ONLY_INCLUDE_IF(solana)
 import SolanaWalletSnap from '@metamask/solana-wallet-snap/dist/preinstalled-snap.json';
+import BitcoinWalletSnap from './snap-bitcoin-wallet-preinstalled-snap.json';
 ///: END:ONLY_INCLUDE_IF
 
 // The casts here are less than ideal but we expect the SnapController to validate the inputs.
