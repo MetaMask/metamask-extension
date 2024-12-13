70d7ed793816c4f4a5bdcbfa0beb5024897e6e2b
Alex <adonesky@gmail.com>
2024-12-13 17:31:26 -0600

    more wip

--- test/e2e/flask/multichain-api/connect.spec.ts
+++ test/e2e/flask/multichain-api/connect.spec.ts
@@ -1,148 +1,46 @@
-import { Driver } from "../../webdriver/driver";
-
-const { By } = require('selenium-webdriver');
-const FixtureBuilder = require('../../fixture-builder');
-const {
-  withFixtures,
+import {
+  DAPP_URL,
   openDapp,
   unlockWallet,
-  DAPP_URL,
-  DAPP_ONE_URL,
   WINDOW_TITLES,
-  defaultGanacheOptions,
-  largeDelayMs,
-  tempToggleSettingRedesignedTransactionConfirmations,
-} = require('../../helpers');
+  withFixtures,
+} from '../../helpers';
+import * as path from 'path';
+import { Driver } from '../../webdriver/driver';
+const FixtureBuilder = require('../../fixture-builder');
 
-describe('Request Queuing for Multiple Dapps and Txs on different networks', function () {
-    it('should batch confirmation txs for different dapps on different networks.', async function () {
-      const port = 8564;
-      const chainId = 1338;
-      await withFixtures(
-        {
-          dapp: true,
-          // dappOptions: {
-          //   numberOfDapps: 2,
-          // },
-          // dappPath: [
-          //   '..',
-          //   '..',
-          //   'node_modules',
-          //   '@metamask',
-          //   'test-dapp-multichain',
-          //   'dist',
-          // ],
-          fixtures: new FixtureBuilder()
-            // .withNetworkControllerDoubleGanache()
-            .withPreferencesControllerUseRequestQueueEnabled()
-            .build(),
-          // ganacheOptions: {
-          //   ...defaultGanacheOptions,
-          //   concurrent: [
-          //     {
-          //       port,
-          //       chainId,
-          //       ganacheOptions2: defaultGanacheOptions,
-          //     },
-          //   ],
-          // },
-          // title: this.test.fullTitle(),
-        },
+describe('Multichain Connect', function () {
+  it('should connect to the multichain test dapp', async function () {
+    await withFixtures(
+      {
+        dapp: true,
+        dappPaths: [
+          path.join(
+            '..',
+            '..',
+            'node_modules',
+            '@metamask',
+            'test-dapp-multichain',
+            'build',
+          ),
+        ],
+        fixtures: new FixtureBuilder()
+          .withNetworkControllerDoubleGanache()
+          .withPreferencesControllerUseRequestQueueEnabled()
+          .build(),
 
-        async ({ driver }: { driver: Driver }) => {
-          await unlockWallet(driver);
+      },
+      async ({ driver }: { driver: Driver }) => {
+        await unlockWallet(driver);
 
-          await tempToggleSettingRedesignedTransactionConfirmations(driver);
+        await openDapp(driver, undefined, DAPP_URL);
 
-          // Open Dapp One
-          await openDapp(driver, undefined, DAPP_URL);
+        // TODO need data-testid for input
 
-          // Connect to dapp 1
-          await driver.clickElement({ text: 'Connect', tag: 'button' });
+        await driver.clickElement({ text: 'Connect', tag: 'button' });
 
-          await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
-
-          await driver.clickElementAndWaitForWindowToClose({
-            text: 'Connect',
-            tag: 'button',
-          });
-
-          await driver.switchToWindowWithTitle(
-            WINDOW_TITLES.ExtensionInFullScreenView,
-          );
-
-          // Network Selector
-          await driver.clickElement('[data-testid="network-display"]');
-
-          // Switch to second network
-          await driver.clickElement({
-            text: 'Localhost 8546',
-            css: 'p',
-          });
-
-          // Wait for the first dapp's connect confirmation to disappear
-          await driver.waitUntilXWindowHandles(2);
-
-          // TODO: Request Queuing bug when opening both dapps at the same time will have them stuck on the same network, with will be incorrect for one of them.
-          // Open Dapp Two
-          await openDapp(driver, undefined, DAPP_ONE_URL);
-
-          // Connect to dapp 2
-          await driver.clickElement({ text: 'Connect', tag: 'button' });
-
-          await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
-
-          await driver.clickElementAndWaitForWindowToClose({
-            text: 'Connect',
-            tag: 'button',
-          });
-
-          // Dapp one send tx
-          await driver.switchToWindowWithUrl(DAPP_URL);
-          await driver.delay(largeDelayMs);
-          await driver.clickElement('#sendButton');
-          await driver.clickElement('#sendButton');
-
-          await driver.delay(largeDelayMs);
-
-          // Dapp two send tx
-          await driver.switchToWindowWithUrl(DAPP_ONE_URL);
-          await driver.delay(largeDelayMs);
-          await driver.clickElement('#sendButton');
-          await driver.clickElement('#sendButton');
-
-          await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
-
-          await driver.waitForSelector(
-            By.xpath("//div[normalize-space(.)='1 of 2']"),
-          );
-
-          // Reject All Transactions
-          await driver.clickElement('.page-container__footer-secondary a');
-
-          // TODO: Do we want to confirm here?
-          await driver.clickElementAndWaitForWindowToClose({
-            text: 'Reject all',
-            tag: 'button',
-          });
-
-          // Wait for confirmation to close
-          // TODO: find a better way to handle different dialog ids
-          await driver.delay(2000);
-
-          // Wait for new confirmations queued from second dapp to open
-          await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
-
-          await driver.waitForSelector(
-            By.xpath("//div[normalize-space(.)='1 of 2']"),
-          );
-
-          // Check correct network on confirm tx.
-          await driver.findElement({
-            css: '[data-testid="network-display"]',
-            text: 'Localhost 8546',
-          });
-        },
-      );
+        await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
+      },
+    );
   });
 });
