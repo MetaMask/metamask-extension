8cdcc0226f3f3fc36f37bce9fd34920b98d7fbf8
David Murdoch <david@davidmurdoch.com>
2024-12-16 11:30:23 -0500

    test

--- test/e2e/playwright/mmi/helpers/custodian-helper.ts
+++ test/e2e/playwright/mmi/helpers/custodian-helper.ts
@@ -4,6 +4,7 @@ const { MMI_E2E_MMI_CONFIG_URL } = process.env;
 
 export async function getCustodianInfoByName(name: string) {
   // First get an admin token
+  console.error('the name', name);
   try {
     const { custodians } = (await axios.get(`${MMI_E2E_MMI_CONFIG_URL}`)).data;
     // TODO: Replace `any` with type
