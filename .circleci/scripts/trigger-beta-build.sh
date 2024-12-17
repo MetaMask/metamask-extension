0f45c7da4430dda46adaca4c313ffcf74054591b
Norbert Elter <72046715+itsyoboieltr@users.noreply.github.com>
2024-12-18 01:37:27 +0400

    fix: trigger-beta-build.sh

--- .circleci/scripts/trigger-beta-build.sh
+++ .circleci/scripts/trigger-beta-build.sh
@@ -5,7 +5,7 @@ set -u
 set -o pipefail
 
 # => Version-v10.24.1
-version="${CIRCLE_BRANCH/Version-v/}"
+version="${BRANCH/Version-v/}"
 current_commit_msg=$(git show -s --format='%s' HEAD)
 
 if [[ $current_commit_msg =~ Version[[:space:]](v[[:digit:]]+.[[:digit:]]+.[[:digit:]]+[-]beta.[[:digit:]]) ]]
