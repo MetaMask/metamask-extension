004e94c26ea4e5d2e645ae5b74869ef2efb4ad21
ffmcgee <51971598+ffmcgee725@users.noreply.github.com>
2024-12-18 16:45:53 +0100

    e2e testing: wallet_getSession - a dapp can check its existing permissions (#29298)
    Adding e2e test cases for [a dapp can checking its existing
    permissions](https://app.zenhub.com/workspaces/wallet-api-platform-63bee08a4e3b9d001108416e/issues/gh/metamask/metamask-planning/3815)

---
+++ test/e2e/flask/multichain-api/testHelpers.ts
@@ -0,0 +1,74 @@
+import * as path from 'path';
+import { DAPP_URL, largeDelayMs, openDapp, unlockWallet } from '../../helpers';
+import { Driver } from '../../webdriver/driver';
+import { KnownRpcMethods, KnownNotifications } from '@metamask/multichain';
+
+/**
+ * Default options for setting up Multichain E2E test environment
+ */
+export const DEFAULT_MULTICHAIN_TEST_DAPP_FIXTURE_OPTIONS = {
+  dapp: true,
+  dappPaths: [
+    path.join(
+      '..',
+      '..',
+      'node_modules',
+      '@metamask',
+      'test-dapp-multichain',
+      'build',
+    ),
+  ],
+};
+
+/**
+ * Unlocks a wallet and provides extension id for dapp to connect to wallet extension.
+ *
+ * @param {Driver} driver - E2E test driver {@link Driver}, wrapping the Selenium WebDriver.
+ * @param extensionId - Extension identifier for web dapp to interact with wallet extension.
+ */
+export async function openMultichainDappAndConnectWalletWithExternallyConnectable(
+  driver: Driver,
+  extensionId: string,
+): Promise<void> {
+  await unlockWallet(driver);
+  await openDapp(driver, undefined, DAPP_URL);
+
+  await driver.fill('[placeholder="Enter extension ID"]', extensionId);
+  await driver.clickElement({ text: 'Connect', tag: 'button' });
+  await driver.delay(largeDelayMs);
+}
+
+/**
+ * Retrieves permitted session scopes by using test driver to interact with web dapp.
+ *
+ * @param {Driver} driver - E2E test driver {@link Driver}, wrapping the Selenium WebDriver.
+ * @returns result containing sessions scopes.
+ */
+export async function getSessionScopes(
+  driver: Driver,
+): Promise<{ sessionScopes: Record<string, unknown> }> {
+  await driver.clickElement({ text: 'wallet_getSession', tag: 'span' });
+
+  const completeResultSummary = await driver.findElements('.result-summary');
+
+  const getSessionResultSummary = completeResultSummary[0];
+  await getSessionResultSummary.click();
+  const getSessionRawResult = await driver.findElement(
+    '#session-method-result-0',
+  );
+
+  return JSON.parse(await getSessionRawResult.getText());
+}
+
+/**
+ * Retrieves the expected session scope for a given set of addresses.
+ *
+ * @param {string} scope - The session scope.
+ * @param {string[]} accounts - The addresses to get session scope for.
+ * @returns the expected session scope.
+ */
+export const getExpectedSessionScope = (scope: string, accounts: string[]) => ({
+  methods: KnownRpcMethods.eip155,
+  notifications: KnownNotifications.eip155,
+  accounts: accounts.map((acc) => `${scope}:${acc.toLowerCase()}`),
+});
