41b46a04daa8f1fa610c65e1a0666a8b7130d4dd
chloeYue <105063779+chloeYue@users.noreply.github.com>
2024-12-18 16:51:16 +0100

    test: [POM] Migrate portfolio e2e tests and permission requests tests to TS and Page Object Model (#29274)
    ## **Description**
    
    - Migrate portfolio e2e tests and permission requests e2e tests to TS
    and Page Object Model
     
    ```
    test/e2e/tests/portfolio/portfolio-site.spec.ts
    test/e2e/json-rpc/wallet_revokePermissions.spec.ts
    test/e2e/json-rpc/wallet_requestPermissions.spec.ts
    ```
    - Remove unused ganach setup
    
    [![Open in GitHub
    Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/27155?quickstart=1)
    
    ## **Related issues**
    
    
    ## **Manual testing steps**
    Check code readability, make sure tests pass.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [x] I've followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
    Extension Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [x] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [x] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- test/e2e/tests/portfolio/portfolio-site.spec.ts
+++ test/e2e/tests/portfolio/portfolio-site.spec.ts
@@ -1,13 +1,13 @@
-const {
-  withFixtures,
-  unlockWallet,
-  defaultGanacheOptions,
-} = require('../../helpers');
-const FixtureBuilder = require('../../fixture-builder');
-const { emptyHtmlPage } = require('../../mock-e2e');
+import { MockttpServer } from 'mockttp';
+import { withFixtures } from '../../helpers';
+import { EMPTY_E2E_TEST_PAGE_TITLE } from '../../constants';
+import FixtureBuilder from '../../fixture-builder';
+import { emptyHtmlPage } from '../../mock-e2e';
+import HomePage from '../../page-objects/pages/home/homepage';
+import { loginWithBalanceValidation } from '../../page-objects/flows/login.flow';
 
 describe('Portfolio site', function () {
-  async function mockPortfolioSite(mockServer) {
+  async function mockPortfolioSite(mockServer: MockttpServer) {
     return await mockServer
       .forGet('https://portfolio.metamask.io/')
       .withQuery({
@@ -27,18 +27,15 @@ describe('Portfolio site', function () {
       {
         dapp: true,
         fixtures: new FixtureBuilder().build(),
-        ganacheOptions: defaultGanacheOptions,
-        title: this.test.fullTitle(),
+        title: this.test?.fullTitle(),
         testSpecificMock: mockPortfolioSite,
       },
       async ({ driver }) => {
-        await unlockWallet(driver);
+        await loginWithBalanceValidation(driver);
+        await new HomePage(driver).openPortfolioPage();
 
         // Click Portfolio site
-        await driver.clickElement('[data-testid="portfolio-link"]');
-        await driver.waitUntilXWindowHandles(2);
-        const windowHandles = await driver.getAllWindowHandles();
-        await driver.switchToWindowWithTitle('E2E Test Page', windowHandles);
+        await driver.switchToWindowWithTitle(EMPTY_E2E_TEST_PAGE_TITLE);
 
         // Verify site
         await driver.waitForUrl({
