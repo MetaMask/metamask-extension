837f31e6eb3d438a8fb2f62e6d3c9db0c179fe8e
Sébastien Van Eyck <sebastien.vaneyck@consensys.net>
2024-12-20 11:16:23 +0100

    fix(29349): restore ledger support for eth_getEncryptionPublicKey

--- app/scripts/controllers/encryption-public-key.ts
+++ app/scripts/controllers/encryption-public-key.ts
@@ -203,14 +203,6 @@ export default class EncryptionPublicKeyController extends BaseController<
     const keyringType = await this._getAccountKeyringType(address);
 
     switch (keyringType) {
-      case KeyringType.ledger: {
-        return new Promise((_, reject) => {
-          reject(
-            new Error('Ledger does not support eth_getEncryptionPublicKey.'),
-          );
-        });
-      }
-
       case KeyringType.trezor: {
         return new Promise((_, reject) => {
           reject(
