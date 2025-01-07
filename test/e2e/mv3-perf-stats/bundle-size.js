MetaMask/metamask-extension

commit 57d564d6603f00d7d7a568efd742412810846489

chore: Remove broken MV3 perf stats (#29408)
## **Description**

Remove broken MV3 reports. These reports relied upon data from the
`mv3-perf-stats` E2E test job, which itself relied upon the
`user-data-dir` chromedriver setting removed in #24696. They have been
broken since that PR.

These reports were very useful in prioritizing MV3 work at the time, but
we haven't needed them recently.

The `mv3-stats` E2E test suite has also been removed (this is an older
version of `mv3-perf-stats` that has been unused for even longer), along
with the charts that were used for this report.

[![Open in GitHub
Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/29408?quickstart=1)

## **Related issues**

Relates to https://github.com/MetaMask/metamask-extension/issues/28572

## **Manual testing steps**

Check that the `metamaskbot` comment no longer has the links to these
broken reports. They look like this:

- mv3: Background Module Init Stats
- mv3: UI Init Stats
- mv3: Module Load Stats

## **Screenshots/Recordings**

N/A

## **Pre-merge author checklist**

- [x] I've followed [MetaMask Contributor
Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
Extension Coding
Standards](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/CODING_GUIDELINES.md).
- [x] I've completed the PR template to the best of my ability
- [x] I’ve included tests if applicable
- [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
if applicable
- [x] I’ve applied the right labels on the PR (see [labeling
guidelines](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
Not required for external contributors.

## **Pre-merge reviewer checklist**

- [ ] I've manually tested the PR (e.g. pull and build branch, run the
app, test code being changed).
- [ ] I confirm that this PR addresses all acceptance criteria described

.circleci/config.yml
@@ -278,9 +278,6 @@ workflows:
       - user-actions-benchmark:
           requires:
             - prep-build-test
-      - stats-module-load-init:
-          requires:
-            - prep-build-test
       - job-publish-prerelease:
           requires:
             - prep-deps
@@ -294,7 +291,6 @@ workflows:
             - prep-build-ts-migration-dashboard
             - benchmark
             - user-actions-benchmark
-            - stats-module-load-init
             - all-tests-pass
       - job-publish-release:
           filters:
@@ -1263,44 +1259,6 @@ jobs:
       - run:
           name: Run page load benchmark
           command: yarn user-actions-benchmark:chrome --out test-artifacts/chrome/benchmark/user_actions.json --retries 2
-      - store_artifacts:
-          path: test-artifacts
-          destination: test-artifacts
-      - persist_to_workspace:
-          root: .
-          paths:
-            - test-artifacts
-
-  stats-module-load-init:
-    executor: node-browsers-small
-    steps:
-      - run: *shallow-git-clone-and-enable-vnc
-      - run: sudo corepack enable
-      - attach_workspace:
-          at: .
-      - run:
-          name: Move test build to dist
-          command: mv ./dist-test ./dist
-      - run:
-          name: Move test zips to builds
-          command: mv ./builds-test ./builds
-      - run:
-          name: Run page load benchmark
-          command: |
-            mkdir -p test-artifacts/chrome/
-            cp -R development/charts/flamegraph test-artifacts/chrome/initialisation
-            cp -R development/charts/flamegraph/chart test-artifacts/chrome/initialisation/background
-            cp -R development/charts/flamegraph/chart test-artifacts/chrome/initialisation/ui
-            cp -R development/charts/table test-artifacts/chrome/load_time
-      - run:
-          name: Run page load benchmark
-          command: yarn mv3:stats:chrome --out test-artifacts/chrome
-      - run:
-          name: Install jq
-          command: sudo apt install jq -y
-      - run:
-          name: Record bundle size at commit
-          command: ./.circleci/scripts/bundle-stats-commit.sh
       - store_artifacts:
           path: test-artifacts
           destination: test-artifacts

test/e2e/mv3-perf-stats/bundle-size.js
@@ -1,140 +1,0 @@
-#!/usr/bin/env node
-
-/* eslint-disable node/shebang */
-const path = require('path');
-const { promises: fs } = require('fs');
-const yargs = require('yargs/yargs');
-const { hideBin } = require('yargs/helpers');
-const {
-  isWritable,
-  getFirstParentDirectoryThatExists,
-} = require('../../helpers/file');
-
-const { exitWithError } = require('../../../development/lib/exit-with-error');
-
-/**
- * The e2e test case is used to capture bundle time statistics for extension.
- */
-
-const backgroundFiles = [
-  'scripts/runtime-lavamoat.js',
-  'scripts/lockdown-more.js',
-  'scripts/sentry-install.js',
-  'scripts/policy-load.js',
-];
-
-const uiFiles = [
-  'scripts/sentry-install.js',
-  'scripts/runtime-lavamoat.js',
-  'scripts/lockdown-more.js',
-  'scripts/policy-load.js',
-];
-
-const BackgroundFileRegex = /background-[0-9]*.js/u;
-const CommonFileRegex = /common-[0-9]*.js/u;
-const UIFileRegex = /ui-[0-9]*.js/u;
-
-async function main() {
-  const { argv } = yargs(hideBin(process.argv)).usage(
-    '$0 [options]',
-    'Run a page load benchmark',
-    (_yargs) =>
-      _yargs.option('out', {
-        description:
-          'Output filename. Output printed to STDOUT of this is omitted.',
-        type: 'string',
-        normalize: true,
-      }),
-  );
-  const { out } = argv;
-
-  const distFolder = 'dist/chrome';
-  const backgroundFileList = [];
-  const uiFileList = [];
-  const commonFileList = [];
-
-  const files = await fs.readdir(distFolder);
-  for (let i = 0; i < files.length; i++) {
-    const file = files[i];
-    if (CommonFileRegex.test(file)) {
-      const stats = await fs.stat(`${distFolder}/${file}`);
-      commonFileList.push({ name: file, size: stats.size });
-    } else if (
-      backgroundFiles.includes(file) ||
-      BackgroundFileRegex.test(file)
-    ) {
-      const stats = await fs.stat(`${distFolder}/${file}`);
-      backgroundFileList.push({ name: file, size: stats.size });
-    } else if (uiFiles.includes(file) || UIFileRegex.test(file)) {
-      const stats = await fs.stat(`${distFolder}/${file}`);
-      uiFileList.push({ name: file, size: stats.size });
-    }
-  }
-
-  const backgroundBundleSize = backgroundFileList.reduce(
-    (result, file) => result + file.size,
-    0,
-  );
-
-  const uiBundleSize = uiFileList.reduce(
-    (result, file) => result + file.size,
-    0,
-  );
-
-  const commonBundleSize = commonFileList.reduce(
-    (result, file) => result + file.size,
-    0,
-  );
-
-  const result = {
-    background: {
-      name: 'background',
-      size: backgroundBundleSize,
-      fileList: backgroundFileList,
-    },
-    ui: {
-      name: 'ui',
-      size: uiBundleSize,
-      fileList: uiFileList,
-    },
-    common: {
-      name: 'common',
-      size: commonBundleSize,
-      fileList: commonFileList,
-    },
-  };
-
-  if (out) {
-    const outPath = `${out}/bundle_size.json`;
-    const outputDirectory = path.dirname(outPath);
-    const existingParentDirectory = await getFirstParentDirectoryThatExists(
-      outputDirectory,
-    );
-    if (!(await isWritable(existingParentDirectory))) {
-      throw new Error('Specified output file directory is not writable');
-    }
-    if (outputDirectory !== existingParentDirectory) {
-      await fs.mkdir(outputDirectory, { recursive: true });
-    }
-    await fs.writeFile(outPath, JSON.stringify(result, null, 2));
-    await fs.writeFile(
-      `${out}/bundle_size_stats.json`,
-      JSON.stringify(
-        {
-          background: backgroundBundleSize,
-          ui: uiBundleSize,
-          common: commonBundleSize,
-          timestamp: new Date().getTime(),
-        },
-        null,
-        2,
-      ),
-    );
-  } else {
-    console.log(JSON.stringify(result, null, 2));
-  }
-}
-
-main().catch((error) => {
-  exitWithError(error);
-});

.prettierignore
@@ -6,7 +6,6 @@ node_modules/**/*
 /app/vendor/**
 /builds/**/*
 /coverage/**/*
-/development/charts/**
 /development/chromereload.js
 /development/ts-migration-dashboard/filesToConvert.json
 /development/ts-migration-dashboard/build/**

development/charts/flamegraph/chart/index.html
@@ -1,167 +1,0 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-    <link
-      rel="stylesheet"
-      href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-    />
-    <link rel="stylesheet" href="../lib/d3-flamegraph.css" />
-
-    <style>
-      /* Space out content a bit */
-      body {
-        padding-top: 20px;
-        padding-bottom: 20px;
-      }
-
-      /* Custom page header */
-      .header {
-        padding-bottom: 20px;
-        padding-right: 15px;
-        padding-left: 15px;
-        border-bottom: 1px solid #e5e5e5;
-      }
-
-      /* Make the masthead heading the same height as the navigation */
-      .header h3 {
-        margin-top: 0;
-        margin-bottom: 0;
-        line-height: 40px;
-      }
-
-      /* Customize container */
-      .container {
-        max-width: 990px;
-      }
-    </style>
-
-    <title>Performance Measurements</title>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-    <div class="container">
-      <div class="header clearfix">
-        <nav>
-          <div class="pull-right">
-            <form class="form-inline" id="form">
-              <a class="btn" href="javascript: resetZoom();">Reset zoom</a>
-              <a class="btn" href="javascript: clear();">Clear</a>
-              <div class="form-group">
-                <input type="text" class="form-control" id="term" />
-              </div>
-              <a class="btn btn-primary" href="javascript: search();">Search</a>
-            </form>
-          </div>
-        </nav>
-        <h3 class="text-muted">d3-flame-graph</h3>
-      </div>
-      <div id="chart"></div>
-      <hr />
-      <div id="details"></div>
-    </div>
-
-    <!-- D3.js -->
-    <script src="https://d3js.org/d3.v7.js"></script>
-
-    <!-- d3-flamegraph -->
-    <script src="../lib/d3-flamegraph.js"></script>
-    <script src="../lib/d3-flamegraph-tooltip.js"></script>
-
-    <script>
-      var chart = flamegraph()
-        .width(960)
-        .cellHeight(18)
-        .transitionDuration(750)
-        .minFrameSize(5)
-        .transitionEase(d3.easeCubic)
-        .sort(true)
-        //Example to sort in reverse order
-        //.sort(function(a,b){ return d3.descending(a.name, b.name);})
-        .title('')
-        .onClick(onClick)
-        .selfValue(false)
-        .setColorMapper((d, originalColor) =>
-          d.highlight ? '#6aff8f' : originalColor,
-        );
-
-      // Example on how to use custom a tooltip.
-      var tip = flamegraph.tooltip
-        .defaultFlamegraphTooltip()
-        .text((d) => 'name: ' + d.data.name + ', value: ' + d.data.value);
-      chart.tooltip(tip);
-
-      var details = document.getElementById('details');
-      chart.setDetailsElement(details);
-
-      // Example on how to use searchById() function in flamegraph.
-      // To invoke this function after loading the graph itself, this function should be registered in d3 datum(data).call()
-      // (See d3.json invocation in this file)
-      function invokeFind() {
-        var searchId = parseInt(location.hash.substring(1), 10);
-        if (searchId) {
-          find(searchId);
-        }
-      }
-
-      // Example on how to use custom labels
-      // var label = function(d) {
-      //  return "name: " + d.name + ", value: " + d.value;
-      // }
-      // chart.label(label);
-
-      // Example of how to set fixed chart height
-      // chart.height(540);
-
-      d3.json('stacks.json')
-        .then((data) => {
-          d3.select('#chart').datum(data).call(chart).call(invokeFind);
-        })
-        .catch((error) => {
-          return console.warn(error);
-        });
-
-      document
-        .getElementById('form')
-        .addEventListener('submit', function (event) {
-          event.preventDefault();
-          search();
-        });
-
-      function search() {
-        var term = document.getElementById('term').value;
-        chart.search(term);
-      }
-
-      function find(id) {
-        var elem = chart.findById(id);
-        if (elem) {
-          console.log(elem);
-          chart.zoomTo(elem);
-        }
-      }
-
-      function clear() {
-        document.getElementById('term').value = '';
-        chart.clear();
-      }
-
-      function resetZoom() {
-        chart.resetZoom();
-      }
-
-      function onClick(d) {
-        console.info(`Clicked on ${d.data.name}, id: "${d.id}"`);
-        history.pushState({ id: d.id }, d.data.name, `#${d.id}`);
-      }
-    </script>
-  </body>
-</html>

development/charts/flamegraph/lib/d3-flamegraph.css
@@ -1,46 +1,0 @@
-.d3-flame-graph rect {
-  stroke: #EEEEEE;
-  fill-opacity: .8;
-}
-
-.d3-flame-graph rect:hover {
-  stroke: #474747;
-  stroke-width: 0.5;
-  cursor: pointer;
-}
-
-.d3-flame-graph-label {
-  pointer-events: none;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-  overflow: hidden;
-  font-size: 12px;
-  font-family: Verdana;
-  margin-left: 4px;
-  margin-right: 4px;
-  line-height: 1.5;
-  padding: 0 0 0;
-  font-weight: 400;
-  color: black;
-  text-align: left;
-}
-
-.d3-flame-graph .fade {
-  opacity: 0.6 !important;
-}
-
-.d3-flame-graph .title {
-  font-size: 20px;
-  font-family: Verdana;
-}
-
-.d3-flame-graph-tip {
-    background-color: black;
-    border: none;
-    border-radius: 3px;
-    padding: 5px 10px 5px 10px;
-    min-width: 250px;
-    text-align: left;
-    color: white;
-    z-index: 10;
-}

development/charts/table/index.html
@@ -1,67 +1,0 @@
-<html>
-  <head>
-    <script src="./jquery.min.js"></script>
-    <style>
-      table {
-        border: 1px solid black;
-        border-collapse: collapse;
-      }
-      tr {
-        border: 1px solid black;
-      }
-    </style>
-    <script>
-      $(function () {
-        var loadTime = [];
-
-        $.getJSON('stats.json', function (data) {
-          $.each(data.children, function (index, datum) {
-            if (datum.name) {
-              var tblRow =
-                '<tr>' +
-                '<td>' +
-                index +
-                '</td>' +
-                '<td>' +
-                datum.name +
-                '</td>' +
-                '<td>' +
-                datum.value +
-                '</td>' +
-                '</tr>';
-              $(tblRow).appendTo('#loaddata tbody');
-            }
-          });
-          $(
-            '<tr>' +
-              '<td>' +
-              'Total' +
-              '</td>' +
-              '<td>' +
-              '--' +
-              '</td>' +
-              '<td>' +
-              data.value +
-              '</td>' +
-              '</tr>',
-          ).appendTo('#loaddata tbody');
-        });
-      });
-    </script>
-  </head>
-
-  <body>
-    <div class="wrapper">
-      <div class="profile">
-        <table id="loaddata">
-          <thead>
-            <th>S.No</th>
-            <th>Name</th>
-            <th>TotalTime</th>
-          </thead>
-          <tbody></tbody>
-        </table>
-      </div>
-    </div>
-  </body>
-</html>

commit 8fa79fbbbe1d1b826d40ccff9a784f962ea3e3fe

fix; Fix bundle size tracking
The bundle size tracking was accidentally broken in #29408. This
restores the bundle size tracking.

.circleci/config.yml
@@ -277,6 +277,9 @@ workflows:
       - user-actions-benchmark:
           requires:
             - prep-build-test
+      - bundle-size:
+          requires:
+            - prep-build-test
       - job-publish-prerelease:
           requires:
             - prep-deps
@@ -294,6 +297,7 @@ workflows:
             - prep-build-ts-migration-dashboard
             - benchmark
             - user-actions-benchmark
+            - bundle-size
             - all-tests-pass
       - job-publish-release:
           filters:
@@ -1262,6 +1266,36 @@ jobs:
       - run:
           name: Run page load benchmark
           command: yarn user-actions-benchmark:chrome --out test-artifacts/chrome/benchmark/user_actions.json --retries 2
+      - store_artifacts:
+          path: test-artifacts
+          destination: test-artifacts
+      - persist_to_workspace:
+          root: .
+          paths:
+            - test-artifacts
+
+  bundle-size:
+    executor: node-browsers-small
+    steps:
+      - run: *shallow-git-clone-and-enable-vnc
+      - run: sudo corepack enable
+      - attach_workspace:
+          at: .
+      - run:
+          name: Move test build to dist
+          command: mv ./dist-test ./dist
+      - run:
+          name: Move test zips to builds
+          command: mv ./builds-test ./builds
+      - run:
+          name: Measure bundle size
+          command: yarn bundle-size --out test-artifacts/chrome
+      - run:
+          name: Install jq
+          command: sudo apt install jq -y
+      - run:
+          name: Record bundle size at commit
+          command: ./.circleci/scripts/bundle-stats-commit.sh
       - store_artifacts:
           path: test-artifacts
           destination: test-artifacts

test/e2e/mv3-perf-stats/bundle-size.js
@@ -1,0 +1,140 @@
+#!/usr/bin/env node
+
+/* eslint-disable node/shebang */
+const path = require('path');
+const { promises: fs } = require('fs');
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const {
+  isWritable,
+  getFirstParentDirectoryThatExists,
+} = require('../../helpers/file');
+
+const { exitWithError } = require('../../../development/lib/exit-with-error');
+
+/**
+ * The e2e test case is used to capture bundle time statistics for extension.
+ */
+
+const backgroundFiles = [
+  'scripts/runtime-lavamoat.js',
+  'scripts/lockdown-more.js',
+  'scripts/sentry-install.js',
+  'scripts/policy-load.js',
+];
+
+const uiFiles = [
+  'scripts/sentry-install.js',
+  'scripts/runtime-lavamoat.js',
+  'scripts/lockdown-more.js',
+  'scripts/policy-load.js',
+];
+
+const BackgroundFileRegex = /background-[0-9]*.js/u;
+const CommonFileRegex = /common-[0-9]*.js/u;
+const UIFileRegex = /ui-[0-9]*.js/u;
+
+async function main() {
+  const { argv } = yargs(hideBin(process.argv)).usage(
+    '$0 [options]',
+    'Run a page load benchmark',
+    (_yargs) =>
+      _yargs.option('out', {
+        description:
+          'Output filename. Output printed to STDOUT of this is omitted.',
+        type: 'string',
+        normalize: true,
+      }),
+  );
+  const { out } = argv;
+
+  const distFolder = 'dist/chrome';
+  const backgroundFileList = [];
+  const uiFileList = [];
+  const commonFileList = [];
+
+  const files = await fs.readdir(distFolder);
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+    if (CommonFileRegex.test(file)) {
+      const stats = await fs.stat(`${distFolder}/${file}`);
+      commonFileList.push({ name: file, size: stats.size });
+    } else if (
+      backgroundFiles.includes(file) ||
+      BackgroundFileRegex.test(file)
+    ) {
+      const stats = await fs.stat(`${distFolder}/${file}`);
+      backgroundFileList.push({ name: file, size: stats.size });
+    } else if (uiFiles.includes(file) || UIFileRegex.test(file)) {
+      const stats = await fs.stat(`${distFolder}/${file}`);
+      uiFileList.push({ name: file, size: stats.size });
+    }
+  }
+
+  const backgroundBundleSize = backgroundFileList.reduce(
+    (result, file) => result + file.size,
+    0,
+  );
+
+  const uiBundleSize = uiFileList.reduce(
+    (result, file) => result + file.size,
+    0,
+  );
+
+  const commonBundleSize = commonFileList.reduce(
+    (result, file) => result + file.size,
+    0,
+  );
+
+  const result = {
+    background: {
+      name: 'background',
+      size: backgroundBundleSize,
+      fileList: backgroundFileList,
+    },
+    ui: {
+      name: 'ui',
+      size: uiBundleSize,
+      fileList: uiFileList,
+    },
+    common: {
+      name: 'common',
+      size: commonBundleSize,
+      fileList: commonFileList,
+    },
+  };
+
+  if (out) {
+    const outPath = `${out}/bundle_size.json`;
+    const outputDirectory = path.dirname(outPath);
+    const existingParentDirectory = await getFirstParentDirectoryThatExists(
+      outputDirectory,
+    );
+    if (!(await isWritable(existingParentDirectory))) {
+      throw new Error('Specified output file directory is not writable');
+    }
+    if (outputDirectory !== existingParentDirectory) {
+      await fs.mkdir(outputDirectory, { recursive: true });
+    }
+    await fs.writeFile(outPath, JSON.stringify(result, null, 2));
+    await fs.writeFile(
+      `${out}/bundle_size_stats.json`,
+      JSON.stringify(
+        {
+          background: backgroundBundleSize,
+          ui: uiBundleSize,
+          common: commonBundleSize,
+          timestamp: new Date().getTime(),
+        },
+        null,
+        2,
+      ),
+    );
+  } else {
+    console.log(JSON.stringify(result, null, 2));
+  }
+}
+
+main().catch((error) => {
+  exitWithError(error);
+});