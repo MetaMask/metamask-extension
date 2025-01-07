8eb79e92dacaf09051b7b1451f426e831659d6a5
Shane Terence Odlum <shane.odlum@consensys.net>
2025-01-07 17:59:05 +0000

    chore: update institutional snap

--- shared/lib/accounts/institutional-wallet-snap.ts
+++ shared/lib/accounts/institutional-wallet-snap.ts
@@ -1,7 +1,7 @@
 import { SnapId } from '@metamask/snaps-sdk';
 // This dependency is still installed as part of the `package.json`, however
 // the Snap is being pre-installed only for Flask build (for the moment).
-import InstitutionalWalletSnap from '../../../app/scripts/snaps/preinstalled-snap.json';
+import InstitutionalWalletSnap from '@metamask/institutional-wallet-snap/dist/preinstalled-snap.json';
 
 export const INSTITUTIONAL_WALLET_SNAP_ID: SnapId =
   InstitutionalWalletSnap.snapId as SnapId;
