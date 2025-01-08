74aba6a73f906d5df8cb480c4b6c7f3bcaf40298
Charly Chevalier <charly.chevalier@consensys.net>
2025-01-08 18:17:59 +0100

    Revert "chore: add local bitcoin snap (hack, needs to be removed)"
    This reverts commit 370f06e2385007ffddf7109a8f4a10d82c70a9fd.

--- shared/lib/accounts/bitcoin-wallet-snap.ts
+++ shared/lib/accounts/bitcoin-wallet-snap.ts
@@ -1,8 +1,7 @@
 import { SnapId } from '@metamask/snaps-sdk';
 // This dependency is still installed as part of the `package.json`, however
 // the Snap is being pre-installed only for Flask build (for the moment).
-// eslint-disable-next-line import/no-restricted-paths
-import BitcoinWalletSnap from '../../../app/scripts/snaps/snap-bitcoin-wallet-preinstalled-snap.json';
+import BitcoinWalletSnap from '@metamask/bitcoin-wallet-snap/dist/preinstalled-snap.json';
 
 export const BITCOIN_WALLET_SNAP_ID: SnapId =
   BitcoinWalletSnap.snapId as SnapId;
