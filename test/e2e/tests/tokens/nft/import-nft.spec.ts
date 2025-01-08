70385d59002975058387af5196335e900cac9cab
Nicholas Gambino <nicholas.gambino@consensys.net>
2025-01-08 10:50:16 -0800

    fix: Collection name no longer present for e2e test to query

--- test/e2e/tests/tokens/nft/import-nft.spec.ts
+++ test/e2e/tests/tokens/nft/import-nft.spec.ts
@@ -31,7 +31,6 @@ describe('Import NFT', function () {
         const nftList = new NftListPage(driver);
         await nftList.importNft(contractAddress, '1');
         await nftList.check_successImportNftMessageIsDisplayed();
-        await nftList.check_nftNameIsDisplayed('TestDappNFTs');
         await nftList.check_nftImageIsDisplayed();
       },
     );
@@ -59,7 +58,6 @@ describe('Import NFT', function () {
         const nftList = new NftListPage(driver);
         await nftList.importNft(contractAddress, '1');
         await nftList.check_successImportNftMessageIsDisplayed();
-        await nftList.check_nftNameIsDisplayed('TestDappNFTs');
         await nftList.check_nftImageIsDisplayed();
 
         // Create new account with default name Account 2
@@ -78,7 +76,6 @@ describe('Import NFT', function () {
         await accountListPage.switchToAccount('Account 1');
         await headerNavbar.check_accountLabel('Account 1');
         await homepage.check_localBlockchainBalanceIsDisplayed(ganacheServer);
-        await nftList.check_nftNameIsDisplayed('TestDappNFTs');
         await nftList.check_nftImageIsDisplayed();
       },
     );
