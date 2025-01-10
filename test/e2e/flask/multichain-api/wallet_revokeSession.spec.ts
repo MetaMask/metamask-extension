28712d741395407180f1fb3c27ae8eb8dcad8ceb
ffmcgee <joao.carlos@consensys.net>
2025-01-10 22:05:23 +0100

    refactor: code review improvements

--- test/e2e/flask/multichain-api/wallet_revokeSession.spec.ts
+++ test/e2e/flask/multichain-api/wallet_revokeSession.spec.ts
@@ -105,7 +105,9 @@ describe('Initializing a session w/ several scopes and accounts, then calling `w
         });
 
         for (const scope of GANACHE_SCOPES) {
-          const invokeMethodRequest = JSON.stringify({
+          const id = 1999133338649204;
+          const data = JSON.stringify({
+            id,
             jsonrpc: '2.0',
             method: 'wallet_invokeMethod',
             params: {
@@ -117,23 +119,51 @@ describe('Initializing a session w/ several scopes and accounts, then calling `w
             },
           });
 
+          const script = `
+            const port = chrome.runtime.connect('${extensionId}');
+            const data = ${data};
+            const result = new Promise((resolve) => {
+              port.onMessage.addListener((msg) => {
+                if (msg.type !== 'caip-x') {
+                  return;
+                }
+                if (msg.data?.id !== ${id}) {
+                  return;
+                }
+
+                if (msg.data.id || msg.data.error) {
+                  resolve(msg)
+                }
+              })
+            })
+            port.postMessage({ type: 'caip-x', data });
+            return result;`;
+
           /**
            * We call `executeScript` to attempt JSON rpc call directly through the injected provider object since when session is revoked,
            * webapp does not provide UI to make call.
            */
-          const result = await driver
-            .executeScript(
-              `return window.ethereum.request(${invokeMethodRequest})`,
-            )
-            .catch((err) => {
-              return err;
-            });
 
-          assert.strictEqual(
-            result instanceof Error,
-            true,
-            `calling wallet_invokeMethod should throw an error for scope ${scope}`,
-          );
+          const expectedError = {
+            code: 4100,
+            message:
+              'The requested account and/or method has not been authorized by the user.',
+          };
+
+          const actualError = await driver
+            .executeScript(script)
+            .then((res) => res.data?.error);
+
+          /**
+           * We make sure it's the expected error by comparing expected error code and message (we ignore stack propery)
+           */
+          Object.entries(expectedError).forEach(([key, value]) => {
+            assert.deepEqual(
+              value,
+              actualError[key],
+              `calling wallet_invokeMethod should throw an error for scope ${scope}`,
+            );
+          });
         }
       },
     );
