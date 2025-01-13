fad6e695ed10635a54da763dcd290a3a61083458
cmd-ob <ola.bale@consensys.net>
2025-01-13 07:50:52 +0000

    feat: add data-testid attributes for improved testing in modals and buttons

--- ui/components/multichain/import-tokens-modal/import-tokens-modal.js
+++ ui/components/multichain/import-tokens-modal/import-tokens-modal.js
@@ -633,6 +633,7 @@ export const ImportTokensModal = ({ onClose }) => {
                               ? Severity.Warning
                               : Severity.Info
                           }
+                          data-testid="custom-token-warning"
                         >
                           <Text variant={TextVariant.bodyMd}>
                             {t(
