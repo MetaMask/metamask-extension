cabe01bd99ff7292f78a50a2d77b01101f5e81c4
cmd-ob <ola.bale@consensys.net>
2025-01-13 07:51:14 +0000

    feat: enhance token transfer confirmation with new gas fee editing and loading checks

--- test/e2e/page-objects/pages/confirmations/redesign/token-transfer-confirmation.ts
+++ test/e2e/page-objects/pages/confirmations/redesign/token-transfer-confirmation.ts
@@ -1,37 +1,70 @@
 import { tEn } from '../../../../../lib/i18n-helpers';
 import { Driver } from '../../../../webdriver/driver';
-import { RawLocator } from '../../../common';
 import TransactionConfirmation from './transaction-confirmation';
 
 class TokenTransferTransactionConfirmation extends TransactionConfirmation {
-  private networkParagraph: RawLocator;
+  private readonly confirmButton = '[data-testid="confirm-footer-button"]';
 
-  private interactingWithParagraph: RawLocator;
+  private readonly editGasFeeButton = '[data-testid="edit-gas-fee-icon"]';
 
-  private networkFeeParagraph: RawLocator;
+  private readonly gasInputs = 'input[type="number"]';
+
+  private readonly interactingWithParagraph = {
+    css: 'p',
+    text: tEn('interactingWith') as string,
+  };
+
+  private readonly networkFee = '[data-testid="first-gas-field"]';
+
+  private readonly networkFeeParagraph = {
+    css: 'p',
+    text: tEn('networkFee') as string,
+  };
+
+  private readonly networkParagraph = {
+    css: 'p',
+    text: tEn('transactionFlowNetwork') as string,
+  };
+
+  private readonly saveButton = { text: 'Save', tag: 'button' };
 
   constructor(driver: Driver) {
     super(driver);
-
     this.driver = driver;
-
-    this.networkParagraph = {
-      css: 'p',
-      text: tEn('transactionFlowNetwork') as string,
-    };
-    this.interactingWithParagraph = {
-      css: 'p',
-      text: tEn('interactingWith') as string,
-    };
-    this.networkFeeParagraph = {
-      css: 'p',
-      text: tEn('networkFee') as string,
-    };
   }
 
-  async check_networkParagraph() {
-    await this.driver.waitForSelector(this.networkParagraph);
+  // Action Methods
+
+  async confirmTx(): Promise<void> {
+    console.log('Click confirm button to confirm transaction');
+    await this.driver.clickElement(this.confirmButton);
   }
+
+  /**
+   * Edits the gas fee by setting custom gas limit and price values
+   *
+   * @param gasLimit - The gas limit value to set
+   * @param gasPrice - The gas price value to set
+   */
+  async editGasFee(gasLimit: string, gasPrice: string): Promise<void> {
+    console.log('Editing gas fee values');
+
+    await this.driver.clickElement(this.editGasFeeButton);
+
+    const inputs = await this.driver.findElements(this.gasInputs);
+    const [gasLimitInput, gasPriceInput] = inputs;
+
+    await gasLimitInput.clear();
+    await gasLimitInput.sendKeys(gasLimit);
+    await gasPriceInput.clear();
+    await gasPriceInput.sendKeys(gasPrice);
+
+    await this.driver.clickElement(this.saveButton);
+
+    console.log('Gas fee values updated successfully');
+  }
+
+  // Check Methods
 
   async check_interactingWithParagraph() {
     await this.driver.waitForSelector(this.interactingWithParagraph);
@@ -39,6 +72,49 @@ class TokenTransferTransactionConfirmation extends TransactionConfirmation {
 
   async check_networkFeeParagraph() {
     await this.driver.waitForSelector(this.networkFeeParagraph);
+  }
+
+  async check_networkParagraph() {
+    await this.driver.waitForSelector(this.networkParagraph);
+  }
+
+  /**
+   * Verifies that the confirm token transfer (redesigned) screen is fully loaded by checking for the presence of the expected symbol, token/gas values and buttons.
+   *
+   * @param transferAmount - The amount of tokens to be transferred.
+   * @param symbol - The symbol of the token to be transferred.
+   * @param expectedNetworkFee - The expected gas/network fee value to be displayed on the page.
+   * @returns A promise that resolves when all specified elements are verified to be present and contain the expected values, indicating the page has fully loaded.
+   * @example
+   * await tokenTransferTransactionConfirmation.check_pageIsLoaded('10', 'ETH', '0.01');
+   */
+  async check_pageIsLoaded(
+    transferAmount: string,
+    symbol: string,
+    expectedNetworkFee: string,
+  ): Promise<void> {
+    try {
+      await Promise.all([
+        this.driver.waitForSelector(this.confirmButton),
+        this.driver.waitForSelector({
+          text: `${transferAmount} ${symbol}`,
+          tag: 'h2',
+        }),
+        this.driver.waitForSelector({
+          css: this.networkFee,
+          text: `${expectedNetworkFee} ETH`,
+        }),
+      ]);
+      console.log(
+        'Confirm token transfer (Redesigned) screen is loaded with expected values',
+      );
+    } catch (e) {
+      console.error(
+        `Timeout while waiting for confirm token transfer (redesigned) screen to be loaded, expected network fee is: ${expectedNetworkFee}, transfer amount is: ${transferAmount} and symbol is: ${symbol}`,
+        e,
+      );
+      throw e;
+    }
   }
 }
 
