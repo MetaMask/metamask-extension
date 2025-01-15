900c51ee1828b85a5661da1f6b58ca9f488050cf
Frederik Bolding <frederik.bolding@gmail.com>
2025-01-15 10:46:55 +0100

    Bump test-snaps and add testing for Ethereum provider usage

--- test/e2e/snaps/enums.js
+++ test/e2e/snaps/enums.js
@@ -1,3 +1,3 @@
 module.exports = {
-  TEST_SNAPS_WEBSITE_URL: 'https://metamask.github.io/snaps/test-snaps/2.17.0',
+  TEST_SNAPS_WEBSITE_URL: 'https://metamask.github.io/snaps/test-snaps/2.18.0',
 };
