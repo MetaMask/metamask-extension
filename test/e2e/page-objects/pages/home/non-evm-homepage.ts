9412e699523c044d1a472c27f863de57b9a61fa6
Javier <javier.vera@consensys.net>
2025-01-15 12:55:40 +0100

    feat: wait for loader to avoid flakiness

--- test/e2e/page-objects/pages/home/non-evm-homepage.ts
+++ test/e2e/page-objects/pages/home/non-evm-homepage.ts
@@ -13,6 +13,7 @@ class NonEvmHomepage extends HomePage {
    * Clicks the send button on the non-EVM account homepage.
    */
   async clickOnSendButton(): Promise<void> {
+    await this.driver.waitForControllersLoaded();
     await this.driver.clickElement(this.sendButton);
   }
 
