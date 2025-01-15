fa94554d2af70d3e350c786fb0c3f05e89dfbb06
Chloe Gao <chloe.gao@consensys.net>
2025-01-15 16:08:37 +0100

    migrate test

--- test/e2e/tests/content-security-policy/content-security-policy.spec.ts
+++ test/e2e/tests/content-security-policy/content-security-policy.spec.ts
@@ -1,12 +1,9 @@
 import { strict as assert } from 'assert';
 import { Suite } from 'mocha';
-import {
-  defaultGanacheOptions,
-  openDapp,
-  unlockWallet,
-  withFixtures,
-} from '../../helpers';
+import { defaultGanacheOptions, withFixtures } from '../../helpers';
 import FixtureBuilder from '../../fixture-builder';
+import TestDapp from '../../page-objects/pages/test-dapp';
+import { loginWithBalanceValidation } from '../../page-objects/flows/login.flow';
 
 describe('Content-Security-Policy', function (this: Suite) {
   it('opening a restricted website should still load the extension', async function () {
@@ -33,9 +30,10 @@ describe('Content-Security-Policy', function (this: Suite) {
         ganacheOptions: defaultGanacheOptions,
         title: this.test?.fullTitle(),
       },
-      async ({ driver }) => {
-        await unlockWallet(driver);
-        await openDapp(driver);
+      async ({ driver, ganacheServer }) => {
+        await loginWithBalanceValidation(driver, ganacheServer);
+        const testDapp = new TestDapp(driver);
+        await testDapp.openTestDappPage();
         const isExtensionLoaded: boolean = await driver.executeScript(
           'return typeof window.ethereum !== "undefined"',
         );
