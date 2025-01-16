0a24d4e90eba4e43f9bbd0c5156fbcf29bfd93d0
Matteo Scurati <matteo.scurati@gmail.com>
2025-01-16 09:42:22 +0100

    fix: :bug: update alt text on NFT images

--- ui/components/multichain/nft-item/nft-item.stories.js
+++ ui/components/multichain/nft-item/nft-item.stories.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { getNftImageAlt } from '../../../helpers/utils/nfts';
 import { NftItem } from '.';
 
 export default {
@@ -27,7 +28,12 @@ export default {
     },
   },
   args: {
-    alt: 'Join Archer and his 6,969 frens as they take a trip further down the rabbit hole in search of a world with vibrant art, great vibes, and psychedelic tales.',
+    alt: getNftImageAlt({
+      name: 'Monkey Trip #2422',
+      tokenId: '2422',
+      description:
+        'Join Archer and his 6,969 frens as they take a trip further down the rabbit hole in search of a world with vibrant art, great vibes, and psychedelic tales.',
+    }),
     name: 'Monkey Trip #2422',
     src: 'https://i.seadn.io/gcs/files/878e670c38e0f02e58bf730c51c30d0c.jpg',
     networkName: 'Ethereum Mainnet',
@@ -37,5 +43,11 @@ export default {
 };
 
 export const DefaultStory = (args) => <NftItem {...args} />;
-
 DefaultStory.storyName = 'Default';
+
+export const NoImageStory = (args) => <NftItem {...args} />;
+NoImageStory.storyName = 'No Image';
+NoImageStory.args = {
+  ...DefaultStory.args,
+  src: '',
+};
