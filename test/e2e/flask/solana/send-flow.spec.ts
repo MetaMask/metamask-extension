c22689af27b31791dc8be0c387031646e8c6c237
Bryan Fullam <bryan.fullam@consensys.net>
2025-01-16 21:36:33 +0100

    test: update e2e tests

--- test/e2e/flask/solana/send-flow.spec.ts
+++ test/e2e/flask/solana/send-flow.spec.ts
@@ -83,13 +83,13 @@ describe('Send full flow of USD', function (this: Suite) {
         );
         assert.equal(
           await homePage.check_ifSwapButtonIsClickable(),
-          false,
-          'Swap button is enabled and it shouldn`t',
+          true,
+          'Swap button is not enabled and it should',
         );
         assert.equal(
           await homePage.check_ifBridgeButtonIsClickable(),
-          false,
-          'Bridge button is enabled  and it should`t',
+          true,
+          'Bridge button is not enabled and it should',
         );
         await homePage.clickOnSendButton();
         const sendSolanaPage = new SendSolanaPage(driver);
@@ -234,13 +234,13 @@ describe('Send full flow of SOL', function (this: Suite) {
         );
         assert.equal(
           await homePage.check_ifSwapButtonIsClickable(),
-          false,
-          'Swap button is enabled and it shouldn`t',
+          true,
+          'Swap button is not enabled and it should',
         );
         assert.equal(
           await homePage.check_ifBridgeButtonIsClickable(),
-          false,
-          'Bridge button is enabled  and it should`t',
+          true,
+          'Bridge button is not enabled and it should',
         );
         await homePage.clickOnSendButton();
         const sendSolanaPage = new SendSolanaPage(driver);
@@ -378,13 +378,13 @@ describe('Send flow flow', function (this: Suite) {
         );
         assert.equal(
           await homePage.check_ifSwapButtonIsClickable(),
-          false,
-          'Swap button is enabled and it should`t',
+          true,
+          'Swap button is not enabled and it should',
         );
         assert.equal(
           await homePage.check_ifBridgeButtonIsClickable(),
-          false,
-          'Bridge button is enabled and it should`t',
+          true,
+          'Bridge button is not enabled and it should',
         );
         await homePage.clickOnSendButton();
         const sendSolanaPage = new SendSolanaPage(driver);
