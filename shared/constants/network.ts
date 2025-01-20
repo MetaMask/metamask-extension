82a9b5d9b8dc56a14bc622c91638929323b4fa64
Med Amine ID <med.amine.idmoussi@gmail.com>
2025-01-20 11:53:37 +0100

    fix: add Mode network logos

--- shared/constants/network.ts
+++ shared/constants/network.ts
@@ -173,6 +173,8 @@ export const CHAIN_IDS = {
   LISK_SEPOLIA: '0x106a',
   INK_SEPOLIA: '0xba5eD',
   INK: '0xdef1',
+  MODE_SEPOLIA: '0x397',
+  MODE: '0x868b',
 } as const;
 
 export const CHAINLIST_CHAIN_IDS_MAP = {
@@ -237,6 +239,9 @@ export const CHAINLIST_CHAIN_IDS_MAP = {
   INK: '0xdef1',
   SONEIUM_MAINNET: '0x74c',
   SONEIUM_TESTNET: '0x79a',
+  MODE_SEPOLIA: '0x397',
+  MODE: '0x868b',
+
 } as const;
 
 // To add a deprecation warning to a network, add it to the array
@@ -291,6 +296,9 @@ export const LISK_SEPOLIA_DISPLAY_NAME = 'Lisk Sepolia';
 export const INK_SEPOLIA_DISPLAY_NAME = 'Ink Sepolia';
 export const INK_DISPLAY_NAME = 'Ink Mainnet';
 export const SONEIUM_DISPLAY_NAME = 'Soneium Mainnet';
+export const MODE_SEPOLIA_DISPLAY_NAME = 'Mode Sepolia';
+export const MODE_DISPLAY_NAME = 'Mode Mainnet';
+
 export const infuraProjectId = process.env.INFURA_PROJECT_ID;
 export const getRpcUrl = ({
   network,
@@ -415,6 +423,7 @@ const CHAINLIST_CURRENCY_SYMBOLS_MAP = {
   LISK: 'ETH',
   SONEIUM_MAINNET: 'ETH',
   SONEIUM_TESTNET: 'ETH',
+  MODE: 'ETH',
 } as const;
 
 export const CHAINLIST_CURRENCY_SYMBOLS_MAP_NETWORK_COLLISION = {
@@ -503,6 +512,8 @@ export const LISK_SEPOLIA_IMAGE_URL = './images/lisk_sepolia.svg';
 export const INK_SEPOLIA_IMAGE_URL = './images/ink-sepolia.svg';
 export const INK_IMAGE_URL = './images/ink.svg';
 export const SONEIUM_IMAGE_URL = './images/soneium.svg';
+export const MODE_SEPOLIA_IMAGE_URL = './images/mode-sepolia.svg';
+export const MODE_IMAGE_URL = './images/mode.svg';
 
 export const INFURA_PROVIDER_TYPES = [
   NETWORK_TYPES.MAINNET,
@@ -615,6 +626,7 @@ export const NETWORK_TO_NAME_MAP = {
   [CHAIN_IDS.METACHAIN_ONE]: METACHAIN_ONE_DISPLAY_NAME,
   [CHAIN_IDS.LISK]: LISK_DISPLAY_NAME,
   [CHAIN_IDS.LISK_SEPOLIA]: LISK_SEPOLIA_DISPLAY_NAME,
+
 } as const;
 
 export const CHAIN_ID_TO_CURRENCY_SYMBOL_MAP = {
@@ -859,6 +871,8 @@ export const CHAIN_ID_TO_NETWORK_IMAGE_URL_MAP = {
   [CHAINLIST_CHAIN_IDS_MAP.INK]: INK_IMAGE_URL,
   [CHAINLIST_CHAIN_IDS_MAP.SONEIUM_MAINNET]: SONEIUM_IMAGE_URL,
   [CHAINLIST_CHAIN_IDS_MAP.SONEIUM_TESTNET]: SONEIUM_IMAGE_URL,
+  [CHAINLIST_CHAIN_IDS_MAP.MODE_SEPOLIA]: MODE_SEPOLIA_IMAGE_URL,
+  [CHAINLIST_CHAIN_IDS_MAP.MODE]: MODE_IMAGE_URL,
 } as const;
 
 export const CHAIN_ID_TO_ETHERS_NETWORK_NAME_MAP = {
@@ -901,6 +915,7 @@ export const CHAIN_ID_TOKEN_IMAGE_MAP = {
     GRAVITY_ALPHA_TESTNET_SEPOLIA_IMAGE_URL,
   [CHAINLIST_CHAIN_IDS_MAP.ZORA_MAINNET]: ETH_TOKEN_IMAGE_URL,
   [CHAIN_IDS.INK]: ETH_TOKEN_IMAGE_URL,
+  [CHAIN_IDS.MODE]: ETH_TOKEN_IMAGE_URL,
 } as const;
 
 export const INFURA_BLOCKED_KEY = 'countryBlocked';
