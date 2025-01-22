0a1b5314a84bc20ec12db7f1048440fdb2db2f58
Monte Lai <monte.lai@consensys.net>
2025-01-22 15:51:25 +0800

    fix: add keyringsMetadata to fixtures

--- test/e2e/default-fixture.js
+++ test/e2e/default-fixture.js
@@ -160,6 +160,12 @@ function defaultFixture(inputChainId = CHAIN_IDS.LOCALHOST) {
       KeyringController: {
         vault:
           '{"data":"WHaP1FrrtV4zUonudIppDifsLHF39g6oPkVksAIdWAHBRzax1uy1asfAJprR7u72t4/HuYz5yPIFQrnNnv+hwQu9GRuty88VKMnvMy+sq8MNtoXI+C54bZpWa8r4iUQfa0Mj/cfJbpFpzOdF1ZYXahTfTcU5WsrHwvJew842CiJR4B2jmCHHXfm/DxLK3WazsVQwXJGx/U71UelGoOOrT8NI28EKrAwgPn+7Xmv0j92gmhau30N7Bo2fr6Zv","iv":"LfD8/tY1EjXzxuemSmDVdA==","keyMetadata":{"algorithm":"PBKDF2","params":{"iterations":600000}},"salt":"nk4xdpmMR+1s5BYe4Vnk++XAQwrISI2bCtbMg7V1wUA="}',
+        keyringsMetadata: [
+          {
+            id: '01JJ5ZKK405Q2PD20DR51KN5QE',
+            name: '',
+          },
+        ],
       },
       MetaMetricsController: {
         eventsBeforeMetricsOptIn: [],
