2e5ea92c9c191d5c9abb9703625307262a76b990
jiexi <jiexiluan@gmail.com>
2025-01-22 14:35:34 -0800

    Update test/e2e/json-rpc/wallet_addEthereumChain.spec.ts
    Co-authored-by: Alex Donesky <adonesky@gmail.com>

--- test/e2e/json-rpc/wallet_addEthereumChain.spec.ts
+++ test/e2e/json-rpc/wallet_addEthereumChain.spec.ts
@@ -167,7 +167,7 @@ describe('Add Ethereum Chain', function () {
 
           assert.deepEqual(beforePermittedChains, []);
 
-          const switchEthereumChainRequest = JSON.stringify({
+          const addEthereumChainRequest = JSON.stringify({
             jsonrpc: '2.0',
             method: 'wallet_addEthereumChain',
             params: [
