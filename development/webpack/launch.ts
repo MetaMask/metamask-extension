1d44e1ac2ba53039fc2c00367cb312c62c7a5307
David Murdoch <187813+davidmurdoch@users.noreply.github.com>
2025-01-23 17:05:35 -0500

    revert

--- development/webpack/launch.ts
+++ development/webpack/launch.ts
@@ -21,7 +21,7 @@ import type { Child, PTY, Stdio, StdName } from './types.ts';
 const rawArgv = process.argv.slice(2);
 
 const alias = { cache: 'c', help: 'h', watch: 'h' };
-type Args = Partial<Record<keyof typeof alias, boolean>>;
+type Args = { [x in keyof typeof alias]?: boolean };
 const args = parser(rawArgv, { alias, boolean: Object.keys(alias) }) as Args;
 
 if (args.cache === false || args.help === true || args.watch === true) {
