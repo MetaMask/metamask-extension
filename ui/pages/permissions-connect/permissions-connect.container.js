ae0bdb4e4f9462394c59135a16462d65b31ac24d
ffmcgee <joao.carlos@consensys.net>
2025-01-26 02:15:17 +0100

    fix: consistently fetch pendingPermissionsFromOrigin to check for pending wallet_switchEthereumChain request

--- ui/pages/permissions-connect/permissions-connect.container.js
+++ ui/pages/permissions-connect/permissions-connect.container.js
@@ -59,18 +59,19 @@ const mapStateToProps = (state, ownProps) => {
   // Some stuff is rendered on `main` that is NOT rendered on this branch. Also, on main, `isRequestingAccounts` evaluates to false,
   // so on `ui/pages/permissions-connect/permissions-connect.component.js`, l151 we go to `history.replace(confirmPermissionPath)`;
   // we should find a solution for specifically triggering `wallet_switchEthereumChain` to also go in here (check `state` here perhaps ?)
-  // Problem 1: We need to differentiate in this component, a legacy `wallet_switchEthereumChain` from any other regular caip25 request []
+  // Problem 1: We need to differentiate in this component, a legacy `wallet_switchEthereumChain` from any other regular caip25 request [?X]
   // Problem 2: `permission.js` file, `PERMISSION_DESCRIPTIONS` object, needs to differentiate legacey wallet_switchEthereumChain from a regular caip25 request [X]
-  // await window.ethereum.request({
-  //     jsonrpc: '2.0',
-  //     method: 'wallet_switchEthereumChain',
-  //     params: [{ chainId: '0x1' }],
-  //   })
 
-  const lastActivity = state.metamask.permissionActivityLog.at(-1); // This does not seem to consistently be correct...
+  const pendingPermissionsFromOrigin =
+    state.metamask.permissionActivityLog.filter(
+      (log) =>
+        log.origin === permissionsRequest?.metadata?.origin &&
+        log.responseTime === null,
+    );
+
+  const lastActivity = pendingPermissionsFromOrigin.at(-1);
   const isLegacySwitchChainRequest =
     lastActivity?.method === 'wallet_switchEthereumChain';
-
   const isRequestingAccounts = Boolean(
     permissionsRequest?.permissions?.[Caip25EndowmentPermissionName] &&
       !isLegacySwitchChainRequest,
