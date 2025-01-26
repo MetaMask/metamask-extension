51b6de6e1562df0b52a839d43cde8544e22bfc2e
dddddanica <zhaodanica@gmail.com>
2025-01-26 23:47:38 +0000

    fix(3742): fix lint

--- ui/pages/settings/developer-options-tab/developer-options-tab.tsx
+++ ui/pages/settings/developer-options-tab/developer-options-tab.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
 import {
