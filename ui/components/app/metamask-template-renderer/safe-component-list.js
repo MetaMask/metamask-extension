3f344153f78ad93ad2214b16681be81c7f656d4f
david0xd <david@timechaser.org>
2025-01-27 13:16:34 +0100

    Refactor how Skeleton component is integrated

--- ui/components/app/metamask-template-renderer/safe-component-list.js
+++ ui/components/app/metamask-template-renderer/safe-component-list.js
@@ -34,7 +34,6 @@ import { SnapDelineator } from '../snaps/snap-delineator';
 import { SnapUIAddress } from '../snaps/snap-ui-address';
 import { SnapUIAvatar } from '../snaps/snap-ui-avatar';
 import { SnapUIBanner } from '../snaps/snap-ui-banner';
-import { SnapUISkeleton } from '../snaps/snap-ui-skeleton';
 import { SnapUIButton } from '../snaps/snap-ui-button';
 import { SnapUICard } from '../snaps/snap-ui-card';
 import { SnapUICheckbox } from '../snaps/snap-ui-checkbox';
@@ -61,6 +60,7 @@ import {
 import { SnapAccountRedirect } from '../../../pages/snap-account-redirect';
 import { CreateNamedSnapAccount } from '../../multichain/create-named-snap-account';
 import SnapAuthorshipHeader from '../snaps/snap-authorship-header';
+import { Skeleton } from '../../component-library/skeleton';
 ///: END:ONLY_INCLUDE_IF
 
 export const safeComponentList = {
@@ -92,7 +92,6 @@ export const safeComponentList = {
   SnapUIAddress,
   SnapUIAvatar,
   SnapUIBanner,
-  SnapUISkeleton,
   SnapUIButton,
   SnapUICard,
   SnapUICheckbox,
@@ -110,6 +109,7 @@ export const safeComponentList = {
   SnapUITooltip,
   span: 'span',
   Spinner,
+  Skeleton,
   Text,
   TextArea,
   TextField,
