328b3acf974a598133740a49a2d6a20f4d1aa28c
Hassan Malik <hbmalik88@gmail.com>
2025-01-28 23:43:30 -0500

    add AvatarIcon to component mapping

--- ui/components/app/snaps/snap-ui-renderer/components/index.ts
+++ ui/components/app/snaps/snap-ui-renderer/components/index.ts
@@ -28,6 +28,7 @@ import { icon } from './icon';
 import { section } from './section';
 import { avatar } from './avatar';
 import { banner } from './banner';
+import { avatarIcon } from './avatar-icon';
 
 export const COMPONENT_MAPPING = {
   Box: box,
@@ -60,4 +61,5 @@ export const COMPONENT_MAPPING = {
   Selector: selector,
   Section: section,
   Banner: banner,
+  AvatarIcon: avatarIcon,
 };
