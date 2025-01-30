c18440b5de3276a3a27ac2958c00bad72e81995d
Jiexi Luan <jiexiluan@gmail.com>
2025-01-30 14:40:05 -0800

    Fix connect page supported request chain id check

--- ui/pages/permissions-connect/connect-page/connect-page.tsx
+++ ui/pages/permissions-connect/connect-page/connect-page.tsx
@@ -98,8 +98,10 @@ export const ConnectPage: React.FC<ConnectPageProps> = ({
     ? [...nonTestNetworks, selectedTestNetwork].map(({ chainId }) => chainId)
     : nonTestNetworks.map(({ chainId }) => chainId);
 
+  const allNetworksList = [...nonTestNetworks, ...testNetworks].map(({ chainId }) => chainId)
+
   const supportedRequestedChainIds = requestedChainIds.filter((chainId) =>
-    selectedNetworksList.includes(chainId),
+    allNetworksList.includes(chainId),
   );
 
   const defaultSelectedChainIds =
