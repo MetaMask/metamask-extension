65d0d7079eb0a7611c95076ed51ed9ec3400de2d
Jongsun Suh <jongsun.suh@icloud.com>
2025-01-31 08:02:52 -0500

    converting ethereum-chain-utils to typescript and moving reususable types to types.ts file

--- app/scripts/lib/rpc-method-middleware/handlers/types.ts
+++ app/scripts/lib/rpc-method-middleware/handlers/types.ts
@@ -1,3 +1,7 @@
+import {
+  AddApprovalOptions,
+  EndFlowOptions,
+} from '@metamask/approval-controller';
 import { JsonRpcParams, JsonRpcRequest } from '@metamask/utils';
 import { MessageType } from '../../../../../shared/constants/app';
 
@@ -10,3 +14,24 @@ export type HandlerRequestType<Params extends JsonRpcParams = JsonRpcParams> =
   Required<JsonRpcRequest<Params>> & {
     origin: string;
   };
+
+export type EndApprovalFlow = ({ id }: EndFlowOptions) => void;
+
+export type GetCaveat = (options: {
+  target: string;
+  caveatType: string;
+}) => Record<string, string[]> | undefined;
+
+export type GetChainPermissionsFeatureFlag = () => boolean;
+
+export type RequestPermittedChainsPermission = (
+  chainIds: string[],
+) => Promise<void>;
+
+export type RequestUserApproval = (
+  options?: AddApprovalOptions,
+) => Promise<unknown>;
+
+export type SetActiveNetwork = (
+  networkConfigurationIdOrType: string,
+) => Promise<void>;
