25c15084b5f44bdb9bd3d4fb537801cb26d1e5b3
Frederik Bolding <frederik.bolding@gmail.com>
2025-02-04 16:26:25 +0100

    Use any for now

--- ui/components/app/snaps/snap-ui-renderer/utils.test.ts
+++ ui/components/app/snaps/snap-ui-renderer/utils.test.ts
@@ -1,4 +1,3 @@
-import type { Struct } from 'superstruct';
 import { JSXElementStruct } from '@metamask/snaps-sdk/jsx';
 import { COMPONENT_MAPPING } from './components';
 
@@ -6,8 +5,9 @@ const EXCLUDED_COMPONENTS = ['Option', 'Radio', 'SelectorOption'];
 
 describe('Snap UI mapping', () => {
   it('supports all exposed components', () => {
-    const elements = (JSXElementStruct.schema as unknown as Struct[])
-      .map((struct: string) => JSON.parse(struct.schema.type.type))
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const elements = (JSXElementStruct.schema as unknown as any[])
+      .map((struct) => JSON.parse(struct.schema.type.type))
       .filter((key) => !EXCLUDED_COMPONENTS.includes(key));
     expect(Object.keys(COMPONENT_MAPPING).sort()).toStrictEqual(
       elements.sort(),
