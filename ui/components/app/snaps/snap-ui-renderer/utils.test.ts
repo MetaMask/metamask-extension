8193410f856a8c49a9b25b134d0687d5ffed9f28
Frederik Bolding <frederik.bolding@gmail.com>
2025-02-04 15:13:29 +0100

    Fix types

--- ui/components/app/snaps/snap-ui-renderer/utils.test.ts
+++ ui/components/app/snaps/snap-ui-renderer/utils.test.ts
@@ -1,3 +1,4 @@
+import type { Struct } from '@metamask/superstruct';
 import { JSXElementStruct } from '@metamask/snaps-sdk/jsx';
 import { COMPONENT_MAPPING } from './components';
 
@@ -5,7 +6,7 @@ const EXCLUDED_COMPONENTS = ['Option', 'Radio', 'SelectorOption'];
 
 describe('Snap UI mapping', () => {
   it('supports all exposed components', () => {
-    const elements = JSXElementStruct.schema
+    const elements = (JSXElementStruct.schema as Struct[])
       .map((struct) => JSON.parse(struct.schema.type.type))
       .filter((key) => !EXCLUDED_COMPONENTS.includes(key));
     expect(Object.keys(COMPONENT_MAPPING).sort()).toStrictEqual(
