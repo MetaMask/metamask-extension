4869c1f2cfe30a9fc6fba447c1f2a280b4762b43
chloeYue <105063779+chloeYue@users.noreply.github.com>
2025-02-05 11:41:24 +0100

    test: [POM] Migrate connections e2e tests to TS and Page Object Model (#29609)
    Follow PR #29384 and continue migrating the remaining connection e2e
    tests to TypeScript and the Page Object Model.
    
    Specs migrated:
    ```
    test/e2e/tests/connections/review-permissions-page.spec.js
    test/e2e/tests/connections/review-switch-permission-page.spec.js
    test/e2e/tests/content-security-policy/content-security-policy.spec.js
    ```
    
    [![Open in GitHub
    Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/27155?quickstart=1)
    
    Fixes:
    
    Check code readability, make sure tests pass.
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    <!-- [screenshots/recordings] -->
    
    <!-- [screenshots/recordings] -->
    
    - [x] I've followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
    Extension Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    - [x] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [x] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: seaona <54408225+seaona@users.noreply.github.com>

--- test/e2e/tests/content-security-policy/content-security-policy.spec.ts
+++ test/e2e/tests/content-security-policy/content-security-policy.spec.ts
@@ -1,7 +1,9 @@
 import { strict as assert } from 'assert';
 import { Suite } from 'mocha';
-import { openDapp, unlockWallet, withFixtures } from '../../helpers';
+import { withFixtures } from '../../helpers';
 import FixtureBuilder from '../../fixture-builder';
+import TestDapp from '../../page-objects/pages/test-dapp';
+import { loginWithBalanceValidation } from '../../page-objects/flows/login.flow';
 
 describe('Content-Security-Policy', function (this: Suite) {
   it('opening a restricted website should still load the extension', async function () {
@@ -27,9 +29,10 @@ describe('Content-Security-Policy', function (this: Suite) {
         fixtures: new FixtureBuilder().build(),
         title: this.test?.fullTitle(),
       },
-      async ({ driver }) => {
-        await unlockWallet(driver);
-        await openDapp(driver);
+      async ({ driver, ganacheServer }) => {
+        await loginWithBalanceValidation(driver, ganacheServer);
+        const testDapp = new TestDapp(driver);
+        await testDapp.openTestDappPage();
         const isExtensionLoaded: boolean = await driver.executeScript(
           'return typeof window.ethereum !== "undefined"',
         );
