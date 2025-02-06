3c858b9d0a48633601c8df1a1ff77c9fce57ac7b
Javier <javier.vera@consensys.net>
2025-02-06 14:08:28 +0100

    feat: lint fix done

--- test/e2e/flask/solana/transactions-activity.spec.ts
+++ test/e2e/flask/solana/transactions-activity.spec.ts
@@ -2,14 +2,10 @@ import { strict as assert } from 'assert';
 import { Suite } from 'mocha';
 
 import SendSolanaPage from '../../page-objects/pages/send/solana-send-page';
-import ConfirmSolanaTxPage from '../../page-objects/pages/send/solana-confirm-tx-page';
-import SolanaTxresultPage from '../../page-objects/pages/send/solana-tx-result-page';
 import NonEvmHomepage from '../../page-objects/pages/home/non-evm-homepage';
-import { commonSolanaAddress, withSolanaAccountSnap } from './common-solana';
 import ActivityListPage from '../../page-objects/pages/home/activity-list';
 
-const splTokenName = 'PKIN';
-describe('Transaction activity', function (this: Suite) {
+describe.skip('Transaction activity list', function (this: Suite) {
   // skipped due tohttps://github.com/MetaMask/snaps/issues/3019
   it('user can see activity list', async function () {
     this.timeout(120000);
@@ -38,108 +34,6 @@ describe('Transaction activity', function (this: Suite) {
         await activityList.check_txAction('Receive', 1);
         await activityList.check_txAmountInActivity('-0.000005', 2);
         await activityList.check_noFailedTransactions();
-      },
-    );
-  });
-});
-describe('Send flow', function (this: Suite) {
-  it('and Transaction fails', async function () {
-    this.timeout(120000); // there is a bug open for this big timeout https://consensyssoftware.atlassian.net/browse/SOL-90
-    await withSolanaAccountSnap(
-      {
-        title: this.test?.fullTitle(),
-        showNativeTokenAsMainBalance: true,
-        mockCalls: true,
-        mockSendTransaction: false,
-        simulateTransaction: true,
-      },
-      async (driver) => {
-        const homePage = new NonEvmHomepage(driver);
-        await homePage.check_pageIsLoaded();
-        await homePage.clickOnSendButton();
-
-        const sendSolanaPage = new SendSolanaPage(driver);
-        await sendSolanaPage.check_pageIsLoaded();
-        await sendSolanaPage.setToAddress(commonSolanaAddress);
-        await sendSolanaPage.openTokenList();
-        await sendSolanaPage.selectTokenFromTokenList(splTokenName);
-        await sendSolanaPage.check_amountCurrencyIsDisplayed(splTokenName);
-        await sendSolanaPage.setAmount('0.1');
-        // assert.equal(await sendSolanaPage.isContinueButtonEnabled(), true, "Continue button is not enabled when address and amount are set");
-        await sendSolanaPage.clickOnContinue();
-        const confirmSolanaPage = new ConfirmSolanaTxPage(driver);
-
-        await confirmSolanaPage.clickOnSend();
-        const failedTxPage = new SolanaTxresultPage(driver);
-        assert.equal(
-          await failedTxPage.check_TransactionStatusText('0.1', false),
-          true,
-          'Transaction amount is not correct',
-        );
-        assert.equal(
-          await failedTxPage.check_TransactionStatus(false),
-          true,
-          'Transaction did not fail as expected',
-        );
-        assert.equal(
-          await failedTxPage.isTransactionDetailDisplayed('From'),
-          true,
-          'From field not displayed and it should',
-        );
-        assert.equal(
-          await failedTxPage.isTransactionDetailDisplayed('Recipient'),
-          true,
-          'Recipient field not displayed and it should',
-        );
-        assert.equal(
-          await failedTxPage.isTransactionDetailDisplayed('Network'),
-          true,
-          'Network field not displayed and it should',
-        );
-        assert.equal(
-          await failedTxPage.isTransactionDetailDisplayed('Transaction speed'),
-          true,
-          'Transaction field not displayed and it should',
-        );
-        assert.equal(
-          await failedTxPage.isTransactionDetailDisplayed('Network fee'),
-          true,
-          'Network fee field not displayed and it should',
-        );
-      },
-    );
-  });
-});
-
-describe('Send flow', function (this: Suite) {
-  it('and Transaction simulation fails', async function () {
-    this.timeout(120000); // there is a bug open for this big timeout https://consensyssoftware.atlassian.net/browse/SOL-90
-    await withSolanaAccountSnap(
-      {
-        title: this.test?.fullTitle(),
-        showNativeTokenAsMainBalance: true,
-        mockCalls: true,
-        mockSendTransaction: false,
-        simulateTransaction: false,
-      },
-      async (driver) => {
-        const homePage = new NonEvmHomepage(driver);
-        await homePage.check_pageIsLoaded();
-        await homePage.clickOnSendButton();
-
-        const sendSolanaPage = new SendSolanaPage(driver);
-        await sendSolanaPage.check_pageIsLoaded();
-        await sendSolanaPage.setToAddress(commonSolanaAddress);
-        await sendSolanaPage.openTokenList();
-        await sendSolanaPage.selectTokenFromTokenList(splTokenName);
-        await sendSolanaPage.check_amountCurrencyIsDisplayed(splTokenName);
-        await sendSolanaPage.setAmount('0.1');
-        await sendSolanaPage.check_TxSimulationFailed();
-        assert.equal(
-          await sendSolanaPage.isContinueButtonEnabled(),
-          false,
-          'Continue button is enabled when transaction simulation fails',
-        );
       },
     );
   });
