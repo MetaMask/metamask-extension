0fac1eafbc30f0f8d90d3ffbd1609e85cd2f4f1f
sahar-fehri <sahar.fehri@consensys.net>
2025-02-06 15:01:46 +0100

    fix: unit test

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -1000,6 +1000,7 @@ export default class MetamaskController extends EventEmitter {
         allowedEvents: [
           'AccountsController:accountAdded',
           'AccountsController:accountRemoved',
+          'AccountsController:accountBalancesUpdated',
         ],
         allowedActions: [
           'AccountsController:listMultichainAccounts',
@@ -3253,8 +3254,6 @@ export default class MetamaskController extends EventEmitter {
         this.multichainRatesController.start();
       },
     );
-    this.multichainBalancesController.start();
-    this.multichainBalancesController.updateBalances();
 
     ///: BEGIN:ONLY_INCLUDE_IF(build-flask)
     this.multichainTransactionsController.start();
