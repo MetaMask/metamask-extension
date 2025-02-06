cb8d677e043f0d1092dfbbf593f6ff8912dbd0a0
Javier <javier.vera@consensys.net>
2025-02-06 14:02:24 +0100

    feat: transaction activity added

--- test/e2e/flask/solana/transactions-activity.spec.ts
+++ test/e2e/flask/solana/transactions-activity.spec.ts
@@ -6,6 +6,7 @@ import ConfirmSolanaTxPage from '../../page-objects/pages/send/solana-confirm-tx
 import SolanaTxresultPage from '../../page-objects/pages/send/solana-tx-result-page';
 import NonEvmHomepage from '../../page-objects/pages/home/non-evm-homepage';
 import { commonSolanaAddress, withSolanaAccountSnap } from './common-solana';
+import ActivityListPage from '../../page-objects/pages/home/activity-list';
 
 const splTokenName = 'PKIN';
 describe('Transaction activity', function (this: Suite) {
@@ -32,108 +33,11 @@ describe('Transaction activity', function (this: Suite) {
           false,
           'Continue button is enabled when no address nor amount',
         );
-        await sendSolanaPage.setToAddress(commonSolanaAddress);
-        assert.equal(
-          await sendSolanaPage.isContinueButtonEnabled(),
-          false,
-          'Continue button is enabled when no address',
-        );
-        await sendSolanaPage.openTokenList();
-        await sendSolanaPage.check_tokenByNameIsDisplayed('SOL');
-
-        await sendSolanaPage.check_tokenByNameIsDisplayed(splTokenName);
-
-        await sendSolanaPage.selectTokenFromTokenList(splTokenName);
-
-        await sendSolanaPage.check_amountCurrencyIsDisplayed(splTokenName);
-        await sendSolanaPage.check_tokenBalanceIsDisplayed('6', splTokenName);
-        await sendSolanaPage.setAmount('0.1');
-        await sendSolanaPage.clickOnContinue();
-
-        const confirmSolanaPage = new ConfirmSolanaTxPage(driver);
-        await sendSolanaPage.clickOnContinue();
-        assert.equal(
-          await confirmSolanaPage.checkAmountDisplayed('0.1', splTokenName),
-          true,
-          'Check amount displayed is wrong',
-        );
-        assert.equal(
-          await confirmSolanaPage.isTransactionDetailDisplayed('From'),
-          true,
-          'From is not displayed and it should',
-        );
-
-        assert.equal(
-          await confirmSolanaPage.isTransactionDetailDisplayed('Recipient'),
-          true,
-          'Recipient is not displayed and it should',
-        );
-        assert.equal(
-          await confirmSolanaPage.isTransactionDetailDisplayed('Network'),
-          true,
-          'Network is not displayed and it should',
-        );
-        assert.equal(
-          await confirmSolanaPage.isTransactionDetailDisplayed(
-            'Transaction speed',
-          ),
-          true,
-          'Transaction speed is not displayed and it should',
-        );
-
-        assert.equal(
-          await confirmSolanaPage.isTransactionDetailDisplayed('Network fee'),
-          true,
-          'Network fee is not displayed and it should',
-        );
-
-        await confirmSolanaPage.clickOnSend();
-        const sentTxPage = new SolanaTxresultPage(driver);
-        assert.equal(
-          await sentTxPage.check_TransactionStatusText(
-            '0.1',
-            true,
-            splTokenName,
-          ),
-          true,
-          'Transaction amount is not correct',
-        );
-        assert.equal(
-          await sentTxPage.check_TransactionStatus(true),
-          true,
-          'Transaction was not sent as expected',
-        );
-        assert.equal(
-          await sentTxPage.isTransactionDetailDisplayed('From'),
-          true,
-          'From field not displayed and it should',
-        );
-
-        assert.equal(
-          await sentTxPage.isTransactionDetailDisplayed('Recipient'),
-          true,
-          'Recipient field not displayed and it should',
-        );
-        assert.equal(
-          await sentTxPage.isTransactionDetailDisplayed('Network'),
-          true,
-          'Network field not displayed and it should',
-        );
-        assert.equal(
-          await sentTxPage.isTransactionDetailDisplayed('Transaction speed'),
-          true,
-          'Transaction field not displayed and it should',
-        );
-        assert.equal(
-          await sentTxPage.isTransactionDetailDisplayed('Network fee'),
-          true,
-          'Network fee field not displayed',
-        );
-        assert.equal(
-          await sentTxPage.check_isViewTransactionLinkDisplayed(),
-          true,
-          'View transaction link is not displayed and it should',
-        );
+        const activityList = new ActivityListPage(driver);
+        await activityList.check_confirmedTxNumberDisplayedInActivity(2);
+        await activityList.check_txAction('Receive', 1);
+        await activityList.check_txAmountInActivity('-0.000005', 2);
+        await activityList.check_noFailedTransactions();
       },
     );
   });
