a7dedf3b5fa3e36d6641dcdf1191a3df5ba86e31
Nicholas Gambino <nicholas.gambino@consensys.net>
2025-02-11 20:44:14 -0800

    fix: Add ellipsis back to token cell title to handle long token names

--- ui/components/app/assets/token-cell/cells/token-cell-title.tsx
+++ ui/components/app/assets/token-cell/cells/token-cell-title.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+  Display,
   FontWeight,
   TextVariant,
 } from '../../../../../helpers/constants/design-system';
@@ -11,6 +12,7 @@ import {
   networkTitleOverrides,
 } from '../../util/networkTitleOverrides';
 import { useI18nContext } from '../../../../../hooks/useI18nContext';
+import Tooltip from '../../../../ui/tooltip';
 
 type TokenCellTitleProps = {
   token: TokenFiatDisplayInfo;
@@ -18,6 +20,29 @@ type TokenCellTitleProps = {
 
 export const TokenCellTitle = ({ token }: TokenCellTitleProps) => {
   const t = useI18nContext();
+
+  if (token.title.length > 12) {
+    return (
+      <Tooltip
+        position="bottom"
+        html={token.title}
+        tooltipInnerClassName="multichain-token-list-item__tooltip"
+      >
+        <Text
+          as="span"
+          fontWeight={FontWeight.Medium}
+          variant={TextVariant.bodyMd}
+          display={Display.Block}
+          ellipsis
+        >
+          {networkTitleOverrides(t as TranslateFunction, token)}
+          {token.isStakeable && (
+            <StakeableLink chainId={token.chainId} symbol={token.symbol} />
+          )}
+        </Text>
+      </Tooltip>
+    );
+  }
 
   // non-ellipsized title
   return (
