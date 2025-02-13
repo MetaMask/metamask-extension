06e9b23357dbb7e18de97c8dad4b5aba5c184558
ffmcgee <joao.carlos@consensys.net>
2025-02-13 17:07:36 +0100

    test: wallet_addEthereumChain.spec.ts (wip)

--- test/e2e/json-rpc/wallet_addEthereumChain.spec.ts
+++ test/e2e/json-rpc/wallet_addEthereumChain.spec.ts
@@ -146,7 +146,7 @@ describe('Add Ethereum Chain', function () {
       );
     });
 
-    it('prompts to switch to the chain when the rpc endpoint being added already exists', async function () {
+    it.skip('prompts to switch to the chain when the rpc endpoint being added already exists', async function () {
       await withFixtures(
         {
           dapp: true,
@@ -254,7 +254,7 @@ describe('Add Ethereum Chain', function () {
       );
     });
 
-    it('prompts to switch to the chain when the rpc endpoint being added already exists', async function () {
+    it.skip('prompts to switch to the chain when the rpc endpoint being added already exists', async function () {
       await withFixtures(
         {
           dapp: true,
