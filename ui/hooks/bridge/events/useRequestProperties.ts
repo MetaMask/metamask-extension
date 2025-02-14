07b1e90cfa44fbd39eb87cafa6fca3d9dd370acc
Micaela Estabillo <micaela.estabillo@consensys.net>
2025-02-13 21:22:01 -0800

    fix: chainIds in quote request metrics

--- ui/hooks/bridge/events/useRequestProperties.ts
+++ ui/hooks/bridge/events/useRequestProperties.ts
@@ -5,7 +5,6 @@ import {
   getFromToken,
   getToToken,
 } from '../../../ducks/bridge/selectors';
-import { Numeric } from '../../../../shared/modules/Numeric';
 
 export const useRequestProperties = () => {
   const { srcChainId, destChainId, srcTokenAddress, destTokenAddress } =
@@ -13,18 +12,14 @@ export const useRequestProperties = () => {
   const fromToken = useSelector(getFromToken);
   const toToken = useSelector(getToToken);
 
-  const chain_id_source =
-    srcChainId && new Numeric(srcChainId, 10).toPrefixedHexString();
-  const chain_id_destination =
-    destChainId && new Numeric(destChainId, 10).toPrefixedHexString();
   const token_symbol_source = fromToken?.symbol;
   const token_symbol_destination = toToken?.symbol;
   const token_address_source = srcTokenAddress;
   const token_address_destination = destTokenAddress;
 
   if (
-    chain_id_source &&
-    chain_id_destination &&
+    srcChainId &&
+    destChainId &&
     token_address_source &&
     token_address_destination &&
     token_symbol_source &&
@@ -32,16 +27,16 @@ export const useRequestProperties = () => {
   ) {
     return {
       quoteRequestProperties: {
-        chain_id_source,
-        chain_id_destination,
+        chain_id_source: srcChainId,
+        chain_id_destination: destChainId,
         token_symbol_source,
         token_symbol_destination,
         token_address_source,
         token_address_destination,
       },
       flippedRequestProperties: {
-        chain_id_source: chain_id_destination,
-        chain_id_destination: chain_id_source,
+        chain_id_source: destChainId,
+        chain_id_destination: srcChainId,
         token_symbol_source: token_symbol_destination,
         token_symbol_destination: token_symbol_source,
         token_address_source: token_address_destination,
