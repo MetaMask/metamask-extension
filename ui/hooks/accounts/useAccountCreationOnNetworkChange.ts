3a292dd65dec8f0bda9118fda392d6ff7cc51ab8
gantunesr <17601467+gantunesr@users.noreply.github.com>
2025-02-17 06:46:47 -0300

    chore: lint

--- ui/hooks/accounts/useAccountCreationOnNetworkChange.ts
+++ ui/hooks/accounts/useAccountCreationOnNetworkChange.ts
@@ -1,37 +1,47 @@
-import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import { CaipChainId } from '@metamask/utils';
-import { useMultichainWalletSnapClient, WalletClientType } from './useMultichainWalletSnapClient';
 import { MultichainNetworks } from '../../../shared/constants/multichain/networks';
-import type { InternalAccountWithBalance } from '../../selectors/selectors.types';
 import { getMetaMaskAccountsOrdered } from '../../selectors';
+import {
+  useMultichainWalletSnapClient,
+  WalletClientType,
+} from './useMultichainWalletSnapClient';
 
-interface UseAccountCreationOnNetworkChangeReturn {
+type UseAccountCreationOnNetworkChangeReturn = {
   createAccount: (chainId: CaipChainId) => Promise<void>;
   isAccountInNetwork: (chainId: CaipChainId) => boolean;
-}
-
-export const useAccountCreationOnNetworkChange = (): UseAccountCreationOnNetworkChangeReturn => {
-  const bitcoinWalletSnapClient = useMultichainWalletSnapClient(WalletClientType.Bitcoin);
-  const solanaWalletSnapClient = useMultichainWalletSnapClient(WalletClientType.Solana);
-  const accounts = useSelector(getMetaMaskAccountsOrdered);
-
-  const createAccount = async (chainId: CaipChainId) => {
-    switch (chainId) {
-      case MultichainNetworks.BITCOIN:
-        await bitcoinWalletSnapClient.createAccount(MultichainNetworks.BITCOIN);
-        break;
-      case MultichainNetworks.SOLANA:
-        await solanaWalletSnapClient.createAccount(MultichainNetworks.SOLANA);
-        break;
-      default:
-        throw new Error(`Unsupported chainId: ${chainId}`);
-    }
-  };
-
-  const isAccountInNetwork = (chainId: CaipChainId) => {
-    return accounts.some(({ scopes }: { scopes: CaipChainId[] }) => scopes.includes(chainId));
-  };
-
-  return { createAccount, isAccountInNetwork };
 };
+
+export const useAccountCreationOnNetworkChange =
+  (): UseAccountCreationOnNetworkChangeReturn => {
+    const bitcoinWalletSnapClient = useMultichainWalletSnapClient(
+      WalletClientType.Bitcoin,
+    );
+    const solanaWalletSnapClient = useMultichainWalletSnapClient(
+      WalletClientType.Solana,
+    );
+    const accounts = useSelector(getMetaMaskAccountsOrdered);
+
+    const createAccount = async (chainId: CaipChainId) => {
+      switch (chainId) {
+        case MultichainNetworks.BITCOIN:
+          await bitcoinWalletSnapClient.createAccount(
+            MultichainNetworks.BITCOIN,
+          );
+          break;
+        case MultichainNetworks.SOLANA:
+          await solanaWalletSnapClient.createAccount(MultichainNetworks.SOLANA);
+          break;
+        default:
+          throw new Error(`Unsupported chainId: ${chainId}`);
+      }
+    };
+
+    const isAccountInNetwork = (chainId: CaipChainId) => {
+      return accounts.some(({ scopes }: { scopes: CaipChainId[] }) =>
+        scopes.includes(chainId),
+      );
+    };
+
+    return { createAccount, isAccountInNetwork };
+  };
