b4ce8fb70e25a1a5a594c7fb6004ddb6886cab0c
Prithpal Sooriya <prithpal.sooriya@consensys.net>
2025-02-17 21:08:33 +0000

    perf: virtualize send account list
    OK seeing some perf improvements... but there is still an initial "lag" when using portfolio view...

--- ui/components/multichain/pages/send/components/send-page-row.tsx
+++ ui/components/multichain/pages/send/components/send-page-row.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import { Box } from '../../../../component-library';
+import { Box, BoxProps } from '../../../../component-library';
 import {
   Display,
   FlexDirection,
 } from '../../../../../helpers/constants/design-system';
 
-export const SendPageRow = ({
-  children,
-}: {
-  children: React.ReactNode | React.ReactNode[];
-}) => (
-  <Box
-    display={Display.Flex}
-    paddingBottom={4}
-    flexDirection={FlexDirection.Column}
-  >
-    {children}
-  </Box>
+export const SendPageRow = React.forwardRef<HTMLDivElement, BoxProps<'div'>>(
+  ({ children, ...boxProps }, ref) => (
+    <Box
+      ref={ref}
+      display={Display.Flex}
+      paddingBottom={4}
+      flexDirection={FlexDirection.Column}
+      {...boxProps}
+    >
+      {children}
+    </Box>
+  ),
 );
--- ui/components/multichain/pages/send/components/your-accounts.tsx
+++ ui/components/multichain/pages/send/components/your-accounts.tsx
@@ -1,8 +1,9 @@
-import React, { useCallback, useContext, useMemo } from 'react';
+import React, { useCallback, useContext, useMemo, useRef } from 'react';
 import type { Dispatch } from 'redux';
 import { useDispatch, useSelector } from 'react-redux';
 import { EthAccountType, KeyringAccountType } from '@metamask/keyring-api';
 import { InternalAccount } from '@metamask/keyring-internal-api';
+import { useVirtualizer } from '@tanstack/react-virtual';
 import {
   getUpdatedAndSortedAccounts,
   getSelectedInternalAccount,
@@ -21,6 +22,7 @@ import {
   MetaMetricsEventCategory,
   MetaMetricsEventName,
 } from '../../../../../../shared/constants/metametrics';
+import { Box } from '../../../../component-library';
 import { SendPageRow } from '.';
 
 type SendPageYourAccountsProps = {
@@ -93,19 +95,55 @@ export const SendPageYourAccounts = ({
   }, [accounts, allowedAccountTypes]);
   const selectedAccount = useSelector(getSelectedInternalAccount);
 
+  const rootRef = useRef(
+    document.getElementsByClassName('app').item(0) as HTMLDivElement,
+  );
+  const virtualizer = useVirtualizer({
+    count: filteredAccounts.length,
+    estimateSize: () => 80,
+    getScrollElement: () => rootRef.current,
+  });
+  const items = virtualizer.getVirtualItems();
+
   return (
-    <SendPageRow>
-      {/* TODO: Replace `any` with type */}
-      {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
-      {filteredAccounts.map((account: any) => (
-        <AccountListItemContainer
-          key={account.address}
-          account={account}
-          selectedAccount={selectedAccount}
-          dispatch={dispatch}
-          trackEvent={trackEvent}
-        />
-      ))}
+    <SendPageRow ref={rootRef}>
+      <Box
+        style={{
+          height: `${virtualizer.getTotalSize()}px`,
+          width: '100%',
+          position: 'relative',
+        }}
+      >
+        {items.map((virtualItem) => {
+          const account = filteredAccounts[virtualItem.index] ?? undefined;
+          if (!account) {
+            return null;
+          }
+
+          return (
+            <Box
+              key={account.address}
+              style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: `${virtualItem.size}px`,
+                transform: `translateY(${
+                  virtualItem.start - virtualizer.options.scrollMargin
+                }px)`,
+              }}
+            >
+              <AccountListItemContainer
+                account={account}
+                selectedAccount={selectedAccount}
+                dispatch={dispatch}
+                trackEvent={trackEvent}
+              />
+            </Box>
+          );
+        })}
+      </Box>
     </SendPageRow>
   );
 };
