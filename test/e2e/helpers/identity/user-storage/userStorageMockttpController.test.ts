85b344ee328f2b28101945a091fca8485d1d2cb1
Desi McAdam <desi.mcadam@gmail.com>
2025-02-18 12:24:33 -0700

    Fix previous test change and make new one in an actual e2e test

--- test/e2e/helpers/identity/user-storage/userStorageMockttpController.test.ts
+++ test/e2e/helpers/identity/user-storage/userStorageMockttpController.test.ts
@@ -22,7 +22,7 @@ describe('UserStorageMockttpController', () => {
         },
       );
 
-      expect(request.json).toEqual([]);
+      expect(request.json).toEqual(null);
     });
 
     it('handles GET requests that have a pre-defined response', async () => {
