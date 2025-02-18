f7a81c9903cfe40ff0cb55a88c11e247e964551f
gantunesr <17601467+gantunesr@users.noreply.github.com>
2025-02-18 09:04:01 -0300

    test: update unit test mocks

--- ui/selectors/multichain-networks.test.ts
+++ ui/selectors/multichain-networks.test.ts
@@ -1,5 +1,7 @@
 import { BtcScope, SolScope } from '@metamask/keyring-api';
-import { RpcEndpointType, NetworkStatus } from '@metamask/network-controller';
+import { type NetworkConfiguration, RpcEndpointType, NetworkStatus } from '@metamask/network-controller';
+import type { Hex, CaipChainId } from '@metamask/utils';
+import { type MultichainNetworkConfiguration } from '@metamask/multichain-network-controller';
 
 import { type NetworkState } from '../../shared/modules/selectors/networks';
 import {
@@ -16,59 +18,86 @@ type TestState = MultichainNetworkControllerState &
     metamask: { solanaSupportEnabled: boolean; bitcoinSupportEnabled: boolean };
   };
 
+const nonEvmNetworks: Record<CaipChainId, MultichainNetworkConfiguration> = {
+  [SolScope.Mainnet]: {
+    chainId: SolScope.Mainnet,
+    name: 'Solana Mainnet',
+    nativeCurrency: `${SolScope.Mainnet}/token:EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v`,
+    isEvm: false,
+  },
+  [BtcScope.Mainnet]: {
+    chainId: BtcScope.Mainnet,
+    name: 'Bitcoin Mainnet',
+    nativeCurrency: `${BtcScope.Mainnet}/slip44:0`,
+    isEvm: false,
+  },
+}
+
+const evmNetworksWithNewConfig: Record<CaipChainId, MultichainNetworkConfiguration> = {
+  'eip155:1': {
+    chainId: 'eip155:1',
+    name: 'Ethereum Mainnet',
+    nativeCurrency: 'ETH',
+    blockExplorerUrls: ['https://etherscan.io'],
+    defaultBlockExplorerUrlIndex: 0,
+    isEvm: true,
+  },
+  'eip155:11155111': {
+    chainId: 'eip155:11155111',
+    name: 'Sepolia',
+    nativeCurrency: 'SepoliaETH',
+    blockExplorerUrls: ['https://sepolia.etherscan.io'],
+    defaultBlockExplorerUrlIndex: 0,
+    isEvm: true,
+  },
+};
+
+const evmNetworksWithOldConfig: Record<Hex, NetworkConfiguration> = {
+  '0x1': {
+    chainId: '0x1',
+    name: 'Ethereum Mainnet',
+    nativeCurrency: 'ETH',
+    rpcEndpoints: [
+      {
+        networkClientId: 'mainnet',
+        type: RpcEndpointType.Infura,
+        url: 'https://mainnet.infura.io/v3/{infuraProjectId}',
+      },
+    ],
+    defaultRpcEndpointIndex: 1,
+    blockExplorerUrls: ['https://etherscan.io'],
+    defaultBlockExplorerUrlIndex: 0,
+    lastUpdatedAt: 1739466375574,
+  },
+  '0xaa36a7': {
+    blockExplorerUrls: ['https://sepolia.etherscan.io'],
+    chainId: '0xaa36a7',
+    defaultBlockExplorerUrlIndex: 0,
+    defaultRpcEndpointIndex: 0,
+    name: 'Sepolia',
+    nativeCurrency: 'SepoliaETH',
+    rpcEndpoints: [
+      {
+        networkClientId: 'sepolia',
+        type: RpcEndpointType.Infura,
+        url: 'https://sepolia.infura.io/v3/{infuraProjectId}',
+      },
+    ],
+  },
+}
+
 const mockMultichainNetworkState: TestState = {
   metamask: {
     solanaSupportEnabled: true,
     bitcoinSupportEnabled: true,
     multichainNetworkConfigurationsByChainId: {
-      [SolScope.Mainnet]: {
-        chainId: SolScope.Mainnet,
-        name: 'Solana Mainnet',
-        nativeCurrency: `${SolScope.Mainnet}/token:EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v`,
-        isEvm: false,
-      },
-      [BtcScope.Mainnet]: {
-        chainId: BtcScope.Mainnet,
-        name: 'Bitcoin Mainnet',
-        nativeCurrency: `${BtcScope.Mainnet}/slip44:0`,
-        isEvm: false,
-      },
+      ...nonEvmNetworks,
     },
     selectedMultichainNetworkChainId: SolScope.Mainnet,
     isEvmSelected: false,
     selectedNetworkClientId: 'mainnet',
     networkConfigurationsByChainId: {
-      '0x1': {
-        chainId: '0x1',
-        name: 'Ethereum Mainnet',
-        nativeCurrency: 'ETH',
-        rpcEndpoints: [
-          {
-            networkClientId: 'mainnet',
-            type: RpcEndpointType.Infura,
-            url: 'https://mainnet.infura.io/v3/{infuraProjectId}',
-          },
-        ],
-        defaultRpcEndpointIndex: 1,
-        blockExplorerUrls: ['https://etherscan.io'],
-        defaultBlockExplorerUrlIndex: 0,
-        lastUpdatedAt: 1739466375574,
-      },
-      '0xaa36a7': {
-        blockExplorerUrls: ['https://sepolia.etherscan.io'],
-        chainId: '0xaa36a7',
-        defaultBlockExplorerUrlIndex: 0,
-        defaultRpcEndpointIndex: 0,
-        name: 'Sepolia',
-        nativeCurrency: 'SepoliaETH',
-        rpcEndpoints: [
-          {
-            networkClientId: 'sepolia',
-            type: RpcEndpointType.Infura,
-            url: 'https://sepolia.infura.io/v3/{infuraProjectId}',
-          },
-        ],
-      },
+      ...evmNetworksWithOldConfig,
     },
     networksMetadata: {
       mainnet: {
@@ -91,18 +120,7 @@ describe('Multichain network selectors', () => {
           mockMultichainNetworkState,
         ),
       ).toStrictEqual({
-        [SolScope.Mainnet]: {
-          chainId: SolScope.Mainnet,
-          name: 'Solana Mainnet',
-          nativeCurrency: `${SolScope.Mainnet}/token:EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v`,
-          isEvm: false,
-        },
-        [BtcScope.Mainnet]: {
-          chainId: BtcScope.Mainnet,
-          name: 'Bitcoin Mainnet',
-          nativeCurrency: `${BtcScope.Mainnet}/slip44:0`,
-          isEvm: false,
-        },
+        ...nonEvmNetworks,
       });
     });
   });
@@ -112,34 +130,8 @@ describe('Multichain network selectors', () => {
       expect(
         getMultichainNetworkConfigurationsByChainId(mockMultichainNetworkState),
       ).toStrictEqual({
-        [SolScope.Mainnet]: {
-          chainId: SolScope.Mainnet,
-          name: 'Solana Mainnet',
-          nativeCurrency: `${SolScope.Mainnet}/token:EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v`,
-          isEvm: false,
-        },
-        [BtcScope.Mainnet]: {
-          chainId: BtcScope.Mainnet,
-          name: 'Bitcoin Mainnet',
-          nativeCurrency: `${BtcScope.Mainnet}/slip44:0`,
-          isEvm: false,
-        },
-        'eip155:1': {
-          chainId: 'eip155:1',
-          name: 'Ethereum Mainnet',
-          nativeCurrency: 'ETH',
-          blockExplorerUrls: ['https://etherscan.io'],
-          defaultBlockExplorerUrlIndex: 0,
-          isEvm: true,
-        },
-        'eip155:11155111': {
-          chainId: 'eip155:11155111',
-          name: 'Sepolia',
-          nativeCurrency: 'SepoliaETH',
-          blockExplorerUrls: ['https://sepolia.etherscan.io'],
-          defaultBlockExplorerUrlIndex: 0,
-          isEvm: true,
-        },
+        ...nonEvmNetworks,
+        ...evmNetworksWithNewConfig,
       });
     });
 
@@ -157,28 +149,8 @@ describe('Multichain network selectors', () => {
           mockMultichainNetworkStateWithSolanaSupportDisabled,
         ),
       ).toStrictEqual({
-        [BtcScope.Mainnet]: {
-          chainId: BtcScope.Mainnet,
-          name: 'Bitcoin Mainnet',
-          nativeCurrency: `${BtcScope.Mainnet}/slip44:0`,
-          isEvm: false,
-        },
-        'eip155:1': {
-          chainId: 'eip155:1',
-          name: 'Ethereum Mainnet',
-          nativeCurrency: 'ETH',
-          blockExplorerUrls: ['https://etherscan.io'],
-          defaultBlockExplorerUrlIndex: 0,
-          isEvm: true,
-        },
-        'eip155:11155111': {
-          chainId: 'eip155:11155111',
-          name: 'Sepolia',
-          nativeCurrency: 'SepoliaETH',
-          blockExplorerUrls: ['https://sepolia.etherscan.io'],
-          defaultBlockExplorerUrlIndex: 0,
-          isEvm: true,
-        },
+        [BtcScope.Mainnet]: nonEvmNetworks[BtcScope.Mainnet],
+        ...evmNetworksWithNewConfig,
       });
     });
 
@@ -198,28 +170,8 @@ describe('Multichain network selectors', () => {
           mockMultichainNetworkStateWithBitcoinSupportDisabled,
         ),
       ).toStrictEqual({
-        [SolScope.Mainnet]: {
-          chainId: SolScope.Mainnet,
-          name: 'Solana Mainnet',
-          nativeCurrency: `${SolScope.Mainnet}/token:EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v`,
-          isEvm: false,
-        },
-        'eip155:1': {
-          chainId: 'eip155:1',
-          name: 'Ethereum Mainnet',
-          nativeCurrency: 'ETH',
-          blockExplorerUrls: ['https://etherscan.io'],
-          defaultBlockExplorerUrlIndex: 0,
-          isEvm: true,
-        },
-        'eip155:11155111': {
-          chainId: 'eip155:11155111',
-          name: 'Sepolia',
-          nativeCurrency: 'SepoliaETH',
-          blockExplorerUrls: ['https://sepolia.etherscan.io'],
-          defaultBlockExplorerUrlIndex: 0,
-          isEvm: true,
-        },
+        [SolScope.Mainnet]: nonEvmNetworks[SolScope.Mainnet],
+        ...evmNetworksWithNewConfig,
       });
     });
 
@@ -237,24 +189,7 @@ describe('Multichain network selectors', () => {
         getMultichainNetworkConfigurationsByChainId(
           mockMultichainNetworkStateWithBitcoinSupportDisabled,
         ),
-      ).toStrictEqual({
-        'eip155:1': {
-          chainId: 'eip155:1',
-          name: 'Ethereum Mainnet',
-          nativeCurrency: 'ETH',
-          blockExplorerUrls: ['https://etherscan.io'],
-          defaultBlockExplorerUrlIndex: 0,
-          isEvm: true,
-        },
-        'eip155:11155111': {
-          chainId: 'eip155:11155111',
-          name: 'Sepolia',
-          nativeCurrency: 'SepoliaETH',
-          blockExplorerUrls: ['https://sepolia.etherscan.io'],
-          defaultBlockExplorerUrlIndex: 0,
-          isEvm: true,
-        },
-      });
+      ).toStrictEqual({ ...evmNetworksWithNewConfig });
     });
   });
 
@@ -276,12 +211,7 @@ describe('Multichain network selectors', () => {
     it('returns the selected non EVM multichain network configuration if isEvmSelected is true', () => {
       expect(
         getSelectedMultichainNetworkConfiguration(mockMultichainNetworkState),
-      ).toStrictEqual({
-        chainId: SolScope.Mainnet,
-        name: 'Solana Mainnet',
-        nativeCurrency: `${SolScope.Mainnet}/token:EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v`,
-        isEvm: false,
-      });
+      ).toStrictEqual(nonEvmNetworks[SolScope.Mainnet]);
     });
     it('returns the selected EVM multichain network configuration if isEvmSelected is false', () => {
       const mockMultichainNetworkStateWithEvmSelected = {
@@ -296,14 +226,7 @@ describe('Multichain network selectors', () => {
         getSelectedMultichainNetworkConfiguration(
           mockMultichainNetworkStateWithEvmSelected,
         ),
-      ).toStrictEqual({
-        chainId: 'eip155:1',
-        name: 'Ethereum Mainnet',
-        nativeCurrency: 'ETH',
-        blockExplorerUrls: ['https://etherscan.io'],
-        defaultBlockExplorerUrlIndex: 0,
-        isEvm: true,
-      });
+      ).toStrictEqual(evmNetworksWithNewConfig['eip155:1']);
     });
   });
 });
