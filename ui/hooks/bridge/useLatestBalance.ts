64b435b779e283e9c430ba588359d5696c650faf
Micaela Estabillo <micaela.estabillo@consensys.net>
2025-02-18 17:10:16 -0800

    chore: show cached balance in useLatestbalance when chainId is not evm

--- ui/hooks/bridge/useLatestBalance.ts
+++ ui/hooks/bridge/useLatestBalance.ts
@@ -1,11 +1,15 @@
-import { useSelector } from 'react-redux';
 import { type Hex, type CaipChainId, isCaipChainId } from '@metamask/utils';
+import { useMemo } from 'react';
 import { Numeric } from '../../../shared/modules/Numeric';
-import { getCurrentChainId } from '../../../shared/modules/selectors/networks';
 import { getSelectedInternalAccount } from '../../selectors';
 import { calcLatestSrcBalance } from '../../../shared/modules/bridge-utils/balance';
 import { useAsyncResult } from '../useAsyncResult';
 import { calcTokenAmount } from '../../../shared/lib/transactions-controller-utils';
+import { useMultichainSelector } from '../useMultichainSelector';
+import {
+  getMultichainBalances,
+  getMultichainCurrentChainId,
+} from '../../selectors/multichain';
 
 /**
  * Custom hook to fetch and format the latest balance of a given token or native asset.
@@ -19,15 +23,21 @@ const useLatestBalance = (
     address: string;
     decimals: number;
     symbol: string;
+    string?: string;
   } | null,
   chainId?: Hex | CaipChainId,
 ) => {
-  const { address: selectedAddress } = useSelector(getSelectedInternalAccount);
-  const currentChainId = useSelector(getCurrentChainId);
+  const { address: selectedAddress, id } = useMultichainSelector(
+    getSelectedInternalAccount,
+  );
+  const currentChainId = useMultichainSelector(getMultichainCurrentChainId);
 
-  const { value: latestBalance } = useAsyncResult<
-    Numeric | undefined
-  >(async () => {
+  const nonEvmBalancesByAccountId = useMultichainSelector(
+    getMultichainBalances,
+  );
+  const nonEvmBalances = nonEvmBalancesByAccountId[id];
+
+  const value = useAsyncResult<Numeric | undefined>(async () => {
     if (
       token?.address &&
       // TODO check whether chainId is EVM when MultichainNetworkController is integrated
@@ -42,8 +52,23 @@ const useLatestBalance = (
         chainId,
       );
     }
+
+    if (isCaipChainId(chainId) && token?.decimals) {
+      return Numeric.from(
+        nonEvmBalances?.[token.address]?.amount ?? token?.string,
+        10,
+      ).shiftedBy(-1 * token.decimals);
+    }
+
     return undefined;
-  }, [currentChainId, token?.address, selectedAddress]);
+  }, [
+    chainId,
+    currentChainId,
+    token,
+    selectedAddress,
+    global.ethereumProvider,
+    nonEvmBalances,
+  ]);
 
   if (token && !token.decimals) {
     throw new Error(
@@ -51,14 +76,13 @@ const useLatestBalance = (
     );
   }
 
-  const tokenDecimals = token?.decimals ? Number(token.decimals) : 1;
-
-  return {
-    balanceAmount:
-      token && latestBalance
-        ? calcTokenAmount(latestBalance.toString(), tokenDecimals)
+  return useMemo(
+    () =>
+      value?.value
+        ? calcTokenAmount(value.value.toString(), token?.decimals)
         : undefined,
-  };
+    [value.value, token?.decimals],
+  );
 };
 
 export default useLatestBalance;
--- ui/pages/bridge/prepare/bridge-input-group.tsx
+++ ui/pages/bridge/prepare/bridge-input-group.tsx
@@ -35,6 +35,8 @@ import { useCopyToClipboard } from '../../../hooks/useCopyToClipboard';
 import { MINUTE } from '../../../../shared/constants/time';
 import { getIntlLocale } from '../../../ducks/locale/locale';
 import { useIsMultichainSwap } from '../hooks/useIsMultichainSwap';
+import { useMultichainSelector } from '../../../hooks/useMultichainSelector';
+import { getMultichainCurrentChainId } from '../../../selectors/multichain';
 import { BridgeAssetPickerButton } from './components/bridge-asset-picker-button';
 
 const sanitizeAmountInput = (textToSanitize: string) => {
@@ -86,11 +88,9 @@ export const BridgeInputGroup = ({
   const currency = useSelector(getCurrentCurrency);
   const locale = useSelector(getIntlLocale);
 
-  const selectedChainId = networkProps?.network?.chainId;
-  const { balanceAmount } = useLatestBalance(token, selectedChainId);
-  // const currentChainId = useMultichainSelector(getMultichainCurrentChainId);
-  // const selectedChainId = networkProps?.network?.chainId ?? currentChainId;
-  // const { balanceAmount } = useLatestBalance(token, selectedChainId);
+  const currentChainId = useMultichainSelector(getMultichainCurrentChainId);
+  const selectedChainId = networkProps?.network?.chainId ?? currentChainId;
+  const balanceAmount = useLatestBalance(token, selectedChainId);
 
   const [, handleCopy] = useCopyToClipboard(MINUTE) as [
     boolean,
