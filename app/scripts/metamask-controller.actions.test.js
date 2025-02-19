f13d425d26292af38f1a615dd1ff607611e0368e
Monte Lai <monte.lai@consensys.net>
2025-02-19 15:16:09 +0800

    fix: skip and update test

--- app/scripts/metamask-controller.actions.test.js
+++ app/scripts/metamask-controller.actions.test.js
@@ -160,27 +160,24 @@ describe('MetaMaskController', function () {
   });
 
   describe('#addNewAccount', function () {
-    it('two parallel calls with same accountCount give same result', async function () {
-      await metamaskController.createNewVaultAndKeychain('test@123');
-      const [addNewAccountResult1, addNewAccountResult2] = await Promise.all([
-        metamaskController.addNewAccount(1),
-        metamaskController.addNewAccount(1),
-      ]);
-      expect(addNewAccountResult1).toStrictEqual(addNewAccountResult2);
-    });
-
-    it('two successive calls with same accountCount give same result', async function () {
-      await metamaskController.createNewVaultAndKeychain('test@123');
-      const addNewAccountResult1 = await metamaskController.addNewAccount(1);
-      const addNewAccountResult2 = await metamaskController.addNewAccount(1);
-      expect(addNewAccountResult1).toStrictEqual(addNewAccountResult2);
-    });
-
     it('two successive calls with different accountCount give different results', async function () {
       await metamaskController.createNewVaultAndKeychain('test@123');
       const addNewAccountResult1 = await metamaskController.addNewAccount(1);
       const addNewAccountResult2 = await metamaskController.addNewAccount(2);
       expect(addNewAccountResult1).not.toStrictEqual(addNewAccountResult2);
+    });
+
+    it('creates a total of two accounts', async function () {
+      await metamaskController.createNewVaultAndKeychain('test@123');
+      const existingNumberOfAccounts =
+        await metamaskController.keyringController.getAccounts();
+      await metamaskController.addNewAccount(1);
+      await metamaskController.addNewAccount(1);
+      const newNumberOfAccounts =
+        await metamaskController.keyringController.getAccounts();
+      expect(newNumberOfAccounts).toHaveLength(
+        existingNumberOfAccounts.length + 2,
+      );
     });
   });
 
