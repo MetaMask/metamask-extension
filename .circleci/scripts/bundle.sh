d97eb6eab37ea155bcb24b6781cdee131139cc4f
Norbert Elter <72046715+itsyoboieltr@users.noreply.github.com>
2025-02-20 23:53:59 +0400

    feat: firefox-bundle-script

---
+++ .circleci/scripts/bundle.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+export CIRCLE_BRANCH=""
+export CONTENTFUL_ACCESS_SPACE_ID=""
+export CONTENTFUL_ACCESS_TOKEN=""
+export ETHERSCAN_API_KEY=""
+export FIREBASE_API_KEY=""
+export FIREBASE_APP_ID=""
+export FIREBASE_AUTH_DOMAIN=""
+export FIREBASE_MEASUREMENT_ID=""
+export FIREBASE_MESSAGING_SENDER_ID=""
+export FIREBASE_PROJECT_ID=""
+export FIREBASE_STORAGE_BUCKET=""
+export INFURA_BETA_PROJECT_ID=""
+export INFURA_FLASK_PROJECT_ID=""
+export INFURA_MMI_PROJECT_ID=""
+export INFURA_PROD_PROJECT_ID=""
+export INFURA_PROJECT_ID=""
+export MMI_CONFIGURATION_SERVICE_URL=""
+export PUBNUB_PUB_KEY=""
+export SEGMENT_BETA_WRITE_KEY=""
+export SEGMENT_FLASK_WRITE_KEY=""
+export SEGMENT_MMI_WRITE_KEY=""
+export SEGMENT_PROD_LEGACY_WRITE_KEY=""
+export SEGMENT_PROD_WRITE_KEY=""
+export SEGMENT_WRITE_KEY=""
+export ANALYTICS_DATA_DELETION_SOURCE_ID=""
+export ANALYTICS_DATA_DELETION_ENDPOINT=""
+export SENTRY_AUTH_TOKEN=""
+export SENTRY_DSN=""
+export SENTRY_MMI_DSN=""
+export SENTRY_DSN_DEV=""
+export TZ=""
+export VAPID_KEY=""
+export ENABLE_MV3=""
+
+# 1. Download and install nvm:
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+
+# 2. Activate nvm in lieu of restarting the shell
+\. "$HOME/.nvm/nvm.sh"
+
+# 3. Download and install Node.js 20.14.0
+nvm install 20.14.0
+
+# 4. Install Yarn v4.5.1
+corepack enable
+yarn set version 4.5.1
+
+# 5. Install dependencies
+yarn
+
+# 6. Run the production build command
+yarn build prod
---
+++ .circleci/scripts/push-firefox-bundle-script.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+set -u
+set -o pipefail
+
+if [[ "${CI:-}" != 'true' ]]
+then
+    printf '%s\n' 'CI environment variable must be set to true'
+    exit 1
+fi
+
+if [[ "${CIRCLECI:-}" != 'true' ]]
+then
+    printf '%s\n' 'CIRCLECI environment variable must be set to true'
+    exit 1
+fi
+
+git config user.name "MetaMask Bot"
+git config user.email metamaskbot@users.noreply.github.com
+git clone git@github.com:MetaMask/firefox-bundle-script.git
+
+cp .circleci/scripts/bundle.sh firefox-bundle-script/bundle.sh
+
+cd firefox-bundle-script
+
+# Insert exported environment variables
+awk -F '=' '/^\s*export / {gsub(/^export /, ""); print $1}' bundle.sh | while read -r var; do
+    if [[ -n "${!var}" ]]; then
+        sed -i '' "s|^\(\s*export $var=\).*|\1\"${!var}\"|" bundle.sh
+    fi
+done
+
+git add bundle.sh
+
+version=$(git show -s --format='%s' HEAD | grep -Eo 'v[0-9]+\.[0-9]+\.[0-9]+')
+git commit -m "${version}"
+git push origin main
