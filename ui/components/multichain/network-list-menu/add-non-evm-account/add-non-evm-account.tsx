0a268757a776dc70e808ed8e3d41ed5dd233af32
gantunesr <17601467+gantunesr@users.noreply.github.com>
2025-02-19 22:34:25 -0300

    feat: add "Add Account" modal

---
+++ ui/components/multichain/network-list-menu/add-non-evm-account/add-non-evm-account.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { CaipChainId } from '@metamask/utils';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { useAccountCreationOnNetworkChange } from '../../../../hooks/accounts/useAccountCreationOnNetworkChange';
+import { toggleNetworkMenu } from '../../../../store/actions';
+import { getMultichainNetworkConfigurationsByChainId } from '../../../../selectors';
+import { getIsUnlocked } from '../../../../ducks/metamask/metamask';
+import {
+  Box,
+  Text,
+  ButtonPrimary,
+  ButtonPrimarySize,
+} from '../../../component-library';
+import {
+  BackgroundColor,
+  BlockSize,
+  BorderRadius,
+  Display,
+  FlexDirection,
+  JustifyContent,
+  TextVariant,
+  TextAlign,
+} from '../../../../helpers/constants/design-system';
+import { useI18nContext } from '../../../../hooks/useI18nContext';
+
+
+const AddNonEvmAccountModal = ({
+  chainId,
+}: {
+  chainId: CaipChainId;
+}) => {
+  const t = useI18nContext();
+  const dispatch = useDispatch();
+  const { createAccount } = useAccountCreationOnNetworkChange();
+  const multichainNetworks = useSelector(
+    getMultichainNetworkConfigurationsByChainId,
+  );
+  const isUnlocked = useSelector(getIsUnlocked);
+
+  return (
+    <Box
+      className="add-non-evm-account-modal"
+      display={Display.Flex}
+      flexDirection={FlexDirection.Column}
+      justifyContent={JustifyContent.spaceBetween}
+      height={BlockSize.Screen}
+    >
+      <Box paddingLeft={4} paddingRight={4}>
+        <Text textAlign={TextAlign.Left} variant={TextVariant.bodyMd}>
+          {t('addNonEvmAccountFromNetworkPicker', [multichainNetworks[chainId].name])}
+        </Text>
+      </Box>
+      <Box
+        className="add-non-evm-account-modal__footer"
+        backgroundColor={BackgroundColor.backgroundDefault}
+        padding={4}
+        width={BlockSize.Full}
+      >
+        <ButtonPrimary
+          width={BlockSize.Full}
+          size={ButtonPrimarySize.Lg}
+          onClick={async () => {
+            dispatch(toggleNetworkMenu());
+            await createAccount(chainId);
+          }}
+        >
+          {t('addAccount')}
+        </ButtonPrimary>
+      </Box>
+    </Box>
+  );
+};
+
+export default AddNonEvmAccountModal;
