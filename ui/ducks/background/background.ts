2f8f7a78ac0e60881d185615bd98ecb952966054
Jongsun Suh <jongsun.suh@icloud.com>
2025-02-21 11:08:07 -0500

    Add `background` Redux slice with types, constants

---
+++ ui/ducks/background/background.ts
@@ -0,0 +1,34 @@
+/* eslint-disable jsdoc/require-param */
+import type { AnyAction } from 'redux';
+import * as actionConstants from '../../store/actionConstants';
+import { BackgroundStateProxy } from '../../../shared/types/background';
+import { initialBackgroundState } from './constants';
+
+export type BackgroundSliceState = {
+  background: BackgroundStateProxy;
+};
+
+/**
+ * Temporary types for this slice so that inference of MetaMask state tree can
+ * occur
+ *
+ * @param state - State
+ * @param action
+ * @returns
+ */
+export default function reduceBackground(
+  state: BackgroundSliceState['background'],
+  action: AnyAction,
+): BackgroundStateProxy {
+  const backgroundState = {
+    ...initialBackgroundState,
+    ...state,
+  };
+  switch (action.type) {
+    case actionConstants.UPDATE_METAMASK_STATE:
+      return { ...backgroundState, ...action.value };
+
+    default:
+      return backgroundState;
+  }
+}
