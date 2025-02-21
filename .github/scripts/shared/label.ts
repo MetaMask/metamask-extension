60cf1c5c63690807d21325fb3ac9ab75542d508d
Desi McAdam <desi.mcadam@gmail.com>
2025-02-21 13:15:54 -0700

    Auto add needs-triage label on bug open

--- .github/scripts/shared/label.ts
+++ .github/scripts/shared/label.ts
@@ -27,6 +27,12 @@ export const externalContributorLabel: Label = {
   description: 'Issue or PR created by user outside org',
 };
 
+export const needsTriageLabel: Label = {
+  name: 'needs-triage',
+  color: '#FA8072',
+  description: 'Issue needs to be triaged',
+};
+
 export const flakyTestsLabel: Label = {
   name: 'flaky tests',
   color: 'BE564E',
