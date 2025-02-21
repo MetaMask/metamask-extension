84bad487e6f90dbe6705899e9592f5c463758c3e
Matthew Walsh <matthew.walsh@consensys.net>
2025-02-21 23:19:15 +0000

    Add unit tests for isSpendingCapUnlimited

--- ui/pages/confirmations/components/confirm/info/approve/hooks/use-approve-token-simulation.test.ts
+++ ui/pages/confirmations/components/confirm/info/approve/hooks/use-approve-token-simulation.test.ts
@@ -2,13 +2,35 @@ import { TransactionMeta } from '@metamask/transaction-controller';
 import { renderHookWithConfirmContextProvider } from '../../../../../../../../test/lib/confirmations/render-helpers';
 import { genUnapprovedApproveConfirmation } from '../../../../../../../../test/data/confirmations/token-approve';
 import { getMockConfirmStateForTransaction } from '../../../../../../../../test/data/confirmations/helper';
-import { useApproveTokenSimulation } from './use-approve-token-simulation';
+import { TOKEN_VALUE_UNLIMITED_THRESHOLD } from '../../shared/constants';
+import {
+  isSpendingCapUnlimited,
+  useApproveTokenSimulation,
+} from './use-approve-token-simulation';
 import { useIsNFT } from './use-is-nft';
 
 jest.mock('./use-is-nft', () => ({
   ...jest.requireActual('./use-is-nft'),
   useIsNFT: jest.fn(),
 }));
+
+describe('isSpendingCapUnlimited', () => {
+  it('returns true if spending cap is equal to threshold', () => {
+    expect(isSpendingCapUnlimited(TOKEN_VALUE_UNLIMITED_THRESHOLD)).toBe(true);
+  });
+
+  it('returns true if spending cap is greater than threshold', () => {
+    expect(isSpendingCapUnlimited(TOKEN_VALUE_UNLIMITED_THRESHOLD + 1)).toBe(
+      true,
+    );
+  });
+
+  it('returns false if spending cap is less than threshold', () => {
+    expect(isSpendingCapUnlimited(TOKEN_VALUE_UNLIMITED_THRESHOLD - 1)).toBe(
+      false,
+    );
+  });
+});
 
 describe('useApproveTokenSimulation', () => {
   beforeEach(() => {
