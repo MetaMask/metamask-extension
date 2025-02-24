c651ca37f505a99b8c7b1304909a059ed202456c
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2025-02-24 21:35:02 +0100

    Add missing hooks

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -6320,6 +6320,14 @@ export default class MetamaskController extends EventEmitter {
           'CronjobController:getBackgroundEvents',
           origin,
         ),
+        getNetworkConfigurationByChainId: this.controllerMessenger.call.bind(
+          this.controllerMessenger,
+          'NetworkController:getNetworkConfigurationByChainId',
+        ),
+        getNetworkClientById: this.controllerMessenger.call.bind(
+          this.controllerMessenger,
+          'NetworkController:getNetworkClientById',
+        ),
         ///: BEGIN:ONLY_INCLUDE_IF(keyring-snaps)
         handleSnapRpcRequest: (args) =>
           this.handleSnapRequest({ ...args, origin }),
