8d265e4220ff745b598ec044917036618c4801cd
Daniel Rocha <68558152+danroc@users.noreply.github.com>
2025-02-24 17:54:36 +0100

    chore: update order of params in JSDoc

--- shared/lib/accounts/accounts.ts
+++ shared/lib/accounts/accounts.ts
@@ -4,8 +4,8 @@ import { InternalAccount } from '@metamask/keyring-internal-api';
  * Get the next available account name based on the suggestion and the list of
  * accounts.
  *
- * @param nameSuggestion - The suggested name for the account
  * @param accounts - The list of accounts to check for name availability
+ * @param nameSuggestion - The suggested name for the account
  * @returns The next available account name based on the suggestion
  */
 export function getUniqueAccountName(
