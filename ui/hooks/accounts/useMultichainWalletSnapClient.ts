6f21f5a616d6af4fbc6a72c0585e1836989884f0
Antonio Regadas <antonio.regadas@consensys.net>
2025-02-25 11:10:57 +0000

    chore: package update and removes redundant calls

--- ui/hooks/accounts/useMultichainWalletSnapClient.ts
+++ ui/hooks/accounts/useMultichainWalletSnapClient.ts
@@ -57,14 +57,6 @@ export class MultichainWalletSnapClient {
     const account = await this.#client.createAccount({
       scope,
     });
-
-    // NOTE: The account's balance is going to be tracked automatically on when the new account
-    // will be added to the Snap bridge keyring (see `MultichainBalancesController:#handleOnAccountAdded`).
-    // However, the balance won't be fetched right away. To workaround this, we trigger the
-    // fetch explicitly here (since we are already in a `async` call) and wait for it to be updated!
-    await multichainUpdateBalance(account.id);
-    // TODO: Remove this and the above line once Snap account creation flow is async
-    await multichainUpdateTransactions(account.id);
   }
 }
 
