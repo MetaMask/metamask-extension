4cd525986e4b3cae69268567646271f4bbd4005b
jpuri <jyotipuri@gmail.com>
2025-02-26 10:44:13 +0530

    update

--- ui/pages/confirmations/components/confirm/info/shared/network-row/network-row.tsx
+++ ui/pages/confirmations/components/confirm/info/shared/network-row/network-row.tsx
@@ -4,7 +4,6 @@ import { useSelector } from 'react-redux';
 import { CHAIN_ID_TO_NETWORK_IMAGE_URL_MAP } from '../../../../../../../../shared/constants/network';
 import { getNetworkConfigurationsByChainId } from '../../../../../../../../shared/modules/selectors/networks';
 import { useI18nContext } from '../../../../../../../hooks/useI18nContext';
-import { ConfirmInfoRow } from '../../../../../../../components/app/confirm/info/row';
 import {
   AvatarNetwork,
   AvatarNetworkSize,
@@ -29,44 +28,45 @@ export const NetworkRow = ({
 }: {
   isShownWithAlertsOnly?: boolean;
 }) => {
-  {
-    const t = useI18nContext();
-    const { currentConfirmation } = useConfirmContext() ?? {};
-    const chainId = (currentConfirmation?.chainId as `0x${string}`) ?? '';
-    const networkConfigurations = useSelector(
-      getNetworkConfigurationsByChainId,
-    );
-    const networkName = chainId ? networkConfigurations[chainId]?.name : '';
+  const t = useI18nContext();
+  const { currentConfirmation } = useConfirmContext() ?? {};
+  const networkConfigurations = useSelector(getNetworkConfigurationsByChainId);
 
-    return (
-      <ConfirmInfoAlertRow
-        alertKey={RowAlertKey.Network}
-        ownerId={currentConfirmation.id}
-        label={t('transactionFlowNetwork')}
-        isShownWithAlertsOnly={isShownWithAlertsOnly}
-      >
-        <Box
-          display={Display.Flex}
-          alignItems={AlignItems.center}
-          flexWrap={FlexWrap.Wrap}
-          gap={2}
-          minWidth={BlockSize.Zero}
-        >
-          <AvatarNetwork
-            borderColor={BorderColor.backgroundDefault}
-            size={AvatarNetworkSize.Xs}
-            src={
-              CHAIN_ID_TO_NETWORK_IMAGE_URL_MAP[
-                chainId as keyof typeof CHAIN_ID_TO_NETWORK_IMAGE_URL_MAP
-              ]
-            }
-            name={networkName}
-          />
-          <Text variant={TextVariant.bodyMd} color={TextColor.textDefault}>
-            {networkName}
-          </Text>
-        </Box>
-      </ConfirmInfoAlertRow>
-    );
+  if (!currentConfirmation) {
+    return null;
   }
+
+  const chainId = (currentConfirmation?.chainId as `0x${string}`) ?? '';
+  const networkName = chainId ? networkConfigurations[chainId]?.name : '';
+
+  return (
+    <ConfirmInfoAlertRow
+      alertKey={RowAlertKey.Network}
+      ownerId={currentConfirmation.id}
+      label={t('transactionFlowNetwork')}
+      isShownWithAlertsOnly={isShownWithAlertsOnly}
+    >
+      <Box
+        display={Display.Flex}
+        alignItems={AlignItems.center}
+        flexWrap={FlexWrap.Wrap}
+        gap={2}
+        minWidth={BlockSize.Zero}
+      >
+        <AvatarNetwork
+          borderColor={BorderColor.backgroundDefault}
+          size={AvatarNetworkSize.Xs}
+          src={
+            CHAIN_ID_TO_NETWORK_IMAGE_URL_MAP[
+              chainId as keyof typeof CHAIN_ID_TO_NETWORK_IMAGE_URL_MAP
+            ]
+          }
+          name={networkName}
+        />
+        <Text variant={TextVariant.bodyMd} color={TextColor.textDefault}>
+          {networkName}
+        </Text>
+      </Box>
+    </ConfirmInfoAlertRow>
+  );
 };
