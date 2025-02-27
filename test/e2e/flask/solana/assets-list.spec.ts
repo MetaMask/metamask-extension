71ee27b5f57ae721dd84f0908057eb4e084799ef
Priya Narayanaswamy <priya.narayanaswamy@consensys.net>
2025-02-27 11:34:20 -0700

    test: add tests for network switching

--- test/e2e/flask/solana/assets-list.spec.ts
+++ test/e2e/flask/solana/assets-list.spec.ts
@@ -8,21 +8,10 @@ import SelectNetwork from '../../page-objects/pages/dialog/select-network';
 describe('Multichain Asset List', function (this: Suite) {
 
   const NETWORK_NAME_MAINNET = 'Ethereum Mainnet';
-  it('displays all assets in the asset list', async function () {
-    await withSolanaAccountSnap(
-      {
-        title: this.test?.fullTitle(),
-        showNativeTokenAsMainBalance: false,
-        mockCalls: true,
-      },
-      async (driver) => {
-        const assetListPage = new AssetListPage(driver);
-        await assetListPage.check_tokenItemNumber(3);
-      },
-    );
-  });
+  const LINEA_NAME_MAINNET = 'Linea Mainnet';
+  const SOLANA_NAME_MAINNET = 'Solana Mainnet';
 
-  it.only('displays assets for the current network when switching networks', async function () {
+  it('displays all assets in the asset list for the selected network', async function () {
     await withSolanaAccountSnap(
       {
         title: this.test?.fullTitle(),
@@ -34,9 +23,28 @@ describe('Multichain Asset List', function (this: Suite) {
         const headerNavbar = new HeaderNavbar(driver);
         const selectNetworkDialog = new SelectNetwork(driver);
         await assetListPage.check_tokenItemNumber(3);
+        await assetListPage.check_tokenExistsInList('Solana', '50');
+        await assetListPage.check_tokenExistsInList('PUMPKIN', '6');
+        await assetListPage.check_tokenExistsInList('seek16z', '5');
+
         await headerNavbar.clickSwitchNetworkDropDown();
         await selectNetworkDialog.selectNetworkName(NETWORK_NAME_MAINNET);
         await assetListPage.check_tokenItemNumber(2);
+
+        await assetListPage.check_tokenExistsInList('Ethereum', '25');
+        await assetListPage.check_tokenExistsInList('TST', '10');
+
+        await headerNavbar.clickSwitchNetworkDropDown();
+        await selectNetworkDialog.selectNetworkName(LINEA_NAME_MAINNET);
+        await assetListPage.check_tokenItemNumber(1);
+        await assetListPage.check_tokenExistsInList('Ethereum', '25');
+
+        await headerNavbar.clickSwitchNetworkDropDown();
+        await selectNetworkDialog.selectNetworkName(SOLANA_NAME_MAINNET);
+        await assetListPage.check_tokenItemNumber(3);
+        await assetListPage.check_tokenExistsInList('Solana', '50');
+        await assetListPage.check_tokenExistsInList('PUMPKIN', '6');
+        await assetListPage.check_tokenExistsInList('seek16z', '5');
       },
     );
   });
