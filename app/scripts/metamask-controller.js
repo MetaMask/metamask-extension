6dd9edf59cdd0c5bfccc06771d7b5d5020778f6b
Michele Esposito <michele@esposito.codes>
2025-02-28 13:28:50 +0100

    chore: bump `@metamask/keyring-controller`

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -24,7 +24,7 @@ import {
   KeyringController,
   KeyringTypes,
   keyringBuilderFactory,
-} from '@metamask/keyring-controller';
+} from '@metamask-previews/keyring-controller';
 import createFilterMiddleware from '@metamask/eth-json-rpc-filters';
 import createSubscriptionManager from '@metamask/eth-json-rpc-filters/subscriptionManager';
 import { JsonRpcError, providerErrors } from '@metamask/rpc-errors';
