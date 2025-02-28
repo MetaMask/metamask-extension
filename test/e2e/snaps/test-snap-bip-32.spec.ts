1ac31d94c2fa0638ebfa4e79b97ab14b3c50623c
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2025-02-28 13:48:51 +0100

    Refactor Snaps E2E tests to reduce duplication

--- test/e2e/snaps/test-snap-bip-32.spec.ts
+++ test/e2e/snaps/test-snap-bip-32.spec.ts
@@ -3,8 +3,6 @@ import { Driver } from '../webdriver/driver';
 import { loginWithoutBalanceValidation } from '../page-objects/flows/login.flow';
 import FixtureBuilder from '../fixture-builder';
 import { withFixtures, WINDOW_TITLES } from '../helpers';
-import SnapInstall from '../page-objects/pages/dialog/snap-install';
-import SnapInstallWarning from '../page-objects/pages/dialog/snap-install-warning';
 
 describe('Test Snap bip-32', function () {
   it('tests various functions of bip-32', async function () {
@@ -17,36 +15,10 @@ describe('Test Snap bip-32', function () {
         await loginWithoutBalanceValidation(driver);
 
         const testSnaps = new TestSnaps(driver);
-        const snapInstall = new SnapInstall(driver);
-        const snapInstallWarning = new SnapInstallWarning(driver);
 
-        // navigate to test snaps page and connect wait for page to load
+        // Navigate to `test-snaps` page, and install the Snap.
         await testSnaps.openPage();
-
-        // find, scroll and click connect to the bip32 snap
-        await testSnaps.clickConnectBip32();
-
-        // switch to metamask extension and click connect
-        await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
-        await snapInstall.check_pageIsLoaded();
-        await snapInstall.clickNextButton();
-
-        // click confirm
-        await snapInstall.clickConfirmButton();
-
-        // wait for permissions popover, click checkboxes and confirm
-        await snapInstallWarning.check_pageIsLoaded();
-        await snapInstallWarning.clickCheckboxPermission();
-        await snapInstallWarning.clickConfirmButton();
-
-        // wait for and click OK and wait for window to close
-        await snapInstall.clickNextButton();
-
-        // switch back to test-snaps window
-        await driver.switchToWindowWithTitle(WINDOW_TITLES.TestSnaps);
-
-        // wait for npm installation success
-        await testSnaps.waitForReconnectButton();
+        await testSnaps.installSnap('#connectbip32', true);
 
         // scroll to and click get public key
         await testSnaps.clickGetPublicKeyButton();
@@ -71,7 +43,7 @@ describe('Test Snap bip-32', function () {
 
         // hit 'approve' on the signature confirmation and wait for window to close
         await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
-        await snapInstall.clickApproveButton();
+        await testSnaps.snapInstall.clickApproveButton();
 
         // switch back to the test-snaps window
         await driver.switchToWindowWithTitle(WINDOW_TITLES.TestSnaps);
@@ -91,7 +63,7 @@ describe('Test Snap bip-32', function () {
         // switch to dialog window
         await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
 
-        await snapInstall.clickApproveButton();
+        await testSnaps.snapInstall.clickApproveButton();
 
         // switch back to test-snaps window
         await driver.switchToWindowWithTitle(WINDOW_TITLES.TestSnaps);
@@ -108,7 +80,7 @@ describe('Test Snap bip-32', function () {
         // switch to dialog window
         await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
 
-        await snapInstall.clickApproveButton();
+        await testSnaps.snapInstall.clickApproveButton();
 
         // switch back to test-snaps window
         await driver.switchToWindowWithTitle(WINDOW_TITLES.TestSnaps);
