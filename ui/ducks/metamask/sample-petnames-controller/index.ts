565136cc4e4c200983da701baabaf57e7f8b038e
Derek Brans <dbrans@gmail.com>
2025-02-28 11:29:44 -0500

    split up sample-petnames-duck

---
+++ ui/ducks/metamask/sample-petnames-controller/index.ts
@@ -0,0 +1,4 @@
+// Export all from individual files
+export * from './sample-petnames-controller-selectors';
+export * from './sample-petnames-controller-thunks';
+export * from './sample-petnames-controller-hooks';
