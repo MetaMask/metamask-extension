7db8aa9fbac107eeb92eef6037c53a62c9a1b890
httpjunkie <eric@httpjunkie.com>
2025-03-03 07:30:11 -0500

    feat: update STX Opt In Description to say "supported networks" rather than listing each network we support in the description.
    - add `stxOptInSupportedNetworksDescription` and remove `stxOptInEthereumBnbDescription` from locales (`en` & `en_GB`)
    
    - update references to this description in `ui/helpers/constants/settings.js`
    
    - update references to this description in `ui/pages/settings/advanced-tab/advanced-tab.component.js`

--- ui/pages/settings/advanced-tab/advanced-tab.component.js
+++ ui/pages/settings/advanced-tab/advanced-tab.component.js
@@ -243,7 +243,7 @@ export default class AdvancedTab extends PureComponent {
         <div className="settings-page__content-item">
           <span>{t('smartTransactions')}</span>
           <div className="settings-page__content-description">
-            {t('stxOptInEthereumBnbDescription', [learMoreLink])}
+            {t('stxOptInSupportedNetworksDescription', [learMoreLink])}
           </div>
         </div>
 
