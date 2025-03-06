affa6caaab8a2598724e63b6a6105f524a0dcb46
Matthew Walsh <matthew.walsh@consensys.net>
2025-03-06 23:43:42 +0000

    Add caveat
    Add token transfer.
    Use time as salt.

--- app/scripts/lib/transaction/hooks/delegation-7702-publish.ts
+++ app/scripts/lib/transaction/hooks/delegation-7702-publish.ts
@@ -1,14 +1,23 @@
 import { TransactionMeta } from '@metamask/transaction-controller';
 import { TransactionControllerInitMessenger } from '../../../controller-init/messengers/transaction-controller-messenger';
-import { Hex, createProjectLogger, hexToNumber } from '@metamask/utils';
+import {
+  Hex,
+  add0x,
+  createProjectLogger,
+  hexToNumber,
+  numberToHex,
+  remove0x,
+} from '@metamask/utils';
 import { Delegation } from '@metamask/signature-controller';
 
 const CHAIN_ID = '0x7a69';
 const DELEGATION_MANAGER_ADDRESS = '0x663F3ad617193148711d28f5334eE4Ed07016602';
 const SMART_CONTRACT_ADDRESS = '0x8438Ad1C834623CfF278AB6829a248E37C2D7E3f';
+const ENFORCER_ADDRESS = '0xBC9129Dc0487fc2E169941C75aABC539f208fb01';
 const RELAY_ACCOUNT = '0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC' as Hex;
+const TOKEN_ADDRESS = '0x8464135c8F25Da09e49BC8782676a84730C318bC' as Hex;
+const TOKEN_AMOUNT = 15000;
 const RELAY_URL = 'http://localhost:3000/';
-const SALT = 123;
 
 const ROOT_AUTHORITY =
   '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff';
@@ -52,12 +61,36 @@ export async function delegation7702PublishHook(
     log('Authorization signature', authorization);
   }
 
+  const tokenAmountPadded = add0x(
+    remove0x(numberToHex(TOKEN_AMOUNT)).padStart(64, '0'),
+  );
+
+  const enforcerTerms = add0x(
+    (
+      [
+        TOKEN_ADDRESS,
+        RELAY_ACCOUNT,
+        tokenAmountPadded,
+        txParams.to,
+        txParams.data ?? '0x',
+      ] as Hex[]
+    )
+      .map(remove0x)
+      .join(''),
+  );
+
   const delegation: Delegation = {
     authority: ROOT_AUTHORITY,
-    caveats: [],
+    caveats: [
+      {
+        enforcer: ENFORCER_ADDRESS,
+        terms: enforcerTerms,
+        args: '0x',
+      },
+    ],
     delegate: RELAY_ACCOUNT,
     delegator: from,
-    salt: SALT,
+    salt: new Date().getTime(),
   };
 
   log('Sign delegation', delegation);
