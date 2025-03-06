738bc0e20790fbbb9e68bceea1b2c7f27fa4480a
João <castrofjoao@gmail.com>
2025-03-06 18:46:13 +0000

    ci: sandbox gha

--- test/e2e/webdriver/chrome.js
+++ test/e2e/webdriver/chrome.js
@@ -61,7 +61,11 @@ class ChromeDriver {
       args.push('--disable-gpu');
     }
 
-    if (process.env.GITHUB_ACTION || isHeadless('SELENIUM')) {
+    if (process.env.GITHUB_ACTION) {
+      args.push('--no-sandbox');
+    }
+
+    if (isHeadless('SELENIUM') && !process.env.GITHUB_ACTION) {
       // TODO: Remove notice and consider non-experimental when results are consistent
       console.warn(
         '*** Running e2e tests in headless mode is experimental and some tests are known to fail for unknown reasons',
