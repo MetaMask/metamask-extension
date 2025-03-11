60c9a272607927d3fd971385d80c12de679debba
Matthew Walsh <matthew.walsh@consensys.net>
2025-03-11 00:46:33 +0000

    Fix linting

--- ui/store/actions/transaction-controller.ts
+++ ui/store/actions/transaction-controller.ts
@@ -1,5 +1,5 @@
-import { TransactionController } from "@metamask/transaction-controller";
-import { submitRequestToBackground } from "../background-connection";
+import { TransactionController } from '@metamask/transaction-controller';
+import { submitRequestToBackground } from '../background-connection';
 
 export async function updateAtomicBatchData(
   request: Parameters<TransactionController['updateAtomicBatchData']>[0],
