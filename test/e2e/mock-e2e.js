845aa8ea3424c070513298f8b2e5f59112282d9a
Micaela Estabillo <micaela.estabillo@consensys.net>
2025-03-11 14:02:16 -0700

    test: mock pricemulti call and revert MegaETH symbol change

--- test/e2e/mock-e2e.js
+++ test/e2e/mock-e2e.js
@@ -630,7 +630,7 @@ async function setupMocking(
 
   await server
     .forGet('https://min-api.cryptocompare.com/data/pricemulti')
-    .withQuery({ fsyms: 'ETH', tsyms: 'usd' })
+    .withQuery({ fsyms: 'ETH,MegaETH', tsyms: 'usd' })
     .thenCallback(() => {
       return {
         statusCode: 200,
