e1765db0c722bc8a2925f286d8f5be1c4da77e4e
seaona <seaona@proton.me>
2025-03-12 16:27:57 +0100

    sentinel

--- test/e2e/mock-e2e.js
+++ test/e2e/mock-e2e.js
@@ -308,17 +308,15 @@ async function setupMocking(
 
   // This endpoint returns metadata for "transaction simulation" supported networks.
   await server.forGet(`${TX_SENTINEL_URL}/network`).thenJson(200, {
-    1: {
-      name: 'Mainnet',
-      group: 'ethereum',
-      chainID: 1,
-      nativeCurrency: { name: 'ETH', symbol: 'ETH', decimals: 18 },
-      network: 'ethereum-mainnet',
-      explorer: 'https://etherscan.io',
-      confirmations: true,
-      smartTransactions: true,
-      hidden: false,
-    },
+    name: 'Mainnet',
+    group: 'ethereum',
+    chainID: 1,
+    nativeCurrency: { name: 'ETH', symbol: 'ETH', decimals: 18 },
+    network: 'ethereum-mainnet',
+    explorer: 'https://etherscan.io',
+    confirmations: true,
+    smartTransactions: true,
+    hidden: false,
   });
 
   await server
