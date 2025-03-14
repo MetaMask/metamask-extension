04af6311b65904010cf7677415cc03a52991bd69
Dan J Miller <danjm.com@gmail.com>
2025-03-14 17:45:52 -0230

    Add tests to catch basic functionality regressions

--- test/e2e/tests/privacy/basic-functionality.spec.ts
+++ test/e2e/tests/privacy/basic-functionality.spec.ts
@@ -12,16 +12,27 @@ import {
   importSRPOnboardingFlow,
 } from '../../page-objects/flows/onboarding.flow';
 
+const EXPECTED_REQUEST_URLS = [
+  'https://client-config.api.cx.metamask.io',
+  'https://on-ramp-content.api.cx.metamask.io',
+  'https://accounts.api.cx.metamask.io',
+  'https://phishing-detection.api.cx.metamask.io',
+  'https://token.api.cx.metamask.io',
+  'https://bridge.api.cx.metamask.io',
+  'https://client-side-detection.api.cx.metamask.io',
+  'https://swap.api.cx.metamask.io',
+  'https://authentication.api.cx.metamask.io',
+  'https://nft.api.cx.metamask.io',
+  'https://price.api.cx.metamask.io'
+];
+const EXPECTED_REQUEST_URLS_SET = new Set(EXPECTED_REQUEST_URLS);
+
+
 async function mockApis(mockServer: Mockttp) {
   return [
-    await mockServer.forGet(METAMASK_STALELIST_URL).thenCallback(() => {
-      return {
-        statusCode: 200,
-        json: [{ fakedata: true }],
-      };
-    }),
     await mockServer
-      .forGet('https://token.api.cx.metamask.io/tokens/1')
+      .forGet(/metamask.io/)
+      .times(15)
       .thenCallback(() => {
         return {
           statusCode: 200,
@@ -86,7 +97,7 @@ describe('MetaMask onboarding', function () {
     );
   });
 
-  it('should not prevent network requests to basic functionality endpoints when the basic functionality toggle is on', async function () {
+  it.only('should not prevent network requests to basic functionality endpoints when the basic functionality toggle is on', async function () {
     await withFixtures(
       {
         fixtures: new FixtureBuilder({ onboarding: true }).build(),
@@ -102,15 +113,19 @@ describe('MetaMask onboarding', function () {
         await switchToNetworkFlow(driver, 'Ethereum Mainnet');
         await homePage.refreshErc20TokenList();
 
+
         // intended delay to allow for network requests to complete
         await driver.delay(1000);
         for (const mockedEndpoint of mockedEndpoints) {
           const requests = await mockedEndpoint.getSeenRequests();
-          assert.equal(
-            requests.length,
-            1,
-            `${mockedEndpoint} should make requests after onboarding`,
-          );
+          const uniqueRequestURLs = new Set();
+          requests.forEach(({ url }: { url: string }) => uniqueRequestURLs.add((new URL(url)).origin))
+          console.log('uniqueRequestURLs', uniqueRequestURLs)
+          if (requests[0].url.match(/metamask.io/)) {
+            assert.deepEqual(uniqueRequestURLs, EXPECTED_REQUEST_URLS_SET);
+          } else if (requests[0].url.match(/cryptocompare.com/)) {
+            assert.deepEqual(uniqueRequestURLs, new Set(['https://min-api.cryptocompare.com']));
+          }
         }
       },
     );
