f636d021a0da00ea32b1e0bd23058b8e1806d6ba
NidhiKJha <menidhikjha@gmail.com>
2025-03-18 19:21:50 +0530

    updated ppom middleware

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -6762,6 +6762,7 @@ export default class MetamaskController extends EventEmitter {
         this.appStateController,
         this.accountsController,
         this.updateSecurityAlertResponse.bind(this),
+        this.selectedNetworkController,
       ),
     );
 
