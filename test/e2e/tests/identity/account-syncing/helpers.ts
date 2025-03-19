cb0025d5e864a4f6823d646d35836800fd86150d
Mathieu Artu <mathieu.artu@consensys.net>
2025-03-19 21:25:52 +0100

    Revert "feat: enable account syncing for MV2 (#31102)"
    This reverts commit fb835986a084acfef37e09c9f8ccac2cb09d9cd5.

--- test/e2e/tests/identity/account-syncing/helpers.ts
+++ test/e2e/tests/identity/account-syncing/helpers.ts
@@ -1,4 +1,6 @@
-export const IS_ACCOUNT_SYNCING_ENABLED = true;
+import { isManifestV3 } from '../../../../../shared/modules/mv3.utils';
+
+export const IS_ACCOUNT_SYNCING_ENABLED = isManifestV3;
 
 export type UserStorageAccount = {
   /**
