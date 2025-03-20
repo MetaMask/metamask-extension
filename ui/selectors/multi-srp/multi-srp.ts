1018e8d6590cb7c831168775f83ad496687f2df1
Monte Lai <monte.lai@consensys.net>
2025-03-20 17:23:20 +0800

    fix: update to just use account passed

--- ui/selectors/multi-srp/multi-srp.ts
+++ ui/selectors/multi-srp/multi-srp.ts
@@ -5,7 +5,6 @@ import {
   getMetaMaskHdKeyrings,
 } from '..';
 import { createDeepEqualSelector } from '../../../shared/modules/selectors/util';
-import { getSelectedInternalAccount } from '../accounts';
 import { getMultichainAggregatedBalance } from '../assets';
 import { isMultichainWalletSnap } from '../../../shared/lib/accounts/snaps';
 import { SnapId } from '@metamask/snaps-sdk';
@@ -47,51 +46,50 @@ const isPrimaryHdOrFirstPartySnapAccount = createDeepEqualSelector(
     if (isMultichainWalletSnap(account.metadata.snap?.id as SnapId)) {
       return true;
     }
+
     return false;
   },
 );
 
 export const getShouldShowSeedPhraseReminder = createDeepEqualSelector(
   (state) => state,
+  (_state, account) => account,
   getSelectedAccountTokensAcrossChains,
   getCrossChainMetaMaskCachedBalances,
   (state, account) => getMultichainAggregatedBalance(state, account),
-  isPrimaryHdOrFirstPartySnapAccount,
+  (state, account) => isPrimaryHdOrFirstPartySnapAccount(state, account),
   (
     state,
+    account,
     tokens: TokensByChainId,
     crossChainBalances: AccountsByChainId,
     aggregatedBalance,
     isPrimaryHdOrFirstPartySnapAccount,
   ) => {
     const { seedPhraseBackedUp, dismissSeedBackUpReminder } = state.metamask;
-    const selectedAccount = getSelectedInternalAccount(state);
 
     // If there is no account, we don't need to show the seed phrase reminder
     // or if the account is not a primary HD or first party snap account
     // It is assumed that imported srp accounts are backed up
-    if (!selectedAccount || !isPrimaryHdOrFirstPartySnapAccount) {
+    if (!account || !isPrimaryHdOrFirstPartySnapAccount) {
       return false;
     }
 
     let hasBalance = false;
 
-    if (isEvmAccountType(selectedAccount.type)) {
+    if (isEvmAccountType(account.type)) {
       hasBalance =
         Object.values(tokens).some((chains) => {
           return (
-            chains?.[selectedAccount.address as keyof typeof chains]?.balance &&
-            Number(
-              chains?.[selectedAccount.address as keyof typeof chains]?.balance,
-            ) > 0
+            chains?.[account.address as keyof typeof chains]?.balance &&
+            Number(chains?.[account.address as keyof typeof chains]?.balance) >
+              0
           );
         }) ||
         Object.values(crossChainBalances).some((chain) => {
           return (
-            chain?.[selectedAccount.address as keyof typeof chain]?.balance &&
-            Number(
-              chain?.[selectedAccount.address as keyof typeof chain]?.balance,
-            ) > 0
+            chain?.[account.address as keyof typeof chain]?.balance &&
+            Number(chain?.[account.address as keyof typeof chain]?.balance) > 0
           );
         });
     } else {
