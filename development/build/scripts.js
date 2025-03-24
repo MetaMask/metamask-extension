d2e8a02178cf8eae0d01fc766920551a52632864
Dario Anongba Varela <dario.anongba@gmail.com>
2025-03-24 12:27:37 +0100

    merge locale fix

--- development/build/scripts.js
+++ development/build/scripts.js
@@ -949,16 +949,17 @@ function setupBundlerDefaults(
     debug: true,
   });
 
+  // Ensure react-devtools is only included in dev builds
+  if (buildTarget !== BUILD_TARGETS.DEV) {
+    bundlerOpts.manualIgnore.push('react-devtools');
+    bundlerOpts.manualIgnore.push('remote-redux-devtools');
+  }
+
   if (buildTarget === BUILD_TARGETS.DIST) {
     bundlerOpts.debug = false; // Disable sourcemaps in prod/dist
   }
 
-  // Ensure react-devtools is only included in dev builds
-  if (buildTarget !== BUILD_TARGETS.DEV) {
-    bundlerOpts.manualIgnore.push('react-devtools', 'remote-redux-devtools');
-  }
-
-  // These dependencies use WASM which we cannot execute in accordance with our CSP
+  // This dependency uses WASM which we cannot execute in accordance with our CSP
   bundlerOpts.manualIgnore.push('@chainsafe/as-sha256');
 
   // Inject environment variables via node-style `process.env`
