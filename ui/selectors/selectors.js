0368d5a229bb73f13f44e6b1cc9370966e8d67c9
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2025-03-25 13:50:08 +0000

    feat: Remove petnames settings toggle (#31207)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    Removes the settings toggle for editable petnames. Removes the
    associated selector, action and UI hook, and adds a migration to remove
    the state property.
    
    While the privacy settings selection during the onboarding flow
    previously used this experimental setting, this PR changes it to use the
    security and privacy setting toggle related to third party requests
    instead.
    
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    2. What is the improvement/solution?
    -->
    
    [![Open in GitHub
    Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/31207?quickstart=1)
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/MetaMask-planning/issues/2992
    
    ## **Manual testing steps**
    
    1. Go to this page...
    2.
    3.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    <img width="1024" alt="Screenshot 2025-03-21 at 16 26 39"
    src="https://github.com/user-attachments/assets/7baec0cf-6524-47d7-9bf2-1904ea74f91c"
    />
    
    <img width="1024" alt="Screenshot 2025-03-21 at 12 28 00"
    src="https://github.com/user-attachments/assets/92bde762-fcf9-4158-a4ac-c6c39ca75a1e"
    />
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    <img width="1024" alt="Screenshot 2025-03-21 at 16 24 57"
    src="https://github.com/user-attachments/assets/e384c906-da7d-48ab-9a7d-aad7cd59a28d"
    />
    
    <img width="1024" alt="Screenshot 2025-03-21 at 12 49 02"
    src="https://github.com/user-attachments/assets/54c539d8-1aa9-4c2b-8364-ade020e33602"
    />
    
    
    ## **Pre-merge author checklist**
    
    - [ ] I've followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
    Extension Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- ui/selectors/selectors.js
+++ ui/selectors/selectors.js
@@ -1249,9 +1249,8 @@ export function getShowTestNetworks(state) {
   return Boolean(showTestNetworks);
 }
 
-export function getPetnamesEnabled(state) {
-  const { petnamesEnabled = true } = getPreferences(state);
-  return petnamesEnabled;
+export function getUseExternalNameSources(state) {
+  return state.metamask.useExternalNameSources;
 }
 
 export const getTokenSortConfig = createDeepEqualSelector(
