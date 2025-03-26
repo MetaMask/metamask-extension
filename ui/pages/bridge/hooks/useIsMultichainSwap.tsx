ba785781a15eed96324bea78fc76a9dc5659eeac
Micaela Estabillo <micaela.estabillo@consensys.net>
2025-03-26 12:25:43 -0700

    chore: redirect to unified swaps experience on reload

--- ui/pages/bridge/hooks/useIsMultichainSwap.tsx
+++ ui/pages/bridge/hooks/useIsMultichainSwap.tsx
@@ -1,17 +1,49 @@
 import { useSelector } from 'react-redux';
-import { useLocation } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
+import { useEffect, useMemo } from 'react';
 import { getMultichainIsSolana } from '../../../selectors/multichain';
+import { getIsSwap } from '../../../ducks/bridge/selectors';
 
 /*
  * This returns true if the url contains swaps=true and the current chain is solana
  * This is used to determine if the user navigated to the bridge page
  * using the Swap or Bridge button
+ *
+ * This appends the swaps=true query param to the url if it is not already set
+ * and the current quoteRequest is a solana swap
  */
 export const useIsMultichainSwap = () => {
-  const { search } = useLocation();
-  const searchParams = new URLSearchParams(search);
+  const { search, pathname } = useLocation();
+  const history = useHistory();
 
   const isSolana = useSelector(getMultichainIsSolana);
 
-  return searchParams.get('swaps') === 'true' && isSolana;
+  const isQuoteRequestSwap = useSelector(getIsSwap);
+
+  const searchParams = useMemo(() => new URLSearchParams(search), [search]);
+  const isSwapQueryParamSet = searchParams.get('swaps') === 'true';
+
+  useEffect(() => {
+    if (isQuoteRequestSwap && isSolana && !isSwapQueryParamSet) {
+      searchParams.set('swaps', 'true');
+      history.replace({
+        pathname,
+        search: searchParams.toString(),
+      });
+    }
+  }, [
+    isQuoteRequestSwap,
+    isSolana,
+    history,
+    search,
+    pathname,
+    isSwapQueryParamSet,
+    searchParams,
+  ]);
+
+  const isSolanaSwap = useMemo(() => {
+    return isSwapQueryParamSet && isSolana;
+  }, [isSolana, isSwapQueryParamSet]);
+
+  return isSolanaSwap;
 };
