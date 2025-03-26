9302575284c5d6f55035789fa76a0ac30fc4e6cb
Howard Braham <howrad@gmail.com>
2025-03-25 20:16:45 -0700

    ci: move benchmarks and bundle-size to GHA

--- .circleci/scripts/bundle-stats-commit.sh
+++ .circleci/scripts/bundle-stats-commit.sh
@@ -10,25 +10,15 @@ then
     exit 1
 fi
 
-if [[ "${CIRCLECI:-}" != 'true' ]]
+if [[ "${GITHUB_REF_NAME}" != "main" ]]
 then
-    printf '%s\n' 'CIRCLECI environment variable must be set to true'
-    exit 1
-fi
-
-if [[ "${CIRCLE_BRANCH}" != "main" ]]
-then
-    printf 'This is not main branch'
+    printf 'This is not the main branch'
     exit 0
 fi
 
-if [[ -z "${GITHUB_TOKEN:-}" ]]
+if [[ -z "${EXTENSION_BUNDLESIZE_STATS_TOKEN:-}" ]]
 then
-    printf '%s\n' 'GITHUB_TOKEN environment variable must be set'
-    exit 1
-elif [[ -z "${GITHUB_TOKEN_USER:-}" ]]
-then
-    printf '%s\n' 'GITHUB_TOKEN_USER environment variable must be set'
+    printf '%s\n' 'EXTENSION_BUNDLESIZE_STATS_TOKEN environment variable must be set'
     exit 1
 fi
 
@@ -38,7 +28,7 @@ git config --global user.email "metamaskbot@users.noreply.github.com"
 
 git config --global user.name "MetaMask Bot"
 
-git clone --depth=1 git@github.com:MetaMask/extension_bundlesize_stats.git temp
+git clone --depth 1 https://github.com/MetaMask/extension_bundlesize_stats.git temp
 
 BUNDLE_SIZE_FILE="test-artifacts/chrome/bundle_size_stats.json"
 STATS_FILE="temp/stats/bundle_size_data.json"
@@ -54,13 +44,13 @@ jq . "$STATS_FILE" > /dev/null || { echo "Error: Existing stats JSON is invalid"
 jq . "$BUNDLE_SIZE_FILE" > /dev/null || { echo "Error: New bundle size JSON is invalid"; exit 1; }
 
 # Check if the SHA already exists in the stats file
-if jq -e "has(\"$CIRCLE_SHA1\")" "$STATS_FILE" > /dev/null; then
-    echo "SHA $CIRCLE_SHA1 already exists in stats file. No new commit needed."
+if jq -e "has(\"$GITHUB_SHA\")" "$STATS_FILE" > /dev/null; then
+    echo "SHA $GITHUB_SHA already exists in stats file. No new commit needed."
     exit 0
 fi
 
 # Append new bundle size data correctly using jq
-jq --arg sha "$CIRCLE_SHA1" --argjson data "$(cat "$BUNDLE_SIZE_FILE")" \
+jq --arg sha "$GITHUB_SHA" --argjson data "$(cat "$BUNDLE_SIZE_FILE")" \
    '. + {($sha): $data}' "$STATS_FILE" > "$TEMP_FILE"
 
 # Overwrite the original JSON file with the corrected version
@@ -71,10 +61,10 @@ cd temp
 # Only add the JSON file
 git add stats/bundle_size_data.json
 
-git commit --message "Adding bundle size at commit: ${CIRCLE_SHA1}"
+git commit --message "Adding bundle size at commit: ${GITHUB_SHA}"
 
-repo_slug="$CIRCLE_PROJECT_USERNAME/extension_bundlesize_stats"
-git push "https://$GITHUB_TOKEN_USER:$GITHUB_TOKEN@github.com/$repo_slug" main
+repo_slug="$GITHUB_REPOSITORY_OWNER/extension_bundlesize_stats"
+git push "https://metamaskbot:$EXTENSION_BUNDLESIZE_STATS_TOKEN@github.com/$repo_slug" main
 
 cd ..
 
