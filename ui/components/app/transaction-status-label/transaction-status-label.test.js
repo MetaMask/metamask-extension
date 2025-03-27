6e6fb81e22c35184c1efba8d0478ba7a07b9f41a
Derek Brans <dbrans@gmail.com>
2025-03-27 07:53:29 -0400

    test: add additional tests for TransactionStatusLabel for status tooltip

--- ui/components/app/transaction-status-label/transaction-status-label.test.js
+++ ui/components/app/transaction-status-label/transaction-status-label.test.js
@@ -78,4 +78,57 @@ describe('TransactionStatusLabel Component', () => {
     expect(screen.getByTestId('tooltip')).toBeInTheDocument();
     expect(screen.getByText(TransactionStatus.failed)).toBeInTheDocument();
   });
+
+  it('should map approved status to signing status', () => {
+    const props = {
+      status: TransactionStatus.approved,
+    };
+
+    render(<TransactionStatusLabel {...props} />);
+    expect(screen.getByText('signing')).toBeInTheDocument();
+  });
+
+  it('should map submitted status to pending when isEarliestNonce is true', () => {
+    const props = {
+      status: TransactionStatus.submitted,
+      isEarliestNonce: true,
+    };
+
+    render(<TransactionStatusLabel {...props} />);
+    expect(
+      screen.getByText(TransactionGroupStatus.pending),
+    ).toBeInTheDocument();
+  });
+
+  it('should map submitted status to queued when isEarliestNonce is false', () => {
+    const props = {
+      status: TransactionStatus.submitted,
+      isEarliestNonce: false,
+    };
+
+    render(<TransactionStatusLabel {...props} />);
+    expect(screen.getByText('queued')).toBeInTheDocument();
+  });
+
+  it('should display date for confirmed transactions when not statusOnly', () => {
+    const props = {
+      status: TransactionStatus.confirmed,
+      date: 'June 1',
+      statusOnly: false,
+    };
+
+    render(<TransactionStatusLabel {...props} />);
+    expect(screen.getByText('June 1')).toBeInTheDocument();
+  });
+
+  it('should display status text for confirmed transactions when statusOnly is true', () => {
+    const props = {
+      status: TransactionStatus.confirmed,
+      date: 'June 1',
+      statusOnly: true,
+    };
+
+    render(<TransactionStatusLabel {...props} />);
+    expect(screen.getByText(TransactionStatus.confirmed)).toBeInTheDocument();
+  });
 });
