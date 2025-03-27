d7038c830d6c6c1be8fee11bf2b3df55e5431fa9
Alex <adonesky@gmail.com>
2025-03-27 16:59:22 -0500

    Throw 5100 error when no requested scopes are supported in `wallet_createSession` call

--- app/scripts/lib/rpc-method-middleware/handlers/wallet-createSession/handler.test.ts
+++ app/scripts/lib/rpc-method-middleware/handlers/wallet-createSession/handler.test.ts
@@ -129,7 +129,13 @@ describe('wallet_createSession', () => {
       normalizedOptionalScopes: {},
     });
     MockMultichain.bucketScopes.mockReturnValue({
-      supportedScopes: {},
+      supportedScopes: {
+        'eip155:1': {
+          methods: [],
+          notifications: [],
+          accounts: ['eip155:1:0x1', 'eip155:1:0x2'],
+        },
+      },
       supportableScopes: {},
       unsupportableScopes: {},
     });
@@ -306,8 +312,28 @@ describe('wallet_createSession', () => {
     expect(isChainIdSupportedBody).toContain('findNetworkClientIdByChainId');
   });
 
+  it('throws an error when no scopes are supported', async () => {
+    const { handler, end } = createMockedHandler();
+    MockMultichain.bucketScopes
+      .mockReturnValueOnce({
+        supportedScopes: {},
+        supportableScopes: {},
+        unsupportableScopes: {},
+      })
+      .mockReturnValueOnce({
+        supportedScopes: {},
+        supportableScopes: {},
+        unsupportableScopes: {},
+      });
+    await handler(baseRequest);
+    expect(end).toHaveBeenCalledWith(
+      new JsonRpcError(5100, 'Requested scopes are not supported'),
+    );
+  });
+
   it('gets a list of evm accounts in the wallet', async () => {
     const { handler, listAccounts } = createMockedHandler();
+
     await handler(baseRequest);
 
     expect(listAccounts).toHaveBeenCalled();
@@ -384,12 +410,6 @@ describe('wallet_createSession', () => {
   it('emits the dapp viewed metrics event', async () => {
     MockUtil.shouldEmitDappViewedEvent.mockReturnValue(true);
     const { handler, sendMetrics } = createMockedHandler();
-
-    MockMultichain.bucketScopes.mockReturnValue({
-      supportedScopes: {},
-      supportableScopes: {},
-      unsupportableScopes: {},
-    });
     await handler(baseRequest);
 
     expect(sendMetrics).toHaveBeenCalledWith({
