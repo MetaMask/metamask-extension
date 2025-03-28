755ece78009caca0044a8172d9fc9e3a301235cd
Guillaume Roux <hello@guillaumerx.fr>
2025-03-28 12:25:05 +0100

    get pr back up to speed

--- ui/components/app/snaps/snap-ui-account-selector/snap-ui-account-selector.tsx
+++ ui/components/app/snaps/snap-ui-account-selector/snap-ui-account-selector.tsx
@@ -1,62 +1,26 @@
 import React, { FunctionComponent } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
-  CaipAccountId,
   CaipChainId,
   KnownCaipNamespace,
   parseCaipChainId,
 } from '@metamask/utils';
-import { InternalAccount } from '@metamask/keyring-internal-api';
 
-import { AccountSelectorValue, State } from '@metamask/snaps-sdk';
+import { AccountSelectorState, State } from '@metamask/snaps-sdk';
 import { EthScope } from '@metamask/keyring-api';
+import { createAccountList } from '@metamask/snaps-utils';
 import { SnapUISelector } from '../snap-ui-selector';
 import {
   getInternalAccounts,
-  getShowFiatInTestnets,
-  getUseBlockie,
+  getMetaMaskAccountsOrdered,
+  InternalAccountWithBalance,
 } from '../../../../selectors';
 
-import { TEST_NETWORK_IDS } from '../../../../../shared/constants/network';
-import { MultichainNetworks } from '../../../../../shared/constants/multichain/networks';
-import {
-  AlignItems,
-  BlockSize,
-  BorderColor,
-  Display,
-  FlexDirection,
-  TextAlign,
-  TextColor,
-  TextVariant,
-} from '../../../../helpers/constants/design-system';
-import {
-  AvatarAccount,
-  AvatarAccountSize,
-  AvatarAccountVariant,
-  AvatarToken,
-  AvatarTokenSize,
-  Box,
-  Text,
-} from '../../../component-library';
-import { shortenAddress } from '../../../../helpers/utils/util';
-import { normalizeSafeAddress } from '../../../../../app/scripts/lib/multichain/address';
-import UserPreferencedCurrencyDisplay from '../../user-preferenced-currency-display/user-preferenced-currency-display.component';
-import { PRIMARY } from '../../../../helpers/constants/common';
-import { useMultichainAccountBalances } from '../../../../hooks/snaps/useMultichainTotalCrossChainFiatBalance';
-import { AvatarGroup } from '../../../multichain';
 import { setSelectedInternalAccount } from '../../../../store/actions';
 import { useSnapInterfaceContext } from '../../../../contexts/snaps';
+import AccountListItem from '../../../multichain/account-list-item/account-list-item';
 
-export function createAddressList(
-  address: string,
-  scopes: CaipChainId[],
-): CaipAccountId[] {
-  return scopes.map((scope) => {
-    return `${scope}:${address}` as CaipAccountId;
-  });
-}
-
-export function createChainIdList(
+function createChainIdList(
   accountScopes: CaipChainId[],
   requestedChainIds?: CaipChainId[],
 ) {
@@ -83,117 +47,6 @@ export function createChainIdList(
   }, [] as CaipChainId[]);
 }
 
-export type SnapUIAccountSelectorOptionProps = {
-  account: InternalAccount;
-  chainIds?: CaipChainId[];
-};
-
-export const SnapUIAccountSelectorOption: FunctionComponent<
-  SnapUIAccountSelectorOptionProps
-> = ({ account, chainIds }) => {
-  const useBlockie = useSelector(getUseBlockie);
-  const showFiatInTestnets = useSelector(getShowFiatInTestnets);
-
-  const { totalFiatBalance, assets } = useMultichainAccountBalances(
-    account,
-    chainIds,
-  );
-
-  const assetsAvatars = assets.map((asset) => ({ avatarValue: asset.iconUrl }));
-
-  const isOnlyMainnet = assets.every((asset) => {
-    const { chainId } = asset;
-    const { namespace, reference } = parseCaipChainId(chainId);
-    if (namespace === KnownCaipNamespace.Eip155) {
-      return !(TEST_NETWORK_IDS as string[]).includes(
-        parseInt(reference, 16).toString(),
-      );
-    }
-
-    return (
-      chainId !== MultichainNetworks.BITCOIN_TESTNET &&
-      chainId !== MultichainNetworks.SOLANA_TESTNET &&
-      chainId !== MultichainNetworks.SOLANA_DEVNET
-    );
-  });
-
-  const shouldShowFiat =
-    isOnlyMainnet || (!isOnlyMainnet && showFiatInTestnets);
-
-  const amountToDisplay = shouldShowFiat
-    ? totalFiatBalance
-    : assets[0].string ?? '0';
-
-  return (
-    <Box
-      display={Display.Flex}
-      width={BlockSize.Full}
-      alignItems={AlignItems.center}
-    >
-      <AvatarAccount
-        borderColor={BorderColor.transparent}
-        size={AvatarAccountSize.Md}
-        address={account.address}
-        variant={
-          useBlockie
-            ? AvatarAccountVariant.Blockies
-            : AvatarAccountVariant.Jazzicon
-        }
-        marginInlineEnd={2}
-      />
-      <Box
-        display={Display.Flex}
-        flexDirection={FlexDirection.Column}
-        width={BlockSize.Full}
-        marginRight={2}
-      >
-        <Text variant={TextVariant.bodyMdMedium} ellipsis>
-          {account.metadata.name}
-        </Text>
-        <Text variant={TextVariant.bodySm} color={TextColor.textAlternative}>
-          {shortenAddress(normalizeSafeAddress(account.address))}
-        </Text>
-      </Box>
-      <Box
-        display={Display.Flex}
-        flexDirection={FlexDirection.Column}
-        height={BlockSize.Full}
-      >
-        <Box>
-          <Text
-            variant={TextVariant.bodySm}
-            color={TextColor.textAlternative}
-            textAlign={TextAlign.End}
-          >
-            <UserPreferencedCurrencyDisplay
-              account={account}
-              value={amountToDisplay}
-              type={PRIMARY}
-              showFiat={shouldShowFiat}
-              textProps={{ color: TextColor.textAlternative }}
-              ethNumberOfDecimals={3}
-              isAggregatedFiatOverviewBalance={true}
-              style={{ flexWrap: 'nowrap' }}
-            />
-          </Text>
-        </Box>
-        <Box style={{ alignSelf: 'flex-end' }}>
-          {assets.length > 1 && (
-            <AvatarGroup members={assetsAvatars} limit={3} />
-          )}
-          {assets.length === 1 && (
-            <AvatarToken
-              src={assets[0].iconUrl}
-              name={assets[0].symbol}
-              size={AvatarTokenSize.Xs}
-            />
-          )}
-        </Box>
-      </Box>
-    </Box>
-  );
-};
-
 export type SnapUIAccountSelectorProps = {
   name: string;
   label?: string;
@@ -210,7 +63,9 @@ export const SnapUIAccountSelector: FunctionComponent<
 > = ({ chainIds, switchSelectedAccount, hideExternalAccounts, ...props }) => {
   const { snapId } = useSnapInterfaceContext();
   const dispatch = useDispatch();
-  const accounts = useSelector(getInternalAccounts);
+  const accounts: InternalAccountWithBalance[] = useSelector(
+    getMetaMaskAccountsOrdered,
+  );
 
   const ownedAccounts = accounts.filter(
     (account) => account.metadata.snap?.id === snapId,
@@ -225,9 +80,10 @@ export const SnapUIAccountSelector: FunctionComponent<
   });
 
   const options = filteredAccounts.map((account) => ({
+    key: 'accountId',
     value: {
       accountId: account.id,
-      addresses: createAddressList(
+      addresses: createAccountList(
         account.address,
         createChainIdList(account.scopes, chainIds),
       ),
@@ -235,21 +91,19 @@ export const SnapUIAccountSelector: FunctionComponent<
     disabled: false,
   }));
 
-  const optionComponents = filteredAccounts.map((account) => (
-    <SnapUIAccountSelectorOption account={account} chainIds={chainIds} />
+  const optionComponents = filteredAccounts.map((account, index) => (
+    <AccountListItem
+      account={account}
+      selected={false}
+      key={index}
+      menuType="none"
+    />
   ));
-
-  const findSelectedOptionIndex = (selectedOptionValue: State | undefined) =>
-    options.findIndex(
-      (option) =>
-        option.value.accountId ===
-        (selectedOptionValue as AccountSelectorValue).accountId,
-    );
 
   const handleSelect = (value: State) => {
     if (switchSelectedAccount) {
       dispatch(
-        setSelectedInternalAccount((value as AccountSelectorValue).accountId),
+        setSelectedInternalAccount((value as AccountSelectorState).accountId),
       );
     }
   };
@@ -260,7 +114,6 @@ export const SnapUIAccountSelector: FunctionComponent<
       options={options}
       {...props}
       optionComponents={optionComponents}
-      findSelectedOptionIndex={findSelectedOptionIndex}
       onSelect={handleSelect}
     />
   );
