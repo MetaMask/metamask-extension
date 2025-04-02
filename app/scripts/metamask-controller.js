25f89ae325aadc2f2211372cef6dda4d6dc0e38a
Patryk Łucka <patryk.lucka@gmail.com>
2025-04-02 11:14:54 +0200

    Update app/scripts/metamask-controller.js
    Co-authored-by: Charly Chevalier <charly.chevalier@consensys.net>

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -4726,6 +4726,7 @@ export default class MetamaskController extends EventEmitter {
           index,
         );
 
+        // We stop discovering accounts if none got discovered for that index.
         if (discovered.length === 0) {
           break;
         }
