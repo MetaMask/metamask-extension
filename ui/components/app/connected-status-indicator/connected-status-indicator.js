ad77fda2e5793c263e8c4d92900c60b725ff4820
Alex Donesky <adonesky@gmail.com>
2025-04-02 12:07:31 -0500

    Update ui/components/app/connected-status-indicator/connected-status-indicator.js

--- ui/components/app/connected-status-indicator/connected-status-indicator.js
+++ ui/components/app/connected-status-indicator/connected-status-indicator.js
@@ -35,7 +35,6 @@ export default function ConnectedStatusIndicator({ onClick, disabled }) {
 
   const permittedAccounts = useSelector(getAllPermittedAccountsForCurrentTab);
 
-  console.log({ permittedAccounts, selectedAccount });
 
   const currentTabIsConnectedToSelectedAddress = permittedAccounts.some(
     (account) => {
