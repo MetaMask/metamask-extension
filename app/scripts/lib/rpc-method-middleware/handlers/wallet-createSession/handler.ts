5bd53f54df221d003f9821031f9701a61f0e6010
jiexi <jiexiluan@gmail.com>
2025-04-03 08:13:25 -0700

    Update app/scripts/lib/rpc-method-middleware/handlers/wallet-createSession/handler.ts
    Co-authored-by: Alex Donesky <adonesky@gmail.com>

--- app/scripts/lib/rpc-method-middleware/handlers/wallet-createSession/handler.ts
+++ app/scripts/lib/rpc-method-middleware/handlers/wallet-createSession/handler.ts
@@ -178,7 +178,7 @@ async function walletCreateSessionHandler(
       ...optionalAccounts,
     ]);
 
-    const supportedAccountAddresses = allAccountAddresses.filter(
+    const supportedRequestedAccountAddresses = allAccountAddresses.filter(
       (accountAddress) => {
         const {
           address,
