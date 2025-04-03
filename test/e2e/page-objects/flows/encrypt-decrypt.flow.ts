1289aab2a1e7f157304f8d165d80cfacd403a631
chloeYue <chloe.gao@consensys.net>
2025-04-03 15:19:27 +0200

    fix test

--- test/e2e/page-objects/flows/encrypt-decrypt.flow.ts
+++ test/e2e/page-objects/flows/encrypt-decrypt.flow.ts
@@ -1,6 +1,6 @@
 import { Driver } from '../../webdriver/driver';
 import { WINDOW_TITLES } from '../../helpers';
-import { DEFAULT_GANACHE_ETH_BALANCE_DEC } from '../../constants';
+import { DEFAULT_LOCAL_NODE_ETH_BALANCE_DEC } from '../../constants';
 import DecryptMessageConfirmation from '../pages/confirmations/redesign/decrypt-message-confirmation';
 import GetEncryptionKeyConfirmation from '../pages/confirmations/redesign/get-encryption-key-confirmation';
 import TestDapp from '../pages/test-dapp';
@@ -10,10 +10,9 @@ import TestDapp from '../pages/test-dapp';
  *
  * @param driver - The driver instance.
  * @param encryptionKey - The expected encryption key to display.
- * @param balanceValue - The balance value to check, default is 25 ETH.
+ * @param balanceValue - The balance value to check, default is DEFAULT_LOCAL_NODE_ETH_BALANCE_DEC.
  */
-// TODO: replace DEFAULT_GANACHE_ETH_BALANCE_DEC with default anvil balance value
-export async function getEncryptionKeyInDapp(driver: Driver, encryptionKey: string, balanceValue: string = DEFAULT_GANACHE_ETH_BALANCE_DEC + ' ETH') {
+export async function getEncryptionKeyInDapp(driver: Driver, encryptionKey: string, balanceValue: string = DEFAULT_LOCAL_NODE_ETH_BALANCE_DEC + ' ETH') {
   const testDapp = new TestDapp(driver);
   await testDapp.clickGetEncryptionKeyButton();
   await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
@@ -33,10 +32,9 @@ export async function getEncryptionKeyInDapp(driver: Driver, encryptionKey: stri
  *
  * @param driver - The driver instance.
  * @param message - The message to decrypt.
- * @param balanceValue - The balance value to check, default is 25 ETH.
+ * @param balanceValue - The balance value to check, default is DEFAULT_LOCAL_NODE_ETH_BALANCE_DEC.
  */
-// TODO: replace DEFAULT_GANACHE_ETH_BALANCE_DEC with default anvil balance value
-export async function decryptMessageAndVerifyResult(driver: Driver, message: string, balanceValue: string = DEFAULT_GANACHE_ETH_BALANCE_DEC + ' ETH') {
+export async function decryptMessageAndVerifyResult(driver: Driver, message: string, balanceValue: string = DEFAULT_LOCAL_NODE_ETH_BALANCE_DEC + ' ETH') {
   console.log('Decrypt message in test dapp and verify the result');
   const testDapp = new TestDapp(driver);
   await testDapp.clickDecryptButton();
