87ca46cce8f29698532744bc0b45243671090bdb
Kevin Bluer <kevin@bluer.com>
2025-04-03 15:19:22 -0500

    chore: switches the bottom sheet confirmation view to a modal

--- ui/pages/remote-mode/setup/hardware-wallet-confirm-modal/remote-mode-hardware-wallet-confirm.component.tsx
+++ ui/pages/remote-mode/setup/hardware-wallet-confirm-modal/remote-mode-hardware-wallet-confirm.component.tsx
@@ -1,21 +1,16 @@
 import React from 'react';
 import {
-  Box,
-  Button,
   Text,
-  Icon,
-  IconName,
 } from '../../../../components/component-library';
-import {
-  FontWeight,
-  TextVariant,
-  Display,
-  JustifyContent,
-  BlockSize,
-  BackgroundColor,
-} from '../../../../helpers/constants/design-system';
-import Card from '../../../../components/ui/card';
 
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from '../../../../components/component-library';
 export default function RemoteModeHardwareWalletConfirm({
   visible,
   onConfirm,
@@ -28,50 +23,26 @@ export default function RemoteModeHardwareWalletConfirm({
   onClose: () => void;
 }) {
 
-  const handleClose = () => {
-    onClose();
-  };
-
-  const handleBack = () => {
-    onBack();
-  };
-
   return (
-    <Card
-      display={visible ? Display.Block : Display.None}
-      style={{
-        position: 'absolute',
-        bottom: 0,
-        left: 0,
-        right: 0,
-        zIndex: 1000,
-        background: 'var(--color-background-default)',
-        padding: '16px',
-      }}
-      backgroundColor={BackgroundColor.backgroundAlternativeSoft}
-    >
-      <Box display={Display.Flex} justifyContent={JustifyContent.spaceBetween}>
-        <Icon name={IconName.ArrowLeft} onClick={handleBack} />
-        <Text variant={TextVariant.headingSm} fontWeight={FontWeight.Bold}>
-          Confirm
-        </Text>
-        <Icon name={IconName.Close} onClick={handleClose} />
-      </Box>
-      <Box marginTop={4} marginBottom={6}>
-        <Text>Prior to clicking confirm:</Text>
-        <ul style={{ listStyleType: 'disc', paddingLeft: '20px' }}>
-          <Text as="li">
-            Be sure your Ledger is plugged in and to select the Ethereum app.
-          </Text>
-          <Text as="li">
-            Enable "smart contract data" or "blind signing" on your Ledger
-            device.
-          </Text>
-        </ul>
-      </Box>
-      <Button onClick={onConfirm} width={BlockSize.Full}>
-        Confirm on hardware wallet
-      </Button>
-    </Card>
+    <Modal onClose={onClose} isOpen={visible}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader onClose={onClose}>Confirm</ModalHeader>
+        <ModalBody>
+          <Text paddingBottom={4}>Ready to confirm on your hardware wallet?</Text>
+          <Text paddingBottom={4}>Prior to clicking confirm:</Text>
+          <ul style={{ listStyleType: 'disc', paddingLeft: '20px' }}>
+            <Text as="li">
+              Be sure your Ledger is plugged in and to select the Ethereum app.
+            </Text>
+            <Text as="li">
+              Enable "smart contract data" or "blind signing" on your Ledger
+              device.
+            </Text>
+          </ul>
+        </ModalBody>
+        <ModalFooter onCancel={onClose} onSubmit={onConfirm} />
+      </ModalContent>
+    </Modal>
   );
 }
