23edf11a1b2f83d990864fc16555ee5d399fe7b6
Matthew Walsh <matthew.walsh@consensys.net>
2025-04-03 13:22:53 +0100

    Add metrics

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -7299,6 +7299,10 @@ export default class MetamaskController extends EventEmitter {
         this.metaMetricsController,
       ),
       // Other dependencies
+      getAccountBalance: (account, chainId) =>
+        this.accountTrackerController.state.accountsByChainId?.[chainId]?.[
+          account
+        ]?.balance,
       getAccountType: this.getAccountType.bind(this),
       getDeviceModel: this.getDeviceModel.bind(this),
       getHardwareTypeForMetric: this.getHardwareTypeForMetric.bind(this),
