83b237a47a587504e6c925b37f0aa51ddf1aff7d
Kevin Bluer <kevin@bluer.com>
2025-04-03 15:27:02 -0500

    chore: reverts footer component banner and removes redundant settings screen

--- ui/pages/home/flask/flask-home-footer.component.js
+++ ui/pages/home/flask/flask-home-footer.component.js
@@ -8,17 +8,11 @@ import {
   MetaMetricsEventName,
 } from '../../../../shared/constants/metametrics';
 import { MetaMetricsContext } from '../../../contexts/metametrics';
-import { Icon, IconName, IconSize, BannerBase, Box } from '../../../components/component-library';
-import { BackgroundColor } from '../../../helpers/constants/design-system';
-import Card from '../../../components/ui/card';
-import { REMOTE_ROUTE } from '../../../helpers/constants/routes';
-
-import { useHistory } from 'react-router-dom';
 
 const FlaskHomeFooter = () => {
   const t = useI18nContext();
   const trackEvent = useContext(MetaMetricsContext);
-  const history = useHistory();
+
   return (
     <>
       <a
@@ -52,20 +46,6 @@ const FlaskHomeFooter = () => {
       >
         {t('needHelpFeedback')}
       </a>
-      <Box padding={4}>
-        <Card backgroundColor={BackgroundColor.backgroundAlternativeSoft} padding={2}>
-          <BannerBase
-              title="Remote Mode is here!"
-              startAccessory={<Icon name={IconName.Info} size={IconSize.Lg} />}
-              actionButtonLabel="Get Started"
-              actionButtonOnClick={() => {
-                history.push(REMOTE_ROUTE);
-              }}
-            >
-              Access your hardware wallet without plugging it in
-          </BannerBase>
-        </Card>
-      </Box>
     </>
   );
 };
