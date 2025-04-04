a802c33324937b60c0a7f6386aded142540f9aed
jpuri <jyotipuri@gmail.com>
2025-04-04 10:50:22 +0530

    update

--- test/e2e/tests/confirmations/transactions/multiple-dapp-network-confirmation-routing.spec.ts
+++ test/e2e/tests/confirmations/transactions/multiple-dapp-network-confirmation-routing.spec.ts
@@ -4,12 +4,12 @@ import FixtureBuilder from '../../../fixture-builder';
 import {
   DAPP_ONE_URL,
   DAPP_URL,
-  largeDelayMs,
-  openDapp,
   unlockWallet,
   WINDOW_TITLES,
   withFixtures,
 } from '../../../helpers';
+import TestDapp from '../../../page-objects/pages/test-dapp';
+import ConnectAccountConfirmation from '../../../page-objects/pages/confirmations/redesign/connect-account-confirmation';
 
 describe('Routing confirmstions from Multiple Dapps and different networks', function () {
   it('Confirmation requests from different DAPPS and networks should be in same queue, it is possible to navigate the queue.', async function () {
@@ -38,116 +38,87 @@ describe('Routing confirmstions from Multiple Dapps and different networks', fun
       },
 
       async ({ driver }) => {
-        if (process.env.EVM_MULTICHAIN_ENABLED === 'true') {
-          await unlockWallet(driver);
+        await unlockWallet(driver);
 
-          // Open Dapp One
-          await openDapp(driver, undefined, DAPP_URL);
+        // Open Dapp One
+        const testDapp1 = new TestDapp(driver);
+        await testDapp1.openTestDappPage({ url: DAPP_URL });
+        // Connect to dapp 1
+        await testDapp1.clickConnectAccountButton();
 
-          // Connect to dapp 1
-          await driver.findClickableElement({ text: 'Connect', tag: 'button' });
-          await driver.clickElement('#connectButton');
+        await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
 
-          await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
+        const connectAccountConfirmation1 = new ConnectAccountConfirmation(
+          driver,
+        );
+        await connectAccountConfirmation1.check_pageIsLoaded();
+        await connectAccountConfirmation1.confirmConnect();
 
-          await driver.clickElement({
-            text: 'Connect',
-            tag: 'button',
-          });
+        // Wait for the first dapp's connect confirmation to disappear
+        await driver.switchToWindowWithTitle(
+          WINDOW_TITLES.ExtensionInFullScreenView,
+        );
+        await driver.waitUntilXWindowHandles(2);
 
-          await driver.switchToWindowWithUrl(DAPP_URL);
+        // Connect to dapp 2
+        const testDapp2 = new TestDapp(driver);
+        await testDapp2.openTestDappPage({ url: DAPP_ONE_URL });
+        await testDapp2.clickConnectAccountButton();
+        await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
+        const connectAccountConfirmation2 = new ConnectAccountConfirmation(
+          driver,
+        );
+        await connectAccountConfirmation2.check_pageIsLoaded();
+        await connectAccountConfirmation2.confirmConnect();
 
-          const switchEthereumChainRequest = JSON.stringify({
-            jsonrpc: '2.0',
-            method: 'wallet_switchEthereumChain',
-            params: [{ chainId: '0x53a' }],
-          });
+        // Switch network on DAPP 2
+        await driver.switchToWindowWithUrl(DAPP_ONE_URL);
+        await testDapp2.request('wallet_switchEthereumChain', [
+          { chainId: '0x53a' },
+        ]);
 
-          // Ensure Dapp One is on Localhost 8546
-          await driver.executeScript(
-            `window.ethereum.request(${switchEthereumChainRequest})`,
-          );
+        // Dapp 1 send 2 tx
+        await driver.switchToWindowWithUrl(DAPP_URL);
+        await testDapp1.clickSimpleSendButton();
+        await testDapp1.clickSimpleSendButton();
 
-          // Should auto switch without prompt since already approved via connect
+        // Dapp 2 send 2 tx
+        await driver.switchToWindowWithUrl(DAPP_ONE_URL);
+        await testDapp2.clickSimpleSendButton();
+        await testDapp2.clickSimpleSendButton();
 
-          await driver.switchToWindowWithTitle(
-            WINDOW_TITLES.ExtensionInFullScreenView,
-          );
+        // Dapp 1 send 1 more tx
+        await driver.switchToWindowWithUrl(DAPP_URL);
+        await testDapp1.clickSimpleSendButton();
 
-          // Wait for the first dapp's connect confirmation to disappear
-          await driver.waitUntilXWindowHandles(2);
+        await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
 
-          // Open Dapp Two
-          await openDapp(driver, undefined, DAPP_ONE_URL);
+        await driver.waitForSelector(
+          By.xpath("//p[normalize-space(.)='1 of 5']"),
+        );
 
-          // Connect to dapp 2
-          await driver.findClickableElement({ text: 'Connect', tag: 'button' });
-          await driver.clickElement('#connectButton');
+        await driver.findElement({
+          css: 'p',
+          text: 'Localhost 8546',
+        });
 
-          await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
+        await driver.clickElement(
+          '[data-testid="confirm-nav__next-confirmation"]',
+        );
+        await driver.clickElement(
+          '[data-testid="confirm-nav__next-confirmation"]',
+        );
 
-          await driver.clickElement({
-            text: 'Connect',
-            tag: 'button',
-          });
+        await driver.findElement({
+          css: 'p',
+          text: 'Localhost 8545',
+        });
 
-          // Dapp 1 send 2 tx
-          await driver.switchToWindowWithUrl(DAPP_URL);
-          await driver.findElement({
-            css: '[id="chainId"]',
-            text: '0x53a',
-          });
-          await driver.clickElement('#sendButton');
-          await driver.clickElement('#sendButton');
-
-          await driver.waitUntilXWindowHandles(4);
-
-          // Dapp 2 send 2 tx
-          await driver.switchToWindowWithUrl(DAPP_ONE_URL);
-          await driver.findElement({
-            css: '[id="chainId"]',
-            text: '0x53a',
-          });
-          await driver.clickElement('#sendButton');
-          await driver.clickElement('#sendButton');
-          // We cannot wait for the dialog, since it is already opened from before
-          await driver.delay(largeDelayMs);
-
-          // Dapp 1 send 1 tx
-          await driver.switchToWindowWithUrl(DAPP_URL);
-          await driver.findElement({
-            css: '[id="chainId"]',
-            text: '0x53a',
-          });
-          await driver.clickElement('#sendButton');
-          // We cannot switch directly, as the dialog is sometimes closed and re-opened
-          await driver.delay(largeDelayMs);
-          await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
-
-          await driver.waitForSelector(
-            By.xpath("//p[normalize-space(.)='1 of 5']"),
-          );
-
-          await driver.findElement({
-            css: 'p',
-            text: 'Localhost 8546',
-          });
-
-          await driver.clickElement(
-            '[data-testid="confirm-nav__next-confirmation"]',
-          );
-
-          await driver.findElement({
-            css: 'p',
-            text: 'Localhost 8546',
-          });
-
-          // Reject All Transactions
-          await driver.clickElementAndWaitForWindowToClose({
-            text: 'Reject all',
-            tag: 'button',
-          });
-        }
+        // Reject All Transactions
+        await driver.clickElementAndWaitForWindowToClose({
+          text: 'Reject all',
+          tag: 'button',
+        });
       },
     );
   });
