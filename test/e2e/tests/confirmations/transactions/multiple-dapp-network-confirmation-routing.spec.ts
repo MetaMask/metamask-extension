a0df7e2f14ff0322101dd767cf7e44d7a975b4ed
jpuri <jyotipuri@gmail.com>
2025-04-04 13:49:34 +0530

    update

--- test/e2e/tests/confirmations/transactions/multiple-dapp-network-confirmation-routing.spec.ts
+++ test/e2e/tests/confirmations/transactions/multiple-dapp-network-confirmation-routing.spec.ts
@@ -38,87 +38,89 @@ describe('Routing confirmstions from Multiple Dapps and different networks', fun
       },
 
       async ({ driver }) => {
-        await unlockWallet(driver);
+        if (process.env.EVM_MULTICHAIN_ENABLED === 'true') {
+          await unlockWallet(driver);
 
-        // Open Dapp One
-        const testDapp1 = new TestDapp(driver);
-        await testDapp1.openTestDappPage({ url: DAPP_URL });
-        // Connect to dapp 1
-        await testDapp1.clickConnectAccountButton();
+          // Open Dapp One
+          const testDapp1 = new TestDapp(driver);
+          await testDapp1.openTestDappPage({ url: DAPP_URL });
+          // Connect to dapp 1
+          await testDapp1.clickConnectAccountButton();
 
-        await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
+          await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
 
-        const connectAccountConfirmation1 = new ConnectAccountConfirmation(
-          driver,
-        );
-        await connectAccountConfirmation1.check_pageIsLoaded();
-        await connectAccountConfirmation1.confirmConnect();
+          const connectAccountConfirmation1 = new ConnectAccountConfirmation(
+            driver,
+          );
+          await connectAccountConfirmation1.check_pageIsLoaded();
+          await connectAccountConfirmation1.confirmConnect();
 
-        // Wait for the first dapp's connect confirmation to disappear
-        await driver.switchToWindowWithTitle(
-          WINDOW_TITLES.ExtensionInFullScreenView,
-        );
-        await driver.waitUntilXWindowHandles(2);
+          // Wait for the first dapp's connect confirmation to disappear
+          await driver.switchToWindowWithTitle(
+            WINDOW_TITLES.ExtensionInFullScreenView,
+          );
+          await driver.waitUntilXWindowHandles(2);
 
-        // Connect to dapp 2
-        const testDapp2 = new TestDapp(driver);
-        await testDapp2.openTestDappPage({ url: DAPP_ONE_URL });
-        await testDapp2.clickConnectAccountButton();
-        await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
-        const connectAccountConfirmation2 = new ConnectAccountConfirmation(
-          driver,
-        );
-        await connectAccountConfirmation2.check_pageIsLoaded();
-        await connectAccountConfirmation2.confirmConnect();
+          // Connect to dapp 2
+          const testDapp2 = new TestDapp(driver);
+          await testDapp2.openTestDappPage({ url: DAPP_ONE_URL });
+          await testDapp2.clickConnectAccountButton();
+          await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
+          const connectAccountConfirmation2 = new ConnectAccountConfirmation(
+            driver,
+          );
+          await connectAccountConfirmation2.check_pageIsLoaded();
+          await connectAccountConfirmation2.confirmConnect();
 
-        // Switch network on DAPP 2
-        await driver.switchToWindowWithUrl(DAPP_ONE_URL);
-        await testDapp2.request('wallet_switchEthereumChain', [
-          { chainId: '0x53a' },
-        ]);
+          // Switch network on DAPP 2
+          await driver.switchToWindowWithUrl(DAPP_ONE_URL);
+          await testDapp2.request('wallet_switchEthereumChain', [
+            { chainId: '0x53a' },
+          ]);
 
-        // Dapp 1 send 2 tx
-        await driver.switchToWindowWithUrl(DAPP_URL);
-        await testDapp1.clickSimpleSendButton();
-        await testDapp1.clickSimpleSendButton();
+          // Dapp 1 send 2 tx
+          await driver.switchToWindowWithUrl(DAPP_URL);
+          await testDapp1.clickSimpleSendButton();
+          await testDapp1.clickSimpleSendButton();
 
-        // Dapp 2 send 2 tx
-        await driver.switchToWindowWithUrl(DAPP_ONE_URL);
-        await testDapp2.clickSimpleSendButton();
-        await testDapp2.clickSimpleSendButton();
+          // Dapp 2 send 2 tx
+          await driver.switchToWindowWithUrl(DAPP_ONE_URL);
+          await testDapp2.clickSimpleSendButton();
+          await testDapp2.clickSimpleSendButton();
 
-        // Dapp 1 send 1 more tx
-        await driver.switchToWindowWithUrl(DAPP_URL);
-        await testDapp1.clickSimpleSendButton();
+          // Dapp 1 send 1 more tx
+          await driver.switchToWindowWithUrl(DAPP_URL);
+          await testDapp1.clickSimpleSendButton();
 
-        await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
+          await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
 
-        await driver.waitForSelector(
-          By.xpath("//p[normalize-space(.)='1 of 5']"),
-        );
+          await driver.waitForSelector(
+            By.xpath("//p[normalize-space(.)='1 of 5']"),
+          );
 
-        await driver.findElement({
-          css: 'p',
-          text: 'Localhost 8546',
-        });
+          await driver.findElement({
+            css: 'p',
+            text: 'Localhost 8546',
+          });
 
-        await driver.clickElement(
-          '[data-testid="confirm-nav__next-confirmation"]',
-        );
-        await driver.clickElement(
-          '[data-testid="confirm-nav__next-confirmation"]',
-        );
+          await driver.clickElement(
+            '[data-testid="confirm-nav__next-confirmation"]',
+          );
+          await driver.clickElement(
+            '[data-testid="confirm-nav__next-confirmation"]',
+          );
 
-        await driver.findElement({
-          css: 'p',
-          text: 'Localhost 8545',
-        });
+          await driver.findElement({
+            css: 'p',
+            text: 'Localhost 8545',
+          });
 
-        // Reject All Transactions
-        await driver.clickElementAndWaitForWindowToClose({
-          text: 'Reject all',
-          tag: 'button',
-        });
+          // Reject All Transactions
+          await driver.clickElementAndWaitForWindowToClose({
+            text: 'Reject all',
+            tag: 'button',
+          });
+        }
       },
     );
   });
