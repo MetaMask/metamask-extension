7de28434c53ed4be4b0fc0f45db8cabc085dbbbd
Matthew Walsh <matthew.walsh@consensys.net>
2025-04-06 14:29:43 +0100

    Add unit tests for useIsGaslessSupported

--- ui/pages/confirmations/hooks/useIsGaslessSupported.ts
+++ ui/pages/confirmations/hooks/useIsGaslessSupported.ts
@@ -30,8 +30,8 @@ export function useIsGaslessSupported() {
 
   return (
     isSmartTransaction ||
-    (atomicBatchChainSupport &&
-      (atomicBatchChainSupport.isSupported ||
-        !atomicBatchChainSupport.delegationAddress))
+    (Boolean(atomicBatchChainSupport) &&
+      (atomicBatchChainSupport?.isSupported ||
+        !atomicBatchChainSupport?.delegationAddress))
   );
 }
