7ba52bd06a6ee02766299bb965ea9ecc98d01513
jpuri <jyotipuri@gmail.com>
2025-04-07 21:24:40 +0530

    update

--- test/e2e/tests/request-queuing/watchAsset-switchChain-watchAsset.spec.js
+++ test/e2e/tests/request-queuing/watchAsset-switchChain-watchAsset.spec.js
@@ -10,9 +10,7 @@ const {
 } = require('../../helpers');
 
 describe('Request Queue WatchAsset -> SwitchChain -> WatchAsset', function () {
-  // todo: reenable this test once this issue is resolved: https://github.com/MetaMask/MetaMask-planning/issues/2406
-  // eslint-disable-next-line mocha/no-skipped-tests
-  it.skip('should not batch subsequent watchAsset token into first watchAsset confirmation with a switchChain in the middle', async function () {
+  it('should not batch subsequent watchAsset token into first watchAsset confirmation with a switchChain in the middle', async function () {
     const port = 8546;
     const chainId = 1338;
     await withFixtures(
@@ -50,7 +48,7 @@ describe('Request Queue WatchAsset -> SwitchChain -> WatchAsset', function () {
         // Wait for token address to populate in dapp
         await driver.switchToWindowWithTitle(WINDOW_TITLES.TestDApp);
         await driver.waitForSelector({
-          css: '#tokenAddresses',
+          css: '#erc20TokenAddresses',
           text: '0x581c3C1A2A4EBDE2A0Df29B5cf4c116E42945947',
         });
 
@@ -82,16 +80,8 @@ describe('Request Queue WatchAsset -> SwitchChain -> WatchAsset', function () {
           '.confirm-add-suggested-token__token-list-item',
         );
 
-        // Confirm only 1 token is present in suggested token list
-        assert.equal(multipleSuggestedtokens.length, 1);
-
-        await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
-
-        await driver.waitUntilXWindowHandles(2);
-
-        /**
-         * Confirm 2nd watchAsset confirmation doesn't pop section
-         */
+        // Confirm only 2 tokens are present in suggested token list
+        assert.equal(multipleSuggestedtokens.length, 2);
       },
     );
   });
