3c1d3c2833be8830f816cdd47688bf139de66f4a
sahar-fehri <sahar.fehri@consensys.net>
2025-04-08 10:30:05 +0200

    fix: fix test

---
+++ app/scripts/controller-init/assets/assets-contract-controller-init.test.ts
@@ -0,0 +1,48 @@
+import { Messenger } from '@metamask/base-controller';
+import { buildControllerInitRequestMock } from '../test/utils';
+import { ControllerInitRequest } from '../types';
+import {
+  AssetsContractControllerMessenger,
+  getAssetsContractControllerMessenger,
+} from '../messengers/assets';
+import { AssetsContractControllerInit } from './assets-contract-controller-init';
+import { AssetsContractController } from '@metamask/assets-controllers';
+jest.mock('@metamask/assets-controllers');
+
+function buildInitRequestMock(): jest.Mocked<
+  ControllerInitRequest<AssetsContractControllerMessenger>
+> {
+  const baseControllerMessenger = new Messenger();
+
+  return {
+    ...buildControllerInitRequestMock(),
+    controllerMessenger: getAssetsContractControllerMessenger(
+      baseControllerMessenger,
+    ),
+    initMessenger: undefined,
+  };
+}
+
+describe('AssetsContractControllerInit', () => {
+  const assetsContractControllerClassMock = jest.mocked(
+    AssetsContractController,
+  );
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('returns controller instance', () => {
+    const requestMock = buildInitRequestMock();
+    expect(AssetsContractControllerInit(requestMock).controller).toBeInstanceOf(
+      AssetsContractController,
+    );
+  });
+
+  it('initializes with correct messenger and state', () => {
+    const requestMock = buildInitRequestMock();
+    AssetsContractControllerInit(requestMock);
+
+    expect(assetsContractControllerClassMock).toHaveBeenCalled();
+  });
+});
