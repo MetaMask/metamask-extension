9d3afbf0b2ea19c4f497abd09baad4bd64b734aa
jpuri <jyotipuri@gmail.com>
2025-04-08 23:22:29 +0530

    update

--- ui/selectors/selectors.js
+++ ui/selectors/selectors.js
@@ -1985,11 +1985,6 @@ export function getShouldShowAggregatedBalancePopover(state) {
   return shouldShowAggregatedBalancePopover;
 }
 
-export function getDismissSmartAccountSuggestionEnabled(state) {
-  const { dismissSmartAccountSuggestionEnabled } = getPreferences(state);
-  return dismissSmartAccountSuggestionEnabled;
-}
-
 export const getConnectedSnapsList = createDeepEqualSelector(
   getSnapsList,
   (snapsData) => {
