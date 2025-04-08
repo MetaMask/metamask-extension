3c38621e6de66d12755102be2c27f2cf58e4cf13
Shane Terence Odlum <shane.odlum@consensys.net>
2025-04-08 12:10:00 +0100

    chore: lint

--- app/scripts/lib/transaction/institutional-snap/InstitutionalSnapController.ts
+++ app/scripts/lib/transaction/institutional-snap/InstitutionalSnapController.ts
@@ -5,19 +5,17 @@ import {
   TransactionStatus,
 } from '@metamask/transaction-controller';
 import type { HandleSnapRequest } from '@metamask/snaps-controllers';
-import { SnapId } from '@metamask/snaps-sdk';
 import { HandlerType } from '@metamask/snaps-utils';
 import { BaseController, RestrictedMessenger } from '@metamask/base-controller';
 import { AccountsControllerGetAccountByAddressAction } from '@metamask/accounts-controller';
 import { ORIGIN_METAMASK } from '@metamask/controller-utils';
+import { INSTITUTIONAL_WALLET_SNAP_ID } from '../../../../../shared/lib/accounts/institutional-wallet-snap';
 import {
   InstitutionalSnapRequestSearchParameters,
   InstitutionalSnapResponse,
 } from './institutional-snap-controller.types';
-import { INSTITUTIONAL_WALLET_SNAP_ID } from '../../../../../shared/lib/accounts/institutional-wallet-snap';
 
-
-const SNAP_ID = InstitutionalWalletSnap.snapId as SnapId;
+const SNAP_ID = INSTITUTIONAL_WALLET_SNAP_ID;
 
 const controllerName = 'InstitutionalSnapController';
 
