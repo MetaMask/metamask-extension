09985c877934106b7bb7c2aabb01f000dffc346a
Maggie <magdalena.mladin@consensys.net>
2025-04-09 16:50:47 +0300

    Updates to insufficient-funds POM

--- test/e2e/page-objects/pages/confirmations/redesign/alert-modal.ts
+++ test/e2e/page-objects/pages/confirmations/redesign/alert-modal.ts
@@ -18,25 +18,6 @@ class AlertModal {
   constructor(driver: Driver) {
     this.driver = driver;
   }
-  async check_pageIsLoaded(): Promise<void> {
-    try {
-      await this.driver.waitForSelector([
-        this.inlineAlertButton,
-      ]);
-    } catch (e) {
-      console.log('Timeout while waiting for inline-alert button', e);
-      throw e;
-    }
-    console.log('in-line alert is loaded');
-  }
-
-  async waitForAlert(): Promise<void> {
-    await this.driver.waitForSelector(this.inlineAlertButton);
-  }
-
-  async waitForInsufficientBalanceAlert(): Promise<void> {
-    await this.driver.waitForSelector(this.insufficientFundsAlert);
-  }
 
   async clickAlertModalButton(): Promise<void> {
     await this.driver.clickElement(this.confirmAlertButton);
