ab8459eb1ad09aa1096c67227ac2fbeb091a8da6
Brian Nguyen <brianacnguyen@gmail.com>
2025-04-09 10:47:00 -0700

    Updated snapshots from buttonshape change

--- ui/components/component-library/button-base/button-base.tsx
+++ ui/components/component-library/button-base/button-base.tsx
@@ -78,7 +78,7 @@ export const ButtonBase: ButtonBaseComponent = React.forwardRef(
         display={Display.InlineFlex}
         justifyContent={JustifyContent.center}
         alignItems={AlignItems.center}
-        borderRadius={BorderRadius.pill}
+        borderRadius={BorderRadius.XL}
         {...(tagProps as TextProps<C>)}
       >
         {startIconName && (
