88e6b0404485b9b1635c7d83b698080523a2543c
OGPoyraz <omergoktugpoyraz@gmail.com>
2025-04-09 13:42:03 +0200

    Update transaction controller version

--- app/scripts/controller-init/confirmations/transaction-controller-init.ts
+++ app/scripts/controller-init/confirmations/transaction-controller-init.ts
@@ -69,7 +69,6 @@ export const TransactionControllerInit: ControllerInitFunction<
   } = getControllers(request);
 
   const controller: TransactionController = new TransactionController({
-    enableTxParamsGasFeeUpdates: true,
     getCurrentNetworkEIP1559Compatibility: () =>
       // @ts-expect-error Controller type does not support undefined return value
       initMessenger.call('NetworkController:getEIP1559Compatibility'),
@@ -103,6 +102,7 @@ export const TransactionControllerInit: ControllerInitFunction<
       queryEntireHistory: false,
       updateTransactions: false,
     },
+    isAutomaticGasFeeUpdateEnabled: () => true,
     isFirstTimeInteractionEnabled: () =>
       preferencesController().state.securityAlertsEnabled,
     isSimulationEnabled: () =>
