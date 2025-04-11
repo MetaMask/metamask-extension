7db980d83f4aa3e84fb5221d4a440a2c3e8619af
Shane T <shane.odlum@consensys.net>
2025-04-11 14:54:19 +0100

    (cherry-pick) chore: enable multi-srp (#31849)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    2. What is the improvement/solution?
    -->
    
    [![Open in GitHub
    Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/31849?quickstart=1)
    
    ## **Related issues**
    
    Fixes:
    
    ## **Manual testing steps**
    
    1. Go to this page...
    2.
    3.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [ ] I've followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
    Extension Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    Co-authored-by: Patryk Łucka <5708018+PatrykLucka@users.noreply.github.com>

--- ui/components/app/toast-master/utils.ts
+++ ui/components/app/toast-master/utils.ts
@@ -1,9 +1,7 @@
 import { PayloadAction } from '@reduxjs/toolkit';
 import { ReactFragment } from 'react';
 import {
-  ///: BEGIN:ONLY_INCLUDE_IF(multi-srp)
   SET_SHOW_NEW_SRP_ADDED_TOAST,
-  ///: END:ONLY_INCLUDE_IF
   SHOW_NFT_DETECTION_ENABLEMENT_TOAST,
 } from '../../../store/actionConstants';
 import { submitRequestToBackground } from '../../../store/background-connection';
@@ -73,11 +71,9 @@ export function submitRequestToBackgroundAndCatch(
   });
 }
 
-///: BEGIN:ONLY_INCLUDE_IF(multi-srp)
 export function setShowNewSrpAddedToast(value: boolean) {
   return {
     type: SET_SHOW_NEW_SRP_ADDED_TOAST,
     payload: value,
   };
 }
-///: END:ONLY_INCLUDE_IF
