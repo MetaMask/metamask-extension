7d86c65bdae967c5f3d0451b5ff61b2931309a77
Frederik Bolding <frederik.bolding@gmail.com>
2025-04-11 12:25:20 +0200

    Fix E2E again

--- test/e2e/snaps/test-snap-managestate.spec.js
+++ test/e2e/snaps/test-snap-managestate.spec.js
@@ -41,9 +41,6 @@ describe('Test Snap manageState', function () {
           tag: 'button',
         });
 
-        // click and dismiss possible scroll element
-        await driver.clickElementSafe('[data-testid="snap-install-scroll"]');
-
         await driver.clickElement({
           text: 'Connect',
           tag: 'button',
@@ -51,6 +48,10 @@ describe('Test Snap manageState', function () {
 
         // wait for and click confirm
         await driver.waitForSelector({ text: 'Confirm' });
+
+        // click and dismiss possible scroll element
+        await driver.clickElementSafe('[data-testid="snap-install-scroll"]');
+
         await driver.clickElement({
           text: 'Confirm',
           tag: 'button',
@@ -189,9 +190,6 @@ describe('Test Snap manageState', function () {
           tag: 'button',
         });
 
-        // click and dismiss possible scroll element
-        await driver.clickElementSafe('[data-testid="snap-install-scroll"]');
-
         await driver.clickElement({
           text: 'Connect',
           tag: 'button',
@@ -199,6 +197,10 @@ describe('Test Snap manageState', function () {
 
         // wait for and click confirm
         await driver.waitForSelector({ text: 'Confirm' });
+
+        // click and dismiss possible scroll element
+        await driver.clickElementSafe('[data-testid="snap-install-scroll"]');
+
         await driver.clickElement({
           text: 'Confirm',
           tag: 'button',
