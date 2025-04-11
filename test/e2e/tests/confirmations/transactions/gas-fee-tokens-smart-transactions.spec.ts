fdf92ad3a488b22850f2723a372dd26b35ae9d50
Matthew Walsh <matthew.walsh@consensys.net>
2025-04-11 10:38:42 +0100

    Add failure test

--- test/e2e/tests/confirmations/transactions/gas-fee-tokens-smart-transactions.spec.ts
+++ test/e2e/tests/confirmations/transactions/gas-fee-tokens-smart-transactions.spec.ts
@@ -20,7 +20,7 @@ const TRANSACTION_HASH_2 =
   '0x62700f83ba1bbc29004bf7aef71ed0ea735de4fd59861b4235200d8fa028281f';
 
 describe('Gas Fee Tokens - Smart Transactions', function (this: Suite) {
-  it('confirms two transactions', async function () {
+  it('confirms two transactions if successful', async function () {
     await withFixtures(
       {
         dapp: true,
@@ -34,10 +34,9 @@ describe('Gas Fee Tokens - Smart Transactions', function (this: Suite) {
         },
         testSpecificMock: (mockServer: MockttpServer) => {
           mockSimulationResponse(mockServer);
-          mockSmartTransactionBatchRequests(mockServer, [
-            TRANSACTION_HASH,
-            TRANSACTION_HASH_2,
-          ]);
+          mockSmartTransactionBatchRequests(mockServer, {
+            transactionHashes: [TRANSACTION_HASH, TRANSACTION_HASH_2],
+          });
         },
         title: this.test?.fullTitle(),
       },
@@ -60,9 +59,10 @@ describe('Gas Fee Tokens - Smart Transactions', function (this: Suite) {
         await gasFeeTokenModal.check_Balance('USDC', '$5.00');
         await gasFeeTokenModal.clickToken('USDC');
 
-        await transactionConfirmation.check_selectedGasFeeTokenPill('USDC');
+        await transactionConfirmation.check_gasFeeSymbol('USDC');
         await transactionConfirmation.check_gasFeeFiat('$1.23');
-        await transactionConfirmation.check_gasFeeToken('1.23');
+        await transactionConfirmation.check_gasFee('1.23');
+        await transactionConfirmation.check_gasFeeTokenFee('$0.43');
         await transactionConfirmation.clickFooterConfirmButton();
 
         await driver.switchToWindowWithTitle(
@@ -74,6 +74,54 @@ describe('Gas Fee Tokens - Smart Transactions', function (this: Suite) {
 
         const activityListPage = new ActivityListPage(driver);
         await activityListPage.check_confirmedTxNumberDisplayedInActivity(2);
+      },
+    );
+  });
+
+  it('fails two transactions if error', async function () {
+    await withFixtures(
+      {
+        dapp: true,
+        fixtures: new FixtureBuilder({ inputChainId: CHAIN_IDS.MAINNET })
+          .withPermissionControllerConnectedToTestDapp()
+          .withPreferencesControllerSmartTransactionsOptedIn()
+          .withNetworkControllerOnMainnet()
+          .build(),
+        localNodeOptions: {
+          hardfork: 'london',
+        },
+        testSpecificMock: (mockServer: MockttpServer) => {
+          mockSimulationResponse(mockServer);
+          mockSmartTransactionBatchRequests(mockServer, {
+            transactionHashes: [TRANSACTION_HASH, TRANSACTION_HASH_2],
+            error: true,
+          });
+        },
+        title: this.test?.fullTitle(),
+      },
+      async ({ driver }: { driver: Driver; localNodes: Anvil }) => {
+        await unlockWallet(driver);
+        await createDappTransaction(driver);
+        await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
+
+        const transactionConfirmation = new TransactionConfirmation(driver);
+        await transactionConfirmation.clickGasFeeTokenPill();
+
+        const gasFeeTokenModal = new GasFeeTokenModal(driver);
+        await gasFeeTokenModal.clickToken('USDC');
+
+        await transactionConfirmation.check_gasFeeSymbol('USDC');
+        await transactionConfirmation.clickFooterConfirmButton();
+
+        await driver.switchToWindowWithTitle(
+          WINDOW_TITLES.ExtensionInFullScreenView,
+        );
+
+        const homepage = new HomePage(driver);
+        await homepage.goToActivityList();
+
+        const activityListPage = new ActivityListPage(driver);
+        await activityListPage.check_failedTxNumberDisplayedInActivity(2);
       },
     );
   });
