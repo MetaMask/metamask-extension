a4bc354314fc88aaa99abcb835926e63ace6f5b5
Norbert Elter <72046715+itsyoboieltr@users.noreply.github.com>
2025-04-15 00:35:17 +0400

    feat: separation of runs

--- .github/scripts/create-e2e-test-report.ts
+++ .github/scripts/create-e2e-test-report.ts
@@ -33,11 +33,26 @@ function formatTime(ms: number): string {
   return humanizer(ms);
 }
 
+/**
+ * Replaces HTML `<strong>` tags with ANSI escape codes to format
+ * text as bold in the console output.
+ */
+function consoleBold(str: string): string {
+  return str
+    .replaceAll('<strong>', '\x1b[1m')
+    .replaceAll('</strong>', '\x1b[0m');
+}
+
+interface TestRun {
+  name: string;
+  testSuites: TestSuite[];
+}
+
 interface TestSuite {
   name: string;
   job: {
-    name: string | null;
-    id: string | null;
+    name: string;
+    id: string;
   };
   path: string;
   date: Date;
@@ -46,20 +61,21 @@ interface TestSuite {
   failed: number;
   skipped: number;
   time: number;
-  testCases: (
-    | {
-        name: string;
-        time: number;
-        status: 'passed';
-      }
-    | {
-        name: string;
-        time: number;
-        status: 'failed';
-        error: string;
-      }
-  )[];
+  testCases: TestCase[];
 }
+
+type TestCase =
+  | {
+      name: string;
+      time: number;
+      status: 'passed';
+    }
+  | {
+      name: string;
+      time: number;
+      status: 'failed';
+      error: string;
+    };
 
 async function main() {
   const {
@@ -85,8 +101,8 @@ async function main() {
       };
 
   try {
+    const testRuns: TestRun[] = [];
     const filenames = await fs.readdir(TEST_RESULTS_PATH);
-    const testSuites: TestSuite[] = [];
 
     for (const filename of filenames) {
       const file = await fs.readFile(
@@ -96,32 +112,27 @@ async function main() {
       const results = await XML.parse(file);
       for (const suite of results.testsuites.testsuite || []) {
         if (!suite.testcase || !suite.$.file) continue;
-        const name = `${suite.$.name}`;
-        const fullPath = `${suite.$.file}`;
-        const testPath = fullPath.slice(fullPath.indexOf(`test${path.sep}`));
-        const date = new Date(suite.$.timestamp);
         const tests = +suite.$.tests;
-        const time = +suite.$.time * 1000; // convert to ms
         const failed = +suite.$.failures;
         const skipped = tests - suite.testcase.length;
         const passed = tests - failed - skipped;
         const testSuite: TestSuite = {
-          name,
-          path: testPath,
+          name: suite.$.name,
+          path: suite.$.file.slice(suite.$.file.indexOf(`test${path.sep}`)),
           job: {
-            name: suite.properties?.[0].property?.[0]?.$.value || null,
-            id: suite.properties?.[0].property?.[1]?.$.value || null,
+            name: suite.properties?.[0].property?.[0]?.$.value ?? '',
+            id: suite.properties?.[0].property?.[1]?.$.value ?? '',
           },
-          date,
+          date: new Date(suite.$.timestamp),
           tests,
           passed,
           failed,
           skipped,
-          time,
+          time: +suite.$.time * 1000, // convert to ms,
           testCases: [],
         };
-        for (const test of suite.testcase) {
-          const testCase: TestSuite['testCases'][number] = {
+        for (const test of suite.testcase || []) {
+          const testCase: TestCase = {
             name: test.$.name,
             time: +test.$.time * 1000, // convert to ms
             status: test.failure ? 'failed' : 'passed',
@@ -129,112 +140,134 @@ async function main() {
           };
           testSuite.testCases.push(testCase);
         }
-        testSuites.push(testSuite);
-      }
-    }
-
-    const deduped: { [path: string]: TestSuite } = {};
-    for (const suite of testSuites) {
-      const existing = deduped[suite.path];
-      // If there is a duplicate, we keep the suite with the latest date
-      if (!existing || existing.date < suite.date) {
-        deduped[suite.path] = suite;
-      }
-    }
-
-    const suites = Object.values(deduped);
-
-    if (suites.length) {
-      const total = suites.reduce(
-        (acc, suite) => ({
-          tests: acc.tests + suite.tests,
-          passed: acc.passed + suite.passed,
-          failed: acc.failed + suite.failed,
-          skipped: acc.skipped + suite.skipped,
-        }),
-        { tests: 0, passed: 0, failed: 0, skipped: 0 },
-      );
-      const times = suites.map((suite) => {
-        const start = suite.date.getTime();
-        const duration = suite.time;
-        return { start, end: start + duration };
-      });
-      const earliestStart = Math.min(...times.map((t) => t.start));
-      const latestEnd = Math.max(...times.map((t) => t.end));
-      const executionTime = latestEnd - earliestStart;
-
-      const conclusion = `<strong>${
-        total.tests
-      }</strong> tests were completed in <strong>${formatTime(
-        executionTime,
-      )}</strong> with <strong>${total.passed}</strong> passed, <strong>${
-        total.failed
-      }</strong> failed and <strong>${total.skipped}</strong> skipped.`;
-
-      // replace html bold with ANSI escape codes
-      console.log(
-        conclusion
-          .replaceAll('<strong>', '\x1b[1m')
-          .replaceAll('</strong>', '\x1b[0m'),
-      );
-
-      core.summary.addRaw(`\n<details>\n`);
-      core.summary.addRaw(`<summary>${conclusion}</summary>\n`);
-
-      const rows = suites.map((suite) => ({
-        'Test suite': process.env.GITHUB_ACTIONS
-          ? `[${suite.path}](https://github.com/${OWNER}/${REPOSITORY}/blob/${BRANCH}/${suite.path})`
-          : suite.path,
-        Passed: suite.passed ? `${suite.passed} ✅` : '',
-        Failed: suite.failed ? `${suite.failed} ❌` : '',
-        Skipped: suite.skipped ? `${suite.skipped} ⏩` : '',
-        Time: formatTime(suite.time),
-      }));
-
-      const columns = Object.keys(rows[0]);
-      const header = `| ${columns.join(' | ')} |`;
-      const alignment = '| :--- | ---: | ---: | ---: | ---: |';
-      const body = rows
-        .map((row) => `| ${Object.values(row).join(' | ')} |`)
-        .join('\n');
-      const table = [header, alignment, body].join('\n');
-
-      console.table(rows);
-
-      core.summary.addRaw(`\n${table}\n`);
-      core.summary.addRaw(`</details>\n`);
-
-      if (total.failed > 0) {
-        core.summary.addRaw(`\n## ❌ Failed tests\n`);
-        console.error(`❌ Failed tests`);
-        for (const suite of suites) {
-          if (suite.failed === 0) continue;
-          core.summary.addRaw(`\n<details open>\n`);
-          core.summary.addRaw(
-            `<summary><strong>${suite.path}</strong></summary>\n`,
-          );
-          if (suite.job.name && suite.job.id && RUN_ID) {
-            core.summary.addRaw(
-              `\n##### Job: [${
-                suite.job.name
-              }](https://github.com/${OWNER}/${REPOSITORY}/actions/runs/${RUN_ID}/job/${
-                suite.job.id
-              }${PR_NUMBER ? `?pr=${PR_NUMBER}` : ''})\n`,
-            );
-          }
-          console.error(suite.name);
-          for (const test of suite.testCases) {
-            if (test.status !== 'failed') continue;
-            core.summary.addRaw(`\n##### ${test.name}\n`);
-            console.error(`- ${test.name}`);
-            core.summary.addRaw(`\n\`\`\`js\n${test.error}\n\`\`\`\n`);
-            console.error(`  ${test.error}\n`);
-          }
-          core.summary.addRaw(`</details>\n`);
+        const testRun: TestRun = {
+          // regex to remove the shard number from the job name
+          name: testSuite.job.name.replace(/\s+\(\d+\)$/, ''),
+          testSuites: [testSuite],
+        };
+        const existingRun = testRuns.find((run) => run.name === testRun.name);
+        if (existingRun) {
+          existingRun.testSuites.push(testSuite);
+        } else {
+          testRuns.push(testRun);
         }
       }
-    } else {
-      core.summary.addRaw('No tests found');
+    }
+
+    for (const testRun of testRuns) {
+      const deduped: { [path: string]: TestSuite } = {};
+      for (const suite of testRun.testSuites) {
+        const existing = deduped[suite.path];
+        // If there is a duplicate, we keep the suite with the latest date
+        if (!existing || existing.date < suite.date) {
+          deduped[suite.path] = suite;
+        }
+      }
+
+      const suites = Object.values(deduped);
+
+      const title = `<strong>${testRun.name}</strong>`;
+      console.log(consoleBold(title));
+
+      if (suites.length) {
+        const total = suites.reduce(
+          (acc, suite) => ({
+            tests: acc.tests + suite.tests,
+            passed: acc.passed + suite.passed,
+            failed: acc.failed + suite.failed,
+            skipped: acc.skipped + suite.skipped,
+          }),
+          { tests: 0, passed: 0, failed: 0, skipped: 0 },
+        );
+
+        core.summary.addRaw(
+          total.failed ? `\n<details open>\n` : `\n<details>\n`,
+        );
+        core.summary.addRaw(`<summary>${title}</summary>\n`);
+
+        const times = suites.map((suite) => {
+          const start = suite.date.getTime();
+          const duration = suite.time;
+          return { start, end: start + duration };
+        });
+        const earliestStart = Math.min(...times.map((t) => t.start));
+        const latestEnd = Math.max(...times.map((t) => t.end));
+        const executionTime = latestEnd - earliestStart;
+
+        const conclusion = `<strong>${
+          total.tests
+        }</strong> tests were completed in <strong>${formatTime(
+          executionTime,
+        )}</strong> with <strong>${total.passed}</strong> passed, <strong>${
+          total.failed
+        }</strong> failed and <strong>${total.skipped}</strong> skipped.`;
+
+        console.log(consoleBold(conclusion));
+        core.summary.addRaw(`\n${conclusion}\n`);
+
+        const rows = suites.map((suite) => ({
+          'Test suite': suite.path,
+          Passed: suite.passed ? `${suite.passed} ✅` : '',
+          Failed: suite.failed ? `${suite.failed} ❌` : '',
+          Skipped: suite.skipped ? `${suite.skipped} ⏩` : '',
+          Time: formatTime(suite.time),
+        }));
+
+        const columns = Object.keys(rows[0]);
+        const header = `| ${columns.join(' | ')} |`;
+        const alignment = '| :--- | ---: | ---: | ---: | ---: |';
+        const body = rows
+          .map((row) => {
+            const data = {
+              ...row,
+              'Test suite': `[${row['Test suite']}](https://github.com/${OWNER}/${REPOSITORY}/blob/${BRANCH}/${row['Test suite']})`,
+            };
+            return `| ${Object.values(data).join(' | ')} |`;
+          })
+          .join('\n');
+        const table = [header, alignment, body].join('\n');
+
+        console.table(rows);
+        core.summary.addRaw(`\n${table}\n`);
+
+        if (total.failed > 0) {
+          console.error(`❌ Failed tests`);
+          core.summary.addRaw(`\n#### ❌ Failed tests\n`);
+          core.summary.addRaw(
+            `\n<hr style="height: 1px; margin-top: -5px; margin-bottom: 10px;">\n`,
+          );
+          for (const suite of suites) {
+            if (suite.failed === 0) continue;
+            console.error(suite.path);
+            core.summary.addRaw(
+              `\n#### [${suite.path}](https://github.com/${OWNER}/${REPOSITORY}/blob/${BRANCH}/${suite.path})\n`,
+            );
+            if (suite.job.name && suite.job.id && RUN_ID) {
+              core.summary.addRaw(
+                `\n##### Job: [${
+                  suite.job.name
+                }](https://github.com/${OWNER}/${REPOSITORY}/actions/runs/${RUN_ID}/job/${
+                  suite.job.id
+                }${PR_NUMBER ? `?pr=${PR_NUMBER}` : ''})\n`,
+              );
+            }
+            for (const test of suite.testCases) {
+              if (test.status !== 'failed') continue;
+              console.error(`- ${test.name}`);
+              console.error(`  ${test.error}\n`);
+              core.summary.addRaw(`\n##### ${test.name}\n`);
+              core.summary.addRaw(`\n\`\`\`js\n${test.error}\n\`\`\`\n`);
+            }
+          }
+        }
+      } else {
+        core.summary.addRaw(`\n<details open>\n`);
+        core.summary.addRaw(`<summary>${title}</summary>\n`);
+        console.log('No tests found');
+        core.summary.addRaw('No tests found');
+      }
+      console.log();
+      core.summary.addRaw(`</details>\n`);
     }
 
     await core.summary.write();
