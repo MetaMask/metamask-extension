fa4b8c3172cc9e0d42a24a7e3ba99f312b78438c
Teng-Web3 <tengzuo.veritas@gmail.com>
2025-04-14 16:19:34 +0800

    feat:convert blockieIdenticon.component.js to typescript

---
+++ ui/components/ui/identicon/blockieIdenticon/blockieIdenticon.component.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+
+type BlockieIdenticonProps = {
+  address: string;
+  diameter: number;
+  alt?: string;
+  borderRadius?: string | number;
+}
+
+const BlockieIdenticon: React.FC<BlockieIdenticonProps> = ({
+  address,
+  diameter,
+  alt = '',
+  borderRadius,
+}) => {
+  const [src, setSrc] = useState<string>('');
+
+  useEffect(() => {
+    const loadBlo = async () => {
+      const { blo } = await import('blo');
+      setSrc(blo(address));
+    };
+    loadBlo();
+  }, [address]);
+
+  return (
+    <img
+      src={src}
+      height={diameter}
+      width={diameter}
+      style={{
+        borderRadius,
+      }}
+      alt={alt}
+    />
+  );
+};
+
+export default BlockieIdenticon;
