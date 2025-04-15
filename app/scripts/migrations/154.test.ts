b061d1803bbca8d3f3f5bceaa5b34a06ab3627e9
gantunesr <17601467+gantunesr@users.noreply.github.com>
2025-04-15 13:15:07 -0400

    fix: add migration

---
+++ app/scripts/migrations/154.test.ts
@@ -0,0 +1,72 @@
+import { AVAILABLE_MULTICHAIN_NETWORK_CONFIGURATIONS } from '@metamask/multichain-network-controller';
+import { migrate, version } from './154';
+import { BtcScope, SolScope } from '@metamask/keyring-api';
+
+const oldVersion = 153;
+
+describe(`migration #${version}`, () => {
+  it('updates the version metadata', async () => {
+    const oldStorage = {
+      meta: { version: oldVersion },
+      data: {},
+    };
+    const newStorage = await migrate(oldStorage);
+    expect(newStorage.meta).toStrictEqual({ version });
+  });
+
+  describe(`migration #${version}`, () => {
+    it('does nothing if MultichainNetworkController is missing', async () => {
+      const oldStorage = {
+        meta: { version: oldVersion },
+        data: {},
+      };
+      const newStorage = await migrate(oldStorage);
+      expect(newStorage.data).toStrictEqual({});
+    });
+
+    it('does nothing if MultichainNetworkController is not an object', async () => {
+      const oldStorage = {
+        meta: { version: oldVersion },
+        data: {
+          NetworkOrderController: 'invalidData',
+        },
+      };
+      const newStorage = await migrate(oldStorage);
+      expect(newStorage.data).toStrictEqual(oldStorage.data);
+    });
+
+    it('does nothing if MultichainNetworkController.multichainNetworkConfigurationsByChainId is not an object', async () => {
+      const oldStorage = {
+        meta: { version: oldVersion },
+        data: {
+          NetworkOrderController: {
+            orderedNetworkList: 'not an array',
+          },
+        },
+      };
+      const newStorage = await migrate(oldStorage);
+      expect(newStorage.data).toStrictEqual(oldStorage.data);
+    });
+
+    it.only('updates the multichainNetworkConfigurationsByChainId to contain test networks', async () => {
+      const oldStorage = {
+        meta: { version: oldVersion },
+        data: {
+          MultichainNetworkController: {
+            multichainNetworkConfigurationsByChainId: {
+              [BtcScope.Mainnet]: AVAILABLE_MULTICHAIN_NETWORK_CONFIGURATIONS[BtcScope.Mainnet],
+              [SolScope.Mainnet]: AVAILABLE_MULTICHAIN_NETWORK_CONFIGURATIONS[SolScope.Mainnet],
+            },
+          },
+        },
+      };
+      const expectedData = {
+        MultichainNetworkController: {
+          multichainNetworkConfigurationsByChainId: AVAILABLE_MULTICHAIN_NETWORK_CONFIGURATIONS,
+        },
+      };
+      const newStorage = await migrate(oldStorage);
+      expect(newStorage.data).toStrictEqual(expectedData);
+    });
+  });
+});
---
+++ app/scripts/migrations/154.ts
@@ -0,0 +1,43 @@
+import { AVAILABLE_MULTICHAIN_NETWORK_CONFIGURATIONS } from '@metamask/multichain-network-controller';
+import { type Hex, hasProperty, isObject } from '@metamask/utils';
+import { cloneDeep } from 'lodash';
+
+type VersionedData = {
+  meta: { version: number };
+  data: Record<string, unknown>;
+};
+
+export const version = 145;
+
+/**
+ * This migration updates the values of the chain IDs in the NetworkOrderController
+ * from Hex to CaipChainId format (CAIP-19).
+ *
+ * @param originalVersionedData - Versioned MetaMask extension state, exactly
+ * what we persist to disk.
+ * @returns Updated versioned MetaMask extension state.
+ */
+export async function migrate(
+  originalVersionedData: VersionedData,
+): Promise<VersionedData> {
+  const versionedData = cloneDeep(originalVersionedData);
+  versionedData.meta.version = version;
+  transformState(versionedData.data);
+  return versionedData;
+}
+
+function transformState(
+  state: Record<string, unknown>,
+): Record<string, unknown> {
+  // Migrate the user's drag + drop preference order for the network menu
+
+  if (
+    hasProperty(state, 'MultichainNetworkController') &&
+    isObject(state.MultichainNetworkController) &&
+    isObject(state.MultichainNetworkController.multichainNetworkConfigurationsByChainId)
+  ) {
+    state.MultichainNetworkController.multichainNetworkConfigurationsByChainId = AVAILABLE_MULTICHAIN_NETWORK_CONFIGURATIONS;
+  }
+
+  return state;
+}
