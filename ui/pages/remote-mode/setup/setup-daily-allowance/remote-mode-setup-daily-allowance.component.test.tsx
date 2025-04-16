b902c06ca0538c7366e282bc264a88cfd30e38a8
Kevin Bluer <kevin@bluer.com>
2025-04-15 20:21:26 -0500

    chore: adds mock state to failing tests

--- ui/pages/remote-mode/setup/setup-daily-allowance/remote-mode-setup-daily-allowance.component.test.tsx
+++ ui/pages/remote-mode/setup/setup-daily-allowance/remote-mode-setup-daily-allowance.component.test.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import configureMockStore from 'redux-mock-store';
 import { renderWithProvider } from '../../../../../test/jest/rendering';
+import mockState from '../../../../../test/data/mock-state.json';
 import RemoteModeSetupDailyAllowance from './remote-mode-setup-daily-allowance.component';
 
 const renderComponent = () => {
-  const store = configureMockStore([])({
+  const store = configureMockStore()({
+    ...mockState,
     metamask: {
+      ...mockState.metamask,
       isRemoteModeEnabled: true,
     },
   });
