550549afc5463beb9d0e5d459f05bc6caf080467
georgewrmarshall <george.marshall@consensys.net>
2025-04-17 11:41:52 +0930

    chore: improving disabled state in tabs component

--- ui/components/ui/tabs/tabs.stories.js
+++ ui/components/ui/tabs/tabs.stories.js
@@ -1,49 +1,48 @@
 import React from 'react';
-import Tab from './tab/tab.component';
 import Tabs from './tabs.component';
+import Tab from './tab/tab.component';
 
 export default {
   title: 'Components/UI/Tabs',
-
-  argTypes: {
-    tabs: {
-      control: 'object',
-      name: 'Tabs',
-    },
-    defaultActiveTabKey: {
-      control: {
-        type: 'text',
+  component: Tabs,
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'Tabs component with support for disabled state. Disabled tabs cannot be clicked and have a muted appearance.',
       },
     },
-    onTabClick: { action: 'onTabClick' },
-    onChange: { action: 'onChange' },
   },
+};
+
+export const Default = {
   args: {
-    tabs: [
-      { name: 'Tab A', content: 'Tab A Content' },
-      { name: 'Tab B', content: 'Tab B Content' },
-      { name: 'Tab C', content: 'Tab C Content' },
+    children: [
+      <Tab key="tab1" name="Tab 1" tabKey="tab1">
+        Content 1
+      </Tab>,
+      <Tab key="tab2" name="Tab 2" tabKey="tab2">
+        Content 2
+      </Tab>,
+      <Tab key="tab3" name="Tab 3" tabKey="tab3">
+        Content 3
+      </Tab>,
     ],
   },
 };
 
-function renderTab({ name, content }, index) {
-  return (
-    <Tab tabKey={name} key={name + index} name={name}>
-      {content}
-    </Tab>
-  );
-}
-
-export const DefaultStory = (args) => {
-  return (
-    <Tabs
-      defaultActiveTabKey={args.defaultActiveTabKey}
-      onTabClick={args.onTabClick}
-    >
-      {args.tabs.map((tabProps, i) => renderTab(tabProps, i, args.t))}
-    </Tabs>
-  );
+export const Disabled = {
+  args: {
+    children: [
+      <Tab key="tab1" name="Tab 1" tabKey="tab1">
+        Content 1
+      </Tab>,
+      <Tab key="tab2" name="Tab 2" tabKey="tab2" disabled>
+        Content 2 (Disabled)
+      </Tab>,
+      <Tab key="tab3" name="Tab 3" tabKey="tab3">
+        Content 3
+      </Tab>,
+    ],
+  },
 };
-
-DefaultStory.storyName = 'Default';
