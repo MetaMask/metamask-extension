6680737c4638c0fef2cb7510dfea03d25c9b06cb
sahar-fehri <sahar.fehri@consensys.net>
2025-04-17 16:22:55 +0200

    fix: fix

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -4230,9 +4230,9 @@ export default class MetamaskController extends EventEmitter {
       ),
 
       // MultichainAssetsRatesController
-      fetchHistoricalPricesForAsset:
-        multichainAssetsRatesController.fetchHistoricalPricesForAsset.bind(
-          multichainAssetsRatesController,
+      fetchHistoricalPricesForAsset: (...args) =>
+        this.multichainAssetsRatesController.fetchHistoricalPricesForAsset(
+          ...args,
         ),
 
       // DetectCollectibleController
