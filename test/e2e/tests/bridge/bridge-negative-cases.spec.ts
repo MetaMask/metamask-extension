7cf635bd879c0b65a5536a7cc7125f7b42654006
Davide Brocchetto <davide.brocchetto@consensys.net>
2025-04-17 19:15:09 -0700

    Added getTxStatus

--- test/e2e/tests/bridge/bridge-negative-cases.spec.ts
+++ test/e2e/tests/bridge/bridge-negative-cases.spec.ts
@@ -1,43 +1,38 @@
 import { Suite } from 'mocha';
-import { Mockttp } from 'mockttp';
 import { unlockWallet, withFixtures } from '../../helpers';
 import HomePage from '../../page-objects/pages/home/homepage';
 import HeaderNavbar from '../../page-objects/pages/header-navbar';
 import SettingsPage from '../../page-objects/pages/settings/settings-page';
 import AdvancedSettings from '../../page-objects/pages/settings/advanced-settings';
 import { Driver } from '../../webdriver/driver';
-import BridgeQuotePage, {
-  BridgeQuote,
-} from '../../page-objects/pages/bridge/quote-page';
-import { getBridgeNegativeCasesFixtures } from './bridge-test-utils';
+import BridgeQuotePage from '../../page-objects/pages/bridge/quote-page';
+import {
+  getQuoteNegativeCasesFixtures,
+  getTxStatusNegativeCasesFixtures,
+} from './bridge-test-utils';
 import ActivityListPage from '../../page-objects/pages/home/activity-list';
-import AccountListPage from '../../page-objects/pages/account-list-page';
+import {
+  INTERNAL_SERVER_ERROR,
+  EMPTY_RESPONSE,
+  GET_QUOTE_INVALID_RESPONSE,
+  FAILED_SOURCE_TRANSACTION,
+  FAILED_DEST_TRANSACTION,
+} from './constants';
 
 describe('Bridge functionality', function (this: Suite) {
   it('should show message that no trade route is available if getQuote returns error 500', async function () {
     await withFixtures(
-      getBridgeNegativeCasesFixtures(async (mockServer) => {
-        return await mockServer.forGet(/getQuote/u).thenCallback(() => {
-          return {
-            statusCode: 500,
-            json: { error: 'Internal server error' },
-          };
-        });
-      }, this.test?.fullTitle()),
+      getQuoteNegativeCasesFixtures(
+        INTERNAL_SERVER_ERROR,
+        this.test?.fullTitle(),
+      ),
       async ({ driver }) => {
         await unlockWallet(driver);
         const homePage = new HomePage(driver);
         await homePage.check_expectedBalanceIsDisplayed();
         await homePage.startBridgeFlow();
 
-        const bridgePage = new BridgeQuotePage(driver);
-        await bridgePage.enterBridgeQuote({
-          amount: '1',
-          tokenFrom: 'ETH',
-          tokenTo: 'ETH',
-          fromChain: 'Ethereum',
-          toChain: 'Linea',
-        });
+        const bridgePage = await enterBridgeQuote(driver);
         await bridgePage.check_noTradeRouteMessageIsDisplayed();
       },
     );
@@ -45,28 +40,14 @@ describe('Bridge functionality', function (this: Suite) {
 
   it('should show message that no trade route is available if getQuote returns empty array', async function () {
     await withFixtures(
-      getBridgeNegativeCasesFixtures(async (mockServer) => {
-        return await mockServer.forGet(/getQuote/u).thenCallback(() => {
-          return {
-            statusCode: 200,
-            json: [],
-          };
-        });
-      }, this.test?.fullTitle()),
+      getQuoteNegativeCasesFixtures(EMPTY_RESPONSE, this.test?.fullTitle()),
       async ({ driver }) => {
         await unlockWallet(driver);
         const homePage = new HomePage(driver);
         await homePage.check_expectedBalanceIsDisplayed();
         await homePage.startBridgeFlow();
 
-        const bridgePage = new BridgeQuotePage(driver);
-        await bridgePage.enterBridgeQuote({
-          amount: '1',
-          tokenFrom: 'ETH',
-          tokenTo: 'ETH',
-          fromChain: 'Ethereum',
-          toChain: 'Linea',
-        });
+        const bridgePage = await enterBridgeQuote(driver);
         await bridgePage.check_noTradeRouteMessageIsDisplayed();
       },
     );
@@ -74,81 +55,119 @@ describe('Bridge functionality', function (this: Suite) {
 
   it('should show message that no trade route is available if getQuote returns invalid response', async function () {
     await withFixtures(
-      getBridgeNegativeCasesFixtures(async (mockServer) => {
-        return await mockServer.forGet(/getQuote/u).thenCallback(() => {
-          return {
-            statusCode: 200,
-            json: [
-              {
-                quote: {
-                  requestId: 'd6d465bf-38a1-452e-a8e4-b95cb9ad5421',
-                  srcChainId: 59144,
-                  srcTokenAmount: '991250',
-                  srcAsset: {
-                    address: null,
-                    chainId: 59144,
-                    assetId: null,
-                    symbol: 'USDC',
-                    decimals: 6,
-                    name: 'USD Coin',
-                    coingeckoId: 'bridged-usd-coin-linea',
-                    aggregators: [],
-                    occurrences: 0,
-                    price: '0',
-                  },
-                  destChainId: 1,
-                  destTokenAmount: '387773599373699',
-                  destAsset: {
-                    address: '0x0000000000000000000000000000000000000000',
-                    chainId: 1,
-                    assetId: 'eip155:1/slip44:60',
-                    symbol: 'ETH',
-                    decimals: 18,
-                    name: 'Ethereum',
-                    coingeckoId: 'ethereum',
-                    aggregators: [],
-                    occurrences: 100,
-                    price: '0',
-                  },
-                  bridgeId: 'lifi',
-                  bridges: ['hop'],
-                  steps: [],
-                  bridgePriceData: {
-                    totalFromAmountUsd: '0.9913',
-                    totalToAmountUsd: '0.6160',
-                    priceImpact: '0.3785937657621305',
-                  },
-                },
-                trade: {
-                  chainId: null,
-                  to: null,
-                  from: null,
-                  value: null,
-                  data: null,
-                  gasLimit: 0,
-                },
-                estimatedProcessingTimeInSeconds: 0,
-              },
-            ],
-          };
-        });
-      }, this.test?.fullTitle()),
+      getQuoteNegativeCasesFixtures(
+        GET_QUOTE_INVALID_RESPONSE,
+        this.test?.fullTitle(),
+      ),
       async ({ driver }) => {
         await unlockWallet(driver);
         const homePage = new HomePage(driver);
         await homePage.check_expectedBalanceIsDisplayed();
         await homePage.startBridgeFlow();
 
-        const bridgePage = new BridgeQuotePage(driver);
-        await bridgePage.enterBridgeQuote({
-          amount: '1',
-          tokenFrom: 'ETH',
-          tokenTo: 'ETH',
-          fromChain: 'Ethereum',
-          toChain: 'Linea',
-        });
+        const bridgePage = await enterBridgeQuote(driver);
         await bridgePage.check_noTradeRouteMessageIsDisplayed();
       },
     );
   });
+
+  it('should show that bridge transaction is pending if getTxStaus returns error 500', async function () {
+    await withFixtures(
+      getTxStatusNegativeCasesFixtures(
+        INTERNAL_SERVER_ERROR,
+        this.test?.fullTitle(),
+      ),
+      async ({ driver }) => {
+        await unlockWallet(driver);
+        const homePage = new HomePage(driver);
+        await homePage.check_expectedBalanceIsDisplayed();
+        await disableSmartTransactions(driver);
+        await homePage.startBridgeFlow();
+
+        const bridgePage = await enterBridgeQuote(driver);
+        await bridgePage.submitQuote();
+
+        await homePage.goToActivityList();
+        await driver.delay(5000);
+        const activityList = new ActivityListPage(driver);
+        await activityList.check_pendingBridgeTransactionActivity();
+      },
+    );
+  });
+
+  it('should show failed bridge activity if getTxStatus returns failed source transaction', async function () {
+    await withFixtures(
+      getTxStatusNegativeCasesFixtures(
+        FAILED_SOURCE_TRANSACTION,
+        this.test?.fullTitle(),
+      ),
+      async ({ driver }) => {
+        await unlockWallet(driver);
+        const homePage = new HomePage(driver);
+        await homePage.check_expectedBalanceIsDisplayed();
+        await disableSmartTransactions(driver);
+        await homePage.startBridgeFlow();
+
+        const bridgePage = await enterBridgeQuote(driver);
+        await bridgePage.submitQuote();
+
+        await homePage.goToActivityList();
+        await driver.delay(5000);
+        const activityList = new ActivityListPage(driver);
+        //await activityList.check_failedTxNumberDisplayedInActivity();
+      },
+    );
+  });
+
+  it('should show failed bridge activity if getTxStatus returns failed destination transaction', async function () {
+    await withFixtures(
+      getTxStatusNegativeCasesFixtures(
+        FAILED_DEST_TRANSACTION,
+        this.test?.fullTitle(),
+      ),
+      async ({ driver }) => {
+        await unlockWallet(driver);
+        const homePage = new HomePage(driver);
+        await homePage.check_expectedBalanceIsDisplayed();
+        await disableSmartTransactions(driver);
+        await homePage.startBridgeFlow();
+
+        const bridgePage = await enterBridgeQuote(driver);
+        await bridgePage.submitQuote();
+
+        await homePage.goToActivityList();
+        await driver.delay(5000);
+        const activityList = new ActivityListPage(driver);
+        //await activityList.check_failedTxNumberDisplayedInActivity();
+      },
+    );
+  });
 });
+
+async function disableSmartTransactions(driver: Driver) {
+  // disable smart transactions
+  const headerNavbar = new HeaderNavbar(driver);
+  await headerNavbar.check_pageIsLoaded();
+  await headerNavbar.openSettingsPage();
+
+  const settingsPage = new SettingsPage(driver);
+  await settingsPage.check_pageIsLoaded();
+  await settingsPage.clickAdvancedTab();
+  const advancedSettingsPage = new AdvancedSettings(driver);
+  await advancedSettingsPage.check_pageIsLoaded();
+  await advancedSettingsPage.toggleSmartTransactions();
+  await settingsPage.closeSettingsPage();
+}
+
+async function enterBridgeQuote(driver: Driver): Promise<BridgeQuotePage> {
+  const bridgePage = new BridgeQuotePage(driver);
+  await bridgePage.enterBridgeQuote({
+    amount: '1',
+    tokenFrom: 'ETH',
+    tokenTo: 'ETH',
+    fromChain: 'Ethereum',
+    toChain: 'Linea',
+  });
+
+  return bridgePage;
+}
