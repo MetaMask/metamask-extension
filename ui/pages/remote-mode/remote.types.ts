5dd1a4818c6a90b6f5259c7a63865a6bfb7b3b9c
Kevin Bluer <kevin@bluer.com>
2025-04-18 17:10:19 -0500

    chore: increases design parity for swap setup screens (and minor refactors)

--- ui/pages/remote-mode/remote.types.ts
+++ ui/pages/remote-mode/remote.types.ts
@@ -3,6 +3,8 @@
 export enum TokenSymbol {
   USDC = 'USDC',
   WETH = 'WETH',
+  WBTC = 'WBTC',
+  BNB = 'BNB',
 }
 export type TokenInfo = {
   symbol: TokenSymbol;
@@ -21,6 +23,16 @@ export const TOKEN_DETAILS: Record<TokenSymbol, TokenInfo> = {
     name: 'WETH',
     iconUrl: './images/eth_logo.png',
   },
+  [TokenSymbol.WBTC]: {
+    symbol: TokenSymbol.WBTC,
+    name: 'WBTC',
+    iconUrl: './images/eth_logo.png', // TODO: add WBTC icon
+  },
+  [TokenSymbol.BNB]: {
+    symbol: TokenSymbol.BNB,
+    name: 'BNB',
+    iconUrl: './images/eth_logo.png', // TODO: add BNB icon
+  },
 };
 
 export type SwapAllowance = {
@@ -30,8 +42,8 @@ export type SwapAllowance = {
 };
 
 export enum ToTokenOption {
-  Any = 'Any available token',
-  HighLiquidity = 'High liquidity tokens',
+  AllowedOutcome = 'Select allowed outcome token',
+  Any = 'Any token on Ethereum Mainnet',
 }
 
 export enum DailyAllowanceTokenTypes {
