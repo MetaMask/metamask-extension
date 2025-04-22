265758cb5adcef9bd902976e57a3eb92290581c1
HJetpoluru <harika.jetpoluru@consensys.net>
2025-04-22 14:17:52 -0400

    Added the state validation

--- test/e2e/flask/snap/preinstalled-example.spec.ts
+++ test/e2e/flask/snap/preinstalled-example.spec.ts
@@ -2,11 +2,13 @@ import { Driver } from '../../webdriver/driver';
 import HeaderNavbar from '../../page-objects/pages/header-navbar';
 import FixtureBuilder from '../../fixture-builder';
 import { loginWithoutBalanceValidation } from '../../page-objects/flows/login.flow';
-import { withFixtures } from '../../helpers';
+import { withFixtures, WINDOW_TITLES } from '../../helpers';
 import SettingsPage from '../../page-objects/pages/settings/settings-page';
 import PreinstalledExampleSettings from '../../page-objects/pages/settings/preinstalled-example-settings';
 import { strict as assert } from 'assert';
 import HomePage from '../../page-objects/pages/home/homepage';
+import { TestSnaps } from '../../page-objects/pages/test-snaps';
+import SnapInstall from '../../page-objects/pages/dialog/snap-install';
 
 describe('Pre-install example', function () {
   it('validate the page loads and the components', async function () {
@@ -30,6 +32,18 @@ describe('Pre-install example', function () {
         await preInstalledExample.check_isToggleOn();
         assert.equal(await preInstalledExample.check_selectedRadioOption('Option 2'), true);
         await preInstalledExample.check_selectedDropdownOption('Option 2');
+
+        // Navigate to `test-snaps` page, click connect Preinstalled button, connect and approve
+        const testSnaps = new TestSnaps(driver);
+        const snapInstall = new SnapInstall(driver);
+        await testSnaps.openPage();
+        await testSnaps.scrollAndClickButton('connectPreinstalledButton');
+        await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
+        await snapInstall.clickConfirmButton();
+        await driver.switchToWindowWithTitle(WINDOW_TITLES.TestSnaps);
+        await testSnaps.clickButton('getSettingsStateButton');
+        const jsonTextValidation = '"setting1": true';
+        await testSnaps.check_messageResultSpan('rpcResultSpan',jsonTextValidation);
       }
   );
   });
