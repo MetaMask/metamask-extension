6ff83a9b1c01b79bb40e466a2b27208858728934
Jyoti Puri <jyotipuri@gmail.com>
2025-04-24 23:52:53 +0000

    chore(runway): cherry-pick feat: Changes in upgrade to smart account confirmation page (#32034)
    ## **Description**
    
    Improvements in upgrade account page.
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/MetaMask-planning/issues/4585
    
    ## **Manual testing steps**
    
    1. Submit account improvement request
    2. Check the page rendered
    
    ## **Screenshots/Recordings**
    <img width="411" alt="Screenshot 2025-04-16 at 3 22 57 PM"
    src="https://github.com/user-attachments/assets/74c5a714-b067-48c1-8e2e-1ab18475ef43"
    />
    
    ## **Pre-merge author checklist**
    
    - [X] I've followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
    Extension Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [X] I've completed the PR template to the best of my ability
    - [X] I’ve included tests if applicable
    - [X] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [X] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- ui/pages/confirmations/components/confirm/info/batch/batch-simulation-details/batch-simulation-details.test.tsx
+++ ui/pages/confirmations/components/confirm/info/batch/batch-simulation-details/batch-simulation-details.test.tsx
@@ -14,7 +14,10 @@ import { AlertMetricsProvider } from '../../../../../../../components/app/alert-
 import { useBalanceChanges } from '../../../../simulation-details/useBalanceChanges';
 import { TokenStandard } from '../../../../../../../../shared/constants/transaction';
 import { buildApproveTransactionData } from '../../../../../../../../test/data/confirmations/token-approve';
-import { RevokeDelegation } from '../../../../../../../../test/data/confirmations/batch-transaction';
+import {
+  downgradeAccountConfirmation,
+  upgradeAccountConfirmationOnly,
+} from '../../../../../../../../test/data/confirmations/batch-transaction';
 import { updateAtomicBatchData } from '../../../../../../../store/controller-actions/transaction-controller';
 import { Confirmation } from '../../../../../types/confirm';
 import { getCustomTxParamsData } from '../../../../../confirm-approve/confirm-approve.util';
@@ -283,7 +286,12 @@ describe('BatchSimulationDetails', () => {
   });
 
   it('return null for transaction of type revokeDelegation', () => {
-    const { container } = render(RevokeDelegation);
+    const { container } = render(downgradeAccountConfirmation);
+    expect(container.firstChild).toBeNull();
+  });
+
+  it('return null for upgrade transaction if there are no nested transactions', () => {
+    const { container } = render(upgradeAccountConfirmationOnly);
     expect(container.firstChild).toBeNull();
   });
 });
