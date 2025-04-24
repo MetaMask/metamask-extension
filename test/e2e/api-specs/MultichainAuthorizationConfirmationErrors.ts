0cddd02433987347939638919eaa77509fc2a8bb
seaona <seaona@proton.me>
2025-04-24 11:50:59 +0200

    fix

--- test/e2e/api-specs/MultichainAuthorizationConfirmationErrors.ts
+++ test/e2e/api-specs/MultichainAuthorizationConfirmationErrors.ts
@@ -112,7 +112,8 @@ export class MultichainAuthorizationConfirmationErrors implements Rule {
               error.message
             } `,
             methodName: method.name,
-            params: [params],
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            params: params as any,
             url: '',
             resultSchema: (method.result as ContentDescriptorObject).schema,
             expectedResult: error,
