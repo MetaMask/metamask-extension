863f723479e5d70c881ce216b9a40617c0be41b4
Guillaume Roux <hello@guillaumerx.fr>
2025-04-28 11:07:14 +0200

    address requested changes

--- ui/components/app/snaps/snap-ui-renderer/components/asset-selector.test.ts
+++ ui/components/app/snaps/snap-ui-renderer/components/asset-selector.test.ts
@@ -3,87 +3,106 @@ import { fireEvent } from '@testing-library/dom';
 import { renderInterface } from '../test-utils';
 
 describe('SnapUIAssetSelector', () => {
-  const mockMetamaskState = {
-    metamask: {
-      internalAccounts: {
-        accounts: {
-          '8c33fc18-6c52-44b1-b8fa-550b934a05ef': {
-            address: '7S3P4HxJpyyigGzodYwHtCxZyUQe9JiBMHyRWXArAaKv',
-            id: '8c33fc18-6c52-44b1-b8fa-550b934a05ef',
-            name: 'Solana Account',
-            keyring: {
-              type: 'Snap Keyring',
-            },
-          },
+  const mockInternalAccount = {
+    accounts: {
+      '8c33fc18-6c52-44b1-b8fa-550b934a05ef': {
+        address: '7S3P4HxJpyyigGzodYwHtCxZyUQe9JiBMHyRWXArAaKv',
+        id: '8c33fc18-6c52-44b1-b8fa-550b934a05ef',
+        name: 'Solana Account',
+        keyring: {
+          type: 'Snap Keyring',
         },
-      },
-      balances: {
-        '8c33fc18-6c52-44b1-b8fa-550b934a05ef': {
-          'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp/slip44:105': {
-            amount: '1',
-            unit: 'SOL',
-          },
-          'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp/token:EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v':
-            {
-              amount: '2',
-              unit: 'USDC',
-            },
-        },
-      },
-      accountsAssets: {
-        '8c33fc18-6c52-44b1-b8fa-550b934a05ef': [
-          'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp/slip44:105',
-          'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp/token:EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v',
-        ],
-      },
-      assetsMetadata: {
-        'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp/slip44:105': {
-          fungible: true,
-          iconUrl:
-            'https://static.cx.metamask.io/api/v2/tokenIcons/assets/solana/5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp/slip44/501.png',
-          name: 'Solana',
-          symbol: 'SOL',
-          units: [
-            {
-              decimals: 9,
-              name: 'Solana',
-              symbol: 'SOL',
-            },
-          ],
-        },
-        'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp/token:EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v':
-          {
-            fungible: true,
-            iconUrl:
-              'https://static.cx.metamask.io/api/v2/tokenIcons/assets/solana/5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp/token/EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v.png',
-            name: 'USDC',
-            symbol: 'USDC',
-            units: [
-              {
-                decimals: 9,
-                name: 'USDC',
-                symbol: 'USDC',
-              },
-            ],
-          },
-      },
-      conversionRates: {
-        'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp/slip44:105': {
-          conversionTime: 1745405595549,
-          currency: 'swift:0/iso4217:USD',
-          expirationTime: 1745409195549,
-          rate: '151.36',
-        },
-        'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp/token:EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v':
-          {
-            conversionTime: 1745405595549,
-            currency: 'swift:0/iso4217:USD',
-            expirationTime: 1745409195549,
-            rate: '1.00',
-          },
       },
     },
   };
+
+  const mockBalances = {
+    '8c33fc18-6c52-44b1-b8fa-550b934a05ef': {
+      'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp/slip44:105': {
+        amount: '1',
+        unit: 'SOL',
+      },
+      'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp/token:EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v':
+        {
+          amount: '2',
+          unit: 'USDC',
+        },
+    },
+  };
+
+  const mockAccountsAssets = {
+    '8c33fc18-6c52-44b1-b8fa-550b934a05ef': [
+      'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp/slip44:105',
+      'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp/token:EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v',
+    ],
+  };
+
+  const mockAssetsMetadata = {
+    'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp/slip44:105': {
+      fungible: true,
+      iconUrl:
+        'https://static.cx.metamask.io/api/v2/tokenIcons/assets/solana/5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp/slip44/501.png',
+      name: 'Solana',
+      symbol: 'SOL',
+      units: [
+        {
+          decimals: 9,
+          name: 'Solana',
+          symbol: 'SOL',
+        },
+      ],
+    },
+    'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp/token:EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v':
+      {
+        fungible: true,
+        iconUrl:
+          'https://static.cx.metamask.io/api/v2/tokenIcons/assets/solana/5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp/token/EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v.png',
+        name: 'USDC',
+        symbol: 'USDC',
+        units: [
+          {
+            decimals: 9,
+            name: 'USDC',
+            symbol: 'USDC',
+          },
+        ],
+      },
+  };
+
+  const mockConversionRates = {
+    'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp/slip44:105': {
+      conversionTime: 1745405595549,
+      currency: 'swift:0/iso4217:USD',
+      expirationTime: 1745409195549,
+      rate: '151.36',
+    },
+    'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp/token:EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v':
+      {
+        conversionTime: 1745405595549,
+        currency: 'swift:0/iso4217:USD',
+        expirationTime: 1745409195549,
+        rate: '1.00',
+      },
+  };
+
+  const mockState = {
+    metamask: {
+      internalAccounts: mockInternalAccount,
+      balances: mockBalances,
+      accountsAssets: mockAccountsAssets,
+      assetsMetadata: mockAssetsMetadata,
+      conversionRates: mockConversionRates,
+    },
+  };
+
+  const mockInterfaceState = {
+    'asset-selector': {
+      asset: 'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp/slip44:105',
+      name: 'Solana',
+      symbol: 'SOL',
+    },
+  };
+
   it('renders an asset selector', () => {
     const { container } = renderInterface(
       Box({
@@ -95,14 +114,8 @@ describe('SnapUIAssetSelector', () => {
         }),
       }),
       {
-        state: {
-          'asset-selector': {
-            asset: 'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp/slip44:105',
-            name: 'Solana',
-            symbol: 'SOL',
-          },
-        },
-        metamaskState: mockMetamaskState,
+        state: mockInterfaceState,
+        metamaskState: mockState,
       },
     );
 
@@ -131,14 +144,8 @@ describe('SnapUIAssetSelector', () => {
         }),
       }),
       {
-        state: {
-          'asset-selector': {
-            asset: 'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp/slip44:105',
-            name: 'Solana',
-            symbol: 'SOL',
-          },
-        },
-        metamaskState: mockMetamaskState,
+        state: mockInterfaceState,
+        metamaskState: mockState,
       },
     );
 
@@ -160,14 +167,8 @@ describe('SnapUIAssetSelector', () => {
         }),
       }),
       {
-        state: {
-          'asset-selector': {
-            asset: 'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp/slip44:105',
-            name: 'Solana',
-            symbol: 'SOL',
-          },
-        },
-        metamaskState: mockMetamaskState,
+        state: mockInterfaceState,
+        metamaskState: mockState,
       },
     );
 
@@ -200,14 +201,8 @@ describe('SnapUIAssetSelector', () => {
         }),
       }),
       {
-        state: {
-          'asset-selector': {
-            asset: 'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp/slip44:105',
-            name: 'Solana',
-            symbol: 'SOL',
-          },
-        },
-        metamaskState: mockMetamaskState,
+        state: mockInterfaceState,
+        metamaskState: mockState,
       },
     );
 
@@ -218,5 +213,32 @@ describe('SnapUIAssetSelector', () => {
     expect(getByText('Asset Selector')).toBeInTheDocument();
 
     expect(container).toMatchSnapshot();
+  });
+
+  it('can show an error', () => {
+    const { container, getByText } = renderInterface(
+      Box({
+        children: Field({
+          label: 'Asset Selector',
+          children: AssetSelector({
+            name: 'asset-selector',
+            addresses: [
+              'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp:7S3P4HxJpyyigGzodYwHtCxZyUQe9JiBMHyRWXArAaKv',
+            ],
+          }),
+          error: 'This is an error',
+        }),
+      }),
+      {
+        state: mockInterfaceState,
+        metamaskState: mockState,
+      },
+    );
+
+    expect(
+      container.getElementsByClassName('snap-ui-renderer__asset-selector'),
+    ).toHaveLength(1);
+
+    expect(getByText('This is an error')).toBeInTheDocument();
   });
 });
