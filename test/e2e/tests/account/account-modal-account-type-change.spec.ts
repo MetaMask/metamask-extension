0e6e47812164c3b71388ae5d5d13352ce1aac573
Jyoti Puri <jyotipuri@gmail.com>
2025-04-28 15:29:41 +0530

    update

--- test/e2e/tests/account/account-modal-account-type-change.spec.ts
+++ test/e2e/tests/account/account-modal-account-type-change.spec.ts
@@ -4,7 +4,7 @@ import { Anvil } from '@viem/anvil';
 import { Driver } from '../../webdriver/driver';
 import { DEFAULT_FIXTURE_ACCOUNT } from '../../constants';
 import FixtureBuilder from '../../fixture-builder';
-import { WINDOW_TITLES, withFixtures } from '../../helpers';
+import { tinyDelayMs, WINDOW_TITLES, withFixtures } from '../../helpers';
 import { loginWithBalanceValidation } from '../../page-objects/flows/login.flow';
 import Eip7702AndSendCalls from '../../page-objects/pages/confirmations/redesign/batch-confirmation';
 import ActivityListPage from '../../page-objects/pages/home/activity-list';
@@ -13,14 +13,12 @@ import { mockEip7702FeatureFlag } from '../confirmations/helpers';
 import HeaderNavbar from '../../page-objects/pages/header-navbar';
 import AccountDetailsModal from '../../page-objects/pages/dialog/account-details-modal';
 
-describe('Upgrade Account', function (this: Suite) {
-  it('an EOA account can be upgraded when triggering a batch tx from a dapp in an odd chain id', async function () {
+describe('Switch Modal - Switch Account', function (this: Suite) {
+  it('Account modal should have options to upgrade / downgrade the account', async function () {
     await withFixtures(
       {
         dapp: true,
-        fixtures: new FixtureBuilder()
-          .withPermissionControllerConnectedToTestDapp()
-          .build(),
+        fixtures: new FixtureBuilder().build(),
         localNodeOptions: [
           {
             type: 'anvil',
@@ -34,54 +32,66 @@ describe('Upgrade Account', function (this: Suite) {
         testSpecificMock: mockEip7702FeatureFlag,
         title: this.test?.fullTitle(),
       },
-      async ({ driver, localNodes }: { driver: Driver; localNodes: Anvil }) => {
+      async ({ driver }: { driver: Driver; localNodes: Anvil }) => {
         await loginWithBalanceValidation(driver);
 
-        // We check that we have an EOA account
-        let accountBytecode = await localNodes[0].getCode(
-          DEFAULT_FIXTURE_ACCOUNT,
-        );
-        assert.strictEqual(accountBytecode, undefined);
-
-        await new HeaderNavbar(driver).openAccountDetailsModal();
+        // Upgrade Account
+        const headerNavbar = new HeaderNavbar(driver);
+        await headerNavbar.openAccountDetailsModal();
         const accountDetailsModal = new AccountDetailsModal(driver);
         await accountDetailsModal.check_pageIsLoaded();
         await accountDetailsModal.triggerAccountSwitch();
 
         const upgradeAndBatchTxConfirmation = new Eip7702AndSendCalls(driver);
-
         await upgradeAndBatchTxConfirmation.check_expectedTxTypeIsDisplayed(
-          'Smart account',
+          "You're switching to a smart account",
         );
         await upgradeAndBatchTxConfirmation.check_expectedInteractingWithIsDisplayed(
           'Account 1',
         );
+        await increaseGasLimit(driver);
+        await upgradeAndBatchTxConfirmation.clickFooterConfirmButton();
 
-        // Confirm upgrade
+        await driver.switchToWindowWithTitle(
+          WINDOW_TITLES.ExtensionInFullScreenView,
+        );
+        const homePage = new HomePage(driver);
+        await homePage.goToActivityList();
+        let activityList = new ActivityListPage(driver);
+        await activityList.check_confirmedTxNumberDisplayedInActivity(1);
+
+        // Downgrade Account
+        await headerNavbar.openAccountDetailsModal();
+        await accountDetailsModal.check_pageIsLoaded();
+        await accountDetailsModal.triggerAccountSwitch();
+
+        await upgradeAndBatchTxConfirmation.check_expectedTxTypeIsDisplayed(
+          "You're switching back to a standard account (EOA).",
+        );
         await upgradeAndBatchTxConfirmation.clickFooterConfirmButton();
 
         await driver.switchToWindowWithTitle(
           WINDOW_TITLES.ExtensionInFullScreenView,
         );
 
-        // Check the batch transaction has been successfully performed
-        // which included x2 simple sends of 0.001ETH, so the ETH balance should also be updated
-        const homePage = new HomePage(driver);
         await homePage.goToActivityList();
-
-        const activityList = new ActivityListPage(driver);
-        await driver.delay(50000);
-
-        await activityList.check_confirmedTxNumberDisplayedInActivity(1);
-        await homePage.check_expectedBalanceIsDisplayed('24.9998', 'ETH');
-
-        // We check that we have an upgraded account
-        accountBytecode = await localNodes[0].getCode(DEFAULT_FIXTURE_ACCOUNT);
-        assert.strictEqual(
-          accountBytecode,
-          '0xef01008438ad1c834623cff278ab6829a248e37c2d7e3f',
-        );
+        activityList = new ActivityListPage(driver);
+        await activityList.check_confirmedTxNumberDisplayedInActivity(2);
       },
     );
   });
 });
+
+// Function is needed to increase the gas limit for upgrade account confirmation
+// There is apparently an issue with Anvil network that prevents correct estimation of gas limit for upgrade.
+const increaseGasLimit = async (driver: Driver) => {
+  await driver.clickElement('[data-testid="edit-gas-fee-icon"]');
+  await driver.clickElement('[data-testid="edit-gas-fee-item-custom"]');
+
+  await driver.clickElement('[data-testid="advanced-gas-fee-edit"]');
+  await driver.fill('[data-testid="gas-limit-input"]', '50000');
+
+  // Submit gas fee changes
+  await driver.clickElement({ text: 'Save', tag: 'button' });
+  await driver.clickElement({ text: 'Advanced' });
+};
