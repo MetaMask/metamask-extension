ab466d10335e25c1345509a75cea494ce7dbcce9
seaona <seaona@proton.me>
2025-04-29 11:20:18 +0200

    revert snap unrelated changes

--- test/e2e/snaps/test-snap-update.spec.ts
+++ test/e2e/snaps/test-snap-update.spec.ts
@@ -32,6 +32,7 @@ describe('Test Snap update', function () {
         await driver.waitForSelector({ text: 'Update request' });
         await snapInstall.check_pageIsLoaded();
         await snapInstall.updateScrollAndClickConfirmButton();
+        await snapInstall.waitForNextButton();
         await snapInstall.clickNextButton();
 
         // Switch to test snap page and validate the version text
