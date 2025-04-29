5675f287940a352214bb89c0834c972acdd45110
David Murdoch <187813+davidmurdoch@users.noreply.github.com>
2025-04-29 10:45:23 -0400

    lint

--- app/scripts/controllers/swaps/index.ts
+++ app/scripts/controllers/swaps/index.ts
@@ -1083,14 +1083,7 @@ export default class SwapsController extends BaseController<
 
   // Sets the network config from the MetaSwap API.
   private async _setSwapsNetworkConfig(network: Network) {
-    let swapsNetworkConfig: {
-      quotes: number;
-      quotesPrefetching: number;
-      stxGetTransactions: number;
-      stxBatchStatus: number;
-      stxStatusDeadline: number;
-      stxMaxFeeMultiplier: number;
-    } | null = null;
+    let swapsNetworkConfig = null;
 
     try {
       swapsNetworkConfig = await this._fetchSwapsNetworkConfig(network);
@@ -1099,32 +1092,20 @@ export default class SwapsController extends BaseController<
     }
     this.update((_state) => {
       _state.swapsState.swapsQuoteRefreshTime =
-        // TODO: Fix in https://github.com/MetaMask/metamask-extension/issues/31880
-        // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
-        swapsNetworkConfig?.quotes || FALLBACK_QUOTE_REFRESH_TIME;
+        swapsNetworkConfig?.quotes ?? FALLBACK_QUOTE_REFRESH_TIME;
       _state.swapsState.swapsQuotePrefetchingRefreshTime =
-        // TODO: Fix in https://github.com/MetaMask/metamask-extension/issues/31880
-        // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
-        swapsNetworkConfig?.quotesPrefetching || FALLBACK_QUOTE_REFRESH_TIME;
+        swapsNetworkConfig?.quotesPrefetching ?? FALLBACK_QUOTE_REFRESH_TIME;
       _state.swapsState.swapsStxGetTransactionsRefreshTime =
-        // TODO: Fix in https://github.com/MetaMask/metamask-extension/issues/31880
-        // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
-        swapsNetworkConfig?.stxGetTransactions ||
+        swapsNetworkConfig?.stxGetTransactions ??
         FALLBACK_SMART_TRANSACTIONS_REFRESH_TIME;
       _state.swapsState.swapsStxBatchStatusRefreshTime =
-        // TODO: Fix in https://github.com/MetaMask/metamask-extension/issues/31880
-        // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
-        swapsNetworkConfig?.stxBatchStatus ||
+        swapsNetworkConfig?.stxBatchStatus ??
         FALLBACK_SMART_TRANSACTIONS_REFRESH_TIME;
       _state.swapsState.swapsStxMaxFeeMultiplier =
-        // TODO: Fix in https://github.com/MetaMask/metamask-extension/issues/31880
-        // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
-        swapsNetworkConfig?.stxMaxFeeMultiplier ||
+        swapsNetworkConfig?.stxMaxFeeMultiplier ??
         FALLBACK_SMART_TRANSACTIONS_MAX_FEE_MULTIPLIER;
       _state.swapsState.swapsStxStatusDeadline =
-        // TODO: Fix in https://github.com/MetaMask/metamask-extension/issues/31880
-        // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
-        swapsNetworkConfig?.stxStatusDeadline ||
+        swapsNetworkConfig?.stxStatusDeadline ??
         FALLBACK_SMART_TRANSACTIONS_DEADLINE;
     });
   }
