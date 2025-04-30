bde9be558b12b8c6c19fb3a6859ae5532c7491d2
NidhiKJha <menidhikjha@gmail.com>
2025-04-30 20:37:22 +0100

    show test networks only on send flow

--- ui/components/multichain/asset-picker-amount/asset-picker-modal/asset-picker-modal-network.tsx
+++ ui/components/multichain/asset-picker-amount/asset-picker-modal/asset-picker-modal-network.tsx
@@ -306,7 +306,7 @@ export const AssetPickerModalNetwork = ({
             )}
           </Box>
         </Box>
-        {testNetworks.length > 0 ? (
+        {process.env.REMOVE_GNS && testNetworks.length > 0 ? (
           <Box
             className="multichain-asset-picker__network-list"
             display={Display.Flex}
