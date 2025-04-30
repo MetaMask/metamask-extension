d8dd487f5af49e024c8e0f17021e8c54b4aca453
ByteZhang <ByteZhang@protonmail.com>
2025-04-30 20:29:51 +0800

    feat: support onekey

--- ui/pages/routes/routes.component.tsx
+++ ui/pages/routes/routes.component.tsx
@@ -30,6 +30,7 @@ import {
   ImportTokensModal,
 } from '../../components/multichain';
 import Alerts from '../../components/app/alerts';
+import OneKeyErrors from '../onekey-errors';
 
 import {
   ASSET_ROUTE,
@@ -76,6 +77,7 @@ import {
   GATOR_PERMISSIONS,
   TOKEN_TRANSFER_ROUTE,
   REVIEW_GATOR_PERMISSIONS_ROUTE,
+  HARDWARE_ONEKEY_ERRORS_ROUTE,
 } from '../../helpers/constants/routes';
 import { getProviderConfig } from '../../../shared/modules/selectors/networks';
 import {
@@ -824,6 +826,10 @@ export default function Routes() {
             component={NonEvmBalanceCheck}
           />
           <Authenticated path={SHIELD_PLAN_ROUTE} component={ShieldPlan} />
+          <Authenticated
+            path={`${HARDWARE_ONEKEY_ERRORS_ROUTE}/:error`}
+            component={OneKeyErrors}
+          />
           <Authenticated path={DEFAULT_ROUTE} component={Home} />
         </Switch>
       </Suspense>
