6193013dad78db0bbf31460483a7cad91a0ca88f
Matthew Walsh <matthew.walsh@consensys.net>
2025-04-30 16:14:26 +0000

    chore(runway): cherry-pick fix: cp-12.18.0 use simulation gas for future gas fee tokens (#32163)
    ## **Description**
    
    Retrieve the gas for gasless fee transfers from the simulation API,
    rather than relying on `eth_estimateGas` which fails if the token is not
    available until after the user transaction.
    
    [![Open in GitHub
    Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/32163?quickstart=1)
    
    ## **Related issues**
    
    Fixes #32396
    
    ## **Manual testing steps**
    
    ## **Screenshots/Recordings**
    
    ### **Before**
    
    ### **After**
    
    ## **Pre-merge author checklist**
    
    - [x] I've followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
    Extension Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- ui/pages/confirmations/components/confirm/info/shared/gas-fee-token-modal/gas-fee-token-modal.stories.tsx
+++ ui/pages/confirmations/components/confirm/info/shared/gas-fee-token-modal/gas-fee-token-modal.stories.tsx
@@ -9,25 +9,14 @@ import { GasFeeTokenModal } from './gas-fee-token-modal';
 import { genUnapprovedContractInteractionConfirmation } from '../../../../../../../../test/data/confirmations/contract-interaction';
 import { GasFeeToken } from '@metamask/transaction-controller';
 import { toHex } from '@metamask/controller-utils';
-
-const GAS_FEE_TOKEN_MOCK: GasFeeToken = {
-  amount: toHex(1000),
-  balance: toHex(2345),
-  decimals: 3,
-  gas: '0x3',
-  maxFeePerGas: '0x4',
-  maxPriorityFeePerGas: '0x5',
-  rateWei: toHex('1798170000000000000'),
-  recipient: '0x1234567890123456789012345678901234567890',
-  symbol: 'USDC',
-  tokenAddress: '0x1234567890123456789012345678901234567891',
-};
+import { GAS_FEE_TOKEN_MOCK } from '../../../../../../../../test/data/confirmations/gas';
 
 const GAS_FEE_TOKEN_2_MOCK: GasFeeToken = {
   amount: toHex(20000),
   balance: toHex(43210),
   decimals: 4,
   gas: '0x3',
+  gasTransfer: '0x3a',
   maxFeePerGas: '0x4',
   maxPriorityFeePerGas: '0x5',
   rateWei: toHex('1798170000000000000'),
