ba5f8fe61ccc882882f9d60642cd57c19d6650b5
Monte Lai <monte.lai@consensys.net>
2025-05-02 16:47:28 +0800

    fix: test

--- ui/components/multichain/account-list-menu/account-list-menu.test.tsx
+++ ui/components/multichain/account-list-menu/account-list-menu.test.tsx
@@ -18,6 +18,7 @@ import messages from '../../../../app/_locales/en/messages.json';
 import {
   CONFIRMATION_V_NEXT_ROUTE,
   CONNECT_HARDWARE_ROUTE,
+  IMPORT_SRP_ROUTE,
 } from '../../../helpers/constants/routes';
 ///: END:ONLY_INCLUDE_IF
 import { ETH_EOA_METHODS } from '../../../../shared/constants/eth-methods';
@@ -714,7 +715,7 @@ describe('AccountListMenu', () => {
       );
       addAccountButton.click();
 
-      expect(getByTestId('import-srp-container')).toBeInTheDocument();
+      expect(historyPushMock).toHaveBeenCalledWith(IMPORT_SRP_ROUTE);
     });
 
     it('shows srp list if there are multiple srps when adding a new account', async () => {
