9fd340860c106db9a5a4c791020b14dd174ed188
Mathieu Artu <mathieu.artu@consensys.net>
2025-05-06 20:05:00 +0200

    feat: add migration

---
+++ app/scripts/migrations/157.test.ts
@@ -0,0 +1,79 @@
+import { migrate, version, VersionedData } from './157';
+
+const oldVersion = 156;
+
+describe(`migration #${version}`, () => {
+  it('updates the version metadata', async () => {
+    const oldStorage: VersionedData = {
+      meta: { version: oldVersion },
+      data: {},
+    };
+    const newStorage = await migrate(oldStorage);
+    expect(newStorage.meta).toStrictEqual({ version });
+  });
+
+  describe(`migration #${version}`, () => {
+    it('deletes the profile syncing state keys, and replaces them with backup and sync ones', async () => {
+      const oldStorage: VersionedData = {
+        meta: { version: oldVersion },
+        data: {
+          UserStorageController: {
+            isProfileSyncingEnabled: true,
+            isProfileSyncingUpdateInProgress: false,
+          },
+        },
+      };
+      const expectedData = {
+        UserStorageController: {
+          isBackupAndSyncEnabled: true,
+          isBackupAndSyncUpdateLoading: false,
+        },
+      };
+      const newStorage = await migrate(oldStorage);
+
+      expect(newStorage.data).toStrictEqual(expectedData);
+    });
+
+    it('keeps the current value of isProfileSyncingEnabled and copies it to isBackupAndSyncEnabled', async () => {
+      const oldStorage: VersionedData = {
+        meta: { version: oldVersion },
+        data: {
+          UserStorageController: {
+            isProfileSyncingEnabled: false,
+            isProfileSyncingUpdateInProgress: false,
+          },
+        },
+      };
+      const expectedData = {
+        UserStorageController: {
+          isBackupAndSyncEnabled: false,
+          isBackupAndSyncUpdateLoading: false,
+        },
+      };
+      const newStorage = await migrate(oldStorage);
+
+      expect(newStorage.data).toStrictEqual(expectedData);
+    });
+
+    it('sets isBackupAndSyncUpdateLoading to false', async () => {
+      const oldStorage: VersionedData = {
+        meta: { version: oldVersion },
+        data: {
+          UserStorageController: {
+            isProfileSyncingEnabled: true,
+            isProfileSyncingUpdateInProgress: true,
+          },
+        },
+      };
+      const expectedData = {
+        UserStorageController: {
+          isBackupAndSyncEnabled: true,
+          isBackupAndSyncUpdateLoading: false,
+        },
+      };
+      const newStorage = await migrate(oldStorage);
+
+      expect(newStorage.data).toStrictEqual(expectedData);
+    });
+  });
+});
