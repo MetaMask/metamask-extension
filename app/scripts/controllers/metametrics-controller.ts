cbf557e51efb6e3b5d45bfdb750ce5f4d116e2ae
Frederik Bolding <frederik.bolding@gmail.com>
2025-05-07 12:29:44 +0200

    chore: Make deprecated category property optional

--- app/scripts/controllers/metametrics-controller.ts
+++ app/scripts/controllers/metametrics-controller.ts
@@ -546,11 +546,11 @@ export default class MetaMetricsController extends BaseController<
   createEventFragment(
     options: Omit<MetaMetricsEventFragment, 'id'>,
   ): MetaMetricsEventFragment {
-    if (!options.successEvent || !options.category) {
+    if (!options.successEvent) {
       throw new Error(
-        `Must specify success event and category. Success event was: ${
+        `Must specify success event. Success event was: ${
           options.event
-        }. Category was: ${options.category}. Payload keys were: ${Object.keys(
+        }. Payload keys were: ${Object.keys(
           options,
         )}. ${
           typeof options.properties === 'object'
@@ -994,11 +994,11 @@ export default class MetaMetricsController extends BaseController<
    */
   #validatePayload(payload: MetaMetricsEventPayload): void {
     // event and category are required fields for all payloads
-    if (!payload.event || !payload.category) {
+    if (!payload.event) {
       throw new Error(
-        `Must specify event and category. Event was: ${
+        `Must specify event. Event was: ${
           payload.event
-        }. Category was: ${payload.category}. Payload keys were: ${Object.keys(
+        }. Payload keys were: ${Object.keys(
           payload,
         )}. ${
           typeof payload.properties === 'object'
