cd133f192365d695918c8cd4166b83baba8af032
Javier Garcia Vera <javier.vera@consensys.net>
2025-05-07 09:55:12 +0200

    fix: skipped test

--- test/e2e/tests/bridge/bridge-cross-chain-swap.spec.ts
+++ test/e2e/tests/bridge/bridge-cross-chain-swap.spec.ts
@@ -16,6 +16,7 @@ import { DEFAULT_FEATURE_FLAGS_RESPONSE } from './constants';
 
 describe('Bridge tests', function (this: Suite) {
   this.timeout(160000); // This test is very long, so we need an unusually high timeout
+  // eslint-disable-next-line mocha/no-skipped-tests
   it.skip('Execute multiple bridge transactions', async function () {
     await withFixtures(
       getBridgeFixtures(
