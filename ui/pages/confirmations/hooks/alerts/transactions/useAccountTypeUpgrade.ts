7866b32d041b051bab9ee27b2d59e6fef15ecd07
Jyoti Puri <jyotipuri@gmail.com>
2025-05-08 15:56:30 +0530

    update

--- ui/pages/confirmations/hooks/alerts/transactions/useAccountTypeUpgrade.ts
+++ ui/pages/confirmations/hooks/alerts/transactions/useAccountTypeUpgrade.ts
@@ -1,7 +1,5 @@
 import { useMemo } from 'react';
-import { TransactionMeta } from '@metamask/transaction-controller';
 import { useI18nContext } from '../../../../../hooks/useI18nContext';
-import { useConfirmContext } from '../../../context/confirm';
 import { Alert } from '../../../../../ducks/confirm-alerts/confirm-alerts';
 import { RowAlertKey } from '../../../../../components/app/confirm/info/row/constants';
 import { Severity } from '../../../../../helpers/constants/design-system';
