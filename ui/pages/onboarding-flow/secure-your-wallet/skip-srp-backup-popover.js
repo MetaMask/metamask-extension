e095c09cb4c0b2cafd0594291c4ed6647df75be2
Lionell Briones <llenoil@gmail.com>
2025-05-10 00:24:36 +0800

    feat: updated secure your wallet page UI

--- ui/pages/onboarding-flow/secure-your-wallet/skip-srp-backup-popover.js
+++ ui/pages/onboarding-flow/secure-your-wallet/skip-srp-backup-popover.js
@@ -1,122 +1,150 @@
-import React, { useState, useContext } from 'react';
 import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import React, { useContext, useState } from 'react';
 import { useI18nContext } from '../../../hooks/useI18nContext';
-import Button from '../../../components/ui/button';
-import Popover from '../../../components/ui/popover';
-import Box from '../../../components/ui/box';
-import {
-  Text,
-  Icon,
-  IconName,
-  IconSize,
-} from '../../../components/component-library';
 import {
   AlignItems,
+  Display,
   IconColor,
-  FLEX_DIRECTION,
-  JustifyContent,
+  TextAlign,
   TextVariant,
 } from '../../../helpers/constants/design-system';
-import { setSeedPhraseBackedUp } from '../../../store/actions';
-import Checkbox from '../../../components/ui/check-box';
-import { ONBOARDING_COMPLETION_ROUTE } from '../../../helpers/constants/routes';
+import {
+  Box,
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  ButtonSize,
+  Checkbox,
+  Button,
+  ButtonVariant,
+  Icon,
+  IconSize,
+  IconName,
+} from '../../../components/component-library';
+import { MetaMetricsContext } from '../../../contexts/metametrics';
 import {
   MetaMetricsEventCategory,
   MetaMetricsEventName,
 } from '../../../../shared/constants/metametrics';
-import { MetaMetricsContext } from '../../../contexts/metametrics';
 import { getHDEntropyIndex } from '../../../selectors/selectors';
+import { setSeedPhraseBackedUp } from '../../../store/actions';
+import {
+  ONBOARDING_COMPLETION_ROUTE,
+  ONBOARDING_METAMETRICS,
+} from '../../../helpers/constants/routes';
+// eslint-disable-next-line import/no-restricted-paths
+import { getPlatform } from '../../../../app/scripts/lib/util';
+import { PLATFORM_FIREFOX } from '../../../../shared/constants/app';
 
-export default function SkipSRPBackup({ handleClose }) {
+export default function SkipSRPBackup({ onClose, secureYourWallet }) {
   const [checked, setChecked] = useState(false);
   const t = useI18nContext();
-  const history = useHistory();
   const dispatch = useDispatch();
   const hdEntropyIndex = useSelector(getHDEntropyIndex);
   const trackEvent = useContext(MetaMetricsContext);
+  const history = useHistory();
+
+  async function onSkipSrpBackup() {
+    await dispatch(setSeedPhraseBackedUp(false));
+    trackEvent({
+      category: MetaMetricsEventCategory.Onboarding,
+      event: MetaMetricsEventName.OnboardingWalletSecuritySkipConfirmed,
+      properties: {
+        hd_entropy_index: hdEntropyIndex,
+      },
+    });
+
+    if (getPlatform() === PLATFORM_FIREFOX) {
+      history.push(ONBOARDING_COMPLETION_ROUTE);
+    } else {
+      history.push(ONBOARDING_METAMETRICS);
+    }
+  }
 
   return (
-    <Popover
-      className="skip-srp-backup-popover"
-      footer={
-        <Box
-          className="skip-srp-backup-popover__footer"
-          justifyContent={JustifyContent.center}
-          alignItems={AlignItems.center}
-        >
-          <Button
-            onClick={() => {
-              trackEvent({
-                category: MetaMetricsEventCategory.Onboarding,
-                event:
-                  MetaMetricsEventName.OnboardingWalletSecuritySkipCanceled,
-                properties: {
-                  hd_entropy_index: hdEntropyIndex,
-                },
-              });
-              handleClose();
-            }}
-            type="secondary"
-            rounded
-          >
-            {t('goBack')}
-          </Button>
-          <Button
-            data-testid="skip-srp-backup"
-            disabled={!checked}
-            type="primary"
-            rounded
-            onClick={async () => {
-              await dispatch(setSeedPhraseBackedUp(false));
-              trackEvent({
-                category: MetaMetricsEventCategory.Onboarding,
-                event:
-                  MetaMetricsEventName.OnboardingWalletSecuritySkipConfirmed,
-                properties: {
-                  hd_entropy_index: hdEntropyIndex,
-                },
-              });
-              history.push(ONBOARDING_COMPLETION_ROUTE);
-            }}
-          >
-            {t('skip')}
-          </Button>
-        </Box>
-      }
+    <Modal
+      isOpen
+      onClose={onClose}
+      className="skip-srp-backup-modal"
+      data-testid="skip-srp-backup-modal"
     >
-      <Box
-        flexDirection={FLEX_DIRECTION.COLUMN}
-        alignItems={AlignItems.center}
-        justifyContent={JustifyContent.center}
-        margin={4}
-      >
-        <Icon
-          name={IconName.Danger}
-          size={IconSize.Xl}
-          className="skip-srp-backup-popover__icon"
-          color={IconColor.errorDefault}
-        />
-        <Text variant={TextVariant.headingMd}>{t('skipAccountSecurity')}</Text>
-        <Box justifyContent={JustifyContent.center} margin={3}>
-          <label className="skip-srp-backup-popover__label">
-            <Checkbox
-              className="skip-srp-backup-popover__checkbox"
-              onClick={() => setChecked(!checked)}
-              checked={checked}
-              dataTestId="skip-srp-backup-popover-checkbox"
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader onClose={onClose}>
+          <Box textAlign={TextAlign.Center}>
+            <Icon
+              name={IconName.Danger}
+              size={IconSize.Xl}
+              className="skip-srp-backup-popover__icon"
+              color={IconColor.errorDefault}
             />
-            <Text className="skip-srp-backup-popover__details">
-              {t('skipAccountSecurityDetails')}
+            <Text
+              variant={TextVariant.headingMd}
+              textAlign={TextAlign.Center}
+              marginTop={4}
+              as="h3"
+            >
+              {t('skipAccountSecurity')}
             </Text>
-          </label>
+          </Box>
+        </ModalHeader>
+        <Box paddingLeft={4} paddingRight={4}>
+          <Checkbox
+            id="skip-srp-backup__checkbox"
+            className="skip-srp-backup__checkbox"
+            data-testid="skip-srp-backup-checkbox"
+            isChecked={checked}
+            alignItems={AlignItems.flexStart}
+            onChange={() => {
+              setChecked(!checked);
+            }}
+            label={
+              <Text variant={TextVariant.bodySmMedium}>
+                {t('skipAccountSecurityDetails')}
+              </Text>
+            }
+          />
+          <Box display={Display.Flex} marginTop={6} gap={4}>
+            <Button
+              variant={ButtonVariant.Secondary}
+              size={ButtonSize.Lg}
+              onClick={() => {
+                trackEvent({
+                  category: MetaMetricsEventCategory.Onboarding,
+                  event:
+                    MetaMetricsEventName.OnboardingWalletSecuritySkipCanceled,
+                  properties: {
+                    hd_entropy_index: hdEntropyIndex,
+                  },
+                });
+                secureYourWallet();
+              }}
+              block
+            >
+              {t('skipAccountSecuritySecureNow')}
+            </Button>
+            <Button
+              data-testid="skip-srp-backup"
+              size={ButtonSize.Lg}
+              disabled={!checked}
+              onClick={onSkipSrpBackup}
+              block
+              danger
+            >
+              {t('skipAccountSecuritySkip')}
+            </Button>
+          </Box>
         </Box>
-      </Box>
-    </Popover>
+      </ModalContent>
+    </Modal>
   );
 }
 
 SkipSRPBackup.propTypes = {
-  handleClose: PropTypes.func,
+  onClose: PropTypes.func.isRequired,
+  secureYourWallet: PropTypes.func.isRequired,
 };
