53d5d03c7f58f4c33c654bba26939a365f2b994a
lwin <lwin@tor.us>
2025-05-11 20:38:25 +0800

    feat: added OAuth Controller and login handlers

---
+++ app/scripts/controllers/oauth/base-login-handler.ts
@@ -0,0 +1,119 @@
+import { AuthConnection } from '@metamask/seedless-onboarding-controller';
+import { LoginHandlerOptions, AuthTokenResponse, OAuthUserInfo } from './types';
+
+/**
+ * Pads a string to a length of 4 characters
+ *
+ * @param input - The base64 encoded string to pad
+ * @returns The padded string
+ */
+function padBase64String(input: string) {
+  const segmentLength = 4;
+  const stringLength = input.length;
+  const diff = stringLength % segmentLength;
+  if (!diff) {
+    return input;
+  }
+  let position = stringLength;
+  let padLength = segmentLength - diff;
+  const paddedStringLength = stringLength + padLength;
+  const buffer = Buffer.alloc(paddedStringLength);
+  buffer.write(input);
+  while (padLength > 0) {
+    buffer.write('=', position);
+    position += 1;
+    padLength -= 1;
+  }
+  return buffer.toString();
+}
+
+export abstract class BaseLoginHandler {
+  public options: LoginHandlerOptions;
+
+  public nonce: string;
+
+  constructor(options: LoginHandlerOptions) {
+    this.options = options;
+    this.nonce = this.#generateNonce();
+  }
+
+  abstract get authConnection(): AuthConnection;
+
+  abstract get scope(): string[];
+
+  /**
+   * Generate the Auth URL to initiate the OAuth login to get the Authorization Code.
+   *
+   * @returns The URL to initiate the OAuth login.
+   */
+  abstract getAuthUrl(): string;
+
+  /**
+   * Get the JWT Token from the Web3Auth Authentication Server.
+   *
+   * @param code - The authorization code from the social login provider.
+   * @returns The JWT Token from the Web3Auth Authentication Server.
+   */
+  abstract getAuthIdToken(code: string): Promise<AuthTokenResponse>;
+
+  /**
+   * Generate the request body data to get the JWT Token from the Web3Auth Authentication Server.
+   *
+   * @param code - The authorization code from the social login provider.
+   * @returns The request data for the Web3Auth Authentication Server.
+   */
+  abstract generateAuthTokenRequestData(code: string): string;
+
+  /**
+   * Get the user's information from the JWT Token.
+   *
+   * @param idToken - The JWT Token from the Web3Auth Authentication Server.
+   * @returns The user's information from the JWT Token.
+   */
+  abstract getUserInfo(idToken: string): Promise<OAuthUserInfo>;
+
+  /**
+   * Make a request to the Web3Auth Authentication Server to get the JWT Token.
+   *
+   * @param requestData - The request data for the Web3Auth Authentication Server.
+   * @returns The JWT Token from the Web3Auth Authentication Server.
+   */
+  protected async requestAuthToken(
+    requestData: string,
+  ): Promise<AuthTokenResponse> {
+    const res = await fetch(
+      `${this.options.authServerUrl}/api/v1/oauth/token`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: requestData,
+      },
+    );
+
+    const data = await res.json();
+    return data;
+  }
+
+  /**
+   * Decode the JWT Token to get the user's information.
+   *
+   * @param idToken - The JWT Token from the Web3Auth Authentication Server.
+   * @returns The user's information from the JWT Token.
+   */
+  protected decodeIdToken(idToken: string): string {
+    const [, idTokenPayload] = idToken.split('.');
+    const base64String = padBase64String(idTokenPayload)
+      .replace(/-/u, '+')
+      .replace(/_/u, '/');
+    // Using buffer here instead of atob because userinfo can contain emojis which are not supported by atob
+    // the browser replacement for atob is https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Uint8Array/fromBase64
+    // which is not supported in all chrome yet
+    return Buffer.from(base64String, 'base64').toString('utf-8');
+  }
+
+  #generateNonce(): string {
+    return Math.random().toString(16).substring(2, 15);
+  }
+}
---
+++ app/scripts/controllers/oauth/types.ts
@@ -0,0 +1,147 @@
+import {
+  ControllerGetStateAction,
+  ControllerStateChangeEvent,
+  RestrictedMessenger,
+} from '@metamask/base-controller';
+import {
+  Web3AuthNetwork,
+  AuthConnection,
+} from '@metamask/seedless-onboarding-controller';
+
+export const controllerName = 'OAuthController';
+
+export type LoginHandlerOptions = {
+  oAuthClientId: string;
+  authServerUrl: string;
+  web3AuthNetwork: Web3AuthNetwork;
+  redirectUri: string;
+  scopes?: string[];
+  /**
+   * The server redirect URI to use for the OAuth login.
+   * This is the URI that the OAuth provider will redirect to after the user has logged in.
+   * This is required for Apple login.
+   */
+  serverRedirectUri?: string;
+};
+
+/**
+ * The state of the {@link OAuthController}
+ */
+export type OAuthControllerState = {
+  // OAuth Controller is stateless
+};
+
+export type OAuthControllerGetStateAction = ControllerGetStateAction<
+  typeof controllerName,
+  OAuthControllerState
+>;
+
+export type OAuthControllerActions = OAuthControllerGetStateAction;
+
+export type OAuthControllerStateChangeEvent = ControllerStateChangeEvent<
+  typeof controllerName,
+  OAuthControllerState
+>;
+
+export type OAuthControllerControllerEvents = OAuthControllerStateChangeEvent;
+
+/**
+ * Actions that this controller is allowed to call.
+ */
+export type AllowedActions = never;
+
+/**
+ * Events that this controller is allowed to subscribe.
+ */
+export type AllowedEvents = never;
+
+export type OAuthControllerMessenger = RestrictedMessenger<
+  typeof controllerName,
+  OAuthControllerActions | AllowedActions,
+  OAuthControllerControllerEvents | AllowedEvents,
+  AllowedActions['type'],
+  AllowedEvents['type']
+>;
+
+/**
+ * The configuration to initiate the OAuth login and get the Authorization Code.
+ *
+ * - clientId: string - the client ID of the OAuth provider
+ * - redirectUri: string - the redirect URI of the OAuth provider
+ * - serverRedirectUri: string - the server redirect URI of the OAuth provider, to be used for Apple login
+ * - scopes: string[] - the scopes of the OAuth provider
+ */
+export type OAuthProviderConfig = {
+  clientId: string;
+  redirectUri?: string;
+  /** for apple, we need to redirect to a server endpoint that will handle the post request and redirect back to client */
+  serverRedirectUri?: string;
+  scopes?: string[];
+};
+
+export type OAuthLoginEnv = {
+  authConnectionId: string;
+  groupedAuthConnectionId: string;
+  googleClientId: string;
+  appleClientId: string;
+  authServerUrl: string;
+  web3AuthNetwork: Web3AuthNetwork;
+  serverRedirectUri?: string;
+};
+
+export type OAuthControllerOptions = {
+  messenger: OAuthControllerMessenger;
+
+  /**
+   * The environment variables required for the OAuth login and get JWT Token.
+   */
+  env: OAuthLoginEnv;
+};
+
+/**
+ * The response from the Web3Auth Authentication Server to get the JWT Token.
+ *
+ * The response is a JSON object with the following properties:
+ * - success: boolean - whether the request was successful
+ * - message: string - the message from the Web3Auth Authentication Server
+ * - jwt_tokens: Record<string, string> - the JWT Tokens issued from the Web3Auth Authentication Server
+ */
+export type AuthTokenResponse = {
+  success: boolean;
+  message: string;
+  /**
+   * The JWT Tokens issued from the Web3Auth Authentication Server.
+   * The key is the audience value and the value is the JWT Token.
+   */
+  jwt_tokens: Record<string, string>;
+};
+
+/**
+ * The result of the OAuth login.
+ *
+ * This is the return value of the {@link OAuthController.startOAuthLogin} method.
+ * It contains the user's information and the JWT Tokens issued from the Web3Auth Authentication Server.
+ *
+ * - authConnection: AuthConnection - the social login type
+ * - authConnectionId: string - the ID of the social login type
+ * - groupedAuthConnectionId: string - the ID of the grouped social login type
+ * - userId: string - the user's ID
+ * - idTokens: string[] - the JWT Tokens issued from the Web3Auth Authentication Server
+ * - socialLoginEmail: string - the email of the user
+ */
+export type OAuthLoginResult = {
+  authConnection: AuthConnection;
+  authConnectionId: string;
+  groupedAuthConnectionId: string;
+  userId: string;
+  idTokens: string[];
+  socialLoginEmail: string;
+};
+
+/**
+ * The user's information extracted from the JWT Token.
+ */
+export type OAuthUserInfo = {
+  email: string;
+  sub: string;
+};
