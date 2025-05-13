9d00796ab4dbaf9b2214d07f4c20e13636b3708c
Harika <153644847+hjetpoluru@users.noreply.github.com>
2025-05-13 08:19:32 -0400

    test: Migrating snaps pom (#32251)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    Migrating snaps tests to pom
    
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    2. What is the improvement/solution?
    -->
    
    [![Open in GitHub
    Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/32251?quickstart=1)
    
    ## **Related issues**
    
    Fixes:
    
    
    
    ## **Manual testing steps**
    
    yarn build:test:webpack
    ENABLE_MV3=false yarn test:e2e:single
    test/e2e/snaps/test-snap-managestate.spec.ts --browser=chrome
    —leaving-running
    ENABLE_MV3=false yarn test:e2e:single
    test/e2e/snaps/test-snap-jsx.spec.ts --browser=chrome —leaving-running
    ENABLE_MV3=false yarn test:e2e:single
    test/e2e/snaps/test-snap-networkaccess.spec.ts --browser=chrome
    —leaving-running
    
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [x] I've followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
    Extension Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

---
+++ test/e2e/snaps/test-snap-jsx.spec.ts
@@ -0,0 +1,34 @@
+import { withFixtures, WINDOW_TITLES } from '../helpers';
+import FixtureBuilder from '../fixture-builder';
+import { TestSnaps } from '../page-objects/pages/test-snaps';
+import { Driver } from '../webdriver/driver';
+import { loginWithoutBalanceValidation } from '../page-objects/flows/login.flow';
+import { openTestSnapClickButtonAndInstall } from '../page-objects/flows/install-test-snap.flow';
+
+describe('Test Snap JSX', function () {
+  it('can use JSX for snap dialog', async function () {
+    await withFixtures(
+      {
+        fixtures: new FixtureBuilder().build(),
+        title: this.test?.fullTitle(),
+      },
+      async ({ driver }: { driver: Driver }) => {
+        await loginWithoutBalanceValidation(driver);
+        const testSnaps = new TestSnaps(driver);
+
+        // Open the test snaps page
+        await openTestSnapClickButtonAndInstall(driver, 'connectjsxButton');
+        await testSnaps.check_installationComplete(
+          'connectjsxButton',
+          'Reconnect to JSX Snap',
+        );
+
+        await testSnaps.clickButton('displayJsxButton');
+        await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
+        await testSnaps.check_Count('0');
+        await testSnaps.clickButton('incrementButton');
+        await testSnaps.check_Count('1');
+      },
+    );
+  });
+});
