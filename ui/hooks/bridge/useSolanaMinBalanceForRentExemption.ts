a33168143fb87008ad7d0ec5c49eac7f5e55c89d
Micaela Estabillo <micaela.estabillo@consensys.net>
2025-05-15 17:07:12 -0700

    chore: refresh min balance on quote and chain change

--- ui/hooks/bridge/useSolanaMinBalanceForRentExemption.ts
+++ ui/hooks/bridge/useSolanaMinBalanceForRentExemption.ts
@@ -1,18 +1,20 @@
 import { useEffect } from 'react';
 import { isSolanaChainId } from '@metamask/bridge-controller';
 import { useDispatch, useSelector } from 'react-redux';
-import { getFromChain } from '../../ducks/bridge/selectors';
+import { getBridgeQuotes, getQuoteRequest } from '../../ducks/bridge/selectors';
 import { setMinimumBalanceForRentExemptionInLamports } from '../../ducks/bridge/bridge';
 
 const useSolanaMinBalanceForRentExemption = () => {
   const dispatch = useDispatch();
-  const fromChain = useSelector(getFromChain);
+  const { srcChainId } = useSelector(getQuoteRequest);
+  const { activeQuote } = useSelector(getBridgeQuotes);
 
+  // Update minimum balance on load, srcChain change, or activeQuote refresh
   useEffect(() => {
-    if (fromChain?.chainId && isSolanaChainId(fromChain.chainId)) {
+    if (srcChainId && isSolanaChainId(srcChainId)) {
       dispatch(setMinimumBalanceForRentExemptionInLamports());
     }
-  }, [dispatch, fromChain?.chainId]);
+  }, [dispatch, srcChainId, activeQuote]);
 };
 
 export default useSolanaMinBalanceForRentExemption;
