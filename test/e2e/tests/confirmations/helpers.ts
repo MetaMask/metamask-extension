0e6ddb3f3894e4bcb23c8e333a353cce1b9dcff9
Matthew Walsh <matthew.walsh@consensys.net>
2025-05-19 09:35:54 +0100

    test: add e2e tests for gas fee token transactions via EIP-7702 (#32435)
    ## **Description**
    
    Add E2E tests for gas fee token transactions via EIP-7702.
    
    [![Open in GitHub
    Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/32435?quickstart=1)
    
    ## **Related issues**
    
    ## **Manual testing steps**
    
    ## **Screenshots/Recordings**
    
    ### **Before**
    
    ### **After**
    
    ## **Pre-merge author checklist**
    
    - [x] I've followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
    Extension Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- test/e2e/tests/confirmations/helpers.ts
+++ test/e2e/tests/confirmations/helpers.ts
@@ -199,8 +199,15 @@ export async function mockEip7702FeatureFlag(mockServer: Mockttp) {
                         '0x4c15775d0c6d5bd37a7aa7aafc62e85597ea705024581b8b5cb0edccc4e6a69e26c495b3ae725815a377c9789bff43bf19e4dd1eaa679e65133e49ceee3ea87f1b',
                     },
                   ],
+                  '0x1': [
+                    {
+                      address: '0xabcabcabcabcabcabcabcabcabcabcabcabcabca',
+                      signature:
+                        '0x5b394cc656b760fc15e855f9b8b9d0eec6337328361771c696d7f5754f0348e06298d34243e815ff8b5ce869e5f310c37dd100c1827e91b56bb208d1fafcf3a71c',
+                    },
+                  ],
                 },
-                supportedChains: ['0xaa36a7', '0x539'],
+                supportedChains: ['0xaa36a7', '0x539', '0x1'],
               },
             },
           ],
