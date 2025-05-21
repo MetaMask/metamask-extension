f20aec876259568a582088b7117d98e145fe0d20
Nguyen Anh Tu <tunguyen.bhtech@gmail.com>
2025-05-21 16:15:36 +0700

    feat: check pw outdated on lock app

--- ui/store/actions.ts
+++ ui/store/actions.ts
@@ -2103,6 +2103,15 @@ export function lockMetamask(): ThunkAction<
     dispatch(showLoadingIndication());
 
     return backgroundSetLocked()
+      .then(() => {
+        // check seedless password outdated when lock app
+        dispatch(checkIsSeedlessPasswordOutdated(true));
+        return Promise.resolve();
+      })
+      .catch((error) => {
+        log.error('Metamask - seedless password outdated check error', error);
+        return Promise.resolve();
+      })
       .then(() => forceUpdateMetamaskState(dispatch))
       .catch((error) => {
         dispatch(displayWarning(getErrorMessage(error)));
