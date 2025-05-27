18c5f5930ff6393ccbbb09c6c4c4a9264a1a8853
Frederik Bolding <frederik.bolding@gmail.com>
2025-05-27 16:05:54 +0200

    feat: Add active flag for snap_getClientStatus

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -6969,6 +6969,9 @@ export default class MetamaskController extends EventEmitter {
         getIsLocked: () => {
           return !this.appStateController.isUnlocked();
         },
+        getIsActive: () => {
+          return this._isClientOpen;
+        },
         getInterfaceState: (...args) =>
           this.controllerMessenger.call(
             'SnapInterfaceController:getInterface',
