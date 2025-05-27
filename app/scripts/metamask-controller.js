250cf2cc67104780c4f9c9c6ac51afc40d3e7cd0
Frederik Bolding <frederik.bolding@gmail.com>
2025-05-27 16:50:32 +0200

    chore: Add hooks for snap_getClientStatus

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -6815,6 +6815,9 @@ export default class MetamaskController extends EventEmitter {
         getIsLocked: () => {
           return !this.appStateController.isUnlocked();
         },
+        getIsActive: () => {
+          return this._isClientOpen;
+        },
         getInterfaceState: (...args) =>
           this.controllerMessenger.call(
             'SnapInterfaceController:getInterface',
