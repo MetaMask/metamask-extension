a329050ce0f6b0b1d47945e66373ce6d88e6c3c3
sahar-fehri <sahar.fehri@consensys.net>
2025-05-29 12:52:12 +0200

    fix: fix checkAndUpdateAllNftsOwnershipStatus

--- ui/components/app/assets/asset-list/asset-list-control-bar/asset-list-control-bar.tsx
+++ ui/components/app/assets/asset-list/asset-list-control-bar/asset-list-control-bar.tsx
@@ -49,8 +49,7 @@ import {
 } from '../../../../../../shared/constants/app';
 import NetworkFilter from '../network-filter';
 import {
-  // TODO: fix this call
-  // checkAndUpdateAllNftsOwnershipStatus,
+  checkAndUpdateAllNftsOwnershipStatus,
   detectNfts,
   detectTokens,
   setTokenNetworkFilter,
@@ -97,6 +96,16 @@ const AssetListControlBar = ({
   const [isNetworkFilterPopoverOpen, setIsNetworkFilterPopoverOpen] =
     useState(false);
   const [isImportNftPopoverOpen, setIsImportNftPopoverOpen] = useState(false);
+
+  const allNetworkClientIds = useMemo(() => {
+    return Object.keys(tokenNetworkFilter).flatMap((chainId) => {
+      const entry = allNetworks[chainId as `0x${string}`];
+      if (!entry) return [];
+      const index = entry.defaultRpcEndpointIndex;
+      const endpoint = entry.rpcEndpoints[index];
+      return endpoint?.networkClientId ? [endpoint.networkClientId] : [];
+    });
+  }, [tokenNetworkFilter, allNetworks]);
 
   const shouldShowRefreshButtons = useMemo(
     () =>
@@ -233,7 +242,10 @@ const AssetListControlBar = ({
     if (isMainnet || isLineaMainnet) {
       dispatch(detectNfts(allChainIds));
     }
-    // checkAndUpdateAllNftsOwnershipStatus();
+    // loop through allNetworkClientIds and call checkAndUpdateAllNftsOwnershipStatus for each one
+    allNetworkClientIds.forEach((networkClientId) => {
+      checkAndUpdateAllNftsOwnershipStatus(networkClientId);
+    });
   };
   const isDisabled = useMemo(() => {
     const isPopularNetwork = FEATURED_NETWORK_CHAIN_IDS.includes(
