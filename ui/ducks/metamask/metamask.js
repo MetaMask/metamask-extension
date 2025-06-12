0d367e1659877662b9879e6fa50de54c607b2397
salimtb <salim.toubal@outlook.com>
2025-06-12 11:49:06 +0200

    fix: fix linter

--- ui/ducks/metamask/metamask.js
+++ ui/ducks/metamask/metamask.js
@@ -250,8 +250,7 @@ export const getNfts = (state) => {
   return allNfts?.[selectedAddress]?.[chainId] ?? [];
 };
 
-export const
-getAllNfts = (state) => {
+export const getAllNfts = (state) => {
   const {
     metamask: { allNfts },
   } = state;
