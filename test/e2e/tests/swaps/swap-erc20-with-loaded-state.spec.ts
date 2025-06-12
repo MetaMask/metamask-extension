5e89334592c970111ca32de105bc1dd9158844e1
seaona <seaona@proton.me>
2025-06-12 17:39:19 +0200

    fix

--- test/e2e/tests/swaps/swap-erc20-with-loaded-state.spec.ts
+++ test/e2e/tests/swaps/swap-erc20-with-loaded-state.spec.ts
@@ -212,7 +212,6 @@ describe('Swap', function () {
       sourceAmount: '10',
       expectedWethBalance: '40',
       expectedEthBalance: '34.99991',
-      dismissWarning: false,
     },
     {
       name: 'should swap ETH to WETH',
@@ -221,7 +220,6 @@ describe('Swap', function () {
       sourceAmount: '10',
       expectedWethBalance: '60',
       expectedEthBalance: '14.99992',
-      dismissWarning: true,
     },
   ];
 
@@ -298,11 +296,7 @@ describe('Swap', function () {
           await swapPage.enterSwapAmount(testCase.sourceAmount);
           await swapPage.selectDestinationToken(testCase.destinationToken);
 
-          // https://github.com/MetaMask/metamask-extension/issues/31426
-          if (testCase.dismissWarning) {
-            await swapPage.dismissManualTokenWarning();
-          }
-          await driver.delay(1500);
+          await swapPage.dismissManualTokenWarning();
           await swapPage.submitSwap();
 
           await homePage.check_expectedTokenBalanceIsDisplayed(
