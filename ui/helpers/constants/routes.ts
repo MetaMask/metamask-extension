7dcea8dbf985dec9dd5538ba77b8ec7cf712e5e2
David Murdoch <187813+davidmurdoch@users.noreply.github.com>
2025-06-12 17:11:23 -0400

    woops

--- ui/helpers/constants/routes.ts
+++ ui/helpers/constants/routes.ts
@@ -340,8 +340,3 @@ export const ONBOARDING_METAMETRICS = '/onboarding/metametrics';
 export const INITIALIZE_EXPERIMENTAL_AREA = '/initialize/experimental-area';
 export const ONBOARDING_EXPERIMENTAL_AREA = '/onboarding/experimental-area';
 ///: END:ONLY_INCLUDE_IF
-
-export const DEEP_LINK_ROUTE = '/link';
-PATH_NAME_MAP.set(DEEP_LINK_ROUTE, 'Deep link Redirect Page');
-
-export const getAllPathRoutes = () => Array.from(PATH_NAME_MAP.keys());
