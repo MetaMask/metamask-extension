30f36b325d7809c1b6afa40f41aa482a55dcbc77
Nicholas Gambino <nicholas.gambino@consensys.net>
2025-06-12 10:53:38 -0700

    fix: Scroll

--- ui/components/multichain/network-manager/network-tabs.tsx
+++ ui/components/multichain/network-manager/network-tabs.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 import { hideModal } from '../../../store/actions';
-import { ModalHeader } from '../../component-library';
+import { ModalHeader, ModalBody } from '../../component-library';
 import { Tab, Tabs } from '../../ui/tabs';
 import { CustomNetworks } from './components/custom-networks';
 import { DefaultNetworks } from './components/default-networks';
@@ -17,23 +17,25 @@ export const NetworkTabs = ({ initialTab }: { initialTab: string }) => {
       <ModalHeader onBack={handleClose} onClose={handleClose}>
         Networks
       </ModalHeader>
-      <Tabs
-        defaultActiveTabKey={initialTab}
-        onTabClick={() => {
-          // Tab click handler - intentionally empty for now
-        }}
-        tabListProps={{
-          className: 'network-manager__tab-list',
-        }}
-        padding={4}
-      >
-        <Tab tabKey="networks" name="Default">
-          <DefaultNetworks />
-        </Tab>
-        <Tab tabKey="custom-networks" name="Custom">
-          <CustomNetworks />
-        </Tab>
-      </Tabs>
+      <ModalBody>
+        <Tabs
+          defaultActiveTabKey={initialTab}
+          onTabClick={() => {
+            // Tab click handler - intentionally empty for now
+          }}
+          tabListProps={{
+            className: 'network-manager__tab-list',
+          }}
+          padding={4}
+        >
+          <Tab tabKey="networks" name="Default">
+            <DefaultNetworks />
+          </Tab>
+          <Tab tabKey="custom-networks" name="Custom">
+            <CustomNetworks />
+          </Tab>
+        </Tabs>
+      </ModalBody>
     </>
   );
 };
