fe0470781fd913f0734dd2a67c095186b597d570
david0xd <david@timechaser.org>
2025-06-13 10:41:26 +0200

    Add Multichain Account Tree component

---
+++ ui/components/multichain-accounts/multichain-accounts-tree/multichain-accounts-tree.test.tsx
@@ -0,0 +1,143 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { EthAccountType, EthScope } from '@metamask/keyring-api';
+import { ETH_EOA_METHODS } from '../../../../shared/constants/eth-methods';
+import { renderWithProvider } from '../../../../test/lib/render-helpers';
+import configureStore from '../../../store/store';
+import mockState from '../../../../test/data/mock-state.json';
+import { MultichainAccountsTree } from '.';
+
+const mockWalletAccountCollection = {
+  'wallet-1': {
+    metadata: {
+      name: 'Wallet 1',
+    },
+    groups: {
+      'group-1': {
+        accounts: [
+          {
+            address: '0x0dcd5d886577d5081b0c52e242ef29e70be3e7bc',
+            id: 'account-1',
+            metadata: {
+              name: 'Account 1',
+              keyring: {
+                type: 'HD Key Tree',
+              },
+            },
+            options: {},
+            methods: ETH_EOA_METHODS,
+            scopes: [EthScope.Eoa],
+            type: EthAccountType.Eoa,
+            balance: '0x0',
+          },
+          {
+            address: '0x123456789abcdef0123456789abcdef012345678',
+            id: 'account-2',
+            metadata: {
+              name: 'Account 2',
+              keyring: {
+                type: 'HD Key Tree',
+              },
+            },
+            options: {},
+            methods: ETH_EOA_METHODS,
+            scopes: [EthScope.Eoa],
+            type: EthAccountType.Eoa,
+            balance: '0x0',
+          },
+        ],
+      },
+    },
+  },
+  'wallet-2': {
+    metadata: {
+      name: 'Wallet 2',
+    },
+    groups: {
+      'group-2': {
+        accounts: [
+          {
+            address: '0xabcdef0123456789abcdef0123456789abcdef01',
+            id: 'account-3',
+            metadata: {
+              name: 'Account 3',
+              keyring: {
+                type: 'HD Key Tree',
+              },
+            },
+            options: {},
+            methods: ETH_EOA_METHODS,
+            scopes: [EthScope.Eoa],
+            type: EthAccountType.Erc4337,
+            balance: '0x0',
+          },
+        ],
+      },
+    },
+  },
+};
+
+const mockConnectedSites = {
+  '0x0dcd5d886577d5081b0c52e242ef29e70be3e7bc': [
+    {
+      origin: 'https://test.dapp',
+      iconUrl: 'https://test.dapp/icon.png',
+    },
+  ],
+};
+
+describe('MultichainAccountsTree', () => {
+  const mockOnClose = jest.fn();
+  const mockOnAccountListItemItemClicked = jest.fn();
+
+  const defaultProps = {
+    walletAccountCollection: mockWalletAccountCollection,
+    allowedAccountTypes: [EthAccountType.Eoa, EthAccountType.Erc4337],
+    connectedSites: mockConnectedSites,
+    currentTabOrigin: 'https://test.dapp',
+    privacyMode: false,
+    selectedAccount: {
+      address: '0x0dcd5d886577d5081b0c52e242ef29e70be3e7bc',
+    },
+    onClose: mockOnClose,
+    onAccountListItemItemClicked: mockOnAccountListItemItemClicked,
+  };
+
+  const renderComponent = (props = {}) => {
+    const store = configureStore(mockState);
+
+    return renderWithProvider(
+      <MultichainAccountsTree {...defaultProps} {...props} />,
+      store,
+    );
+  };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('renders wallet and account items correctly', () => {
+    renderComponent();
+
+    expect(screen.getByText('Wallet 1')).toBeInTheDocument();
+    expect(screen.getByText('Wallet 2')).toBeInTheDocument();
+
+    expect(screen.getByText('Account 1')).toBeInTheDocument();
+    expect(screen.getByText('Account 2')).toBeInTheDocument();
+    expect(screen.getByText('Account 3')).toBeInTheDocument();
+  });
+
+  it('filters accounts by allowed types', () => {
+    // Only show EOA accounts
+    renderComponent({
+      allowedAccountTypes: [EthAccountType.Eoa],
+    });
+
+    // Account 1 and 2 are EOA and should be visible
+    expect(screen.getByText('Account 1')).toBeInTheDocument();
+    expect(screen.getByText('Account 2')).toBeInTheDocument();
+
+    // Account 3 is ERC4337 and should not be visible
+    expect(screen.queryByText('Account 3')).not.toBeInTheDocument();
+  });
+});
---
+++ ui/components/multichain-accounts/multichain-accounts-tree/multichain-accounts-tree.tsx
@@ -0,0 +1,192 @@
+import React, { useMemo } from 'react';
+import PropTypes from 'prop-types';
+import { Box, ButtonLink, ButtonLinkSize, Text } from '../../component-library';
+import {
+  AlignItems,
+  Display,
+  FontWeight,
+  JustifyContent,
+  TextColor,
+  TextVariant,
+} from '../../../helpers/constants/design-system';
+import {
+  AccountListItem,
+  AccountListItemMenuTypes,
+} from '../../multichain/account-list-item';
+import { ConsolidatedWallets } from '../../../selectors/multichain-accounts/multichain-accounts-selectors.types';
+import { MergedInternalAccount } from '../../../selectors/selectors.types';
+
+export type MultichainAccountsTreeProps = {
+  walletAccountCollection: ConsolidatedWallets;
+  allowedAccountTypes: string[];
+  connectedSites: Record<string, { origin: string; iconUrl?: string }[]>;
+  currentTabOrigin?: string;
+  privacyMode?: boolean;
+  accountListItemProps?: Record<string, unknown>;
+  selectedAccount: { address: string };
+  onClose: () => void;
+  onAccountListItemItemClicked: (account: MergedInternalAccount) => void;
+};
+
+export const MultichainAccountsTree = ({
+  walletAccountCollection,
+  allowedAccountTypes,
+  connectedSites,
+  currentTabOrigin,
+  privacyMode,
+  accountListItemProps,
+  selectedAccount,
+  onClose,
+  onAccountListItemItemClicked,
+}: MultichainAccountsTreeProps) => {
+  const accountListItems = useMemo(() => {
+    return Object.entries(walletAccountCollection).reduce(
+      (accumulatedItems, [walletId, walletData]) => {
+        const walletName = walletData.metadata?.name;
+
+        const walletHeader = (
+          <Box
+            key={`wallet-header-${walletId}`}
+            display={Display.Flex}
+            justifyContent={JustifyContent.spaceBetween}
+            alignItems={AlignItems.center}
+            paddingLeft={4}
+            paddingRight={4}
+          >
+            <Text
+              variant={TextVariant.bodySm}
+              fontWeight={FontWeight.Medium}
+              color={TextColor.textAlternative}
+            >
+              {walletName}
+            </Text>
+            <ButtonLink
+              size={ButtonLinkSize.Sm}
+              color={TextColor.primaryDefault}
+              fontWeight={FontWeight.Normal}
+            >
+              Details
+            </ButtonLink>
+          </Box>
+        );
+
+        // Process all groups in the wallet and collect its account items
+        const groupsItems = Object.entries(walletData.groups || {}).flatMap(
+          ([groupId, groupData]) => {
+            // Filter accounts by allowed types
+            const filteredAccounts = groupData.accounts.filter((account) =>
+              allowedAccountTypes.includes(account.type),
+            );
+
+            if (filteredAccounts.length === 0) {
+              return [];
+            }
+
+            // Create account items for group
+            const accountItems = filteredAccounts.map((account) => {
+              const connectedSite = connectedSites[account.address]?.find(
+                ({ origin }) => origin === currentTabOrigin,
+              );
+
+              return (
+                <Box
+                  className="multichain-account-menu-popover__list--menu-item"
+                  key={account.address}
+                >
+                  <AccountListItem
+                    onClick={onAccountListItemItemClicked}
+                    account={account}
+                    key={account.address}
+                    selected={selectedAccount.address === account.address}
+                    closeMenu={onClose}
+                    connectedAvatar={connectedSite?.iconUrl}
+                    menuType={AccountListItemMenuTypes.Account}
+                    currentTabOrigin={currentTabOrigin}
+                    privacyMode={privacyMode}
+                    showSrpPill={false}
+                    {...accountListItemProps}
+                  />
+                </Box>
+              );
+            });
+
+            return [
+              <Box
+                key={`group-${groupId}`}
+                style={{
+                  borderBottom: '1px solid var(--color-border-muted)',
+                }}
+              >
+                {accountItems}
+              </Box>,
+            ];
+          },
+        );
+
+        return [...accumulatedItems, walletHeader, ...groupsItems];
+      },
+      [] as React.ReactNode[],
+    );
+  }, [
+    walletAccountCollection,
+    allowedAccountTypes,
+    connectedSites,
+    currentTabOrigin,
+    privacyMode,
+    accountListItemProps,
+    selectedAccount,
+    onClose,
+    onAccountListItemItemClicked,
+  ]);
+
+  return <>{accountListItems}</>;
+};
+
+MultichainAccountsTree.propTypes = {
+  /**
+   * Collection of wallets with account metadata
+   */
+  walletAccountCollection: PropTypes.object.isRequired,
+
+  /**
+   * Array of account types allowed to be rendered
+   */
+  allowedAccountTypes: PropTypes.array.isRequired,
+
+  /**
+   * Mapping of account addresses to connected sites
+   */
+  connectedSites: PropTypes.object.isRequired,
+
+  /**
+   * Origin of the current tab
+   */
+  currentTabOrigin: PropTypes.string,
+
+  /**
+   * Whether privacy mode is enabled
+   */
+  privacyMode: PropTypes.bool,
+
+  /**
+   * Additional props to pass to the AccountListItem component
+   */
+  accountListItemProps: PropTypes.object,
+
+  /**
+   * Currently selected account
+   */
+  selectedAccount: PropTypes.shape({
+    address: PropTypes.string.isRequired,
+  }).isRequired,
+
+  /**
+   * Function to call when closing the menu
+   */
+  onClose: PropTypes.func.isRequired,
+
+  /**
+   * Function to call when an account list item is clicked
+   */
+  onAccountListItemItemClicked: PropTypes.func.isRequired,
+};
