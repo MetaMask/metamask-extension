285efe9cca766459953876f3a298cdf891e137c3
Matthew Walsh <matthew.walsh@consensys.net>
2025-06-13 22:08:08 +0100

    Add container utils

--- app/scripts/lib/transaction/hooks/enforce-simulation-hook.test.ts
+++ app/scripts/lib/transaction/hooks/enforce-simulation-hook.test.ts
@@ -1,23 +1,15 @@
 import {
-  SimulationTokenStandard,
   TransactionContainerType,
-  TransactionControllerEstimateGasAction,
   TransactionMeta,
   TransactionStatus,
 } from '@metamask/transaction-controller';
 import { EnforceSimulationHook } from './enforce-simulation-hook';
 import { TransactionControllerInitMessenger } from '../../../controller-init/messengers/transaction-controller-messenger';
-import { Messenger } from '@metamask/base-controller';
-import { cloneDeep } from 'lodash';
-import { DELEGATOR_CONTRACTS } from '@metamask/delegation-deployments';
-import { Hex, remove0x } from '@metamask/utils';
+import { Hex } from '@metamask/utils';
 import { ORIGIN_METAMASK } from '@metamask/controller-utils';
-import { DelegationControllerSignDelegationAction } from '@metamask/delegation-controller';
+import { applyTransactionContainers } from '../containers/util';
 
-const ESTIMATE_GAS_MOCK = '0x123' as Hex;
-const TOKEN_MOCK = '0x4567890abcdef1234567890abcdef1234567890a' as Hex;
-const DELEGATION_SIGNATURE_MOCK = '0x456aaabbbcccdddeee123' as Hex;
-const TOKEN_ID_MOCK = '0xabcdef345712ad' as Hex;
+jest.mock('../containers/util');
 
 const BALANCE_CHANGE_MOCK = {
   difference: '0x1' as Hex,
@@ -49,250 +41,69 @@ const TRANSACTION_META_MOCK: TransactionMeta = {
 };
 
 describe('EnforceSimulationHook', () => {
-  let messenger: TransactionControllerInitMessenger;
+  let messenger = {} as TransactionControllerInitMessenger;
 
-  const estimateGasMock: jest.MockedFn<
-    TransactionControllerEstimateGasAction['handler']
-  > = jest.fn();
-
-  const signDelegationMock: jest.MockedFn<
-    DelegationControllerSignDelegationAction['handler']
-  > = jest.fn();
+  const applyTransactionContainersMock = jest.mocked(
+    applyTransactionContainers,
+  );
 
   beforeEach(() => {
     jest.resetAllMocks();
 
     process.env.ENABLE_ENFORCED_SIMULATIONS = true as never;
 
-    const baseMessenger = new Messenger<
-      | DelegationControllerSignDelegationAction
-      | TransactionControllerEstimateGasAction,
-      never
-    >();
+    applyTransactionContainersMock.mockResolvedValue({
+      updateTransaction: jest.fn(),
+    });
+  });
 
-    baseMessenger.registerActionHandler(
-      'TransactionController:estimateGas',
-      estimateGasMock,
-    );
+  it('applies enforced simulations container if after simulate hook', async () => {
+    const updateTransactionMock = jest.fn();
 
-    baseMessenger.registerActionHandler(
-      'DelegationController:signDelegation',
-      signDelegationMock,
-    );
-
-    messenger = baseMessenger.getRestricted({
-      name: 'TransactionControllerInitMessenger',
-      allowedActions: [
-        'DelegationController:signDelegation',
-        'TransactionController:estimateGas',
-      ],
-      allowedEvents: [],
+    applyTransactionContainersMock.mockResolvedValue({
+      updateTransaction: updateTransactionMock,
     });
 
-    estimateGasMock.mockResolvedValue({
-      gas: ESTIMATE_GAS_MOCK,
-      simulationFails: {
-        debug: {},
-      },
+    const hook = new EnforceSimulationHook({
+      messenger,
+    }).getAfterSimulateHook();
+
+    const { updateTransaction } =
+      (await hook({
+        transactionMeta: TRANSACTION_META_MOCK,
+      })) ?? {};
+
+    expect(updateTransaction).toBe(updateTransactionMock);
+
+    expect(applyTransactionContainersMock).toHaveBeenCalledWith(
+      expect.objectContaining({
+        isApproved: false,
+      }),
+    );
+  });
+
+  it('applies enforced simulations container if before sign hook', async () => {
+    const updateTransactionMock = jest.fn();
+
+    applyTransactionContainersMock.mockResolvedValue({
+      updateTransaction: updateTransactionMock,
     });
 
-    signDelegationMock.mockResolvedValue(DELEGATION_SIGNATURE_MOCK);
-  });
-
-  it('updates transaction target to delegation manager', async () => {
     const hook = new EnforceSimulationHook({
       messenger,
-    }).getAfterSimulateHook();
+    }).getBeforeSignHook();
 
     const { updateTransaction } =
       (await hook({
         transactionMeta: TRANSACTION_META_MOCK,
       })) ?? {};
 
-    const newTransaction = cloneDeep(TRANSACTION_META_MOCK);
-    updateTransaction?.(newTransaction);
+    expect(updateTransaction).toBe(updateTransactionMock);
 
-    expect(newTransaction.txParams.to).toBe(
-      DELEGATOR_CONTRACTS['1.3.0']['1'].DelegationManager,
-    );
-  });
-
-  it('updates transaction data to redeemDelegations call', async () => {
-    const hook = new EnforceSimulationHook({
-      messenger,
-    }).getAfterSimulateHook();
-
-    const { updateTransaction } =
-      (await hook({
-        transactionMeta: TRANSACTION_META_MOCK,
-      })) ?? {};
-
-    const newTransaction = cloneDeep(TRANSACTION_META_MOCK);
-    updateTransaction?.(newTransaction);
-
-    expect(newTransaction.txParams.data?.substring(0, 10)).toStrictEqual(
-      '0xcef6d209',
-    );
-  });
-
-  it('updates transaction value to zero', async () => {
-    const hook = new EnforceSimulationHook({
-      messenger,
-    }).getAfterSimulateHook();
-
-    const { updateTransaction } =
-      (await hook({
-        transactionMeta: TRANSACTION_META_MOCK,
-      })) ?? {};
-
-    const newTransaction = cloneDeep(TRANSACTION_META_MOCK);
-    updateTransaction?.(newTransaction);
-
-    expect(newTransaction.txParams.value).toBe('0x0');
-  });
-
-  it('adds container type to transaction', async () => {
-    const hook = new EnforceSimulationHook({
-      messenger,
-    }).getAfterSimulateHook();
-
-    const { updateTransaction } =
-      (await hook({
-        transactionMeta: TRANSACTION_META_MOCK,
-      })) ?? {};
-
-    const newTransaction = cloneDeep(TRANSACTION_META_MOCK);
-    updateTransaction?.(newTransaction);
-
-    expect(newTransaction.containerTypes).toStrictEqual([
-      TransactionContainerType.EnforcedSimulations,
-    ]);
-  });
-
-  it('includes native balance change caveat', async () => {
-    const hook = new EnforceSimulationHook({
-      messenger,
-    }).getAfterSimulateHook();
-
-    const { updateTransaction } =
-      (await hook({
-        transactionMeta: TRANSACTION_META_MOCK,
-      })) ?? {};
-
-    const newTransaction = cloneDeep(TRANSACTION_META_MOCK);
-    updateTransaction?.(newTransaction);
-
-    expect(newTransaction.txParams.data).toStrictEqual(
-      expect.stringContaining(
-        remove0x(
-          DELEGATOR_CONTRACTS['1.3.0']['1'].NativeBalanceChangeEnforcer,
-        ).toLowerCase(),
-      ),
-    );
-  });
-
-  it('includes erc20 token balance change caveat', async () => {
-    const hook = new EnforceSimulationHook({
-      messenger,
-    }).getAfterSimulateHook();
-
-    const { updateTransaction } =
-      (await hook({
-        transactionMeta: {
-          ...TRANSACTION_META_MOCK,
-          simulationData: {
-            tokenBalanceChanges: [
-              {
-                ...BALANCE_CHANGE_MOCK,
-                address: TOKEN_MOCK,
-                standard: SimulationTokenStandard.erc20,
-              },
-            ],
-          },
-        },
-      })) ?? {};
-
-    const newTransaction = cloneDeep(TRANSACTION_META_MOCK);
-    updateTransaction?.(newTransaction);
-
-    expect(newTransaction.txParams.data).toStrictEqual(
-      expect.stringContaining(
-        remove0x(
-          DELEGATOR_CONTRACTS['1.3.0']['1'].ERC20BalanceChangeEnforcer,
-        ).toLowerCase(),
-      ),
-    );
-  });
-
-  it('includes erc721 token balance change caveat', async () => {
-    const hook = new EnforceSimulationHook({
-      messenger,
-    }).getAfterSimulateHook();
-
-    const { updateTransaction } =
-      (await hook({
-        transactionMeta: {
-          ...TRANSACTION_META_MOCK,
-          simulationData: {
-            tokenBalanceChanges: [
-              {
-                ...BALANCE_CHANGE_MOCK,
-                address: TOKEN_MOCK,
-                id: TOKEN_ID_MOCK,
-                standard: SimulationTokenStandard.erc721,
-              },
-            ],
-          },
-        },
-      })) ?? {};
-
-    const newTransaction = cloneDeep(TRANSACTION_META_MOCK);
-    updateTransaction?.(newTransaction);
-
-    expect(newTransaction.txParams.data).toStrictEqual(
-      expect.stringContaining(
-        remove0x(
-          DELEGATOR_CONTRACTS['1.3.0']['1'].ERC721BalanceChangeEnforcer,
-        ).toLowerCase(),
-      ),
-    );
-  });
-
-  it('includes erc1155 token balance change caveat', async () => {
-    const hook = new EnforceSimulationHook({
-      messenger,
-    }).getAfterSimulateHook();
-
-    const { updateTransaction } =
-      (await hook({
-        transactionMeta: {
-          ...TRANSACTION_META_MOCK,
-          simulationData: {
-            tokenBalanceChanges: [
-              {
-                ...BALANCE_CHANGE_MOCK,
-                address: TOKEN_MOCK,
-                id: TOKEN_ID_MOCK,
-                standard: SimulationTokenStandard.erc1155,
-              },
-            ],
-          },
-        },
-      })) ?? {};
-
-    const newTransaction = cloneDeep(TRANSACTION_META_MOCK);
-    updateTransaction?.(newTransaction);
-
-    expect(newTransaction.txParams.data).toStrictEqual(
-      expect.stringContaining(
-        remove0x(
-          DELEGATOR_CONTRACTS['1.3.0']['1'].ERC1155BalanceChangeEnforcer,
-        ).toLowerCase(),
-      ),
-    );
-
-    expect(newTransaction.txParams.data).toStrictEqual(
-      expect.stringContaining(remove0x(TOKEN_ID_MOCK).toLowerCase()),
+    expect(applyTransactionContainersMock).toHaveBeenCalledWith(
+      expect.objectContaining({
+        isApproved: true,
+      }),
     );
   });
 
@@ -359,40 +170,6 @@ describe('EnforceSimulationHook', () => {
         })) ?? {};
 
       expect(updateTransaction).toBeUndefined();
-    });
-  });
-
-  describe('if before sign hook', () => {
-    it('signs delegation', async () => {
-      const hook = new EnforceSimulationHook({
-        messenger,
-      }).getBeforeSignHook();
-
-      const { updateTransaction } =
-        (await hook({
-          transactionMeta: TRANSACTION_META_MOCK,
-        })) ?? {};
-
-      const newTransaction = cloneDeep(TRANSACTION_META_MOCK);
-      updateTransaction?.(newTransaction);
-
-      expect(newTransaction.txParams.data).toStrictEqual(
-        expect.stringContaining(
-          remove0x(DELEGATION_SIGNATURE_MOCK).toLowerCase(),
-        ),
-      );
-    });
-
-    it('does not estimate gas', async () => {
-      const hook = new EnforceSimulationHook({
-        messenger,
-      }).getBeforeSignHook();
-
-      await hook({
-        transactionMeta: TRANSACTION_META_MOCK,
-      });
-
-      expect(estimateGasMock).not.toHaveBeenCalled();
     });
   });
 });
--- app/scripts/lib/transaction/hooks/enforce-simulation-hook.ts
+++ app/scripts/lib/transaction/hooks/enforce-simulation-hook.ts
@@ -1,32 +1,13 @@
 import {
   AfterSimulateHook,
   BeforeSignHook,
-  SimulationData,
-  SimulationTokenStandard,
   TransactionContainerType,
   TransactionMeta,
-  TransactionParams,
 } from '@metamask/transaction-controller';
-import { Hex, createProjectLogger, hexToNumber } from '@metamask/utils';
+import { createProjectLogger } from '@metamask/utils';
 import { ORIGIN_METAMASK } from '@metamask/controller-utils';
 import { TransactionControllerInitMessenger } from '../../../controller-init/messengers/transaction-controller-messenger';
-import {
-  DeleGatorEnvironment,
-  Delegation,
-  ExecutionMode,
-  ExecutionStruct,
-  SINGLE_DEFAULT_MODE,
-  createCaveatBuilder,
-  createDelegation,
-  getDeleGatorEnvironment,
-} from '../../../../../shared/lib/delegation';
-import {
-  UnsignedDelegation,
-  encodeRedeemDelegations,
-} from '../../../../../shared/lib/delegation/delegation';
-
-const MOCK_DELEGATION_SIGNATURE =
-  '0x2261a7810ed3e9cde160895909e138e2f68adb2da86fcf98ea0840701df107721fb369ab9b52550ea98832c09f8185284aca4c94bd345e867a4f4461868dd7751b';
+import { applyTransactionContainers } from '../containers/util';
 
 const log = createProjectLogger('enforce-simulation-hook');
 
@@ -59,13 +40,10 @@ export class EnforceSimulationHook {
     const { isFinal } = options;
 
     const {
-      chainId,
       containerTypes,
       delegationAddress,
-      networkClientId,
       origin,
       simulationData,
-      txParams,
       txParamsOriginal,
     } = transactionMeta;
 
@@ -75,7 +53,8 @@ export class EnforceSimulationHook {
     }
 
     if (
-      containerTypes?.includes(TransactionContainerType.EnforcedSimulations)
+      containerTypes?.includes(TransactionContainerType.EnforcedSimulations) &&
+      !isFinal
     ) {
       log('Skipping as simulation already enforced');
       return {};
@@ -104,212 +83,21 @@ export class EnforceSimulationHook {
       throw new Error('Original transaction parameters not found');
     }
 
-    const from = txParams.from as Hex;
-    const chainIdDecimal = hexToNumber(chainId);
-    const delegationEnvironment = getDeleGatorEnvironment(chainIdDecimal);
-    const delegationManagerAddress = delegationEnvironment.DelegationManager;
+    const newContainerTypes = [
+      ...(containerTypes ?? []),
+      TransactionContainerType.EnforcedSimulations,
+    ];
 
-    log('Starting', delegationAddress);
-
-    const delegation = generateDelegation({
-      accountAddress: from,
-      environment: delegationEnvironment,
-      simulationData,
+    const { updateTransaction } = await applyTransactionContainers({
+      isApproved: isFinal ?? false,
+      messenger: this.#messenger,
+      transactionMeta,
+      types: newContainerTypes,
     });
-
-    log('Delegation', delegation);
-
-    let delegationSignature = MOCK_DELEGATION_SIGNATURE as Hex;
-
-    if (isFinal) {
-      log('Signing delegation');
-
-      delegationSignature = (await this.#messenger.call(
-        'DelegationController:signDelegation',
-        {
-          chainId,
-          delegation,
-        },
-      )) as Hex;
-    }
-
-    log('Delegation signature', delegationSignature);
-
-    const data = generateCalldata({
-      transaction: isFinal ? (txParamsOriginal as TransactionParams) : txParams,
-      delegation: { ...delegation, signature: delegationSignature },
-    });
-
-    log('Data', data);
-
-    let newGas: Hex | undefined;
-
-    if (!isFinal) {
-      const { gas } = await this.#messenger.call(
-        'TransactionController:estimateGas',
-        { ...txParams, data, to: delegationManagerAddress, value: '0x0' },
-        networkClientId,
-        {
-          ignoreDelegationSignatures: true,
-        },
-      );
-
-      log('Estimated gas', gas);
-
-      newGas = gas;
-    }
 
     return {
       skipSimulation: true,
-      updateTransaction: (transaction: TransactionMeta) => {
-        transaction.txParams.data = data;
-        transaction.txParams.to = delegationManagerAddress;
-        transaction.txParams.value = '0x0';
-
-        transaction.containerTypes = [
-          ...(transaction.containerTypes ?? []),
-          TransactionContainerType.EnforcedSimulations,
-        ];
-
-        if (newGas) {
-          transaction.txParams.gas = newGas;
-        }
-      },
+      updateTransaction,
     };
   }
-}
-
-function generateDelegation({
-  accountAddress,
-  environment,
-  simulationData,
-}: {
-  accountAddress: Hex;
-  environment: DeleGatorEnvironment;
-  simulationData: SimulationData;
-}): UnsignedDelegation {
-  const caveats = generateCaveats(accountAddress, environment, simulationData);
-
-  log('Caveats', caveats);
-
-  const delegation = createDelegation({
-    from: accountAddress,
-    to: accountAddress,
-    caveats,
-  });
-
-  return delegation;
-}
-
-export function generateCalldata({
-  transaction,
-  delegation,
-}: {
-  transaction: TransactionParams;
-  delegation: Delegation;
-}): Hex {
-  const delegations = [[delegation]];
-  const modes: ExecutionMode[] = [SINGLE_DEFAULT_MODE];
-
-  const executions: ExecutionStruct[][] = [
-    [
-      {
-        target: transaction.to as Hex,
-        callData: (transaction.data as Hex) ?? '0x',
-        value: transaction.value ? BigInt(transaction.value) : 0n,
-      },
-    ],
-  ];
-
-  return encodeRedeemDelegations({
-    delegations,
-    modes,
-    executions,
-  });
-}
-
-function generateCaveats(
-  recipient: Hex,
-  environment: DeleGatorEnvironment,
-  simulationData: SimulationData,
-) {
-  const caveatBuilder = createCaveatBuilder(environment);
-  const { nativeBalanceChange, tokenBalanceChanges = [] } = simulationData;
-
-  if (nativeBalanceChange) {
-    const { difference, isDecrease: enforceDecrease } = nativeBalanceChange;
-    const delta = BigInt(difference);
-
-    log('Caveat - Native Balance Change', {
-      enforceDecrease,
-      recipient,
-      delta,
-    });
-
-    caveatBuilder.addCaveat(
-      'nativeBalanceChange',
-      enforceDecrease,
-      recipient,
-      delta,
-    );
-  }
-
-  for (const tokenChange of tokenBalanceChanges) {
-    const {
-      difference,
-      isDecrease: enforceDecrease,
-      address: token,
-      standard,
-      id: tokenIdHex,
-    } = tokenChange;
-
-    const delta = BigInt(difference);
-    const tokenId = tokenIdHex ? BigInt(tokenIdHex) : 0n;
-
-    log('Caveat - Token Balance Change', {
-      enforceDecrease,
-      token,
-      recipient,
-      delta,
-    });
-
-    switch (standard) {
-      case SimulationTokenStandard.erc20:
-        caveatBuilder.addCaveat(
-          'erc20BalanceChange',
-          enforceDecrease,
-          token,
-          recipient,
-          delta,
-        );
-        break;
-
-      case SimulationTokenStandard.erc721:
-        caveatBuilder.addCaveat(
-          'erc721BalanceChange',
-          enforceDecrease,
-          token,
-          recipient,
-          delta,
-        );
-        break;
-
-      case SimulationTokenStandard.erc1155:
-        caveatBuilder.addCaveat(
-          'erc1155BalanceChange',
-          enforceDecrease,
-          token,
-          recipient,
-          tokenId,
-          delta,
-        );
-        break;
-
-      default:
-        log('Unsupported token standard', standard);
-        break;
-    }
-  }
-
-  return caveatBuilder.build();
 }
