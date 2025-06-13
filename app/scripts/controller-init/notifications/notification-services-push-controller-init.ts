260a93633b8fbd387d267abead359d35fd0ea29c
Prithpal Sooriya <prithpal.sooriya@consensys.net>
2025-06-13 21:51:18 +0100

    fix: fix missing property policies for firebase push notifications

--- app/scripts/controller-init/notifications/notification-services-push-controller-init.ts
+++ app/scripts/controller-init/notifications/notification-services-push-controller-init.ts
@@ -19,22 +19,24 @@ export const NotificationServicesPushControllerInit: ControllerInitFunction<
   NotificationServicesPushController,
   NotificationServicesPushControllerMessenger
 > = ({ controllerMessenger, persistedState }) => {
+  const env = {
+    apiKey: process.env.FIREBASE_API_KEY ?? '',
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN ?? '',
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET ?? '',
+    projectId: process.env.FIREBASE_PROJECT_ID ?? '',
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID ?? '',
+    appId: process.env.FIREBASE_APP_ID ?? '',
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID ?? '',
+    vapidKey: process.env.VAPID_KEY ?? '',
+  };
+
   const controller = new NotificationServicesPushController({
     messenger: controllerMessenger,
     state: {
       ...defaultState,
       ...persistedState.NotificationServicesPushController,
     },
-    env: {
-      apiKey: process.env.FIREBASE_API_KEY ?? '',
-      authDomain: process.env.FIREBASE_AUTH_DOMAIN ?? '',
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET ?? '',
-      projectId: process.env.FIREBASE_PROJECT_ID ?? '',
-      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID ?? '',
-      appId: process.env.FIREBASE_APP_ID ?? '',
-      measurementId: process.env.FIREBASE_MEASUREMENT_ID ?? '',
-      vapidKey: process.env.VAPID_KEY ?? '',
-    },
+    env,
     config: {
       isPushFeatureEnabled: isManifestV3,
       platform: 'extension',
@@ -51,6 +53,14 @@ export const NotificationServicesPushControllerInit: ControllerInitFunction<
       },
     },
   });
+
+  // Test example to run firebase and showcase this policy issue
+  // We should get a token back from `createRegToken`, but instead we get `null`.
+  console.log('TEST: START');
+  createRegToken(env)
+    .then((token) => console.log('TEST: HERE IS MY TOKEN', token))
+    .catch((e) => console.log('TEST: FAILURE', e))
+    .finally(() => console.log('TEST: END'));
 
   return {
     controller,
