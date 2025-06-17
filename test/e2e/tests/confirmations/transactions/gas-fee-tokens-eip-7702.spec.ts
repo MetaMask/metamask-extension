925b07d8a4d3d34c56b8c586ea62ee24c88ccca2
seaona <seaona@proton.me>
2025-06-17 10:05:09 +0200

    close

--- test/e2e/tests/confirmations/transactions/gas-fee-tokens-eip-7702.spec.ts
+++ test/e2e/tests/confirmations/transactions/gas-fee-tokens-eip-7702.spec.ts
@@ -53,6 +53,7 @@ describe('Gas Fee Tokens - EIP-7702', function (this: Suite) {
         await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
 
         const transactionConfirmation = new TransactionConfirmation(driver);
+        await transactionConfirmation.closeGasFeeToastMessage();
         await transactionConfirmation.clickAdvancedDetailsButton();
         await transactionConfirmation.clickGasFeeTokenPill();
 
