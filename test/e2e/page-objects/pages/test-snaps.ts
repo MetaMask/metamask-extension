fd65ec6d446ade31f1e0bf4e63dadf7f9142ec26
Frederik Bolding <frederik.bolding@gmail.com>
2025-06-18 12:22:43 +0200

    Improve E2E stability

--- test/e2e/page-objects/pages/test-snaps.ts
+++ test/e2e/page-objects/pages/test-snaps.ts
@@ -351,9 +351,20 @@ export class TestSnaps {
         try {
           await this.clickButton('getWebSocketState');
 
-          const { open, origin, blockNumber } = JSON.parse(
-            await resultElement.getText(),
-          );
+          // Wait for response from Snap.
+          await this.driver.waitForSelector('#getWebSocketState', {
+            state: 'enabled',
+          });
+
+          const text = await resultElement.getText();
+
+          const { open, origin, blockNumber } = JSON.parse(text);
+
+          console.log('Retrieved WebSocket state:', {
+            open,
+            origin,
+            blockNumber,
+          });
 
           const blockNumberMatch =
             typeof state.blockNumber === 'string'
@@ -367,7 +378,7 @@ export class TestSnaps {
           return false;
         }
       },
-      { timeout: veryLargeDelayMs, interval: 200 },
+      { timeout: veryLargeDelayMs * 2, interval: 200 },
     );
   }
 }
