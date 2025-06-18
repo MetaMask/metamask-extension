8b9f1cfda1b210978d1ece76db86092d7c8866ea
lwin <lwin@tor.us>
2025-06-18 17:24:41 +0800

    feat: add grant_type request jwt token payload

--- app/scripts/services/oauth/google-login-handler.ts
+++ app/scripts/services/oauth/google-login-handler.ts
@@ -82,6 +82,7 @@ export class GoogleLoginHandler extends BaseLoginHandler {
       login_provider: this.authConnection,
       network: web3AuthNetwork,
       code_verifier: this.#codeVerifier,
+      grant_type: 'offline',
     };
 
     return JSON.stringify(requestData);
