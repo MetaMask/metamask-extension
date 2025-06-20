acf569e9feb5c6b5163d09c37977df9caca0f8eb
lwin <lwin@tor.us>
2025-06-21 03:25:36 +0800

    feat: update identity api and requestPermission in web-authenticator-factory

--- app/scripts/services/oauth/web-authenticator-factory.ts
+++ app/scripts/services/oauth/web-authenticator-factory.ts
@@ -1,3 +1,4 @@
+import browser from 'webextension-polyfill';
 import { PLATFORM_FIREFOX } from '../../../../shared/constants/app';
 import { getPlatform } from '../../lib/util';
 import { WebAuthenticator } from './types';
@@ -29,10 +30,20 @@ function generateNonce(): string {
 }
 
 function getIdentityAPI(): typeof chrome.identity | typeof browser.identity {
-  const isFirefox = getPlatform() === PLATFORM_FIREFOX;
-  return isFirefox
-    ? globalThis.browser.identity // use browser.identity for Firefox
-    : chrome.identity; // use chrome.identity for Chromium based browsers
+  // if chrome.identity API is available, we will use it
+  // note that, in firefox, chrome.identity is available
+  // but only some of the methods are supported
+  // learn more here {@link https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/identity#browser_compatibility}
+  if (
+    chrome?.identity &&
+    'getRedirectURL' in chrome.identity &&
+    'launchWebAuthFlow' in chrome.identity
+  ) {
+    return chrome.identity;
+  }
+
+  // otherwise use browser.identity API
+  return browser.identity;
 }
 
 async function launchWebAuthFlow(
@@ -54,11 +65,16 @@ function getRedirectURL(): string {
 async function requestIdentityPermission(): Promise<boolean> {
   const isFirefox = getPlatform() === PLATFORM_FIREFOX;
   if (isFirefox) {
-    // in Firefox (MV2), 'identity' permission is not supported with the runtime permission request.
-    // so we have to set 'identity' permission as required permission, in the manifest.json file
-    return true;
+    // in Firefox, 'identity' permission is not supported with the runtime permission request.
+    // so we have to set 'identity' permission as required permission, in the firefox manifest.json file
+    // since, the 'identity' was requested as installation permission, we don't need to re-request it here.
+    // However, we still need to check if the permission is granted, coz the existing extension users might not have the permission.
+    const grantedPermissions = browser.permissions;
+    console.log('grantedPermissions', grantedPermissions);
+    return grantedPermissions.contains({ permissions: ['identity'] });
   }
 
+  // for other browsers, we can request the permission at runtime
   const permissionGranted = await chrome.permissions.request({
     permissions: ['identity'],
   });
