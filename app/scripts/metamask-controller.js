142505515abec759322574fa963419ec210f60a2
Alex Donesky <adonesky@gmail.com>
2025-06-20 16:30:34 -0500

    add missing hooks to wallet_addEthereumChain

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -6646,6 +6646,18 @@ export default class MetamaskController extends EventEmitter {
         this.networkController.getNetworkConfigurationByChainId.bind(
           this.networkController,
         ),
+      setTokenNetworkFilter: (chainId) => {
+        const { tokenNetworkFilter } =
+          this.preferencesController.getPreferences();
+        if (chainId && Object.keys(tokenNetworkFilter).length === 1) {
+          this.preferencesController.setPreference('tokenNetworkFilter', {
+            [chainId]: true,
+          });
+        }
+      },
+      setEnabledNetworks: (chainIds) => {
+        this.networkOrderController.setEnabledNetworks(chainIds);
+      },
       getCurrentChainIdForDomain: (domain) => {
         const networkClientId =
           this.selectedNetworkController.getNetworkClientIdForDomain(domain);
@@ -6868,19 +6880,6 @@ export default class MetamaskController extends EventEmitter {
             // for the origin do not exist
             console.log(e);
           }
-        },
-
-        setTokenNetworkFilter: (chainId) => {
-          const { tokenNetworkFilter } =
-            this.preferencesController.getPreferences();
-          if (chainId && Object.keys(tokenNetworkFilter).length === 1) {
-            this.preferencesController.setPreference('tokenNetworkFilter', {
-              [chainId]: true,
-            });
-          }
-        },
-        setEnabledNetworks: (chainIds) => {
-          this.networkOrderController.setEnabledNetworks(chainIds);
         },
 
         updateCaveat: this.permissionController.updateCaveat.bind(
