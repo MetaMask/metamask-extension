19ac702b03df912e61f72d746a52b24c23e4e7ea
Nicholas Gambino <nicholas.gambino@consensys.net>
2025-06-23 08:01:32 -0700

    fix: Remove exclusive test

--- test/e2e/tests/request-queuing/dapp1-switch-dapp2-send.spec.js
+++ test/e2e/tests/request-queuing/dapp1-switch-dapp2-send.spec.js
@@ -168,7 +168,7 @@ describe('Request Queuing Dapp 1, Switch Tx -> Dapp 2 Send Tx', function () {
     );
   });
 
-  it.only('should queue send tx after switch network confirmation and transaction should target the correct network after switch is cancelled.', async function () {
+  it('should queue send tx after switch network confirmation and transaction should target the correct network after switch is cancelled.', async function () {
     const port = 8546;
     const chainId = 1338;
     await withFixtures(
