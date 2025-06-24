04185c1e116abd51cf18705262d5d5c27f8b3a25
zjb0807 <zjb0807@qq.com>
2025-06-24 12:28:26 +0800

    resolve conflicts

--- shared/constants/network.ts
+++ shared/constants/network.ts
@@ -342,7 +342,6 @@ export const SOPHON_TESTNET_DISPLAY_NAME = 'Sophon Testnet';
 export const EDUCHAIN_DISPLAY_NAME = 'EDU Chain';
 export const APECHAIN_DISPLAY_NAME = 'ApeChain';
 export const APECHAIN_TESTNET_DISPLAY_NAME = 'ApeChain Testnet';
-export const SEI_DISPLAY_NAME = 'Sei Network';
 export const ACALA_DISPLAY_NAME = 'Acala';
 export const KARURA_DISPLAY_NAME = 'Karura';
 
@@ -411,7 +410,6 @@ export const CURRENCY_SYMBOLS = {
   BERACHAIN: 'BERA',
   EDUCHAIN: 'EDU',
   APECHAIN: 'APE',
-  SEI: 'SEI',
   ACALA: 'ACA',
   KARURA: 'KAR',
 } as const;
