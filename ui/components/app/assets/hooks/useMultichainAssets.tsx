79a842e4d7b0bb2843a467545812b8b87607eefc
Nicholas Gambino <nicholas.gambino@consensys.net>
2025-06-24 11:25:00 -0700

    fix: Solana token list

--- ui/components/app/assets/hooks/useMultichainAssets.tsx
+++ ui/components/app/assets/hooks/useMultichainAssets.tsx
@@ -5,7 +5,6 @@ import { getMultichainSelectedAccountCachedBalance } from '../../../../selectors
 import {
   getEnabledNetworksByNamespace,
   getSelectedInternalAccount,
-  isGlobalNetworkSelectorRemoved,
 } from '../../../../selectors';
 import {
   TranslateFunction,
@@ -23,6 +22,8 @@ import { TokenWithFiatAmount } from '../types';
 import { getMultiChainAssets } from '../../../../selectors/assets';
 import { filterAssets } from '../util/filter';
 import useNetworkFilter from './useNetworkFilter';
+
+const isGlobalNetworkSelectorRemoved = true;
 
 const useMultiChainAssets = () => {
   const t = useI18nContext();
