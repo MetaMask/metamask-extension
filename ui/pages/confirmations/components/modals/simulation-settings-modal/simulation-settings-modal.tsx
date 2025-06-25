9a6ec46df06e96af5a44fc55f956242503af051e
Matthew Walsh <matthew.walsh@consensys.net>
2025-06-25 14:20:44 +0100

    Create slippage local component

--- ui/pages/confirmations/components/modals/simulation-settings-modal/simulation-settings-modal.tsx
+++ ui/pages/confirmations/components/modals/simulation-settings-modal/simulation-settings-modal.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import {
   TransactionContainerType,
   TransactionMeta,
@@ -73,51 +73,22 @@ export const SimulationSettingsModal = ({
       selectEnableEnforcedSimulations(state, transactionId),
   );
 
-  const defaultSlippage = useSelector(selectEnforcedSimulationsDefaultSlippage);
-
-  const savedSlippage = useSelector((state: ConfirmMetamaskState) =>
-    selectEnforcedSimulationsSlippage(state, transactionId),
-  );
-
   const isEnforcedSimulationApplied = containerTypes?.includes(
     TransactionContainerType.EnforcedSimulations,
   );
 
   const [enabled, setEnabled] = useState(isEnforcedSimulationsEnabled);
-  const [customSlippage, setCustomSlippage] = useState(savedSlippage);
-
-  const [isCustomSlippage, setIsCustomSlippage] = useState(
-    savedSlippage !== defaultSlippage,
-  );
-
-  const handleCustomSlippageChange = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>) => {
-      const { value } = event.target;
-      const parsedValue = parseInt(value, 10);
-      setCustomSlippage(parsedValue);
-    },
-    [],
-  );
-
-  const handleDefaultSlippageClick = useCallback(() => {
-    setIsCustomSlippage(false);
-  }, []);
-
-  const handleCustomSlippageClick = useCallback(() => {
-    setIsCustomSlippage(true);
-  }, []);
+  const [updatedSlippage, setUpdatedSlippage] = useState<number>();
 
   const handleUpdateClick = useCallback(async () => {
     await setEnableEnforcedSimulationsForTransaction(transactionId, enabled);
 
     let newContainerTypes = containerTypes || [];
 
-    const slippage = isCustomSlippage ? customSlippage : defaultSlippage;
-
-    if (slippage !== savedSlippage) {
+    if (updatedSlippage !== undefined) {
       await setEnforcedSimulationsSlippageForTransaction(
         transactionId,
-        slippage,
+        updatedSlippage,
       );
     }
 
@@ -144,14 +115,11 @@ export const SimulationSettingsModal = ({
     onClose?.();
   }, [
     containerTypes,
-    customSlippage,
-    defaultSlippage,
     enabled,
-    isCustomSlippage,
     isEnforcedSimulationApplied,
     onClose,
-    savedSlippage,
     transactionId,
+    updatedSlippage,
   ]);
 
   return (
@@ -200,39 +168,10 @@ export const SimulationSettingsModal = ({
               {t('simulationSettingsModalEnforceToggleDescription')}
             </Text>
           </Section>
-          <Section>
-            <Text variant={TextVariant.bodyMdMedium}>Slippage tolerance</Text>
-            <Box
-              display={Display.Flex}
-              flexDirection={FlexDirection.Row}
-              gap={2}
-            >
-              <Pill
-                text={`${defaultSlippage}%`}
-                isSelected={!isCustomSlippage}
-                onClick={handleDefaultSlippageClick}
-              />
-              <Pill
-                text="Custom"
-                isSelected={isCustomSlippage}
-                onClick={handleCustomSlippageClick}
-              />
-              {isCustomSlippage && (
-                <TextField
-                  type={TextFieldType.Number}
-                  onChange={handleCustomSlippageChange}
-                  value={customSlippage}
-                />
-              )}
-            </Box>
-            <Text
-              variant={TextVariant.bodyMd}
-              color={TextColor.textAlternativeSoft}
-            >
-              Set the percentage difference you're comfortable with for the
-              displayed balance changes.
-            </Text>
-          </Section>
+          <Slippage
+            transactionId={transactionId}
+            onChange={setUpdatedSlippage}
+          />
           <ButtonPrimary
             onClick={handleUpdateClick}
             data-testid="simulation-settings-modal-update"
@@ -244,6 +183,80 @@ export const SimulationSettingsModal = ({
     </Modal>
   );
 };
+
+function Slippage({
+  onChange,
+  transactionId,
+}: {
+  onChange?: (slippage: number | undefined) => void;
+  transactionId: string;
+}) {
+  const defaultSlippage = useSelector(selectEnforcedSimulationsDefaultSlippage);
+
+  const savedSlippage = useSelector((state: ConfirmMetamaskState) =>
+    selectEnforcedSimulationsSlippage(state, transactionId),
+  );
+
+  const [customSlippage, setCustomSlippage] = useState(savedSlippage);
+
+  const [isCustomSlippage, setIsCustomSlippage] = useState(
+    savedSlippage !== defaultSlippage,
+  );
+
+  const handleCustomSlippageChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const { value } = event.target;
+      const parsedValue = parseInt(value, 10);
+      setCustomSlippage(parsedValue);
+    },
+    [],
+  );
+
+  const handleDefaultSlippageClick = useCallback(() => {
+    setIsCustomSlippage(false);
+  }, []);
+
+  const handleCustomSlippageClick = useCallback(() => {
+    setIsCustomSlippage(true);
+  }, []);
+
+  const slippage = isCustomSlippage ? customSlippage : defaultSlippage;
+  const changeValue = slippage === savedSlippage ? undefined : slippage;
+
+  useEffect(() => {
+    onChange?.(changeValue);
+  }, [changeValue, onChange]);
+
+  return (
+    <Section>
+      <Text variant={TextVariant.bodyMdMedium}>Slippage tolerance</Text>
+      <Box display={Display.Flex} flexDirection={FlexDirection.Row} gap={2}>
+        <Pill
+          text={`${defaultSlippage}%`}
+          isSelected={!isCustomSlippage}
+          onClick={handleDefaultSlippageClick}
+        />
+        <Pill
+          text="Custom"
+          isSelected={isCustomSlippage}
+          onClick={handleCustomSlippageClick}
+        />
+      </Box>
+      {isCustomSlippage && (
+        <TextField
+          type={TextFieldType.Number}
+          onChange={handleCustomSlippageChange}
+          value={customSlippage}
+          endAccessory={'%'}
+        />
+      )}
+      <Text variant={TextVariant.bodyMd} color={TextColor.textAlternativeSoft}>
+        Set the percentage difference you're comfortable with for the displayed
+        balance changes.
+      </Text>
+    </Section>
+  );
+}
 
 function Pill({
   isSelected = false,
