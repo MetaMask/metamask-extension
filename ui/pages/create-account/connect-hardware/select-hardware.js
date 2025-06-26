2cbb6e18d6f2e40e2d42aa4f4fbb8cfe1fab5931
Xiaoming Wang <dawnseeker8@gmail.com>
2025-06-26 20:11:12 +0800

    Change the styling to make the front same as another text.

--- ui/pages/create-account/connect-hardware/select-hardware.js
+++ ui/pages/create-account/connect-hardware/select-hardware.js
@@ -298,7 +298,7 @@ export default class SelectHardware extends Component {
               >
                 {this.context.t('ledgerFirefoxNotSupportedDescription1')}
                 <a
-                  className="hw-connect__msg-link"
+                  className="hw-connect__href-link"
                   href={LEDGER_FIREFOX_NOT_SUPPORTED_URL}
                   target="_blank"
                   rel="noreferrer"
