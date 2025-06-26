fd3e3123de44ffb049293a9af48db4a72abaa4c6
lwin <lwin@tor.us>
2025-06-26 11:55:52 +0800

    fix: fixed lint and tests

--- test/e2e/tests/settings/change-password.spec.ts
+++ test/e2e/tests/settings/change-password.spec.ts
@@ -1,20 +1,26 @@
+import { Mockttp } from 'mockttp';
 import { withFixtures } from '../../helpers';
 import FixtureBuilder from '../../fixture-builder';
-import { loginWithBalanceValidation } from '../../page-objects/flows/login.flow';
 import SettingsPage from '../../page-objects/pages/settings/settings-page';
 import HeaderNavbar from '../../page-objects/pages/header-navbar';
 import ChangePasswordPage from '../../page-objects/pages/settings/change-password-page';
 import PrivacySettings from '../../page-objects/pages/settings/privacy-settings';
-import { MOCK_GOOGLE_ACCOUNT, WALLET_PASSWORD } from '../../constants';
 import LoginPage from '../../page-objects/pages/login-page';
 import HomePage from '../../page-objects/pages/home/homepage';
-import { Mockttp } from 'mockttp';
-import { createNewWalletWithSocialLoginOnboardingFlow } from '../../page-objects/flows/onboarding.flow';
+import {
+  completeCreateNewWalletOnboardingFlow,
+  createNewWalletWithSocialLoginOnboardingFlow,
+} from '../../page-objects/flows/onboarding.flow';
 import { OAuthMockttpService } from '../../helpers/seedless-onboarding/mocks';
 import { Driver } from '../../webdriver/driver';
 import OnboardingCompletePage from '../../page-objects/pages/onboarding/onboarding-complete-page';
 
-async function doPasswordChangeAndLockWallet(driver: Driver, currentPassword: string, newPassword: string, isSocialLogin: boolean = false) {
+async function doPasswordChangeAndLockWallet(
+  driver: Driver,
+  currentPassword: string,
+  newPassword: string,
+  isSocialLogin: boolean = false,
+) {
   // navigate to security & privacy settings
   const headerNavbar = new HeaderNavbar(driver);
   await headerNavbar.openSettingsPage();
@@ -42,28 +48,34 @@ async function doPasswordChangeAndLockWallet(driver: Driver, currentPassword: st
 }
 
 describe('Change wallet password', function () {
+  const OLD_PASSWORD = 'oldPassword';
   const NEW_PASSWORD = 'newPassword';
 
   it('should change wallet password and able to unlock with new password', async function () {
     await withFixtures(
       {
-        fixtures: new FixtureBuilder().build(),
+        fixtures: new FixtureBuilder({ onboarding: true }).build(),
         title: this.test?.fullTitle(),
       },
       async ({ driver }) => {
-        await loginWithBalanceValidation(driver);
+        await completeCreateNewWalletOnboardingFlow({
+          driver,
+          skipSRPBackup: true,
+          password: OLD_PASSWORD,
+        });
+        const homePage = new HomePage(driver);
+        await homePage.check_pageIsLoaded();
 
-        await doPasswordChangeAndLockWallet(driver, WALLET_PASSWORD, NEW_PASSWORD);
+        await doPasswordChangeAndLockWallet(driver, OLD_PASSWORD, NEW_PASSWORD);
 
         const loginPage = new LoginPage(driver);
 
         // Try to login with old password, should show incorrect password message
-        await loginPage.loginToHomepage(WALLET_PASSWORD);
+        await loginPage.loginToHomepage(OLD_PASSWORD);
         await loginPage.check_incorrectPasswordMessageIsDisplayed();
 
         // Login with new password, should login successfully
         await loginPage.loginToHomepage(NEW_PASSWORD);
-        const homePage = new HomePage(driver);
         await homePage.check_pageIsLoaded();
       },
     );
@@ -83,6 +95,7 @@ describe('Change wallet password', function () {
       async ({ driver }: { driver: Driver }) => {
         await createNewWalletWithSocialLoginOnboardingFlow({
           driver,
+          password: OLD_PASSWORD,
         });
 
         const onboardingCompletePage = new OnboardingCompletePage(driver);
@@ -91,12 +104,17 @@ describe('Change wallet password', function () {
         const homePage = new HomePage(driver);
         await homePage.check_pageIsLoaded();
 
-        await doPasswordChangeAndLockWallet(driver, WALLET_PASSWORD, NEW_PASSWORD, true);
+        await doPasswordChangeAndLockWallet(
+          driver,
+          OLD_PASSWORD,
+          NEW_PASSWORD,
+          true,
+        );
 
         const loginPage = new LoginPage(driver);
 
         // // Try to login with old password, should show incorrect password message
-        await loginPage.loginToHomepage(WALLET_PASSWORD);
+        await loginPage.loginToHomepage(OLD_PASSWORD);
         await loginPage.check_incorrectPasswordMessageIsDisplayed();
 
         // Login with new password, should login successfully
