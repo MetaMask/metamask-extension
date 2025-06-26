164d8ce7be543a8917015c96cd7e5c4712329e29
Hassan Malik <hbmalik88@gmail.com>
2025-06-26 16:06:44 -0400

    refactor: remove unnecessary code from evm account details

--- ui/pages/multichain-accounts/account-details/evm-account-details.tsx
+++ ui/pages/multichain-accounts/account-details/evm-account-details.tsx
@@ -1,59 +1,20 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { AppSliceState } from '../../../ducks/app/app';
-import { getInternalAccountByAddress } from '../../../selectors';
 import { BaseAccountDetails } from '../base-account-details/base-account-details';
-import { AccountDetailsRow } from '../../../components/multichain-accounts/account-details-row';
-import { useI18nContext } from '../../../hooks/useI18nContext';
-import { IconName } from '../../../components/component-library/icon';
-import { ButtonIcon, ButtonIconSize } from '../../../components/component-library';
-import { IconColor } from '../../../helpers/constants/design-system';
+import { InternalAccount } from '@metamask/keyring-internal-api';
+import { SmartContractAccountToggleSection } from '../../../components/multichain-accounts/smart-contract-account-toggle-section';
 
 type EVMAccountDetailsProps = {
-  children?: React.ReactNode | React.ReactNode[];
+  address: string;
+  account: InternalAccount;
 };
 
-export const EVMAccountDetails = ({ children }: EVMAccountDetailsProps) => {
-  const address = useSelector(
-    (state: AppSliceState) => state.appState.accountDetailsAddress,
-  );
-  const account = useSelector((state) =>
-    getInternalAccountByAddress(state, address),
-  );
-  const t = useI18nContext();
-
-  if (!account) {
-    return null;
-  }
-
-  const { type } = account;
-  const isERC4337 = type === 'eip155:erc4337';
-
+export const EVMAccountDetails = ({
+  address,
+  account,
+}: EVMAccountDetailsProps) => {
   return (
-    <BaseAccountDetails>
-      {/* EVM-specific account details */}
-      {isERC4337 && (
-        <AccountDetailsRow
-          label={t('accountType')}
-          value={t('smartAccount')}
-          style={{
-            marginTop: '16px',
-            marginBottom: '1px',
-            borderTopLeftRadius: '8px',
-            borderTopRightRadius: '8px',
-            borderBottomLeftRadius: '8px',
-            borderBottomRightRadius: '8px',
-          }}
-        />
-      )}
-
-      {/* Add more EVM-specific rows here as needed */}
-      {/* For example: */}
-      {/* - Gas settings */}
-      {/* - Network-specific information */}
-      {/* - Contract interaction history */}
-
-      {children}
+    <BaseAccountDetails address={address} account={account}>
+      <SmartContractAccountToggleSection />
     </BaseAccountDetails>
   );
 };
