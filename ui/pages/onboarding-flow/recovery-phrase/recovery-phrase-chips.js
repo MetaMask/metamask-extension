ce03a67b5c3b25cd0a7f0c4fb4c99c9e52fb887e
Lionell Briones <llenoil@gmail.com>
2025-06-27 16:45:56 +0800

    fix: srp backup navigation and quiz cp-12.22.0 (#33922)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    This PR fixes navigation issues on Backup SRP flow and setting active
    textfield while doing confirm SRP.
    
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    2. What is the improvement/solution?
    -->
    
    [![Open in GitHub
    Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/33922?quickstart=1)
    
    ## **Related issues**
    
    Fixes:
    Bug link: https://github.com/MetaMask/metamask-extension/issues/33914
    This happens because when user remove a quizWork by clicking second
    TextField the app selects the first empty word on the list which is
    wrong. Solution is to select the index of the word removed from the quiz
    to be active.
    
    Bug link: https://github.com/MetaMask/metamask-extension/issues/33823
    We don't have control on the route history if user is using browser's
    back/prev buttons. To solve this, we should not allow users to go to any
    SRP backup pages if the SRP has been backed up already.
    
    Bug link: https://github.com/MetaMask/metamask-extension/issues/33862
    **Import flow**: This happens because when user refresh page on
    `create-wallet` page we lose the imported SRP which is just saved on app
    state. The solution is if the SRP is not present on the app state then
    we will redirect user to import page again to do the import.
    
    Now, everytime the user is in the `create wallet` page during import
    flow it only means he has SRP saved on state, and the flow will continue
    correctly.
    
    ## **Manual testing steps**
    
    1. Refer to bug links
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    Refer to the links
    https://github.com/MetaMask/metamask-extension/issues/33914
    https://github.com/MetaMask/metamask-extension/issues/33823
    https://github.com/MetaMask/metamask-extension/issues/33862
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    Fix for adding/removing quiz words issue
    https://github.com/MetaMask/metamask-extension/issues/33914
    
    https://github.com/user-attachments/assets/686a390b-4ec2-42b3-8063-a4a2df7c846e
    
    Fix for incorrect navigation when refreshing on create-wallet page
    https://github.com/MetaMask/metamask-extension/issues/33862
    
    https://github.com/user-attachments/assets/e9a60d3c-c9be-439b-ab03-c9fff196f519
    
    Fix for preventing user to go back to SRP backup flow when SRP is backed
    up already
    https://github.com/MetaMask/metamask-extension/issues/33823
    
    https://github.com/user-attachments/assets/65121efa-947f-428f-88f1-3437289eb1c6
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [x] I've followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
    Extension Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [x] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [x] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- ui/pages/onboarding-flow/recovery-phrase/recovery-phrase-chips.js
+++ ui/pages/onboarding-flow/recovery-phrase/recovery-phrase-chips.js
@@ -94,7 +94,7 @@ export default function RecoveryPhraseChips({
       };
 
       setQuizAnswers(newQuizAnswers);
-      setIndexToFocus(setNextTargetIndex(newQuizAnswers));
+      setIndexToFocus(newQuizAnswers[targetIndex].index);
     },
     [quizAnswers],
   );
