024080edf920f27de09e78961de9fb9c3815edfc
Nick Gambino <35090461+gambinish@users.noreply.github.com>
2025-06-27 12:32:45 -0700

    feat: Network Manager (disabled) (#33941)
    ## **Description**
    
    This PR introduces the UI implementation the Network Manager feature.
    Controller updates were merged
    [here](https://github.com/MetaMask/metamask-extension/pull/33478) and
    [here](https://github.com/MetaMask/metamask-extension/pull/33877).
    Initially, this PR was built off of the [NetworkManager
    UI](https://github.com/MetaMask/metamask-extension/pull/33542) with a
    nuance that enabled networks should be managed per CAIP `namespace`.
    This allows us to keep contextual awareness of which networks are
    enabled per L1, allowing us to switch back and forth more seamlessly.
    
    > ⚡ This feature is being merged into main BEHIND the `REMOVE_GNS`
    feature flag. To test this feature, make sure you set this to`true` in
    your .metamaskrc file. If this flag is `false` no regressions should be
    present, and no test/e2e failures should exist. The old Network Filter
    will be in the UI.
    
    Also updates the `transaction-list` component to use
    `AssetListControlBar` which is needed for consistency across tabs.
    
    The main intent here is to manage and remember which enabled networks a
    user has selected on each Caip namespace.
    
    [![Open in GitHub
    Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/33751?quickstart=1)
    
    ## **Related issues**
    
    Fixes:
    
    ## **Manual testing steps**
    
    1. Only EVM networks are shown on EVM accounts; only Solana is shown on
    Solana accounts.
    - Important nuance: Enabled networks are tracked per caip namespace.
    Switching between EVM and Solana preserves each network’s previous
    state, so returning to EVM restores your previously enabled EVM
    networks.
    2. dApp connectivity:
    - Adding a network through a dApp should switch to that network, as well
    as enable that network in Metamask (this will be the only enabled
    network). This is to clearly show the user that they are transacting on
    the correct chain.
    - If the network isn’t added yet, MM prompts to add the chain, which
    then becomes the only enabled network by default.
    3. Custom EVM tokens are not mixed with those popular networks. Should
    be consistent across tabs, meaning defi, NFTs and transaction activity
    all use the same network manager to filter assets.
    4. Only one custom network is shown at a time; multiple popular networks
    may be shown.
    
    TODO in fast follows:
    Aggregated Balance
    Migration to migrate existing user's tokenNetworkFilter to
    `enabledNetworkMap` so that on version upgrade they will have their
    existing networks enabled automatically.
    
    ## **Screenshots/Recordings**
    
    Recording with `REMOVE_GNS=false`
    
    
    https://github.com/user-attachments/assets/cca6130a-0ed2-44a4-95b0-69d3afe79695
    
    Recording with `REMOVE_GNS=true`
    
    
    https://github.com/user-attachments/assets/88a295e2-575e-4254-a316-da27e0089e71
    
    ## **Pre-merge author checklist**
    
    - [x] I've followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
    Extension Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: erd <797371+erdimaden@users.noreply.github.com>

--- test/e2e/tests/responsive-ui/metamask-responsive-ui.spec.ts
+++ test/e2e/tests/responsive-ui/metamask-responsive-ui.spec.ts
@@ -12,6 +12,8 @@ import { loginWithBalanceValidation } from '../../page-objects/flows/login.flow'
 import { sendRedesignedTransactionToAddress } from '../../page-objects/flows/send-transaction.flow';
 import { CHAIN_IDS } from '../../../../shared/constants/network';
 
+const isGlobalNetworkSelectorRemoved = process.env.REMOVE_GNS === 'true';
+
 describe('MetaMask Responsive UI', function (this: Suite) {
   const driverOptions = { constrainWindowSize: true };
   it('Creating a new wallet', async function () {
@@ -85,6 +87,19 @@ describe('MetaMask Responsive UI', function (this: Suite) {
           amount: '1',
         });
         await new HomePage(driver).check_pageIsLoaded();
+
+        // Network Selector
+        if (isGlobalNetworkSelectorRemoved) {
+          await driver.clickElement('[data-testid="sort-by-networks"]');
+          await driver.clickElement({
+            text: 'Custom',
+            tag: 'button',
+          });
+          await driver.clickElement('[data-testid="Localhost 8545"]');
+          await driver.clickElement(
+            '[data-testid="modal-header-close-button"]',
+          );
+        }
 
         // check confirmed transaction is displayed in activity list
         const activityList = new ActivityListPage(driver);
