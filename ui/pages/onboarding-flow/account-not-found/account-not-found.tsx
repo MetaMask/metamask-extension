0256804d34bb0aece1129c36245cf30c938a9b2f
lwin <lwin@tor.us>
2025-06-27 18:03:53 +0800

    fix: fixed gap between button in account-not-found page

--- ui/pages/onboarding-flow/account-not-found/account-not-found.tsx
+++ ui/pages/onboarding-flow/account-not-found/account-not-found.tsx
@@ -133,6 +133,7 @@ export default function AccountNotFound() {
         justifyContent={JustifyContent.center}
         alignItems={AlignItems.center}
         width={BlockSize.Full}
+        gap={4}
       >
         <Button
           data-testid="onboarding-complete-done"
