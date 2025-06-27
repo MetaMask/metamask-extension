92303818b37dfb5f545ce2ccfc6b4857815e3e57
Nicholas Gambino <nicholas.gambino@consensys.net>
2025-06-27 14:13:41 -0700

    fix: Only override for dev build

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -1034,16 +1034,7 @@ export default class MetamaskController extends EventEmitter {
 
     let initialNetworkOrderControllerState;
 
-    if (process.env.IN_TEST) {
-      initialNetworkOrderControllerState = {
-        orderedNetworkList: [],
-        enabledNetworkMap: {
-          [KnownCaipNamespace.Eip155]: {
-            [CHAIN_IDS.LOCALHOST]: true,
-          },
-        },
-      };
-    } else if (
+    if (
       process.env.METAMASK_DEBUG ||
       process.env.METAMASK_ENVIRONMENT === 'test'
     ) {
