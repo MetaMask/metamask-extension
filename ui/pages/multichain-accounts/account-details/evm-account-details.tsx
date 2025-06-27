5206ed6d0bf28b493f101711ac639cefd9879176
Hassan Malik <hbmalik88@gmail.com>
2025-06-27 12:11:09 -0400

    chore: move 7702 toggles out of this PR

--- ui/pages/multichain-accounts/account-details/evm-account-details.tsx
+++ ui/pages/multichain-accounts/account-details/evm-account-details.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { InternalAccount } from '@metamask/keyring-internal-api';
 import { BaseAccountDetails } from '../base-account-details/base-account-details';
-import { SmartContractAccountToggleSection } from '../../../components/multichain-accounts/smart-contract-account-toggle-section';
 
 type EVMAccountDetailsProps = {
   address: string;
@@ -12,9 +11,5 @@ export const EVMAccountDetails = ({
   address,
   account,
 }: EVMAccountDetailsProps) => {
-  return (
-    <BaseAccountDetails address={address} account={account}>
-      <SmartContractAccountToggleSection address={address} />
-    </BaseAccountDetails>
-  );
+  return <BaseAccountDetails address={address} account={account} />;
 };
