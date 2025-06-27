61ade76854b146a0a7666c2f3910e0c4360b360a
Alex Mendonca <alex.mendonca@consensys.net>
2025-06-27 19:53:31 +0100

    fix caip address in wallet_createSession e2e

--- test/e2e/flask/multichain-api/wallet_createSession.spec.ts
+++ test/e2e/flask/multichain-api/wallet_createSession.spec.ts
@@ -384,7 +384,7 @@ describe('Multichain API', function () {
     });
   });
 
-  describe('Dapp has existing session with 2 scopes and 2 accounts and then calls `wallet_createSession` with different scopes and accounts', function () {
+  describe('Dapp has existing session with 2 scopes and 1 account and then calls `wallet_createSession` with different scopes and accounts', function () {
     const OLD_SCOPES = ['eip155:1337', 'eip155:1'];
     const NEW_SCOPES = ['eip155:1338', 'eip155:1000'];
     const TREZOR_ACCOUNT = '0xf68464152d7289d7ea9a2bec2e0035c45188223c';
@@ -434,7 +434,9 @@ describe('Multichain API', function () {
             async (scope) =>
               await driver.clickElement(`input[name="${scope}"]`),
           );
-          await testDapp.initCreateSessionScopes(NEW_SCOPES, [TREZOR_ACCOUNT]);
+          await testDapp.initCreateSessionScopes(NEW_SCOPES, [
+            `eip155:0:${TREZOR_ACCOUNT}`,
+          ]);
 
           const connectAccountConfirmation = new ConnectAccountConfirmation(
             driver,
