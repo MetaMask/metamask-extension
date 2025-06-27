dc68e726836d700f6f9786604b235a327c627f98
Micaela Estabillo <micaela.estabillo@consensys.net>
2025-06-27 10:39:52 -0700

    chore: remove unnecessary Row and set fixed height for balance row

--- ui/pages/bridge/prepare/bridge-input-group.tsx
+++ ui/pages/bridge/prepare/bridge-input-group.tsx
@@ -266,25 +266,23 @@ export const BridgeInputGroup = ({
         </AssetPicker>
       </Row>
 
-      <Row justifyContent={JustifyContent.spaceBetween}>
-        <Row>
-          <Text
-            variant={TextVariant.bodyMd}
-            fontWeight={FontWeight.Normal}
-            color={
-              isAmountReadOnly && isEstimatedReturnLow
-                ? TextColor.warningDefault
-                : TextColor.textAlternativeSoft
-            }
-            textAlign={TextAlign.End}
-            ellipsis
-          >
-            {isAmountReadOnly && isLoading && amountFieldProps.value === '0'
-              ? t('bridgeCalculatingAmount')
-              : undefined}
-            {amountInFiat && formatCurrencyAmount(amountInFiat, currency, 2)}
-          </Text>
-        </Row>
+      <Row justifyContent={JustifyContent.spaceBetween} style={{ height: 24 }}>
+        <Text
+          variant={TextVariant.bodyMd}
+          fontWeight={FontWeight.Normal}
+          color={
+            isAmountReadOnly && isEstimatedReturnLow
+              ? TextColor.warningDefault
+              : TextColor.textAlternativeSoft
+          }
+          textAlign={TextAlign.End}
+          ellipsis
+        >
+          {isAmountReadOnly && isLoading && amountFieldProps.value === '0'
+            ? t('bridgeCalculatingAmount')
+            : undefined}
+          {amountInFiat && formatCurrencyAmount(amountInFiat, currency, 2)}
+        </Text>
         {!isAmountReadOnly && balanceAmount && token && (
           <Text
             display={Display.Flex}
@@ -311,31 +309,32 @@ export const BridgeInputGroup = ({
             )}
           </Text>
         )}
-        {isAmountReadOnly && token && selectedChainId && (
-          <Text
-            display={Display.Flex}
-            gap={1}
-            variant={TextVariant.bodyMd}
-            color={TextColor.textAlternativeSoft}
-            onClick={() => {
-              handleAddressClick();
-            }}
-            as={'a'}
-            style={{
-              cursor: isAmountReadOnly ? 'pointer' : 'default',
-              textDecoration: isAmountReadOnly ? 'underline' : 'none',
-            }}
-          >
-            {isNativeAddress(token.address)
-              ? undefined
-              : shortenString(token.address, {
-                  truncatedCharLimit: 11,
-                  truncatedStartChars: 4,
-                  truncatedEndChars: 4,
-                  skipCharacterInEnd: false,
-                })}
-          </Text>
-        )}
+        {isAmountReadOnly &&
+          token &&
+          selectedChainId &&
+          !isNativeAddress(token.address) && (
+            <Text
+              display={Display.Flex}
+              gap={1}
+              variant={TextVariant.bodyMd}
+              color={TextColor.textAlternativeSoft}
+              onClick={() => {
+                handleAddressClick();
+              }}
+              as={'a'}
+              style={{
+                cursor: isAmountReadOnly ? 'pointer' : 'default',
+                textDecoration: isAmountReadOnly ? 'underline' : 'none',
+              }}
+            >
+              {shortenString(token.address, {
+                truncatedCharLimit: 11,
+                truncatedStartChars: 4,
+                truncatedEndChars: 4,
+                skipCharacterInEnd: false,
+              })}
+            </Text>
+          )}
       </Row>
     </Column>
   );
