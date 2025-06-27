73d3606f8a8114ee6de539eef5a53035a81209aa
lwin <lwin@tor.us>
2025-06-27 17:52:39 +0800

    feat: enabled SeedlessOnboardingFeature in test env

--- development/build/set-environment-variables.js
+++ development/build/set-environment-variables.js
@@ -61,6 +61,9 @@ module.exports.setEnvironmentVariables = function setEnvironmentVariables({
       isDevBuild && variables.getMaybe('TEST_GAS_FEE_FLOWS') === true,
     DEEP_LINK_HOST: variables.getMaybe('DEEP_LINK_HOST'),
     DEEP_LINK_PUBLIC_KEY: variables.getMaybe('DEEP_LINK_PUBLIC_KEY'),
+    SEEDLESS_ONBOARDING_ENABLED: isTestBuild
+      ? 'true'
+      : variables.getMaybe('SEEDLESS_ONBOARDING_ENABLED'),
     GOOGLE_CLIENT_ID: getGoogleClientId({
       buildType,
       variables,
