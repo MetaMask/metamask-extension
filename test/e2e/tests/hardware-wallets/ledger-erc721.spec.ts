55c77af665f7c629e7b3e8bad5bd8b689133edf2
Sébastien Van Eyck <sebastien.vaneyck@consensys.net>
2025-06-28 08:53:12 +0200

    feat: add balance validation

--- test/e2e/tests/hardware-wallets/ledger-erc721.spec.ts
+++ test/e2e/tests/hardware-wallets/ledger-erc721.spec.ts
@@ -1,10 +1,9 @@
 import { Suite } from 'mocha';
-import { Driver } from '../../webdriver/driver';
 import TestDappPage from '../../page-objects/pages/test-dapp';
 import FixtureBuilder from '../../fixture-builder';
 import { WINDOW_TITLES, withFixtures } from '../../helpers';
 import { KNOWN_PUBLIC_KEY_ADDRESSES } from '../../../stub/keyring-bridge';
-import { loginWithoutBalanceValidation } from '../../page-objects/flows/login.flow';
+import { loginWithBalanceValidation } from '../../page-objects/flows/login.flow';
 import CreateContractModal from '../../page-objects/pages/dialog/create-contract';
 
 describe('Ledger Hardware', function (this: Suite) {
@@ -25,7 +24,12 @@ describe('Ledger Hardware', function (this: Suite) {
           KNOWN_PUBLIC_KEY_ADDRESSES[0].address,
           '0x100000000000000000000',
         )) ?? console.error('localNodes is undefined or empty');
-        await loginWithoutBalanceValidation(driver);
+        await loginWithBalanceValidation(
+          driver,
+          undefined,
+          undefined,
+          '1208925.8196',
+        );
         const testDappPage = new TestDappPage(driver);
         await testDappPage.openTestDappPage();
         await testDappPage.check_pageIsLoaded();
