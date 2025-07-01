13f00a22c44499f81529364d042b8e7222664995
Vinicius Stevam <vinicius.stevam@consensys.net>
2025-07-01 06:10:31 +0100

    fix: hide nested transaction tag when only 1 transaction is present

--- ui/pages/confirmations/components/transactions/nested-transaction-tag/nested-transaction-tag.tsx
+++ ui/pages/confirmations/components/transactions/nested-transaction-tag/nested-transaction-tag.tsx
@@ -25,7 +25,7 @@ export function NestedTransactionTag() {
   const isBatch = isBatchTransaction(nestedTransactions);
   const functionNames = useNestedTransactionLabels({ nestedTransactions });
 
-  if (!isBatch) {
+  if (!isBatch || nestedTransactions?.length === 1) {
     return null;
   }
   const tooltip = t('transactionIncludesTypes', [functionNames.join(', ')]);
