df0653808fdf474c95cba732063641166c92ce0d
HJetpoluru <harika.jetpoluru@consensys.net>
2025-07-01 14:29:11 -0500

    Removed the only tag added to test

--- test/e2e/tests/multichain/asset-list.spec.ts
+++ test/e2e/tests/multichain/asset-list.spec.ts
@@ -124,7 +124,7 @@ describe('Multichain Asset List', function (this: Suite) {
       },
     );
   });
-  it.only('shows correct asset and balance when swapping on a different chain', async function () {
+  it('shows correct asset and balance when swapping on a different chain', async function () {
     await withFixtures(
       {
         fixtures: new FixtureBuilder().build(),
