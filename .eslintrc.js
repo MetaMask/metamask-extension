f437ed058eb5c6aab2e5c06574060f90f602f120
dddddanica <zhaodanica@gmail.com>
2025-07-02 16:28:09 +0100

    chore: include todo in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -207,6 +207,8 @@ module.exports = {
             ignoreRestSiblings: true,
           },
         ],
+        // This rule temporarily applies the latest `@typescript-eslint/restrict-template-expressions` config found in `@metamask/eslint-config`.
+        // TODO: Remove once `@metamask/eslint-config` is updated to `^14.0.0`.
         '@typescript-eslint/restrict-template-expressions': [
           'error',
           {
