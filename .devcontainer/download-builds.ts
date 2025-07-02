7e919089a301fd34dfa8f8aa5deb21849d72934f
Norbert Elter <72046715+itsyoboieltr@users.noreply.github.com>
2025-07-03 01:26:55 +0400

    feat: cli args

--- .devcontainer/download-builds.ts
+++ .devcontainer/download-builds.ts
@@ -1,92 +1,127 @@
-import { promisify } from 'node:util';
-import { exec as callbackExec } from 'node:child_process';
+import { execSync } from 'node:child_process';
 import unzipper from 'unzipper';
 import { version } from '../package.json';
+import { program, Option } from 'commander';
 
-const exec = promisify(callbackExec);
+const getBranch = () =>
+  execSync('git symbolic-ref --short HEAD').toString().trim();
 
-const getBranch = async () => {
-  const { stdout } = await exec('git symbolic-ref --short HEAD');
-  return stdout.trim();
-};
-
-async function main() {
-  const { Octokit } = await import('octokit');
-
-  const env = {
-    OWNER: process.env.OWNER || 'metamask',
-    REPOSITORY: process.env.REPOSITORY || 'metamask-extension',
-    WORKFLOW_ID: process.env.WORKFLOW_ID || 'main.yml',
-    BRANCH: process.env.BRANCH || (await getBranch()),
-    BUILD_TYPE: process.env.BUILD_TYPE || 'main',
-    GITHUB_TOKEN: process.env.GITHUB_TOKEN || '',
-    AWS_CLOUDFRONT_URL: 'https://diuv6g5fj9pvx.cloudfront.net',
-  };
-
-  console.log('Downloading latest builds from');
-  console.log(`- Branch: ${env.BRANCH}`);
-  console.log(`- Build type: ${env.BUILD_TYPE}`);
-
-  const github = new Octokit({ auth: env.GITHUB_TOKEN });
-
-  const runs = await github.rest.actions.listWorkflowRuns({
-    owner: env.OWNER,
-    repo: env.REPOSITORY,
-    workflow_id: env.WORKFLOW_ID,
-    branch: env.BRANCH,
-    status: 'success',
-    per_page: 1,
-  });
-
-  const latestRun = runs.data.workflow_runs.at(0);
-
-  if (!latestRun)
-    throw new Error(`No successful builds found on branch '${env.BRANCH}'`);
-
-  console.log(`- Run number: ${latestRun.id}`);
-
-  const HOST_URL = `${env.AWS_CLOUDFRONT_URL}/${env.REPOSITORY}/${latestRun.id}`;
-
-  const buildMap = {
-    main: {
-      chrome: `${HOST_URL}/build-dist-browserify/builds/metamask-chrome-${version}.zip`,
-      firefox: `${HOST_URL}/build-dist-mv2-browserify/builds/metamask-firefox-${version}.zip`,
-    },
-    beta: {
-      chrome: `${HOST_URL}/build-beta-browserify/builds/metamask-beta-chrome-${version}-beta.0.zip`,
-      firefox: `${HOST_URL}/build-beta-mv2-browserify/builds/metamask-beta-firefox-${version}-beta.0.zip`,
-    },
-    flask: {
-      chrome: `${HOST_URL}/build-flask-browserify/builds/metamask-flask-chrome-${version}-flask.0.zip`,
-      firefox: `${HOST_URL}/build-flask-mv2-browserify/builds/metamask-flask-firefox-${version}-flask.0.zip`,
-    },
-    test: {
-      chrome: `${HOST_URL}/build-test-browserify/builds/metamask-chrome-${version}.zip`,
-      firefox: `${HOST_URL}/build-test-mv2-browserify/builds/metamask-firefox-${version}.zip`,
-    },
-    'test-flask': {
-      chrome: `${HOST_URL}/build-test-flask-browserify/builds/metamask-flask-chrome-${version}-flask.0.zip`,
-      firefox: `${HOST_URL}/build-test-flask-mv2-browserify/builds/metamask-flask-firefox-${version}-flask.0.zip`,
-    },
-  };
-
-  const builds: { chrome: string; firefox: string } | undefined =
-    buildMap[env.BUILD_TYPE];
-
-  if (!builds)
-    throw new Error(`No builds found for build type '${env.BUILD_TYPE}'`);
-
-  console.log(`Downloading build for chrome from ${builds.chrome}`);
-  console.log(`Downloading build for firefox from ${builds.firefox}`);
-
-  await Promise.all(
-    Object.entries(builds).map(async ([platform, url]) => {
-      const artifact = await fetch(url);
-      const buffer = Buffer.from(await artifact.arrayBuffer());
-      const zip = await unzipper.Open.buffer(buffer);
-      await zip.extract({ path: `dist/${platform}` });
-    }),
-  );
+interface DownloadBuildsArgs {
+  owner: string;
+  repository: string;
+  workflowId: string;
+  branch: string;
+  buildType: string;
+  githubToken: string;
+  awsCloudfrontUrl: string;
 }
 
-main().then(() => console.log('Builds downloaded and unzipped successfully.'));
+program
+  .addOption(
+    new Option('-o, --owner <string>', 'owner of the repository')
+      .default('metamask')
+      .env('OWNER'),
+  )
+  .addOption(
+    new Option('-r, --repository <string>', 'repository name')
+      .default('metamask-extension')
+      .env('REPOSITORY'),
+  )
+  .addOption(
+    new Option('-w, --workflow-id <string>', 'workflow id')
+      .default('main.yml')
+      .env('WORKFLOW_ID'),
+  )
+  .addOption(
+    new Option('-b, --branch <string>', 'branch name')
+      .default(getBranch())
+      .env('BRANCH'),
+  )
+  .addOption(
+    new Option('-t, --build-type <string>', 'build type')
+      .default('main')
+      .env('BUILD_TYPE')
+      .choices(['main', 'beta', 'flask', 'test', 'test-flask']),
+  )
+  .addOption(
+    new Option('-g, --github-token <string>', 'github token')
+      .default('')
+      .env('GITHUB_TOKEN'),
+  )
+  .addOption(
+    new Option('-a, --aws-cloudfront-url <string>', 'aws cloudfront url')
+      .default('https://diuv6g5fj9pvx.cloudfront.net')
+      .env('AWS_CLOUDFRONT_URL'),
+  )
+  .action(async (args: DownloadBuildsArgs) => {
+    const { Octokit } = await import('octokit');
+
+    console.log('Downloading latest builds from');
+    console.log(`- Branch: ${args.branch}`);
+    console.log(`- Build type: ${args.buildType}`);
+
+    const github = new Octokit({ auth: args.githubToken });
+
+    const runs = await github.rest.actions.listWorkflowRuns({
+      owner: args.owner,
+      repo: args.repository,
+      workflow_id: args.workflowId,
+      branch: args.branch,
+      status: 'success',
+      per_page: 1,
+    });
+
+    const latestRun = runs.data.workflow_runs.at(0);
+
+    if (!latestRun)
+      throw new Error(`No successful builds found on branch '${args.branch}'`);
+
+    console.log(`- Run number: ${latestRun.id}`);
+
+    const HOST_URL = `${args.awsCloudfrontUrl}/${args.repository}/${latestRun.id}`;
+
+    const buildMap = {
+      main: {
+        chrome: `${HOST_URL}/build-dist-browserify/builds/metamask-chrome-${version}.zip`,
+        firefox: `${HOST_URL}/build-dist-mv2-browserify/builds/metamask-firefox-${version}.zip`,
+      },
+      beta: {
+        chrome: `${HOST_URL}/build-beta-browserify/builds/metamask-beta-chrome-${version}-beta.0.zip`,
+        firefox: `${HOST_URL}/build-beta-mv2-browserify/builds/metamask-beta-firefox-${version}-beta.0.zip`,
+      },
+      flask: {
+        chrome: `${HOST_URL}/build-flask-browserify/builds/metamask-flask-chrome-${version}-flask.0.zip`,
+        firefox: `${HOST_URL}/build-flask-mv2-browserify/builds/metamask-flask-firefox-${version}-flask.0.zip`,
+      },
+      test: {
+        chrome: `${HOST_URL}/build-test-browserify/builds/metamask-chrome-${version}.zip`,
+        firefox: `${HOST_URL}/build-test-mv2-browserify/builds/metamask-firefox-${version}.zip`,
+      },
+      'test-flask': {
+        chrome: `${HOST_URL}/build-test-flask-browserify/builds/metamask-flask-chrome-${version}-flask.0.zip`,
+        firefox: `${HOST_URL}/build-test-flask-mv2-browserify/builds/metamask-flask-firefox-${version}-flask.0.zip`,
+      },
+    };
+
+    const builds: { chrome: string; firefox: string } | undefined =
+      buildMap[args.buildType];
+
+    if (!builds)
+      throw new Error(`No builds found for build type '${args.buildType}'`);
+
+    console.log(`Downloading build for chrome from ${builds.chrome}`);
+    console.log(`Downloading build for firefox from ${builds.firefox}`);
+
+    await Promise.all(
+      Object.entries(builds).map(async ([platform, url]) => {
+        const artifact = await fetch(url);
+        const buffer = Buffer.from(await artifact.arrayBuffer());
+        const zip = await unzipper.Open.buffer(buffer);
+        await zip.extract({ path: `dist/${platform}` });
+      }),
+    );
+
+    console.log('Builds downloaded and unzipped successfully.');
+  });
+
+program.parse();
