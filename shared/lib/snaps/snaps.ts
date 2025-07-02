863f9d7d6ac5ce6c53b8bd59978c7557c06e16c2
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2025-07-02 12:59:11 +0200

    Add `FORCE_PREINSTALLED_SNAPS` environment variable for local preinstalled Snap development

--- shared/lib/snaps/snaps.ts
+++ shared/lib/snaps/snaps.ts
@@ -1,5 +1,14 @@
 import { SnapId } from '@metamask/snaps-sdk';
 
+/**
+ * Whether to force local Snaps to be treated as preinstalled Snaps.
+ *
+ * This is used in development environments to allow Snaps to use features that
+ * are normally reserved for preinstalled Snaps.
+ */
+const FORCE_PREINSTALLED_SNAPS =
+  process.env.FORCE_PREINSTALLED_SNAPS === 'true';
+
 export const PREINSTALLED_SNAPS = [
   'npm:@metamask/message-signing-snap',
   'npm:@metamask/ens-resolver-snap',
@@ -17,5 +26,13 @@ export const PREINSTALLED_SNAPS = [
  * @returns True if Snap is a preinstalled Snap, false otherwise.
  */
 export function isSnapPreinstalled(snapId: SnapId) {
+  ///: BEGIN:ONLY_INCLUDE_IF(build-flask)
+  // For development purposes, allow local Snaps to be treated as preinstalled
+  // Snaps if the `FORCE_PREINSTALLED_SNAPS` environment variable is enabled.
+  if (FORCE_PREINSTALLED_SNAPS && snapId.startsWith('local:')) {
+    return true;
+  }
+  ///: END:ONLY_INCLUDE_IF
+
   return PREINSTALLED_SNAPS.some((snap) => snap === snapId);
 }
