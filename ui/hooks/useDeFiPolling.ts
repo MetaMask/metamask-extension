255c63e2aa5e720d408858b7129ad0e15c4fa279
JP <jp.sainsbury@consensys.net>
2025-07-02 06:14:13 -0500

    feat: fix lint

--- ui/hooks/useDeFiPolling.ts
+++ ui/hooks/useDeFiPolling.ts
@@ -1,20 +1,6 @@
 import { useSelector } from 'react-redux';
-import {
-  getChainIdsToPoll,
-  getUseTokenDetection,
-  isGlobalNetworkSelectorRemoved,
-} from '../selectors';
-import { getEnabledChainIds } from '../selectors/multichain/networks';
-import {
-  deFiStartPolling,
-  deFiStopPolling,
-  tokenDetectionStartPolling,
-  tokenDetectionStopPollingByPollingToken,
-} from '../store/actions';
-import {
-  getCompletedOnboarding,
-  getIsUnlocked,
-} from '../ducks/metamask/metamask';
+import { deFiStartPolling, deFiStopPolling } from '../store/actions';
+import { getCompletedOnboarding } from '../ducks/metamask/metamask';
 import useMultiPolling from './useMultiPolling';
 
 const useDeFiPolling = () => {
