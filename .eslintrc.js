c39eb787f38fa87b945620690294b3cadc154e52
dddddanica <zhaodanica@gmail.com>
2025-07-02 16:01:48 +0100

    chore: include new rule in eslintrc as todo to remove in the future

--- .eslintrc.js
+++ .eslintrc.js
@@ -207,6 +207,9 @@ module.exports = {
             ignoreRestSiblings: true,
           },
         ],
+        // This rule temporarily applies the latest `@typescript-eslint/no-redundant-type-constituents` config found in `@metamask/eslint-config`.
+        // TODO: Remove once `@metamask/eslint-config` is updated to `^14.0.0`.
+        '@typescript-eslint/no-redundant-type-constituents': 'off',
       },
       settings: {
         'import/resolver': {
