360b9fd56361558591d6c4aaa3b1d102171220b7
Matthew Walsh <matthew.walsh@consensys.net>
2025-07-04 13:22:25 +0100

    Subtract balances from targets
    Combine targets on same token.

--- ui/pages/confirmations/hooks/transactions/useIntentsTargets.ts
+++ ui/pages/confirmations/hooks/transactions/useIntentsTargets.ts
@@ -4,14 +4,12 @@ import { Interface } from '@ethersproject/abi';
 import { abiERC20 } from '@metamask/metamask-eth-abis';
 import { Hex, add0x } from '@metamask/utils';
 import { useMemo } from 'react';
-import { AssetType } from '@metamask/bridge-controller';
 import { useSelector } from 'react-redux';
 import { BigNumber } from 'bignumber.js';
 import { NATIVE_TOKEN_ADDRESS } from '../../../../helpers/constants/intents';
 import { useConfirmContext } from '../../context/confirm';
 import { useMultichainBalances } from '../../../../hooks/useMultichainBalances';
 import { selectTransactionFeeById } from '../../../../selectors';
-import { useTokenFiatRates } from './useTokenFiatRate';
 
 export type IntentsTarget = {
   targetTokenAddress: Hex;
@@ -19,58 +17,60 @@ export type IntentsTarget = {
 };
 
 export function useIntentsTargets() {
+  const { assetsWithBalance } = useMultichainBalances();
+  const fullTargets = useTargets();
+
   const { currentConfirmation: transactionMeta } =
     useConfirmContext<TransactionMeta>();
 
-  const { assetsWithBalance } = useMultichainBalances();
+  const { chainId } = transactionMeta;
 
-  const { assets, chainId, id: transactionId, txParams } = transactionMeta;
+  return useMemo(
+    () =>
+      getPartialTargets(
+        getUniqueTargets(fullTargets),
+        assetsWithBalance,
+        chainId,
+      ),
+    [fullTargets, assetsWithBalance, chainId],
+  );
+}
+
+function useTargets() {
+  const { currentConfirmation: transactionMeta } =
+    useConfirmContext<TransactionMeta>();
+
+  const { assets, id: transactionId, txParams } = transactionMeta;
   const { data, to, value } = txParams;
 
-  const targets: IntentsTarget[] = [];
+  const gasTarget = useGasTarget(transactionId);
+  const valueTarget = useValueTarget(value);
+  const tokenTransferTarget = useTokenTransferTarget(data, to);
 
-  const nativeBalance = assetsWithBalance.find(
-    (asset) => asset.type === AssetType.native && asset.chainId === chainId,
+  const assetTargets = useMemo(
+    () =>
+      assets?.map((asset) => ({
+        targetTokenAddress: asset.address,
+        targetAmount: asset.amount,
+      })) ?? [],
+    [JSON.stringify(assets)],
   );
 
-  const { hexMaximumTransactionFee } = useSelector((state) =>
-    selectTransactionFeeById(state, transactionId),
-  );
+  return useMemo(() => {
+    return [
+      ...(gasTarget ? [gasTarget] : []),
+      ...(valueTarget ? [valueTarget] : []),
+      ...(tokenTransferTarget ? [tokenTransferTarget] : []),
+      ...assetTargets,
+    ];
+  }, [gasTarget, valueTarget, tokenTransferTarget, assetTargets]);
+}
 
-  const nativeBalanceRequired = new BigNumber(hexMaximumTransactionFee, 16)
-    .mul(1.5)
-    .shift(-18);
-
-  const nativeFiatRate = useTokenFiatRates(
-    [NATIVE_TOKEN_ADDRESS],
-    chainId,
-  )?.[0];
-
-  const nativeBalanceRequiredFiat = nativeBalanceRequired.mul(
-    nativeFiatRate ?? 1,
-  );
-
-  const nativeBalanceOneDollar = new BigNumber(1).div(nativeFiatRate ?? 1);
-
-  if (
-    new BigNumber(nativeBalance?.balance ?? '0').lessThan(nativeBalanceRequired)
-  ) {
-    targets.push({
-      targetTokenAddress: NATIVE_TOKEN_ADDRESS,
-      targetAmount: add0x(
-        new BigNumber(nativeBalanceRequiredFiat).lessThan(1)
-          ? nativeBalanceOneDollar.shift(18).toString(16)
-          : nativeBalanceRequired.shift(18).toString(16),
-      ),
-    });
-  }
-
-  if (value && value !== '0x0') {
-    targets.push({
-      targetTokenAddress: NATIVE_TOKEN_ADDRESS,
-      targetAmount: value as Hex,
-    });
-  }
+function useTokenTransferTarget(
+  data: string | undefined,
+  to: string | undefined,
+) {
+  let targetAmount: Hex | undefined;
 
   try {
     const result = new Interface(abiERC20).decodeFunctionData(
@@ -78,22 +78,114 @@ export function useIntentsTargets() {
       data ?? '0x',
     );
 
-    const targetAmount = toHex(result._value);
-
-    targets.push({
-      targetTokenAddress: to as Hex,
-      targetAmount,
-    });
+    targetAmount = toHex(result._value);
   } catch {
     // Intentionally empty
   }
 
-  targets.push(
-    ...(assets?.map((asset) => ({
-      targetTokenAddress: asset.address,
-      targetAmount: asset.amount,
-    })) ?? []),
+  return useMemo(() => {
+    if (!targetAmount || !to) {
+      return undefined;
+    }
+
+    return {
+      targetTokenAddress: to as Hex,
+      targetAmount,
+    };
+  }, [targetAmount, to]);
+}
+
+function useValueTarget(value: string | undefined) {
+  return useMemo(() => {
+    if (!value || value === '0x0') {
+      return undefined;
+    }
+
+    return {
+      targetTokenAddress: NATIVE_TOKEN_ADDRESS,
+      targetAmount: value as Hex,
+    };
+  }, [value]);
+}
+
+function useGasTarget(transactionId: string) {
+  const { hexMaximumTransactionFee } = useSelector((state) =>
+    selectTransactionFeeById(state, transactionId),
   );
 
-  return useMemo(() => targets, [JSON.stringify(targets)]);
+  const gasFeeWei = add0x(
+    new BigNumber(hexMaximumTransactionFee, 16).shift(-9).toString(16),
+  );
+
+  return useMemo(() => {
+    return {
+      targetTokenAddress: NATIVE_TOKEN_ADDRESS,
+      targetAmount: gasFeeWei,
+    };
+  }, [gasFeeWei]);
+}
+
+function getPartialTargets(
+  targets: IntentsTarget[],
+  balances: ReturnType<typeof useMultichainBalances>['assetsWithBalance'],
+  chainId: Hex,
+): IntentsTarget[] {
+  return targets.reduce((acc, target) => {
+    const asset = balances.find(
+      (asset) =>
+        (asset.address.toLowerCase() ===
+          target.targetTokenAddress.toLowerCase() ||
+          (asset.isNative &&
+            target.targetTokenAddress === NATIVE_TOKEN_ADDRESS)) &&
+        asset.chainId === chainId,
+    );
+
+    if (!asset?.balance) {
+      return acc;
+    }
+
+    const { balance } = asset;
+    const decimals = asset.decimals ?? 18;
+
+    const requiredBalance = new BigNumber(target.targetAmount, 16)
+      .shift(-decimals)
+      .minus(balance);
+
+    const requiredBalanceRaw = add0x(
+      requiredBalance.shift(decimals).toString(16),
+    );
+
+    if (requiredBalance.lessThanOrEqualTo(0)) {
+      return acc;
+    }
+
+    acc.push({
+      ...target,
+      targetAmount: requiredBalanceRaw,
+    });
+
+    return acc;
+  }, [] as IntentsTarget[]);
+}
+
+function getUniqueTargets(targets: IntentsTarget[]): IntentsTarget[] {
+  return targets.reduce((acc, target) => {
+    const existingTarget = acc.find(
+      (t) =>
+        t.targetTokenAddress.toLowerCase() ===
+        target.targetTokenAddress.toLowerCase(),
+    );
+
+    if (existingTarget) {
+      existingTarget.targetAmount = add0x(
+        new BigNumber(existingTarget.targetAmount, 16)
+          .plus(new BigNumber(target.targetAmount, 16))
+          .toString(16),
+      );
+    } else {
+      acc.push({ ...target });
+    }
+
+    return acc;
+  }, [] as IntentsTarget[]);
 }
