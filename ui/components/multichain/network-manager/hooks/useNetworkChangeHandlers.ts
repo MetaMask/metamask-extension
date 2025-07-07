27ce273d4b52209a40ec71219b2b7dca0ce5f279
salimtb <salim.toubal@outlook.com>
2025-07-08 00:17:32 +0200

    fix: fix linter

--- ui/components/multichain/network-manager/hooks/useNetworkChangeHandlers.ts
+++ ui/components/multichain/network-manager/hooks/useNetworkChangeHandlers.ts
@@ -95,7 +95,6 @@ export const useNetworkChangeHandlers = () => {
     (chainId: CaipChainId) => {
       const { namespace } = parseCaipChainId(chainId);
       const hexChainId = convertCaipToHexChainId(chainId);
-      // const hexCurrentChainId = convertCaipToHexChainId(currentChainId);
       const { defaultRpcEndpoint } = getRpcDataByChainId(chainId, evmNetworks);
       const finalNetworkClientId = defaultRpcEndpoint.networkClientId;
 
@@ -138,9 +137,6 @@ export const useNetworkChangeHandlers = () => {
       );
 
       if (isGlobalNetworkSelectorRemoved && isDeselecting) {
-        const enabledNetworkKeys = Object.keys(
-          enabledNetworksByNamespace || {},
-        );
         const firstEnabledNetwork = enabledNetworkKeys[0];
 
         const firstEnabledNetworkConfig = firstEnabledNetwork
