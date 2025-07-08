d3721c13769faf93711427f8c00e2d833e810ed3
Mathieu Artu <mathieu.artu@consensys.net>
2025-07-08 17:05:51 +0200

    feat: intercept & authenticate accounts API requests

--- app/scripts/controller-init/identity/authentication-controller-init.ts
+++ app/scripts/controller-init/identity/authentication-controller-init.ts
@@ -5,6 +5,7 @@ import {
 import { Platform } from '@metamask/profile-sync-controller/sdk';
 import { ControllerInitFunction } from '../types';
 import { AuthenticationControllerMessenger } from '../messengers/identity';
+import { fetchIntercept } from '../../lib/fetch-intercept';
 
 /**
  * Initialize the Authentication controller.
@@ -26,6 +27,29 @@ export const AuthenticationControllerInit: ControllerInitFunction<
     metametrics: {
       getMetaMetricsId,
       agent: Platform.EXTENSION,
+    },
+  });
+
+  // Automatically authenticate all requests to Accounts API
+  const ACCOUNTS_API_URL = 'https://accounts.api.cx.metamask.io';
+  const URLS_TO_AUTHENTICATE = [ACCOUNTS_API_URL];
+  fetchIntercept.register({
+    request: async (input, init = {}) => {
+      if (
+        typeof input === 'string' &&
+        URLS_TO_AUTHENTICATE.some((url) => input.startsWith(url))
+      ) {
+        // Get the bearer token from the controller
+        const token = await controller.getBearerToken();
+
+        // Inject the bearer token into the existing request
+        if (!init.headers) {
+          init.headers = {};
+        }
+        init.headers = new Headers(init.headers);
+        init.headers.set('Authorization', `Bearer ${token}`);
+      }
+      return [input, init];
     },
   });
 
---
+++ app/scripts/lib/fetch-intercept/index.ts
@@ -0,0 +1,102 @@
+interface Interceptor {
+  request?: (
+    ...args: Parameters<typeof fetch>
+  ) => Parameters<typeof fetch> | Promise<Parameters<typeof fetch>>;
+  requestError?: (error: any) => any;
+  response?: (response: Response) => Response | Promise<Response>;
+  responseError?: (error: any) => any;
+}
+
+interface FetchWithAuthAPI {
+  register: (interceptor: Interceptor) => () => void;
+  clear: () => void;
+}
+
+function attach(): FetchWithAuthAPI {
+  const interceptors: Interceptor[] = [];
+  const originalFetch = globalThis.fetch;
+
+  globalThis.fetch = function (...args: Parameters<typeof fetch>) {
+    return interceptor(originalFetch, interceptors, ...args);
+  };
+
+  return {
+    register: function (interceptor: Interceptor) {
+      interceptors.push(interceptor);
+      return () => {
+        const index = interceptors.indexOf(interceptor);
+        if (index >= 0) {
+          interceptors.splice(index, 1);
+        }
+      };
+    },
+    clear: function () {
+      interceptors.length = 0;
+    },
+  };
+}
+
+async function interceptor(
+  originalFetch: typeof fetch,
+  interceptors: Interceptor[],
+  ...args: Parameters<typeof fetch>
+): Promise<Response> {
+  const reversedInterceptors = interceptors.reduce(
+    (array: Interceptor[], interceptor: Interceptor) =>
+      [interceptor].concat(array),
+    [],
+  );
+
+  let currentArgs: Parameters<typeof fetch> = args;
+
+  for (const { request, requestError } of reversedInterceptors) {
+    if (request || requestError) {
+      try {
+        if (request) {
+          const result = await request(...currentArgs);
+          currentArgs = result || currentArgs;
+        }
+      } catch (error) {
+        if (requestError) {
+          await requestError(error);
+        } else {
+          throw error;
+        }
+      }
+    }
+  }
+
+  const request = new Request(...currentArgs);
+  let response: Response;
+
+  try {
+    response = await originalFetch(request);
+    (response as any).request = request;
+  } catch (error: any) {
+    error.request = request;
+    throw error;
+  }
+
+  for (const {
+    response: responseInterceptor,
+    responseError,
+  } of reversedInterceptors) {
+    if (responseInterceptor || responseError) {
+      try {
+        if (responseInterceptor) {
+          response = await responseInterceptor(response);
+        }
+      } catch (error) {
+        if (responseError) {
+          await responseError(error);
+        } else {
+          throw error;
+        }
+      }
+    }
+  }
+
+  return response;
+}
+
+export const fetchIntercept = attach();
