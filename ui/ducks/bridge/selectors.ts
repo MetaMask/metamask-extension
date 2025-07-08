2fa62d5370f842d41d47625b17b86fe23738f9a9
Micaela Estabillo <micaela.estabillo@consensys.net>
2025-07-08 13:47:36 -0700

    Revert "fix: read isSingleSwapBridgeButtonEnabled flag to show unified swaps and bridge"
    This reverts commit d278a1da42d952bc0b9ac6f9eef12c81ac424733.

--- ui/ducks/bridge/selectors.ts
+++ ui/ducks/bridge/selectors.ts
@@ -705,9 +705,8 @@ export const getIsUnifiedUIEnabled = createSelector(
 
     const caipChainId = formatChainIdToCaip(chainId);
 
-    // TODO remove this when bridge-controller's types are updated
-    return bridgeFeatureFlags?.chains?.[caipChainId] ? Boolean(
-       'isSingleSwapBridgeButtonEnabled' in bridgeFeatureFlags.chains[caipChainId] ? bridgeFeatureFlags.chains[caipChainId].isSingleSwapBridgeButtonEnabled : false,
-    ) : false;
+    return Boolean(
+      bridgeFeatureFlags?.chains?.[caipChainId]?.isUnifiedUIEnabled,
+    );
   },
 );
