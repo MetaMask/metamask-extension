68ba4f8c409dcbaa3240afcbca5b26d4808aba9e
Jongsun Suh <jongsun.suh@icloud.com>
2025-07-08 11:35:38 -0400

    fix: Open `VisitSupportDataConsentModal` when `GlobalMenu` "Support" button is clicked (#33658)
    ## **Description**
    
    Opens `<VisitSupportDataConsentModal />` when `Global ("3-dot") menu -
    "Support"` is clicked. Previously, clicking the "Support" button
    directly opened a tab to the support site. We now want this logic to be
    handled by the modal.
    
    Replicates the behavior of clicking `Settings - About - "Visit our
    support center"`, as implemented in
    https://github.com/MetaMask/metamask-extension/pull/30415.
    
    [![Open in GitHub
    Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/33658?quickstart=1)
    
    ## **Related issues**
    
    - Fixes https://github.com/MetaMask/MetaMask-planning/issues/5163
    - Follow-up: updates existing usages of `<VisitSupportDataConsentModal
    />` to be managed with redux:
    https://github.com/MetaMask/metamask-extension/pull/33969
    
    ## **Manual testing steps**
    
      1. Click Global ("3-dot") menu - Support
    2. Verify that a modal titled "Share device details with support"
    appears.
      3. Clicking outside of the modal should make it close.
      4. Clicking inside of the modal should not make it close.
    5. Clicking on either button in the modal should make it close and open
    a new tab showing the support page.
    
    <!--
    B. No regressions in `<InfoTab />`.
    1. Click Global ("3-dot") menu - Settings - About - "Visit our support
    center"
      2. Repeat steps A-2 to A-5. 
    
    C. No regressions in `<ErrorPage />`.
    1. Click Global ("3-dot") menu - Settings - Developer Options -
    "Generate A Page Crash" - "Contact support"
      2. Repeat steps A-2 to A-5. 
    -->
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    
    https://github.com/user-attachments/assets/37f60958-f7af-4d09-909a-069c78d5a472
    
    ## **Pre-merge author checklist**
    
    - [ ] I've followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
    Extension Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- ui/store/actions.ts
+++ ui/store/actions.ts
@@ -2477,6 +2477,12 @@ export function showConfTxPage({ id }: Partial<TransactionMeta> = {}) {
   };
 }
 
+export function setShowSupportDataConsentModal(show: boolean) {
+  return {
+    type: actionConstants.SET_SHOW_SUPPORT_DATA_CONSENT_MODAL,
+    payload: show,
+  };
+}
 export function addToken(
   {
     address,
