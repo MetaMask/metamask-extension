1cb92fe554b07c0c9a27623b22bdfe07e6a77dd9
Alejandro Som <560018+alucardzom@users.noreply.github.com>
2025-07-08 13:06:33 +0200

    chore: update get semver version script

--- .github/scripts/get-next-semver-version.sh
+++ .github/scripts/get-next-semver-version.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 FORCE_NEXT_SEMVER_VERSION=$1
-BRANCH_NAMING_CONVENTION=$2
 
 # If FORCE_NEXT_SEMVER_VERSION is defined and not empty, use its value and skip the next operations
 if [ -n "$FORCE_NEXT_SEMVER_VERSION" ]
@@ -10,14 +9,11 @@ then
   exit 0
 fi
 
-# Get the highest version from release branches based on naming convention
-if [ "$BRANCH_NAMING_CONVENTION" = "Version-vX.Y.Z" ]; then
-  # Pattern for Version-vX.Y.Z branches
-  VERSION_BRANCHES=$(git branch -r | grep -o 'Version-v[0-9]*\.[0-9]*\.[0-9]*' | grep -o '[0-9]*\.[0-9]*\.[0-9]*' | sort --version-sort | tail -n 1)
-else
-  # Default pattern for release/x.y.z branches
-  VERSION_BRANCHES=$(git branch -r | grep -o 'release/[0-9]*\.[0-9]*\.[0-9]*' | grep -o '[0-9]*\.[0-9]*\.[0-9]*' | sort --version-sort | tail -n 1)
-fi
+# Pattern for Version-vX.Y.Z branches
+VERSION_BRANCHES_VERSION_V=$(git branch -r | grep -o 'Version-v[0-9]*\.[0-9]*\.[0-9]*' | grep -o '[0-9]*\.[0-9]*\.[0-9]*' | sort --version-sort | tail -n 1)
+# Default pattern for release/x.y.z branches
+VERSION_BRANCHES_RELEASE=$(git branch -r | grep -o 'release/[0-9]*\.[0-9]*\.[0-9]*' | grep -o '[0-9]*\.[0-9]*\.[0-9]*' | sort --version-sort | tail -n 1)
+
 
 # Get the highest version from tags
 VERSION_TAGS=$(git tag | grep -o 'v[0-9]*\.[0-9]*\.[0-9]*' | grep -o '[0-9]*\.[0-9]*\.[0-9]*' | sort --version-sort | tail -n 1)
@@ -26,7 +22,8 @@ VERSION_TAGS=$(git tag | grep -o 'v[0-9]*\.[0-9]*\.[0-9]*' | grep -o '[0-9]*\.[0
 VERSION_PACKAGE=$(node -p "require('../../package.json').version")
 
 # Compare versions and keep the highest one
-HIGHEST_VERSION=$(printf "%s\n%s\n%s" "$VERSION_BRANCHES" "$VERSION_TAGS" "$VERSION_PACKAGE" | sort --version-sort | tail -n 1)
+HIGHEST_VERSION=$(printf "%s\n%s\n%s\n%s" "$VERSION_BRANCHES_VERSION_V" "$VERSION_BRANCHES_RELEASE" "$VERSION_TAGS" "$VERSION_PACKAGE" | sort --version-sort | tail -n 1)
+echo "HIGHEST_VERSION=${HIGHEST_VERSION}, VERSION_BRANCHES_VERSION_V=${VERSION_BRANCHES_VERSION_V}, VERSION_BRANCHES_RELEASE=${VERSION_BRANCHES_RELEASE}, VERSION_TAGS=${VERSION_TAGS}, VERSION_PACKAGE=${VERSION_PACKAGE}"
 
 # Increment the minor version of the highest version found and reset the patch version to 0
 NEXT_VERSION=$(echo "$HIGHEST_VERSION" | awk -F. -v OFS=. '{$2++; $3=0; print}')
