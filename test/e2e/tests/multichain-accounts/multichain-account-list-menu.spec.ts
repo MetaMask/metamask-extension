8df839fdbb0e21083b5a576be8ddc39d632f7668
david0xd <david@timechaser.org>
2025-07-10 15:25:35 +0200

    Add e2e test for hardware wallet

--- test/e2e/tests/multichain-accounts/multichain-account-list-menu.spec.ts
+++ test/e2e/tests/multichain-accounts/multichain-account-list-menu.spec.ts
@@ -1,7 +1,7 @@
 import { Suite } from 'mocha';
 import AccountListPage from '../../page-objects/pages/account-list-page';
 import { Driver } from '../../webdriver/driver';
-import { withMultichainAccountsDesignEnabled } from './common';
+import { AccountType, withMultichainAccountsDesignEnabled } from './common';
 
 describe('Multichain Accounts - Account tree', function (this: Suite) {
   it('should display basic wallets and accounts', async function () {
@@ -33,6 +33,39 @@ describe('Multichain Accounts - Account tree', function (this: Suite) {
         await accountListPage.check_accountBalanceDisplayed('$0.00');
         await accountListPage.check_accountDisplayedInAccountList('Account 1');
         await accountListPage.check_accountDisplayedInAccountList('Account 2');
+        await accountListPage.check_numberOfAvailableAccounts(2);
+      },
+    );
+  });
+
+  it('should display wallet and accounts for hardware wallet', async function () {
+    await withMultichainAccountsDesignEnabled(
+      {
+        title: this.test?.fullTitle(),
+        accountType: AccountType.HardwareWallet,
+      },
+      async (driver: Driver) => {
+        const accountListPage = new AccountListPage(driver);
+        await accountListPage.check_pageIsLoaded();
+
+        // Ensure that wallet information is displayed
+        await accountListPage.check_walletDisplayedInAccountListMenu(
+          'Wallet 1',
+        );
+        await accountListPage.check_walletDisplayedInAccountListMenu('Ledger');
+        await accountListPage.check_walletDetailsButtonIsDisplayed();
+
+        // Ensure that accounts within the wallets are displayed
+        await accountListPage.check_accountAddressDisplayedInAccountList(
+          '0x5CfE7...6a7e1',
+        );
+        await accountListPage.check_accountAddressDisplayedInAccountList(
+          '0xF6846...8223c',
+        );
+        await accountListPage.check_accountBalanceDisplayed('$42,500.00');
+        await accountListPage.check_accountBalanceDisplayed('$0.00');
+        await accountListPage.check_accountDisplayedInAccountList('Account 1');
+        await accountListPage.check_accountDisplayedInAccountList('Ledger 1');
         await accountListPage.check_numberOfAvailableAccounts(2);
       },
     );
