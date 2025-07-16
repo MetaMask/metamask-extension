bdea40746971715a7b46a0b9646031691456f9e4
Patryk Łucka <5708018+PatrykLucka@users.noreply.github.com>
2025-07-16 11:24:59 +0200

    test: use withSolanaAccountSnap instead of adding solana account manually

--- test/e2e/tests/multichain-accounts/multichain-wallet-details.spec.ts
+++ test/e2e/tests/multichain-accounts/multichain-wallet-details.spec.ts
@@ -3,60 +3,31 @@ import { Mockttp } from 'mockttp';
 import AccountListPage from '../../page-objects/pages/account-list-page';
 import WalletDetailsPage from '../../page-objects/pages/wallet-details-page';
 import { Driver } from '../../webdriver/driver';
-import { withFixtures } from '../../helpers';
 import HeaderNavbar from '../../page-objects/pages/header-navbar';
-import HomePage from '../../page-objects/pages/home/homepage';
-import FixtureBuilder from '../../fixture-builder';
-import { loginWithBalanceValidation } from '../../page-objects/flows/login.flow';
-import { ACCOUNT_TYPE } from '../../constants';
 import { mockMultichainAccountsFeatureFlag } from './common';
+import { withSolanaAccountSnap } from '../solana/common-solana';
 
 describe('Multichain Accounts - Wallet Details', function (this: Suite) {
   it('should view wallet details with one Ethereum and one Solana account and show SRP backup reminder', async function () {
-    await withFixtures(
+    await withSolanaAccountSnap(
       {
-        fixtures: new FixtureBuilder()
-          .withKeyringControllerMultiSRP()
-          .withEnabledNetworks({
-            eip155: {
-              '0x539': true,
-            },
-            solana: {
-              'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp': true,
-            },
-          })
-          .withPreferencesController({
-            dismissSeedBackUpReminder: false,
-          })
-          .build(),
         title: this.test?.fullTitle(),
-        testSpecificMock: async (mockServer: Mockttp) => {
+        numberOfAccounts: 1,
+        withFixtureBuilder: (builder) =>
+          builder
+            .withKeyringControllerMultiSRP()
+            .withPreferencesController({
+              dismissSeedBackUpReminder: false,
+            }),
+        withCustomMocks: async (mockServer: Mockttp) => {
           return mockMultichainAccountsFeatureFlag(mockServer);
         },
-        manifestFlags: {
-          remoteFeatureFlags: {
-            addSolanaAccount: true,
-          },
-        },
-        dapp: true,
       },
-      async ({ driver }: { driver: Driver }) => {
-        await loginWithBalanceValidation(driver);
-        const homePage = new HomePage(driver);
-        await homePage.check_pageIsLoaded();
+      async (driver: Driver, mockServer: Mockttp, extensionId: string) => {
         const headerNavbar = new HeaderNavbar(driver);
         await headerNavbar.openAccountMenu();
 
         const accountListPage = new AccountListPage(driver);
-        await accountListPage.check_pageIsLoaded();
-
-        // Create a Solana account to match test scenario requirements
-        await accountListPage.addAccount({
-          accountType: ACCOUNT_TYPE.Solana,
-          accountName: 'Solana Account 1',
-        });
-        await homePage.check_pageIsLoaded();
-        await headerNavbar.openAccountMenu();
         await accountListPage.check_pageIsLoaded();
 
         await accountListPage.check_walletDetailsButtonIsDisplayed();
@@ -66,55 +37,28 @@ describe('Multichain Accounts - Wallet Details', function (this: Suite) {
         await walletDetailsPage.checkPageIsLoaded();
 
         await walletDetailsPage.checkWalletNameIsDisplayed('Wallet 1');
-        await walletDetailsPage.checkBalanceIsDisplayed('$42,500.00');
+        await walletDetailsPage.checkBalanceIsDisplayed('$5,643.50');
         await walletDetailsPage.checkAccountIsDisplayed('Account 1');
-        await walletDetailsPage.checkAccountIsDisplayed('Solana Account 1');
+        await walletDetailsPage.checkAccountIsDisplayed('Solana 1');
       },
     );
   });
 
   it('should add new Ethereum account from wallet details', async function () {
-    await withFixtures(
+    await withSolanaAccountSnap(
       {
-        fixtures: new FixtureBuilder()
-          .withKeyringControllerMultiSRP()
-          .withEnabledNetworks({
-            eip155: {
-              '0x539': true,
-            },
-            solana: {
-              'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp': true,
-            },
-          })
-          .build(),
         title: this.test?.fullTitle(),
-        testSpecificMock: async (mockServer: Mockttp) => {
+        numberOfAccounts: 1,
+        withFixtureBuilder: (builder) => builder.withKeyringControllerMultiSRP(),
+        withCustomMocks: async (mockServer: Mockttp) => {
           return mockMultichainAccountsFeatureFlag(mockServer);
         },
-        manifestFlags: {
-          remoteFeatureFlags: {
-            addSolanaAccount: true,
-          },
-        },
-        dapp: true,
       },
-      async ({ driver }: { driver: Driver }) => {
-        await loginWithBalanceValidation(driver);
-        const homePage = new HomePage(driver);
-        await homePage.check_pageIsLoaded();
+      async (driver: Driver, mockServer: Mockttp, extensionId: string) => {
         const headerNavbar = new HeaderNavbar(driver);
         await headerNavbar.openAccountMenu();
 
         const accountListPage = new AccountListPage(driver);
-        await accountListPage.check_pageIsLoaded();
-
-        // Create a Solana account to match test scenario requirements
-        await accountListPage.addAccount({
-          accountType: ACCOUNT_TYPE.Solana,
-          accountName: 'Solana Account 1',
-        });
-        await homePage.check_pageIsLoaded();
-        await headerNavbar.openAccountMenu();
         await accountListPage.check_pageIsLoaded();
 
         await accountListPage.check_walletDetailsButtonIsDisplayed();
