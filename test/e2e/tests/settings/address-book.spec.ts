088e2a9403eb9cb5194a3dcdac21fbd3c496076c
Nidhi Kumari <nidhi.kumari@consensys.net>
2025-07-16 17:19:36 +0100

    test: added new address book test (#34271)
    This PR is to add e2e tests for contacts and address book
    Tests check the implementation of network picker
    
    CHANGELOG entry:
    
    ## **Related issues**
    
    Fixes:
    
    ## **Manual testing steps**
    
    1. Go to this page...
    2.
    3.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [ ] I've followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
    Extension Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: Howard Braham <howrad@gmail.com>

--- test/e2e/tests/settings/address-book.spec.ts
+++ test/e2e/tests/settings/address-book.spec.ts
@@ -139,6 +139,35 @@ describe('Address Book', function (this: Suite) {
     );
   });
 
+  it('Adds a new contact to the address book on a different chain', async function () {
+    await withFixtures(
+      {
+        fixtures: new FixtureBuilder().build(),
+        title: this.test?.fullTitle(),
+      },
+      async ({ driver }) => {
+        await loginWithBalanceValidation(driver);
+
+        await new HeaderNavbar(driver).openSettingsPage();
+        const settingsPage = new SettingsPage(driver);
+        await settingsPage.check_pageIsLoaded();
+        await settingsPage.goToContactsSettings();
+
+        const contactsPage = new ContactsPage(driver);
+        await contactsPage.check_pageIsLoaded();
+        await contactsPage.addContactNewChain(
+          'Test User',
+          '0x56A355d3427bC2B1E22c78197AF091230919Cc2A',
+          'Sepolia',
+        );
+        await contactsPage.check_contactDisplayed({
+          contactName: 'Test User',
+          address: shortenAddress('0x56A355d3427bC2B1E22c78197AF091230919Cc2A'),
+        });
+      },
+    );
+  });
+
   it('Edit entry in address book', async function () {
     await withFixtures(
       {
@@ -173,6 +202,7 @@ describe('Address Book', function (this: Suite) {
           existingContactName: 'Test Name 1',
           newContactName: 'Test Name Edit',
           newContactAddress: '0x74cE91B75935D6Bedc27eE002DeFa566c5946f74',
+          newNetwork: 'Sepolia',
         });
         await contactsPage.check_contactDisplayed({
           contactName: 'Test Name Edit',
@@ -181,7 +211,6 @@ describe('Address Book', function (this: Suite) {
       },
     );
   });
-
   it('Deletes existing entry from address book', async function () {
     await withFixtures(
       {
@@ -219,6 +248,45 @@ describe('Address Book', function (this: Suite) {
           contactName: 'Test Name 1',
           address: shortenAddress('0x2f318C334780961FB129D2a6c30D0763d9a5C970'),
           shouldDisplay: false,
+        });
+      },
+    );
+  });
+
+  it('User can add same address contacts on different chains', async function () {
+    await withFixtures(
+      {
+        fixtures: new FixtureBuilder().build(),
+        title: this.test?.fullTitle(),
+      },
+      async ({ driver }) => {
+        await loginWithBalanceValidation(driver);
+
+        await new HeaderNavbar(driver).openSettingsPage();
+        const settingsPage = new SettingsPage(driver);
+        await settingsPage.check_pageIsLoaded();
+        await settingsPage.goToContactsSettings();
+
+        const contactsPage = new ContactsPage(driver);
+        await contactsPage.check_pageIsLoaded();
+        await contactsPage.addContact(
+          'Test User 1',
+          '0x56A355d3427bC2B1E22c78197AF091230919Cc2A',
+        );
+        await contactsPage.check_contactDisplayed({
+          contactName: 'Test User 1',
+          address: shortenAddress('0x56A355d3427bC2B1E22c78197AF091230919Cc2A'),
+        });
+
+        await contactsPage.check_pageIsLoaded();
+        await contactsPage.addContactNewChain(
+          'Test User 2',
+          '0x56A355d3427bC2B1E22c78197AF091230919Cc2A',
+          'Sepolia',
+        );
+        await contactsPage.check_contactDisplayed({
+          contactName: 'Test User 2',
+          address: shortenAddress('0x56A355d3427bC2B1E22c78197AF091230919Cc2A'),
         });
       },
     );
