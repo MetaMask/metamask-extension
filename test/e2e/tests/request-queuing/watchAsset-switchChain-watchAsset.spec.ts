7f0d1013be88eb20269255d7c6aef938d06cb3b9
Maggie <magdalena.mladin@consensys.net>
2025-07-17 10:12:21 +0300

    test: Migrate request queuing watchAsset-switchChain-watchAsset test to Typescript and POM

---
+++ test/e2e/tests/request-queuing/watchAsset-switchChain-watchAsset.spec.ts
@@ -0,0 +1,87 @@
+import { Suite } from 'mocha';
+import { withFixtures, WINDOW_TITLES } from '../../helpers';
+import FixtureBuilder from '../../fixture-builder';
+import { Driver } from '../../webdriver/driver';
+import { loginWithBalanceValidation } from '../../page-objects/flows/login.flow';
+import TestDapp from '../../page-objects/pages/test-dapp';
+import TransactionConfirmation from '../../page-objects/pages/confirmations/redesign/transaction-confirmation';
+import AddTokensModal from '../../page-objects/pages/dialog/add-tokens';
+
+describe('Request Queue WatchAsset -> SwitchChain -> WatchAsset', function (this: Suite) {
+  it('should not batch subsequent watchAsset token into first watchAsset confirmation with a switchChain in the middle', async function () {
+    const port = 8546;
+    const chainId = 1338;
+    await withFixtures(
+      {
+        dapp: true,
+        fixtures: new FixtureBuilder()
+          .withNetworkControllerDoubleNode()
+          .withPermissionControllerConnectedToTestDapp()
+          .build(),
+        localNodeOptions: [
+          {
+            type: 'anvil',
+          },
+          {
+            type: 'anvil',
+            options: {
+              port,
+              chainId,
+            },
+          },
+        ],
+        title: this.test?.fullTitle(),
+      },
+
+      async ({ driver }: { driver: Driver }) => {
+        await loginWithBalanceValidation(driver);
+
+        const testDapp = new TestDapp(driver);
+        await testDapp.openTestDappPage();
+
+        // Create Token
+        await testDapp.findAndClickCreateToken();
+        await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
+
+        const transactionConfirmation = new TransactionConfirmation(driver);
+        await transactionConfirmation.clickFooterConfirmButton();
+
+        // Wait for token address to populate in dapp
+        await driver.switchToWindowWithTitle(WINDOW_TITLES.TestDApp);
+        await testDapp.check_TokenAddressesValue(
+          '0x581c3C1A2A4EBDE2A0Df29B5cf4c116E42945947',
+        );
+
+        // Watch Asset 1st call
+        await testDapp.clickAddTokenToWallet();
+
+        await driver.switchToWindowWithTitle(WINDOW_TITLES.TestDApp);
+
+        // Switch Ethereum Chain
+        const switchEthereumChainRequest = JSON.stringify({
+          jsonrpc: '2.0',
+          method: 'wallet_switchEthereumChain',
+          params: [{ chainId: '0x53a' }],
+        });
+
+        await driver.executeScript(
+          `window.ethereum.request(${switchEthereumChainRequest})`,
+        );
+
+        await driver.switchToWindowWithTitle(WINDOW_TITLES.TestDApp);
+
+        // Watch Asset 2nd call
+        await testDapp.clickAddTokenToWallet();
+
+        await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
+
+        // Wait for token to show in list of tokens to watch
+        const addTokensModal = new AddTokensModal(driver);
+        await addTokensModal.check_pageIsLoaded();
+
+        // Confirm only 2 tokens are present in suggested token list
+        await addTokensModal.check_SuggestedTokensCount(2);
+      },
+    );
+  });
+});
