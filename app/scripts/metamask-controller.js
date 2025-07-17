602efc918c0dafd897872afd771d2d357545837c
Mark Stacey <markjstacey@gmail.com>
2025-07-17 18:26:57 -0230

    chore: Add buttons to test error capture
    Add two buttons to the "Developer Options" settings page to test that
    captured Sentry errors work correctly (one button for the UI, one for
    the background).
    
    We have two similar buttons already for unhandled errors, but not for
    explicitly handled errors.
    
    These will assist with manual testing for this bug: #34371

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -4351,6 +4351,7 @@ export default class MetamaskController extends EventEmitter {
 
       // E2E testing
       throwTestError: this.throwTestError.bind(this),
+      captureTestError: this.captureTestError.bind(this),
 
       // NameController
       updateProposedNames: this.nameController.updateProposedNames.bind(
@@ -8384,6 +8385,21 @@ export default class MetamaskController extends EventEmitter {
       const error = new Error(message);
       error.name = 'TestError';
       throw error;
+    });
+  }
+
+  /**
+   * Capture an artificial error in a timeout handler for testing purposes.
+   *
+   * @param message - The error message.
+   * @deprecated This is only mean to facilitiate E2E testing. We should not
+   * use this for handling errors.
+   */
+  captureTestError(message) {
+    setTimeout(() => {
+      const error = new Error(message);
+      error.name = 'TestError';
+      global.sentry.captureException(error);
     });
   }
 
