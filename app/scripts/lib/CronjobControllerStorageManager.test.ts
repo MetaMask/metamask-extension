cc9cebecde28950d0010b03176ba8785bf4a6257
Mark Stacey <markjstacey@gmail.com>
2025-07-18 16:10:35 -0230

    Add unit tests

---
+++ app/scripts/lib/CronjobControllerStorageManager.test.ts
@@ -0,0 +1,81 @@
+import {
+  CronjobControllerStorageManager,
+  CronjobControllerStorageKey,
+} from './CronjobControllerStorageManager';
+import browser from 'webextension-polyfill';
+
+jest.mock('webextension-polyfill', () => {
+  return {
+    storage: {
+      local: {
+        get: jest.fn(),
+        set: jest.fn(),
+      },
+    },
+  };
+});
+const mockedBrowser = jest.mocked(browser);
+
+describe('CronjobControllerStorageManager', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+    mockedBrowser.storage.local.get.mockImplementation(async () => {
+      return {
+        mockKey: 'mockData',
+      };
+    });
+    mockedBrowser.storage.local.set.mockImplementation(() => Promise.resolve());
+  });
+
+  describe('init', () => {
+    it('requests the correct state key', async () => {
+      const manager = new CronjobControllerStorageManager();
+
+      await manager.init();
+
+      expect(browser.storage.local.get).toHaveBeenCalledWith(
+        CronjobControllerStorageKey,
+      );
+    });
+  });
+
+  describe('getInitialState', () => {
+    it('throws if called before initialization', () => {
+      const manager = new CronjobControllerStorageManager();
+
+      expect(() => manager.getInitialState()).toThrow(
+        'CronjobStorageManager not yet initialized',
+      );
+    });
+
+    it('returns initial controller state', async () => {
+      const manager = new CronjobControllerStorageManager();
+      await manager.init();
+
+      const data = manager.getInitialState();
+
+      expect(data).toStrictEqual({ mockKey: 'mockData' });
+    });
+  });
+
+  describe('set', () => {
+    it('throws if called before initialization', () => {
+      const manager = new CronjobControllerStorageManager();
+
+      expect(() => manager.set({ foo: 'bar' })).toThrow(
+        'CronjobStorageManager not yet initialized',
+      );
+    });
+
+    it('sets state', async () => {
+      const manager = new CronjobControllerStorageManager();
+      await manager.init();
+
+      manager.set({ updatedMockkey: 'updatedData' });
+
+      expect(browser.storage.local.set).toHaveBeenCalledWith({
+        [CronjobControllerStorageKey]: { updatedMockkey: 'updatedData' },
+      });
+    });
+  });
+});
