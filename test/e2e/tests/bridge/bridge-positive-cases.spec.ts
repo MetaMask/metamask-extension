977041efec004a12e7c8698d5a90965891adf20d
salimtb <salim.toubal@outlook.com>
2025-07-18 14:03:14 +0200

    fix: add e2e test

--- test/e2e/tests/bridge/bridge-positive-cases.spec.ts
+++ test/e2e/tests/bridge/bridge-positive-cases.spec.ts
@@ -1,5 +1,5 @@
 import { Suite } from 'mocha';
-import { unlockWallet, withFixtures } from '../../helpers';
+import { largeDelayMs, unlockWallet, withFixtures } from '../../helpers';
 import HomePage from '../../page-objects/pages/home/homepage';
 import {
   switchToNetworkFromSendFlow,
@@ -8,6 +8,10 @@ import {
 import { disableStxSetting } from '../../page-objects/flows/toggle-stx-setting.flow';
 import { DEFAULT_BRIDGE_FEATURE_FLAGS } from './constants';
 import { bridgeTransaction, getBridgeFixtures } from './bridge-test-utils';
+import BridgeQuotePage from '../../page-objects/pages/bridge/quote-page';
+import NetworkManager, {
+  NetworkId,
+} from '../../page-objects/pages/network-manager';
 
 describe('Bridge tests', function (this: Suite) {
   this.timeout(160000); // This test is very long, so we need an unusually high timeout
@@ -87,6 +91,50 @@ describe('Bridge tests', function (this: Suite) {
           },
           6,
         );
+      },
+    );
+  });
+
+  it('Execute bridge transactions on non enabled networks', async function () {
+    await withFixtures(
+      getBridgeFixtures(
+        this.test?.fullTitle(),
+        DEFAULT_BRIDGE_FEATURE_FLAGS,
+        false,
+      ),
+      async ({ driver }) => {
+        await unlockWallet(driver);
+
+        // disable Linea network
+        const networkManager = new NetworkManager(driver);
+        await networkManager.openNetworkManager();
+        await networkManager.deselectNetwork(NetworkId.LINEA);
+        await networkManager.closeNetworkManager();
+
+        await driver.delay(largeDelayMs);
+
+        // disable smart transactions step by step for all bridge flows
+        // we cannot use fixtures because migration 135 overrides the opt in value to true
+        await disableStxSetting(driver);
+
+        // Navigate to Bridge page
+        const homePage = new HomePage(driver);
+        await homePage.startBridgeFlow();
+
+        const bridgePage = new BridgeQuotePage(driver);
+        await bridgePage.enterBridgeQuote({
+          amount: '25',
+          tokenFrom: 'ETH',
+          tokenTo: 'DAI',
+          fromChain: 'Linea',
+          toChain: 'Ethereum',
+        });
+
+        await bridgePage.goBack();
+
+        // check if the Linea network is selected
+        await networkManager.openNetworkManager();
+        await networkManager.checkNetworkIsSelected(NetworkId.LINEA);
       },
     );
   });
