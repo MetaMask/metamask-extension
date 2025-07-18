59fdbe9e61750c5afa05141472f2842b666e4fcc
Mark Stacey <markjstacey@gmail.com>
2025-07-18 13:56:56 -0230

    fix: Fix frequent writes from CronjobController
    Prevent frequent high-volume writes caused by state updates from the
    CronjobController. The controller state includes an event with a
    timestamp that can be updated very frequently if the user has a snap
    installed with a scheduled recurring event with a short interval. It
    can even run while the wallet is otherwise idle, causing the entirety
    of wallet state to be written to disk repeatedly.
    
    We have temporarily mitigated the problem by moving the
    CronjobController state to a new top-level storage key, so that it
    can be written to independently of other state. This controller's state
    is very small and should not cause any significant amount of write
    volume.

---
+++ app/scripts/lib/CronjobControllerStorageManager.ts
@@ -0,0 +1,57 @@
+import type { Json } from '@metamask/utils';
+
+export const CronjobControllerStorageKey = 'temp-cronjob-storage';
+
+/**
+ * A storage manager for CronjobController state.
+ *
+ * @deprecated This is a temporary fix, please do not use this class (or any
+ * similar patterns) elsewhere.
+ */
+export class CronjobControllerStorageManager {
+  /**
+   * The initial CronjobController data.
+   */
+  #initialStorage: Json = null;
+
+  /**
+   * Whether the storage manager has been initialized or not.
+   */
+  #initialized = false;
+
+  /**
+   * Initialize the storage manager.
+   */
+  async init() {
+    this.#initialStorage = await browser.storage.local.get(
+      CronjobControllerStorageKey,
+    );
+    this.#initialized = true;
+  }
+
+  /**
+   * Get the initial CronjobController state.
+   *
+   * @returns The initial CronjobController state.
+   */
+  getInitialState() {
+    if (this.#initialized) {
+      throw new Error('CronjobStorageManager not yet initialized');
+    }
+    return this.#initialStorage;
+  }
+
+  /**
+   * Set the CronjobController state.
+   *
+   * @param data - The CronjobController state to set.
+   */
+  set(data: Json) {
+    if (this.#initialized) {
+      throw new Error('CronjobStorageManager not yet initialized');
+    }
+    browser.storage.local
+      .set({ [CronjobControllerStorageKey]: data })
+      .catch(console.error);
+  }
+}
