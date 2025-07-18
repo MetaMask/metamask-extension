753d86a10baeb3c110c7ca173bb7842f524e148b
Mark Stacey <markjstacey@gmail.com>
2025-07-18 09:25:30 -0230

    chore: Improve accuracy of test error inline comments
    The inline comments on functions relating to "test errors" have been
    updated to clarify that they're used in both manual and E2E testing,
    not just E2E testing. This better explains why they're not enabled
    only in test builds.

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -4368,7 +4368,7 @@ export default class MetamaskController extends EventEmitter {
           notificationServicesController,
         ),
 
-      // E2E testing
+      // Testing
       throwTestError: this.throwTestError.bind(this),
       captureTestError: this.captureTestError.bind(this),
 
@@ -8493,7 +8493,7 @@ export default class MetamaskController extends EventEmitter {
    * Throw an artificial error in a timeout handler for testing purposes.
    *
    * @param message - The error message.
-   * @deprecated This is only mean to facilitiate E2E testing. We should not
+   * @deprecated This is only mean to facilitiate manual and E2E testing. We should not
    * use this for handling errors.
    */
   throwTestError(message) {
@@ -8508,7 +8508,7 @@ export default class MetamaskController extends EventEmitter {
    * Capture an artificial error in a timeout handler for testing purposes.
    *
    * @param message - The error message.
-   * @deprecated This is only mean to facilitiate E2E testing. We should not
+   * @deprecated This is only mean to facilitiate manual and E2E tests testing. We should not
    * use this for handling errors.
    */
   captureTestError(message) {
