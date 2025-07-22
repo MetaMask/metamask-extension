484b79bbe0e26ddfae4e6c6380b36195febf821c
Mark Stacey <markjstacey@gmail.com>
2025-07-22 10:17:35 -0230

    fix: Prevent redundant persist store updates
    The `ComposableObserbaleStore` class has been updated to prevent no-op
    state updates in cases where a non-persisted property is updated. The
    store now introspects patches to see if any of them impact any
    persisted property.
    
    Relates to #33879

--- app/scripts/metamask-controller.test.js
+++ app/scripts/metamask-controller.test.js
@@ -110,6 +110,13 @@ function* ulidGenerator(ulids = mockULIDs) {
   throw new Error('should not be called after exhausting provided IDs');
 }
 
+/**
+ * Generate mock patches for a complete state replacement.
+ */
+function getMockPatches() {
+  return[{ op: 'replace', path: [], value: {} }];
+}
+
 let mockUlidGenerator = ulidGenerator();
 
 jest.mock('ulid', () => ({
@@ -380,6 +387,7 @@ describe('MetaMaskController', () => {
       metamaskController.controllerMessenger.publish(
         'PreferencesController:stateChange',
         preferences,
+        getMockPatches()
       );
       await flushPromises();
     }
@@ -3759,6 +3767,7 @@ describe('MetaMaskController', () => {
         metamaskController.controllerMessenger.publish(
           'CurrencyRateController:stateChange',
           { currentCurrency: mockCurrency },
+          getMockPatches()
         );
 
         expect(
