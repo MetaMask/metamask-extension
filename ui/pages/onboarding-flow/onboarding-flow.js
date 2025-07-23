9f8d70c7e28091fde9ba9560ddf631e6725d5dd7
lwin <lwin@tor.us>
2025-07-23 13:27:11 +0800

    fix: fixed incorrect navigtion in social login flow upon clicking browser back button

--- ui/pages/onboarding-flow/onboarding-flow.js
+++ ui/pages/onboarding-flow/onboarding-flow.js
@@ -224,7 +224,7 @@ export default function OnboardingFlow() {
     }
 
     setSecretRecoveryPhrase(retrievedSecretRecoveryPhrase);
-    history.push(nextRoute);
+    history.replace(nextRoute);
   };
 
   const handleImportWithRecoveryPhrase = async (password, srp) => {
