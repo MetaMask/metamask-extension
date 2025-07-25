deaef8ce7db9416bd0cebbd7caa502b0ed925927
Nidhi Kumari <nidhi.kumari@consensys.net>
2025-07-25 18:56:56 +0000

    release(runway): cherry-pick fix: enable multiple networks when added via dapp cp-13.0.0 (#34507)
    When adding a new network via a dapp, the network gets added and
    selected and all pre selected networks should stay selected.
    
    
    ## **Related issues**
    
    Fixes: #34454 
    
    ## **Manual testing steps**
    
    1. Run extension
    2. conect to dapp, 
    3. check enabled networks in MM
    4. trying adding new network via dapp
    5. New network should be added and selected in network manager along
    with previously selected network
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    NA
    ### **After**
    
    
    
    https://github.com/user-attachments/assets/1db122e3-511b-4df8-b6ee-aa4830b90814
    
    
    
    
    ## **Pre-merge author checklist**
    
    - [ ] I've followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
    Extension Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -7452,6 +7452,11 @@ export default class MetamaskController extends EventEmitter {
       setEnabledNetworks: (chainIds, namespace) => {
         this.networkOrderController.setEnabledNetworks(chainIds, namespace);
       },
+      getEnabledNetworks: (namespace) => {
+        return (
+          this.networkOrderController.state.enabledNetworkMap[namespace] || {}
+        );
+      },
       getCurrentChainIdForDomain: (domain) => {
         const networkClientId =
           this.selectedNetworkController.getNetworkClientIdForDomain(domain);
