2b698301a36d74fe6c91e4d7b9c0943e33fd86c0
imblue <106779544+imblue-dabadee@users.noreply.github.com>
2025-07-28 12:02:02 -0500

    fix: dapp scanning on connect cp-13.0.0 (#34606)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    2. What is the improvement/solution?
    -->
    
    Previously, it would call dapp scanning every time `eth_accounts` is
    called however we want it to call only when the user is:
    1. connected
    2. prompted with a connect screen
    
    This change correctly fixes for that.
    
    [![Open in GitHub
    Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/34606?quickstart=1)
    
    <!--
    If this PR is not End-User-Facing and should not show up in the
    CHANGELOG, you can choose to either:
    1. Write `CHANGELOG entry: null`
    2. Label with `no-changelog`
    
    If this PR is End-User-Facing, please write a short User-Facing
    description in the past tense like:
    `CHANGELOG entry: Added a new tab for users to see their NFTs`
    `CHANGELOG entry: Fixed a bug that was causing some NFTs to flicker`
    
    (This helps the Release Engineer do their job more quickly and
    accurately)
    -->
    
    CHANGELOG entry:
    
    Fixes:
    https://github.com/orgs/MetaMask/projects/103/views/5?pane=issue&itemId=121226503&issue=MetaMask%7CMetaMask-planning%7C5429
    
    1. Open the network inspector for the extension
    2. Open a dapp (e.g. `opensea.io`).
    3. Check that
    `https://dapp-scanning.api.cx.metamask.io/v2/scan?url={dappUrl}` has not
    been sent (if not connected)
    4. Connect to the dapp
    5. Check that
    `https://dapp-scanning.api.cx.metamask.io/v2/scan?url={dappUrl}` has
    been made.
    6. Add a console.log to the `trust-signals-middleware.ts`
    ```javascript
    } else if (isConnected(req, getPermittedAccounts)) {
            // add here
            console.log("test");
            scanUrl(req, phishingController);
    ```
    7. Go to the dapp again and check that if the console.log appears, that
    no network request has been made.
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    <!-- [screenshots/recordings] -->
    
    <!-- [screenshots/recordings] -->
    
    - [x] I've followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
    Extension Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -7573,6 +7573,7 @@ export default class MetamaskController extends EventEmitter {
         this.appStateController,
         this.phishingController,
         this.preferencesController,
+        this.getPermittedAccounts.bind(this),
       ),
     );
 
