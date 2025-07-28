afd4fd08e009908a92af480fe3e38e9ee4a0081b
Tuna <tunguyenanh@tunatech.org>
2025-07-28 15:59:15 +0700

    fix: correct toprf get pubkey request mock

--- test/e2e/helpers/seedless-onboarding/mocks.ts
+++ test/e2e/helpers/seedless-onboarding/mocks.ts
@@ -440,6 +440,8 @@ export class OAuthMockttpService {
             pub_key: options?.passwordOutdated
               ? MockAuthPubKey
               : this.#latestAuthPubKey,
+            key_index: 1,
+            node_index: nodeIndex,
           },
         },
       };
