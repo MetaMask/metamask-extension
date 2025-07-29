a2555587c571d1ed3ffb567d20c25a075dc1c771
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2025-07-29 11:07:20 +0200

    Remove undefined property from `trace` result

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -7910,7 +7910,11 @@ export default class MetamaskController extends EventEmitter {
           this.controllerMessenger,
           'NetworkController:getNetworkClientById',
         ),
-        startTrace: trace,
+        startTrace: (options) => {
+          // eslint-disable-next-line no-unused-vars
+          const { _isStandaloneSpan, ...result } = trace(options);
+          return result;
+        },
         endTrace,
         ///: BEGIN:ONLY_INCLUDE_IF(keyring-snaps)
         handleSnapRpcRequest: (args) =>
