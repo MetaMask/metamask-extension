315db454f8bf5a993ac6e39027addf60bbed4698
ffmcgee <joao.carlos@consensys.net>
2025-07-30 09:38:06 +0200

    test: add e2e test making sure that permissions granted in the switch chain flow are additive and don't overwrite previous permissions

--- test/e2e/tests/network/switch-custom-network.spec.ts
+++ test/e2e/tests/network/switch-custom-network.spec.ts
@@ -1,10 +1,12 @@
+import { strict as assert } from 'assert';
 import { Suite } from 'mocha';
 import FixtureBuilder from '../../fixture-builder';
-import { WINDOW_TITLES, withFixtures } from '../../helpers';
+import { regularDelayMs, WINDOW_TITLES, withFixtures } from '../../helpers';
 import AddNetworkConfirmation from '../../page-objects/pages/confirmations/redesign/add-network-confirmations';
 import TestDapp from '../../page-objects/pages/test-dapp';
 import { loginWithBalanceValidation } from '../../page-objects/flows/login.flow';
 import AssetListPage from '../../page-objects/pages/home/asset-list';
+import { getPermittedChains } from './common';
 
 describe('Switch ethereum chain', function (this: Suite) {
   it('should successfully change the network in response to wallet_switchEthereumChain', async function () {
@@ -94,6 +96,46 @@ describe('Switch ethereum chain', function (this: Suite) {
           text: 'Polygon',
           tag: 'p',
         });
+      },
+    );
+  });
+
+  it('should incrementally add new requested network to existing permissions without overriding them', async function () {
+    await withFixtures(
+      {
+        dapp: true,
+        fixtures: new FixtureBuilder()
+          .withPermissionControllerConnectedToTestDapp() // Connected to Localhost
+          .build(),
+        title: this.test?.fullTitle(),
+      },
+      async ({ driver }) => {
+        await loginWithBalanceValidation(driver);
+        const testDapp = new TestDapp(driver);
+        await testDapp.openTestDappPage();
+        await testDapp.check_pageIsLoaded();
+
+        const switchEthereumChainRequest = JSON.stringify({
+          jsonrpc: '2.0',
+          method: 'wallet_switchEthereumChain',
+          params: [{ chainId: '0x1' }], // Hex Chain ID for Ethereum Mainnet
+        });
+
+        await driver.executeScript(
+          `window.ethereum.request(${switchEthereumChainRequest})`,
+        );
+
+        await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
+
+        await driver.clickElement({ text: 'Confirm', tag: 'button' });
+
+        await driver.delay(regularDelayMs);
+
+        await driver.switchToWindowWithTitle(WINDOW_TITLES.TestDApp);
+
+        const afterPermittedChains = await getPermittedChains(driver);
+
+        assert.deepEqual(afterPermittedChains, ['0x539', '0x1']); // Hex Chain IDs for Localhost and Ethereum Mainnet
       },
     );
   });
