42bfb6080aa4678c7bbfec209979abd0fa48d4c7
Lwin <147362763+lwin-kyaw@users.noreply.github.com>
2025-07-31 17:12:33 +0800

    fix: incorrect navigation upon refreshing the page during the social login onboarding (#34716)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    2. What is the improvement/solution?
    -->
    
    This PR fixes the incorrect navigation upon refreshing the page with
    social login.
    After social login, user will be redirected to the `create-password`
    page or `onboarding-unlock` page depending on the account availability.
    Refreshing on the page (`create-password` or `onboarding-unlock`) should
    not redirect back to the login screen as social login is already
    completed.
    
    [![Open in GitHub
    Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/34716?quickstart=1)
    
    ## **Changelog**
    
    <!--
    If this PR is not End-User-Facing and should not show up in the
    CHANGELOG, you can choose to either:
    1. Write `CHANGELOG entry: null`
    2. Label with `no-changelog`
    
    If this PR is End-User-Facing, please write a short User-Facing
    description in the past tense like:
    `CHANGELOG entry: Added a new tab for users to see their NFTs`
    `CHANGELOG entry: Fixed a bug that was causing some NFTs to flicker`
    
    (This helps the Release Engineer do their job more quickly and
    accurately)
    -->
    
    CHANGELOG entry:
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/metamask-extension/issues/34655
    
    ## **Manual testing steps**
    
    1. Go to this page...
    2.
    3.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [ ] I've followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
    Extension Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: Chaitanya Potti <chaitanya.potti@gmail.com>

---
+++ ui/selectors/seedless-onboarding/social-sync.test.ts
@@ -0,0 +1,101 @@
+import { AuthConnection } from '@metamask/seedless-onboarding-controller';
+import {
+  BackupState,
+  getIsSocialLoginFlowInitialized,
+  getSocialLoginEmail,
+  getSocialLoginType,
+} from './social-sync';
+
+const MOCK_NODE_AUTH_TOKENS = [
+  {
+    authToken: 'authToken',
+    nodeIndex: 1,
+    nodePubKey: 'nodePubKey',
+  },
+  {
+    authToken: 'authToken2',
+    nodeIndex: 2,
+    nodePubKey: 'nodePubKey2',
+  },
+  {
+    authToken: 'authToken3',
+    nodeIndex: 3,
+    nodePubKey: 'nodePubKey3',
+  },
+];
+
+const MOCK_STATE: BackupState = {
+  metamask: {
+    userId: 'mock-user-id',
+    accessToken: 'mock-access-token',
+    refreshToken: 'mock-refresh-token',
+    nodeAuthTokens: MOCK_NODE_AUTH_TOKENS,
+    socialBackupsMetadata: [],
+    socialLoginEmail: 'mock-social-login-email',
+    authConnection: AuthConnection.Google,
+  },
+};
+
+describe('social-sync selectors', () => {
+  describe('#getSocialLoginType', () => {
+    it('returns the social login type', () => {
+      expect(getSocialLoginType(MOCK_STATE)).toBe(AuthConnection.Google);
+    });
+  });
+
+  describe('#getSocialLoginEmail', () => {
+    it('returns the social login email', () => {
+      expect(getSocialLoginEmail(MOCK_STATE)).toBe('mock-social-login-email');
+    });
+
+    it('returns undefined if the social login email is not set', () => {
+      const state = {
+        ...MOCK_STATE,
+        metamask: {
+          ...MOCK_STATE.metamask,
+          socialLoginEmail: undefined,
+        },
+      };
+      expect(getSocialLoginEmail(state)).toBeUndefined();
+    });
+  });
+
+  describe('#getIsSocialLoginFlowInitialized', () => {
+    it('returns true if the social login flow has been initialized and the user is authenticated', () => {
+      expect(getIsSocialLoginFlowInitialized(MOCK_STATE)).toBe(true);
+    });
+
+    it('returns false if the social login flow has not been initialized', () => {
+      const state = {
+        ...MOCK_STATE,
+        metamask: {
+          ...MOCK_STATE.metamask,
+          socialLoginEmail: undefined,
+        },
+      };
+      expect(getIsSocialLoginFlowInitialized(state)).toBe(false);
+    });
+
+    it('returns false if the user is not authenticated', () => {
+      const state = {
+        ...MOCK_STATE,
+        metamask: {
+          ...MOCK_STATE.metamask,
+          userId: undefined,
+        },
+      };
+      expect(getIsSocialLoginFlowInitialized(state)).toBe(false);
+    });
+
+    it('returns false if the `accessToken` is not set in the state', () => {
+      const state = {
+        ...MOCK_STATE,
+        metamask: {
+          ...MOCK_STATE.metamask,
+          accessToken: undefined,
+        },
+      };
+      expect(getIsSocialLoginFlowInitialized(state)).toBe(false);
+    });
+  });
+});
