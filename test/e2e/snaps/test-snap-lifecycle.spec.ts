c00ae22057df8d7bc67fd11b09bac6be854b3853
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2025-08-01 00:13:26 +0200

    Remove timeout

--- test/e2e/snaps/test-snap-lifecycle.spec.ts
+++ test/e2e/snaps/test-snap-lifecycle.spec.ts
@@ -3,12 +3,7 @@ import { TestSnaps } from '../page-objects/pages/test-snaps';
 import SnapInstall from '../page-objects/pages/dialog/snap-install';
 import FixtureBuilder from '../fixture-builder';
 import { loginWithoutBalanceValidation } from '../page-objects/flows/login.flow';
-import {
-  unlockWallet,
-  withFixtures,
-  WINDOW_TITLES,
-  veryLargeDelayMs,
-} from '../helpers';
+import { unlockWallet, withFixtures, WINDOW_TITLES } from '../helpers';
 import { openTestSnapClickButtonAndInstall } from '../page-objects/flows/install-test-snap.flow';
 import { mockLifecycleHooksSnap } from '../mock-response-data/snaps/snap-binary-mocks';
 
@@ -67,7 +62,7 @@ describe('Test Snap Lifecycle Hooks', function () {
           } catch {
             return false;
           }
-        }, veryLargeDelayMs);
+        });
 
         await unlockWallet(driver, { navigate: false });
 
