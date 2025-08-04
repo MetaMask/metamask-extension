cda0557e27a96c29b2a9413671a2b33d54024fe8
Lionell Briones <llenoil@gmail.com>
2025-08-04 18:15:41 +0800

    feat: added onboarding download app page

---
+++ ui/pages/onboarding-flow/download-app/download-app.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import OnboardingPinExtension from './download-app';
+
+export default {
+  title: 'Pages/OnboardingFlow/OnboardingPinExtension',
+};
+
+export const DefaultStory = () => <OnboardingPinExtension />;
+
+DefaultStory.storyName = 'Default';
---
+++ ui/pages/onboarding-flow/download-app/download-app.test.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import reactRouterDom from 'react-router-dom';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { renderWithProvider } from '../../../../test/jest';
+import {
+  ONBOARDING_PIN_EXTENSION_ROUTE,
+  ONBOARDING_WELCOME_ROUTE,
+} from '../../../helpers/constants/routes';
+import { FirstTimeFlowType } from '../../../../shared/constants/onboarding';
+import DownloadApp from './download-app';
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useHistory: jest.fn(() => ({
+    push: jest.fn(),
+    replace: jest.fn(),
+  })),
+}));
+
+const pushMock = jest.fn();
+const replaceMock = jest.fn();
+
+describe('Creation Successful Onboarding View', () => {
+  const arrangeMocks = (
+    stateOverrides = {
+      metamask: {
+        isBackupAndSyncEnabled: false,
+        participateInMetaMetrics: true,
+        isSignedIn: false,
+        useExternalServices: true,
+        internalAccounts: {
+          accounts: {
+            accountId: {
+              address: '0x0000000000000000000000000000000000000000',
+              metadata: {
+                keyring: {
+                  type: 'HD Key Tree',
+                  accounts: ['0x0000000000000000000000000000000000000000'],
+                },
+              },
+            },
+          },
+          selectedAccount: 'accountId',
+        },
+      },
+      appState: {
+        externalServicesOnboardingToggleState: true,
+      },
+    },
+  ) => {
+    const mockStore = {
+      metamask: {
+        providerConfig: {
+          type: 'test',
+        },
+        ...stateOverrides.metamask,
+      },
+      appState: {
+        ...stateOverrides.appState,
+      },
+    };
+    const store = configureMockStore([thunk])(mockStore);
+
+    jest
+      .spyOn(reactRouterDom, 'useHistory')
+      .mockImplementation()
+      .mockReturnValue({ push: pushMock, replace: replaceMock });
+
+    return store;
+  };
+
+  describe('When the "Done" button is clicked', () => {
+    it('should redirect to the onboarding pin extension route', async () => {
+      const store = arrangeMocks();
+
+      const { getByText } = renderWithProvider(<DownloadApp />, store);
+      const continueButton = getByText('Continue');
+
+      fireEvent.click(continueButton);
+
+      expect(pushMock).toHaveBeenCalledTimes(1);
+      expect(pushMock).toHaveBeenCalledWith(ONBOARDING_PIN_EXTENSION_ROUTE);
+    });
+  });
+
+  describe('When the user has not created a wallet', () => {
+    it('should redirect to the onboarding welcome route', () => {
+      const mockState = {
+        metamask: {
+          internalAccounts: {
+            accounts: {},
+            selectedAccount: '',
+          },
+          keyrings: [
+            {
+              type: 'HD Key Tree',
+              accounts: ['0x0000000000000000000000000000000000000000'],
+            },
+          ],
+          firstTimeFlowType: FirstTimeFlowType.create,
+          seedPhraseBackedUp: true,
+        },
+      };
+
+      const store = arrangeMocks(mockState);
+      renderWithProvider(<DownloadApp />, store);
+
+      expect(replaceMock).toHaveBeenCalledTimes(1);
+      expect(replaceMock).toHaveBeenCalledWith(ONBOARDING_WELCOME_ROUTE);
+    });
+  });
+});
