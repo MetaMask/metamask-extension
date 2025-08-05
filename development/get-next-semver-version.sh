7bd2fecf287140572c1f4e28a42bc1ccc3d51620
Gauthier Petetin <gauthier.petetin@consensys.net>
2025-08-05 08:55:58 +0200

    fix(next semver version): stop relying on package.json when crafting next semver version

--- development/get-next-semver-version.sh
+++ development/get-next-semver-version.sh
@@ -15,11 +15,8 @@ VERSION_BRANCHES=$(git branch -r | grep -o 'Version-v[0-9]*\.[0-9]*\.[0-9]*' | g
 # Get the highest version from tags
 VERSION_TAGS=$(git tag | grep -o 'v[0-9]*\.[0-9]*\.[0-9]*' | grep -o '[0-9]*\.[0-9]*\.[0-9]*' | sort --version-sort | tail -n 1)
 
-# Get the version from package.json
-VERSION_PACKAGE=$(node -p "require('./package.json').version")
-
 # Compare versions and keep the highest one
-HIGHEST_VERSION=$(printf "%s\n%s\n%s" "$VERSION_BRANCHES" "$VERSION_TAGS" "$VERSION_PACKAGE" | sort --version-sort | tail -n 1)
+HIGHEST_VERSION=$(printf "%s\n%s" "$VERSION_BRANCHES" "$VERSION_TAGS" | sort --version-sort | tail -n 1)
 
 # Increment the minor version of the highest version found and reset the patch version to 0
 NEXT_VERSION=$(echo "$HIGHEST_VERSION" | awk -F. -v OFS=. '{$2++; $3=0; print}')
