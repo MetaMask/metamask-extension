39327e9bd3ddfbce69daf12bd1c81a0672573181
seaona <seaona@proton.me>
2025-08-07 08:51:22 +0200

    mock

--- test/e2e/tests/account/forgot-password.spec.ts
+++ test/e2e/tests/account/forgot-password.spec.ts
@@ -1,5 +1,7 @@
 import { Mockttp } from 'mockttp';
-import { MockedEndpoint } from '../../mock-e2e';
+import { USER_STORAGE_FEATURE_NAMES } from '@metamask/profile-sync-controller/sdk';
+import { mockIdentityServices } from '../identity/mocks';
+import { UserStorageMockttpController } from '../../helpers/identity/user-storage/userStorageMockttpController';
 import { withFixtures } from '../../helpers';
 import FixtureBuilder from '../../fixture-builder';
 import { Driver } from '../../webdriver/driver';
@@ -13,46 +15,31 @@ import { loginWithBalanceValidation } from '../../page-objects/flows/login.flow'
 
 const newPassword = 'this is the best password ever';
 
-async function seeAuthenticationRequest(mockServer: Mockttp) {
-  return await mockServer
-    .forPost('https://authentication.api.cx.metamask.io/api/v2/srp/login')
-    // the goal is to know when this request happens, not to mock any specific response
-    .thenCallback(() => {
-      return {
-        statusCode: 200,
-      };
-    });
-}
-
 describe('Forgot password', function () {
+  const userStorageMockttpController = new UserStorageMockttpController();
   it('resets password and then unlock wallet with new password', async function () {
     await withFixtures(
       {
         fixtures: new FixtureBuilder().build(),
-        testSpecificMock: seeAuthenticationRequest,
+        testSpecificMock: (server: Mockttp) => {
+          userStorageMockttpController.setupPath(
+            USER_STORAGE_FEATURE_NAMES.addressBook,
+            server,
+          );
+
+          return mockIdentityServices(server, userStorageMockttpController);
+        },
         title: this.test?.fullTitle(),
       },
       async ({
         driver,
         localNodes,
-        mockedEndpoint,
       }: {
         driver: Driver;
         localNodes: Anvil[] | Ganache[] | undefined[];
-        mockedEndpoint: MockedEndpoint;
       }) => {
         await loginWithBalanceValidation(driver, localNodes[0]);
 
-        // Lock Wallet
-        // We need to wait for this request to happen, before locking the wallet, to avoid the error 'unable to proceed, wallet is locked'
-        // https://github.com/MetaMask/core/blob/main/packages/profile-sync-controller/src/controllers/authentication/AuthenticationController.ts#L263
-        await driver.waitUntil(
-          async () => {
-            const requests = await mockedEndpoint.getSeenRequests();
-            return requests.length > 3;
-          },
-          { interval: 200, timeout: 10000 },
-        );
         const homePage = new HomePage(driver);
         await homePage.headerNavbar.check_pageIsLoaded();
         await homePage.headerNavbar.lockMetaMask();
--- test/e2e/tests/account/lock-account.spec.ts
+++ test/e2e/tests/account/lock-account.spec.ts
@@ -1,49 +1,33 @@
 import { Suite } from 'mocha';
 import { Mockttp } from 'mockttp';
-import { MockedEndpoint } from '../../mock-e2e';
+import { USER_STORAGE_FEATURE_NAMES } from '@metamask/profile-sync-controller/sdk';
+import { mockIdentityServices } from '../identity/mocks';
+import { UserStorageMockttpController } from '../../helpers/identity/user-storage/userStorageMockttpController';
 import { withFixtures } from '../../helpers';
 import FixtureBuilder from '../../fixture-builder';
 import HomePage from '../../page-objects/pages/home/homepage';
 import { Driver } from '../../webdriver/driver';
 import { loginWithBalanceValidation } from '../../page-objects/flows/login.flow';
 
-async function seeAuthenticationRequest(mockServer: Mockttp) {
-  return await mockServer
-    .forPost('https://authentication.api.cx.metamask.io/api/v2/srp/login')
-    // the goal is to know when this request happens, not to mock any specific response
-    .thenCallback(() => {
-      return {
-        statusCode: 200,
-      };
-    });
-}
-
 describe('Lock and unlock', function (this: Suite) {
+  const userStorageMockttpController = new UserStorageMockttpController();
   it('successfully unlocks after lock', async function () {
     await withFixtures(
       {
         fixtures: new FixtureBuilder().build(),
-        testSpecificMock: seeAuthenticationRequest,
+        testSpecificMock: (server: Mockttp) => {
+          userStorageMockttpController.setupPath(
+            USER_STORAGE_FEATURE_NAMES.addressBook,
+            server,
+          );
+
+          return mockIdentityServices(server, userStorageMockttpController);
+        },
         title: this.test?.fullTitle(),
       },
-      async ({
-        driver,
-        mockedEndpoint,
-      }: {
-        driver: Driver;
-        mockedEndpoint: MockedEndpoint;
-      }) => {
+      async ({ driver }: { driver: Driver }) => {
         await loginWithBalanceValidation(driver);
 
-        // We need to wait for this request to happen, before locking the wallet, to avoid the error 'unable to proceed, wallet is locked'
-        // https://github.com/MetaMask/core/blob/main/packages/profile-sync-controller/src/controllers/authentication/AuthenticationController.ts#L263
-        await driver.waitUntil(
-          async () => {
-            const requests = await mockedEndpoint.getSeenRequests();
-            return requests.length > 3;
-          },
-          { interval: 200, timeout: 10000 },
-        );
         const homePage = new HomePage(driver);
         await homePage.headerNavbar.lockMetaMask();
         await loginWithBalanceValidation(driver);
