482851899546240225946b2802ef8a3812c37e3a
ghgoodreau <hunter.goodreau@consensys.net>
2025-08-08 18:19:56 -0500

    remove unused export

--- ui/pages/bridge/quotes/index.ts
+++ ui/pages/bridge/quotes/index.ts
@@ -1,3 +1,2 @@
-export { BridgeQuoteCard } from './bridge-quote-card';
 export { MultichainBridgeQuoteCard } from './multichain-bridge-quote-card';
 export { BridgeQuotesModal } from './bridge-quotes-modal';
