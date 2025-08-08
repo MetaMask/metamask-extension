9cdbd3ecd60d39a9427baee36a1e177b2e8fc161
Frederik Bolding <frederik.bolding@gmail.com>
2025-08-08 14:08:57 +0200

    test: Reject promise when child_process errors

--- development/lib/run-command.js
+++ development/lib/run-command.js
@@ -113,6 +113,8 @@ async function runInShell(command, args, output = undefined) {
         errorSignal = signal;
         return reject(internalError);
       });
+
+      childProcess.once('error', (error) => reject(error));
     });
   } catch (error) {
     /**
