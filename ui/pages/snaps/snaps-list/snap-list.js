f004a57e97fd8fb97633c5d88124f7763f99f8fc
dddddanica <zhaodanica@gmail.com>
2025-08-12 01:09:06 +0100

    refactor(5559): migrate snaps system to react-router-v5-compat

--- ui/pages/snaps/snaps-list/snap-list.js
+++ ui/pages/snaps/snaps-list/snap-list.js
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom-v5-compat';
 import SnapListItem from '../../../components/app/snaps/snap-list-item';
 import { useI18nContext } from '../../../hooks/useI18nContext';
 import {
@@ -40,10 +40,10 @@ import { getSnapRoute } from '../../../helpers/utils/util';
 
 const SnapList = () => {
   const t = useI18nContext();
-  const history = useHistory();
+  const navigate = useNavigate();
   const settingsRef = useRef();
   const onClick = (snap) => {
-    history.push(getSnapRoute(snap.id));
+    navigate(getSnapRoute(snap.id));
   };
 
   useEffect(() => {
@@ -63,7 +63,7 @@ const SnapList = () => {
               ariaLabel="Back"
               iconName="arrow-left"
               size="sm"
-              onClick={() => history.push(DEFAULT_ROUTE)}
+              onClick={() => navigate(DEFAULT_ROUTE)}
             />
           }
         >
