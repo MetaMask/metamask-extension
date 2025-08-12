7333d46a1e1179deb114743a860b5fb9a5afff17
Dario Anongba Varela <dario.anongba@gmail.com>
2025-08-12 17:44:43 +0200

    swaps

--- shared/lib/accounts/bitcoin-wallet-snap.ts
+++ shared/lib/accounts/bitcoin-wallet-snap.ts
@@ -1,7 +1,6 @@
 import { SnapId } from '@metamask/snaps-sdk';
 
 export const BITCOIN_WALLET_SNAP_ID: SnapId =
-  //'npm:@metamask/bitcoin-wallet-snap' as SnapId;
-  'local:http://localhost:8080' as SnapId;
+  'npm:@metamask/bitcoin-wallet-snap' as SnapId;
 
 export const BITCOIN_WALLET_NAME: string = 'Bitcoin';
