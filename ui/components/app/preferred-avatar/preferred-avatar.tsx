afb5462dbf03e7f8afcbcfaa06fb71449c11c0ef
Francis Nepomuceno <n3ps@users.noreply.github.com>
2025-08-15 11:50:37 -0400

    create PreferredAvatar

---
+++ ui/components/app/preferred-avatar/preferred-avatar.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import {
+  AvatarAccount,
+  AvatarAccountProps,
+  AvatarAccountVariant,
+} from '@metamask/design-system-react';
+import { getUseBlockie } from '../../../selectors/selectors';
+
+export function PreferredAvatar(props: Omit<AvatarAccountProps, 'ref'>) {
+  const useBlockie = useSelector(getUseBlockie);
+
+  return (
+    <AvatarAccount
+      variant={
+        useBlockie
+          ? AvatarAccountVariant.Blockies
+          : AvatarAccountVariant.Jazzicon
+      }
+      {...props}
+    />
+  );
+}
