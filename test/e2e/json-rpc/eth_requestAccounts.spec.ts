8702156e1456ac5e2c8210b1dee0a1c7aad64b84
Jiexi Luan <jiexiluan@gmail.com>
2025-08-15 08:54:48 -0700

    add locked scenario eth_requestAccounts

--- test/e2e/json-rpc/eth_requestAccounts.spec.ts
+++ test/e2e/json-rpc/eth_requestAccounts.spec.ts
@@ -3,9 +3,9 @@ import { WINDOW_TITLES, withFixtures } from '../helpers';
 import { loginWithBalanceValidation } from '../page-objects/flows/login.flow';
 import FixtureBuilder from '../fixture-builder';
 import { Driver } from '../webdriver/driver';
-import { WindowHandles } from '../background-socket/window-handles';
 import LoginPage from '../page-objects/pages/login-page';
 import ConnectAccountConfirmation from '../page-objects/pages/confirmations/redesign/connect-account-confirmation';
+import TestDapp from '../page-objects/pages/test-dapp';
 
 describe('eth_requestAccounts', function () {
   it('returns permitted accounts when there are permitted accounts and the wallet is unlocked', async function () {
@@ -21,7 +21,9 @@ describe('eth_requestAccounts', function () {
         await loginWithBalanceValidation(driver);
 
         // eth_requestAccounts
-        await driver.openNewPage(`http://127.0.0.1:8080`);
+        const testDapp = new TestDapp(driver);
+        await testDapp.openTestDappPage();
+        await testDapp.checkPageIsLoaded();
 
         const requestAccountRequest: string = JSON.stringify({
           jsonrpc: '2.0',
@@ -50,7 +52,9 @@ describe('eth_requestAccounts', function () {
       },
       async ({ driver }: { driver: Driver }) => {
         // eth_requestAccounts
-        await driver.openNewPage(`http://127.0.0.1:8080`);
+        const testDapp = new TestDapp(driver);
+        await testDapp.openTestDappPage();
+        await testDapp.checkPageIsLoaded();
 
         const requestAccountRequest: string = JSON.stringify({
           jsonrpc: '2.0',
@@ -68,7 +72,7 @@ describe('eth_requestAccounts', function () {
     );
   });
 
-  it.only('prompts for login when there are no permitted accounts and the wallet is locked', async function () {
+  it('prompts for login when there are no permitted accounts and the wallet is locked', async function () {
     await withFixtures(
       {
         dapp: true,
@@ -76,31 +80,38 @@ describe('eth_requestAccounts', function () {
         title: this.test?.fullTitle(),
       },
       async ({ driver }: { driver: Driver }) => {
-        // eth_requestAccounts
-        await driver.openNewPage(`http://127.0.0.1:8080`);
+        const testDapp = new TestDapp(driver);
+        await testDapp.openTestDappPage();
+        await testDapp.checkPageIsLoaded();
 
         const requestAccountRequest: string = JSON.stringify({
           jsonrpc: '2.0',
           method: 'eth_requestAccounts',
         });
 
-        const requestAccount: Promise<string[]> = driver.executeScript(
-          `return window.ethereum.request(${requestAccountRequest})`,
+        await driver.executeScript(
+          `window.requestAccount = window.ethereum.request(${requestAccountRequest})`,
         );
 
         await driver.waitUntilXWindowHandles(3);
         await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
-        const loginPage = new LoginPage(this.driver);
+        const loginPage = new LoginPage(driver);
         await loginPage.checkPageIsLoaded();
         await loginPage.loginToHomepage();
 
         const connectAccountConfirmation = new ConnectAccountConfirmation(
-          this.driver,
+          driver,
         );
         await connectAccountConfirmation.checkPageIsLoaded();
         await connectAccountConfirmation.confirmConnect();
+        await driver.switchToWindowWithTitle(WINDOW_TITLES.TestDApp);
+        await testDapp.checkPageIsLoaded();
 
-        assert.deepStrictEqual(await requestAccount, [
+        const requestAccount = await driver.executeScript(
+          `return window.requestAccount;`,
+        );
+
+        assert.deepStrictEqual(requestAccount, [
           '0x5cfe73b6021e818b776b421b1c4db2474086a7e1',
         ]);
       },
