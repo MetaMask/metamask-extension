a9a3c89d247388adbc396b2aafd4afe408909eb1
salimtb <salim.toubal@consensys.net>
2025-08-17 15:04:00 +0200

    chore: bump assets controllers to v73.3.0

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -1017,6 +1017,8 @@ export default class MetamaskController extends EventEmitter {
         'PreferencesController:getState',
         'AccountsController:getSelectedAccount',
         'AccountsController:listAccounts',
+        'AccountTrackerController:updateNativeBalances',
+        'AccountTrackerController:updateStakedBalances',
       ],
       allowedEvents: [
         'PreferencesController:stateChange',
@@ -1029,6 +1031,9 @@ export default class MetamaskController extends EventEmitter {
     this.tokenBalancesController = new TokenBalancesController({
       messenger: tokenBalancesMessenger,
       state: initState.TokenBalancesController,
+      useAccountsAPI: false,
+      queryMultipleAccounts:
+        this.preferencesController.state.useMultiAccountBalanceChecker,
       interval: 30000,
     });
 
