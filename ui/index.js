MetaMask/metamask-extension

commit 1e37cc7afed92eb3d21190b1bf9599b5f110b67f

refactor: update UI's background communications to use a single notification listener instead of two (#33211)
## **Description**

Currently, UI listens to `runtime.Port` messages in two places: one is
via our `PortStream` library, and the other is via
`extensionPort.onMessage.addListener` (that is on the "raw" port). For
regular application communications (i.e., NOT start-up error messages)
this PR removes the "raw" port listener and sets up the handlers that
were using that method to use our `PortStream` instead.

This needs to be done because we will soon implement a custom "wire
protocol" for port communications, and so all communication over the
port need to understand this new protocol. The new protocol is being
implemented in https://github.com/MetaMask/metamask-extension/pull/33165

A side effect of the changes I've made here is that in MV3 the UI will
no longer _attempt_ to reconnect to the background service if it ever
disconnects. This isn't a problem because a) it didn't actually work,
and b) our service_worker never gets shutdown (which was the original
reason for the broken reconnection logic).
The reason I removed this reconnection logic is that I would have had to
refactor it to work with the new "single port listener" change, and
refactoring something that is unused and broken doesn't seem like a good
idea.

This PR is blocking the Sev 1 fix:
https://github.com/MetaMask/metamask-extension/pull/33165

[![Open in GitHub
Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/33211?quickstart=1)

## **Related issues**

Related to:

https://github.com/MetaMask/metamask-extension/issues/22533

<!--
## **Changelog**
## **Manual testing steps**

1. Go to this page...
2.
3.

## **Screenshots/Recordings**


### **Before**


### **After**

-->

## **Pre-merge author checklist**

- [x] I've followed [MetaMask Contributor
Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
Extension Coding

ui/index.js
@@ -42,22 +42,35 @@ import { setBackgroundConnection } from './store/background-connection';
 import { getStartupTraceTags } from './helpers/utils/tags';
 import { SEEDLESS_PASSWORD_OUTDATED_CHECK_INTERVAL_MS } from './constants';
 
-export { displayStateCorruptionError } from './helpers/utils/state-corruption-html';
+export { CriticalStartupErrorHandler } from './helpers/utils/critical-startup-error-handler';
+
+const METHOD_START_UI_SYNC = 'startUISync';
 
 log.setLevel(global.METAMASK_DEBUG ? 'debug' : 'warn', false);
 
-let reduxStore;
+/**
+ * @type {PromiseWithResolvers<ReturnType<typeof configureStore>>}
+ */
+const reduxStore = Promise.withResolvers();
 
 /**
  * Method to update backgroundConnection object use by UI
  *
  * @param backgroundConnection - connection object to background
+ * @param handleStartUISync - function to call when startUISync notification is received
  */
-export const updateBackgroundConnection = (backgroundConnection) => {
+export const connectToBackground = (
+  backgroundConnection,
+  handleStartUISync,
+) => {
   setBackgroundConnection(backgroundConnection);
-  backgroundConnection.onNotification((data) => {
-    if (data.method === 'sendUpdate') {
-      reduxStore.dispatch(actions.updateMetamaskState(data.params[0]));
+  backgroundConnection.onNotification(async (data) => {
+    const { method } = data;
+    if (method === 'sendUpdate') {
+      const store = await reduxStore.promise;
+      store.dispatch(actions.updateMetamaskState(data.params[0]));
+    } else if (method === METHOD_START_UI_SYNC) {
+      await handleStartUISync();
     } else {
       throw new Error(
         `Internal JSON-RPC Notification Not Handled:\n\n ${JSON.stringify(
@@ -76,7 +89,7 @@ export default async function launchMetamaskUi(opts) {
     backgroundConnection.getState.bind(backgroundConnection),
   );
 
-  const store = await startApp(metamaskState, backgroundConnection, opts);
+  const store = await startApp(metamaskState, opts);
 
   await backgroundConnection.startPatches();
 
@@ -89,15 +102,10 @@ export default async function launchMetamaskUi(opts) {
  * Method to setup initial redux store for the ui application
  *
  * @param {*} metamaskState - flatten background state
- * @param {*} backgroundConnection - rpc client connecting to the background process
  * @param {*} activeTab - active browser tab
  * @returns redux store
  */
-export async function setupInitialStore(
-  metamaskState,
-  backgroundConnection,
-  activeTab,
-) {
+export async function setupInitialStore(metamaskState, activeTab) {
   // parse opts
   if (!metamaskState.featureFlags) {
     metamaskState.featureFlags = {};
@@ -126,8 +134,6 @@ export async function setupInitialStore(
       en: enLocaleMessages,
     },
   };
-
-  updateBackgroundConnection(backgroundConnection);
 
   if (getEnvironmentType() === ENVIRONMENT_TYPE_POPUP) {
     const { origin } = draftInitialState.activeTab;
@@ -163,7 +169,7 @@ export async function setupInitialStore(
   }
 
   const store = configureStore(draftInitialState);
-  reduxStore = store;
+  reduxStore.resolve(store);
 
   const unapprovedTxs = getUnapprovedTransactions(metamaskState);
 
@@ -189,7 +195,7 @@ export async function setupInitialStore(
   return store;
 }
 
-async function startApp(metamaskState, backgroundConnection, opts) {
+async function startApp(metamaskState, opts) {
   const { traceContext } = opts;
 
   const tags = getStartupTraceTags({ metamask: metamaskState });
@@ -200,8 +206,7 @@ async function startApp(metamaskState, backgroundConnection, opts) {
       parentContext: traceContext,
       tags,
     },
-    () =>
-      setupInitialStore(metamaskState, backgroundConnection, opts.activeTab),
+    () => setupInitialStore(metamaskState, opts.activeTab),
   );
 
   // global metamask api - used by tooling

app/scripts/lib/state-corruption/state-corruption-recovery.test.ts
@@ -84,7 +84,7 @@ describe('CorruptionHandler.handleStateCorruptionError', () => {
                   method: '__INVALID__',
                 },
               });
-            } else if (message.data.method === 'RELOAD') {
+            } else if (message.data.method === 'RELOAD_WINDOW') {
               reloadFn(ui);
             }
           });

app/scripts/lib/state-corruption/state-corruption-recovery.ts
@@ -244,7 +244,7 @@ export class CorruptionHandler {
                   // `connectedPorts` on disconnection.
                   tryPostMessage(connectedPort, {
                     data: {
-                      method: 'RELOAD',
+                      method: 'RELOAD_WINDOW',
                     },
                   });
                 });

app/scripts/ui.js
@@ -19,8 +19,8 @@ import { StreamProvider } from '@metamask/providers';
 import { createIdRemapMiddleware } from '@metamask/json-rpc-engine';
 import log from 'loglevel';
 import launchMetaMaskUi, {
-  updateBackgroundConnection,
-  displayStateCorruptionError,
+  CriticalStartupErrorHandler,
+  connectToBackground,
   // TODO: Remove restricted import
   // eslint-disable-next-line import/no-restricted-paths
 } from '../../ui';
@@ -34,7 +34,6 @@ import { checkForLastErrorAndLog } from '../../shared/modules/browser-runtime.ut
 import { SUPPORT_LINK } from '../../shared/lib/ui-utils';
 import { getErrorHtml } from '../../shared/lib/error-utils';
 import { endTrace, trace, TraceName } from '../../shared/lib/trace';
-import { METHOD_DISPLAY_STATE_CORRUPTION_ERROR } from '../../shared/constants/state-corruption';
 import ExtensionPlatform from './platforms/extension';
 import { setupMultiplex } from './lib/stream-utils';
 import { getEnvironmentType, getPlatform } from './lib/util';
@@ -42,12 +41,15 @@ import metaRPCClientFactory from './lib/metaRPCClientFactory';
 
 const PHISHING_WARNING_PAGE_TIMEOUT = 1 * 1000; // 1 Second
 const PHISHING_WARNING_SW_STORAGE_KEY = 'phishing-warning-sw-registered';
-const METHOD_START_UI_SYNC = 'startUISync';
 
+/**
+ * @type {HTMLElement}
+ */
 const container = document.getElementById('app-content');
 
-let extensionPort;
-let isUIInitialised = false;
+/**
+ * @typedef {import("@metamask/object-multiplex/dist/Substream").Substream} Substream
+ */
 
 /**
  * An error thrown if the phishing warning page takes too long to load.
@@ -86,106 +88,52 @@ async function start() {
   const windowType = getEnvironmentType();
 
   // setup stream to background
-  extensionPort = browser.runtime.connect({ name: windowType });
+  const extensionPort = browser.runtime.connect({ name: windowType });
 
-  let connectionStream = new PortStream(extensionPort);
+  // Set up error handlers as early as possible to ensure we are ready to
+  // handle any errors that occur at any time
+  const criticalErrorHandler = new CriticalStartupErrorHandler(
+    extensionPort,
+    container,
+  );
+  criticalErrorHandler.install();
 
-  const activeTab = await queryCurrentActiveTab(windowType);
-
-  /*
-   * In case of MV3 the issue of blank screen was very frequent, it is caused by UI initialising before background is ready to send state.
-   * Code below ensures that UI is rendered only after "startUISync"
-   * messages are received thus the background is ready, and ensures that streams and
-   * phishing warning page load only after the "startUISync" message is received.
-   * In case the UI is already rendered, only update the streams.
-   */
-  const messageListener = async (message) => {
-    const method = message?.data?.method;
-
-    switch (method) {
-      case METHOD_START_UI_SYNC:
-        await handleStartUISync();
-        break;
-      case METHOD_DISPLAY_STATE_CORRUPTION_ERROR:
-        handleDisplayStateCorruptionError(message.data.params);
-        break;
-      case 'RELOAD':
-        window.location.reload();
-        break;
-      default:
-    }
-  };
+  const connectionStream = new PortStream(extensionPort);
+  const subStreams = connectSubstreams(connectionStream);
+  const backgroundConnection = metaRPCClientFactory(subStreams.controller);
+  connectToBackground(backgroundConnection, handleStartUISync);
 
   async function handleStartUISync() {
     endTrace({ name: TraceName.BackgroundConnect });
 
-    if (isManifestV3 && isUIInitialised) {
-      // Currently when service worker is revived we create new streams
-      // in later version we might try to improve it by reviving same streams.
-      updateUiStreams(connectionStream);
-    } else {
-      await initializeUiWithTab(
-        activeTab,
-        connectionStream,
-        windowType,
-        traceContext,
-      );
-    }
+    // this means we've received a message from the background, and so
+    // background startup has succeed, so we don't need to listen for error
+    // messages anymore
+    criticalErrorHandler.uninstall();
+
+    // Only after startUiSync has started can we set up the provider connection
+    // The provider connection *must* be set up before the UI is initialized, as
+    // it sets a global variable, `ethereumProvider`, that the UI relies on.
+    await setupProviderConnection(subStreams.provider);
+
+    const activeTab = await queryCurrentActiveTab(windowType);
+
+    await initializeUiWithTab(
+      activeTab,
+      backgroundConnection,
+      windowType,
+      traceContext,
+    );
 
     if (isManifestV3) {
       await loadPhishingWarningPage();
-    } else {
-      extensionPort.onMessage.removeListener(messageListener);
     }
-  }
-
-  /**
-   * @typedef {import('../../shared/constants/errors').ErrorLike} ErrorLike
-   */
-
-  /**
-   * Updates the DOM with the state corruption error UI.
-   *
-   * @param {{ error: ErrorLike, hasBackup: boolean, currentLocale?: string }} params
-   */
-  function handleDisplayStateCorruptionError({
-    error,
-    hasBackup,
-    currentLocale,
-  }) {
-    displayStateCorruptionError(
-      container,
-      extensionPort,
-      error,
-      hasBackup,
-      currentLocale,
-    );
-  }
-
-  if (isManifestV3) {
-    // resetExtensionStreamAndListeners takes care to remove listeners from closed streams
-    // it also creates new streams and attaches event listeners to them
-    const resetExtensionStreamAndListeners = () => {
-      extensionPort.onMessage.removeListener(messageListener);
-      extensionPort.onDisconnect.removeListener(
-        resetExtensionStreamAndListeners,
-      );
-
-      extensionPort = browser.runtime.connect({ name: windowType });
-      connectionStream = new PortStream(extensionPort);
-      extensionPort.onMessage.addListener(messageListener);
-      extensionPort.onDisconnect.addListener(resetExtensionStreamAndListeners);
-    };
-
-    extensionPort.onDisconnect.addListener(resetExtensionStreamAndListeners);
   }
 
   trace({
     name: TraceName.BackgroundConnect,
     parentContext: traceContext,
   });
-
-  extensionPort.onMessage.addListener(messageListener);
 }
 
 /**
@@ -283,8 +231,6 @@ async function initializeUiWithTab(
 
     endTrace({ name: TraceName.UIStartup });
 
-    isUIInitialised = true;
-
     if (process.env.IN_TEST) {
       window.document?.documentElement?.classList.add('controller-loaded');
     }
@@ -298,12 +244,6 @@ async function initializeUiWithTab(
   } catch (err) {
     displayCriticalError('troubleStarting', err);
   }
-}
-
-// Function to update new backgroundConnection in the UI
-function updateUiStreams(connectionStream) {
-  const backgroundConnection = connectToAccountManager(connectionStream);
-  updateBackgroundConnection(backgroundConnection);
 }
 
 async function queryCurrentActiveTab(windowType) {
@@ -348,9 +288,7 @@ async function queryCurrentActiveTab(windowType) {
   return { id, title, origin, protocol, url };
 }
 
-async function initializeUi(activeTab, connectionStream, traceContext) {
-  const backgroundConnection = connectToAccountManager(connectionStream);
-
+async function initializeUi(activeTab, backgroundConnection, traceContext) {
   return await launchMetaMaskUi({
     activeTab,
     container,
@@ -375,44 +313,36 @@ async function displayCriticalError(errorKey, err, metamaskState) {
 }
 
 /**
- * Establishes a connection to the background and a Web3 provider
+ * Establishes a connections between the PortStream (background) and various UI
+ * streams.
  *
- * @param {PortDuplexStream} connectionStream - PortStream instance establishing a background connection
+ * @param {PortStream} connectionStream - PortStream instance establishing a background connection
+ * @returns The multiplexed streams
  */
-function connectToAccountManager(connectionStream) {
+function connectSubstreams(connectionStream) {
   const mx = setupMultiplex(connectionStream);
-  const controllerConnectionStream = mx.createStream('controller');
 
-  const backgroundConnection = setupControllerConnection(
-    controllerConnectionStream,
-  );
+  const controllerSubstream = mx.createStream('controller');
+  const providerSubstream = mx.createStream('provider');
 
-  setupWeb3Connection(mx.createStream('provider'));
-
-  return backgroundConnection;
+  return {
+    controller: controllerSubstream,
+    provider: providerSubstream,
+  };
 }
 
 /**
  * Establishes a streamed connection to a Web3 provider
  *
- * @param {PortDuplexStream} connectionStream - PortStream instance establishing a background connection
+ * @param {Substream} connectionStream - PortStream instance establishing a background connection
  */
-function setupWeb3Connection(connectionStream) {
+async function setupProviderConnection(connectionStream) {
   const providerStream = new StreamProvider(connectionStream, {
     rpcMiddleware: [createIdRemapMiddleware()],
   });
   connectionStream.on('error', console.error.bind(console));
   providerStream.on('error', console.error.bind(console));
-  providerStream.initialize().then(() => {
-    global.ethereumProvider = providerStream;
-  });
-}
 
-/**
- * Establishes a streamed connection to the background account manager
- *
- * @param {PortDuplexStream} controllerConnectionStream - PortStream instance establishing a background connection
- */
-function setupControllerConnection(controllerConnectionStream) {
-  return metaRPCClientFactory(controllerConnectionStream);
+  await providerStream.initialize();
+  global.ethereumProvider = providerStream;
 }

test/lib/render-helpers.js
@@ -6,12 +6,13 @@ import { userEvent } from '@testing-library/user-event';
 import { Router } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { createMemoryHistory } from 'history';
+import { noop } from 'lodash';
 import configureStore from '../../ui/store/store';
 import { I18nContext, LegacyI18nProvider } from '../../ui/contexts/i18n';
 import { LegacyMetaMetricsProvider } from '../../ui/contexts/metametrics';
 import { getMessage } from '../../ui/helpers/utils/i18n-helper';
 import * as en from '../../app/_locales/en/messages.json';
-import { setupInitialStore } from '../../ui';
+import { setupInitialStore, connectToBackground } from '../../ui';
 import Root from '../../ui/pages';
 
 export const I18nProvider = (props) => {
@@ -186,9 +187,9 @@ export async function integrationTestRender(extendedRenderOptions) {
     ...renderOptions
   } = extendedRenderOptions;
 
-  const store = await setupInitialStore(preloadedState, backgroundConnection, {
-    activeTab,
-  });
+  connectToBackground(backgroundConnection, noop);
+
+  const store = await setupInitialStore(preloadedState, activeTab);
 
   return {
     ...render(<Root store={store} />, { ...renderOptions }),

ui/helpers/utils/critical-startup-error-handler.ts
@@ -1,0 +1,99 @@
+import type browser from 'webextension-polyfill';
+import { isObject, hasProperty } from '@metamask/utils';
+import log from 'loglevel';
+import { METHOD_DISPLAY_STATE_CORRUPTION_ERROR } from '../../../shared/constants/state-corruption';
+import type { ErrorLike } from '../../../shared/constants/errors';
+import { displayStateCorruptionError } from './state-corruption-html';
+
+type Message = {
+  data: {
+    method: string;
+    params?: Record<string, unknown> | unknown[];
+  };
+};
+
+export class CriticalStartupErrorHandler {
+  #port: browser.Runtime.Port;
+
+  #container: HTMLElement;
+
+  /**
+   * Creates an instance of CriticalStartupErrorHandler.
+   * This class listens for critical startup errors from the background script
+   * and displays appropriate error messages in the UI.
+   *
+   * @param port - The port to listen for messages on.
+   * @param container - The container element to display the error in.
+   */
+  constructor(port: browser.Runtime.Port, container: HTMLElement) {
+    this.#port = port;
+    this.#container = container;
+  }
+
+  /**
+   * Handles incoming messages from the background script.
+   *
+   * @param message - The message received from the background script.
+   */
+  #handler = (message: Message) => {
+    if (!isObject(message) || !hasProperty(message, 'data')) {
+      // Ignore messages that are not objects or do not have a 'data' property,
+      // they're likely for some other purpose
+      return;
+    }
+    const { data } = message;
+    if (!isObject(data) || !hasProperty(data, 'method')) {
+      // Ignore the data property if it not an object or does not have a
+      // 'method' property, they're likely for some other purpose
+      return;
+    }
+    const { method } = data;
+    // Currently, we only handle RELOAD_WINDOW, and the state corruption error
+    // message, but we will be adding more in the future.
+    if (method === 'RELOAD_WINDOW') {
+      // This is a special case where we want to reload the page
+      window.location.reload();
+    } else if (method === METHOD_DISPLAY_STATE_CORRUPTION_ERROR) {
+      if (!hasProperty(data, 'params') || !isObject(data.params)) {
+        log.error(
+          'Received state corruption error message without valid params:',
+          message,
+        );
+        return;
+      }
+
+      const { error, hasBackup, currentLocale } = data.params as {
+        error: ErrorLike;
+        hasBackup: boolean;
+        currentLocale?: string;
+      };
+      displayStateCorruptionError(
+        this.#container,
+        this.#port,
+        error,
+        hasBackup,
+        currentLocale,
+      );
+    }
+  };
+
+  /**
+   * Connects error listeners to the provided port to handle state corruption errors.
+   * This function listens for messages from the background script and displays
+   * a state corruption error if the appropriate message is received.
+   *
+   * Critical error messages are transferred over a raw browser `Port`, not with
+   * `PortStream` wrapper. We want to be as close to the "metal" as possible here,
+   * it minimize abstractions that could cause further issues.
+   */
+  install() {
+    this.#port.onMessage.addListener(this.#handler);
+  }
+
+  /**
+   * Uninstalls the error listeners from the port.
+   */
+  uninstall() {
+    this.#port.onMessage.removeListener(this.#handler);
+  }
+}

commit 409907d3c2af1761cd9b39dfada3f12c8029bbce

fix: fix ui not loading in old browsers due to use `Promise.withResolvers` (#35175)
## **Description**

`Promise.withResolvers` is not supported in very old versions of Chrome
and Firefox, we used to polyfill it, but the polyfill was silently
failing due to `lavamoat` protections, but our tests were still passing
because we only test on new versions of browsers.

We fixed this by switching all uses of this API to our own local helper
function, `withResolvers`, that implements the same API, _the same day_
the PR introducing a new usage was also merged, but I had failed to
realize I had used the unsupported API in the new PR.

Fixes the `Promise.withResolvers` usage issue introduced by
https://github.com/MetaMask/metamask-extension/pull/33211 by using this
fix for `Promise.withResolvers` introduced the same day:
https://github.com/MetaMask/metamask-extension/pull/34255

[![Open in GitHub
Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/35175?quickstart=1)

## **Changelog**

CHANGELOG entry: fix ui not loading in old browsers due to use
`Promise.withResolvers`

<!--
## **Related issues**

Fixes:

## **Manual testing steps**

1. Go to this page...
2.
3.

## **Screenshots/Recordings**


### **Before**


### **After**


## **Pre-merge author checklist**

- [ ] I've followed [MetaMask Contributor
Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
Extension Coding
Standards](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/CODING_GUIDELINES.md).
- [ ] I've completed the PR template to the best of my ability
- [ ] I’ve included tests if applicable
- [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
if applicable
- [ ] I’ve applied the right labels on the PR (see [labeling
guidelines](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
Not required for external contributors.

ui/index.js
@@ -6,6 +6,7 @@ import browser from 'webextension-polyfill';
 import { isInternalAccountInPermittedAccountIds } from '@metamask/chain-agnostic-permission';
 
 import { captureException } from '../shared/lib/sentry';
+import { withResolvers } from '../shared/lib/promise-with-resolvers';
 // TODO: Remove restricted import
 // eslint-disable-next-line import/no-restricted-paths
 import { getEnvironmentType } from '../app/scripts/lib/util';
@@ -53,7 +54,7 @@ log.setLevel(global.METAMASK_DEBUG ? 'debug' : 'warn', false);
 /**
  * @type {PromiseWithResolvers<ReturnType<typeof configureStore>>}
  */
-const reduxStore = Promise.withResolvers();
+const reduxStore = withResolvers();
 
 /**
  * Method to update backgroundConnection object use by UI