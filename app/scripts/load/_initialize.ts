7af65c1d324c106cb0b3d721268ed05c13442f78
Norbert Elter <72046715+itsyoboieltr@users.noreply.github.com>
2025-08-20 00:19:50 +0400

    Update _initialize.ts

--- app/scripts/load/_initialize.ts
+++ app/scripts/load/_initialize.ts
@@ -2,11 +2,9 @@
 // currently only used in webpack build.
 
 if (process.env.ENABLE_LAVAMOAT === 'true') {
-  // `lockdown-more` requires `lockdown`'s `harden`, which is only applied when lavamoat is also applied
-  // the original author of `lockdown-more` believed this should be run immediately after `lockdown`, but that
-  // is no longer possible to guarantee, because lockdown is abstracted away into the lavamoat webpack plugin
-  // and the lavamoat team doesn't want to include lockdown-more in the plugin itself as `lockdown` strictly
-  // adheres to the ECMAScript spec.
+  // lockdown-more actually doesn't really work under webpack
+  // The security team is working on adding an improved version of lockdown-more to the lavamoat webpack plugin.
+  // TODO: delete this file when the improved version is available.
   require('../lockdown-more');
 }
 
