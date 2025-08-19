e58c7c33dea236b156cc73127896ef235f6aa648
Antonio Regadas <antonio.regadas@consensys.net>
2025-08-19 17:03:13 +0100

    chore: working

--- shared/lib/deep-links/routes/nonevm.ts
+++ shared/lib/deep-links/routes/nonevm.ts
@@ -1,11 +1,5 @@
 import { Route } from './route';
 
-export enum NonEvmChainType {
-  SOLANA = 'solana',
-  BITCOIN = 'bitcoin',
-  TRON = 'tron',
-}
-
 export enum NonEvmQueryParams {
   CHAIN_ID = 'chainId',
 }
@@ -16,17 +10,15 @@ export default new Route({
   pathname: '/nonevm',
   getTitle: (_: URLSearchParams) => 'deepLink_theNonEvmPage',
   handler: function handler(params: URLSearchParams) {
-    const chainId = params.get(NonEvmQueryParams.CHAIN_ID)?.toLowerCase();
+    const chainId = params.get(NonEvmQueryParams.CHAIN_ID);
 
-    // Validate chain type
-    if (!chainId || !Object.values(NonEvmChainType).includes(chainId as NonEvmChainType)) {
-      throw new Error(`Unsupported chain type: ${chainId}`);
+    if (!chainId) {
+      throw new Error('Missing chainId parameter');
     }
 
     const query = new URLSearchParams(params);
     query.set(NonEvmQueryParams.CHAIN_ID, chainId);
 
-    // Route to the dedicated component that will check balances and redirect
     return {
       path: NONEVM_BALANCE_CHECK_ROUTE,
       query,
--- ui/pages/nonevm-balance-check/index.tsx
+++ ui/pages/nonevm-balance-check/index.tsx
@@ -1,16 +1,25 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
+import { CaipChainId } from '@metamask/utils';
 import { useMultichainBalances } from '../../hooks/useMultichainBalances';
 import { NonEvmQueryParams } from '../../../shared/lib/deep-links/routes/nonevm';
-import { BaseUrl } from '../../../shared/constants/urls';
 import { SWAP_ROUTE } from '../../../shared/lib/deep-links/routes/route';
+import { BridgeQueryParams } from '../../../shared/lib/deep-links/routes/swap';
+import { RampsMetaMaskEntry } from '../../hooks/ramps/useRamps/useRamps';
+import { getDataCollectionForMarketing, getMetaMetricsId, getParticipateInMetaMetrics } from '../../selectors';
+import { BaseUrl } from '../../../shared/constants/urls';
 
 const { getExtensionURL } = globalThis.platform;
 
 export const NonEvmBalanceCheck = () => {
   const location = useLocation();
+  const metaMetricsId = useSelector(getMetaMetricsId);
+  const isMetaMetricsEnabled = useSelector(getParticipateInMetaMetrics);
+  const isMarketingEnabled = useSelector(getDataCollectionForMarketing);
+
   const params = new URLSearchParams(location.search);
-  const chainId = params.get(NonEvmQueryParams.CHAIN_ID)?.toLowerCase();
+  const chainId = params.get(NonEvmQueryParams.CHAIN_ID)?.toLowerCase() as CaipChainId;
 
   const { assetsWithBalance } = useMultichainBalances();
 
@@ -19,20 +28,34 @@ export const NonEvmBalanceCheck = () => {
       return;
     }
 
-    // Find balance for the chainId
-    const chainBalance = assetsWithBalance.find(
-      (asset) => asset.chainId === chainId && asset.isNative
+    const hasPositiveBalance = assetsWithBalance.some(
+      (asset) => asset.balance && asset.balance !== '0'
     );
 
-    if (chainBalance && chainBalance.balance !== '0') {
+    if (hasPositiveBalance) {
       const query = new URLSearchParams();
       query.set('sourceToken', chainId);
+      query.set(BridgeQueryParams.SWAPS, 'true');
       window.location.href = getExtensionURL(SWAP_ROUTE, query.toString());
     } else {
+      const buyParams = new URLSearchParams();
+
+      buyParams.set('metamaskEntry', RampsMetaMaskEntry.BuySellButton);
+      buyParams.set('chainId', chainId);
+      if (metaMetricsId) {
+        buyParams.set('metametricsId', metaMetricsId);
+      }
+      buyParams.set('metricsEnabled', String(isMetaMetricsEnabled));
+      if (isMarketingEnabled) {
+        buyParams.set('marketingEnabled', String(isMarketingEnabled));
+      }
+
       const buyUrl = new URL('/buy', BaseUrl.Portfolio);
+      buyUrl.search = buyParams.toString();
+
       window.location.href = buyUrl.toString();
     }
-  }, [chainId, assetsWithBalance, params]);
+  }, [chainId, assetsWithBalance, metaMetricsId, isMetaMetricsEnabled, isMarketingEnabled]);
 
   return null;
 };
