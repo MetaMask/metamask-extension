5c13260e24b4db6bc2bc400154aff309e1366f68
Gauthier Petetin <gauthier.petetin@consensys.net>
2025-08-20 17:39:01 +0200

    fix(next semver version): revert calculation method to no longer rely on package.json

--- development/get-next-semver-version.sh
+++ development/get-next-semver-version.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
 FORCE_NEXT_SEMVER_VERSION=$1
-
 # If FORCE_NEXT_SEMVER_VERSION is defined and not empty, use its value and skip the next operations
 if [ -n "$FORCE_NEXT_SEMVER_VERSION" ]
 then
@@ -9,7 +7,16 @@ then
   exit 0
 fi
 
-# Get the version from package.json
-VERSION_PACKAGE=$(node -p "require(process.env.GITHUB_WORKSPACE + '/package.json').version")
+# Get the highest version from release branches
+VERSION_BRANCHES=$(git branch -r | grep -o 'Version-v[0-9]*\.[0-9]*\.[0-9]*' | grep -o '[0-9]*\.[0-9]*\.[0-9]*' | sort --version-sort | tail -n 1)
 
-echo "NEXT_SEMVER_VERSION=${VERSION_PACKAGE}" >> "$GITHUB_ENV"
+# Get the highest version from tags
+VERSION_TAGS=$(git tag | grep -o 'v[0-9]*\.[0-9]*\.[0-9]*' | grep -o '[0-9]*\.[0-9]*\.[0-9]*' | sort --version-sort | tail -n 1)
+
+# Compare versions and keep the highest one
+HIGHEST_VERSION=$(printf "%s\n%s" "$VERSION_BRANCHES" "$VERSION_TAGS" | sort --version-sort | tail -n 1)
+
+# Increment the minor version of the highest version found and reset the patch version to 0
+NEXT_VERSION=$(echo "$HIGHEST_VERSION" | awk -F. -v OFS=. '{$2++; $3=0; print}')
+
+echo "NEXT_SEMVER_VERSION=${NEXT_VERSION}" >> "$GITHUB_ENV"
