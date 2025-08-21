bbea9060c6e6ecc83dc05d20287519bb35f6ce6f
Jyoti Puri <jyotipuri@gmail.com>
2025-08-21 22:26:47 +0530

    update

--- test/e2e/tests/confirmations/signatures/permit.spec.ts
+++ test/e2e/tests/confirmations/signatures/permit.spec.ts
@@ -106,7 +106,7 @@ describe('Confirmation Signature - Permit', function (this: Suite) {
           primaryType: 'Permit',
           uiCustomizations: ['redesigned_confirmation', 'permit'],
           location: 'confirmation',
-          decodingChangeTypes: ['LISTING', 'RECEIVE'],
+          decodingChangeTypes: ['LISTING', 'RECEIVE'], //
           decodingResponse: 'CHANGE',
           decodingDescription: null,
           requestedThrough: MetaMetricsRequestedThrough.EthereumProvider,
