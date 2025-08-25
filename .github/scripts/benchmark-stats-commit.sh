4ef0b7f47c294c1d85f8add29f8e030935d83f28
ffmcgee <joao.carlos@consensys.net>
2025-08-25 12:43:38 +0200

    build: replace EXTENSION_BENCHMARK_STATS_TOKEN in config files

--- .github/scripts/benchmark-stats-commit.sh
+++ .github/scripts/benchmark-stats-commit.sh
@@ -4,7 +4,6 @@ set -e
 set -u
 set -o pipefail
 
-# if [[ -z "${EXTENSION_BUNDLESIZE_STATS_TOKEN:-}" ]]; then
 if [[ -z "${EXTENSION_BENCHMARK_STATS_TOKEN:-}" ]]; then
     printf '%s\n' 'EXTENSION_BENCHMARK_STATS_TOKEN environment variable must be set'
     exit 1
@@ -68,8 +67,6 @@ git commit --message "Adding page load benchmark data at commit: ${GITHUB_SHA}"
 
 repo_slug="${GITHUB_REPOSITORY_OWNER}/extension_benchmark_stats"
 
-# TODO: [ffmcgee] "EXTENSION_BUNDLESIZE_STATS_TOKEN" to "EXTENSION_BENCHMARK_STATS_TOKEN"
-# git push "https://metamaskbot:${EXTENSION_BUNDLESIZE_STATS_TOKEN}@github.com/${repo_slug}" gh-pages
 git push "https://metamaskbot:${EXTENSION_BENCHMARK_STATS_TOKEN}@github.com/${repo_slug}" gh-pages
 
 cd ..
