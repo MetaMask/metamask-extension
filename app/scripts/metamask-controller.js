d73d2c5dc61bbc81e664103ab718838054c25410
Jiexi Luan <jiexiluan@gmail.com>
2025-08-27 10:57:57 -0700

    Do not set GNS on wallet_add/switchEthereumChain when the dapp is permissioned

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -7385,7 +7385,6 @@ export default class MetamaskController extends EventEmitter {
         this.networkController,
       ),
       setActiveNetwork: async (networkClientId) => {
-        await this.networkController.setActiveNetwork(networkClientId);
         // if the origin has the CAIP-25 permission
         // we set per dapp network selection state
         if (
@@ -7398,6 +7397,8 @@ export default class MetamaskController extends EventEmitter {
             origin,
             networkClientId,
           );
+        } else {
+          await this.networkController.setActiveNetwork(networkClientId);
         }
       },
       getNetworkConfigurationByChainId:
