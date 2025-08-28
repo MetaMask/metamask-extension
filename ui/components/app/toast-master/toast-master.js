e09f0f17a23df9dc2a26eae84d2b73d8e0d08625
Francis Nepomuceno <n3ps@users.noreply.github.com>
2025-08-28 15:57:56 -0400

    more

--- ui/components/app/toast-master/toast-master.js
+++ ui/components/app/toast-master/toast-master.js
@@ -36,12 +36,12 @@ import {
   hidePermittedNetworkToast,
 } from '../../../store/actions';
 import {
-  AvatarAccount,
   AvatarAccountSize,
   AvatarNetwork,
   Icon,
   IconName,
 } from '../../component-library';
+import { PreferredAvatar } from '../preferred-avatar';
 import { Toast, ToastContainer } from '../../multichain';
 import { SurveyToast } from '../../ui/survey-toast';
 import { PasswordChangeToastType } from '../../../../shared/constants/app-state';
@@ -122,10 +122,9 @@ function ConnectAccountToast() {
         dataTestId="connect-account-toast"
         key="connect-account-toast"
         startAdornment={
-          <AvatarAccount
+          <PreferredAvatar
             address={account.address}
-            size={AvatarAccountSize.Md}
-            borderColor={BorderColor.transparent}
+            className="shrink-0 self-center"
           />
         }
         text={t('accountIsntConnectedToastText', [
