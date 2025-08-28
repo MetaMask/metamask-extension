6f8b907706d1e7bdd285141d76aaae2c3dbaf160
Xiaoming Wang <dawnseeker8@gmail.com>
2025-08-28 06:30:03 +0100

    Implement the trezor e2e forget device happy path.

--- test/e2e/page-objects/pages/hardware-wallet/connect-hardware-wallet-page.ts
+++ test/e2e/page-objects/pages/hardware-wallet/connect-hardware-wallet-page.ts
@@ -20,6 +20,8 @@ class ConnectHardwareWalletPage {
 
   private readonly continueButton = { text: 'Continue', tag: 'button' };
 
+  private readonly closeButton = '[data-testid="hardware-connect-close-btn"]';
+
   constructor(driver: Driver) {
     this.driver = driver;
   }
@@ -54,6 +56,11 @@ class ConnectHardwareWalletPage {
   async clickContinueButton(): Promise<void> {
     console.log(`Click continue button`);
     await this.driver.clickElement(this.continueButton);
+  }
+
+  async clickCloseButton(): Promise<void> {
+    console.log(`Click close button`);
+    await this.driver.clickElement(this.closeButton);
   }
 
   async openConnectTrezorPage(): Promise<void> {
---
+++ test/e2e/tests/hardware-wallets/trezor/trezor-forget-device.spec.ts
@@ -0,0 +1,58 @@
+import { Suite } from 'mocha';
+import { Driver } from '../../../webdriver/driver';
+import { Ganache } from '../../../seeder/ganache';
+import FixtureBuilder from '../../../fixture-builder';
+import { withFixtures } from '../../../helpers';
+import { KNOWN_PUBLIC_KEY_ADDRESSES } from '../../../../stub/keyring-bridge';
+import ActivityListPage from '../../../page-objects/pages/home/activity-list';
+import HomePage from '../../../page-objects/pages/home/homepage';
+import { sendRedesignedTransactionToAddress } from '../../../page-objects/flows/send-transaction.flow';
+import { loginWithoutBalanceValidation } from '../../../page-objects/flows/login.flow';
+import HeaderNavbar from '../../../page-objects/pages/header-navbar';
+import AccountListPage from '../../../page-objects/pages/account-list-page';
+import ConnectHardwareWalletPage from '../../../page-objects/pages/hardware-wallet/connect-hardware-wallet-page';
+import SelectHardwareWalletAccountPage from '../../../page-objects/pages/hardware-wallet/select-hardware-wallet-account-page';
+
+const RECIPIENT = '0x0Cc5261AB8cE458dc977078A3623E2BaDD27afD3';
+
+describe('Trezor Hardware', function (this: Suite) {
+  it('forgets device and checks if it is removed from the list', async function () {
+    await withFixtures(
+      {
+        fixtures: new FixtureBuilder().withTrezorAccount().build(),
+        title: this.test?.fullTitle(),
+      },
+      async ({ driver }: { driver: Driver }) => {
+        await loginWithoutBalanceValidation(driver);
+        const homePage = new HomePage(driver);
+        await homePage.checkPageIsLoaded();
+        const headerNavbar = new HeaderNavbar(driver);
+        await headerNavbar.openAccountMenu();
+        const accountListPage = new AccountListPage(driver);
+        await accountListPage.checkPageIsLoaded();
+        await accountListPage.openConnectHardwareWalletModal();
+
+        const connectHardwareWalletPage = new ConnectHardwareWalletPage(driver);
+        await connectHardwareWalletPage.checkPageIsLoaded();
+        await connectHardwareWalletPage.openConnectTrezorPage();
+
+        const selectTrezorAccountPage = new SelectHardwareWalletAccountPage(
+          driver,
+        );
+        await selectTrezorAccountPage.checkPageIsLoaded();
+
+        await selectTrezorAccountPage.clickForgetDeviceButton();
+
+        await connectHardwareWalletPage.checkPageIsLoaded();
+        await connectHardwareWalletPage.clickCloseButton();
+
+        await homePage.checkPageIsLoaded();
+        await headerNavbar.openAccountMenu();
+        await accountListPage.checkPageIsLoaded();
+        await accountListPage.checkAccountIsNotDisplayedInAccountList(
+          'Trezor 1',
+        );
+      },
+    );
+  });
+});
