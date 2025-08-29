c81b6b5c6168548fa73df9f217d5aceb40d7cd2f
Jyoti Puri <jyotipuri@gmail.com>
2025-08-29 13:15:08 +0530

    feat: Implementing send metrics context and hooks (#35461)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    Adding context and hooks for metrics in new send flow.
    
    ## **Changelog**
    
    <!--
    If this PR is not End-User-Facing and should not show up in the
    CHANGELOG, you can choose to either:
    1. Write `CHANGELOG entry: null`
    2. Label with `no-changelog`
    
    If this PR is End-User-Facing, please write a short User-Facing
    description in the past tense like:
    `CHANGELOG entry: Added a new tab for users to see their NFTs`
    `CHANGELOG entry: Fixed a bug that was causing some NFTs to flicker`
    
    (This helps the Release Engineer do their job more quickly and
    accurately)
    -->
    
    CHANGELOG entry:
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/MetaMask-planning/issues/5663
    
    ## **Manual testing steps**
    NA
    
    ## **Screenshots/Recordings**
    NA
    
    ## **Pre-merge author checklist**
    
    - [X] I've followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
    Extension Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [X] I've completed the PR template to the best of my ability
    - [X] I’ve included tests if applicable
    - [X] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [X] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- ui/pages/confirmations/hooks/send/useAmountValidation.test.ts
+++ ui/pages/confirmations/hooks/send/useAmountValidation.test.ts
@@ -4,9 +4,10 @@ import { useAmountValidation } from './useAmountValidation';
 
 describe('useAmountValidation', () => {
   it('return field for amount error', () => {
-    const { result } = renderHookWithProvider(() => useAmountValidation(), {
-      state: mockState,
-    });
+    const { result } = renderHookWithProvider(
+      () => useAmountValidation(),
+      mockState,
+    );
     expect(result.current).toEqual({ amountError: undefined });
   });
 });
