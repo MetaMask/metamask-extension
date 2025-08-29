ad1814688ab9204c6c710d6ab2bcde8318572b75
Jyoti Puri <jyotipuri@gmail.com>
2025-08-29 08:43:16 +0530

    Capturing amount metrics events

--- ui/pages/confirmations/components/send/amount-recipient/amount-recipient.tsx
+++ ui/pages/confirmations/components/send/amount-recipient/amount-recipient.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useState } from 'react';
 
 import { Button } from '../../../../../components/component-library';
+import { useAmountSelectionMetrics } from '../../../hooks/send/metrics/useAmountSelectionMetrics';
 import { useNavigateSendPage } from '../../../hooks/send/useNavigateSendPage';
 import { useSendActions } from '../../../hooks/send/useSendActions';
 import { Amount } from '../amount/amount';
@@ -11,9 +12,11 @@ export const AmountRecipient = () => {
   const [to, setTo] = useState<string | undefined>();
   const { goToPreviousPage } = useNavigateSendPage();
   const { handleSubmit } = useSendActions();
+  const { captureAmountSelected } = useAmountSelectionMetrics();
 
   const onClick = useCallback(() => {
     handleSubmit(to);
+    captureAmountSelected();
   }, [handleSubmit, to]);
 
   return (
--- ui/pages/confirmations/components/send/amount/amount.tsx
+++ ui/pages/confirmations/components/send/amount/amount.tsx
@@ -7,6 +7,7 @@ import {
   TextField,
 } from '../../../../../components/component-library';
 import { useAmountValidation } from '../../../hooks/send/useAmountValidation';
+import { useAmountSelectionMetrics } from '../../../hooks/send/metrics/useAmountSelectionMetrics';
 import { useBalance } from '../../../hooks/send/useBalance';
 import { useCurrencyConversions } from '../../../hooks/send/useCurrencyConversions';
 import { useMaxAmount } from '../../../hooks/send/useMaxAmount';
@@ -22,26 +23,54 @@ export const Amount = () => {
   const { getNativeValue } = useCurrencyConversions();
   const { getMaxAmount } = useMaxAmount();
   const { isNonEvmNativeSendType } = useSendType();
+  const {
+    setAmountInputMethodManual,
+    setAmountInputMethodPasted,
+    setAmountInputMethodPressedMax,
+    setAmountInputTypeFiat,
+    setAmountInputTypeToken,
+  } = useAmountSelectionMetrics();
 
   const onChange = useCallback(
     (event) => {
       const newValue = event.target.value;
       updateValue(fiatMode ? getNativeValue(newValue) : newValue);
       setAmount(newValue);
+      setAmountInputMethodManual();
     },
     [fiatMode, getNativeValue, setAmount, updateValue],
   );
 
   const toggleFiatMode = useCallback(() => {
+    const newFiatMode = !fiatMode;
     setAmount('');
-    setFiatMode(!fiatMode);
-  }, [fiatMode, setAmount, setFiatMode]);
+    setFiatMode(newFiatMode);
+    if (newFiatMode) {
+      setAmountInputTypeFiat();
+    } else {
+      setAmountInputTypeToken();
+    }
+  }, [
+    fiatMode,
+    setAmount,
+    setAmountInputTypeFiat,
+    setAmountInputTypeToken,
+    setFiatMode,
+  ]);
 
   const updateToMax = useCallback(() => {
     const maxValue = getMaxAmount() ?? '0';
     updateValue(fiatMode ? getNativeValue(maxValue) : maxValue);
     setAmount(maxValue);
-  }, [fiatMode, getMaxAmount, getNativeValue, setAmount, updateValue]);
+    setAmountInputMethodPressedMax();
+  }, [
+    fiatMode,
+    getMaxAmount,
+    getNativeValue,
+    setAmount,
+    setAmountInputMethodPressedMax,
+    updateValue,
+  ]);
 
   const isERC1155 = asset?.standard === ERC1155;
   const isERC721 = asset?.standard === ERC721;
@@ -53,7 +82,11 @@ export const Amount = () => {
   return (
     <div>
       <p>AMOUNT</p>
-      <TextField value={amount} onChange={onChange} />
+      <TextField
+        onChange={onChange}
+        onPaste={setAmountInputMethodPasted}
+        value={amount}
+      />
       <Text>Balance: {balance}</Text>
       <Text>Error: {amountError}</Text>
       {!isERC1155 && (
