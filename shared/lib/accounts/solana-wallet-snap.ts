5ea8a72e22a5ddfd1943e920ba71c959e430c324
Alejandro Garcia Anglada <aganglada@gmail.com>
2025-09-01 17:07:22 +0200

    fix: revert solana id

--- shared/lib/accounts/solana-wallet-snap.ts
+++ shared/lib/accounts/solana-wallet-snap.ts
@@ -1,6 +1,5 @@
 import { SnapId } from '@metamask/snaps-sdk';
 
 export const SOLANA_WALLET_SNAP_ID: SnapId =
-  // 'npm:@metamask/solana-wallet-snap' as SnapId;
-  'local:http://localhost:8080' as SnapId;
+  'npm:@metamask/solana-wallet-snap' as SnapId;
 export const SOLANA_WALLET_NAME: string = 'Solana';
