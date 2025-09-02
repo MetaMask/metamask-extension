a88b9d5ca0d9927ddca85db175c43357c941ccfa
ffmcgee <51971598+ffmcgee725@users.noreply.github.com>
2025-09-02 19:50:56 +0200

    fix: do not change the GNS on wallet_add/switchEthereumChain when the dapp has permissions (#35559)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    Currently on extension when the dapp/snap changes it's currently
    selected network, it also changes the GNS. The GNS should only change if
    the dapp/snap has no permissions, otherwise only the per dapp selected
    network should change if the dapp/snap does have permissions. This PR
    fixes that.
    
    [![Open in GitHub
    Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/35439?quickstart=1)
    
    ## **Changelog**
    
    <!--
    If this PR is not End-User-Facing and should not show up in the
    CHANGELOG, you can choose to either:
    1. Write `CHANGELOG entry: null`
    2. Label with `no-changelog`
    
    If this PR is End-User-Facing, please write a short User-Facing
    description in the past tense like:
    `CHANGELOG entry: Added a new tab for users to see their NFTs`
    `CHANGELOG entry: Fixed a bug that was causing some NFTs to flicker`
    
    (This helps the Release Engineer do their job more quickly and
    accurately)
    -->
    
    CHANGELOG entry: Fixed a bug that was causing the current network for
    dapps without permitted accounts to change unnecessarily
    
    ## **Related issues**
    
    See: https://consensys.slack.com/archives/C02GENU377A/p1756308662019399
    See: https://github.com/MetaMask/metamask-extension/pull/35430
    
    ## **Manual testing steps**
    
    1. Note the wallet's currently globally selected network by going to any
    unpermissioned webpage and running `window.ethereum.chainId`
    2. Go to https://metamask.github.io/snaps/test-snaps/latest/
    3. Connect Ethereum Provider Snap
    4. Change the chain to one that does not match the globally selected
    network
    5. Check that the wallet's globally selected network has not changed by
    repeating step 1
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [ ] I've followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
    Extension Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -7367,7 +7367,6 @@ export default class MetamaskController extends EventEmitter {
         this.networkController,
       ),
       setActiveNetwork: async (networkClientId) => {
-        await this.networkController.setActiveNetwork(networkClientId);
         // if the origin has the CAIP-25 permission
         // we set per dapp network selection state
         if (
@@ -7380,6 +7379,8 @@ export default class MetamaskController extends EventEmitter {
             origin,
             networkClientId,
           );
+        } else {
+          await this.networkController.setActiveNetwork(networkClientId);
         }
       },
       getNetworkConfigurationByChainId:
