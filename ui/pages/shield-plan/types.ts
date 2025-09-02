d991948429c28f002a2155bb87e0eff10290325c
Lionell Briones <llenoil@gmail.com>
2025-09-02 16:08:16 +0800

    feat: remove code owners - add todo on shield plan and type

--- ui/pages/shield-plan/types.ts
+++ ui/pages/shield-plan/types.ts
@@ -6,11 +6,13 @@ export const PAYMENT_METHODS = {
 export type PaymentMethod =
   (typeof PAYMENT_METHODS)[keyof typeof PAYMENT_METHODS];
 
+// TODO: should come from subscription controller
 export const SHIELD_PLAN_PRICES = {
   ANNUAL: '$80',
   MONTHLY: '$8',
 } as const;
 
+// TODO: should come from subscription controller
 export const PLAN_TYPES = {
   ANNUAL: 'annual',
   MONTHLY: 'monthly',
