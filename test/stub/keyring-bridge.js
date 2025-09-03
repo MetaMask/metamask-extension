f8ed4ca4ef84aea33a0787fe81ecedaa236bb8d9
Sébastien Van Eyck <sebastien.vaneyck@consensys.net>
2025-09-03 12:41:49 +0200

    feat: add Trezor e2e tests for legacy and EIP1559 transactions (#35524)
    ## **Description**
    
    Adds legacy and EIP-1559 transaction test cases for Trezor related e2e
    tests
    
    Fixes:
    https://github.com/MetaMask/accounts-planning/issues/962?issue=MetaMask%7Caccounts-planning%7C972
    
    ## **Manual testing steps**
    
    Local testing
    1. run `yarn && yarn start:test` in one terminal tab and wait for build
    end
    2. run `yarn test:e2e:single
    test/e2e/tests/hardware-wallets/trezor/trezor-send.spec.ts` on another
    terminal tab
    
    Otherwise check this PR CI.
    
    ## **Pre-merge author checklist**
    
    - [x] I've followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
    Extension Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- test/stub/keyring-bridge.js
+++ test/stub/keyring-bridge.js
@@ -120,6 +120,22 @@ export class FakeTrezorBridge extends FakeKeyringBridge {
   }
 
   async ethereumSignTransaction({ transaction }) {
+    const txType = Number(transaction.type);
+    let hardfork;
+    switch (txType) {
+      case 0:
+        hardfork = 'muirGlacier';
+        break;
+      case 1:
+        throw new Error(
+          'Unsupported transaction type: EIP-2930 (type 1) not yet implemented in FakeTrezorBridge.',
+        );
+      case 2:
+        hardfork = 'london';
+        break;
+      default:
+        throw new Error(`Unsupported transaction type: ${txType}`);
+    }
     const common = Common.custom({
       chain: {
         name: 'localhost',
@@ -127,7 +143,7 @@ export class FakeTrezorBridge extends FakeKeyringBridge {
         networkId: transaction.chainId,
       },
       chainId: transaction.chainId,
-      hardfork: 'istanbul',
+      hardfork,
     });
 
     const signedTransaction = TransactionFactory.fromTxData(transaction, {
