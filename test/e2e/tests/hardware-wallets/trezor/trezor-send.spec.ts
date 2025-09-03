5f6203bc2e3e140302f7c1dec27d845392d8648f
Sébastien Van Eyck <sebastien.vaneyck@consensys.net>
2025-09-03 12:10:56 +0200

    fix: comments from review

--- test/e2e/tests/hardware-wallets/trezor/trezor-send.spec.ts
+++ test/e2e/tests/hardware-wallets/trezor/trezor-send.spec.ts
@@ -1,13 +1,13 @@
 import { Suite } from 'mocha';
 import { Driver } from '../../../webdriver/driver';
-import { Ganache } from '../../../seeder/ganache';
+import { Anvil } from '../../../seeder/anvil';
 import FixtureBuilder from '../../../fixture-builder';
 import { withFixtures } from '../../../helpers';
 import { KNOWN_PUBLIC_KEY_ADDRESSES } from '../../../../stub/keyring-bridge';
 import ActivityListPage from '../../../page-objects/pages/home/activity-list';
 import HomePage from '../../../page-objects/pages/home/homepage';
 import { sendRedesignedTransactionToAddress } from '../../../page-objects/flows/send-transaction.flow';
-import { loginWithoutBalanceValidation } from '../../../page-objects/flows/login.flow';
+import { loginWithBalanceValidation } from '../../../page-objects/flows/login.flow';
 
 const RECIPIENT = '0x0Cc5261AB8cE458dc977078A3623E2BaDD27afD3';
 
@@ -30,16 +30,20 @@ describe('Trezor Hardware', function (this: Suite) {
           localNodes,
         }: {
           driver: Driver;
-          localNodes: Ganache[] | undefined[];
+          localNodes: Anvil[] | undefined[];
         }) => {
           // Seed the Trezor account with balance
           (await localNodes?.[0]?.setAccountBalance(
-            KNOWN_PUBLIC_KEY_ADDRESSES[0].address,
+            KNOWN_PUBLIC_KEY_ADDRESSES[0].address as `0x${string}`,
             '0x100000000000000000000',
           )) ?? console.error('localNodes is undefined or empty');
-          await loginWithoutBalanceValidation(driver);
+          await loginWithBalanceValidation(
+            driver,
+            localNodes?.[0],
+            undefined,
+            '1208925.8196',
+          );
           const homePage = new HomePage(driver);
-          await homePage.checkExpectedBalanceIsDisplayed('1208925.8196');
           await sendRedesignedTransactionToAddress({
             driver,
             recipientAddress: RECIPIENT,
