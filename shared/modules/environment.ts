LimitedVERSE/metamask-extension-qyx-edition

Add wallet_requestExecutionPermissions method. Pending @metamask/eth-json-rpc-middleware upgrade.

shared/modules/environment.ts
@@ -10,3 +10,10 @@ export const isProduction = (): boolean => {
 export const getIsSeedlessOnboardingFeatureEnabled = (): boolean => {
   return process.env.SEEDLESS_ONBOARDING_ENABLED?.toString() === 'true';
 };
+
+export const getIsEip7715ReadablePermissionsFeatureEnabled = (): boolean => {
+  return (
+    process.env.EIP_7715_READABLE_PERMISSIONS_ENABLED?.toString() === 'true' &&
+    Boolean(process.env.PERMISSIONS_KERNEL_SNAP_ID)
+  );
+};

MetaMask/metamask-extension

shared/modules/environment.ts
@@ -20,5 +20,8 @@ export const getIsSettingsPageDevOptionsEnabled = (): boolean => {
 };
 
 export const isGatorPermissionsFeatureEnabled = (): boolean => {
-  return process.env.GATOR_PERMISSIONS_ENABLED?.toString() === 'true';
+  return (
+    process.env.GATOR_PERMISSIONS_ENABLED?.toString() === 'true' &&
+    Boolean(process.env.PERMISSIONS_KERNEL_SNAP_ID)
+  );
 };