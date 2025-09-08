2af655a0706937fc75527c901456f7cd22db09e8
Lionell Briones <llenoil@gmail.com>
2025-09-08 12:04:53 +0800

    feat: added shield claim form without file uploader

---
+++ ui/pages/settings/transaction-shield-tab/submit-claim-form/submit-claim-form-state.ts
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+
+export const useSubmitClaimFormState = () => {
+  const [firstName, setFirstName] = useState<string>('');
+  const [lastName, setLastName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [lossAmount, setLossAmount] = useState<string>('');
+  const [impactedWalletAddress, setImpactedWalletAddress] =
+    useState<string>('');
+  const [impactedTxHash, setImpactedTxHash] = useState<string>('');
+  const [reimbursementWalletAddress, setReimbursementWalletAddress] =
+    useState<string>('');
+  const [description, setDescription] = useState<string>('');
+
+  return {
+    firstName,
+    setFirstName,
+    lastName,
+    setLastName,
+    email,
+    setEmail,
+    lossAmount,
+    setLossAmount,
+    impactedWalletAddress,
+    setImpactedWalletAddress,
+    impactedTxHash,
+    setImpactedTxHash,
+    reimbursementWalletAddress,
+    setReimbursementWalletAddress,
+    description,
+    setDescription,
+    clear: () => {
+      setFirstName('');
+      setLastName('');
+      setEmail('');
+      setLossAmount('');
+      setImpactedWalletAddress('');
+      setImpactedTxHash('');
+      setReimbursementWalletAddress('');
+      setDescription('');
+    },
+  };
+};
