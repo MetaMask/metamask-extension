b067aa1a365f7e3d601665f6c491d91ce16c54ce
Vinicius Stevam <vinicius.stevam@consensys.net>
2025-09-08 10:57:37 +0100

    fix:  point AccountGroup.metadata.name instead of internal account name

--- ui/pages/confirmations/hooks/useConfirmationRecipientInfo.ts
+++ ui/pages/confirmations/hooks/useConfirmationRecipientInfo.ts
@@ -1,27 +1,51 @@
 import { useSelector } from 'react-redux';
 import { getAccountByAddress } from '../../../helpers/utils/util';
-import { accountsWithSendEtherInfoSelector } from '../../../selectors';
+import {
+  accountsWithSendEtherInfoSelector,
+  getIsMultichainAccountsState2Enabled,
+} from '../../../selectors';
 import { getConfirmationSender } from '../components/confirm/utils';
 import { useConfirmContext } from '../context/confirm';
+import { getAccountGroupsByAddress } from '../../../selectors/multichain-accounts/account-tree';
+import { MultichainAccountsState } from '../../../selectors/multichain-accounts/account-tree.types';
 
 function useConfirmationRecipientInfo() {
   const { currentConfirmation } = useConfirmContext();
   const allAccounts = useSelector(accountsWithSendEtherInfoSelector);
+  const allAccountGroups = useSelector((state: MultichainAccountsState) =>
+    getAccountGroupsByAddress(
+      state,
+      allAccounts.map((a) => a.address),
+    ),
+  );
 
-  let senderAddress, senderName;
+  const isMultichainAccountsState2Enabled = useSelector(
+    getIsMultichainAccountsState2Enabled,
+  );
+
+  let senderAddress: string | undefined;
+  let senderName: string | undefined;
+
   if (currentConfirmation) {
     const { from } = getConfirmationSender(currentConfirmation);
-    const fromAccount = getAccountByAddress(allAccounts, from);
-
     senderAddress = from;
-    senderName = fromAccount?.metadata?.name;
+    if (isMultichainAccountsState2Enabled) {
+      const group = allAccountGroups.find((g) =>
+        g.accounts.some(
+          (a) => a.address?.toLowerCase() === from?.toLowerCase(),
+        ),
+      );
+      senderName = group?.metadata?.name;
+    } else {
+      const allInternalAccounts = allAccountGroups.flatMap((g) => g.accounts);
+      const fromAccount = getAccountByAddress(allInternalAccounts, from);
+      senderName = fromAccount?.metadata?.name;
+    }
   }
 
   return {
-    // TODO: Fix in https://github.com/MetaMask/metamask-extension/issues/31880
-    // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
-    senderAddress: senderAddress || '',
-    senderName: senderName || '',
+    senderAddress: senderAddress ?? '',
+    senderName: senderName ?? '',
   };
 }
 
