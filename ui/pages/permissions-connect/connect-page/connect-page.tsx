dd0abd9d25b545ab7d7c00647e36b49d911e84ef
mindofmar <martin.peko@consensys.net>
2025-09-08 22:26:25 -0500

    feat: add verified trust signal to dapp connection

--- ui/pages/permissions-connect/connect-page/connect-page.tsx
+++ ui/pages/permissions-connect/connect-page/connect-page.tsx
@@ -22,6 +22,7 @@ import {
 } from '@metamask/utils';
 
 import { isEqual } from 'lodash';
+import { Tooltip } from 'react-tippy';
 import { useI18nContext } from '../../../hooks/useI18nContext';
 import {
   getPermissions,
@@ -38,6 +39,9 @@ import {
   ButtonLink,
   ButtonSize,
   ButtonVariant,
+  Icon,
+  IconName,
+  IconSize,
   Text,
 } from '../../../components/component-library';
 import {
@@ -54,6 +58,7 @@ import {
   BorderRadius,
   Display,
   FlexDirection,
+  IconColor,
   JustifyContent,
   TextAlign,
   TextColor,
@@ -82,6 +87,8 @@ import {
 } from '../../../selectors/selectors.types';
 import { CreateSolanaAccountModal } from '../../../components/multichain/create-solana-account-modal/create-solana-account-modal';
 import { mergeCaip25CaveatValues } from '../../../../shared/lib/caip25-caveat-merger';
+import { useOriginTrustSignals } from '../../../hooks/useOriginTrustSignals';
+import { TrustSignalDisplayState } from '../../../hooks/useTrustSignals';
 import {
   PermissionsRequest,
   getCaip25CaveatValueFromPermissions,
@@ -441,6 +448,9 @@ export const ConnectPage: React.FC<ConnectPageProps> = ({
   ]);
 
   const title = transformOriginToTitle(targetSubjectMetadata.origin);
+  const originTrustSignals = useOriginTrustSignals(
+    targetSubjectMetadata.origin,
+  );
 
   return (
     <Page
@@ -491,9 +501,27 @@ export const ConnectPage: React.FC<ConnectPageProps> = ({
             </AvatarBase>
           )}
         </Box>
-        <Text variant={TextVariant.headingLg} marginBottom={1}>
-          {title}
-        </Text>
+        <Box
+          display={Display.Flex}
+          alignItems={AlignItems.center}
+          justifyContent={JustifyContent.center}
+          gap={2}
+          marginBottom={1}
+        >
+          <Text variant={TextVariant.headingLg}>{title}</Text>
+          {originTrustSignals.state === TrustSignalDisplayState.Verified && (
+            <Tooltip
+              title={t('alertReasonOriginTrustSignalVerified')}
+              position="bottom"
+            >
+              <Icon
+                name={IconName.VerifiedFilled}
+                color={IconColor.infoDefault}
+                size={IconSize.Sm}
+              />
+            </Tooltip>
+          )}
+        </Box>
         <Box display={Display.Flex} justifyContent={JustifyContent.center}>
           <Text color={TextColor.textAlternative}>
             {t('connectionDescription')}
