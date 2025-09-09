c7ef113c59da2765834031eb47452981ec02f1da
Alejandro Som <560018+alucardzom@users.noreply.github.com>
2025-09-09 12:02:34 +0200

    chore: restore

--- development/fitness-functions/index.ts
+++ development/fitness-functions/index.ts
@@ -3,11 +3,9 @@ import { getDiffByAutomationType } from './common/get-diff';
 import { IRule, RULES, runFitnessFunctionRule } from './rules';
 
 const automationType: AUTOMATION_TYPE = process.argv[2] as AUTOMATION_TYPE;
+
 const diff = getDiffByAutomationType(automationType);
 
-if (!diff || typeof diff !== 'string') {
-  console.error('Invalid diff output:', diff, 'for automationType:', automationType);
-  process.exit(1);
+if (typeof diff === 'string') {
+  RULES.forEach((rule: IRule): void => runFitnessFunctionRule(rule, diff));
 }
-
-RULES.forEach((rule: IRule): void => runFitnessFunctionRule(rule, diff));
