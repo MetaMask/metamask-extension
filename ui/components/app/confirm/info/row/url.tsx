MetaMask/metamask-extension

commit c16aefbb97fad078da8cc1b6793c014ddba65492

fix: origin truncation issue by displaying hostname (#35443)
<!--
Please submit this PR as a draft initially.
Do not mark it as "Ready for review" until the template has been
completely filled out, and PR status checks have passed at least once.
-->

## **Description**

Fixes: https://github.com/MetaMask/MetaMask-planning/issues/5325

See above issue for description and reproduction steps.

## **Screenshots/Recordings**

<!-- If applicable, add screenshots and/or recordings to visualize the
before and after of your change. -->

### **Before**

<img width="3024" height="1630" alt="image"
src="https://github.com/user-attachments/assets/19e0a5bb-4b10-462c-b5db-3ae6953df24d"
/>

### **After**

<img width="512" height="732" alt="image"
src="https://github.com/user-attachments/assets/e9791c8b-e80d-4fa6-b9c8-0c5e88bd58c2"
/>

## **Pre-merge author checklist**

- [x] I've followed [MetaMask Contributor
Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
Extension Coding
Standards](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/CODING_GUIDELINES.md).
- [x] I've completed the PR template to the best of my ability
- [x] I’ve included tests if applicable
- [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
if applicable
- [x] I’ve applied the right labels on the PR (see [labeling
guidelines](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
Not required for external contributors.

## **Pre-merge reviewer checklist**

- [x] I've manually tested the PR (e.g. pull and build branch, run the
app, test code being changed).
- [x] I confirm that this PR addresses all acceptance criteria described
in the ticket it closes and includes the necessary testing evidence such
as recordings and or screenshots.

ui/pages/permissions-connect/connect-page/__snapshots__/connect-page.test.tsx.snap
@@ -30,7 +30,7 @@ exports[`ConnectPage should render correctly 1`] = `
                   class="mm-box mm-text mm-avatar-base mm-avatar-base--size-xl mm-avatar-favicon mm-text--body-lg-medium mm-text--text-transform-uppercase mm-box--display-flex mm-box--justify-content-center mm-box--align-items-center mm-box--color-text-default mm-box--background-color-background-muted mm-box--rounded-full mm-box--border-color-transparent box--border-style-solid box--border-width-1"
                 >
                   <img
-                    alt="github.io logo"
+                    alt="metamask.github.io logo"
                     class="mm-avatar-favicon__image"
                     src="https://metamask.github.io/test-dapp/metamask-fox.svg"
                   />
@@ -41,7 +41,7 @@ exports[`ConnectPage should render correctly 1`] = `
                 style="z-index: 1; background: transparent;"
               >
                 <img
-                  alt="github.io logo"
+                  alt="metamask.github.io logo"
                   class="mm-avatar-favicon__image"
                   src="https://metamask.github.io/test-dapp/metamask-fox.svg"
                 />
@@ -50,7 +50,7 @@ exports[`ConnectPage should render correctly 1`] = `
             <h2
               class="mm-box mm-text mm-text--heading-lg mm-box--margin-bottom-1 mm-box--color-text-default"
             >
-              github.io
+              metamask.github.io
             </h2>
             <div
               class="mm-box mm-box--display-flex mm-box--justify-content-center"
@@ -371,7 +371,7 @@ exports[`ConnectPage should render with defaults from the requested permissions 
                   class="mm-box mm-text mm-avatar-base mm-avatar-base--size-xl mm-avatar-favicon mm-text--body-lg-medium mm-text--text-transform-uppercase mm-box--display-flex mm-box--justify-content-center mm-box--align-items-center mm-box--color-text-default mm-box--background-color-background-muted mm-box--rounded-full mm-box--border-color-transparent box--border-style-solid box--border-width-1"
                 >
                   <img
-                    alt="github.io logo"
+                    alt="metamask.github.io logo"
                     class="mm-avatar-favicon__image"
                     src="https://metamask.github.io/test-dapp/metamask-fox.svg"
                   />
@@ -382,7 +382,7 @@ exports[`ConnectPage should render with defaults from the requested permissions 
                 style="z-index: 1; background: transparent;"
               >
                 <img
-                  alt="github.io logo"
+                  alt="metamask.github.io logo"
                   class="mm-avatar-favicon__image"
                   src="https://metamask.github.io/test-dapp/metamask-fox.svg"
                 />
@@ -391,7 +391,7 @@ exports[`ConnectPage should render with defaults from the requested permissions 
             <h2
               class="mm-box mm-text mm-text--heading-lg mm-box--margin-bottom-1 mm-box--color-text-default"
             >
-              github.io
+              metamask.github.io
             </h2>
             <div
               class="mm-box mm-box--display-flex mm-box--justify-content-center"

ui/components/app/permission-connect-header/permission-connect-header.js
@@ -25,8 +25,7 @@ const PermissionConnectHeader = ({ requestId, origin, iconUrl }) => {
   const transformOriginToTitle = (rawOrigin) => {
     try {
       const url = new URL(rawOrigin);
-      const parts = url.hostname.split('.');
-      return parts.slice(-2).join('.');
+      return url.hostname;
     } catch (e) {
       return 'Unknown Origin';
     }

ui/components/app/permission-connect-header/permission-connect-header.test.js
@@ -11,8 +11,8 @@ describe('Permission Connect Header', () => {
     origin: 'https://metamask.github.io',
     iconUrl: 'https://metamask.github.io/test-dapp/metamask-fox.svg',
   };
-  const expectedTitle = 'github.io';
-  const expectedAltImageText = 'github.io logo';
+  const expectedTitle = 'metamask.github.io';
+  const expectedAltImageText = 'metamask.github.io logo';
 
   it('renders permission connect header', () => {
     const { getByAltText } = renderWithProvider(

ui/helpers/utils/util.js
@@ -885,8 +885,7 @@ export const transformOriginToTitle = (rawOrigin) => {
       return url.hostname;
     }
 
-    const parts = url.hostname.split('.');
-    return parts.slice(-2).join('.');
+    return url.hostname;
   } catch (e) {
     return 'Unknown Origin';
   }

ui/helpers/utils/util.test.js
@@ -1479,7 +1479,7 @@ describe('util', () => {
     it('should return the correct title for origin with subdomain', () => {
       expect(
         util.transformOriginToTitle('https://metamask.github.io/test-dapp/'),
-      ).toBe('github.io');
+      ).toBe('metamask.github.io');
     });
 
     it('should return the correct title for localhost', () => {

ui/pages/permissions-connect/connect-page/connect-page.test.tsx
@@ -101,7 +101,7 @@ describe('ConnectPage', () => {
   it('should render image icon correctly', () => {
     const { getAllByAltText } = render();
 
-    const images = getAllByAltText('github.io logo');
+    const images = getAllByAltText('metamask.github.io logo');
     expect(images.length).toBe(2);
     expect(images[0]).toHaveAttribute(
       'src',
@@ -129,7 +129,7 @@ describe('ConnectPage', () => {
     });
 
     const divElement = container.querySelector('div.mm-avatar-base--size-lg');
-    expect(divElement).toHaveTextContent('g');
+    expect(divElement).toHaveTextContent('m');
   });
 
   it('should render fallback icon correctly for IP address as an origin', () => {
@@ -154,7 +154,7 @@ describe('ConnectPage', () => {
 
   it('should render title correctly', () => {
     const { getByText } = render();
-    expect(getByText('github.io')).toBeDefined();
+    expect(getByText('metamask.github.io')).toBeDefined();
   });
 
   it('should render subtitle correctly', () => {

commit 3705009eeaa8b6c2db9276187d8688bd775e1b02

fix: dapp connect title overflow (#35715)
<!--
Please submit this PR as a draft initially.
Do not mark it as "Ready for review" until the template has been
completely filled out, and PR status checks have passed at least once.
-->

## **Description**

- [x] Fixes an issue introduced in
https://github.com/MetaMask/metamask-extension/pull/35443 which causes
very long URLs to overflow during dapp connection. Long URLs now
properly break without overflowing.
- [x] It also cleans up some minor tech debt from
https://github.com/MetaMask/metamask-extension/pull/35459 where it was
discovered that the strings `Malicious site` and `Suspicious site`
already existed in the `messages.json` list of translations. This PR
removes the duplication.

<!--
Write a short description of the changes included in this pull request,
also include relevant motivation and context. Have in mind the following
questions:
1. What is the reason for the change?
2. What is the improvement/solution?
-->

[![Open in GitHub
Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/35715?quickstart=1)


## **Related issues**

Fixes: https://github.com/MetaMask/MetaMask-planning/issues/5762

## **Screenshots/Recordings**

<!-- If applicable, add screenshots and/or recordings to visualize the
before and after of your change. -->

### **Before**
<img width="512" height="732" alt="image"
src="https://github.com/user-attachments/assets/e6a3a46c-5e98-4545-8756-9b0e02cf0c8c"
/>

<!-- [screenshots/recordings] -->

### **After**
<img width="512" height="732" alt="image"
src="https://github.com/user-attachments/assets/b20ad753-d8e2-4340-bdb0-ec0d67dc2105"
/>

<!-- [screenshots/recordings] -->

## **Pre-merge author checklist**

- [x] I've followed [MetaMask Contributor
Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
Extension Coding
Standards](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/CODING_GUIDELINES.md).

ui/components/app/confirm/info/row/url.tsx
@@ -92,7 +92,7 @@ export const ConfirmInfoRowUrl = ({ url }: ConfirmInfoRowUrlProps) => {
     if (originTrustSignals.state === TrustSignalDisplayState.Malicious) {
       return (
         <Tooltip
-          title={t('trustSignalUrlMalicious')}
+          title={t('alertReasonOriginTrustSignalMalicious')}
           position="bottom"
           style={{ display: 'flex' }}
         >
@@ -114,7 +114,7 @@ export const ConfirmInfoRowUrl = ({ url }: ConfirmInfoRowUrlProps) => {
     if (originTrustSignals.state === TrustSignalDisplayState.Warning) {
       return (
         <Tooltip
-          title={t('trustSignalUrlSuspicious')}
+          title={t('alertReasonOriginTrustSignalWarning')}
           position="bottom"
           style={{ display: 'flex' }}
         >
@@ -131,7 +131,7 @@ export const ConfirmInfoRowUrl = ({ url }: ConfirmInfoRowUrlProps) => {
     if (originTrustSignals.state === TrustSignalDisplayState.Verified) {
       return (
         <Tooltip
-          title={t('trustSignalUrlVerified')}
+          title={t('alertReasonOriginTrustSignalVerified')}
           position="bottom"
           style={{ display: 'flex' }}
         >

ui/pages/permissions-connect/connect-page/__snapshots__/connect-page.test.tsx.snap
@@ -49,6 +49,7 @@ exports[`ConnectPage should render correctly 1`] = `
             </div>
             <h2
               class="mm-box mm-text mm-text--heading-lg mm-box--margin-bottom-1 mm-box--color-text-default"
+              style="word-break: break-word; white-space: normal;"
             >
               metamask.github.io
             </h2>
@@ -463,6 +464,7 @@ exports[`ConnectPage should render with defaults from the requested permissions 
             </div>
             <h2
               class="mm-box mm-text mm-text--heading-lg mm-box--margin-bottom-1 mm-box--color-text-default"
+              style="word-break: break-word; white-space: normal;"
             >
               metamask.github.io
             </h2>

ui/pages/permissions-connect/connect-page/connect-page.tsx
@@ -491,7 +491,14 @@ export const ConnectPage: React.FC<ConnectPageProps> = ({
             </AvatarBase>
           )}
         </Box>
-        <Text variant={TextVariant.headingLg} marginBottom={1}>
+        <Text
+          variant={TextVariant.headingLg}
+          marginBottom={1}
+          style={{
+            wordBreak: 'break-word',
+            whiteSpace: 'normal',
+          }}
+        >
           {title}
         </Text>
         <Box display={Display.Flex} justifyContent={JustifyContent.center}>