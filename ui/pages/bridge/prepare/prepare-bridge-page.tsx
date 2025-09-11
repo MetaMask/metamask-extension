c5c4b740122d29df727a24f7e433076d066c377b
ghgoodreau <hunter.goodreau@consensys.net>
2025-09-11 10:38:29 -0500

    lint fix

--- ui/pages/bridge/prepare/prepare-bridge-page.tsx
+++ ui/pages/bridge/prepare/prepare-bridge-page.tsx
@@ -619,7 +619,7 @@ const PrepareBridgePage = ({
                   toToken &&
                   dispatch(
                     trackUnifiedSwapBridgeEvent(
-                      UnifiedSwapBridgeEventName.InputSourceDestinationSwitched,
+                      UnifiedSwapBridgeEventName.InputSourceDestinationFlipped,
                       {
                         // TODO: Fix in https://github.com/MetaMask/metamask-extension/issues/31860
                         // eslint-disable-next-line @typescript-eslint/naming-convention
