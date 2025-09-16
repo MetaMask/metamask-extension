9fca627f1804acaf3f54577cca28dd1b3c8d290c
Tuna <tunguyenanh@tunatech.org>
2025-09-16 18:00:45 +0700

    fix: correct comment

--- app/scripts/controller-init/messengers/subscription/subscription-controller-messenger.ts
+++ app/scripts/controller-init/messengers/subscription/subscription-controller-messenger.ts
@@ -1,22 +1,19 @@
-import {
-  AuthenticationController,
-} from '@metamask/profile-sync-controller';
+import { AuthenticationController } from '@metamask/profile-sync-controller';
 import { Messenger } from '@metamask/base-controller';
 
 type Actions =
   | AuthenticationController.AuthenticationControllerGetBearerToken
   | AuthenticationController.AuthenticationControllerPerformSignOut;
 
-type Events =
-  | AuthenticationController.AuthenticationControllerStateChangeEvent;
+type Events = AuthenticationController.AuthenticationControllerStateChangeEvent;
 
 export type SubscriptionControllerMessenger = ReturnType<
   typeof getSubscriptionControllerMessenger
 >;
 
 /**
- * Get a restricted messenger for the Multichain Assets controller. This is scoped to the
- * actions and events that the multichain Assets controller is allowed to handle.
+ * Get a restricted messenger for the Subscription controller. This is scoped to the
+ * actions and events that the Subscription controller is allowed to handle.
  *
  * @param messenger - The controller messenger to restrict.
  * @returns The restricted controller messenger.
@@ -26,9 +23,7 @@ export function getSubscriptionControllerMessenger(
 ) {
   return messenger.getRestricted({
     name: 'SubscriptionController',
-    allowedEvents: [
-      'AuthenticationController:stateChange',
-    ],
+    allowedEvents: ['AuthenticationController:stateChange'],
     allowedActions: [
       'AuthenticationController:getBearerToken',
       'AuthenticationController:performSignOut',
@@ -37,7 +32,7 @@ export function getSubscriptionControllerMessenger(
 }
 
 type InitActions =
-  | AuthenticationController.AuthenticationControllerGetBearerToken;
+  AuthenticationController.AuthenticationControllerGetBearerToken;
 
 type InitEvents = never;
 
