014ed788b6019cc0b21a08b026f6577a34bc3625
Micaela Estabillo <micaela.estabillo@consensys.net>
2025-09-16 09:34:03 -0700

    fix: switched event name

--- ui/pages/bridge/prepare/prepare-bridge-page.tsx
+++ ui/pages/bridge/prepare/prepare-bridge-page.tsx
@@ -621,7 +621,7 @@ const PrepareBridgePage = ({
                   toToken &&
                   dispatch(
                     trackUnifiedSwapBridgeEvent(
-                      UnifiedSwapBridgeEventName.InputSourceDestinationFlipped,
+                      UnifiedSwapBridgeEventName.InputSourceDestinationSwitched,
                       {
                         // TODO: Fix in https://github.com/MetaMask/metamask-extension/issues/31860
                         // eslint-disable-next-line @typescript-eslint/naming-convention
