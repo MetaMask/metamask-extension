ee0b528fdb244d50d53328abd7b1f715efef6c3a
Antonio Regadas <antonio.regadas@consensys.net>
2025-09-17 15:36:58 +0100

    chore: clean up

--- ui/components/multichain/network-manager/components/custom-networks/custom-networks.tsx
+++ ui/components/multichain/network-manager/components/custom-networks/custom-networks.tsx
@@ -164,7 +164,7 @@ export const CustomNetworks = React.memo(() => {
   const renderedTestNetworks = useMemo(() => {
     const filteredTestNetworks = orderedTestNetworks.filter((network) => {
       // If EVM network is selected, only show EVM networks
-      if (isEvmNetworkSelected) {
+      if (isEvmNetworkSelected || isMultichainAccountsFeatureEnabled) {
         return network.isEvm;
       }
       // If non-EVM network is selected, only show non-EVM networks
@@ -178,6 +178,7 @@ export const CustomNetworks = React.memo(() => {
     orderedTestNetworks,
     isEvmNetworkSelected,
     generateMultichainNetworkListItem,
+    isMultichainAccountsFeatureEnabled,
   ]);
 
   // Memoize the padding value to prevent unnecessary re-renders
