bdf9608b7e8c5adeb6344cc8483238670f7fb272
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2025-09-17 12:57:18 +0200

    Fix lint issue

--- app/scripts/controller-init/messengers/bridge-status-controller-messenger.test.ts
+++ app/scripts/controller-init/messengers/bridge-status-controller-messenger.test.ts
@@ -1,12 +1,11 @@
 import { Messenger, RestrictedMessenger } from '@metamask/base-controller';
-import {
-  getBridgeStatusControllerMessenger,
-} from './bridge-status-controller-messenger';
+import { getBridgeStatusControllerMessenger } from './bridge-status-controller-messenger';
 
 describe('getBridgeStatusControllerMessenger', () => {
   it('returns a restricted messenger', () => {
     const messenger = new Messenger<never, never>();
-    const BridgeStatusControllerMessenger = getBridgeStatusControllerMessenger(messenger);
+    const BridgeStatusControllerMessenger =
+      getBridgeStatusControllerMessenger(messenger);
 
     expect(BridgeStatusControllerMessenger).toBeInstanceOf(RestrictedMessenger);
   });
