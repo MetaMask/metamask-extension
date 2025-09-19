4ec72abcbf5a3110e5c59d51871688671c89eb86
georgewrmarshall <george.marshall@consensys.net>
2025-09-18 17:03:12 -0700

    chore: updating tests

--- ui/components/app/assets/defi-list/defi-list.test.tsx
+++ ui/components/app/assets/defi-list/defi-list.test.tsx
@@ -194,12 +194,12 @@ describe('DeFiDetailsPage', () => {
     });
 
     await waitFor(() => {
+      // Check for new TabEmptyState content
       expect(
-        screen.getByText('We may not support your protocol yet.'),
+        screen.getByText('Lend, borrow, and trade, right in your wallet.'),
       ).toBeInTheDocument();
-      expect(
-        screen.getByText("Can't find what you're looking for?"),
-      ).toBeInTheDocument();
+      expect(screen.getByText('Explore DeFi')).toBeInTheDocument();
+      expect(screen.getByTestId('defi-tab-empty-state')).toBeInTheDocument();
     });
   });
 });
