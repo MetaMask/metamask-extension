1ac2b2d900024110aa5a946446d5fb395e2a7113
Jeff Smale <6363749+jeffsmale90@users.noreply.github.com>
2025-09-23 21:59:14 +1200

    Preinstall gator permissions snaps - set up a new build feature 'gator-permissions' - add 'gator-permissions' to flask build type - add preinstall artifacts to snap constants - add gator permissions snaps to preinstalled snaps constant

--- shared/lib/snaps/snaps.ts
+++ shared/lib/snaps/snaps.ts
@@ -19,6 +19,8 @@ export const PREINSTALLED_SNAPS = [
   'npm:@metamask/preinstalled-example-snap',
   'npm:@metamask/bitcoin-wallet-snap',
   'npm:@metamask/solana-wallet-snap',
+  'npm:@metamask/permissions-kernel-snap',
+  'npm:@metamask/gator-permissions-snap',
 ];
 
 /**
