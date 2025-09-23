48e0718e99118c40b768e300427bd183456b9812
Norbert Elter <72046715+itsyoboieltr@users.noreply.github.com>
2025-09-23 18:45:17 +0400

    Revert "chore: lavamoat webpack git build"
    This reverts commit 24fc0e2fa0eb5148af65ae29a80c2a63b1f35bb4.

--- development/webpack/utils/plugins/LavamoatPlugin/index.ts
+++ development/webpack/utils/plugins/LavamoatPlugin/index.ts
@@ -3,7 +3,6 @@ import type { WebpackPluginInstance, RuleSetRule, Chunk } from 'webpack';
 import {
   LavaMoatPlugin,
   exclude as LavamoatExcludeLoader,
-  // @ts-expect-error - types are broken due to git install
 } from '@lavamoat/webpack';
 import type { Args } from '../../cli';
 
@@ -94,6 +93,7 @@ export const lavamoatPlugin = (args: Args) =>
         'fetch',
         'AbortController',
         'OffscreenCanvas',
+        // @ts-expect-error - globals chromedriver needs to function
         /cdc_[a-zA-Z0-9]+_[a-zA-Z]+/iu,
         'name',
         'performance',
