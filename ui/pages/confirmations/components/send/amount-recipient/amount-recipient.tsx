53c2ecf0e88773cffa9533bb0efe9456eb6b35da
Jyoti Puri <jyotipuri@gmail.com>
2025-09-24 15:50:57 +0530

    fix: amount error in fiat mode (#36127)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    Fix issue with amount validation in fiat mode.
    
    ## **Changelog**
    
    <!--
    If this PR is not End-User-Facing and should not show up in the
    CHANGELOG, you can choose to either:
    1. Write `CHANGELOG entry: null`
    2. Label with `no-changelog`
    
    If this PR is End-User-Facing, please write a short User-Facing
    description in the past tense like:
    `CHANGELOG entry: Added a new tab for users to see their NFTs`
    `CHANGELOG entry: Fixed a bug that was causing some NFTs to flicker`
    
    (This helps the Release Engineer do their job more quickly and
    accurately)
    -->
    
    CHANGELOG entry:
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/metamask-extension/issues/36085
    Fixes: https://github.com/MetaMask/metamask-extension/issues/36131
    Fixes: https://github.com/MetaMask/metamask-extension/issues/36191
    Fixes: https://github.com/MetaMask/metamask-extension/issues/36203
    
    ## **Manual testing steps**
    
    1. Go to amount page in send
    2. Toggle to fiat mode and enter value "abc"
    3. It should not throw
    
    ## **Screenshots/Recordings**
    TODO
    
    ## **Pre-merge author checklist**
    
    - [X] I've followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
    Extension Coding
    Standards](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [X] I've completed the PR template to the best of my ability
    - [X] I’ve included tests if applicable
    - [X] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [X] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- ui/pages/confirmations/components/send/amount-recipient/amount-recipient.tsx
+++ ui/pages/confirmations/components/send/amount-recipient/amount-recipient.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 
 import LoadingScreen from '../../../../../components/ui/loading-screen';
 import {
@@ -14,7 +14,6 @@ import {
 } from '../../../../../helpers/constants/design-system';
 import { useI18nContext } from '../../../../../hooks/useI18nContext';
 import { useAmountSelectionMetrics } from '../../../hooks/send/metrics/useAmountSelectionMetrics';
-import { useAmountValidation } from '../../../hooks/send/useAmountValidation';
 import { useSendActions } from '../../../hooks/send/useSendActions';
 import { useSendContext } from '../../../context/send';
 import { useRecipientValidation } from '../../../hooks/send/useRecipientValidation';
@@ -25,13 +24,13 @@ import { Asset } from '../../../types/send';
 
 export const AmountRecipient = () => {
   const t = useI18nContext();
+  const [amountValueError, setAmountValueError] = useState<string>();
   const { asset, toResolved } = useSendContext();
   const { handleSubmit } = useSendActions();
   const { captureAmountSelected } = useAmountSelectionMetrics();
-  const { amountError } = useAmountValidation();
   const { recipientError } = useRecipientValidation();
 
-  const hasError = Boolean(amountError) || Boolean(recipientError);
+  const hasError = Boolean(amountValueError) || Boolean(recipientError);
   const isDisabled = hasError || !toResolved;
 
   const onClick = useCallback(() => {
@@ -55,7 +54,7 @@ export const AmountRecipient = () => {
       <Box>
         <SendHero asset={asset as Asset} />
         <Recipient />
-        <Amount />
+        <Amount setAmountValueError={setAmountValueError} />
       </Box>
       <Button
         disabled={isDisabled}
@@ -66,7 +65,7 @@ export const AmountRecipient = () => {
         }
         marginBottom={4}
       >
-        {amountError ?? t('continue')}
+        {amountValueError ?? t('continue')}
       </Button>
     </Box>
   );
