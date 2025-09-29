2bf834fe6b45353a1b9ef6dab5143782a2407925
Prithpal Sooriya <prithpal.sooriya@consensys.net>
2025-09-29 16:17:08 +0100

    fix: fix network manager tab selection

--- shared/constants/network.ts
+++ shared/constants/network.ts
@@ -1501,7 +1501,6 @@ export const FEATURED_NETWORK_CHAIN_IDS = [
 
 export const FEATURED_NETWORK_CHAIN_IDS_MULTICHAIN = [
   SolScope.Mainnet,
-  SolScope.Devnet,
   BtcScope.Mainnet,
   CHAIN_IDS.MAINNET,
   ...FEATURED_RPCS.map((rpc) => rpc.chainId),
