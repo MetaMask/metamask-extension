430f2f741f3516e3d3b30885a55a7f9c2f09fea5
Michele Esposito <michele@esposito.codes>
2025-09-29 20:20:59 +0200

    fix: add migration to remove `AppStateController.qrHardware`

---
+++ app/scripts/migrations/179.test.ts
@@ -0,0 +1,51 @@
+import { migrate, version } from './179';
+
+const oldVersion = 178;
+
+describe(`migration #${version}`, () => {
+  it('updates the version metadata', async () => {
+    const oldStorage = {
+      meta: { version: oldVersion },
+      data: {},
+    };
+    const newStorage = await migrate(oldStorage);
+    expect(newStorage.meta).toStrictEqual({ version });
+  });
+
+  describe(`migration #${version}`, () => {
+    it('deletes AppStateController.qrHardware from the state', async () => {
+      const oldStorage = {
+        meta: { version: oldVersion },
+        data: {
+          AppStateController: {
+            qrHardware: { foo: 'bar' },
+            other: 'data',
+          },
+        },
+      };
+
+      const newStorage = await migrate(oldStorage);
+
+      expect(newStorage.data).toStrictEqual({
+        AppStateController: {
+          other: 'data',
+        },
+      });
+    });
+
+    it('does nothing if AppStateController.qrHardware is not present', async () => {
+      const oldStorage = {
+        meta: { version: oldVersion },
+        data: {
+          AppStateController: {
+            other: 'data',
+          },
+        },
+      };
+
+      const newStorage = await migrate(oldStorage);
+
+      expect(newStorage.data).toStrictEqual(oldStorage.data);
+    });
+  });
+});
