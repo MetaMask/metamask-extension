d3ba9b36bf138778c1fe8f0f190533a395693ca2
Micaela Estabillo <micaela.estabillo@consensys.net>
2025-09-30 13:13:48 -0700

    feat: use streaming preview

--- ui/ducks/bridge/selectors.ts
+++ ui/ducks/bridge/selectors.ts
@@ -769,11 +769,13 @@ export const getBridgeQuotes = createSelector(
     ({ bridge: { sortOrder } }: BridgeAppState) => sortOrder,
     ({ bridge: { selectedQuote } }: BridgeAppState) => selectedQuote,
   ],
-  (controllerStates, sortOrder, selectedQuote) =>
-    selectBridgeQuotes(controllerStates, {
+  (controllerStates, sortOrder, selectedQuote) => {
+    const quotes = selectBridgeQuotes(controllerStates, {
       sortOrder,
       selectedQuote,
-    }),
+    });
+    return quotes;
+  },
 );
 
 export const getIsBridgeTx = createDeepEqualSelector(
