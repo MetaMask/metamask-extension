f836c21dfdf18fc7b1457d88572248bbc1efbded
Mathieu Artu <mathieu.artu@consensys.net>
2025-10-02 00:36:06 +0200

    fix: perf issue in hook

--- ui/hooks/accounts/useAccountsOperationsLoadingStates.ts
+++ ui/hooks/accounts/useAccountsOperationsLoadingStates.ts
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { useMemo } from 'react';
+import { useMemo, useEffect } from 'react';
 import { AccountTreeController } from '@metamask/account-tree-controller';
 import { useI18nContext } from '../useI18nContext';
 import { forceUpdateMetamaskState } from '../../store/actions';
@@ -15,17 +15,18 @@ export const useAccountsOperationsLoadingStates = () => {
     (state: AppState) => state.metamask.isAccountTreeSyncingInProgress,
   );
 
-  const areAnyOperationsLoading = useMemo(() => {
-    const update = () => {
-      // Hack: force the patches to be applied to the state
-      // Somehow, the AccountTreeController state updates are picked up by the PatchStore,
-      // but not by the React-Redux subscription.
-      // This forces the React-Redux subscription to pick up the changes.
-      forceUpdateMetamaskState(dispatch);
-      return isAccountTreeSyncingInProgress;
-    };
-    return update();
-  }, [isAccountTreeSyncingInProgress, dispatch]);
+  // Hack: force the patches to be applied to the state
+  // Somehow, the AccountTreeController state updates are picked up by the PatchStore,
+  // but not by the React-Redux subscription.
+  // This forces the React-Redux subscription to pick up the changes.
+  useEffect(() => {
+    forceUpdateMetamaskState(dispatch);
+  }, [isAccountTreeSyncingInProgress]);
+
+  // Since areAnyOperationsLoading just comprises isAccountTreeSyncingInProgress,
+  // we can use isAccountTreeSyncingInProgress directly instead of memoizing areAnyOperationsLoading.
+  // We can use useMemo when we add more loading states in the future.
+  const areAnyOperationsLoading = isAccountTreeSyncingInProgress;
 
   // We order by priority, the first one that matches will be shown
   const loadingMessage = useMemo(() => {
