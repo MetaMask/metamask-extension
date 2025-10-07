6a5c82f03bc923b40856b2036f8d43728747bfb9
Micaela Estabillo <micaela.estabillo@consensys.net>
2025-10-07 10:34:53 -0700

    chore: deprecate swap query param

--- shared/lib/deep-links/routes/swap.ts
+++ shared/lib/deep-links/routes/swap.ts
@@ -2,7 +2,6 @@ import { parseAssetID } from './helpers';
 import { Route, SWAP_ROUTE } from './route';
 
 export enum BridgeQueryParams {
-  SWAPS = 'swaps',
   AMOUNT = 'amount',
   /**
    * This is the Caip asset type of the token to bridge or swap from
@@ -38,13 +37,6 @@ export default new Route({
       const parsedTo = parseAssetID(to);
       if (parsedTo) {
         query.set(BridgeQueryParams.TO, to);
-        if (parsedFrom) {
-          // if `from` and `to` reference the same chain, add the `swap` param
-          // to trigger the "Swap" screen by default
-          if (parsedFrom.chainId.id === parsedTo.chainId.id) {
-            query.set(BridgeQueryParams.SWAPS, 'true');
-          }
-        }
       }
     }
     if (amount) {
