a6f82ac670667d2967541eecf6fedc16c9b5eca8
juanmigdr <juanmi.gdr@gmail.com>
2025-10-08 13:03:27 +0200

    fix: broken tests

--- ui/components/app/assets/token-cell/cells/token-cell-percent-change.tsx
+++ ui/components/app/assets/token-cell/cells/token-cell-percent-change.tsx
@@ -1,32 +1,49 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { getNativeTokenAddress } from '@metamask/assets-controllers';
-import { Hex } from '@metamask/utils';
+import { CaipAssetType, Hex } from '@metamask/utils';
+import { getMarketData } from '../../../../../selectors';
 import { TokenFiatDisplayInfo } from '../../types';
 import { PercentageChange } from '../../../../multichain/token-list-item/price/percentage-change';
+import { getAssetsRates } from '../../../../../selectors/assets';
 import { isEvmChainId } from '../../../../../../shared/lib/asset-utils';
 
 type TokenCellPercentChangeProps = {
   token: TokenFiatDisplayInfo;
-  price: number | undefined;
-  comparePrice: number | undefined;
+  price?: number;
+  comparePrice?: number;
 };
 
 export const TokenCellPercentChange = React.memo(
   ({ token, price, comparePrice }: TokenCellPercentChangeProps) => {
-    // Allow compare null and undefined
-    // eslint-disable-next-line no-eq-null
-    if (price == null || comparePrice == null) {
-      return null;
-    }
-
     const isEvm = isEvmChainId(token.chainId);
+    const multiChainMarketData = useSelector(getMarketData);
+    const nonEvmConversionRates = useSelector(getAssetsRates);
 
     const tokenAddress =
       token.isNative && isEvm
         ? getNativeTokenAddress(token.chainId as Hex)
         : token.address;
 
-    const tokenPercentageChange = ((price - comparePrice) / comparePrice) * 100;
+    // Compare null and undefined
+    // eslint-disable-next-line no-eq-null
+    if (price != null && comparePrice != null) {
+      const tokenPercentageChange =
+        ((price - comparePrice) / comparePrice) * 100;
+
+      return (
+        <PercentageChange
+          value={tokenPercentageChange}
+          address={tokenAddress}
+        />
+      );
+    }
+
+    const tokenPercentageChange = isEvm
+      ? multiChainMarketData?.[token.chainId]?.[tokenAddress]
+          ?.pricePercentChange1d
+      : nonEvmConversionRates?.[tokenAddress as CaipAssetType]?.marketData
+          ?.pricePercentChange?.P1D;
 
     return (
       <PercentageChange value={tokenPercentageChange} address={tokenAddress} />
