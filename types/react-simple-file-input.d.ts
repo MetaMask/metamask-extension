73df9c36fdc85d7fb85fd87533188f3194cfc1f4
Lionell Briones <llenoil@gmail.com>
2025-10-09 12:25:22 +0800

    feat: remove extra line breaks on type def

--- types/react-simple-file-input.d.ts
+++ types/react-simple-file-input.d.ts
@@ -9,7 +9,6 @@ declare module 'react-simple-file-input' {
       result: string | ArrayBuffer | null;
     };
   };
-
   /**
    * Props for the react-simple-file-input FileInput component
    */
@@ -20,14 +19,12 @@ declare module 'react-simple-file-input' {
      * @param files - The selected files
      */
     onChange?: (files: FileList) => void;
-
     /**
      * Callback fired when file content is loaded (when readAs is specified)
      *
      * @param event - Event containing the file content in event.target.result
      */
     onLoad?: (event: FileLoadEvent) => void;
-
     /**
      * How to read the file content. When specified, onLoad will be called with the result
      * - 'text': Read as text string
@@ -36,37 +33,30 @@ declare module 'react-simple-file-input' {
      * - 'binaryString': Read as binary string
      */
     readAs?: 'text' | 'dataURL' | 'arrayBuffer' | 'binaryString';
-
     /**
      * Whether to allow multiple file selection
      */
     multiple?: boolean;
-
     /**
      * File types to accept (MIME types)
      */
     accept?: string;
-
     /**
      * Input value (usually empty string to reset)
      */
     value?: string;
-
     /**
      * CSS class name
      */
     className?: string;
-
     /**
      * Inline styles
      */
     style?: React.CSSProperties;
-
     /**
      * Element ID
      */
     id?: string;
-
     /**
      * Test ID for testing
      */
