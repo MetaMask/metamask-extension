a59cdfa2872626e3af087ac3b4d6d0170528c716
sophieqgu <sophieqgu@gmail.com>
2025-10-13 11:14:37 -0400

    Add intro step

---
+++ ui/components/app/rewards/onboarding/onboarding-modal.tsx
@@ -0,0 +1,73 @@
+import React, { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  ModalContentSize,
+} from '../../../component-library';
+import { ThemeType } from '../../../../../shared/constants/preferences';
+import {
+  AlignItems,
+  JustifyContent,
+} from '../../../../helpers/constants/design-system';
+import {
+  selectOnboardingModalOpen,
+  selectOnboardingActiveStep,
+} from '../../../../ducks/rewards/selectors';
+import { setOnboardingModalOpen } from '../../../../ducks/rewards';
+import { OnboardingStep } from '../../../../ducks/rewards/types';
+import OnboardingIntroStep from './onboarding-intro-step';
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export default function OnboardingModal() {
+  const isOpen = useSelector(selectOnboardingModalOpen);
+  const onboardingStep = useSelector(selectOnboardingActiveStep);
+  const dispatch = useDispatch();
+
+  const handleClose = useCallback(() => {
+    dispatch(setOnboardingModalOpen(false));
+  }, [dispatch]);
+
+  const renderContent = useCallback(() => {
+    switch (onboardingStep) {
+      case OnboardingStep.INTRO:
+        return <OnboardingIntroStep />;
+      default:
+        return <OnboardingIntroStep />;
+    }
+  }, [onboardingStep]);
+
+  return (
+    <Modal
+      data-testid="rewards-onboarding-modal"
+      isOpen={isOpen}
+      onClose={handleClose}
+      className="rewards-onboarding-modal"
+    >
+      <ModalOverlay />
+      <ModalContent
+        alignItems={AlignItems.center}
+        justifyContent={JustifyContent.center}
+        size={ModalContentSize.Md}
+        modalDialogProps={{
+          paddingTop: 0,
+          paddingBottom: 0,
+          style: { height: '800px' },
+        }}
+      >
+        <ModalHeader
+          data-theme={ThemeType.dark}
+          closeButtonProps={{
+            className: 'absolute top-2 right-2',
+          }}
+          paddingBottom={0}
+          onClose={handleClose}
+        />
+
+        {renderContent()}
+      </ModalContent>
+    </Modal>
+  );
+}
