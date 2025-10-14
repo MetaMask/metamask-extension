7a8b1decab714b0bb6acc5d108dc1a333ee15656
Frederik Bolding <frederik.bolding@gmail.com>
2025-10-14 13:38:03 +0200

    Fill out JSDoc

--- shared/lib/sentry-release.js
+++ shared/lib/sentry-release.js
@@ -1,9 +1,8 @@
 /**
- *
- *
- * @param {string} environment
- * @param {string} version
- * @returns
+ * Get the Sentry release string from the current build environment and version.
+ * @param {string} environment - The build environment, preferably `METAMASK_ENVIRONMENT`.
+ * @param {string} version - The SemVer version string.
+ * @returns A combined string that fits the Sentry release name conventions.
  */
 export function getSentryRelease(environment, version) {
   const packageName =
