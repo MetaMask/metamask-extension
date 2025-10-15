9f017635127314d8a2bd2ab283b2908b24f396fd
Tuna <tunguyenanh@tunatech.org>
2025-10-15 14:11:24 +0700

    fix: dev profile sync token

--- app/scripts/controller-init/identity/authentication-controller-init.ts
+++ app/scripts/controller-init/identity/authentication-controller-init.ts
@@ -8,6 +8,22 @@ import {
   AuthenticationControllerInitMessenger,
   AuthenticationControllerMessenger,
 } from '../messengers/identity';
+import { ENVIRONMENT } from '../../../../development/build/constants';
+
+/**
+ * Check if the build is a Development or Test build.
+ *
+ * @returns true if the build is a Development or Test build, false otherwise
+ */
+function isDevOrTestBuild() {
+  return (
+    process.env.METAMASK_ENVIRONMENT === ENVIRONMENT.DEVELOPMENT ||
+    process.env.METAMASK_ENVIRONMENT === ENVIRONMENT.TESTING ||
+    // TODO: use dev for beta build, remove if outside of beta build
+    process.env.METAMASK_ENVIRONMENT === ENVIRONMENT.STAGING ||
+    process.env.METAMASK_ENVIRONMENT === ENVIRONMENT.OTHER
+  );
+}
 
 /**
  * Initialize the Authentication controller.
@@ -35,7 +51,7 @@ export const AuthenticationControllerInit: ControllerInitFunction<
       agent: Platform.EXTENSION,
     },
     config: {
-      env: Env.DEV,
+      env: isDevOrTestBuild() ? Env.DEV : Env.PRD,
     },
   });
 
