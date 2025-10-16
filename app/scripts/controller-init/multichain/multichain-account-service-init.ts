189c3122fa6b612c8dbf110335bf222fc0deea4d
Fabio Bozzo <fabio.bozzo@consensys.net>
2025-10-16 15:44:04 +0200

    Update app/scripts/controller-init/multichain/multichain-account-service-init.ts
    Co-authored-by: Charly Chevalier <charly.chevalier@consensys.net>

--- app/scripts/controller-init/multichain/multichain-account-service-init.ts
+++ app/scripts/controller-init/multichain/multichain-account-service-init.ts
@@ -46,7 +46,7 @@ export const MultichainAccountServiceInit: ControllerInitFunction<
       ///: BEGIN:ONLY_INCLUDE_IF(bitcoin)
       btcProvider,
       ///: END:ONLY_INCLUDE_IF
-    ].filter(Boolean),
+    ]
   });
 
   const preferencesState = initMessenger.call('PreferencesController:getState');
