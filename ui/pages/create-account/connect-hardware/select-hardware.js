de3419ae7a83eb3dbd72236191a4ae1ec1a32be1
Michele Balistreri <michele@bitgamma.com>
2025-10-16 12:28:05 +0900

    correct tracking event

--- ui/pages/create-account/connect-hardware/select-hardware.js
+++ ui/pages/create-account/connect-hardware/select-hardware.js
@@ -891,7 +891,7 @@ export default class SelectHardware extends Component {
               onClick={() => {
                 this.context.trackEvent({
                   category: MetaMetricsEventCategory.Navigation,
-                  event: 'Clicked Keycard Shell Learn More',
+                  event: 'Clicked Keycard Shell Buy Now',
                 });
                 openWindow(HardwareAffiliateLinks.kshell);
               }}
