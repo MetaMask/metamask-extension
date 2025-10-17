23d5d8e83233a11d44652059cf61e58cb9ae8501
Jiexi Luan <jiexiluan@gmail.com>
2025-10-17 12:09:16 -0700

    lint

--- app/scripts/lib/snap-keyring/metrics.test.ts
+++ app/scripts/lib/snap-keyring/metrics.test.ts
@@ -170,121 +170,120 @@ describe('getSnapAndHardwareInfoForMetrics', () => {
         account_snap_version: 'snapVersion',
       });
     });
+  });
+
+  describe('when the wallet is locked', () => {
+    it('does not resolve keyring and device info for a HD account', async () => {
+      messenger.call
+        .mockReturnValueOnce({
+          address: '0x123',
+          id: 'cf8dace4-9439-4bd4-b3a8-88c821c8fcb3',
+          metadata: {
+            name: 'Account 1',
+            keyring: {
+              type: 'HD Key Tree',
+            },
+          },
+          options: {},
+          methods: [
+            'personal_sign',
+            'eth_signTransaction',
+            'eth_signTypedData_v1',
+            'eth_signTypedData_v3',
+            'eth_signTypedData_v4',
+          ],
+          type: 'eip155:eoa',
+        })
+        .mockReturnValueOnce({ isUnlocked: false });
+
+      const result = await getSnapAndHardwareInfoForMetrics(
+        getAccountType,
+        getDeviceModel,
+        getHardwareTypeForMetric,
+        messenger,
+      );
+
+      expect(getAccountType).not.toHaveBeenCalled();
+      expect(getDeviceModel).not.toHaveBeenCalled();
+      expect(getHardwareTypeForMetric).not.toHaveBeenCalled();
+      expect(messenger.call).toHaveBeenNthCalledWith(
+        1,
+        'AccountsController:getSelectedAccount',
+      );
+      expect(messenger.call).toHaveBeenNthCalledWith(
+        2,
+        'KeyringController:getState',
+      );
+      expect(result).toEqual({
+        // TODO: Fix in https://github.com/MetaMask/metamask-extension/issues/31860
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        account_snap_type: undefined,
+        // TODO: Fix in https://github.com/MetaMask/metamask-extension/issues/31860
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        account_snap_version: undefined,
+      });
     });
 
-    describe('when the wallet is locked', () => {
-      it('does not resolve keyring and device info for a HD account', async () => {
-        messenger.call
-          .mockReturnValueOnce({
-            address: '0x123',
-            id: 'cf8dace4-9439-4bd4-b3a8-88c821c8fcb3',
-            metadata: {
-              name: 'Account 1',
-              keyring: {
-                type: 'HD Key Tree',
-              },
+    it('resolves only snap info for a snap account', async () => {
+      messenger.call
+        .mockReturnValueOnce({
+          address: '0x123',
+          id: 'cf8dace4-9439-4bd4-b3a8-88c821c8fcb3',
+          metadata: {
+            name: 'Account 1',
+            keyring: {
+              type: 'Snap Keyring',
             },
-            options: {},
-            methods: [
-              'personal_sign',
-              'eth_signTransaction',
-              'eth_signTypedData_v1',
-              'eth_signTypedData_v3',
-              'eth_signTypedData_v4',
-            ],
-            type: 'eip155:eoa',
-          })
-          .mockReturnValueOnce({ isUnlocked: false });
-
-        const result = await getSnapAndHardwareInfoForMetrics(
-          getAccountType,
-          getDeviceModel,
-          getHardwareTypeForMetric,
-          messenger,
-        );
-
-        expect(getAccountType).not.toHaveBeenCalled();
-        expect(getDeviceModel).not.toHaveBeenCalled();
-        expect(getHardwareTypeForMetric).not.toHaveBeenCalled();
-        expect(messenger.call).toHaveBeenNthCalledWith(
-          1,
-          'AccountsController:getSelectedAccount',
-        );
-        expect(messenger.call).toHaveBeenNthCalledWith(
-          2,
-          'KeyringController:getState',
-        );
-        expect(result).toEqual({
-          // TODO: Fix in https://github.com/MetaMask/metamask-extension/issues/31860
-          // eslint-disable-next-line @typescript-eslint/naming-convention
-          account_snap_type: undefined,
-          // TODO: Fix in https://github.com/MetaMask/metamask-extension/issues/31860
-          // eslint-disable-next-line @typescript-eslint/naming-convention
-          account_snap_version: undefined,
-        });
-      });
-
-      it('resolves only snap info for a snap account', async () => {
-        messenger.call
-          .mockReturnValueOnce({
-            address: '0x123',
-            id: 'cf8dace4-9439-4bd4-b3a8-88c821c8fcb3',
-            metadata: {
-              name: 'Account 1',
-              keyring: {
-                type: 'Snap Keyring',
-              },
-              snap: {
-                id: 'snapId',
-                name: 'mock-name',
-                enabled: true,
-              },
+            snap: {
+              id: 'snapId',
+              name: 'mock-name',
+              enabled: true,
             },
-            options: {},
-            methods: [
-              'personal_sign',
-              'eth_signTransaction',
-              'eth_signTypedData_v1',
-              'eth_signTypedData_v3',
-              'eth_signTypedData_v4',
-            ],
-            type: 'eip155:eoa',
-          })
-          .mockReturnValueOnce({ id: 'snapId', version: 'snapVersion' })
-          .mockReturnValueOnce({ isUnlocked: false });
+          },
+          options: {},
+          methods: [
+            'personal_sign',
+            'eth_signTransaction',
+            'eth_signTypedData_v1',
+            'eth_signTypedData_v3',
+            'eth_signTypedData_v4',
+          ],
+          type: 'eip155:eoa',
+        })
+        .mockReturnValueOnce({ id: 'snapId', version: 'snapVersion' })
+        .mockReturnValueOnce({ isUnlocked: false });
 
-        const result = await getSnapAndHardwareInfoForMetrics(
-          getAccountType,
-          getDeviceModel,
-          getHardwareTypeForMetric,
-          messenger,
-        );
+      const result = await getSnapAndHardwareInfoForMetrics(
+        getAccountType,
+        getDeviceModel,
+        getHardwareTypeForMetric,
+        messenger,
+      );
 
-        expect(getAccountType).not.toHaveBeenCalled();
-        expect(getDeviceModel).not.toHaveBeenCalled();
-        expect(getHardwareTypeForMetric).not.toHaveBeenCalled();
-        expect(messenger.call).toHaveBeenNthCalledWith(
-          1,
-          'AccountsController:getSelectedAccount',
-        );
-        expect(messenger.call).toHaveBeenNthCalledWith(
-          2,
-          'SnapController:get',
-          'snapId',
-        );
-        expect(messenger.call).toHaveBeenNthCalledWith(
-          3,
-          'KeyringController:getState',
-        );
-        expect(result).toEqual({
-          // TODO: Fix in https://github.com/MetaMask/metamask-extension/issues/31860
-          // eslint-disable-next-line @typescript-eslint/naming-convention
-          account_snap_type: 'snapId',
-          // TODO: Fix in https://github.com/MetaMask/metamask-extension/issues/31860
-          // eslint-disable-next-line @typescript-eslint/naming-convention
-          account_snap_version: 'snapVersion',
-        });
+      expect(getAccountType).not.toHaveBeenCalled();
+      expect(getDeviceModel).not.toHaveBeenCalled();
+      expect(getHardwareTypeForMetric).not.toHaveBeenCalled();
+      expect(messenger.call).toHaveBeenNthCalledWith(
+        1,
+        'AccountsController:getSelectedAccount',
+      );
+      expect(messenger.call).toHaveBeenNthCalledWith(
+        2,
+        'SnapController:get',
+        'snapId',
+      );
+      expect(messenger.call).toHaveBeenNthCalledWith(
+        3,
+        'KeyringController:getState',
+      );
+      expect(result).toEqual({
+        // TODO: Fix in https://github.com/MetaMask/metamask-extension/issues/31860
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        account_snap_type: 'snapId',
+        // TODO: Fix in https://github.com/MetaMask/metamask-extension/issues/31860
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        account_snap_version: 'snapVersion',
       });
-      });
-
+    });
+  });
 });
