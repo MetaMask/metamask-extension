10cd5c53f77f4616e3d419f1f125c80ff832c662
David Murdoch <david@davidmurdoch.com>
2025-10-22 16:45:46 -0400

    fix typo

--- app/scripts/app-init.js
+++ app/scripts/app-init.js
@@ -18,8 +18,8 @@ const listener = new ExtensionLazyListener(chrome, {
  */
 globalThis.stateHooks = globalThis.stateHooks || {};
 
-// Set the lazy listener on globalThis.stateHooks so that other
-// other _bundles_ can access it.
+// Set the lazy listener on globalThis.stateHooks so that other _bundles_ can
+// access it.
 globalThis.stateHooks.lazyListener = listener;
 
 // Represents if importAllScripts has been run
