c6228a954075debe6cee00bcf75cb4295c4fd1ab
Francis Nepomuceno <n3ps@users.noreply.github.com>
2025-10-23 15:13:46 -0400

    cleanup

--- ui/pages/confirmations/hooks/useAddEthereumChain.ts
+++ ui/pages/confirmations/hooks/useAddEthereumChain.ts
@@ -1,14 +1,9 @@
 import { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
-import { RpcEndpointType } from '@metamask/network-controller';
 import { ApprovalType } from '@metamask/controller-utils';
 import type { TransactionMeta } from '@metamask/transaction-controller';
 import { useConfirmContext } from '../context/confirm';
-import {
-  addNetwork,
-  resolvePendingApproval,
-  setNewNetworkAdded,
-} from '../../../store/actions';
+import { resolvePendingApproval } from '../../../store/actions';
 import type { MetaMaskReduxDispatch } from '../../../store/store';
 import type { AddEthereumChainContext } from '../external/add-ethereum-chain/types';
 
@@ -20,37 +15,6 @@ export const useAddEthereumChain = () => {
 
   const onSubmit = useCallback(async () => {
     await dispatch(resolvePendingApproval(id, requestData));
-
-    const originIsMetaMask = origin === 'metamask';
-
-    if (originIsMetaMask) {
-      const blockExplorer = requestData.rpcPrefs.blockExplorerUrl;
-
-      const addedNetwork = await dispatch(
-        addNetwork({
-          chainId: requestData.chainId,
-          name: requestData.chainName,
-          nativeCurrency: requestData.ticker,
-          blockExplorerUrls: blockExplorer ? [blockExplorer] : [],
-          defaultBlockExplorerUrlIndex: blockExplorer ? 0 : undefined,
-          defaultRpcEndpointIndex: 0,
-          rpcEndpoints: [
-            {
-              url: requestData.rpcUrl,
-              failoverUrls: requestData.failoverRpcUrls,
-              type: RpcEndpointType.Custom,
-            },
-          ],
-        }),
-      );
-
-      await dispatch(
-        setNewNetworkAdded({
-          networkConfigurationId: addedNetwork.rpcEndpoints[0].networkClientId,
-          nickname: requestData.chainName,
-        }),
-      );
-    }
   }, [id, origin, requestData, dispatch]);
 
   return {
