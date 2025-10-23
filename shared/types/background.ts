3f30e8fa68db49314fa66ee370aafbd91b7d4c46
ameliejyc <amelie.chan@gmail.com>
2025-10-23 11:49:40 +0100

    chore: fix lint issues

--- shared/types/background.ts
+++ shared/types/background.ts
@@ -121,6 +121,7 @@ export type ControllerStatePropertiesEnumerated = {
   // TODO: Fix in https://github.com/MetaMask/metamask-extension/issues/31860
   // eslint-disable-next-line @typescript-eslint/naming-convention
   hadAdvancedGasFeesSetPriorToMigration92_3: AppStateControllerState['hadAdvancedGasFeesSetPriorToMigration92_3'];
+  hasFunds: AppStateControllerState['hasFunds'];
   activeQrCodeScanRequest: AppStateControllerState['activeQrCodeScanRequest'];
   nftsDropdownState: AppStateControllerState['nftsDropdownState'];
   surveyLinkLastClickedOrClosed: AppStateControllerState['surveyLinkLastClickedOrClosed'];
