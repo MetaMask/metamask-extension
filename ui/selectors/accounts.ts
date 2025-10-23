af1afb8d9bc50833303fcecfa4e60fc9f7af0eab
Jongsun Suh <jongsun.suh@icloud.com>
2025-10-23 14:50:59 -0400

    Fix identity-function output selectors that do not provide memoization
    See: https://redux.js.org/usage/deriving-data-selectors#createselector-overview

--- ui/selectors/accounts.ts
+++ ui/selectors/accounts.ts
@@ -44,15 +44,13 @@ export function isNonEvmAccount(account: InternalAccount) {
 }
 
 export const getInternalAccounts = createSelector(
-  (state: AccountsState) =>
-    Object.values(state.metamask.internalAccounts.accounts),
-  (accounts) => accounts,
+  getInternalAccountsObject,
+  (accounts) => Object.values(accounts),
 );
 
-export const getInternalAccountsObject = createSelector(
-  (state: AccountsState) => state.metamask.internalAccounts.accounts,
-  (internalAccounts) => internalAccounts,
-);
+export function getInternalAccountsObject(state: AccountsState) {
+  return state.metamask.internalAccounts.accounts;
+}
 
 export const getMemoizedInternalAccountByAddress = createDeepEqualSelector(
   [getInternalAccounts, (_state, address) => address],
