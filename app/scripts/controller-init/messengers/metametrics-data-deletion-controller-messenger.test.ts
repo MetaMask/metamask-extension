cd5f040dc2ce564cd2846ac88f4b715fe69b8b79
Michele Esposito <michele@esposito.codes>
2025-10-24 14:45:19 +0200

    fix `metametrics-data-deletion-controller-messenger.test.ts`

--- app/scripts/controller-init/messengers/metametrics-data-deletion-controller-messenger.test.ts
+++ app/scripts/controller-init/messengers/metametrics-data-deletion-controller-messenger.test.ts
@@ -1,6 +1,6 @@
 import { Messenger } from '@metamask/messenger';
-import { getMetaMetricsDataDeletionControllerMessenger } from './metametrics-data-deletion-controller-messenger';
 import { getRootMessenger } from '../../lib/messenger';
+import { getMetaMetricsDataDeletionControllerMessenger } from './metametrics-data-deletion-controller-messenger';
 
 describe('getMetaMetricsDataDeletionControllerMessenger', () => {
   it('returns a restricted messenger', () => {
