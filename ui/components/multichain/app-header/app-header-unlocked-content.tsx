026b41738b3e17bc3068647ea26fdaf25ea3cd49
Fabio Bozzo <fabio.bozzo@consensys.net>
2025-10-29 14:49:55 +0100

    prevent navigation to invalid routes in case selectedMultichainAccountId is null

--- ui/components/multichain/app-header/app-header-unlocked-content.tsx
+++ ui/components/multichain/app-header/app-header-unlocked-content.tsx
@@ -256,31 +256,33 @@ export const AppHeaderUnlockedContent = ({
           />
           <>{!isMultichainAccountsState2Enabled && CopyButton}</>
         </Text>
-        <Link
-          to={`${MULTICHAIN_ACCOUNT_ADDRESS_LIST_PAGE_ROUTE}/${encodeURIComponent(selectedMultichainAccountId)}`}
-          data-testid="networks-subtitle-test-id"
-        >
-          <Box
-            display={Display.Flex}
-            className="networks-subtitle"
-            alignItems={AlignItems.center}
-            gap={1}
-            paddingInline={2}
+        {selectedMultichainAccountId && (
+          <Link
+            to={`${MULTICHAIN_ACCOUNT_ADDRESS_LIST_PAGE_ROUTE}/${encodeURIComponent(selectedMultichainAccountId)}`}
+            data-testid="networks-subtitle-test-id"
           >
-            <Text
-              color={TextColor.textAlternative}
-              variant={TextVariant.bodyXsMedium}
+            <Box
+              display={Display.Flex}
+              className="networks-subtitle"
+              alignItems={AlignItems.center}
+              gap={1}
+              paddingInline={2}
             >
-              {networksLabel}
-            </Text>
-            <Icon
-              name={IconNameDesignSystem.Copy}
-              size={IconSizeDesignSystem.Xs}
-              color={IconColorDesignSystem.IconAlternative}
-              data-testid="copy-network-addresses-icon"
-            />
-          </Box>
-        </Link>
+              <Text
+                color={TextColor.textAlternative}
+                variant={TextVariant.bodyXsMedium}
+              >
+                {networksLabel}
+              </Text>
+              <Icon
+                name={IconNameDesignSystem.Copy}
+                size={IconSizeDesignSystem.Xs}
+                color={IconColorDesignSystem.IconAlternative}
+                data-testid="copy-network-addresses-icon"
+              />
+            </Box>
+          </Link>
+        )}
       </Box>
     );
   }, [
