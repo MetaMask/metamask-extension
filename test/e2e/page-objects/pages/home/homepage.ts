700b72a097b83f8badb780aa8bee18dceb9fb7a8
Davide Brocchetto <davide.brocchetto@consensys.net>
2025-10-31 20:53:07 -0700

    Fixed transactions

--- test/e2e/page-objects/pages/home/homepage.ts
+++ test/e2e/page-objects/pages/home/homepage.ts
@@ -430,7 +430,7 @@ class HomePage {
   ): Promise<void> {
     let expectedBalance: string;
     if (localNode) {
-      expectedBalance = (await localNode.getBalance(address)).toString();
+      expectedBalance = (await localNode.getBalance(address)).toFixed(3);
     } else {
       expectedBalance = '25';
     }
