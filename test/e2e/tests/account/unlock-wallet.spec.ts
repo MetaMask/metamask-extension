53b04d5eb9da04bc35f6c14db912bf6eaed081d0
seaona <mariona@gmx.es>
2025-11-03 09:33:59 +0100

    fgix

--- test/e2e/tests/account/unlock-wallet.spec.ts
+++ test/e2e/tests/account/unlock-wallet.spec.ts
@@ -21,7 +21,10 @@ describe('Unlock wallet - ', function () {
       {
         fixtures: new FixtureBuilder().build(),
         title: this.test?.fullTitle(),
-        ignoredConsoleErrors: ['unable to proceed, wallet is locked'],
+        ignoredConsoleErrors: [
+          'unable to proceed, wallet is locked',
+          'The snap "npm:@metamask/message-signing-snap" has been terminated during execution', // issue #37342
+        ],
       },
       async ({
         driver,
