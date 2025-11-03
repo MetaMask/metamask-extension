4fa1f0413645a296e2187497424aa53e4f50b075
lwin <lwin.kyaw@consensys.net>
2025-11-04 00:28:27 +0800

    try removing ignoredError in reset-wallet.spec

--- test/e2e/tests/reset-wallet/reset-wallet.spec.ts
+++ test/e2e/tests/reset-wallet/reset-wallet.spec.ts
@@ -28,7 +28,6 @@ describe('Reset Wallet - ', function () {
         ignoredConsoleErrors: [
           'unable to proceed, wallet is locked',
           'The snap "npm:@metamask/message-signing-snap" has been terminated during execution', // issue #37342
-          'npm:@metamask/message-signing-snap was stopped and the request was cancelled. This is likely because the Snap crashed.',
         ],
         testSpecificMock: (server: Mockttp) => {
           // using this to mock the OAuth Service (Web Authentication flow + Auth server)
@@ -100,7 +99,6 @@ describe('Reset Wallet - ', function () {
         ignoredConsoleErrors: [
           'unable to proceed, wallet is locked',
           'The snap "npm:@metamask/message-signing-snap" has been terminated during execution', // issue #37342
-          'npm:@metamask/message-signing-snap was stopped and the request was cancelled. This is likely because the Snap crashed.',
         ],
         title: this.test?.fullTitle(),
       },
@@ -180,7 +178,6 @@ describe('Reset Wallet - ', function () {
         ignoredConsoleErrors: [
           'unable to proceed, wallet is locked',
           'The snap "npm:@metamask/message-signing-snap" has been terminated during execution', // issue #37342
-          'npm:@metamask/message-signing-snap was stopped and the request was cancelled. This is likely because the Snap crashed.',
         ],
       },
       async ({ driver }: { driver: Driver }) => {
