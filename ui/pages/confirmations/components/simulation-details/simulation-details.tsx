d440ce03147508c96ec2ae6172e83789fa93311b
Kevin Le Jeune <kevin.le-jeune@consensys.net>
2025-11-04 15:50:56 +0100

    fix: simulation details padding

--- ui/pages/confirmations/components/simulation-details/simulation-details.tsx
+++ ui/pages/confirmations/components/simulation-details/simulation-details.tsx
@@ -279,7 +279,7 @@ const SimulationDetailsLayout: React.FC<{
             : BorderColor.borderDefault
         }
         paddingInline={3}
-        paddingTop={1}
+        paddingTop={2}
         paddingBottom={2}
         margin={isTransactionsRedesign ? null : 4}
         gap={3}
@@ -306,7 +306,7 @@ const SimulationDetailsLayout: React.FC<{
           : BorderColor.borderDefault
       }
       paddingInline={3}
-      paddingTop={1}
+      paddingTop={2}
       paddingBottom={2}
       margin={isTransactionsRedesign ? null : 4}
       gap={3}
