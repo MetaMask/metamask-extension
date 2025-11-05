81e64cd3b38749363c947587d3de6d72f51d3ff3
Goktug Poyraz <omergoktugpoyraz@gmail.com>
2025-11-05 09:40:31 +0100

    Update tests

--- ui/pages/confirmations/components/confirm/info/hooks/useTokenDetails.ts
+++ ui/pages/confirmations/components/confirm/info/hooks/useTokenDetails.ts
@@ -1,20 +1,32 @@
 import { TransactionMeta } from '@metamask/transaction-controller';
 import { useSelector } from 'react-redux';
 import { useI18nContext } from '../../../../../../hooks/useI18nContext';
-import { selectERC20TokensByChain } from '../../../../../../selectors';
+import {
+  getAllTokens,
+  selectERC20TokensByChain,
+} from '../../../../../../selectors';
+import { Token } from '../../../../../../components/app/assets/types';
 
 export const useTokenDetails = (transactionMeta: TransactionMeta) => {
   const t = useI18nContext();
   const {
     chainId,
-    txParams: { to },
+    txParams: { to, from },
   } = transactionMeta ?? { txParams: {} };
-  const tokenListByChain = useSelector(selectERC20TokensByChain);
-  const { iconUrl: tokenImage, symbol: tokenSymbol } = tokenListByChain[chainId]
-    ?.data?.[to?.toLowerCase() as string] ?? {
-    iconUrl: undefined,
-    symbol: t('unknown'),
-  };
+  const erc20TokensByChain = useSelector(selectERC20TokensByChain);
+  const allTokens = useSelector(getAllTokens);
+
+  const erc20Token =
+    erc20TokensByChain[chainId]?.data?.[to?.toLowerCase() as string];
+  const tokenListToken = allTokens?.[chainId]?.[from as string].find(
+    (token: Token) =>
+      token.address?.toLowerCase() === (to?.toLowerCase() as string),
+  );
+
+  const tokenImage =
+    erc20Token?.iconUrl || tokenListToken?.iconUrl || undefined;
+  const tokenSymbol =
+    erc20Token?.symbol || tokenListToken?.symbol || t('unknown');
 
   return { tokenImage, tokenSymbol };
 };
