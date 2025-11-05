a771b4a0a225ab4c9aa730b7f7268a9d0a03e617
Michele Esposito <michele@esposito.codes>
2025-11-05 11:04:10 +0100

    remove unnecessary return type from encryptor factory

--- app/scripts/lib/encryptor-factory.ts
+++ app/scripts/lib/encryptor-factory.ts
@@ -13,7 +13,6 @@ import {
   EncryptionKey,
   KeyDerivationOptions,
 } from '@metamask/browser-passworder';
-import type { ExportableKeyEncryptor } from '@metamask/keyring-controller';
 
 /**
  * A factory function for the encrypt method of the browser-passworder library,
@@ -108,9 +107,7 @@ const isVaultUpdatedFactory = (iterations: number) => (vault: string) =>
  * @param iterations - The number of iterations to use for the PBKDF2 algorithm.
  * @returns An encryptor set with the given number of iterations.
  */
-export const encryptorFactory = (
-  iterations: number,
-): ExportableKeyEncryptor<EncryptionKey | CryptoKey, KeyDerivationOptions> => ({
+export const encryptorFactory = (iterations: number) => ({
   encrypt: encryptFactory(iterations),
   encryptWithKey,
   encryptWithDetail: encryptWithDetailFactory(iterations),
