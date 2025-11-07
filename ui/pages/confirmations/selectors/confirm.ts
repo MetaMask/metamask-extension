e761117025824fa23c7b4dbf017104341366a265
dddddanica <zhaodanica@gmail.com>
2025-11-07 07:43:16 +0000

    refactor(5564): try to fix e2e tests

--- ui/pages/confirmations/selectors/confirm.ts
+++ ui/pages/confirmations/selectors/confirm.ts
@@ -5,25 +5,10 @@ import { getPendingApprovals } from '../../../selectors/approvals';
 import { createDeepEqualSelector } from '../../../../shared/modules/selectors/util';
 import { ConfirmMetamaskState } from '../types/confirm';
 
-// Include all confirmation types that should be navigable.
-// This list was incomplete before v5-compat migration, causing navigation to skip certain types.
 const ConfirmationApprovalTypes = [
-  // Signature types
   ApprovalType.PersonalSign,
   ApprovalType.EthSignTypedData,
-  // Transaction type
   ApprovalType.Transaction,
-  // Network management types
-  ApprovalType.SwitchEthereumChain,
-  ApprovalType.AddEthereumChain,
-  // Snap dialog types
-  ApprovalType.SnapDialogAlert,
-  ApprovalType.SnapDialogConfirmation,
-  ApprovalType.SnapDialogPrompt,
-  ApprovalType.SnapDialogDefault,
-  // Result types
-  ApprovalType.ResultSuccess,
-  ApprovalType.ResultError,
 ];
 
 export function pendingConfirmationsSelector(state: ConfirmMetamaskState) {
