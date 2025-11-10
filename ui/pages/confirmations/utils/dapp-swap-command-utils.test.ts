170a26a52be6deb372c54fd6cae73f146d3c2c85
Jyoti Puri <jyotipuri@gmail.com>
2025-11-10 15:58:21 +0530

    Fix metamask quote fee percentage value

--- ui/pages/confirmations/utils/dapp-swap-command-utils.test.ts
+++ ui/pages/confirmations/utils/dapp-swap-command-utils.test.ts
@@ -16,6 +16,7 @@ describe('dapp-swap command utils', () => {
         quotesInput: {
           destChainId: '0x2105',
           destTokenAddress: '0xfde4c96c8593536e31f229ea8f37b2ada2699bb2',
+          fee: 250,
           gasIncluded: false,
           gasIncluded7702: false,
           srcChainId: '0x2105',
@@ -55,6 +56,7 @@ describe('dapp-swap command utils', () => {
         quotesInput: {
           destChainId: '0x2105',
           destTokenAddress: '0xfde4c96c8593536e31f229ea8f37b2ada2699bb2',
+          fee: 250,
           gasIncluded: false,
           gasIncluded7702: false,
           srcChainId: '0x2105',
@@ -96,6 +98,7 @@ describe('dapp-swap command utils', () => {
         quotesInput: {
           destChainId: '0x2105',
           destTokenAddress: '0x0000000000000000000000000000000000000000',
+          fee: 250,
           gasIncluded: false,
           gasIncluded7702: false,
           srcChainId: '0x2105',
@@ -139,6 +142,7 @@ describe('dapp-swap command utils', () => {
         quotesInput: {
           destChainId: '0x2105',
           destTokenAddress: '0x0000000000000000000000000000000000000000',
+          fee: 250,
           gasIncluded: false,
           gasIncluded7702: false,
           srcChainId: '0x2105',
@@ -181,6 +185,7 @@ describe('dapp-swap command utils', () => {
         quotesInput: {
           destChainId: '0x2105',
           destTokenAddress: '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913',
+          fee: 250,
           gasIncluded: false,
           gasIncluded7702: false,
           srcChainId: '0x2105',
@@ -225,6 +230,7 @@ describe('dapp-swap command utils', () => {
         quotesInput: {
           destChainId: '0x2105',
           destTokenAddress: '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913',
+          fee: 250,
           gasIncluded: false,
           gasIncluded7702: false,
           srcChainId: '0x2105',
