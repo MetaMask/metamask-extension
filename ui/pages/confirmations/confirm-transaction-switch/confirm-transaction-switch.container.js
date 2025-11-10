426ccc942932cb7ae3183d1071583bcf7646fbc2
dddddanica <zhaodanica@gmail.com>
2025-11-10 20:25:56 +0000

    refactor: refactor extractIdFromPathname and add unit tests

--- ui/pages/confirmations/confirm-transaction-switch/confirm-transaction-switch.container.js
+++ ui/pages/confirmations/confirm-transaction-switch/confirm-transaction-switch.container.js
@@ -6,6 +6,7 @@ import {
 } from '../../../selectors';
 import { CONFIRM_TRANSACTION_ROUTE } from '../../../helpers/constants/routes';
 import withRouterHooks from '../../../helpers/higher-order-components/with-router-hooks/with-router-hooks';
+import { extractIdFromPathname } from '../../routes/utils';
 import ConfirmTransactionSwitch from './confirm-transaction-switch.component';
 
 const mapStateToProps = (state, ownProps) => {
@@ -13,10 +14,7 @@ const mapStateToProps = (state, ownProps) => {
   const { pathname = '' } = location;
 
   const confirmTransactionRoute = `${CONFIRM_TRANSACTION_ROUTE}/`;
-  // v5-compat: Extract ID from pathname, split by [/?#] to handle query params/hashes (e.g., /confirm-transaction/123?query=foo → "123")
-  const urlId = pathname.includes(confirmTransactionRoute)
-    ? pathname.split(confirmTransactionRoute)[1]?.split(/[/?#]/u)[0]
-    : null;
+  const urlId = extractIdFromPathname(pathname, confirmTransactionRoute);
 
   const transactionId = params.id || urlId;
 
--- ui/pages/confirmations/utils/index.ts
+++ ui/pages/confirmations/utils/index.ts
@@ -1,1 +1,2 @@
 export * from './confirm';
+export * from './url';
