19e44cd4161e770298aeb49d189ab149a39da25a
ghgoodreau <hunter.goodreau@consensys.net>
2025-11-10 16:05:06 -0600

    undo network.ts change

--- ui/pages/confirmations/utils/network.ts
+++ ui/pages/confirmations/utils/network.ts
@@ -1,6 +1,5 @@
 import { CaipChainId, isCaipChainId, Hex } from '@metamask/utils';
 import { TrxScope } from '@metamask/keyring-api';
-import { ChainId } from '@metamask/bridge-controller';
 import { CHAIN_ID_TO_NETWORK_IMAGE_URL_MAP } from '../../../../shared/constants/network';
 import { MULTICHAIN_TOKEN_IMAGE_MAP } from '../../../../shared/constants/multichain/networks';
 
@@ -11,10 +10,11 @@ export function getImageForChainId(chainId: string): string | undefined {
   }[chainId];
 }
 
-// TODO: Import isTronChainId from @metamask/bridge-controller once it's exported from the main entry point
 export const isTronChainId = (chainId: Hex | number | CaipChainId | string) => {
-  if (isCaipChainId(chainId)) {
-    return chainId === TrxScope.Mainnet.toString();
-  }
-  return chainId.toString() === ChainId.TRON.toString();
+  return (
+    isCaipChainId(chainId) &&
+    [`${TrxScope.Mainnet}`, `${TrxScope.Nile}`, `${TrxScope.Shasta}`].includes(
+      chainId,
+    )
+  );
 };
