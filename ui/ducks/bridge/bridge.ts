1acae09f8a439a41da2b7c59d17daf1c1f070b33
Micaela Estabillo <micaela.estabillo@consensys.net>
2025-11-12 14:03:54 -0800

    chore: rm address comparison

--- ui/ducks/bridge/bridge.ts
+++ ui/ducks/bridge/bridge.ts
@@ -108,11 +108,10 @@ const bridgeSlice = createSlice({
       state.fromTokenInputValue = null;
       // Unset toToken if it's the same as the fromToken
       if (
-        state.fromToken?.address === state.toToken?.address ||
-        (state.fromToken?.assetId &&
-          state.toToken?.assetId &&
-          state.fromToken.assetId?.toLowerCase() ===
-            state.toToken.assetId?.toLowerCase())
+        state.fromToken?.assetId &&
+        state.toToken?.assetId &&
+        state.fromToken.assetId.toLowerCase() ===
+          state.toToken.assetId.toLowerCase()
       ) {
         state.toToken = null;
       }
