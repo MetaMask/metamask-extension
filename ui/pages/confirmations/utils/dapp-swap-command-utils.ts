b2e4a09763498da237ed823dbe5c1b229193de8a
Jyoti Puri <jyotipuri@gmail.com>
2025-11-12 15:11:06 +0530

    update

--- ui/pages/confirmations/utils/dapp-swap-command-utils.ts
+++ ui/pages/confirmations/utils/dapp-swap-command-utils.ts
@@ -157,7 +157,6 @@ export function getCommandValues(
     destChainId: chainId,
     gasIncluded: false,
     gasIncluded7702: false,
-    fee: 250,
   } as GenericQuoteRequest;
 
   if (commandBytes.includes(V3_SWAP_EXACT_OUT)) {
