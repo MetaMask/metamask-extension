71f1f7f87876aa91c498e0bf923f9948df81f11e
Baptiste Marchand <75846779+baptiste-marchand@users.noreply.github.com>
2025-11-12 16:09:13 +0100

    [WIP] test: add Bitcoin Wallet Standard E2E tests

--- test/e2e/flask/btc/btc-create-account.spec.ts
+++ test/e2e/flask/btc/btc-create-account.spec.ts
@@ -13,142 +13,157 @@ import { withBtcAccountSnap } from './common-btc';
 
 describe('Create BTC Account', function (this: Suite) {
   it('create BTC account from the menu', async function () {
-    await withBtcAccountSnap(async (driver) => {
-      const headerNavbar = new HeaderNavbar(driver);
-      await headerNavbar.checkPageIsLoaded();
-      await headerNavbar.checkAccountLabel(DEFAULT_BTC_ACCOUNT_NAME);
-    }, this.test?.fullTitle());
+    await withBtcAccountSnap(
+      { title: this.test?.fullTitle() },
+      async (driver) => {
+        const headerNavbar = new HeaderNavbar(driver);
+        await headerNavbar.checkPageIsLoaded();
+        await headerNavbar.checkAccountLabel(DEFAULT_BTC_ACCOUNT_NAME);
+      },
+    );
   });
 
   it('can create multiple BTC accounts', async function () {
-    await withBtcAccountSnap(async (driver) => {
-      // check that we have one BTC account
-      const headerNavbar = new HeaderNavbar(driver);
-      await headerNavbar.checkPageIsLoaded();
-      await headerNavbar.checkAccountLabel(DEFAULT_BTC_ACCOUNT_NAME);
+    await withBtcAccountSnap(
+      { title: this.test?.fullTitle() },
+      async (driver) => {
+        // check that we have one BTC account
+        const headerNavbar = new HeaderNavbar(driver);
+        await headerNavbar.checkPageIsLoaded();
+        await headerNavbar.checkAccountLabel(DEFAULT_BTC_ACCOUNT_NAME);
 
-      // check user cannot create second BTC account
-      await headerNavbar.openAccountMenu();
-      const accountListPage = new AccountListPage(driver);
-      await accountListPage.checkPageIsLoaded();
-      await accountListPage.checkNumberOfAvailableAccounts(2);
-      await accountListPage.openAddAccountModal();
-      assert.equal(
-        await accountListPage.isBtcAccountCreationButtonEnabled(),
-        true,
-      );
-    }, this.test?.fullTitle());
+        // check user cannot create second BTC account
+        await headerNavbar.openAccountMenu();
+        const accountListPage = new AccountListPage(driver);
+        await accountListPage.checkPageIsLoaded();
+        await accountListPage.checkNumberOfAvailableAccounts(2);
+        await accountListPage.openAddAccountModal();
+        assert.equal(
+          await accountListPage.isBtcAccountCreationButtonEnabled(),
+          true,
+        );
+      },
+    );
   });
 
   it('can cancel the removal of BTC account', async function () {
-    await withBtcAccountSnap(async (driver) => {
-      // check that we have one BTC account
-      const headerNavbar = new HeaderNavbar(driver);
-      await headerNavbar.checkPageIsLoaded();
-      await headerNavbar.checkAccountLabel(DEFAULT_BTC_ACCOUNT_NAME);
+    await withBtcAccountSnap(
+      { title: this.test?.fullTitle() },
+      async (driver) => {
+        // check that we have one BTC account
+        const headerNavbar = new HeaderNavbar(driver);
+        await headerNavbar.checkPageIsLoaded();
+        await headerNavbar.checkAccountLabel(DEFAULT_BTC_ACCOUNT_NAME);
 
-      // check user can cancel the removal of the BTC account
-      await headerNavbar.openAccountMenu();
-      const accountListPage = new AccountListPage(driver);
-      await accountListPage.checkPageIsLoaded();
-      await accountListPage.removeAccount(DEFAULT_BTC_ACCOUNT_NAME, false);
-      await headerNavbar.checkAccountLabel(DEFAULT_BTC_ACCOUNT_NAME);
+        // check user can cancel the removal of the BTC account
+        await headerNavbar.openAccountMenu();
+        const accountListPage = new AccountListPage(driver);
+        await accountListPage.checkPageIsLoaded();
+        await accountListPage.removeAccount(DEFAULT_BTC_ACCOUNT_NAME, false);
+        await headerNavbar.checkAccountLabel(DEFAULT_BTC_ACCOUNT_NAME);
 
-      // check the number of accounts. it should be 2.
-      await headerNavbar.openAccountMenu();
-      await accountListPage.checkPageIsLoaded();
-      await accountListPage.checkNumberOfAvailableAccounts(2);
-    }, this.test?.fullTitle());
+        // check the number of accounts. it should be 2.
+        await headerNavbar.openAccountMenu();
+        await accountListPage.checkPageIsLoaded();
+        await accountListPage.checkNumberOfAvailableAccounts(2);
+      },
+    );
   });
 
   it('can recreate BTC account after deleting it', async function () {
-    await withBtcAccountSnap(async (driver) => {
-      // check that we have one BTC account
-      const headerNavbar = new HeaderNavbar(driver);
-      await headerNavbar.checkPageIsLoaded();
-      await headerNavbar.checkAccountLabel(DEFAULT_BTC_ACCOUNT_NAME);
+    await withBtcAccountSnap(
+      { title: this.test?.fullTitle() },
+      async (driver) => {
+        // check that we have one BTC account
+        const headerNavbar = new HeaderNavbar(driver);
+        await headerNavbar.checkPageIsLoaded();
+        await headerNavbar.checkAccountLabel(DEFAULT_BTC_ACCOUNT_NAME);
 
-      // get the address of the BTC account and remove it
-      await headerNavbar.openAccountMenu();
-      const accountListPage = new AccountListPage(driver);
-      await accountListPage.checkPageIsLoaded();
-      await accountListPage.openAccountDetailsModal(DEFAULT_BTC_ACCOUNT_NAME);
+        // get the address of the BTC account and remove it
+        await headerNavbar.openAccountMenu();
+        const accountListPage = new AccountListPage(driver);
+        await accountListPage.checkPageIsLoaded();
+        await accountListPage.openAccountDetailsModal(DEFAULT_BTC_ACCOUNT_NAME);
 
-      const accountDetailsModal = new AccountDetailsModal(driver);
-      await accountDetailsModal.checkPageIsLoaded();
-      const accountAddress = await accountDetailsModal.getAccountAddress();
-      await headerNavbar.openAccountMenu();
-      await accountListPage.removeAccount(DEFAULT_BTC_ACCOUNT_NAME);
+        const accountDetailsModal = new AccountDetailsModal(driver);
+        await accountDetailsModal.checkPageIsLoaded();
+        const accountAddress = await accountDetailsModal.getAccountAddress();
+        await headerNavbar.openAccountMenu();
+        await accountListPage.removeAccount(DEFAULT_BTC_ACCOUNT_NAME);
 
-      // Recreate account and check that the address is the same
-      await headerNavbar.openAccountMenu();
-      await accountListPage.openAddAccountModal();
-      assert.equal(
-        await accountListPage.isBtcAccountCreationButtonEnabled(),
-        true,
-      );
-      await accountListPage.closeAccountModal();
-      await headerNavbar.openAccountMenu();
-      await accountListPage.addAccount({ accountType: ACCOUNT_TYPE.Bitcoin });
-      await headerNavbar.checkAccountLabel(DEFAULT_BTC_ACCOUNT_NAME);
+        // Recreate account and check that the address is the same
+        await headerNavbar.openAccountMenu();
+        await accountListPage.openAddAccountModal();
+        assert.equal(
+          await accountListPage.isBtcAccountCreationButtonEnabled(),
+          true,
+        );
+        await accountListPage.closeAccountModal();
+        await headerNavbar.openAccountMenu();
+        await accountListPage.addAccount({ accountType: ACCOUNT_TYPE.Bitcoin });
+        await headerNavbar.checkAccountLabel(DEFAULT_BTC_ACCOUNT_NAME);
 
-      await headerNavbar.openAccountMenu();
-      await accountListPage.checkPageIsLoaded();
-      await accountListPage.openAccountDetailsModal(DEFAULT_BTC_ACCOUNT_NAME);
-      await accountDetailsModal.checkPageIsLoaded();
-      const recreatedAccountAddress =
-        await accountDetailsModal.getAccountAddress();
+        await headerNavbar.openAccountMenu();
+        await accountListPage.checkPageIsLoaded();
+        await accountListPage.openAccountDetailsModal(DEFAULT_BTC_ACCOUNT_NAME);
+        await accountDetailsModal.checkPageIsLoaded();
+        const recreatedAccountAddress =
+          await accountDetailsModal.getAccountAddress();
 
-      assert(accountAddress === recreatedAccountAddress);
-    }, this.test?.fullTitle());
+        assert(accountAddress === recreatedAccountAddress);
+      },
+    );
   });
 
   it('can recreate BTC account after restoring wallet with SRP', async function () {
-    await withBtcAccountSnap(async (driver) => {
-      // check that we have one BTC account
-      const headerNavbar = new HeaderNavbar(driver);
-      await headerNavbar.checkPageIsLoaded();
-      await headerNavbar.checkAccountLabel(DEFAULT_BTC_ACCOUNT_NAME);
+    await withBtcAccountSnap(
+      { title: this.test?.fullTitle() },
+      async (driver) => {
+        // check that we have one BTC account
+        const headerNavbar = new HeaderNavbar(driver);
+        await headerNavbar.checkPageIsLoaded();
+        await headerNavbar.checkAccountLabel(DEFAULT_BTC_ACCOUNT_NAME);
 
-      await headerNavbar.openAccountMenu();
-      const accountListPage = new AccountListPage(driver);
-      await accountListPage.checkPageIsLoaded();
-      await accountListPage.openAccountDetailsModal(DEFAULT_BTC_ACCOUNT_NAME);
-      const accountDetailsModal = new AccountDetailsModal(driver);
-      await accountDetailsModal.checkPageIsLoaded();
-      const accountAddress = await accountDetailsModal.getAccountAddress();
+        await headerNavbar.openAccountMenu();
+        const accountListPage = new AccountListPage(driver);
+        await accountListPage.checkPageIsLoaded();
+        await accountListPage.openAccountDetailsModal(DEFAULT_BTC_ACCOUNT_NAME);
+        const accountDetailsModal = new AccountDetailsModal(driver);
+        await accountDetailsModal.checkPageIsLoaded();
+        const accountAddress = await accountDetailsModal.getAccountAddress();
 
-      // go to privacy settings page and get the SRP
-      await headerNavbar.openSettingsPage();
-      const settingsPage = new SettingsPage(driver);
-      await settingsPage.goToPrivacySettings();
+        // go to privacy settings page and get the SRP
+        await headerNavbar.openSettingsPage();
+        const settingsPage = new SettingsPage(driver);
+        await settingsPage.goToPrivacySettings();
 
-      const privacySettings = new PrivacySettings(driver);
-      await privacySettings.openRevealSrpQuiz();
-      await privacySettings.completeRevealSrpQuiz();
-      await privacySettings.fillPasswordToRevealSrp(WALLET_PASSWORD);
-      const seedPhrase = await privacySettings.getSrpInRevealSrpDialog();
-      await driver.clickElement({ tag: 'button', text: 'Close' });
-      await driver.clickElement(
-        '.settings-page__header__title-container__close-button',
-      );
+        const privacySettings = new PrivacySettings(driver);
+        await privacySettings.openRevealSrpQuiz();
+        await privacySettings.completeRevealSrpQuiz();
+        await privacySettings.fillPasswordToRevealSrp(WALLET_PASSWORD);
+        const seedPhrase = await privacySettings.getSrpInRevealSrpDialog();
+        await driver.clickElement({ tag: 'button', text: 'Close' });
+        await driver.clickElement(
+          '.settings-page__header__title-container__close-button',
+        );
 
-      // lock metamask and reset wallet by clicking forgot password button
-      await headerNavbar.lockMetaMask();
-      await new LoginPage(driver).gotoResetPasswordPage();
-      const resetPasswordPage = new ResetPasswordPage(driver);
-      await resetPasswordPage.checkPageIsLoaded();
-      await resetPasswordPage.resetPassword(seedPhrase, WALLET_PASSWORD);
+        // lock metamask and reset wallet by clicking forgot password button
+        await headerNavbar.lockMetaMask();
+        await new LoginPage(driver).gotoResetPasswordPage();
+        const resetPasswordPage = new ResetPasswordPage(driver);
+        await resetPasswordPage.checkPageIsLoaded();
+        await resetPasswordPage.resetPassword(seedPhrase, WALLET_PASSWORD);
 
-      // check discovered account address is the same
-      await headerNavbar.openAccountMenu();
-      await accountListPage.checkPageIsLoaded();
-      await accountListPage.openAccountDetailsModal(DEFAULT_BTC_ACCOUNT_NAME);
-      await accountDetailsModal.checkPageIsLoaded();
-      const discoveredAccountAddress =
-        await accountDetailsModal.getAccountAddress();
+        // check discovered account address is the same
+        await headerNavbar.openAccountMenu();
+        await accountListPage.checkPageIsLoaded();
+        await accountListPage.openAccountDetailsModal(DEFAULT_BTC_ACCOUNT_NAME);
+        await accountDetailsModal.checkPageIsLoaded();
+        const discoveredAccountAddress =
+          await accountDetailsModal.getAccountAddress();
 
-      assert(accountAddress === discoveredAccountAddress);
-    }, this.test?.fullTitle());
+        assert(accountAddress === discoveredAccountAddress);
+      },
+    );
   });
 });
--- test/e2e/flask/btc/btc-send.spec.ts
+++ test/e2e/flask/btc/btc-send.spec.ts
@@ -11,45 +11,69 @@ import { withBtcAccountSnap } from './common-btc';
 describe.skip('BTC Account - Send', function (this: Suite) {
   const recipientAddress = 'bc1qsqvczpxkgvp3lw230p7jffuuqnw9pp4j5tawmf';
   it('fields validation', async function () {
-    await withBtcAccountSnap(async (driver) => {
-      const homePage = new BitcoinHomepage(driver);
-      await homePage.checkPageIsLoaded();
-      await homePage.checkIsExpectedBitcoinBalanceDisplayed(
-        DEFAULT_BTC_BALANCE,
-      );
-      await homePage.startSendFlow();
+    await withBtcAccountSnap(
+      { title: this.test?.fullTitle() },
+      async (driver) => {
+        const homePage = new BitcoinHomepage(driver);
+        await homePage.checkPageIsLoaded();
+        await homePage.checkIsExpectedBitcoinBalanceDisplayed(
+          DEFAULT_BTC_BALANCE,
+        );
+        await homePage.startSendFlow();
 
-      const bitcoinSendPage = new BitcoinSendPage(driver);
-      await bitcoinSendPage.checkPageIsLoaded();
-      assert.equal(await bitcoinSendPage.checkAssetPickerIsDisplayed(), false);
-      assert.equal(await bitcoinSendPage.checkAmountFieldIsDisplayed(), false);
-      await bitcoinSendPage.fillRecipientAddress('invalidBTCAddress');
-      await bitcoinSendPage.checkAddressFieldValidationError(
-        'Invalid Bitcoin address',
-      );
-      assert.equal(await bitcoinSendPage.checkContinueButtonIsDisabled(), true);
-      await bitcoinSendPage.fillRecipientAddress(recipientAddress);
-      assert.equal(await bitcoinSendPage.checkContinueButtonIsDisabled(), true);
-      await bitcoinSendPage.fillAmount('50');
-      await bitcoinSendPage.checkAmountValidationError(
-        'Funds are insufficient to cover amount plus fee',
-      );
-      assert.equal(await bitcoinSendPage.checkContinueButtonIsDisabled(), true);
-      await bitcoinSendPage.fillAmount('0');
-      await bitcoinSendPage.checkAmountValidationError(
-        'Amount below dust limit',
-      );
-      assert.equal(await bitcoinSendPage.checkContinueButtonIsDisabled(), true);
-      await bitcoinSendPage.fillAmount('0.1');
-      await driver.delay(1000);
-      assert.equal(
-        await bitcoinSendPage.checkContinueButtonIsDisabled(),
-        false,
-      );
-      await bitcoinSendPage.clearRecipientAddress();
-      await driver.delay(1000);
-      assert.equal(await bitcoinSendPage.checkContinueButtonIsDisabled(), true);
-    }, this.test?.fullTitle());
+        const bitcoinSendPage = new BitcoinSendPage(driver);
+        await bitcoinSendPage.checkPageIsLoaded();
+        assert.equal(
+          await bitcoinSendPage.checkAssetPickerIsDisplayed(),
+          false,
+        );
+        assert.equal(
+          await bitcoinSendPage.checkAmountFieldIsDisplayed(),
+          false,
+        );
+        await bitcoinSendPage.fillRecipientAddress('invalidBTCAddress');
+        await bitcoinSendPage.checkAddressFieldValidationError(
+          'Invalid Bitcoin address',
+        );
+        assert.equal(
+          await bitcoinSendPage.checkContinueButtonIsDisabled(),
+          true,
+        );
+        await bitcoinSendPage.fillRecipientAddress(recipientAddress);
+        assert.equal(
+          await bitcoinSendPage.checkContinueButtonIsDisabled(),
+          true,
+        );
+        await bitcoinSendPage.fillAmount('50');
+        await bitcoinSendPage.checkAmountValidationError(
+          'Funds are insufficient to cover amount plus fee',
+        );
+        assert.equal(
+          await bitcoinSendPage.checkContinueButtonIsDisabled(),
+          true,
+        );
+        await bitcoinSendPage.fillAmount('0');
+        await bitcoinSendPage.checkAmountValidationError(
+          'Amount below dust limit',
+        );
+        assert.equal(
+          await bitcoinSendPage.checkContinueButtonIsDisabled(),
+          true,
+        );
+        await bitcoinSendPage.fillAmount('0.1');
+        await driver.delay(1000);
+        assert.equal(
+          await bitcoinSendPage.checkContinueButtonIsDisabled(),
+          false,
+        );
+        await bitcoinSendPage.clearRecipientAddress();
+        await driver.delay(1000);
+        assert.equal(
+          await bitcoinSendPage.checkContinueButtonIsDisabled(),
+          true,
+        );
+      },
+    );
   });
 
   it('can complete the send flow', async function () {
@@ -57,70 +81,76 @@ describe.skip('BTC Account - Send', function (this: Suite) {
     const expectedFee = '281';
     const expectedTotal = '0.50000281';
 
-    await withBtcAccountSnap(async (driver) => {
-      const homePage = new BitcoinHomepage(driver);
-      await homePage.checkPageIsLoaded();
-      await homePage.checkIsExpectedBitcoinBalanceDisplayed(
-        DEFAULT_BTC_BALANCE,
-      );
-      await homePage.startSendFlow();
+    await withBtcAccountSnap(
+      { title: this.test?.fullTitle() },
+      async (driver) => {
+        const homePage = new BitcoinHomepage(driver);
+        await homePage.checkPageIsLoaded();
+        await homePage.checkIsExpectedBitcoinBalanceDisplayed(
+          DEFAULT_BTC_BALANCE,
+        );
+        await homePage.startSendFlow();
 
-      const bitcoinSendPage = new BitcoinSendPage(driver);
-      await bitcoinSendPage.checkPageIsLoaded();
-      await bitcoinSendPage.fillRecipientAddress(recipientAddress);
-      await bitcoinSendPage.fillAmount(sendAmount);
-      await bitcoinSendPage.clickContinueButton();
+        const bitcoinSendPage = new BitcoinSendPage(driver);
+        await bitcoinSendPage.checkPageIsLoaded();
+        await bitcoinSendPage.fillRecipientAddress(recipientAddress);
+        await bitcoinSendPage.fillAmount(sendAmount);
+        await bitcoinSendPage.clickContinueButton();
 
-      // ------------------------------------------------------------------------------
-      // From here, we have moved to the confirmation screen (second part of the flow).
+        // ------------------------------------------------------------------------------
+        // From here, we have moved to the confirmation screen (second part of the flow).
 
-      const bitcoinReviewTxPage = new BitcoinReviewTxPage(driver);
-      await bitcoinReviewTxPage.checkPageIsLoaded();
-      await bitcoinReviewTxPage.checkSendAmountIsDisplayed(sendAmount);
-      await bitcoinReviewTxPage.checkNetworkFeeIsDisplayed(expectedFee);
-      await bitcoinReviewTxPage.checkFeeRateIsDisplayed(
-        Math.floor(DEFAULT_BTC_FEE_RATE).toString(),
-      );
-      await bitcoinReviewTxPage.checkTotalAmountIsDisplayed(expectedTotal);
-      await bitcoinReviewTxPage.clickSendButton();
+        const bitcoinReviewTxPage = new BitcoinReviewTxPage(driver);
+        await bitcoinReviewTxPage.checkPageIsLoaded();
+        await bitcoinReviewTxPage.checkSendAmountIsDisplayed(sendAmount);
+        await bitcoinReviewTxPage.checkNetworkFeeIsDisplayed(expectedFee);
+        await bitcoinReviewTxPage.checkFeeRateIsDisplayed(
+          Math.floor(DEFAULT_BTC_FEE_RATE).toString(),
+        );
+        await bitcoinReviewTxPage.checkTotalAmountIsDisplayed(expectedTotal);
+        await bitcoinReviewTxPage.clickSendButton();
 
-      // TODO: Test that the transaction appears in the activity tab once activity tab is implemented for Bitcoin
-      await homePage.checkPageIsLoaded();
-    }, this.test?.fullTitle());
+        // TODO: Test that the transaction appears in the activity tab once activity tab is implemented for Bitcoin
+        await homePage.checkPageIsLoaded();
+      },
+    );
   });
 
   it('can send the max amount', async function () {
     const expectedFee = 0.00000219;
 
-    await withBtcAccountSnap(async (driver) => {
-      const homePage = new BitcoinHomepage(driver);
-      await homePage.checkPageIsLoaded();
-      await homePage.checkIsExpectedBitcoinBalanceDisplayed(
-        DEFAULT_BTC_BALANCE,
-      );
-      await homePage.startSendFlow();
+    await withBtcAccountSnap(
+      { title: this.test?.fullTitle() },
+      async (driver) => {
+        const homePage = new BitcoinHomepage(driver);
+        await homePage.checkPageIsLoaded();
+        await homePage.checkIsExpectedBitcoinBalanceDisplayed(
+          DEFAULT_BTC_BALANCE,
+        );
+        await homePage.startSendFlow();
 
-      const bitcoinSendPage = new BitcoinSendPage(driver);
-      await bitcoinSendPage.checkPageIsLoaded();
-      await bitcoinSendPage.fillRecipientAddress(recipientAddress);
-      await bitcoinSendPage.selectMaxAmount();
-      await bitcoinSendPage.clickContinueButton();
+        const bitcoinSendPage = new BitcoinSendPage(driver);
+        await bitcoinSendPage.checkPageIsLoaded();
+        await bitcoinSendPage.fillRecipientAddress(recipientAddress);
+        await bitcoinSendPage.selectMaxAmount();
+        await bitcoinSendPage.clickContinueButton();
 
-      // ------------------------------------------------------------------------------
-      // From here, we have moved to the confirmation screen (second part of the flow).
+        // ------------------------------------------------------------------------------
+        // From here, we have moved to the confirmation screen (second part of the flow).
 
-      const bitcoinReviewTxPage = new BitcoinReviewTxPage(driver);
-      await bitcoinReviewTxPage.checkPageIsLoaded();
-      await bitcoinReviewTxPage.checkSendAmountIsDisplayed(
-        (DEFAULT_BTC_BALANCE - expectedFee).toString(),
-      );
-      await bitcoinReviewTxPage.checkTotalAmountIsDisplayed(
-        DEFAULT_BTC_BALANCE.toString(),
-      );
-      await bitcoinReviewTxPage.clickSendButton();
+        const bitcoinReviewTxPage = new BitcoinReviewTxPage(driver);
+        await bitcoinReviewTxPage.checkPageIsLoaded();
+        await bitcoinReviewTxPage.checkSendAmountIsDisplayed(
+          (DEFAULT_BTC_BALANCE - expectedFee).toString(),
+        );
+        await bitcoinReviewTxPage.checkTotalAmountIsDisplayed(
+          DEFAULT_BTC_BALANCE.toString(),
+        );
+        await bitcoinReviewTxPage.clickSendButton();
 
-      // TODO: Test that the transaction appears in the activity tab once activity tab is implemented for Bitcoin
-      await homePage.checkPageIsLoaded();
-    }, this.test?.fullTitle());
+        // TODO: Test that the transaction appears in the activity tab once activity tab is implemented for Bitcoin
+        await homePage.checkPageIsLoaded();
+      },
+    );
   });
 });
