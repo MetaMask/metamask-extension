e859548f488b94a268d8b37c333682600b1e056a
seaona <mariona@gmx.es>
2025-11-13 11:40:31 +0100

    rev

--- test/e2e/mock-e2e-allowlist.js
+++ test/e2e/mock-e2e-allowlist.js
@@ -1,6 +1,9 @@
 // Please do not add any more items to this list.
 // This list is temporary and the goal is to reduce it to 0, meaning all requests are mocked in our e2e tests.
 const ALLOWLISTED_URLS = [
+  // Snaps
+  'https://acl.execution.metamask.io/latest/registry.json',
+  'https://acl.execution.metamask.io/latest/signature.json',
   // Vault Decryptor
   'https://metamask.github.io/vault-decryptor',
   'https://metamask.github.io/vault-decryptor/',
