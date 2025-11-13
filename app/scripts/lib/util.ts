5e453c7fdbe082a7fefeab3057e066964ffff700
NidhiKJha <menidhikjha@gmail.com>
2025-11-14 01:09:34 +0530

    feat: sidepanel code clean up

--- app/scripts/lib/util.ts
+++ app/scripts/lib/util.ts
@@ -14,9 +14,7 @@ import {
   ENVIRONMENT_TYPE_BACKGROUND,
   ENVIRONMENT_TYPE_FULLSCREEN,
   ENVIRONMENT_TYPE_NOTIFICATION,
-  ///: BEGIN:ONLY_INCLUDE_IF(build-experimental)
   ENVIRONMENT_TYPE_SIDEPANEL,
-  ///: END:ONLY_INCLUDE_IF
   ENVIRONMENT_TYPE_POPUP,
   PLATFORM_BRAVE,
   PLATFORM_CHROME,
@@ -41,11 +39,9 @@ const getEnvironmentTypeMemo = memoize((url) => {
   } else if (parsedUrl.pathname === '/notification.html') {
     return ENVIRONMENT_TYPE_NOTIFICATION;
   }
-  ///: BEGIN:ONLY_INCLUDE_IF(build-experimental)
   else if (parsedUrl.pathname === '/sidepanel.html') {
     return ENVIRONMENT_TYPE_SIDEPANEL;
   }
-  ///: END:ONLY_INCLUDE_IF
   return ENVIRONMENT_TYPE_BACKGROUND;
 });
 
