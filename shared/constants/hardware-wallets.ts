f74529fc34c1d63a191ec56662bb5655cca3ab9e
gantunesr <17601467+gantunesr@users.noreply.github.com>
2025-11-19 11:20:26 -0300

    fix: solve extra count

--- shared/constants/hardware-wallets.ts
+++ shared/constants/hardware-wallets.ts
@@ -1,3 +1,5 @@
+import { KeyringTypes } from '@metamask/keyring-controller';
+
 /**
  * Hardware wallets supported by MetaMask.
  */
@@ -136,3 +138,30 @@ export enum MarketingActionNames {
 }
 
 export const LEDGER_USB_VENDOR_ID = '0x2c97';
+
+export const DEVICE_KEYRING_MAP = {
+  [HardwareDeviceNames.ledger]: KeyringTypes.ledger,
+  [HardwareDeviceNames.trezor]: KeyringTypes.trezor,
+  [HardwareDeviceNames.oneKey]: KeyringTypes.oneKey,
+  [HardwareDeviceNames.lattice]: KeyringTypes.lattice,
+  [HardwareDeviceNames.qr]: KeyringTypes.qr,
+};
+
+export const U2F_ERROR = 'U2F';
+
+export const LEDGER_ERRORS_CODES = {
+  '0x650f': 'ledgerErrorConnectionIssue',
+  '0x5515': 'ledgerErrorDevicedLocked',
+  '0x6501': 'ledgerErrorEthAppNotOpen',
+  '0x6a80': 'ledgerErrorTransactionDataNotPadded',
+};
+
+export const LEDGER_LIVE_PATH = `m/44'/60'/0'/0/0`;
+export const MEW_PATH = `m/44'/60'/0'`;
+export const BIP44_PATH = `m/44'/60'/0'/0`;
+
+export const LATTICE_STANDARD_BIP44_PATH = `m/44'/60'/0'/0/x`;
+export const LATTICE_LEDGER_LIVE_PATH = `m/44'/60'/x'/0/0`;
+export const LATTICE_MEW_PATH = `m/44'/60'/0'/x`;
+
+export const TREZOR_TESTNET_PATH = `m/44'/1'/0'/0`;
