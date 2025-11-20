1423536c4146b8f3434684bbf4f1c4267dacd4f4
Micaela Estabillo <micaela.estabillo@consensys.net>
2025-11-20 14:26:28 -0800

    fix: remove logs

--- test/e2e/tests/bridge/swap-positive-cases.spec.ts
+++ test/e2e/tests/bridge/swap-positive-cases.spec.ts
@@ -47,10 +47,6 @@ describe('Swap tests', function (this: Suite) {
           'Unified SwapBridge Submitted',
           'Unified SwapBridge Completed',
         ];
-        console.log(
-          '====sse requestedToCompletedEvents',
-          requestedToCompletedEvents,
-        );
         requestedToCompletedEvents.forEach((e, idx) => {
           assert.ok(
             e.event === expectedEvents[idx],
@@ -115,10 +111,6 @@ describe('Swap tests', function (this: Suite) {
           'Unified SwapBridge Submitted',
           'Unified SwapBridge Completed',
         ];
-        console.log(
-          '====requestedToCompletedEvents',
-          requestedToCompletedEvents,
-        );
         requestedToCompletedEvents.forEach((e, idx) => {
           assert.ok(
             e.event === expectedEvents[idx],
