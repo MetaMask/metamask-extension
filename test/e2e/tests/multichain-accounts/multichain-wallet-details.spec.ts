c470247f69f4f107ac32222123604f7d58a92c13
Davide Brocchetto <davide.brocchetto@consensys.net>
2025-11-20 10:48:33 -0800

    Fix tests after salim PR

--- test/e2e/tests/multichain-accounts/multichain-wallet-details.spec.ts
+++ test/e2e/tests/multichain-accounts/multichain-wallet-details.spec.ts
@@ -6,48 +6,7 @@ import { Driver } from '../../webdriver/driver';
 import HeaderNavbar from '../../page-objects/pages/header-navbar';
 import FixtureBuilder from '../../fixture-builder';
 import { loginWithBalanceValidation } from '../../page-objects/flows/login.flow';
-
-async function mockPriceApi(mockServer: Mockttp) {
-  const spotPricesMockEth = await mockServer
-    .forGet(
-      /^https:\/\/price\.api\.cx\.metamask\.io\/v2\/chains\/\d+\/spot-prices/u,
-    )
-
-    .thenCallback(() => ({
-      statusCode: 200,
-      json: {
-        '0x0000000000000000000000000000000000000000': {
-          id: 'ethereum',
-          price: 1,
-          marketCap: 112500000,
-          totalVolume: 4500000,
-          dilutedMarketCap: 120000000,
-          pricePercentChange1d: 0,
-        },
-      },
-    }));
-  const mockExchangeRates = await mockServer
-    .forGet('https://price.api.cx.metamask.io/v1/exchange-rates')
-    .thenCallback(() => ({
-      statusCode: 200,
-      json: {
-        eth: {
-          name: 'Ether',
-          ticker: 'eth',
-          value: 1 / 1700,
-          currencyType: 'crypto',
-        },
-        usd: {
-          name: 'US Dollar',
-          ticker: 'usd',
-          value: 1,
-          currencyType: 'fiat',
-        },
-      },
-    }));
-
-  return [spotPricesMockEth, mockExchangeRates];
-}
+import { mockPriceApi } from '../tokens/utils/mocks';
 
 describe('Multichain Accounts - Wallet Details', function (this: Suite) {
   it('should view wallet details with one Ethereum', async function () {
@@ -68,7 +27,7 @@ describe('Multichain Accounts - Wallet Details', function (this: Suite) {
           driver,
           undefined,
           undefined,
-          '$42,500.00',
+          '$85,025.00',
         );
         const headerNavbar = new HeaderNavbar(driver);
         await headerNavbar.openAccountMenu();
@@ -82,7 +41,7 @@ describe('Multichain Accounts - Wallet Details', function (this: Suite) {
         await accountListPage.checkAccountNameIsDisplayed('Account 2');
 
         await accountListPage.checkMultichainAccountBalanceDisplayed(
-          '$42,500.00',
+          '$85,025.00',
         );
       },
     );
