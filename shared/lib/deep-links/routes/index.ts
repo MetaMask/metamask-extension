dd4197330b79e6f117de2c13c20e5f147c22bf6e
david0xd <david@timechaser.org>
2025-11-20 10:38:30 +0100

    Add deep link for settings page
    Fix locales consistency
    
    Fix lint in locales
    
    Add e2e test for settings deep link
    
    Add tab param for settings deep link
    
    Add await

--- shared/lib/deep-links/routes/index.ts
+++ shared/lib/deep-links/routes/index.ts
@@ -6,6 +6,7 @@ import nonevm from './nonevm';
 import perps from './perps';
 import predict from './predict';
 import rewards from './rewards';
+import settings from './settings';
 
 import type { Route } from './route';
 import shield from './shield';
@@ -47,3 +48,4 @@ addRoute(perps);
 addRoute(predict);
 addRoute(rewards);
 addRoute(shield);
+addRoute(settings);
