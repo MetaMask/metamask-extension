cb8a2acb77391e538343504a55b77134142a7d0e
salimtb <salim.toubal@consensys.net>
2025-11-20 11:36:54 +0100

    fix: fix e2e tests

--- app/scripts/controller-init/assets/token-rates-controller-init.ts
+++ app/scripts/controller-init/assets/token-rates-controller-init.ts
@@ -30,6 +30,7 @@ export const TokenRatesControllerInit: ControllerInitFunction<
     state: persistedState.TokenRatesController,
     tokenPricesService: new CodefiTokenPricesServiceV2(),
     disabled: !preferencesState.useCurrencyRateCheck,
+    interval: 5000,
   });
 
   initMessenger.subscribe(
