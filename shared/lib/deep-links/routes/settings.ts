5297d2c702cbc59dc6baf0600723c9a7d6ae291b
david0xd <david@timechaser.org>
2025-11-20 17:44:29 +0100

    Add tab param for settings deep link

--- shared/lib/deep-links/routes/settings.ts
+++ shared/lib/deep-links/routes/settings.ts
@@ -3,7 +3,17 @@ import { SETTINGS_ROUTE, Route } from './route';
 export default new Route({
   pathname: '/settings',
   getTitle: (_: URLSearchParams) => 'deepLink_theSettingsPage',
-  handler: function handler(_: URLSearchParams) {
-    return { path: SETTINGS_ROUTE, query: new URLSearchParams() };
+  handler: function handler(params: URLSearchParams) {
+    const settingsTab = params.get('tab');
+
+    switch (settingsTab) {
+      case 'security':
+        return {
+          path: `${SETTINGS_ROUTE}/security`,
+          query: new URLSearchParams(),
+        };
+      default:
+        return { path: SETTINGS_ROUTE, query: new URLSearchParams() };
+    }
   },
 });
