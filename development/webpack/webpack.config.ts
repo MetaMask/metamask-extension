13a52f8613f0dfb33a3c9df90c221a98c946903f
Jongsun Suh <jongsun.suh@icloud.com>
2025-11-21 13:04:50 -0500

    Make regex groups non-capturing

--- development/webpack/webpack.config.ts
+++ development/webpack/webpack.config.ts
@@ -326,7 +326,7 @@ const config = {
         type: 'asset/resource',
       },
       {
-        test: /(?!\.(test|stories|container))\.(?:m?[jt]s|[jt]sx)$/u,
+        test: /(?!\.(?:test|stories|container))\.(?:m?[jt]s|[jt]sx)$/u,
         include: UI_DIR_RE,
         exclude: NODE_MODULES_RE,
         use: [
