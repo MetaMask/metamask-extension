61cf42b3d6b68efb87bb913d9ccd576b6cf0d5e9
David Murdoch <david@davidmurdoch.com>
2025-11-24 18:10:04 -0500

    this!

--- test/e2e/tests/deep-link/helpers.ts
+++ test/e2e/tests/deep-link/helpers.ts
@@ -96,9 +96,5 @@ export function cartesianProduct<T extends unknown[][]>(...sets: T) {
 export function getHashParams(url: URL) {
   const hash = url.hash.slice(1); // remove leading '#'
   const hashQuery = hash.split('?')[1] ?? '';
-  const hashParams = new URLSearchParams(hashQuery);
-  const encodedUrl = hashParams.get('u') ?? '';
-  const decodedUrl = decodeURIComponent(encodedUrl);
-  const decodedQuery = decodedUrl.split('?')[1] ?? '';
-  return new URLSearchParams(decodedQuery);
+  return new URLSearchParams(hashQuery);
 }
