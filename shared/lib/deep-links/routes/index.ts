71172e701ab3f1d5bcb3cab0549b0bb21e57a606
David Murdoch <187813+davidmurdoch@users.noreply.github.com>
2025-11-24 13:31:36 -0500

    add a test route

--- shared/lib/deep-links/routes/index.ts
+++ shared/lib/deep-links/routes/index.ts
@@ -33,6 +33,11 @@ export function addRoute(route: Route) {
   routes.set(route.pathname, route);
 }
 
+if (process.env.ENABLE_SETTINGS_PAGE_DEV_OPTIONS || process.env.IN_TEST) {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires, node/global-require
+  addRoute(require('./test-route'));
+}
+
 addRoute(buy);
 addRoute(home);
 addRoute(notifications);
