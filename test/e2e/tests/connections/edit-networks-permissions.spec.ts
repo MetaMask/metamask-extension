a413e5529b02cb9febaabe57cc79a43c18407b87
Davide Brocchetto <davide.brocchetto@consensys.net>
2025-11-25 00:18:53 -0800

    Fix permissions

--- test/e2e/tests/connections/edit-networks-permissions.spec.ts
+++ test/e2e/tests/connections/edit-networks-permissions.spec.ts
@@ -42,7 +42,7 @@ describe('Edit Networks Permissions', function () {
         await sitePermissionPage.editPermissionsForNetwork(['Ethereum']);
 
         // Default Chains Connected: Ethereum, Linea, Base, Arbitrum, BSC, Optimism, Polygon, Solana, BTC
-        await sitePermissionPage.checkConnectedNetworksNumber(12);
+        await sitePermissionPage.checkConnectedNetworksNumber(11);
       },
     );
   });
