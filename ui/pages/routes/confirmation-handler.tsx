daf3120482ca0cec0649f4ff8e5f55e3b7fd1260
Francis Nepomuceno <n3ps@users.noreply.github.com>
2025-11-26 13:05:40 -0500

    no ignore

--- ui/pages/routes/confirmation-handler.tsx
+++ ui/pages/routes/confirmation-handler.tsx
@@ -15,7 +15,7 @@ import { getEnvironmentType } from '../../../app/scripts/lib/util';
 import {
   ENVIRONMENT_TYPE_NOTIFICATION,
   ENVIRONMENT_TYPE_FULLSCREEN,
-  SNAP_MANAGE_ACCOUNTS_CONFIRMATION_TYPES,
+  // SNAP_MANAGE_ACCOUNTS_CONFIRMATION_TYPES,
 } from '../../../shared/constants/app';
 import {
   getHasApprovalFlows,
@@ -29,17 +29,17 @@ import {
 } from '../../ducks/swaps/swaps';
 import { useNavState } from '../../contexts/navigation-state';
 
-const APPROVAL_TYPES = [
-  ///: BEGIN:ONLY_INCLUDE_IF(keyring-snaps)
-  SNAP_MANAGE_ACCOUNTS_CONFIRMATION_TYPES.confirmAccountCreation,
-  SNAP_MANAGE_ACCOUNTS_CONFIRMATION_TYPES.confirmAccountRemoval,
-  SNAP_MANAGE_ACCOUNTS_CONFIRMATION_TYPES.showNameSnapAccount,
-  SNAP_MANAGE_ACCOUNTS_CONFIRMATION_TYPES.showSnapAccountRedirect,
-  ///: END:ONLY_INCLUDE_IF
-  'wallet_installSnap',
-  'wallet_updateSnap',
-  'wallet_installSnapResult',
-];
+// const APPROVAL_TYPES = [
+//   ///: BEGIN:ONLY_INCLUDE_IF(keyring-snaps)
+//   SNAP_MANAGE_ACCOUNTS_CONFIRMATION_TYPES.confirmAccountCreation,
+//   SNAP_MANAGE_ACCOUNTS_CONFIRMATION_TYPES.confirmAccountRemoval,
+//   SNAP_MANAGE_ACCOUNTS_CONFIRMATION_TYPES.showNameSnapAccount,
+//   SNAP_MANAGE_ACCOUNTS_CONFIRMATION_TYPES.showSnapAccountRedirect,
+//   ///: END:ONLY_INCLUDE_IF
+//   'wallet_installSnap',
+//   'wallet_updateSnap',
+//   'wallet_installSnapResult',
+// ];
 
 export const ConfirmationHandler = () => {
   const navigate = useNavigate();
@@ -69,29 +69,29 @@ export const ConfirmationHandler = () => {
   const canRedirect = !isNotification && !stayOnHomePage;
 
   // Flows that *should* navigate in fullscreen, based on E2E specs
-  const hasSnapApproval = pendingApprovals.some((approval) =>
-    APPROVAL_TYPES.includes(approval.type),
-  );
+  // const hasSnapApproval = pendingApprovals.some((approval) =>
+  //   APPROVAL_TYPES.includes(approval.type),
+  // );
 
   // Flows that *should not* navigate in fullscreen, based on E2E specs
-  const hasSmartTransactionStatus = pendingApprovals.some(
-    (approval) =>
-      approval.type === 'smartTransaction:showSmartTransactionStatusPage' &&
-      approval.origin !== 'metamask' &&
-      approval.origin !== 'MetaMask',
-  );
+  // const hasSmartTransactionStatus = pendingApprovals.some(
+  //   (approval) =>
+  //     approval.type === 'smartTransaction:showSmartTransactionStatusPage' &&
+  //     approval.origin !== 'metamask' &&
+  //     approval.origin !== 'MetaMask',
+  // );
 
-  const ignored = useMemo(() => {
-    return (
-      isFullscreen &&
-      (hasSmartTransactionStatus || (!hasSnapApproval && !hasApprovalFlows))
-    );
-  }, [
-    isFullscreen,
-    hasSmartTransactionStatus,
-    hasSnapApproval,
-    hasApprovalFlows,
-  ]);
+  // const ignored = useMemo(() => {
+  //   return (
+  //     isFullscreen &&
+  //     (hasSmartTransactionStatus || (!hasSnapApproval && !hasApprovalFlows))
+  //   );
+  // }, [
+  //   isFullscreen,
+  //   hasSmartTransactionStatus,
+  //   hasSnapApproval,
+  //   hasApprovalFlows,
+  // ]);
 
   // Ported from home.component - checkStatusAndNavigate()
   useEffect(() => {
@@ -100,9 +100,9 @@ export const ConfirmationHandler = () => {
       return;
     }
 
-    if (ignored) {
-      return;
-    }
+    // if (ignored) {
+    //   return;
+    // }
 
     if (canRedirect && showAwaitingSwapScreen) {
       navigate(AWAITING_SWAP_ROUTE);
@@ -135,7 +135,7 @@ export const ConfirmationHandler = () => {
     pendingApprovals,
     showAwaitingSwapScreen,
     swapsFetchParams,
-    ignored,
+    // ignored,
   ]);
 
   return null;
