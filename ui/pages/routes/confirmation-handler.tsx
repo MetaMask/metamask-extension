45dd9fb03e4a1fa0f830b0e634c019006b357703
Francis Nepomuceno <n3ps@users.noreply.github.com>
2025-11-26 16:11:04 -0500

    mimic didMount, didUpdate

--- ui/pages/routes/confirmation-handler.tsx
+++ ui/pages/routes/confirmation-handler.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo } from 'react';
+import { useCallback, useEffect, useMemo, useRef } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom-v5-compat';
 import { useSelector } from 'react-redux';
 
@@ -14,8 +14,8 @@ import { getConfirmationRoute } from '../confirmations/hooks/useConfirmationNavi
 import { getEnvironmentType } from '../../../app/scripts/lib/util';
 import {
   ENVIRONMENT_TYPE_NOTIFICATION,
-  ENVIRONMENT_TYPE_FULLSCREEN,
-  // SNAP_MANAGE_ACCOUNTS_CONFIRMATION_TYPES,
+  // ENVIRONMENT_TYPE_FULLSCREEN,
+  SNAP_MANAGE_ACCOUNTS_CONFIRMATION_TYPES,
 } from '../../../shared/constants/app';
 import {
   getHasApprovalFlows,
@@ -29,17 +29,17 @@ import {
 } from '../../ducks/swaps/swaps';
 import { useNavState } from '../../contexts/navigation-state';
 
-// const APPROVAL_TYPES = [
-//   ///: BEGIN:ONLY_INCLUDE_IF(keyring-snaps)
-//   SNAP_MANAGE_ACCOUNTS_CONFIRMATION_TYPES.confirmAccountCreation,
-//   SNAP_MANAGE_ACCOUNTS_CONFIRMATION_TYPES.confirmAccountRemoval,
-//   SNAP_MANAGE_ACCOUNTS_CONFIRMATION_TYPES.showNameSnapAccount,
-//   SNAP_MANAGE_ACCOUNTS_CONFIRMATION_TYPES.showSnapAccountRedirect,
-//   ///: END:ONLY_INCLUDE_IF
-//   'wallet_installSnap',
-//   'wallet_updateSnap',
-//   'wallet_installSnapResult',
-// ];
+const APPROVAL_TYPES = [
+  ///: BEGIN:ONLY_INCLUDE_IF(keyring-snaps)
+  SNAP_MANAGE_ACCOUNTS_CONFIRMATION_TYPES.confirmAccountCreation,
+  SNAP_MANAGE_ACCOUNTS_CONFIRMATION_TYPES.confirmAccountRemoval,
+  SNAP_MANAGE_ACCOUNTS_CONFIRMATION_TYPES.showNameSnapAccount,
+  SNAP_MANAGE_ACCOUNTS_CONFIRMATION_TYPES.showSnapAccountRedirect,
+  ///: END:ONLY_INCLUDE_IF
+  // 'wallet_installSnap',
+  // 'wallet_updateSnap',
+  // 'wallet_installSnapResult',
+];
 
 export const ConfirmationHandler = () => {
   const navigate = useNavigate();
@@ -49,7 +49,7 @@ export const ConfirmationHandler = () => {
 
   const envType = getEnvironmentType();
   const isNotification = envType === ENVIRONMENT_TYPE_NOTIFICATION;
-  const isFullscreen = envType === ENVIRONMENT_TYPE_FULLSCREEN;
+  // const isFullscreen = envType === ENVIRONMENT_TYPE_FULLSCREEN;
 
   const showAwaitingSwapScreen = useSelector(getShowAwaitingSwapScreen);
   const hasSwapsQuotes = useSelector(getHasSwapsQuotes);
@@ -57,6 +57,8 @@ export const ConfirmationHandler = () => {
   const swapsFetchParams = useSelector(getFetchParams);
   const pendingApprovals = useSelector(selectPendingApprovalsForNavigation);
   const hasApprovalFlows = useSelector(getHasApprovalFlows);
+
+  const isFirst = useRef(true);
 
   // Read stayOnHomePage from both v5 location.state and v5-compat navState
   const stayOnHomePage = useMemo(
@@ -68,41 +70,11 @@ export const ConfirmationHandler = () => {
 
   const canRedirect = !isNotification && !stayOnHomePage;
 
-  // Flows that *should* navigate in fullscreen, based on E2E specs
-  // const hasSnapApproval = pendingApprovals.some((approval) =>
-  //   APPROVAL_TYPES.includes(approval.type),
-  // );
-
-  // Flows that *should not* navigate in fullscreen, based on E2E specs
-  // const hasSmartTransactionStatus = pendingApprovals.some(
-  //   (approval) =>
-  //     approval.type === 'smartTransaction:showSmartTransactionStatusPage' &&
-  //     approval.origin !== 'metamask' &&
-  //     approval.origin !== 'MetaMask',
-  // );
-
-  // const ignored = useMemo(() => {
-  //   return (
-  //     isFullscreen &&
-  //     (hasSmartTransactionStatus || (!hasSnapApproval && !hasApprovalFlows))
-  //   );
-  // }, [
-  //   isFullscreen,
-  //   hasSmartTransactionStatus,
-  //   hasSnapApproval,
-  //   hasApprovalFlows,
-  // ]);
-
-  // Ported from home.component - checkStatusAndNavigate()
-  useEffect(() => {
+  const checkStatusAndNavigate = useCallback(() => {
     // Only run when on home/default page (for now)
     if (pathname !== DEFAULT_ROUTE) {
       return;
     }
-
-    // if (ignored) {
-    //   return;
-    // }
 
     if (canRedirect && showAwaitingSwapScreen) {
       navigate(AWAITING_SWAP_ROUTE);
@@ -124,18 +96,61 @@ export const ConfirmationHandler = () => {
     }
   }, [
     canRedirect,
-    hasBridgeQuotes,
-    // hasSmartTransactionStatus,
     hasApprovalFlows,
+    hasBridgeQuotes,
     hasSwapsQuotes,
-    // hasSnapApproval,
-    isFullscreen,
     navigate,
-    pathname,
     pendingApprovals,
+    pathname,
     showAwaitingSwapScreen,
     swapsFetchParams,
-    // ignored,
+  ]);
+
+  // Flows that *should* navigate in fullscreen, based on E2E specs
+  const hasAllowedPopupRedirectApprovals = pendingApprovals.some((approval) =>
+    APPROVAL_TYPES.includes(approval.type),
+  );
+
+  // Flows that *should not* navigate in fullscreen, based on E2E specs
+  // const hasSmartTransactionStatus = pendingApprovals.some(
+  //   (approval) =>
+  //     approval.type === 'smartTransaction:showSmartTransactionStatusPage' &&
+  //     approval.origin !== 'metamask' &&
+  //     approval.origin !== 'MetaMask',
+  // );
+
+  // const ignored = useMemo(() => {
+  //   return (
+  //     isFullscreen &&
+  //     (hasSmartTransactionStatus || (!hasSnapApproval && !hasApprovalFlows))
+  //   );
+  // }, [
+  //   isFullscreen,
+  //   hasSmartTransactionStatus,
+  //   hasSnapApproval,
+  //   hasApprovalFlows,
+  // ]);
+
+  // Ported from home.component - componentDidMount()
+  useEffect(() => {
+    checkStatusAndNavigate();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []); // Run only once on mount
+
+  // Ported from home.component - componendDidUpdate()
+  useEffect(() => {
+    if (isFirst.current) {
+      isFirst.current = false;
+      return; // prevent running on first render
+    }
+
+    if (isNotification || hasAllowedPopupRedirectApprovals) {
+      checkStatusAndNavigate();
+    }
+  }, [
+    isNotification,
+    hasAllowedPopupRedirectApprovals,
+    checkStatusAndNavigate,
   ]);
 
   return null;
