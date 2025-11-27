8bd86070c28033ae13134afeccd954cb7455ee02
seaona <mariona@gmx.es>
2025-11-27 12:54:23 +0100

    fix

--- test/e2e/page-objects/pages/home/asset-list.ts
+++ test/e2e/page-objects/pages/home/asset-list.ts
@@ -290,8 +290,10 @@ class AssetListPage {
     await this.driver.waitForSelector(
       this.customNetworkSelectedOption(networkName),
     );
-    await this.driver.delay(3000);
     await this.driver.clickElement(this.customTokenModalOption);
+    await this.driver.assertElementNotPresent(this.tokenSearchInput, {
+      waitAtLeastGuard: 1000,
+    });
     await this.driver.waitForSelector(this.modalWarningBanner);
     await this.driver.fill(this.tokenAddressInput, tokenAddress);
     await this.driver.waitForSelector(this.tokenSymbolTitle);
