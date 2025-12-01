b5f959937b2163d90f514400d8af9c2361d48676
sophieqgu <sophieqgu@gmail.com>
2025-12-01 13:27:56 -0500

    Add onboarding route
    Fix lint
    
    Change url
    
    Change url
    
    Add translation key
    
    Update messages.json
    
    Update messages.json

---
+++ shared/lib/deep-links/routes/onboarding.ts
@@ -0,0 +1,11 @@
+import { Route, ZENDESK_URLS } from './route';
+
+export default new Route({
+  pathname: '/onboarding',
+  getTitle: (_: URLSearchParams) => 'deepLink_theOnboardingPage',
+  handler: function handler(_params: URLSearchParams) {
+    return {
+      redirectTo: new URL(ZENDESK_URLS.IMPORT_ACCOUNTS),
+    };
+  },
+});
