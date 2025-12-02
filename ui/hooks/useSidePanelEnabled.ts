f5530f3f122cf67cc0b7df3b68478fed3553a318
Francis Nepomuceno <n3ps@users.noreply.github.com>
2025-12-02 10:02:04 -0500

    testing

--- ui/hooks/useSidePanelEnabled.ts
+++ ui/hooks/useSidePanelEnabled.ts
@@ -1,27 +1,8 @@
-import { useMemo } from 'react';
-import { useSelector } from 'react-redux';
-import { getRemoteFeatureFlags } from '../selectors/remote-feature-flags';
-import { getIsSidePanelFeatureEnabled } from '../../shared/modules/environment';
-
 /**
  * Custom hook to check if sidepanel feature is enabled.
- * Checks both the build-time environment flag AND the LaunchDarkly feature flag.
  *
  * @returns boolean - True if sidepanel feature is enabled, false otherwise
  */
 export const useSidePanelEnabled = (): boolean => {
-  const remoteFeatureFlags = useSelector(getRemoteFeatureFlags);
-
-  const isSidePanelEnabled = useMemo(() => {
-    const isBuildEnabled = getIsSidePanelFeatureEnabled();
-
-    const isFeatureFlagEnabled = Boolean(
-      remoteFeatureFlags?.extensionUxSidepanel,
-    );
-
-    // Both must be true for sidepanel to be enabled
-    return isBuildEnabled && isFeatureFlagEnabled;
-  }, [remoteFeatureFlags]);
-
-  return isSidePanelEnabled;
+  return process.env.IS_SIDEPANEL?.toString() === 'true';
 };
