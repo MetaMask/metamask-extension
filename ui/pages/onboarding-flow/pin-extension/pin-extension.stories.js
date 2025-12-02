de5754b4cd2aaf69a52308b265eb514586842ab8
Ganesh Suresh Patra <ganesh.patra@consensys.net>
2025-12-02 12:00:45 +0530

    fix: reverted pin-extension changes

---
+++ ui/pages/onboarding-flow/pin-extension/pin-extension.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import OnboardingPinExtension from './pin-extension';
+
+export default {
+  title: 'Pages/OnboardingFlow/OnboardingPinExtension',
+};
+
+export const DefaultStory = () => <OnboardingPinExtension />;
+
+DefaultStory.storyName = 'Default';
