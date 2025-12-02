223e809c86205fd317ac5443451f96796c03e732
seaona <mariona@gmx.es>
2025-12-02 16:17:19 +0100

    fix dist

--- test/e2e/page-objects/flows/onboarding.flow.ts
+++ test/e2e/page-objects/flows/onboarding.flow.ts
@@ -497,6 +497,10 @@ export const completeCreateNewWalletOnboardingFlowWithCustomSettings = async ({
   await onboardingCompletePage.checkPageIsLoaded();
 
   await onboardingCompletePage.completeOnboarding();
+  if (process.env.SELENIUM_BROWSER !== Browser.CHROME) {
+    // wait for the sidepanel to open
+    await driver.delay(3000);
+  }
 
   await handleSidepanelPostOnboarding(driver);
 };
