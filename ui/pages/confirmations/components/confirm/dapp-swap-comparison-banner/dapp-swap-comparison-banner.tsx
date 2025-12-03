dcc8dc399cea46eb081acf7b884ce1e38c57463d
Jyoti Puri <jyotipuri@gmail.com>
2025-12-03 17:59:37 +0530

    dapp swap comparison: ui fixes

--- ui/pages/confirmations/components/confirm/dapp-swap-comparison-banner/dapp-swap-comparison-banner.tsx
+++ ui/pages/confirmations/components/confirm/dapp-swap-comparison-banner/dapp-swap-comparison-banner.tsx
@@ -218,7 +218,7 @@ const DappSwapComparisonInner = () => {
                   className="dapp-swap_text-rewards"
                   variant={TextVariant.BodyXs}
                 >
-                  ${rewards.text}
+                  {rewards.text}
                 </Text>
               </>
             )}
