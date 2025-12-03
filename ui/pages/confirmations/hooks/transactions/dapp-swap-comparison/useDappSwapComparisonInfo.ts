7789a1ce42d189071c0f0f972206f66367de0aa7
Jyoti Puri <jyotipuri@gmail.com>
2025-12-03 18:24:45 +0530

    update

--- ui/pages/confirmations/hooks/transactions/dapp-swap-comparison/useDappSwapComparisonInfo.ts
+++ ui/pages/confirmations/hooks/transactions/dapp-swap-comparison/useDappSwapComparisonInfo.ts
@@ -6,6 +6,7 @@ import { TransactionMeta } from '@metamask/transaction-controller';
 import { useEffect, useMemo } from 'react';
 import { useSelector } from 'react-redux';
 
+import { captureException } from '../../../../../../shared/lib/sentry';
 import {
   getBestQuote,
   getBalanceChangeFromSimulationData,
@@ -120,82 +121,90 @@ export function useDappSwapComparisonInfo() {
   ]);
 
   useEffect(() => {
-    if (
-      !bestQuote ||
-      !simulationData ||
-      swapInfo?.destTokenAmountMin === undefined ||
-      !swapInfo ||
-      !tokenDetails
-    ) {
-      return;
-    }
+    try {
+      if (
+        !bestQuote ||
+        !simulationData ||
+        swapInfo?.destTokenAmountMin === undefined ||
+        !swapInfo ||
+        !tokenDetails
+      ) {
+        return;
+      }
 
-    const swapComparisonLatency = updateSwapComparisonLatency();
+      const swapComparisonLatency = updateSwapComparisonLatency();
 
-    const { destTokenAddress, srcTokenAddress, srcTokenAmount } = swapInfo;
-    const {
-      quote: { destTokenAmount, minDestTokenAmount },
-    } = bestQuote;
+      const { destTokenAddress, srcTokenAddress, srcTokenAmount } = swapInfo;
+      const {
+        quote: { destTokenAmount, minDestTokenAmount },
+      } = bestQuote;
 
-    const totalGasInQuote = getGasUSDValue(getGasFromQuote(bestQuote));
+      const totalGasInQuote = getGasUSDValue(getGasFromQuote(bestQuote));
 
-    const confirmationGasUsd = getGasUSDValue(
-      new BigNumber(gasUsed ?? gasLimitNoBuffer ?? gas ?? '0x0', 16),
-    );
+      const confirmationGasUsd = getGasUSDValue(
+        new BigNumber(gasUsed ?? gasLimitNoBuffer ?? gas ?? '0x0', 16),
+      );
 
-    const destTokenBalanceChange = getBalanceChangeFromSimulationData(
-      destTokenAddress as Hex,
-      simulationData,
-    );
+      const destTokenBalanceChange = getBalanceChangeFromSimulationData(
+        destTokenAddress as Hex,
+        simulationData,
+      );
 
-    captureDappSwapComparisonMetricsProperties({
-      properties: {
-        swap_dapp_comparison: 'completed',
-        swap_dapp_commands: commands ?? '',
-        swap_dapp_from_token_simulated_value_usd: getTokenUSDValue(
-          srcTokenAmount,
-          srcTokenAddress as Hex,
-        ),
-        swap_dapp_to_token_simulated_value_usd: getDestinationTokenUSDValue(
-          destTokenBalanceChange,
-        ),
-        swap_dapp_minimum_received_value_usd: getDestinationTokenUSDValue(
-          swapInfo?.destTokenAmountMin as Hex,
-        ),
-        swap_dapp_network_fee_usd: confirmationGasUsd,
-        swap_mm_from_token_simulated_value_usd: getTokenUSDValue(
-          srcTokenAmount,
-          srcTokenAddress as Hex,
-        ),
-        swap_mm_to_token_simulated_value_usd:
-          getDestinationTokenUSDValue(destTokenAmount),
-        swap_mm_minimum_received_value_usd:
-          getDestinationTokenUSDValue(minDestTokenAmount),
-        swap_mm_slippage: (bestQuote.quote as unknown as { slippage: string })
-          .slippage,
-        swap_mm_quote_provider: (
-          bestQuote.quote as unknown as { aggregator: string }
-        ).aggregator,
-        swap_mm_network_fee_usd: totalGasInQuote,
-        swap_comparison_total_latency_ms: swapComparisonLatency,
-        swap_mm_quote_response_latency_ms:
-          quoteResponseLatency?.toString() ?? 'N_A',
-      },
-      sensitiveProperties: {
-        swap_from_token_contract: srcTokenAddress,
-        swap_from_token_symbol:
-          getTokenValueFromRecord<TokenStandAndDetails>(
-            tokenDetails,
+      captureDappSwapComparisonMetricsProperties({
+        properties: {
+          swap_dapp_comparison: 'completed',
+          swap_dapp_commands: commands ?? '',
+          swap_dapp_from_token_simulated_value_usd: getTokenUSDValue(
+            srcTokenAmount,
             srcTokenAddress as Hex,
-          )?.symbol ?? 'N/A',
-        swap_to_token_contract: destTokenAddress,
-        swap_to_token_symbol:
-          getTokenValueFromRecord<TokenStandAndDetails>(
-            tokenDetails,
-            destTokenAddress as Hex,
-          )?.symbol ?? 'N/A',
-      },
-    });
+          ),
+          swap_dapp_to_token_simulated_value_usd: getDestinationTokenUSDValue(
+            destTokenBalanceChange,
+          ),
+          swap_dapp_minimum_received_value_usd: getDestinationTokenUSDValue(
+            swapInfo?.destTokenAmountMin as Hex,
+          ),
+          swap_dapp_network_fee_usd: confirmationGasUsd,
+          swap_mm_from_token_simulated_value_usd: getTokenUSDValue(
+            srcTokenAmount,
+            srcTokenAddress as Hex,
+          ),
+          swap_mm_to_token_simulated_value_usd:
+            getDestinationTokenUSDValue(destTokenAmount),
+          swap_mm_minimum_received_value_usd:
+            getDestinationTokenUSDValue(minDestTokenAmount),
+          swap_mm_slippage: (bestQuote.quote as unknown as { slippage: string })
+            .slippage,
+          swap_mm_quote_provider: (
+            bestQuote.quote as unknown as { aggregator: string }
+          ).aggregator,
+          swap_mm_network_fee_usd: totalGasInQuote,
+          swap_comparison_total_latency_ms: swapComparisonLatency,
+          swap_mm_quote_response_latency_ms:
+            quoteResponseLatency?.toString() ?? 'N_A',
+        },
+        sensitiveProperties: {
+          swap_from_token_contract: srcTokenAddress,
+          swap_from_token_symbol:
+            getTokenValueFromRecord<TokenStandAndDetails>(
+              tokenDetails,
+              srcTokenAddress as Hex,
+            )?.symbol ?? 'N/A',
+          swap_to_token_contract: destTokenAddress,
+          swap_to_token_symbol:
+            getTokenValueFromRecord<TokenStandAndDetails>(
+              tokenDetails,
+              destTokenAddress as Hex,
+            )?.symbol ?? 'N/A',
+        },
+      });
+    } catch (error) {
+      captureException(error);
+      captureDappSwapComparisonFailed(
+        `Error calculating metrics values: ${(error as Error).toString()}`,
+        commands,
+      );
+    }
   }, [
     bestQuote,
     captureDappSwapComparisonFailed,
