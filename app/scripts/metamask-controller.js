0a80b3572a692045ac711f233d18771cb43efa2d
lwin <lwin.kyaw@consensys.net>
2025-12-04 02:16:57 +0800

    feat: update seedless-onboarding-controller to v7.1

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -151,8 +151,8 @@ import {
 
 import {
   HardwareDeviceNames,
+  HardwareKeyringType,
   LedgerTransportTypes,
-  KEYRING_DEVICE_PROPERTY_MAP,
 } from '../../shared/constants/hardware-wallets';
 import { KeyringType } from '../../shared/constants/keyring';
 import { RestrictedMethods } from '../../shared/constants/permissions';
@@ -1445,15 +1445,11 @@ export default class MetamaskController extends EventEmitter {
         (snap) => !snap.preinstalled,
       )
     ) {
-      this.snapController.updateRegistry().catch((error) => {
-        if (
-          !error.message.includes(
-            'The Snaps platform requires basic functionality to be used.',
-          )
-        ) {
-          console.error(error);
-        }
-      });
+      try {
+        this.snapController.updateRegistry();
+      } catch {
+        // Ignore
+      }
     }
   }
 
@@ -3021,6 +3017,10 @@ export default class MetamaskController extends EventEmitter {
       ),
 
       // SeedlessOnboardingController
+      getToprfNodeDetails:
+        this.seedlessOnboardingController.fetchNodeDetails.bind(
+          this.seedlessOnboardingController,
+        ),
       authenticate: this.seedlessOnboardingController.authenticate.bind(
         this.seedlessOnboardingController,
       ),
@@ -5452,7 +5452,7 @@ export default class MetamaskController extends EventEmitter {
   async getHardwareTypeForMetric(address) {
     return await this.keyringController.withKeyring(
       { address },
-      ({ keyring }) => KEYRING_DEVICE_PROPERTY_MAP[keyring.type],
+      ({ keyring }) => HardwareKeyringType[keyring.type],
     );
   }
 
@@ -5490,7 +5490,7 @@ export default class MetamaskController extends EventEmitter {
       case KeyringType.lattice:
       case KeyringType.qr:
       case KeyringType.ledger:
-        return KEYRING_DEVICE_PROPERTY_MAP[keyringType];
+        return 'hardware';
       case KeyringType.imported:
         return 'imported';
       case KeyringType.snap:
