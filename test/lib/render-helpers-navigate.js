65617cdea8f3f79a2e1a377c0784f4e944c20570
dddddanica <zhaodanica@gmail.com>
2025-12-04 17:09:17 +0000

    refactor(5566): fix jest lint issue

--- test/lib/render-helpers-navigate.js
+++ test/lib/render-helpers-navigate.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars -- ESLint is confused here */
+/* global jest */
 import React, { useMemo, useState } from 'react';
 import { Provider } from 'react-redux';
 import { render } from '@testing-library/react';
