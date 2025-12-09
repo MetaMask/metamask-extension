d9982f5a3e27b89098732cfb499a63ded46c22bb
seaona <mariona@gmx.es>
2025-12-09 09:55:38 +0100

    fix

--- test/e2e/run-api-specs-multichain.ts
+++ test/e2e/run-api-specs-multichain.ts
@@ -192,8 +192,10 @@ async function main() {
     }) => {
       await unlockWallet(driver);
 
-      // Navigate to extension home screen
-      await driver.navigate(PAGES.HOME);
+      const homePage = new HomePage(driver);
+      await homePage.checkPageIsLoaded();
+      // We don't have balance so we expect to see Fund Your Wallet
+      await homePage.checkExpectedBalanceIsDisplayed('0', 'ETH');
 
       // Open Dapp
       await driver.openNewPage(DAPP_URL);
