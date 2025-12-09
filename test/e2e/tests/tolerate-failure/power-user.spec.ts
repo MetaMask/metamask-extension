1296e089e672cd82e2f948089a057316ae657b9b
Javier Garcia Vera <javier.vera@consensys.net>
2025-12-09 13:06:20 +0100

    test: increased timeout for login page on performance tests

--- test/e2e/tests/tolerate-failure/power-user.spec.ts
+++ test/e2e/tests/tolerate-failure/power-user.spec.ts
@@ -33,7 +33,7 @@ describe('Power user persona', function () {
       async ({ driver }: { driver: Driver }) => {
         await driver.navigate();
         const loginPage = new LoginPage(driver);
-        await loginPage.checkPageIsLoaded();
+        await loginPage.checkPageIsLoaded(30000);
         await loginPage.loginToHomepage();
         const homePage = new HomePage(driver);
         await homePage.checkPageIsLoaded({ timeout: 30000 }); // Since here the requests are not mocked, let's wait longer
