d131b3c13ddf74626414e6542abc2648a49a8d47
Amanda Yeoh <amanda.yeoh@consensys.net>
2025-12-09 16:51:42 +0800

    chore: fix hidden accounts icon spacing and color

--- ui/components/multichain-accounts/multichain-account-list/multichain-account-list.tsx
+++ ui/components/multichain-accounts/multichain-account-list/multichain-account-list.tsx
@@ -419,7 +419,7 @@ export const MultichainAccountList = ({
           justifyContent={JustifyContent.spaceBetween}
           alignItems={AlignItems.center}
           paddingLeft={4}
-          paddingRight={4}
+          paddingRight={6}
           paddingTop={2}
           paddingBottom={2}
           width={BlockSize.Full}
@@ -436,8 +436,8 @@ export const MultichainAccountList = ({
             name={
               isHiddenAccountsExpanded ? IconName.ArrowUp : IconName.ArrowDown
             }
-            size={IconSize.Sm}
-            color={IconColor.iconDefault}
+            size={IconSize.Md}
+            color={IconColor.iconAlternative}
           />
         </Box>
       );
