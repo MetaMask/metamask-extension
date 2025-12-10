4dcdfd56061629321ecbd86e5497c3cbc3747de7
seaona <mariona@gmx.es>
2025-12-10 15:22:30 +0100

    fix

--- test/e2e/flask/multichain-api/evm/wallet_revokeSession.spec.ts
+++ test/e2e/flask/multichain-api/evm/wallet_revokeSession.spec.ts
@@ -74,7 +74,7 @@ describe('Initializing a session w/ several scopes and accounts, then calling `w
         await testDapp.revokeSession();
 
         const parsedResult = await testDapp.getSession({
-          numberOfResultItems: 2,
+          numberOfResultItems: 3,
         });
         const resultSessionScopes = parsedResult.sessionScopes;
         assert.deepStrictEqual(
