12c58d8b28d853b7522878b4249dd28ef99bc957
Charly Chevalier <charly.chevalier@consensys.net>
2025-12-10 19:09:08 +0100

    chore: lint

--- test/e2e/tests/solana/constants.ts
+++ test/e2e/tests/solana/constants.ts
@@ -1,3 +1,4 @@
 export const SOLANA_URL_REGEX_MAINNET =
   /^https:\/\/solana-(mainnet|devnet)\.infura\.io\/v3*/u;
-export const SOLANA_URL_REGEX_DEVNET = /^https:\/\/solana-devnet\.infura\.io\/v3\/.*/u;
+export const SOLANA_URL_REGEX_DEVNET =
+  /^https:\/\/solana-devnet\.infura\.io\/v3\/.*/u;
