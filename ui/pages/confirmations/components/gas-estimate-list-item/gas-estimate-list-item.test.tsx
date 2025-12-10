26306aa5d46719d7327704b20c2cd6311e337312
Goktug Poyraz <omergoktugpoyraz@gmail.com>
2025-12-10 10:17:24 +0100

    Update more paths

--- ui/pages/confirmations/components/gas-estimate-list-item/gas-estimate-list-item.test.tsx
+++ ui/pages/confirmations/components/gas-estimate-list-item/gas-estimate-list-item.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { fireEvent } from '@testing-library/react';
-import { renderWithProvider } from '../../../../../test/jest';
+import { renderWithProvider } from '../../../../../test/lib/render-helpers-navigate';
 import { GasOption } from '../../types/gas';
 import {
   GasEstimateListHeader,
