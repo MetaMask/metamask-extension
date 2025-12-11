a41404edbd48b6dec7b3cd43625f37c80e8551ae
Tatiana Bobritsky <tatiana.bobritsky@consensys.net>
2025-12-11 11:24:40 -0500

    Order selectors and methods alphabetically

--- test/e2e/page-objects/pages/confirmations/redesign/erc20-approve-transaction-confirmation.ts
+++ test/e2e/page-objects/pages/confirmations/redesign/erc20-approve-transaction-confirmation.ts
@@ -2,34 +2,9 @@ import { tEn } from '../../../../../lib/i18n-helpers';
 import TransactionConfirmation from './transaction-confirmation';
 
 class ERC20ApproveTransactionConfirmation extends TransactionConfirmation {
-  private readonly spendingCapRequestTitle = {
-    css: 'h2',
-    text: tEn('confirmTitlePermitTokens') as string,
-  };
-
-  private readonly spendingCapPermissionDescription = {
-    css: 'p',
-    text: tEn('confirmTitleDescERC20ApproveTransaction') as string,
-  };
-
   private readonly estimatedChangesSection = {
     css: 'p',
     text: tEn('simulationDetailsTitle') as string,
-  };
-
-  private readonly spendingCapSection = {
-    css: 'p',
-    text: tEn('spendingCap') as string,
-  };
-
-  private readonly spenderSection = {
-    css: 'p',
-    text: tEn('spender') as string,
-  };
-
-  private readonly requestFromSection = {
-    css: 'p',
-    text: tEn('requestFrom') as string,
   };
 
   private readonly interactingWithSection = {
@@ -42,42 +17,42 @@ class ERC20ApproveTransactionConfirmation extends TransactionConfirmation {
     text: tEn('methodData') as string,
   };
 
-  async checkSpendingCapRequestTitle(): Promise<void> {
-    console.log('Verify spending cap request title is displayed');
-    await this.driver.waitForSelector(this.spendingCapRequestTitle);
-  }
+  private readonly requestFromSection = {
+    css: 'p',
+    text: tEn('requestFrom') as string,
+  };
 
-  async checkSpendingCapPermissionDescription(): Promise<void> {
-    console.log('Verify spending cap permission description is displayed');
-    await this.driver.waitForSelector(this.spendingCapPermissionDescription);
+  private readonly spenderSection = {
+    css: 'p',
+    text: tEn('spender') as string,
+  };
+
+  private readonly spendingCapPermissionDescription = {
+    css: 'p',
+    text: tEn('confirmTitleDescERC20ApproveTransaction') as string,
+  };
+
+  private readonly spendingCapRequestTitle = {
+    css: 'h2',
+    text: tEn('confirmTitlePermitTokens') as string,
+  };
+
+  private readonly spendingCapSection = {
+    css: 'p',
+    text: tEn('spendingCap') as string,
+  };
+
+  async checkAdvancedDetailsSections(): Promise<void> {
+    console.log('Verify all advanced details sections are displayed');
+    await this.checkInteractingWithSection();
+    await this.checkMethodSection();
+    await this.checkRequestFromSection();
+    await this.checkSpenderSection();
   }
 
   async checkEstimatedChangesSection(): Promise<void> {
     console.log('Verify estimated changes section is displayed');
     await this.driver.waitForSelector(this.estimatedChangesSection);
-  }
-
-  async checkSpendingCapSection(): Promise<void> {
-    console.log('Verify spending cap section is displayed');
-    await this.driver.waitForSelector(this.spendingCapSection);
-  }
-
-  async checkSpendingCapAmount(expectedAmount: string): Promise<void> {
-    console.log(`Verify spending cap amount ${expectedAmount} is displayed`);
-    await this.driver.waitForSelector({
-      css: 'p',
-      text: expectedAmount,
-    });
-  }
-
-  async checkSpenderSection(): Promise<void> {
-    console.log('Verify spender section is displayed');
-    await this.driver.waitForSelector(this.spenderSection);
-  }
-
-  async checkRequestFromSection(): Promise<void> {
-    console.log('Verify request from section is displayed');
-    await this.driver.waitForSelector(this.requestFromSection);
   }
 
   async checkInteractingWithSection(): Promise<void> {
@@ -90,12 +65,37 @@ class ERC20ApproveTransactionConfirmation extends TransactionConfirmation {
     await this.driver.waitForSelector(this.methodSection);
   }
 
-  async checkAdvancedDetailsSections(): Promise<void> {
-    console.log('Verify all advanced details sections are displayed');
-    await this.checkSpenderSection();
-    await this.checkRequestFromSection();
-    await this.checkInteractingWithSection();
-    await this.checkMethodSection();
+  async checkRequestFromSection(): Promise<void> {
+    console.log('Verify request from section is displayed');
+    await this.driver.waitForSelector(this.requestFromSection);
+  }
+
+  async checkSpenderSection(): Promise<void> {
+    console.log('Verify spender section is displayed');
+    await this.driver.waitForSelector(this.spenderSection);
+  }
+
+  async checkSpendingCapAmount(expectedAmount: string): Promise<void> {
+    console.log(`Verify spending cap amount ${expectedAmount} is displayed`);
+    await this.driver.waitForSelector({
+      css: 'p',
+      text: expectedAmount,
+    });
+  }
+
+  async checkSpendingCapPermissionDescription(): Promise<void> {
+    console.log('Verify spending cap permission description is displayed');
+    await this.driver.waitForSelector(this.spendingCapPermissionDescription);
+  }
+
+  async checkSpendingCapRequestTitle(): Promise<void> {
+    console.log('Verify spending cap request title is displayed');
+    await this.driver.waitForSelector(this.spendingCapRequestTitle);
+  }
+
+  async checkSpendingCapSection(): Promise<void> {
+    console.log('Verify spending cap section is displayed');
+    await this.driver.waitForSelector(this.spendingCapSection);
   }
 }
 
