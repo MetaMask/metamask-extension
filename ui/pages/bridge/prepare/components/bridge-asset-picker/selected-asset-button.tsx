7fe50f533de40c043066c82d51afcc09adb5c219
Micaela Estabillo <micaela.estabillo@consensys.net>
2025-12-11 11:48:29 -0800

    fix: style network icons

--- ui/pages/bridge/prepare/components/bridge-asset-picker/selected-asset-button.tsx
+++ ui/pages/bridge/prepare/components/bridge-asset-picker/selected-asset-button.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { formatChainIdToCaip } from '@metamask/bridge-controller';
 import {
   AvatarNetwork,
   AvatarNetworkSize,
@@ -32,44 +33,48 @@ export const SelectedAssetButton = ({
   ...props
 }: {
   asset: BridgeToken;
-} & SelectButtonProps<'div'>) => (
-  <SelectButton
-    borderRadius={BorderRadius.pill}
-    backgroundColor={BackgroundColor.backgroundDefault}
-    borderColor={BorderColor.borderMuted}
-    style={{
-      padding: 8,
-      paddingRight: 11,
-      minWidth: 'fit-content',
-    }}
-    size={SelectButtonSize.Lg}
-    alignItems={AlignItems.center}
-    descriptionProps={{
-      overflowWrap: OverflowWrap.BreakWord,
-      ellipsis: false,
-    }}
-    caretIconProps={{
-      name: IconName.ArrowDown,
-      style: { display: Display.None },
-    }}
-    label={asset.symbol}
-    startAccessory={
-      <BadgeWrapper
-        key={asset.assetId}
-        style={{ marginRight: 2 }}
-        badge={
-          <AvatarNetwork
-            name={NETWORK_TO_SHORT_NETWORK_NAME_MAP[asset.chainId]}
-            src={BRIDGE_CHAIN_ID_TO_NETWORK_IMAGE_MAP[asset.chainId]}
-            size={AvatarNetworkSize.Xs}
-            style={{ borderWidth: 1, borderRadius: 6 }}
-            hasBorder
-          />
-        }
-      >
-        <AvatarToken src={asset.image} name={asset.symbol} />
-      </BadgeWrapper>
-    }
-    {...props}
-  />
-);
+} & SelectButtonProps<'div'>) => {
+  const caipChainId = formatChainIdToCaip(asset.chainId);
+
+  return (
+    <SelectButton
+      borderRadius={BorderRadius.pill}
+      backgroundColor={BackgroundColor.backgroundDefault}
+      borderColor={BorderColor.borderMuted}
+      style={{
+        padding: 8,
+        paddingRight: 11,
+        minWidth: 'fit-content',
+      }}
+      size={SelectButtonSize.Lg}
+      alignItems={AlignItems.center}
+      descriptionProps={{
+        overflowWrap: OverflowWrap.BreakWord,
+        ellipsis: false,
+      }}
+      caretIconProps={{
+        name: IconName.ArrowDown,
+        style: { display: Display.None },
+      }}
+      label={asset.symbol}
+      startAccessory={
+        <BadgeWrapper
+          key={asset.assetId}
+          style={{ marginRight: 2 }}
+          badge={
+            <AvatarNetwork
+              name={NETWORK_TO_SHORT_NETWORK_NAME_MAP[caipChainId]}
+              src={BRIDGE_CHAIN_ID_TO_NETWORK_IMAGE_MAP[caipChainId]}
+              size={AvatarNetworkSize.Xs}
+              style={{ borderWidth: 1, borderRadius: 6 }}
+              hasBorder
+            />
+          }
+        >
+          <AvatarToken src={asset.image} name={asset.symbol} />
+        </BadgeWrapper>
+      }
+      {...props}
+    />
+  );
+};
