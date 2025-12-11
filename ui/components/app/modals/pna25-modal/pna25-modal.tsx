1152f2a5a6523c0097cc0fb13dbcbfc535e9b7d5
Howard Braham <howrad@gmail.com>
2025-12-10 22:07:34 -0300

    changed react-router-dom to react-router-dom-v5-compat

--- ui/components/app/modals/pna25-modal/pna25-modal.tsx
+++ ui/components/app/modals/pna25-modal/pna25-modal.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useContext, useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom-v5-compat';
 import {
   Box,
   BoxAlignItems,
