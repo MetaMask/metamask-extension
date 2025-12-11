96aa13b29390b904ee85dab1f8f5a9a663243c65
Amanda Yeoh <amanda.yeoh@consensys.net>
2025-12-11 11:46:44 +0800

    chore: fix cursor bug

--- ui/components/app/import-token/token-list/token-list-placeholder/token-list-placeholder.component.js
+++ ui/components/app/import-token/token-list/token-list-placeholder/token-list-placeholder.component.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import ZENDESK_URLS from '../../../../../helpers/constants/zendesk-url';
 import { useI18nContext } from '../../../../../hooks/useI18nContext';
 import { useTheme } from '../../../../../hooks/useTheme';
@@ -14,15 +14,16 @@ export default function TokenListPlaceholder() {
       ? '/images/empty-state-tokens-dark.png'
       : '/images/empty-state-tokens-light.png';
 
+  const handleLearnMore = useCallback(() => {
+    global.platform.openTab({ url: ZENDESK_URLS.ADD_CUSTOM_TOKENS });
+  }, []);
+
   return (
     <TabEmptyState
       icon={<img src={tokenIcon} alt={t('tokens')} width={72} height={72} />}
       description={t('addAcquiredTokens')}
       actionButtonText={t('learnMoreUpperCase')}
-      actionButtonProps={{
-        href: ZENDESK_URLS.ADD_CUSTOM_TOKENS,
-        externalLink: true,
-      }}
+      onAction={handleLearnMore}
       className="mx-auto"
     />
   );
