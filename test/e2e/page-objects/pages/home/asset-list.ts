4b71014c2fe56d985e3c7d8b94ae95bff5123fca
seaona <mariona@gmx.es>
2025-12-12 12:37:23 +0100

    fix

--- test/e2e/page-objects/pages/home/asset-list.ts
+++ test/e2e/page-objects/pages/home/asset-list.ts
@@ -348,7 +348,7 @@ class AssetListPage {
     await this.driver.clickElement(this.tokenOptionsButton);
     await this.driver.clickElement(this.importTokensButton);
     await this.driver.waitForSelector(this.importTokenModalTitle);
-    await this.driver.fill(this.tokenSearchInput, tokenName);
+    await this.driver.pasteIntoField(this.tokenSearchInput, tokenName);
     // Wait until the token search matches 1 result to prevent flakiness with token result re-renders
     await this.waitUntilTokenSearchMatch(1);
     await this.driver.clickElement({ text: tokenName, tag: 'p' });
