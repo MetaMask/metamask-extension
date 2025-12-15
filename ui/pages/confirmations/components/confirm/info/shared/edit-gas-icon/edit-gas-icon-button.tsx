536cb5cfbf59ba1ca726d76a5dffbed7f85a08b7
Goktug Poyraz <omergoktugpoyraz@gmail.com>
2025-12-15 13:36:26 +0100

    Enable gas modals

--- ui/pages/confirmations/components/confirm/info/shared/edit-gas-icon/edit-gas-icon-button.tsx
+++ ui/pages/confirmations/components/confirm/info/shared/edit-gas-icon/edit-gas-icon-button.tsx
@@ -1,53 +1,48 @@
-import React, { Dispatch, SetStateAction } from 'react';
+import React, { useState, useCallback } from 'react';
+import { TransactionMeta } from '@metamask/transaction-controller';
 import {
   Button,
   ButtonSize,
   ButtonVariant,
   IconName,
 } from '../../../../../../../components/component-library';
-import { useTransactionModalContext } from '../../../../../../../contexts/transaction-modal';
 import { IconColor } from '../../../../../../../helpers/constants/design-system';
 import { useTransactionEventFragment } from '../../../../../hooks/useTransactionEventFragment';
+import { GasFeeModal } from '../../../../modals/gas-fee-modal/gas-fee-modal';
+import { useConfirmContext } from '../../../../../context/confirm';
 
-export const EditGasIconButton = ({
-  supportsEIP1559,
-  setShowCustomizeGasPopover,
-}: {
-  supportsEIP1559: boolean;
-  setShowCustomizeGasPopover: Dispatch<SetStateAction<boolean>>;
-}) => {
-  const { openModal } = useTransactionModalContext() as {
-    openModal: (modalId: string) => void;
-  };
+export const EditGasIconButton = (): JSX.Element => {
+  const [isGasModalsVisible, setIsGasModalsVisible] = useState(false);
+  const { currentConfirmation: transactionMeta } =
+    useConfirmContext<TransactionMeta>();
   const { updateTransactionEventFragment } = useTransactionEventFragment();
 
-  const openEditEIP1559TxGasFeeModal = () => {
-    updateTransactionEventFragment({
-      // TODO: Fix in https://github.com/MetaMask/metamask-extension/issues/31860
-      // eslint-disable-next-line @typescript-eslint/naming-convention
-      gas_edit_attempted: 'basic',
-    });
-    openModal('editGasFee');
-  };
-
-  const openEditGasFeeLegacyTxModal = () => {
-    setShowCustomizeGasPopover(true);
-  };
-
-  const openEditGasFeeModal = () =>
-    supportsEIP1559
-      ? openEditEIP1559TxGasFeeModal()
-      : openEditGasFeeLegacyTxModal();
+  const openGasFeeModal = useCallback(() => {
+    updateTransactionEventFragment(
+      {
+        // TODO: Fix in https://github.com/MetaMask/metamask-extension/issues/31860
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        gas_edit_attempted: 'basic',
+      },
+      transactionMeta.id,
+    );
+    setIsGasModalsVisible(true);
+  }, [updateTransactionEventFragment, transactionMeta.id]);
 
   return (
-    <Button
-      style={{ textDecoration: 'none' }}
-      size={ButtonSize.Auto}
-      variant={ButtonVariant.Link}
-      startIconName={IconName.Edit}
-      color={IconColor.primaryDefault}
-      data-testid="edit-gas-fee-icon"
-      onClick={openEditGasFeeModal}
-    />
+    <>
+      <Button
+        style={{ textDecoration: 'none' }}
+        size={ButtonSize.Auto}
+        variant={ButtonVariant.Link}
+        startIconName={IconName.Edit}
+        color={IconColor.primaryDefault}
+        data-testid="edit-gas-fee-icon"
+        onClick={openGasFeeModal}
+      />
+      {isGasModalsVisible && (
+        <GasFeeModal setGasModalVisible={setIsGasModalsVisible} />
+      )}
+    </>
   );
 };
