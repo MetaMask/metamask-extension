1533b8fa4e3c8312659e7fb393ade1f2c4d5fbf4
Jeff Smale <6363749+jeffsmale90@users.noreply.github.com>
2025-12-15 16:43:18 +1300

    Add comment explaining limitation of isBlocked state variable.

--- app/scripts/lib/rpcBlockingMiddleware.ts
+++ app/scripts/lib/rpcBlockingMiddleware.ts
@@ -12,10 +12,18 @@ type CreateRpcBlockingMiddlewareOptions = {
   errorMessage?: string;
   /**
    * State of the middleware.
+   * Note: `isBlocked` is not concurrent safe - an operation may set `isBlocked` to `false` while a prior operation is still in progress, prematurely unblocking the middleware.
    */
   state: { isBlocked: boolean };
 };
 
+/**
+ * Creates a JsonRpcMiddleware function that blocks requests to snaps when the state isBlocked is true.
+ * @param options - The options for the middleware.
+ * @param options.errorMessage - The error message to return when requests are blocked.
+ * @param options.state - The state of the middleware.
+ * @returns A JsonRpcMiddleware function that blocks requests to snaps when the state isBlocked is true.
+ */
 export default function createRpcBlockingMiddleware({
   errorMessage = 'Cannot process requests at this time',
   state,
