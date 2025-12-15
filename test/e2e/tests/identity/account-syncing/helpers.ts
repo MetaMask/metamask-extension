c5ae99906d50d03c1c4733bccd21bccca71178ad
seaona <mariona@gmx.es>
2025-12-15 11:13:51 +0100

    lint

--- test/e2e/tests/identity/account-syncing/helpers.ts
+++ test/e2e/tests/identity/account-syncing/helpers.ts
@@ -14,7 +14,6 @@ export const arrangeTestUtils = (
   driver: Driver,
   userStorageMockttpController: UserStorageMockttpController,
 ) => {
-
   const prepareEventsEmittedCounter = (
     event: AsEnum<typeof UserStorageMockttpControllerEvents>,
   ) => {
