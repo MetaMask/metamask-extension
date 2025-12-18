b9d196a28bbd0fc9cf929c8cc1bdae6df2ed365a
Goktug Poyraz <omergoktugpoyraz@gmail.com>
2025-12-18 11:56:34 +0100

    Update file name

--- test/e2e/run-all.ts
+++ test/e2e/run-all.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import { hideBin } from 'yargs/helpers';
 import yargs from 'yargs/yargs';
-import { extractPassedTestPaths } from '../../.github/scripts/extract-failed-tests';
+import { extractPassedTestPaths } from '../../.github/scripts/extract-passed-tests';
 import {
   formatTime,
   normalizeTestPath,
