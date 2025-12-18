690ef08a90afc5174e77b9a0bdb69bc500e74d2d
Jyoti Puri <jyotipuri@gmail.com>
2025-12-18 14:56:41 +0530

    update

--- ui/pages/confirmations/context/confirm/index.tsx
+++ ui/pages/confirmations/context/confirm/index.tsx
@@ -2,6 +2,7 @@ import React, {
   ReactElement,
   createContext,
   useContext,
+  useEffect,
   useMemo,
   useState,
 } from 'react';
@@ -9,6 +10,9 @@ import React, {
 import useCurrentConfirmation from '../../hooks/useCurrentConfirmation';
 import useSyncConfirmPath from '../../hooks/useSyncConfirmPath';
 import { Confirmation } from '../../types/confirm';
+import { usePrevious } from '../../../../hooks/usePrevious';
+import { DEFAULT_ROUTE } from '../../../../helpers/constants/routes';
+import { useNavigate } from 'react-router-dom';
 
 export type ConfirmContextType = {
   currentConfirmation: Confirmation;
@@ -24,10 +28,18 @@ export const ConfirmContextProvider: React.FC<{
   children: ReactElement;
   confirmationId?: string;
 }> = ({ children, confirmationId }) => {
+  const navigate = useNavigate();
   const [isScrollToBottomCompleted, setIsScrollToBottomCompleted] =
     useState(true);
   const { currentConfirmation } = useCurrentConfirmation(confirmationId);
+  const prevCurrentConfirmation = usePrevious(currentConfirmation);
   useSyncConfirmPath(currentConfirmation);
+
+  useEffect(() => {
+    if (prevCurrentConfirmation && !currentConfirmation) {
+      navigate(DEFAULT_ROUTE, { state: { stayOnHomePage: true } });
+    }
+  }, [currentConfirmation, prevCurrentConfirmation, navigate]);
 
   const value = useMemo(
     () => ({
