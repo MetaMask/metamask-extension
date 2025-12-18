8c4df42e61874f7e66fe67548c54a765126d4bf6
Tuna <tunguyenanh@tunatech.org>
2025-12-18 16:41:02 +0700

    feat: handle FORCE_AUTH_MATCH_BUILD for authentication env

---
+++ shared/modules/authentication/config.ts
@@ -0,0 +1,27 @@
+import { Env } from '@metamask/profile-sync-controller/sdk';
+import { ENVIRONMENT } from '../../../development/build/constants';
+
+/**
+ * Check if the force auth match build is enabled.
+ *
+ * @returns true if the force auth match build is enabled, false otherwise
+ */
+export function isForceAuthMatchBuild() {
+  return process.env.FORCE_AUTH_MATCH_BUILD?.toString() === 'true';
+}
+
+export function isDevEnvironment() {
+  return process.env.METAMASK_ENVIRONMENT === ENVIRONMENT.DEVELOPMENT;
+}
+
+export function loadAuthenticationConfig(): Env {
+  if (!isForceAuthMatchBuild()) {
+    return Env.PRD;
+  }
+
+  const buildType = process.env.METAMASK_BUILD_TYPE;
+  if (buildType === 'beta' || buildType === 'uat') {
+    return Env.UAT;
+  }
+  return Env.PRD;
+}
