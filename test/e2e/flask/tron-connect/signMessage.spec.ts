9b86bfc943d6b47d6bd4b0074cb28d2cbce2ee52
NikosFotiadis <nikosfotiadis0@gmail.com>
2025-12-19 18:26:13 +0200

    test: tron sign message e2e

---
+++ test/e2e/flask/tron-connect/signMessage.spec.ts
@@ -0,0 +1,52 @@
+import { withTronAccountSnap } from './common-tron';
+import { TestDappTron } from '../../page-objects/pages/test-dapp-tron';
+import { DEFAULT_TRON_ADDRESS, WINDOW_TITLES } from '../../constants';
+import {
+  DEFAULT_TRON_TEST_DAPP_FIXTURE_OPTIONS,
+  connectTronTestDapp,
+  clickConfirmButton,
+  assertSignedMessageIsValid
+} from './testHelpers';
+import { veryLargeDelayMs } from '../../helpers';
+
+describe('Tron Connect - Sign Message - e2e tests', function () {
+  describe(`Tron Connect - Sign Message`, function () {
+    it('Should be able to sign a message', async function () {
+      await withTronAccountSnap(
+        {
+          ...DEFAULT_TRON_TEST_DAPP_FIXTURE_OPTIONS,
+          title: this.test?.fullTitle(),
+        },
+        async (driver) => {
+        const messageToSign = 'Hello, world!';
+        const testDappTron = new TestDappTron(driver);
+
+        await testDappTron.openTestDappPage();
+        await testDappTron.checkPageIsLoaded();
+        await testDappTron.switchTo();
+
+        // 1. Connect
+        await connectTronTestDapp(driver, testDappTron);
+
+        const signMessageTest = await testDappTron.getSignMessageTest();
+        await signMessageTest.setMessage(messageToSign);
+
+        await signMessageTest.signMessage();
+
+        await driver.delay(veryLargeDelayMs);
+        await driver.switchToWindowWithTitle(WINDOW_TITLES.Dialog);
+        await clickConfirmButton(driver);
+
+        await driver.switchToWindowWithTitle(WINDOW_TITLES.TronTestDApp);
+
+        const signedMessage = await signMessageTest.getSignedMessage();
+
+        assertSignedMessageIsValid({
+          signature: signedMessage[0],
+          originalMessageString: messageToSign,
+          addressBase58: DEFAULT_TRON_ADDRESS,
+        });
+      });
+    });
+  })
+});
