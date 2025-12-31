22ed2ed240a897237b6823052e6b81ddf1821475
gantunesr <17601467+gantunesr@users.noreply.github.com>
2025-12-30 22:16:23 -0300

    chore: add card component

---
+++ ui/components/multichain/multi-srp/srp-card/index.ts
@@ -0,0 +1,1 @@
+export { SrpCard } from './srp-card';
---
+++ ui/components/multichain/multi-srp/srp-card/srp-card.tsx
@@ -0,0 +1,184 @@
+import React, { useState, useContext, useCallback } from 'react';
+import { useSelector } from 'react-redux';
+import type { AccountWalletId, AccountGroupId } from '@metamask/account-api';
+
+import { MetaMetricsContext } from '../../../../contexts/metametrics';
+import { useWalletInfo } from '../../../../hooks/multichain-accounts/useWalletInfo';
+import { useI18nContext } from '../../../../hooks/useI18nContext';
+import { getIsPrimarySeedPhraseBackedUp } from '../../../../ducks/metamask/metamask';
+import {
+  useSingleWalletAccountsBalanceCallback,
+} from '../../../../hooks/multichain-accounts/useWalletBalance';
+import {
+  MetaMetricsEventCategory,
+  MetaMetricsEventName,
+} from '../../../../../shared/constants/metametrics';
+
+import Card from '../../../ui/card';
+import {
+  Box,
+  IconName,
+  Icon,
+  Text,
+  IconSize,
+} from '../../../component-library';
+import {
+  JustifyContent,
+  Display,
+  TextColor,
+  FlexDirection,
+  AlignItems,
+  BlockSize,
+  TextVariant,
+  IconColor,
+} from '../../../../helpers/constants/design-system';
+import { SrpListItem } from '../srp-list';
+
+/**
+ * Props for the SrpCard component.
+ */
+type SrpCardProps = {
+  index: number;
+  walletId: AccountWalletId;
+  shouldTriggerBackup: boolean;
+  onActionComplete: (id: string, triggerBackup?: boolean) => void;
+  isSettingsPage?: boolean;
+  hideShowAccounts?: boolean;
+};
+
+export const SrpCard = ({
+  index,
+  walletId,
+  shouldTriggerBackup,
+  onActionComplete,
+  isSettingsPage = false,
+  hideShowAccounts = false,
+}: SrpCardProps) => {
+  const t = useI18nContext();
+  const trackEvent = useContext(MetaMetricsContext);
+  const { multichainAccounts, keyringId, isSRPBackedUp } =
+    useWalletInfo(walletId);
+  const isPrimarySeedPhraseBackedUp = useSelector(
+    getIsPrimarySeedPhraseBackedUp,
+  );
+  const [showAccounts, setShowAccounts] = useState<boolean>(false);
+  const walletAccountBalance = useSingleWalletAccountsBalanceCallback(walletId);
+
+  const showHideText = useCallback((index: number, numberOfAccounts: number): string => {
+    if (numberOfAccounts > 1) {
+      return showAccounts
+        ? t('SrpListHideAccounts', [numberOfAccounts])
+        : t('SrpListShowAccounts', [numberOfAccounts]);
+    }
+    return showAccounts
+      ? t('SrpListHideSingleAccount', [numberOfAccounts])
+      : t('SrpListShowSingleAccount', [numberOfAccounts]);
+  }, [showAccounts, t]);
+
+  return (
+    <Card
+      key={`srp-${keyringId}`}
+      data-testid={`hd-keyring-${keyringId}`}
+      onClick={() => {
+        trackEvent({
+          category: MetaMetricsEventCategory.Accounts,
+          event: MetaMetricsEventName.SecretRecoveryPhrasePickerClicked,
+          properties: {
+            // TODO: Fix in https://github.com/MetaMask/metamask-extension/issues/31860
+            // eslint-disable-next-line @typescript-eslint/naming-convention
+            button_type: 'srp_select',
+          },
+        });
+        onActionComplete(keyringId, shouldTriggerBackup);
+      }}
+      className="select-srp__container"
+      marginBottom={3}
+    >
+      <Box
+        display={Display.Flex}
+        flexDirection={FlexDirection.Row}
+        alignItems={AlignItems.center}
+        justifyContent={JustifyContent.spaceBetween}
+      >
+        <Box>
+          <Text variant={TextVariant.bodyMdMedium}>
+            {t('srpListName', [index + 1])}
+          </Text>
+          {!hideShowAccounts && (
+            <Text
+              variant={TextVariant.bodySm}
+              color={TextColor.primaryDefault}
+              className="srp-list__show-accounts"
+              data-testid={`srp-list-show-accounts-${index}`}
+              onClick={(event: React.MouseEvent) => {
+                event.stopPropagation();
+                trackEvent({
+                  category: MetaMetricsEventCategory.Accounts,
+                  event:
+                    MetaMetricsEventName.SecretRecoveryPhrasePickerClicked,
+                  properties: {
+                    // TODO: Fix in https://github.com/MetaMask/metamask-extension/issues/31860
+                    // eslint-disable-next-line @typescript-eslint/naming-convention
+                    button_type: 'details',
+                  },
+                });
+                setShowAccounts((prevState) => !prevState);
+              }}
+            >
+              {showHideText(index, multichainAccounts.length)}
+            </Text>
+          )}
+        </Box>
+        <Box
+          display={Display.Flex}
+          alignItems={AlignItems.center}
+          gap={1}
+        >
+          {isSettingsPage && (
+            <Text
+              variant={TextVariant.bodyMdMedium}
+              color={
+                shouldTriggerBackup
+                  ? TextColor.errorDefault
+                  : TextColor.textAlternative
+              }
+            >
+              {shouldTriggerBackup
+                ? t('srpListStateNotBackedUp')
+                : t('srpListStateBackedUp')}
+            </Text>
+          )}
+          <Icon
+            name={IconName.ArrowRight}
+            size={IconSize.Sm}
+            color={
+              shouldTriggerBackup && isSettingsPage
+                ? IconColor.errorDefault
+                : IconColor.iconAlternative
+            }
+          />
+        </Box>
+      </Box>
+      {showAccounts && (
+        <Box>
+          <Box
+            width={BlockSize.Full}
+            className="srp-list__divider"
+            marginTop={2}
+            marginBottom={2}
+          />
+          {multichainAccounts.map((group) => {
+            return (
+              <SrpListItem
+                key={`account-${group.id}`}
+                accountId={group.id}
+                accountName={group.metadata.name}
+                balance={walletAccountBalance(group.id) ?? ''}
+              />
+            );
+          })}
+        </Box>
+      )}
+    </Card>
+  );
+};
