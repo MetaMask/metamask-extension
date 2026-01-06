6bc101e47206b74bbf8c14c76cf16b572b0a571e
Monte Lai <monte.lai@consensys.net>
2026-01-06 20:53:19 +0800

    fix: temp iframe url

--- app/offscreen/ledger.ts
+++ app/offscreen/ledger.ts
@@ -97,7 +97,7 @@ function setupMessageListeners(iframe: HTMLIFrameElement) {
 export default async function init() {
   return new Promise<void>((resolve) => {
     const iframe = document.createElement('iframe');
-    iframe.src = 'https://localhost:5173/'; // https://metamask.github.io/ledger-iframe-bridge/9.0.1/';
+    iframe.src = 'https://montelaidev.github.io/ledger-iframe-bridge/10.0.1/'; // https://metamask.github.io/ledger-iframe-bridge/9.0.1/';
     console.log('IFRAME SRC', iframe.src);
     iframe.allow = 'hid';
     iframe.onload = () => {
