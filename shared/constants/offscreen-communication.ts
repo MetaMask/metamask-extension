8cc3f6a79a08b846b0c92263eb0d75ca426d9092
Monte Lai <monte.lai@consensys.net>
2026-01-06 21:11:31 +0800

    temp: update known origin for test

--- shared/constants/offscreen-communication.ts
+++ shared/constants/offscreen-communication.ts
@@ -101,5 +101,5 @@ export enum KnownOrigins {
   lattice = 'https://lattice.gridplus.io',
   // TODO: Fix in https://github.com/MetaMask/metamask-extension/issues/31860
   // eslint-disable-next-line @typescript-eslint/naming-convention
-  ledger = 'https://metamask.github.io',
+  ledger = 'https://montelaidev.github.io',
 }
