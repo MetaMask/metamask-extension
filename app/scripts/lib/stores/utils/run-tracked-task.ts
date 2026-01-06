60bdc781cc1191f1d891c98d089faafc52954214
David Murdoch <187813+davidmurdoch@users.noreply.github.com>
2026-01-06 12:48:53 -0500

    add comment

--- app/scripts/lib/stores/utils/run-tracked-task.ts
+++ app/scripts/lib/stores/utils/run-tracked-task.ts
@@ -38,11 +38,19 @@ export async function runTrackedTask<Return>(
           err instanceof Error ? err.message : String(err),
         );
 
-        // Creates an *error event* (Issue), separate from the span:
+        // Creates an *error event* (Issue), separate from the span.
         sentry.captureException(err, {
           tags: { 'task.name': taskName, 'task.status': 'failed' },
         });
 
+        // rethrow the error after capturing it so the process crashes, as
+        // we can't know how to recover from here. This will result in the
+        // exception being captured by sentry _again_ by the global error
+        // handler, and that's fine. We capture here because we want to add this
+        // taskName tag to the error event so we can easily find and filter it
+        // later. All this is sentry tracking code in here is temporary until we
+        // are confident that the migration code is stable enough, and we can
+        // roll it out to 100%.
         throw err;
       }
     },
