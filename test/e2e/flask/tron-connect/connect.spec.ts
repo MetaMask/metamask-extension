fb349d64019f69a82e1cc795d779e8e98db90315
Nikos Fotiadis <nikosfotiadis0@gmail.com>
2026-01-06 10:49:21 +0200

    test: tron connect refresh page test

--- test/e2e/flask/tron-connect/connect.spec.ts
+++ test/e2e/flask/tron-connect/connect.spec.ts
@@ -7,6 +7,7 @@ import {
   assertConnected,
   assertDisconnected
 } from './testHelpers';
+import { regularDelayMs } from '../../helpers';
 
 describe('Tron Connect - Connect & disconnect - e2e tests', function () {
   describe(`Tron Connect - Connect & disconnect`, function () {
@@ -36,7 +37,7 @@ describe('Tron Connect - Connect & disconnect - e2e tests', function () {
       });
     });
 
-    it.skip('Should be able to disconnect and connect again', async function () {
+    it('Should be able to disconnect and connect again', async function () {
       await withTronAccountSnap(
         {
           ...DEFAULT_TRON_TEST_DAPP_FIXTURE_OPTIONS,
@@ -75,6 +76,44 @@ describe('Tron Connect - Connect & disconnect - e2e tests', function () {
 
         const connectedAccountAfterConnect = await header.getAccount();
         assertConnected(connectedAccountAfterConnect, DEFAULT_TRON_ADDRESS_SHORT);
+      });
+    });
+
+    it('Should not disconnect the dapp after refreshing the page', async function () {
+      await withTronAccountSnap(
+        {
+          ...DEFAULT_TRON_TEST_DAPP_FIXTURE_OPTIONS,
+          title: this.test?.fullTitle(),
+        },
+        async (driver) => {
+        const testDappTron = new TestDappTron(driver);
+
+        await testDappTron.openTestDappPage();
+        await testDappTron.checkPageIsLoaded();
+        await testDappTron.switchTo();
+
+        await connectTronTestDapp(driver, testDappTron);
+        
+        const header = await testDappTron.getHeader();
+        const connectionStatus = await header.getConnectionStatus();
+
+        assertConnected(connectionStatus);
+        
+        const connectedAccount = await header.getAccount();
+
+        assertConnected(connectedAccount, DEFAULT_TRON_ADDRESS_SHORT);
+
+        await driver.refresh();
+
+        await testDappTron.checkPageIsLoaded();
+
+        await driver.delay(regularDelayMs);
+
+        const connectionStatusAfterRefresh = await header.getConnectionStatus();
+        assertConnected(connectionStatusAfterRefresh);
+
+        const connectedAccountAfterRefresh = await header.getAccount();
+        assertConnected(connectedAccountAfterRefresh, DEFAULT_TRON_ADDRESS_SHORT);
       });
     });
   })
