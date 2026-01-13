2e805b10bf6b61b7441038b1a9183217a74fbb54
Ganesh Suresh Patra <ganesh.patra@consensys.net>
2026-01-13 10:59:20 +0530

    fix: updated route for lock

--- ui/pages/routes/routes.component.tsx
+++ ui/pages/routes/routes.component.tsx
@@ -156,7 +156,7 @@ const OnboardingFlow = mmLazy(
   (() => import('../onboarding-flow/index.ts')) as unknown as DynamicImportType,
 );
 const Lock = mmLazy(
-  (() => import('../lock/index.js')) as unknown as DynamicImportType,
+  (() => import('../lock/index.ts')) as unknown as DynamicImportType,
 );
 const UnlockPage = mmLazy(
   (() => import('../unlock-page/index.ts')) as unknown as DynamicImportType,
