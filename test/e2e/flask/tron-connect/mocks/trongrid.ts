bc15bd66ab5c867f7e279c3e463dc08df0d2a1ca
Nikos Fotiadis <nikosfotiadis0@gmail.com>
2026-01-15 11:41:25 +0200

    chore: lint

--- test/e2e/flask/tron-connect/mocks/trongrid.ts
+++ test/e2e/flask/tron-connect/mocks/trongrid.ts
@@ -2,100 +2,9 @@ import { Mockttp } from 'mockttp';
 import { TRANSACTION_HASH_MOCK } from '../common-tron';
 
 const TRONGRID_API_URL = 'https://api.trongrid.io';
-const TRONGRID_SHASTA_API_URL = 'https://api.shasta.trongrid.io';
 
-export const mockGetBlock = (mockServer: Mockttp) =>
-  mockServer
-    .forPost(`${TRONGRID_SHASTA_API_URL}/wallet/getblock`)
-    .thenJson(200, {
-      blockID: 'xxxxxxxx',
-      block_header: {
-        raw_data: {
-          number: 60677731,
-          txTrieRoot:
-            '0000000000000000000000000000000000000000000000000000000000000000',
-          witness_address: '41ce9b5acfce023822bcdf302333668cce2ba60bca',
-          parentHash:
-            '00000000039dde62a6faec6860eb9c2271b497d97031f90f336ab564662fc005',
-          version: 32,
-          // Use a recent timestamp to satisfy freshness checks
-          timestamp: Date.now() - 2000,
-        },
-        witness_signature:
-          '354261801bf88973cc144d74d81f90e3ebeb8ea6029b42412757e7e996df6d3a2ad22db54675d77be3774cc067e47f374a9bc8ffbdeb0cb62c6057be26201c9000',
-      },
-    });
-
-export const mockBroadcastTransaction = (mockServer: Mockttp) =>
-  mockServer
-    .forPost(`${TRONGRID_SHASTA_API_URL}/wallet/broadcasttransaction`)
-    .thenJson(200, {
-      result: 'SUCCESS',
-      txid: TRANSACTION_HASH_MOCK,
-    });
-
-export const mockTriggerSmartContract = (mockServer: Mockttp) =>
-  mockServer
-    .forPost(`${TRONGRID_SHASTA_API_URL}/wallet/triggersmartcontract`)
-    .thenJson(200, {
-      result: {
-        result: true,
-      },
-      transaction: {
-        visible: false,
-        txID: 'dfb62695b5027a6c960666a1dcb560d706f9e401c44acfc8ec7a3191949fa21f',
-        raw_data: {
-          contract: [
-            {
-              parameter: {
-                value: {
-                  data: 'a9059cbb00000000000000000000000032f9c0c487f21716b7a8f12906b7528899026558000000000000000000000000000000000000000000000000000000000754d4c0',
-                  owner_address: '41588c5216750cceaad16cf5a757e3f7b32835a5e1',
-                  contract_address:
-                    '4142a1e39aefa49290f2b3f9ed688d7cecf86cd6e0',
-                },
-                type_url: 'type.googleapis.com/protocol.TriggerSmartContract',
-              },
-              type: 'TriggerSmartContract',
-            },
-          ],
-          ref_block_bytes: '60fb',
-          ref_block_hash: 'cdef6776380058bf',
-          expiration: 1766159955000,
-          fee_limit: 100000000,
-          timestamp: 1766159895515,
-        },
-        raw_data_hex:
-          '0a0260fb2208cdef6776380058bf40b8e8d8bab3335aae01081f12a9010a31747970652e676f6f676c65617069732e636f6d2f70726f746f636f6c2e54726967676572536d617274436f6e747261637412740a1541588c5216750cceaad16cf5a757e3f7b32835a5e112154142a1e39aefa49290f2b3f9ed688d7cecf86cd6e02244a9059cbb00000000000000000000000032f9c0c487f21716b7a8f12906b7528899026558000000000000000000000000000000000000000000000000000000000754d4c070db97d5bab333900180c2d72f',
-      },
-    });
-
-export const mockAccountRequest = (mockServer: Mockttp) =>
-  mockServer
-    .forGet(
-      `${TRONGRID_API_URL}/v1/accounts/TJ3QZbBREK1Xybe1jf4nR9Attb8i54vGS3`,
-    )
-    .thenJson(200, accountsResponse);
-
-export const mockTransactionsRequest = (mockServer: Mockttp) =>
-  mockServer
-    .forGet(
-      `${TRONGRID_API_URL}/v1/accounts/TJ3QZbBREK1Xybe1jf4nR9Attb8i54vGS3/transactions`,
-    )
-    .thenJson(200, transactionsResponse);
-
-export const mockTransactionsTRC20Request = (mockServer: Mockttp) =>
-  mockServer
-    .forGet(
-      `${TRONGRID_API_URL}/v1/accounts/TJ3QZbBREK1Xybe1jf4nR9Attb8i54vGS3/transactions/trc20`,
-    )
-    .thenJson(200, thisransactionsTRC20Response);
-
-export const mockAccountResourcesRequest = (mockServer: Mockttp) =>
-  mockServer
-    .forPost(`${TRONGRID_API_URL}/wallet/getaccountresource`)
-    .thenJson(200, accountResourcesResponse);
-
+// We disable this rule because the resposes we mock are using snake_case
+/* eslint-disable @typescript-eslint/naming-convention */
 const accountsResponse = {
   data: [
     {
@@ -246,3 +155,93 @@ const accountResourcesResponse = {
   TotalEnergyLimit: 180000000000,
   TotalEnergyWeight: 19125511029,
 };
+
+export const mockGetBlock = (mockServer: Mockttp) =>
+  mockServer
+    .forPost(`${TRONGRID_API_URL}/wallet/getblock`)
+    .thenJson(200, {
+      blockID: 'xxxxxxxx',
+      block_header: {
+        raw_data: {
+          number: 60677731,
+          txTrieRoot:
+            '0000000000000000000000000000000000000000000000000000000000000000',
+          witness_address: '41ce9b5acfce023822bcdf302333668cce2ba60bca',
+          parentHash:
+            '00000000039dde62a6faec6860eb9c2271b497d97031f90f336ab564662fc005',
+          version: 32,
+          // Use a recent timestamp to satisfy freshness checks
+          timestamp: Date.now() - 2000,
+        },
+        witness_signature:
+          '354261801bf88973cc144d74d81f90e3ebeb8ea6029b42412757e7e996df6d3a2ad22db54675d77be3774cc067e47f374a9bc8ffbdeb0cb62c6057be26201c9000',
+      },
+    });
+
+export const mockBroadcastTransaction = (mockServer: Mockttp) =>
+  mockServer
+    .forPost(`${TRONGRID_API_URL}/wallet/broadcasttransaction`)
+    .thenJson(200, {
+      result: 'SUCCESS',
+      txid: TRANSACTION_HASH_MOCK,
+    });
+
+export const mockTriggerSmartContract = (mockServer: Mockttp) =>
+  mockServer
+    .forPost(`${TRONGRID_API_URL}/wallet/triggersmartcontract`)
+    .thenJson(200, {
+      result: {
+          result: true
+      },
+      transaction: {
+          visible: false,
+          txID: "c58a836f6a2a06f56b3fa35cc64513499c7b3ee4b1045760a66128842868f49c",
+          raw_data: {
+              contract: [
+                  {
+                      parameter: {
+                          value: {
+                              data: "a9059cbb00000000000000000000000032f9c0c487f21716b7a8f12906b7528899026558000000000000000000000000000000000000000000000000000000000754d4c0",
+                              owner_address: "41588c5216750cceaad16cf5a757e3f7b32835a5e1",
+                              contract_address: "41a614f803b6fd780986a42c78ec9c7f77e6ded13c"
+                          },
+                          type_url: "type.googleapis.com/protocol.TriggerSmartContract"
+                      },
+                      type: "TriggerSmartContract"
+                  }
+              ],
+              ref_block_bytes: "8b15",
+              ref_block_hash: "5fca48bd51bf3f1d",
+              expiration: 1768469721000,
+              fee_limit: 100000000,
+              timestamp: 1768469664343
+          },
+          raw_data_hex: "0a028b1522085fca48bd51bf3f1d40a8df8988bc335aae01081f12a9010a31747970652e676f6f676c65617069732e636f6d2f70726f746f636f6c2e54726967676572536d617274436f6e747261637412740a1541588c5216750cceaad16cf5a757e3f7b32835a5e1121541a614f803b6fd780986a42c78ec9c7f77e6ded13c2244a9059cbb00000000000000000000000032f9c0c487f21716b7a8f12906b7528899026558000000000000000000000000000000000000000000000000000000000754d4c070d7a48688bc33900180c2d72f"
+      }
+    });
+
+export const mockAccountRequest = (mockServer: Mockttp) =>
+  mockServer
+    .forGet(
+      `${TRONGRID_API_URL}/v1/accounts/TJ3QZbBREK1Xybe1jf4nR9Attb8i54vGS3`,
+    )
+    .thenJson(200, accountsResponse);
+
+export const mockTransactionsRequest = (mockServer: Mockttp) =>
+  mockServer
+    .forGet(
+      `${TRONGRID_API_URL}/v1/accounts/TJ3QZbBREK1Xybe1jf4nR9Attb8i54vGS3/transactions`,
+    )
+    .thenJson(200, transactionsResponse);
+
+export const mockTransactionsTRC20Request = (mockServer: Mockttp) =>
+  mockServer
+    .forGet(
+      `${TRONGRID_API_URL}/v1/accounts/TJ3QZbBREK1Xybe1jf4nR9Attb8i54vGS3/transactions/trc20`,
+    )
+    .thenJson(200, thisransactionsTRC20Response);
+
+export const mockAccountResourcesRequest = (mockServer: Mockttp) =>
+  mockServer
+    .forPost(`${TRONGRID_API_URL}/wallet/getaccountresource`)
+    .thenJson(200, accountResourcesResponse);
