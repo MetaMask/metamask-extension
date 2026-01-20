d16058086c0c79a93368fb48c7572aaf9a011506
Francis Nepomuceno <n3ps@users.noreply.github.com>
2026-01-19 22:05:26 -0500

    attempt

--- ui/components/multichain/account-overview/account-overview-tabs.tsx
+++ ui/components/multichain/account-overview/account-overview-tabs.tsx
@@ -154,6 +154,7 @@ export const AccountOverviewTabs = ({
       <AssetListTokenDetection />
 
       <Tabs<AccountOverviewTab>
+        defaultActiveTabKey={activeTabKey ?? undefined}
         activeTabKey={activeTabKey}
         onTabClick={handleTabClick}
         tabListProps={{
