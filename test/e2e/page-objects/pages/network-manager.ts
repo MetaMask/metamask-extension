a5008de534c1519f3e41f88edc169caa946c21c9
Patryk Lucka <5708018+PatrykLucka@users.noreply.github.com>
2026-01-20 16:03:38 +0100

    refactor: enhance selector logic for selected and deselected network list items in NetworkManager

--- test/e2e/page-objects/pages/network-manager.ts
+++ test/e2e/page-objects/pages/network-manager.ts
@@ -24,10 +24,10 @@ class NetworkManager {
     '[data-testid="network-manager-select-all"]';
 
   private readonly selectedNetworkListItem = (selector: string) =>
-    `${selector}.multichain-network-list-item--selected`;
+    `:is(${selector}.multichain-network-list-item--selected, ${selector} .multichain-network-list-item--selected)`;
 
   private readonly deselectedNetworkListItem = (selector: string) =>
-    `${selector}.multichain-network-list-item--deselected`;
+    `:is(${selector}.multichain-network-list-item--deselected, ${selector} .multichain-network-list-item--deselected)`;
 
   private readonly networkListItem = (networkName: string) =>
     `[data-testid="network-list-item-${networkName}"]`;
