d6529c20e5fb18fa3fd03e1934806c77519f2f35
Matthew Walsh <matthew.walsh@consensys.net>
2026-01-22 13:49:01 +0000

    Test: add back component map entry

--- ui/pages/confirmations/components/confirm/info/info.tsx
+++ ui/pages/confirmations/components/confirm/info/info.tsx
@@ -10,7 +10,7 @@ import { SignatureRequestType } from '../../../types/confirm';
 import { AddEthereumChain } from '../../../external/add-ethereum-chain/add-ethereum-chain';
 import { ConfirmInfoSection } from '../../../../../components/app/confirm/info/row/section';
 import { Skeleton } from '../../../../../components/component-library/skeleton';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { EXAMPLE_CUSTOM_AMOUNT_TRANSACTION_TYPE } from '../../developer/confirmations-developer-options';
 import { ExampleCustomAmountConfirmation } from '../../developer/example-custom-amount-confirmation';
 import ApproveInfo from './approve/approve';
 import BaseTransactionInfo from './base-transaction-info/base-transaction-info';
@@ -77,6 +77,9 @@ const Info = () => {
       [TransactionType.tokenMethodTransferFrom]: () => NFTTokenTransferInfo,
 
       [ApprovalType.AddEthereumChain]: () => AddEthereumChain,
+
+      [EXAMPLE_CUSTOM_AMOUNT_TRANSACTION_TYPE]: () =>
+        ExampleCustomAmountConfirmation,
     }),
     [currentConfirmation],
   );
