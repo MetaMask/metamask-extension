34ffb462f405e15bd9452e65e227e621f9755fec
Charly Chevalier <charly.chevalier@consensys.net>
2026-01-22 18:44:14 +0100

    refactor: still use main account of overview

--- ui/components/multichain/account-overview/account-overview.tsx
+++ ui/components/multichain/account-overview/account-overview.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { isEqual } from 'lodash';
 import {
   EthMethod,
   BtcMethod,
   SolMethod,
-  TrxAccountType,
   TrxMethod,
+  isEvmAccountType,
 } from '@metamask/keyring-api';
-import { ALLOWED_BRIDGE_CHAIN_IDS } from '@metamask/bridge-controller';
 import {
-  getSelectedInternalAccount,
   getIsDefiPositionsEnabled,
   isBalanceCached,
   getIsSwapsChain,
@@ -23,10 +20,7 @@ import {
 } from '../../../selectors/multichain';
 import { getSelectedMultichainNetworkConfiguration } from '../../../selectors/multichain/networks';
 import { CoinOverview } from '../../app/wallet-overview/coin-overview';
-import {
-  getInternalAccountsFromSelectedAccountGroup,
-  getSelectedAccountGroup,
-} from '../../../selectors/multichain-accounts/account-tree';
+import { getInternalAccountsFromSelectedAccountGroup } from '../../../selectors/multichain-accounts/account-tree';
 import { AccountOverviewLayout } from './account-overview-layout';
 import { AccountOverviewCommonProps } from './common';
 
@@ -37,20 +31,25 @@ export type AccountOverviewProps = AccountOverviewCommonProps & {
 // TODO: Fix in https://github.com/MetaMask/metamask-extension/issues/31860
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export function AccountOverview(props: AccountOverviewProps) {
-  // Get account and network info
+  // Get accounts
   const accounts = useSelector(getInternalAccountsFromSelectedAccountGroup);
+
+  // Get network info
   const { chainId } = useSelector(getSelectedMultichainNetworkConfiguration);
+  const isEvm = useSelector(getMultichainIsEvm);
 
   // Get balance info
   const balance = useSelector(getMultichainSelectedAccountCachedBalance);
+  const balanceIsCachedForEvm = useSelector(isBalanceCached);
   // Only valid for EVM accounts, but all account groups have at least one EVM account.
-  const balanceIsCached = useSelector(isBalanceCached);
+  const balanceIsCached = isEvm ? balanceIsCachedForEvm : false;
 
   // Get chain capabilities
   const isBuyableChain = useSelector(getIsNativeTokenBuyable);
-  const isSwapsOrBridgeChain = (
-    ALLOWED_BRIDGE_CHAIN_IDS as readonly string[]
-  ).includes(chainId);
+  const isSwapsChain = useSelector((state) => getIsSwapsChain(state, chainId));
+  const isBridgeChain = useSelector((state) =>
+    getIsBridgeChain(state, chainId),
+  );
 
   // Check signing capability across all account types
   const isSigningEnabled = accounts.some(
@@ -65,6 +64,13 @@ export function AccountOverview(props: AccountOverviewProps) {
   // Get tab visibility
   const defiPositionsEnabled = useSelector(getIsDefiPositionsEnabled);
 
+  // Get main account of that group
+  // NOTE: This is still widely used accross all our components.
+  // TODO: This needs to be refactored in the future to support multiple
+  // accounts properly (with account groups).
+  const account =
+    accounts.find(({ type }) => isEvmAccountType(type)) ?? accounts[0];
+
   return (
     <AccountOverviewLayout
       showTokens={true}
@@ -74,13 +80,13 @@ export function AccountOverview(props: AccountOverviewProps) {
       {...props}
     >
       <CoinOverview
-        account={accounts[0]}
+        account={account}
         balance={balance}
         balanceIsCached={balanceIsCached}
         chainId={chainId}
         isSigningEnabled={isSigningEnabled}
-        isSwapsChain={isSwapsOrBridgeChain}
-        isBridgeChain={isSwapsOrBridgeChain}
+        isSwapsChain={isSwapsChain}
+        isBridgeChain={isBridgeChain}
         isBuyableChain={isBuyableChain}
       />
     </AccountOverviewLayout>
