5f2c42b198043cf812818c50e03b9c38c3bbdd28
bizk <santiago.yanzon1999@gmail.com>
2026-01-23 10:23:57 -0300

    chore: name refactor on Tempo image naming

--- shared/constants/network.ts
+++ shared/constants/network.ts
@@ -719,7 +719,7 @@ export const MSU_NATIVE_TOKEN_IMAGE_URL = './images/msu-native.svg';
 export const BOB_IMAGE_URL = './images/bob.svg';
 export const ROOTSTOCK_IMAGE_URL = './images/rootstock.svg';
 export const ROOTSTOCK_NATIVE_TOKEN_IMAGE_URL = './images/rootstock-native.svg';
-export const TEMPO_PATHUSD_TESTNET_IMAGE_URL = './images/tempo-path-usd.svg';
+export const TEMPO_TESTNET_TOKEN_IMAGE_URL = './images/tempo-path-usd.svg';
 export const TEMPO_TESTNET_IMAGE_URL = './images/tempo-testnet.svg';
 
 export const INFURA_PROVIDER_TYPES = [
