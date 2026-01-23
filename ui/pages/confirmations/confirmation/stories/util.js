de9be8487654e616d5db3100a8dc6879f1df8b55
Charly Chevalier <charly.chevalier@consensys.net>
2026-01-23 18:37:23 +0100

    fix: add missing tokenBalances in confirmations storybook state

--- ui/pages/confirmations/confirmation/stories/util.js
+++ ui/pages/confirmations/confirmation/stories/util.js
@@ -63,6 +63,7 @@ const STORE_MOCK = {
       },
     },
     tokenList: {},
+    tokenBalances: testData.metamask.tokenBalances,
     internalAccounts: testData.metamask.internalAccounts,
     accountsByChainId: testData.metamask.accountsByChainId,
     accountTree: testData.metamask.accountTree,
