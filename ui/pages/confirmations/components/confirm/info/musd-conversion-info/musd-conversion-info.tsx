f3bd8c5e04de5f572562bf0b281d7abc1635753c
Matthew Walsh <matthew.walsh@consensys.net>
2026-01-26 18:41:29 +0000

    feat(confirmations): integrate useAddToken with perps and mUSD deposits
    - Fix TypeScript compilation errors in useAddToken.test.ts by updating mock return types
    - Integrate useAddToken hook with perps-deposit-info component for USDC on Arbitrum
    - Integrate useAddToken hook with musd-conversion-info component for MUSD on Mainnet
    - Fix bug in musd-conversion-button where token address was incorrectly used as recipient

--- ui/pages/confirmations/components/confirm/info/musd-conversion-info/musd-conversion-info.tsx
+++ ui/pages/confirmations/components/confirm/info/musd-conversion-info/musd-conversion-info.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import { useAddToken } from '../../../../hooks/tokens';
 import { CustomAmountInfo } from '../../../info/custom-amount-info';
+import { MAINNET_MUSD } from '../../../../constants/musd';
 
 export const MusdConversionInfo = () => {
+  useAddToken({
+    chainId: MAINNET_MUSD.chainId,
+    decimals: MAINNET_MUSD.decimals,
+    name: MAINNET_MUSD.name,
+    symbol: MAINNET_MUSD.symbol,
+    tokenAddress: MAINNET_MUSD.address,
+  });
+
   return <CustomAmountInfo hasMax />;
 };
