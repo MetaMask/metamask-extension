149f40f93da47cb3b7db027e9e47dbc040b157f6
Micaela Estabillo <micaela.estabillo@consensys.net>
2026-01-26 16:03:13 -0800

    chore: add cache management utils

---
+++ ui/pages/bridge/utils/cache.ts
@@ -0,0 +1,90 @@
+import { MINUTE } from '@metamask/controller-utils';
+import { createHash } from 'crypto';
+import localforage from 'localforage';
+import {
+  getStorageItem,
+  removeStorageItem,
+  setStorageItem,
+} from '../../../../shared/lib/storage-helpers';
+
+const BRIDGE_CACHE_PREFIX = 'bridgeCache';
+const MAX_CACHE_AGE = MINUTE * 15;
+const DEFAULT_CACHE_PAGE_KEY = 'default';
+
+const hashData = (body: object) => {
+  return createHash('sha256').update(JSON.stringify(body)).digest('hex');
+};
+
+export const getCacheKey = (url: string, body: object) => {
+  const bodyHash = hashData(body);
+  return `${BRIDGE_CACHE_PREFIX}:${url}:${bodyHash}`;
+};
+
+const isStale = (cachedItem: { lastUpdated: number }) =>
+  cachedItem.lastUpdated &&
+  Date.now() - cachedItem.lastUpdated >= MAX_CACHE_AGE;
+
+export const retrieveCachedResponse = async (
+  cacheKey: string,
+  after: string = DEFAULT_CACHE_PAGE_KEY,
+) => {
+  try {
+    const storageItem = await getStorageItem(cacheKey);
+    const cachedPage = storageItem?.[after];
+
+    if (cachedPage) {
+      const { hash, cachedResponse } = cachedPage;
+      const isDataInvalid = hash !== hashData(cachedResponse);
+      if (isStale(storageItem) || isDataInvalid) {
+        await removeStorageItem(cacheKey);
+        return null;
+      }
+      return cachedResponse;
+    }
+  } catch (error) {
+    // Invalidate cache if error occurs during retrieval
+    await removeStorageItem(cacheKey);
+  }
+};
+
+export const updateCache = async (
+  response: object,
+  cacheKey: string,
+  after: string = DEFAULT_CACHE_PAGE_KEY,
+) => {
+  try {
+    const newCachedPage = {
+      cachedResponse: response,
+      hash: hashData(response),
+    };
+
+    const storageItem = await getStorageItem(cacheKey);
+    await setStorageItem(cacheKey, {
+      ...(storageItem ? storageItem : {}),
+      [after]: newCachedPage,
+      lastUpdated: Date.now(),
+    });
+  } catch (error) {
+    // Invalidate cache if update fails
+    await removeStorageItem(cacheKey);
+  }
+};
+
+/**
+ * This function is called when the bridge page is unloaded or reset.
+ * It removes all bridge cache items that are either stale or are search results.
+ */
+export const clearAllBridgeCacheItems = async () => {
+  const cacheKeys = await localforage.keys();
+  await Promise.allSettled(
+    cacheKeys
+      .filter(Boolean)
+      .filter((key) => key.startsWith(BRIDGE_CACHE_PREFIX))
+      .map(async (key) => {
+        const cachedItem = await getStorageItem(key);
+        if (isStale(cachedItem) || key.includes('search')) {
+          await removeStorageItem(key);
+        }
+      }),
+  );
+};
