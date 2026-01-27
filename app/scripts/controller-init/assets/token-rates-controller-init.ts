69a60e1779d7aeda40dd2df8200689298475c06d
salimtb <salim.toubal@consensys.net>
2026-01-27 11:23:00 +0100

    fix: clean up

--- app/scripts/controller-init/assets/token-rates-controller-init.ts
+++ app/scripts/controller-init/assets/token-rates-controller-init.ts
@@ -30,7 +30,6 @@ export const TokenRatesControllerInit: ControllerInitFunction<
     state: persistedState.TokenRatesController,
     tokenPricesService: new CodefiTokenPricesServiceV2(),
     disabled: !preferencesState.useCurrencyRateCheck,
-    interval: 10_000,
   });
 
   initMessenger.subscribe(
