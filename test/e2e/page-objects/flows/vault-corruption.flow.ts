fb3bad2c72716c94f73e5059ffc189f317c7a451
David Murdoch <david@davidmurdoch.com>
2026-01-27 18:02:56 -0500

    remove delay

--- test/e2e/page-objects/flows/vault-corruption.flow.ts
+++ test/e2e/page-objects/flows/vault-corruption.flow.ts
@@ -82,7 +82,6 @@ export async function onboardThenTriggerCorruptionFlow(
     participateInMetaMetrics,
     skipSRPBackup: true,
   });
-  await driver.delay(5000);
 
   const homePage = new HomePage(driver);
   await homePage.checkPageIsLoaded();
