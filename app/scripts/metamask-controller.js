8bdd38f838d1051108bd41e3022798b7da68ed31
sahar-fehri <sahar.fehri@consensys.net>
2026-01-29 18:37:54 +0100

    chore: upgrade assets-controllers to v99

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -8374,7 +8374,6 @@ export default class MetamaskController extends EventEmitter {
 
   toggleExternalServices(useExternal) {
     this.preferencesController.toggleExternalServices(useExternal);
-    this.tokenListController.updatePreventPollingOnNetworkRestart(!useExternal);
     const subscriptionState = this.controllerMessenger.call(
       'SubscriptionController:getState',
     );
