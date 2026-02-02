06524828c6a2d31eaf59f1a373eaff8b94e33d16
Norbert Elter <72046715+itsyoboieltr@users.noreply.github.com>
2026-02-03 03:36:58 +0400

    fix: better description

--- development/webpack/webpack.config.ts
+++ development/webpack/webpack.config.ts
@@ -120,7 +120,7 @@ const plugins: WebpackPluginInstance[] = [
     // eslint-disable-next-line @typescript-eslint/naming-convention
     manifest_version: MANIFEST_VERSION,
     description: commitHash
-      ? `${args.env} ${args.type} build from git id: ${commitHash.substring(0, 8)}`
+      ? `${args.type} build for ${args.env} from git id: ${commitHash.substring(0, 8)}`
       : null,
     version: version.version,
     versionName: version.versionName,
