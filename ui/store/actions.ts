20e86b0a8de9aa04f26c0d2c0cb58d8fcec74dcb
Tuna <tunguyenanh@tunatech.org>
2026-02-02 17:12:43 +0700

    refactor: remove redundant action

--- ui/store/actions.ts
+++ ui/store/actions.ts
@@ -802,23 +802,6 @@ export function setDefaultSubscriptionPaymentOptions(
   };
 }
 
-export function clearLastUsedSubscriptionPaymentDetails(
-  product: ProductType,
-): ThunkAction<void, MetaMaskReduxState, unknown, AnyAction> {
-  return async (dispatch: MetaMaskReduxDispatch) => {
-    try {
-      await submitRequestToBackground('clearLastSelectedPaymentMethod', [
-        product,
-      ]);
-      await forceUpdateMetamaskState(dispatch);
-    } catch (error) {
-      log.error('[clearLastUsedSubscriptionPaymentDetails] error', error);
-      dispatch(displayWarning(error));
-      throw error;
-    }
-  };
-}
-
 export function setShieldSubscriptionMetricsProps(
   payload: ShieldSubscriptionMetricsPropsFromUI,
 ): ThunkAction<void, MetaMaskReduxState, unknown, AnyAction> {
