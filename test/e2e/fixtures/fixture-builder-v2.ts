ff49e209be0f56b191943aefa1946aceb0f750aa
seaona <mariona@gmx.es>
2026-02-04 14:33:54 +0100

    builder v2

---
+++ test/e2e/fixtures/fixture-builder-v2.ts
@@ -0,0 +1,33 @@
+import defaultFixtureJson from './default-fixture.json';
+import onboardingFixtureJson from './onboarding-fixture.json';
+
+function defaultFixture() {
+  return defaultFixtureJson;
+}
+
+function onboardingFixture() {
+  return onboardingFixtureJson;
+}
+
+type FixtureType = typeof defaultFixtureJson | typeof onboardingFixtureJson;
+
+class FixtureBuilderV2 {
+  fixture: FixtureType;
+
+  /**
+   * Constructs a new instance of the FixtureBuilder class.
+   *
+   * @param {object} [options] - The options for the constructor.
+   * @param {boolean} [options.onboarding] - Indicates if onboarding is enabled.
+   */
+  constructor({ onboarding = false } = {}) {
+    this.fixture =
+      onboarding === true ? onboardingFixture() : defaultFixture();
+  }
+
+  build() {
+    return this.fixture;
+  }
+}
+
+export default FixtureBuilderV2;
