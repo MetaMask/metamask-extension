7736e7d92afea0034680ab490abc7a9d9b5b301a
Francis Nepomuceno <n3ps@users.noreply.github.com>
2026-02-03 22:25:14 -0500

    chore: remove dead code

--- ui/components/multichain/asset-picker-amount/asset-picker-modal/asset-picker-modal-tabs.tsx
+++ ui/components/multichain/asset-picker-amount/asset-picker-modal/asset-picker-modal-tabs.tsx
@@ -1,61 +1,20 @@
-import React, { ReactElement, useState } from 'react';
-import { useI18nContext } from '../../../../hooks/useI18nContext';
-import { Tab, Tabs } from '../../../ui/tabs';
+import React, { ReactElement } from 'react';
 
 export enum TabName {
   TOKENS = 'tokens',
-  NFTS = 'nfts',
 }
 
 /**
- * AssetPickerModalTabs component.
+ * AssetPickerModalTabs component - renders only the tokens tab content.
  *
  * @param props
- * @param props.defaultActiveTabKey - The key of the default active tab.
- * @param props.children - The child components to be displayed within tabs.
- * @param props.visibleTabs - A list of visible tabs.
- * @returns A Tabs instance with the provided visible children.
+ * @param props.children - The child components to be displayed.
+ * @returns The tokens tab content.
  */
 export const AssetPickerModalTabs = ({
-  defaultActiveTabKey = TabName.TOKENS,
   children,
-  visibleTabs = [TabName.TOKENS, TabName.NFTS],
 }: {
-  defaultActiveTabKey?: TabName;
   children: ReactElement[];
-  visibleTabs?: TabName[];
 }) => {
-  const t = useI18nContext();
-  const [activeTab, setActiveTab] = useState(defaultActiveTabKey);
-
-  if (visibleTabs.length > 1) {
-    return (
-      <Tabs
-        activeTab={activeTab}
-        onTabClick={setActiveTab}
-        tabListProps={{ className: 'px-4' }}
-      >
-        {visibleTabs.map((tabName) => {
-          return (
-            <Tab
-              key={tabName}
-              name={t(tabName)}
-              tabKey={tabName}
-              className="flex-1"
-            >
-              {children.find(({ key }) => key === tabName)}
-            </Tab>
-          );
-        })}
-      </Tabs>
-    );
-  }
-
-  return (
-    <>
-      {visibleTabs.map((tabName) =>
-        children.find(({ key }) => key === tabName),
-      )}
-    </>
-  );
+  return <>{children.find(({ key }) => key === TabName.TOKENS)}</>;
 };
