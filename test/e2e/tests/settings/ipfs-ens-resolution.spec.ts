ee6cfd86d51493ce995cccd982fd970a59bfba72
seaona <mariona@gmx.es>
2026-02-04 14:34:21 +0100

    rmv quality gate triger

--- test/e2e/tests/settings/ipfs-ens-resolution.spec.ts
+++ test/e2e/tests/settings/ipfs-ens-resolution.spec.ts
@@ -7,7 +7,7 @@ import PrivacySettings from '../../page-objects/pages/settings/privacy-settings'
 import SettingsPage from '../../page-objects/pages/settings/settings-page';
 import { loginWithBalanceValidation } from '../../page-objects/flows/login.flow';
 
-describe('Settings TEST', function () {
+describe('Settings', function () {
   const ENS_NAME = 'metamask.eth';
   const ENS_NAME_URL = `https://${ENS_NAME}/`;
   const ENS_DESTINATION_URL = `https://app.ens.domains/name/${ENS_NAME}`;
