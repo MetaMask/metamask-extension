0161a83273c673d9505357078dc207784cfe8211
ameliejyc <amelie.chan@gmail.com>
2026-02-09 12:16:49 +0000

    fix: hide legacy network manager modal in sidepanel confirmation

--- ui/hooks/useModalState.ts
+++ ui/hooks/useModalState.ts
@@ -1,14 +1,21 @@
 import { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
-import { closeNetworkMenu } from '../store/actions';
+import { closeNetworkMenu, hideModal } from '../store/actions';
+import { useAppSelector } from '../store/store';
 
 export function useModalState() {
   const dispatch = useDispatch();
-
-  const closeModals = useCallback(
-    () => dispatch(closeNetworkMenu()),
-    [dispatch],
+  const legacyModalName = useAppSelector(
+    ({ appState }) => appState.modal.modalState?.name,
   );
+
+  const closeModals = useCallback(() => {
+    dispatch(closeNetworkMenu());
+    // Close legacy modal if NETWORK_MANAGER is open
+    if (legacyModalName === 'NETWORK_MANAGER') {
+      dispatch(hideModal());
+    }
+  }, [dispatch, legacyModalName]);
 
   return {
     closeModals,
