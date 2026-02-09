2d66e86b0138dd5b8b63c1687cdf9d88e4daf1dc
ameliejyc <amelie.chan@gmail.com>
2026-02-09 15:42:44 +0000

    chore: remove check for network manager modal

--- ui/hooks/useModalState.ts
+++ ui/hooks/useModalState.ts
@@ -1,21 +1,14 @@
 import { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 import { closeNetworkMenu, hideModal } from '../store/actions';
-import { useAppSelector } from '../store/store';
 
 export function useModalState() {
   const dispatch = useDispatch();
-  const legacyModalName = useAppSelector(
-    ({ appState }) => appState.modal.modalState?.name,
-  );
 
   const closeModals = useCallback(() => {
     dispatch(closeNetworkMenu());
-    // Close legacy modal if NETWORK_MANAGER is open
-    if (legacyModalName === 'NETWORK_MANAGER') {
-      dispatch(hideModal());
-    }
-  }, [dispatch, legacyModalName]);
+    dispatch(hideModal()); // Close any open legacy modals
+  }, [dispatch]);
 
   return {
     closeModals,
