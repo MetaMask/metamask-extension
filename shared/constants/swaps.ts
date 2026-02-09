7bf18b4889b65a34ccc20a8770e06fbf00ba4a78
Maxime OUAIRY <maxime.ouairy-ext@consensys.net>
2026-02-09 21:53:34 +0100

    feat: remove HyperEVM entry for StablecoinsByChainId

--- shared/constants/swaps.ts
+++ shared/constants/swaps.ts
@@ -570,8 +570,4 @@ export const StablecoinsByChainId: Partial<Record<string, Set<string>>> = {
   [CHAIN_IDS.MONAD]: new Set([
     '0x754704Bc059F8C67012fEd69BC8A327a5aafb603', // USDC
   ]),
-  [CHAIN_IDS.HYPE]: new Set([
-    '0xb88339CB7199b77E23DB6E890353E22632Ba630f', // USDC
-    '0xB8CE59FC3717ada4C02eaDF9682A9e934F625ebb', // USD₮0
-  ]),
 };
