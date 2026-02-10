110c4b6d71be1ac010bea6f5ab41c734e39e071e
Frederik Bolding <frederik.bolding@gmail.com>
2026-02-10 16:52:20 +0100

    Add another example

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -439,6 +439,7 @@ import { ProfileMetricsControllerInit } from './controller-init/profile-metrics-
 import { ProfileMetricsServiceInit } from './controller-init/profile-metrics-service-init';
 import { QueryService } from './queries/QueryService';
 import { ActivityDataService } from './queries/ActivityDataService';
+import { AssetDataService } from './queries/AssetDataService';
 
 export const METAMASK_CONTROLLER_EVENTS = {
   // Fired after state changes that impact the extension badge (unapproved msg count)
@@ -934,6 +935,13 @@ export default class MetamaskController extends EventEmitter {
     this.activityDataService = new ActivityDataService(
       activityDataServiceMessenger,
     );
+
+    const assetDataServiceMessenger = new Messenger({
+      namespace: 'AssetDataService',
+      parent: this.controllerMessenger,
+    });
+
+    this.assetDataService = new AssetDataService(assetDataServiceMessenger);
 
     this.controllerMessenger.subscribe(
       'TransactionController:transactionStatusUpdated',
@@ -2572,6 +2580,13 @@ export default class MetamaskController extends EventEmitter {
       accumulator[`${dataService}:getActivity`] = (options) =>
         this.controllerMessenger.call(
           `${dataService}:getActivity`,
+          ...options.queryKey.slice(1),
+          options,
+        );
+
+      accumulator[`${dataService}:getAssets`] = (options) =>
+        this.controllerMessenger.call(
+          `${dataService}:getAssets`,
           ...options.queryKey.slice(1),
           options,
         );
