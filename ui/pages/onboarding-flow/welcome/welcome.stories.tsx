1af3750a9b8ac5cf065660dfdbf89883202994ad
David Murdoch <187813+davidmurdoch@users.noreply.github.com>
2026-02-11 19:29:34 -0500

    i dunno

--- ui/pages/onboarding-flow/welcome/welcome.stories.tsx
+++ ui/pages/onboarding-flow/welcome/welcome.stories.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import RiveWasmProvider from '../../../contexts/rive-wasm';
 import OnboardingWelcome from './welcome';
 
 export default {
   title: 'Pages/OnboardingFlow/Welcome',
 };
 
-export const DefaultStory = (args) => (
-  <RiveWasmProvider>
-    <OnboardingWelcome {...args} />
-  </RiveWasmProvider>
-);
+export const DefaultStory = (args) => <OnboardingWelcome {...args} />;
 
 DefaultStory.storyName = 'Default';
