341c5cdaf585c6b8398364bf8c4831cf2f68e718
Stanley Yuen <102275989+stanleyyconsensys@users.noreply.github.com>
2026-02-13 13:53:08 +0800

    fix: lint

--- app/scripts/controller-init/messengers/static-assets-service-messenger.test.ts
+++ app/scripts/controller-init/messengers/static-assets-service-messenger.test.ts
@@ -1,9 +1,6 @@
 import { Messenger } from '@metamask/messenger';
 import { getRootMessenger } from '../../lib/messenger';
-import {
-  getStaticAssetsServiceInitMessenger,
-  getStaticAssetsServiceMessenger,
-} from './static-assets-service-messenger';
+import { getStaticAssetsServiceMessenger } from './static-assets-service-messenger';
 
 describe('getStaticAssetsServiceMessenger', () => {
   it('returns a restricted messenger', () => {
@@ -11,14 +8,5 @@ describe('getStaticAssetsServiceMessenger', () => {
     const controllerMessenger = getStaticAssetsServiceMessenger(messenger);
 
     expect(controllerMessenger).toBeInstanceOf(Messenger);
-  });
-});
-
-describe('getStaticAssetsServiceInitMessenger', () => {
-  it('returns a restricted messenger', () => {
-    const messenger = getRootMessenger<never, never>();
-    const initMessenger = getStaticAssetsServiceInitMessenger(messenger);
-
-    expect(initMessenger).toBeInstanceOf(Messenger);
   });
 });
