0f4cd12d772acf874a1aae29dd291831ead7fb14
Nicholas Gambino <nicholas.gambino@consensys.net>
2026-02-13 12:13:43 -0800

    chore: missing hooks

--- ui/hooks/perps/index.ts
+++ ui/hooks/perps/index.ts
@@ -4,3 +4,22 @@ export type {
   UsePerpsOrderFormOptions,
   UsePerpsOrderFormReturn,
 } from './usePerpsOrderForm';
+
+export { useUserHistory } from './useUserHistory';
+export type {
+  UseUserHistoryParams,
+  UseUserHistoryResult,
+} from './useUserHistory';
+
+export { usePerpsTransactionHistory } from './usePerpsTransactionHistory';
+export type {
+  UsePerpsTransactionHistoryParams,
+  UsePerpsTransactionHistoryResult,
+} from './usePerpsTransactionHistory';
+
+export { usePerpsMarginCalculations } from './usePerpsMarginCalculations';
+export type {
+  UsePerpsMarginCalculationsParams,
+  UsePerpsMarginCalculationsReturn,
+  MarginRiskAssessment,
+} from './usePerpsMarginCalculations';
