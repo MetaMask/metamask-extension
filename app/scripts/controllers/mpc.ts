4a5b9d424cd43f7ca8fbb2d3c93e4902e8ecd03e
Matthias Geihs <matthias.geihs@consensys.net>
2026-02-13 12:34:59 +0100

    lint

--- app/scripts/controllers/mpc.ts
+++ app/scripts/controllers/mpc.ts
@@ -1,7 +1,10 @@
-import { KeyringController, KeyringTypes } from "@metamask/keyring-controller";
-import { MPCKeyring } from "@metamask/eth-mpc-keyring";
+import { KeyringController, KeyringTypes } from '@metamask/keyring-controller';
+import { MPCKeyring } from '@metamask/eth-mpc-keyring';
 
-export async function createMpcKeyring(keyringController: KeyringController, verifierId: string) {
+export async function createMpcKeyring(
+  keyringController: KeyringController,
+  verifierId: string,
+) {
   const { id } = await keyringController.addNewKeyring(KeyringTypes.mpc);
   await keyringController.withKeyring({ id }, async ({ keyring }) => {
     await (keyring as MPCKeyring).setup({ verifierIds: [verifierId] });
