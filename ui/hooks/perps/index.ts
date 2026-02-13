16e379b9136c9eedb7566ac70cbefc945fd01329
Nicholas Gambino <nicholas.gambino@consensys.net>
2026-02-13 09:14:32 -0800

    chore: update imports

--- ui/hooks/perps/index.ts
+++ ui/hooks/perps/index.ts
@@ -1,5 +1,5 @@
 export { usePerpsOrderForm } from './usePerpsOrderForm';
-export { usePerpsEligibility } from './usePerpsEligibility';
+export { usePerpsEligibility } from './usePerpsEligibility
 export type {
   UsePerpsOrderFormOptions,
   UsePerpsOrderFormReturn,
