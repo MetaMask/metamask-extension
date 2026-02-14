2d088a228baf3b4eca918a4031930f53d0b2fd08
Francis Nepomuceno <n3ps@users.noreply.github.com>
2026-02-14 00:53:09 -0500

    fork PR

--- ui/components/multichain/activity-v2/activity-details-content/shared/index.ts
+++
@@ -1,6 +0,0 @@
-export { DateRow } from './date-row';
-export { NetworkRow } from './network-row';
-export { Row } from './row';
-export { StatusRow } from './status-row';
-export { TokenAmountBlock } from './token-amount-block';
-export { TransactionHashRow } from './transaction-hash-row';
