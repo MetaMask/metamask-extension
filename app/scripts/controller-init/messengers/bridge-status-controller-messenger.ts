68c3858b925f663ee5c953c9f7cd16b9cd3702ad
ghgoodreau <hunter.goodreau@consensys.net>
2026-02-16 19:56:34 -0600

    fix controller init

--- app/scripts/controller-init/messengers/bridge-status-controller-messenger.ts
+++ app/scripts/controller-init/messengers/bridge-status-controller-messenger.ts
@@ -1,45 +1,13 @@
-import { Messenger } from '@metamask/messenger';
-import type { AccountsControllerGetAccountByAddressAction } from '@metamask/accounts-controller';
-import type { HandleSnapRequest } from '@metamask/snaps-controllers';
-import type {
-  NetworkControllerFindNetworkClientIdByChainIdAction,
-  NetworkControllerGetNetworkClientByIdAction,
-  NetworkControllerGetStateAction,
-} from '@metamask/network-controller';
-import type { RemoteFeatureFlagControllerGetStateAction } from '@metamask/remote-feature-flag-controller';
 import {
-  type TransactionControllerGetStateAction,
-  TransactionControllerTransactionConfirmedEvent,
-  TransactionControllerTransactionFailedEvent,
-} from '@metamask/transaction-controller';
-import type {
-  BridgeBackgroundAction,
-  BridgeControllerAction,
-} from '@metamask/bridge-controller';
-import type { GetGasFeeState } from '@metamask/gas-fee-controller';
-import { MultichainTransactionsControllerTransactionConfirmedEvent } from '@metamask/multichain-transactions-controller';
+  Messenger,
+  MessengerActions,
+  MessengerEvents,
+} from '@metamask/messenger';
+import type { BridgeStatusControllerMessenger as BridgeStatusControllerBaseMessenger } from '@metamask/bridge-status-controller';
 import { RootMessenger } from '../../lib/messenger';
 
-type AllowedActions =
-  | NetworkControllerFindNetworkClientIdByChainIdAction
-  | NetworkControllerGetStateAction
-  | NetworkControllerGetNetworkClientByIdAction
-  | HandleSnapRequest
-  | TransactionControllerGetStateAction
-  | BridgeControllerAction<BridgeBackgroundAction.TRACK_METAMETRICS_EVENT>
-  | BridgeControllerAction<BridgeBackgroundAction.STOP_POLLING_FOR_QUOTES>
-  | GetGasFeeState
-  | AccountsControllerGetAccountByAddressAction
-  | RemoteFeatureFlagControllerGetStateAction;
-
-type AllowedEvents =
-  | MultichainTransactionsControllerTransactionConfirmedEvent
-  | TransactionControllerTransactionFailedEvent
-  | TransactionControllerTransactionConfirmedEvent;
-
-export type BridgeStatusControllerMessenger = ReturnType<
-  typeof getBridgeStatusControllerMessenger
->;
+export type BridgeStatusControllerMessenger =
+  BridgeStatusControllerBaseMessenger;
 
 /**
  * Create a messenger restricted to the allowed actions and events of the
@@ -48,13 +16,11 @@ export type BridgeStatusControllerMessenger = ReturnType<
  * @param messenger - The base messenger used to create the restricted
  * messenger.
  */
-export function getBridgeStatusControllerMessenger(
-  messenger: RootMessenger<AllowedActions, AllowedEvents>,
-) {
+export function getBridgeStatusControllerMessenger(messenger: RootMessenger) {
   const controllerMessenger = new Messenger<
     'BridgeStatusController',
-    AllowedActions,
-    AllowedEvents,
+    MessengerActions<BridgeStatusControllerMessenger>,
+    MessengerEvents<BridgeStatusControllerMessenger>,
     typeof messenger
   >({
     namespace: 'BridgeStatusController',
@@ -75,7 +41,6 @@ export function getBridgeStatusControllerMessenger(
       'RemoteFeatureFlagController:getState',
     ],
     events: [
-      'MultichainTransactionsController:transactionConfirmed',
       'TransactionController:transactionFailed',
       'TransactionController:transactionConfirmed',
     ],
