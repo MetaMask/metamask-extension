12a9fa3f9a45b2480a43a93f8e0eb90cdcba1f0c
gantunesr <17601467+gantunesr@users.noreply.github.com>
2026-02-17 16:49:41 -0300

    refactor: remove selector

--- ui/components/app/toast-master/selectors.ts
+++ ui/components/app/toast-master/selectors.ts
@@ -115,33 +115,6 @@ export function selectNftDetectionEnablementToast(
 
 // If there is more than one connected account to activeTabOrigin,
 // *BUT* the current account is not one of them, show the banner
-export function selectShowConnectAccountToast(
-  state: State & Pick<MetaMaskReduxState, 'activeTab'>,
-  account: InternalAccount,
-): boolean {
-  const allowShowAccountSetting = getAlertEnabledness(state).unconnectedAccount;
-  const activeTabOrigin = getOriginOfCurrentTab(state);
-  const connectedAccounts = getAllPermittedAccountsForCurrentTab(state);
-
-  // We only support connection with EVM or Solana accounts
-  // This check prevents Bitcoin snap accounts from showing the toast
-  const isEvmAccount = isEvmAccountType(account?.type);
-  const isSolanaAccountSelected = isSolanaAccount(account);
-  const isConnectableAccount = isEvmAccount || isSolanaAccountSelected;
-
-  const showConnectAccountToast =
-    allowShowAccountSetting &&
-    account &&
-    activeTabOrigin &&
-    isConnectableAccount &&
-    connectedAccounts.length > 0 &&
-    !isInternalAccountInPermittedAccountIds(account, connectedAccounts);
-
-  return Boolean(showConnectAccountToast);
-}
-
-// If there is more than one connected account to activeTabOrigin,
-// *BUT* the current account is not one of them, show the banner
 export function selectShowConnectAccountGroupToast(
   state: State & Pick<MetaMaskReduxState, 'activeTab'>,
   accountGroup: AccountGroupWithInternalAccounts,
