1464c1369550a3b8d36435e26d5beee5de4acd7a
Priya Narayanaswamy <priya.narayanaswamy@consensys.net>
2026-02-18 13:12:23 +0100

    test: fix flakiness

--- test/e2e/tests/responsive-ui/metamask-responsive-ui.spec.ts
+++ test/e2e/tests/responsive-ui/metamask-responsive-ui.spec.ts
@@ -12,6 +12,7 @@ import { completeCreateNewWalletOnboardingFlow } from '../../page-objects/flows/
 import { loginWithBalanceValidation } from '../../page-objects/flows/login.flow';
 import { sendRedesignedTransactionToAddress } from '../../page-objects/flows/send-transaction.flow';
 import { CHAIN_IDS } from '../../../../shared/constants/network';
+import { PAGES } from 'test/e2e/webdriver/driver';
 
 describe('MetaMask Responsive UI', function (this: Suite) {
   const driverOptions = { constrainWindowSize: true };
@@ -41,7 +42,7 @@ describe('MetaMask Responsive UI', function (this: Suite) {
         title: this.test?.fullTitle(),
       },
       async ({ driver }) => {
-        await driver.navigate();
+        await driver.navigate(PAGES.HOME, {waitForControllersTimeout: 30000});
 
         // Click forgot password button and reset password
         const loginPage = new LoginPage(driver);
