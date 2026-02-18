725f1d3b8617c8fa9adedcf157fa47fac90a38e3
Francis Nepomuceno <n3ps@users.noreply.github.com>
2026-02-18 09:39:05 -0500

    cleanup

--- test/e2e/helpers/mock-server.ts
+++ test/e2e/helpers/mock-server.ts
@@ -1,6 +1,4 @@
 import { strict as assert } from 'assert';
-import { Mockttp, MockttpServer } from 'mockttp';
-import { ACCOUNTS_API_BASE_URL } from '../../../shared/constants/accounts';
 import { Driver } from '../webdriver/driver';
 import { MockedEndpoint } from '../mock-e2e';
 
@@ -27,17 +25,4 @@ export async function expectNoMockRequest(
   const isPending = await mockedEndpoint.isPending();
 
   assert.ok(isPending, 'Expected no requests');
-}
-
-export async function mockTransactions(
-  mockServer: Mockttp | MockttpServer,
-  data: unknown[] = [],
-) {
-  return mockServer
-    .forGet(`${ACCOUNTS_API_BASE_URL}/v4/multiaccount/transactions`)
-    .always()
-    .thenJson(200, {
-      data,
-      pageInfo: { hasNextPage: false, count: data.length },
-    });
 }
