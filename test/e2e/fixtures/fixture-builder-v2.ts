5c1d5b005d80b7ce75d26d54ad22d9bdff5d4c96
HJetpoluru <harika.jetpoluru@consensys.net>
2026-02-18 13:29:43 -0500

    Migrate withTransactionControllerApprovedTransaction and withTransactionControllerCompletedAndIncomingTransaction

--- test/e2e/fixtures/fixture-builder-v2.ts
+++ test/e2e/fixtures/fixture-builder-v2.ts
@@ -3,6 +3,7 @@ import type {
   PermissionConstraint,
   PermissionControllerState,
 } from '@metamask/permission-controller';
+import { CHAIN_IDS } from '../../../shared/constants/network';
 import {
   DAPP_URL,
   DAPP_URL_LOCALHOST,
@@ -47,10 +48,275 @@ class FixtureBuilderV2 {
     return this;
   }
 
+  withTransactionController(data: Record<string, unknown>): this {
+    const tc = this.fixture.data.TransactionController ?? {};
+    merge(this.fixture.data, { TransactionController: tc });
+    merge(this.fixture.data.TransactionController, data);
+    return this;
+  }
+
   /* ==================================================================
                               CUSTOM METHODS
      ==================================================================
   */
+  withTransactionControllerApprovedTransaction(): this {
+    return this.withTransactionController({
+      transactions: {
+        '13a01e77-a368-4bb9-aba9-e7435580e3b9': {
+          chainId: CHAIN_IDS.LOCALHOST,
+          history: [
+            {
+              chainId: CHAIN_IDS.LOCALHOST,
+              id: '13a01e77-a368-4bb9-aba9-e7435580e3b9',
+              loadingDefaults: true,
+              origin: 'metamask',
+              status: 'unapproved',
+              time: 1617228030067,
+              txParams: {
+                from: '0x5cfe73b6021e818b776b421b1c4db2474086a7e1',
+                gas: '0x61a8',
+                maxFeePerGas: '0x59682f0c',
+                maxPriorityFeePerGas: '0x59682f00',
+                to: '0x2f318C334780961FB129D2a6c30D0763d9a5C970',
+                type: '0x2',
+                value: '0xde0b6b3a7640000',
+              },
+              type: 'simpleSend',
+            },
+            [
+              {
+                note: 'Added new unapproved transaction.',
+                op: 'replace',
+                path: '/loadingDefaults',
+                timestamp: 1617228030069,
+                value: false,
+              },
+            ],
+            [
+              {
+                op: 'add',
+                path: '/txParams/nonce',
+                value: '0x0',
+                note: 'transactions#approveTransaction',
+                timestamp: 1617228031069,
+              },
+            ],
+          ],
+          id: '13a01e77-a368-4bb9-aba9-e7435580e3b9',
+          loadingDefaults: false,
+          origin: 'metamask',
+          primaryTransaction: {
+            chainId: CHAIN_IDS.LOCALHOST,
+            id: '13a01e77-a368-4bb9-aba9-e7435580e3b9',
+            loadingDefaults: true,
+            origin: 'metamask',
+            status: 'approved',
+            time: 1617228030067,
+            txParams: {
+              from: '0x5cfe73b6021e818b776b421b1c4db2474086a7e1',
+              gas: '0x61a8',
+              maxFeePerGas: '0x59682f0c',
+              maxPriorityFeePerGas: '0x59682f00',
+              to: '0x2f318C334780961FB129D2a6c30D0763d9a5C970',
+              type: '0x2',
+              value: '0xde0b6b3a7640000',
+            },
+            type: 'sentEther',
+          },
+          status: 'approved',
+          time: 1617228030067,
+          txParams: {
+            from: '0x5cfe73b6021e818b776b421b1c4db2474086a7e1',
+            gas: '0x61a8',
+            maxFeePerGas: '0x59682f0c',
+            maxPriorityFeePerGas: '0x59682f00',
+            to: '0x2f318C334780961FB129D2a6c30D0763d9a5C970',
+            type: '0x2',
+            value: '0xde0b6b3a7640000',
+          },
+          type: 'simpleSend',
+        },
+      },
+    });
+  }
+
+  withTransactionControllerCompletedTransaction(): this {
+    return this.withTransactionController({
+      transactions: {
+        '0c9342ce-ef3f-4cab-9425-8e57144256a6': {
+          chainId: CHAIN_IDS.LOCALHOST,
+          history: [
+            {
+              chainId: CHAIN_IDS.LOCALHOST,
+              id: '0c9342ce-ef3f-4cab-9425-8e57144256a6',
+              loadingDefaults: true,
+              origin: 'metamask',
+              status: 'unapproved',
+              time: 1671635506502,
+              txParams: {
+                from: '0x5cfe73b6021e818b776b421b1c4db2474086a7e1',
+                gas: '0x5208',
+                maxFeePerGas: '0x4c03c96f8',
+                maxPriorityFeePerGas: '0x59682f00',
+                to: '0x2f318C334780961FB129D2a6c30D0763d9a5C970',
+                type: '0x2',
+                value: '0xde0b6b3a7640000',
+              },
+              type: 'simpleSend',
+            },
+            [
+              {
+                note: 'Added new unapproved transaction.',
+                op: 'replace',
+                path: '/loadingDefaults',
+                timestamp: 1671635506520,
+                value: false,
+              },
+            ],
+            [
+              {
+                note: 'confTx: user approved transaction',
+                op: 'replace',
+                path: '/txParams/maxFeePerGas',
+                timestamp: 1671635510589,
+                value: '0x4d7fc07fb',
+              },
+            ],
+            [
+              {
+                note: 'txStateManager: setting status to approved',
+                op: 'replace',
+                path: '/status',
+                timestamp: 1671635510589,
+                value: 'approved',
+              },
+            ],
+            [
+              {
+                note: 'transactions#approveTransaction',
+                op: 'add',
+                path: '/txParams/nonce',
+                timestamp: 1671635510592,
+                value: '0x2',
+              },
+            ],
+            [
+              {
+                note: 'txStateManager: setting status to signed',
+                op: 'replace',
+                path: '/status',
+                timestamp: 1671635510651,
+                value: 'signed',
+              },
+            ],
+            [
+              {
+                note: 'transactions#publishTransaction',
+                op: 'add',
+                path: '/rawTx',
+                timestamp: 1671635510653,
+                value:
+                  '0x02f87205028459682f008504d7fc07fb825208947d17148ed7ec802e4458e94deec1ef28aef645e987038d7ea4c6800080c001a0c60aeaef1556a52b009e3973f06c64d5cd6dc935463afd0d2b1c00661655e47ea061b121db8f2cb2241b1454d1794256e5634d26a5b873e89a816efe210377492a',
+              },
+            ],
+            [
+              {
+                note: 'txStateManager: setting status to submitted',
+                op: 'replace',
+                path: '/status',
+                timestamp: 1671635510753,
+                value: 'submitted',
+              },
+            ],
+            [
+              {
+                note: 'txStateManager: setting status to confirmed',
+                op: 'replace',
+                path: '/status',
+                timestamp: 1671635522978,
+                value: 'confirmed',
+              },
+              {
+                op: 'add',
+                path: '/txReceipt',
+                value: {
+                  blockNumber: '7cbf95',
+                  from: '0x5cfe73b6021e818b776b421b1c4db2474086a7e1',
+                  gasUsed: '5208',
+                  status: '0x1',
+                  to: '0x2f318C334780961FB129D2a6c30D0763d9a5C970',
+                  type: '0x2',
+                },
+              },
+            ],
+            [
+              {
+                note: 'transactions#confirmTransaction - add txReceipt',
+                op: 'replace',
+                path: '/blockTimestamp',
+                timestamp: 1671635522999,
+                value: '63a32240',
+              },
+            ],
+          ],
+          id: '0c9342ce-ef3f-4cab-9425-8e57144256a6',
+          loadingDefaults: false,
+          origin: 'metamask',
+          status: 'confirmed',
+          submittedTime: 1671635510753,
+          time: 1671635506502,
+          txParams: {
+            from: '0x5cfe73b6021e818b776b421b1c4db2474086a7e1',
+            gas: '0x5208',
+            to: '0x2f318C334780961FB129D2a6c30D0763d9a5C970',
+            type: '0x2',
+            value: '0xde0b6b3a7640000',
+          },
+          hash: '0xe5e7b95690f584b8f66b33e31acc6184fea553fa6722d42486a59990d13d5fa2',
+          txReceipt: {
+            blockNumber: {
+              length: 1,
+              negative: 0,
+              words: [8175509, null],
+            },
+            from: '0x5cfe73b6021e818b776b421b1c4db2474086a7e1',
+            status: '0x1',
+            to: '0x2f318C334780961FB129D2a6c30D0763d9a5C970',
+            type: '0x2',
+          },
+          type: 'simpleSend',
+        },
+      },
+    });
+  }
+
+  withTransactionControllerIncomingTransaction(): this {
+    return this.withTransactionController({
+      transactions: {
+        '8a13fd36-fdad-48ae-8b6a-c8991026d550': {
+          blockNumber: '1',
+          chainId: CHAIN_IDS.LOCALHOST,
+          hash: '0xf1af8286e4fa47578c2aec5f08c108290643df978ebc766d72d88476eee90bab',
+          id: '8a13fd36-fdad-48ae-8b6a-c8991026d550',
+          status: 'confirmed',
+          time: 1671635520000,
+          txParams: {
+            from: '0xc87261ba337be737fa744f50e7aaf4a920bdfcd6',
+            gas: '0x5208',
+            gasPrice: '0x329af9707',
+            to: '0x5cfe73b6021e818b776b421b1c4db2474086a7e1',
+            value: '0xDE0B6B3A7640000',
+          },
+          type: 'incoming',
+        },
+      },
+    });
+  }
+
+  withTransactionControllerCompletedAndIncomingTransaction(): this {
+    return this.withTransactionControllerCompletedTransaction().withTransactionControllerIncomingTransaction();
+  }
+
   withPermissionControllerConnectedToTestDapp({
     account = '',
     useLocalhostHostname = false,
