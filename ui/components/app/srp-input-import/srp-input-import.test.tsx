3d9d7cb9c77df210264cb4861d04556c95db0905
lwin <lwin.kyaw@consensys.net>
2026-02-19 11:00:38 +0800

    fix: clipboard permission request in SidePanel

--- ui/components/app/srp-input-import/srp-input-import.test.tsx
+++ ui/components/app/srp-input-import/srp-input-import.test.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 import { fireEvent, waitFor } from '@testing-library/react';
 import { renderWithProvider } from '../../../../test/lib/render-helpers-navigate';
 import * as browserRuntime from '../../../../shared/modules/browser-runtime.utils';
-import { PLATFORM_FIREFOX } from '../../../../shared/constants/app';
+import {
+  ENVIRONMENT_TYPE_SIDEPANEL,
+  PLATFORM_FIREFOX,
+} from '../../../../shared/constants/app';
 import SrpInputImport from './srp-input-import';
+
+const mockGetEnvironmentType = jest.fn().mockReturnValue('popup');
+
+jest.mock('../../../../app/scripts/lib/util', () => ({
+  ...jest.requireActual('../../../../app/scripts/lib/util'),
+  getEnvironmentType: () => mockGetEnvironmentType(),
+}));
 
 const mockClipboardReadText = jest.fn().mockResolvedValue('some mock text');
 
@@ -14,6 +24,11 @@ Object.defineProperty(navigator, 'clipboard', {
 });
 
 describe('SrpInputImport', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    mockGetEnvironmentType.mockReturnValue('popup');
+  });
+
   it('should render', () => {
     const { getByTestId } = renderWithProvider(
       <SrpInputImport onChange={jest.fn()} />,
@@ -25,6 +40,23 @@ describe('SrpInputImport', () => {
     jest
       .spyOn(browserRuntime, 'getBrowserName')
       .mockReturnValue(PLATFORM_FIREFOX);
+
+    const { getByTestId } = renderWithProvider(
+      <SrpInputImport onChange={jest.fn()} />,
+    );
+    const pasteButton = getByTestId('srp-input-import__paste-button');
+    fireEvent.click(pasteButton);
+
+    await waitFor(() => {
+      expect(browser.permissions.request).toHaveBeenCalledWith({
+        permissions: ['clipboardRead'],
+      });
+      expect(mockClipboardReadText).toHaveBeenCalled();
+    });
+  });
+
+  it('should ask for explicit permission to read the clipboard in Chrome side panel', async () => {
+    mockGetEnvironmentType.mockReturnValue(ENVIRONMENT_TYPE_SIDEPANEL);
 
     const { getByTestId } = renderWithProvider(
       <SrpInputImport onChange={jest.fn()} />,
