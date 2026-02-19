b0d73f45114c36c0e28ebb1770f44a21700ed41e
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2026-02-19 12:36:41 +0000

    changes

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -2508,13 +2508,16 @@ export default class MetamaskController extends EventEmitter {
       multichainAssetsRatesController,
       ///: END:ONLY_INCLUDE_IF
       staticAssetsController,
+      assetsController,
     } = this;
 
     return {
       // etc
-      setCurrentCurrency: currencyRateController.setCurrentCurrency.bind(
-        currencyRateController,
-      ),
+      setCurrentCurrency: assetsController
+        ? assetsController.setCurrentCurrency.bind(assetsController)
+        : currencyRateController.setCurrentCurrency.bind(
+            currencyRateController,
+          ),
       // @deprecated Use setAvatarType instead
       setUseBlockie: preferencesController.setUseBlockie.bind(
         preferencesController,
