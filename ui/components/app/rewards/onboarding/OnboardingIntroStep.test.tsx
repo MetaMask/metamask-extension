593f7e24e0398efee5fa2703723a111d12529945
David Murdoch <187813+davidmurdoch@users.noreply.github.com>
2026-02-19 18:46:47 -0500

    fix mock

--- ui/components/app/rewards/onboarding/OnboardingIntroStep.test.tsx
+++ ui/components/app/rewards/onboarding/OnboardingIntroStep.test.tsx
@@ -65,7 +65,7 @@ jest.mock('../../../../ducks/rewards/selectors', () => ({
 }));
 
 // Mock hardware wallet selector
-jest.mock('../../../../../shared/modules/selectors', () => ({
+jest.mock('../../../../../shared/modules/selectors/hardware-wallet', () => ({
   isHardwareWallet: jest.fn(),
 }));
 
@@ -100,7 +100,7 @@ describe('OnboardingIntroStep', () => {
     selectCandidateSubscriptionId,
   } = jest.requireMock('../../../../ducks/rewards/selectors');
   const { isHardwareWallet } = jest.requireMock(
-    '../../../../../shared/modules/selectors',
+    '../../../../../shared/modules/selectors/hardware-wallet',
   );
   const { useAppSelector } = jest.requireMock('../../../../store/store');
   const { useGeoRewardsMetadata } = jest.requireMock(
