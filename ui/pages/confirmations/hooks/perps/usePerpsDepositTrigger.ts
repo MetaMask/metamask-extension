33b6186f923b7a583f90107c5e347d89ed5224fa
geositta <matthew.denton@consensys.net>
2026-02-19 12:20:09 -0600

    chore: rm unused confirmation types

--- ui/pages/confirmations/hooks/perps/usePerpsDepositTrigger.ts
+++ ui/pages/confirmations/hooks/perps/usePerpsDepositTrigger.ts
@@ -1,16 +1,13 @@
 import type {
   PerpsDepositConfirmationOptions,
-  PerpsDepositConfirmationResponse,
   PerpsDepositConfirmationResult,
 } from './usePerpsDepositConfirmation';
 import { usePerpsDepositConfirmation } from './usePerpsDepositConfirmation';
 
-export type PerpsDepositTriggerResponse = PerpsDepositConfirmationResponse;
-export type PerpsDepositTriggerOptions = Omit<
+type UsePerpsDepositTriggerOptions = Omit<
   PerpsDepositConfirmationOptions,
   'navigateOnCreate'
 >;
-export type PerpsDepositTriggerResult = PerpsDepositConfirmationResult;
 
 /**
  * Pay/Confirmations-owned entrypoint for starting the Perps deposit confirmation flow.
@@ -23,8 +20,8 @@ export type PerpsDepositTriggerResult = PerpsDepositConfirmationResult;
  * @param options
  */
 export function usePerpsDepositTrigger(
-  options: PerpsDepositTriggerOptions = {},
-): PerpsDepositTriggerResult {
+  options: UsePerpsDepositTriggerOptions = {},
+): PerpsDepositConfirmationResult {
   return usePerpsDepositConfirmation({
     ...options,
     navigateOnCreate: true,
