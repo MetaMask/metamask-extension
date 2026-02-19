4a0c19f5030bb03a6ead767829333468d684d580
sahar-fehri <sahar.fehri@consensys.net>
2026-02-19 13:15:50 +0100

    fix: fix

--- app/scripts/controller-init/messengers/multichain/multichain-assets-controller-messenger.ts
+++ app/scripts/controller-init/messengers/multichain/multichain-assets-controller-messenger.ts
@@ -7,15 +7,13 @@ import {
 import { Messenger } from '@metamask/messenger';
 import { GetPermissions } from '@metamask/permission-controller';
 import { GetAllSnaps, HandleSnapRequest } from '@metamask/snaps-controllers';
-import { PhishingControllerBulkScanTokensAction } from '@metamask/phishing-controller';
 import { RootMessenger } from '../../../lib/messenger';
 
 type Actions =
   | HandleSnapRequest
   | GetAllSnaps
   | GetPermissions
-  | AccountsControllerListMultichainAccountsAction
-  | PhishingControllerBulkScanTokensAction;
+  | AccountsControllerListMultichainAccountsAction;
 
 type Events =
   | AccountsControllerAccountAddedEvent
@@ -57,7 +55,6 @@ export function getMultichainAssetsControllerMessenger(
       'SnapController:handleRequest',
       'SnapController:getAll',
       'AccountsController:listMultichainAccounts',
-      'PhishingController:bulkScanTokens',
     ],
   });
   return controllerMessenger;
