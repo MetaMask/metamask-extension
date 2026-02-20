979dd8374d201b117841c9c65f676b2df90e1441
Javier Garcia Vera <javier.vera@consensys.net>
2026-02-20 12:33:34 +0100

    test: fix issue after fixing conflicts

--- test/e2e/page-objects/pages/bridge/quote-page.ts
+++ test/e2e/page-objects/pages/bridge/quote-page.ts
@@ -69,6 +69,7 @@ class BridgeQuotePage {
 
   private slippageCustomInput =
     'input[data-testid="bridge__tx-settings-modal-custom-input"]';
+
   private networkNameSelector = (network: string) =>
     `[data-testid="${network}"]`;
 
@@ -265,6 +266,8 @@ class BridgeQuotePage {
       const input = document.querySelector('${this.slippageCustomInput}');
       if (input) { input.blur(); }
     `);
+  }
+
   async selectNetwork(network: string): Promise<void> {
     await this.driver.clickElement(this.networkSelector);
     await this.driver.clickElement(this.networkNameSelector(network));
