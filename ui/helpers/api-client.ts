6a5dd10ed7a4b281613f04cec455ca25def6a1c0
Francis Nepomuceno <n3ps@users.noreply.github.com>
2026-02-20 13:43:22 -0500

    type

--- ui/helpers/api-client.ts
+++ ui/helpers/api-client.ts
@@ -1,7 +1,10 @@
-import type { QueryClient } from '@tanstack/query-core';
 import { createApiPlatformClient } from '@metamask/core-backend';
 import { submitRequestToBackground } from '../store/background-connection';
 import { queryClient } from '../contexts/query-client';
+
+type QueryClient = NonNullable<
+  Parameters<typeof createApiPlatformClient>[0]['queryClient']
+>;
 
 export const apiClient = createApiPlatformClient({
   clientProduct: 'metamask-extension',
