428e1a4a498e8ff2b887d5ec916844f21658753a
John Whiles <john.whiles@consensys.net>
2026-02-23 13:52:39 +0000

    chore: update tests and remove baseline violations

--- ui/pages/confirmations/components/info/musd-claim-info/musd-claim-info.test.tsx
+++ ui/pages/confirmations/components/info/musd-claim-info/musd-claim-info.test.tsx
@@ -55,6 +55,11 @@ jest.mock('../../../../../store/actions', () => ({
   }),
 }));
 
+jest.mock('../../../../../store/background-connection', () => ({
+  ...jest.requireActual('../../../../../store/background-connection'),
+  submitRequestToBackground: jest.fn().mockResolvedValue(undefined),
+}));
+
 jest.mock('../../../../../components/app/musd/merkl-client', () => ({
   getClaimedAmountFromContract: jest.fn().mockResolvedValue(null),
 }));
