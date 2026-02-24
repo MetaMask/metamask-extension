df02d37243b90e7dee14f21c666e9f6aade586b3
Micaela Estabillo <micaela.estabillo@consensys.net>
2026-02-24 15:57:27 -0800

    fix: lint error

--- ui/pages/bridge/prepare/bridge-input-group.tsx
+++ ui/pages/bridge/prepare/bridge-input-group.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import {
   formatChainIdToCaip,
