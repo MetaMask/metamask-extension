2e638427d31746d2e6392b69d7cccb5efe490bd8
Javier Garcia Vera <javier.vera@consensys.net>
2026-02-24 15:46:04 +0100

    test: changes related to PR review

--- test/e2e/tests/solana/web-socket-connection.spec.ts
+++ test/e2e/tests/solana/web-socket-connection.spec.ts
@@ -2,9 +2,9 @@ import { strict as assert } from 'assert';
 import { Suite } from 'mocha';
 import { Driver } from '../../webdriver/driver';
 import LocalWebSocketServer from '../../websocket-server';
-import FixtureBuilder from '../../fixtures/fixture-builder';
 import { withFixtures } from '../../helpers';
 import { loginWithBalanceValidation } from '../../page-objects/flows/login.flow';
+import FixtureBuilderV2 from '../../fixtures/fixture-builder-v2';
 
 async function waitForWebsocketConnections(
   driver: Driver,
@@ -29,10 +29,10 @@ describe('Solana Web Socket', function (this: Suite) {
   it('a websocket connection is open when MetaMask full view is open', async function () {
     await withFixtures(
       {
-        fixtures: new FixtureBuilder().build(),
+        fixtures: new FixtureBuilderV2().build(),
         title: this.test?.fullTitle(),
       },
-      async ({ driver }) => {
+      async ({ driver }: { driver: Driver }) => {
         await loginWithBalanceValidation(driver);
 
         await waitForWebsocketConnections(driver, 1);
@@ -43,10 +43,10 @@ describe('Solana Web Socket', function (this: Suite) {
   it('the websocket connection is maintained for a grace period when MetaMask window is closed', async function () {
     await withFixtures(
       {
-        fixtures: new FixtureBuilder().build(),
+        fixtures: new FixtureBuilderV2().build(),
         title: this.test?.fullTitle(),
       },
-      async ({ driver }) => {
+      async ({ driver }: { driver: Driver }) => {
         await loginWithBalanceValidation(driver);
         await waitForWebsocketConnections(driver, 1);
 
@@ -63,10 +63,10 @@ describe('Solana Web Socket', function (this: Suite) {
   it('websocket connection is shared between multiple MetaMask windows', async function () {
     await withFixtures(
       {
-        fixtures: new FixtureBuilder().build(),
+        fixtures: new FixtureBuilderV2().build(),
         title: this.test?.fullTitle(),
       },
-      async ({ driver }) => {
+      async ({ driver }: { driver: Driver }) => {
         await loginWithBalanceValidation(driver);
 
         await waitForWebsocketConnections(driver, 1);
@@ -81,11 +81,13 @@ describe('Solana Web Socket', function (this: Suite) {
         await driver.closeWindow();
 
         await waitForWebsocketConnections(driver, 1);
+        // Verify that websocket connection is NOT closed - second MM window still open (give it some time)
 
         await driver.switchToWindowWithTitle('MetaMask');
         await driver.closeWindow();
 
         await waitForWebsocketConnections(driver, 1);
+        // The websocket close grace period is 5 minutes, we can't wait for this long to check if it's closed
       },
     );
   });
