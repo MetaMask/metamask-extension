51442cdfea930ac205387ba3e81351522c794f7d
geositta <matthew.denton@consensys.net>
2026-02-25 23:41:49 -0600

    refactor(perps): extract shared TP/SL normalization helper for perps flows

--- ui/pages/perps/perps-market-detail-page.tsx
+++ ui/pages/perps/perps-market-detail-page.tsx
@@ -61,6 +61,7 @@ import {
   getDisplayName,
   safeDecodeURIComponent,
   getChangeColor,
+  normalizeTpslPrices,
 } from '../../components/app/perps/utils';
 import { PerpsDetailPageSkeleton } from '../../components/app/perps/perps-skeletons';
 import { Skeleton } from '../../components/component-library/skeleton';
