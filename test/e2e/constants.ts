69a9694be9fd6b8e36bb048c762ca41cf6293dd6
Prithpal Sooriya <prithpal.sooriya@consensys.net>
2026-02-27 14:23:53 +0000

    test: fix e2e failing tests

--- test/e2e/constants.ts
+++ test/e2e/constants.ts
@@ -168,8 +168,8 @@ export const MOCK_REMOTE_FEATURE_FLAGS_RESPONSE = {
   feature1: true,
   feature2: false,
   feature3: {
-    name: 'groupC',
-    value: 'valueC',
+    name: 'groupA',
+    value: 'valueA',
   },
 };
 
