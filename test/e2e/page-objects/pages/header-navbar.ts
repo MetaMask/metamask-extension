b8657a8f26bfb24c4cba9bb5424fa9b9c8ce40cf
jvbriones <1674192+jvbriones@users.noreply.github.com>
2026-03-02 12:26:39 +0100

    Update test/e2e/page-objects/pages/header-navbar.ts
    Co-authored-by: seaona <54408225+seaona@users.noreply.github.com>

--- test/e2e/page-objects/pages/header-navbar.ts
+++ test/e2e/page-objects/pages/header-navbar.ts
@@ -14,7 +14,7 @@ class HeaderNavbar {
   private readonly copyAddressButton = '[aria-label="Copy address"]';
 
   private readonly threeDotMenuButton =
-    'button[data-testid="account-options-menu-button"]';
+    '[data-testid="account-options-menu-button"]';
 
   private readonly accountSnapButton = { text: 'Snaps', tag: 'div' };
 
