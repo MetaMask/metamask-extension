ad1234d91ef94ea904c55e9b19647b6d14795a47
Nicholas Smith <nick.smith@consensys.net>
2026-03-05 16:48:32 -0600

    fix: unit tests and baseline

--- ui/components/app/musd/musd-buy-get-cta.test.tsx
+++ ui/components/app/musd/musd-buy-get-cta.test.tsx
@@ -6,6 +6,7 @@ import { fireEvent, screen } from '@testing-library/react';
 import { renderWithProvider } from '../../../../test/lib/render-helpers-navigate';
 import configureStore from '../../../store/store';
 import { BuyGetMusdCtaVariant } from '../../../hooks/musd/useMusdCtaVisibility';
+import { enLocale as messages } from '../../../../test/lib/i18n-helpers';
 import { MusdBuyGetCta } from './musd-buy-get-cta';
 
 // Mock useI18nContext
@@ -135,7 +136,7 @@ describe('MusdBuyGetCta', () => {
         store,
       );
 
-      const elements = screen.getAllByText('Get mUSD');
+      const elements = screen.getAllByText(messages.musdGetMusd.message);
       expect(elements.length).toBeGreaterThanOrEqual(1);
     });
 
@@ -197,7 +198,7 @@ describe('MusdBuyGetCta', () => {
         store,
       );
 
-      const elements = screen.getAllByText('Buy mUSD');
+      const elements = screen.getAllByText(messages.musdBuyMusd.message);
       expect(elements.length).toBeGreaterThanOrEqual(1);
     });
 
