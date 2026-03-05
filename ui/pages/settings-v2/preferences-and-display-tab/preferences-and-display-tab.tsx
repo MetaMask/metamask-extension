327a1dab8a9bd8c325b6d9fa51c96ae29f6d29ac
ameliejyc <amelie.chan@gmail.com>
2026-03-05 14:13:31 +0000

    chore: use shared SettingsTab component

--- ui/pages/settings-v2/preferences-and-display-tab/preferences-and-display-tab.tsx
+++ ui/pages/settings-v2/preferences-and-display-tab/preferences-and-display-tab.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Box } from '@metamask/design-system-react';
+import { SettingItemConfig } from '../types';
+import { SettingsTab } from '../shared';
 import { ThemeItem } from './theme-item';
 import { LanguageItem } from './language-item';
 import { AccountIdenticonItem } from './account-identicon-item';
@@ -7,42 +8,25 @@ import { ShowExtensionItem } from './show-extension-item';
 import { ManageInstitutionalWalletItem } from './manage-institutional-wallet-item';
 import { ShowDefaultAddressItem } from './show-default-address-item';
 
-type SettingItemConfig = {
-  id: string;
-  component: React.FC;
-};
-
 /** Registry of setting items for the Preferences and Display page. Add new items here */
 const PREFERENCES_AND_DISPLAY_SETTING_ITEMS: SettingItemConfig[] = [
   { id: 'theme', component: ThemeItem },
   { id: 'language', component: LanguageItem },
   { id: 'account-identicon', component: AccountIdenticonItem },
   { id: 'show-default-address', component: ShowDefaultAddressItem },
-  { id: 'show-extension', component: ShowExtensionItem },
+  {
+    id: 'show-extension',
+    component: ShowExtensionItem,
+    hasDividerBefore: true,
+  },
   {
     id: 'manage-institutional-wallet',
     component: ManageInstitutionalWalletItem,
   },
 ];
 
-export const PreferencesAndDisplay = ({
-  children,
-}: {
-  children: SettingItemConfig[];
-}) => {
-  return (
-    <Box>
-      {children.map(({ id, component: Component }) => (
-        <Component key={id} />
-      ))}
-    </Box>
-  );
-};
-
-const PreferencesAndDisplayList = () => (
-  <PreferencesAndDisplay>
-    {PREFERENCES_AND_DISPLAY_SETTING_ITEMS}
-  </PreferencesAndDisplay>
+const PreferencesAndDisplayTab = () => (
+  <SettingsTab items={PREFERENCES_AND_DISPLAY_SETTING_ITEMS} />
 );
 
-export default PreferencesAndDisplayList;
+export default PreferencesAndDisplayTab;
