e0128bccb7c2e361e75c5dd77bfe3425cdbd111c
ffmcgee <joao.carlos@consensys.net>
2026-03-05 12:43:13 +0100

    test: fix call to openDappNetworkMenu

--- test/e2e/tests/network/deprecated-networks.spec.ts
+++ test/e2e/tests/network/deprecated-networks.spec.ts
@@ -4,6 +4,7 @@ import { withFixtures } from '../../helpers';
 import { DAPP_URL, WINDOW_TITLES } from '../../constants';
 import SelectNetwork from '../../page-objects/pages/dialog/select-network';
 import { loginWithBalanceValidation } from '../../page-objects/flows/login.flow';
+import HeaderNavbar from '../../page-objects/pages/header-navbar';
 
 describe('Deprecated networks', function (this: Suite) {
   it('User should not find goerli network when clicking on the network selector', async function () {
@@ -16,6 +17,8 @@ describe('Deprecated networks', function (this: Suite) {
       async ({ driver }) => {
         // Navigate to extension home screen
         await loginWithBalanceValidation(driver);
+        const headerNavbar = new HeaderNavbar(driver);
+
         // Open the first dapp which starts on chain '0x539
         await driver.openNewPage(DAPP_URL);
         await driver.clickElement({
@@ -40,7 +43,7 @@ describe('Deprecated networks', function (this: Suite) {
           .window()
           .setRect({ width: 400, height: 600 });
 
-        await driver.openDappNetworkMenu();
+        await headerNavbar.openDappNetworkMenu();
 
         const selectNetworkDialog = new SelectNetwork(driver);
         await selectNetworkDialog.checkPageIsLoaded();
