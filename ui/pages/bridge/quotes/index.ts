70a2cc3b9d494dcd771f012b85185d0582139dc8
ghgoodreau <hunter.goodreau@consensys.net>
2026-03-09 15:54:56 -0500

    fix unused export

--- ui/pages/bridge/quotes/index.ts
+++ ui/pages/bridge/quotes/index.ts
@@ -1,3 +1,2 @@
 export { MultichainBridgeQuoteCard } from './multichain-bridge-quote-card';
-export { MultichainBridgeQuoteCardSkeleton } from './multichain-bridge-quote-card-skeleton';
 export { BridgeQuotesModal } from './bridge-quotes-modal';
