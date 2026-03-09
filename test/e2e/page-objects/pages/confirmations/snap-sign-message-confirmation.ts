a56193a7e014e3b653b6cbaebf6c29b1d9d2c33a
LeVinhGithub <levinh.hcmut@gmail.com>
2026-03-09 22:16:17 +0700

    test: clean code

--- test/e2e/page-objects/pages/confirmations/snap-sign-message-confirmation.ts
+++ test/e2e/page-objects/pages/confirmations/snap-sign-message-confirmation.ts
@@ -31,16 +31,6 @@ class SnapSignMessageConfirmation {
     });
   }
 
-  async checkNetworkIsDisplayed(networkName: string): Promise<void> {
-    console.log(
-      `Checking network ${networkName} is displayed on snap sign message confirmation page.`,
-    );
-    await this.driver.waitForSelector({
-      text: networkName,
-      tag: 'p',
-    });
-  }
-
   async checkPageIsLoaded(): Promise<void> {
     try {
       await this.driver.waitForMultipleSelectors([
