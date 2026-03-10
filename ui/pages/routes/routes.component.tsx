0345de08ccf06df23032c01338bac60a929052a0
Francis Nepomuceno <n3ps@users.noreply.github.com>
2026-03-10 13:32:55 -0400

    merge fix

--- ui/pages/routes/routes.component.tsx
+++ ui/pages/routes/routes.component.tsx
@@ -685,14 +685,14 @@ export const routeConfig = [
     basicFunctionalityRequired: false,
   }),
   createRouteWithLayout({
-    path: `${MULTICHAIN_ACCOUNT_ADDRESS_LIST_PAGE_ROUTE}/:accountGroupId`,
+    path: MULTICHAIN_ACCOUNT_ADDRESS_LIST_PAGE_ROUTE,
     component: MultichainAccountAddressListPage,
     layout: RootLayout,
     authenticated: true,
     basicFunctionalityRequired: false,
   }),
   createRouteWithLayout({
-    path: `${MULTICHAIN_ACCOUNT_PRIVATE_KEY_LIST_PAGE_ROUTE}/:accountGroupId`,
+    path: MULTICHAIN_ACCOUNT_PRIVATE_KEY_LIST_PAGE_ROUTE,
     component: MultichainAccountPrivateKeyListPage,
     layout: RootLayout,
     authenticated: true,
