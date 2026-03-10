2a9e608b5917de0aa1c687a5bab3e94755ff5b7b
Maxime OUAIRY <maxime.ouairy-ext@consensys.net>
2026-03-10 15:38:44 +0100

    fix: swap activity fee label paid by metamask

--- ui/pages/bridge/transaction-details/transaction-details.tsx
+++ ui/pages/bridge/transaction-details/transaction-details.tsx
@@ -21,6 +21,7 @@ import {
   Icon,
   IconName,
   IconSize,
+  SuccessPill,
   Text,
 } from '../../../components/component-library';
 import {
@@ -427,19 +428,23 @@ const CrossChainSwapTxDetails = () => {
             <TransactionDetailRow
               title={t('bridgeTxDetailsTotalGasFee')}
               value={
-                <>
-                  {data?.hexGasTotal &&
-                    gasCurrency?.decimals &&
-                    gasCurrency?.symbol &&
-                    formatTokenAmount(
-                      locale,
-                      new Numeric(data.hexGasTotal, 16)
-                        .toBase(10)
-                        .shiftedBy(gasCurrency.decimals ?? 0)
-                        .toString(),
-                      gasCurrency?.symbol,
-                    )}
-                </>
+                data?.isGasFeeSponsored ? (
+                  <SuccessPill label={t('swapGasFeesSponsored')} />
+                ) : (
+                  <>
+                    {data?.hexGasTotal &&
+                      gasCurrency?.decimals &&
+                      gasCurrency?.symbol &&
+                      formatTokenAmount(
+                        locale,
+                        new Numeric(data.hexGasTotal, 16)
+                          .toBase(10)
+                          .shiftedBy(gasCurrency.decimals ?? 0)
+                          .toString(),
+                        gasCurrency?.symbol,
+                      )}
+                  </>
+                )
               }
             />
           </Box>
@@ -452,14 +457,12 @@ const CrossChainSwapTxDetails = () => {
             flexDirection={FlexDirection.Column}
             gap={2}
           >
-            <TransactionDetailRow
-              title={t('bridgeTxDetailsNonce')}
-              value={
-                srcChainTxMeta?.txParams.nonce
-                  ? hexToDecimal(srcChainTxMeta?.txParams.nonce)
-                  : undefined
-              }
-            />
+            {srcChainTxMeta?.txParams.nonce && (
+              <TransactionDetailRow
+                title={t('bridgeTxDetailsNonce')}
+                value={hexToDecimal(srcChainTxMeta.txParams.nonce)}
+              />
+            )}
           </Box>
         </React.Fragment>
       </Content>
