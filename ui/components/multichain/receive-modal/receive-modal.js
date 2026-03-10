44460fb1ac0c07fd9aeddd62fe4cc248f2a153a0
Francis Nepomuceno <n3ps@users.noreply.github.com>
2026-03-10 09:41:46 -0400

    revert

--- ui/components/multichain/receive-modal/receive-modal.js
+++ ui/components/multichain/receive-modal/receive-modal.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useRef } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import {
@@ -24,58 +24,16 @@ export const ReceiveModal = ({ address, onClose }) => {
     metadata: { name },
   } = useSelector((state) => getInternalAccountByAddress(state, address));
   const data = useMemo(() => ({ data: address }), [address]);
-  const dialogRef = useRef(null);
-  const hasClosedRef = useRef(false);
 
   useEffect(() => {
     endTrace({ name: TraceName.ReceiveModal });
   }, []);
 
-  const closeOnce = useCallback(() => {
-    if (hasClosedRef.current) {
-      return;
-    }
-    hasClosedRef.current = true;
-    onClose();
-  }, [onClose]);
-
-  const handleClose = useCallback(() => {
-    const el = dialogRef.current?.querySelector('.mm-modal-content__dialog');
-    if (!el) {
-      closeOnce();
-      return;
-    }
-    const didReplace = el.classList.replace(
-      'page-enter-animation',
-      'page-exit-animation',
-    );
-    if (!didReplace) {
-      closeOnce();
-      return;
-    }
-
-    el.addEventListener('animationend', closeOnce, { once: true });
-    el.addEventListener('animationcancel', closeOnce, { once: true });
-
-    window.requestAnimationFrame(() => {
-      if (typeof el.getAnimations !== 'function') {
-        return;
-      }
-
-      if (el.getAnimations().length === 0) {
-        closeOnce();
-      }
-    });
-  }, [closeOnce]);
-
   return (
-    <Modal isOpen onClose={handleClose}>
+    <Modal isOpen onClose={onClose}>
       <ModalOverlay />
-      <ModalContent
-        ref={dialogRef}
-        modalDialogProps={{ className: 'page-enter-animation' }}
-      >
-        <ModalHeader marginBottom={4} onClose={handleClose}>
+      <ModalContent>
+        <ModalHeader marginBottom={4} onClose={onClose}>
           {t('receive')}
         </ModalHeader>
         <Box
