1776aeaf1c2d2bc85a8cfede09a465fb3b61d9c3
ameliejyc <amelie.chan@gmail.com>
2026-03-10 10:10:18 +0000

    chore: use createToggleItem where available

--- ui/pages/settings-v2/privacy-tab/privacy-tab.tsx
+++ ui/pages/settings-v2/privacy-tab/privacy-tab.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
 import { SettingItemConfig } from '../types';
-import { SettingsTab } from '../shared';
+import { SettingsTab, createToggleItem } from '../shared';
+import { getPreferences } from '../../../selectors';
+import {
+  setUseMultiAccountBalanceChecker,
+  setSkipDeepLinkInterstitial,
+} from '../../../store/actions';
+import type { MetaMaskReduxState } from '../../../store/store';
 import { BasicFunctionalityToggleItem } from './basic-functionality-item';
-import { BatchAccountBalanceRequestsToggleItem } from './batch-account-balance-requests-item';
-import { SkipLinkConfirmationToggleItem } from './skip-link-confirmation-item';
 import { MetametricsToggleItem } from './metametrics-item';
 import { DataCollectionToggleItem } from './data-collection-item';
+
+const BatchAccountBalanceRequestsToggleItem = createToggleItem({
+  name: 'BatchAccountBalanceRequestsToggleItem',
+  titleKey: 'useMultiAccountBalanceChecker',
+  descriptionKey: 'useMultiAccountBalanceCheckerSettingDescriptionV2',
+  selector: (state: MetaMaskReduxState) =>
+    state.metamask.useMultiAccountBalanceChecker,
+  action: setUseMultiAccountBalanceChecker,
+  dataTestId: 'batch-account-balance-requests-toggle',
+});
+
+const SkipLinkConfirmationToggleItem = createToggleItem({
+  name: 'SkipLinkConfirmationToggleItem',
+  titleKey: 'skipLinkConfirmationScreens',
+  descriptionKey: 'skipLinkConfirmationScreensDescription',
+  selector: (state: MetaMaskReduxState) =>
+    Boolean(getPreferences(state).skipDeepLinkInterstitial),
+  action: setSkipDeepLinkInterstitial,
+  dataTestId: 'skip-link-confirmation-toggle',
+});
 
 /** Registry of setting items for the Privacy page. Add new items here */
 const PRIVACY_SETTING_ITEMS: SettingItemConfig[] = [
