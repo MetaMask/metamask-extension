a6eadb057de988bd13f0ba5319df67b0765fbc17
Francis Nepomuceno <n3ps@users.noreply.github.com>
2026-03-10 16:49:15 -0400

    update test

--- ui/components/multichain-accounts/multichain-account-menu/multichain-account-menu.test.tsx
+++ ui/components/multichain-accounts/multichain-account-menu/multichain-account-menu.test.tsx
@@ -10,6 +10,7 @@ import {
   MetaMetricsEventName,
 } from '../../../../shared/constants/metametrics';
 import { enLocale as messages } from '../../../../test/lib/i18n-helpers';
+import { MULTICHAIN_ACCOUNT_DETAILS_PAGE_ROUTE } from '../../../helpers/constants/routes';
 import { MultichainAccountMenu } from './multichain-account-menu';
 import type { MultichainAccountMenuProps } from './multichain-account-menu.types';
 
@@ -218,7 +219,7 @@ describe('MultichainAccountMenu', () => {
     }
 
     expect(mockUseNavigate).toHaveBeenCalledWith(
-      '/multichain-account-details/entropy%3A01JKAF3DSGM3AB87EM9N0K41AJ%2Fdefault',
+      `${MULTICHAIN_ACCOUNT_DETAILS_PAGE_ROUTE}?accountGroupId=entropy%3A01JKAF3DSGM3AB87EM9N0K41AJ%2Fdefault`,
     );
   });
 
