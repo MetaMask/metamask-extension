0421f6d62b60ed1ff4acd491acc6c86a1db943fc
ameliejyc <amelie.chan@gmail.com>
2026-03-10 16:50:56 +0000

    feat: add MakeSmartContractsEasierItem and NFT items

--- ui/pages/settings-v2/privacy-tab/third-party-apis-sub-page.tsx
+++ ui/pages/settings-v2/privacy-tab/third-party-apis-sub-page.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import { SettingItemConfig } from '../types';
-import { SettingsTab } from '../shared';
+import { SettingsTab, createToggleItem } from '../shared';
+import { DisplayNftMediaToggleItem } from '../shared/display-nft-media-item';
+import { AutodetectNftsToggleItem } from '../shared/autodetect-nfts-item';
+import { setUse4ByteResolution } from '../../../store/actions';
+import type { MetaMaskReduxState } from '../../../store/store';
+
+const MakeSmartContractsEasierItem = createToggleItem({
+  name: 'MakeSmartContractsEasierItem',
+  titleKey: 'makeSmartContractsEasier',
+  descriptionKey: 'makeSmartContractsEasierDescription',
+  selector: (state: MetaMaskReduxState) => state.metamask.use4ByteResolution,
+  action: setUse4ByteResolution,
+  dataTestId: 'smart-contracts-easier-toggle',
+});
 
 /** Registry of setting items for the Third-party APIs sub-page */
 const THIRD_PARTY_APIS_ITEMS: SettingItemConfig[] = [
+  { id: 'use-4byte-resolution', component: MakeSmartContractsEasierItem },
+  {
+    id: 'display-nft-media',
+    component: DisplayNftMediaToggleItem,
+    hasDividerBefore: true,
+  },
+  { id: 'autodetect-nfts', component: AutodetectNftsToggleItem },
 ];
 
 const ThirdPartyApisSubPage = () => (
