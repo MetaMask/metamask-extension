2ab51d0dc8c5492ab670c61e760ab88ac46d52b0
ameliejyc <amelie.chan@gmail.com>
2026-03-11 10:06:05 +0000

    feat: add show ENS domains settings

--- ui/pages/settings-v2/privacy-tab/third-party-apis-sub-page.tsx
+++ ui/pages/settings-v2/privacy-tab/third-party-apis-sub-page.tsx
@@ -1,23 +1,64 @@
 import React from 'react';
+import { Text, TextColor, TextVariant } from '@metamask/design-system-react';
 import { SettingItemConfig } from '../types';
 import { SettingsTab, createToggleItem } from '../shared';
 import { DisplayNftMediaToggleItem } from '../shared/display-nft-media-item';
 import { AutodetectNftsToggleItem } from '../shared/autodetect-nfts-item';
-import { setUse4ByteResolution } from '../../../store/actions';
+import {
+  setUse4ByteResolution,
+  setUseAddressBarEnsResolution,
+} from '../../../store/actions';
 import type { MetaMaskReduxState } from '../../../store/store';
+import { IpfsGatewayItem } from './ipfs-gateway-item';
 
-const MakeSmartContractsEasierItem = createToggleItem({
-  name: 'MakeSmartContractsEasierItem',
+const ShowENSDomainsToggleItem = createToggleItem({
+  name: 'ShowENSDomainsToggleItem',
+  titleKey: 'ensDomainsSettingTitle',
+  formatDescription: (t) => (
+    <>
+      <Text color={TextColor.TextAlternative} variant={TextVariant.BodyMd}>
+        {t('ensDomainsSettingDescriptionIntroduction')}
+      </Text>
+      <ul className="list-disc pl-6">
+        <li>
+          <Text color={TextColor.TextAlternative} variant={TextVariant.BodyMd}>
+            {t('ensDomainsSettingDescriptionPart1')}
+          </Text>
+        </li>
+        <li>
+          <Text color={TextColor.TextAlternative} variant={TextVariant.BodyMd}>
+            {t('ensDomainsSettingDescriptionPart2')}
+          </Text>
+        </li>
+      </ul>
+      <Text color={TextColor.TextAlternative} variant={TextVariant.BodyMd}>
+        {t('ensDomainsSettingDescriptionOutroduction')}
+      </Text>
+    </>
+  ),
+  selector: (state: MetaMaskReduxState) =>
+    state.metamask.useAddressBarEnsResolution,
+  action: setUseAddressBarEnsResolution,
+  dataTestId: 'ens-domains-toggle',
+});
+
+const MakeSmartContractsEasierToggleItem = createToggleItem({
+  name: 'MakeSmartContractsEasierToggleItem',
   titleKey: 'makeSmartContractsEasier',
   descriptionKey: 'makeSmartContractsEasierDescription',
   selector: (state: MetaMaskReduxState) => state.metamask.use4ByteResolution,
   action: setUse4ByteResolution,
-  dataTestId: 'smart-contracts-easier-toggle',
+  dataTestId: 'make-smart-contracts-easier-toggle',
 });
 
 /** Registry of setting items for the Third-party APIs sub-page */
 const THIRD_PARTY_APIS_ITEMS: SettingItemConfig[] = [
-  { id: 'use-4byte-resolution', component: MakeSmartContractsEasierItem },
+  { id: 'ipfs-gateway', component: IpfsGatewayItem },
+  { id: 'show-ens-domains', component: ShowENSDomainsToggleItem },
+  {
+    id: 'make-smart-contracts-easier',
+    component: MakeSmartContractsEasierToggleItem,
+  },
   {
     id: 'display-nft-media',
     component: DisplayNftMediaToggleItem,
