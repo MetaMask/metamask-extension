c4b6840aefb692a9590b75129f48605b01feb71b
HJetpoluru <harika.jetpoluru@consensys.net>
2026-03-11 14:48:09 -0400

    Fixed lint

--- test/e2e/fixtures/fixture-builder-v2.ts
+++ test/e2e/fixtures/fixture-builder-v2.ts
@@ -2,7 +2,6 @@ import { merge, cloneDeep } from 'lodash';
 import type { AccountsControllerState } from '@metamask/accounts-controller';
 import type { AddressBookControllerState } from '@metamask/address-book-controller';
 import type { CurrencyRateState } from '@metamask/assets-controllers';
-import type { KeyringControllerState } from '@metamask/keyring-controller';
 import { type NameControllerState, NameType } from '@metamask/name-controller';
 import type { NetworkEnablementControllerState } from '@metamask/network-enablement-controller';
 import type { SelectedNetworkControllerState } from '@metamask/selected-network-controller';
@@ -105,24 +104,13 @@ class FixtureBuilderV2 {
     return this;
   }
 
-  /**
-   * Marks the Snaps install privacy warning as already shown so the UI does
-   * not display it. Use for flows that only test update/reconnect and do not
-   * need the first-time install privacy step.
-   */
-  withSnapsPrivacyWarningAlreadyShown(): this {
-    return this.withAppStateController({
-      snapsInstallPrivacyWarningShown: true,
-    });
-  }
-
-  withMetaVersion(version: number): this {
-    this.fixture.meta.version = version;
+  withCurrencyController(data: Partial<CurrencyRateState>): this {
+    merge(this.fixture.data.CurrencyController, data);
     return this;
   }
 
-  withCurrencyController(data: Partial<CurrencyRateState>): this {
-    merge(this.fixture.data.CurrencyController, data);
+  withNameController(data: Partial<NameControllerState>): this {
+    merge(this.fixture.data.NameController, data);
     return this;
   }
 
@@ -135,6 +123,11 @@ class FixtureBuilderV2 {
     data: Partial<NetworkEnablementControllerState>,
   ): this {
     merge(this.fixture.data.NetworkEnablementController, data);
+    return this;
+  }
+
+  withMetaVersion(version: number): this {
+    this.fixture.meta.version = version;
     return this;
   }
 
@@ -159,15 +152,16 @@ class FixtureBuilderV2 {
     return this;
   }
 
+  withSnapsPrivacyWarningAlreadyShown(): this {
+    return this.withAppStateController({
+      snapsInstallPrivacyWarningShown: true,
+    });
+  }
+
   withSelectedNetworkController(
     data: Partial<SelectedNetworkControllerState>,
   ): this {
     merge(this.fixture.data.SelectedNetworkController, data);
-    return this;
-  }
-
-  withNameController(data: Partial<NameControllerState>): this {
-    merge(this.fixture.data.NameController, data);
     return this;
   }
 
@@ -208,6 +202,19 @@ class FixtureBuilderV2 {
     this.fixture.data.NetworkEnablementController.enabledNetworkMap =
       data as FixtureType['data']['NetworkEnablementController']['enabledNetworkMap'];
     return this;
+  }
+
+  withKeyringController(
+    data: Partial<FixtureType['data']['KeyringController']>,
+  ): this {
+    merge(this.fixture.data.KeyringController, data);
+    return this;
+  }
+
+  withKeyringControllerMultiSRP(): this {
+    return this.withKeyringController({
+      vault: MULTI_SRP_FIXTURE_VAULT,
+    });
   }
 
   withLedgerAccount(): this {
@@ -342,6 +349,11 @@ class FixtureBuilderV2 {
     });
   }
 
+  withMetaMetricsController(data: Partial<MetaMetricsControllerState>): this {
+    merge(this.fixture.data.MetaMetricsController, data);
+    return this;
+  }
+
   withNoNames(): this {
     // Direct assignment instead of merge so existing petname entries are cleared if any
     this.fixture.data.NameController.names = {
@@ -427,6 +439,107 @@ class FixtureBuilderV2 {
     return this.withPermissionController({ subjects });
   }
 
+  withSnapController(
+    data: Partial<FixtureType['data']['SnapController']>,
+  ): this {
+    (this.fixture.data as Record<string, unknown>).SnapController ??= {};
+    merge(this.fixture.data.SnapController, data);
+    return this;
+  }
+
+  withSnapControllerOnStartLifecycleSnap(): this {
+    return this.withPermissionController({
+      subjects: {
+        'npm:@metamask/lifecycle-hooks-example-snap': {
+          origin: 'npm:@metamask/lifecycle-hooks-example-snap',
+          permissions: {
+            'endowment:lifecycle-hooks': {
+              caveats: null,
+              date: 1750244440562,
+              id: '0eKn8SjGEH6o_6Mhcq3Lw',
+              invoker: 'npm:@metamask/lifecycle-hooks-example-snap',
+              parentCapability: 'endowment:lifecycle-hooks',
+            },
+            // eslint-disable-next-line @typescript-eslint/naming-convention
+            snap_dialog: {
+              caveats: null,
+              date: 1750244440562,
+              id: 'Fbme_UWcuSK92JqfrT4G2',
+              invoker: 'npm:@metamask/lifecycle-hooks-example-snap',
+              parentCapability: 'snap_dialog',
+            },
+          },
+        },
+      },
+    }).withSnapController({
+      isReady: true,
+      snapStates: {},
+      unencryptedSnapStates: {},
+      snaps: {
+        'npm:@metamask/lifecycle-hooks-example-snap': {
+          auxiliaryFiles: [],
+          blocked: false,
+          enabled: true,
+          id: 'npm:@metamask/lifecycle-hooks-example-snap',
+          initialPermissions: {
+            'endowment:lifecycle-hooks': {},
+            // eslint-disable-next-line @typescript-eslint/naming-convention
+            snap_dialog: {},
+          },
+          localizationFiles: [],
+          manifest: {
+            description:
+              'MetaMask example snap demonstrating the use of the `onStart`, `onInstall`, and `onUpdate` lifecycle hooks.',
+            initialPermissions: {
+              'endowment:lifecycle-hooks': {},
+              // eslint-disable-next-line @typescript-eslint/naming-convention
+              snap_dialog: {},
+            },
+            manifestVersion: '0.1',
+            platformVersion: '8.1.0',
+            proposedName: 'Lifecycle Hooks Example Snap',
+            repository: {
+              type: 'git',
+              url: 'https://github.com/MetaMask/snaps.git',
+            },
+            source: {
+              location: {
+                npm: {
+                  filePath: 'dist/bundle.js',
+                  packageName: '@metamask/lifecycle-hooks-example-snap',
+                  registry: 'https://registry.npmjs.org',
+                },
+              },
+              shasum: '5tlM5E71Fbeid7I3F0oQURWL7/+0620wplybtklBCHQ=',
+            },
+            version: '2.2.0',
+          },
+          /* eslint-disable no-template-curly-in-string */
+          sourceCode:
+            '(()=>{var e={d:(n,t)=>{for(var a in t)e.o(t,a)&&!e.o(n,a)&&Object.defineProperty(n,a,{enumerable:!0,get:t[a]})},o:(e,n)=>Object.prototype.hasOwnProperty.call(e,n),r:e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})}},n={};(()=>{"use strict";function t(e,n,t){if("string"==typeof e)throw new Error(`An HTML element ("${String(e)}") was used in a Snap component, which is not supported by Snaps UI. Please use one of the supported Snap components.`);if(!e)throw new Error("A JSX fragment was used in a Snap component, which is not supported by Snaps UI. Please use one of the supported Snap components.");return e({...n,key:t})}function a(e){return Object.fromEntries(Object.entries(e).filter((([,e])=>void 0!==e)))}function r(e){return n=>{const{key:t=null,...r}=n;return{type:e,props:a(r),key:t}}}e.r(n),e.d(n,{onInstall:()=>p,onStart:()=>l,onUpdate:()=>d});const o=r("Box"),s=r("Text"),l=async()=>await snap.request({method:"snap_dialog",params:{type:"alert",content:t(o,{children:t(s,{children:\'The client was started successfully, and the "onStart" handler was called.\'})})}}),p=async()=>await snap.request({method:"snap_dialog",params:{type:"alert",content:t(o,{children:t(s,{children:\'The Snap was installed successfully, and the "onInstall" handler was called.\'})})}}),d=async()=>await snap.request({method:"snap_dialog",params:{type:"alert",content:t(o,{children:t(s,{children:\'The Snap was updated successfully, and the "onUpdate" handler was called.\'})})}})})(),module.exports=n})();',
+          /* eslint-enable no-template-curly-in-string */
+          status: 'stopped',
+          version: '2.2.0',
+          versionHistory: [
+            {
+              date: 1750244439310,
+              origin: 'https://metamask.github.io',
+              version: '2.2.0',
+            },
+          ],
+        },
+      },
+    });
+  }
+
+  withSmartTransactionsOptedOut(): this {
+    return this.withPreferencesController({
+      preferences: {
+        smartTransactionsOptInStatus: false,
+      },
+    });
+  }
+
   withSelectedNetwork(
     networkClientId: NetworkClientIdValue = NETWORK_CLIENT_ID.MAINNET,
   ): this {
@@ -452,196 +565,10 @@ class FixtureBuilderV2 {
     });
   }
 
-  withTrezorAccount(): this {
-    return this.withAccountsController({
-      internalAccounts: {
-        accounts: {
-          'd5e45e4a-3b04-4a09-a5e1-39762e5c6be4': {
-            id: 'd5e45e4a-3b04-4a09-a5e1-39762e5c6be4',
-            address: DEFAULT_FIXTURE_ACCOUNT_LOWERCASE,
-            options: {
-              entropySource: '01KGHAX3WXGMX9H76THHSSV553',
-              derivationPath: "m/44'/60'/0'/0/0",
-              groupIndex: 0,
-              entropy: {
-                type: 'mnemonic',
-                id: '01KGHAX3WXGMX9H76THHSSV553',
-                derivationPath: "m/44'/60'/0'/0/0",
-                groupIndex: 0,
-              },
-            },
-            methods: [
-              'personal_sign',
-              'eth_sign',
-              'eth_signTransaction',
-              'eth_signTypedData_v1',
-              'eth_signTypedData_v3',
-              'eth_signTypedData_v4',
-            ],
-            type: 'eip155:eoa',
-            scopes: ['eip155:0'],
-            metadata: {
-              name: 'Account 1',
-              importTime: 1724486724986,
-              lastSelected: 1665507600000,
-              keyring: {
-                type: 'HD Key Tree',
-              },
-            },
-          },
-          [HARDWARE_WALLET_ACCOUNT_ID]: {
-            id: HARDWARE_WALLET_ACCOUNT_ID,
-            address: TREZOR_ADDRESS,
-            options: {},
-            methods: [
-              'personal_sign',
-              'eth_sign',
-              'eth_signTransaction',
-              'eth_signTypedData_v1',
-              'eth_signTypedData_v3',
-              'eth_signTypedData_v4',
-            ],
-            type: 'eip155:eoa',
-            scopes: ['eip155:0'],
-            metadata: {
-              name: 'Trezor 1',
-              importTime: 1724486729079,
-              keyring: {
-                type: 'Trezor Hardware',
-              },
-              lastSelected: 1724486729083,
-            },
-          },
-        },
-        selectedAccount: HARDWARE_WALLET_ACCOUNT_ID,
-      },
-    }).withKeyringController({ vault: TREZOR_VAULT });
-  }
-
   withShowNativeTokenAsMainBalanceEnabled(): this {
     return this.withPreferencesController({
       preferences: {
         showNativeTokenAsMainBalance: true,
-      },
-    });
-  }
-
-  withKeyringController(
-    data: Partial<FixtureType['data']['KeyringController']>,
-  ): this {
-    merge(this.fixture.data.KeyringController, data);
-    return this;
-  }
-
-  withKeyringControllerMultiSRP(): this {
-    return this.withKeyringController({
-      vault: MULTI_SRP_FIXTURE_VAULT,
-    });
-  }
-
-  withMetaMetricsController(data: Partial<MetaMetricsControllerState>): this {
-    merge(this.fixture.data.MetaMetricsController, data);
-    return this;
-  }
-
-  withSnapController(
-    data: Partial<FixtureType['data']['SnapController']>,
-  ): this {
-    (this.fixture.data as Record<string, unknown>).SnapController ??= {};
-    merge(this.fixture.data.SnapController, data);
-    return this;
-  }
-
-  withSnapControllerOnStartLifecycleSnap(): this {
-    return this.withPermissionController({
-      subjects: {
-        'npm:@metamask/lifecycle-hooks-example-snap': {
-          origin: 'npm:@metamask/lifecycle-hooks-example-snap',
-          permissions: {
-            'endowment:lifecycle-hooks': {
-              caveats: null,
-              date: 1750244440562,
-              id: '0eKn8SjGEH6o_6Mhcq3Lw',
-              invoker: 'npm:@metamask/lifecycle-hooks-example-snap',
-              parentCapability: 'endowment:lifecycle-hooks',
-            },
-            // eslint-disable-next-line @typescript-eslint/naming-convention -- snap_dialog is Snaps API permission name
-            snap_dialog: {
-              caveats: null,
-              date: 1750244440562,
-              id: 'Fbme_UWcuSK92JqfrT4G2',
-              invoker: 'npm:@metamask/lifecycle-hooks-example-snap',
-              parentCapability: 'snap_dialog',
-            },
-          },
-        },
-      },
-    }).withSnapController({
-      isReady: true,
-      snapStates: {},
-      unencryptedSnapStates: {},
-      snaps: {
-        'npm:@metamask/lifecycle-hooks-example-snap': {
-          auxiliaryFiles: [],
-          blocked: false,
-          enabled: true,
-          id: 'npm:@metamask/lifecycle-hooks-example-snap',
-          initialPermissions: {
-            'endowment:lifecycle-hooks': {},
-            // eslint-disable-next-line @typescript-eslint/naming-convention -- snap_dialog is Snaps API permission name
-            snap_dialog: {},
-          },
-          localizationFiles: [],
-          manifest: {
-            description:
-              'MetaMask example snap demonstrating the use of the `onStart`, `onInstall`, and `onUpdate` lifecycle hooks.',
-            initialPermissions: {
-              'endowment:lifecycle-hooks': {},
-              // eslint-disable-next-line @typescript-eslint/naming-convention -- snap_dialog is Snaps API permission name
-              snap_dialog: {},
-            },
-            manifestVersion: '0.1',
-            platformVersion: '8.1.0',
-            proposedName: 'Lifecycle Hooks Example Snap',
-            repository: {
-              type: 'git',
-              url: 'https://github.com/MetaMask/snaps.git',
-            },
-            source: {
-              location: {
-                npm: {
-                  filePath: 'dist/bundle.js',
-                  packageName: '@metamask/lifecycle-hooks-example-snap',
-                  registry: 'https://registry.npmjs.org',
-                },
-              },
-              shasum: '5tlM5E71Fbeid7I3F0oQURWL7/+0620wplybtklBCHQ=',
-            },
-            version: '2.2.0',
-          },
-          /* Minified snap source - contains literal "${String(e)}" from bundled code */
-          /* eslint-disable no-template-curly-in-string */
-          sourceCode:
-            '(()=>{var e={d:(n,t)=>{for(var a in t)e.o(t,a)&&!e.o(n,a)&&Object.defineProperty(n,a,{enumerable:!0,get:t[a]})},o:(e,n)=>Object.prototype.hasOwnProperty.call(e,n),r:e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})}},n={};(()=>{"use strict";function t(e,n,t){if("string"==typeof e)throw new Error(`An HTML element ("${String(e)}") was used in a Snap component, which is not supported by Snaps UI. Please use one of the supported Snap components.`);if(!e)throw new Error("A JSX fragment was used in a Snap component, which is not supported by Snaps UI. Please use one of the supported Snap components.");return e({...n,key:t})}function a(e){return Object.fromEntries(Object.entries(e).filter((([,e])=>void 0!==e)))}function r(e){return n=>{const{key:t=null,...r}=n;return{type:e,props:a(r),key:t}}}e.r(n),e.d(n,{onInstall:()=>p,onStart:()=>l,onUpdate:()=>d});const o=r("Box"),s=r("Text"),l=async()=>await snap.request({method:"snap_dialog",params:{type:"alert",content:t(o,{children:t(s,{children:\'The client was started successfully, and the "onStart" handler was called.\'})})}}),p=async()=>await snap.request({method:"snap_dialog",params:{type:"alert",content:t(o,{children:t(s,{children:\'The Snap was installed successfully, and the "onInstall" handler was called.\'})})}}),d=async()=>await snap.request({method:"snap_dialog",params:{type:"alert",content:t(o,{children:t(s,{children:\'The Snap was updated successfully, and the "onUpdate" handler was called.\'})})}})})(),module.exports=n})();',
-          /* eslint-enable no-template-curly-in-string */
-          status: 'stopped',
-          version: '2.2.0',
-          versionHistory: [
-            {
-              date: 1750244439310,
-              origin: 'https://metamask.github.io',
-              version: '2.2.0',
-            },
-          ],
-        },
-      },
-    });
-  }
-
-  withSmartTransactionsOptedOut(): this {
-    return this.withPreferencesController({
-      preferences: {
-        smartTransactionsOptInStatus: false,
       },
     });
   }
@@ -725,6 +652,72 @@ class FixtureBuilderV2 {
       type: TransactionType.incoming,
     };
     return this.withTransactionController({ transactions: [incomingTx] });
+  }
+
+  withTrezorAccount(): this {
+    return this.withAccountsController({
+      internalAccounts: {
+        accounts: {
+          'd5e45e4a-3b04-4a09-a5e1-39762e5c6be4': {
+            id: 'd5e45e4a-3b04-4a09-a5e1-39762e5c6be4',
+            address: DEFAULT_FIXTURE_ACCOUNT_LOWERCASE,
+            options: {
+              entropySource: '01KGHAX3WXGMX9H76THHSSV553',
+              derivationPath: "m/44'/60'/0'/0/0",
+              groupIndex: 0,
+              entropy: {
+                type: 'mnemonic',
+                id: '01KGHAX3WXGMX9H76THHSSV553',
+                derivationPath: "m/44'/60'/0'/0/0",
+                groupIndex: 0,
+              },
+            },
+            methods: [
+              'personal_sign',
+              'eth_sign',
+              'eth_signTransaction',
+              'eth_signTypedData_v1',
+              'eth_signTypedData_v3',
+              'eth_signTypedData_v4',
+            ],
+            type: 'eip155:eoa',
+            scopes: ['eip155:0'],
+            metadata: {
+              name: 'Account 1',
+              importTime: 1724486724986,
+              lastSelected: 1665507600000,
+              keyring: {
+                type: 'HD Key Tree',
+              },
+            },
+          },
+          [HARDWARE_WALLET_ACCOUNT_ID]: {
+            id: HARDWARE_WALLET_ACCOUNT_ID,
+            address: TREZOR_ADDRESS,
+            options: {},
+            methods: [
+              'personal_sign',
+              'eth_sign',
+              'eth_signTransaction',
+              'eth_signTypedData_v1',
+              'eth_signTypedData_v3',
+              'eth_signTypedData_v4',
+            ],
+            type: 'eip155:eoa',
+            scopes: ['eip155:0'],
+            metadata: {
+              name: 'Trezor 1',
+              importTime: 1724486729079,
+              keyring: {
+                type: 'Trezor Hardware',
+              },
+              lastSelected: 1724486729083,
+            },
+          },
+        },
+        selectedAccount: HARDWARE_WALLET_ACCOUNT_ID,
+      },
+    }).withKeyringController({ vault: TREZOR_VAULT });
   }
 
   build() {
