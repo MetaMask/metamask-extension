81f94f8b4f2184e9bb6c6f4c05ffde7f8b112cc5
saraantole <sara.antole@gmail.com>
2026-03-11 15:18:46 +0100

    test: remove unnecessary UTM parameter in site-matching whitelist entry

--- app/scripts/constants/marketing-site-whitelist.ts
+++ app/scripts/constants/marketing-site-whitelist.ts
@@ -3,7 +3,7 @@ export const COOKIE_ID_MARKETING_WHITELIST = [
   'https://learn.metamask.io',
   'https://mmi-support.zendesk.com',
   'https://community.metamask.io',
-  'https://support.metamask.io?utm_source=extension',
+  'https://support.metamask.io',
 ];
 
 if (process.env.IN_TEST) {
