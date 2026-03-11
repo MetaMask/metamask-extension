0ccf738d57b00d3096bdd1fc919d774500f7e013
Micaela Estabillo <micaela.estabillo@consensys.net>
2026-03-11 09:51:28 -0700

    fix: rm unused import

--- ui/pages/bridge/prepare/bridge-input-group.tsx
+++ ui/pages/bridge/prepare/bridge-input-group.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useSelector, shallowEqual } from 'react-redux';
 import {
   formatChainIdToCaip,
