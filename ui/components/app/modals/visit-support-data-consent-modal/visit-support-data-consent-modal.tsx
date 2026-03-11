e560f47a03560f1c74b03841390856ca147fb683
saraantole <sara.antole@gmail.com>
2026-03-11 20:16:02 +0100

    fix: fix URL normalization inconsistency between accept and reject paths

--- ui/components/app/modals/visit-support-data-consent-modal/visit-support-data-consent-modal.tsx
+++ ui/components/app/modals/visit-support-data-consent-modal/visit-support-data-consent-modal.tsx
@@ -56,24 +56,22 @@ const VisitSupportDataConsentModal: React.FC<
       shieldCustomerId?: string;
     }) => {
       onClose();
-      let supportLinkWithUserId = SUPPORT_LINK as string;
-      const queryParams = new URLSearchParams();
-      queryParams.append('metamask_version', params.version);
+      const url = new URL(SUPPORT_LINK as string);
+      url.searchParams.append('metamask_version', params.version);
       if (params.profileId) {
-        queryParams.append('metamask_profile_id', params.profileId);
+        url.searchParams.append('metamask_profile_id', params.profileId);
       }
       if (params.metaMetricsId) {
-        queryParams.append('metamask_metametrics_id', params.metaMetricsId);
+        url.searchParams.append(
+          'metamask_metametrics_id',
+          params.metaMetricsId,
+        );
       }
       if (params.shieldCustomerId) {
-        queryParams.append('shield_id', params.shieldCustomerId);
+        url.searchParams.append('shield_id', params.shieldCustomerId);
       }
 
-      const queryString = queryParams.toString();
-      if (queryString) {
-        const separator = supportLinkWithUserId.includes('?') ? '&' : '?';
-        supportLinkWithUserId += `${separator}${queryString}`;
-      }
+      const supportLinkWithUserId = url.toString();
 
       trackEvent(
         {
