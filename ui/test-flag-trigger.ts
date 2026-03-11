73e525bcb9bc449dc68b3bc4a5c73ea6d5cd11c7
Maggie <magdalena.mladin@consensys.net>
2026-03-11 17:34:13 +0200

    test: trigger feature flag registry check

--- ui/test-flag-trigger.ts
+++ ui/test-flag-trigger.ts
@@ -3,6 +3,5 @@ import { getRemoteFeatureFlags } from './selectors/remote-feature-flags';
 type State = Parameters<typeof getRemoteFeatureFlags>[0];
 
 export function getTestFlag(state: State) {
-  const flags = getRemoteFeatureFlags(state);
-  return flags?.thisIsAFakeTestFlag;
+  return getRemoteFeatureFlags(state)?.thisIsAFakeTestFlag;
 }
