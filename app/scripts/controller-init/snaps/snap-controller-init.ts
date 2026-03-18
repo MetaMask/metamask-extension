19c3ddd2806fe258ed0f39d6886bd5fe1f2bc139
Salah-Eddine Saakoun <salah-eddine.saakoun@consensys.net>
2026-03-18 10:57:01 +0100

    chore: replace preview builds with released package versions

--- app/scripts/controller-init/snaps/snap-controller-init.ts
+++ app/scripts/controller-init/snaps/snap-controller-init.ts
@@ -110,7 +110,9 @@ export const SnapControllerInit: ControllerInitFunction<
     // the expected type.
     // TODO: Look into the type mismatch.
     state: persistedState.SnapController,
-
+    // @ts-expect-error: `controllerMessenger` is not compatible with the
+    // expected type.
+    // TODO: Look into the type mismatch.
     messenger: controllerMessenger,
     featureFlags: {
       allowLocalSnaps,
