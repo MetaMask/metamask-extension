e26d70135bdc7d8576b8254cc0459b21b7f22172
salimtb <salim.toubal@consensys.net>
2026-03-19 15:35:50 +0100

    fix: fix unit tests

--- ui/store/actions.test.js
+++ ui/store/actions.test.js
@@ -1955,6 +1955,7 @@ describe('Actions', () => {
       expect(addCustomAssetStub.firstCall.args).toStrictEqual([
         accountId,
         'eip155:1/erc20:0xbbb',
+        undefined,
       ]);
 
       const actionTypes = store.getActions().map((a) => a.type);
