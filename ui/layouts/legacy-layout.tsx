2a7c29d654dc6e544d172ff3244f399ac17a69b6
Francis Nepomuceno <n3ps@users.noreply.github.com>
2026-03-22 01:10:22 -0400

    lint

--- ui/layouts/legacy-layout.tsx
+++ ui/layouts/legacy-layout.tsx
@@ -4,8 +4,6 @@ import { Outlet } from 'react-router-dom';
 /**
  * Temporary layout until we migrate each page that uses this.
  *
- * @param props - Props
- * @param props.children - Child component to render
  * @returns Component wrapped in legacy structure
  */
 export const LegacyLayout = () => {
