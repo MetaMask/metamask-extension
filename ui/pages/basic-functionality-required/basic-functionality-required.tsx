8ad47891f035b012d9cc18f9e5f93491eb103142
Francis Nepomuceno <n3ps@users.noreply.github.com>
2026-03-23 00:47:00 -0400

    restore locale

--- ui/pages/basic-functionality-required/basic-functionality-required.tsx
+++ ui/pages/basic-functionality-required/basic-functionality-required.tsx
@@ -39,7 +39,7 @@ const SEGMENT_CTA_MAPPING: Record<string, string> = {
   snaps: 'basicFunctionalityRequired_openSnapsPage',
   swaps: 'basicFunctionalityRequired_openSwapsPage',
   defi: 'basicFunctionalityRequired_openDefiPage',
-  musd: 'basicFunctionalityRequired_openMUSDPage',
+  musd: 'basicFunctionalityRequired_openMusdConversionPage',
   'nonevm-balance-check':
     'basicFunctionalityRequired_openCreateSnapAccountPage',
   'shield-plan': 'basicFunctionalityRequired_openTransactionShieldPage',
