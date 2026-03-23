09692b3b03c476f5f4435ea7897a296d8faa9a38
David Murdoch <david@davidmurdoch.com>
2026-03-23 10:55:48 -0400

    fix: restore setimmediate webpack polyfill

--- app/scripts/lib/segment/analytics.js
+++ app/scripts/lib/segment/analytics.js
@@ -101,7 +101,7 @@ export default class Analytics {
    */
   enqueue(type, msg, callback = noop) {
     if (!this.enable) {
-      setTimeout(callback, 0);
+      setImmediate(callback);
       return;
     }
 
@@ -158,7 +158,7 @@ export default class Analytics {
    */
   flush(callback = noop) {
     if (!this.enable) {
-      setTimeout(callback, 0);
+      setImmediate(callback);
       return Promise.resolve();
     }
 
@@ -168,7 +168,7 @@ export default class Analytics {
     }
 
     if (!this.queue.length) {
-      setTimeout(callback, 0);
+      setImmediate(callback);
       return Promise.resolve();
     }
 
@@ -183,10 +183,10 @@ export default class Analytics {
     };
 
     const done = (err) => {
-      setTimeout(() => {
+      setImmediate(() => {
         callbacks.forEach((fn) => fn(err, data));
         callback(err, data);
-      }, 0);
+      });
     };
 
     const headers = {
