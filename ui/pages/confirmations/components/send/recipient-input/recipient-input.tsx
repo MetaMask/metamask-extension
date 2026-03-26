6c258e765ee5b45ac3e5364ac81e0127bc411da4
Jyoti Puri <jyotipuri@gmail.com>
2026-03-26 15:28:15 +0530

    update

--- ui/pages/confirmations/components/send/recipient-input/recipient-input.tsx
+++ ui/pages/confirmations/components/send/recipient-input/recipient-input.tsx
@@ -153,8 +153,8 @@ export const RecipientInput = ({
       ) : (
         <TextField
           error={isHardError}
-// TODO: @MetaMask/design-system-engineers: This seems to be the only current use case for a TextField warning state; align on whether TextField should support warning styling (or if there’s a preferred DS pattern).
-className={isWarning ? 'border-warning-default' : undefined}
+          // TODO: @MetaMask/design-system-engineers: This seems to be the only current use case for a TextField warning state; align on whether TextField should support warning styling (or if there’s a preferred DS pattern).
+          className={isWarning ? 'border-warning-default' : undefined}
           endAccessory={
             recipients.length > 0 ? (
               <ButtonIcon
