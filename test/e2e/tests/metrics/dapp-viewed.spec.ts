6848fd031d5863d4f3c1b7493352b645e92d0cc1
seaona <mariona@gmx.es>
2026-03-26 12:45:38 +0100

    log

--- test/e2e/tests/metrics/dapp-viewed.spec.ts
+++ test/e2e/tests/metrics/dapp-viewed.spec.ts
@@ -108,6 +108,7 @@ describe('Dapp viewed Event', function () {
           publicAddress: DEFAULT_FIXTURE_ACCOUNT,
         });
         const events = await getEventPayloads(driver, mockedEndpoints);
+        console.log(events);
         assert.equal(events.length, 0);
       },
     );
