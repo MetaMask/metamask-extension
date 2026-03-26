94d0a5575c97f3ec35b0d172eafb6622ae87ad42
Frederik Bolding <frederik.bolding@gmail.com>
2026-03-26 15:09:46 +0100

    Add type assertion

--- shared/constants/data-services.ts
+++ shared/constants/data-services.ts
@@ -1,2 +1,2 @@
-// A list of all data services available in the client.
-export const DATA_SERVICES = [];
+// A list of all names of data services available in the client.
+export const DATA_SERVICES: string[] = [];
