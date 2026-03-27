706fa6b21175beb11946f2fd07c17cde103782fb
Charly Chevalier <charly.chevalier@consensys.net>
2026-03-27 11:43:49 +0100

    fix: missing lastSelected (.storybook)

--- .storybook/test-data.js
+++ .storybook/test-data.js
@@ -383,6 +383,7 @@ const state = {
                 },
                 hidden: false,
                 pinned: false,
+                lastSelected: 0,
               },
             },
           },
