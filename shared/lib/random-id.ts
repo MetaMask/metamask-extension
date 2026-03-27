9292f18572937965b717faf810e80ec5a5be2f1f
dddddanica <zhaodanica@gmail.com>
2026-03-27 22:55:35 +0000

    refactor: resolve Initial seed range reduced from 53 bits to 32 bits

--- shared/lib/random-id.ts
+++ shared/lib/random-id.ts
@@ -1,11 +1,12 @@
 const MAX = Number.MAX_SAFE_INTEGER;
 
-const seed = new Uint32Array(1);
+const seed = new BigUint64Array(1);
 crypto.getRandomValues(seed);
-let idCounter = seed[0];
+let idCounter = Number(seed[0] & BigInt(MAX));
 
 export default function createRandomId(): number {
   idCounter %= MAX;
-  // eslint-disable-next-line no-plusplus
-  return idCounter++;
+  const id = idCounter;
+  idCounter += 1;
+  return id;
 }
