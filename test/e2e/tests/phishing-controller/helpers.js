115d5963229b50128ff3ca78cb4b71e7721d39e1
seaona <mariona@gmx.es>
2026-03-30 16:31:46 +0200

    split

--- test/e2e/tests/phishing-controller/helpers.js
+++ test/e2e/tests/phishing-controller/helpers.js
@@ -16,10 +16,15 @@ const BlockProvider = {
   MetaMask: 'metamask',
 };
 
+/** Default C2 domain hash used across phishing E2E fixtures. */
+const DEFAULT_BLOCKED_DOMAIN =
+  'a379a6f6eeafb9a55e378c118034e2751e682fab9f2d30ab13d2125586ce1947';
+
 module.exports = {
   METAMASK_HOTLIST_DIFF_URL,
   METAMASK_STALELIST_URL,
   C2_DOMAIN_BLOCKLIST_URL,
   BlockProvider,
   ListNames,
+  DEFAULT_BLOCKED_DOMAIN,
 };
