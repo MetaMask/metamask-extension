21d5d3701ecc6f9088bff98aae1790bea644af28
dddddanica <zhaodanica@gmail.com>
2026-03-30 14:18:03 +0100

    refactor: fix lint

--- shared/lib/random-id.ts
+++ shared/lib/random-id.ts
@@ -2,7 +2,7 @@ const MAX = Number.MAX_SAFE_INTEGER;
 
 const seed = new BigUint64Array(1);
 crypto.getRandomValues(seed);
-let idCounter = Number(seed[0] & BigInt(MAX));
+let idCounter = Number(seed[0] % (BigInt(MAX) + 1n));
 
 export default function createRandomId(): number {
   idCounter %= MAX;
