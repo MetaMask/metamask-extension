4558e96f9e09606ab20563681b5bcc79ccb30bdc
NidhiKJha <menidhikjha@gmail.com>
2026-03-31 15:29:38 +0100

    updated jest

--- ui/pages/settings-v2/shared/autodetect-nfts-item.tsx
+++ ui/pages/settings-v2/shared/autodetect-nfts-item.tsx
@@ -38,7 +38,7 @@ export const AutodetectNftsToggleItem = () => {
         }
         dispatch(setUseNftDetection(!value));
       }}
-      dataTestId="use-nft-detection"
+      dataTestId="use-nft-detection-input"
       containerDataTestId="use-nft-detection"
     />
   );
