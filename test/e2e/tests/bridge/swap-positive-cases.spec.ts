56d2648545eccd2d269b01d339e937bdac175697
Micaela Estabillo <micaela.estabillo@consensys.net>
2026-03-31 09:42:37 -0700

    fix: swap e2e test

--- test/e2e/tests/bridge/swap-positive-cases.spec.ts
+++ test/e2e/tests/bridge/swap-positive-cases.spec.ts
@@ -6,14 +6,14 @@ import HomePage from '../../page-objects/pages/home/homepage';
 import { BRIDGE_FEATURE_FLAGS_WITH_SSE_ENABLED } from './constants';
 import { bridgeTransaction, getBridgeFixtures } from './bridge-test-utils';
 
-describe.only('Swap tests', function (this: Suite) {
+describe('Swap tests', function (this: Suite) {
   this.timeout(160000); // This test is very long, so we need an unusually high timeout
   it('updates recommended swap quote incrementally when SSE events are received', async function () {
     await withFixtures(
       {
         ...getBridgeFixtures(
           this.test?.fullTitle(),
-          BRIDGE_FEATURE_FLAGS_WITH_SSE_ENABLED,
+          { ...BRIDGE_FEATURE_FLAGS_WITH_SSE_ENABLED, refreshRate: 30000 },
           false,
           true,
         ),
@@ -36,7 +36,7 @@ describe.only('Swap tests', function (this: Suite) {
             tokenFrom: 'ETH',
             tokenTo: 'MUSD',
           },
-          submitDelay: 5000,
+          submitDelay: 10000,
           expectedDestAmount: '3,839',
         });
 
