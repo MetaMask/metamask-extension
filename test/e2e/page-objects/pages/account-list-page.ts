3354612e12f65860f0252f0ff289d32bf30c948b
Ganesh Suresh Patra <ganesh.patra@consensys.net>
2026-04-01 14:57:39 +0530

    fix: update e2e test case

--- test/e2e/page-objects/pages/account-list-page.ts
+++ test/e2e/page-objects/pages/account-list-page.ts
@@ -218,7 +218,7 @@ class AccountListPage {
 
   private readonly importSrpConfirmButton = {
     text: 'Continue',
-    tag: 'button',
+    tag: 'span',
   };
 
   private readonly exportSrpButton = {
