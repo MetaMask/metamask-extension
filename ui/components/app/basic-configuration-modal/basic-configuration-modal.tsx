f70d311d5d4754784029e7947efb5f6f2ab11e94
Francis Nepomuceno <n3ps@users.noreply.github.com>
2026-04-08 14:01:26 -0400

    more

--- ui/components/app/basic-configuration-modal/basic-configuration-modal.tsx
+++ ui/components/app/basic-configuration-modal/basic-configuration-modal.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useMemo, useState } from 'react';
+import React, { useContext, useMemo } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import {
