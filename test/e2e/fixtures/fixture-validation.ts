7bef3cdb597717ea2ff23c264e286046963f76dd
seaona <mariona@gmx.es>
2026-04-09 09:09:14 +0200

    v2

--- test/e2e/fixtures/fixture-validation.ts
+++ test/e2e/fixtures/fixture-validation.ts
@@ -52,7 +52,7 @@ const getFixtureIgnoredKeys = (): string[] => [
   'data.AppMetadataController.firstTimeInfo.date',
   'data.AppMetadataController.firstTimeInfo.version',
   'data.AppStateController.lastUpdatedAt',
-  'data.AppStateController.newPrivacyPolicyToastShownDate',
+  'data.AppStateController.newPrivacyPolicyToastClickedOrClosed',
   'data.AppStateController.onboardingDate',
   'data.AppStateController.recoveryPhraseReminderLastShown',
   'data.AppStateController.termsOfUseLastAgreed',
