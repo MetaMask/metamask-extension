03aa71a439cdee8c70aadd9afca865131844504e
Guillaume Roux <guillaumeroux123@gmail.com>
2026-04-09 16:20:03 +0200

    fix import

--- app/scripts/messenger-client-init/seedless-onboarding/oauth-service-init.test.ts
+++ app/scripts/messenger-client-init/seedless-onboarding/oauth-service-init.test.ts
@@ -3,8 +3,8 @@ import { getOAuthServiceMessenger } from '../messengers/seedless-onboarding';
 import { getRootMessenger } from '../../lib/messenger';
 import { buildControllerInitRequestMock } from '../test/utils';
 import { OAuthService } from '../../services/oauth/oauth-service';
-import { OAuthServiceInit } from './oauth-service-init';
 import { OAuthServiceMessenger } from '../../services/oauth/types';
+import { OAuthServiceInit } from './oauth-service-init';
 
 function buildInitRequestMock(): jest.Mocked<
   ControllerInitRequest<OAuthServiceMessenger>
