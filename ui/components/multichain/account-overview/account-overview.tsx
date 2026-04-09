6be0e303f69fa63bfe28b720e6f7791f671475b1
gantunesr <17601467+gantunesr@users.noreply.github.com>
2026-04-08 22:51:57 -0400

    chore(multichain): simplify account overview to ETH-only

--- ui/components/multichain/account-overview/account-overview.tsx
+++ ui/components/multichain/account-overview/account-overview.tsx
@@ -1,16 +1,6 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import {
-  BtcAccountType,
-  EthAccountType,
-  SolAccountType,
-  TrxAccountType,
-} from '@metamask/keyring-api';
-import { getSelectedInternalAccount } from '../../../selectors';
 import { AccountOverviewEth } from './account-overview-eth';
-import { AccountOverviewUnknown } from './account-overview-unknown';
 import { AccountOverviewCommonProps } from './common';
-import { AccountOverviewNonEvm } from './account-overview-non-evm';
 
 export type AccountOverviewProps = AccountOverviewCommonProps & {
   useExternalServices: boolean;
@@ -19,24 +9,9 @@ export type AccountOverviewProps = AccountOverviewCommonProps & {
 // TODO: Fix in https://github.com/MetaMask/metamask-extension/issues/31860
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export function AccountOverview(props: AccountOverviewProps) {
-  const account = useSelector(getSelectedInternalAccount);
-
-  const renderAccountOverviewOption = () => {
-    switch (account.type) {
-      case EthAccountType.Eoa:
-      case EthAccountType.Erc4337:
-        return <AccountOverviewEth {...props}></AccountOverviewEth>;
-      case BtcAccountType.P2pkh:
-      case BtcAccountType.P2sh:
-      case BtcAccountType.P2wpkh:
-      case BtcAccountType.P2tr:
-      case SolAccountType.DataAccount:
-      case TrxAccountType.Eoa:
-        return <AccountOverviewNonEvm {...props}></AccountOverviewNonEvm>;
-      default:
-        return <AccountOverviewUnknown {...props}></AccountOverviewUnknown>;
-    }
-  };
+  const renderAccountOverviewOption = () => (
+    <AccountOverviewEth {...props}></AccountOverviewEth>
+  );
 
   return <>{renderAccountOverviewOption()}</>;
 }
