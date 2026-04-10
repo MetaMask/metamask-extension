8a0e20964ec8790d713a44f299713a9ed28c1a16
seaona <mariona@gmx.es>
2026-04-10 15:40:15 +0200

    dis

--- test/e2e/page-objects/flows/bridge.flow.ts
+++ test/e2e/page-objects/flows/bridge.flow.ts
@@ -16,6 +16,7 @@ import HomePage from '../pages/home/homepage';
  * @param testParams.expectedDestAmount - The expected quoted destination amounts in the quote page
  * @param testParams.submitDelay - The delay to wait before submitting the transaction, must be less than the refresh interval of the stream
  * @param testParams.expectedStatus - The expected state of the transaction
+ * @param testParams.dismissStatusPage - Whether to dismiss the status page after submitting
  */
 export const bridgeTransaction = async ({
   driver,
@@ -26,6 +27,7 @@ export const bridgeTransaction = async ({
   expectedSwapTokens,
   expectedDestAmount,
   submitDelay,
+  dismissStatusPage = false,
 }: {
   driver: Driver;
   quote: BridgeQuote;
@@ -35,6 +37,7 @@ export const bridgeTransaction = async ({
   expectedSwapTokens?: Pick<BridgeQuote, 'tokenFrom' | 'tokenTo'>;
   expectedDestAmount: string;
   submitDelay?: number;
+  dismissStatusPage?: boolean;
 }) => {
   const homePage = new HomePage(driver);
   await homePage.checkPageIsLoaded();
@@ -50,7 +53,7 @@ export const bridgeTransaction = async ({
   if (expectedDestAmount) {
     await bridgePage.checkDestAmount(expectedDestAmount);
   }
-  await bridgePage.submitQuote();
+  await bridgePage.submitQuote({ dismissStatusPage });
 
   await homePage.goToActivityList();
 
