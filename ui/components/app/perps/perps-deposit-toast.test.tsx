786ed9e93cc91bc21a6888196d9d99ec5fb22c77
geositta <matthew.denton@consensys.net>
2026-04-14 17:30:44 -0500

    chore: add locale

--- ui/components/app/perps/perps-deposit-toast.test.tsx
+++ ui/components/app/perps/perps-deposit-toast.test.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { act, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import configureStore from '../../../store/store';
