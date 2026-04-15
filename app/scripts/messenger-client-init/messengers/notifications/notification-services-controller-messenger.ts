255c2202a55d8396ba25d69ccaaaea7b81191be4
manlio <manlio.poltronieri@gmail.com>
2026-04-15 14:43:10 +0200

    Update controller

--- app/scripts/messenger-client-init/messengers/notifications/notification-services-controller-messenger.ts
+++ app/scripts/messenger-client-init/messengers/notifications/notification-services-controller-messenger.ts
@@ -32,6 +32,7 @@ export function getNotificationServicesControllerMessenger(
       'AuthenticationController:isSignedIn',
       'AuthenticationController:performSignIn',
       // Push Actions
+      'NotificationServicesPushController:addPushNotificationLinks',
       'NotificationServicesPushController:enablePushNotifications',
       'NotificationServicesPushController:disablePushNotifications',
       'NotificationServicesPushController:deletePushNotificationLinks',
