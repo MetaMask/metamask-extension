fe6aa2c0b1fb9c8fd84db87c643ca7bfa26e719f
Maxime OUAIRY <maxime.ouairy-ext@consensys.net>
2026-04-15 12:59:50 +0200

    fix: tempo useHasInsufficientBalance return false non-7702 PR comments

--- ui/pages/confirmations/hooks/useHasInsufficientBalance.ts
+++ ui/pages/confirmations/hooks/useHasInsufficientBalance.ts
@@ -44,35 +44,20 @@ export function useHasInsufficientBalance(): {
   );
 
   const { nativeCurrencySymbol } = useNativeCurrencySymbol(chainId);
-
-  let hasInsufficientBalance;
+  const hasNoNativeAsset = NO_NATIVE_ASSET_CHAIN_IDS.has(chainId);
   /**
-   * Specific cases for chains with no native asset like Tempo.
-   * - On normal Tempo 7702 flows, there is never enough native balance because there is no native.
-   * - However, in some conditions we want to force-disable 7702 (like for with HW), causing Tempo to fall
-   * into a degraded "legacy" flow. In this flow (detected when `excludeNativeTokenForFee` is false on a Tempo chain)
-   * the old version of MetaMask used to read the RPC `getbalance` with a big number 424242424242...
-   * This made MetaMask assume that the user had enough native token at all times, allowing the user to
-   * be able to try to interact with the chain. The newest version of MetaMask sets "0" for Tempo native balance
-   * which means that this "legacy flow" won't work anymore because MetaMask "sees" that there is not enough native token.
-   * The line below restores the old "has enough balance no matter what" behavior that we had previously,
-   * in the specific cases where 7702 is disabled for Tempo.
-   * This should be a temporary solution until gasless/7702 is supported by hardware wallets.
+   * Tempo (7702) special case: Force "enough native balance" in legacy flow
+   * (when `excludeNativeTokenForFee` is false) to restore old MetaMask behavior.
+   * New MM reports "0" balance, breaking legacy flow. Temporary fix until HW
+   * supports gasless/7702.
    */
-  if (NO_NATIVE_ASSET_CHAIN_IDS.has(chainId)) {
-    /**
-     * In Tempo:
-     * - When `excludeNativeTokenForFee` is `true`, we are in the 7702 flow (force no native token).
-     * - When `excludeNativeTokenForFee` is unset, we are in a non-7702 fallback (assume unlimited native token).
-     */
-    hasInsufficientBalance = Boolean(excludeNativeTokenForFee);
-  } else {
-    hasInsufficientBalance = !isBalanceSufficient({
-      amount: totalValue,
-      gasTotal: maxFeeHex,
-      balance,
-    });
-  }
+  const hasInsufficientBalance = hasNoNativeAsset
+    ? Boolean(excludeNativeTokenForFee)
+    : !isBalanceSufficient({
+        amount: totalValue,
+        gasTotal: maxFeeHex,
+        balance,
+      });
 
   return {
     hasInsufficientBalance,
