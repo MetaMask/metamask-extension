7298c20d5ce79d675a916dea2ea3e2228f4a094c
LeVinhGithub <levinh.hcmut@gmail.com>
2026-04-15 22:10:01 +0700

    test: init

--- test/e2e/tests/multichain-accounts/add-wallet.spec.ts
+++ test/e2e/tests/multichain-accounts/add-wallet.spec.ts
@@ -16,7 +16,6 @@ import {
 } from '../identity/account-syncing/mock-data';
 import { mockPriceApi } from '../tokens/utils/mocks';
 import { mockIdentityServices } from '../identity/mocks';
-import { withMultichainAccountsDesignEnabled } from './common';
 
 const DEFAULT_LOCAL_NODE_USD_BALANCE = '85,025.00';
 
@@ -88,16 +87,23 @@ describe('Add wallet', function () {
   it('Add wallet using SRP', async function () {
     const E2E_SRP =
       'bench top weekend buyer spoon side resist become detect gauge eye feed';
-    await withMultichainAccountsDesignEnabled(
+    await withFixtures(
       {
+        fixtures: new FixtureBuilderV2()
+          .withShowNativeTokenAsMainBalanceDisabled()
+          .withKeyringControllerMultiSRP()
+          .withEnabledNetworks({ eip155: { '0x1': true } })
+          .build(),
         title: this.test?.fullTitle(),
-        testSpecificMock: mockPriceApi,
       },
-      async (driver: Driver) => {
+      async ({ driver }: { driver: Driver }) => {
+        await login(driver, { validateBalance: false });
+        const headerNavbar = new HeaderNavbar(driver);
+        await headerNavbar.openAccountMenu();
+
         const accountListPage = new AccountListPage(driver);
         await accountListPage.checkPageIsLoaded();
         await accountListPage.startImportSecretPhrase(E2E_SRP);
-        const headerNavbar = new HeaderNavbar(driver);
         await headerNavbar.openAccountMenu();
         await accountListPage.checkPageIsLoaded();
         await accountListPage.checkNumberOfAvailableAccounts(3);
