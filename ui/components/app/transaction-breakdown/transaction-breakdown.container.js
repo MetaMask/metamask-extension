d2a342955cfab4d1c28f76679af32c4c9f2a2cbe
Florin Dzeladini <florin.dzeladini@consensys.net>
2026-04-16 17:54:24 +0200

    chore: lint

--- ui/components/app/transaction-breakdown/transaction-breakdown.container.js
+++ ui/components/app/transaction-breakdown/transaction-breakdown.container.js
@@ -1,14 +1,15 @@
 import { connect } from 'react-redux';
+import { isHardwareWallet } from '../../../selectors';
 import TransactionBreakdown from './transaction-breakdown.component';
 import { getTransactionBreakdownData } from './transaction-breakdown-utils';
 
 const mapStateToProps = (state, ownProps) => {
-  const { transaction, isTokenApprove, isHardwareWalletAccount } = ownProps;
+  const { transaction, isTokenApprove } = ownProps;
   const data = getTransactionBreakdownData({
     state,
     transaction,
     isTokenApprove,
-    isHardwareWalletAccount,
+    isHardwareWalletAccount: isHardwareWallet(state),
   });
   return data;
 };
