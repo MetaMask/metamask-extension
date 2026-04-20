4fe307f4292e746fc80d25fd367fef5df22eedbb
ameliejyc <amelie.chan@gmail.com>
2026-04-20 16:49:56 +0100

    chore: move experimental tab

--- ui/pages/settings-v2/settings-registry.ts
+++ ui/pages/settings-v2/settings-registry.ts
@@ -274,9 +274,7 @@ export const SETTINGS_V2_ROUTES: Record<string, SettingsV2RouteMeta> = {
   [EXPERIMENTAL_ROUTE]: {
     labelKey: 'experimental',
     parentPath: SETTINGS_V2_ROUTE,
-    component: mmLazy(
-      () => import('../settings/experimental-tab/experimental-tab.tsx'),
-    ),
+    component: mmLazy(() => import('./experimental-tab/experimental-tab.tsx')),
     isTab: true,
     iconName: IconName.Flask,
   },
