4f947c957c868de03ec35a587cb3f7c2aae9edf2
ameliejyc <amelie.chan@gmail.com>
2026-05-06 15:51:32 +0100

    chore: move transaction shield out of settings

--- ui/pages/settings-v2/settings-registry.ts
+++ ui/pages/settings-v2/settings-registry.ts
@@ -33,7 +33,7 @@ import {
   THIRD_PARTY_APIS_ROUTE,
 } from '../../helpers/constants/routes';
 import { mmLazy } from '../../helpers/utils/mm-lazy';
-import { CLAIMS_TAB_KEYS } from '../settings/transaction-shield-tab/types';
+import { CLAIMS_TAB_KEYS } from '../shield/transaction-shield/types';
 
 /**
  * Route definition for a Settings V2 page.
@@ -219,7 +219,7 @@ export const SETTINGS_V2_ROUTES: Record<string, SettingsV2RouteMeta> = {
     labelKey: 'shieldTx',
     parentPath: SETTINGS_V2_ROUTE,
     component: mmLazy(
-      () => import('../settings/transaction-shield-tab/index.ts'),
+      () => import('../shield/transaction-shield/index.ts'),
     ),
     isTab: true,
     iconName: IconName.ShieldLock,
@@ -246,7 +246,7 @@ export const SETTINGS_V2_ROUTES: Record<string, SettingsV2RouteMeta> = {
     labelKey: 'shieldClaimsListTitle',
     parentPath: TRANSACTION_SHIELD_ROUTE,
     component: mmLazy(
-      () => import('../settings/transaction-shield-tab/claims-area/index.ts'),
+      () => import('../shield/transaction-shield/claims-area/index.ts'),
     ),
   },
   [TRANSACTION_SHIELD_CLAIM_ROUTES.NEW.FULL]: {
