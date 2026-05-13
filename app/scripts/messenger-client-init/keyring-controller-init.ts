f753b81105945766d9381db010b7260773720335
Hassan Malik <hbmalik88@gmail.com>
2026-05-13 17:32:59 -0400

    feat: add hw keyring v2 builders

--- app/scripts/messenger-client-init/keyring-controller-init.ts
+++ app/scripts/messenger-client-init/keyring-controller-init.ts
@@ -4,6 +4,7 @@ import {
   type KeyringV2Builder,
 } from '@metamask/keyring-controller';
 import { QrKeyring, QrKeyringScannerBridge } from '@metamask/eth-qr-keyring';
+import { QrKeyring as QrKeyringV2 } from '@metamask/eth-qr-keyring/v2';
 import { KeyringClass } from '@metamask/keyring-utils';
 import LatticeKeyring from 'eth-lattice-keyring';
 import {
@@ -12,9 +13,14 @@ import {
   TrezorKeyring,
 } from '@metamask/eth-trezor-keyring';
 import {
+  OneKeyKeyring as OneKeyKeyringV2,
+  TrezorKeyring as TrezorKeyringV2,
+} from '@metamask/eth-trezor-keyring/v2';
+import {
   LedgerIframeBridge,
   LedgerKeyring,
 } from '@metamask/eth-ledger-bridge-keyring';
+import { LedgerKeyring as LedgerKeyringV2 } from '@metamask/eth-ledger-bridge-keyring/v2';
 import { hardwareKeyringBuilderFactory } from '../lib/hardware-keyring-builder-factory';
 import { isManifestV3 } from '../../../shared/lib/mv3.utils';
 import { qrKeyringBuilderFactory } from '../lib/qr-keyring-builder-factory';
@@ -120,11 +126,61 @@ export const KeyringControllerInit: MessengerClientInitFunction<
     { type: LatticeKeyring.type },
   );
 
+  const ledgerKeyringV2Builder: KeyringV2Builder = Object.assign(
+    (keyring: unknown, metadata: { id: string }) =>
+      new LedgerKeyringV2({
+        legacyKeyring: keyring as ConstructorParameters<
+          typeof LedgerKeyringV2
+        >[0]['legacyKeyring'],
+        entropySource: metadata.id,
+      }),
+    { type: LedgerKeyring.type },
+  );
+
+  const qrKeyringV2Builder: KeyringV2Builder = Object.assign(
+    (keyring: unknown, metadata: { id: string }) =>
+      new QrKeyringV2({
+        legacyKeyring: keyring as ConstructorParameters<
+          typeof QrKeyringV2
+        >[0]['legacyKeyring'],
+        entropySource: metadata.id,
+      }),
+    { type: QrKeyring.type },
+  );
+
+  const trezorKeyringV2Builder: KeyringV2Builder = Object.assign(
+    (keyring: unknown, metadata: { id: string }) =>
+      new TrezorKeyringV2({
+        legacyKeyring: keyring as ConstructorParameters<
+          typeof TrezorKeyringV2
+        >[0]['legacyKeyring'],
+        entropySource: metadata.id,
+      }),
+    { type: TrezorKeyring.type },
+  );
+
+  const onekeyKeyringV2Builder: KeyringV2Builder = Object.assign(
+    (keyring: unknown, metadata: { id: string }) =>
+      new OneKeyKeyringV2({
+        legacyKeyring: keyring as ConstructorParameters<
+          typeof OneKeyKeyringV2
+        >[0]['legacyKeyring'],
+        entropySource: metadata.id,
+      }),
+    { type: OneKeyKeyring.type },
+  );
+
   const messengerClient = new KeyringController({
     state: persistedState.KeyringController,
     messenger: controllerMessenger,
     keyringBuilders: additionalKeyrings,
-    keyringV2Builders: [latticeKeyringV2Builder],
+    keyringV2Builders: [
+      latticeKeyringV2Builder,
+      ledgerKeyringV2Builder,
+      qrKeyringV2Builder,
+      trezorKeyringV2Builder,
+      onekeyKeyringV2Builder,
+    ],
     encryptor: encryptor || encryptorFactory(600_000),
   });
 
