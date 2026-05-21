7bdf4a9554547ccac1ec4d3b6ec1edadcd40ac83
Matthew Walsh <matthew.walsh@consensys.net>
2026-05-21 12:08:39 +0100

    feat(confirmations): remove redundant Send header from universal confirmation

--- ui/pages/confirmations/components/confirm/info/universal-transaction/rows/universal-transaction-heading-row.tsx
+++ ui/pages/confirmations/components/confirm/info/universal-transaction/rows/universal-transaction-heading-row.tsx
@@ -9,11 +9,9 @@ import {
   Text,
 } from '../../../../../../../components/component-library';
 import {
-  TextAlign,
   TextColor,
   TextVariant,
 } from '../../../../../../../helpers/constants/design-system';
-import { useI18nContext } from '../../../../../../../hooks/useI18nContext';
 import { useUniversalTransactionDataOptional } from '../../../../../hooks/transactions/useUniversalTransactionData';
 import SendHeadingLayout from '../../shared/send-heading-layout/send-heading-layout';
 
@@ -22,7 +20,6 @@ const ASSET_IMAGE_BY_SYMBOL: Record<string, string> = {
 };
 
 export function UniversalTransactionHeadingRow() {
-  const t = useI18nContext();
   const data = useUniversalTransactionDataOptional();
 
   if (!data) {
@@ -32,30 +29,20 @@ export function UniversalTransactionHeadingRow() {
   const assetImageUrl = ASSET_IMAGE_BY_SYMBOL[data.assetSymbol];
 
   return (
-    <>
-      <Text
-        variant={TextVariant.headingLg}
-        paddingTop={4}
-        paddingBottom={2}
-        textAlign={TextAlign.Center}
-      >
-        {t('send')}
-      </Text>
-      <SendHeadingLayout
-        image={
-          <AvatarToken
-            name={data.assetSymbol}
-            src={assetImageUrl}
-            size={AvatarTokenSize.Xl}
-          />
-        }
-      >
-        <Box paddingBottom={1}>
-          <Text variant={TextVariant.headingLg} color={TextColor.inherit}>
-            {`${data.formattedAmount} ${data.assetSymbol}`}
-          </Text>
-        </Box>
-      </SendHeadingLayout>
-    </>
+    <SendHeadingLayout
+      image={
+        <AvatarToken
+          name={data.assetSymbol}
+          src={assetImageUrl}
+          size={AvatarTokenSize.Xl}
+        />
+      }
+    >
+      <Box paddingBottom={1}>
+        <Text variant={TextVariant.headingLg} color={TextColor.inherit}>
+          {`${data.formattedAmount} ${data.assetSymbol}`}
+        </Text>
+      </Box>
+    </SendHeadingLayout>
   );
 }
