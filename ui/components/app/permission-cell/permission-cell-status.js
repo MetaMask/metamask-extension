2b71e588e5ef5853d1df0d040baf2aa195039260
kirillzyusko <zyusko.kirik@gmail.com>
2026-05-21 13:51:10 +0200

    fix: eslint

--- ui/components/app/permission-cell/permission-cell-status.js
+++ ui/components/app/permission-cell/permission-cell-status.js
@@ -4,7 +4,6 @@ import { useSelector } from 'react-redux';
 import {
   AvatarAccountSize,
   Box,
-  BoxFlexDirection,
   BoxAlignItems,
   BoxJustifyContent,
 } from '@metamask/design-system-react';
