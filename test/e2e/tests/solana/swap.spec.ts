553fe355cd6ef2665e97bddff4839f6cafec8221
seaona <mariona@gmx.es>
2026-05-25 08:53:11 +0200

    rm unused

--- test/e2e/tests/solana/swap.spec.ts
+++ test/e2e/tests/solana/swap.spec.ts
@@ -380,7 +380,6 @@ async function mockSwapSOLtoUSDC(
     : mockSwapSOLtoUSDCLegacy(mockServer);
 }
 
-const SOL_ACCOUNT_ID = '688e01b8-3134-4ef4-80e6-8772bab38ef7';
 const SOL_CAIP_ASSET = 'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp/slip44:501';
 const USDC_CAIP_ASSET =
   'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp/token:EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v';
@@ -390,16 +389,6 @@ const USDC_PRICE = 0.999761;
 
 /** Matches default FixtureBuilderV2 account IDs for Solana swap E2E. */
 const SOLANA_SWAP_ASSETS_CONTROLLER_FIXTURE = {
-  assetsBalance: {
-    [SOL_ACCOUNT_ID]: {
-      [SOL_CAIP_ASSET]: {
-        amount: '50',
-      },
-      [USDC_CAIP_ASSET]: {
-        amount: '8.908267',
-      },
-    },
-  },
   assetsInfo: {
     [SOL_CAIP_ASSET]: {
       decimals: 9,
@@ -590,9 +579,6 @@ describe('Swap on Solana', function () {
                   },
                 },
                 MultichainAssetsController: {
-                  accountsAssets: {
-                    [SOL_ACCOUNT_ID]: [SOL_CAIP_ASSET, USDC_CAIP_ASSET],
-                  },
                   assetsMetadata: {
                     [USDC_CAIP_ASSET]: {
                       fungible: true,
