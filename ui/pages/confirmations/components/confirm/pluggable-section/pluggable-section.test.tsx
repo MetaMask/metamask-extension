9b1282b359ac0bc0c864d6db0ea45a022aa954bb
Goktug Poyraz <omergoktugpoyraz@gmail.com>
2026-05-26 10:04:17 +0200

    feat(confirmations): hide pluggable section for Pay transactions

--- ui/pages/confirmations/components/confirm/pluggable-section/pluggable-section.test.tsx
+++ ui/pages/confirmations/components/confirm/pluggable-section/pluggable-section.test.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { TransactionType } from '@metamask/transaction-controller';
 
 import { renderWithConfirmContextProvider } from '../../../../../../test/lib/confirmations/render-helpers';
-import { getMockPersonalSignConfirmState } from '../../../../../../test/data/confirmations/helper';
+import {
+  getMockConfirmStateForTransaction,
+  getMockPersonalSignConfirmState,
+} from '../../../../../../test/data/confirmations/helper';
+import { genUnapprovedContractInteractionConfirmation } from '../../../../../../test/data/confirmations/contract-interaction';
 import configureStore from '../../../../../store/store';
+import { PAY_TRANSACTION_TYPES } from '../../../constants/pay';
 import PluggableSection from './pluggable-section';
 
 const render = () => {
@@ -10,8 +16,26 @@ const render = () => {
   return renderWithConfirmContextProvider(<PluggableSection />, store);
 };
 
+const renderWithTransaction = (type: TransactionType) => {
+  const confirmation = {
+    ...genUnapprovedContractInteractionConfirmation({ chainId: '0x1' }),
+    type,
+  };
+  const state = getMockConfirmStateForTransaction(confirmation);
+  const store = configureStore(state);
+  return renderWithConfirmContextProvider(<PluggableSection />, store);
+};
+
 describe('PluggableSection', () => {
-  it('should render correctly', () => {
+  it('renders without throwing', () => {
     expect(() => render()).not.toThrow();
   });
+
+  it.each(PAY_TRANSACTION_TYPES)(
+    'returns null for pay transaction type: %s',
+    (transactionType) => {
+      const { container } = renderWithTransaction(transactionType);
+      expect(container).toBeEmptyDOMElement();
+    },
+  );
 });
