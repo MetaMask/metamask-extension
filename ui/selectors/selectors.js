ac383b5199ad07f92b3cfce0706002dac4ee9c64
Tai Nguyen TT <tai.nguyen@consensys.net>
2026-05-28 11:13:46 +0700

    feat(passkey): disable passkey unlock in mobile browsers

--- ui/selectors/selectors.js
+++ ui/selectors/selectors.js
@@ -74,7 +74,7 @@ import {
 
 // TODO: Remove restricted import
 // eslint-disable-next-line import-x/no-restricted-paths
-import { addHexPrefix, getEnvironmentType } from '../../app/scripts/lib/util';
+import { addHexPrefix, getDeviceType, getEnvironmentType } from '../../app/scripts/lib/util';
 import {
   TEST_CHAINS,
   MAINNET_DISPLAY_NAME,
@@ -138,6 +138,7 @@ import { STATIC_MAINNET_TOKEN_LIST } from '../../shared/constants/tokens';
 import { DAY } from '../../shared/constants/time';
 import { TERMS_OF_USE_LAST_UPDATED } from '../../shared/constants/terms';
 import {
+  DEVICE_TYPE,
   ENVIRONMENT_TYPE_SIDEPANEL,
   ENVIRONMENT_TYPE_POPUP,
 } from '../../shared/constants/app';
@@ -2906,7 +2907,8 @@ export function getIsPasskeyFeatureAvailable(state) {
     getIsPasskeyFeatureEnabled() &&
     isWebAuthnSupported() &&
     !getIsSocialLoginFlow(state) &&
-    !isFirefoxBrowser()
+    !isFirefoxBrowser() &&
+    getDeviceType() !== DEVICE_TYPE.MOBILE
   );
 }
 
