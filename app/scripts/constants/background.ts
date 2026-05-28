ccf0af6ada3b1e48c92d2cc463fc0722d69e45a0
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2026-05-28 12:01:35 +0200

    Prevent Snaps origins from connecting

---
+++ app/scripts/constants/background.ts
@@ -0,0 +1,7 @@
+export const BLOCKED_PORTS = ['trezor-connect'];
+
+// See: https://consensyssoftware.atlassian.net/browse/WPC-1012
+export const BLOCKED_ORIGINS = [
+  'execution.metamask.io',
+  'execution.consensys.io',
+];
