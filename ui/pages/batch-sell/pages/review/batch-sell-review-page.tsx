90086c03f641a6c525e145f51c3d1d6a8757e327
GeorgeGkas <georgegkas@gmail.com>
2026-06-02 11:53:08 +0300

    chore: add todo

--- ui/pages/batch-sell/pages/review/batch-sell-review-page.tsx
+++ ui/pages/batch-sell/pages/review/batch-sell-review-page.tsx
@@ -24,6 +24,8 @@ import { hasAnyEnabledAsset } from './utils/hasAnyEnabledAsset';
 
 // TODO: fix zero value usdc
 // TODO: fix execute batch sell on evm due to network fees
+// TODO: exclude RWA from asset list
+// TODO: move batch sell config to root level LF flag using RemoteFeatureFlagController
 
 export const BatchSellReviewPage = () => {
   const [selectReceivedAssetModalIsOpen, setSelectReceivedAssetModalIsOpen] =
