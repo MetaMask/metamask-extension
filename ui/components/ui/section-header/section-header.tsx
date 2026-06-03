00aa04a91cb8074107bc04f1ce4b79e2856005c0
ameliejyc <amelie.chan@gmail.com>
2026-06-03 21:09:34 +0100

    fix: change react.element

--- ui/components/ui/section-header/section-header.tsx
+++ ui/components/ui/section-header/section-header.tsx
@@ -9,7 +9,7 @@ export type SectionHeaderProps = {
 export const SectionHeader = ({
   label,
   className,
-}: SectionHeaderProps): React.JSX.Element => (
+}: SectionHeaderProps): JSX.Element => (
   <Box className={twMerge('px-4 py-2 bg-background-default', className)}>
     <Text className="text-sm text-alternative">{label}</Text>
   </Box>
