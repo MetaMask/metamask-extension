4d986b0c958ced7a8571a2174db9c854b10bd9ef
dddddanica <zhaodanica@gmail.com>
2026-06-03 10:49:01 +0100

    chore: fix lavamoat and failed unit tests

--- ui/components/app/snaps/snap-ui-renderer/components/date-time-picker.test.ts
+++ ui/components/app/snaps/snap-ui-renderer/components/date-time-picker.test.ts
@@ -1,7 +1,18 @@
 import { Box, DateTimePicker, Field } from '@metamask/snaps-sdk/jsx';
 import { renderInterface } from '../test-utils';
 
+const MOCK_SYSTEM_TIME = new Date('2026-06-02T21:43:00.000Z');
+
 describe('SnapUIDateTimePicker', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+    jest.setSystemTime(MOCK_SYSTEM_TIME);
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
   it('renders a date time picker', () => {
     const { container } = renderInterface(
       Box({
