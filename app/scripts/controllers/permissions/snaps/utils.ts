94eab472886fa5bf0ba1ae297fc448806b1ae3df
Charly Chevalier <charly.chevalier@consensys.net>
2026-06-04 14:37:35 +0200

    chore: lint

--- app/scripts/controllers/permissions/snaps/utils.ts
+++ app/scripts/controllers/permissions/snaps/utils.ts
@@ -1,6 +1,4 @@
-import {
-  KeyringControllerWithKeyringV2UnsafeAction,
-} from '@metamask/keyring-controller';
+import { KeyringControllerWithKeyringV2UnsafeAction } from '@metamask/keyring-controller';
 import type { HdKeyring } from '@metamask/eth-hd-keyring/v2';
 import { KeyringType } from '@metamask/keyring-api/v2';
 import { RootMessenger } from '../../../lib/messenger';
