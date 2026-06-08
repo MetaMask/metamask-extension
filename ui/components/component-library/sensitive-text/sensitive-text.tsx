825e32675a5480996807c23266435cf6134362ae
georgewrmarshall <george.marshall@consensys.net>
2026-06-08 13:21:19 -0700

    chore: upgrade MetaMask design system packages

--- ui/components/component-library/sensitive-text/sensitive-text.tsx
+++ ui/components/component-library/sensitive-text/sensitive-text.tsx
@@ -5,6 +5,13 @@ import {
   SensitiveTextLength,
 } from './sensitive-text.types';
 
+/**
+ * @deprecated This component is deprecated and will be removed in a future release.
+ * Please use the SensitiveText component from @metamask/design-system-react instead.
+ * @see {@link https://github.com/MetaMask/metamask-design-system/blob/main/packages/design-system-react/MIGRATION.md#sensitivetext-component | Migration Guide}
+ * @see {@link https://metamask.github.io/metamask-design-system/?path=/docs/react-components-sensitivetext--docs | Storybook Documentation}
+ * @see {@link https://github.com/MetaMask/metamask-design-system/tree/main/packages/design-system-react/src/components/SensitiveText | Component Source}
+ */
 export const SensitiveText = React.forwardRef<
   HTMLParagraphElement,
   SensitiveTextProps
