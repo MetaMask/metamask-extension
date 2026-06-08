911eab7c5410f78660c2899828362b30cc2cd7b2
Monte Lai <monte.lai@consensys.net>
2026-06-08 17:28:45 +0800

    fix: export

--- ui/hooks/hardware-wallets/index.ts
+++ ui/hooks/hardware-wallets/index.ts
@@ -1,8 +1,13 @@
+export { useHwSignTracker } from './useHwSignTracker';
 export { useHwSwapConfirmationMonitoring } from './useHwSwapConfirmationMonitoring';
 export { useHwSwapConnectionMonitoring } from './useHwSwapConnectionMonitoring';
 export { useHwSwapNavigation } from './useHwSwapNavigation';
 export { useHwSwapQuoteData } from './useHwSwapQuoteData';
 export { useHwSwapSubmission } from './useHwSwapSubmission';
+export type {
+  HwSignTrackerAction,
+  UseHwSignTrackerOptions,
+} from './useHwSignTracker';
 export type {
   LockedQuote,
   HardwareWalletSignatureAction,
