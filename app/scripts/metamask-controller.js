631dde81757883efe5f2321c0fa53c1493f3fda9
Matthew Walsh <matthew.walsh@consensys.net>
2026-06-09 11:43:58 +0100

    bump: transaction-controller preview build (core#9012)
    - Add previewBuilds entry for @metamask/transaction-controller@67.0.0-preview-a3b313179
    - Remove incomingTransactions constructor option (hardwired disabled, no functional change)
    - Remove startIncomingTransactionPolling/stopIncomingTransactionPolling from init, controller API, and background methods
    - Remove TransactionController:incomingTransactionsReceived from TokenBalancesController and AssetsController messenger delegates
    - Remove AccountActivityService:statusChanged, AccountsController:selectedAccountChange, BackendWebSocketService:connectionStateChanged from TC messenger AllowedEvents

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -1722,7 +1722,6 @@ export default class MetamaskController extends EventEmitter {
   }
 
   stopNetworkRequests() {
-    this.txController.stopIncomingTransactionPolling();
     this.tokenDetectionController.disable();
     if (
       !this.controllerMessenger.call(
