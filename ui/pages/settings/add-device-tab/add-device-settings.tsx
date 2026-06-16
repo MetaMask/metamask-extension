838c2c0383e501cddb762574333cb1a9cc7fecb1
Lionell Briones <llenoil@gmail.com>
2026-06-16 18:25:26 +0800

    change entry point of add-device

--- ui/pages/settings/add-device-tab/add-device-settings.tsx
+++ ui/pages/settings/add-device-tab/add-device-settings.tsx
@@ -25,7 +25,7 @@ import { AddDeviceSettingsStep } from './constant';
 const AddDeviceSettings = () => {
   const navigate = useNavigate();
   const t = useI18nContext();
-  const [step, setStep] = useState(AddDeviceSettingsStep.Success);
+  const [step, setStep] = useState(AddDeviceSettingsStep.ScanQrCode);
 
   const handleNextStep = (type: AddDeviceSettingsStep) => {
     setStep(type);
