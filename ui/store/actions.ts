c14b4daa41ff066d4ea457e19cc033b5252aeb03
NidhiKJha <menidhikjha@gmail.com>
2026-06-19 10:36:21 +0100

    lint fix

--- ui/store/actions.ts
+++ ui/store/actions.ts
@@ -2271,7 +2271,7 @@ export function deleteExpiredNotifications(): ThunkAction<
 
         return Boolean(
           notification.readDate &&
-            new Date(notification.readDate) < expirationTime,
+          new Date(notification.readDate) < expirationTime,
         );
       })
       .map(({ id }) => id);
