5f49458daac7acf028e1af9f302c628af6c1c538
Charly Chevalier <charly.chevalier@consensys.net>
2026-06-19 12:43:28 +0200

    fix: remove builders from controller-list (fix type errors with .name)

--- app/scripts/messenger-client-init/controller-list.ts
+++ app/scripts/messenger-client-init/controller-list.ts
@@ -110,8 +110,6 @@ import { MetaMetricsController } from '../controllers/metametrics-controller';
 import { OAuthService } from '../services/oauth/oauth-service';
 import { SnapsNameProvider } from '../lib/SnapsNameProvider';
 import { AppStateController } from '../controllers/app-state-controller';
-import { SnapKeyringBuilder } from '../lib/snap-keyring/snap-keyring';
-import { SnapKeyringV2Builder } from '../lib/snap-keyring/snap-keyring-v2';
 import { SubscriptionService } from '../services/subscription/subscription-service';
 import { AccountOrderController } from '../controllers/account-order';
 import { AlertController } from '../controllers/alert-controller';
@@ -201,8 +199,6 @@ export type MessengerClient =
   | SnapController
   | SnapInterfaceController
   | SnapInsightsController
-  | SnapKeyringBuilder
-  | SnapKeyringV2Builder
   | SnapRegistryController
   | SubscriptionController
   | SnapsNameProvider
