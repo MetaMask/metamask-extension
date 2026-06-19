ff59347eaf0fe1a528acd0406323e02fd8476e8b
Lionell Briones <llenoil@gmail.com>
2026-06-19 18:03:27 +0800

    add error ui handles for otp and update success view

--- ui/pages/settings/add-device-tab/components/success.tsx
+++ ui/pages/settings/add-device-tab/components/success.tsx
@@ -13,11 +13,8 @@ import {
   FontWeight,
   BoxFlexDirection,
   BoxAlignItems,
-  BoxJustifyContent,
-  BoxBackgroundColor,
 } from '@metamask/design-system-react';
 import { useI18nContext } from '../../../../hooks/useI18nContext';
-import { ScrollContainer } from '../../../../contexts/scroll-container';
 
 type SuccessProps = {
   onDone: () => void;
@@ -26,51 +23,14 @@ type SuccessProps = {
 const Success = ({ onDone }: SuccessProps) => {
   const t = useI18nContext();
 
-  const wallets = [
-    {
-      name: 'Main Wallet',
-      accountsCount: 5,
-    },
-    {
-      name: 'Secondary Wallet',
-      accountsCount: 2,
-    },
-    {
-      name: 'Tertiary Wallet',
-      accountsCount: 3,
-    },
-    {
-      name: 'Quaternary Wallet',
-      accountsCount: 4,
-    },
-    {
-      name: 'Quinary Wallet',
-      accountsCount: 1,
-    },
-    {
-      name: 'Senary Wallet',
-      accountsCount: 0,
-    },
-    {
-      name: 'Septenary Wallet',
-      accountsCount: 0,
-    },
-    {
-      name: 'Octonary Wallet',
-      accountsCount: 0,
-    },
-    {
-      name: 'Nonary Wallet',
-      accountsCount: 0,
-    },
-    {
-      name: 'Decenary Wallet',
-      accountsCount: 0,
-    },
-  ];
-
   return (
-    <Box flexDirection={BoxFlexDirection.Column} className="flex-1 min-h-0">
+    <Box
+      alignItems={BoxAlignItems.Center}
+      flexDirection={BoxFlexDirection.Column}
+      gap={8}
+      paddingTop={8}
+      className="flex-1"
+    >
       <Box
         className="text-center"
         flexDirection={BoxFlexDirection.Column}
@@ -99,52 +59,6 @@ const Success = ({ onDone }: SuccessProps) => {
           {t('add_device_success_desc', [5, 2])}
         </Text>
       </Box>
-      <ScrollContainer className="flex flex-1 flex-col overflow-y-auto min-h-0 mt-8">
-        <Box flexDirection={BoxFlexDirection.Column}>
-          {wallets.map((wallet) => (
-            <Box
-              key={wallet.name}
-              flexDirection={BoxFlexDirection.Row}
-              alignItems={BoxAlignItems.Center}
-              justifyContent={BoxJustifyContent.Between}
-              padding={2}
-            >
-              <Box
-                flexDirection={BoxFlexDirection.Row}
-                alignItems={BoxAlignItems.Center}
-                gap={3}
-              >
-                <Box
-                  backgroundColor={BoxBackgroundColor.PrimaryMuted}
-                  padding={2}
-                  className="rounded-md"
-                >
-                  <Icon name={IconName.Wallet} />
-                </Box>
-                <Box
-                  flexDirection={BoxFlexDirection.Column}
-                  alignItems={BoxAlignItems.Start}
-                >
-                  <Text
-                    variant={TextVariant.BodyMd}
-                    fontWeight={FontWeight.Medium}
-                    color={TextColor.TextDefault}
-                  >
-                    {wallet.name}
-                  </Text>
-                  <Text
-                    variant={TextVariant.BodySm}
-                    color={TextColor.TextAlternative}
-                  >
-                    {`${wallet.accountsCount} ${t('accounts')}`}
-                  </Text>
-                </Box>
-              </Box>
-              <Icon name={IconName.Check} color={IconColor.SuccessDefault} />
-            </Box>
-          ))}
-        </Box>
-      </ScrollContainer>
       <Button className="w-full mt-10" onClick={onDone}>
         {t('done')}
       </Button>
