aa94280badf37f4c12dd6fe07212ce6d41391598
Francis Nepomuceno <n3ps@users.noreply.github.com>
2026-06-19 00:10:50 -0400

    refactor

--- ui/selectors/activity.test.ts
+++ ui/selectors/activity.test.ts
@@ -97,7 +97,9 @@ describe('selectNonEvmTransactionsForActivity', () => {
 });
 
 describe('selectEvmAddress', () => {
-  const evmAddressInGroup = '0x0dcd5d886577d5081b0c52e242ef29e70be3e7bc';
+  const { internalAccounts } = typedMockState.metamask;
+  const evmAddressInGroup =
+    internalAccounts.accounts[internalAccounts.selectedAccount].address;
 
   it('returns the EVM address from the selected account group when an EVM account is globally selected', () => {
     expect(selectEvmAddress(typedMockState)).toBe(evmAddressInGroup);
