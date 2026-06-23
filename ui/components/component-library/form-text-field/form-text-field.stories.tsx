e5220a4c6fc3d758a4d7b3e0540befbfd6b85a77
georgewrmarshall <george.marshall@consensys.net>
2026-06-23 18:34:16 +0100

    fix: restore React import in story files using classic JSX transform

--- ui/components/component-library/form-text-field/form-text-field.stories.tsx
+++ ui/components/component-library/form-text-field/form-text-field.stories.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent } from 'react';
+import React, { ChangeEvent } from 'react';
 import { StoryFn, Meta } from '@storybook/react';
 import { useArgs } from '@storybook/client-api';
 
--- ui/components/component-library/header-base/header-base.stories.tsx
+++ ui/components/component-library/header-base/header-base.stories.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { StoryFn, Meta } from '@storybook/react';
 import { Text } from '../text';
 
