d9b8d4b7c5c4685abbc5d6de5bcc96e240c6e396
Florin Dzeladini <florin.dzeladini@consensys.net>
2026-06-29 16:20:39 +0200

    chore: footer.ts revert

--- ui/components/app/snaps/snap-ui-renderer/components/footer.ts
+++ ui/components/app/snaps/snap-ui-renderer/components/footer.ts
@@ -74,7 +74,8 @@ export const footer: UIComponentFactory<FooterElement> = ({
         ...buttonMapped.props,
         snapVariant: buttonMapped.props?.variant,
         variant:
-          providedChildren.length > 1 && index === providedChildren.length - 1
+          providedChildren.length > 1 &&
+          index === providedChildren.length - 1
             ? ButtonVariant.Primary
             : ButtonVariant.Secondary,
         isSnapAction: true,
