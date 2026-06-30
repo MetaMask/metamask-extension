8a097cd31fcb9da812652764811e0f9a95170cc4
GeorgeGkas <georgegkas@gmail.com>
2026-06-30 21:08:16 +0300

    fix: type errors

--- test/e2e/feature-flags/feature-flag-registry.ts
+++ test/e2e/feature-flags/feature-flag-registry.ts
@@ -294,15 +294,6 @@ export const FEATURE_FLAG_REGISTRY: Record<string, FeatureFlagRegistryEntry> = {
     },
     status: FeatureFlagStatus.Active,
   },
-  bridgeQuoteStatusManager: {
-    name: 'bridgeQuoteStatusManager',
-    type: FeatureFlagType.Remote,
-    inProd: true,
-    productionDefault: {
-      versions: {},
-    },
-    status: FeatureFlagStatus.Active,
-  },
   bridgeConfig: {
     name: 'bridgeConfig',
     type: FeatureFlagType.Remote,
