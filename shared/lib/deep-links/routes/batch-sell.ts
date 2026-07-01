4be7b315af42f6f6ea7d3867bc6c0772c59602a2
Christian Montoya <christian.montoya@consensys.net>
2026-07-01 18:17:24 -0400

    simplify test

--- shared/lib/deep-links/routes/batch-sell.ts
+++ shared/lib/deep-links/routes/batch-sell.ts
@@ -8,10 +8,8 @@ import { Route } from './route';
 export const batchSell = new Route({
   pathname: '/batch-sell',
   getTitle: (_: URLSearchParams) => 'deepLink_theBatchSellPage',
-  handlerSearchParams: 'original',
-  handler: function handler(params: URLSearchParams) {
+  handler: function handler() {
     const deeplinkUrl = new URL('/batch-sell', DEEP_LINK_ORIGIN);
-    params.forEach((value, key) => deeplinkUrl.searchParams.append(key, value));
 
     return createHomeQrCodeDestination(
       HomeQueryParams.BatchSellDeeplinkUrl,
