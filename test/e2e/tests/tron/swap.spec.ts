041535f7f5d896406fa21b8ea43c846bd01f1a63
Frederic HENG <frederic.heng@consensys.net>
2026-07-05 15:47:09 +0200

    test: update Tron balance check e2e

--- test/e2e/tests/tron/swap.spec.ts
+++ test/e2e/tests/tron/swap.spec.ts
@@ -34,7 +34,7 @@ describe('Swap on Tron', function () {
 
         const homePage = new HomePage(driver);
         await homePage.checkPageIsLoaded();
-        await homePage.checkExpectedBalanceIsDisplayed('6.07');
+        await homePage.checkExpectedBalanceIsDisplayed('106.07');
 
         const swapPage = new SwapPage(driver);
         await homePage.clickOnSwapButton();
@@ -74,7 +74,7 @@ describe('Swap on Tron', function () {
 
         const homePage = new HomePage(driver);
         await homePage.checkPageIsLoaded();
-        await homePage.checkExpectedBalanceIsDisplayed('6.07');
+        await homePage.checkExpectedBalanceIsDisplayed('106.07');
 
         const swapPage = new SwapPage(driver);
         await homePage.clickOnSwapButton();
