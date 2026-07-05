de5f57af0bce81b298215eb51080386426a7fe96
salimtb <salim.toubal@consensys.net>
2026-07-05 13:11:07 +0200

    fix: clean up

--- test/e2e/tests/tokens/add-token-using-search.spec.ts
+++ test/e2e/tests/tokens/add-token-using-search.spec.ts
@@ -226,7 +226,7 @@ describe('Add existing token using search', function () {
         testSpecificMock: mockBscApis,
       },
       async ({ driver }) => {
-        await login(driver);
+        await login(driver, { validateBalance: false });
 
         const tokensTab = new TokensTab(driver);
         await tokensTab.importTokenBySearch({
