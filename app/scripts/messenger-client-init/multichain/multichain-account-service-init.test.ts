4ef8ceac2179e44251bc29cadc308eb1434c6092
Stanley Yuen <102275989+stanleyyconsensys@users.noreply.github.com>
2026-07-06 15:41:34 +0800

    fix: test

--- app/scripts/messenger-client-init/multichain/multichain-account-service-init.test.ts
+++ app/scripts/messenger-client-init/multichain/multichain-account-service-init.test.ts
@@ -1,4 +1,5 @@
 import {
+  AccountProviderWrapper,
   MultichainAccountService,
   MultichainAccountServiceMessenger,
 } from '@metamask/multichain-account-service';
@@ -17,6 +18,7 @@ import {
 } from '../messengers/accounts';
 import { PreferencesControllerGetStateAction } from '../../controllers/preferences-controller';
 import { MultichainAccountServiceInit } from './multichain-account-service-init';
+import { RemoteFeatureFlagControllerGetStateAction } from '@metamask/remote-feature-flag-controller';
 
 jest.mock('@metamask/multichain-account-service');
 
@@ -30,7 +32,7 @@ function buildInitRequestMock(): jest.Mocked<
 > {
   const baseControllerMessenger = new Messenger<
     MockAnyNamespace,
-    PreferencesControllerGetStateAction | ActionConstraint,
+    PreferencesControllerGetStateAction | RemoteFeatureFlagControllerGetStateAction | ActionConstraint,
     never
   >({ namespace: MOCK_ANY_NAMESPACE });
 
@@ -92,6 +94,9 @@ describe('MultichainAccountServiceInit', () => {
 
       expect(multichainAccountServiceClassMock).toHaveBeenCalledWith({
         messenger: requestMock.controllerMessenger,
+        providers: [
+          expect.any(AccountProviderWrapper),
+        ],
         providerConfigs: expect.any(Object),
         config: expect.any(Object),
         ensureOnboardingComplete: requestMock.ensureOnboardingComplete,
