d38004fc3ba9219da66f52f6131c579bc074e619
Norbert Elter <72046715+itsyoboieltr@users.noreply.github.com>
2026-07-08 16:38:36 +0400

    Update index.ts

--- development/webpack/utils/plugins/LavamoatPlugin/index.ts
+++ development/webpack/utils/plugins/LavamoatPlugin/index.ts
@@ -16,6 +16,79 @@ const nullUnsafeEntries: Set<string> = new Set([
   'bootstrap',
 ]);
 
+const getScuttleGlobalThisExceptions = (args: Args) => [
+  // globals used by different mm deps outside of lm compartment
+  'window',
+  'Proxy',
+  'toString',
+  'getComputedStyle',
+  'addEventListener',
+  'removeEventListener',
+  'ShadowRoot',
+  'HTMLElement',
+  'HTMLFormElement',
+  'Element',
+  'pageXOffset',
+  'pageYOffset',
+  'visualViewport',
+  'Reflect',
+  'Set',
+  'Object',
+  'navigator',
+  'harden',
+  'console',
+  'WeakSet',
+  'Event',
+  'EventTarget',
+  // globals used by the browser to generate notifications
+  'Image',
+  'fetch',
+  'AbortController',
+  'OffscreenCanvas',
+  /cdc_[a-zA-Z0-9]+_[a-zA-Z]+/iu,
+  'name',
+  'performance',
+  'parseFloat',
+  'innerWidth',
+  'innerHeight',
+  'Symbol',
+  'Math',
+  'DOMRect',
+  'Number',
+  'Array',
+  'crypto',
+  'Function',
+  'Uint8Array',
+  'String',
+  'Promise',
+  'JSON',
+  'Date',
+  // Selenium atoms construct regexes while locating elements.
+  'RegExp',
+  // globals sentry needs to function
+  '__SENTRY__',
+  'appState',
+  'extra',
+  'stateHooks',
+  'sentryHooks',
+  'sentry',
+  'logEncryptedVault',
+  // needed by Sentry and react-router-dom v6 HashRouter
+  'history',
+  // globals used by react-dom
+  'getSelection',
+  // globals opera needs to function
+  'opr',
+  // for @popperjs/core and snap simple keyring site
+  'devicePixelRatio',
+  // for @tanstack/react-virtual
+  'ResizeObserver',
+  'setTimeout',
+  'clearTimeout',
+  // globals used by e2e
+  ...(args.test ? ['ret_nodes', 'browser', 'chrome', 'indexedDB'] : []),
+];
+
 export const lavamoatPlugin = (args: Args) =>
   new LavaMoatPlugin({
     rootDir,
@@ -58,19 +131,8 @@ export const lavamoatPlugin = (args: Args) =>
               enabled: true,
               // Globals used by the service worker
               exceptions: [
+                ...getScuttleGlobalThisExceptions(args),
                 'importScripts',
-                'console',
-                'performance',
-                'setTimeout',
-                'clearTimeout',
-                '__SENTRY__',
-                'sentry',
-                'sentryHooks',
-                'stateHooks',
-                'appState',
-                'extra',
-                'logEncryptedVault',
-                'history',
               ],
             },
           },
@@ -104,78 +166,7 @@ export const lavamoatPlugin = (args: Args) =>
       enabled: true,
       // Scuttler depends on Snow
       scuttlerName: args.snow ? 'SCUTTLER' : undefined,
-      exceptions: [
-        // globals used by different mm deps outside of lm compartment
-        'window',
-        'Proxy',
-        'toString',
-        'getComputedStyle',
-        'addEventListener',
-        'removeEventListener',
-        'ShadowRoot',
-        'HTMLElement',
-        'HTMLFormElement',
-        'Element',
-        'pageXOffset',
-        'pageYOffset',
-        'visualViewport',
-        'Reflect',
-        'Set',
-        'Object',
-        'navigator',
-        'harden',
-        'console',
-        'WeakSet',
-        'Event',
-        'EventTarget',
-        // globals used by the browser to generate notifications
-        'Image',
-        'fetch',
-        'AbortController',
-        'OffscreenCanvas',
-        /cdc_[a-zA-Z0-9]+_[a-zA-Z]+/iu,
-        'name',
-        'performance',
-        'parseFloat',
-        'innerWidth',
-        'innerHeight',
-        'Symbol',
-        'Math',
-        'DOMRect',
-        'Number',
-        'Array',
-        'crypto',
-        'Function',
-        'Uint8Array',
-        'String',
-        'Promise',
-        'JSON',
-        'Date',
-        // Selenium atoms construct regexes while locating elements.
-        'RegExp',
-        // globals sentry needs to function
-        '__SENTRY__',
-        'appState',
-        'extra',
-        'stateHooks',
-        'sentryHooks',
-        'sentry',
-        'logEncryptedVault',
-        // needed by Sentry and react-router-dom v6 HashRouter
-        'history',
-        // globals used by react-dom
-        'getSelection',
-        // globals opera needs to function
-        'opr',
-        // for @popperjs/core and snap simple keyring site
-        'devicePixelRatio',
-        // for @tanstack/react-virtual
-        'ResizeObserver',
-        'setTimeout',
-        'clearTimeout',
-        // globals used by e2e
-        ...(args.test ? ['ret_nodes', 'browser', 'chrome', 'indexedDB'] : []),
-      ],
+      exceptions: getScuttleGlobalThisExceptions(args),
     },
   });
 
