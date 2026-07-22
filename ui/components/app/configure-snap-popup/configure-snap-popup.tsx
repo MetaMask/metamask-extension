e0432282d3301965be90181389efb990eb637ebb
Mark Stacey <mark.stacey@consensys.net>
2026-07-22 01:31:02 -0230

    chore: remove unused ESLint directives from `ui/components`
    Unused ESLint ignore directives have been removed from the
    `ui/components` directory.
    
    This helps unblock the ESLint v9 upgrade (it complains about unused
    directives by default).

--- ui/components/app/configure-snap-popup/configure-snap-popup.tsx
+++ ui/components/app/configure-snap-popup/configure-snap-popup.tsx
@@ -26,8 +26,6 @@ export enum ConfigureSnapPopupType {
   INSTALL = 'install',
 }
 
-// TODO: Fix in https://github.com/MetaMask/metamask-extension/issues/31860
-// eslint-disable-next-line @typescript-eslint/naming-convention
 export default function ConfigureSnapPopup({
   type,
   isOpen,
