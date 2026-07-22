37754551f213ea6254f2aac7c0116069e6e88679
Mark Stacey <mark.stacey@consensys.net>
2026-07-22 17:38:03 -0230

    Remove rule accidentally added when resolving conflicts

--- .eslintrc.js
+++ .eslintrc.js
@@ -580,13 +580,6 @@ module.exports = defineConfig([
 
       // Static hex values are only discouraged in application code, using them in tests is OK.
       '@metamask/design-tokens/color-no-hex': 'off',
-
-      // *.snap files weren't parsed by previous versions of this eslint
-      // config section, but something got fixed somewhere, and now this rule
-      // causes failures. We need to turn it off instead of fix them because
-      // we aren't even remotely close to being in alignment. If it bothers
-      // you open a PR to fix it yourself.
-      'jest/no-large-snapshots': 'off',
       'jest/no-restricted-matchers': 'off',
 
       /**
