55c93169b1da9eba57394c9f090a05023957a65e
Mathieu Artu <mathieu.artu@consensys.net>
2026-07-23 21:58:48 +0200

    fix: lint & yarn.lock

--- shared/constants/offscreen-communication.ts
+++ shared/constants/offscreen-communication.ts
@@ -42,8 +42,6 @@ export enum OffscreenCommunicationEvents {
   connectivityChange = 'connectivity-change',
   // TODO: Fix in https://github.com/MetaMask/metamask-extension/issues/31860
   // eslint-disable-next-line @typescript-eslint/naming-convention
-  // TODO: Fix in https://github.com/MetaMask/metamask-extension/issues/31860
-  // eslint-disable-next-line @typescript-eslint/naming-convention
   ledgerModeReady = 'ledger-mode-ready',
   // TODO: Fix in https://github.com/MetaMask/metamask-extension/issues/31860
   // eslint-disable-next-line @typescript-eslint/naming-convention
