0ff084adc5deb0e393ca1e2eb18fa67420453518
georgewrmarshall <george.marshall@consensys.net>
2026-07-28 10:44:58 -0700

    fix(pure-black): use !bg-transparent to override ButtonBase inline background

--- ui/pages/confirmations/components/send/network-filter/network-filter.tsx
+++ ui/pages/confirmations/components/send/network-filter/network-filter.tsx
@@ -248,7 +248,7 @@ export const NetworkFilter = ({
           size={ButtonBaseSize.Sm}
           startIconName={IconName.Filter}
           startIconProps={{ marginInlineEnd: 1, size: IconSize.Md }}
-          className="bg-transparent hover:bg-hover active:bg-pressed"
+          className="!bg-transparent hover:bg-hover active:bg-pressed"
           borderRadius={BorderRadius.LG}
           color={
             isSingleNetworkSelected
