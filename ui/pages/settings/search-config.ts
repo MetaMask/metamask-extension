b4e16443b16a7a5126cf11866bbc6781197c9a33
ameliejyc <amelie.chan@gmail.com>
2026-07-28 13:57:52 +0100

    chore: remove from search config for now

--- ui/pages/settings/search-config.ts
+++ ui/pages/settings/search-config.ts
@@ -58,7 +58,6 @@ export const PREFERENCES_ITEMS = {
   'account-identicon': 'accountIdenticon',
   'show-default-address': 'showDefaultAddress',
   'show-extension': 'showExtensionInFullSizeView',
-  'show-x-widget': 'showWebWidgetOnX',
   'manage-institutional-wallet': 'manageInstitutionalWallets',
 } as const;
 
