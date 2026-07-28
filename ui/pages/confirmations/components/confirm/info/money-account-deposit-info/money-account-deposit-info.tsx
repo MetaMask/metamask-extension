d0bd322bbca3e4a98a59fdde1e81394d8102a84e
jpuri <jyotipuri@gmail.com>
2026-07-28 17:37:31 +0530

    update

--- ui/pages/confirmations/components/confirm/info/money-account-deposit-info/money-account-deposit-info.tsx
+++ ui/pages/confirmations/components/confirm/info/money-account-deposit-info/money-account-deposit-info.tsx
@@ -3,8 +3,6 @@ import { useAddToken } from '../../../../hooks/tokens/useAddToken';
 import { CustomAmountInfo } from '../../../info/custom-amount-info';
 import { MAINNET_MUSD } from '../../../../constants/musd';
 
-// Display the deposit amount in fiat ($) rather than the MUSD token symbol,
-// matching the mobile "Add funds" screen.
 const MONEY_ACCOUNT_DEPOSIT_CURRENCY = 'usd';
 
 export const MoneyAccountDepositInfo = () => {
