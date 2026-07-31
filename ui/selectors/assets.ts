d7004cd3b585c94cbb36cf048f941cc88ac35bad
Stanley Yuen <102275989+stanleyyconsensys@users.noreply.github.com>
2026-07-31 12:41:52 +0800

    chore: update asset selector

--- ui/selectors/assets.ts
+++ ui/selectors/assets.ts
@@ -1347,8 +1347,12 @@ export const selectAccountGroupBalanceIsLoadedForEmptyState = createSelector(
       Object.keys(allMainnetNetworksMap?.eip155 || {}),
     );
     const mainnetNonEvmChainIds = new Set(
-      Object.keys(allMainnetNetworksMap?.solana || {}).concat(
-        Object.keys(allMainnetNetworksMap?.bip122 || {}),
+      [
+        KnownCaipNamespace.Solana,
+        KnownCaipNamespace.Bip122,
+        KnownCaipNamespace.Stellar,
+      ].flatMap((namespace) =>
+        Object.keys(allMainnetNetworksMap?.[namespace] || {}),
       ),
     );
 
