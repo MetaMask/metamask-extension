6ffb7121920ee0091ec292377cb22746c8a0441b
Alex Mendonca <alex.mendonca@consensys.net>
2026-08-06 15:26:29 +0100

    fix: import

--- ui/hooks/ramps/useRampsAnalytics.test.tsx
+++ ui/hooks/ramps/useRampsAnalytics.test.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react';
 import {
   MetaMetricsEventCategory,
   MetaMetricsEventName,
