b76c8ed0cb8c5b36e9d79dcc3c717dc662834ef9
micaelae <micaela.estabillo@consensys.net>
2026-08-07 16:40:03 -0700

    fix: priceImpact

--- test/data/bridge/mock-bridge-store.ts
+++ test/data/bridge/mock-bridge-store.ts
@@ -19,7 +19,10 @@ import { ETH_SCOPE_EOA } from '@metamask/keyring-utils';
 import type { SmartTransactionsNetworks } from '../../../shared/lib/selectors/feature-flags';
 import type { ChainValueOrderOverride } from '../../../shared/lib/bridge/chain-value-order';
 import { CHAIN_IDS } from '../../../shared/constants/network';
-import type { BridgeAppState } from '../../../ui/ducks/bridge/selectors';
+import type {
+  BridgeAppState,
+  getValidationErrors,
+} from '../../../ui/ducks/bridge/selectors';
 import { createSwapsMockStore } from '../../jest/mock-store';
 import { mockNetworkState } from '../../stub/networks';
 import { ETH_EOA_METHODS } from '../../../shared/constants/eth-methods';
@@ -27,6 +30,22 @@ import { NETWORK_TO_SHORT_NETWORK_NAME_MAP } from '../../../shared/constants/bri
 import { KeyringType } from '../../../shared/constants/keyring';
 import { mockTokenData } from './mock-token-data';
 
+export const DEFAULT_VALIDATION_ERRORS: ReturnType<typeof getValidationErrors> =
+  {
+    isNoQuotesAvailable: false,
+    isInsufficientGasForQuote: false,
+    isInsufficientBalance: false,
+    isStockMarketClosed: false,
+    isQuoteExpired: false,
+    isPriceImpactWarning: false,
+    isPriceImpactError: false,
+    isInsufficientGasBalance: false,
+    isInsufficientNativeReserve: false,
+    isTxAlertPresent: false,
+    isTxAlertLoading: false,
+    isNetworkFeeUnavailable: false,
+    isEstimatedReturnLow: false,
+  };
 export const MOCK_LEDGER_ACCOUNT = {
   id: 'bf588376-0492-4a35-b653-0f1304a6c5f1',
   address: '0xb3864b298f4fddbbbd2fa5cf1a2a2748932b3b82',
