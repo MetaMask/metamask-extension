e1f3dec4ed3ed27c9db455ac649dc2fe5122d7ef
lwin <lwin.kyaw@consensys.net>
2026-08-11 13:03:50 +0800

    fix: fixed sonar

--- ui/selectors/selectors.js
+++ ui/selectors/selectors.js
@@ -4102,7 +4102,7 @@ export function getDeferredDeepLink(state) {
 export const getDeferredDeepLinkParameters = createResultEqualSelector(
   getDeferredDeepLink,
   (deferredDeepLink) => {
-    if (!deferredDeepLink || !deferredDeepLink.referringLink) {
+    if (!deferredDeepLink?.referringLink) {
       return null;
     }
 
