129dc8665b05201faa5d9a42be9297b083932ff1
HJetpoluru <harika.jetpoluru@consensys.net>
2026-08-12 15:09:01 -0400

    add bridge quote page boundary selector

--- test/e2e/page-objects/pages/bridge/quote-page.ts
+++ test/e2e/page-objects/pages/bridge/quote-page.ts
@@ -29,6 +29,10 @@ class BridgeQuotePage {
 
   private backButton = '[aria-label="Back"]';
 
+  private readonly bridgeQuotePage = {
+    testId: 'parent-selector-bridge-quote',
+  };
+
   private closeButton = '[aria-label="Close"]';
 
   private confirmButton =
@@ -242,7 +246,7 @@ class BridgeQuotePage {
    */
   async checkPageIsLoaded(timeout: number = 10000): Promise<void> {
     try {
-      await this.driver.waitForSelector(this.sourceAssetPickerButton, {
+      await this.driver.waitForSelector(this.bridgeQuotePage, {
         timeout,
       });
     } catch (e) {
