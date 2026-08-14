513196a2fecc6e387386ccffcb2c730d1cd77098
David Murdoch <187813+davidmurdoch@users.noreply.github.com>
2026-08-14 15:38:20 -0400

    chore(deep-links): fix formatting

--- ui/pages/deep-link/deep-link.tsx
+++ ui/pages/deep-link/deep-link.tsx
@@ -298,8 +298,7 @@ export const DeepLink = () => {
   );
   const isPendingDeepLinkRequest = useSelector(
     (state: MetaMaskReduxState) =>
-      requestId &&
-      state.metamask.pendingDeepLinkRequestIds.includes(requestId),
+      requestId && state.metamask.pendingDeepLinkRequestIds.includes(requestId),
   );
 
   const [viewState, setViewState] =
