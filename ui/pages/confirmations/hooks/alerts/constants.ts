0627ee8ea9b8b4c7cfe378f461786d06e0a5e609
Alex Donesky <adonesky@gmail.com>
2026-08-17 15:40:21 -0500

    refactor: deduplicate Blockaid alert reason mappings

--- ui/pages/confirmations/hooks/alerts/constants.ts
+++ ui/pages/confirmations/hooks/alerts/constants.ts
@@ -20,28 +20,50 @@ export enum AlertsName {
   Blockaid = 'blockaid',
 }
 
+const APPROVAL_REASONS = [
+  BlockaidReason.approvalFarming,
+  BlockaidReason.permitFarming,
+  BlockaidReason.setApprovalForAll,
+] as const;
+
+const MARKETPLACE_REASONS = [
+  BlockaidReason.blurFarming,
+  BlockaidReason.seaportFarming,
+] as const;
+
+const SIGNATURE_REASONS = [
+  BlockaidReason.rawSignatureFarming,
+  BlockaidReason.tradeOrderFarming,
+] as const;
+
+const TRANSFER_REASONS = [
+  BlockaidReason.rawNativeTokenTransfer,
+  BlockaidReason.transferFarming,
+  BlockaidReason.transferFromFarming,
+] as const;
+
+function createReasonMap<Value extends string>(
+  entries: readonly (readonly [readonly BlockaidReason[], Value])[],
+): Readonly<Partial<Record<BlockaidReason, Value>>> {
+  return Object.freeze(
+    Object.fromEntries(
+      entries.flatMap(([reasons, value]) =>
+        reasons.map((reason) => [reason, value] as const),
+      ),
+    ),
+  );
+}
+
 /** Reason to description translation key mapping. Grouped by translations. */
-export const REASON_TO_DESCRIPTION_TKEY = Object.freeze({
-  [BlockaidReason.approvalFarming]: 'blockaidDescriptionApproveFarming',
-  [BlockaidReason.permitFarming]: 'blockaidDescriptionApproveFarming',
-  [BlockaidReason.setApprovalForAll]: 'blockaidDescriptionApproveFarming',
-
-  [BlockaidReason.blurFarming]: 'blockaidDescriptionMarketplaceFarming',
-  [BlockaidReason.seaportFarming]: 'blockaidDescriptionMarketplaceFarming',
-
-  [BlockaidReason.errored]: 'blockaidDescriptionErrored',
-
-  [BlockaidReason.maliciousDomain]: 'blockaidDescriptionMaliciousDomain',
-
-  [BlockaidReason.rawSignatureFarming]: 'blockaidDescriptionHighRiskSignature',
-  [BlockaidReason.tradeOrderFarming]: 'blockaidDescriptionHighRiskSignature',
-
-  [BlockaidReason.rawNativeTokenTransfer]: 'blockaidDescriptionTransferFarming',
-  [BlockaidReason.transferFarming]: 'blockaidDescriptionTransferFarming',
-  [BlockaidReason.transferFromFarming]: 'blockaidDescriptionTransferFarming',
-
-  [BlockaidReason.other]: 'blockaidDescriptionRiskSignals',
-});
+export const REASON_TO_DESCRIPTION_TKEY = createReasonMap([
+  [APPROVAL_REASONS, 'blockaidDescriptionApproveFarming'],
+  [MARKETPLACE_REASONS, 'blockaidDescriptionMarketplaceFarming'],
+  [[BlockaidReason.errored], 'blockaidDescriptionErrored'],
+  [[BlockaidReason.maliciousDomain], 'blockaidDescriptionMaliciousDomain'],
+  [SIGNATURE_REASONS, 'blockaidDescriptionHighRiskSignature'],
+  [TRANSFER_REASONS, 'blockaidDescriptionTransferFarming'],
+  [[BlockaidReason.other], 'blockaidDescriptionRiskSignals'],
+]);
 
 /**
  * Amount-bearing variants of banner descriptions, used when a formatted fiat
@@ -70,47 +92,32 @@ export const REASON_TO_MARKETPLACE_NAME = Object.freeze({
 });
 
 /** Reason to title translation key mapping. */
-export const REASON_TO_TITLE_TKEY = Object.freeze({
-  [BlockaidReason.approvalFarming]: 'blockaidTitleHighRiskApproval',
-  [BlockaidReason.permitFarming]: 'blockaidTitleHighRiskApproval',
-  [BlockaidReason.setApprovalForAll]: 'blockaidTitleHighRiskApproval',
-
-  [BlockaidReason.blurFarming]: 'blockaidTitleHighRiskApproval',
-  [BlockaidReason.seaportFarming]: 'blockaidTitleHighRiskApproval',
-
-  [BlockaidReason.errored]: 'blockaidTitleMayNotBeSafe',
-
-  [BlockaidReason.maliciousDomain]: 'blockaidTitleSiteFlaggedUnsafe',
-
-  [BlockaidReason.rawSignatureFarming]: 'blockaidTitleHighRiskSignature',
-  [BlockaidReason.tradeOrderFarming]: 'blockaidTitleHighRiskSignature',
-
-  [BlockaidReason.rawNativeTokenTransfer]: 'blockaidTitleHighRiskTransfer',
-  [BlockaidReason.transferFarming]: 'blockaidTitleHighRiskTransfer',
-  [BlockaidReason.transferFromFarming]: 'blockaidTitleHighRiskTransfer',
-
-  [BlockaidReason.other]: 'blockaidTitleRiskSignalsDetected',
-});
+export const REASON_TO_TITLE_TKEY = createReasonMap([
+  [
+    [...APPROVAL_REASONS, ...MARKETPLACE_REASONS],
+    'blockaidTitleHighRiskApproval',
+  ],
+  [[BlockaidReason.errored], 'blockaidTitleMayNotBeSafe'],
+  [[BlockaidReason.maliciousDomain], 'blockaidTitleSiteFlaggedUnsafe'],
+  [SIGNATURE_REASONS, 'blockaidTitleHighRiskSignature'],
+  [TRANSFER_REASONS, 'blockaidTitleHighRiskTransfer'],
+  [[BlockaidReason.other], 'blockaidTitleRiskSignalsDetected'],
+]);
 
 /**
  * Reason to request-type noun translation key mapping. The noun is composed
  * into the confirm-anyway modal message ("...high-risk signals in this
  * approval").
  */
-export const REASON_TO_REQUEST_TYPE_TKEY = Object.freeze({
-  [BlockaidReason.approvalFarming]: 'blockaidRequestTypeApproval',
-  [BlockaidReason.permitFarming]: 'blockaidRequestTypeApproval',
-  [BlockaidReason.setApprovalForAll]: 'blockaidRequestTypeApproval',
-  [BlockaidReason.blurFarming]: 'blockaidRequestTypeApproval',
-  [BlockaidReason.seaportFarming]: 'blockaidRequestTypeApproval',
-
-  [BlockaidReason.rawSignatureFarming]: 'blockaidRequestTypeSignature',
-  [BlockaidReason.tradeOrderFarming]: 'blockaidRequestTypeSignature',
-
-  [BlockaidReason.rawNativeTokenTransfer]: 'blockaidRequestTypeTransfer',
-  [BlockaidReason.transferFarming]: 'blockaidRequestTypeTransfer',
-  [BlockaidReason.transferFromFarming]: 'blockaidRequestTypeTransfer',
-
-  [BlockaidReason.maliciousDomain]: 'blockaidRequestTypeRequest',
-  [BlockaidReason.other]: 'blockaidRequestTypeRequest',
-});
+export const REASON_TO_REQUEST_TYPE_TKEY = createReasonMap([
+  [
+    [...APPROVAL_REASONS, ...MARKETPLACE_REASONS],
+    'blockaidRequestTypeApproval',
+  ],
+  [SIGNATURE_REASONS, 'blockaidRequestTypeSignature'],
+  [TRANSFER_REASONS, 'blockaidRequestTypeTransfer'],
+  [
+    [BlockaidReason.maliciousDomain, BlockaidReason.other],
+    'blockaidRequestTypeRequest',
+  ],
+]);
