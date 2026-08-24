b426b6c00cda8807e9581f74aae1cca7b4a68f5b
HJetpoluru <harika.jetpoluru@consensys.net>
2026-08-24 11:44:51 -0400

    add stable parent selectors to settings pages

--- ui/pages/shield/plan/shield-plan.test.tsx
+++ ui/pages/shield/plan/shield-plan.test.tsx
@@ -27,6 +27,8 @@ describe('Change payment method', () => {
     const mockStore = configureMockStore([])(mockState);
     const { getByTestId } = renderWithProvider(<ShieldPlan />, mockStore);
 
-    expect(getByTestId('shield-plan-page')).toBeInTheDocument();
+    expect(
+      getByTestId('parent-selector-shield-plan-page'),
+    ).toBeInTheDocument();
   });
 });
