e460cb2626464adf9e0ed1a09df03ac71c68c8d0
Prithpal Sooriya <prithpal.sooriya@consensys.net>
2026-08-28 11:46:03 +0100

    test: fix flakey e2e test.
    New scam regression test must handle mUSD as this is added into the wallet by default.

--- test/e2e/tests/tokens/spam-token-cleanup.spec.ts
+++ test/e2e/tests/tokens/spam-token-cleanup.spec.ts
@@ -325,6 +325,7 @@ describe('Spam token cleanup', function (this: Suite) {
           ...NATIVE_TOKEN_NAMES,
           LEGITIMATE_ASSET.name,
           CUSTOM_ASSET.name,
+          MUSD_ASSET.name,
         ]);
 
         const { assetsInfo, assetsBalance } = await getTrackedAssetIds(driver);
