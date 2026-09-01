e7a6c0656269a92791950c8b3aad5ea43f44a70c
ameliejyc <amelie.chan@gmail.com>
2026-09-01 10:10:32 +0100

    chore: remove unused prop

--- ui/components/app/hyperliquid-deposit-prompt/hyperliquid-deposit-prompt.types.ts
+++ ui/components/app/hyperliquid-deposit-prompt/hyperliquid-deposit-prompt.types.ts
@@ -4,5 +4,4 @@ export type HyperliquidDepositPromptResult =
 
 export type HyperliquidDepositPromptProps = {
   onActionComplete: (result: HyperliquidDepositPromptResult) => void;
-  selectedAddress?: string;
 };
