8d87bf6fd3968b43fe529baabb78f49839568a0e
NidhiKJha <menidhikjha@gmail.com>
2026-09-07 18:19:01 +0530

    added fixtures

--- ui/pages/settings/privacy-tab/basic-functionality-item.tsx
+++ ui/pages/settings/privacy-tab/basic-functionality-item.tsx
@@ -8,6 +8,7 @@ import {
 } from '../../../store/actions';
 import { openBasicFunctionalityModal } from '../../../ducks/app/app';
 import { getIsBasicFunctionalityConsolidationEnabled } from '../../../selectors/multichain/feature-flags';
+import { getSocialLoginType } from '../../../selectors/onboarding';
 import { SettingsToggleItem } from '../shared/settings-toggle-item';
 import { useAnalytics } from '../../../hooks/useAnalytics';
 import {
@@ -23,9 +24,7 @@ export const BasicFunctionalityToggleItem = () => {
   const dispatch = useDispatch();
   const { trackEvent, createEventBuilder } = useAnalytics();
   const useExternalServices = useSelector(getUseExternalServices);
-  const isSocialLoginUser = useSelector((state) =>
-    Boolean(state.metamask.authConnection),
-  );
+  const isSocialLoginUser = Boolean(useSelector(getSocialLoginType));
   const isBasicFunctionalityConsolidationEnabled = useSelector(
     getIsBasicFunctionalityConsolidationEnabled,
   );
