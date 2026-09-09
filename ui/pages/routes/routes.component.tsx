fec8f5cf808a3c1053dd03c4d8eaace117d5dda8
Pedro Brighenti <pedro.brighenti@consensys.net>
2026-09-09 16:54:26 +0100

    feat(deeplinks): trace processed and navigated deep links
    Instrument background parse/verify plus UI destination settlement, including unlock submit, so Sentry can measure intake-to-navigation without changing interstitial security.

--- ui/pages/routes/routes.component.tsx
+++ ui/pages/routes/routes.component.tsx
@@ -114,6 +114,7 @@ import { pageChanged } from '../../ducks/history/history';
 import { getCompletedOnboarding } from '../../ducks/metamask/metamask';
 import { getIsUnlocked } from '../../ducks/metamask/base-selectors';
 import { useI18nContext } from '../../hooks/useI18nContext';
+import { useDeepLinkNavigationTrace } from '../../hooks/useDeepLinkNavigationTrace';
 import RewardsPage from '../rewards';
 import Home from '../home/home.tsx';
 import { DEFAULT_AUTO_LOCK_TIME_LIMIT } from '../../../shared/constants/preferences';
@@ -792,6 +793,7 @@ export default function Routes() {
 
   useNavigateRouteListener();
   useBasicFunctionalityConsolidation();
+  useDeepLinkNavigationTrace();
 
   const isUsingRedesignedConfirmationType = useIsRedesignedConfirmationType();
 
