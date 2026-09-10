a346dfe1ee95a9ab069f760479f9e06ede11bb10
ameliejyc <amelie.chan@gmail.com>
2026-09-10 15:38:16 +0100

    fix: lint

--- app/scripts/services/legacy-background-api-service-method-action-types.ts
+++ app/scripts/services/legacy-background-api-service-method-action-types.ts
@@ -135,6 +135,14 @@ export type LegacyBackgroundApiServiceGetPhishingResultAction = {
 };
 
 /**
+ * Closes the notification popup window if one is open.
+ */
+export type LegacyBackgroundApiServiceCloseNotificationPopupAction = {
+  type: `LegacyBackgroundApiService:closeNotificationPopup`;
+  handler: LegacyBackgroundApiService['closeNotificationPopup'];
+};
+
+/**
  * Marks the notification popup as having been automatically closed.
  *
  * This lets us differentiate between the cases where we close the
@@ -1161,6 +1169,7 @@ export type LegacyBackgroundApiServiceMethodActions =
   | LegacyBackgroundApiServiceRequestSafeReloadAction
   | LegacyBackgroundApiServiceOpenUpdateTabAndReloadAction
   | LegacyBackgroundApiServiceGetPhishingResultAction
+  | LegacyBackgroundApiServiceCloseNotificationPopupAction
   | LegacyBackgroundApiServiceMarkNotificationPopupAsAutomaticallyClosedAction
   | LegacyBackgroundApiServiceMarkPasswordForgottenAction
   | LegacyBackgroundApiServiceUnMarkPasswordForgottenAction
