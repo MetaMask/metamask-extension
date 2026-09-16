c508ae0e346701f74e4f0d7ab5194d45c03cc8c3
John Whiles <john.whiles@consensys.net>
2026-09-16 14:41:29 +0100

    improve how we handle zero balances in money homepage widget

--- ui/pages/money/money-home-page.tsx
+++ ui/pages/money/money-home-page.tsx
@@ -37,7 +37,10 @@ import { useMoneyActivityItems } from '../../hooks/money/use-money-activity-item
 import { useMoneyActivityItemClick } from '../../hooks/money/use-money-activity-item-click';
 import { useMoneyAnalytics } from '../../hooks/money/useMoneyAnalytics';
 import { useTrackOnce } from '../../hooks/useTrackOnce';
-import { moneyFormatUsd } from '../../helpers/money/format';
+import {
+  isMoneyBalanceFunded,
+  moneyFormatUsd,
+} from '../../helpers/money/format';
 import { selectMoneyEarningSectionEnabled } from '../../selectors/money/money-account-feature-flags';
 import { getPrivacyMode } from '../../selectors/selectors';
 import { reportMoneyError } from '../../helpers/money/report-money-error';
@@ -71,7 +74,6 @@ import { MoneyTransferSheet } from './components/money-transfer-sheet';
  */
 const IS_MONEY_TRANSFER_SHEET_ENABLED: boolean = false;
 
-const MONEY_FUNDED_BALANCE_THRESHOLD = 0.01;
 const ACTION_BUTTON_ROW_BUTTON_COUNT = 2;
 const MONEY_ONBOARDING_ARTWORK = './images/money-onboarding-stepper-step-1.png';
 const FORMATTED_ZERO = moneyFormatUsd(new BigNumber(0));
@@ -155,8 +157,7 @@ export function MoneyHomePage() {
   const isMoneyEarningSectionEnabled = useSelector(
     selectMoneyEarningSectionEnabled,
   );
-  const isFunded =
-    tokenTotal?.abs().gte(MONEY_FUNDED_BALANCE_THRESHOLD) === true;
+  const isFunded = isMoneyBalanceFunded(tokenTotal);
   const { last30DaysQuery, sinceInceptionQuery } = useMoneyAccountInterest({
     enabled:
       availability.isAvailable && isMoneyEarningSectionEnabled && isFunded,
