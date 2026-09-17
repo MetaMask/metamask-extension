4bcfca9e28c08f0b701ce39d9403751d9bfd0f0d
Howard Braham <howrad@gmail.com>
2026-09-17 10:24:59 -0400

    `unexpectedError` is back

--- development/verify-locale-strings.js
+++ development/verify-locale-strings.js
@@ -314,6 +314,7 @@ async function verifyEnglishLocale() {
     'appDescription',
     'rejected',
     'signed',
+    'unexpectedError',
     // used via CSS
     'CSS_loadingTakingTooLongMessageText',
     'CSS_loadingTakingTooLongActionText',
