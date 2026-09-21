4cb9741da7d9cd90c752061b0b94d9323e552e05
gabrieledm <gabriele.delmonte-ext@consensys.net>
2026-09-21 16:18:09 +0200

    chore: eslint

--- ui/ducks/bridge/selectors.ts
+++ ui/ducks/bridge/selectors.ts
@@ -1069,10 +1069,10 @@ export const getQuoteRequestInsufficientBal = createSelector(
   (fromTokenBalance, validatedSrcAmount, insufficientNativeReserveError) =>
     Boolean(
       insufficientNativeReserveError ||
-        (validatedSrcAmount &&
-          fromTokenBalance &&
-          !Number.isNaN(Number(fromTokenBalance)) &&
-          new BigNumber(fromTokenBalance).lt(validatedSrcAmount)),
+      (validatedSrcAmount &&
+        fromTokenBalance &&
+        !Number.isNaN(Number(fromTokenBalance)) &&
+        new BigNumber(fromTokenBalance).lt(validatedSrcAmount)),
     ),
 );
 
@@ -1166,10 +1166,10 @@ export const computeQuoteValidationErrors = (
   const totalNetworkFeeAmount = getTotalNetworkFee(quote)?.normalizedAmount;
   const isNetworkFeeUnavailable = Boolean(
     quote &&
-      srcChainId &&
-      (isBitcoinChainId(srcChainId) || isTronChainId(srcChainId)) &&
-      !isGasless &&
-      new BigNumber(totalNetworkFeeAmount ?? '0').lte(0),
+    srcChainId &&
+    (isBitcoinChainId(srcChainId) || isTronChainId(srcChainId)) &&
+    !isGasless &&
+    new BigNumber(totalNetworkFeeAmount ?? '0').lte(0),
   );
 
   const priceImpactNumber = getPriceImpactNumber(quote);
@@ -1180,32 +1180,32 @@ export const computeQuoteValidationErrors = (
     // Shown prior to fetching quotes (native reserve error takes precedence)
     isInsufficientGasBalance: Boolean(
       nativeBalance &&
-        !quote &&
-        validatedSrcAmount &&
-        fromToken &&
-        !isGasless &&
-        (isNativeAddress(fromToken.assetId)
-          ? new BigNumber(nativeBalance)
-              .sub(minimumBalanceToKeep)
-              .lte(validatedSrcAmount)
-          : new BigNumber(nativeBalance).lte(0)),
+      !quote &&
+      validatedSrcAmount &&
+      fromToken &&
+      !isGasless &&
+      (isNativeAddress(fromToken.assetId)
+        ? new BigNumber(nativeBalance)
+            .sub(minimumBalanceToKeep)
+            .lte(validatedSrcAmount)
+        : new BigNumber(nativeBalance).lte(0)),
     ),
     isInsufficientNativeReserve,
     isNetworkFeeUnavailable,
     // Shown after fetching quotes
     isInsufficientGasForQuote: Boolean(
       !isNetworkFeeUnavailable &&
-        nativeBalance &&
-        quote &&
-        fromToken &&
-        fromTokenInputValue &&
-        !isGasless &&
-        isNativeBalanceInsufficientForQuote(
-          quote,
-          nativeBalance,
-          fromToken.assetId,
-          minimumBalanceToKeep,
-        ),
+      nativeBalance &&
+      quote &&
+      fromToken &&
+      fromTokenInputValue &&
+      !isGasless &&
+      isNativeBalanceInsufficientForQuote(
+        quote,
+        nativeBalance,
+        fromToken.assetId,
+        minimumBalanceToKeep,
+      ),
     ),
     isInsufficientBalance:
       validatedSrcAmount &&
@@ -1225,8 +1225,8 @@ export const computeQuoteValidationErrors = (
         : false,
     isPriceImpactWarning: Boolean(
       priceImpactNumber &&
-        priceImpactNumber > warning &&
-        priceImpactNumber <= error,
+      priceImpactNumber > warning &&
+      priceImpactNumber <= error,
     ),
     isPriceImpactError: Boolean(priceImpactNumber && priceImpactNumber > error),
   };
@@ -1372,10 +1372,10 @@ const _getBaseValidationErrors = createDeepEqualSelector(
         quoteStreamCompleteData?.hasQuotes === false ||
         Boolean(
           !activeQuote &&
-            isValidQuoteRequest(quoteRequest) &&
-            quotesLastFetchedMs &&
-            !isLoading &&
-            quotesRefreshCount > 0,
+          isValidQuoteRequest(quoteRequest) &&
+          quotesLastFetchedMs &&
+          !isLoading &&
+          quotesRefreshCount > 0,
         ),
       isDestAssetRequireActivate,
     };
