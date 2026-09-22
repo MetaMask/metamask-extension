5ca0f0c29f4bc2da5013c2cdaa00911a2a3dc76b
Frederik Bolding <frederik.bolding@gmail.com>
2026-09-22 12:04:38 +0200

    fix: Increase Snaps idle timeout

--- app/scripts/messenger-client-init/snaps/snap-controller-init.ts
+++ app/scripts/messenger-client-init/snaps/snap-controller-init.ts
@@ -2,7 +2,7 @@ import {
   SnapController,
   SnapControllerMessenger,
 } from '@metamask/snaps-controllers';
-import { createDeferredPromise, Json } from '@metamask/utils';
+import { createDeferredPromise, Duration, inMilliseconds } from '@metamask/utils';
 import { MessengerClientInitFunction } from '../types';
 import {
   EndowmentPermissions,
@@ -104,6 +104,7 @@ export const SnapControllerInit: MessengerClientInitFunction<
     state: persistedState.SnapController,
 
     messenger: controllerMessenger,
+    maxIdleTime: inMilliseconds(2, Duration.Minute),
     featureFlags: {
       allowLocalSnaps,
       requireAllowlist,
