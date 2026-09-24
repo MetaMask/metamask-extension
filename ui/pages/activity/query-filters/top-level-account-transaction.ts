6aa54e65e22827df523700973380499006cfb072
Francis Nepomuceno <n3ps@users.noreply.github.com>
2026-09-23 20:45:20 -0400

    add accountid to top level filter

--- ui/pages/activity/query-filters/top-level-account-transaction.ts
+++ ui/pages/activity/query-filters/top-level-account-transaction.ts
@@ -1,12 +1,28 @@
 import type { V1TransactionByHashResponse } from '@metamask/core-backend';
+import {
+  isCaipAccountId,
+  parseCaipAccountId,
+  type CaipAccountId,
+} from '@metamask/utils';
 import { isEqualCaseInsensitive as equalsIgnoreCase } from '../../../../shared/lib/string-utils';
+
+type TransactionWithAccountId = V1TransactionByHashResponse & {
+  accountId?: CaipAccountId;
+};
 
 export function isTopLevelAccountTransaction(
   transaction: V1TransactionByHashResponse,
   subjectAddress: string,
 ) {
+  const { accountId } = transaction as TransactionWithAccountId;
+  const accountAddress =
+    accountId && isCaipAccountId(accountId)
+      ? parseCaipAccountId(accountId).address
+      : '';
+
   return (
     equalsIgnoreCase(transaction.from, subjectAddress) ||
-    equalsIgnoreCase(transaction.to, subjectAddress)
+    equalsIgnoreCase(transaction.to, subjectAddress) ||
+    equalsIgnoreCase(accountAddress, subjectAddress)
   );
 }
